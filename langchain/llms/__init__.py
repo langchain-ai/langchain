014f43cacc983b928061890a8cb151848e221a5b
Harrison Chase <hw.chase.17@gmail.com>
2022-10-25 22:00:33 -0700

    Harrison/add huggingface hub (#23)
    Add support for huggingface hub
    
    I could not find a good way to enforce stop tokens over the huggingface
    hub api - that needs to hopefully be cleaned up in the future

--- langchain/llms/__init__.py
+++ langchain/llms/__init__.py
@@ -1,5 +1,6 @@
 """Wrappers on top of large language models APIs."""
 from langchain.llms.cohere import Cohere
+from langchain.llms.huggingface_hub import HuggingFaceHub
 from langchain.llms.openai import OpenAI
 
-__all__ = ["Cohere", "OpenAI"]
+__all__ = ["Cohere", "OpenAI", "HuggingFaceHub"]
