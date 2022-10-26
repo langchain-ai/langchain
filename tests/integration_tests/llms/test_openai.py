314ee7fbc4ea077f4e844e5aca84d3cb727c982a
Harrison Chase <hw.chase.17@gmail.com>
2022-10-25 20:22:16 -0700

    fix test name (#22)

--- tests/integration_tests/llms/test_openai.py
+++ tests/integration_tests/llms/test_openai.py
@@ -3,8 +3,8 @@
 from langchain.llms.openai import OpenAI
 
 
-def test_cohere_call() -> None:
-    """Test valid call to cohere."""
+def test_openai_call() -> None:
+    """Test valid call to openai."""
     llm = OpenAI(max_tokens=10)
     output = llm("Say foo:")
     assert isinstance(output, str)
