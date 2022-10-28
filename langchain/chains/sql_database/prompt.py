af81e9ca9c54c5039368ca11f5f63007f2ad04b0
Harrison Chase <hw.chase.17@gmail.com>
2022-10-27 23:21:47 -0700

    add sql database (#35)

---
+++ langchain/chains/sql_database/prompt.py
@@ -0,0 +1,20 @@
+# flake8: noqa
+from langchain.prompt import Prompt
+
+_DEFAULT_TEMPLATE = """Given an input question, first create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer. 
+Use the following format:
+
+Question: "Question here"
+SQLQuery: "SQL Query to run"
+SQLResult: "Result of the SQLQuery"
+Answer: "Final answer here"
+
+Only use the following tables:
+
+{table_info}
+
+Question: {input}"""
+PROMPT = Prompt(
+    input_variables=["input", "table_info", "dialect"],
+    template=_DEFAULT_TEMPLATE,
+)
