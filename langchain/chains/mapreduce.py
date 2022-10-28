fd9738305cd89b850929ed0aeebcf53ea014ba8f
Harrison Chase <hw.chase.17@gmail.com>
2022-10-27 18:29:29 -0700

    cr

--- langchain/chains/mapreduce.py
+++ langchain/chains/mapreduce.py
@@ -1,4 +1,8 @@
-"""Map-reduce chain."""
+"""Map-reduce chain.
+
+Splits up a document, sends the smaller parts to the LLM with one prompt,
+then combines the results with another one.
+"""
 
 from typing import Dict, List
 
@@ -10,7 +14,31 @@ from langchain.llms.base import LLM
 from langchain.prompt import Prompt
 
 
+def split_text(
+    text: str, separator: str, chunk_size: int, chunk_overlap: int
+) -> List[str]:
+    """Split text into multiple components."""
+    # First we naively split the large input into a bunch of smaller ones.
+    splits = text.split(separator)
+    # We now want to combine these smaller pieces into medium size
+    # chunks to send to the LLM.
+    docs = []
+    current_doc: List[str] = []
+    total = 0
+    for d in splits:
+        if total > chunk_size:
+            docs.append(separator.join(current_doc))
+            while total > chunk_overlap:
+                total -= len(current_doc[0])
+                current_doc = current_doc[1:]
+        current_doc.append(d)
+        total += len(d)
+    docs.append(separator.join(current_doc))
+    return docs
+
+
 class MapReduceChain(Chain, BaseModel):
+    """Map-reduce chain."""
 
     llm: LLM
     """LLM wrapper to use."""
@@ -48,23 +76,12 @@ class MapReduceChain(Chain, BaseModel):
     def _run(self, inputs: Dict[str, str]) -> Dict[str, str]:
         map_llm = LLMChain(llm=self.llm, prompt=self.map_prompt)
         reduce_llm = LLMChain(llm=self.llm, prompt=self.reduce_prompt)
-        # First we naively split the large input into a bunch of smaller ones.
-        splits = inputs[self.input_key].split(self.separator)
-        # We now want to combine these smaller pieces into medium size
-        # chunks to send to the LLM. This is the "map" part.
-        docs = []
-        current_doc: List[str] = []
-        total = 0
-        for d in splits:
-            if total > self.chunk_size:
-                docs.append(self.separator.join(current_doc))
-                while total > self.chunk_overlap:
-                    total -= len(current_doc[0])
-                    current_doc = current_doc[1:]
-            current_doc.append(d)
-            total += len(d)
-        docs.append(self.separator.join(current_doc))
+        # Split the larger text into smaller chunks.
+        docs = split_text(
+            inputs[self.input_key], self.separator, self.chunk_size, self.chunk_overlap
+        )
         # Now that we have the chunks, we send them to the LLM and track results.
+        #  This is the "map" part.
         summaries = []
         for d in docs:
             inputs = {self.map_prompt.input_variables[0]: d}
@@ -81,4 +98,5 @@ class MapReduceChain(Chain, BaseModel):
         return {self.output_key: output}
 
     def run(self, text: str) -> str:
+        """Run the map-reduce logic on the input text."""
         return self({self.input_key: text})[self.output_key]
