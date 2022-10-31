a672f876d2671b270bb4e748d9a1a5820c2e51bd
Harrison Chase <hw.chase.17@gmail.com>
2022-10-30 22:39:06 -0700

    stash

--- langchain/docstore/base.py
+++ langchain/docstore/base.py
@@ -1,6 +1,6 @@
 """Interface to access to place that stores documents."""
 from abc import ABC, abstractmethod
-from typing import Optional, Tuple
+from typing import Optional, Tuple, List
 
 from langchain.docstore.document import Document
 
@@ -10,8 +10,19 @@ class Docstore(ABC):
 
     @abstractmethod
     def search(self, search: str) -> Tuple[str, Optional[Document]]:
-        """Search for document.
+        """Search for specific document.
 
-        If page exists, return the page summary, and a Document object.
-        If page does not exist, return similar entries.
+        If page exists, return a Document object.
+        If page does not exist, return a string explaining the error.
         """
+
+    def similarity_search(self, search: str) -> List[Document]:
+        """Search for documents similar to the query.
+
+        By default, just call the generic search method.
+        """
+        _, document = self.search(search)
+        if document is not None:
+            return [document]
+        else:
+            return []
