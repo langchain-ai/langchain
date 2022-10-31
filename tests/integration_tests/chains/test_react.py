f6630ea11103f014edd9dabda70b5953ec056997
Harrison Chase <hw.chase.17@gmail.com>
2022-10-31 08:33:24 -0700

    stash

--- tests/integration_tests/chains/test_react.py
+++ tests/integration_tests/chains/test_react.py
@@ -2,12 +2,13 @@
 
 from langchain.chains.react.base import ReActChain
 from langchain.llms.openai import OpenAI
+from langchain.docstore.wikipedia import Wikipedia
 
 
 def test_react() -> None:
     """Test functionality on a prompt."""
     llm = OpenAI(temperature=0)
-    react = ReActChain(llm=llm)
+    react = ReActChain(llm=llm, docstore=Wikipedia())
     question = "Were Scott Derrickson and Ed Wood of the same nationality?"
     output = react.run(question)
     assert output == "yes"
