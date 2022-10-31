e72f17060f98c6a6ce5b12a63950295c9c127a9c
Harrison Chase <hw.chase.17@gmail.com>
2022-10-31 08:45:48 -0700

    update docstore

--- tests/integration_tests/chains/test_react.py
+++ tests/integration_tests/chains/test_react.py
@@ -1,14 +1,18 @@
 """Integration test for self ask with search."""
 
 from langchain.chains.react.base import ReActChain
-from langchain.llms.openai import OpenAI
 from langchain.docstore.wikipedia import Wikipedia
+from langchain.llms.openai import OpenAI
 
 
 def test_react() -> None:
     """Test functionality on a prompt."""
     llm = OpenAI(temperature=0)
     react = ReActChain(llm=llm, docstore=Wikipedia())
-    question = "Were Scott Derrickson and Ed Wood of the same nationality?"
+    question = (
+        "Author David Chanoff has collaborated with a U.S. Navy admiral "
+        "who served as the ambassador to the United Kingdom under "
+        "which President?"
+    )
     output = react.run(question)
-    assert output == "yes"
+    assert output == "Bill Clinton"
