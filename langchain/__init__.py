2456a547de12f5777397b49c88d4418fb3f62a59
Harrison Chase <hw.chase.17@gmail.com>
2022-11-05 14:41:53 -0700

    mrkl (#42)

--- langchain/__init__.py
+++ langchain/__init__.py
@@ -8,6 +8,7 @@ with open(Path(__file__).absolute().parents[0] / "VERSION") as _f:
 from langchain.chains import (
     LLMChain,
     LLMMathChain,
+    MRKLChain,
     PythonChain,
     ReActChain,
     SelfAskWithSearchChain,
@@ -37,4 +38,5 @@ __all__ = [
     "SQLDatabase",
     "SQLDatabaseChain",
     "FAISS",
+    "MRKLChain",
 ]
---
+++ langchain/chains/mrkl/prompt.py
@@ -0,0 +1,19 @@
+# flake8: noqa
+BASE_TEMPLATE = """Answer the following questions as best you can. You have access to the following tools:
+
+{tools}
+
+Use the following format:
+
+Question: the input question you must answer
+Thought: you should always think about what to do
+Action: the action to take, should be one of [{tool_names}]
+Action Input: the input to the action
+Observation: the result of the action
+... (this Thought/Action/Action Input/Observation can repeat N times)
+Thought: I now know the final answer
+Final Answer: the final answer to the original input question
+
+Begin!
+
+Question: {{input}}"""
