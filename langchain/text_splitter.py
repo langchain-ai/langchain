b7cc412a9b40dff83e8b8f76361f81c70032e6de
Harrison Chase <hw.chase.17@gmail.com>
2022-11-06 16:02:11 -0800

    stash

--- langchain/text_splitter.py
+++ langchain/text_splitter.py
@@ -1,6 +1,6 @@
 """Functionality for splitting text."""
 from abc import abstractmethod
-from typing import List
+from typing import List, Any
 
 
 class TextSplitter:
@@ -11,8 +11,9 @@ class TextSplitter:
         """Split text into multiple components."""
 
 
-class CharacterTextSplitter(TextSplitter):
-    """Implementation of splitting text that looks at characters."""
+
+from abc import ABC, abstractmethod
+class IterativeTextSplitter(TextSplitter, ABC):
 
     def __init__(
         self, separator: str = "\n\n", chunk_size: int = 4000, chunk_overlap: int = 200
@@ -26,6 +27,10 @@ class CharacterTextSplitter(TextSplitter):
         self._separator = separator
         self._chunk_size = chunk_size
         self._chunk_overlap = chunk_overlap
+
+    @abstractmethod
+    def _get_chunk_size(self, text: str) -> int:
+        """Return the size of this chunk of text."""
 
     def split_text(self, text: str) -> List[str]:
         """Split incoming text and return chunks."""
@@ -46,3 +51,28 @@ class CharacterTextSplitter(TextSplitter):
             total += len(d)
         docs.append(self._separator.join(current_doc))
         return docs
+
+
+class CharacterTextSplitter(IterativeTextSplitter):
+    """Implementation of splitting text that looks at characters."""
+
+    def _get_chunk_size(self, text: str) -> int:
+        return len(text)
+
+class HuggingFaceTokenizerSplitter(IterativeTextSplitter):
+    def __init__(
+        self, tokenizer: Any, separator: str = "\n\n", chunk_size: int = 4000, chunk_overlap: int = 200
+    ):
+        """Initialize with parameters."""
+        try:
+            from transformers import PreTrainedTokenizerBase
+            if not isinstance(tokenizer, PreTrainedTokenizerBase):
+                raise ValueError
+
+            self.tokenizer = tokenizer
+        except ImportError:
+            raise ValueError
+        super().__init__(separator=separator, chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+
+    def _get_chunk_size(self, text: str) -> int:
+        return len(self.tokenizer.encode(text))
