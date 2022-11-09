ee0640ed960689a1192c424ca518a49df50e553b
abderrahmane issam <abderrahmanissam285@gmail.com>
2022-11-09 20:53:12 +0100

    Add hf and cohere to embeddings init all

--- langchain/embeddings/__init__.py
+++ langchain/embeddings/__init__.py
@@ -1,4 +1,6 @@
 """Wrappers around embedding modules."""
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from langchain.embeddings.cohere import CohereEmbeddings
 
-__all__ = ["OpenAIEmbeddings"]
+__all__ = ["OpenAIEmbeddings", "HuggingFaceEmbeddings", "CohereEmbeddings"]
