567592e5c26839771ed6f07cce422159fc8d9b90
abderrahmane issam <abderrahmanissam285@gmail.com>
2022-11-09 21:05:15 +0100

    Run make format

--- langchain/embeddings/__init__.py
+++ langchain/embeddings/__init__.py
@@ -1,6 +1,6 @@
 """Wrappers around embedding modules."""
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 from langchain.embeddings.cohere import CohereEmbeddings
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from langchain.embeddings.openai import OpenAIEmbeddings
 
 __all__ = ["OpenAIEmbeddings", "HuggingFaceEmbeddings", "CohereEmbeddings"]
