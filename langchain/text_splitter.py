ce0444c95064a0bff132f25cd8a4cd049c35a7ed
Delip Rao <deliprao@gmail.com>
2022-11-09 13:20:05 -0500

    Add classes for NLTK and Spacy

--- langchain/text_splitter.py
+++ langchain/text_splitter.py
@@ -6,31 +6,20 @@ from typing import List
 class TextSplitter:
     """Interface for splitting text into chunks."""
 
-    @abstractmethod
-    def split_text(self, text: str) -> List[str]:
-        """Split text into multiple components."""
-
-
-class CharacterTextSplitter(TextSplitter):
-    """Implementation of splitting text that looks at characters."""
-
-    def __init__(
-        self, separator: str = "\n\n", chunk_size: int = 4000, chunk_overlap: int = 200
-    ):
-        """Initialize with parameters."""
+    def __init__(self, chunk_size, chunk_overlap):
         if chunk_overlap > chunk_size:
             raise ValueError(
                 f"Got a larger chunk overlap ({chunk_overlap}) than chunk size "
                 f"({chunk_size}), should be smaller."
             )
-        self._separator = separator
         self._chunk_size = chunk_size
         self._chunk_overlap = chunk_overlap
 
+    @abstractmethod
     def split_text(self, text: str) -> List[str]:
-        """Split incoming text and return chunks."""
-        # First we naively split the large input into a bunch of smaller ones.
-        splits = text.split(self._separator)
+        """Split text into multiple components."""
+
+    def _merge_splits(self, splits: List[str]) -> List[str]:
         # We now want to combine these smaller pieces into medium size
         # chunks to send to the LLM.
         docs = []
@@ -46,3 +35,58 @@ class CharacterTextSplitter(TextSplitter):
             total += len(d)
         docs.append(self._separator.join(current_doc))
         return docs
+
+
+class CharacterTextSplitter(TextSplitter):
+    """Implementation of splitting text that looks at characters."""
+
+    def __init__(
+        self, separator: str = "\n\n", chunk_size: int = 4000, chunk_overlap: int = 200
+    ):
+        super(CharacterTextSplitter, self).__init__(chunk_size, chunk_overlap)
+        self._separator = separator
+
+    def split_text(self, text: str) -> List[str]:
+        """Split incoming text and return chunks."""
+        # First we naively split the large input into a bunch of smaller ones.
+        splits = text.split(self._separator)
+        return self._merge_splits(splits)
+
+
+class NLTKTextSplitter(TextSplitter):
+    def __init__(chunk_size: int = 4000, chunk_overlap: int = 200):
+        super(NLTKTextSplitter, self).__init__(chunk_size, chunk_overlap)
+        try:
+            from nltk.tokenize import sent_tokenize
+
+            self._tokenizer = sent_tokenize
+        except ImportError:
+            raise ImportError(
+                "NLTK is not installed, please install it with " "`pip install nltk`."
+            )
+
+    def split_text(self, text: str) -> List[str]:
+        """Split incoming text and return chunks."""
+        # First we naively split the large input into a bunch of smaller ones.
+        splits = self._tokenizer(text)
+        return self._merge_splits(splits)
+
+
+class SpacyTextSplitter(TextSplitter):
+    def __init__(
+        pipeline="en_core_web_sm", chunk_size: int = 4000, chunk_overlap: int = 200
+    ):
+        super(SpacyTextSplitter, self).__init__(chunk_size, chunk_overlap)
+        try:
+            import spacy
+
+            self._tokenizer = spacy.load(pipeline)
+        except ImportError:
+            raise ImportError(
+                "Spacy is not installed, please install it with " "`pip install spacy`."
+            )
+
+    def split_text(self, text: str) -> List[str]:
+        """Split incoming text and return chunks."""
+        splits = (str(s) for s in self._tokenizer(text).sents)
+        return self._merge_splits(self._tokenizer(text).sents)
