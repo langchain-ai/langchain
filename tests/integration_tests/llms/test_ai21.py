206f448831f7c017cf784ebb9c4d1ef05eb51a20
Tomer Asida <tomera@ai21.com>
2022-11-10 12:31:01 +0200

    use default model in AI21 integration test

--- tests/integration_tests/llms/test_ai21.py
+++ tests/integration_tests/llms/test_ai21.py
@@ -5,6 +5,6 @@ from langchain.llms.ai21 import AI21
 
 def test_ai21_call() -> None:
     """Test valid call to ai21."""
-    llm = AI21(model='j1-large', maxTokens=10)
+    llm = AI21(maxTokens=10)
     output = llm("Say foo:")
     assert isinstance(output, str)
