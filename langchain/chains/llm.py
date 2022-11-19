f689971505973deb450c7788cae49ccc18a598ca
Harrison Chase <hw.chase.17@gmail.com>
2022-11-19 07:33:42 -0800

    add verbose option to prompt

--- langchain/chains/llm.py
+++ langchain/chains/llm.py
@@ -6,6 +6,7 @@ from pydantic import BaseModel, Extra
 from langchain.chains.base import Chain
 from langchain.llms.base import LLM
 from langchain.prompts.base import BasePrompt
+from langchain.input import print_text
 
 
 class LLMChain(Chain, BaseModel):
@@ -51,7 +52,9 @@ class LLMChain(Chain, BaseModel):
     def _call(self, inputs: Dict[str, Any]) -> Dict[str, str]:
         selected_inputs = {k: inputs[k] for k in self.prompt.input_variables}
         prompt = self.prompt.format(**selected_inputs)
-
+        if self.verbose:
+            print("Prompt after formatting:")
+            print_text(prompt, color="green", end="\n")
         kwargs = {}
         if "stop" in inputs:
             kwargs["stop"] = inputs["stop"]
