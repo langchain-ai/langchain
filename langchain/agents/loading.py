5d887970f6939476d065c0e1b5a9e99ba1dbfbe9
Harrison Chase <hw.chase.17@gmail.com>
2022-11-22 18:02:20 -0800

    change to agent (#173)

--- langchain/agents/loading.py
+++ langchain/agents/loading.py
@@ -8,7 +8,7 @@ from langchain.agents.self_ask_with_search.base import SelfAskWithSearchAgent
 from langchain.agents.tools import Tool
 from langchain.llms.base import LLM
 
-AGENT_TYPE_TO_CLASS = {
+AGENT_TO_CLASS = {
     "zero-shot-react-description": ZeroShotAgent,
     "react-docstore": ReActDocstoreAgent,
     "self-ask-with-search": SelfAskWithSearchAgent,
@@ -18,7 +18,7 @@ AGENT_TYPE_TO_CLASS = {
 def initialize_agent(
     tools: List[Tool],
     llm: LLM,
-    agent_type: str = "zero-shot-react-description",
+    agent: str = "zero-shot-react-description",
     **kwargs: Any,
 ) -> Agent:
     """Load agent given tools and LLM.
@@ -26,17 +26,17 @@ def initialize_agent(
     Args:
         tools: List of tools this agent has access to.
         llm: Language model to use as the agent.
-        agent_type: The agent to use. Valid options are:
+        agent: The agent to use. Valid options are:
             `zero-shot-react-description`, `react-docstore`, `self-ask-with-search`.
         **kwargs: Additional key word arguments to pass to the agent.
 
     Returns:
         An agent.
     """
-    if agent_type not in AGENT_TYPE_TO_CLASS:
+    if agent not in AGENT_TO_CLASS:
         raise ValueError(
-            f"Got unknown agent type: {agent_type}. "
-            f"Valid types are: {AGENT_TYPE_TO_CLASS.keys()}."
+            f"Got unknown agent type: {agent}. "
+            f"Valid types are: {AGENT_TO_CLASS.keys()}."
         )
-    agent_cls = AGENT_TYPE_TO_CLASS[agent_type]
+    agent_cls = AGENT_TO_CLASS[agent]
     return agent_cls.from_llm_and_tools(llm, tools, **kwargs)
