65e5fac57e36b4e803934b28ce5a5ff3cbe72d1c
bagatur <baskaryan@gmail.com>
2022-11-25 12:35:50 -0800

    explicit task

--- tests/integration_tests/embeddings/test_huggingface_hub.py
+++ tests/integration_tests/embeddings/test_huggingface_hub.py
@@ -5,7 +5,7 @@ from langchain.embeddings import HuggingFaceHubEmbeddings
 def test_huggingfacehub_embedding_documents() -> None:
     """Test huggingfacehub embeddings."""
     documents = ["foo bar"]
-    embedding = HuggingFaceHubEmbeddings()
+    embedding = HuggingFaceHubEmbeddings(task="feature-extraction")
     output = embedding.embed_documents(documents)
     assert len(output) == 1
     assert len(output[0]) == 768
@@ -14,6 +14,6 @@ def test_huggingfacehub_embedding_documents() -> None:
 def test_huggingfacehub_embedding_query() -> None:
     """Test huggingfacehub embeddings."""
     document = "foo bar"
-    embedding = HuggingFaceHubEmbeddings()
+    embedding = HuggingFaceHubEmbeddings(task="feature-extraction")
     output = embedding.embed_query(document)
     assert len(output) == 768
