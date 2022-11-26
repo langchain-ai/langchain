6eab5254e5a5c422ea0c2e84e7dbd2022876991d
Harrison Chase <hw.chase.17@gmail.com>
2022-11-26 06:03:08 -0800

    add docs for custom agents (#196)

--- langchain/agents/__init__.py
+++ langchain/agents/__init__.py
@@ -1,7 +1,7 @@
 """Routing chains."""
 from langchain.agents.agent import Agent
 from langchain.agents.loading import initialize_agent
-from langchain.agents.mrkl.base import MRKLChain
+from langchain.agents.mrkl.base import MRKLChain, ZeroShotAgent
 from langchain.agents.react.base import ReActChain
 from langchain.agents.self_ask_with_search.base import SelfAskWithSearchChain
 from langchain.agents.tools import Tool
@@ -13,4 +13,5 @@ __all__ = [
     "Agent",
     "Tool",
     "initialize_agent",
+    "ZeroShotAgent",
 ]
