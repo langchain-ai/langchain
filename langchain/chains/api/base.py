55a2ba681b6b570316c417de33dffd07d64e8187
thecooltechguy <subby@galiboo.com>
2022-12-01 18:13:01 -0800

    passed linting and added kwargs

--- langchain/chains/api/base.py
+++ langchain/chains/api/base.py
@@ -1,5 +1,5 @@
 from langchain import LLMChain
-from typing import Optional, List
+from typing import Optional, List, Any, Dict
 from pydantic import BaseModel
 from langchain.chains.base import Chain
 from langchain.chains.api.prompt import API_URL_PROMPT, API_RESPONSE_PROMPT
@@ -30,7 +30,7 @@ class APIChain(Chain, BaseModel):
     """Output keys this chain expects."""
     return  [self.output_key]
 
-  def _call(self, inputs):
+  def _call(self, inputs : Dict[str, str]) -> Dict[str, str]:
       question = inputs[self.question_key]
       api_url = self.api_request_chain.predict(question=question,api_docs=self.api_docs)
       api_response = self.requests_chain.run(api_url)
@@ -38,8 +38,8 @@ class APIChain(Chain, BaseModel):
       return {self.output_key: answer}
 
   @classmethod
-  def from_llm_and_api_docs(cls, llm: LLM, api_docs: str):
+  def from_llm_and_api_docs(cls, llm: LLM, api_docs: str, **kwargs : Any) -> "APIChain":
     get_request_chain = LLMChain(llm=llm, prompt=API_URL_PROMPT)
     requests_wrapper = RequestsChain()
     get_answer_chain = LLMChain(llm=llm, prompt=API_RESPONSE_PROMPT)
-    return cls(request_chain=get_request_chain, answer_chain=get_answer_chain, requests_wrapper=requests_wrapper, api_docs=api_docs)
+    return cls(request_chain=get_request_chain, answer_chain=get_answer_chain, requests_wrapper=requests_wrapper, api_docs=api_docs, **kwargs)
