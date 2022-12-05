d8c6c2aa192e13142f4bade5b5683742dc199b82
Harrison Chase <hw.chase.17@gmail.com>
2022-12-05 10:52:13 -0800

    cr

--- langchain/chains/__init__.py
+++ langchain/chains/__init__.py
@@ -3,13 +3,13 @@ from langchain.chains.api.base import APIChain
 from langchain.chains.conversation.base import ConversationChain
 from langchain.chains.llm import LLMChain
 from langchain.chains.llm_math.base import LLMMathChain
+from langchain.chains.llm_search.base import LLMSearchChain
 from langchain.chains.pal.base import PALChain
 from langchain.chains.qa_with_sources.base import QAWithSourcesChain
 from langchain.chains.qa_with_sources.vector_db import VectorDBQAWithSourcesChain
 from langchain.chains.sequential import SequentialChain, SimpleSequentialChain
 from langchain.chains.sql_database.base import SQLDatabaseChain
 from langchain.chains.vector_db_qa.base import VectorDBQA
-from langchain.chains.llm_search.base import LLMSearchChain
 
 __all__ = [
     "LLMChain",
