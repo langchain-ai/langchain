7c95f41d2719d9755aff5860606f447c176c2004
Harrison Chase <hw.chase.17@gmail.com>
2022-12-04 17:44:40 -0800

    Harrison/version 0028 (#259)

--- langchain/__init__.py
+++ langchain/__init__.py
@@ -1,10 +1,5 @@
 """Main entrypoint into package."""
 
-from pathlib import Path
-
-with open(Path(__file__).absolute().parents[0] / "VERSION") as _f:
-    __version__ = _f.read().strip()
-
 from langchain.agents import MRKLChain, ReActChain, SelfAskWithSearchChain
 from langchain.chains import (
     ConversationChain,
