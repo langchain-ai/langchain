0e20cd4f2d739453cd5604e64c3e7d97791fbad6
Harrison Chase <hw.chase.17@gmail.com>
2022-12-05 19:11:41 -0800

    add summary notebook

--- langchain/chains/summarize/refine_prompt.py
+++ langchain/chains/summarize/refine_prompt.py
@@ -7,14 +7,14 @@ REFINE_PROMPT_TMPL = (
     "We have the opportunity to refine the existing summary"
     "(only if needed) with some more context below.\n"
     "------------\n"
-    "{context_str}\n"
+    "{text}\n"
     "------------\n"
     "Given the new context, refine the original summary"
     "If the context isn't useful, return the original summary."
 )
 REFINE_PROMPT = PromptTemplate(
-    input_variables=["query_str", "existing_answer", "context_str"],
-    template=DEFAULT_REFINE_PROMPT_TMPL,
+    input_variables=["existing_answer", "text"],
+    template=REFINE_PROMPT_TMPL,
 )
 
 
