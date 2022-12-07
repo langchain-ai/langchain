c080d23b2d01825ec493e50f7936ee7c03b1548b
Harrison Chase <hw.chase.17@gmail.com>
2022-12-06 22:26:26 -0800

    cr

--- tests/unit_tests/test_bash.py
+++ tests/unit_tests/test_bash.py
@@ -1,22 +1,25 @@
+"""Test the bash utility."""
 import subprocess
 from pathlib import Path
 
 from langchain.utilities.bash import BashProcess
+
 
 def test_pwd_command() -> None:
     """Test correct functionality."""
     session = BashProcess()
     commands = ["pwd"]
     output = session.run(commands)
-    print(output)
 
-    assert output["outputs"] == [subprocess.check_output("pwd", shell=True).decode()]
+    assert output == subprocess.check_output("pwd", shell=True).decode()
+
 
 def test_incorrect_command() -> None:
     """Test handling of incorrect command."""
     session = BashProcess()
     output = session.run(["invalid_command"])
-    assert output["success"] is False
+    assert output == "Command 'invalid_command' returned non-zero exit status 127."
+
 
 def test_create_directory_and_files(tmp_path: Path) -> None:
     """Test creation of a directory and files in a temporary directory."""
@@ -31,14 +34,12 @@ def test_create_directory_and_files(tmp_path: Path) -> None:
         f"touch {temp_dir}/file1.txt",
         f"touch {temp_dir}/file2.txt",
         f"echo 'hello world' > {temp_dir}/file2.txt",
-        f"cat {temp_dir}/file2.txt"
+        f"cat {temp_dir}/file2.txt",
     ]
 
     output = session.run(commands)
-    assert output["success"] is True
-    assert output["outputs"][-1] == "hello world"
+    assert output == "hello world"
 
     # check that the files were created in the temporary directory
     output = session.run([f"ls {temp_dir}"])
-    assert output["success"] is True
-    assert output["outputs"] == ["file1.txt\nfile2.txt"]
+    assert output == "file1.txt\nfile2.txt"
