834b39179246eae8e8c75cb9b89921b83a465b51
Harrison Chase <hw.chase.17@gmail.com>
2022-12-06 22:55:27 -0800

    update notebooks (#275)

--- langchain/chains/__init__.py
+++ langchain/chains/__init__.py
@@ -10,6 +10,7 @@ from langchain.chains.qa_with_sources.base import QAWithSourcesChain
 from langchain.chains.qa_with_sources.vector_db import VectorDBQAWithSourcesChain
 from langchain.chains.sequential import SequentialChain, SimpleSequentialChain
 from langchain.chains.sql_database.base import SQLDatabaseChain
+from langchain.chains.transform import TransformChain
 from langchain.chains.vector_db_qa.base import VectorDBQA
 
 __all__ = [
@@ -27,4 +28,5 @@ __all__ = [
     "VectorDBQAWithSourcesChain",
     "APIChain",
     "LLMRequestsChain",
+    "TransformChain",
 ]
