97e2551b2d45b656145c1d8a6f90c28360471096
Harrison Chase <hw.chase.17@gmail.com>
2022-12-11 08:49:09 -0800

    rfc for return stuff

--- langchain/llms/base.py
+++ langchain/llms/base.py
@@ -1,14 +1,26 @@
 """Base interface for large language models to expose."""
 from abc import ABC, abstractmethod
-from typing import Any, List, Mapping, Optional
+from typing import Any, List, Mapping, Optional, NamedTuple
+
+
+class LLMResult(NamedTuple):
+    """Class that contains all relevant information for an LLM Result."""
+    result: str
+    log_probs: List[float]
+    llm_output: dict
+    """For arbitrary LLM provider specific output."""
+
 
 
 class LLM(ABC):
     """LLM wrapper should take in a prompt and return a string."""
 
-    @abstractmethod
+    def generate(self, prompt: str, stop: Optional[List[str]] = None) -> LLMResult:
+        """Run the LLM on the given prompt and input."""
+
     def __call__(self, prompt: str, stop: Optional[List[str]] = None) -> str:
         """Run the LLM on the given prompt and input."""
+        return self.generate(prompt, stop=stop).result
 
     @property
     def _identifying_params(self) -> Mapping[str, Any]:
