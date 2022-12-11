19a9fa16a9df812f04f5b7dd9359b0f7b2a0435b
Shobith Alva <shobith@users.noreply.github.com>
2022-12-11 07:09:06 -0800

    Add `clear()` method for `Memory` (#305)
    a simple helper to clear the buffer in `Conversation*Memory` classes

--- langchain/chains/base.py
+++ langchain/chains/base.py
@@ -29,6 +29,10 @@ class Memory(BaseModel, ABC):
     def save_context(self, inputs: Dict[str, Any], outputs: Dict[str, str]) -> None:
         """Save the context of this model run to memory."""
 
+    @abstractmethod
+    def clear(self) -> None:
+        """Clear memory contents."""
+
 
 def _get_verbosity() -> bool:
     return langchain.verbose
