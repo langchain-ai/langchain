a530cdb7f62050898b20cbd6cba6941a4e2d386c
Harrison Chase <hw.chase.17@gmail.com>
2022-12-11 20:04:48 -0800

    cr

--- langchain/llms/base.py
+++ langchain/llms/base.py
@@ -1,26 +1,41 @@
 """Base interface for large language models to expose."""
 from abc import ABC, abstractmethod
-from typing import Any, List, Mapping, Optional, NamedTuple
+from typing import Any, List, Mapping, NamedTuple, Optional
+
+
+class Generation(NamedTuple):
+    """Output of a single generation."""
+
+    text: str
+    """Generated text output."""
+    # TODO: add log probs
 
 
 class LLMResult(NamedTuple):
     """Class that contains all relevant information for an LLM Result."""
-    result: str
-    log_probs: List[float]
-    llm_output: dict
-    """For arbitrary LLM provider specific output."""
 
+    generations: List[Generation]
+    """List of the things generated."""
+    llm_output: Optional[dict] = None
+    """For arbitrary LLM provider specific output."""
 
 
 class LLM(ABC):
     """LLM wrapper should take in a prompt and return a string."""
 
-    def generate(self, prompt: str, stop: Optional[List[str]] = None) -> LLMResult:
+    def generate(
+        self, prompts: List[str], stop: Optional[List[str]] = None
+    ) -> LLMResult:
         """Run the LLM on the given prompt and input."""
+        generations = []
+        for prompt in prompts:
+            text = self(prompt, stop=stop)
+            generations.append(Generation(text=text))
+        return LLMResult(generations=generations)
 
+    @abstractmethod
     def __call__(self, prompt: str, stop: Optional[List[str]] = None) -> str:
         """Run the LLM on the given prompt and input."""
-        return self.generate(prompt, stop=stop).result
 
     @property
     def _identifying_params(self) -> Mapping[str, Any]:
