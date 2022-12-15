c1a5edfd6a341c22c2aef81c5444085df478ba55
Harrison Chase <hw.chase.17@gmail.com>
2022-12-14 21:59:20 -0800

    cr

--- langchain/chains/combine_documents/map_reduce.py
+++ langchain/chains/combine_documents/map_reduce.py
@@ -2,14 +2,50 @@
 
 from __future__ import annotations
 
-from typing import Any, Dict, List
+from typing import Any, Callable, Dict, List
 
 from pydantic import BaseModel, Extra, root_validator
 
 from langchain.chains.combine_documents.base import BaseCombineDocumentsChain
 from langchain.chains.llm import LLMChain
 from langchain.docstore.document import Document
-from langchain.prompts import PromptTemplate
+
+
+def _split_list_of_docs(
+    docs: List[Document], length_func: Callable, token_max: int, **kwargs: Any
+) -> List[List[Document]]:
+    new_result_doc_list = []
+    _sub_result_docs = []
+    for doc in docs:
+        _sub_result_docs.append(doc)
+        _num_tokens = length_func(_sub_result_docs, **kwargs)
+        if _num_tokens > token_max:
+            if len(_sub_result_docs) == 1:
+                raise ValueError(
+                    "A single document was longer than the context length,"
+                    " we cannot handle this."
+                )
+            new_result_doc_list.append(_sub_result_docs[:-1])
+            _sub_result_docs = _sub_result_docs[-1:]
+    new_result_doc_list.append(_sub_result_docs)
+    return new_result_doc_list
+
+
+def _reduce_chunks(
+    doc_list: List[List[Document]],
+    combine_document_chain: BaseCombineDocumentsChain,
+    **kwargs: Any,
+) -> List[Document]:
+    result_docs = []
+    for docs in doc_list:
+        result = combine_document_chain.combine_docs(docs, **kwargs)
+        combined_metadata = docs[0].metadata
+        for doc in docs[1:]:
+            for k, v in doc.metadata:
+                if k in combined_metadata:
+                    combined_metadata[k] += f", {v}"
+        result_docs.append(Document(page_content=result, metadata=combined_metadata))
+    return result_docs
 
 
 class MapReduceDocumentsChain(BaseCombineDocumentsChain, BaseModel):
@@ -22,7 +58,6 @@ class MapReduceDocumentsChain(BaseCombineDocumentsChain, BaseModel):
     document_variable_name: str
     """The variable name in the llm_chain to put the documents in.
     If only one variable in the llm_chain, this need not be provided."""
-    document_prompt: PromptTemplate
 
     class Config:
         """Configuration for this pydantic object."""
@@ -51,77 +86,35 @@ class MapReduceDocumentsChain(BaseCombineDocumentsChain, BaseModel):
                 )
         return values
 
-    def compute_tokens(self, results: List, docs: List[Document], **kwargs: Any) -> int:
-        """Checks to see if, after combining a set of documents into one text, if it is too long."""
-        question_result_key = self.llm_chain.output_key
-        result_docs = [
-            Document(page_content=r[question_result_key], metadata=docs[i].metadata)
-            # This uses metadata from the docs, and the textual results from `results`
-            for i, r in enumerate(results)
-        ]
-        doc_dicts = []
-        for doc in result_docs:
-            base_info = {"page_content": doc.page_content}
-            base_info.update(doc.metadata)
-            document_info = {
-                k: base_info[k] for k in self.document_prompt.input_variables
-                # TODO: how does this work with plain QA, as opposed to this which is designed to work with
-                # QA with sources right now?
-            }
-            doc_dicts.append(document_info)
-        # Format each document according to the prompt
-        doc_strings = [self.document_prompt.format(**doc) for doc in doc_dicts]
-        combined_texts = "\n\n".join(doc_strings)
+    def combine_docs(
+        self, docs: List[Document], token_max: int = 3000, **kwargs: Any
+    ) -> str:
+        """Combine documents in a map reduce manner.
 
-        # tokenize the text and compute num tokens
-        num_tokens = self.combine_document_chain.llm_chain.llm.get_num_tokens(combined_texts)
-        return num_tokens
-
-    def combine_docs(self, docs: List[Document],
-                     token_max=3000,
-                     **kwargs: Any) -> str:
+        Combine by mapping first chain over all documents, then reducing the results.
+        This reducing can be done recursively if needed (if there are many documents).
         """
-        Combine by mapping first chain over all, then
-        if NUM OF TOKENS OF combined_texts is too large for context window,
-        combine some text and run self.llm_chain.apply again,
-        keep doing this in a while loop until the num of tokens in the combined text fits, then
-        stuff it into a final chain once the NUM OF TOKENS OF combined_texts is within the limit.
-        """
-
         results = self.llm_chain.apply(
             # FYI - this is parallelized and so it is fast.
             [{**{self.document_variable_name: d.page_content}, **kwargs} for d in docs]
         )
         question_result_key = self.llm_chain.output_key
-
-        num_tokens = self.compute_tokens(results, docs, **kwargs)
-        print(num_tokens)
-
-        # and if NUM OF TOKENS OF combined_texts is too large for context window,
-        # then combine some texts and keep running self.llm_chain.apply again until it fits.
-        while num_tokens > token_max:
-            # docs is a List[Document] where each Document has .page_content w/ the text
-            alld = []
-            for i in range(len(results)):
-                if (i % 2) != 0:
-                    alld.append(Document(page_content="\n\n".join([results[i][question_result_key],
-                                                                   results[i - 1][question_result_key]]),
-                                         # TODO: combine metadata from [i] and [i-1]?
-                                         metadata=docs[i].metadata))
-
-            results = self.llm_chain.apply(
-                [{**{self.document_variable_name: d.page_content}, **kwargs} for d in alld]
-            )
-            num_tokens = self.compute_tokens(results, docs, **kwargs)
-            print(num_tokens)
-
-        print("Starting final reduce_chain / merging into final answer")
         result_docs = [
             Document(page_content=r[question_result_key], metadata=docs[i].metadata)
             # This uses metadata from the docs, and the textual results from `results`
             for i, r in enumerate(results)
         ]
-        print("Combining docs.")
-        out = self.combine_document_chain.combine_docs(result_docs, **kwargs)
-        print("Done combining docs.")
-        return out
+        length_func = self.combine_document_chain.prompt_length
+        num_tokens = length_func(result_docs, **kwargs)
+        while num_tokens is not None and num_tokens > token_max:
+            new_result_doc_list = _split_list_of_docs(
+                result_docs, length_func, token_max, **kwargs
+            )
+            result_docs = _reduce_chunks(
+                new_result_doc_list, self.combine_document_chain, **kwargs
+            )
+            num_tokens = self.combine_document_chain.prompt_length(
+                result_docs, **kwargs
+            )
+        output = self.combine_document_chain.combine_docs(result_docs, **kwargs)
+        return output
--- langchain/chains/qa_with_sources/__init__.py
+++ langchain/chains/qa_with_sources/__init__.py
@@ -57,7 +57,6 @@ def _load_map_reduce_chain(
         llm_chain=map_chain,
         combine_document_chain=combine_document_chain,
         document_variable_name=map_reduce_document_variable_name,
-        document_prompt=document_prompt,
         **kwargs,
     )
 
