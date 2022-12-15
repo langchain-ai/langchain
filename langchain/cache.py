e6e8dbb91b08826139e070bc5159260e26819642
Harrison Chase <hw.chase.17@gmail.com>
2022-12-14 21:04:50 -0800

    cr

--- langchain/cache.py
+++ langchain/cache.py
@@ -1,12 +1,11 @@
-"""Base interface for cache."""
+"""Beta Feature: base interface for cache."""
 from abc import ABC, abstractmethod
 from typing import Dict, Optional, Tuple
 
-from sqlalchemy import Column, String
-
-from sqlalchemy.orm import Session
-from sqlalchemy import select, create_engine, insert
+from sqlalchemy import Column, String, create_engine, select
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
+
 
 class BaseCache(ABC):
     """Base interface for cache."""
@@ -36,29 +35,40 @@ class InMemoryCache(BaseCache):
         self._cache[(prompt, llm_string)] = return_val
 
 
-
 Base = declarative_base()
 
-class LLMCache(Base):
-    __tablename__ = 'llm_cache'
+
+class LLMCache(Base):  # type: ignore
+    """Table definition for LLM cache."""
+
+    __tablename__ = "llm_cache"
     prompt = Column(String, primary_key=True)
     llm = Column(String, primary_key=True)
     response = Column(String)
 
 
 class SQLiteCache(BaseCache):
+    """Implementation of a cache that is backed by SQLite."""
 
-    def __init__(self, db_name=".langchain.db"):
-        self.engine = create_engine(f"sqlite:///{db_name}")
+    def __init__(self, database_path: str = ".langchain.db"):
+        """Initialize with database path."""
+        self.engine = create_engine(f"sqlite:///{database_path}")
         Base.metadata.create_all(self.engine)
 
     def lookup(self, prompt: str, llm_string: str) -> Optional[str]:
-        stmt = select(LLMCache.response).where(LLMCache.prompt==prompt).where(LLMCache.llm == llm_string)
+        """Look up based on prompt and llm_string."""
+        stmt = (
+            select(LLMCache.response)
+            .where(LLMCache.prompt == prompt)
+            .where(LLMCache.llm == llm_string)
+        )
         with Session(self.engine) as session:
             for row in session.execute(stmt):
                 return row[0]
+        return None
 
     def update(self, prompt: str, llm_string: str, return_val: str) -> None:
+        """Update cache based on prompt and llm_string."""
         item = LLMCache(prompt=prompt, llm=llm_string, response=return_val)
         with Session(self.engine) as session, session.begin():
             session.add(item)
