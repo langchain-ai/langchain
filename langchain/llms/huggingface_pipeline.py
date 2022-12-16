5e4654eae8a7dbbcbe6857c6895d2476f751e387
mrbean <sam@you.com>
2022-12-15 23:18:24 -0500

    clean up and add tests

--- langchain/llms/huggingface_pipeline.py
+++ langchain/llms/huggingface_pipeline.py
@@ -1,14 +1,29 @@
 """Wrapper around HuggingFace APIs."""
 from typing import Any, Dict, List, Mapping, Optional
-from pydantic import BaseModel, Extra, root_validator
-from langchain.llms.base import LLM
 
+from pydantic import BaseModel, Extra, root_validator
+
+from langchain.llms.base import LLM
+from langchain.llms.utils import enforce_stop_tokens
 
 DEFAULT_MODEL_ID = "gpt2"
 VALID_TASKS = ("text2text-generation", "text-generation")
 
 
-class HuggingFaceLocal(LLM, BaseModel):
+class HuggingFacePipeline(LLM, BaseModel):
+    """Wrapper around HuggingFace Pipeline API.
+
+    To use, you should have the ``transformers`` python package installed.
+
+    Only supports `text-generation` and `text2text-generation` for now.
+
+    Example:
+        .. code-block:: python
+
+            from langchain import HuggingFacePipeline
+            hf = HuggingFacePipeline(model_id="gpt2")
+    """
+
     pipeline: Any  #: :meta private:
     model_id: str = DEFAULT_MODEL_ID
     """Model name to use."""
@@ -19,26 +34,39 @@ class HuggingFaceLocal(LLM, BaseModel):
 
     class Config:
         """Configuration for this pydantic object."""
+
         extra = Extra.forbid
 
     @root_validator()
     def validate_environment(cls, values: Dict) -> Dict:
+        """Validate python package exists in environment."""
         try:
-            from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+            from transformers import AutoModelForCausalLM, AutoTokenizer
+            from transformers import pipeline as hf_pipeline
 
-            tokenizer = AutoTokenizer.from_pretrained("gpt2")
-            model = AutoModelForCausalLM.from_pretrained("gpt2", problem_type="")
-            local_pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer)
-            if local_pipeline.task not in VALID_TASKS:
+            model_id = values["model_id"]
+            tokenizer = AutoTokenizer.from_pretrained(model_id)
+            model = AutoModelForCausalLM.from_pretrained(model_id)
+
+            pipeline = hf_pipeline(
+                values["task"],
+                model=model,
+                tokenizer=tokenizer,
+                model_kwargs=values["model_kwargs"],
+            )
+            if pipeline.task not in VALID_TASKS:
                 raise ValueError(
-                    f"Got invalid task {local_pipeline.task}, "
+                    f"Got invalid task {pipeline.task}, "
                     f"currently only {VALID_TASKS} are supported"
                 )
+            values["pipeline"] = pipeline
+
         except ImportError:
             raise ValueError(
                 "Could not import transformers python package. "
                 "Please it install it with `pip install transformers`."
             )
+
         return values
 
     @property
@@ -46,7 +74,7 @@ class HuggingFaceLocal(LLM, BaseModel):
         """Get the identifying parameters."""
         _model_kwargs = self.model_kwargs or {}
         return {
-            **{"repo_id": self.repo_id, "task": self.task},
+            **{"model_id": self.model_id, "task": self.task},
             **{"model_kwargs": _model_kwargs},
         }
 
@@ -54,9 +82,8 @@ class HuggingFaceLocal(LLM, BaseModel):
     def _llm_type(self) -> str:
         return "huggingface_local"
 
-    def __call__(self, prompt: str, stop: Optional[List[str]] = None) -> str:
-        _model_kwargs = self.model_kwargs or {}
-        response = self.pipeline(inputs=prompt, params=_model_kwargs)
+    def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
+        response = self.pipeline(text_inputs=prompt)
         if "error" in response:
             raise ValueError(f"Error raised by inference API: {response['error']}")
         if self.pipeline.task == "text-generation":
@@ -69,4 +96,8 @@ class HuggingFaceLocal(LLM, BaseModel):
                 f"Got invalid task {self.pipeline.task}, "
                 f"currently only {VALID_TASKS} are supported"
             )
+        if stop is not None:
+            # This is a bit hacky, but I can't figure out a better way to enforce
+            # stop tokens when making calls to huggingface_hub.
+            text = enforce_stop_tokens(text, stop)
         return text
