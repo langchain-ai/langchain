d6b53e3f78797ab15c7ae04c3b3d0af41de8c227
mrbean <sam@you.com>
2022-12-15 23:51:23 -0500

    i have no idea what im doing

--- tests/integration_tests/llms/test_huggingface_pipeline.py
+++ tests/integration_tests/llms/test_huggingface_pipeline.py
@@ -9,21 +9,21 @@ from langchain.llms.loading import load_llm
 from tests.integration_tests.llms.utils import assert_llm_equality
 
 
-def test_huggingface_local_text_generation() -> None:
+def test_huggingface_pipeline_text_generation() -> None:
     """Test valid call to HuggingFace text generation model."""
     llm = HuggingFacePipeline(model_id="gpt2", model_kwargs={"max_new_tokens": 10})
     output = llm("Say foo:")
     assert isinstance(output, str)
 
 
-def test_huggingface_local_text2text_generation() -> None:
+def test_huggingface_pipeline_text2text_generation() -> None:
     """Test valid call to HuggingFace text2text model."""
     llm = HuggingFacePipeline(model_id="google/flan-t5-xl")
     output = llm("The capital of New York is")
     assert output == "Albany"
 
 
-def test_huggingface_local_call_error() -> None:
+def test_huggingface_pipeline_call_error() -> None:
     """Test valid call to HuggingFace that errors."""
     llm = HuggingFacePipeline(model_kwargs={"max_new_tokens": -1})
     with pytest.raises(ValueError):
