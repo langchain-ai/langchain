4ff88a703b32fc867dc6ad67404dffe2000cccc9
mrbean <43734688+sam-h-bean@users.noreply.github.com>
2022-12-17 10:00:04 -0500

    Add HuggingFacePipeline LLM (#353)
    https://github.com/hwchase17/langchain/issues/354
    
    Add support for running your own HF pipeline locally. This would allow
    you to get a lot more dynamic with what HF features and models you
    support since you wouldn't be beholden to what is hosted in HF hub. You
    could also do stuff with HF Optimum to quantize your models and stuff to
    get pretty fast inference even running on a laptop.

--- langchain/llms/__init__.py
+++ langchain/llms/__init__.py
@@ -5,10 +5,18 @@ from langchain.llms.ai21 import AI21
 from langchain.llms.base import LLM
 from langchain.llms.cohere import Cohere
 from langchain.llms.huggingface_hub import HuggingFaceHub
+from langchain.llms.huggingface_pipeline import HuggingFacePipeline
 from langchain.llms.nlpcloud import NLPCloud
 from langchain.llms.openai import OpenAI
 
-__all__ = ["Cohere", "NLPCloud", "OpenAI", "HuggingFaceHub", "AI21"]
+__all__ = [
+    "Cohere",
+    "NLPCloud",
+    "OpenAI",
+    "HuggingFaceHub",
+    "HuggingFacePipeline",
+    "AI21",
+]
 
 type_to_cls_dict: Dict[str, Type[LLM]] = {
     "ai21": AI21,
@@ -16,4 +24,5 @@ type_to_cls_dict: Dict[str, Type[LLM]] = {
     "huggingface_hub": HuggingFaceHub,
     "nlpcloud": NLPCloud,
     "openai": OpenAI,
+    "huggingface_pipeline": HuggingFacePipeline,
 }
