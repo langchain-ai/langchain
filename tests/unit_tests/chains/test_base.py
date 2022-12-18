54d46d7bd52a1fb2385d225b646ef49e0d3490a6
Ankush Gola <ankush.gola@gmail.com>
2022-12-17 20:24:30 -0800

    add kwargs and args support for chain.run

--- tests/unit_tests/chains/test_base.py
+++ tests/unit_tests/chains/test_base.py
@@ -12,6 +12,7 @@ class FakeChain(Chain, BaseModel):
 
     be_correct: bool = True
     the_input_keys: List[str] = ["foo"]
+    the_output_keys: List[str] = ["bar"]
 
     @property
     def input_keys(self) -> List[str]:
@@ -21,7 +22,7 @@ class FakeChain(Chain, BaseModel):
     @property
     def output_keys(self) -> List[str]:
         """Output key of bar."""
-        return ["bar"]
+        return self.the_output_keys
 
     def _call(self, inputs: Dict[str, str]) -> Dict[str, str]:
         if self.be_correct:
@@ -63,3 +64,45 @@ def test_single_input_error() -> None:
     chain = FakeChain(the_input_keys=["foo", "bar"])
     with pytest.raises(ValueError):
         chain("bar")
+
+
+def test_run_single_arg() -> None:
+    """Test run method with single arg."""
+    chain = FakeChain()
+    output = chain.run("bar")
+    assert output == "baz"
+
+
+def test_run_multiple_args() -> None:
+    """Test run method with multiple args."""
+    chain = FakeChain(the_input_keys=["foo", "bar"])
+    output = chain.run("bar", "foo")
+    assert output == "baz"
+
+
+def test_run_multiple_args_error() -> None:
+    """Test run method with multiple args errors as expected."""
+    chain = FakeChain(the_input_keys=["foo", "bar"])
+    with pytest.raises(ValueError):
+        chain.run("bar", "foo", "baz")
+
+
+def test_run_kwargs() -> None:
+    """Test run method with kwargs."""
+    chain = FakeChain(the_input_keys=["foo", "bar"])
+    output = chain.run(foo="bar", bar="foo")
+    assert output == "baz"
+
+
+def test_run_kwargs_error() -> None:
+    """Test run method with kwargs errors as expected."""
+    chain = FakeChain(the_input_keys=["foo", "bar"])
+    with pytest.raises(ValueError):
+        chain.run(foo="bar", baz="foo")
+
+
+def test_run_args_and_kwargs_error() -> None:
+    """Test run method with args and kwargs."""
+    chain = FakeChain(the_input_keys=["foo", "bar"])
+    with pytest.raises(ValueError):
+        chain.run("bar", foo="bar")
