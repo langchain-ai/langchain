c104d507bf01cb44962284260d08560c83d799a1
Harrison Chase <hw.chase.17@gmail.com>
2022-12-20 22:24:08 -0500

    Harrison/improve data augmented generation docs (#390)
    Co-authored-by: cameronccohen <cameron.c.cohen@gmail.com>
    Co-authored-by: Cameron Cohen <cameron.cohen@quantco.com>

--- langchain/vectorstores/__init__.py
+++ langchain/vectorstores/__init__.py
@@ -2,5 +2,7 @@
 from langchain.vectorstores.base import VectorStore
 from langchain.vectorstores.elastic_vector_search import ElasticVectorSearch
 from langchain.vectorstores.faiss import FAISS
+from langchain.vectorstores.pinecone import Pinecone
+from langchain.vectorstores.weaviate import Weaviate
 
-__all__ = ["ElasticVectorSearch", "FAISS", "VectorStore"]
+__all__ = ["ElasticVectorSearch", "FAISS", "VectorStore", "Pinecone", "Weaviate"]
