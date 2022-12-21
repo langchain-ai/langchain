fb9b461fe92b82fe7493782afdf6aa4033e13e72
Keiji Kanazawa <{ID}+{username}@users.noreply.github.com>
2022-12-21 14:25:30 -0800

    Update with Harrison's refactor of OpenAI LLM

--- langchain/llms/__init__.py
+++ langchain/llms/__init__.py
@@ -8,9 +8,7 @@ from langchain.llms.huggingface_hub import HuggingFaceHub
 from langchain.llms.huggingface_pipeline import HuggingFacePipeline
 from langchain.llms.nlpcloud import NLPCloud
 from langchain.llms.openai import OpenAI
-
-# Need to import AzureOpenAI after OpenAI to avoid circular import
-from langchain.llms.azure_openai import AzureOpenAI
+from langchain.llms.openai import AzureOpenAI
 
 __all__ = [
     "Cohere",
