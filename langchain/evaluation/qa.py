0db686d260b6cef8264fa0aff26e98cfe46029e0
Harrison Chase <hw.chase.17@gmail.com>
2022-12-24 11:12:39 -0500

    cr

--- langchain/evaluation/qa.py
+++ langchain/evaluation/qa.py
@@ -1,11 +1,11 @@
-
+"""Utility functions related to question answering."""
 from __future__ import annotations
 from typing import List
 
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
-from collections import Counter
 from langchain.llms.base import BaseLLM
+import re
 template = """You are a teacher grading a quiz.
 You are given a question, the student's answer, and the true answer, and are asked to score it as either CORRECT or INCORRECT.
 
@@ -23,32 +23,57 @@ TRUE ANSWER: {answer}
 GRADE:"""
 prompt = PromptTemplate(input_variables=['query', 'result', "answer"], template=template)
 
+template = """You are a teacher coming up with questions to ask on a quiz. 
+Given the following document, please generate a question and answer based on that document.
+
+Example Format:
+<Begin Document>
+...
+<End Document>
+QUESTION: question here
+ANSWER: answer here
+
+These questions should be detailed and be based explicitly on information in the document. Begin!
+
+<Begin Document>
+{doc}
+<End Document>"""
+generate_question_prompt = PromptTemplate(input_variables=['doc'], template=template)
 
 
-class QAEvaluator:
-    """Class with methods for evaluating question/answering systems."""
+def generate_questions(llm: BaseLLM, texts: List[str]) -> List[dict]:
+    """Generate a question/answer pair based on each piece of text."""
+    llm_chain = LLMChain(llm=llm, prompt=generate_question_prompt)
+    outputs = llm_chain.apply([{"doc": d} for d in texts])
+    pairs = []
+    for output in outputs:
+        regex = r"QUESTION: (.*?)\nANSWER: (.*)"
+        match = re.search(regex, output['text'])
+        if match:
+            question = match.group(1)
+            answer = match.group(2)
+            pairs.append({"query": question, "answer": answer})
+    return pairs
 
-    @classmethod
-    def from_llm(cls, llm: BaseLLM) -> QAEvaluator:
-        """Load base evaluator from an LLM."""
-        llm_chain = LLMChain(llm=llm, prompt=prompt, output_key="grade")
-        return cls(llm_chain)
 
-    def __init__(self, evaluation_chain: LLMChain):
-        """Initialize with the LLM chain used for grading."""
-        self.evaluation_chain = evaluation_chain
+def evaluate_question_answering(
+        llm: BaseLLM,
+        examples: List[dict],
+        predictions: List[dict],
+        question_key: str = "query",
+        answer_key: str = "answer",
+        prediction_key: str = "result",
+        output_key: str = "grade"
+) -> List[dict]:
+    """Evaluate question answering examples and predictions."""
+    inputs = []
+    for i, example in enumerate(examples):
+        _input = {
+            "query": example[question_key],
+            "answer": example[answer_key],
+            "result": predictions[i][prediction_key]
+        }
+        inputs.append(_input)
+    evaluation_chain = LLMChain(llm=llm, prompt=prompt, output_key=output_key)
+    return evaluation_chain.apply(inputs)
 
-    def score(self, evaluated_outputs: List[dict]):
-        """Score the evaluations."""
-        return Counter([output["grade"] for output in evaluated_outputs])
-
-    def run_eval(self, predictions: List[dict]):
-        """Run the evaluation chain."""
-        return self.evaluation_chain.apply(predictions)
-
-    def run_and_score(self, examples: List[dict], chain):
-        """Run and score predictions."""
-        predictions = chain.apply(examples)
-        evaluated_outputs = self.run_eval(predictions)
-        score = self.score(evaluated_outputs)
-        return predictions, evaluated_outputs, score
