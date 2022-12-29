467fce3e7c12807a41d0eb1a3fad4059fb2eacf1
Harrison Chase <hw.chase.17@gmail.com>
2022-12-29 18:41:11 -0500

    cr

--- langchain/callbacks/__init__.py
+++ langchain/callbacks/__init__.py
@@ -1,8 +1,15 @@
 """Callback handlers that allow listening to events in LangChain."""
 from langchain.callbacks.base import BaseCallbackManager
 from langchain.callbacks.shared import SharedCallbackManager
+from langchain.callbacks.stdout import StdOutCallbackHandler
 
 
 def get_callback_manager() -> BaseCallbackManager:
     """Return the shared callback manager."""
     return SharedCallbackManager()
+
+
+def set_default_callback_manager() -> None:
+    """Set default callback manager."""
+    callback = get_callback_manager()
+    callback.add_handler(StdOutCallbackHandler())
