9b09cfe85076f8c11717f15c5852841cb956393a
Harrison Chase <hw.chase.17@gmail.com>
2022-12-28 23:30:02 -0500

    conversational agent

---
+++ langchain/agents/conversational/base.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+
+import re
+from typing import Any, Callable, List, NamedTuple, Optional, Tuple
+
+from langchain.agents.agent import Agent
+from langchain.agents.mrkl.prompt import FORMAT_INSTRUCTIONS
+from langchain.agents.conversational.prompt import PREFIX, SUFFIX
+from langchain.agents.tools import Tool
+from langchain.llms.base import BaseLLM
+from langchain.prompts import PromptTemplate
+
+
+class ConversationalAgent(Agent):
+    """Agent for the MRKL chain."""
+
+    @property
+    def observation_prefix(self) -> str:
+        """Prefix to append the observation with."""
+        return "Observation: "
+
+    @property
+    def llm_prefix(self) -> str:
+        """Prefix to append the llm call with."""
+        return "Thought:"
+
+    @classmethod
+    def create_prompt(
+        cls,
+        tools: List[Tool],
+        prefix: str = PREFIX,
+        suffix: str = SUFFIX,
+        input_variables: Optional[List[str]] = None,
+    ) -> PromptTemplate:
+        """Create prompt in the style of the zero shot agent.
+
+        Args:
+            tools: List of tools the agent will have access to, used to format the
+                prompt.
+            prefix: String to put before the list of tools.
+            suffix: String to put after the list of tools.
+            input_variables: List of input variables the final prompt will expect.
+
+        Returns:
+            A PromptTemplate with the template assembled from the pieces here.
+        """
+        tool_strings = "\n".join([f"{tool.name}: {tool.description}" for tool in tools])
+        tool_names = ", ".join([tool.name for tool in tools])
+        format_instructions = FORMAT_INSTRUCTIONS.format(tool_names=tool_names)
+        template = "\n\n".join([prefix, tool_strings, format_instructions, suffix])
+        if input_variables is None:
+            input_variables = ["input", "chat_history", "agent_scratchpad"]
+        return PromptTemplate(template=template, input_variables=input_variables)
+
+    @property
+    def finish_tool_name(self) -> str:
+        """Name of the tool to use to finish the chain."""
+        return "AI"
+
+    def _extract_tool_and_input(self, llm_output: str) -> Optional[Tuple[str, str]]:
+        if "AI: " in llm_output:
+            return "AI", llm_output.split("AI: ")[-1]
+        regex = r"Action: (.*?)\nAction Input: (.*)"
+        match = re.search(regex, llm_output)
+        if not match:
+            raise ValueError(f"Could not parse LLM output: `{llm_output}`")
+        action = match.group(1)
+        action_input = match.group(2)
+        return action, action_input.strip(" ").strip('"')
---
+++ langchain/agents/conversational/prompt.py
@@ -0,0 +1,17 @@
+PREFIX = """The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
+
+The AI has access to the following tools:"""
+SUFFIX = """You do NOT need to use these tools. For most normal conversation, you will not need to, and you can just respond directly to the Human.
+
+When you have a response to say to the Human, you MUST use the format:
+
+```
+AI: [your repsonse here]
+```
+
+Begin!"
+
+Current conversation:
+{chat_history}
+Human: {input}
+{agent_scratchpad}"""
