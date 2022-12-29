658e10c69a517a1532ffbd9e15f79ce6851d9fc8
Shreya Rajpal <shreya.rajpal@gmail.com>
2022-12-29 18:41:58 +0530

    Update theme

--- docs/conf.py
+++ docs/conf.py
@@ -74,8 +74,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
-# html_theme = "sphinx_typlog_theme"
+html_theme = "sphinx_book_theme"
 
 html_context = {
     "display_github": True,  # Integrate GitHub
