c1c77708b750c37b68506ec94f92bb9c43cc5cc6
Bruno Bornsztein <bruno.bornsztein@gmail.com>
2022-12-29 07:01:26 -0600

    prompt fixes

--- langchain/agents/conversational/prompt.py
+++ langchain/agents/conversational/prompt.py
@@ -1,15 +1,20 @@
 PREFIX = """The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
-
 The AI has access to the following tools:"""
 SUFFIX = """You do NOT need to use these tools. For most normal conversation, you will not need to, and you can just respond directly to the Human.
+
+If the Human asks a question about a specific event or time period and doesn't provide any context or details, you MUST ask for more information in order to better understand the context and provide a more accurate and useful response. You MUST ask for more information using the format:
+
+```
+AI: [request for more information here]
+```
 
 When you have a response to say to the Human, you MUST use the format:
 
 ```
-AI: [your repsonse here]
+AI: [your response here]
 ```
 
-Begin!"
+Begin!
 
 Current conversation:
 {chat_history}
