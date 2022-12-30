a7112ca148daae9fb25d9caacd4b0a5bdaa6f446
Harrison Chase <hw.chase.17@gmail.com>
2022-12-29 19:55:15 -0500

    cr

--- tests/unit_tests/callbacks/test_callback_manager.py
+++ tests/unit_tests/callbacks/test_callback_manager.py
@@ -8,7 +8,7 @@ from langchain.callbacks.base import (
     CallbackManager,
 )
 from langchain.callbacks.shared import SharedCallbackManager
-from langchain.schema import LLMResult
+from langchain.schema import AgentAction, LLMResult
 
 
 class FakeCallbackHandler(BaseCallbackHandler):
@@ -52,12 +52,12 @@ class FakeCallbackHandler(BaseCallbackHandler):
         self.errors += 1
 
     def on_tool_start(
-        self, serialized: Dict[str, Any], action: str, tool_input: str, **extra: str
+        self, serialized: Dict[str, Any], action: AgentAction, **extra: str
     ) -> None:
         """Run when tool starts running."""
         self.starts += 1
 
-    def on_tool_end(self, output: str) -> None:
+    def on_tool_end(self, output: str, **kwargs: Any) -> None:
         """Run when tool ends running."""
         self.ends += 1
 
@@ -73,10 +73,10 @@ def _test_callback_manager(
     manager.on_llm_start({}, [])
     manager.on_llm_end(LLMResult(generations=[]))
     manager.on_llm_error(Exception())
-    manager.on_chain_start({}, {})
+    manager.on_chain_start({"name": "foo"}, {})
     manager.on_chain_end({})
     manager.on_chain_error(Exception())
-    manager.on_tool_start({}, "", "")
+    manager.on_tool_start({}, AgentAction("", "", ""))
     manager.on_tool_end("")
     manager.on_tool_error(Exception())
     for handler in handlers:
