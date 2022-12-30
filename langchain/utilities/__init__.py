f0e3bb4ac4bb3cb746cd32e832d21aa8068d5a07
Harrison Chase <hw.chase.17@gmail.com>
2022-12-30 08:06:57 -0500

    Harrison/new search engine (#477)
    Co-authored-by: Nicolas <nicolascamara29@gmail.com>

--- langchain/utilities/__init__.py
+++ langchain/utilities/__init__.py
@@ -1,6 +1,8 @@
 """General utilities."""
 from langchain.utilities.bash import BashProcess
+from langchain.utilities.google_search import GoogleSearchAPIWrapper
 
 __all__ = [
     "BashProcess",
+    "GoogleSearchAPIWrapper",
 ]
