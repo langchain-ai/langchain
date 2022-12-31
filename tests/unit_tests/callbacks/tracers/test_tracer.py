e5f38f4d578a0ac2bc353a1128bcd5fbf0927b3a
Ankush Gola <ankush.gola@gmail.com>
2022-12-30 23:49:45 -0800

    multithreaded test

--- tests/unit_tests/callbacks/tracers/test_tracer.py
+++ tests/unit_tests/callbacks/tracers/test_tracer.py
@@ -1,12 +1,77 @@
 """Test Tracer classes."""
+from __future__ import annotations
 
-from langchain.callbacks.tracers.base import Tracer, SharedTracer, LLMRun, ChainRun, ToolRun
+from concurrent.futures import ThreadPoolExecutor
+from langchain.callbacks.tracers.base import Tracer, SharedTracer, LLMRun, ChainRun, ToolRun, BaseTracer
 
 from typing import Any, Dict, List, Optional, Union
 from pydantic import BaseModel
 from langchain.schema import AgentAction, LLMResult
 from freezegun import freeze_time
 from datetime import datetime
+import threading
+
+
+@freeze_time("2023-01-01")
+def _get_compare_run() -> Union[LLMRun, ChainRun, ToolRun]:
+    return ChainRun(
+        id=None,
+        start_time=datetime.utcnow(),
+        end_time=datetime.utcnow(),
+        extra={},
+        execution_order=1,
+        serialized={},
+        inputs={},
+        outputs={},
+        child_runs=[
+            ToolRun(
+                id=None,
+                start_time=datetime.utcnow(),
+                end_time=datetime.utcnow(),
+                extra={},
+                execution_order=2,
+                serialized={},
+                tool_input="test",
+                output="test",
+                action="action",
+                child_runs=[
+                    LLMRun(
+                        id=None,
+                        start_time=datetime.utcnow(),
+                        end_time=datetime.utcnow(),
+                        extra={},
+                        execution_order=3,
+                        serialized={},
+                        prompts=[],
+                        response=LLMResult([[]])
+                    )
+                ]
+            ),
+            LLMRun(
+                id=None,
+                start_time=datetime.utcnow(),
+                end_time=datetime.utcnow(),
+                extra={},
+                execution_order=4,
+                serialized={},
+                prompts=[],
+                response=LLMResult([[]])
+            )
+        ]
+    )
+
+
+def _perform_nested_run(tracer: BaseTracer):
+    """Perform a nested run."""
+
+    tracer.on_chain_start(serialized={}, inputs={})
+    tracer.on_tool_start(serialized={}, action=AgentAction(tool="action", tool_input="test", log=""))
+    tracer.on_llm_start(serialized={}, prompts=[])
+    tracer.on_llm_end(response=LLMResult([[]]))
+    tracer.on_tool_end("test")
+    tracer.on_llm_start(serialized={}, prompts=[])
+    tracer.on_llm_end(response=LLMResult([[]]))
+    tracer.on_chain_end(outputs={})
 
 
 class FakeTracer(Tracer):
@@ -22,6 +87,32 @@ class FakeTracer(Tracer):
         """Persist a run."""
 
         assert run == self.compare_run
+
+    def _add_child_run(
+        self,
+        parent_run: Union[ChainRun, ToolRun],
+        child_run: Union[LLMRun, ChainRun, ToolRun],
+    ) -> None:
+        """Add child run to a chain run or tool run."""
+
+        parent_run.child_runs.append(child_run)
+
+    def _generate_id(self) -> Optional[Union[int, str]]:
+        """Generate an id for a run."""
+
+        return None
+
+
+class FakeSharedTracer(SharedTracer):
+    """Fake shared tracer that records LangChain execution."""
+
+    runs = []
+
+    def _persist_run(self, run: Union[LLMRun, ChainRun, ToolRun]) -> None:
+        """Persist a run."""
+
+        with self._lock:
+            self.runs.append(run)
 
     def _add_child_run(
         self,
@@ -100,57 +191,32 @@ def test_tracer_tool_run() -> None:
 def test_tracer_nested_run() -> None:
     """Test tracer on a nested run."""
 
-    tracer = FakeTracer(compare_run=ChainRun(
-        id=None,
-        start_time=datetime.utcnow(),
-        end_time=datetime.utcnow(),
-        extra={},
-        execution_order=1,
-        serialized={},
-        inputs={},
-        outputs={},
-        child_runs=[
-            ToolRun(
-                id=None,
-                start_time=datetime.utcnow(),
-                end_time=datetime.utcnow(),
-                extra={},
-                execution_order=2,
-                serialized={},
-                tool_input="test",
-                output="test",
-                action="action",
-                child_runs=[
-                    LLMRun(
-                        id=None,
-                        start_time=datetime.utcnow(),
-                        end_time=datetime.utcnow(),
-                        extra={},
-                        execution_order=3,
-                        serialized={},
-                        prompts=[],
-                        response=LLMResult([[]])
-                    )
-                ]
-            ),
-            LLMRun(
-                id=None,
-                start_time=datetime.utcnow(),
-                end_time=datetime.utcnow(),
-                extra={},
-                execution_order=4,
-                serialized={},
-                prompts=[],
-                response=LLMResult([[]])
-            )
-        ]
-    ))
+    tracer = FakeTracer(compare_run=_get_compare_run())
+    _perform_nested_run(tracer)
 
-    tracer.on_chain_start(serialized={}, inputs={})
-    tracer.on_tool_start(serialized={}, action=AgentAction(tool="action", tool_input="test", log=""))
-    tracer.on_llm_start(serialized={}, prompts=[])
-    tracer.on_llm_end(response=LLMResult([[]]))
-    tracer.on_tool_end("test")
-    tracer.on_llm_start(serialized={}, prompts=[])
-    tracer.on_llm_end(response=LLMResult([[]]))
-    tracer.on_chain_end(outputs={})
+
+@freeze_time("2023-01-01")
+def test_shared_tracer_nested_run() -> None:
+    """Test shared tracer on a nested run."""
+
+    tracer = FakeSharedTracer()
+    _perform_nested_run(tracer)
+    assert tracer.runs == [_get_compare_run()]
+
+
+@freeze_time("2023-01-01")
+def test_shared_tracer_nested_run_multithreaded() -> None:
+    """Test shared tracer on a nested run."""
+
+    tracer = FakeSharedTracer()
+    threads = []
+    num_threads = 10
+    for _ in range(num_threads):
+        thread = threading.Thread(target=_perform_nested_run, args=(tracer,))
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+
+    assert tracer.runs == [_get_compare_run()] * num_threads
