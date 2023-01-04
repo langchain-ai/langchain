4f4b8655f780232251b8e9971a4b24204552bb1d
Harrison Chase <hw.chase.17@gmail.com>
2023-01-04 14:52:16 -0800

    fix caching and add test

---
+++ tests/unit_tests/llms/test_base.py
@@ -0,0 +1,22 @@
+"""Test base LLM functionality."""
+import langchain
+from langchain.cache import InMemoryCache
+from langchain.schema import Generation, LLMResult
+from tests.unit_tests.llms.fake_llm import FakeLLM
+
+
+def test_caching() -> None:
+    """Test caching behavior."""
+    langchain.llm_cache = InMemoryCache()
+    llm = FakeLLM()
+    params = llm._llm_dict()
+    params["stop"] = None
+    llm_string = str(sorted([(k, v) for k, v in params.items()]))
+    langchain.llm_cache.update("foo", llm_string, [Generation(text="fizz")])
+    output = llm.generate(["foo", "bar"])
+    langchain.llm_cache = None
+    expected_output = LLMResult(
+        generations=[[Generation(text="fizz")], [Generation(text="foo")]],
+        llm_output=None,
+    )
+    assert output == expected_output
