eef13166205d3e196b90226c139f1b22d4979e43
Harrison Chase <hw.chase.17@gmail.com>
2023-01-06 07:25:55 -0800

    RFC: conversational agent (#464)
    Co-authored-by: Bruno Bornsztein <bruno.bornsztein@gmail.com>

---
+++ langchain/agents/conversational/base.py
@@ -0,0 +1,103 @@
+"""An agent designed to hold a conversation in addition to using tools."""
+from __future__ import annotations
+
+import re
+from typing import Any, List, Optional, Tuple
+
+from langchain.agents.agent import Agent
+from langchain.agents.conversational.prompt import FORMAT_INSTRUCTIONS, PREFIX, SUFFIX
+from langchain.agents.tools import Tool
+from langchain.callbacks.base import BaseCallbackManager
+from langchain.chains import LLMChain
+from langchain.llms import BaseLLM
+from langchain.prompts import PromptTemplate
+
+
+class ConversationalAgent(Agent):
+    """An agent designed to hold a conversation in addition to using tools."""
+
+    ai_prefix: str = "AI"
+
+    @property
+    def observation_prefix(self) -> str:
+        """Prefix to append the observation with."""
+        return "Observation: "
+
+    @property
+    def llm_prefix(self) -> str:
+        """Prefix to append the llm call with."""
+        return "Thought:"
+
+    @classmethod
+    def create_prompt(
+        cls,
+        tools: List[Tool],
+        prefix: str = PREFIX,
+        suffix: str = SUFFIX,
+        ai_prefix: str = "AI",
+        human_prefix: str = "Human",
+        input_variables: Optional[List[str]] = None,
+    ) -> PromptTemplate:
+        """Create prompt in the style of the zero shot agent.
+
+        Args:
+            tools: List of tools the agent will have access to, used to format the
+                prompt.
+            prefix: String to put before the list of tools.
+            suffix: String to put after the list of tools.
+            ai_prefix: String to use before AI output.
+            human_prefix: String to use before human output.
+            input_variables: List of input variables the final prompt will expect.
+
+        Returns:
+            A PromptTemplate with the template assembled from the pieces here.
+        """
+        tool_strings = "\n".join(
+            [f"> {tool.name}: {tool.description}" for tool in tools]
+        )
+        tool_names = ", ".join([tool.name for tool in tools])
+        format_instructions = FORMAT_INSTRUCTIONS.format(
+            tool_names=tool_names, ai_prefix=ai_prefix, human_prefix=human_prefix
+        )
+        template = "\n\n".join([prefix, tool_strings, format_instructions, suffix])
+        if input_variables is None:
+            input_variables = ["input", "chat_history", "agent_scratchpad"]
+        return PromptTemplate(template=template, input_variables=input_variables)
+
+    @property
+    def finish_tool_name(self) -> str:
+        """Name of the tool to use to finish the chain."""
+        return self.ai_prefix
+
+    def _extract_tool_and_input(self, llm_output: str) -> Optional[Tuple[str, str]]:
+        if f"{self.ai_prefix}: " in llm_output:
+            return self.ai_prefix, llm_output.split(f"{self.ai_prefix}: ")[-1]
+        regex = r"Action: (.*?)\nAction Input: (.*)"
+        match = re.search(regex, llm_output)
+        if not match:
+            raise ValueError(f"Could not parse LLM output: `{llm_output}`")
+        action = match.group(1)
+        action_input = match.group(2)
+        return action, action_input.strip(" ").strip('"')
+
+    @classmethod
+    def from_llm_and_tools(
+        cls,
+        llm: BaseLLM,
+        tools: List[Tool],
+        callback_manager: Optional[BaseCallbackManager] = None,
+        ai_prefix: str = "AI",
+        human_prefix: str = "Human",
+        **kwargs: Any,
+    ) -> Agent:
+        """Construct an agent from an LLM and tools."""
+        cls._validate_tools(tools)
+        prompt = cls.create_prompt(
+            tools, ai_prefix=ai_prefix, human_prefix=human_prefix
+        )
+        llm_chain = LLMChain(
+            llm=llm,
+            prompt=prompt,
+            callback_manager=callback_manager,
+        )
+        return cls(llm_chain=llm_chain, ai_prefix=ai_prefix, **kwargs)
