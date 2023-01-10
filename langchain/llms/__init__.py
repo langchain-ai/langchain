b5317cb6f9029b832896de59a7741a8f39cd3686
Mike Lambert <mike.lambert@anthropic.com>
2023-01-10 00:24:38 -0800

    Add first pass at anthropic LLM

--- langchain/llms/__init__.py
+++ langchain/llms/__init__.py
@@ -1,6 +1,7 @@
 """Wrappers on top of large language models APIs."""
 from typing import Dict, Type
 
+from langchain.llms.anthropic import Anthropic
 from langchain.llms.ai21 import AI21
 from langchain.llms.base import BaseLLM
 from langchain.llms.cohere import Cohere
@@ -10,6 +11,7 @@ from langchain.llms.nlpcloud import NLPCloud
 from langchain.llms.openai import AzureOpenAI, OpenAI
 
 __all__ = [
+    "Anthropic",
     "Cohere",
     "NLPCloud",
     "OpenAI",
@@ -21,6 +23,7 @@ __all__ = [
 
 type_to_cls_dict: Dict[str, Type[BaseLLM]] = {
     "ai21": AI21,
+    "anthropic": Anthropic,
     "cohere": Cohere,
     "huggingface_hub": HuggingFaceHub,
     "nlpcloud": NLPCloud,
