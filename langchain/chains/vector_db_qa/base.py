8ab09c18a1ae610563d99888679e23f904972a3c
Harrison Chase <hw.chase.17@gmail.com>
2023-01-12 06:09:32 -0800

    Return source documents option in VectorDBQA (#585) (#592)
    Co-authored-by: lesscomfortable <pancho_ingham@hotmail.com>
    
    Co-authored-by: Francisco Ingham <fpingham@gmail.com>
    Co-authored-by: lesscomfortable <pancho_ingham@hotmail.com>

--- langchain/chains/vector_db_qa/base.py
+++ langchain/chains/vector_db_qa/base.py
@@ -36,6 +36,8 @@ class VectorDBQA(Chain, BaseModel):
     """Chain to use to combine the documents."""
     input_key: str = "query"  #: :meta private:
     output_key: str = "result"  #: :meta private:
+    return_source_documents: bool = False
+    """Return the source documents."""
 
     class Config:
         """Configuration for this pydantic object."""
@@ -45,7 +47,7 @@ class VectorDBQA(Chain, BaseModel):
 
     @property
     def input_keys(self) -> List[str]:
-        """Return the singular input key.
+        """Return the input keys.
 
         :meta private:
         """
@@ -53,11 +55,14 @@ class VectorDBQA(Chain, BaseModel):
 
     @property
     def output_keys(self) -> List[str]:
-        """Return the singular output key.
+        """Return the output keys.
 
         :meta private:
         """
-        return [self.output_key]
+        _output_keys = [self.output_key]
+        if self.return_source_documents:
+            _output_keys = _output_keys + ["source_documents"]
+        return _output_keys
 
     # TODO: deprecate this
     @root_validator(pre=True)
@@ -96,10 +101,30 @@ class VectorDBQA(Chain, BaseModel):
             document_variable_name="context",
             document_prompt=document_prompt,
         )
+
         return cls(combine_documents_chain=combine_documents_chain, **kwargs)
 
-    def _call(self, inputs: Dict[str, str]) -> Dict[str, str]:
+    def _call(
+        self,
+        inputs: Dict[str, str],
+    ) -> Dict[str, Any]:
+        """Run similarity search and llm on input query.
+
+        If inputs contains 'return_source_documents' as 'True', returns
+        the retrieved documents as well under the key 'source_documents'.
+
+        Example:
+        .. code-block:: python
+
+        res = vectordbqa({'query': 'This is my query', 'return_source_documents': True})
+        answer, docs = res['result'], res['source_documents']
+        """
         question = inputs[self.input_key]
+
         docs = self.vectorstore.similarity_search(question, k=self.k)
         answer, _ = self.combine_documents_chain.combine_docs(docs, question=question)
-        return {self.output_key: answer}
+
+        if self.return_source_documents:
+            return {self.output_key: answer, "source_documents": docs}
+        else:
+            return {self.output_key: answer}
