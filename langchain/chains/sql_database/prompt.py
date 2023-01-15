d8417d086e471047ca91bf83da835757702d819e
lesscomfortable <pancho_ingham@hotmail.com>
2023-01-15 01:37:28 -0300

    a small correction in sql prompt to add flexibility in the 'LIMIT' clause

--- langchain/chains/sql_database/prompt.py
+++ langchain/chains/sql_database/prompt.py
@@ -1,7 +1,7 @@
 # flake8: noqa
 from langchain.prompts.prompt import PromptTemplate
 
-_DEFAULT_TEMPLATE = """Given an input question, first create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer. Always limit your query to at most {top_k} results using the LIMIT clause. You can order the results by a relevant column to return the most interesting examples in the database.
+_DEFAULT_TEMPLATE = """Given an input question, first create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer. Unless the user specifies in his question a specific number of examples he wishes to obtain, always limit your query to at most {top_k} results using the LIMIT clause. You can order the results by a relevant column to return the most interesting examples in the database.
 Use the following format:
 
 Question: "Question here"
