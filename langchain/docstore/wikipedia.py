95d91b2891beb2f7f27387de9842c5cca0f69e13
Harrison Chase <hw.chase.17@gmail.com>
2023-01-15 16:40:39 -0800

    cr

--- langchain/docstore/wikipedia.py
+++ langchain/docstore/wikipedia.py
@@ -30,8 +30,10 @@ class Wikipedia(Docstore):
 
         try:
             page_content = wikipedia.page(search).content
-            url          = wikipedia.page(search).url
-            result: Union[str, Document] = Document(page_content=page_content,metadata={"page": url})
+            url = wikipedia.page(search).url
+            result: Union[str, Document] = Document(
+                page_content=page_content, metadata={"page": url}
+            )
         except wikipedia.PageError:
             result = f"Could not find [{search}]. Similar: {wikipedia.search(search)}"
         except wikipedia.DisambiguationError:
