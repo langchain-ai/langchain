e2470e58333dd04fdfa2393939b01929d133b757
Francisco Ingham <fpingham@gmail.com>
2023-01-15 22:10:03 -0300

    Modified api and sql prompts to shorten response length (#618)
    Included in the API prompt to ask for as least information in possible
    in API calls.
    Asked in the SQL prompt to add a limit clause when building SQL queries.
    Included a 'top_k' parameter which suggests a reasonable limit size for
    the query.
    
    Co-authored-by: lesscomfortable <pancho_ingham@hotmail.com>
    Co-authored-by: Harrison Chase <hw.chase.17@gmail.com>

--- langchain/chains/api/prompt.py
+++ langchain/chains/api/prompt.py
@@ -2,12 +2,19 @@
 from langchain.prompts.prompt import PromptTemplate
 
 API_URL_PROMPT_TEMPLATE = """You are given the below API Documentation:
-
 {api_docs}
+Using this documentation, generate the full API url to call for answering the user question.
+You should build the API url so as to get a response that is as short as possible, while still getting the necessary information to answer the question. Pay attention in the following example, how there are many pieces of information that are deliberately not included in the API call. You should do the same.
 
-Using this documentation, generate the full API url to call for answering this question: {question}
+EXAMPLE
+Question: Please tell me what the weather is like right now in London?
+API url:https://api.open-meteo.com/v1/forecast?latitude=51.5074&longitude=0.1278&current_weather=true&timezone=auto
+END OF EXAMPLE
 
-API url: """
+Question:{question}
+API url:
+"""
+
 API_URL_PROMPT = PromptTemplate(
     input_variables=[
         "api_docs",
