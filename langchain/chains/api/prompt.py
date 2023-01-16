06f2383f706a0434d3f54a5c396d6b4f2b2e9705
Harrison Chase <hw.chase.17@gmail.com>
2023-01-15 17:24:56 -0800

    cr

--- langchain/chains/api/prompt.py
+++ langchain/chains/api/prompt.py
@@ -6,14 +6,8 @@ API_URL_PROMPT_TEMPLATE = """You are given the below API Documentation:
 Using this documentation, generate the full API url to call for answering the user question.
 You should build the API url so as to get a response that is as short as possible, while still getting the necessary information to answer the question. Pay attention in the following example, how there are many pieces of information that are deliberately not included in the API call. You should do the same.
 
-EXAMPLE
-Question: Please tell me what the weather is like right now in London?
-API url:https://api.open-meteo.com/v1/forecast?latitude=51.5074&longitude=0.1278&current_weather=true&timezone=auto
-END OF EXAMPLE
-
 Question:{question}
-API url:
-"""
+API url:"""
 
 API_URL_PROMPT = PromptTemplate(
     input_variables=[
