f0a0061dfc0c481a60cd17d0b711dfbb7edf2967
Francisco Ingham <fpingham@gmail.com>
2023-01-17 03:45:14 -0300

    Exclude reference to 'example' in api prompt (#629)
    Co-authored-by: lesscomfortable <pancho_ingham@hotmail.com>

--- langchain/chains/api/prompt.py
+++ langchain/chains/api/prompt.py
@@ -4,7 +4,7 @@ from langchain.prompts.prompt import PromptTemplate
 API_URL_PROMPT_TEMPLATE = """You are given the below API Documentation:
 {api_docs}
 Using this documentation, generate the full API url to call for answering the user question.
-You should build the API url so as to get a response that is as short as possible, while still getting the necessary information to answer the question. Pay attention in the following example, how there are many pieces of information that are deliberately not included in the API call. You should do the same.
+You should build the API url in order to get a response that is as short as possible, while still getting the necessary information to answer the question. Pay attention to deliberately exclude any unnecessary pieces of data in the API call.
 
 Question:{question}
 API url:"""
