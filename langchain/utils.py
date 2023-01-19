7ab5252ba4952176c7f225107129147fb5a55ced
Harrison Chase <hw.chase.17@gmail.com>
2023-01-19 09:29:20 -0800

    cr

--- langchain/utils.py
+++ langchain/utils.py
@@ -4,7 +4,7 @@ from typing import Any, Dict, Optional
 
 
 def get_from_dict_or_env(
-    data: Dict[str, Any], key: str, env_key: str, default: Optional[Any] = None
+    data: Dict[str, Any], key: str, env_key: str, default: Optional[str] = None
 ) -> str:
     """Get a value from a dictionary or an environment variable."""
     if key in data and data[key]:
