f313f6a2d9ee5c6366924efa68092f815b0941b8
Harrison Chase <hw.chase.17@gmail.com>
2023-01-21 17:39:15 -0800

    openai callback

--- langchain/callbacks/openai_info.py
+++ langchain/callbacks/openai_info.py
@@ -2,13 +2,18 @@
 from typing import Any, Dict, List, Optional
 
 from langchain.callbacks.base import BaseCallbackHandler
-from langchain.input import print_text
 from langchain.schema import AgentAction, AgentFinish, LLMResult
 
 
 class OpenAICallbackHandler(BaseCallbackHandler):
     """Callback Handler that tracks OpenAI info."""
+
     total_tokens: int = 0
+
+    @property
+    def always_verbose(self) -> bool:
+        """Whether to call verbose callbacks even if verbose is False."""
+        return True
 
     def on_llm_start(
         self, serialized: Dict[str, Any], prompts: List[str], **kwargs: Any
@@ -16,13 +21,15 @@ class OpenAICallbackHandler(BaseCallbackHandler):
         """Print out the prompts."""
         pass
 
-    def on_llm_end(self, response: LLMResult) -> None:
+    def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
         """Do nothing."""
-        if "token_usage" in response.llm_output:
-            if "total_tokens" in response.llm_output["token_usage"]:
-                self.total_tokens += response.llm_output["token_usage"]["total_tokens"]
+        if response.llm_output is not None:
+            if "token_usage" in response.llm_output:
+                token_usage = response.llm_output["token_usage"]
+                if "total_tokens" in token_usage:
+                    self.total_tokens += token_usage["total_tokens"]
 
-    def on_llm_error(self, error: Exception) -> None:
+    def on_llm_error(self, error: Exception, **kwargs: Any) -> None:
         """Do nothing."""
         pass
 
@@ -32,11 +39,11 @@ class OpenAICallbackHandler(BaseCallbackHandler):
         """Print out that we are entering a chain."""
         pass
 
-    def on_chain_end(self, outputs: Dict[str, Any]) -> None:
+    def on_chain_end(self, outputs: Dict[str, Any], **kwargs: Any) -> None:
         """Print out that we finished a chain."""
         pass
 
-    def on_chain_error(self, error: Exception) -> None:
+    def on_chain_error(self, error: Exception, **kwargs: Any) -> None:
         """Do nothing."""
         pass
 
@@ -61,7 +68,7 @@ class OpenAICallbackHandler(BaseCallbackHandler):
         """If not the final action, print out observation."""
         pass
 
-    def on_tool_error(self, error: Exception) -> None:
+    def on_tool_error(self, error: Exception, **kwargs: Any) -> None:
         """Do nothing."""
         pass
 
