6beac9c69822d7da0dba91c6aa076fc1a1c13d44
Brendan <brendan_leahey@brown.edu>
2023-01-22 09:40:00 -0800

    Adding todos and exceptionhandler class

---
+++ langchain/chains/sql_database/exception_handler.py
@@ -0,0 +1,12 @@
+"""
+TODO: Depending on how many exceptions end up being handled, this could be
+moved to a method in the SQLDatabaseChain class.
+"""
+
+class ExceptionHandler:
+    """
+    Class for handling exceptions for the SQLDatabaseChain class.
+    
+    see list of exceptions here: 
+    https://docs.sqlalchemy.org/en/20/core/exceptions.html
+    """
