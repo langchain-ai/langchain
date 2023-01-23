9ea504d067bd94804b013b8a460a8003658e9c41
Harrison Chase <hw.chase.17@gmail.com>
2023-01-22 23:37:01 -0800

    Harrison/openai callback (#684)

--- langchain/callbacks/__init__.py
+++ langchain/callbacks/__init__.py
@@ -1,5 +1,9 @@
 """Callback handlers that allow listening to events in LangChain."""
+from contextlib import contextmanager
+from typing import Generator
+
 from langchain.callbacks.base import BaseCallbackHandler, BaseCallbackManager
+from langchain.callbacks.openai_info import OpenAICallbackHandler
 from langchain.callbacks.shared import SharedCallbackManager
 from langchain.callbacks.stdout import StdOutCallbackHandler
 
@@ -18,3 +22,13 @@ def set_handler(handler: BaseCallbackHandler) -> None:
 def set_default_callback_manager() -> None:
     """Set default callback manager."""
     set_handler(StdOutCallbackHandler())
+
+
+@contextmanager
+def get_openai_callback() -> Generator[OpenAICallbackHandler, None, None]:
+    """Get OpenAI callback handler in a context manager."""
+    handler = OpenAICallbackHandler()
+    manager = get_callback_manager()
+    manager.add_handler(handler)
+    yield handler
+    manager.remove_handler(handler)
