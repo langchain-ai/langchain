0ffeabd14fe25ae3cea51a965f142e39c4d256a0
Harrison Chase <hw.chase.17@gmail.com>
2023-01-24 21:36:19 -0800

    Harrison/serialize llm chain (#671)

--- langchain/chains/__init__.py
+++ langchain/chains/__init__.py
@@ -6,6 +6,7 @@ from langchain.chains.llm_bash.base import LLMBashChain
 from langchain.chains.llm_checker.base import LLMCheckerChain
 from langchain.chains.llm_math.base import LLMMathChain
 from langchain.chains.llm_requests import LLMRequestsChain
+from langchain.chains.loading import load_chain
 from langchain.chains.mapreduce import MapReduceChain
 from langchain.chains.moderation import OpenAIModerationChain
 from langchain.chains.pal.base import PALChain
@@ -39,4 +40,5 @@ __all__ = [
     "MapReduceChain",
     "OpenAIModerationChain",
     "SQLDatabaseSequentialChain",
+    "load_chain",
 ]
