69b156a00cb5fe1c7b003cb74dba280cab6b04c1
Smit Shah <who828@gmail.com>
2023-01-26 02:35:44 +0530

    Order imports correctly

--- langchain/chains/qa_with_sources/vector_db.py
+++ langchain/chains/qa_with_sources/vector_db.py
@@ -1,14 +1,13 @@
 """Question-answering with sources over a vector database."""
-import sys
 
 from typing import Any, Dict, List
 
 from pydantic import BaseModel, Field
 
 from langchain.chains.qa_with_sources.base import BaseQAWithSourcesChain
+from langchain.chains.combine_documents.stuff import StuffDocumentsChain
 from langchain.docstore.document import Document
 from langchain.vectorstores.base import VectorStore
-from langchain.chains.combine_documents.stuff import StuffDocumentsChain
 
 
 class VectorDBQAWithSourcesChain(BaseQAWithSourcesChain, BaseModel):
