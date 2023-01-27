cb55ad23ab0383ad51152384083571cd7905e163
Harrison Chase <hw.chase.17@gmail.com>
2023-01-26 19:48:47 -0800

    (wip) Harrison/serialize agents (#725)

--- langchain/agents/loading.py
+++ langchain/agents/loading.py
@@ -1,14 +1,16 @@
-"""Load agent."""
-from typing import Any, List, Optional
+"""Functionality for loading agents."""
+import json
+from pathlib import Path
+from typing import Any, Union
 
-from langchain.agents.agent import AgentExecutor
+import yaml
+
+from langchain.agents.agent import Agent
 from langchain.agents.conversational.base import ConversationalAgent
 from langchain.agents.mrkl.base import ZeroShotAgent
 from langchain.agents.react.base import ReActDocstoreAgent
 from langchain.agents.self_ask_with_search.base import SelfAskWithSearchAgent
-from langchain.agents.tools import Tool
-from langchain.callbacks.base import BaseCallbackManager
-from langchain.llms.base import BaseLLM
+from langchain.chains.loading import load_chain, load_chain_from_config
 
 AGENT_TO_CLASS = {
     "zero-shot-react-description": ZeroShotAgent,
@@ -18,42 +20,41 @@ AGENT_TO_CLASS = {
 }
 
 
-def initialize_agent(
-    tools: List[Tool],
-    llm: BaseLLM,
-    agent: str = "zero-shot-react-description",
-    callback_manager: Optional[BaseCallbackManager] = None,
-    **kwargs: Any,
-) -> AgentExecutor:
-    """Load agent given tools and LLM.
+def load_agent_from_config(config: dict, **kwargs: Any) -> Agent:
+    """Load agent from Config Dict."""
+    if "_type" not in config:
+        raise ValueError("Must specify an agent Type in config")
+    config_type = config.pop("_type")
 
-    Args:
-        tools: List of tools this agent has access to.
-        llm: Language model to use as the agent.
-        agent: The agent to use. Valid options are:
-            `zero-shot-react-description`
-            `react-docstore`
-            `self-ask-with-search`
-            `conversational-react-description`.
-        callback_manager: CallbackManager to use. Global callback manager is used if
-            not provided. Defaults to None.
-        **kwargs: Additional key word arguments to pass to the agent.
+    if config_type not in AGENT_TO_CLASS:
+        raise ValueError(f"Loading {config_type} agent not supported")
 
-    Returns:
-        An agent.
-    """
-    if agent not in AGENT_TO_CLASS:
-        raise ValueError(
-            f"Got unknown agent type: {agent}. "
-            f"Valid types are: {AGENT_TO_CLASS.keys()}."
-        )
-    agent_cls = AGENT_TO_CLASS[agent]
-    agent_obj = agent_cls.from_llm_and_tools(
-        llm, tools, callback_manager=callback_manager
-    )
-    return AgentExecutor.from_agent_and_tools(
-        agent=agent_obj,
-        tools=tools,
-        callback_manager=callback_manager,
-        **kwargs,
-    )
+    agent_cls = AGENT_TO_CLASS[config_type]
+    if "llm_chain" in config:
+        config["llm_chain"] = load_chain_from_config(config.pop("llm_chain"))
+    elif "llm_chain_path" in config:
+        config["llm_chain"] = load_chain(config.pop("llm_chain_path"))
+    else:
+        raise ValueError("One of `llm_chain` and `llm_chain_path` should be specified.")
+    combined_config = {**config, **kwargs}
+    return agent_cls(**combined_config)  # type: ignore
+
+
+def load_agent(file: Union[str, Path], **kwargs: Any) -> Agent:
+    """Load agent from file."""
+    # Convert file to Path object.
+    if isinstance(file, str):
+        file_path = Path(file)
+    else:
+        file_path = file
+    # Load from either json or yaml.
+    if file_path.suffix == ".json":
+        with open(file_path) as f:
+            config = json.load(f)
+    elif file_path.suffix == ".yaml":
+        with open(file_path, "r") as f:
+            config = yaml.safe_load(f)
+    else:
+        raise ValueError("File type must be json or yaml")
+    # Load the agent from the config now.
+    return load_agent_from_config(config, **kwargs)
