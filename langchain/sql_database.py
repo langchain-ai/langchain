f58141f5ed81976e8620bd1a19fdc0f091fda91f
Francisco Ingham <fpingham@gmail.com>
2023-01-28 13:32:05 -0300

    Sample row in table info for SQLDatabase (#769)
    The agents usually benefit from understanding what the data looks like
    to be able to filter effectively. Sending just one row in the table info
    allows the agent to understand the data before querying and get better
    results.
    
    ---------
    
    Co-authored-by: Francisco Ingham <>

--- langchain/sql_database.py
+++ langchain/sql_database.py
@@ -16,6 +16,7 @@ class SQLDatabase:
         schema: Optional[str] = None,
         ignore_tables: Optional[List[str]] = None,
         include_tables: Optional[List[str]] = None,
+        sample_row_in_table_info: bool = False,
     ):
         """Create engine from database URI."""
         self._engine = engine
@@ -39,6 +40,7 @@ class SQLDatabase:
                 raise ValueError(
                     f"ignore_tables {missing_tables} not found in database"
                 )
+        self._sample_row_in_table_info = sample_row_in_table_info
 
     @classmethod
     def from_uri(cls, database_uri: str, **kwargs: Any) -> SQLDatabase:
@@ -69,14 +71,31 @@ class SQLDatabase:
             if missing_tables:
                 raise ValueError(f"table_names {missing_tables} not found in database")
             all_table_names = table_names
+
         template = "Table '{table_name}' has columns: {columns}."
+
+        if self._sample_row_in_table_info:
+            template += (
+                " Here is an example row for this table"
+                " (long strings are truncated): {sample_row}."
+            )
+
         tables = []
         for table_name in all_table_names:
+            if self._sample_row_in_table_info:
+                sample_row = self.run(f"SELECT * FROM '{table_name}' LIMIT 1")
+                sample_row = " ".join([str(i)[:100] for i in eval(sample_row)[0]])
+
             columns = []
             for column in self._inspector.get_columns(table_name, schema=self._schema):
                 columns.append(f"{column['name']} ({str(column['type'])})")
             column_str = ", ".join(columns)
-            table_str = template.format(table_name=table_name, columns=column_str)
+            if self._sample_row_in_table_info:
+                table_str = template.format(
+                    table_name=table_name, columns=column_str, sample_row=sample_row
+                )
+            else:
+                table_str = template.format(table_name=table_name, columns=column_str)
             tables.append(table_str)
         return "\n".join(tables)
 
