d2c449dc13a42da61a865598f8eb226071695820
Harrison Chase <hw.chase.17@gmail.com>
2023-01-28 18:17:30 -0800

    cr

--- langchain/agents/tools.py
+++ langchain/agents/tools.py
@@ -1,8 +1,7 @@
 """Interface for tools."""
-import functools
 from dataclasses import dataclass
-from inspect import getfullargspec, signature
-from typing import Callable, Optional, Union
+from inspect import signature
+from typing import Any, Callable, Optional, Union
 
 
 @dataclass
@@ -14,14 +13,15 @@ class Tool:
     description: Optional[str] = None
     return_direct: bool = False
 
-    def __call__(self, *args: str, **kwargs: str) -> str:
+    def __call__(self, *args: Any, **kwargs: Any) -> str:
+        """Make tools callable by piping through to `func`."""
         return self.func(*args, **kwargs)
 
 
 def tool(
     *args: Union[str, Callable], return_direct: bool = False
 ) -> Union[Callable, Tool]:
-    """Decorator to make tools out of functions, can be used with or without arguments.
+    """Make tools out of functions, can be used with or without arguments.
 
     Requires:
         - Function must be of type (str) -> str
@@ -41,7 +41,7 @@ def tool(
                 return
     """
 
-    def _make_with_name(tool_name: str, return_direct: bool = False) -> Callable:
+    def _make_with_name(tool_name: str) -> Callable:
         def _make_tool(func: Callable[[str], str]) -> Tool:
             assert func.__doc__, "Function must have a docstring"
             # Description example:
@@ -60,16 +60,16 @@ def tool(
     if len(args) == 1 and isinstance(args[0], str):
         # if the argument is a string, then we use the string as the tool name
         # Example usage: @tool("search", return_direct=True)
-        return _make_with_name(args[0], return_direct=return_direct)
+        return _make_with_name(args[0])
     elif len(args) == 1 and callable(args[0]):
         # if the argument is a function, then we use the function name as the tool name
         # Example usage: @tool
-        return _make_with_name(args[0].__name__, return_direct=return_direct)(args[0])
+        return _make_with_name(args[0].__name__)(args[0])
     elif len(args) == 0:
         # if there are no arguments, then we use the function name as the tool name
         # Example usage: @tool(return_direct=True)
         def _partial(func: Callable[[str], str]) -> Tool:
-            return _make_with_name(func.__name__, return_direct=return_direct)(func)
+            return _make_with_name(func.__name__)(func)
 
         return _partial
     else:
