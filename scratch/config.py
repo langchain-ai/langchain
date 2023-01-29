b10e42483ff0d5fb9c0cecccde1fed668d4a1785
Brendan <brendan_leahey@brown.edu>
2023-01-29 13:44:29 -0500

    small change

---
+++ scratch/config.py
@@ -0,0 +1,6 @@
+class config:
+    # OpenAI API Key
+    # https://beta.openai.com/account/api-keys
+    # https://beta.openai.com/docs/api-reference/authentication
+    # https://beta.openai.com/docs/api-reference/authentication#authentication
+    OPENAI_API_KEY="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"
