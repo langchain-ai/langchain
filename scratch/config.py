8ebc4ced03b57b5a20c6336cfc9b832738c60b9e
Brendan <brendan_leahey@brown.edu>
2023-01-29 13:58:54 -0500

    fixed change

---
+++ scratch/config.py
@@ -0,0 +1,11 @@
+import os
+
+class config:
+    def __init__(self):
+        self.OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+    # OpenAI API Key
+    # https://beta.openai.com/account/api-keys
+    # https://beta.openai.com/docs/api-reference/authentication
+    # https://beta.openai.com/docs/api-reference/authentication#authentication
+    OPENAI_API_KEY : str
