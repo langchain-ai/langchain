b30ed2621fe7aaf5d6fae48df50e101045695d6d
Sean Spriggens <ssprigge@syr.edu>
2023-01-30 12:08:55 -0500

    add unit tests and fix ngram_overlap_score output type

--- langchain/prompts/example_selector/ngram_overlap.py
+++ langchain/prompts/example_selector/ngram_overlap.py
@@ -2,7 +2,7 @@
     https://www.nltk.org/_modules/nltk/translate/bleu_score.html
     https://aclanthology.org/P02-1040.pdf
     """
-from typing import Dict, List, Tuple
+from typing import Any, Dict, List, Tuple
 
 import numpy as np
 from nltk.translate.bleu_score import SmoothingFunction, sentence_bleu
@@ -11,8 +11,9 @@ from pydantic import BaseModel, validator
 from langchain.prompts.example_selector.base import BaseExampleSelector
 from langchain.prompts.prompt import PromptTemplate
 
-def ngram_overlap_score(source: List[str], example: List[str]) -> float:
-    """Computes ngram overlap score (1<= n <=4) of source and example as sentence_bleu score:
+def ngram_overlap_score(source: List[str], example: list[str]) -> Any:
+    """Computes ngram overlap score (1<= n <=4) of source and example as sentence_bleu score (0<= score <=1).
+        Uses sentence_bleu with method1 smoothing function and auto reweighting.
     https://www.nltk.org/_modules/nltk/translate/bleu_score.html
     https://aclanthology.org/P02-1040.pdf
     """
@@ -37,27 +38,33 @@ class NGramOverlapExampleSelector(BaseExampleSelector, BaseModel):
     example_prompt: PromptTemplate
     """Prompt template used to format the examples."""
 
-    threshold: float = 0.0
-    """Threshold score, at which algorithm stops."""
+    threshold: float = -1.0
+    """Threshold at which algorithm stops. Set to -1.0 by default.
+        If threshold is negative, select_examples will not exclude any example, only sort them by ngram_overlap_score.
+        If threshold is greater than 1.0, select_examples will exclude all examples and return an empty list.
+        If threshold is set to 0.0, select_examples will exclude examples with no ngram overlap with input.
+    """
 
     def add_example(self, example: Dict[str, str]) -> None:
         """Add new example to list."""
         self.examples.append(example)
 
     def select_examples(self, input_variables: Dict[str, str]) -> List[dict]:
-        """Select examples and ordering based on ngram overlap."""
+        """Returns list of examples sorted, by ngram_overlap_score with input, in descending order.
+            Excludes any examples whose ngram_overlap_score is less than or equal to threshold.
+        """
         inputs = list(input_variables.values())
         examples = []
-        print(f"inputs={inputs}")
+        print(f"inputs={inputs},type={type(inputs)}")
         k = len(self.examples)
         score = [0] * k
+        first_prompt_template_key = self.example_prompt.input_variables[0]
+        print(f"type={type(first_prompt_template_key)}")
         for i in range(k):
             score[i] = ngram_overlap_score(
-                inputs, [self.examples[i][self.example_prompt.input_variables[0]]]
+                inputs, [self.examples[i][first_prompt_template_key]]           
             )
-            print(type([self.examples[i][self.example_prompt.input_variables[0]]]))
-            print([self.examples[i][self.example_prompt.input_variables[0]]])
-            print(score[i])
+        print(score)
 
         while True:
             arg_max = np.argmax(score)
@@ -66,6 +73,7 @@ class NGramOverlapExampleSelector(BaseExampleSelector, BaseModel):
 
             examples.append(self.examples[arg_max])
             print(examples)
-            score[arg_max] = -1
+            score[arg_max] = self.threshold - 1
+            print(score)
 
         return examples
--- tests/unit_tests/prompts/test_ngram_overlap_example_selector.py
+++ tests/unit_tests/prompts/test_ngram_overlap_example_selector.py
@@ -2,7 +2,7 @@
 
 import pytest
 
-from langchain.prompts.example_selector.ngram_overlap import NGramOverlapExampleSelector
+from langchain.prompts.example_selector.ngram_overlap import NGramOverlapExampleSelector, ngram_overlap_score
 from langchain.prompts.prompt import PromptTemplate
 
 EXAMPLES = [
@@ -29,7 +29,7 @@ def test_selector_valid(selector: NGramOverlapExampleSelector) -> None:
     """Test NGramOverlapExampleSelector can select examples."""
     sentence = "Spot can run."
     output = selector.select_examples({"input": sentence})
-    assert output == [EXAMPLES[2], EXAMPLES[0]]
+    assert output == [EXAMPLES[2], EXAMPLES[0], EXAMPLES[1]]
 
 
 def test_selector_add_example(selector: NGramOverlapExampleSelector) -> None:
@@ -38,4 +38,30 @@ def test_selector_add_example(selector: NGramOverlapExampleSelector) -> None:
     selector.add_example(new_example)
     sentence = "Spot can run."
     output = selector.select_examples({"input": sentence})
-    assert output == [EXAMPLES[2], EXAMPLES[0]] + [new_example]
+    assert output == [EXAMPLES[2], EXAMPLES[0]] + [new_example] +[EXAMPLES[1]]
+
+def test_selector_threshold_zero(selector: NGramOverlapExampleSelector) -> None:
+    """Tests that NGramOverlapExampleSelector excludes examples with no ngram overlap with input, if threshold is set to 0.0."""
+    selector.threshold = 0.0
+    sentence = "Spot can run."
+    output = selector.select_examples({"input": sentence})
+    assert output == [EXAMPLES[2], EXAMPLES[0]]
+
+def test_selector_threshold_more_than_one(selector: NGramOverlapExampleSelector) -> None:
+    """Tests that NGramOverlapExampleSelector excludes all examples, if threshold is greater than 1.0."""
+    selector.threshold = 1.0 + 1e-9
+    sentence = "Spot can run."
+    output = selector.select_examples({"input": sentence})
+    assert output == []
+
+def test_ngram_overlap_score(selector: NGramOverlapExampleSelector) -> None:
+    """Tests that ngram_overlap_score returns 0.0 for no ngram overlap, 1.0 for complete ngram overlap, and between 0.0 and 1.0 for some ngram overlap."""
+    selector.threshold = 1.0 + 1e-9
+    none = ngram_overlap_score(["Spot can run."], ["My dog barks."])
+    some =  ngram_overlap_score(["Spot can run."], ["See Spot run."])
+    complete = ngram_overlap_score(["Spot can run."], ["Spot can run."]) 
+
+    check = [abs(none-0.0) < 1e-9, 0.0 < some < 1.0, abs(complete-1.0) < 1e-9] 
+    assert check == [True, True, True]
+
+
