6aa428e35b7d936e7d6d557408a7d04c533a2e77
jzluo <20971593+jzluo@users.noreply.github.com>
2023-01-31 17:45:00 -0500

    choose number of sample rows in table info for SQLDatabase

--- langchain/sql_database.py
+++ langchain/sql_database.py
@@ -16,7 +16,7 @@ class SQLDatabase:
         schema: Optional[str] = None,
         ignore_tables: Optional[List[str]] = None,
         include_tables: Optional[List[str]] = None,
-        sample_row_in_table_info: bool = False,
+        sample_rows_in_table_info: int = 0,
     ):
         """Create engine from database URI."""
         self._engine = engine
@@ -40,7 +40,7 @@ class SQLDatabase:
                 raise ValueError(
                     f"ignore_tables {missing_tables} not found in database"
                 )
-        self._sample_row_in_table_info = sample_row_in_table_info
+        self._sample_rows_in_table_info = sample_rows_in_table_info
 
     @classmethod
     def from_uri(cls, database_uri: str, **kwargs: Any) -> SQLDatabase:
@@ -83,15 +83,25 @@ class SQLDatabase:
             column_str = ", ".join(columns)
             table_str = template.format(table_name=table_name, columns=column_str)
 
-            if self._sample_row_in_table_info:
+            if self._sample_rows_in_table_info:
                 row_template = (
-                    " Here is an example row for this table"
-                    " (long strings are truncated): {sample_row}."
+                    " Here is an example of {n_rows} rows from this table "
+                    "(long strings are truncated):\n"
+                    "{sample_rows}"
                 )
-                sample_row = self.run(f"SELECT * FROM '{table_name}' LIMIT 1")
-                if len(eval(sample_row)) > 0:
-                    sample_row = " ".join([str(i)[:100] for i in eval(sample_row)[0]])
-                    table_str += row_template.format(sample_row=sample_row)
+                sample_rows = self.run(
+                    f"SELECT * FROM '{table_name}' LIMIT "
+                    f"{self._sample_rows_in_table_info}"
+                )
+                sample_rows = eval(sample_rows)
+                if len(sample_rows) > 0:
+                    n_rows = len(sample_rows)
+                    sample_rows = "\n".join(
+                        [" ".join([str(i)[:100] for i in row]) for row in sample_rows]
+                    )
+                    table_str += row_template.format(
+                        n_rows=n_rows, sample_rows=sample_rows
+                    )
 
             tables.append(table_str)
         return "\n".join(tables)
