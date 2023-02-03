313fd40fae5ae1a0c18fe9bd5670f5f684a320d1
John (Jet Token) <churchjohnr@protonmail.com>
2023-02-03 02:37:27 -0800

    guard directive

--- langchain/utilities/guards.py
+++ langchain/utilities/guards.py
@@ -1,9 +1,10 @@
 """A utilities for guarding the output of a given chain or agent.
 """
 
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from typing import Any, Callable, List
 
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
 from langchain.utilities.normalization import normalize_boolean_output
 
 
@@ -14,7 +15,7 @@ class Guard:
         llm (LLM): The LLM to be used to guard the output of the chain or agent.
         restrictions (list): A list of strings that describe the restrictions that the output of the chain or agent must conform to.
         retries (int, optional): The number of times the chain or agent should be called recursively if the output violates the restrictions. Defaults to 0.
-    
+
     Raises:
         Exception: If the output violates the restrictions and the maximum number of retries has been exceeded.
 
@@ -40,8 +41,8 @@ class Guard:
     """
 
     prompt = PromptTemplate(
-    input_variables=["function_output", "restrictions"],
-    template="""Determine if the output of a function violates any restrictions. If the output violates any of the restrictions, respond with your reason followed by ¥. If the output does not violate any restrictions, respond with your reason followed by ƒ. If you are unsure, respond with your reason followed by ƒ.
+        input_variables=["function_output", "restrictions"],
+        template="""Determine if the output of a function violates any restrictions. If the output violates any of the restrictions, respond with your reason followed by ¥. If the output does not violate any restrictions, respond with your reason followed by ƒ. If you are unsure, respond with your reason followed by ƒ.
 
     output: Lorum ipsum dolor sit amet
     restrictions: must not include profanity
@@ -59,34 +60,48 @@ class Guard:
     restrictions:{restrictions}
     response:
     """,
-    # TODO: Should probably replace some of the n-shot examples with more realistic ones about race and profanity. Not sure about the idea of introducing racist and profane sentences into the repo for that purpose?
+        # TODO: Should probably replace some of the n-shot examples with more realistic ones about race and profanity. Not sure about the idea of introducing racist and profane sentences into the repo for that purpose?
     )
 
-    def __init__(self, llm, restrictions, retries=0):
+    def __init__(
+        self, llm: Callable, restrictions: List[str], retries: int = 0
+    ) -> None:
         self.llm = llm
         self.restrictions = restrictions
         self.retries = retries
 
-    def __call__(self, func):
-        def wrapper(*args, **kwargs):
+    def __call__(self, func: Callable) -> Callable:
+        def wrapper(*args: Any, **kwargs: Any) -> str:
             guard_chain = LLMChain(llm=self.llm, prompt=self.prompt)
             if self.retries < 0:
                 raise Exception("Restriction violated. Maximum retries exceeded.")
             try:
-                concatenated_restrictions = ', '.join(self.restrictions)
+                concatenated_restrictions = ", ".join(self.restrictions)
                 result = func(*args, **kwargs)
                 # guard chain returns true in case of a violation.
-                guard_chain_output = guard_chain.run(function_output=result, restrictions=concatenated_restrictions)
-                if normalize_boolean_output(guard_chain_output, true_values=["¥"], false_values=["ƒ"]):
-                    raise Exception("Restriction violated. Attempted answer: " + result + ". Reasoning: " + guard_chain_output + "." )
+                guard_chain_output = guard_chain.run(
+                    function_output=result, restrictions=concatenated_restrictions
+                )
+                # rare characters are used for normalization so that an explanation can be included but easily be filtered out without worrying about the explanation containing the same characters as the true or false values.
+                if normalize_boolean_output(
+                    guard_chain_output, true_values=["¥"], false_values=["ƒ"]
+                ):
+                    raise Exception(
+                        "Restriction violated. Attempted answer: "
+                        + result
+                        + ". Reasoning: "
+                        + guard_chain_output
+                        + "."
+                    )
                 else:
                     return result
             except Exception as e:
                 # if template is in args add restriction to the end of the template
                 if "template" in kwargs:
-                    kwargs["template"] = kwargs["template"] + f"the response {self.restrictions[0]}"
+                    kwargs["template"] = (
+                        kwargs["template"] + f"the response {self.restrictions[0]}"
+                    )
                 self.retries = self.retries - 1
                 return wrapper(*args, **kwargs)
+
         return wrapper
-
-
