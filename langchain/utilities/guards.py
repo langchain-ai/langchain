ad545db6813614e1023e3545dd04457b65221645
John (Jet Token) <churchjohnr@protonmail.com>
2023-02-04 17:08:01 -0800

    Add custom guard, base class

--- langchain/utilities/guards.py
+++ langchain/utilities/guards.py
@@ -1,19 +1,82 @@
 """A utilities for guarding the output of a given chain or agent.
 """
 
-from typing import Any, Callable, List
+from typing import Any, Callable, List, Tuple
 
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from langchain.utilities.normalization import normalize_boolean_output
 
 
-class Guard:
-    """The Guard class is a decorator that can be applied to any chain or agent to either throw an error or recursively call the chain or agent when the output of said chain or agent conforms to one or more restrictions.
+class BaseGuard:
+    """The Guard class is a decorator that can be applied to any chain or agent to either throw an error or recursively call the chain or agent when the output of said chain or agent violates the rules of the guard. The BaseGuard alone does nothing but can be subclassed an the resolve_guard function overwritten to create more specific guards.
+
+    Args:
+        retries (int, optional): The number of times the chain or agent should be called recursively if the output violates the restrictions. Defaults to 0.
+
+    Raises:
+        Exception: If the output violates the restrictions and the maximum number of retries has been exceeded.
+
+    """
+
+    def __init__(self, retries: int = 0, *args: Any, **kwargs: Any) -> None:
+        self.retries = retries
+
+    def resolve_guard(
+        self, llm_response: str, *args: Any, **kwargs: Any
+    ) -> Tuple[bool, str]:
+        """Function to determine if guard was violated (if response should be blocked)
+        Can be overwritten when subclassing Guard class to expand on guard functionality
+
+        Args:
+            llm_response (str): the llm_response string to be tested against the guard.
+
+        Returns:
+            tuple:
+                bool: True if guard was violated, False otherwise.
+                str: The message to be displayed when the guard is violated (if guard was violated).
+        """
+        return False, ""
+
+    def handle_violation(self, message: str, *args: Any, **kwargs: Any) -> Exception:
+        """Function to handle violation of guard.
+
+        Args:
+            message (str): the message to be displayed when the guard is violated.
+
+        Raises:
+            Exception: the message passed to the function.
+        """
+        raise Exception(message)
+
+    def __call__(self, func: Callable) -> Callable:
+        def wrapper(*args: Any, **kwargs: Any) -> str:
+            if self.retries < 0:
+                raise Exception("Restriction violated. Maximum retries exceeded.")
+            try:
+                llm_response = func(*args, **kwargs)
+                guard_result, violation_message = self.resolve_guard(llm_response)
+                if guard_result:
+                    self.handle_violation(violation_message)
+                else:
+                    return llm_response
+            except Exception as e:
+                self.retries = self.retries - 1
+                # Check retries to avoid infinite recursion if exception is something other than a violation of the guard
+                if self.retries >= 0:
+                    return wrapper(*args, **kwargs)
+                else:
+                    raise e
+
+        return wrapper
+
+
+class RestrictionGuard(BaseGuard):
+    """The RestrictionGuard class is a decorator that can be applied to any chain or agent to either throw an error or recursively call the chain or agent when the output of said chain or agent violates one or more restrictions.
 
     Args:
         llm (LLM): The LLM to be used to guard the output of the chain or agent.
-        restrictions (list): A list of strings that describe the restrictions that the output of the chain or agent must conform to.
+        restrictions (list): A list of strings that describe the restrictions that the output of the chain or agent must conform to. The restrictions should be in the form of "must not x" or "must x" for best results.
         retries (int, optional): The number of times the chain or agent should be called recursively if the output violates the restrictions. Defaults to 0.
 
     Raises:
@@ -32,7 +95,7 @@ class Guard:
             )
             chain = LLMChain(llm=OpenAI(), prompt=prompt)
 
-            @Guard(llm=llm, restrictions=['must not include profanity', 'must not mention race.'], retries=1)
+            @RestrictionGuard(llm=llm, restrictions=['must not include profanity', 'must not mention race.'], retries=1)
             def call_chain():
                 return chain.run(adjective="political")
 
@@ -42,7 +105,7 @@ class Guard:
 
     prompt = PromptTemplate(
         input_variables=["function_output", "restrictions"],
-        template="""Determine if the output of a function violates any restrictions. If the output violates any of the restrictions, respond with your reason followed by ¥. If the output does not violate any restrictions, respond with your reason followed by ƒ. If you are unsure, respond with your reason followed by ƒ.
+        template="""Determine if the output of a function violates any restrictions. If the output violates any of the restrictions, respond with your reason followed by ¥. If the output does not violate any restrictions, respond with your reason followed by ƒ.
 
     output: Lorum ipsum dolor sit amet
     restrictions: must not include profanity
@@ -70,33 +133,105 @@ class Guard:
         self.restrictions = restrictions
         self.retries = retries
 
-    def __call__(self, func: Callable) -> Callable:
-        def wrapper(*args: Any, **kwargs: Any) -> str:
-            guard_chain = LLMChain(llm=self.llm, prompt=self.prompt)
-            if self.retries < 0:
-                raise Exception("Restriction violated. Maximum retries exceeded.")
-            try:
-                concatenated_restrictions = ", ".join(self.restrictions)
-                result = func(*args, **kwargs)
-                # guard chain returns true in case of a violation.
-                guard_chain_output = guard_chain.run(
-                    function_output=result, restrictions=concatenated_restrictions
-                )
-                # rare characters are used for normalization so that an explanation can be included but easily be filtered out without worrying about the explanation containing the same characters as the true or false values.
-                if normalize_boolean_output(
-                    guard_chain_output, true_values=["¥"], false_values=["ƒ"]
-                ):
-                    raise Exception(
-                        "Restriction violated. Attempted answer: "
-                        + result
-                        + ". Reasoning: "
-                        + guard_chain_output
-                        + "."
-                    )
-                else:
-                    return result
-            except Exception as e:
-                self.retries = self.retries - 1
-                return wrapper(*args, **kwargs)
+    def resolve_guard(
+        self, llm_response: str, *args: Any, **kwargs: Any
+    ) -> Tuple[bool, str]:
+        """Function to determine if guard was violated. Uses a custom guard chain to determine is a set of restrictions was violated.
 
-        return wrapper
+        Args:
+            llm_response (str): the llm_response string to be tested against the guard.
+
+        Returns:
+            tuple:
+                bool: True if guard was violated, False otherwise.
+                str: The message to be displayed when the guard is violated (if guard was violated).
+        """
+        guard_chain = LLMChain(llm=self.llm, prompt=self.prompt)
+        concatenated_restrictions = ", ".join(self.restrictions)
+
+        # guard chain returns true in case of a violation.
+        guard_chain_output = guard_chain.run(
+            function_output=llm_response, restrictions=concatenated_restrictions
+        )
+        violation_message = (
+            "Restriction violated. Attempted answer: "
+            + llm_response
+            + ". Reasoning: "
+            + guard_chain_output
+            + "."
+        )
+        # rare characters are used for normalization so that an explanation can be included but easily be filtered out without worrying about the explanation containing the same characters as the true or false values.
+        return (
+            normalize_boolean_output(
+                guard_chain_output, true_values=["¥"], false_values=["ƒ"]
+            ),
+            violation_message,
+        )
+
+
+class CustomGuard(BaseGuard):
+    """The CustomGuard class is a decorator that can be applied to any chain or agent to either throw an error or recursively call the chain or agent when the output of said chain or agent violates a provided guard function
+
+    Args:
+        guard_function (func): The function to be used to guard the output of the chain or agent. The function should take the output of the chain or agent as its only argument and return a boolean value where True means the guard has been violated. Optionally, return a tuple where the first element is a boolean value and the second element is a string that will be displayed when the guard is violated. If the string is ommited the default message will be used.
+        retries (int, optional): The number of times the chain or agent should be called recursively if the output violates the restrictions. Defaults to 0.
+
+    Raises:
+        Exception: If the output violates the restrictions and the maximum number of retries has been exceeded.
+
+    Example:
+        .. code-block:: python
+
+            from langchain import LLMChain, OpenAI, PromptTemplate
+
+            llm = OpenAI(temperature=0.9)
+
+            prompt_template = "Tell me a {adjective} joke"
+            prompt = PromptTemplate(
+                input_variables=["adjective"], template=prompt_template
+            )
+            chain = LLMChain(llm=OpenAI(), prompt=prompt)
+
+            def is_long(llm_output):
+                return len(llm_output) > 100
+
+            @CustomGuard(guard_function=is_long, retries=1)
+            def call_chain():
+                return chain.run(adjective="political")
+
+            call_chain()
+
+    """
+
+    def __init__(self, guard_function: Callable, retries: int = 0) -> None:
+        self.guard_function = guard_function
+        self.retries = retries
+
+    def resolve_guard(
+        self, llm_response: str, *args: Any, **kwargs: Any
+    ) -> Tuple[bool, str]:
+        """Function to determine if guard was violated. Uses custom guard function.
+
+        Args:
+            llm_response (str): the llm_response string to be tested against the guard.
+
+        Returns:
+            tuple:
+                bool: True if guard was violated, False otherwise.
+                str: The message to be displayed when the guard is violated (if guard was violated).
+        """
+        response = self.guard_function(llm_response)
+
+        if type(response) is tuple:
+            boolean_output, message = response
+            violation_message = message
+        elif type(response) is bool:
+            boolean_output = response
+            violation_message = (
+                "Restriction violated. Attempted answer: " + llm_response + "."
+            )
+        else:
+            raise Exception(
+                "Custom guard function must return either a boolean or a tuple of a boolean and a string."
+            )
+        return (boolean_output, violation_message)
