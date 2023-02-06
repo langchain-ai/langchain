e8a4c88b5236a914d1a6b9e16409b92cb7db4c91
John (Jet Token) <churchjohnr@protonmail.com>
2023-02-06 13:05:51 -0800

    forgot import in example

--- langchain/utilities/guards.py
+++ langchain/utilities/guards.py
@@ -177,6 +177,7 @@ class CustomGuard(BaseGuard):
         .. code-block:: python
 
             from langchain import LLMChain, OpenAI, PromptTemplate
+            from langchain.utilities.guards import CustomGuard
 
             llm = OpenAI(temperature=0.9)
 
