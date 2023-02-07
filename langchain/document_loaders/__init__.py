ba4546b6e615d414da717513bcb27c9fdb872a74
Harrison Chase <hw.chase.17@gmail.com>
2023-02-06 20:50:17 -0800

    add obsidian loader

--- langchain/document_loaders/__init__.py
+++ langchain/document_loaders/__init__.py
@@ -3,6 +3,7 @@
 from langchain.document_loaders.directory import DirectoryLoader
 from langchain.document_loaders.html import UnstructuredHTMLLoader
 from langchain.document_loaders.notion import NotionDirectoryLoader
+from langchain.document_loaders.obsidian import ObsidianLoader
 from langchain.document_loaders.pdf import UnstructuredPDFLoader
 from langchain.document_loaders.powerpoint import UnstructuredPowerPointLoader
 from langchain.document_loaders.readthedocs import ReadTheDocsLoader
@@ -16,4 +17,5 @@ __all__ = [
     "UnstructuredHTMLLoader",
     "UnstructuredPowerPointLoader",
     "UnstructuredPDFLoader",
+    "ObsidianLoader",
 ]
