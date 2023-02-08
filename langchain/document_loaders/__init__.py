bd5cf921510642498bf70ad8c2051f37731f8b4d
Harrison Chase <hw.chase.17@gmail.com>
2023-02-08 00:35:33 -0800

    Harrison/add roam loader (#939)

--- langchain/document_loaders/__init__.py
+++ langchain/document_loaders/__init__.py
@@ -3,6 +3,8 @@
 from langchain.document_loaders.directory import DirectoryLoader
 from langchain.document_loaders.docx import UnstructuredDocxLoader
 from langchain.document_loaders.email import UnstructuredEmailLoader
+from langchain.document_loaders.gcs_directory import GCSDirectoryLoader
+from langchain.document_loaders.gcs_file import GCSFileLoader
 from langchain.document_loaders.googledrive import GoogleDriveLoader
 from langchain.document_loaders.html import UnstructuredHTMLLoader
 from langchain.document_loaders.notion import NotionDirectoryLoader
@@ -10,7 +12,11 @@ from langchain.document_loaders.obsidian import ObsidianLoader
 from langchain.document_loaders.pdf import UnstructuredPDFLoader
 from langchain.document_loaders.powerpoint import UnstructuredPowerPointLoader
 from langchain.document_loaders.readthedocs import ReadTheDocsLoader
+from langchain.document_loaders.roam import RoamLoader
+from langchain.document_loaders.s3_directory import S3DirectoryLoader
+from langchain.document_loaders.s3_file import S3FileLoader
 from langchain.document_loaders.unstructured import UnstructuredFileLoader
+from langchain.document_loaders.youtube import YoutubeLoader
 
 __all__ = [
     "UnstructuredFileLoader",
@@ -24,4 +30,10 @@ __all__ = [
     "ObsidianLoader",
     "UnstructuredDocxLoader",
     "UnstructuredEmailLoader",
+    "RoamLoader",
+    "YoutubeLoader",
+    "S3FileLoader",
+    "S3DirectoryLoader",
+    "GCSFileLoader",
+    "GCSDirectoryLoader",
 ]
