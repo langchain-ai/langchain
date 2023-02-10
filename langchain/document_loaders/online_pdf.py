e351eed32ae677380c3169eab94898a861844650
Zander Chase <zander@unfold.ag>
2023-02-10 10:43:48 -0700

    updating online PDF

--- langchain/document_loaders/online_pdf.py
+++ langchain/document_loaders/online_pdf.py
@@ -1,16 +1,30 @@
 """Loader that loads online PDF files."""
 
+import tempfile
+from typing import List
+
 import requests
 
-from langchain.document_loaders import UnstructuredPDFLoader
+from langchain.docstore.document import Document
+from langchain.document_loaders.base import BaseLoader
+from langchain.document_loaders.pdf import UnstructuredPDFLoader
 
 
-class OnlinePDFLoader(UnstructuredPDFLoader):
+class OnlinePDFLoader(BaseLoader):
     """Loader that loads online PDFs."""
 
-    def __init__(self, file_path: str):
+    def __init__(self, web_path: str):
         """Initialize with file path."""
-        r = requests.get(file_path)
-        with open("example_data/online_file.pdf", "wb") as f:
-            f.write(r.content)
-        self.file_path = "example_data/online_file.pdf"
+        self.web_path = web_path
+
+    def load(self) -> List[Document]:
+        """Load documents."""
+
+        r = requests.get(self.web_path)
+        with tempfile.TemporaryDirectory() as temp_dir:
+            file_path = f"{temp_dir}/online_file.pdf"
+            file = open(file_path, "wb")
+            file.write(r.content)
+            file.close()
+            loader = UnstructuredPDFLoader(file_path)
+            return loader.load()
