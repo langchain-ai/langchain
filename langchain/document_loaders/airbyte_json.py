44523bb26aae402b383563a02cae9d073f04f805
Harrison Chase <harrisonchase@Harrisons-MacBook-Pro.local>
2023-02-10 17:55:26 -0800

    cr

--- langchain/document_loaders/airbyte_json.py
+++ langchain/document_loaders/airbyte_json.py
@@ -1,9 +1,27 @@
 """Loader that loads local airbyte json files."""
 import json
-from typing import List
+from typing import Any, List
 
 from langchain.docstore.document import Document
 from langchain.document_loaders.base import BaseLoader
+
+
+def _stringify_value(val: Any) -> str:
+    if isinstance(val, str):
+        return val
+    elif isinstance(val, dict):
+        return "\n" + _stringify_dict(val)
+    elif isinstance(val, list):
+        return "\n".join(_stringify_value(v) for v in val)
+    else:
+        return str(val)
+
+
+def _stringify_dict(data: dict) -> str:
+    text = ""
+    for key, value in data.items():
+        text += key + ": " + _stringify_value(data[key]) + "\n"
+    return text
 
 
 class AirbyteJSONLoader(BaseLoader):
@@ -18,7 +36,6 @@ class AirbyteJSONLoader(BaseLoader):
         text = ""
         for line in open(self.file_path, "r"):
             data = json.loads(line)["_airbyte_data"]
-            for key in data:
-                text = text + key + ": " + data[key] + "\n"
+            text += _stringify_dict(data)
         metadata = {"source": self.file_path}
         return [Document(page_content=text, metadata=metadata)]
