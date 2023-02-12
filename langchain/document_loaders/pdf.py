90cbe49786e4a813cb80a49c1e0b6e587aa21b4b
Harrison Chase <hw.chase.17@gmail.com>
2023-02-12 07:29:26 -0800

    pdfminer (#1003)

--- langchain/document_loaders/pdf.py
+++ langchain/document_loaders/pdf.py
@@ -27,3 +27,26 @@ class UnstructuredPDFLoader(BaseLoader):
         text = "\n\n".join([str(el) for el in elements])
         metadata = {"source": self.file_path}
         return [Document(page_content=text, metadata=metadata)]
+
+
+class PDFMinerLoader(BaseLoader):
+    """Loader that uses PDFMiner to load PDF files."""
+
+    def __init__(self, file_path: str):
+        """Initialize with file path."""
+        try:
+            from pdfminer.high_level import extract_text  # noqa:F401
+        except ImportError:
+            raise ValueError(
+                "pdfminer package not found, please install it with "
+                "`pip install pdfminer.six`"
+            )
+        self.file_path = file_path
+
+    def load(self) -> List[Document]:
+        """Load file."""
+        from pdfminer.high_level import extract_text
+
+        text = extract_text(self.file_path)
+        metadata = {"source": self.file_path}
+        return [Document(page_content=text, metadata=metadata)]
