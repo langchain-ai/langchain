a71eed1e65db9608789a731d945d04fd9ee417d6
Harrison Chase <hw.chase.17@gmail.com>
2023-02-12 07:36:11 -0800

    Harrison/unstructured structured (#1004)

--- langchain/document_loaders/pdf.py
+++ langchain/document_loaders/pdf.py
@@ -3,30 +3,16 @@ from typing import List
 
 from langchain.docstore.document import Document
 from langchain.document_loaders.base import BaseLoader
+from langchain.document_loaders.unstructured import UnstructuredFileLoader
 
 
-class UnstructuredPDFLoader(BaseLoader):
+class UnstructuredPDFLoader(UnstructuredFileLoader):
     """Loader that uses unstructured to load PDF files."""
 
-    def __init__(self, file_path: str):
-        """Initialize with file path."""
-        try:
-            import unstructured  # noqa:F401
-        except ImportError:
-            raise ValueError(
-                "unstructured package not found, please install it with "
-                "`pip install unstructured`"
-            )
-        self.file_path = file_path
-
-    def load(self) -> List[Document]:
-        """Load file."""
+    def _get_elements(self) -> List:
         from unstructured.partition.pdf import partition_pdf
 
-        elements = partition_pdf(filename=self.file_path)
-        text = "\n\n".join([str(el) for el in elements])
-        metadata = {"source": self.file_path}
-        return [Document(page_content=text, metadata=metadata)]
+        return partition_pdf(filename=self.file_path)
 
 
 class PDFMinerLoader(BaseLoader):
