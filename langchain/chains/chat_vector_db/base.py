f939c8f8a84f8784983ff2362220fb1e4dab8689
Ankush Gola <ankush.gola@gmail.com>
2023-02-13 12:39:51 -0800

    rm executor

--- langchain/chains/chat_vector_db/base.py
+++ langchain/chains/chat_vector_db/base.py
@@ -94,10 +94,8 @@ class ChatVectorDBChain(Chain, BaseModel):
             )
         else:
             new_question = question
-        # Run similarity search in executor to avoid blocking event loop
-        docs = await asyncio.get_event_loop().run_in_executor(
-            None, self.vectorstore.similarity_search, new_question, 4
-        )
+        # TODO: This blocks the event loop, but it's not clear how to avoid it.
+        docs = self.vectorstore.similarity_search(new_question, k=4)
         new_inputs = inputs.copy()
         new_inputs["question"] = new_question
         new_inputs["chat_history"] = chat_history_str
