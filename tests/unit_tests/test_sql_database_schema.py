865ee1d1db8bda045c76c02363a4d90658a07862
Francisco Ingham <>
2023-02-14 12:10:38 -0300

    replaced table info by a create table command plus select * with limit (default 3), as per text2sql paper

--- tests/unit_tests/test_sql_database_schema.py
+++ tests/unit_tests/test_sql_database_schema.py
@@ -46,10 +46,14 @@ def test_table_info() -> None:
     metadata_obj.create_all(engine)
     db = SQLDatabase(engine, schema="schema_a")
     output = db.table_info
-    expected_output = (
-        "Table 'user' has columns: user_id (INTEGER), user_name (VARCHAR).",
-    )
-    assert sorted(output.split("\n")) == sorted(expected_output)
+    expected_output = """
+    CREATE TABLE schema_a."user"(user_id INTEGER, user_name VARCHAR NOT NULL, PRIMARY KEY(user_id));
+
+    SELECT * FROM 'user' LIMIT 3
+    user_id user_name
+    """
+
+    assert sorted(" ".join(output.split())) == sorted(" ".join(expected_output.split()))
 
 
 def test_sql_database_run() -> None:
