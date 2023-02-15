6aee365c2c9daf36c9a1f601d592e5f74067fdd5
Nimisha Mehta <nmehta@confluent.io>
2023-02-15 12:39:00 -0800

    formatting

--- langchain/llms/sagemaker_endpoint.py
+++ langchain/llms/sagemaker_endpoint.py
@@ -1,22 +1,27 @@
 """Wrapper around Sagemaker InvokeEndpoint API."""
 import json
-from typing import Any, List, Mapping, Optional
+from typing import Any, List, Dict, Mapping, Optional
 
-from pydantic import BaseModel, Extra
+from pydantic import BaseModel, Extra, root_validator
 
 from langchain.llms.base import LLM
 from langchain.llms.utils import enforce_stop_tokens
 
-VALID_TASKS = ("text2text-generation", "text-generation")
-
-
 class SagemakerEndpoint(LLM, BaseModel):
     """Wrapper around custom Sagemaker Inference Endpoints.
+    To use, you must supply the endpoint name from your deployed 
+    Sagemaker model & the region where it is deployed.
 
-    To use, you should pass the AWS IAM Role and Role Session Name as
-    named parameters to the constructor.
+    To authenticate, the AWS client uses the following methods to 
+    automatically load credentials:
+    https://boto3.amazonaws.com/v1/documentation/api/latest/guide/credentials.html
 
-    Only supports `text-generation` and `text2text-generation` for now.
+    If a specific credential profile should be used, you must pass 
+    the name of the profile from the ~/.aws/credentials file that is to be used.
+
+    Make sure the credentials / roles used have the required policies to 
+    access the Sagemaker endpoint.
+    See: https://docs.aws.amazon.com/IAM/latest/UserGuide/access_policies.html
     """
 
     """
@@ -27,34 +32,73 @@ class SagemakerEndpoint(LLM, BaseModel):
             endpoint_name = (
                 "https://runtime.sagemaker.us-west-2.amazonaws.com/endpoints/abcdefghijklmnop/invocations"
             )
+            region_name = (
+                "us-west-2"
+            )
+            credentials_profile_name = (
+                "default"
+            )
             se = SagemakerEndpoint(
                 endpoint_name=endpoint_name,
-                role_arn="role_arn",
-                role_session_name="role_session_name"
+                region_name=region_name,
+                credentials_profile_name=credentials_profile_name
             )
     """
 
     endpoint_name: str = ""
-    """# The name of the endpoint. Must be unique within an AWS Region."""
-    task: Optional[str] = None
-    """Task to call the model with. Should be a task that returns `generated_text`."""
+    """The name of the endpoint from the deployed Sagemaker model. 
+    Must be unique within an AWS Region."""
+    region_name: str = ""
+    """The aws region where the Sagemaker model is deployed, eg. `us-west-2`."""
     model_kwargs: Optional[dict] = None
     """Key word arguments to pass to the model."""
-
-    role_arn: Optional[str] = None
-    role_session_name: Optional[str] = None
+    credentials_profile_name: Optional[str] = None
+    """The name of the profile in the ~/.aws/credentials or ~/.aws/config files, which 
+    has either access keys or role information specified.
+    If not specified, the default credential profile or, if on an EC2 instance, 
+    credentials from IMDS will be used.
+    See: https://boto3.amazonaws.com/v1/documentation/api/latest/guide/credentials.html
+    """
 
     class Config:
         """Configuration for this pydantic object."""
 
         extra = Extra.forbid
 
+    @root_validator()
+    def validate_environment(cls, values: Dict) -> Dict:
+        """Validate that AWS credentials to and python package exists in environment."""
+        try:
+            import boto3
+
+            try:
+                
+                if values["credentials_profile_name"] is not None:
+                    session = boto3.Session(profile_name=values["credentials_profile_name"])
+                else:
+                    # use default credentials
+                    session = boto3.Session()
+                
+            except Exception as e:
+                raise ValueError(
+                    "Could not load credentials to authenticate with AWS client, error. "
+                    "Please check that credentials in the specified profile name are valid. "
+                    "https://boto3.amazonaws.com/v1/documentation/api/latest/guide/credentials.html"
+                ) from e
+
+        except ImportError:
+            raise ValueError(
+                "Could not import boto3 python package. "
+                "Please it install it with `pip install boto3`."
+            )
+        return values
+
     @property
     def _identifying_params(self) -> Mapping[str, Any]:
         """Get the identifying parameters."""
         _model_kwargs = self.model_kwargs or {}
         return {
-            **{"endpoint_name": self.endpoint_name, "task": self.task},
+            **{"endpoint_name": self.endpoint_name},
             **{"model_kwargs": _model_kwargs},
         }
 
@@ -78,36 +122,19 @@ class SagemakerEndpoint(LLM, BaseModel):
 
                 response = se("Tell me a joke.")
         """
+
         import boto3
 
-        session = boto3.Session(profile_name="test-profile-name")
-        sagemaker_runtime = session.client("sagemaker-runtime", region_name="us-west-2")
+        if self.credentials_profile_name is not None:
+            session = boto3.Session(profile_name=self.credentials_profile_name)
+        else:
+            # use default credentials
+            session = boto3.Session()
 
-        # TODO: use AWS IAM assumed roles to authenticate from the EC2 instance
-        # def role_arn_to_session(**args):
-        #     """
-        #     Usage :
-        #         session = role_arn_to_session(
-        #             RoleArn='arn:aws:iam::012345678901:role/example-role',
-        #             RoleSessionName='ExampleSessionName')
-        #         client = session.client('sqs')
-        #     """
-        #     client = boto3.client('sts')
-        #     response = client.assume_role(**args)
-        #     return boto3.Session(
-        #         aws_access_key_id=response['Credentials']['AccessKeyId'],
-        #         aws_secret_access_key=response['Credentials']['SecretAccessKey'],
-        #         aws_session_token=response['Credentials']['SessionToken'])
-
-        # session = role_arn_to_session(RoleArn="$role-arn",
-        # RoleSessionName="test-role-session-name")
-        # sagemaker_runtime = session.client(
-        # "sagemaker-runtime", region_name="us-west-2"
-        # )
+        sagemaker_runtime = session.client("sagemaker-runtime", region_name=self.region_name)
 
         _model_kwargs = self.model_kwargs or {}
 
-        # payload samples
         parameter_payload = {"inputs": prompt, "parameters": _model_kwargs}
 
         input_en = json.dumps(parameter_payload).encode("utf-8")
@@ -126,7 +153,7 @@ class SagemakerEndpoint(LLM, BaseModel):
         text = response_json[0]["generated_text"]
         if stop is not None:
             # This is a bit hacky, but I can't figure out a better way to enforce
-            # stop tokens when making calls to huggingface_hub.
+            # stop tokens when making calls to the sagemaker endpoint.
             text = enforce_stop_tokens(text, stop)
 
         return text
