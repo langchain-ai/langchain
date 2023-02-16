f145792abe1b767d9c77e931eb9613938c5a0ff4
Jonathan Pedoeem <jonathanped@gmail.com>
2023-02-16 01:48:09 -0500

    Update PromptLayerOpenAI LLM to include support for ASYNC API  (#1066)
    This PR updates `PromptLayerOpenAI` to now support requests using the
    [Async
    API](https://langchain.readthedocs.io/en/latest/modules/llms/async_llm.html)
    It also updates the documentation on Async API to let users know that
    PromptLayerOpenAI also supports this.
    
    `PromptLayerOpenAI` now redefines `_agenerate` a similar was to how it
    redefines `_generate`

--- langchain/llms/promptlayer_openai.py
+++ langchain/llms/promptlayer_openai.py
@@ -53,3 +53,27 @@ class PromptLayerOpenAI(OpenAI, BaseModel):
                 get_api_key(),
             )
         return generated_responses
+
+    async def _agenerate(
+        self, prompts: List[str], stop: Optional[List[str]] = None
+    ) -> LLMResult:
+        from promptlayer.utils import get_api_key, promptlayer_api_request
+
+        request_start_time = datetime.datetime.now().timestamp()
+        generated_responses = await super()._agenerate(prompts, stop)
+        request_end_time = datetime.datetime.now().timestamp()
+        for i in range(len(prompts)):
+            prompt = prompts[i]
+            resp = generated_responses.generations[i]
+            promptlayer_api_request(
+                "langchain.PromptLayerOpenAI.async",
+                "langchain",
+                [prompt],
+                self._identifying_params,
+                self.pl_tags,
+                resp[0].text,
+                request_start_time,
+                request_end_time,
+                get_api_key(),
+            )
+        return generated_responses
