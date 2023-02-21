bb24d3033466bc3d5df3d33b29d50106f47df021
Dennis Antela Martinez <dennis.antela@gmail.com>
2023-02-21 04:05:04 +0000

    add gitbook document loader (#1180)
    Added a GitBook document loader. It lets you both, (1) fetch text from
    any single GitBook page, or (2) fetch all relative paths and return
    their respective content in Documents.
    
    I've modified the `scrape` method in the `WebBaseLoader` to accept
    custom web paths if given, but happy to remove it and move that logic
    into the `GitbookLoader` itself.

--- langchain/document_loaders/__init__.py
+++ langchain/document_loaders/__init__.py
@@ -9,6 +9,7 @@ from langchain.document_loaders.email import UnstructuredEmailLoader
 from langchain.document_loaders.evernote import EverNoteLoader
 from langchain.document_loaders.gcs_directory import GCSDirectoryLoader
 from langchain.document_loaders.gcs_file import GCSFileLoader
+from langchain.document_loaders.gitbook import GitbookLoader
 from langchain.document_loaders.googledrive import GoogleDriveLoader
 from langchain.document_loaders.gutenberg import GutenbergLoader
 from langchain.document_loaders.hn import HNLoader
@@ -50,6 +51,7 @@ __all__ = [
     "S3FileLoader",
     "TextLoader",
     "HNLoader",
+    "GitbookLoader",
     "S3DirectoryLoader",
     "GCSFileLoader",
     "GCSDirectoryLoader",
