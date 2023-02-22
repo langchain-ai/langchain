93842db45ccf8599e72f7266407bd5f09259b0fc
x <0xloem@gmail.com>
2023-02-21 19:32:36 -0500

    draft per-token processor for huggingface_pipeline

--- langchain/llms/huggingface_pipeline.py
+++ langchain/llms/huggingface_pipeline.py
@@ -6,13 +6,112 @@ from typing import Any, List, Mapping, Optional
 from pydantic import BaseModel, Extra
 
 from langchain.llms.base import LLM
-from langchain.llms.utils import enforce_stop_tokens
 
 DEFAULT_MODEL_ID = "gpt2"
 DEFAULT_TASK = "text-generation"
 VALID_TASKS = ("text2text-generation", "text-generation")
 
 logger = logging.getLogger()
+
+class HuggingFaceTokenProcessor:
+    '''A LogitsProcessor to handle stop sequences and streaming.'''
+
+    def __init__(self, llm, tokenizer, stop, eos_token_id, generation_offset):
+        self.llm = llm
+        self.tokenizer = tokenizer
+        self.stop = stop
+        self.eos_token_id = eos_token_id
+        self.generation_offset = generation_offset
+        self.pre_lens = None
+        self.cur_seqs = None
+        self.stop_lens = None
+
+    def generate(self, *params, **kwparams):
+        # call pipeline which will then pass token ids to _logits_processor
+        outputs = self.llm.pipeline(
+            *params,
+            logits_processor = [self._logits_processor],
+            **kwparams
+        )
+        # trim output
+        batchsize = len(outputs)
+        seqs = [
+            outputs[batch_idx]["generated_text"][self.generation_offset :][: self.stop_lens[batch_idx]]
+            for batch_idx in range(batchsize)
+        ]
+        # process any terminating token, which isn't passed to a logits_processor
+        self._update_seqs(seqs)
+        # stream the last delayed token
+        self._finish_seqs()
+        return seqs
+
+    def _logits_processor(self, input_ids, logits):
+        '''Decodes the latest input_ids and hands them off to _update_seqs to process.'''
+        # for now this unneccessarily re-decodes the same starting logits repeatedly
+        seqs = [
+            seq[self.generation_offset:]
+            for seq in self.tokenizer.batch_decode(input_ids)
+        ]
+        return self._update_seqs(seqs, logits)
+
+    def _update_seqs(self, new_seqs, logits=None):
+        '''Performs streaming and processes stop tokens.
+           Tokens are streamed with a 1-token delay to ensure the decoder has trailing context.
+           Sequences are ended by causing generation of eos_token_id.'''
+        batchsize = len(new_seqs)
+        if self.cur_seqs is None:
+            # init data now we know the batchsize
+            # data is parallel lists for now
+            self.pre_lens = [0] * batchsize
+            self.cur_seqs = [''] * batchsize
+            self.stop_lens = [None] * batchsize
+        # update information on delayed tokens
+        pre_pre_lens = self.pre_lens
+        self.pre_lens = [len(pre_seq) for pre_seq in self.cur_seqs]
+        self.cur_seqs = new_seqs
+
+        for batch_idx in range(len(self.cur_seqs)):
+            # data for each batch index
+            pre_pre_len = pre_pre_lens[batch_idx]
+            pre_len = self.pre_lens[batch_idx]
+            seq = self.cur_seqs[batch_idx]
+            stop_len = self.stop_lens[batch_idx]
+
+            # stream
+            if self.llm.streaming and pre_pre_lens:
+                token = seq[: stop_len][pre_pre_len : pre_len]
+                if len(token):
+                    self.llm.callback_manager.on_llm_new_token(
+                        token,
+                        verbose=self.llm.verbose,
+                        batch_idx=batch_idx
+                    )
+
+            # stop
+            if self.stop is not None and logits is not None:
+                for stop in self.stop:
+                    start_offset = max(0, pre_pre_len - (len(stop) - 1))
+                    found_offset = seq.find(stop, start_offset)
+                    if found_offset >= 0:
+                        logits[batch_idx][self.eos_token_id] = float('inf')
+                        self.stop_lens[batch_idx] = found_offset
+
+        return logits
+
+    def _finish_seqs(self):
+        '''Streams out the last delayed token.'''
+        if self.llm.streaming and self.pre_lens:
+            for batch_idx in range(len(self.cur_seqs)):
+                seq = self.cur_seqs[batch_idx]
+                pre_len = self.pre_lens[batch_idx]
+                stop_len = self.stop_lens[batch_idx]
+                token = seq[pre_len : stop_len]
+                if len(token):
+                    self.llm.callback_manager.on_llm_new_token(
+                        token,
+                        verbose=self.llm.verbose,
+                        batch_idx=batch_idx
+                    )
 
 
 class HuggingFacePipeline(LLM, BaseModel):
@@ -49,6 +148,8 @@ class HuggingFacePipeline(LLM, BaseModel):
     """Model name to use."""
     model_kwargs: Optional[dict] = None
     """Key word arguments to pass to the model."""
+    streaming: bool = False
+    """Whether to stream the results or not."""
 
     class Config:
         """Configuration for this pydantic object."""
@@ -62,6 +163,7 @@ class HuggingFacePipeline(LLM, BaseModel):
         task: str,
         device: int = -1,
         model_kwargs: Optional[dict] = None,
+        pipeline_kwargs: Optional[dict] = None,
         **kwargs: Any,
     ) -> LLM:
         """Construct the pipeline object from model_id and task."""
@@ -80,6 +182,7 @@ class HuggingFacePipeline(LLM, BaseModel):
             )
 
         _model_kwargs = model_kwargs or {}
+        _pipeline_kwargs = pipeline_kwargs or {}
         tokenizer = AutoTokenizer.from_pretrained(model_id, **_model_kwargs)
 
         try:
@@ -121,6 +224,7 @@ class HuggingFacePipeline(LLM, BaseModel):
             tokenizer=tokenizer,
             device=device,
             model_kwargs=_model_kwargs,
+            **_pipeline_kwargs,
         )
         if pipeline.task not in VALID_TASKS:
             raise ValueError(
@@ -147,19 +251,23 @@ class HuggingFacePipeline(LLM, BaseModel):
         return "huggingface_pipeline"
 
     def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
-        response = self.pipeline(prompt)
         if self.pipeline.task == "text-generation":
             # Text generation return includes the starter text.
-            text = response[0]["generated_text"][len(prompt) :]
+            generation_offset = len(prompt)
         elif self.pipeline.task == "text2text-generation":
-            text = response[0]["generated_text"]
+            generation_offset = 0
         else:
             raise ValueError(
                 f"Got invalid task {self.pipeline.task}, "
                 f"currently only {VALID_TASKS} are supported"
             )
-        if stop is not None:
-            # This is a bit hacky, but I can't figure out a better way to enforce
-            # stop tokens when making calls to huggingface_hub.
-            text = enforce_stop_tokens(text, stop)
-        return text
+
+        token_processor = HuggingFaceTokenProcessor(
+            llm=self,
+            tokenizer=self.pipeline.tokenizer,
+            stop=stop,
+            eos_token_id=self.pipeline.model.config.eos_token_id,
+            generation_offset=generation_offset,
+        )
+
+        return token_processor.generate(prompt)[0]
--- tests/integration_tests/llms/test_huggingface_pipeline.py
+++ tests/integration_tests/llms/test_huggingface_pipeline.py
@@ -4,15 +4,17 @@ from pathlib import Path
 
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
+from langchain.callbacks.base import CallbackManager
 from langchain.llms.huggingface_pipeline import HuggingFacePipeline
 from langchain.llms.loading import load_llm
 from tests.integration_tests.llms.utils import assert_llm_equality
+from tests.unit_tests.callbacks.fake_callback_handler import FakeCallbackHandler
 
 
 def test_huggingface_pipeline_text_generation() -> None:
     """Test valid call to HuggingFace text generation model."""
     llm = HuggingFacePipeline.from_model_id(
-        model_id="gpt2", task="text-generation", model_kwargs={"max_new_tokens": 10}
+        model_id="gpt2", task="text-generation", pipeline_kwargs={"max_new_tokens": 10}
     )
     output = llm("Say foo:")
     assert isinstance(output, str)
@@ -30,7 +32,7 @@ def test_huggingface_pipeline_text2text_generation() -> None:
 def test_saving_loading_llm(tmp_path: Path) -> None:
     """Test saving/loading an HuggingFaceHub LLM."""
     llm = HuggingFacePipeline.from_model_id(
-        model_id="gpt2", task="text-generation", model_kwargs={"max_new_tokens": 10}
+        model_id="gpt2", task="text-generation", pipeline_kwargs={"max_new_tokens": 10}
     )
     llm.save(file_path=tmp_path / "hf.yaml")
     loaded_llm = load_llm(tmp_path / "hf.yaml")
@@ -48,3 +50,30 @@ def test_init_with_pipeline() -> None:
     llm = HuggingFacePipeline(pipeline=pipe)
     output = llm("Say foo:")
     assert isinstance(output, str)
+
+def test_pipeline_streaming_callback() -> None:
+    """Test that streaming correctly invokes on_llm_new_token callback."""
+    callback_handler = FakeCallbackHandler()
+    callback_manager = CallbackManager([callback_handler])
+    llm = HuggingFacePipeline.from_model_id(
+        model_id="gpt2",
+        task="text-generation",
+        pipeline_kwargs={"max_new_tokens": 10},
+        streaming=True,
+        callback_manager=callback_manager,
+        verbose=True,
+    )
+    llm("This sentence has 100 words:")
+    assert callback_handler.llm_streams == 10
+
+def test_stop() -> None:
+    """Test stop logic."""
+    query = "an ordered list of five items:\n1. item one\n2."
+    llm = HuggingFacePipeline.from_model_id(
+        model_id="gpt2",
+        task="text-generation",
+        pipeline_kwargs={"max_new_tokens": 10, "do_sample": False},
+    )
+    output = llm(query, stop=["3"])
+    assert output.endswith("\n")
+    assert "3" not in output
