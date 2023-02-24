0ce5316eceb46b2cd3a7cbe3a9fb184a65b74caf
Enrico Shippole <enricoship@gmail.com>
2023-02-23 23:47:33 -0500

    Lint again

--- langchain/llms/__init__.py
+++ langchain/llms/__init__.py
@@ -5,9 +5,6 @@ from langchain.llms.ai21 import AI21
 from langchain.llms.aleph_alpha import AlephAlpha
 from langchain.llms.anthropic import Anthropic
 from langchain.llms.bananadev import Banana
-from langchain.llms.modal import Modal
-from langchain.llms.stochasticai import StochasticAI
-from langchain.llms.writer import Writer
 from langchain.llms.base import BaseLLM
 from langchain.llms.cerebriumai import CerebriumAI
 from langchain.llms.cohere import Cohere
@@ -17,25 +14,26 @@ from langchain.llms.gooseai import GooseAI
 from langchain.llms.huggingface_endpoint import HuggingFaceEndpoint
 from langchain.llms.huggingface_hub import HuggingFaceHub
 from langchain.llms.huggingface_pipeline import HuggingFacePipeline
+from langchain.llms.modal import Modal
 from langchain.llms.nlpcloud import NLPCloud
 from langchain.llms.openai import AzureOpenAI, OpenAI
 from langchain.llms.petals import Petals
 from langchain.llms.promptlayer_openai import PromptLayerOpenAI
 from langchain.llms.self_hosted import SelfHostedPipeline
 from langchain.llms.self_hosted_hugging_face import SelfHostedHuggingFaceLLM
+from langchain.llms.stochasticai import StochasticAI
+from langchain.llms.writer import Writer
 
 __all__ = [
     "Anthropic",
     "AlephAlpha",
-    "Modal",
-    "StochasticAI",
-    "Writer",
     "Banana",
     "CerebriumAI",
     "Cohere",
     "DeepInfra",
     "ForefrontAI",
     "GooseAI",
+    "Modal",
     "NLPCloud",
     "OpenAI",
     "Petals",
@@ -47,6 +45,8 @@ __all__ = [
     "SelfHostedPipeline",
     "SelfHostedHuggingFaceLLM",
     "PromptLayerOpenAI",
+    "StochasticAI",
+    "Writer",
 ]
 
 type_to_cls_dict: Dict[str, Type[BaseLLM]] = {
