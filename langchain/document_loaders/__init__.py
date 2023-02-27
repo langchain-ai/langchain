f45403f191b88227531432318bb4c7826b5f9eeb
Ingo Kleiber <ikleiber@googlemail.com>
2023-02-27 02:27:00 +0100

    add CoNLL-U document loader (#1297)
    I've added a simple
    [CoNLL-U](https://universaldependencies.org/format.html) document
    loader. CoNLL-U is a common format for NLP tasks and is used, for
    example, in the Universal Dependencies treebank corpora. The loader
    reads a single file in standard CoNLL-U format and returns a document.

--- langchain/document_loaders/__init__.py
+++ langchain/document_loaders/__init__.py
@@ -3,6 +3,7 @@
 from langchain.document_loaders.airbyte_json import AirbyteJSONLoader
 from langchain.document_loaders.azlyrics import AZLyricsLoader
 from langchain.document_loaders.college_confidential import CollegeConfidentialLoader
+from langchain.document_loaders.conllu import CoNLLULoader
 from langchain.document_loaders.directory import DirectoryLoader
 from langchain.document_loaders.docx import UnstructuredDocxLoader
 from langchain.document_loaders.email import UnstructuredEmailLoader
@@ -77,4 +78,5 @@ __all__ = [
     "SRTLoader",
     "FacebookChatLoader",
     "NotebookLoader",
+    "CoNLLULoader",
 ]
