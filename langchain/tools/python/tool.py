275eae4b3e60ee2c37b0a39aeebe339f0ea91768
Harrison Chase <hw.chase.17@gmail.com>
2023-02-28 21:37:01 -0800

    cr

--- langchain/tools/python/tool.py
+++ langchain/tools/python/tool.py
@@ -1,4 +1,6 @@
 """A tool for running python code in a REPL."""
+import ast
+from typing import Dict, Optional
 
 from pydantic import Field
 
@@ -17,13 +19,48 @@ class PythonREPLTool(BaseTool):
     description = (
         "A Python shell. Use this to execute python commands. "
         "Input should be a valid python command. "
-        "If you expect output it should be printed out."
+        "If you want to see the output of a value, you should print it out "
+        "with `print(...)`."
     )
     python_repl: PythonREPL = Field(default_factory=_get_default_python_repl)
 
     def _run(self, query: str) -> str:
         """Use the tool."""
         return self.python_repl.run(query)
+
+    async def _arun(self, query: str) -> str:
+        """Use the tool asynchronously."""
+        raise NotImplementedError("PythonReplTool does not support async")
+
+
+class PythonAstREPLTool(BaseTool):
+    """A tool for running python code in a REPL."""
+
+    name = "python_repl_ast"
+    description = (
+        "A Python shell. Use this to execute python commands. "
+        "Input should be a valid python command. "
+        "When using this tool, sometimes output is abbreviated - "
+        "make sure it does not look abbreviated before using it in your answer."
+    )
+    globals: Optional[Dict] = Field(default_factory=dict)
+    locals: Optional[Dict] = Field(default_factory=dict)
+
+    def _run(self, query: str) -> str:
+        """Use the tool."""
+        try:
+            tree = ast.parse(query)
+            module = ast.Module(tree.body[:-1], type_ignores=[])
+            exec(ast.unparse(module), self.globals, self.locals)
+            module_end = ast.Module(tree.body[-1:], type_ignores=[])
+            module_end_str = ast.unparse(module_end)
+            try:
+                return eval(module_end_str, self.globals, self.locals)
+            except Exception:
+                exec(module_end_str, self.globals, self.locals)
+                return ""
+        except Exception as e:
+            return str(e)
 
     async def _arun(self, query: str) -> str:
         """Use the tool asynchronously."""
