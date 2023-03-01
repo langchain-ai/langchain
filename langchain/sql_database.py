acffe63b4d8e44d2f13301e00df8c142c24672b9
jzluo <20971593+jzluo@users.noreply.github.com>
2023-02-28 21:20:29 -0500

    separate columns by tabs instead of single space in sql sample rows

--- langchain/sql_database.py
+++ langchain/sql_database.py
@@ -113,7 +113,7 @@ class SQLDatabase:
                 )
 
                 # save the columns in string format
-                columns_str = " ".join([col.name for col in table.columns])
+                columns_str = "\t".join([col.name for col in table.columns])
 
                 try:
                     # get the sample rows
@@ -125,7 +125,7 @@ class SQLDatabase:
                         )
 
                     # save the sample rows in string format
-                    sample_rows_str = "\n".join([" ".join(row) for row in sample_rows])
+                    sample_rows_str = "\n".join(["\t".join(row) for row in sample_rows])
 
                 # in some dialects when there are no rows in the table a
                 # 'ProgrammingError' is returned
