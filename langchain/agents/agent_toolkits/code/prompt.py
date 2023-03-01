eaebea185b144c5f963d44ab6890b030b686a4a1
samee99 <53830393+samee99@users.noreply.github.com>
2023-02-28 23:07:52 -0800

    Create prompt.py

---
+++ langchain/agents/agent_toolkits/code/prompt.py
@@ -0,0 +1,4 @@
+PREFIX = """Understand, write a single neuron neural network in PyTorch.
+Take synthetic data for y=2x. Train for 1000 epochs and print every 100 epochs.
+Return prediction for x = 5
+"""
