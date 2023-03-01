41c744b319f739be60484545733a555a0e5172a9
Harrison Chase <hw.chase.17@gmail.com>
2023-03-01 11:47:01 -0800

    chatgpt wrapper (#1367)

--- langchain/llms/__init__.py
+++ langchain/llms/__init__.py
@@ -16,7 +16,7 @@ from langchain.llms.huggingface_hub import HuggingFaceHub
 from langchain.llms.huggingface_pipeline import HuggingFacePipeline
 from langchain.llms.modal import Modal
 from langchain.llms.nlpcloud import NLPCloud
-from langchain.llms.openai import AzureOpenAI, OpenAI
+from langchain.llms.openai import AzureOpenAI, OpenAI, OpenAIChat
 from langchain.llms.petals import Petals
 from langchain.llms.promptlayer_openai import PromptLayerOpenAI
 from langchain.llms.self_hosted import SelfHostedPipeline
@@ -36,6 +36,7 @@ __all__ = [
     "Modal",
     "NLPCloud",
     "OpenAI",
+    "OpenAIChat",
     "Petals",
     "HuggingFaceEndpoint",
     "HuggingFaceHub",
