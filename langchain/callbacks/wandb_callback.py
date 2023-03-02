13f5fb679c30b00dceb1e3ea303762391aa663d9
Anish Shah <anish@wandb.com>
2023-03-02 12:39:28 -0500

    Update wandb_callback.py
    Remove the main function due to the example causing circular imports. Moved to colab

--- langchain/callbacks/wandb_callback.py
+++ langchain/callbacks/wandb_callback.py
@@ -9,7 +9,6 @@ import hashlib
 import json
 import tempfile
 from copy import deepcopy
-from datetime import datetime
 from pathlib import Path
 from typing import Any, Dict, List, Optional, Sequence, Union
 
@@ -17,13 +16,7 @@ import pandas as pd
 import spacy
 import textstat
 
-from langchain.agents import initialize_agent, load_tools
-from langchain.callbacks.base import BaseCallbackHandler, CallbackManager
-from langchain.callbacks.stdout import StdOutCallbackHandler
-from langchain.chains import LLMChain, SimpleSequentialChain
-from langchain.input import print_text
-from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
+from langchain.callbacks.base import BaseCallbackHandler
 from langchain.schema import AgentAction, AgentFinish, LLMResult
 
 
@@ -718,74 +711,3 @@ Please report any issues to https://github.com/wandb/wandb/issues with the tag `
                 if complexity_metrics
                 else self.complexity_metrics,
             )
-
-
-def main():
-    """Main function.
-
-    This function is used to test the callback handler.
-    Scenarios:
-    1. OpenAI LLM
-    2. Chain with multiple SubChains on multiple generations
-    3. Agent with Tools
-    """
-    session_group = datetime.now().strftime("%m.%d.%Y_%H.%M.%S")
-    wandb_callback = WandbCallbackHandler(
-        job_type="inference",
-        project="langchain_callback_demo",
-        group=f"minimal_{session_group}",
-        name="llm",
-        tags=["test"],
-    )
-    manager = CallbackManager([StdOutCallbackHandler(), wandb_callback])
-    llm = OpenAI(temperature=0, callback_manager=manager, verbose=True)
-
-    # SCENARIO 1 - LLM
-    llm_result = llm.generate(["Tell me a joke", "Tell me a poem"] * 3)
-    wandb_callback.flush_tracker(llm, name="simple_sequential")
-
-    # SCENARIO 2 - Chain
-    template = """You are a playwright. Given the title of play, it is your job to write a synopsis for that title.
-    Title: {title}
-    Playwright: This is a synopsis for the above play:"""
-    prompt_template = PromptTemplate(input_variables=["title"], template=template)
-    synopsis_chain = LLMChain(llm=llm, prompt=prompt_template, callback_manager=manager)
-
-    template = """You are a play critic from the New York Times. Given the synopsis of play, it is your job to write a review for that play.
-    Play Synopsis:
-    {synopsis}
-    Review from a New York Times play critic of the above play:"""
-    prompt_template = PromptTemplate(input_variables=["synopsis"], template=template)
-    review_chain = LLMChain(llm=llm, prompt=prompt_template, callback_manager=manager)
-
-    overall_chain = SimpleSequentialChain(
-        chains=[synopsis_chain, review_chain], verbose=True, callback_manager=manager
-    )
-
-    test_prompts = [
-        {
-            "input": "documentary about good video games that push the boundary of game design"
-        },
-        {"input": "cocaine bear vs heroin wolf"},
-        {"input": "the best in class mlops tooling"},
-    ]
-    overall_chain.apply(test_prompts)
-    wandb_callback.flush_tracker(overall_chain, name="agent")
-
-    # SCENARIO 3 - Agent with Tools
-    tools = load_tools(["serpapi", "llm-math"], llm=llm, callback_manager=manager)
-    agent = initialize_agent(
-        tools,
-        llm,
-        agent="zero-shot-react-description",
-        callback_manager=manager,
-        verbose=True,
-    )
-    agent.run(
-        "Who is Leo DiCaprio's girlfriend? What is her current age raised to the 0.43 power?"
-    )
-    wandb_callback.flush_tracker(agent, reset=False, finish=True)
-
-
-if __name__ == "__main__":
-    main()
