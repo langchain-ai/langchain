ab801e825e31669c26c93853e912606bc4716640
Harrison Chase <hw.chase.17@gmail.com>
2023-03-08 08:31:29 -0800

    add concept of prompt collection (#1507)

--- langchain/chains/question_answering/stuff_prompt.py
+++ langchain/chains/question_answering/stuff_prompt.py
@@ -1,5 +1,15 @@
 # flake8: noqa
 from langchain.prompts import PromptTemplate
+from langchain.chains.prompt_selector import (
+    ConditionalPromptSelector,
+    is_chat_model,
+)
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+
 
 prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
@@ -9,4 +19,19 @@ Question: {question}
 Helpful Answer:"""
 PROMPT = PromptTemplate(
     template=prompt_template, input_variables=["context", "question"]
+)
+
+system_template = """Use the following pieces of context to answer the users question. 
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+----------------
+{context}"""
+messages = [
+    SystemMessagePromptTemplate.from_template(system_template),
+    HumanMessagePromptTemplate.from_template("{question}"),
+]
+CHAT_PROMPT = ChatPromptTemplate.from_messages(messages)
+
+
+PROMPT_SELECTOR = ConditionalPromptSelector(
+    default_prompt=PROMPT, conditionals=[(is_chat_model, CHAT_PROMPT)]
 )
