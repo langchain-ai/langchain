9e809138d2a4b206cdebecd9ff84fd06b643c072
Matt Robinson <mrobinson@unstructuredai.io>
2023-03-09 09:49:20 -0500

    fix for dev versions; import loader

--- langchain/document_loaders/markdown.py
+++ langchain/document_loaders/markdown.py
@@ -12,8 +12,11 @@ class UnstructuredMarkdownLoader(UnstructuredFileLoader):
         from unstructured.partition.md import partition_md
 
 
+        # NOTE(MthwRobinson) - enables the loader to work when you're using pre-release
+        # versions of unstructured like 0.4.17-dev1
+        _unstructured_version = __unstructured_version__.split("-")[0]
         unstructured_version = tuple(
-            [int(x) for x in __unstructured_version__.split(".")]
+            [int(x) for x in _unstructured_version.split(".")]
         )
 
         if unstructured_version < (0, 4, 16):
