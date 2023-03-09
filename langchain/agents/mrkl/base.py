7eba828e1b9542de4a0b67d1da2ffcfa83c6baca
Harrison Chase <hw.chase.17@gmail.com>
2023-03-08 17:41:17 -0800

    Harrison/update regex (#1534)
    Co-authored-by: Luis <57528712+LuisLechugaRuiz@users.noreply.github.com>

--- langchain/agents/mrkl/base.py
+++ langchain/agents/mrkl/base.py
@@ -40,7 +40,7 @@ def get_action_and_input(llm_output: str) -> Tuple[str, str]:
     """
     if FINAL_ANSWER_ACTION in llm_output:
         return "Final Answer", llm_output.split(FINAL_ANSWER_ACTION)[-1].strip()
-    regex = r"Action: (.*?)\nAction Input: (.*)"
+    regex = r"Action: (.*?)[\n]*Action Input: (.*)"
     match = re.search(regex, llm_output, re.DOTALL)
     if not match:
         raise ValueError(f"Could not parse LLM output: `{llm_output}`")
