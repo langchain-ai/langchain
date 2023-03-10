bb23e9d2e1ceaa1f009f2dfb8227e920686c20b8
Harrison Chase <hw.chase.17@gmail.com>
2023-03-10 12:36:21 -0800

    cr

--- langchain/agents/chat/prompt.py
+++ langchain/agents/chat/prompt.py
@@ -3,14 +3,14 @@ PREFIX = """Answer the following questions as best you can. You have access to t
 FORMAT_INSTRUCTIONS = """The way you use the tools is by specifying a json blob.
 Specifically, this json should have a `action` key (with the name of the tool to use) and a `action_input` key (with the input to the tool going here).
 
-The only tool names you should use are: {tool_names}
+The only values that should be in the "action" field are: {tool_names}
 
 The $JSON_BLOB should only contain a SINGLE action, do NOT return a list of multiple actions. Here is an example of a valid $JSON_BLOB:
 
 ```
 {{{{
-  "action": "calculator",
-  "action_input": "1 + 2"
+  "action": $TOOL_NAME,
+  "action_input": $INPUT
 }}}}
 ```
 
