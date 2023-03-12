b9669444fc49c976da27f8b48ed43ab2cb19b92b
Harrison Chase <hw.chase.17@gmail.com>
2023-03-12 16:36:15 -0700

    cr

--- langchain/evaluation/loading.py
+++ langchain/evaluation/loading.py
@@ -1,8 +1,8 @@
-from typing import List, Dict
+from typing import Dict, List
 
 
 def load_dataset(uri: str) -> List[Dict]:
     from datasets import load_dataset
 
     dataset = load_dataset(f"LangChainDatasets/{uri}")
-    return [d for d in dataset['train']]
+    return [d for d in dataset["train"]]
