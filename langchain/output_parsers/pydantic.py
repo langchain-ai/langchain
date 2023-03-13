454fd6f81bf8b3db491f73b5929218b1824ba8bc
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2023-03-13 00:03:58 -0700

    hm

--- langchain/output_parsers/pydantic.py
+++ langchain/output_parsers/pydantic.py
@@ -1,9 +1,13 @@
 import json
+import subprocess
 from typing import Type
 
 from pydantic import BaseModel
-
+from tempfile import TemporaryDirectory
 from langchain.output_parsers.base import BaseOutputParser
+import logging
+
+logger = logging.getLogger(__name__)
 
 format_instructions = """The output should be a markdown code snippet formatted in the following schema:
 
@@ -12,69 +16,18 @@ format_instructions = """The output should be a markdown code snippet formatted 
 ```"""
 line_template = """"{name}": {type}"""
 
-
-def _get_description_from_prop(prop: dict, depth: int) -> str:
-    """Return the optional description of a property."""
-    indent = "\t" * depth
-    return f"{indent}// {prop['description']}\n" if "description" in prop else ""
+MAX_DEPTH = 6
 
 
-def _get_code_line(name: str, type: str, depth: int) -> str:
-    """Return the code line for a property."""
-    indent = "\t" * depth
-    return f"{indent}{line_template.format(name=name, type=type)}"
-
-
-def _get_sub_string(k: str, v: dict, depth: int) -> str:
-    """Return the sub-string for a property."""
-    description = _get_description_from_prop(v, depth)
-    if "type" not in v:
-        print(k, v, depth)
-    code = _get_code_line(k, v["type"], depth)
-    return description + code
-
-
-def process_nested_schema(k: str, v: dict, depth: int, definitions: dict) -> str:
-    ref = v["$ref"]
-    if not ref.startswith("#/definitions/"):
-        # Arbitrary JSON-schema references not yet supported.
-        raise ValueError(f"Unsupported $ref: {ref}")
-    ref = ref[len("#/definitions/") :]
-    if ref in definitions:
-        nested_type = get_nested_schema_str(definitions[ref], depth + 1)
-        result += _get_code_line(k, nested_type, depth)
-    else:
-        raise ValueError(f"Unknown definition {ref}")
-
-
-def get_nested_schema_str(schema: dict, depth: int = 1) -> str:
-    properties = schema.get("properties", {})
-    definitions = schema.get("definitions", {})
-    result = "{\n"
-    for k, v in properties.items():
-        if "$ref" in v:
-            result += process_nested_schema(
-                k, definitions[v["$ref"]], depth, definitions
-            )
-        elif "allOf" in v:
-            # Nested subschema are emitted as an anyOf
-            for sub_schema in v["allOf"]:
-                if "$ref" in sub_schema:
-                    result += process_nested_schema(
-                        k, definitions[sub_schema["$ref"]], depth, definitions
-                    )
-                elif "type" in sub_schema:
-                    result += _get_code_line(k, sub_schema["type"], depth)
-                else:
-                    raise ValueError(f"Unknown type: {sub_schema}")
-        elif "anyOf" in v:
-            raise NotImplementedError(f"Union types not yet supported: {v}")
-        else:
-            arg_str = _get_sub_string(k, v, depth)
-            result += f"{arg_str}\n"
-    final_indent = "\t" * (depth - 1)
-    result += f"{final_indent}}}"
-    return result
+def parse_json_schema_to_typescript(schema: dict) -> str:
+    """Return the string representation of schema."""
+    with TemporaryDirectory() as tmpdir:
+        with open(os.path.join(tmpdir, "schema.json"), "w") as f:
+            json.dump(schema, f)
+        schema_str = subprocess.check_output(
+            ["json2ts", "-i", "schema.json"], cwd=tmpdir
+        ).decode("utf-8")
+    return schema_str
 
 
 class PydanticOutputParser(BaseOutputParser):
