c22bc9d40053c8d249f4c5a82b23281db76d88a6
Tim Asp <707699+timothyasp@users.noreply.github.com>
2023-03-13 12:38:38 -0700

    lint

--- langchain/document_loaders/web_base.py
+++ langchain/document_loaders/web_base.py
@@ -8,7 +8,8 @@ from langchain.document_loaders.base import BaseLoader
 
 header_template = {
     "User-Agent": "",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*"
+    ";q=0.8",
     "Accept-Language": "en-US,en;q=0.5",
     "Referer": "https://www.google.com/",
     "DNT": "1",
