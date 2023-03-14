2170b631c043bbd5b9a6aa69a47c57b5b4252479
Brian Thorne <hardbyte@gmail.com>
2023-03-14 19:07:35 +1300

    Bugfix - allow custom input variables in chat zero shot agent's prompt (#1624)
    I was trying out the `chat-zero-shot-react-description` agent for
    [qabot](https://github.com/hardbyte/qabot/blob/dbbd31bb2702d433ed573bb177f1d1272d36f2ae/qabot/agents/data_query_chain.py#L35-L52)
    but langchain 0.0.108 doesn't correctly use custom 'input_variables` in
    the prompt template.

--- langchain/agents/chat/base.py
+++ langchain/agents/chat/base.py
@@ -73,9 +73,9 @@ class ChatAgent(Agent):
             SystemMessagePromptTemplate.from_template(template),
             HumanMessagePromptTemplate.from_template("{input}\n\n{agent_scratchpad}"),
         ]
-        return ChatPromptTemplate(
-            input_variables=["input", "agent_scratchpad"], messages=messages
-        )
+        if input_variables is None:
+            input_variables = ["input", "agent_scratchpad"]
+        return ChatPromptTemplate(input_variables=input_variables, messages=messages)
 
     @classmethod
     def from_llm_and_tools(
