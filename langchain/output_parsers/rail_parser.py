afee6e9ea4df35440777e19704d80242c04fbfb0
Shreya Rajpal <shreya.rajpal@gmail.com>
2023-03-14 17:53:09 -0700

    added guardrails output parser

---
+++ langchain/output_parsers/rail_parser.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+from typing import Dict
+
+from guardrails import Guard
+
+from langchain.output_parsers.base import BaseOutputParser
+from langchain.prompts import PromptTemplate
+
+
+class GuardrailOutputParser(BaseOutputParser):
+    guard = Guard
+
+    def _type(self) -> str:
+        return "guardrail"
+
+    @classmethod
+    def from_rail(cls, rail_file: str, num_reasks: int = 1) -> GuardrailOutputParser:
+        return cls(guard=Guard.from_rail(rail_file, num_reasks=num_reasks))
+
+    @classmethod
+    def from_rail_string(cls, rail_str: str, num_reasks: int = 1) -> GuardrailOutputParser:
+        return cls(guard=Guard.from_rail_string(rail_str, num_reasks=num_reasks))
+
+    def get_format_instructions(self) -> str:
+        return self.guard.raw_prompt.format_instructions
+
+    def to_prompt_template(self) -> PromptTemplate:
+        return PromptTemplate(
+            template=self.guard.base_prompt,
+            input_variables=self.guard.prompt.input_variables
+        )
+
+    def parse(self, text: str) -> Dict:
+        return self.guard.parse(text)
