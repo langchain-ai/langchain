7b411e2a4eb9dd59fdd3c1cb4d1e4f514a7b9c0b
Aidan Holland <thehappydinoa@gmail.com>
2023-03-17 09:24:48 -0400

    chore: refactor blackboard loader

--- langchain/document_loaders/blackboard.py
+++ langchain/document_loaders/blackboard.py
@@ -3,7 +3,7 @@ import contextlib
 import re
 from pathlib import Path
 from typing import Any, List, Optional, Tuple
-from urllib.parse import unquote, urlparse
+from urllib.parse import unquote
 
 from langchain.docstore.document import Document
 from langchain.document_loaders.web_base import WebBaseLoader
@@ -11,7 +11,26 @@ from langchain.document_loaders import PyPDFLoader, DirectoryLoader
 
 
 class BlackboardLoader(WebBaseLoader):
-    """Loader that loads all documents from a blackboard course."""
+    """Loader that loads all documents from a Blackboard course.
+
+    This loader is not compatible with all Blackboard courses. It is only
+    compatible with courses that use the new Blackboard interface.
+    To use this loader, you must have the BbRouter cookie. You can get this
+    cookie by logging into the course and then copying the value of the
+    BbRouter cookie from the browser's developer tools.
+
+    Example:
+        .. code-block:: python
+
+            from langchain.document_loaders import BlackboardLoader
+
+            loader = BlackboardLoader(
+                blackboard_course_url="https://blackboard.example.com/webapps/blackboard/execute/announcement?method=search&context=course_entry&course_id=_123456_1",
+                bbrouter="expires:12345...",
+            )
+            documents = loader.load()
+
+    """
 
     base_url: str
     folder_path: str
@@ -20,14 +39,24 @@ class BlackboardLoader(WebBaseLoader):
     def __init__(
         self,
         blackboard_course_url: str,
-        basic_auth: Optional[Tuple[str, str]] = None,
-        bbrouter: Optional[str] = None,
-        cookies: Optional[dict] = None,
+        bbrouter: str,
         load_all_recursively: bool = True,
+        basic_auth: Optional[Tuple[str, str]] = None,
+        cookies: Optional[dict] = None,
     ):
         """Initialize with blackboard course url.
 
         The BbRouter cookie is required for most blackboard courses.
+
+        Args:
+            blackboard_course_url: Blackboard course url.
+            bbrouter: BbRouter cookie.
+            load_all_recursively: If True, load all documents recursively.
+            basic_auth: Basic auth credentials.
+            cookies: Cookies.
+
+        Raises:
+            ValueError: If blackboard course url is invalid.
         """
         super().__init__(blackboard_course_url)
         # Get base url
@@ -41,14 +70,20 @@ class BlackboardLoader(WebBaseLoader):
             )
         if basic_auth is not None:
             self.session.auth = basic_auth
-        if cookies is not None:
-            self.session.cookies.update(cookies)
-        if bbrouter is not None:
-            self.session.cookies.update({"BbRouter": bbrouter})
+        # Combine cookies
+        if cookies is None:
+            cookies = {}
+        cookies.update({"BbRouter": bbrouter})
+        self.session.cookies.update(cookies)
         self.load_all_recursively = load_all_recursively
         self.check_bs4()
 
-    def check_bs4(self):
+    def check_bs4(self) -> None:
+        """Check if BeautifulSoup4 is installed.
+
+        Raises:
+            ImportError: If BeautifulSoup4 is not installed.
+        """
         try:
             import bs4  # noqa: F401
         except ImportError:
@@ -57,19 +92,12 @@ class BlackboardLoader(WebBaseLoader):
                 "Please install it with `pip install beautifulsoup4`."
             )
 
-    def _scrape(self, url: str) -> Any:
-        from bs4 import BeautifulSoup
-
-        html_doc = self.session.get(url)
-        soup = BeautifulSoup(html_doc.text, "html.parser")
-        return soup
-
-    def scrape(self) -> Any:
-        """Scrape data from webpage and return it in BeautifulSoup format."""
-        return self._scrape(self.web_path)
-
     def load(self) -> List[Document]:
-        """Load documents."""
+        """Load data into document objects.
+
+        Returns:
+            List of documents.
+        """
         if self.load_all_recursively:
             soup_info = self.scrape()
             self.folder_path = self._get_folder_path(soup_info)
@@ -88,39 +116,15 @@ class BlackboardLoader(WebBaseLoader):
             self.folder_path = self._get_folder_path(soup_info)
             return self._get_documents(soup_info)
 
-    def _get_documents(self, soup: Any) -> List[Document]:
-        """Fetch content from page and return Documents."""
-        from bs4 import Tag
-
-        # Get content list
-        content_list = soup.find("ul", {"class": "contentList"})
-        if content_list is None:
-            raise ValueError("No content list found.")
-        # Get all attachments
-        attachments = []
-        for attachment in content_list.find_all("ul", {"class": "attachments"}):
-            for link in attachment.find_all("a"):
-                link: Tag
-                href = link.get("href")
-                # Only add if href is not None and the text contains .pdf
-                if href is not None:
-                    attachments.append(href)
-        # Make sure the folder exists
-        Path(self.folder_path).mkdir(parents=True, exist_ok=True)
-        # Download all attachments
-        for attachment in attachments:
-            self.download(attachment)
-        # Create the document loader
-        loader = DirectoryLoader(
-            path=Path(self.folder_path), glob="*.pdf", loader_cls=PyPDFLoader
-        )
-        # Load the documents
-        documents = loader.load()
-        # Return all documents
-        return documents
-
     def _get_folder_path(self, soup: Any) -> str:
-        """Get the folder path to save the documents in."""
+        """Get the folder path to save the documents in.
+
+        Args:
+            soup: BeautifulSoup4 soup object.
+
+        Returns:
+            Folder path.
+        """
         # Get the course name
         course_name = soup.find("span", {"id": "crumb_1"})
         if course_name is None:
@@ -128,11 +132,88 @@ class BlackboardLoader(WebBaseLoader):
         course_name = course_name.text.strip()
         # Prepare the folder path
         course_name_clean = (
-            unquote(course_name).replace(" ", "_").replace("/", "_").replace(":", "_")
+            unquote(course_name)
+            .replace(" ", "_")
+            .replace("/", "_")
+            .replace(":", "_")
+            .replace(",", "_")
+            .replace("?", "_")
+            .replace("'", "_")
+            .replace("!", "_")
+            .replace('"', "_")
         )
         # Get the folder path
         folder_path = Path(".") / course_name_clean
         return str(folder_path)
+
+    def _get_documents(self, soup: Any) -> List[Document]:
+        """Fetch content from page and return Documents.
+
+        Args:
+            soup: BeautifulSoup4 soup object.
+
+        Returns:
+            List of documents.
+        """
+        attachments = self._get_attachments(soup)
+        self._download_attachments(attachments)
+        documents = self._load_documents()
+        return documents
+
+    def _get_attachments(self, soup: Any) -> List[str]:
+        """Get all attachments from a page.
+
+        Args:
+            soup: BeautifulSoup4 soup object.
+
+        Returns:
+            List of attachments.
+        """
+        from bs4 import Tag, BeautifulSoup
+
+        # Get content list
+        content_list = soup.find("ul", {"class": "contentList"})
+        if content_list is None:
+            raise ValueError("No content list found.")
+        content_list: BeautifulSoup  # type: ignore
+        # Get all attachments
+        attachments = []
+        for attachment in content_list.find_all("ul", {"class": "attachments"}):
+            attachment: Tag  # type: ignore
+            for link in attachment.find_all("a"):
+                link: Tag  # type: ignore
+                href = link.get("href")
+                # Only add if href is not None and does not start with #
+                if href is not None and not href.startswith("#"):
+                    attachments.append(href)
+        return attachments
+
+    def _download_attachments(self, attachments: List[str]) -> None:
+        """Download all attachments.
+
+        Args:
+            attachments: List of attachments.
+        """
+        # Make sure the folder exists
+        Path(self.folder_path).mkdir(parents=True, exist_ok=True)
+        # Download all attachments
+        for attachment in attachments:
+            self.download(attachment)
+
+    def _load_documents(self) -> List[Document]:
+        """Load all documents in the folder.
+
+        Returns:
+            List of documents.
+        """
+        # Create the document loader
+        loader = DirectoryLoader(
+            path=self.folder_path, glob="*.pdf", loader_cls=PyPDFLoader  # type: ignore
+        )
+        # Load the documents
+        documents = loader.load()
+        # Return all documents
+        return documents
 
     def _get_paths(self, soup: Any) -> List[str]:
         """Get all relative paths in the navbar."""
@@ -146,10 +227,14 @@ class BlackboardLoader(WebBaseLoader):
                 relative_paths.append(href)
         return relative_paths
 
-    def download(self, url: str):
-        """Download a file from a url."""
+    def download(self, path: str) -> None:
+        """Download a file from a url.
+
+        Args:
+            path: Path to the file.
+        """
         # Get the file content
-        response = self.session.get(self.base_url + url, allow_redirects=True)
+        response = self.session.get(self.base_url + path, allow_redirects=True)
         # Get the filename
         filename = self.parse_filename(response.url)
         # Write the file to disk
@@ -157,56 +242,51 @@ class BlackboardLoader(WebBaseLoader):
             f.write(response.content)
 
     def parse_filename(self, url: str) -> str:
-        """Parse the filename from a url."""
-        # Check if the file path is a pdf
-        filename = Path(url)
-        if filename.suffix == ".pdf":
-            # If so, return the file path
-            filename = filename.name
-        else:
-            # Using regex to get the filename from the url
-            filename_matches = re.search(r"filename%2A%3DUTF-8%27%27(.+)", url)
-            # If the regex matches
-            if filename_matches:
-                # Get the filename from the first group
-                filename = filename_matches.group(1)
-            else:
-                # Otherwise, raise
-                raise ValueError(f"Could not parse filename from {url}")
-        # Check to see if it is a pdf
-        if ".pdf" not in filename:
-            # If not a pdf, raise
-            raise ValueError(f"Incorrect file type: {filename}")
-        # Remove everything after the .pdf
-        filename = filename.split(".pdf")[0] + ".pdf"
-        # Url decode the filename
-        filename = unquote(filename)
-        # Replace %20 with spaces
-        filename = filename.replace("%20", " ")
-        # Return the filename
-        return filename
+        """Parse the filename from a url.
 
-    def parse_qs(query_string: str) -> dict:
-        """Parse a query string into a dict."""
-        # Split the query string into a list of key=value pairs
-        pairs = query_string.split("&")
-        # Initialize an empty dict
-        query_dict = {}
-        # Iterate over the list of key=value pairs
-        for pair in pairs:
-            # Split the pair into a list of key and value
-            key, value = pair.split("=")
-            # Add the key and value to the dict
-            query_dict[key] = value
-        # Return the dict
-        return query_dict
+        Args:
+            url: Url to parse the filename from.
+
+        Returns:
+            The filename.
+        """
+        if (url_path := Path(url)) and url_path.suffix == ".pdf":
+            return url_path.name
+        else:
+            return self._parse_filename_from_url(url)
+
+    def _parse_filename_from_url(self, url: str) -> str:
+        """Parse the filename from a url.
+
+        Args:
+            url: Url to parse the filename from.
+
+        Returns:
+            The filename.
+
+        Raises:
+            ValueError: If the filename could not be parsed.
+        """
+        filename_matches = re.search(r"filename%2A%3DUTF-8%27%27(.+)", url)
+        if filename_matches:
+            filename = filename_matches.group(1)
+        else:
+            raise ValueError(f"Could not parse filename from {url}")
+        if ".pdf" not in filename:
+            raise ValueError(f"Incorrect file type: {filename}")
+        filename = filename.split(".pdf")[0] + ".pdf"
+        filename = unquote(filename)
+        filename = filename.replace("%20", " ")
+        return filename
 
 
 if __name__ == "__main__":
     loader = BlackboardLoader(
-        "https://<YOUR BLACKBOARD URL HERE>/webapps/blackboard/content/listContent.jsp?course_id=_<YOUR COURSE ID HERE>_1&content_id=_<YOUR CONTENT ID HERE>_1&mode=reset",
+        "https://<YOUR BLACKBOARD URL"
+        " HERE>/webapps/blackboard/content/listContent.jsp?course_id=_<YOUR COURSE ID"
+        " HERE>_1&content_id=_<YOUR CONTENT ID HERE>_1&mode=reset",
+        "<YOUR BBROUTER COOKIE HERE>",
         load_all_recursively=True,
-        bbrouter="<YOUR BBROUTER COOKIE HERE>",
     )
     documents = loader.load()
     print(f"Loaded {len(documents)} pages of PDFs from {loader.web_path}")
