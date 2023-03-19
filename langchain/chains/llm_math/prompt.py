67f3af43864ee302101c2dbc2ad2874410e3e8cc
vbarda <vadim.barda@gmail.com>
2023-03-19 17:18:15 -0400

    update the LLM math prompt to use simpler logic

--- langchain/chains/llm_math/prompt.py
+++ langchain/chains/llm_math/prompt.py
@@ -1,24 +1,15 @@
 # flake8: noqa
 from langchain.prompts.prompt import PromptTemplate
 
-_PROMPT_TEMPLATE = """You are GPT-3, and you can't do math.
+_PROMPT_TEMPLATE = """Translate a math problem into Python code that can be executed in Python 3 REPL. Use the output of running this code to answer the question.
 
-You can do basic math, and your memorization abilities are impressive, but you can't do any complex calculations that a human could not do in their head. You also have an annoying tendency to just make up highly specific, but wrong, answers.
-
-So we hooked you up to a Python 3 kernel, and now you can execute code. If anyone gives you a hard math problem, just use this format and we’ll take care of the rest:
-
-Question: ${{Question with hard calculation.}}
+Question: ${{Question with math problem.}}
 ```python
-${{Code that prints what you need to know}}
+${{Code that solves the problem and prints the solution}}
 ```
 ```output
-${{Output of your code}}
+${{Output of running the code}}
 ```
-Answer: ${{Answer}}
-
-Otherwise, use this simpler format:
-
-Question: ${{Question without hard calculation}}
 Answer: ${{Answer}}
 
 Begin.
