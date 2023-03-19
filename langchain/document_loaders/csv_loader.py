b79222425c4895c0d0c2045c59832fca7c5c1bc3
Harrison Chase <hw.chase.17@gmail.com>
2023-03-19 10:08:32 -0700

    cr

--- langchain/document_loaders/csv_loader.py
+++ langchain/document_loaders/csv_loader.py
@@ -11,9 +11,12 @@ class CSVLoader(BaseLoader):
     Each document represents one row of the CSV file. Every row is converted into a
     key/value pair and outputted to a new line in the document's page_content.
 
-    The source for each document loaded from csv is set to the value of the `file_path` argument for all doucments by default.
-    You can override this by setting the `source_column` argument to the name of a column in the CSV file.
-    The source of each document will then be set to the value of the column with the name specified in `source_column`.
+    The source for each document loaded from csv is set to the value of the
+    `file_path` argument for all doucments by default.
+    You can override this by setting the `source_column` argument to the
+    name of a column in the CSV file.
+    The source of each document will then be set to the value of the column
+    with the name specified in `source_column`.
 
     Output Example:
         .. code-block:: txt
@@ -23,7 +26,12 @@ class CSVLoader(BaseLoader):
             column3: value3
     """
 
-    def __init__(self, file_path: str, source_column: str = None, csv_args: Optional[Dict] = None):
+    def __init__(
+        self,
+        file_path: str,
+        source_column: Optional[str] = None,
+        csv_args: Optional[Dict] = None,
+    ):
         self.file_path = file_path
         self.source_column = source_column
         if csv_args is None:
@@ -40,13 +48,13 @@ class CSVLoader(BaseLoader):
         with open(self.file_path, newline="") as csvfile:
             csv = DictReader(csvfile, **self.csv_args)  # type: ignore
             for i, row in enumerate(csv):
-                docs.append(
-                    Document(
-                        page_content="\n".join(
-                            f"{k.strip()}: {v.strip()}" for k, v in row.items()
-                        ),
-                        metadata={"source": row[self.source_column] if self.source_column else self.file_path, "row": i},
-                    )
-                )
+                content = "\n".join(f"{k.strip()}: {v.strip()}" for k, v in row.items())
+                if self.source_column is not None:
+                    source = row[self.source_column]
+                else:
+                    source = self.file_path
+                metadata = {"source": source, "row": i}
+                doc = Document(page_content=content, metadata=metadata)
+                docs.append(doc)
 
         return docs
