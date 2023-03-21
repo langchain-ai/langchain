d5f6019d21c918e65ca6d97829ffa090452693f6
Eli Berg <ejdberg@gmail.com>
2023-03-21 15:43:02 -0700

    Propagate "filter" arg in Chroma similarity_search to delegated call to similarity_search_with_score

--- tests/integration_tests/vectorstores/fake_embeddings.py
+++ tests/integration_tests/vectorstores/fake_embeddings.py
@@ -10,9 +10,13 @@ class FakeEmbeddings(Embeddings):
     """Fake embeddings functionality for testing."""
 
     def embed_documents(self, texts: List[str]) -> List[List[float]]:
-        """Return simple embeddings."""
+        """Return simple embeddings which encode each text as its index
+        in the input list."""
         return [[float(1.0)] * 9 + [float(i)] for i in range(len(texts))]
 
     def embed_query(self, text: str) -> List[float]:
-        """Return simple embeddings."""
+        """Return simple query embedding matching the embedding of the 
+        first text of any list passed to embed_documents. Computed distance 
+        to any embedded document will be the index of that document as it was 
+        passed to embed_documents."""
         return [float(1.0)] * 9 + [float(0.0)]
