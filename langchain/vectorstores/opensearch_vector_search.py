1942c2a0f697ce960b8a1903a0a7dc8ad44d8d7e
Mehmet Öner Yalçın <oneryalcin@gmail.com>
2023-03-23 02:37:56 +0000

    Fix the issue of OpenSearch index name being non-deterministic (#1901)
    - Allow to get it from kwargs (index_name) or
    - Allow to get it from env var (OPENSEARCH_INDEX_NAME)
    - if none available generate an uuid index name as fallback
    
    References: https://github.com/hwchase17/langchain/issues/1900

--- langchain/vectorstores/opensearch_vector_search.py
+++ langchain/vectorstores/opensearch_vector_search.py
@@ -363,7 +363,8 @@ class OpenSearchVectorSearch(VectorStore):
         embeddings = embedding.embed_documents(texts)
         _validate_embeddings_and_bulk_size(len(embeddings), bulk_size)
         dim = len(embeddings[0])
-        index_name = uuid.uuid4().hex
+        # Get the index name from either from kwargs or ENV Variable before falling back to random generation
+        index_name = get_from_dict_or_env(kwargs, "index_name", "OPENSEARCH_INDEX_NAME", default=uuid.uuid4().hex)
         is_appx_search = _get_kwargs_value(kwargs, "is_appx_search", True)
         if is_appx_search:
             engine = _get_kwargs_value(kwargs, "engine", "nmslib")
