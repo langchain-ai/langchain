12f868b292eb3e56c22f772d3a150a32c10e4c0b
Eli <ejdberg@gmail.com>
2023-03-22 19:40:10 -0700

    Propagate "filter" arg in Chroma similarity_search (#1869)
    Technically a duplicate fix to #1619 but with unit tests and a small
    documentation update
    - Propagate `filter` arg in Chroma `similarity_search` to delegated call
    to `similarity_search_with_score`
    - Add `filter` arg to `similarity_search_by_vector`
    - Clarify doc strings on FakeEmbeddings

--- tests/integration_tests/vectorstores/fake_embeddings.py
+++ tests/integration_tests/vectorstores/fake_embeddings.py
@@ -10,9 +10,13 @@ class FakeEmbeddings(Embeddings):
     """Fake embeddings functionality for testing."""
 
     def embed_documents(self, texts: List[str]) -> List[List[float]]:
-        """Return simple embeddings."""
+        """Return simple embeddings.
+        Embeddings encode each text as its index."""
         return [[float(1.0)] * 9 + [float(i)] for i in range(len(texts))]
 
     def embed_query(self, text: str) -> List[float]:
-        """Return simple embeddings."""
+        """Return constant query embeddings.
+        Embeddings are identical to embed_documents(texts)[0].
+        Distance to each text will be that text's index,
+        as it was passed to embed_documents."""
         return [float(1.0)] * 9 + [float(0.0)]
