cc4c1887c6a7ce0916e247b855f2019966c2d3c2
tonyabracadabra <tongxupeng.cpu@gmail.com>
2023-03-26 16:31:56 +0800

    chore: pydantic for aiplugin

--- langchain/tools/plugin.py
+++ langchain/tools/plugin.py
@@ -1,28 +1,56 @@
 from __future__ import annotations
 
+from typing import Optional
+
 import requests
+from pydantic import BaseModel
 
 from langchain.tools.base import BaseTool
 
 
+class ApiConfig(BaseModel):
+    type: str
+    url: str
+    has_user_authentication: Optional[bool] = False
+
+
+class AIPlugin(BaseModel):
+    schema_version: str
+    name_for_model: str
+    name_for_human: str
+    description_for_model: str
+    description_for_human: str
+    auth: Optional[dict] = None
+    api: ApiConfig
+    logo_url: Optional[str]
+    contact_email: Optional[str]
+    legal_info_url: Optional[str]
+
+
 class AIPluginTool(BaseTool):
+    plugin: AIPlugin
     api_spec: str
 
     @classmethod
     def from_plugin_url(cls, url: str) -> AIPluginTool:
-        response = requests.get(url).json()
+        cls.plugin = AIPlugin(**requests.get(url).json())
         description = (
             f"Call this tool to get the OpenAPI spec (and usage guide) "
-            f"for interacting with the {response['name_for_human']} API. "
+            f"for interacting with the {cls.plugin.name_for_human} API. "
             f"You should only call this ONCE! What is the "
-            f"{response['name_for_human']} API useful for? "
-        ) + response["description_for_human"]
+            f"{cls.plugin.name_for_human} API useful for? "
+        ) + cls.plugin.description_for_human
+
         api_spec = (
-            f"Usage Guide: {response['description_for_model']}\n\n"
-            f"OpenAPI Spec: {requests.get(response['api']['url']).json()}"
+            f"Usage Guide: {cls.plugin.description_for_model}\n\n"
+            f"OpenAPI Spec: {requests.get(cls.plugin.api.url).json()}"
         )
+
         return cls(
-            name=response["name_for_model"], description=description, api_spec=api_spec
+            name=cls.plugin.name_for_model,
+            description=description,
+            plugin=cls.plugin,
+            api_spec=api_spec,
         )
 
     def _run(self, tool_input: str) -> str:
