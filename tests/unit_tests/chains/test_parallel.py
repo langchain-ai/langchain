fa5fe81f3203d3582c2ddd23cf536053f122f083
Michael Chang <95changmichael13@gmail.com>
2023-03-28 18:19:48 -0400

    test: tests concurrent behavior of SimpleParallelChain in commit cf1b626
    This also updates the tests such that the output variables and the
    chains get combined into a single dictionary, given the refactor from
    commit cf1b626.

--- tests/unit_tests/chains/test_parallel.py
+++ tests/unit_tests/chains/test_parallel.py
@@ -1,4 +1,6 @@
 """Test parallel functionality."""
+import random
+import time
 from typing import Dict, List
 
 import pytest
@@ -26,6 +28,7 @@ class FakeChain(Chain, BaseModel):
         return self.output_variables
 
     def _call(self, inputs: Dict[str, str]) -> Dict[str, str]:
+        time.sleep(random.uniform(1, 2))
         outputs = {}
         for var in self.output_variables:
             variables = [inputs[k] for k in self.input_variables]
@@ -37,23 +40,26 @@ def test_parallel_usage_single_input() -> None:
     """Test parallel on single input."""
     input_variables = ["input"]
     chain = SimpleParallelChain(
-        chains=[
-            FakeChain(
+        input_variables=input_variables,
+        chains={
+            "output1": FakeChain(
                 input_variables=input_variables,
                 output_variables=["chain_out1"],
                 chain_id=1,
             ),
-            FakeChain(
+            "output2": FakeChain(
                 input_variables=input_variables,
                 output_variables=["chain_out2"],
                 chain_id=2,
             ),
-        ],
-        input_variables=input_variables,
-        output_variables=["output1", "output2"],
+        },
     )
     output = chain("bar")
-    expected_output = {"input": "bar", "output1": "bar 1", "output2": "bar 2"}
+    expected_output = {
+        "input": "bar",
+        "output1/chain_out1": "bar 1",
+        "output2/chain_out2": "bar 2",
+    }
     assert output == expected_output
 
 
@@ -61,24 +67,26 @@ def test_parallel_usage_multiple_inputs() -> None:
     """Test parallel on multiple inputs."""
     input_variables = ["input1", "input2"]
     chain = SimpleParallelChain(
-        chains=[
-            FakeChain(
+        input_variables=input_variables,
+        chains={
+            "output1": FakeChain(
                 input_variables=input_variables,
                 output_variables=["chain_out1"],
                 chain_id=1,
             ),
-            FakeChain(
+            "output2": FakeChain(
                 input_variables=input_variables,
                 output_variables=["chain_out2"],
                 chain_id=2,
             ),
-        ],
-        input_variables=input_variables,
-        output_variables=["output1", "output2"],
+        },
     )
     inputs = {"input1": "foo", "input2": "bar"}
     output = chain(inputs)
-    expected_output = {"output1": "foo bar 1", "output2": "foo bar 2"}
+    expected_output = {
+        "output1/chain_out1": "foo bar 1",
+        "output2/chain_out2": "foo bar 2",
+    }
     assert output == {**inputs, **expected_output}
 
 
@@ -86,40 +94,91 @@ def test_parallel_usage_one_chain_single_output() -> None:
     """Test parallel on multiple inputs."""
     input_variables = ["input1", "input2"]
     chain = SimpleParallelChain(
-        chains=[
-            FakeChain(
+        input_variables=input_variables,
+        chains={
+            "output1": FakeChain(
                 input_variables=input_variables,
                 output_variables=["chain_out1"],
                 chain_id=1,
             ),
-        ],
-        input_variables=input_variables,
-        output_variables=["output1"],
+        },
     )
     inputs = {"input1": "foo", "input2": "bar"}
     output = chain(inputs)
-    expected_output = {"output1": "foo bar 1"}
+    expected_output = {"output1/chain_out1": "foo bar 1"}
     assert output == {**inputs, **expected_output}
 
 
-def test_parallel_error_mismatched_inputs() -> None:
-    """Test error is raised when the input variables to the parallel chain do not match the input variables to the chains."""
+def test_parallel_error_zero_chains() -> None:
+    """Test error is raised when there are no chains."""
     with pytest.raises(ValueError):
         SimpleParallelChain(
-            chains=[
-                FakeChain(
+            input_variables=["input1", "input2"],
+            chains={},
+        )
+
+
+# async tests
+def test_parallel_concurrency_speedup() -> None:
+    """Test concurrent execution runs faster than serial execution."""
+    num_child_chains = 10
+
+    input_variables = ["input1", "input2"]
+
+    chain = SimpleParallelChain(
+        input_variables=input_variables,
+        chains={
+            f"output{i}": FakeChain(
+                input_variables=input_variables,
+                output_variables=["chain_out{i}"],
+                chain_id=i,
+            )
+            for i in range(num_child_chains)
+        },
+    )
+
+    inputs = {"input1": "foo", "input2": "bar"}
+
+    # measure time with concurrency
+    start_time_concurrent = time.time()
+    chain(inputs)
+    end_time_concurrent = time.time()
+
+    # measure time without concurrency
+    chain.concurrent = False
+    start_time_serial = time.time()
+    chain(inputs)
+    end_time_serial = time.time()
+
+    # check that concurrent execution is faster.
+    # Serial execution will run for >= 10 sec because each child chain sleeps >= sec.
+    # Parallel execution will run for <= 2 sec because each child chain sleeps <= 2 sec.
+    assert (
+        end_time_concurrent - start_time_concurrent
+        < end_time_serial - start_time_serial
+    )
+
+
+# if we allow child chains to have different inputs, we should remove the following tests
+
+
+def test_parallel_error_mismatched_inputs() -> None:
+    """Test error is raised when input variables to the parallel chain do not match those of the child chains."""
+    with pytest.raises(ValueError):
+        SimpleParallelChain(
+            input_variables=["input1", "input3"],
+            chains={
+                "output1": FakeChain(
                     input_variables=["input1", "input2"],
                     output_variables=["chain_out1"],
                     chain_id=1,
                 ),
-                FakeChain(
+                "output2": FakeChain(
                     input_variables=["input1", "input2"],
                     output_variables=["chain_out2"],
                     chain_id=2,
                 ),
-            ],
-            input_variables=["input1", "input3"],
-            output_variables=["output1", "output2"],
+            },
         )
 
 
@@ -127,89 +186,17 @@ def test_parallel_error_mismatched_inputs_between_chains() -> None:
     """Test error is raised when the input variables to different chains do not match."""
     with pytest.raises(ValueError):
         SimpleParallelChain(
-            chains=[
-                FakeChain(
+            input_variables=["input1", "input2"],
+            chains={
+                "output1": FakeChain(
                     input_variables=["input1", "input2"],
                     output_variables=["chain_out1"],
                     chain_id=1,
                 ),
-                FakeChain(
+                "output2": FakeChain(
                     input_variables=["input1", "input3"],
                     output_variables=["chain_out2"],
                     chain_id=2,
                 ),
-            ],
-            input_variables=["input1", "input2"],
-            output_variables=["output1", "output2"],
-        )
-
-
-def test_parallel_error_single_chain_multiple_outputs() -> None:
-    """Test error is raised when there is a single chain but only multiple outputs."""
-    input_variables = ["input1", "input2"]
-    with pytest.raises(ValueError):
-        SimpleParallelChain(
-            chains=[
-                FakeChain(
-                    input_variables=input_variables,
-                    output_variables=["chain_out1"],
-                    chain_id=1,
-                ),
-            ],
-            input_variables=input_variables,
-            output_variables=["output1", "output2"],
-        )
-
-
-def test_parallel_error_multiple_chains_single_output() -> None:
-    """Test error is raised when there are multiple chains but only a single output."""
-    input_variables = ["input1", "input2"]
-    with pytest.raises(ValueError):
-        SimpleParallelChain(
-            chains=[
-                FakeChain(
-                    input_variables=input_variables,
-                    output_variables=["chain_out1"],
-                    chain_id=1,
-                ),
-                FakeChain(
-                    input_variables=input_variables,
-                    output_variables=["chain_out2"],
-                    chain_id=2,
-                ),
-            ],
-            input_variables=input_variables,
-            output_variables=["output1"],
-        )
-
-
-def test_parallel_error_different_number_of_chains_than_outputs() -> None:
-    """Test error is raised when there are different numbers of chains than outputs."""
-    input_variables = ["input1", "input2"]
-    with pytest.raises(ValueError):
-        SimpleParallelChain(
-            chains=[
-                FakeChain(
-                    input_variables=input_variables,
-                    output_variables=["chain_out1"],
-                    chain_id=1,
-                ),
-                FakeChain(
-                    input_variables=input_variables,
-                    output_variables=["chain_out2"],
-                    chain_id=2,
-                ),
-            ],
-            input_variables=input_variables,
-            output_variables=["output1", "output2", "output3"],
-        )
-
-
-def test_parallel_error_zero_chains() -> None:
-    """Test error is raised when there are no chains."""
-    with pytest.raises(ValueError):
-        SimpleParallelChain(
-            chains=[],
-            input_variables=["input1", "input2"],
-            output_variables=["output1", "output2"],
+            },
         )
