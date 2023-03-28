b0aaa758a288879e3bc78855aca624a3bcfec7a5
Moshe <hello@moshemalka.me>
2023-03-28 02:33:02 -0400

    WhatsApp Document Loader (#2012)
    Add support for WhatsApp Document Loading
    
    Co-authored-by: Harrison Chase <hw.chase.17@gmail.com>

--- langchain/document_loaders/__init__.py
+++ langchain/document_loaders/__init__.py
@@ -55,6 +55,8 @@ from langchain.document_loaders.unstructured import (
 )
 from langchain.document_loaders.url import UnstructuredURLLoader
 from langchain.document_loaders.web_base import WebBaseLoader
+from langchain.document_loaders.whatsapp_chat import WhatsAppChatLoader
+
 from langchain.document_loaders.word_document import UnstructuredWordDocumentLoader
 from langchain.document_loaders.youtube import (
     GoogleApiClient,
@@ -113,6 +115,7 @@ __all__ = [
     "GoogleApiClient",
     "CSVLoader",
     "BlackboardLoader",
+    "WhatsAppChatLoader",
     "DataFrameLoader",
     "AzureBlobStorageFileLoader",
     "AzureBlobStorageContainerLoader",
