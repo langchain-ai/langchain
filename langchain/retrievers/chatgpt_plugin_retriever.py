f89f2c696fe7b28cb79e4c71ce256d7cb373ff34
Kei Kamikawa <Code-Hex@users.noreply.github.com>
2023-03-29 23:44:45 +0900

    async support for ChatGPTPluginRetriever

--- langchain/retrievers/chatgpt_plugin_retriever.py
+++ langchain/retrievers/chatgpt_plugin_retriever.py
@@ -1,5 +1,6 @@
-from typing import List
+from typing import List, Optional
 
+import aiohttp
 import requests
 from pydantic import BaseModel
 
@@ -9,6 +10,7 @@ from langchain.schema import BaseRetriever, Document
 class ChatGPTPluginRetriever(BaseRetriever, BaseModel):
     url: str
     bearer_token: str
+    aiosession: Optional[aiohttp.ClientSession] = None
 
     def get_relevant_documents(self, query: str) -> List[Document]:
         response = requests.post(
@@ -20,6 +22,31 @@ class ChatGPTPluginRetriever(BaseRetriever, BaseModel):
             },
         )
         results = response.json()["results"][0]["results"]
+        docs = []
+        for d in results:
+            content = d.pop("text")
+            docs.append(Document(page_content=content, metadata=d))
+        return docs
+
+    async def aget_relevant_documents(self, query: str) -> List[Document]:
+        url = f"{self.url}/query"
+        json = {"queries": [{"query": query}]}
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.bearer_token}",
+        }
+
+        if not self.aiosession:
+            async with aiohttp.ClientSession() as session:
+                async with session.post(url, headers=headers, json=json) as response:
+                    res = await response.json()
+        else:
+            async with self.aiosession.post(
+                url, headers=headers, json=json
+            ) as response:
+                res = await response.json()
+
+        results = res["results"][0]["results"]
         docs = []
         for d in results:
             content = d.pop("text")
