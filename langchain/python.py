a62602731cf66fdb1257b9789335f92bb5558c69
Nikolaos Smyrnioudis <smyrnioudis.nikolaos@outlook.com>
2023-03-29 18:28:24 +0200

    Remove backticks if included in Python REPL from LLM

--- langchain/python.py
+++ langchain/python.py
@@ -6,14 +6,25 @@ from typing import Dict, Optional
 from pydantic import BaseModel, Field
 
 
+def remove_backticks_if_they_exist(command: str) -> str:
+    if (len(command) >= 6 and command[:3] == "```" and command[:3] == "```"):
+        command = command[3:-3]
+    return command
+
+
 class PythonREPL(BaseModel):
     """Simulates a standalone Python REPL."""
 
     globals: Optional[Dict] = Field(default_factory=dict, alias="_globals")
     locals: Optional[Dict] = Field(default_factory=dict, alias="_locals")
 
+    
+    
     def run(self, command: str) -> str:
         """Run command with own globals/locals and returns anything printed."""
+        # Ignore any backticks if provided by chat models, ChatGPT does this
+        command = remove_backticks_if_they_exist(command)
+            
         old_stdout = sys.stdout
         sys.stdout = mystdout = StringIO()
         try:
