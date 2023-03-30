491a6d5b9717a455b2ff3941e08a0d763eeb1c61
kevinNejad <keyv.krmn@gmail.com>
2023-03-30 10:30:46 +0100

    add error message to the google drive document loader

--- langchain/document_loaders/googledrive.py
+++ langchain/document_loaders/googledrive.py
@@ -143,6 +143,7 @@ class GoogleDriveLoader(BaseLoader, BaseModel):
 
         from googleapiclient.discovery import build
         from googleapiclient.http import MediaIoBaseDownload
+        from googleapiclient.errors import HttpError
 
         creds = self._load_credentials()
         service = build("drive", "v3", credentials=creds)
@@ -151,8 +152,17 @@ class GoogleDriveLoader(BaseLoader, BaseModel):
         fh = BytesIO()
         downloader = MediaIoBaseDownload(fh, request)
         done = False
-        while done is False:
-            status, done = downloader.next_chunk()
+        try:
+            while done is False:
+                status, done = downloader.next_chunk()
+            
+        except HttpError as e:
+            if e.resp.status == 404:
+                print("File not found: {}".format(id))
+            else:
+                print("An error occurred: {}".format(e))
+            return None
+
         text = fh.getvalue().decode("utf-8")
         metadata = {"source": f"https://docs.google.com/document/d/{id}/edit"}
         return Document(page_content=text, metadata=metadata)
