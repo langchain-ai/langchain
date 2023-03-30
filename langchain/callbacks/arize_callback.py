a45b6bfb732a549095d27a56c3908e06bf0d01fc
Harrison Chase <hw.chase.17@gmail.com>
2023-03-29 22:45:24 -0700

    cr

--- langchain/callbacks/arize_callback.py
+++ langchain/callbacks/arize_callback.py
@@ -1,18 +1,12 @@
 # Import the necessary packages for ingestion
-from typing import Any, Dict, List, Optional, Union
 import uuid
+from typing import Any, Dict, List, Optional, Union
+
+import pandas as pd
+
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain.schema import AgentAction, AgentFinish, LLMResult
-import pandas as pd
-from datetime import datetime
 
-# Import Arize package
-from arize.api import Client
-from arize.utils.types import ModelTypes, Environments, Schema, Metrics
-from arize.utils.types import Environments, ModelTypes, EmbeddingColumnNames, Schema
-from arize.utils.types import ModelTypes, Environments, Schema, Metrics
-from arize.utils.types import EmbeddingColumnNames, Embedding
-from arize.pandas.embeddings import EmbeddingGenerator, UseCases 
 
 class ArizeCallbackHandler(BaseCallbackHandler):
     """Callback Handler that logs to Arize platform."""
@@ -24,7 +18,6 @@ class ArizeCallbackHandler(BaseCallbackHandler):
         SPACE_KEY: Optional[str] = None,
         API_KEY: Optional[str] = None,
     ) -> None:
-
         """Initialize callback handler."""
 
         super().__init__()
@@ -37,24 +30,32 @@ class ArizeCallbackHandler(BaseCallbackHandler):
         self.space_key = SPACE_KEY
         self.api_key = API_KEY
 
-        # Initialize empty lists to store the prompt/response pairs and other necessary data.
-        self.prompt_records = []
-        self.response_records = []
-        self.prediction_ids = []
-        self.pred_timestamps = []
-        self.response_embeddings = []
-        self.prompt_embeddings = []
-        self.prompt_tokens = 0 
-        self.completion_tokens = 0 
+        # Initialize empty lists to store the prompt/response pairs
+        # and other necessary data.
+        self.prompt_records: List = []
+        self.response_records: List = []
+        self.prediction_ids: List = []
+        self.pred_timestamps: List = []
+        self.response_embeddings: List = []
+        self.prompt_embeddings: List = []
+        self.prompt_tokens = 0
+        self.completion_tokens = 0
         self.total_tokens = 0
-        
-        # Create an embedding generator for generating embeddings from prompts and responses.
-        self.generator = EmbeddingGenerator.from_use_case(use_case=UseCases.NLP.SEQUENCE_CLASSIFICATION,
-                                                          model_name="distilbert-base-uncased",
-                                                          tokenizer_max_length=512,
-                                                          batch_size=256)
-        
-        # Create an Arize client and check if the SPACE_KEY and API_KEY are not set to the default values.
+
+        from arize.api import Client
+        from arize.pandas.embeddings import EmbeddingGenerator, UseCases
+
+        # Create an embedding generator for generating embeddings
+        # from prompts and responses.
+        self.generator = EmbeddingGenerator.from_use_case(
+            use_case=UseCases.NLP.SEQUENCE_CLASSIFICATION,
+            model_name="distilbert-base-uncased",
+            tokenizer_max_length=512,
+            batch_size=256,
+        )
+
+        # Create an Arize client and check if the SPACE_KEY and API_KEY
+        # are not set to the default values.
         self.arize_client = Client(space_key=SPACE_KEY, api_key=API_KEY)
         if SPACE_KEY == "SPACE_KEY" or API_KEY == "API_KEY":
             raise ValueError("❌ CHANGE SPACE AND API KEYS")
@@ -76,56 +77,76 @@ class ArizeCallbackHandler(BaseCallbackHandler):
     def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
         """Log data to Arize when an LLM ends."""
 
-        # Record token usage of the LLM 
-        self.prompt_tokens = response.llm_output['token_usage']['prompt_tokens']
-        self.total_tokens = response.llm_output['token_usage']['total_tokens']
-        self.completion_tokens = response.llm_output['token_usage']['completion_tokens']
+        from arize.utils.types import (
+            Embedding,
+            Environments,
+            ModelTypes,
+        )
+
+        # Record token usage of the LLM
+        if response.llm_output is not None:
+            self.prompt_tokens = response.llm_output["token_usage"]["prompt_tokens"]
+            self.total_tokens = response.llm_output["token_usage"]["total_tokens"]
+            self.completion_tokens = response.llm_output["token_usage"][
+                "completion_tokens"
+            ]
         i = 0
 
-        # Go through each prompt response pair and generate embeddings as well as timestamp and prediction ids
+        # Go through each prompt response pair and generate embeddings as
+        # well as timestamp and prediction ids
         for generations in response.generations:
             for generation in generations:
-              prompt = self.prompt_records[i]
-              prompt_embedding = pd.Series(self.generator.generate_embeddings(text_col=pd.Series(prompt.replace("\n", " "))).reset_index(drop=True))
-              response = generation.text.replace("\n", " ")
-              response_embedding = pd.Series(self.generator.generate_embeddings(text_col=pd.Series(generation.text.replace("\n", " "))).reset_index(drop=True))
-              pred_id = str(uuid.uuid4())
-              pred_timestamp = datetime.now().timestamp()
+                prompt = self.prompt_records[i]
+                prompt_embedding = pd.Series(
+                    self.generator.generate_embeddings(
+                        text_col=pd.Series(prompt.replace("\n", " "))
+                    ).reset_index(drop=True)
+                )
+                generated_text = generation.text.replace("\n", " ")
+                response_embedding = pd.Series(
+                    self.generator.generate_embeddings(
+                        text_col=pd.Series(generation.text.replace("\n", " "))
+                    ).reset_index(drop=True)
+                )
+                pred_id = str(uuid.uuid4())
 
+                # Define embedding features for Arize ingestion
+                embedding_features = {
+                    "prompt_embedding": Embedding(
+                        vector=pd.Series(prompt_embedding[0]), data=prompt
+                    ),
+                    "response_embedding": Embedding(
+                        vector=pd.Series(response_embedding[0]), data=generated_text
+                    ),
+                }
+                tags = {
+                    "Prompt Tokens": self.prompt_tokens,
+                    "Completion Tokens": self.completion_tokens,
+                    "Total Tokens": self.total_tokens,
+                }
 
-              # Define embedding features for Arize ingestion 
-              embedding_features = {
-              "prompt_embedding": Embedding(vector=pd.Series(prompt_embedding[0]),data=prompt),
-              "response_embedding": Embedding(vector=pd.Series(response_embedding[0]),data=response),
-              }
-              tags = {
-                  'Prompt Tokens': self.prompt_tokens,
-                  'Completion Tokens': self.completion_tokens,
-                  'Total Tokens': self.total_tokens
-              }
+                # Log each prompt response data into arize
+                future = self.arize_client.log(
+                    prediction_id=pred_id,
+                    tags=tags,
+                    prediction_label="1",
+                    model_id=self.model_id,
+                    model_type=ModelTypes.SCORE_CATEGORICAL,
+                    model_version=self.model_version,
+                    environment=Environments.PRODUCTION,
+                    embedding_features=embedding_features,
+                )
 
+                result = future.result()
+                if result.status_code == 200:
+                    print("✅ Successfully logged data to Arize!")
+                else:
+                    print(
+                        f"❌ Logging failed with status code {result.status_code}"
+                        f' and message "{result.text}"'
+                    )
 
-              # Log each prompt response data into arize
-              future = self.arize_client.log(
-              prediction_id=pred_id,
-              tags=tags,
-              prediction_label="1",
-              model_id=self.model_id,
-              model_type=ModelTypes.SCORE_CATEGORICAL,
-              model_version=self.model_version,
-              environment=Environments.PRODUCTION,
-              embedding_features=embedding_features
-              )
-
-              result = future.result()
-              if result.status_code == 200:
-                  print(f"✅ Successfully logged data to Arize!")
-              else:
-                  print(
-                      f'❌ Logging failed with status code {result.status_code} and message "{result.text}"'
-                  )
-
-              i = i + 1
+                i = i + 1
 
     def on_llm_error(
         self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
