bbaf99f4ac9e67c1c30b2ff753971cead83997f9
Harrison Chase <hw.chase.17@gmail.com>
2023-03-29 22:29:38 -0700

    Harrison/engine args (#2177)
    Co-authored-by: Alvaro Sevilla <alvarosevilla95@gmail.com>

--- langchain/sql_database.py
+++ langchain/sql_database.py
@@ -69,9 +69,12 @@ class SQLDatabase:
         self._metadata.reflect(bind=self._engine)
 
     @classmethod
-    def from_uri(cls, database_uri: str, **kwargs: Any) -> SQLDatabase:
+    def from_uri(
+        cls, database_uri: str, engine_args: Optional[dict] = None, **kwargs: Any
+    ) -> SQLDatabase:
         """Construct a SQLAlchemy engine from URI."""
-        return cls(create_engine(database_uri), **kwargs)
+        _engine_args = engine_args or {}
+        return cls(create_engine(database_uri, **_engine_args), **kwargs)
 
     @property
     def dialect(self) -> str:
