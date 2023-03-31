c605405a6cfd062989a32b16143fddb13cff1fcf
jerwelborn <jeremy.welborn@gmail.com>
2023-03-31 15:42:54 -0700

    tweak prompt: remove api specific few shots; reject out of domain queries

--- langchain/agents/agent_toolkits/openapi/planner_prompt.py
+++ langchain/agents/agent_toolkits/openapi/planner_prompt.py
@@ -2,40 +2,39 @@
 
 from langchain.prompts.prompt import PromptTemplate
 
-# TODO: remove API-specific few shots.
-API_PLANNER_PROMPT = """You are a planner that plans a sequence of API calls to assist with user queries against an API.  You have access to GET and POST endpoints, documented below.
+
+API_PLANNER_PROMPT = """You are a planner that plans a sequence of API calls to assist with user queries against an API.
+
+You should:
+1) evaluate whether the user query can be solved by the API documentated below. If no, say why.
+2) if yes, generate a plan of API calls and say what they are doing step by step.
+
+You should only use API endpoints documented below ("Endpoints you can use:").
 Some user queries can be resolved in a single API call, but some will require several API calls.
-You will generate a plan of API calls and explain what they are accomplishing step by step.
-The plan will be passed to an API controller that can format them into web requests and return the responses.
+The plan will be passed to an API controller that can format it into web requests and return the responses.
 
 ----
 
-Endpoints:
+Here are some examples:
+
+Fake endpoints for examples:
+GET /user to get information about the current user
+GET /products/search search across products
+POST /users/{{id}}/cart to add products to a user's cart
+
+User query: tell me a joke
+Plan: Sorry, this API's domain is shopping, not comedy.
+
+Usery query: I want to buy a couch
+Plan: 1. GET /products/search to search for couches
+2. GET /user to find the user's id
+3. POST /users/{{id}}/cart to add a couch to the user's cart
+
+----
+
+Here are endpoints you can use. Do not reference any of the endpoints above.
 
 {endpoints}
-
-----
-
-Suppose we're interested in Spotify's API:
-
-Good examples:
-
-User query: Find me songs by Norah Jones.
-Plan:
-1. GET /search to get the artist ID for Norah Jones.
-2. GET /artists/{{id}}/top-tracks to get the artist's top-tracks.
-
-User query: Give me some jazz recommendations.
-Plan:
-1. GET /recommendations/available-genre-seeds to find valid recommendation seeds.
-2. GET /recommendations to find recommendations.
-
-Bad examples:
-
-User query: Add the song Central Park West to my queue
-Plan:
-1. POST /me/player/queue to add the track to the user's queue
-Explanation: This plan did not look up the track id for the song. It also did not look up information about the user.
 
 ----
 
