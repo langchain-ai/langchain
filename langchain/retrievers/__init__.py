6891a2a84d28780ad6edaa227968cbd335f40690
Harrison Chase <hw.chase.17@gmail.com>
2023-04-01 08:59:04 -0700

    remote retriever (#2232)

--- langchain/retrievers/__init__.py
+++ langchain/retrievers/__init__.py
@@ -1,3 +1,4 @@
 from langchain.retrievers.chatgpt_plugin_retriever import ChatGPTPluginRetriever
+from langchain.retrievers.remote_retriever import RemoteLangChainRetriever
 
-__all__ = ["ChatGPTPluginRetriever"]
+__all__ = ["ChatGPTPluginRetriever", "RemoteLangChainRetriever"]
