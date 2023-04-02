8622d12e96dbf1f809582c0c9cd938b708537513
akmhmgc <akimuhamaguchi@icloud.com>
2023-04-02 18:36:34 +0900

    refactored serpapi wrapper

--- langchain/utilities/serpapi.py
+++ langchain/utilities/serpapi.py
@@ -125,28 +125,14 @@ class SerpAPIWrapper(BaseModel):
         """Process response from SerpAPI."""
         if "error" in res.keys():
             raise ValueError(f"Got error from SerpAPI: {res['error']}")
-        if "answer_box" in res.keys() and "answer" in res["answer_box"].keys():
-            toret = res["answer_box"]["answer"]
-        elif "answer_box" in res.keys() and "snippet" in res["answer_box"].keys():
-            toret = res["answer_box"]["snippet"]
-        elif (
-            "answer_box" in res.keys()
-            and "snippet_highlighted_words" in res["answer_box"].keys()
-        ):
-            toret = res["answer_box"]["snippet_highlighted_words"][0]
-        elif (
-            "sports_results" in res.keys()
-            and "game_spotlight" in res["sports_results"].keys()
-        ):
-            toret = res["sports_results"]["game_spotlight"]
-        elif (
-            "knowledge_graph" in res.keys()
-            and "description" in res["knowledge_graph"].keys()
-        ):
-            toret = res["knowledge_graph"]["description"]
-        elif "snippet" in res["organic_results"][0].keys():
-            toret = res["organic_results"][0]["snippet"]
 
-        else:
-            toret = "No good search result found"
+        toret = (
+            res.get("answer_box").get("answer")
+            or res.get("answer_box").get("snippet")
+            or res.get("answer_box").get("snippet_highlighted_words")[0]
+            or res.get("sports_results").get("game_spotlight")
+            or res.get("knowledge_graph").get("description")
+            or res.get("organic_results")[0].get("snippet")
+            or "No good search result found"
+        )
         return toret
