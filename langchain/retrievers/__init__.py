41832042cc9c646375acbc51e312520716ae02c0
Harrison Chase <hw.chase.17@gmail.com>
2023-04-04 14:09:57 -0700

    Harrison/pinecone hybrid (#2405)

--- langchain/retrievers/__init__.py
+++ langchain/retrievers/__init__.py
@@ -1,5 +1,11 @@
 from langchain.retrievers.chatgpt_plugin_retriever import ChatGPTPluginRetriever
 from langchain.retrievers.metal import MetalRetriever
+from langchain.retrievers.pinecone_hybrid_search import PineconeHybridSearchRetriever
 from langchain.retrievers.remote_retriever import RemoteLangChainRetriever
 
-__all__ = ["ChatGPTPluginRetriever", "RemoteLangChainRetriever", "MetalRetriever"]
+__all__ = [
+    "ChatGPTPluginRetriever",
+    "RemoteLangChainRetriever",
+    "PineconeHybridSearchRetriever",
+    "MetalRetriever",
+]
