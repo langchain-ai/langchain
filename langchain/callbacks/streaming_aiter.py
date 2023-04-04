c27b6173aa8dd7987d19e05e88ffce806b20d068
Nuno Campos <nuno@boringbits.io>
2023-04-04 13:51:40 +0100

    Update comments

--- langchain/callbacks/streaming_aiter.py
+++ langchain/callbacks/streaming_aiter.py
@@ -4,6 +4,8 @@ from typing import Any, AsyncIterator, Dict, List, Literal, Union, cast
 from langchain.callbacks.base import AsyncCallbackHandler
 from langchain.schema import LLMResult
 
+# TODO If used by two LLM runs in parallel this won't work as expected
+
 
 class AsyncIteratorCallbackHandler(AsyncCallbackHandler):
     """Callback handler that returns an async iterator."""
@@ -25,8 +27,6 @@ class AsyncIteratorCallbackHandler(AsyncCallbackHandler):
     ) -> None:
         # If two calls are made in a row, this resets the state
         self.done.clear()
-        # TODO If two calls are made in parallel, this will misbehave
-        # To be fixed with the concurrency work
 
     async def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
         self.queue.put_nowait(token)
@@ -44,7 +44,7 @@ class AsyncIteratorCallbackHandler(AsyncCallbackHandler):
     async def aiter(self) -> AsyncIterator[str]:
         while not self.queue.empty() or not self.done.is_set():
             # Wait for the next token in the queue,
-            # but stop waiting once the done event is set
+            # but stop waiting if the done event is set
             done, _ = await asyncio.wait(
                 [
                     asyncio.ensure_future(self.queue.get()),
