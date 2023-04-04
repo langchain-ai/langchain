04cf9f0e2d2c0ed93d45808f57abc201eec53fd5
Nuno Campos <nuno@boringbits.io>
2023-04-04 13:46:53 +0100

    Implement AsyncIteratorCallbackHandler

---
+++ langchain/callbacks/streaming_aiter.py
@@ -0,0 +1,64 @@
+import asyncio
+from typing import Any, AsyncIterator, Dict, List, Literal, Union, cast
+
+from langchain.callbacks.base import AsyncCallbackHandler
+from langchain.schema import LLMResult
+
+
+class AsyncIteratorCallbackHandler(AsyncCallbackHandler):
+    """Callback handler that returns an async iterator."""
+
+    queue: asyncio.Queue[str]
+
+    done: asyncio.Event
+
+    @property
+    def always_verbose(self) -> bool:
+        return True
+
+    def __init__(self) -> None:
+        self.queue = asyncio.Queue()
+        self.done = asyncio.Event()
+
+    async def on_llm_start(
+        self, serialized: Dict[str, Any], prompts: List[str], **kwargs: Any
+    ) -> None:
+        # If two calls are made in a row, this resets the state
+        self.done.clear()
+        # TODO If two calls are made in parallel, this will misbehave
+        # To be fixed with the concurrency work
+
+    async def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
+        self.queue.put_nowait(token)
+
+    async def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
+        self.done.set()
+
+    async def on_llm_error(
+        self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
+    ) -> None:
+        self.done.set()
+
+    # TODO implement the other methods
+
+    async def aiter(self) -> AsyncIterator[str]:
+        while not self.queue.empty() or not self.done.is_set():
+            # Wait for the next token in the queue,
+            # but stop waiting once the done event is set
+            done, _ = await asyncio.wait(
+                [
+                    asyncio.ensure_future(self.queue.get()),
+                    asyncio.ensure_future(self.done.wait()),
+                ],
+                return_when=asyncio.FIRST_COMPLETED,
+            )
+
+            # Extract the value of the first completed task
+            token_or_done = cast(str | Literal[True], done.pop().result())
+
+            # If the extracted value is the boolean True, the done event was set
+            if token_or_done is True:
+                break
+
+            # Otherwise, the extracted value is a token, which we yield
+            yield token_or_done
