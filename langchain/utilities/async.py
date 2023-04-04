83a9c3a41b26406d81f8c6744658081be9b36c8e
Zachary Jones <zjones@zetaglobal.com>
2023-04-04 12:54:16 -0400

    Add utility for async timeout

---
+++ langchain/utilities/async.py
@@ -0,0 +1,12 @@
+"""Shims for async features that may be missing from older python versions"""
+
+import sys
+
+
+if sys.version_info[:2] < (3, 11):
+    from async_timeout import timeout as asyncio_timeout
+else:
+    from asyncio import timeout as asyncio_timeout
+
+
+__all__ = ["asyncio_timeout"]
