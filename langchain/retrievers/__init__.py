5b61eddd5bd12eeb067f867c9b50ec0b977d233f
Harrison Chase <hw.chase.17@gmail.com>
2023-04-05 07:36:49 -0700

    Harrison/tfidf retriever (#2440)

--- langchain/retrievers/__init__.py
+++ langchain/retrievers/__init__.py
@@ -3,6 +3,7 @@ from langchain.retrievers.elastic_search_bm25 import ElasticSearchBM25Retriever
 from langchain.retrievers.metal import MetalRetriever
 from langchain.retrievers.pinecone_hybrid_search import PineconeHybridSearchRetriever
 from langchain.retrievers.remote_retriever import RemoteLangChainRetriever
+from langchain.retrievers.tfidf import TFIDFRetriever
 
 __all__ = [
     "ChatGPTPluginRetriever",
@@ -10,4 +11,5 @@ __all__ = [
     "PineconeHybridSearchRetriever",
     "MetalRetriever",
     "ElasticSearchBM25Retriever",
+    "TFIDFRetriever",
 ]
