fb2c1e66ecd970c67c2b6d7c16a043160abc4acd
Harrison Chase <hw.chase.17@gmail.com>
2023-04-05 07:22:10 -0700

    add tfidf retriever

--- langchain/retrievers/__init__.py
+++ langchain/retrievers/__init__.py
@@ -3,6 +3,7 @@ from langchain.retrievers.elastic_search_bm25 import ElasticSearchBM25Retriever
 from langchain.retrievers.metal import MetalRetriever
 from langchain.retrievers.pinecone_hybrid_search import PineconeHybridSearchRetriever
 from langchain.retrievers.remote_retriever import RemoteLangChainRetriever
+from langchain.retrievers.tfidf import TFIDFRetriever
 
 __all__ = [
     "ChatGPTPluginRetriever",
