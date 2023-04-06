52874a76bde11774c67baec5d3fc70cf099d17b6
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2023-04-06 11:34:20 -0700

    Add Request body

--- langchain/tools/openapi/utils/api_models.py
+++ langchain/tools/openapi/utils/api_models.py
@@ -1,13 +1,14 @@
 """Pydantic models for parsing an OpenAPI spec."""
-
+import logging
 from enum import Enum
 from typing import Any, Dict, List, Optional, Sequence, Tuple, Type, Union
 
-from openapi_schema_pydantic import MediaType, Parameter, Reference, Schema
+from openapi_schema_pydantic import MediaType, Parameter, Reference, RequestBody, Schema
 from pydantic import BaseModel, Field
 
 from langchain.tools.openapi.utils.openapi_utils import HTTPVerb, OpenAPISpec
 
+logger = logging.getLogger(__name__)
 PRIMITIVE_TYPES = {
     "integer": int,
     "number": float,
@@ -40,10 +41,17 @@ class APIPropertyLocation(Enum):
             )
 
 
+_SUPPORTED_MEDIA_TYPES = ("application/json",)
+
 SUPPORTED_LOCATIONS = {
     APIPropertyLocation.QUERY,
     APIPropertyLocation.PATH,
 }
+INVALID_LOCATION_TEMPL = (
+    'Unsupported APIPropertyLocation "{location}"'
+    " for parameter {name}. "
+    + f"Valid values are {[loc.value for loc in SUPPORTED_LOCATIONS]}"
+)
 
 SCHEMA_TYPE = Union[str, Type, tuple, None, Enum]
 
@@ -138,11 +146,10 @@ class APIProperty(APIPropertyBase):
         return schema_type
 
     @staticmethod
-    def _validate_location(location: APIPropertyLocation) -> None:
+    def _validate_location(location: APIPropertyLocation, name: str) -> None:
         if location not in SUPPORTED_LOCATIONS:
             raise NotImplementedError(
-                f'Unsupported APIPropertyLocation "{location}". '
-                f"Valid values are {SUPPORTED_LOCATIONS}"
+                INVALID_LOCATION_TEMPL.format(location=location, name=name)
             )
 
     @staticmethod
@@ -165,11 +172,22 @@ class APIProperty(APIPropertyBase):
 
         return schema
 
+    @staticmethod
+    def is_supported_location(location: str) -> bool:
+        """Return whether the provided location is supported."""
+        try:
+            return APIPropertyLocation.from_str(location) in SUPPORTED_LOCATIONS
+        except ValueError:
+            return False
+
     @classmethod
     def from_parameter(cls, parameter: Parameter, spec: OpenAPISpec) -> "APIProperty":
         """Instantiate from an OpenAPI Parameter."""
         location = APIPropertyLocation.from_str(parameter.param_in)
-        cls._validate_location(location)
+        cls._validate_location(
+            location,
+            parameter.name,
+        )
         cls._validate_content(parameter.content)
         schema = cls._get_schema(parameter, spec)
         schema_type = cls._get_schema_type(parameter, schema)
@@ -187,18 +205,192 @@ class APIProperty(APIPropertyBase):
 class APIRequestBodyProperty(APIPropertyBase):
     """A model for a request body property."""
 
-    properties: List[APIProperty] = Field(alias="properties")
+    properties: List["APIRequestBodyProperty"] = Field(alias="properties")
     """The sub-properties of the property."""
+
+    # This is useful for handling nested property cycles.
+    # We can define separate types in that case.
+    references_used: List[str] = Field(alias="references_used")
+    """The references used by the property."""
+
+    @classmethod
+    def _process_object_schema(
+        cls, schema: Schema, spec: OpenAPISpec, references_used: List[str]
+    ) -> Tuple[Union[str, List[str], None], List["APIRequestBodyProperty"]]:
+        properties = []
+        required_props = schema.required or []
+        if schema.properties is None:
+            raise ValueError(
+                f"No properties found when processing object schema: {schema}"
+            )
+        for prop_name, prop_schema in schema.properties.items():
+            if isinstance(prop_schema, Reference):
+                ref_name = prop_schema.ref.split("/")[-1]
+                if ref_name not in references_used:
+                    references_used.append(ref_name)
+                    prop_schema = spec.get_referenced_schema(prop_schema)
+                else:
+                    continue
+
+            properties.append(
+                cls.from_schema(
+                    schema=prop_schema,
+                    name=prop_name,
+                    required=prop_name in required_props,
+                    spec=spec,
+                    references_used=references_used,
+                )
+            )
+        return schema.type, properties
+
+    @classmethod
+    def _process_array_schema(
+        cls, schema: Schema, name: str, spec: OpenAPISpec, references_used: List[str]
+    ) -> str:
+        items = schema.items
+        if items is not None:
+            if isinstance(items, Reference):
+                ref_name = items.ref.split("/")[-1]
+                if ref_name not in references_used:
+                    references_used.append(ref_name)
+                    items = spec.get_referenced_schema(items)
+                else:
+                    pass
+                return f"Array<{ref_name}>"
+            else:
+                pass
+
+            if isinstance(items, Schema):
+                array_type = cls.from_schema(
+                    schema=items,
+                    name=f"{name}Item",
+                    required=True,  # TODO: Add required
+                    spec=spec,
+                    references_used=references_used,
+                )
+                return f"Array<{array_type.type}>"
+
+        return "array"
+
+    @classmethod
+    def from_schema(
+        cls,
+        schema: Schema,
+        name: str,
+        required: bool,
+        spec: OpenAPISpec,
+        references_used: Optional[List[str]] = None,
+    ) -> "APIRequestBodyProperty":
+        """Recursively populate from an OpenAPI Schema."""
+        if references_used is None:
+            references_used = []
+
+        schema_type = schema.type
+        properties: List[APIRequestBodyProperty] = []
+        if schema_type == "object" and schema.properties:
+            schema_type, properties = cls._process_object_schema(
+                schema, spec, references_used
+            )
+        elif schema_type == "array":
+            schema_type = cls._process_array_schema(schema, name, spec, references_used)
+        elif schema_type in PRIMITIVE_TYPES:
+            # Use the primitive type directly
+            pass
+        elif schema_type is None:
+            # No typing specified/parsed. WIll map to 'any'
+            pass
+        else:
+            raise ValueError(f"Unsupported type: {schema_type}")
+
+        return cls(
+            name=name,
+            required=required,
+            type=schema_type,
+            default=schema.default,
+            description=schema.description,
+            properties=properties,
+            references_used=references_used,
+        )
 
 
 class APIRequestBody(BaseModel):
     """A model for a request body."""
+
+    description: Optional[str] = Field(alias="description")
+    """The description of the request body."""
 
     properties: List[APIRequestBodyProperty] = Field(alias="properties")
 
     # E.g., application/json - we only support JSON at the moment.
     media_type: str = Field(alias="media_type")
     """The media type of the request body."""
+
+    @classmethod
+    def _process_supported_media_type(
+        cls,
+        media_type_obj: MediaType,
+        spec: OpenAPISpec,
+    ) -> List[APIRequestBodyProperty]:
+        """Process the media type of the request body."""
+        references_used = []
+        schema = media_type_obj.media_type_schema
+        if isinstance(schema, Reference):
+            references_used.append(schema.ref.split("/")[-1])
+            schema = spec.get_referenced_schema(schema)
+        if schema is None:
+            raise ValueError(
+                f"Could not resolve schema for media type: {media_type_obj}"
+            )
+        api_request_body_properties = []
+        required_properties = schema.required or []
+        if schema.type == "object" and schema.properties:
+            for prop_name, prop_schema in schema.properties.items():
+                if isinstance(prop_schema, Reference):
+                    prop_schema = spec.get_referenced_schema(prop_schema)
+
+                api_request_body_properties.append(
+                    APIRequestBodyProperty.from_schema(
+                        schema=prop_schema,
+                        name=prop_name,
+                        required=prop_name in required_properties,
+                        spec=spec,
+                    )
+                )
+        else:
+            api_request_body_properties.append(
+                APIRequestBodyProperty(
+                    name="body",
+                    required=True,
+                    type=schema.type,
+                    default=schema.default,
+                    description=schema.description,
+                    properties=[],
+                    references_used=references_used,
+                )
+            )
+
+        return api_request_body_properties
+
+    @classmethod
+    def from_request_body(
+        cls, request_body: RequestBody, spec: OpenAPISpec
+    ) -> "APIRequestBody":
+        """Instantiate from an OpenAPI RequestBody."""
+        properties = []
+        for media_type, media_type_obj in request_body.content.items():
+            if media_type not in _SUPPORTED_MEDIA_TYPES:
+                continue
+            api_request_body_properties = cls._process_supported_media_type(
+                media_type_obj,
+                spec,
+            )
+            properties.extend(api_request_body_properties)
+
+        return cls(
+            description=request_body.description,
+            properties=properties,
+            media_type=media_type,
+        )
 
 
 class APIOperation(BaseModel):
@@ -226,8 +418,33 @@ class APIOperation(BaseModel):
     # """The properties of the operation."""
     # components: Dict[str, BaseModel] = Field(alias="components")
 
-    # request_body: Optional[APIRequestBody] = Field(alias="request_body")
-    # """The request body of the operation."""
+    request_body: Optional[APIRequestBody] = Field(alias="request_body")
+    """The request body of the operation."""
+
+    @staticmethod
+    def _get_properties_from_parameters(
+        parameters: List[Parameter], spec: OpenAPISpec
+    ) -> List[APIProperty]:
+        """Get the properties of the operation."""
+        properties = []
+        for param in parameters:
+            if APIProperty.is_supported_location(param.param_in):
+                properties.append(APIProperty.from_parameter(param, spec))
+            elif param.required:
+                raise ValueError(
+                    INVALID_LOCATION_TEMPL.format(
+                        location=param.param_in, name=param.name
+                    )
+                )
+            else:
+                logger.warning(
+                    INVALID_LOCATION_TEMPL.format(
+                        location=param.param_in, name=param.name
+                    )
+                    + " Ignoring optional parameter"
+                )
+                pass
+        return properties
 
     @classmethod
     def from_openapi_url(
@@ -250,8 +467,14 @@ class APIOperation(BaseModel):
         """Create an APIOperation from an OpenAPI spec."""
         operation = spec.get_operation(path, method)
         parameters = spec.get_parameters_for_operation(operation)
-        properties = [APIProperty.from_parameter(param, spec) for param in parameters]
+        properties = cls._get_properties_from_parameters(parameters, spec)
         operation_id = OpenAPISpec.get_cleaned_operation_id(operation, path, method)
+        request_body = spec.get_request_body_for_operation(operation)
+        api_request_body = (
+            APIRequestBody.from_request_body(request_body, spec)
+            if request_body is not None
+            else None
+        )
         return cls(
             operation_id=operation_id,
             description=operation.description,
@@ -259,6 +482,7 @@ class APIOperation(BaseModel):
             path=path,
             method=method,
             properties=properties,
+            request_body=api_request_body,
         )
 
     @staticmethod
@@ -281,10 +505,40 @@ class APIOperation(BaseModel):
         else:
             return str(type_)
 
+    def _format_nested_properties(
+        self, properties: List[APIRequestBodyProperty], indent: int = 2
+    ) -> str:
+        """Format nested properties."""
+        formatted_props = []
+
+        for prop in properties:
+            prop_name = prop.name
+            prop_type = self.ts_type_from_python(prop.type)
+            prop_required = "" if prop.required else "?"
+            prop_desc = f"/* {prop.description} */" if prop.description else ""
+
+            if prop.properties:
+                nested_props = self._format_nested_properties(
+                    prop.properties, indent + 2
+                )
+                prop_type = f"{{\n{nested_props}\n{' ' * indent}}}"
+
+            formatted_props.append(
+                f"{prop_desc}\n{' ' * indent}{prop_name}{prop_required}: {prop_type},"
+            )
+
+        return "\n".join(formatted_props)
+
     def to_typescript(self) -> str:
         """Get typescript string representation of the operation."""
         operation_name = self.operation_id
         params = []
+
+        if self.request_body:
+            formatted_request_body_props = self._format_nested_properties(
+                self.request_body.properties
+            )
+            params.append(formatted_request_body_props)
 
         for prop in self.properties:
             prop_name = prop.name
