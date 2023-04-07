a629b6af0c6d4dd0969077ace8bee3adf0305f32
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2023-04-06 22:18:19 -0700

    Add eval generation

--- langchain/chains/api/openapi/requests_chain.py
+++ langchain/chains/api/openapi/requests_chain.py
@@ -2,9 +2,6 @@
 
 import json
 import re
-from typing import Dict
-
-from pydantic import root_validator
 
 from langchain.chains.api.openapi.prompts import REQUEST_TEMPLATE
 from langchain.chains.llm import LLMChain
@@ -16,30 +13,18 @@ from langchain.schema import BaseOutputParser
 class APIRequesterOutputParser(BaseOutputParser):
     """Parse the request and error tags."""
 
-    @root_validator()
-    def validate_environment(cls, values: Dict) -> Dict:
-        """Validate that json5 package exists."""
+    def _load_json_block(self, serialized_block: str) -> str:
         try:
-            import json5  # noqa: F401
-
-        except ImportError:
-            raise ValueError(
-                "Could not import json5 python package. "
-                "Please it install it with `pip install json5`."
-            )
-        return values
+            return json.dumps(json.loads(serialized_block, strict=False))
+        except json.JSONDecodeError:
+            return "ERROR serializing request."
 
     def parse(self, llm_output: str) -> str:
         """Parse the request and error tags."""
-        import json5
 
         json_match = re.search(r"```json(.*?)```", llm_output, re.DOTALL)
         if json_match:
-            typescript_block = json_match.group(1).strip()
-            try:
-                return json.dumps(json5.loads(typescript_block))
-            except json.JSONDecodeError:
-                return "ERROR serializing request"
+            return self._load_json_block(json_match.group(1).strip())
         message_match = re.search(r"```text(.*?)```", llm_output, re.DOTALL)
         if message_match:
             return f"MESSAGE: {message_match.group(1).strip()}"
