235e59fe8dc3779b5e055600ffb116438b613cc4
Harrison Chase <hw.chase.17@gmail.com>
2023-04-08 13:15:39 -0700

    cr

--- langchain/memory/motorhead_memory.py
+++ langchain/memory/motorhead_memory.py
@@ -1,64 +1,58 @@
-import os
-import json
-from typing import List, Dict, Any, Optional
+from typing import Any, Dict, List, Optional
+
 import requests
-from chat_memory import BaseChatMemory
 
-MOTORHEAD_URL = os.environ.get("MOTORHEAD_URL", "http://localhost:8080")
+from langchain.memory.chat_memory import BaseChatMemory
+from langchain.schema import get_buffer_string
 
-class MotorheadMemoryMessage:
-    def __init__(self, role: str, content: str):
-        self.role = role
-        self.content = content
-
-class MotorheadMemoryInput:
-    def __init__(self, chat_history, return_messages: bool, session_id: str, input_key: Optional[str] = None, output_key: Optional[str] = None):
-        self.chat_history = chat_history
-        self.return_messages = return_messages
-        self.session_id = session_id
-        self.input_key = input_key
-        self.output_key = output_key
 
 class MotorheadMemory(BaseChatMemory):
-    def __init__(self, fields: Optional[MotorheadMemoryInput] = None):
-        if fields is None:
-            fields = MotorheadMemoryInput(None, False, '')
+    url: str = "http://localhost:8080"
+    timeout = 3000
+    memory_key = "history"
+    session_id: str
+    context: Optional[str] = None
 
-        super().__init__(return_messages=fields.return_messages, input_key=fields.input_key, output_key=fields.output_key, chat_history=fields.chat_history)
-
-        self.motorhead_url = MOTORHEAD_URL
-        self.timeout = 3000
-        self.memory_key = "history"
-        self.session_id = fields.session_id
-        self.context = None
-
-    async def init(self):
-        res = requests.get(f"{MOTORHEAD_URL}/sessions/{self.session_id}/memory", timeout=self.timeout, headers={"Content-Type": "application/json"})
+    async def init(self) -> None:
+        res = requests.get(
+            f"{self.url}/sessions/{self.session_id}/memory",
+            timeout=self.timeout,
+            headers={"Content-Type": "application/json"},
+        )
         res_data = res.json()
         messages = res_data.get("messages", [])
         context = res_data.get("context", "NONE")
 
         for message in messages:
             if message["role"] == "AI":
-                self.chat_history.add_ai_chat_message(message["content"])
+                self.chat_memory.add_ai_message(message["content"])
             else:
-                self.chat_history.add_user_message(message["content"])
+                self.chat_memory.add_user_message(message["content"])
 
         if context and context != "NONE":
             self.context = context
 
-    def load_memory_variables(self, values):
+    def load_memory_variables(self, values: Dict[str, Any]) -> Dict[str, Any]:
         if self.return_messages:
-            return {self.memory_key: self.chat_history.messages}
+            return {self.memory_key: self.chat_memory.messages}
         else:
-            return {self.memory_key: get_buffer_string(self.chat_history.messages)}
+            return {self.memory_key: get_buffer_string(self.chat_memory.messages)}
 
-    def save_context(self, input_values, output_values):
-        requests.post(f"{MOTORHEAD_URL}/sessions/{self.session_id}/memory", timeout=self.timeout, json={
-            "messages": [
-                {"role": "Human", "content": f"{input_values['input']}"},
-                {"role": "AI", "content": f"{output_values['response']}"}
-            ]
-        }, headers={"Content-Type": "application/json"})
+    @property
+    def memory_variables(self) -> List[str]:
+        return [self.memory_key]
 
-        super().save_context(input_values, output_values)
+    def save_context(self, inputs: Dict[str, Any], outputs: Dict[str, str]) -> None:
+        input_str, output_str = self._get_input_output(inputs, outputs)
+        requests.post(
+            f"{self.url}/sessions/{self.session_id}/memory",
+            timeout=self.timeout,
+            json={
+                "messages": [
+                    {"role": "Human", "content": f"{input_str}"},
+                    {"role": "AI", "content": f"{output_str}"},
+                ]
+            },
+            headers={"Content-Type": "application/json"},
+        )
+        super().save_context(inputs, outputs)
