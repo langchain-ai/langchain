b7a725474661d20277eea7ad4da251e370ea1837
Rushabh Agarwal <26388764+rushout09@users.noreply.github.com>
2023-04-10 23:52:52 +0530

    Issue #1719 SqlDatabaseToolkit should have custom llm for QueryCheckerTool (#2655)

--- langchain/agents/agent_toolkits/sql/toolkit.py
+++ langchain/agents/agent_toolkits/sql/toolkit.py
@@ -4,6 +4,8 @@ from typing import List
 from pydantic import Field
 
 from langchain.agents.agent_toolkits.base import BaseToolkit
+from langchain.llms.base import BaseLLM
+from langchain.llms.openai import OpenAI
 from langchain.sql_database import SQLDatabase
 from langchain.tools import BaseTool
 from langchain.tools.sql_database.tool import (
@@ -18,6 +20,7 @@ class SQLDatabaseToolkit(BaseToolkit):
     """Toolkit for interacting with SQL databases."""
 
     db: SQLDatabase = Field(exclude=True)
+    llm: BaseLLM = OpenAI(temperature=0)
 
     @property
     def dialect(self) -> str:
@@ -35,5 +38,5 @@ class SQLDatabaseToolkit(BaseToolkit):
             QuerySQLDataBaseTool(db=self.db),
             InfoSQLDatabaseTool(db=self.db),
             ListSQLDatabaseTool(db=self.db),
-            QueryCheckerTool(db=self.db),
+            QueryCheckerTool(db=self.db, llm=self.llm),
         ]
