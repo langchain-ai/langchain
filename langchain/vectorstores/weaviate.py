3eb9d7232bc623a7cb97c9d1f8999f4eb25e1bcc
cs0lar <cristiano.solarino@gmail.com>
2023-04-10 11:27:52 +0100

    Fixes hwchase17/langchain#1211

--- langchain/vectorstores/weaviate.py
+++ langchain/vectorstores/weaviate.py
@@ -6,8 +6,19 @@ from uuid import uuid4
 
 from langchain.docstore.document import Document
 from langchain.embeddings.base import Embeddings
+from langchain.utils import get_from_dict_or_env
 from langchain.vectorstores.base import VectorStore
 
+def _default_schema(index_name: str) -> Dict:
+    return {
+        "class": index_name,
+        "properties": [
+            {
+                "name": "text",
+                "dataType": ["text"],
+            }
+        ],
+    }
 
 class Weaviate(VectorStore):
     """Wrapper around Weaviate vector database.
@@ -29,6 +40,7 @@ class Weaviate(VectorStore):
         client: Any,
         index_name: str,
         text_key: str,
+        embedding: Embeddings,
         attributes: Optional[List[str]] = None,
     ):
         """Initialize with Weaviate client."""
@@ -46,6 +58,7 @@ class Weaviate(VectorStore):
         self._client = client
         self._index_name = index_name
         self._text_key = text_key
+        self._embedding = embedding
         self._query_attrs = [self._text_key]
         if attributes is not None:
             self._query_attrs.extend(attributes)
@@ -77,17 +90,21 @@ class Weaviate(VectorStore):
     def similarity_search(
         self, query: str, k: int = 4, **kwargs: Any
     ) -> List[Document]:
-        """Look up similar documents in weaviate."""
-        content: Dict[str, Any] = {"concepts": [query]}
-        if kwargs.get("search_distance"):
-            content["certainty"] = kwargs.get("search_distance")
-        query_obj = self._client.query.get(self._index_name, self._query_attrs)
-        result = query_obj.with_near_text(content).with_limit(k).do()
-        docs = []
-        for res in result["data"]["Get"][self._index_name]:
-            text = res.pop(self._text_key)
-            docs.append(Document(page_content=text, metadata=res))
-        return docs
+        """Return docs most similar to query.
+
+        Args:
+            query: Text to look up documents similar to.
+            k: Number of Documents to return. Defaults to 4.
+
+        Returns:
+            List of Documents most similar to the query.
+        """
+        embedding = self._embedding.embed_query(query)
+        return self.similarity_search_by_vector(
+            embedding,
+            k,
+            **kwargs
+        )
 
     def similarity_search_by_vector(
         self, embedding: List[float], k: int = 4, **kwargs: Any
@@ -109,6 +126,72 @@ class Weaviate(VectorStore):
         embedding: Embeddings,
         metadatas: Optional[List[dict]] = None,
         **kwargs: Any,
-    ) -> VectorStore:
-        """Not implemented for Weaviate yet."""
-        raise NotImplementedError("weaviate does not currently support `from_texts`.")
+    ) -> Weaviate:
+        """Construct Weaviate wrapper from raw documents.
+
+        This is a user-friendly interface that:
+            1. Embeds documents.
+            2. Creates a new index for the embeddings in the Weaviate instance.
+            3. Adds the documents to the newly created Weaviate index.
+
+        This is intended to be a quick way to get started.
+
+        Example:
+            .. code-block:: python
+
+                from langchain.vectorstores.weaviate import Weaviate
+                from langchain.embeddings import OpenAIEmbeddings
+                embeddings = OpenAIEmbeddings()
+                weaviate = Weaviate.from_texts(
+                    texts,
+                    embeddings,
+                    weaviate_url="http://localhost:8080"
+                )
+        """
+        weaviate_url = get_from_dict_or_env(
+            kwargs, "weaviate_url", "WEAVIATE_URL"
+        )
+
+        try:
+            import weaviate
+            from weaviate import Client
+            from weaviate.util import get_valid_uuid
+        except ImportError:
+            raise ValueError(
+                "Could not import weaviate python  package. "
+                "Please install it with `pip instal weaviate-client`"
+            )
+        
+        client = Client(weaviate_url)
+        index_name = kwargs.get("index_name", f"LangChain_{uuid4().hex}")
+        embeddings = embedding.embed_documents(texts)
+        text_key = "text"
+        schema = _default_schema(index_name)
+        attributes = list(metadatas[0].keys()) if metadatas else None
+
+        # check whether the index already exists
+        if not client.schema.contains(schema):
+            client.schema.create_class(schema)
+
+        with client.batch as batch:
+            for i, text in enumerate(texts):
+                data_properties = {
+                    text_key: text,
+                }
+                if metadatas is not None:
+                    for key in metadatas[i].keys():
+                        data_properties[key] = metadatas[i][key]
+
+                _id = get_valid_uuid(uuid4())
+
+                batch.add_data_object(
+                    uuid=_id,
+                    data_object=data_properties, 
+                    class_name=index_name,
+                    vector=embeddings[i]
+                )
+
+            batch.flush()
+
+        return cls(client, index_name, text_key, embedding, attributes)
+
---
+++ tests/integration_tests/vectorstores/test_weaviate.py
@@ -0,0 +1,68 @@
+"""Test Weaviate functionality."""
+import logging
+from typing import Generator, List, Union
+
+import pytest
+import weaviate
+from weaviate import Client
+
+from langchain.docstore.document import Document
+from langchain.document_loaders import TextLoader
+from langchain.vectorstores.weaviate import Weaviate
+from langchain.text_splitter import CharacterTextSplitter
+from tests.integration_tests.vectorstores.fake_embeddings import FakeEmbeddings
+
+logging.basicConfig(level=logging.DEBUG)
+
+"""
+cd tests/integration_tests/vectorstores/docker-compose
+docker compose -f weaviate.yml up
+"""
+
+class TestWeaviate:
+    @pytest.fixture(scope="class", autouse=True)
+    def weaviate_url(self) -> Union[str, Generator[str, None, None]]:
+        """Return the weaviate url."""
+        url = "http://localhost:8080"
+        yield url
+
+        # Clear the test index
+        client = Client(url)
+        client.schema.delete_all()
+
+    @pytest.fixture(scope="class")
+    def documents(self) -> Generator[List[Document], None, None]:
+        """Return a generator that yields a list of documents."""
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+
+        documents = TextLoader(
+            os.path.join(os.path.dirname(__file__), "fixtures", "sharks.txt")
+        ).load()
+        yield text_splitter.split_documents(documents)
+
+
+    def test_similarity_search_without_metadata(self, weaviate_url: str) -> None:
+        """Test end to end construction and search without metadata."""
+        texts = ["foo", "bar", "baz"]
+        docsearch = Weaviate.from_texts(
+            texts, 
+            FakeEmbeddings(), 
+            weaviate_url=weaviate_url,
+        )
+
+        output = docsearch.similarity_search("foo", k=1)
+        assert output == [Document(page_content="foo")]
+
+
+    def test_similarity_search_with_metadata(self, weaviate_url: str) -> None:
+        """Test end to end construction and search with metadata."""
+        texts = ["foo", "bar", "baz"]
+        metadatas = [{"page": i} for i in range(len(texts))]
+        docsearch = Weaviate.from_texts(
+            texts,
+            FakeEmbeddings(),
+            metadatas=metadatas,
+            weaviate_url=weaviate_url
+        )
+        output = docsearch.similarity_search("foo", k=1)
+        assert output == [Document(page_content="foo", metadata={"page": 0})]
