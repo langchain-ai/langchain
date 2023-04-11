8ab439a3a0f403c9a3da1db7fe4de950c3d2895f
Ankit Jain <ankneo@users.noreply.github.com>
2023-04-11 09:36:10 +0530

    Add support for Postgres for saving chat history (#2578)
    Add support for Postgres DB to store the Chat history.

--- langchain/memory/__init__.py
+++ langchain/memory/__init__.py
@@ -6,6 +6,8 @@ from langchain.memory.buffer_window import ConversationBufferWindowMemory
 from langchain.memory.chat_message_histories.dynamodb import DynamoDBChatMessageHistory
 from langchain.memory.chat_message_histories.in_memory import ChatMessageHistory
 from langchain.memory.chat_message_histories.redis import RedisChatMessageHistory
+from langchain.memory.chat_message_histories.postgres import PostgresChatMessageHistory
+
 from langchain.memory.combined import CombinedMemory
 from langchain.memory.entity import (
     ConversationEntityMemory,
@@ -36,4 +38,5 @@ __all__ = [
     "ConversationTokenBufferMemory",
     "RedisChatMessageHistory",
     "DynamoDBChatMessageHistory",
+    "PostgresChatMessageHistory"
 ]
--- langchain/memory/chat_message_histories/__init__.py
+++ langchain/memory/chat_message_histories/__init__.py
@@ -1,7 +1,9 @@
 from langchain.memory.chat_message_histories.dynamodb import DynamoDBChatMessageHistory
 from langchain.memory.chat_message_histories.redis import RedisChatMessageHistory
+from langchain.memory.chat_message_histories.postgres import PostgresChatMessageHistory
 
 __all__ = [
     "DynamoDBChatMessageHistory",
     "RedisChatMessageHistory",
+    "PostgresChatMessageHistory",
 ]
