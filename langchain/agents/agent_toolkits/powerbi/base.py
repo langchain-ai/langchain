944ff35ee5475caaad27b752f5e9a855ce8bf117
Eduard van Valkenburg <github@vanvalkenburg.eu>
2023-04-11 15:58:01 +0200

    added chatmodel constructor

--- langchain/agents/agent_toolkits/powerbi/base.py
+++ langchain/agents/agent_toolkits/powerbi/base.py
@@ -9,7 +9,6 @@ from langchain.agents.agent_toolkits.powerbi.prompt import (
     POWERBI_SUFFIX,
 )
 from langchain.agents.agent_toolkits.powerbi.toolkit import PowerBIToolkit
-from langchain.agents.mrkl.base import ZeroShotAgent
 from langchain.agents.mrkl.prompt import FORMAT_INSTRUCTIONS
 from langchain.callbacks.base import BaseCallbackManager
 from langchain.chains.llm import LLMChain
