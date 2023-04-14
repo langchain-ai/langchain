e2a81add348d9c6bf2986199e48dbbc4988c3677
Rahul Behal <rahulbehal01@hotmail.com>
2023-04-13 22:41:24 -0700

    Added task orchestration chains

---
+++ langchain/chains/task_orchestration/task_execution.py
@@ -0,0 +1,21 @@
+from langchain import LLMChain, PromptTemplate
+from langchain.llms import BaseLLM
+
+
+class TaskExecutionChain(LLMChain):
+    """Chain to execute tasks."""
+
+    @classmethod
+    def from_llm(cls, llm: BaseLLM, verbose: bool = True) -> LLMChain:
+        """Get the response parser."""
+        execution_template = (
+            "You are an AI who performs one task based on the following objective: {objective}."
+            " Take into account these previously completed tasks: {context}."
+            " Your task: {task}."
+            " Response:"
+        )
+        prompt = PromptTemplate(
+            template=execution_template,
+            input_variables=["objective", "context", "task"],
+        )
+        return cls(prompt=prompt, llm=llm, verbose=verbose)
