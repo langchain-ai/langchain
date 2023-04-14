4e3f95400d9c1c5e678da209cf892b064776489c
Dev 2049 <dev.dev2049@gmail.com>
2023-04-14 14:19:58 -0700

    cr

--- langchain/retrievers/contextual_compression.py
+++ langchain/retrievers/contextual_compression.py
@@ -1,21 +1,28 @@
 """"""
-from typing import Any, Dict, List, Optional
+from typing import Any, Callable, Dict, List, Optional
 
 from pydantic import BaseModel, Extra, Field, root_validator
 
 from langchain import BasePromptTemplate, LLMChain, PromptTemplate
 from langchain.schema import BaseLanguageModel, BaseRetriever, Document
 
-prompt_template = """Write a concise summary of the following text. Make note of any key entities and words:
 
-"{context}"
+def _get_default_chain_prompt() -> PromptTemplate:
+    template = """Given the following question and context, extract any part of the context *as is* that is relevant to answer the question.
 
-CONCISE SUMMARY:"""
-PROMPT = PromptTemplate(template=prompt_template, input_variables=["context"])
+    > Question: {question}
+    > Context:
+    >>>
+    {context}
+    >>>
+    Extracted relevant parts:"""
+
+    return PromptTemplate(template=template, input_variables=["question", "context"])
 
 
-def _get_default_document_prompt() -> PromptTemplate:
-    return PromptTemplate(input_variables=["page_content"], template="{page_content}")
+def default_get_input(query: str, doc: Document) -> Dict[str, Any]:
+    """Return the compression chain input."""
+    return {"question": query, "context": doc.page_content}
 
 
 class ContextualCompressionRetriever(BaseRetriever, BaseModel):
@@ -23,14 +30,12 @@ class ContextualCompressionRetriever(BaseRetriever, BaseModel):
 
     llm_chain: LLMChain
     """LLM wrapper to use for compressing documents."""
+
     base_retriever: BaseRetriever
-    document_prompt: BasePromptTemplate = Field(
-        default_factory=_get_default_document_prompt
-    )
-    """Prompt to use to format each document."""
-    document_variable_name: str
-    """The variable name in the llm_chain to put the documents in.
-    If only one variable in the llm_chain, this need not be provided."""
+    """Base Retriever to use for getting relevant documents."""
+
+    get_input: Callable[[str, Document], dict]
+    """Callable for constructing the chain input from the query and a Document."""
 
     class Config:
         """Configuration for this pydantic object."""
@@ -38,39 +43,11 @@ class ContextualCompressionRetriever(BaseRetriever, BaseModel):
         extra = Extra.forbid
         arbitrary_types_allowed = True
 
-    @root_validator(pre=True)
-    def get_default_document_variable_name(cls, values: Dict) -> Dict:
-        """Get default document variable name, if not provided."""
-        if "document_variable_name" not in values:
-            llm_chain_variables = values["llm_chain"].prompt.input_variables
-            if len(llm_chain_variables) == 1:
-                values["document_variable_name"] = llm_chain_variables[0]
-            else:
-                raise ValueError(
-                    "document_variable_name must be provided if there are "
-                    "multiple llm_chain_variables"
-                )
-        else:
-            llm_chain_variables = values["llm_chain"].prompt.input_variables
-            if values["document_variable_name"] not in llm_chain_variables:
-                raise ValueError(
-                    f"document_variable_name {values['document_variable_name']} was "
-                    f"not found in llm_chain input_variables: {llm_chain_variables}"
-                )
-        return values
-
-    def _get_input(self, doc: Document) -> Dict[str, Any]:
-        """"""
-        all_info = {"page_content": doc.page_content, **doc.metadata}
-        doc_info = {k: all_info[k] for k in self.document_prompt.input_variables}
-        doc_str = self.document_prompt.format(**doc_info)
-        return {self.document_variable_name: doc_str}
-
-    def _compress_docs(self, docs: List[Document]) -> List[Document]:
+    def _compress_docs(self, query: str, docs: List[Document]) -> List[Document]:
         """Compress page content of raw documents."""
         compressed_docs = []
         for doc in docs:
-            input = self._get_input(doc)
+            input = self.get_input(query, doc)
             output = self.llm_chain.predict(**input)
             compressed_docs.append(Document(page_content=output, metadata=doc.metadata))
         return compressed_docs
@@ -85,7 +62,7 @@ class ContextualCompressionRetriever(BaseRetriever, BaseModel):
             List of relevant documents
         """
         docs = self.base_retriever.get_relevant_documents(query)
-        compressed_docs = self._compress_docs(docs)
+        compressed_docs = self._compress_docs(query, docs)
         return compressed_docs
 
     async def aget_relevant_documents(self, query: str) -> List[Document]:
@@ -98,18 +75,19 @@ class ContextualCompressionRetriever(BaseRetriever, BaseModel):
             List of relevant documents
         """
         raise NotImplementedError(
-            "Async `aget_relevant_documents` function not implemented yet."
+            "ContextualCompressionRetriever does not support async."
         )
 
     @classmethod
     def from_llm(
         cls,
         llm: BaseLanguageModel,
-        base_retriever: BaseRetriever,
         prompt: Optional[PromptTemplate] = None,
+        get_input: Optional[Callable[[str, Document], str]] = None,
         **kwargs: Any,
     ) -> "ContextualCompressionRetriever":
         """Initialize from LLM."""
-        _prompt = prompt if prompt is not None else PROMPT
+        _prompt = prompt if prompt is not None else _get_default_chain_prompt()
+        _get_input = get_input if get_input is not None else default_get_input
         llm_chain = LLMChain(llm=llm, prompt=_prompt)
-        return cls(llm_chain=llm_chain, base_retriever=base_retriever, **kwargs)
+        return cls(llm_chain=llm_chain, get_input=_get_input, **kwargs)
