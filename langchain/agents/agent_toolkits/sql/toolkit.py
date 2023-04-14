9f862ef9cb47e8e1677953e342dea7b03246f634
HH <cnhhoang850@gmail.com>
2023-04-14 14:52:53 +0700

    fixed deafult usage of openai model in SQLDatabaseToolkit

--- langchain/agents/agent_toolkits/sql/toolkit.py
+++ langchain/agents/agent_toolkits/sql/toolkit.py
@@ -20,7 +20,7 @@ class SQLDatabaseToolkit(BaseToolkit):
     """Toolkit for interacting with SQL databases."""
 
     db: SQLDatabase = Field(exclude=True)
-    llm: BaseLLM = Field(default_factory=lambda: OpenAI(temperature=0))
+    llm: BaseLLM = Field(exclude=True)
 
     @property
     def dialect(self) -> str:
