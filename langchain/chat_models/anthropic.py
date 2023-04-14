5a0e4387990acff92abe48abfd56ab59c17a8cb8
Mike Lambert <mike.lambert@anthropic.com>
2023-04-14 18:46:16 -0400

    Fix 'stop_sequences can't be none' error, need to pass it (with array) or not-pass-it-at-all

--- langchain/chat_models/anthropic.py
+++ langchain/chat_models/anthropic.py
@@ -98,7 +98,7 @@ class ChatAnthropic(BaseChatModel, _AnthropicCommon):
         self, messages: List[BaseMessage], stop: Optional[List[str]] = None
     ) -> ChatResult:
         prompt = self._convert_messages_to_prompt(messages)
-        params = {"prompt": prompt, "stop_sequences": stop, **self._default_params}
+        params = {"prompt": prompt, "stop_sequences": stop or [], **self._default_params}
 
         if self.streaming:
             completion = ""
@@ -120,7 +120,7 @@ class ChatAnthropic(BaseChatModel, _AnthropicCommon):
         self, messages: List[BaseMessage], stop: Optional[List[str]] = None
     ) -> ChatResult:
         prompt = self._convert_messages_to_prompt(messages)
-        params = {"prompt": prompt, "stop_sequences": stop, **self._default_params}
+        params = {"prompt": prompt, "stop_sequences": stop or [], **self._default_params}
 
         if self.streaming:
             completion = ""
