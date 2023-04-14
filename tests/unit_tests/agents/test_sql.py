29c4b17dfa8d1d6baf6598065d0eba0b7ab63204
Harrison Chase <hw.chase.17@gmail.com>
2023-04-13 22:01:04 -0700

    cr

--- tests/unit_tests/agents/test_sql.py
+++ tests/unit_tests/agents/test_sql.py
@@ -1,6 +1,5 @@
 from langchain.agents import create_sql_agent
 from langchain.agents.agent_toolkits import SQLDatabaseToolkit
-from langchain.llms.openai import OpenAI
 from langchain.sql_database import SQLDatabase
 from tests.unit_tests.llms.fake_llm import FakeLLM
 
