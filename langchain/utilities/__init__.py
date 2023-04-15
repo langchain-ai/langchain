6bf461920e7bfd27a4260bfebf444e20b62ed0a0
dev2049 <130488702+dev2049@users.noreply.github.com>
2023-04-15 10:50:25 -0700

    Move PythonRepl -> langchain.utilities (#2917)

--- langchain/utilities/__init__.py
+++ langchain/utilities/__init__.py
@@ -1,5 +1,4 @@
 """General utilities."""
-from langchain.python import PythonREPL
 from langchain.requests import TextRequestsWrapper
 from langchain.utilities.apify import ApifyWrapper
 from langchain.utilities.bash import BashProcess
@@ -7,6 +6,7 @@ from langchain.utilities.bing_search import BingSearchAPIWrapper
 from langchain.utilities.google_search import GoogleSearchAPIWrapper
 from langchain.utilities.google_serper import GoogleSerperAPIWrapper
 from langchain.utilities.openweathermap import OpenWeatherMapAPIWrapper
+from langchain.utilities.python import PythonREPL
 from langchain.utilities.searx_search import SearxSearchWrapper
 from langchain.utilities.serpapi import SerpAPIWrapper
 from langchain.utilities.wikipedia import WikipediaAPIWrapper
@@ -16,7 +16,6 @@ __all__ = [
     "ApifyWrapper",
     "BashProcess",
     "TextRequestsWrapper",
-    "PythonREPL",
     "GoogleSearchAPIWrapper",
     "GoogleSerperAPIWrapper",
     "WolframAlphaAPIWrapper",
@@ -25,4 +24,5 @@ __all__ = [
     "BingSearchAPIWrapper",
     "WikipediaAPIWrapper",
     "OpenWeatherMapAPIWrapper",
+    "PythonREPL",
 ]
