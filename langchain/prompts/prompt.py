8e5082260db044fdd1d5a80335b482d13ebaa9c8
engkheng <ongengkheng929@example.com>
2023-04-17 11:05:16 +0800

    Support inference of input_variables for Jinja2 template

--- langchain/prompts/prompt.py
+++ langchain/prompts/prompt.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from string import Formatter
 from typing import Any, Dict, List, Union
 
+from jinja2 import Environment, meta
 from pydantic import Extra, root_validator
 
 from langchain.prompts.base import (
@@ -12,6 +13,13 @@ from langchain.prompts.base import (
     StringPromptTemplate,
     check_valid_template,
 )
+
+
+def _get_jinja2_variables_from_template(template: str) -> List[str]:
+    env = Environment()
+    ast = env.parse(template)
+    variables = meta.find_undeclared_variables(ast)
+    return list(variables)
 
 
 class PromptTemplate(StringPromptTemplate):
@@ -125,9 +133,15 @@ class PromptTemplate(StringPromptTemplate):
     @classmethod
     def from_template(cls, template: str, **kwargs: Any) -> PromptTemplate:
         """Load a prompt template from a template."""
-        input_variables = {
-            v for _, v, _, _ in Formatter().parse(template) if v is not None
-        }
+        if "template_format" in kwargs and kwargs["template_format"] == "jinja2":
+            # Get the variables for the template
+            input_variables = _get_jinja2_variables_from_template(template)
+
+        else:
+            input_variables = {
+                v for _, v, _, _ in Formatter().parse(template) if v is not None
+            }
+
         return cls(
             input_variables=list(sorted(input_variables)), template=template, **kwargs
         )
