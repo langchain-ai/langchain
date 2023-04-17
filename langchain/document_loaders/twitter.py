1910376259fde702b0759c4d03d6feace48df89c
vowelparrot <130414180+vowelparrot@users.noreply.github.com>
2023-04-17 10:46:57 -0700

    Update types and args

--- langchain/document_loaders/twitter.py
+++ langchain/document_loaders/twitter.py
@@ -1,7 +1,24 @@
-from typing import List, Optional
+"""Twitter document loader."""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, Dict, List, Optional, Sequence
 
 from langchain.docstore.document import Document
 from langchain.document_loaders.base import BaseLoader
+
+if TYPE_CHECKING:
+    import tweepy
+    from tweepy import OAuthHandler
+
+
+def _dependable_tweepy_import() -> "tweepy":
+    try:
+        import tweepy
+    except ImportError:
+        raise ValueError(
+            "requests package not found, please install it with " "`pip install tweepy`"
+        )
+    return tweepy
 
 
 class TwitterTweetLoader(BaseLoader):
@@ -16,49 +33,54 @@ class TwitterTweetLoader(BaseLoader):
 
     def __init__(
         self,
-        access_token: str,
-        access_token_secret: str,
-        consumer_key: str,
-        consumer_secret: str,
-        twitter_users: List[str],
+        auth_handler: "OAuthHandler",
+        twitter_users: Sequence[str],
         number_tweets: Optional[int] = 100,
     ):
-        self.access_token = access_token
-        self.access_token_secret = access_token_secret
-        self.consumer_key = consumer_key
-        self.consumer_secret = consumer_secret
+        self.auth = auth_handler
 
         self.twitter_users = twitter_users
         self.number_tweets = number_tweets
 
     def load(self) -> List[Document]:
-        try:
-            import tweepy
-            import tweepy.parsers
-        except ImportError:
-            raise ValueError(
-                "requests package not found, please install it with "
-                "`pip install tweepy`"
-            )
-
-        auth = tweepy.OAuthHandler(
-            access_token=self.access_token,
-            access_token_secret=self.access_token_secret,
-            consumer_key=self.consumer_key,
-            consumer_secret=self.consumer_secret,
-        )
-        api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+        """Load tweets."""
+        tweepy = _dependable_tweepy_import()
+        api = tweepy.API(self.auth, parser=tweepy.parsers.JSONParser())
 
         results = []
         for username in self.twitter_users:
             tweets = api.user_timeline(screen_name=username, count=self.number_tweets)
-
-            response = ""
-            for tweet in tweets:
-                response += (
-                    f"Created At:{tweet['created_at']},Content:{tweet['text']}\n"
-                )
-
+            response = self._format_tweets(tweets)
             user = api.get_user(screen_name=username)
             results.append(Document(page_content=response, metadata=user))
         return results
+
+    def _format_tweets(self, tweets: List[Dict[str, Any]]) -> str:
+        """Format tweets into a string."""
+        response = ""
+        for tweet in tweets:
+            response += f"Created At:{tweet['created_at']},Content:{tweet['text']}\n"
+        return response
+
+    @classmethod
+    def from_secrets(
+        cls,
+        access_token: str,
+        access_token_secret: str,
+        consumer_key: str,
+        consumer_secret: str,
+        twitter_users: Sequence[str],
+        number_tweets: Optional[int] = 100,
+    ) -> "TwitterTweetLoader":
+        tweepy = _dependable_tweepy_import()
+        auth = tweepy.OAuthHandler(
+            access_token=access_token,
+            access_token_secret=access_token_secret,
+            consumer_key=consumer_key,
+            consumer_secret=consumer_secret,
+        )
+        return cls(
+            auth_handler=auth,
+            twitter_users=twitter_users,
+            number_tweets=number_tweets,
+        )
