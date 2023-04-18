2bdb06051e58ae624625d78ab407a558fdfa5757
Dev 2049 <dev.dev2049@gmail.com>
2023-04-18 10:17:16 -0700

    cosine unit test

--- langchain/math_utils.py
+++ langchain/math_utils.py
@@ -6,15 +6,17 @@ import numpy as np
 Matrix = Union[List[List[float]], List[np.ndarray], np.ndarray]
 
 
-def cosine_similarity(x: Matrix, y: Matrix) -> np.ndarray:
+def cosine_similarity(X: Matrix, Y: Matrix) -> np.ndarray:
     """Row-wise cosine similarity between two equal-width matrices."""
-    x = np.array(x)
-    y = np.array(y)
-    if x.shape[1] != y.shape[1]:
-        raise ValueError("Number of columns in x and y must be the same.")
+    if len(X) == 0 or len(Y) == 0:
+        return np.array([])
+    X = np.array(X)
+    Y = np.array(Y)
+    if X.shape[1] != Y.shape[1]:
+        raise ValueError("Number of columns in X and Y must be the same.")
 
-    x_norm = np.linalg.norm(x, axis=1)
-    y_norm = np.linalg.norm(y, axis=1)
-    similarity = np.dot(x, y.T) / np.outer(x_norm, y_norm)
+    X_norm = np.linalg.norm(X, axis=1)
+    Y_norm = np.linalg.norm(Y, axis=1)
+    similarity = np.dot(X, Y.T) / np.outer(X_norm, Y_norm)
     similarity[np.isnan(similarity) | np.isinf(similarity)] = 0.0
     return similarity
