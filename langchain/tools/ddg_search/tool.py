a1e2f5703a4aa141749d10888a8ccfe002f77dd0
itai <itai.marks@gmail.com>
2023-04-20 06:59:27 +0300

    DuckDuckGo Search Tool - A Free to use Search Engine Alternative

---
+++ langchain/tools/ddg_search/tool.py
@@ -0,0 +1,24 @@
+"""Tool for the DuckDuckGo search API."""
+
+from langchain.tools.base import BaseTool
+from langchain.utilities.duckduckgo_search import DuckDuckGoSearchAPIWrapper
+
+
+class DuckDuckGoSearchRun(BaseTool):
+    """Tool that adds the capability to query the DuckDuckGo search API."""
+
+    name = "DuckDuckGo Search"
+    description = (
+        "A wrapper around DuckDuckGo Search. "
+        "Useful for when you need to answer questions about current events. "
+        "Input should be a search query."
+    )
+    api_wrapper: DuckDuckGoSearchAPIWrapper
+
+    def _run(self, query: str) -> str:
+        """Use the tool."""
+        return self.api_wrapper.run(query)
+
+    async def _arun(self, query: str) -> str:
+        """Use the tool asynchronously."""
+        raise NotImplementedError("DuckDuckGoSearch does not support async")
