9f5749892badbfa2117354531ffe17ff1c4c712d
Harrison Chase <hw.chase.17@gmail.com>
2023-04-19 20:48:26 -0700

    cr

--- tests/unit_tests/memory/chat_message_histories/test_file.py
+++ tests/unit_tests/memory/chat_message_histories/test_file.py
@@ -1,6 +1,6 @@
-import os
 import tempfile
 from pathlib import Path
+from typing import Generator
 
 import pytest
 
@@ -9,14 +9,14 @@ from langchain.schema import AIMessage, HumanMessage
 
 
 @pytest.fixture
-def file_chat_message_history():
+def file_chat_message_history() -> Generator[FileChatMessageHistory, None, None]:
     with tempfile.TemporaryDirectory() as temp_dir:
         file_path = Path(temp_dir) / "test_chat_history.json"
         file_chat_message_history = FileChatMessageHistory(str(file_path))
         yield file_chat_message_history
 
 
-def test_add_messages(file_chat_message_history):
+def test_add_messages(file_chat_message_history: FileChatMessageHistory) -> None:
     file_chat_message_history.add_user_message("Hello!")
     file_chat_message_history.add_ai_message("Hi there!")
 
@@ -28,7 +28,7 @@ def test_add_messages(file_chat_message_history):
     assert messages[1].content == "Hi there!"
 
 
-def test_clear_messages(file_chat_message_history):
+def test_clear_messages(file_chat_message_history: FileChatMessageHistory) -> None:
     file_chat_message_history.add_user_message("Hello!")
     file_chat_message_history.add_ai_message("Hi there!")
 
@@ -37,7 +37,7 @@ def test_clear_messages(file_chat_message_history):
     assert len(messages) == 0
 
 
-def test_multiple_sessions(file_chat_message_history):
+def test_multiple_sessions(file_chat_message_history: FileChatMessageHistory) -> None:
     # First session
     file_chat_message_history.add_user_message("Hello, AI!")
     file_chat_message_history.add_ai_message("Hello, how can I help you?")
@@ -52,10 +52,8 @@ def test_multiple_sessions(file_chat_message_history):
     assert messages[0].content == "Hello, AI!"
     assert messages[1].content == "Hello, how can I help you?"
     assert messages[2].content == "Tell me a joke."
-    assert (
-        messages[3].content
-        == "Why did the chicken cross the road? To get to the other side!"
-    )
+    expected_content = "Why did the chicken cross the road? To get to the other side!"
+    assert messages[3].content == expected_content
 
     # Second session (reinitialize FileChatMessageHistory)
     file_path = file_chat_message_history.file_path
@@ -69,7 +67,5 @@ def test_multiple_sessions(file_chat_message_history):
     assert messages[0].content == "Hello, AI!"
     assert messages[1].content == "Hello, how can I help you?"
     assert messages[2].content == "Tell me a joke."
-    assert (
-        messages[3].content
-        == "Why did the chicken cross the road? To get to the other side!"
-    )
+    expected_content = "Why did the chicken cross the road? To get to the other side!"
+    assert messages[3].content == expected_content
