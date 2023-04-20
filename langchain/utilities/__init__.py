b7f2061736ce6009b9a1b50f92e10d2d4f49c5b3
Harrison Chase <hw.chase.17@gmail.com>
2023-04-20 07:57:07 -0700

    Harrison/google places (#3207)
    Co-authored-by: Cao Hoang <65607230+cnhhoang850@users.noreply.github.com>
    Co-authored-by: vowelparrot <130414180+vowelparrot@users.noreply.github.com>

--- langchain/utilities/__init__.py
+++ langchain/utilities/__init__.py
@@ -4,6 +4,7 @@ from langchain.utilities.apify import ApifyWrapper
 from langchain.utilities.arxiv import ArxivAPIWrapper
 from langchain.utilities.bash import BashProcess
 from langchain.utilities.bing_search import BingSearchAPIWrapper
+from langchain.utilities.google_places_api import GooglePlacesAPIWrapper
 from langchain.utilities.google_search import GoogleSearchAPIWrapper
 from langchain.utilities.google_serper import GoogleSerperAPIWrapper
 from langchain.utilities.openweathermap import OpenWeatherMapAPIWrapper
@@ -20,6 +21,7 @@ __all__ = [
     "TextRequestsWrapper",
     "GoogleSearchAPIWrapper",
     "GoogleSerperAPIWrapper",
+    "GooglePlacesAPIWrapper",
     "WolframAlphaAPIWrapper",
     "SerpAPIWrapper",
     "SearxSearchWrapper",
