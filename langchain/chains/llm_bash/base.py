d46328cd08d527174a0dfc356100aef3637e568a
fergus <fergusfettes@gmail.com>
2023-04-21 14:13:38 +0100

    persistent bash

--- langchain/chains/llm_bash/base.py
+++ langchain/chains/llm_bash/base.py
@@ -26,6 +26,7 @@ class LLMBashChain(Chain):
     input_key: str = "question"  #: :meta private:
     output_key: str = "answer"  #: :meta private:
     prompt: BasePromptTemplate = PROMPT
+    persistent: bool = False
 
     class Config:
         """Configuration for this pydantic object."""
@@ -51,28 +52,61 @@ class LLMBashChain(Chain):
 
     def _call(self, inputs: Dict[str, str]) -> Dict[str, str]:
         llm_executor = LLMChain(prompt=self.prompt, llm=self.llm)
-        bash_executor = BashProcess()
+        bash_executor = BashProcess(persistent=self.persistent)
         self.callback_manager.on_text(inputs[self.input_key], verbose=self.verbose)
 
         t = llm_executor.predict(question=inputs[self.input_key])
         self.callback_manager.on_text(t, color="green", verbose=self.verbose)
 
         t = t.strip()
-        if t.startswith("```bash"):
+        if "```bash" in t:
             # Split the string into a list of substrings
-            command_list = t.split("\n")
-            print(command_list)
+            command_list = self.get_code(t)
+            command_list = [s for s in command_list if s]
 
-            # Remove the first and last substrings
-            command_list = [s for s in command_list[1:-1]]
+            if self.verbose:
+                self.callback_manager.on_text("\nCode: ", verbose=self.verbose)
+                self.callback_manager.on_text(command_list, color="yellow", verbose=self.verbose)
+
             output = bash_executor.run(command_list)
 
             self.callback_manager.on_text("\nAnswer: ", verbose=self.verbose)
             self.callback_manager.on_text(output, color="yellow", verbose=self.verbose)
-
         else:
-            raise ValueError(f"unknown format from LLM: {t}")
+            output = t
+            # raise ValueError(f"unknown format from LLM: {t}")
         return {self.output_key: output}
+
+    def get_code_and_remainder(self, t: str) -> str:
+        """Get a python code block from the LLM result.
+
+        :meta private:
+        """
+        splits = t.split("```bash")  
+        split = splits[1].split("```")
+    
+        if len(splits)==2:
+            return split[0], "```".join(split[1:])
+        else:
+            return split[0], "```".join(split[1:]) + "```bash" + "```bash".join(splits[2:])
+
+    def get_code_blocks(self, t: str) -> List[str]:
+        """Get multiple python code blocks from the LLM result.
+
+        :meta private:
+        """
+        code_blocks = []
+        while "```bash" in t:
+            code, t = self.get_code_and_remainder(t)
+            code_blocks.append(code)
+        return code_blocks
+
+    def get_code(self, t: str) -> str:
+        """Get a python code block from the LLM result.
+
+        :meta private:
+        """
+        return ("\n".join(self.get_code_blocks(t))).split("\n")
 
     @property
     def _chain_type(self) -> str:
--- langchain/utilities/bash.py
+++ langchain/utilities/bash.py
@@ -3,22 +3,42 @@ import subprocess
 from typing import List, Union
 
 
+# Function to create a reusable Bash process
+def create_bash_process():
+    global pexpect
+    import pexpect
+    process = pexpect.spawn('bash', encoding='utf-8')
+    # Set the prompt to something we can find
+    process.sendline('export PS1=UNIQUE_PROMPT_ASDFFDSA')
+    process.expect('UNIQUE_PROMPT_ASDFFDSA')
+    return process
+
+
 class BashProcess:
     """Executes bash commands and returns the output."""
 
-    def __init__(self, strip_newlines: bool = False, return_err_output: bool = False):
+    def __init__(self, strip_newlines: bool = False, return_err_output: bool = False, persistent=False):
         """Initialize with stripping newlines."""
         self.strip_newlines = strip_newlines
         self.return_err_output = return_err_output
+        if persistent:
+            self.process = create_bash_process()
 
     def run(self, commands: Union[str, List[str]]) -> str:
         """Run commands and return final output."""
         if isinstance(commands, str):
             commands = [commands]
         commands = ";".join(commands)
+        if hasattr(self, 'process'):
+            return self._run_persistent(commands)
+        else:
+            return self._run(commands)
+
+    def _run(self, command: str) -> str:
+        """Run commands and return final output."""
         try:
             output = subprocess.run(
-                commands,
+                command,
                 shell=True,
                 check=True,
                 stdout=subprocess.PIPE,
@@ -31,3 +51,23 @@ class BashProcess:
         if self.strip_newlines:
             output = output.strip()
         return output
+
+    def _run_persistent(self, command: str) -> str:
+        """Run commands and return final output."""
+        self.process.sendline(command)
+
+        # Clear the output with an empty string
+        self.process.expect('UNIQUE_PROMPT_ASDFFDSA', timeout=10)
+        self.process.sendline('')
+
+        try:
+            self.process.expect(['UNIQUE_PROMPT_ASDFFDSA', pexpect.EOF], timeout=10)
+        except pexpect.TIMEOUT:
+            if "sudo" in command:
+                return "Timed out while waiting for password, sudo not supported yet. Be careful!"
+            return "Timeout error"
+        if self.process.after == pexpect.EOF:
+            return f"Exited with error status: {self.process.exitstatus}"
+        if self.strip_newlines:
+            return self.process.before.strip()
+        return self.process.before
