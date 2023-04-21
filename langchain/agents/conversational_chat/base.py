08268993ab694aca9d63c27884df496c10faeebc
huangsong <huangsong@leyantech.com>
2023-04-21 15:55:05 +0800

    change to my own response template easily.

--- langchain/agents/conversational_chat/base.py
+++ langchain/agents/conversational_chat/base.py
@@ -36,6 +36,7 @@ class ConversationalChatAgent(Agent):
     """An agent designed to hold a conversation in addition to using tools."""
 
     output_parser: AgentOutputParser = Field(default_factory=ConvoOutputParser)
+    tool_response_template: str = TEMPLATE_TOOL_RESPONSE
 
     @classmethod
     def _get_default_output_parser(cls, **kwargs: Any) -> AgentOutputParser:
@@ -93,7 +94,7 @@ class ConversationalChatAgent(Agent):
         for action, observation in intermediate_steps:
             thoughts.append(AIMessage(content=action.log))
             human_message = HumanMessage(
-                content=TEMPLATE_TOOL_RESPONSE.format(observation=observation)
+                content=self.tool_response_template.format(observation=observation)
             )
             thoughts.append(human_message)
         return thoughts
