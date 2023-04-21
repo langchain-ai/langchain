48f1bb2ee7b01ec1a055f8246391e6df7256cf8a
Anentropic <ego@anentropic.com>
2023-04-21 17:55:27 +0100

    fix for issue when loading Python files with non-default encoding

--- langchain/document_loaders/python.py
+++ langchain/document_loaders/python.py
@@ -2,15 +2,13 @@ import tokenize
 
 from langchain.document_loaders.text import TextLoader
 
+
 class PythonLoader(TextLoader):
-    pass
+    """
+    Load Python files, respecting any non-default encoding if specified.
+    """
 
-# class PythonLoader(TextLoader):
-#     """
-#     Load Python files, respecting any non-default encoding if specified.
-#     """
-
-#     def __init__(self, file_path: str):
-#         with open(file_path, 'rb') as f:
-#             encoding, _ = tokenize.detect_encoding(f.readline)
-#         super().__init__(file_path=file_path, encoding=encoding)
+    def __init__(self, file_path: str):
+        with open(file_path, 'rb') as f:
+            encoding, _ = tokenize.detect_encoding(f.readline)
+        super().__init__(file_path=file_path, encoding=encoding)
