3da62b30bff6918cbe181d8039bfeb4ea8f64607
Honkware <119620994+Honkware@users.noreply.github.com>
2023-04-22 02:42:49 +0000

    Formatting

--- langchain/document_loaders/chatgpt.py
+++ langchain/document_loaders/chatgpt.py
@@ -13,29 +13,37 @@ def concatenate_rows(message: dict, title: str) -> str:
 
     sender = message["author"]["role"] if message["author"] else "unknown"
     text = message["content"]["parts"][0]
-    date = datetime.datetime.fromtimestamp(message["create_time"]).strftime("%Y-%m-%d %H:%M:%S")
+    date = datetime.datetime.fromtimestamp(message["create_time"]).strftime(
+        "%Y-%m-%d %H:%M:%S"
+    )
     return f"{title} - {sender} on {date}: {text}\n\n"
 
 
 class ChatGPTLoader(BaseLoader):
     """Loader that loads conversations from exported ChatGPT data."""
+
     def __init__(self, log_file: str, num_logs: int = -1):
         self.log_file = log_file
         self.num_logs = num_logs
 
     def load(self) -> List[Document]:
         with open(self.log_file, encoding="utf8") as f:
-            data = json.load(f)[:self.num_logs] if self.num_logs else json.load(f)
+            data = json.load(f)[: self.num_logs] if self.num_logs else json.load(f)
 
         documents = []
         for d in data:
             title = d["title"]
             messages = d["mapping"]
-            text = "".join([
-                concatenate_rows(messages[key]["message"], title)
-                for idx, key in enumerate(messages)
-                if not (idx == 0 and messages[key]["message"]["author"]["role"] == "system")
-            ])
+            text = "".join(
+                [
+                    concatenate_rows(messages[key]["message"], title)
+                    for idx, key in enumerate(messages)
+                    if not (
+                        idx == 0
+                        and messages[key]["message"]["author"]["role"] == "system"
+                    )
+                ]
+            )
             metadata = {"source": str(self.log_file)}
             documents.append(Document(page_content=text, metadata=metadata))
 
