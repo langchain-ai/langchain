df7a624f891a4c433325f99d3b83123a07e97e11
Daniel Whitenack <whitenack.daniel@gmail.com>
2023-04-22 19:34:50 -0400

    tests and docs for Prediction Guard LLM wrapper

---
+++ tests/integration_tests/llms/test_predictionguard.py
@@ -0,0 +1,9 @@
+"""Test Prediction Guard API wrapper."""
+
+from langchain.llms.predictionguard import PredictionGuard
+
+def test_predictionguard_call() -> None:
+    """Test valid call to prediction guard."""
+    llm = PredictionGuard(name="default-text-gen")
+    output = llm("Say foo:")
+    assert isinstance(output, str)
