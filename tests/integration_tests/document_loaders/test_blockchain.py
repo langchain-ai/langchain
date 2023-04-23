330ddfe8f4548b81a163a2ea81a45dbbec67eecf
Jon Saginaw <saginawj@users.noreply.github.com>
2023-04-23 07:56:21 -0400

    initial blockchain docLoader and integrationTest for pulling nfts to a list of documents

---
+++ tests/integration_tests/document_loaders/test_blockchain.py
@@ -0,0 +1,17 @@
+import os
+import unittest
+from langchain.document_loaders import BlockchainDocumentLoader
+
+class TestBlockchainDocumentLoader(unittest.TestCase):
+    def setUp(self):
+        self.contract_address = "0x1a92f7381b9f03921564a437210bb9396471050c"  #CoolCats contract address
+        self.loader = BlockchainDocumentLoader(self.contract_address)
+
+    def test_get_nfts(self):
+        result = self.loader.load()
+        print(result[:2])
+        
+        self.assertIsInstance(result, list)
+
+if __name__ == "__main__":
+    unittest.main()
