8306f8c3d0df30686ac4933fce3e29a6ee3129ad
jeffvestal <53237856+jeffvestal@users.noreply.github.com>
2023-04-23 15:52:36 +0000

    added integration test for elasticsearch_embeddings.py

---
+++ tests/integration_tests/embeddings/test_elasticsearch_embeddings.py
@@ -0,0 +1,44 @@
+import pytest
+from elasticsearch import Elasticsearch
+from elasticsearch.client import MlClient
+from langchain.embeddings.elasticsearch_embeddings import ElasticsearchEmbeddings
+
+
+@pytest.fixture
+def es_connection():
+    # Replace with your actual Elasticsearch connection details
+    return Elasticsearch("http://localhost:9200")
+
+
+@pytest.fixture
+def model_id():
+    # Replace with your actual model_id
+    return "your_model_id"
+
+
+def test_elasticsearch_embedding_documents(es_connection, model_id) -> None:
+    """Test Elasticsearch embeddings."""
+    documents = ["foo bar"]
+    embedding = ElasticsearchEmbeddings(es_connection, model_id)
+    output = embedding.embed_documents(documents)
+    assert len(output) == 1
+    assert len(output[0]) == 768  # Change 768 to the expected embedding size
+
+
+def test_elasticsearch_embedding_documents_multiple(es_connection, model_id) -> None:
+    """Test Elasticsearch embeddings."""
+    documents = ["foo bar", "bar foo", "foo"]
+    embedding = ElasticsearchEmbeddings(es_connection, model_id)
+    output = embedding.embed_documents(documents)
+    assert len(output) == 3
+    assert len(output[0]) == 768  # Change 768 to the expected embedding size
+    assert len(output[1]) == 768  # Change 768 to the expected embedding size
+    assert len(output[2]) == 768  # Change 768 to the expected embedding size
+
+
+def test_elasticsearch_embedding_query(es_connection, model_id) -> None:
+    """Test Elasticsearch embeddings."""
+    document = "foo bar"
+    embedding = ElasticsearchEmbeddings(es_connection, model_id)
+    output = embedding.embed_query(document)
+    assert len(output) == 768  # Change 768 to the expected embedding size
