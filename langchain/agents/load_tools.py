91094b79694372633de107dd839aef88e705f48a
vowelparrot <130414180+vowelparrot@users.noreply.github.com>
2023-04-25 10:09:15 -0700

    Load tools

--- langchain/agents/load_tools.py
+++ langchain/agents/load_tools.py
@@ -1,7 +1,7 @@
 # flake8: noqa
 """Load tools."""
 import warnings
-from typing import Any, List, Optional, Callable
+from typing import Any, Dict, List, Optional, Callable, Tuple
 from mypy_extensions import KwArg
 
 from langchain.agents.tools import Tool
@@ -217,8 +217,10 @@ def _get_searx_search_results_json(**kwargs: Any) -> BaseTool:
 def _get_bing_search(**kwargs: Any) -> BaseTool:
     return BingSearchRun(api_wrapper=BingSearchAPIWrapper(**kwargs))
 
+
 def _get_ddg_search(**kwargs: Any) -> BaseTool:
     return DuckDuckGoSearchTool(api_wrapper=DuckDuckGoSearchAPIWrapper(**kwargs))
+
 
 def _get_human_tool(**kwargs: Any) -> BaseTool:
     return HumanInputRun(**kwargs)
@@ -230,7 +232,7 @@ _EXTRA_LLM_TOOLS = {
     "podcast-api": (_get_podcast_api, ["listen_api_key"]),
 }
 
-_EXTRA_OPTIONAL_TOOLS: dict[str, tuple[Callable[[KwArg(Any)], BaseTool], List[str]]] = {
+_EXTRA_OPTIONAL_TOOLS: Dict[str, Tuple[Callable[[KwArg(Any)], BaseTool], List[str]]] = {
     "wolfram-alpha": (_get_wolfram_alpha, ["wolfram_alpha_appid"]),
     "google-search": (_get_google_search, ["google_api_key", "google_cse_id"]),
     "google-search-results-json": (
