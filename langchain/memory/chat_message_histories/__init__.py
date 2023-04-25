9fc7b7583c85115f5fa82a0d430863ee59821e16
Zura Isakadze <z.isakadze@gmail.com>
2023-04-25 20:02:10 +0400

    Add SQLiteChatMessageHistory
    It's based on already existing PostgresChatMessageHistory

--- langchain/memory/chat_message_histories/__init__.py
+++ langchain/memory/chat_message_histories/__init__.py
@@ -3,11 +3,13 @@ from langchain.memory.chat_message_histories.dynamodb import DynamoDBChatMessage
 from langchain.memory.chat_message_histories.file import FileChatMessageHistory
 from langchain.memory.chat_message_histories.postgres import PostgresChatMessageHistory
 from langchain.memory.chat_message_histories.redis import RedisChatMessageHistory
+from langchain.memory.chat_message_histories.sqlite import SQLiteChatMessageHistory
 
 __all__ = [
     "DynamoDBChatMessageHistory",
     "RedisChatMessageHistory",
     "PostgresChatMessageHistory",
+    "SQLiteChatMessageHistory",
     "FileChatMessageHistory",
     "CosmosDBChatMessageHistory",
 ]
