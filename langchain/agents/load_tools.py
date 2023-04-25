0ca9528cf2adf4c5d54e0d452fdd70835a51ed5f
Mike Wang <62768671+skcoirz@users.noreply.github.com>
2023-04-25 10:08:29 -0700

    [simple] add duckduckgo to load_tools.py (#3376)
    - duckduckgo was introduced in
    https://github.com/hwchase17/langchain/pull/3206/, but it’s not enabled
    in load_tools.py yet.
    - this PR simply enables it in load_tools.py with a straightforward
    agent call (as described in get_started doc) to verify the
    functionality.
    - duckduckgo integration test passed.
    
    TODO:
    - not seeing an agent specific integration. Since I’m still ramping up
    here, will add it in a following PR.
    - it looks like DuckDuckGo is using a different naming convention with
    postfix “-Tool” vs “-Run” comparing with others. The work is
    straightforward and it is a great way to explore the codebase, so I
    decided to take the chance to deeply read the codebase and then make the
    straightforward change in the next PR. :)
    
    This is my first PR. Please let me know if there is anything I can
    improve. Thank you so much!
    
    Agent Call with DuckDuckGo instead. <img width="1279" alt="Screenshot
    2023-04-22 at 8 44 18 PM"
    src="https://user-images.githubusercontent.com/62768671/233819592-85932514-46e8-406c-8d52-672ec4161b42.png">

--- langchain/agents/load_tools.py
+++ langchain/agents/load_tools.py
@@ -1,7 +1,8 @@
 # flake8: noqa
 """Load tools."""
 import warnings
-from typing import Any, List, Optional
+from typing import Any, List, Optional, Callable
+from mypy_extensions import KwArg
 
 from langchain.agents.tools import Tool
 from langchain.callbacks.base import BaseCallbackManager
@@ -14,6 +15,7 @@ from langchain.requests import TextRequestsWrapper
 from langchain.tools.arxiv.tool import ArxivQueryRun
 from langchain.tools.base import BaseTool
 from langchain.tools.bing_search.tool import BingSearchRun
+from langchain.tools.ddg_search.tool import DuckDuckGoSearchTool
 from langchain.tools.google_search.tool import GoogleSearchResults, GoogleSearchRun
 from langchain.tools.human.tool import HumanInputRun
 from langchain.tools.python.tool import PythonREPLTool
@@ -31,6 +33,7 @@ from langchain.utilities import ArxivAPIWrapper
 from langchain.utilities.apify import ApifyWrapper
 from langchain.utilities.bash import BashProcess
 from langchain.utilities.bing_search import BingSearchAPIWrapper
+from langchain.utilities.duckduckgo_search import DuckDuckGoSearchAPIWrapper
 from langchain.utilities.google_search import GoogleSearchAPIWrapper
 from langchain.utilities.google_serper import GoogleSerperAPIWrapper
 from langchain.utilities.searx_search import SearxSearchWrapper
@@ -214,6 +217,8 @@ def _get_searx_search_results_json(**kwargs: Any) -> BaseTool:
 def _get_bing_search(**kwargs: Any) -> BaseTool:
     return BingSearchRun(api_wrapper=BingSearchAPIWrapper(**kwargs))
 
+def _get_ddg_search(**kwargs: Any) -> BaseTool:
+    return DuckDuckGoSearchTool(api_wrapper=DuckDuckGoSearchAPIWrapper(**kwargs))
 
 def _get_human_tool(**kwargs: Any) -> BaseTool:
     return HumanInputRun(**kwargs)
@@ -225,7 +230,7 @@ _EXTRA_LLM_TOOLS = {
     "podcast-api": (_get_podcast_api, ["listen_api_key"]),
 }
 
-_EXTRA_OPTIONAL_TOOLS = {
+_EXTRA_OPTIONAL_TOOLS: dict[str, tuple[Callable[[KwArg(Any)], BaseTool], List[str]]] = {
     "wolfram-alpha": (_get_wolfram_alpha, ["wolfram_alpha_appid"]),
     "google-search": (_get_google_search, ["google_api_key", "google_cse_id"]),
     "google-search-results-json": (
@@ -237,6 +242,7 @@ _EXTRA_OPTIONAL_TOOLS = {
         ["searx_host", "engines", "num_results", "aiosession"],
     ),
     "bing-search": (_get_bing_search, ["bing_subscription_key", "bing_search_url"]),
+    "ddg-search": (_get_ddg_search, []),
     "google-serper": (_get_google_serper, ["serper_api_key"]),
     "serpapi": (_get_serpapi, ["serpapi_api_key", "aiosession"]),
     "searx-search": (_get_searx_search, ["searx_host", "engines", "aiosession"]),
