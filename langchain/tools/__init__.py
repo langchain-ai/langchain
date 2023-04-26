a2690241944f08a212ed9383a4afdf18a937b9e5
Zander Chase <130414180+vowelparrot@users.noreply.github.com>
2023-04-26 16:21:34 -0700

    Align names of search tools (#3620)
    Tools for Bing, DDG and Google weren't consistent even though the
    underlying implementations were.
    All three services now have the same tools and implementations to easily
    switch and experiment when building chains.

--- langchain/tools/__init__.py
+++ langchain/tools/__init__.py
@@ -1,19 +1,27 @@
 """Core toolkit implementations."""
 
 from langchain.tools.base import BaseTool
-from langchain.tools.ddg_search.tool import DuckDuckGoSearchTool
+from langchain.tools.bing_search.tool import BingSearchResults, BingSearchRun
+from langchain.tools.ddg_search.tool import DuckDuckGoSearchResults, DuckDuckGoSearchRun
 from langchain.tools.google_places.tool import GooglePlacesTool
+from langchain.tools.google_search.tool import GoogleSearchResults, GoogleSearchRun
 from langchain.tools.ifttt import IFTTTWebhook
 from langchain.tools.openapi.utils.api_models import APIOperation
 from langchain.tools.openapi.utils.openapi_utils import OpenAPISpec
 from langchain.tools.plugin import AIPluginTool
 
 __all__ = [
-    "BaseTool",
-    "IFTTTWebhook",
     "AIPluginTool",
-    "OpenAPISpec",
     "APIOperation",
+    "BingSearchResults",
+    "BingSearchRun",
+    "DuckDuckGoSearchResults",
+    "DuckDuckGoSearchRun",
+    "DuckDuckGoSearchRun",
     "GooglePlacesTool",
-    "DuckDuckGoSearchTool",
+    "GoogleSearchResults",
+    "GoogleSearchRun",
+    "IFTTTWebhook",
+    "OpenAPISpec",
+    "BaseTool",
 ]
