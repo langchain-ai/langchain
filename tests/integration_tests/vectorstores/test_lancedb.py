4673ee0a76a194900d6577beed22cebb7bf4698e
Minh Le <minhle@canva.com>
2023-04-26 14:15:09 +1000

    add tests

--- tests/integration_tests/vectorstores/test_lancedb.py
+++ tests/integration_tests/vectorstores/test_lancedb.py
@@ -7,13 +7,36 @@ def test_lancedb():
     embed = FakeEmbeddings()
     db = lancedb.connect("/tmp/lancedb")
     texts = ["text 1", "text 2", "item 3"]
+    vectors = embed.embed_documents(texts)
     table = db.create_table(
         "my_table",
         data=[
-            {"vector": embed.embed_documents([text])[0], "id": text, "text": text}
-            for text in texts
+            {"vector": vectors[idx], "id": text, "text": text}
+            for idx, text in enumerate(texts)
         ],
         mode="overwrite",
     )
     store = LanceDB(table, embedding_function=embed.embed_documents)
-    store.similarity_search("ext")
+    result = store.similarity_search("text 1")
+    result_texts = [doc.page_content for doc in result]
+    assert "text 1" in result_texts
+
+
+def test_lancedb_add_texts():
+    embed = FakeEmbeddings()
+    db = lancedb.connect("/tmp/lancedb")
+    texts = ["text 1"]
+    vectors = embed.embed_documents(texts)
+    table = db.create_table(
+        "my_table",
+        data=[
+            {"vector": vectors[idx], "id": text, "text": text}
+            for idx, text in enumerate(texts)
+        ],
+        mode="overwrite",
+    )
+    store = LanceDB(table, embedding_function=embed.embed_documents)
+    store.add_texts(["text 2"])
+    result = store.similarity_search("text 2")
+    result_texts = [doc.page_content for doc in result]
+    assert "text 2" in result_texts
