967094a39bf1e244b1fad01833faaef12c2906e8
Taqi Jaffri <tjaffri@docugami.com>
2023-04-26 17:52:35 -0700

    Improvements to SQLDatabaseChain

--- langchain/chains/sql_database/base.py
+++ langchain/chains/sql_database/base.py
@@ -2,6 +2,8 @@
 from __future__ import annotations
 
 from typing import Any, Dict, List, Optional
+from langchain.prompts.prompt import PromptTemplate
+from langchain.tools.sql_database.prompt import QUERY_CHECKER
 
 from pydantic import Extra, Field
 
@@ -11,6 +13,8 @@ from langchain.chains.sql_database.prompt import DECIDER_PROMPT, PROMPT, SQL_PRO
 from langchain.prompts.base import BasePromptTemplate
 from langchain.schema import BaseLanguageModel
 from langchain.sql_database import SQLDatabase
+
+INTERMEDIATE_STEPS_KEY = "intermediate_steps"
 
 
 class SQLDatabaseChain(Chain):
@@ -38,6 +42,11 @@ class SQLDatabaseChain(Chain):
     """Whether or not to return the intermediate steps along with the final answer."""
     return_direct: bool = False
     """Whether or not to return the result of querying the SQL table directly."""
+    use_query_checker: bool = False
+    """Whether or not the query checker tool should be used to attempt 
+    to fix the initial SQL from the LLM."""
+    query_checker_prompt: Optional[BasePromptTemplate] = None
+    """The prompt template that should be used by the query checker"""
 
     class Config:
         """Configuration for this pydantic object."""
@@ -62,7 +71,7 @@ class SQLDatabaseChain(Chain):
         if not self.return_intermediate_steps:
             return [self.output_key]
         else:
-            return [self.output_key, "intermediate_steps"]
+            return [self.output_key, INTERMEDIATE_STEPS_KEY]
 
     def _call(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
         prompt = self.prompt or SQL_PROMPTS.get(self.database.dialect, PROMPT)
@@ -80,28 +89,67 @@ class SQLDatabaseChain(Chain):
             "stop": ["\nSQLResult:"],
         }
         intermediate_steps = []
-        sql_cmd = llm_chain.predict(**llm_inputs)
-        intermediate_steps.append(sql_cmd)
-        self.callback_manager.on_text(sql_cmd, color="green", verbose=self.verbose)
-        result = self.database.run(sql_cmd)
-        intermediate_steps.append(result)
-        self.callback_manager.on_text("\nSQLResult: ", verbose=self.verbose)
-        self.callback_manager.on_text(result, color="yellow", verbose=self.verbose)
-        # If return direct, we just set the final result equal to the sql query
-        if self.return_direct:
-            final_result = result
-        else:
-            self.callback_manager.on_text("\nAnswer:", verbose=self.verbose)
-            input_text += f"{sql_cmd}\nSQLResult: {result}\nAnswer:"
-            llm_inputs["input"] = input_text
-            final_result = llm_chain.predict(**llm_inputs)
-            self.callback_manager.on_text(
-                final_result, color="green", verbose=self.verbose
-            )
-        chain_result: Dict[str, Any] = {self.output_key: final_result}
-        if self.return_intermediate_steps:
-            chain_result["intermediate_steps"] = intermediate_steps
-        return chain_result
+        try:
+            intermediate_steps.append(llm_inputs)
+            sql_cmd = llm_chain.predict(**llm_inputs)
+            intermediate_steps.append(sql_cmd)
+            if not self.use_query_checker:
+                self.callback_manager.on_text(
+                    sql_cmd, color="green",
+                    verbose=self.verbose
+                )
+                result = self.database.run(sql_cmd)
+            else:
+                query_checker_prompt = self.query_checker_prompt or PromptTemplate(
+                    template=QUERY_CHECKER,
+                    input_variables=["query", "dialect"]
+                )
+                query_checker_chain = LLMChain(
+                    llm=self.llm,
+                    prompt=query_checker_prompt
+                )
+                query_checker_inputs = {
+                    "query": sql_cmd,
+                    "dialect": self.database.dialect
+                }
+                intermediate_steps.append(query_checker_inputs)
+                checked_sql_command: str = query_checker_chain.predict(
+                    **query_checker_inputs
+                )
+                intermediate_steps.append(checked_sql_command)
+                self.callback_manager.on_text(
+                    checked_sql_command,
+                    color="green",
+                    verbose=self.verbose
+                )
+                result = self.database.run(checked_sql_command)
+                sql_cmd = checked_sql_command
+
+            intermediate_steps.append(result)
+            self.callback_manager.on_text("\nSQLResult: ", verbose=self.verbose)
+            self.callback_manager.on_text(result, color="yellow", verbose=self.verbose)
+            # If return direct, we just set the final result equal to
+            # the result of the sql query result, otherwise try to get a human readable
+            # final answer
+            if self.return_direct:
+                final_result = result
+            else:
+                self.callback_manager.on_text("\nAnswer:", verbose=self.verbose)
+                input_text += f"{sql_cmd}\nSQLResult: {result}\nAnswer:"
+                llm_inputs["input"] = input_text
+                final_result = llm_chain.predict(**llm_inputs)
+                self.callback_manager.on_text(
+                    final_result, color="green", verbose=self.verbose
+                )
+            chain_result: Dict[str, Any] = {self.output_key: final_result}        
+            if self.return_intermediate_steps:
+                chain_result[INTERMEDIATE_STEPS_KEY] = intermediate_steps
+            return chain_result
+        except Exception as exc:
+            # Append intermediate steps to exception, to aid in logging and later
+            # improvement of few shot prompt seeds
+            exc.intermediate_steps = intermediate_steps
+            raise exc
 
     @property
     def _chain_type(self) -> str:
@@ -160,7 +208,7 @@ class SQLDatabaseSequentialChain(Chain):
         if not self.return_intermediate_steps:
             return [self.output_key]
         else:
-            return [self.output_key, "intermediate_steps"]
+            return [self.output_key, INTERMEDIATE_STEPS_KEY]
 
     def _call(self, inputs: Dict[str, str]) -> Dict[str, str]:
         _table_names = self.sql_chain.database.get_usable_table_names()
