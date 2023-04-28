e60489e9392860da35179ae05d8e536c94bf9fbe
Ankush Gola <ankush.gola@gmail.com>
2023-04-28 16:52:33 -0700

    fix lint

--- langchain/chains/llm_bash/base.py
+++ langchain/chains/llm_bash/base.py
@@ -94,7 +94,7 @@ class LLMBashChain(Chain):
         _run_manager.on_text(t, color="green", verbose=self.verbose)
         t = t.strip()
         try:
-            command_list = self.llm_chain.prompt.output_parser.parse(t)  # type: ignore[union-attr]
+            command_list = self.llm_chain.prompt.output_parser.parse(t)  # type: ignore[union-attr] # noqa: E501
         except OutputParserException as e:
             _run_manager.on_chain_error(e, verbose=self.verbose)
             raise e
