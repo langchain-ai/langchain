0d45b147740ef225aaf35aea8343efb44ef3655b
Rafal Wojdyla <ravwojdyla@gmail.com>
2023-04-29 00:33:22 +0100

    Expose kwargs in LLMChainExtractor.from_llm

--- langchain/retrievers/document_compressors/chain_extract.py
+++ langchain/retrievers/document_compressors/chain_extract.py
@@ -69,9 +69,10 @@ class LLMChainExtractor(BaseDocumentCompressor):
         llm: BaseLanguageModel,
         prompt: Optional[PromptTemplate] = None,
         get_input: Optional[Callable[[str, Document], str]] = None,
+        llm_chain_kwargs: Optional[dict] = None,
     ) -> "LLMChainExtractor":
         """Initialize from LLM."""
         _prompt = prompt if prompt is not None else _get_default_chain_prompt()
         _get_input = get_input if get_input is not None else default_get_input
-        llm_chain = LLMChain(llm=llm, prompt=_prompt)
+        llm_chain = LLMChain(llm=llm, prompt=_prompt, **(llm_chain_kwargs or {}))
         return cls(llm_chain=llm_chain, get_input=_get_input)
