d2b6dec80ce8cc265ead484f0dad64dba013004b
Lester Solbakken <lesters@oath.com>
2023-04-28 12:35:43 +0200

    Add Vespa retriever

---
+++ langchain/retrievers/vespa_retriever.py
@@ -0,0 +1,40 @@
+"""Wrappwer for retrieving documents from Vespa."""
+
+import json
+
+from typing import List
+from langchain.schema import Document, BaseRetriever
+from vespa.application import Vespa
+
+
+class VespaRetriever(BaseRetriever):
+
+    def __init__(self, app: Vespa, body: dict, content_field: str):
+        self._application = app
+        self._query_body = body
+        self._content_field = content_field
+
+    def get_relevant_documents(self, query: str) -> List[Document]:
+        body = self._query_body.copy()
+        body["query"] = query
+        response = self._application.query(body)
+
+        if not str(response.status_code).startswith('2'):
+            raise RuntimeError("Could not retrieve data from Vespa. Error code: {}".format(response.status_code))
+
+        root = response.json["root"]
+        if "errors" in root:
+            raise RuntimeError(json.dumps(root["errors"]))
+
+        hits = []
+        for child in response.hits:
+            page_content = child["fields"][self._content_field]
+            metadata = {
+                "id": child["id"]
+            }
+            hits.append(Document(page_content=page_content, metadata=metadata))
+        return hits
+
+    async def aget_relevant_documents(self, query: str) -> List[Document]:
+        raise NotImplementedError
+
