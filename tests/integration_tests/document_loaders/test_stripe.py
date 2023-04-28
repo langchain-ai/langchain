a9b75a59a754b40d246cc5f41cc5ff88206e17d9
Ismail Pelaseyed <homanp@gmail.com>
2023-04-28 23:38:28 +0200

    Add `Stripe` document loader

---
+++ tests/integration_tests/document_loaders/test_stripe.py
@@ -0,0 +1,13 @@
+from langchain.document_loaders.stripe import StripeLoader
+
+
+access_token = ""
+resource = "charges"
+
+def test_stripe_loader() -> None:
+    """Test Figma file loader."""
+    stripe_loader = StripeLoader(access_token, resource)
+    documents = stripe_loader.load()
+    print(documents)
+
+    assert len(documents) == 1
