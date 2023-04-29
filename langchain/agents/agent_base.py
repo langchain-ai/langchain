38d42fec90c128d54725058730f1a4c8fa91e4c5
Mike W <62768671+skcoirz@users.noreply.github.com>
2023-04-29 11:07:37 -0700

    restructured with a shared base class

--- langchain/agents/agent_base.py
+++ langchain/agents/agent_base.py
@@ -8,7 +8,7 @@ from pathlib import Path
 from typing import Any, Dict, List, Optional, Sequence, Tuple, Union
 
 import yaml
-from pydantic import BaseModel
+from pydantic import BaseAc, validator
 
 from langchain.callbacks.base import BaseCallbackManager
 from langchain.schema import (
@@ -20,9 +20,16 @@ from langchain.tools.base import BaseTool
 
 logger = logging.getLogger(__name__)
 
+class BaseActionAgent(BaseModel):
+    """Base Action Agent class."""
 
-class BaseSingleActionAgent(BaseModel):
-    """Base Agent class."""
+    early_stop_msg: str = "Agent stopped due to max iterations."
+
+    @validator("early_stop_msg")
+    def validate_early_stop_msg(cls, v: str) -> str:
+        if v == "":
+            raise ValueError("Early stop message cannot be empty.")
+        return v
 
     @property
     def return_values(self) -> List[str]:
@@ -69,6 +76,53 @@ class BaseSingleActionAgent(BaseModel):
 
         :meta private:
         """
+
+    @property
+    def _agent_type(self) -> str:
+        """Return Identifier of agent type."""
+        raise NotImplementedError
+
+    def dict(self, **kwargs: Any) -> Dict:
+        """Return dictionary representation of agent."""
+        _dict = super().dict()
+        _dict["_type"] = str(self._agent_type)
+        return _dict
+
+    def save(self, file_path: Union[Path, str]) -> None:
+        """Save the agent.
+
+        Args:
+            file_path: Path to file to save the agent to.
+
+        Example:
+        .. code-block:: python
+
+            # If working with agent executor
+            agent.agent.save(file_path="path/agent.yaml")
+        """
+        # Convert file to Path object.
+        if isinstance(file_path, str):
+            save_path = Path(file_path)
+        else:
+            save_path = file_path
+
+        directory_path = save_path.parent
+        directory_path.mkdir(parents=True, exist_ok=True)
+
+        # Fetch dictionary to save
+        agent_dict = self.dict()
+
+        if save_path.suffix == ".json":
+            with open(file_path, "w") as f:
+                json.dump(agent_dict, f, indent=4)
+        elif save_path.suffix == ".yaml":
+            with open(file_path, "w") as f:
+                yaml.dump(agent_dict, f, default_flow_style=False)
+        else:
+            raise ValueError(f"{save_path} must be json or yaml")
+
+    def tool_run_logging_kwargs(self) -> Dict:
+        return {}
 
     def return_stopped_response(
         self,
@@ -80,12 +134,17 @@ class BaseSingleActionAgent(BaseModel):
         if early_stopping_method == "force":
             # `force` just returns a constant string
             return AgentFinish(
-                {"output": "Agent stopped due to iteration limit or time limit."}, ""
+                {"output": self.early_stop_msg}, ""
             )
         else:
             raise ValueError(
                 f"Got unsupported early_stopping_method `{early_stopping_method}`"
             )
+
+class BaseSingleActionAgent(BaseActionAgent):
+    """Base Single Action Agent class."""
+
+    early_stop_msg: str = "Agent stopped due to iteration limit or time limit."
 
     @classmethod
     def from_llm_and_tools(
@@ -97,161 +156,13 @@ class BaseSingleActionAgent(BaseModel):
     ) -> BaseSingleActionAgent:
         raise NotImplementedError
 
-    @property
-    def _agent_type(self) -> str:
-        """Return Identifier of agent type."""
-        raise NotImplementedError
+class BaseMultiActionAgent(BaseActionAgent):
+    """Base Multi Action Agent class."""
 
-    def dict(self, **kwargs: Any) -> Dict:
-        """Return dictionary representation of agent."""
-        _dict = super().dict()
-        _dict["_type"] = str(self._agent_type)
-        return _dict
-
-    def save(self, file_path: Union[Path, str]) -> None:
-        """Save the agent.
-
-        Args:
-            file_path: Path to file to save the agent to.
-
-        Example:
-        .. code-block:: python
-
-            # If working with agent executor
-            agent.agent.save(file_path="path/agent.yaml")
-        """
-        # Convert file to Path object.
-        if isinstance(file_path, str):
-            save_path = Path(file_path)
-        else:
-            save_path = file_path
-
-        directory_path = save_path.parent
-        directory_path.mkdir(parents=True, exist_ok=True)
-
-        # Fetch dictionary to save
-        agent_dict = self.dict()
-
-        if save_path.suffix == ".json":
-            with open(file_path, "w") as f:
-                json.dump(agent_dict, f, indent=4)
-        elif save_path.suffix == ".yaml":
-            with open(file_path, "w") as f:
-                yaml.dump(agent_dict, f, default_flow_style=False)
-        else:
-            raise ValueError(f"{save_path} must be json or yaml")
-
-    def tool_run_logging_kwargs(self) -> Dict:
-        return {}
+    early_stop_msg: str = "Agent stopped due to max iterations."
 
 
-class BaseMultiActionAgent(BaseModel):
-    """Base Agent class."""
 
-    @property
-    def return_values(self) -> List[str]:
-        """Return values of the agent."""
-        return ["output"]
+  
 
-    def get_allowed_tools(self) -> Optional[List[str]]:
-        return None
 
-    @abstractmethod
-    def plan(
-        self, intermediate_steps: List[Tuple[AgentAction, str]], **kwargs: Any
-    ) -> Union[List[AgentAction], AgentFinish]:
-        """Given input, decided what to do.
-
-        Args:
-            intermediate_steps: Steps the LLM has taken to date,
-                along with observations
-            **kwargs: User inputs.
-
-        Returns:
-            Actions specifying what tool to use.
-        """
-
-    @abstractmethod
-    async def aplan(
-        self, intermediate_steps: List[Tuple[AgentAction, str]], **kwargs: Any
-    ) -> Union[List[AgentAction], AgentFinish]:
-        """Given input, decided what to do.
-
-        Args:
-            intermediate_steps: Steps the LLM has taken to date,
-                along with observations
-            **kwargs: User inputs.
-
-        Returns:
-            Actions specifying what tool to use.
-        """
-
-    @property
-    @abstractmethod
-    def input_keys(self) -> List[str]:
-        """Return the input keys.
-
-        :meta private:
-        """
-
-    def return_stopped_response(
-        self,
-        early_stopping_method: str,
-        intermediate_steps: List[Tuple[AgentAction, str]],
-        **kwargs: Any,
-    ) -> AgentFinish:
-        """Return response when agent has been stopped due to max iterations."""
-        if early_stopping_method == "force":
-            # `force` just returns a constant string
-            return AgentFinish({"output": "Agent stopped due to max iterations."}, "")
-        else:
-            raise ValueError(
-                f"Got unsupported early_stopping_method `{early_stopping_method}`"
-            )
-
-    @property
-    def _agent_type(self) -> str:
-        """Return Identifier of agent type."""
-        raise NotImplementedError
-
-    def dict(self, **kwargs: Any) -> Dict:
-        """Return dictionary representation of agent."""
-        _dict = super().dict()
-        _dict["_type"] = str(self._agent_type)
-        return _dict
-
-    def save(self, file_path: Union[Path, str]) -> None:
-        """Save the agent.
-
-        Args:
-            file_path: Path to file to save the agent to.
-
-        Example:
-        .. code-block:: python
-
-            # If working with agent executor
-            agent.agent.save(file_path="path/agent.yaml")
-        """
-        # Convert file to Path object.
-        if isinstance(file_path, str):
-            save_path = Path(file_path)
-        else:
-            save_path = file_path
-
-        directory_path = save_path.parent
-        directory_path.mkdir(parents=True, exist_ok=True)
-
-        # Fetch dictionary to save
-        agent_dict = self.dict()
-
-        if save_path.suffix == ".json":
-            with open(file_path, "w") as f:
-                json.dump(agent_dict, f, indent=4)
-        elif save_path.suffix == ".yaml":
-            with open(file_path, "w") as f:
-                yaml.dump(agent_dict, f, default_flow_style=False)
-        else:
-            raise ValueError(f"{save_path} must be json or yaml")
-
-    def tool_run_logging_kwargs(self) -> Dict:
-        return {}
