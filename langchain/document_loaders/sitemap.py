0786e3bc35bf0a441fb70e6a74903b95fc57872a
Martin Holzhauer <martin@holzhauer.eu>
2023-04-29 18:44:51 +0200

    added support for blockwise sitemap indexing

--- langchain/document_loaders/sitemap.py
+++ langchain/document_loaders/sitemap.py
@@ -1,5 +1,6 @@
 """Loader that fetches a sitemap and loads those URLs."""
 import re
+import itertools
 from typing import Any, Callable, List, Optional
 
 from langchain.document_loaders.web_base import WebBaseLoader
@@ -9,6 +10,10 @@ from langchain.schema import Document
 def _default_parsing_function(content: Any) -> str:
     return str(content.get_text())
 
+def _batch_block(iterable, size):
+    it = iter(iterable)
+    while item := list(itertools.islice(it, size)):
+        yield item
 
 class SitemapLoader(WebBaseLoader):
     """Loader that fetches a sitemap and loads those URLs."""
@@ -18,6 +23,8 @@ class SitemapLoader(WebBaseLoader):
         web_path: str,
         filter_urls: Optional[List[str]] = None,
         parsing_function: Optional[Callable] = None,
+        blocksize: Optional[int] = None,
+        blocknum: Optional[int] = None,
     ):
         """Initialize with webpage path and optional filter URLs.
 
@@ -26,6 +33,8 @@ class SitemapLoader(WebBaseLoader):
             filter_urls: list of strings or regexes that will be applied to filter the
                 urls that are parsed and loaded
             parsing_function: Function to parse bs4.Soup output
+            blocksize: number of sitemap locations per block
+            blocknum: the number of the block that should be loaded - zero indexed
         """
 
         try:
@@ -39,6 +48,8 @@ class SitemapLoader(WebBaseLoader):
 
         self.filter_urls = filter_urls
         self.parsing_function = parsing_function or _default_parsing_function
+        self.blocksize = blocksize
+        self.blocknum = blocknum
 
     def parse_sitemap(self, soup: Any) -> List[dict]:
         """Parse sitemap xml and load into a list of dicts."""
@@ -75,6 +86,16 @@ class SitemapLoader(WebBaseLoader):
         soup = self.scrape("xml")
 
         els = self.parse_sitemap(soup)
+
+        if self.blocksize is not None and self.blocknum is not None:
+            elblocks = list(_batch_block(els, self.blocksize))
+            blockcount = len(elblocks)
+            if blockcount - 1 < self.blocknum:
+                raise ValueError(
+                    "Selected sitemap does not contain enough blocks for given blocknum"
+                )
+            else:
+                els = elblocks[self.blocknum]
 
         results = self.scrape_all([el["loc"].strip() for el in els if "loc" in el])
 
--- tests/integration_tests/document_loaders/test_sitemap.py
+++ tests/integration_tests/document_loaders/test_sitemap.py
@@ -1,5 +1,5 @@
 from langchain.document_loaders import SitemapLoader
-
+import pytest
 
 def test_sitemap() -> None:
     """Test sitemap loader."""
@@ -9,11 +9,34 @@ def test_sitemap() -> None:
     assert "🦜🔗" in documents[0].page_content
 
 
+def test_sitemap_block() -> None:
+    """Test sitemap loader."""
+    loader = SitemapLoader("https://langchain.readthedocs.io/sitemap.xml", blocksize=1, blocknum=1)
+    documents = loader.load()
+    assert len(documents) == 1
+    assert "🦜🔗" in documents[0].page_content
+
+
+def test_sitemap_block_only_one() -> None:
+    """Test sitemap loader."""
+    loader = SitemapLoader("https://langchain.readthedocs.io/sitemap.xml", blocksize=1000000, blocknum=0)
+    documents = loader.load()
+    assert len(documents) > 1
+    assert "🦜🔗" in documents[0].page_content
+
+
+def test_sitemap_block_does_not_exists() -> None:
+    """Test sitemap loader."""
+    loader = SitemapLoader("https://langchain.readthedocs.io/sitemap.xml", blocksize=1000000, blocknum=15)
+    with pytest.raises(ValueError):
+        documents = loader.load()
+
+
 def test_filter_sitemap() -> None:
     """Test sitemap loader."""
     loader = SitemapLoader(
         "https://langchain.readthedocs.io/sitemap.xml",
-        filter_urls=["https://langchain.readthedocs.io/en/stable/"],
+        filter_urls=["https://python.langchain.com/en/stable/"],
     )
     documents = loader.load()
     assert len(documents) == 1
