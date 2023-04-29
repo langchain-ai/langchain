7f41e26572f2574e59c822d81ea3b3fae3c4ac14
Harrison Chase <hw.chase.17@gmail.com>
2023-04-28 20:59:56 -0700

    Langchain with reddit (#3661) (#3768)
    I have added a reddit document loader which fetches the text from the
    Posts of Subreddits or Reddit users, using the `praw` Python package. I
    have also added an example notebook reddit.ipynb in order to guide users
    to use this dataloader.
    This code was made in format similar to twiiter document loader. I have
    run code formating, linting and also checked the code myself for
    different scenarios.
    
    This is my first contribution to an open source project and I am really
    excited about this. If you want to suggest some improvements in my code,
    I will be happy to do it. :)
    
    Co-authored-by: Taaha Bajwa <taaha.s.bajwa@gmail.com>

--- langchain/document_loaders/__init__.py
+++ langchain/document_loaders/__init__.py
@@ -63,6 +63,7 @@ from langchain.document_loaders.pdf import (
 from langchain.document_loaders.powerpoint import UnstructuredPowerPointLoader
 from langchain.document_loaders.python import PythonLoader
 from langchain.document_loaders.readthedocs import ReadTheDocsLoader
+from langchain.document_loaders.reddit import RedditPostsLoader
 from langchain.document_loaders.roam import RoamLoader
 from langchain.document_loaders.rtf import UnstructuredRTFLoader
 from langchain.document_loaders.s3_directory import S3DirectoryLoader
@@ -123,6 +124,7 @@ __all__ = [
     "GitLoader",
     "GitbookLoader",
     "GoogleApiClient",
+    "RedditPostsLoader",
     "GoogleApiYoutubeLoader",
     "GoogleDriveLoader",
     "GutenbergLoader",
