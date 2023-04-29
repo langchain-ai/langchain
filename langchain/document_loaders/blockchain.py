79e9bcd79438b713faea8b7359e8a54c150ef358
Jon Saginaw <saginawj@users.noreply.github.com>
2023-04-29 14:13:57 -0400

    get_all_nfts implemented and tested

--- langchain/document_loaders/blockchain.py
+++ langchain/document_loaders/blockchain.py
@@ -25,11 +25,15 @@ class BlockchainDocumentLoader(BaseLoader):
     If no BlockchainType is specified, the default is Ethereum mainnet.
 
     The Loader uses the Alchemy API to interact with the blockchain.
+    ALCHEMY_API_KEY environment variable must be set to use this loader.
 
     The API returns 100 NFTs per request and can be paginated using the
     startToken parameter.
 
-    ALCHEMY_API_KEY environment variable must be set to use this loader.
+    If get_all_tokens is set to True, the loader will get all tokens
+    on the contract.  Note that for contracts with a large number of tokens,
+    this may take a long time (e.g. 10k tokens is 100 requests).
+    Default value is false for this reason.
 
     Future versions of this loader can:
         - Support additional Alchemy APIs (e.g. getTransactions, etc.)
@@ -42,11 +46,13 @@ class BlockchainDocumentLoader(BaseLoader):
         blockchainType: BlockchainType = BlockchainType.ETH_MAINNET,
         api_key: str = "docs-demo",
         startToken: str = "",
+        get_all_tokens: bool = False,
     ):
         self.contract_address = contract_address
         self.blockchainType = blockchainType.value
         self.api_key = os.environ.get("ALCHEMY_API_KEY") or api_key
         self.startToken = startToken
+        self.get_all_tokens = get_all_tokens
 
         if not self.api_key:
             raise ValueError("Alchemy API key not provided.")
@@ -55,34 +61,77 @@ class BlockchainDocumentLoader(BaseLoader):
             raise ValueError(f"Invalid contract address {self.contract_address}")
 
     def load(self) -> List[Document]:
-        url = (
-            f"https://{self.blockchainType}.g.alchemy.com/nft/v2/"
-            f"{self.api_key}/getNFTsForCollection?withMetadata="
-            f"True&contractAddress={self.contract_address}"
-            f"&startToken={self.startToken}"
-        )
+        result = []
 
-        response = requests.get(url)
+        current_start_token = self.startToken
 
-        if response.status_code != 200:
-            raise ValueError(f"Request failed with status code {response.status_code}")
+        while True:
+            url = (
+                f"https://{self.blockchainType}.g.alchemy.com/nft/v2/"
+                f"{self.api_key}/getNFTsForCollection?withMetadata="
+                f"True&contractAddress={self.contract_address}"
+                f"&startToken={current_start_token}"
+            )
 
-        items = response.json()["nfts"]
+            response = requests.get(url)
 
-        if not (items):
+            if response.status_code != 200:
+                raise ValueError(
+                    f"Request failed with status code {response.status_code}"
+                )
+
+            items = response.json()["nfts"]
+
+            if not items:
+                break
+
+            for item in items:
+                content = str(item)
+                tokenId = item["id"]["tokenId"]
+                metadata = {
+                    "source": self.contract_address,
+                    "blockchain": self.blockchainType,
+                    "tokenId": tokenId,
+                }
+                result.append(Document(page_content=content, metadata=metadata))
+
+            if not self.get_all_tokens:
+                break
+
+            current_start_token = self._get_next_tokenId(result[-1].metadata["tokenId"])
+
+        if not result:
             raise ValueError(
                 f"No NFTs found for contract address {self.contract_address}"
             )
 
-        result = []
-
-        for item in items:
-            content = str(item)
-            tokenId = item["id"]["tokenId"]
-            metadata = {
-                "source": self.contract_address,
-                "blockchain": self.blockchainType,
-                "tokenId": tokenId,
-            }
-            result.append(Document(page_content=content, metadata=metadata))
         return result
+
+    def _get_next_tokenId(self, tokenId: str) -> str:
+        value_type = self._detect_value_type(tokenId)
+
+        if value_type == "hex_0x":
+            value_int = int(tokenId, 16)
+        elif value_type == "hex_0xbf":
+            value_int = int(tokenId[2:], 16)
+        else:
+            value_int = int(tokenId)
+
+        result = value_int + 1
+
+        if value_type == "hex_0x":
+            return "0x" + format(result, "0" + str(len(tokenId) - 2) + "x")
+        elif value_type == "hex_0xbf":
+            return "0xbf" + format(result, "0" + str(len(tokenId) - 4) + "x")
+        else:
+            return str(result)
+
+    def _detect_value_type(self, tokenId: str) -> str:
+        if isinstance(tokenId, int):
+            return "int"
+        elif tokenId.startswith("0x"):
+            return "hex_0x"
+        elif tokenId.startswith("0xbf"):
+            return "hex_0xbf"
+        else:
+            return "hex_0xbf"
--- tests/integration_tests/document_loaders/test_blockchain.py
+++ tests/integration_tests/document_loaders/test_blockchain.py
@@ -26,7 +26,7 @@ def test_get_nfts_with_pagination() -> None:
     contract_address = (
         "0x1a92f7381b9f03921564a437210bb9396471050c"  # CoolCats contract address
     )
-    startToken = "20"
+    startToken = "0x0000000000000000000000000000000000000000000000000000000000000077"
 
     result = BlockchainDocumentLoader(
         contract_address,
@@ -62,3 +62,33 @@ def test_get_nfts_invalid_contract() -> None:
         str(error_NoNfts.value)
         == "No NFTs found for contract address " + contract_address
     )
+
+
+@pytest.mark.skipif(not alchemyKeySet, reason="Alchemy API key not provided.")
+def test_get_all() -> None:
+    contract_address = (
+        "0x448676ffCd0aDf2D85C1f0565e8dde6924A9A7D9"  # Polygon contract address
+    )
+    result = BlockchainDocumentLoader(
+        contract_address=contract_address,
+        blockchainType=BlockchainType.POLYGON_MAINNET,
+        api_key=os.environ["ALCHEMY_API_KEY"],
+        startToken="100",
+        get_all_tokens=True,
+    ).load()
+
+    assert len(result) > 0, "No NFTs returned"
+    
+@pytest.mark.skip(reason="This test calls the API 100 times")
+def test_get_all_10k() -> None:
+    contract_address = (
+        "0x1a92f7381b9f03921564a437210bb9396471050c"  # Cool Cats contract address
+    )
+    result = BlockchainDocumentLoader(
+        contract_address=contract_address,
+        blockchainType=BlockchainType.ETH_MAINNET,
+        api_key=os.environ["ALCHEMY_API_KEY"],
+        get_all_tokens=True,
+    ).load()
+
+    assert len(result) ==9964, "Incorrect number of NFTs returned"
