af5e719b5149e76a9b95548b50f3018b50bf1e74
Pablo-Olmos <pablolmos0@gmail.com>
2023-05-01 00:34:47 +0200

    add unit test for list output parser

---
+++ tests/unit_tests/output_parsers/test_list_parser.py
@@ -0,0 +1,12 @@
+from langchain.output_parsers.list import CommaSeparatedListOutputParser
+
+def test_single_item():
+    """Test that a string with a single item is parsed to a list with that item."""
+    parser = CommaSeparatedListOutputParser()
+    assert parser.parse("foo") == ["foo"]
+
+
+def test_multiple_items():
+    """Test that a string with multiple comma-separated items is parsed to a list."""
+    parser = CommaSeparatedListOutputParser()
+    assert parser.parse("foo, bar, baz") == ["foo", "bar", "baz"]
