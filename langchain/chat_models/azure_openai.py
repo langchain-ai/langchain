00c95e874fe1666362b7ef416db7237f92335d4d
Rob Kopel <R0bk@users.noreply.github.com>
2023-05-01 13:30:45 +1000

    Added error to highlight content filter

--- langchain/chat_models/azure_openai.py
+++ langchain/chat_models/azure_openai.py
@@ -110,3 +110,16 @@ class AzureChatOpenAI(ChatOpenAI):
             **super()._default_params,
             "engine": self.deployment_name,
         }
+
+    def _create_chat_result(self, response: Mapping[str, Any]) -> ChatResult:
+        generations = []
+        print(response)
+        for res in response["choices"]:
+            if "finish_reason" in res and res["finish_reason"] == "content_filter":
+                raise ValueError("Azure has not provided the response due to a content"
+                                 " filter being triggered")
+            message = _convert_dict_to_message(res["message"])
+            gen = ChatGeneration(message=message)
+            generations.append(gen)
+        llm_output = {"token_usage": response["usage"], "model_name": self.model_name}
+        return ChatResult(generations=generations, llm_output=llm_output)
