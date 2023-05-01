d8a94a5c488a0fbdb7ec0912a38f126ea7924f82
Harrison Chase <hw.chase.17@gmail.com>
2023-05-01 15:44:27 -0700

    Update langchain/embeddings/openai.py

--- langchain/embeddings/openai.py
+++ langchain/embeddings/openai.py
@@ -107,9 +107,8 @@ class OpenAIEmbeddings(BaseModel, Embeddings):
     model: str = "text-embedding-ada-002"
     deployment: str = model  # to support Azure OpenAI Service custom deployment names
     openai_api_version: str = "2022-12-01"
-    openai_api_base: Optional[
-        str
-    ] = None  # to support Azure OpenAI Service custom endpoints
+    # to support Azure OpenAI Service custom endpoints
+    openai_api_base: Optional[str] = None  
     openai_api_type: Optional[
         str
     ] = None  # to support Azure OpenAI Service custom endpoints
