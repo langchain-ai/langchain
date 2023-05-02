4bf8354dd5d1c9da863ce51d635c5900705abe94
Mike Wang <62768671+skcoirz@users.noreply.github.com>
2023-05-01 20:39:17 -0700

    [simple] add ddg-search to __init__ for easier loading (#3933)
    the same as other tools

--- langchain/utilities/__init__.py
+++ langchain/utilities/__init__.py
@@ -5,6 +5,7 @@ from langchain.utilities.arxiv import ArxivAPIWrapper
 from langchain.utilities.awslambda import LambdaWrapper
 from langchain.utilities.bash import BashProcess
 from langchain.utilities.bing_search import BingSearchAPIWrapper
+from langchain.utilities.duckduckgo_search import DuckDuckGoSearchAPIWrapper
 from langchain.utilities.google_places_api import GooglePlacesAPIWrapper
 from langchain.utilities.google_search import GoogleSearchAPIWrapper
 from langchain.utilities.google_serper import GoogleSerperAPIWrapper
@@ -21,6 +22,7 @@ __all__ = [
     "ArxivAPIWrapper",
     "BashProcess",
     "TextRequestsWrapper",
+    "DuckDuckGoSearchAPIWrapper",
     "GoogleSearchAPIWrapper",
     "GoogleSerperAPIWrapper",
     "GooglePlacesAPIWrapper",
