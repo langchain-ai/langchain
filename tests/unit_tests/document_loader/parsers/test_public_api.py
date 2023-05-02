0af9990954e6b74eae249f727239039f3df2583b
Eugene Yurtsev <eyurtsev@gmail.com>
2023-05-02 11:43:24 -0400

    UPdate

--- tests/unit_tests/document_loader/parsers/test_public_api.py
+++ tests/unit_tests/document_loader/parsers/test_public_api.py
@@ -1,8 +1,6 @@
 from langchain.document_loaders.parsers import __all__
 
 
-def test_public_api():
-    """Simple test to verify that the public API wasn't broken.."""
-    assert __all__ == [
-        "BaseBlobParser",
-    ]
+def test_public_api() -> None:
+    """Simple test to verify that the public API wasn't broken."""
+    assert __all__ == ["BaseBlobParser"]
