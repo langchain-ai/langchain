fb6e1eda5879002a4bcb250ffe8e059ab473463e
Mike W <62768671+skcoirz@users.noreply.github.com>
2023-05-03 12:04:49 -0700

    added test for pandas agent

---
+++ tests/integration_tests/agent/test_pandas_agent.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel
+from langchain.agents import create_pandas_dataframe_agent
+from langchain.agents.agent import AgentExecutor
+
+from langchain.llms import OpenAI
+from pandas import DataFrame
+import numpy as np
+import pytest
+import re
+
+class TestData(BaseModel):
+    df: DataFrame
+    dim: int
+    class Config:
+        arbitrary_types_allowed = True
+
+@pytest.fixture(scope="function")
+def data() -> TestData:
+    dim = 4
+    random_data = np.random.rand(dim, dim)
+    df = DataFrame(random_data, columns=['name', 'age', 'food', 'sport'])
+    return TestData(df=df, dim=4)
+
+class TestPandasAgent():
+    def test_pandas_agent_creation(self, data):
+        agent = create_pandas_dataframe_agent(OpenAI(temperature=0), data.df)
+        assert isinstance(agent, AgentExecutor)
+
+    def test_data_reading(self, data):
+        agent = create_pandas_dataframe_agent(OpenAI(), data.df)
+        assert isinstance(agent, AgentExecutor)
+        response = agent.run("how many rows in df? Give me a number.")
+        result = re.search(rf".*({data.dim}).*", response)
+        assert result.group(1) is not None
+
+
