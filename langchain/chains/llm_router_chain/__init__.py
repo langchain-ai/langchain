d2bb73bb5033bcbc8cd95d49df49aa542ab6b930
KNarayan <schoolhouse@Krishnans-Mac-mini.local>
2023-05-03 00:14:23 -0700

    #3555 addressed package desccription updates for vector store externalization

--- langchain/chains/llm_router_chain/__init__.py
+++ langchain/chains/llm_router_chain/__init__.py
@@ -1,1 +1,1 @@
-"""Chain pipeline that models a gateway pattern for selecting the most relevant chain based on a vector collection"""
+"""Chain pipeline that models a gateway pattern for selecting the most relevant chain based on a function parameter passed by the caller"""
