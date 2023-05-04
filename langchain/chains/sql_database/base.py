b9d8f744520db52027aaa52b577e857ff88abca7
吴迪奥 <wudiao@51kik.com>
2023-05-04 17:14:06 +0800

    fix bug of no prompt in SQLDatabaseChain

--- langchain/chains/sql_database/base.py
+++ langchain/chains/sql_database/base.py
@@ -141,7 +141,7 @@ class SQLDatabaseChain(Chain):
     ) -> SQLDatabaseChain:
         prompt = prompt or SQL_PROMPTS.get(db.dialect, PROMPT)
         llm_chain = LLMChain(llm=llm, prompt=prompt)
-        return cls(llm_chain=llm_chain, database=db, **kwargs)
+        return cls(llm_chain=llm_chain,prompt=llm_chain.prompt, database=db, **kwargs)
 
 
 class SQLDatabaseSequentialChain(Chain):
