51cd2efe0386f88c88806b8bf81a77cf67367157
Serper <roger@serper.dev>
2023-05-04 02:31:09 +0100

    google-serper async support + full json result + Google images, places and news support

--- tests/integration_tests/utilities/test_googleserper_api.py
+++ tests/integration_tests/utilities/test_googleserper_api.py
@@ -1,9 +1,29 @@
 """Integration test for Serper.dev's Google Search API Wrapper."""
 from langchain.utilities.google_serper import GoogleSerperAPIWrapper
-
+import pytest
 
 def test_call() -> None:
     """Test that call gives the correct answer."""
     search = GoogleSerperAPIWrapper()
     output = search.run("What was Obama's first name?")
     assert "Barack Hussein Obama II" in output
+
+async def test_results() -> None:
+    """Test that call gives the correct answer."""
+    search = GoogleSerperAPIWrapper()
+    output = search.results("What was Obama's first name?")
+    assert "Barack Hussein Obama II" in output.get('answerBox').get('answer')
+
+@pytest.mark.asyncio
+async def test_async_call() -> None:
+    """Test that call gives the correct answer."""
+    search = GoogleSerperAPIWrapper()
+    output = await search.arun("What was Obama's first name?")
+    assert "Barack Hussein Obama II" in output
+
+@pytest.mark.asyncio
+async def test_async_results() -> None:
+    """Test that call gives the correct answer."""
+    search = GoogleSerperAPIWrapper()
+    output = await search.aresults("What was Obama's first name?")
+    assert "Barack Hussein Obama II" in output.get('answerBox').get('answer')
