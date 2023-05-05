7e9bc9c8082fe505b33b6e267ed6935faca3ba5a
leo-gan <leo.gan.57@gmail.com>
2023-05-05 13:27:15 -0700

    addressed PR comment

--- langchain/document_loaders/__init__.py
+++ langchain/document_loaders/__init__.py
@@ -121,7 +121,6 @@ __all__ = [
     "BlockchainDocumentLoader",
     "CSVLoader",
     "ChatGPTLoader",
-    "ChatGPTLoader",
     "CoNLLULoader",
     "CollegeConfidentialLoader",
     "ConfluenceLoader",
