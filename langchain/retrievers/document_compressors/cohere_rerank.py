1f8809dcbd86c6bf1fae450d59c3c02c66c97e4c
Abhinav Verma <abhinav_win12@yahoo.co.in>
2023-05-05 19:38:54 +0530

    Update cohere_rerank.py
    Change type of client to Any

--- langchain/retrievers/document_compressors/cohere_rerank.py
+++ langchain/retrievers/document_compressors/cohere_rerank.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Dict, Sequence
+from typing import TYPE_CHECKING, Dict, Sequence,Any
 
 from pydantic import root_validator
 
@@ -13,7 +13,7 @@ if TYPE_CHECKING:
 
 
 class CohereRerank(BaseDocumentCompressor):
-    client: Client
+    client: Any
     top_n: int = 3
     model: str = "rerank-english-v2.0"
 
