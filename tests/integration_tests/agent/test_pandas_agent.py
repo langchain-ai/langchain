8af19881103b4311837c5335f2a5096c5051a86b
Mike W <62768671+skcoirz@users.noreply.github.com>
2023-05-05 09:51:18 -0700

    use df directly as comment suggested

--- tests/integration_tests/agent/test_pandas_agent.py
+++ tests/integration_tests/agent/test_pandas_agent.py
@@ -3,38 +3,28 @@ import re
 import numpy as np
 import pytest
 from pandas import DataFrame
-from pydantic import BaseModel
 
 from langchain.agents import create_pandas_dataframe_agent
 from langchain.agents.agent import AgentExecutor
 from langchain.llms import OpenAI
 
 
-class TestData(BaseModel):
-    df: DataFrame
-    dim: int
-
-    class Config:
-        arbitrary_types_allowed = True
-
-
 @pytest.fixture(scope="function")
-def data() -> TestData:
-    dim = 4
-    random_data = np.random.rand(dim, dim)
+def data() -> DataFrame:
+    random_data = np.random.rand(4, 4)
     df = DataFrame(random_data, columns=["name", "age", "food", "sport"])
-    return TestData(df=df, dim=4)
+    return df
 
 
-def test_pandas_agent_creation(data: TestData) -> None:
-    agent = create_pandas_dataframe_agent(OpenAI(temperature=0), data.df)
+def test_pandas_agent_creation(df: DataFrame) -> None:
+    agent = create_pandas_dataframe_agent(OpenAI(temperature=0), df)
     assert isinstance(agent, AgentExecutor)
 
 
-def test_data_reading(data: TestData) -> None:
-    agent = create_pandas_dataframe_agent(OpenAI(temperature=0), data.df)
+def test_data_reading(df: DataFrame) -> None:
+    agent = create_pandas_dataframe_agent(OpenAI(temperature=0), df)
     assert isinstance(agent, AgentExecutor)
     response = agent.run("how many rows in df? Give me a number.")
-    result = re.search(rf".*({data.dim}).*", response)
+    result = re.search(rf".*({df.shape[0]}).*", response)
     assert result is not None
     assert result.group(1) is not None
