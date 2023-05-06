99b69e2d5f70a9f6bdbd9de0119ccbcd96d82fe7
vowelparrot <130414180+vowelparrot@users.noreply.github.com>
2023-05-05 17:37:37 -0700

    Add v2 docker compose file + command

--- langchain/server.py
+++ langchain/server.py
@@ -1,12 +1,23 @@
 """Script to run langchain-server locally using docker-compose."""
+import argparse
 import shutil
 import subprocess
 from pathlib import Path
 
 
+def parse_args() -> bool:
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--v2", action="store_true")
+    return parser.parse_args().v2
+
+
 def main() -> None:
     """Run the langchain server locally."""
-    p = Path(__file__).absolute().parent / "docker-compose.yaml"
+    is_v2 = parse_args()
+    filename = "docker-compose-v2.yaml" if is_v2 else "docker-compose.yaml"
+    p = Path(__file__).absolute().parent / filename
+    print(p)
 
     if shutil.which("docker-compose") is None:
         docker_compose_command = ["docker", "compose"]
