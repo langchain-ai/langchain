d09bb4a037993ac47686f9eb83df0683ef681652
Jacob Valdez <jacob.valdez@limboid.ai>
2023-05-07 16:11:15 -0500

    Fixed indent typo

--- langchain/output_parsers/chained.py
+++ langchain/output_parsers/chained.py
@@ -13,23 +13,20 @@ class ChainedOutputParser(BaseOutputParser[T]):
 
     parsers: list[BaseOutputParser]
 
+    def parse(self, text: str) -> T:
+        """Parse the output of an LLM call by chaining multiple parsers together."""
+        value = text
+        for parser in self.parsers:
+            value = parser.parse(value)
+        return value
 
-def parse(self, text: str) -> T:
-    """Parse the output of an LLM call by chaining multiple parsers together."""
-    value = text
-    for parser in self.parsers:
-        value = parser.parse(value)
-    return value
+    def parse_with_prompt(self, completion: str, prompt: PromptValue) -> Any:
+        """Parse the output of an LLM call with a given prompt by chaining multiple parsers together."""
+        value = completion
+        for parser in self.parsers:
+            value = parser.parse_with_prompt(value, prompt)
+        return value
 
-
-def parse_with_prompt(self, completion: str, prompt: PromptValue) -> Any:
-    """Parse the output of an LLM call with a given prompt by chaining multiple parsers together."""
-    value = completion
-    for parser in self.parsers:
-        value = parser.parse_with_prompt(value, prompt)
-    return value
-
-
-def get_format_instructions(self) -> str:
-    """Get the formatting instructions for the first parser in the chain."""
-    return self.parsers[0].get_format_instructions()
+    def get_format_instructions(self) -> str:
+        """Get the formatting instructions for the first parser in the chain."""
+        return self.parsers[0].get_format_instructions()
