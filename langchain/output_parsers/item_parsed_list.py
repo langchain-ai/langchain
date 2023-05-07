1b523e0dd8f0fce29b788f5a12dde19076a8c146
Jacob Valdez <jacob.valdez@limboid.ai>
2023-05-07 15:54:57 -0500

    parametrized separator

--- langchain/output_parsers/item_parsed_list.py
+++ langchain/output_parsers/item_parsed_list.py
@@ -8,9 +8,10 @@ T = TypeVar("T")
 class ItemParsedListOutputParser(BaseOutputParser[list[T]], Generic[T]):
     item_parser: BaseOutputParser[T]
     item_name = "item"
+    separator = "\n"
 
     def parse(self, response: str) -> Iterable[T]:
-        items = response.split("\n")
+        items = response.split(separator)
         items = [item.strip() for item in items]
         if self.item_parser is None:
             return items
@@ -19,6 +20,6 @@ class ItemParsedListOutputParser(BaseOutputParser[list[T]], Generic[T]):
 
     def get_format_instructions(self) -> str:
         if self.item_parser is None:
-            return f"Write each {self.item_name} on a new line. Do not include any other text in your answer."
+            return f'Write each {self.item_name} separated by a single "{self.separator}". Do not include any other text in your answer.'
         else:
-            return f"Write each {self.item_name} on a new line. Each {self.item_name} should be formatted as follows:\n{self.item_parser.get_format_instructions()}"
+            return f'Write each {self.item_name} separated by a single "{self.separator}". Each {self.item_name} should be formatted as follows:\n{self.item_parser.get_format_instructions()}'
