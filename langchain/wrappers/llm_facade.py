24143107537d3e5b9fd3179aa33f9f28ec34eb0d
Jacob Valdez <jacob.valdez@limboid.ai>
2023-05-07 16:57:33 -0500

    LLMFacade arg typo

--- langchain/wrappers/llm_facade.py
+++ langchain/wrappers/llm_facade.py
@@ -28,7 +28,7 @@ class LLMFacade(LLM):
     @staticmethod
     def of(llm) -> LLMFacade:
         if isinstance(llm, BaseChatModel):
-            return LLMFacade(llm=llm)
+            return LLMFacade(chat_model=llm)
         elif isinstance(llm, BaseLanguageModel):
             return llm
         else:
