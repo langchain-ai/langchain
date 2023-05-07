34e936fbcb4b43638f7972b4fdbcf791d4c5cdf2
Jacob Valdez <jacob.valdez@limboid.ai>
2023-05-07 16:51:59 -0500

    Added tests

--- langchain/wrappers/chat_model_facade.py
+++ langchain/wrappers/chat_model_facade.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from typing import List, Optional
+from langchain.callbacks.manager import CallbackManagerForLLMRun
 
 from langchain.chat_models.base import BaseChatModel, SimpleChatModel
 from langchain.schema import BaseMessage
@@ -11,7 +12,12 @@ from langchain.utils import serialize_msgs
 class ChatModelFacade(SimpleChatModel):
     llm: BaseLanguageModel
 
-    def _call(self, messages: List[BaseMessage], stop: Optional[List[str]] = None) -> str:
+    def _call(
+        self,
+        messages: List[BaseMessage],
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+    ) -> str:
         if isinstance(self.llm, BaseChatModel):
             return self.llm(messages, stop=stop).content
         elif isinstance(self.llm, BaseLanguageModel):
@@ -26,7 +32,7 @@ class ChatModelFacade(SimpleChatModel):
         if isinstance(llm, BaseChatModel):
             return llm
         elif isinstance(llm, BaseLanguageModel):
-            return cls(llm)
+            return cls(llm=llm)
         else:
             raise ValueError(
                 f"Invalid llm type: {type(llm)}. Must be a chat model or language model."
