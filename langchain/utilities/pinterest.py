eb916bc9010100bf33a7999b031be69c0a44eb57
Hiroshi Murakami <piroz@users.noreply.github.com>
2023-05-10 06:14:55 +0900

    initialize sdk client

--- langchain/utilities/pinterest.py
+++ langchain/utilities/pinterest.py
@@ -1,5 +1,30 @@
 """Util that calls Pinterest REST API"""
+from typing import Any, Dict, List, Optional
+
 from pydantic import BaseModel
 
+from langchain.utils import get_from_dict_or_env
+
+
 class PinteresetAPIWrapper(BaseModel):
-    pass
+    @root_validator()
+    def validate_environment(cls, values: Dict) -> Dict:
+        """Validate that api key and python package exists in environment."""
+        pinterest_access_token = get_from_dict_or_env(
+            values, "pinterest_access_token", "PINTEREST_ACCESS_TOKEN"
+        )
+        values["pinterest_access_token"] = pinterest_access_token
+
+        try:
+            from pinterest.client import PinterestSDKClient
+
+        except ImportError:
+            raise ImportError(
+                "pinterest-python-sdk is not installed. "
+                "Please install it with `pip install pinterest-api-sdk`"
+            )
+
+        client = PinterestSDKClient.create_client_with_token(pinterest_access_token)
+        values["client"] = client
+
+        return values
