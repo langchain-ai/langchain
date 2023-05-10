d53f7f56168ed574b4e8e0570edb53ff63da7a40
Ankush Gola <ankush.gola@gmail.com>
2023-05-10 10:58:04 -0700

    cr

--- langchain/callbacks/manager.py
+++ langchain/callbacks/manager.py
@@ -33,7 +33,7 @@ tracing_callback_var: ContextVar[Optional[LangChainTracer]] = ContextVar(  # noq
     "tracing_callback", default=None
 )
 tracing_v2_callback_var: ContextVar[
-    Optional[LangChainTracer]
+    Optional[LangChainTracerV2]
 ] = ContextVar(  # noqa: E501
     "tracing_callback_v2", default=None
 )
@@ -732,9 +732,10 @@ def _configure(
         or tracer is not None
         or os.environ.get("LANGCHAIN_HANDLER") is not None
     )
-    tracerv2 = tracing_v2_callback_var.get()
-    tracingv2_enabled_ = (
-        os.environ.get("LANGCHAIN_TRACING_V2") is not None or tracer is not None
+
+    tracer_v2 = tracing_v2_callback_var.get()
+    tracing_v2_enabled_ = (
+        os.environ.get("LANGCHAIN_TRACING_V2") is not None or tracer_v2 is not None
     )
     tracer_session = os.environ.get("LANGCHAIN_SESSION")
     if tracer_session is None:
@@ -755,12 +756,12 @@ def _configure(
                 handler = LangChainTracer()
                 handler.load_session(tracer_session)
                 callback_manager.add_handler(handler, True)
-        if tracingv2_enabled_ and not any(
+        if tracing_v2_enabled_ and not any(
             isinstance(handler, LangChainTracerV2)
             for handler in callback_manager.handlers
         ):
-            if tracerv2:
-                callback_manager.add_handler(tracerv2, True)
+            if tracer_v2:
+                callback_manager.add_handler(tracer_v2, True)
             else:
                 handler = LangChainTracerV2()
                 handler.load_session(tracer_session)
