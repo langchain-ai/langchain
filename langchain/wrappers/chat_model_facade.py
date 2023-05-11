19289f0169aec217ad5e65713ec2742e653bb2a8
Jacob Valdez <jacobfv@msn.com>
2023-05-11 14:31:42 -0500

    save

--- langchain/wrappers/chat_model_facade.py
+++ langchain/wrappers/chat_model_facade.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 
-from typing import List, Optional
-from langchain.callbacks.manager import CallbackManagerForLLMRun
+from typing import Any, Coroutine, List, Optional
 
+from langchain.callbacks.manager import (
+    AsyncCallbackManagerForLLMRun,
+    CallbackManagerForLLMRun,
+)
 from langchain.chat_models.base import BaseChatModel, SimpleChatModel
-from langchain.schema import BaseMessage
 from langchain.llms.base import BaseLanguageModel
+from langchain.schema import BaseMessage, ChatResult
 from langchain.utils import serialize_msgs
 
 
@@ -37,3 +40,17 @@ class ChatModelFacade(SimpleChatModel):
             raise ValueError(
                 f"Invalid llm type: {type(llm)}. Must be a chat model or language model."
             )
+
+    def _agenerate(
+        self,
+        messages: List[BaseMessage],
+        stop: List[str] | None = None,
+        run_manager: AsyncCallbackManagerForLLMRun | None = None,
+    ) -> Coroutine[Any, Any, ChatResult]:
+        return super()._agenerate(messages, stop, run_manager)
+
+    # `_type` req'd to pass `test_all_subclasses_implement_unique_type` test: FAILED tests/unit_tests/output_parsers/test_base_output_parser.py::test_all_subclasses_implement_unique_type - AssertionError: Duplicate types: {<property object at 0x7f300b31a7f0>}
+
+    @property
+    def _type(self) -> str:
+        return "chat_model_facade"
