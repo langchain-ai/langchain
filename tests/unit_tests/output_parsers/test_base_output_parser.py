b32786f7c24738798a633273218d84f6ee2e493e
Dev 2049 <dev.dev2049@gmail.com>
2023-05-11 01:09:20 -0700

    fmt

--- tests/unit_tests/output_parsers/test_base_output_parser.py
+++ tests/unit_tests/output_parsers/test_base_output_parser.py
@@ -1,27 +1,33 @@
 """Test the BaseOutputParser class and its sub-classes."""
 from abc import ABC
-from typing import List, Set, Type, Optional
-from unittest.mock import MagicMock
+from typing import List, Optional, Set, Type
 
 import pytest
 
 from langchain.schema import BaseOutputParser
 
-_PARSERS_TO_SKIP = {"FakeOutputParser", "BaseOutputParser"}
 
-
-def non_abstract_subclasses(cls: Type[ABC], to_skip: Optional[Set]=None) -> List[Type]:
+def non_abstract_subclasses(
+    cls: Type[ABC], to_skip: Optional[Set] = None
+) -> List[Type]:
     """Recursively find all non-abstract subclasses of a class."""
+    _to_skip = to_skip or set()
     subclasses = []
     for subclass in cls.__subclasses__():
         if not getattr(subclass, "__abstractmethods__", None):
-            if subclass.__name__ not in to_skip:
+            if subclass.__name__ not in _to_skip:
                 subclasses.append(subclass)
-        subclasses.extend(non_abstract_subclasses(subclass, to_skip))
+        subclasses.extend(non_abstract_subclasses(subclass, to_skip=_to_skip))
     return subclasses
 
 
-@pytest.mark.parametrize("cls", non_abstract_subclasses(BaseOutputParser, _PARSERS_TO_SKIP))
+_PARSERS_TO_SKIP = {"FakeOutputParser", "BaseOutputParser"}
+_NON_ABSTRACT_PARSERS = non_abstract_subclasses(
+    BaseOutputParser, to_skip=_PARSERS_TO_SKIP
+)
+
+
+@pytest.mark.parametrize("cls", _NON_ABSTRACT_PARSERS)
 def test_subclass_implements_type(cls: Type[BaseOutputParser]) -> None:
     try:
         cls._type
@@ -31,7 +37,7 @@ def test_subclass_implements_type(cls: Type[BaseOutputParser]) -> None:
 
 def test_all_subclasses_implement_unique_type() -> None:
     types = []
-    for cls in non_abstract_subclasses(BaseOutputParser, _PARSERS_TO_SKIP):
+    for cls in _NON_ABSTRACT_PARSERS:
         try:
             types.append(cls._type)
         except NotImplementedError:
