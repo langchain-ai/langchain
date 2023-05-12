18a0f6f40934da7dfa80925637c7f923c9483cf5
UmerHA <40663591+UmerHA@users.noreply.github.com>
2023-05-12 16:55:42 +0200

    Added reflexion explainer notebook

---
+++ tests/unit_tests/agents/test_reflexion.py
@@ -0,0 +1,15 @@
+
+from langchain.agents.reflexion.base import ReflexionChain
+from langchain.llms import OpenAI
+    
+def text_max_trials() -> None:
+    pass
+
+def text_max_iterations_in_trials() -> None:
+    pass
+
+def text_duplicate_actions() -> None:
+    pass
+
+def text_reflection() -> None:
+    pass
