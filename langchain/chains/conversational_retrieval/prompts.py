ea59b85ceb0693b8f99a885dcbf0d46e1836e3a3
Grady Matthias Oktavian <gradyoktavian@gmail.com>
2023-05-14 07:40:05 +0700

    Update prompts.py
    Add chat history to QA Prompt

--- langchain/chains/conversational_retrieval/prompts.py
+++ langchain/chains/conversational_retrieval/prompts.py
@@ -9,12 +9,16 @@ Follow Up Input: {question}
 Standalone question:"""
 CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
 
-prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+prompt_template = """You will be given context and conversation history. Use the following pieces of context to answer the question at the end. Use the following pieces of conversation chat history to know what prior questions have been asked, and what answers have been given. You can refer to the chat history as needed. When trying to answer the question, act as a polite and empathic person. If you don't know the answer, just politely say that you don't know, don't try to make up an answer.
 
+Context:
 {context}
+
+
+Chat History: {chat_history}
 
 Question: {question}
 Helpful Answer:"""
 QA_PROMPT = PromptTemplate(
-    template=prompt_template, input_variables=["context", "question"]
+    template=prompt_template, input_variables=["context", "chat_history", "question"]
 )
