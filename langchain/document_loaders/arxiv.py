2a8e49ed0e47fddd3b9e2a1a0573d56b22123889
leo-gan <leo.gan.57@gmail.com>
2023-05-13 19:33:15 -0700

    refactored to `class BaseLoader(BaseRetriever)`. integr tests are OK

--- langchain/document_loaders/arxiv.py
+++ langchain/document_loaders/arxiv.py
@@ -14,7 +14,7 @@ class ArxivLoader(BaseLoader):
 
     def __init__(
         self,
-        query: str,
+        query: Optional[str] = None,
         load_max_docs: Optional[int] = 100,
         load_all_available_meta: Optional[bool] = False,
     ):
@@ -22,10 +22,11 @@ class ArxivLoader(BaseLoader):
         self.load_max_docs = load_max_docs
         self.load_all_available_meta = load_all_available_meta
 
-    def load(self) -> List[Document]:
+    def load(self, query: Optional[str] = None) -> List[Document]:
         arxiv_client = ArxivAPIWrapper(
             load_max_docs=self.load_max_docs,
             load_all_available_meta=self.load_all_available_meta,
         )
-        docs = arxiv_client.load(self.query)
+        query = query or self.query
+        docs = arxiv_client.load(query)
         return docs
