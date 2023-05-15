5679cb26ad8e4f31f3a909dc4834e22d6b07af95
Akinwande Komolafe <47945512+Sensei-akin@users.noreply.github.com>
2023-05-15 02:02:33 +0100

    Added Telegram Integration by Auth (#4110)
    Dear Reviewers,
    
    I am pleased to present my latest pull request that includes exciting
    new changes to the existing codebase. I have implemented a new feature
    that enables users to directly authenticate and extract data from
    specific Telegram channels. This functionality offers a significant
    advantage over the previous version, which required users to read from a
    file path. With the new implementation, users can easily connect and
    integrate with Telegram, facilitating more efficient and accurate data
    extraction.
    
    Moreover, I have added a novel method that collects messages and their
    related responses in the form of a message thread, resulting in more
    accurate information retrieval. The output of this method is a Langchain
    document that streamlines data presentation and analysis.
    
    Overall, these changes represent a significant improvement in our
    codebase's functionality, enabling our users to extract data more
    efficiently and accurately. With the option of directly authenticating
    and connecting to Telegram or reading from a file path, users have
    greater flexibility and can choose the most suitable method for their
    requirements.
    
    Thank you for your time and consideration.
    
    Best regards
    
    ---------
    
    Co-authored-by: Akinwande Komolafe <akhinoz@gmail.com>

--- langchain/document_loaders/__init__.py
+++ langchain/document_loaders/__init__.py
@@ -79,7 +79,10 @@ from langchain.document_loaders.slack_directory import SlackDirectoryLoader
 from langchain.document_loaders.spreedly import SpreedlyLoader
 from langchain.document_loaders.srt import SRTLoader
 from langchain.document_loaders.stripe import StripeLoader
-from langchain.document_loaders.telegram import TelegramChatLoader
+from langchain.document_loaders.telegram import (
+    TelegramChatApiLoader,
+    TelegramChatFileLoader,
+)
 from langchain.document_loaders.text import TextLoader
 from langchain.document_loaders.toml import TomlLoader
 from langchain.document_loaders.twitter import TwitterTweetLoader
@@ -176,9 +179,13 @@ __all__ = [
     "SeleniumURLLoader",
     "SitemapLoader",
     "SlackDirectoryLoader",
+
+    "TelegramChatFileLoader",
+    "TelegramChatApiLoader",
+
     "SpreedlyLoader",
     "StripeLoader",
-    "TelegramChatLoader",
+    
     "TextLoader",
     "TomlLoader",
     "TwitterTweetLoader",
