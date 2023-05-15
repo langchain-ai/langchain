bd41f0417a1827c465efc950c04fec31d3de9c9a
Dev 2049 <dev.dev2049@gmail.com>
2023-05-15 16:25:54 -0700

    nit

--- langchain/llms/google_palm.py
+++ langchain/llms/google_palm.py
@@ -2,7 +2,6 @@
 from __future__ import annotations
 
 import logging
-from functools import wraps
 from typing import Any, Callable, Dict, List, Optional
 
 from pydantic import BaseModel, root_validator
