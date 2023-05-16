5f83fd649a60e92e3d6d2848a0ef1994d3a5684e
Raduan77 <rfalshedivat@edu.hse.ru>
2023-05-16 20:28:15 +0200

    fix(document_loaders/telegram): fix pandas calls + add tests

--- tests/integration_tests/document_loaders/test_telegram.py
+++ tests/integration_tests/document_loaders/test_telegram.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 
-from langchain.document_loaders import TelegramChatFileLoader
+from langchain.document_loaders import TelegramChatFileLoader, TelegramChatApiLoader
 
 
 def test_telegram_chat_file_loader() -> None:
-    """Test TelegramChatLoader."""
+    """Test TelegramChatFileLoader."""
     file_path = Path(__file__).parent.parent / "examples/telegram.json"
     loader = TelegramChatFileLoader(str(file_path))
     docs = loader.load()
@@ -15,4 +15,18 @@ def test_telegram_chat_file_loader() -> None:
         "Henry on 2020-01-01T00:00:02: It's 2020...\n\n"
         "Henry on 2020-01-01T00:00:04: Fireworks!\n\n"
         "Grace ðŸ§¤ ðŸ\x8d’ on 2020-01-01T00:00:05: You're a minute late!\n\n"
+    )
+
+
+def test_telegram_channel_loader_parsing() -> None:
+    """Test TelegramChatApiLoader."""
+    file_path = Path(__file__).parent.parent / "examples/telegram_channel.json"
+    # if we don't provide any value, it will skip fetching from telegram and will check the parsing logic.
+    loader = TelegramChatApiLoader(file_path=file_path)
+    docs = loader.load()
+
+    assert len(docs) == 1
+    print(docs[0].page_content)
+    assert docs[0].page_content == (
+        "Hello, world!.\nLLMs are awesome! Langchain is great. Telegram is the best!."
     )
