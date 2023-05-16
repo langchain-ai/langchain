138dc9ec8edab56786f2cc7605b82f79ea81826a
Sébastien M. Popoff <sebastien.popoff@espci.fr>
2023-05-16 06:50:44 +0200

    use file_path instead of path

--- langchain/utilities/bibtex.py
+++ langchain/utilities/bibtex.py
@@ -1,7 +1,8 @@
 """Util that calls bibtexparser."""
 import logging
 import re
-from typing import Any, Dict, List, BinaryIO
+from pathlib import Path
+from typing import Any, Dict, List
 
 from pydantic import BaseModel, Extra, root_validator
 
@@ -105,7 +106,7 @@ class BibtexparserWrapper(BaseModel):
                     meta[field.capitalize()] = entry.get(field)
         return meta
 
-    def run(self, file: BinaryIO) -> str:
+    def run(self, file_path: str) -> str:
         """
         Load bibtex file using bibtexparser and get the article meta information.
         See https://bibtexparser.readthedocs.io/en/master/
@@ -113,10 +114,11 @@ class BibtexparserWrapper(BaseModel):
         """
         print("Running bibtexparser wrapper")
         try:
-            docs = [
-                self._meta_str(entry)
-                for entry in self.load_file(file).entries[: self.load_max_docs]
-            ]
+            with open(file_path) as file:
+                docs = [
+                    self._meta_str(entry)
+                    for entry in self.load_file(file).entries[: self.load_max_docs]
+                ]
             return (
                 "\n\n".join(docs)[: self.doc_content_chars_max]
                 if docs
@@ -125,7 +127,7 @@ class BibtexparserWrapper(BaseModel):
         except self.bibtexparser_exceptions as ex:
             return f"Bibtexparser exception: {ex}"
 
-    def load(self, file: str) -> List[Document]:
+    def load(self, file_path: str) -> List[Document]:
         """
         Load bibtex file using bibtexparser and get the article texts plus the
         article metadata.
@@ -146,29 +148,29 @@ class BibtexparserWrapper(BaseModel):
         try:
             docs: List[Document] = []
             pdf_regex = re.compile(self.pdf_pattern)
-            for entry in self.load_file(file).entries[: self.load_max_docs]:
-                print("meta")
-                print(self._get_metata(entry))
-                try:
-                    # regex is usefull for Zotero flavor bibtex files
-                    doc_file_names = pdf_regex.findall(entry.get("file", ""))
-                    print("doc_file_names")
-                    print(doc_file_names)
-                    text: str = ""
-                    for doc_file_name in doc_file_names:
-                        with fitz.open(doc_file_name) as doc_file:
-                            text += "\n".join(page.get_text() for page in doc_file)
-                    if not text:
-                        # if nothing retrieved, just use the abstract as content
-                        text: str = entry.get("abstract", "")
+            with open(file_path) as file:
+                for entry in self.load_file(file).entries[: self.load_max_docs]:
+                    try:
+                        # regex is usefull for Zotero flavor bibtex files
+                        doc_file_names = pdf_regex.findall(entry.get("file", ""))
+                        doc_file_paths = [
+                            Path(file_path).parent / name for name in doc_file_names
+                        ]
+                        text: str = ""
+                        for doc_file_name in doc_file_paths:
+                            with fitz.open(doc_file_name) as doc_file:
+                                text += "\n".join(page.get_text() for page in doc_file)
+                        if not text:
+                            # if nothing retrieved, just use the abstract as content
+                            text: str = entry.get("abstract", "")
 
-                    doc = Document(
-                        page_content=text[: self.doc_content_chars_max],
-                        metadata=self._get_metata(entry),
-                    )
-                    docs.append(doc)
-                except FileNotFoundError as f_ex:
-                    logger.debug(f_ex)
+                        doc = Document(
+                            page_content=text[: self.doc_content_chars_max],
+                            metadata=self._get_metata(entry),
+                        )
+                        docs.append(doc)
+                    except FileNotFoundError as f_ex:
+                        logger.debug(f_ex)
             return docs
         except self.bibtexparser_exceptions as ex:
             logger.debug("Error on bibtexparser: %s", ex)
