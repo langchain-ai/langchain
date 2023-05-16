47d58daa4631152958b4e54e550b29eb2abd6108
Dev 2049 <dev.dev2049@gmail.com>
2023-05-16 15:00:52 -0700

    imports

--- tests/unit_tests/document_loader/test_confluence.py
+++ tests/unit_tests/document_loader/test_confluence.py
@@ -14,10 +14,7 @@ def mock_confluence():  # type: ignore
         yield mock_confluence
 
 
-@pytest.mark.requires("atlassian-python-api")
-@pytest.mark.requires("html2text")
-@pytest.mark.requires("beautifulsoup4")
-@pytest.mark.requires("lxml")
+@pytest.mark.requires("atlassian", "bs4", "lxml")
 class TestConfluenceLoader:
     CONFLUENCE_URL = "https://example.atlassian.com/wiki"
     MOCK_USERNAME = "user@gmail.com"
