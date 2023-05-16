c0674448d4e9514672df1a114953a89a40d85c65
Eugene Yurtsev <eyurtsev@gmail.com>
2023-05-16 14:27:27 -0400

    q

--- tests/unit_tests/test_pytest_config.py
+++ tests/unit_tests/test_pytest_config.py
@@ -3,7 +3,7 @@ import pytest_socket
 import requests
 
 
-def test_socket_disabled():
+def test_socket_disabled() -> None:
     """This test should fail."""
     with pytest.raises(pytest_socket.SocketBlockedError):
         requests.get("https://www.example.com")
