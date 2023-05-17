7fbe260905db2a78393fe90fb774f814ee9ab3e5
German Martin <gmartin.py@gmail.com>
2023-05-17 02:45:06 -0300

    Moving configuration into initializer, updating docs.

--- langchain/document_loaders/trello.py
+++ langchain/document_loaders/trello.py
@@ -10,34 +10,41 @@ class TrelloLoader(BaseLoader):
     Args:
         api_key (str): Trello API key.
         api_token (str): Trello API token.
+        board_name (str): The name of the Trello board.
+        include_card_name (bool): Whether to include the name of the card in the document. Defaults to False.
+        include_comments (bool): Whether to include the comments on the card in the document. Defaults to False.
+        include_checklist (bool): Whether to include the checklist on the card in the document. Defaults to False.
+        card_filter (str, optional): Use "closed" / "open". Defaults to "all".
+        extra_metadata (tuple[str]): List of additional metadata fields to include as document metadata. Defaults to ["due_date", "labels", "list", "is_closed"].
+
     """
 
-    def __init__(self, api_key: str, api_token: str):
+    def __init__(
+        self, 
+        api_key: str, 
+        api_token: str,
+        board_name: str,
+        include_card_name: bool = True,
+        include_comments: bool = True,
+        include_checklist: bool = True,
+        card_filter: str = "all",
+        extra_metadata: tuple[str, ...] = ("due_date", "labels", "list", "is_closed"),
+    ):
         """Initialize Trello loader."""
         self.api_key = api_key
         self.api_token = api_token
+        self.board_name = board_name
+        self.include_card_name = include_card_name
+        self.include_comments = include_comments
+        self.include_checklist = include_checklist
+        self.extra_metadata = extra_metadata
+        self.card_filter = card_filter
 
-    def load(
-        self,
-        board_name: str,
-        card_filter: Optional[str] = "all",
-        include_card_name: Optional[bool] = True,
-        include_comments: Optional[bool] = True,
-        include_checklist: Optional[bool] = True,
-        extra_metadata: Optional[List[str]] = [
-            "due_date", "labels", "list", "is_closed"],
-    ) -> List[Document]:
+    def load(self) -> List[Document]:
         """Loads all cards from the specified Trello board.
         You can filter the cards, metadata and text included by using the optional parameters.
 
-        Args:
-            board_name (str): The name of the Trello board.
-            card_filter (str, optional): Use "closed" / "open". Defaults to "all".
-            include_card_name (bool, optional): Whether to include the name of the card in the document. Defaults to False.
-            include_comments (bool, optional): Whether to include the comments on the card in the document. Defaults to False.
-            include_checklist (bool, optional): Whether to include the checklist on the card in the document. Defaults to False.
-            extra_metadata (List[str], optional): List of additional metadata fields to include as document metadata. Defaults to ["due_date", "labels", "list", "is_closed"].
-        Returns:
+         Returns:
             A list of documents, one for each card in the board.
         """
 
@@ -61,33 +68,28 @@ class TrelloLoader(BaseLoader):
 
         # Find the board with the matching name
         board = next((b for b in client.list_boards()
-                     if b.name == board_name), None)
+                     if b.name == self.board_name), None)
         if board:
             board_id = board.id
         else:
-            raise ValueError(f"Board `{board_name}` not found.")
-        
-        # Get the lists in the board
-        lists = board.list_lists()
+            raise ValueError(f"Board `{self.board_name}` not found.")
 
         # Create a dictionary with the list IDs as keys and the list names as values
-        list_dict = {}
-        for list_item in lists:
-            list_dict[list_item.id] = list_item.name
+        list_dict = {list_item.id: list_item.name for list_item in board.list_lists()}
             
         # Get Cards on the board
         board = client.get_board(board_id)
-        cards = board.get_cards(card_filter=card_filter)
+        cards = board.get_cards(card_filter=self.card_filter)
         for card in cards:
             text_content = ""
-            if include_card_name:
+            if self.include_card_name:
                 text_content = card.name + "\n"
             description = card.description.strip()
             if description:
                 text_content += BeautifulSoup(card.description,
                                               "lxml").get_text()
 
-            if include_checklist:
+            if self.include_checklist:
                 # Get all the checklit items on the card
                 items = []
                 for checklist in card.checklists:
@@ -95,7 +97,7 @@ class TrelloLoader(BaseLoader):
                         [f"{item['name']}:{item['state']}" for item in checklist.items])
                     text_content += f"\n{checklist.name}\n" + "\n".join(items)
 
-            if include_comments:
+            if self.include_comments:
                 # Get all the comments on the card
                 comments = [BeautifulSoup(comment['data']['text'], "lxml").get_text()
                             for comment in card.comments]
@@ -109,14 +111,14 @@ class TrelloLoader(BaseLoader):
             }
 
             # Extra metadata fields. Card object is not subscriptable.
-            if "labels" in extra_metadata:
+            if "labels" in self.extra_metadata:
                 metadata["labels"] = [label.name for label in card.labels]
-            if "list" in extra_metadata:
+            if "list" in self.extra_metadata:
                 if card.list_id in list_dict:
                     metadata["list"] = list_dict[card.list_id]
-            if "is_close" in extra_metadata:
+            if "is_close" in self.extra_metadata:
                 metadata["is_closed"] = card.is_closed
-            if "due_date" in extra_metadata:
+            if "due_date" in self.extra_metadata:
                 metadata["due_date"] = card.due_date
 
             doc = Document(page_content=text_content, metadata=metadata)
