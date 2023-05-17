651483f906c2ca6aad1ec0c8a1411cb2bff783f1
Ali Mirlou <alimirlou@gmail.com>
2023-05-17 17:31:44 +0330

    Add no_avx2 argument to dependable_faiss_import

--- langchain/vectorstores/faiss.py
+++ langchain/vectorstores/faiss.py
@@ -1,6 +1,7 @@
 """Wrapper around FAISS vector database."""
 from __future__ import annotations
 
+import os
 import math
 import pickle
 import uuid
@@ -17,10 +18,24 @@ from langchain.vectorstores.base import VectorStore
 from langchain.vectorstores.utils import maximal_marginal_relevance
 
 
-def dependable_faiss_import() -> Any:
-    """Import faiss if available, otherwise raise error."""
+def dependable_faiss_import(no_avx2: Optional[bool] = None) -> Any:
+    """
+    Import faiss if available, otherwise raise error.
+    If FAISS_NO_AVX2 environment variable is set, it will be considered
+    to load FAISS with no AVX2 optimization.
+
+    Args:
+        no_avx2: Load FAISS strictly with no AVX2 optimization
+            so that the vectorstore is portable and compatible with other devices.
+    """
+    if no_avx2 is None and 'FAISS_NO_AVX2' in os.environ:
+        no_avx2 = bool(os.getenv('FAISS_NO_AVX2'))
+
     try:
-        import faiss
+        if no_avx2:
+            from faiss import swigfaiss as faiss
+        else:
+            import faiss
     except ImportError:
         raise ValueError(
             "Could not import faiss python package. "
