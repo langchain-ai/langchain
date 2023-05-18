21ec50009b10bac6c9e173b8b4dda1af669eb5cb
blob42 <contact@blob42.xyz>
2023-05-18 20:38:38 +0200

    move tests to unit tests (#4481)

---
+++ tests/unit_tests/document_loader/test_text_loader.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+import pytest
+
+from langchain.document_loaders import DirectoryLoader, TextLoader
+
+class TestTextLoader:
+
+    @pytest.mark.requires("chardet")
+    def test_load_directory(self) -> None:
+        """Test text loader."""
+        path = Path(__file__).parent.parent / "examples"
+        files = path.glob("**/*.txt")
+        loader = DirectoryLoader(str(path), glob="**/*.txt", loader_cls=TextLoader)
+        loader_detect_encoding = DirectoryLoader(
+            str(path),
+            glob="**/*.txt",
+            loader_kwargs={"autodetect_encoding": True},
+            loader_cls=TextLoader,
+        )
+
+        with pytest.raises((UnicodeDecodeError, RuntimeError)):
+            loader.load()
+
+        docs = loader_detect_encoding.load()
+        assert len(docs) == len(list(files))
