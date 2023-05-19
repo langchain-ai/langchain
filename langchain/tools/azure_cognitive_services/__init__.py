b58930ecbe15df48b532a310c250ff486537287e
tiwe <weitian@microsoft.com>
2023-05-19 09:41:11 +0000

    add AzureCogsFormRecognizerTool

--- langchain/tools/azure_cognitive_services/__init__.py
+++ langchain/tools/azure_cognitive_services/__init__.py
@@ -1,5 +1,9 @@
 """Azure Cognitive Services Tools."""
 
 from langchain.tools.azure_cognitive_services.image_analysis import AzureCogsImageAnalysisTool
+from langchain.tools.azure_cognitive_services.form_recognizer import AzureCogsFormRecognizerTool
 
-__all__ = ["AzureCogsImageAnalysisTool"]
+__all__ = [
+    "AzureCogsImageAnalysisTool",
+    "AzureCogsFormRecognizerTool",
+]
