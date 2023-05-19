ebb8639896758f2f89feea61ed5e8e59053741b6
Jingchao Cao <cjingcha@amazon.com>
2023-05-18 21:53:57 -0700

    Fix type inconsistency issue in SageMaker response

--- langchain/llms/sagemaker_endpoint.py
+++ langchain/llms/sagemaker_endpoint.py
@@ -241,7 +241,7 @@ class SagemakerEndpoint(LLM):
         except Exception as e:
             raise ValueError(f"Error raised by inference endpoint: {e}")
 
-        text = self.content_handler.transform_output(response["Body"])
+        text = self.content_handler.transform_output(response["Body"].read())
         if stop is not None:
             # This is a bit hacky, but I can't figure out a better way to enforce
             # stop tokens when making calls to the sagemaker endpoint.
