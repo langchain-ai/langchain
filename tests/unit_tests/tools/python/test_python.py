59c1ae50c5bca91b4470273e58d96ae6c4503014
Deepak S V <svdeepak99@users.noreply.github.com>
2023-05-19 12:17:21 -0700

    Added 7 new test cases

--- tests/unit_tests/tools/python/test_python.py
+++ tests/unit_tests/tools/python/test_python.py
@@ -1,6 +1,7 @@
 """Test Python REPL Tools."""
 import sys
 
+import numpy as np
 import pytest
 
 from langchain.tools.python.tool import (
@@ -17,6 +18,18 @@ def test_python_repl_tool_single_input() -> None:
     assert int(tool.run("print(1 + 1)").strip()) == 2
 
 
+def test_python_repl_print():
+    program = """
+import numpy as np
+v1 = np.array([1, 2, 3])
+v2 = np.array([4, 5, 6])
+dot_product = np.dot(v1, v2)
+print("The dot product is {:d}.".format(dot_product))
+    """
+    tool = PythonREPLTool()
+    assert tool.run(program) == "The dot product is 32.\n"
+
+
 @pytest.mark.skipif(
     sys.version_info < (3, 9), reason="Requires python version >= 3.9 to run."
 )
@@ -25,6 +38,65 @@ def test_python_ast_repl_tool_single_input() -> None:
     tool = PythonAstREPLTool()
     assert tool.is_single_input
     assert tool.run("1 + 1") == 2
+
+
+def test_python_ast_repl_return():
+    program = """
+```
+import numpy as np
+v1 = np.array([1, 2, 3])
+v2 = np.array([4, 5, 6])
+dot_product = np.dot(v1, v2)
+int(dot_product)
+```
+    """
+    tool = PythonAstREPLTool()
+    assert tool.run(program) == 32
+
+
+def test_python_ast_repl_print():
+    program = """python
+string = "racecar"
+if string == string[::-1]:
+    print(string, "is a palindrome")
+else:
+    print(string, "is not a palindrome")"""
+    tool = PythonAstREPLTool()
+    assert tool.run(program) == "racecar is a palindrome\n"
+
+
+def test_python_ast_repl_raise_exception():
+    data = {"Name": ["John", "Alice"], "Age": [30, 25]}
+    program = """
+import pandas as pd
+df = pd.DataFrame(data)
+df['Gender']
+    """
+    tool = PythonAstREPLTool(locals={"data": data})
+    expected_outputs = (
+        "KeyError: 'Gender'",
+        "ModuleNotFoundError: No module named 'pandas'",
+    )
+    assert tool.run(program) in expected_outputs
+
+
+def test_python_ast_repl_one_line_print():
+    program = 'print("The square of {} is {:.2f}".format(3, 3**2))'
+    tool = PythonAstREPLTool()
+    assert tool.run(program) == "The square of 3 is 9.00\n"
+
+
+def test_python_ast_repl_one_line_return():
+    arr = np.array([1, 2, 3, 4, 5])
+    tool = PythonAstREPLTool(locals={"arr": arr})
+    program = "`(arr**2).sum()   # Returns sum of squares`"
+    assert tool.run(program) == 55
+
+
+def test_python_ast_repl_one_line_exception():
+    program = "[1, 2, 3][4]"
+    tool = PythonAstREPLTool()
+    assert tool.run(program) == "IndexError: list index out of range"
 
 
 def test_sanitize_input() -> None:
