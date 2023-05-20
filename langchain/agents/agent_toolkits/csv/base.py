e4fd220bffdf9a9808a4478e4b02104c86279459
Nicholas Liu <nicholas.j.liu@gmail.com>
2023-05-19 17:47:49 -0700

    Add multi-csv/df support in csv and dataframe agent toolkits

--- langchain/agents/agent_toolkits/csv/base.py
+++ langchain/agents/agent_toolkits/csv/base.py
@@ -8,13 +8,27 @@ from langchain.base_language import BaseLanguageModel
 
 def create_csv_agent(
     llm: BaseLanguageModel,
-    path: str,
+    path: Any,
     pandas_kwargs: Optional[dict] = None,
-    **kwargs: Any
+    **kwargs: Any,
 ) -> AgentExecutor:
     """Create csv agent by loading to a dataframe and using pandas agent."""
-    import pandas as pd
+    try:
+        import pandas as pd
+    except ImportError:
+        raise ValueError(
+            "pandas package not found, please install with `pip install pandas`"
+        )
 
     _kwargs = pandas_kwargs or {}
-    df = pd.read_csv(path, **_kwargs)
+    if isinstance(path, str):
+        df = pd.read_csv(path, **_kwargs)
+    elif isinstance(path, list):
+        df = []
+        for item in path:
+            if not isinstance(item, str):
+                raise ValueError(f"Expected str, got {type(path)}")
+            df.append(pd.read_csv(item, **_kwargs))
+    else:
+        raise ValueError(f"Expected str or list, got {type(path)}")
     return create_pandas_dataframe_agent(llm, df, **kwargs)
--- langchain/agents/agent_toolkits/pandas/base.py
+++ langchain/agents/agent_toolkits/pandas/base.py
@@ -3,9 +3,11 @@ from typing import Any, Dict, List, Optional
 
 from langchain.agents.agent import AgentExecutor
 from langchain.agents.agent_toolkits.pandas.prompt import (
+    MULTI_DF_PREFIX,
     PREFIX,
     SUFFIX_NO_DF,
     SUFFIX_WITH_DF,
+    SUFFIX_WITH_MULTI_DF,
 )
 from langchain.agents.mrkl.base import ZeroShotAgent
 from langchain.base_language import BaseLanguageModel
@@ -18,7 +20,7 @@ def create_pandas_dataframe_agent(
     llm: BaseLanguageModel,
     df: Any,
     callback_manager: Optional[BaseCallbackManager] = None,
-    prefix: str = PREFIX,
+    prefix: Optional[str] = None,
     suffix: Optional[str] = None,
     input_variables: Optional[List[str]] = None,
     verbose: bool = False,
@@ -38,29 +40,62 @@ def create_pandas_dataframe_agent(
             "pandas package not found, please install with `pip install pandas`"
         )
 
-    if not isinstance(df, pd.DataFrame):
-        raise ValueError(f"Expected pandas object, got {type(df)}")
+    num_dfs = 1
+    if isinstance(df, list):
+        for item in df:
+            if not isinstance(item, pd.DataFrame):
+                raise ValueError(f"Expected pandas object, got {type(df)}")
+        num_dfs = len(df)
+    else:
+        if not isinstance(df, pd.DataFrame):
+            raise ValueError(f"Expected pandas object, got {type(df)}")
     if include_df_in_prompt is not None and suffix is not None:
         raise ValueError("If suffix is specified, include_df_in_prompt should not be.")
+
     if suffix is not None:
         suffix_to_use = suffix
         if input_variables is None:
-            input_variables = ["df", "input", "agent_scratchpad"]
-    else:
-        if include_df_in_prompt:
-            suffix_to_use = SUFFIX_WITH_DF
-            input_variables = ["df", "input", "agent_scratchpad"]
+            input_variables = ["df_head", "input", "agent_scratchpad"]
+        elif num_dfs > 1:
+            input_variables = ["dfs_head", "input", "agent_scratchpad"]
+    elif include_df_in_prompt:
+        if num_dfs > 1:
+            suffix_to_use = SUFFIX_WITH_MULTI_DF
+            input_variables = ["dfs_head", "input", "agent_scratchpad"]
         else:
-            suffix_to_use = SUFFIX_NO_DF
-            input_variables = ["input", "agent_scratchpad"]
-    tools = [PythonAstREPLTool(locals={"df": df})]
+            suffix_to_use = SUFFIX_WITH_DF
+            input_variables = ["df_head", "input", "agent_scratchpad"]
+    else:
+        suffix_to_use = SUFFIX_NO_DF
+        input_variables = ["input", "agent_scratchpad"]
+
+    if num_dfs > 1:
+        input_variables += ["num_dfs"]
+
+    if prefix is None:
+        prefix = MULTI_DF_PREFIX if num_dfs > 1 else PREFIX
+
+    if num_dfs > 1:
+        df_locals = {}
+        for i, dataframe in enumerate(df):
+            df_locals[f"df{i + 1}"] = dataframe
+        tools = [PythonAstREPLTool(locals=df_locals)]
+    else:
+        tools = [PythonAstREPLTool(locals={"df": df})]
+
     prompt = ZeroShotAgent.create_prompt(
         tools, prefix=prefix, suffix=suffix_to_use, input_variables=input_variables
     )
-    if "df" in input_variables:
-        partial_prompt = prompt.partial(df=str(df.head().to_markdown()))
-    else:
-        partial_prompt = prompt
+
+    partial_prompt = prompt.partial()
+    if "df_head" in input_variables:
+        partial_prompt = partial_prompt.partial(df_head=str(df.head().to_markdown()))
+    elif "dfs_head" in input_variables:
+        dfs_head = "\n\n".join([d.head().to_markdown() for d in df])
+        partial_prompt = partial_prompt.partial(num_dfs=str(num_dfs), dfs_head=dfs_head)
+    if "num_dfs" in input_variables:
+        partial_prompt = partial_prompt.partial(num_dfs=str(num_dfs))
+
     llm_chain = LLMChain(
         llm=llm,
         prompt=partial_prompt,
