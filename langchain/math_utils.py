bdd257b63bc82c2b2a802f0e1c8e94bf37998817
hwaking <chzyy@hotmail.com>
2023-05-21 20:31:10 +0800

    Update math_utils.py
    add cosine_simirarity_v1 method which is 5 times faster than the cosine_similarity method.

--- langchain/math_utils.py
+++ langchain/math_utils.py
@@ -3,6 +3,7 @@ from typing import List, Union
 
 import numpy as np
 
+
 Matrix = Union[List[List[float]], List[np.ndarray], np.ndarray]
 
 
@@ -23,3 +24,22 @@ def cosine_similarity(X: Matrix, Y: Matrix) -> np.ndarray:
     similarity = np.dot(X, Y.T) / np.outer(X_norm, Y_norm)
     similarity[np.isnan(similarity) | np.isinf(similarity)] = 0.0
     return similarity
+
+def cosine_similarity_v1(X: Matrix, Y: Matrix) -> np.ndarray:
+    """Row-wise cosine similarity between two equal-width matrices using cosine_similarity from sklearn library which is more faster."""
+    from sklearn.metrics.pairwise import cosine_similarity
+    
+    if len(X) == 0 or len(Y) == 0:
+        return np.array([])
+    X = np.array(X)
+    Y = np.array(Y)
+    
+    if X.shape[1] != Y.shape[1]:
+        raise ValueError(
+            f"Number of columns in X and Y must be the same. X has shape {X.shape} "
+            f"and Y has shape {Y.shape}."
+        )
+    similarity = cosine_similarity(X, Y)
+    similarity[np.isnan(similarity) | np.isinf(similarity)] = 0.0
+    return similarity
+    
