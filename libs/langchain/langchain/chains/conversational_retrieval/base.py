8fd2aae880558e5d8a3bd92948532290146e0df7
Jiaping Zhang <jiaping.zhang@gemini.com>
2023-05-21 18:18:44 -0700

    fix with ruff

--- libs/langchain/langchain/chains/conversational_retrieval/base.py
+++ libs/langchain/langchain/chains/conversational_retrieval/base.py
@@ -25,8 +25,8 @@ from langchain.chains.base import Chain
 from langchain.chains.combine_documents.base import BaseCombineDocumentsChain
 from langchain.chains.combine_documents.stuff import StuffDocumentsChain
 from langchain.chains.conversational_retrieval.prompts import (
-    CONDENSE_QUESTION_PROMPT,
     CHAT_RETRIEVAL_QA_PROMPT,
+    CONDENSE_QUESTION_PROMPT,
 )
 from langchain.chains.llm import LLMChain
 from langchain.chains.question_answering import load_qa_chain
