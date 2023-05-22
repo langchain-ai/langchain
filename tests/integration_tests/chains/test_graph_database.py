1c89d6bfb53cb9a30e1b5cec7756d80a99e10800
Tomaz Bratanic <bratanic.tomaz@gmail.com>
2023-05-22 06:23:03 +0200

    Add Cypher query language generating chain (#4881)
    # Add Neo4j support and Cypher generating Chain
    
    The idea is to add support for querying Graph Databases that use Cypher
    query language like Neo4j.
    Since queries need to be constructed at query time, we use a similar
    approach as the text2sql, but instead of generating SQL statements, we
    generate Cypher statements based on the provided graph schema, which
    makes this a generic solution.
    
    # Todo
    
    - [x] Integration test
    - [x] Better exception handling 
    
    
    Fixes # (issue)
    https://github.com/hwchase17/langchain/issues/4625
    
    ---------
    
    Co-authored-by: Harrison Chase <hw.chase.17@gmail.com>

---
+++ tests/integration_tests/chains/test_graph_database.py
@@ -0,0 +1,53 @@
+"""Test Graph Database Chain."""
+import os
+
+from langchain.chains.graph_qa.cypher import GraphCypherQAChain
+from langchain.graphs import Neo4jGraph
+from langchain.llms.openai import OpenAI
+
+
+def test_connect_neo4j() -> None:
+    """Test that Neo4j database is correctly instantiated and connected."""
+    assert os.environ.get("NEO4J_URL") is not None
+    assert os.environ.get("NEO4J_USERNAME") is not None
+    assert os.environ.get("NEO4J_PASSWORD") is not None
+
+    graph = Neo4jGraph(
+        url=os.environ.get("NEO4J_URL"),
+        username=os.environ.get("NEO4J_USERNAME"),
+        password=os.environ.get("NEO4J_PASSWORD"),
+    )
+
+    output = graph.query(
+        """
+    RETURN "test" AS output
+    """
+    )
+    expected_output = [{"output": "test"}]
+    assert output == expected_output
+
+
+def test_cypher_generating_run() -> None:
+    """Test that Cypher statement is correctly generated and executed."""
+    assert os.environ.get("NEO4J_URL") is not None
+    assert os.environ.get("NEO4J_USERNAME") is not None
+    assert os.environ.get("NEO4J_PASSWORD") is not None
+
+    graph = Neo4jGraph(
+        url=os.environ.get("NEO4J_URL"),
+        username=os.environ.get("NEO4J_USERNAME"),
+        password=os.environ.get("NEO4J_PASSWORD"),
+    )
+    # Delete all nodes in the graph
+    graph.query("MATCH (n) DETACH DELETE n")
+    # Create two nodes and a relationship
+    graph.query(
+        "CREATE (a:Actor {name:'Bruce Willis'})-[:ACTED_IN]->(:Movie {title: 'Pulp Fiction'})"
+    )
+    # Refresh schema information
+    graph.refresh_schema()
+
+    chain = GraphCypherQAChain.from_llm(OpenAI(temperature=0), graph=graph)
+    output = chain.run("Who played in Pulp Fiction?")
+    expected_output = " Bruce Willis played in Pulp Fiction."
+    assert output == expected_output
