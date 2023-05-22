b5ff13490a674818c686563f2cdebbb738183398
Dev 2049 <dev.dev2049@gmail.com>
2023-05-22 09:47:40 -0700

    cr

--- langchain/math_utils.py
+++ langchain/math_utils.py
@@ -1,5 +1,5 @@
 """Math utils."""
-from typing import List, Union
+from typing import List, Optional, Tuple, Union
 
 import numpy as np
 
@@ -25,38 +25,32 @@ def cosine_similarity(X: Matrix, Y: Matrix) -> np.ndarray:
     return similarity
 
 
-def get_top_k_cosine_similarity(v1: Matrix, v2: Matrix, top_k=5, threshold_score=0.9) -> List[List[tuple]]:
-    """ Row-wise cosine similarity between two equal-width matrices and return the max top_k score and index, the score all greater than threshold_score
-    :param v1: matrix
-    :param v2: matrix
-    :param top_k: int, top k score
-    :param threshold_score: float
-    :return: list of index and score tuple, just like: [[(index, score),...], ...]
-    
-    Example:
-        ## test input
-        x = [[1, 2, 3, 4], [1, 2, 2, 2]]
-        y = [[1, 2, 3, 5], [1, 2, 9, 5], [2, 2, 3, 5]]
-        index_score_list = get_top_k_cosine_similarity(x, y, top_k=2, threshold_score=0.94)
-        print('index_score_list：', index_score_list)
-        ## output result
-        index_score_list： [[(0, 0.9939990885479664), (2, 0.9860132971832692)], [(2, 0.9415130835240085)]]
+def cosine_similarity_top_k(
+    X: Matrix,
+    Y: Matrix,
+    top_k: Optional[int] = 5,
+    score_threshold: Optional[float] = None,
+) -> Tuple[List[Tuple[int, int]], List[float]]:
+    """Row-wise cosine similarity with optional top-k and score threshold filtering.
+
+    Args:
+        X: Matrix.
+        Y: Matrix, same width as X.
+        top_k: Max number of results to return.
+        score_threshold: Minimum cosine similarity of results.
+
+    Returns:
+        Tuple of two lists. First contains two-tuples of indices (X_idx, Y_idx),
+            second contains corresponding cosine similarities.
     """
-    score_array = cosine_similarity(np.array(v1), np.array(v2))
-    index_score_list = []
-    for i in range(score_array.shape[0]):
-        # get the score which greater than threshold_score
-        row = score_array[i]
-        indices = np.argwhere(row > threshold_score).flatten()
-        values = row[indices]
-        # sort the value order by the score
-        sorted_order = np.argsort(values)[::-1]
-        # keep the max top k items
-        if len(sorted_order) > top_k:
-            sorted_order = sorted_order[:top_k]
-        sorted_indices = indices[sorted_order]
-        sorted_values = values[sorted_order]
-        # create tuple which contains the index and value score
-        tuples = list(zip(sorted_indices, sorted_values))
-        index_score_list.append(tuples)
-    return index_score_list
+    if len(X) == 0 or len(Y) == 0:
+        return [], []
+    score_array = cosine_similarity(X, Y)
+    sorted_idxs = score_array.flatten().argsort()[::-1]
+    top_k = top_k or len(sorted_idxs)
+    top_idxs = sorted_idxs[:top_k]
+    score_threshold = score_threshold or 0.0
+    top_idxs = top_idxs[score_array.flatten()[top_idxs] > score_threshold]
+    ret_idxs = [(x // score_array.shape[1], x % score_array.shape[1]) for x in top_idxs]
+    scores = score_array.flatten()[top_idxs].tolist()
+    return ret_idxs, scores
