02cca635efe38cb166d6aec25d0748c2d34d157f
Rick Braddy <rick.braddy@pharma.solutions>
2023-05-22 20:20:01 +0000

    Reformatted code with Black

--- langchain/agents/conversational_chat/output_parser.py
+++ langchain/agents/conversational_chat/output_parser.py
@@ -21,26 +21,30 @@ class ConvoOutputParser(AgentOutputParser):
             cleaned_output = text.strip()
 
             # Removal of control characters from the input text
-            cleaned_output = ''.join(ch for ch in cleaned_output if unicodedata.category(ch)[0]!="C")
+            cleaned_output = "".join(
+                ch for ch in cleaned_output if unicodedata.category(ch)[0] != "C"
+            )
 
-            '''
+            """
             The input text may contain one or more responses from the ChatGPT API.
             Each response is enclosed within a ```json and ``` pair.
             The following lines of code split the text into sections, each section containing one response.
-            '''
+            """
             sections = cleaned_output.split("```json")
 
-            '''
+            """
             Next, we remove any trailing "```" from each section.
             This gives us a list of cleaned_output strings, each of which should be a valid JSON string.
-            '''
-            cleaned_sections: List[str] = [section.rstrip("```") for section in sections if section.rstrip("```")]
+            """
+            cleaned_sections: List[str] = [
+                section.rstrip("```") for section in sections if section.rstrip("```")
+            ]
 
-            '''
+            """
             Now we attempt to parse each cleaned_output string as JSON.
             If successful, we append the parsed JSON object to the responses list.
             If not, we simply ignore that string and move to the next one.
-            '''
+            """
             responses = []
             for output in cleaned_sections:
                 try:
@@ -49,11 +53,11 @@ class ConvoOutputParser(AgentOutputParser):
                 except json.JSONDecodeError:
                     pass  # Continue to the next section
 
-            '''
+            """
             Once we have the list of responses, we process each response based on its action value.
             If the action is "Final Answer", we return an AgentFinish object.
             Otherwise, we return an AgentAction object.
-            '''
+            """
             agent_action = None
             for response in responses:
                 action, action_input = response["action"], response["action_input"]
@@ -71,15 +75,14 @@ class ConvoOutputParser(AgentOutputParser):
             raise OutputParserException("No suitable action found in LLM output")
 
         except Exception as e:
-            '''
+            """
             If any exception is raised during the above process,
             we catch it here and raise an OutputParserException instead.
             This is to ensure that any issues are clearly flagged as related to the parsing process.
-            '''
+            """
             raise OutputParserException(f"Could not parse LLM output: {text}") from e
 
     # This property method is required by the base class and simply returns the type of this parser
     @property
     def _type(self) -> str:
         return "conversational_chat"
-
