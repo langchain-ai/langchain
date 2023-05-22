6486e9ef1562f3ff6bff316e6a6f1c8da3834762
Rick Braddy <rick.braddy@pharma.solutions>
2023-05-22 19:49:12 +0000

    resolved lint issues and reverified parsing of code is correct

--- langchain/agents/conversational_chat/output_parser.py
+++ langchain/agents/conversational_chat/output_parser.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import json
-from typing import Union
+from typing import Union, List
 import unicodedata
 
 from langchain.agents import AgentOutputParser
@@ -21,28 +21,28 @@ class ConvoOutputParser(AgentOutputParser):
             cleaned_output = text.strip()
 
             # Removal of control characters from the input text
-            cleaned_output = ''.join(ch for ch in text if unicodedata.category(ch)[0]!="C")
+            cleaned_output = ''.join(ch for ch in cleaned_output if unicodedata.category(ch)[0]!="C")
 
             '''
-            The input text may contain one or more responses from the ChatGPT API. 
-            Each response is enclosed within a ```json and ``` pair. 
+            The input text may contain one or more responses from the ChatGPT API.
+            Each response is enclosed within a ```json and ``` pair.
             The following lines of code split the text into sections, each section containing one response.
             '''
             sections = cleaned_output.split("```json")
 
             '''
-            Next, we remove any trailing "```" from each section. 
+            Next, we remove any trailing "```" from each section.
             This gives us a list of cleaned_output strings, each of which should be a valid JSON string.
             '''
-            cleaned_output = [section.rstrip("```") for section in sections if section.rstrip("```")]
+            cleaned_sections: List[str] = [section.rstrip("```") for section in sections if section.rstrip("```")]
 
             '''
-            Now we attempt to parse each cleaned_output string as JSON. 
+            Now we attempt to parse each cleaned_output string as JSON.
             If successful, we append the parsed JSON object to the responses list.
             If not, we simply ignore that string and move to the next one.
             '''
             responses = []
-            for output in cleaned_output:
+            for output in cleaned_sections:
                 try:
                     response = json.loads(output)
                     responses.append(response)
@@ -50,21 +50,30 @@ class ConvoOutputParser(AgentOutputParser):
                     pass  # Continue to the next section
 
             '''
-            Once we have the list of responses, we process each response based on its action value. 
-            If the action is "Final Answer", we return an AgentFinish object. 
+            Once we have the list of responses, we process each response based on its action value.
+            If the action is "Final Answer", we return an AgentFinish object.
             Otherwise, we return an AgentAction object.
             '''
+            agent_action = None
             for response in responses:
                 action, action_input = response["action"], response["action_input"]
                 if action == "Final Answer":
                     return AgentFinish({"output": action_input}, text)
                 else:
-                    return AgentAction(action, action_input, text)
+                    agent_action = AgentAction(action, action_input, text)
+
+            if agent_action is not None:
+                return agent_action
+            else:
+                raise OutputParserException("No suitable action found in LLM output")
+
+            # If no suitable action is found, raise an exception
+            raise OutputParserException("No suitable action found in LLM output")
 
         except Exception as e:
             '''
-            If any exception is raised during the above process, 
-            we catch it here and raise an OutputParserException instead. 
+            If any exception is raised during the above process,
+            we catch it here and raise an OutputParserException instead.
             This is to ensure that any issues are clearly flagged as related to the parsing process.
             '''
             raise OutputParserException(f"Could not parse LLM output: {text}") from e
