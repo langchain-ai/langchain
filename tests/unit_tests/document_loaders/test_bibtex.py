3172eafba4f3aee3d23e313793c467f55a11e66a
Eugene Yurtsev <eyurtsev@gmail.com>
2023-05-23 12:12:05 -0400

    x

--- tests/unit_tests/document_loaders/test_bibtex.py
+++ tests/unit_tests/document_loaders/test_bibtex.py
@@ -1,27 +1,35 @@
 from pathlib import Path
 
 from langchain.document_loaders.bibtex import BibtexLoader
+import pytest
 
-BIBTEX_EXAMPLE_FILE = Path(__file__).parent.parent.parent / "examples" / "bibtex.bib"
+BIBTEX_EXAMPLE_FILE = (
+    Path(__file__).parent.parent.parent
+    / "integration_tests"
+    / "examples"
+    / "bibtex.bib"
+)
 
 
+# @pytest.mark.requires("pymupdf")
 def test_load_success() -> None:
     """Test that returns one document"""
     loader = BibtexLoader(file_path=str(BIBTEX_EXAMPLE_FILE), load_max_docs=2)
     docs = loader.load()
     assert len(docs) == 1
-    print(docs[0].metadata)
-    print(docs[0].page_content)
 
     for doc in docs:
         assert doc.page_content
         assert doc.metadata
         assert set(doc.metadata) == {
-            "ID",
-            "Published",
-            "Title",
-            "Publication",
-            "Authors",
-            "Summary",
-            "URL",
+            "id",
+            "published",
+            "title",
+            "publication",
+            "authors",
+            "summary",
+            "url",
+            "editor",
+            "booktitle",
+            "organization",
         }
