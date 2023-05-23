e72058f6373288b658b630bfbd19a8c7b745bc48
Daniel King <43149077+dakinggg@users.noreply.github.com>
2023-05-23 15:59:08 -0700

    Add MosaicML inference endpoints (#4607)
    # Add MosaicML inference endpoints
    This PR adds support in langchain for MosaicML inference endpoints. We
    both serve a select few open source models, and allow customers to
    deploy their own models using our inference service. Docs are here
    (https://docs.mosaicml.com/en/latest/inference.html), and sign up form
    is here (https://forms.mosaicml.com/demo?utm_source=langchain). I'm not
    intimately familiar with the details of langchain, or the contribution
    process, so please let me know if there is anything that needs fixing or
    this is the wrong way to submit a new integration, thanks!
    
    I'm also not sure what the procedure is for integration tests. I have
    tested locally with my api key.
    
    ## Who can review?
    @hwchase17
    
    ---------
    
    Co-authored-by: Harrison Chase <hw.chase.17@gmail.com>

--- langchain/embeddings/__init__.py
+++ langchain/embeddings/__init__.py
@@ -17,6 +17,7 @@ from langchain.embeddings.huggingface import (
 from langchain.embeddings.huggingface_hub import HuggingFaceHubEmbeddings
 from langchain.embeddings.jina import JinaEmbeddings
 from langchain.embeddings.llamacpp import LlamaCppEmbeddings
+from langchain.embeddings.mosaicml import MosaicMLInstructorEmbeddings
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.embeddings.sagemaker_endpoint import SagemakerEndpointEmbeddings
 from langchain.embeddings.self_hosted import SelfHostedEmbeddings
@@ -40,6 +41,7 @@ __all__ = [
     "TensorflowHubEmbeddings",
     "SagemakerEndpointEmbeddings",
     "HuggingFaceInstructEmbeddings",
+    "MosaicMLInstructorEmbeddings",
     "SelfHostedEmbeddings",
     "SelfHostedHuggingFaceEmbeddings",
     "SelfHostedHuggingFaceInstructEmbeddings",
