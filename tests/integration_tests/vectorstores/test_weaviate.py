7b288043a5c112a39db715408b14280f207d2776
Dev 2049 <dev.dev2049@gmail.com>
2023-05-22 18:18:05 -0700

    fix

--- tests/integration_tests/vectorstores/test_weaviate.py
+++ tests/integration_tests/vectorstores/test_weaviate.py
@@ -93,10 +93,15 @@ class TestWeaviate:
         )
         output = docsearch.similarity_search(
             "foo",
-            k=2,
+            k=1,
             additional=["certainty"],
         )
-        assert output == [Document(page_content="foo", metadata={"page": 0, "_additional": {"certainty": 0}})]
+        assert output == [
+            Document(
+                page_content="foo",
+                metadata={"page": 0, "_additional": {"certainty": 1}},
+            )
+        ]
 
     @pytest.mark.vcr(ignore_localhost=True)
     def test_similarity_search_with_uuids(
