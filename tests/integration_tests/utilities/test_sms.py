5245499a0d86e2b581b81047e2a12390b2aee6ab
Ted Martinez <tedma4@gmail.com>
2023-05-23 08:34:57 -0700

    feat: add twilio sms tool

---
+++ tests/integration_tests/utilities/test_sms.py
@@ -0,0 +1,9 @@
+"""Integration test for Sms."""
+from langchain.utilities.sms import Sms
+
+
+def test_call() -> None:
+    """Test that call runs."""
+    chain = Sms()
+    output = chain.run("Message", '+16162904619')
+    assert output
