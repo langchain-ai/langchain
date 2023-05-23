314e6273c868e263bc132136a80d9049f49190c4
vempaliakhil96 <vempaliakhil96@gmail.com>
2023-05-23 09:46:56 -0400

    refactor: :zap: Changed logic for `from_documents` to use `from_texts`

--- langchain/retrievers/tfidf.py
+++ langchain/retrievers/tfidf.py
@@ -25,6 +25,7 @@ class TFIDFRetriever(BaseRetriever, BaseModel):
         cls,
         texts: List[str],
         tfidf_params: Optional[Dict[str, Any]] = None,
+        metadatas: Optional[List[Dict]] = None,
         **kwargs: Any
     ) -> "TFIDFRetriever":
         from sklearn.feature_extraction.text import TfidfVectorizer
@@ -32,22 +33,18 @@ class TFIDFRetriever(BaseRetriever, BaseModel):
         tfidf_params = tfidf_params or {}
         vectorizer = TfidfVectorizer(**tfidf_params)
         tfidf_array = vectorizer.fit_transform(texts)
-        docs = [Document(page_content=t) for t in texts]
+        if metadatas is None:
+            metadatas = [dict()] * len(texts)
+        assert len(metadatas) == len(texts)
+        docs = [Document(page_content=t, metadata=m) for t, m in zip(texts, metadatas)]
         return cls(vectorizer=vectorizer, docs=docs, tfidf_array=tfidf_array, **kwargs)
 
     @classmethod
     def from_documents(
-        cls,
-        documents: List[Document],
-        tfidf_params: Optional[Dict[str, Any]] = None,
-        **kwargs: Any
+        cls, documents: List[Document], **kwargs: Any
     ) -> "TFIDFRetriever":
-        from sklearn.feature_extraction.text import TfidfVectorizer
-
-        tfidf_params = tfidf_params or {}
-        vectorizer = TfidfVectorizer(**tfidf_params)
-        tfidf_array = vectorizer.fit_transform([d.page_content for d in documents])
-        return cls(vectorizer=vectorizer, docs=documents, tfidf_array=tfidf_array, **kwargs)
+        metadatas, texts = zip(*[(d.metadata, d.page_content) for d in documents])
+        return cls.from_texts(texts, metadatas=metadatas)
 
     def get_relevant_documents(self, query: str) -> List[Document]:
         from sklearn.metrics.pairwise import cosine_similarity
