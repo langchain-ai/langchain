48cad651c177a40adc8caaf750b9805e15d77877
byhsu <byhsu@linkedin.com>
2023-05-24 00:30:21 -0700

    Allow readthedoc loader to pass custom html tag
    Signed-off-by: byhsu <byhsu@linkedin.com>

---
+++ tests/unit_tests/document_loaders/test_readthedoc.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+import pytest
+from pytest import raises
+from pytest_mock import MockerFixture
+
+from langchain.document_loaders.readthedocs import ReadTheDocsLoader
+
+
+@pytest.mark.requires("bs4")
+def test_main_id_main_content():
+    loader = ReadTheDocsLoader(
+        Path(__file__).parent / "test_docs" / "readthedocs" / "main_id_main_content"
+    )
+    documents = loader.load()
+    assert len(documents[0].page_content) != 0
+
+
+@pytest.mark.requires("bs4")
+def test_div_role_main():
+    loader = ReadTheDocsLoader(
+        Path(__file__).parent / "test_docs" / "readthedocs" / "div_role_main"
+    )
+    documents = loader.load()
+    assert len(documents[0].page_content) != 0
+
+
+@pytest.mark.requires("bs4")
+def test_custom():
+    loader = ReadTheDocsLoader(
+        Path(__file__).parent / "test_docs" / "readthedocs" / "custom",
+        custom_html_tag=("article", {"role": "main"}),
+    )
+    documents = loader.load()
+    assert len(documents[0].page_content) != 0
+
+
+@pytest.mark.requires("bs4")
+def test_empty():
+    loader = ReadTheDocsLoader(
+        Path(__file__).parent / "test_docs" / "readthedocs" / "custom",
+    )
+    documents = loader.load()
+    assert len(documents[0].page_content) == 0
