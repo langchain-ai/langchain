14dbb6f633a5fd97584015a15b83a35357f20e6d
tommasodelorenzo <tdelorenzo.ai@gmail.com>
2023-05-24 12:48:43 +0200

    get_based_model_name helper

--- langchain/callbacks/openai_info.py
+++ langchain/callbacks/openai_info.py
@@ -30,14 +30,18 @@ MODEL_COST_PER_1K_TOKENS = {
     "davinci-finetuned": 0.12,
 }
 
+def get_based_model_name(
+    model_name:str,
+) -> str:
+    if "ft-" in model_name:
+        model_name = f"{model_name.split(':')[0]}-finetuned"
+
+    return model_name
 
 def get_openai_token_cost_for_model(
     model_name: str, num_tokens: int, is_completion: bool = False
 ) -> float:
-    # handling finetuned models
-    if "ft-" in model_name:
-        model_name = f"{model_name.split(':')[0]}-finetuned"
-
+    model_name = get_based_model_name(model_name)
     suffix = "-completion" if is_completion and model_name.startswith("gpt-4") else ""
     model = model_name.lower() + suffix
     if model not in MODEL_COST_PER_1K_TOKENS:
@@ -91,7 +95,9 @@ class OpenAICallbackHandler(BaseCallbackHandler):
         token_usage = response.llm_output["token_usage"]
         completion_tokens = token_usage.get("completion_tokens", 0)
         prompt_tokens = token_usage.get("prompt_tokens", 0)
-        model_name = response.llm_output.get("model_name")
+        model_name = get_based_model_name(
+            response.llm_output.get("model_name")
+        )
         if model_name and model_name in MODEL_COST_PER_1K_TOKENS:
             completion_cost = get_openai_token_cost_for_model(
                 model_name, completion_tokens, is_completion=True
