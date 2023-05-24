734d7968df8a66e33f6a5650e8ba6836fb9c86d7
Xiangrui Meng <meng@databricks.com>
2023-05-23 21:18:36 -0700

    style

--- langchain/llms/databricks_endpoint.py
+++ langchain/llms/databricks_endpoint.py
@@ -133,85 +133,89 @@ def get_default_api_token() -> str:
 class DatabricksEndpoint(LLM):
     """LLM wrapper around a Databricks serving endpoint or a cluster driver proxy app.
     It supports two endpoint types:
-    - Serving endpoint (recommended for both production and development).
+
+    * **Serving endpoint** (recommended for both production and development).
       We assume that an LLM was registered and deployed to a serving endpoint.
       To wrap it as an LLM you must have "Can Query" permission to the endpoint.
-      Set `endpoint_name` accordingly and do not set `cluster_id` and
-      `cluster_driver_port`.
+      Set ``endpoint_name`` accordingly and do not set ``cluster_id`` and
+      ``cluster_driver_port``.
       The expected model signature is:
-      - inputs:
-        ```
-        [{"name": "prompt", "type": "string"},
-         {"name": "stop", "type": "list[string]"}]
-        ```
-      - outputs: `[{"type": "string"}]`
 
-    - Cluster driver proxy app (recommended for interactive development).
+      * inputs::
+
+          [{"name": "prompt", "type": "string"},
+           {"name": "stop", "type": "list[string]"}]
+
+      * outputs: ``[{"type": "string"}]``
+
+    * **Cluster driver proxy app** (recommended for interactive development).
       One can load an LLM on a Databricks interactive cluster and start a local HTTP
-      server on the driver node to serve the model at "/" using HTTP POST method with
-      JSON input/output.
-      Please use a port number between [3000, 8000] and let the server listen to the
-      driver IP address or simply `0.0.0.0` instead of localhost only.
+      server on the driver node to serve the model at ``/`` using HTTP POST method
+      with JSON input/output.
+      Please use a port number between ``[3000, 8000]`` and let the server listen to
+      the driver IP address or simply ``0.0.0.0`` instead of localhost only.
       To wrap it as an LLM you must have "Can Attach To" permission to the cluster.
-      Set `cluster_id` and `cluster_driver_port` and do not set `endpoint_name`.
+      Set ``cluster_id`` and ``cluster_driver_port`` and do not set ``endpoint_name``.
       The expected server schema (using JSON schema) is:
-      - inputs:
-        ```
-        {"type":"object",
-         "properties":{
-            "prompt":{"type":"string"},
-            "stop":{"type":"array","items":{"type":"string"}}},
-         "required":["prompt"]}`
-        ```
-      - outputs: `{"type":"string"}`
+
+      * inputs::
+
+          {"type": "object",
+           "properties": {
+              "prompt": {"type": "string"},
+              "stop": {"type": "array", "items": {"type": "string"}}},
+           "required": ["prompt"]}`
+
+      * outputs: ``{"type": "string"}``
 
     If the endpoint model signature is different or you want to set extra params,
     you can use `transform_input_fn` and `transform_output_fn` to apply necessary
-    transformations
-    before and after the query.
+    transformations before and after the query.
     """
 
     host: str = Field(default_factory=get_default_host)
     """Databricks workspace hostname.
     If not provided, the default value is determined by
-      - the ``DATABRICKS_HOST`` environment variable if present, or
-      - the hostname of the current Databricks workspace if running inside
-        a Databricks notebook attached to an interactive cluster in "single user"
-        or "no isolation shared" mode.
+
+    * the ``DATABRICKS_HOST`` environment variable if present, or
+    * the hostname of the current Databricks workspace if running inside
+      a Databricks notebook attached to an interactive cluster in "single user"
+      or "no isolation shared" mode.
     """
 
     api_token: str = Field(default_factory=get_default_api_token)
     """Databricks personal access token.
     If not provided, the default value is determined by
-      - the ``DATABRICKS_API_TOKEN`` environment variable if present, or
-      - an automatically generated temporary token if running inside a Databricks
-        notebook attached to an interactive cluster in "single user" or
-        "no isolation shared" mode.
+
+    * the ``DATABRICKS_API_TOKEN`` environment variable if present, or
+    * an automatically generated temporary token if running inside a Databricks
+      notebook attached to an interactive cluster in "single user" or
+      "no isolation shared" mode.
     """
 
     endpoint_name: Optional[str] = None
     """Name of the model serving endpont.
     You must specify the endpoint name to connect to a model serving endpoint.
-    You must not set both `endpoint_name` and `cluster_id`.
+    You must not set both ``endpoint_name`` and ``cluster_id``.
     """
 
     cluster_id: Optional[str] = None
     """ID of the cluster if connecting to a cluster driver proxy app.
-    If neither `endpoint_name` nor `cluster_id` is not provided and the code runs
+    If neither ``endpoint_name`` nor ``cluster_id`` is not provided and the code runs
     inside a Databricks notebook attached to an interactive cluster in "single user"
     or "no isolation shared" mode, the current cluster ID is used as default.
-    You must not set both `endpoint_name` and `cluster_id`.
+    You must not set both ``endpoint_name`` and ``cluster_id``.
     """
 
     cluster_driver_port: Optional[str] = None
     """The port number used by the HTTP server running on the cluster driver node.
     The server should listen on the driver IP address or simply ``0.0.0.0`` to connect.
-    We recommend the server using a port number between [3000, 8000].
+    We recommend the server using a port number between ``[3000, 8000]``.
     """
 
     transform_input_fn: Optional[Callable] = None
-    """A function that transforms (prompt, stop) into a JSON-compatible request that
-    the endpoint accepts.
+    """A function that transforms ``(prompt, stop)`` into a JSON-compatible request
+    that the endpoint accepts.
     For example, you can insert additional parameters like temperature.
     """
 
