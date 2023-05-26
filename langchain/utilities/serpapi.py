764a68e00177355a77b9121b23805341e1e98b5d
Aymen Furter <aymen.furter@outlook.com>
2023-05-26 15:38:13 +0000

    fix: linting

--- langchain/utilities/serpapi.py
+++ langchain/utilities/serpapi.py
@@ -147,7 +147,7 @@ class SerpAPIWrapper(BaseModel):
             "shopping_results" in res.keys()
             and "title" in res["shopping_results"][0].keys()
         ):
-           toret = res["shopping_results"][:3]
+            toret = res["shopping_results"][:3]
         elif (
             "knowledge_graph" in res.keys()
             and "description" in res["knowledge_graph"].keys()
