66a66006983bb3250259d03939c9078805566f41
Zeeland <zeeland@foxmail.com>
2023-05-29 22:53:48 +0800

    pref: optimize SQLDatabaseToolkit tool description

--- langchain/tools/sql_database/tool.py
+++ langchain/tools/sql_database/tool.py
@@ -34,13 +34,11 @@ class QuerySQLDataBaseTool(BaseSQLDatabaseTool, BaseTool):
     """Tool for querying a SQL database."""
 
     name = "query_sql_db"
-    description = (
-        "Input to this tool is a detailed and correct SQL query, output is a result from the database."
-        "If the query is not correct, an error message will be returned."
-        "If an error is returned, rewrite the query, check the query, and try again."
-        "If you encounter an issue with Unknown column 'xxxx' in 'field list', using "
-        "schema_sql_db to query the correct table fields"
-    )
+    description = """
+    Input to this tool is a detailed and correct SQL query, output is a result from the database.
+    If the query is not correct, an error message will be returned.
+    If an error is returned, rewrite the query, check the query, and try again.
+    """
 
     def _run(
         self,
@@ -63,8 +61,7 @@ class InfoSQLDatabaseTool(BaseSQLDatabaseTool, BaseTool):
 
     name = "schema_sql_db"
     description = """
-    Input to this tool is a comma-separated list of tables, output is the schema and sample rows for those tables.
-    Be sure that the tables actually exist by calling list_tables_sql_db first!
+    Input to this tool is a comma-separated list of tables, output is the schema and sample rows for those tables.    
 
     Example Input: "table1, table2, table3"
     """
