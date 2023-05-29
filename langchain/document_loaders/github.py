26c9b6d9a12babc523cafa8e7c23bec2f650ab35
UmerHA <40663591+UmerHA@users.noreply.github.com>
2023-05-29 15:24:38 +0200

    Check-in

--- langchain/document_loaders/github.py
+++ langchain/document_loaders/github.py
@@ -1,104 +1,88 @@
+from datetime import datetime
+from typing import Any, Dict, List
+
 import requests
-from typing import List, Any
+from pydantic import BaseModel, root_validator
+
 from langchain.docstore.document import Document
 from langchain.document_loaders.base import BaseLoader
+from langchain.utils import get_from_dict_or_env
 
 
-class GitHubLoader(BaseLoader):
-    """Load GitHub repo data via the GitHub REST API."""
+class GitHubLoader(BaseLoader, BaseModel):
+    """Load issues of a GitHub repository."""
 
-    def __init__(self, repo: str, access_token: str):
-        """Initialize GitHubLoader with repo and access_token."""
-        self.repo = repo
-        self.access_token = access_token
-        self.headers = {
+    repo: str = ""
+    """Name of repository"""
+    access_token: str = ""
+    """Personal access token - see https://github.com/settings/tokens?type=beta"""
+
+    @root_validator(pre=True)
+    def validate_environment(cls, values: Dict) -> Dict:
+        """Validate that access token exists in environment."""
+        values["access_token"] = get_from_dict_or_env(
+            values, "access_token", "GITHUB_PERSONAL_ACCESS_TOKEN"
+        )
+        return values
+
+    @property
+    def headers(self) -> Dict[str, str]:
+        return {
             "Accept": "application/vnd.github+json",
             "Authorization": f"Bearer {self.access_token}",
         }
 
-    def load(self, **kwargs: Any) -> List[Document]:
-        """Get important GitHub repository information.
+    def load(self, include_prs: bool = True, **kwargs: Any) -> List[Document]:
+        """
+        Get issues of a GitHub repository.
 
         Args:
-            filter: Can be one of assigned, created, mentioned, subscribed, repos, all
-            state: Can be onf of open, closed, all
-            labels: A list of comma separated label names. Example: bug,ui,@high
-            collab boolean
-            orgs boolean
-            owned boolean
-            pulls boolean
-            sort - Can be one of created, updated, comments
-            desc - asc, desc
-            since - YYYY-MM-DDTHH:MM:SSZ
+            include_prs: Should pull requests als be returned, or only issues?
+            milestone: If an integer is passed, it should refer to a milestone by its number field.
+                If the string * is passed, issues with any milestone are accepted.
+                If the string none is passed, issues without milestones are returned.
+            state: State of the issues to return. Can be one of: open, closed, all. Default is 'open'.
+            assignee: Assigned user. Pass none for no user and * for any user.
+            creator: The user that created the issue.
+            mentioned: A user that's mentioned in the issue.
+            labels: A list of comma separated label names. Example: bug,ui,@high.
+            sort: What to sort results by. Can be one of: created, updated, comments.
+                Default is 'created'.
+            direction: The direction to sort the results by. Can be one of: asc, desc.
+                Default is 'desc'.
+            since: Only show notifications updated after the given time.
+                This is a timestamp in ISO 8601 format: YYYY-MM-DDTHH:MM:SSZ.
 
-        Returns a  are:
-            - title
-            - content
-            - url,
-            - creation time
-            - creator of the issue
-            - number of comments
+        Returns:
+            A list of Documents with attributes:
+                - page_content
+                - metadata
+                    - url
+                    - title
+                    - creator
+                    - creation_time
+                    - last_update_time
+                    - closed_time
+                    - number of comments
+                    - state
+                    - labels
+                    - assignee
+                    - assignees
+                    - milestone
+                    - locked
+                    - number
+                    - is_pull_request
         """
-        url = f"https://api.github.com/repos/{self.repo}/issues"
+        url = self.build_url(**kwargs)
         response = requests.get(url, headers=self.headers)
         response.raise_for_status()
 
         issues = response.json()
-        return [self.parse_issue(issue) for issue in issues]
-
-    def build_url(self, **kwargs: Any) -> str:
-        valid_kwargs = {
-            "filter": {
-                "assigned",
-                "created",
-                "mentioned",
-                "subscribed",
-                "repos",
-                "all",
-            },
-            "state": {"open", "closed", "all"},
-            "sort": {"created", "updated", "comments"},
-            "desc": {"asc", "desc"},
-            "collab": {True, False},
-            "orgs": {True, False},
-            "owned": {True, False},
-            "pulls": {True, False},
-        }
-
-        for key, value in kwargs.items():
-            if key in ["labels", "since"]:
-                # we validate "labels" and "since" below
-                continue
-
-            if key not in valid_kwargs:
-                raise ValueError(f"Invalid keyword argument: {key}")
-            if value not in valid_kwargs[key]:
-                raise ValueError(
-                    f"Invalid value for {key}: {value}. Expected one of {valid_kwargs[key]}"
-                )
-
-        if "labels" in kwargs and not isinstance(kwargs["labels"], list):
-            raise ValueError("Invalid value for labels: Expected a list of strings")
-
-        if "since" in kwargs:
-            try:
-                datetime.datetime.strptime(kwargs["since"], "%Y-%m-%dT%H:%M:%SZ")
-            except ValueError:
-                raise ValueError(
-                    "Invalid value for since: Expected a date string in YYYY-MM-DDTHH:MM:SSZ format"
-                )
-
-        return "todo"
-
-    def load_filtered(self, query: str) -> List[Document]:
-        """Search for GitHub issues based on a query."""
-        url = f"https://api.github.com/search/issues"
-        params = {"q": f"repo:{self.repo} {query}"}
-        response = requests.get(url, headers=self.headers, params=params)
-        response.raise_for_status()
-
-        issues = response.json()["items"]
-        return [self.parse_issue(issue) for issue in issues]
+        documents = [self.parse_issue(issue) for issue in issues]
+        if include_prs:
+            return documents
+        else:
+            return [doc for doc in documents if not doc.metadata["is_pull_request"]]
 
     def parse_issue(self, issue: dict) -> Document:
         """Create Document objects from a list of GitHub issues."""
@@ -108,5 +92,56 @@ class GitHubLoader(BaseLoader):
             "creator": issue["user"]["login"],
             "creation_time": issue["created_at"],
             "comments": issue["comments"],
+            "state": issue["state"],
+            "labels": [label["name"] for label in issue["labels"]],
+            "assignee": issue["assignee"]["login"] if issue["assignee"] else None,
+            "milestone": issue["milestone"]["title"] if issue["milestone"] else None,
+            "locked": issue["locked"],
+            "number": issue["number"],
+            "is_pull_request": "pull_request" in issue,
         }
         return Document(page_content=issue["body"], metadata=metadata)
+
+    def build_url(self, **kwargs: Any) -> str:
+        valid_kwargs = {
+            "milestone": {"*", "none"},  # todo: add milestone number
+            "state": {"open", "closed", "all"},
+            "sort": {"created", "updated", "comments"},
+            "direction": {"asc", "desc"},
+        }
+
+        for key, value in kwargs.items():
+            # we validate these below or not at all
+            if key in ["labels", "since", "assignee", "creator", "mentioned"]:
+                continue
+
+            if key not in valid_kwargs:
+                raise ValueError(f"Invalid keyword argument: {key}")
+            if value not in valid_kwargs[key]:
+                raise ValueError(
+                    f"Invalid value for {key}: {value}. "
+                    f"Expected one of {valid_kwargs[key]}"
+                )
+
+        if "labels" in kwargs and not isinstance(kwargs["labels"], list):
+            raise ValueError("Invalid value for labels: Expected a list of strings")
+
+        if "since" in kwargs:
+            try:
+                datetime.strptime(kwargs["since"], "%Y-%m-%dT%H:%M:%SZ")
+            except ValueError:
+                raise ValueError(
+                    "Invalid value for since: Expected a date string in "
+                    "YYYY-MM-DDTHH:MM:SSZ format"
+                )
+
+        url = f"https://api.github.com/repos/{self.repo}/issues"
+        query_params_list = []
+        for k, v in kwargs.items():
+            if k == "labels" and isinstance(v, list):
+                # labels values should be a comma-separated list of values
+                v = ",".join(v)
+            query_params_list.append(f"{k}={v}")
+        query_params = "&".join(query_params_list)
+        full_url = f"{url}?{query_params}"
+        return full_url
--- tests/integration_tests/document_loaders/test_github.py
+++ tests/integration_tests/document_loaders/test_github.py
@@ -1,30 +1,116 @@
-from typing import List
+import pytest
+from pytest_mock import MockerFixture
 
-from langchain.document_loaders import GitHubLoader
-from langchain.schema import Document
-
-
-def assert_docs(docs: List[Document]) -> None:
-    for doc in docs:
-        assert doc.page_content
-        assert doc.metadata
-        assert set(doc.metadata) == {"url", "title", "creator", "creation_time", "comments"}
+from langchain.docstore.document import Document
+from langchain.document_loaders.github import GitHubLoader
 
 
-def test_load_success() -> None:
-    """Test that returns one document"""
-    loader = GitHubLoader(query="1605.08386", load_max_docs=2)
+def test_initialization() -> None:
+    loader = GitHubLoader(repo="repo", access_token="access_token")
+    assert loader.repo == "repo"
+    assert loader.access_token == "access_token"
+    assert loader.headers == {
+        "Accept": "application/vnd.github+json",
+        "Authorization": "Bearer access_token",
+    }
 
-    docs = loader.load()
-    assert len(docs) == 1
-    print(docs[0].metadata)
-    print(docs[0].page_content)
-    assert_docs(docs)
+
+def test_load(mocker: MockerFixture) -> None:
+    mocker.patch("requests.get", return_value=mocker.MagicMock(json=lambda: []))
+    loader = GitHubLoader(repo="repo", access_token="access_token")
+    documents = loader.load()
+    assert documents == []
 
 
-def test_failure() -> None:
-    """Test that returns no docs"""
-    loader = GitHubLoader(load_max_docs=2)
-    docs = loader.load()
+def test_parse_issue() -> None:
+    issue = {
+        "html_url": "https://github.com/repo/issue/1",
+        "title": "Example Issue 1",
+        "user": {"login": "username1"},
+        "created_at": "2023-01-01T00:00:00Z",
+        "comments": 1,
+        "state": "open",
+        "labels": [{"name": "bug"}],
+        "assignee": {"login": "username2"},
+        "milestone": {"title": "v1.0"},
+        "locked": "False",
+        "number": "1",
+        "body": "This is an example issue 1",
+    }
+    expected_document = Document(
+        page_content=issue["body"],  # type: ignore
+        metadata={
+            "url": issue["html_url"],
+            "title": issue["title"],
+            "creator": issue["user"]["login"],  # type: ignore
+            "creation_time": issue["created_at"],
+            "comments": issue["comments"],
+            "state": issue["state"],
+            "labels": [label["name"] for label in issue["labels"]],  # type: ignore
+            "assignee": issue["assignee"]["login"],  # type: ignore
+            "milestone": issue["milestone"]["title"],  # type: ignore
+            "locked": issue["locked"],
+            "number": issue["number"],
+        },
+    )
+    loader = GitHubLoader(repo="repo", access_token="access_token")
+    document = loader.parse_issue(issue)
+    assert document == expected_document
 
-    assert len(docs) == 0
+
+def test_build_url() -> None:
+    loader = GitHubLoader(repo="repo", access_token="access_token")
+
+    # No parameters
+    url = loader.build_url()
+    assert url == "https://api.github.com/repos/repo/issues?"
+
+    # parameters: state,  sort
+    url = loader.build_url(state="open", sort="created")
+    assert url == "https://api.github.com/repos/repo/issues?state=open&sort=created"
+
+    # parameters: milestone, state, assignee, creator, mentioned, labels, sort,
+    # direction, since
+    url = loader.build_url(
+        milestone="*",
+        state="closed",
+        assignee="user1",
+        creator="user2",
+        mentioned="user3",
+        labels=["bug", "ui", "@high"],
+        sort="comments",
+        direction="asc",
+        since="2023-05-26T00:00:00Z",
+    )
+    assert url == (
+        "https://api.github.com/repos/repo/issues?milestone=*&state=closed"
+        "&assignee=user1&creator=user2&mentioned=user3&labels=bug,ui,@high"
+        "&sort=comments&direction=asc&since=2023-05-26T00:00:00Z"
+    )
+
+    # Invalid parameter
+    with pytest.raises(ValueError):
+        loader.build_url(invalid="parameter")
+
+    # Invalid value for valid parameter
+    with pytest.raises(ValueError):
+        loader.build_url(state="invalid_state")
+
+    # Invalid type for labels
+    with pytest.raises(ValueError):
+        loader.build_url(labels="not_a_list")
+
+    # Invalid date format for since
+    with pytest.raises(ValueError):
+        loader.build_url(since="not_a_date")
+
+
+def test_integration() -> None:
+    title = (
+        "ChatOpenAI models don't work with prompts created via ChatPromptTemplate."
+        "from_role_strings"
+    )
+    loader = GitHubLoader(repo="hwchase17/langchain")
+    data = loader.load(creator="UmerHA")
+    titles = [d.metadata["title"] for d in data]
+    assert title in titles
