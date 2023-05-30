0cc2bd49d98b86af77d06af6f5319513e7ff72e9
UmerHA <40663591+UmerHA@users.noreply.github.com>
2023-05-30 05:11:21 +0200

    DocumentLoader for GitHub (#5408)
    # Creates GitHubLoader (#5257)
    
    GitHubLoader is a DocumentLoader that loads issues and PRs from GitHub.
    
    Fixes #5257
    
    ---------
    
    Co-authored-by: Dev 2049 <dev.dev2049@gmail.com>

--- langchain/document_loaders/__init__.py
+++ langchain/document_loaders/__init__.py
@@ -37,6 +37,7 @@ from langchain.document_loaders.gcs_directory import GCSDirectoryLoader
 from langchain.document_loaders.gcs_file import GCSFileLoader
 from langchain.document_loaders.git import GitLoader
 from langchain.document_loaders.gitbook import GitbookLoader
+from langchain.document_loaders.github import GitHubIssuesLoader
 from langchain.document_loaders.googledrive import GoogleDriveLoader
 from langchain.document_loaders.gutenberg import GutenbergLoader
 from langchain.document_loaders.hn import HNLoader
@@ -153,6 +154,7 @@ __all__ = [
     "GCSDirectoryLoader",
     "GCSFileLoader",
     "GitLoader",
+    "GitHubIssuesLoader",
     "GitbookLoader",
     "GoogleApiClient",
     "GoogleApiYoutubeLoader",
