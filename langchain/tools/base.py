39ac4b2b5f3cc9d9aed992e2360fa697d78e1a9c
Kim Minjong <make.dirty.code@gmail.com>
2023-05-30 16:07:34 +0900

    Change _parse_input implementation in BaseTool
    - Change to respect the BaseModel more than the results of the agent.

--- langchain/tools/base.py
+++ langchain/tools/base.py
@@ -188,7 +188,7 @@ class BaseTool(ABC, BaseModel, metaclass=ToolMetaclass):
         else:
             if input_args is not None:
                 result = input_args.parse_obj(tool_input)
-                return {k: v for k, v in result.dict().items() if k in tool_input}
+                return result.dict()
         return tool_input
 
     @root_validator()
