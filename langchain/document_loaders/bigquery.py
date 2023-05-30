65d9a183849e78935ce318f38c350aa10428ec42
Dev 2049 <dev.dev2049@gmail.com>
2023-05-30 15:49:10 -0700

    cr

--- langchain/document_loaders/bigquery.py
+++ langchain/document_loaders/bigquery.py
@@ -1,9 +1,12 @@
-from typing import List, Optional
+from __future__ import annotations
 
-import google.auth
+from typing import TYPE_CHECKING, List, Optional
 
 from langchain.docstore.document import Document
 from langchain.document_loaders.base import BaseLoader
+
+if TYPE_CHECKING:
+    from google.auth.credentials import Credentials
 
 
 class BigQueryLoader(BaseLoader):
@@ -22,18 +25,22 @@ class BigQueryLoader(BaseLoader):
         project: Optional[str] = None,
         page_content_columns: Optional[List[str]] = None,
         metadata_columns: Optional[List[str]] = None,
-        credentials: Optional[google.auth.credentials.Credentials] = None,
+        credentials: Optional[Credentials] = None,
     ):
-        """
-        :param query: The query to run in BigQuery.
-        :param project: Optional. The project to run the query in.
-        :param page_content_columns: Optional. The columns to write into the `page_content` of the document.
-        :param metadata_columns: Optional. The columns to write into the `metadata` of the document.
-        :param credentials : google.auth.credentials.Credentials, optional
+        """Initialize BigQuery document loader.
+
+        Args:
+            query: The query to run in BigQuery.
+            project: Optional. The project to run the query in.
+            page_content_columns: Optional. The columns to write into the `page_content`
+                of the document.
+            metadata_columns: Optional. The columns to write into the `metadata` of the
+                document.
+            credentials : google.auth.credentials.Credentials, optional
             Credentials for accessing Google APIs. Use this parameter to override
-            default credentials, such as to use Compute Engine
-            :class:`google.auth.compute_engine.Credentials` or Service Account
-            :class:`google.oauth2.service_account.Credentials` directly.
+                default credentials, such as to use Compute Engine
+                (`google.auth.compute_engine.Credentials`) or Service Account
+                (`google.oauth2.service_account.Credentials`) credentials directly.
         """
         self.query = query
         self.project = project
