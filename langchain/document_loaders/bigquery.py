b4a9a1a8e6404a58d75bdb8ad670a97e02c1debd
Natalie <nsheils@users.noreply.github.com>
2023-05-30 16:19:43 -0500

    added credentials

--- langchain/document_loaders/bigquery.py
+++ langchain/document_loaders/bigquery.py
@@ -11,19 +11,33 @@ class BigQueryLoader(BaseLoader):
     are written into the `page_content` of the document. The `metadata_columns`
     are written into the `metadata` of the document. By default, all columns
     are written into the `page_content` and none into the `metadata`.
+
     """
 
     def __init__(
-        self,
-        query: str,
-        project: Optional[str] = None,
-        page_content_columns: Optional[List[str]] = None,
-        metadata_columns: Optional[List[str]] = None,
+            self,
+            query: str,
+            project: Optional[str] = None,
+            page_content_columns: Optional[List[str]] = None,
+            metadata_columns: Optional[List[str]] = None,
+            credentials=None,
     ):
+        """
+        :param query: The query to run in BigQuery.
+        :param project: Optional. The project to run the query in.
+        :param page_content_columns: Optional. The columns to write into the `page_content` of the document.
+        :param metadata_columns: Optional. The columns to write into the `metadata` of the document.
+        :param credentials : google.auth.credentials.Credentials, optional
+            Credentials for accessing Google APIs. Use this parameter to override
+            default credentials, such as to use Compute Engine
+            :class:`google.auth.compute_engine.Credentials` or Service Account
+            :class:`google.oauth2.service_account.Credentials` directly.
+        """
         self.query = query
         self.project = project
         self.page_content_columns = page_content_columns
         self.metadata_columns = metadata_columns
+        self.credentials = credentials
 
     def load(self) -> List[Document]:
         try:
@@ -34,7 +48,7 @@ class BigQueryLoader(BaseLoader):
                 "Please install it with `pip install google-cloud-bigquery`."
             ) from ex
 
-        bq_client = bigquery.Client(self.project)
+        bq_client = bigquery.Client(credentials=self.credentials, project=self.project)
         query_result = bq_client.query(self.query).result()
         docs: List[Document] = []
 
