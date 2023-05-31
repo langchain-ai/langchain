5401a23c26a573150bf8baa4c587599c2a3d9f42
Krasus <chen@krasus.red>
2023-05-31 14:41:41 +0800

    translate all prompt to chinese，need more test

--- locate.sh
+++ locate.sh
@@ -3,6 +3,6 @@ find . -type f -name "*.py" -print0 | xargs -0 xgettext -o locale/en_US/LC_MESSA
 
 # msginit  --locale=zh_CN.UTF-8 -i locale/en_US/LC_MESSAGES/langchain.pot -o locale/zh_CN/LC_MESSAGES/langchain.po
 
-msgmerge -U locale/zh_CN/LC_MESSAGES/agents.po locale/en_US/LC_MESSAGES/langchain.pot
+msgmerge -U locale/zh_CN/LC_MESSAGES/langchain.po locale/en_US/LC_MESSAGES/langchain.pot
 
 msgfmt locale/zh_CN/LC_MESSAGES/langchain.po -o locale/zh_CN/LC_MESSAGES/langchain.mo
