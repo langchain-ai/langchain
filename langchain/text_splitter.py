bf9f79be14371bb4fe0d0f380c85b0e25c7f2243
Harrison Chase <hw.chase.17@gmail.com>
2023-05-31 07:21:20 -0700

    add more vars to text splitter (#5503)

--- langchain/text_splitter.py
+++ langchain/text_splitter.py
@@ -443,6 +443,8 @@ class RecursiveCharacterTextSplitter(TextSplitter):
                 "\nfunction ",
                 "\nconst ",
                 "\nlet ",
+                "\nvar ",
+                "\nclass ",
                 # Split along control flow statements
                 "\nif ",
                 "\nfor ",
