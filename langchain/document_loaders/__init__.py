3156234b80289f14718289cd51f5145f1a2ff475
Matt Robinson <mrobinson@unstructuredai.io>
2023-06-03 15:44:12 -0400

    feat: add `UnstructuredExcelLoader` for `.xlsx` and `.xls` files (#5617)
    # Unstructured Excel Loader
    
    Adds an `UnstructuredExcelLoader` class for `.xlsx` and `.xls` files.
    Works with `unstructured>=0.6.7`. A plain text representation of the
    Excel file will be available under the `page_content` attribute in the
    doc. If you use the loader in `"elements"` mode, an HTML representation
    of the Excel file will be available under the `text_as_html` metadata
    key. Each sheet in the Excel document is its own document.
    
    ### Testing
    
    ```python
    from langchain.document_loaders import UnstructuredExcelLoader
    
    loader = UnstructuredExcelLoader(
        "example_data/stanley-cups.xlsx",
        mode="elements"
    )
    docs = loader.load()
    ```
    
    ## Who can review?
    
    @hwchase17
    @eyurtsev

--- langchain/document_loaders/__init__.py
+++ langchain/document_loaders/__init__.py
@@ -32,6 +32,7 @@ from langchain.document_loaders.email import (
 )
 from langchain.document_loaders.epub import UnstructuredEPubLoader
 from langchain.document_loaders.evernote import EverNoteLoader
+from langchain.document_loaders.excel import UnstructuredExcelLoader
 from langchain.document_loaders.facebook_chat import FacebookChatLoader
 from langchain.document_loaders.figma import FigmaFileLoader
 from langchain.document_loaders.gcs_directory import GCSDirectoryLoader
@@ -223,6 +224,7 @@ __all__ = [
     "UnstructuredAPIFileLoader",
     "UnstructuredEPubLoader",
     "UnstructuredEmailLoader",
+    "UnstructuredExcelLoader",
     "UnstructuredFileIOLoader",
     "UnstructuredFileLoader",
     "UnstructuredHTMLLoader",
