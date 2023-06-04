1fedabad33f8a7fea88d6fea673037ed8d7b6f14
benharris2510 <45742646+benharris2510@users.noreply.github.com>
2023-06-04 22:11:28 +0100

    Add metadata filter capability to similarity search functions

--- langchain/vectorstores/supabase.py
+++ langchain/vectorstores/supabase.py
@@ -113,32 +113,29 @@ class SupabaseVectorStore(VectorStore):
         return self._add_vectors(self._client, self.table_name, vectors, documents)
 
     def similarity_search(
-        self, query: str, k: int = 4, **kwargs: Any
+        self, query: str, k: int = 4, filter: Optional[Dict[str, str]] = None, **kwargs: Any
     ) -> List[Document]:
         vectors = self._embedding.embed_documents([query])
-        return self.similarity_search_by_vector(vectors[0], k)
+        return self.similarity_search_by_vector(vectors[0], k, filter=filter)
 
     def similarity_search_by_vector(
-        self, embedding: List[float], k: int = 4, **kwargs: Any
+        self, embedding: List[float], k: int = 4, filter: Optional[Dict[str, str]] = None, **kwargs: Any
     ) -> List[Document]:
-        result = self.similarity_search_by_vector_with_relevance_scores(embedding, k)
-
+        result = self.similarity_search_by_vector_with_relevance_scores(embedding, k, filter=filter)
         documents = [doc for doc, _ in result]
-
         return documents
 
     def similarity_search_with_relevance_scores(
-        self, query: str, k: int = 4, **kwargs: Any
+        self, query: str, k: int = 4, filter: Optional[Dict[str, str]] = None, **kwargs: Any
     ) -> List[Tuple[Document, float]]:
         vectors = self._embedding.embed_documents([query])
-        return self.similarity_search_by_vector_with_relevance_scores(vectors[0], k)
+        return self.similarity_search_by_vector_with_relevance_scores(vectors[0], k, filter=filter)
 
     def similarity_search_by_vector_with_relevance_scores(
-        self, query: List[float], k: int
+        self, query: List[float], k: int, filter: Optional[Dict[str, str]] = None
     ) -> List[Tuple[Document, float]]:
         match_documents_params = dict(query_embedding=query, match_count=k)
         res = self._client.rpc(self.query_name, match_documents_params).execute()
-
         match_result = [
             (
                 Document(
@@ -148,17 +145,15 @@ class SupabaseVectorStore(VectorStore):
                 search.get("similarity", 0.0),
             )
             for search in res.data
-            if search.get("content")
+            if search.get("content") and (filter is None or search.get("metadata", {}) == filter)
         ]
-
         return match_result
 
     def similarity_search_by_vector_returning_embeddings(
-        self, query: List[float], k: int
+        self, query: List[float], k: int, filter: Optional[Dict[str, str]] = None
     ) -> List[Tuple[Document, float, np.ndarray[np.float32, Any]]]:
         match_documents_params = dict(query_embedding=query, match_count=k)
         res = self._client.rpc(self.query_name, match_documents_params).execute()
-
         match_result = [
             (
                 Document(
@@ -166,16 +161,15 @@ class SupabaseVectorStore(VectorStore):
                     page_content=search.get("content", ""),
                 ),
                 search.get("similarity", 0.0),
-                # Supabase returns a vector type as its string represation (!).
+                # Supabase returns a vector type as its string representation (!).
                 # This is a hack to convert the string to numpy array.
                 np.fromstring(
                     search.get("embedding", "").strip("[]"), np.float32, sep=","
                 ),
             )
             for search in res.data
-            if search.get("content")
+            if search.get("content") and (filter is None or search.get("metadata", {}) == filter)
         ]
-
         return match_result
 
     @staticmethod
