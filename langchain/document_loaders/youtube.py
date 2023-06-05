23e867caaf40488bd9f98859b22ea16b8c005d9a
Harrison Chase <hw.chase.17@gmail.com>
2023-06-05 16:26:00 -0700

    cr

--- langchain/document_loaders/youtube.py
+++ langchain/document_loaders/youtube.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 import logging
 from pathlib import Path
-from typing import Any, Dict, List, Optional, Union, Sequence
+from typing import Any, Dict, List, Optional, Sequence, Union
 from urllib.parse import parse_qs, urlparse
 
 from pydantic import root_validator
