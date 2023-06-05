3cbaaaccc5cd1c2de8ad58beb9bc6f0533bdd592
rlm <pexpresss31@gmail.com>
2023-06-05 16:44:46 -0700

    Update and get it working w/ OpenAIWhisperParser

--- langchain/document_loaders/parsers/audio.py
+++ langchain/document_loaders/parsers/audio.py
@@ -12,9 +12,18 @@ class OpenAIWhisperParser(BaseBlobParser):
     def lazy_parse(self, blob: Blob) -> Iterator[Document]:
         """Lazily parse the blob."""
 
+        import io 
         import openai
 
+        # Need if we pass an in-memory blob from AudioSplitter().lazy_split(video)
+        file_obj = io.BytesIO(blob.data)
+        file_obj.name = "input_split.mp3"
+
+        # This fails when split is not read from disk (e.g., w/ GenericLoader)
+        # OAI API is missing "f.name"
         with blob.as_bytes_io() as f:
+            # *** This is a hack to unblock ***
+            f = file_obj
             transcript = openai.Audio.transcribe("whisper-1", f)
             yield Document(
                 page_content=transcript.text, metadata={"source": blob.source}
