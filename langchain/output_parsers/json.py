0d19ad119c1593a302772c1ae1152671a7861346
Konstantinos Voulgaropoulos <voulkon93@gmail.com>
2023-06-05 19:02:18 +0300

    json parser for when 3 backtiks are missing

--- langchain/output_parsers/json.py
+++ langchain/output_parsers/json.py
@@ -9,7 +9,12 @@ from langchain.schema import OutputParserException
 
 def parse_json_markdown(json_string: str) -> dict:
     # Try to find JSON string within triple backticks
-    match = re.search(r"```(json)?(.*?)```", json_string, re.DOTALL)
+
+    # The amount of backticks at the end of the LLM response can vary,
+    # so we use a regex to match what we want even if we have fewer than 3 backticks
+    # As a tradeoff, we might fetch some backticks in the command below: match.group(2)
+    # We will get rid of them using rstrip() later
+    match = re.search(r"```(json)?(.*)(`{1,3})?", json_string, re.DOTALL)
 
     # If no match found, assume the entire string is a JSON string
     if match is None:
@@ -17,6 +22,8 @@ def parse_json_markdown(json_string: str) -> dict:
     else:
         # If match found, use the content within the backticks
         json_str = match.group(2)
+        # In case we have picked some trailing backticks, get rid of them
+        json_str = json_str.rstrip("`")
 
     # Strip whitespace and newlines from the start and end
     json_str = json_str.strip()
