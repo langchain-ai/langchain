e1fa1a41d0b2d7f476627a6798e98f02ebe4a83d
rlm <pexpresss31@gmail.com>
2023-06-06 09:54:56 -0700

    Address comments

--- langchain/document_loaders/blob_loaders/__init__.py
+++ langchain/document_loaders/blob_loaders/__init__.py
@@ -2,4 +2,4 @@ from langchain.document_loaders.blob_loaders.file_system import FileSystemBlobLo
 from langchain.document_loaders.blob_loaders.schema import Blob, BlobLoader
 from langchain.document_loaders.blob_loaders.youtube_audio import YoutubeAudioLoader
 
-__all__ = ["BlobLoader", "Blob", "FileSystemBlobLoader","YoutubeAudioLoader"]
+__all__ = ["BlobLoader", "Blob", "FileSystemBlobLoader", "YoutubeAudioLoader"]
--- langchain/document_loaders/parsers/audio.py
+++ langchain/document_loaders/parsers/audio.py
@@ -13,32 +13,44 @@ class OpenAIWhisperParser(BaseBlobParser):
         """Lazily parse the blob."""
 
         import io
-        import openai
-        from pydub import AudioSegment
+
+        try:
+            import openai
+        except ImportError:
+            raise ValueError(
+                "openai package not found, please install it with "
+                "`pip install openai`"
+            )
+        try:
+            from pydub import AudioSegment
+        except ImportError:
+            raise ValueError(
+                "pydub package not found, please install it with " "`pip install pydub`"
+            )
 
         # Audio file from disk
         audio = AudioSegment.from_file(blob.path)
 
-        # Define the duration of each chunk in minutes to meet 25MB OpenAI size limit to Whisper API
+        # Define the duration of each chunk in minutes
+        # Need to meet 25MB size limit for Whisper API
         chunk_duration = 20
-
-        # Calculate the chunk duration in milliseconds
         chunk_duration_ms = chunk_duration * 60 * 1000
 
-        # Split the audio into chunk_duration_ms chunks 
-        for split_number,i in enumerate(range(0, len(audio), chunk_duration_ms)):
-            
-            print(f"Transcribing part {split_number}!")
-
-            # Audio chunk 
+        # Split the audio into chunk_duration_ms chunks
+        for split_number, i in enumerate(range(0, len(audio), chunk_duration_ms)):
+            # Audio chunk
             chunk = audio[i : i + chunk_duration_ms]
-            audio_split = chunk.export(format="mp3").read()
-            file_obj = io.BytesIO(audio_split)
-            file_obj.name = blob.source + f"_part_{split_number}.mp3"
+            file_obj = io.BytesIO(chunk.export(format="mp3").read())
+            if blob.source is not None:
+                file_obj.name = blob.source + f"_part_{split_number}.mp3"
+            else:
+                file_obj.name = f"part_{split_number}.mp3"
 
             # Transcribe
+            print(f"Transcribing part {split_number+1}!")
             transcript = openai.Audio.transcribe("whisper-1", file_obj)
 
             yield Document(
-                page_content=transcript.text, metadata={"source": file_obj.name}
+                page_content=transcript.text,
+                metadata={"source": blob.source, "chunk": split_number},
             )
