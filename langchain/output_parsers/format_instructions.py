e674cf419eb56e0f70127e9dcc618152f27a4cd2
SvMax <Biancucci95@gmail.com>
2023-06-07 17:43:33 +0200

    added opption in StructuredOutputParser class to return only the json format without the introducing instructions

--- langchain/output_parsers/format_instructions.py
+++ langchain/output_parsers/format_instructions.py
@@ -8,6 +8,15 @@ STRUCTURED_FORMAT_INSTRUCTIONS = """The output should be a markdown code snippet
 }}
 ```"""
 
+STRUCTURED_INPUT_FORMAT_INSTRUCTIONS = """The input will be a markdown code snippet formatted in the following schema, including the leading and trailing "```json" and "```":
+
+```json
+{{
+{format}
+}}
+"""
+
+
 PYDANTIC_FORMAT_INSTRUCTIONS = """The output should be formatted as a JSON instance that conforms to the JSON schema below.
 
 As an example, for the schema {{"properties": {{"foo": {{"title": "Foo", "description": "a list of strings", "type": "array", "items": {{"type": "string"}}}}}}, "required": ["foo"]}}}}
--- langchain/output_parsers/structured.py
+++ langchain/output_parsers/structured.py
@@ -4,7 +4,10 @@ from typing import Any, List
 
 from pydantic import BaseModel
 
-from langchain.output_parsers.format_instructions import STRUCTURED_FORMAT_INSTRUCTIONS
+from langchain.output_parsers.format_instructions import (
+    STRUCTURED_FORMAT_INSTRUCTIONS,
+    STRUCTURED_INPUT_FORMAT_INSTRUCTIONS
+)
 from langchain.output_parsers.json import parse_and_check_json_markdown
 from langchain.schema import BaseOutputParser
 
@@ -32,11 +35,43 @@ class StructuredOutputParser(BaseOutputParser):
     ) -> StructuredOutputParser:
         return cls(response_schemas=response_schemas)
 
-    def get_format_instructions(self) -> str:
+    def get_format_instructions(self, input_format=False) -> str:
+        """
+        Method to get the format instructions for the output parser.
+
+        Args:
+            input_format (bool): Whether to get the format instructions for the input or output. Defaults to False (output format instructions)
+
+        example:
+        ```python
+        from langchain.output_parsers.structured import StructuredOutputParser, ResponseSchema
+
+        response_schemas = [
+            ResponseSchema(name="foo", description="a list of strings", type="List[string]", items={"type": "string"}),
+            ResponseSchema(name="bar", description="a string", type="string"),
+        ]
+
+        parser = StructuredOutputParser.from_response_schemas(response_schemas)
+
+        print(parser.get_format_instructions())
+
+        output:
+        # The output should be a markdown code snippet formatted in the following schema, including the leading and trailing "```json" and "```":
+        #
+        # ```json
+        # {
+        #     "foo": List[string]  // a list of strings
+        #     "bar": string  // a string
+        # }
+
+        NOTE: if the input_format is True, only the first row change with the following:
+        # The input will be a markdown code snippet formatted in the following schema, including the leading and trailing "```json" and "```":
+        """
         schema_str = "\n".join(
             [_get_sub_string(schema) for schema in self.response_schemas]
         )
         return STRUCTURED_FORMAT_INSTRUCTIONS.format(format=schema_str)
+
 
     def parse(self, text: str) -> Any:
         expected_keys = [rs.name for rs in self.response_schemas]
