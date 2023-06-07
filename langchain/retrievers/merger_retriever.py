bdfa1fef17ff2292910871f8cacede6cce6017ab
german <gmartin.py@gmail.com>
2023-06-07 05:29:56 -0300

    Cleanup removing document formatter processing.

--- langchain/retrievers/merger_retriever.py
+++ langchain/retrievers/merger_retriever.py
@@ -1,33 +1,27 @@
-from langchain.schema import BaseRetriever
-from langchain.schema import BaseDocumentTransformer, Document
 from typing import List
-import asyncio
+from langchain.schema import BaseRetriever
 
 
 class MergerRetriever(BaseRetriever):
     """
-    This class merges the results of multiple retrievers and then refines the results using a list of transformers.
+    This class merges the results of multiple retrievers.
 
     Args:
         retrievers: A list of retrievers to merge.
-        transformers: A list of transformers to refine the results.
     """
 
     def __init__(
         self,
         retrievers: List[BaseRetriever],
-        transformers: List[BaseDocumentTransformer],
     ):
         """
         Initialize the MergerRetriever class.
 
         Args:
             retrievers: A list of retrievers to merge.
-            transformers: A list of transformers to refine the results.
         """
 
         self.retrievers = retrievers
-        self.transformers = transformers
 
     def get_relevant_documents(self, query: str) -> List[str]:
         """
@@ -43,10 +37,7 @@ class MergerRetriever(BaseRetriever):
         # Merge the results of the retrievers.
         merged_documents = self.merge_documents(query)
 
-        # Refine the results using the transformers.
-        refined_documents = self.refine_documents(merged_documents)
-
-        return refined_documents
+        return merged_documents
 
     async def aget_relevant_documents(self, query: str) -> List[str]:
         """
@@ -62,10 +53,7 @@ class MergerRetriever(BaseRetriever):
         # Merge the results of the retrievers.
         merged_documents = await self.amerge_documents(query)
 
-        # Refine the results using the transformers.
-        refined_documents = await self.arefine_documents(merged_documents)
-
-        return refined_documents
+        return merged_documents
 
     def merge_documents(self, query: str) -> List[str]:
         """
@@ -119,46 +107,3 @@ class MergerRetriever(BaseRetriever):
                     merged_documents.append(doc[i])
 
         return merged_documents
-
-    def refine_documents(self, documents: List[str]) -> List[str]:
-        """
-        Refine the results of the retrievers using a list of transformers.
-
-        Args:
-            documents: The documents to refine.
-
-        Returns:
-            A list of refined documents.
-        """
-
-        # Create a list to store the refined documents.
-        refined_documents = documents
-
-        # Iterate over the transformers and refine the documents.
-        for transformer in self.transformers:
-            refined_documents = transformer.transform_documents(refined_documents)
-
-        return refined_documents
-
-    async def arefine_documents(self, documents: List[str]) -> List[str]:
-        """
-        Asynchronously refine the results of the retrievers using a list of transformers.
-
-        Args:
-            documents: The documents to refine.
-
-        Returns:
-            A list of refined documents.
-        """
-
-        # Create a list to store the refined documents.
-        refined_documents = documents
-
-        # Iterate over the transformers and refine the documents.
-        for transformer in self.transformers:
-            if hasattr(transformer, "atransform_documents"):
-                refined_documents = await transformer.atransform_documents(
-                    refined_documents
-                )
-
-        return refined_documents
