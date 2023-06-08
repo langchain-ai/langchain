dfeace45a09d8627d22cd447760d94e54ffbe255
Lior Durahly <lior.durahly@superwise.ai>
2023-06-08 19:17:45 +0300

    Fix the issue where the parameters passed to VertexAI ignored #5889

--- langchain/llms/vertexai.py
+++ langchain/llms/vertexai.py
@@ -68,7 +68,7 @@ class _VertexAICommon(BaseModel):
     @classmethod
     def _try_init_vertexai(cls, values: Dict) -> None:
         allowed_params = ["project", "location", "credentials"]
-        params = {k: v for k, v in values.items() if v in allowed_params}
+        params = {k: v for k, v in values.items() if k in allowed_params}
         init_vertexai(**params)
         return None
 
