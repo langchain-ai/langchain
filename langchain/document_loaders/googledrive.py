63689d424aadc40f95db68ddf70ac1df575b602e
Mark <mark.edmondson@cellpointmobile.com>
2023-06-11 22:04:21 +0200

    no need to write to a file

--- langchain/document_loaders/googledrive.py
+++ langchain/document_loaders/googledrive.py
@@ -119,7 +119,8 @@ class GoogleDriveLoader(BaseLoader, BaseModel):
                 creds.refresh(Request())
             elif "GOOGLE_APPLICATION_CREDENTIALS" not in os.environ:
                 creds, project = default()
-                self.token_path = Path("/tmp") / "token.json"
+                # no need to write to file
+                return creds
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
                     str(self.credentials_path), SCOPES
