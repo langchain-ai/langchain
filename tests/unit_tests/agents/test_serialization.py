ddbbcd12c94667b4a46914ba07b2f03f6bf3cc09
Juhee Kim <kimjuhi96@snu.ac.kr>
2023-06-12 09:29:02 +0900

    fix mrkl test

--- tests/unit_tests/agents/test_serialization.py
+++ tests/unit_tests/agents/test_serialization.py
@@ -4,11 +4,18 @@ from tempfile import TemporaryDirectory
 from langchain.agents.agent_types import AgentType
 from langchain.agents.initialize import initialize_agent, load_agent
 from langchain.llms.fake import FakeListLLM
+from langchain.agents.tools import Tool
 
 
 def test_mrkl_serialization() -> None:
     agent = initialize_agent(
-        [],
+        [        
+            Tool(
+                name="Test tool",
+                func=lambda x: x,
+                description="Test description",
+            )
+        ],
         FakeListLLM(responses=[]),
         agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
         verbose=True,
