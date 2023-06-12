c70ffd16116eb51707b84dbc69b54c7b1fa32d36
Fan Li <lifan0127@gmail.com>
2023-06-12 10:31:26 -0400

    Support external RDF store through SPARQLWrapper

--- langchain/graphs/rdf_graph.py
+++ langchain/graphs/rdf_graph.py
@@ -33,13 +33,13 @@ class RDFGraph:
     """
 
     def __init__(
-        self, source: Optional[str] = None, username: Optional[str] = None, password: Optional[str] = None
+        self, source: Optional[str] = None, username: Optional[str] = None, password: Optional[str] = None, http_auth: Optional[str] = None, http_method: Optional[str] = 'GET'
     ) -> None:
         """Connect to an external RDF store or create an in-memory store."""
         self.source = source or "in-memory-store"
         try:
             from rdflib import Graph
-            from SPARQLWrapper import SPARQLWrapper, JSON
+            from SPARQLWrapper import SPARQLWrapper, JSON, RDFXML, BASIC, DIGEST, POST
         except ImportError:
             raise ValueError(
                 "Please install rdflib and sparqlwrapper first: "
@@ -59,19 +59,32 @@ class RDFGraph:
             class ExternalGraph:
                 """Wrapper for SPARQLWrapper to have the same APIs as RDFLib"""
 
-                def __init__(self, url: str) -> None:
-                    self.graph = SPARQLWrapper(url)
-                    self.graph.setReturnFormat(JSON)
+                def __init__(self, url: str, username: Optional[str] = None, password: Optional[str] = None, http_auth: Optional[str] = None, http_method: Optional[str] = 'GET') -> None:
+                    self.wrapper = SPARQLWrapper(url)
+                    if username is not None and password is not None:
+                        if http_auth.lower() == 'digest':
+                            self.wrapper.setHTTPAuth(DIGEST)
+                        else:
+                            self.wrapper.setHTTPAuth(BASIC)
+                        self.wrapper.setCredentials(username, password)
 
                 def query(self, query: str) -> Dict[str, Any]:
-                    self.graph.setQuery(query)
-                    return self.graph.queryAndConvert()
+                    query_type = Graph().query(query, use_store_provided=False).type
+                    self.wrapper.setQuery(query)
+                    self.wrapper.setMethod(http_method)
+                    if query_type == 'CONSTRUCT':
+                        self.wrapper.setReturnFormat(RDFXML)
+                        return Graph().parse(data=self.wrapper.query().response.read(), format='xml')
+                    self.wrapper.setReturnFormat(JSON)
+                    return self.wrapper.queryAndConvert()['results']['bindings']
                 
-                def update(self, query: str) -> Dict[str, Any]:
-                    self.graph.setQuery(query)
-                    return self.graph.queryAndConvert()
-
-            self.graph = ExternalGraph(source)
+                def update(self, update: str) -> Dict[str, Any]:
+                    self.wrapper.setQuery(update)
+                    self.wrapper.setReturnFormat(RDFXML)
+                    self.wrapper.setMethod(POST)
+                    self.wrapper.query()
+            
+            self.graph = ExternalGraph(source, username, password, http_auth)
             try:
                 self.graph.query("SELECT * WHERE {?s ?p ?o} LIMIT 1")
             except Exception as e:
@@ -89,12 +102,12 @@ class RDFGraph:
         """Returns the schema of the RDF database"""
         return self.schema
 
-    def query(self, query: str, bindings: dict = {}) -> List[Dict[str, Any]]:
+    def query(self, query: str) -> List[Dict[str, Any]]:
         """Query RDF database."""
-        from rdflib.plugins.sparql import prepareQuery
+        from rdflib import Graph
         try:
-            results = self.graph.query(prepareQuery(query), initBindings=bindings)
-            return [row.asdict() for row in results]
+            results = self.graph.query(query)
+            return list(results)           
         except Exception as e:
             raise ValueError(f"Could not query RDF store {self.source}: {e}")
             
@@ -110,4 +123,5 @@ class RDFGraph:
         """
         Refreshes the RDF graph schema information.
         """
-        self.schema = self.graph.query(schema_query).serialize(format='turtle').decode('utf-8')
+        result = self.graph.query(schema_query).serialize(format='turtle')
+        self.schema = result if type(result) is str else result.decode()
