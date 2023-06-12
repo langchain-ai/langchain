e1b65051e102b707155c5ec7ca2f55cb7d409c4d
jupyterjazz <saba.sturua@jina.ai>
2023-06-12 08:56:44 +0200

    style: ruff ruff
    Signed-off-by: jupyterjazz <saba.sturua@jina.ai>

--- langchain/retrievers/__init__.py
+++ langchain/retrievers/__init__.py
@@ -3,6 +3,7 @@ from langchain.retrievers.azure_cognitive_search import AzureCognitiveSearchRetr
 from langchain.retrievers.chatgpt_plugin_retriever import ChatGPTPluginRetriever
 from langchain.retrievers.contextual_compression import ContextualCompressionRetriever
 from langchain.retrievers.databerry import DataberryRetriever
+from langchain.retrievers.docarray import DocArrayRetriever
 from langchain.retrievers.elastic_search_bm25 import ElasticSearchBM25Retriever
 from langchain.retrievers.knn import KNNRetriever
 from langchain.retrievers.metal import MetalRetriever
@@ -18,7 +19,6 @@ from langchain.retrievers.vespa_retriever import VespaRetriever
 from langchain.retrievers.weaviate_hybrid_search import WeaviateHybridSearchRetriever
 from langchain.retrievers.wikipedia import WikipediaRetriever
 from langchain.retrievers.zep import ZepRetriever
-from langchain.retrievers.docarray import DocArrayRetriever
 
 __all__ = [
     "ArxivRetriever",
