277776896b5b999927b65be6a2adc0252d1f31c4
Sims Juju <sims@Ju.lan>
2023-06-13 11:36:05 -0700

    git reverted contextual compression changes

--- langchain/retrievers/contextual_compression.py
+++ langchain/retrievers/contextual_compression.py
@@ -51,9 +51,7 @@ class ContextualCompressionRetriever(BaseRetriever, BaseModel):
         """
         docs = await self.base_retriever.aget_relevant_documents(query)
         if docs:
-            compressed_docs = await self.base_compressor.acompress_documents(
-                docs, query
-            )
+            compressed_docs = await self.base_compressor.acompress_documents(docs, query)
             return list(compressed_docs)
         else:
             return []
