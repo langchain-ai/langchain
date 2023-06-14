99e1cdd8cdcc3b7cd39108ec29b187a531c3da75
Saleh Hindi <saleh.hindi.one@gmail.com>
2023-06-14 07:20:04 -0400

    Fix ChatModel so both Chat and vanilla models show up in PromptLayerUI

--- langchain/callbacks/promptlayer_callback.py
+++ langchain/callbacks/promptlayer_callback.py
@@ -1,19 +1,60 @@
 import os
 import datetime
-from typing import Dict, Any, List
+from typing import Dict, Any, List, Optional, Tuple
 
 import promptlayer
 from promptlayer.utils import get_api_key, promptlayer_api_request
 
 from langchain.llms import OpenAI, Replicate
 from langchain.chat_models import ChatOpenAI
-from langchain.schema import HumanMessage, LLMResult
+from langchain.schema import BaseMessage, HumanMessage, AIMessage, LLMResult, UUID, SystemMessage
 from langchain.callbacks.base import BaseCallbackHandler
 
 
 class PromptLayerHandler(BaseCallbackHandler):
-    # TODO: Do I need this function?
-    # def on_chat_model_start(serialized, messages, run_id, parent_run_id)
+    def _default_params(self):
+        # TODO: Should I be setting defaults here for PL?
+        return {
+            "model_name": "gpt-4",
+            "temperature": 0,
+        }
+
+
+    def _convert_message_to_dict(self, message):
+        if isinstance(message, HumanMessage):
+            message_dict = {"role": "user", "content": message.content}
+        elif isinstance(message, AIMessage):
+            message_dict = {"role": "assistant", "content": message.content}
+        elif isinstance(message, SystemMessage):
+            message_dict = {"role": "system", "content": message.content}
+        else:
+            raise ValueError(f"Got unknown type {message}")
+        if "name" in message.additional_kwargs:
+            message_dict["name"] = message.additional_kwargs["name"]
+        return message_dict
+
+
+    def _create_message_dicts(self, messages):
+        params: Dict[str, Any] = self._default_params()
+        message_dicts = [self._convert_message_to_dict(m) for m in messages[0]]
+        return message_dicts, params
+
+
+    def on_chat_model_start(
+        self,
+        serialized: Dict[str, Any],
+        messages: List[List[BaseMessage]],
+        *,
+        run_id: UUID,
+        parent_run_id: Optional[UUID] = None,
+        tags: Optional[List[str]] = None,
+        **kwargs: Any,
+    ) -> Any:
+        self.request_start_time = datetime.datetime.now().timestamp()
+
+        self.messages = messages
+        self.invocation_params = kwargs.get('invocation_params', {})
+        self.name = self.invocation_params.get('_type', 'No Type')
 
     def __init__(self, request_id_func=None, pl_tags=[]):
         self.request_id_func = request_id_func
@@ -35,7 +76,6 @@ class PromptLayerHandler(BaseCallbackHandler):
         self.invocation_params = kwargs.get('invocation_params', {})
         self.name = self.invocation_params.get('_type', 'No Type')
 
-
     def on_llm_end(self, response: LLMResult, **kwargs: Any) -> Any:
         self.request_end_time = datetime.datetime.now().timestamp()
 
@@ -46,13 +86,40 @@ class PromptLayerHandler(BaseCallbackHandler):
                 "text": generation.text,
                 "llm_output": response.llm_output,
             }
+
+            if self.name == 'openai-chat':
+                function_name = f"openai.ChatCompletion.create"
+                message_dicts, model_params = self._create_message_dicts(self.messages)
+                model_input = []
+                model_response, model_params = self._create_message_dicts([[generation.message]])
+                model_response = {
+                    "choices": [
+                        {
+                            "finish_reason": "stop",
+                            "index": 0,
+                            "message": {
+                                "content": generation.message.content,
+                                "role": "assistant"
+                            }
+                        }
+                    ],
+                    "usage": response.llm_output.get('token_usage', {'prompt_tokens': 0, 'completion_tokens': 0, 'total_tokens': 0})
+                }
+                model_params['messages'] = message_dicts
+            else:
+                function_name = f"langchain.OpenAI"
+                model_input = [self.prompts[i]]
+                model_params = self.invocation_params
+                model_response = resp
+
+            # import pdb; pdb.set_trace()
             pl_request_id = promptlayer_api_request(
-                f"langchain.{self.name}",
+                function_name, # TODO: should be self.name but would break promptlayerUI. Should add catchall
                 "langchain",
-                [self.prompts[i]],
-                self.invocation_params,
+                model_input,
+                model_params,
                 self.pl_tags,
-                resp,
+                model_response,
                 self.request_start_time,
                 self.request_end_time,
                 get_api_key(),
@@ -108,5 +175,6 @@ chat_llm = ChatOpenAI(
     )]
 )
 llm_results = chat_llm([
-    HumanMessage(content="What comes after 1,2,3 ?")
+    HumanMessage(content="What comes after 1,2,3 ?"),
+    HumanMessage(content="Tell me another joke?"),
 ])
