3bbf9ae70f1e6ab245f1dfc9b06ae74fda74de54
Harrison Chase <hw.chase.17@gmail.com>
2023-06-13 18:44:38 -0700

    cr

--- langchain/agents/openai_functions_agent/base.py
+++ langchain/agents/openai_functions_agent/base.py
@@ -170,7 +170,7 @@ class OpenAIFunctionsAgent(BaseSingleActionAgent):
         )
         messages = prompt.to_messages()
         predicted_message = self.llm.predict_messages(
-            messages, functions=self.functions
+            messages, functions=self.functions, callbacks=callbacks
         )
         agent_decision = _parse_ai_message(predicted_message)
         return agent_decision
