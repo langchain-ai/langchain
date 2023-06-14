66d80aa12374eb5534ccc6ece63374e4a8c82319
Harrison Chase <hw.chase.17@gmail.com>
2023-06-14 16:40:39 -0700

    cr

--- langchain/agents/agent_toolkits/json/base.py
+++ langchain/agents/agent_toolkits/json/base.py
@@ -1,30 +1,22 @@
 """Json agent."""
 from typing import Any, Dict, List, Optional
 
-from langchain.agents.agent import AgentExecutor, BaseSingleActionAgent
-from langchain.agents.agent_toolkits.json.prompt import (
-    JSON_PREFIX,
-    JSON_SUFFIX,
-    JSON_SUFFIX_FUNCTIONS,
-)
+from langchain.agents.agent import AgentExecutor
+from langchain.agents.agent_toolkits.json.prompt import JSON_PREFIX, JSON_SUFFIX
 from langchain.agents.agent_toolkits.json.toolkit import JsonToolkit
 from langchain.agents.mrkl.base import ZeroShotAgent
 from langchain.agents.mrkl.prompt import FORMAT_INSTRUCTIONS
-from langchain.agents.openai_functions_agent.base import OpenAIFunctionsAgent
-from langchain.agents.types import AgentType
 from langchain.base_language import BaseLanguageModel
 from langchain.callbacks.base import BaseCallbackManager
 from langchain.chains.llm import LLMChain
-from langchain.schema import SystemMessage
 
 
 def create_json_agent(
     llm: BaseLanguageModel,
     toolkit: JsonToolkit,
-    prefix: Optional[str] = None,
-    suffix: Optional[str] = None,
-    agent_type: AgentType = AgentType.ZERO_SHOT_REACT_DESCRIPTION,
     callback_manager: Optional[BaseCallbackManager] = None,
+    prefix: str = JSON_PREFIX,
+    suffix: str = JSON_SUFFIX,
     format_instructions: str = FORMAT_INSTRUCTIONS,
     input_variables: Optional[List[str]] = None,
     verbose: bool = False,
@@ -33,37 +25,20 @@ def create_json_agent(
 ) -> AgentExecutor:
     """Construct a json agent from an LLM and tools."""
     tools = toolkit.get_tools()
-    prefix = prefix or JSON_PREFIX
-    agent: BaseSingleActionAgent
-    if agent_type == AgentType.ZERO_SHOT_REACT_DESCRIPTION:
-        suffix = suffix or JSON_SUFFIX
-        prompt = ZeroShotAgent.create_prompt(
-            tools,
-            prefix=prefix,
-            suffix=suffix,
-            format_instructions=format_instructions,
-            input_variables=input_variables,
-        )
-        llm_chain = LLMChain(
-            llm=llm,
-            prompt=prompt,
-            callback_manager=callback_manager,
-        )
-        tool_names = [tool.name for tool in tools]
-        agent = ZeroShotAgent(llm_chain=llm_chain, allowed_tools=tool_names, **kwargs)
-    elif agent_type == AgentType.OPENAI_FUNCTIONS:
-        suffix = suffix or JSON_SUFFIX_FUNCTIONS
-        system_message = SystemMessage(content=prefix + suffix)
-        _prompt = OpenAIFunctionsAgent.create_prompt(system_message=system_message)
-        agent = OpenAIFunctionsAgent(
-            llm=llm,
-            prompt=_prompt,
-            tools=tools,
-            callback_manager=callback_manager,
-            **kwargs,
-        )
-    else:
-        raise ValueError(f"Agent type {agent_type} not supported at the moment.")
+    prompt = ZeroShotAgent.create_prompt(
+        tools,
+        prefix=prefix,
+        suffix=suffix,
+        format_instructions=format_instructions,
+        input_variables=input_variables,
+    )
+    llm_chain = LLMChain(
+        llm=llm,
+        prompt=prompt,
+        callback_manager=callback_manager,
+    )
+    tool_names = [tool.name for tool in tools]
+    agent = ZeroShotAgent(llm_chain=llm_chain, allowed_tools=tool_names, **kwargs)
     return AgentExecutor.from_agent_and_tools(
         agent=agent,
         tools=tools,
--- langchain/agents/agent_toolkits/json/prompt.py
+++ langchain/agents/agent_toolkits/json/prompt.py
@@ -5,7 +5,7 @@ Your goal is to return a final answer by interacting with the JSON.
 You have access to the following tools which help you learn more about the JSON you are interacting with.
 Only use the below tools. Only use the information returned by the below tools to construct your final answer.
 Do not make up any information that is not contained in the JSON.
-Your input to the tools should be in the form of `data["key"][0]` where `data` is the JSON blob you are interacting with, and the syntax used is Python.
+Your input to the tools should be in the form of `data["key"][0]` where `data` is the JSON blob you are interacting with, and the syntax used is Python. 
 You should only use keys that you know for a fact exist. You must validate that a key exists by seeing it previously when calling `json_spec_list_keys`. 
 If you have not seen a key in one of those responses, you cannot use it.
 You should only add one key at a time to the path. You cannot add multiple keys at once.
@@ -23,7 +23,3 @@ JSON_SUFFIX = """Begin!"
 Question: {input}
 Thought: I should look at the keys that exist in data to see what I have access to
 {agent_scratchpad}"""
-
-JSON_SUFFIX_FUNCTIONS = """
-You should always start by looking at the keys that exist in data to see what you have access to.
-"""
