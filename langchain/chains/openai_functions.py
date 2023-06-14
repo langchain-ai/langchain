429c2ed78c2e7b1f382f73da7e9b81bf1c60565c
Francisco Ingham <24279597+fpingham@users.noreply.github.com>
2023-06-14 20:58:28 -0300

    first stab at functions chain

--- langchain/chains/openai_functions.py
+++ langchain/chains/openai_functions.py
@@ -1,9 +1,11 @@
-from typing import Dict, Any, Optional, List
-
-from langchain.callbacks.manager import AsyncCallbackManagerForChainRun, CallbackManagerForChainRun
-from langchain.chains.base import Chain
+from typing import Any, Dict, List, Optional
 
 from langchain.base_language import BaseLanguageModel
+from langchain.callbacks.manager import (
+    AsyncCallbackManagerForChainRun,
+    CallbackManagerForChainRun,
+)
+from langchain.chains.base import Chain
 from langchain.prompts.base import BasePromptTemplate
 
 
@@ -14,11 +16,31 @@ class OpenAIFunctionsChain(Chain):
 
     @property
     def input_keys(self) -> List[str]:
-        return self.prompt.input_variables + ["__tools__"]
+        return self.prompt.input_variables + ["entity_schema"]
 
     @property
     def output_keys(self) -> List[str]:
         return ["output"]
+    
+    def _get_functions(self, entity_schema): 
+        entity_name = entity_schema['entity_name']
+        properties = entity_schema['properties']
+        required = entity_schema['required']
+
+        func = {}
+        func['name'] = f"get_{entity_name}_info"
+        func['description'] = f"Use this function to save the information of each {entity_name} that were mentioned in the passage"
+
+        parameters = {"type": "object"}
+        parameters["required"] = required
+        parameters["properties"] = {}
+
+        for k,v in properties.items():
+            parameters["properties"][k] = {'title': k, 'type': v}
+
+        func['parameters'] = parameters
+
+        return func
 
     def _call(self, inputs: Dict[str, Any], run_manager: Optional[CallbackManagerForChainRun] = None) -> Dict[str, Any]:
         _inputs = {k: v for k,v in inputs.items() if k in self.prompt.input_variables}
@@ -26,8 +48,9 @@ class OpenAIFunctionsChain(Chain):
         messages = prompt.to_messages()
         _run_manager = run_manager or CallbackManagerForChainRun.get_noop_manager()
         callbacks = _run_manager.get_child()
+        function = self._get_functions(inputs['entity_schema'])
         predicted_message = self.llm.predict_messages(
-            messages, functions=inputs["__tools__"], callbacks=callbacks
+            messages, functions=[function], callbacks=callbacks
         )
         return {"output": predicted_message}
 
@@ -38,7 +61,8 @@ class OpenAIFunctionsChain(Chain):
         messages = prompt.to_messages()
         _run_manager = run_manager or AsyncCallbackManagerForChainRun.get_noop_manager()
         callbacks = _run_manager.get_child()
+        function = self._get_functions(inputs['entity_schema'])
         predicted_message = await self.llm.apredict_messages(
-            messages, functions=inputs["__tools__"], callbacks=callbacks
+            messages, functions=[function], callbacks=callbacks
         )
         return {"output": predicted_message}
