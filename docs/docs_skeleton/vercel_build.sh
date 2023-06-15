c47109aed1efccfabce20d2d635d36cfb63ba32b
Dev 2049 <dev.dev2049@gmail.com>
2023-06-15 15:19:11 -0700

    poetry

--- docs/docs_skeleton/vercel_build.sh
+++ docs/docs_skeleton/vercel_build.sh
@@ -7,7 +7,7 @@ source .venv/bin/activate
 pip install -r requirements.txt
 mkdir -p docs_skeleton/static/api_reference
 cd api_reference
-poetry run make html
+make html
 cp -r _build/* ../docs_skeleton/static/api_reference
 cd ..
 cp -r extras/* docs_skeleton/docs
