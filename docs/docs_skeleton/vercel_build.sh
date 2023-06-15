8cce2f624f021f70f2796af9b1c2f2bd8e2a6dc5
Dev 2049 <dev.dev2049@gmail.com>
2023-06-15 16:18:19 -0700

    wip

--- docs/docs_skeleton/vercel_build.sh
+++ docs/docs_skeleton/vercel_build.sh
@@ -3,18 +3,17 @@
 cd ..
 
 curl https://pyenv.run | bash
-
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 source ~/.bashrc
 
-pyenv install 3.10
-pyenv global 3.10
-python --version
-python -m venv .venv
+pyenv install 3.9
+pyenv global 3.9
+python3 --version
+python3 -m venv .venv
 source .venv/bin/activate
-python -m pip install -r requirements.txt
+python3 -m pip3 install -r requirements.txt
 mkdir -p docs_skeleton/static/api_reference
 cd api_reference
 make html
