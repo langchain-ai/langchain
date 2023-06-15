7007f9f4e34743575b0252bf8a7215191b2c39ae
Vinay Kakade <vinaykakade@gmail.com>
2023-06-15 13:11:59 +0530

    Update infino callback

--- langchain/callbacks/infino_callback.py
+++ langchain/callbacks/infino_callback.py
@@ -13,8 +13,8 @@ def import_infino() -> Any:
     except ImportError:
         raise ImportError(
             "To use the Infino callbacks manager you need to have the"
-            " `infino` python package installed."
-            "Please install it with from Infino's repo."
+            " `infinopy` python package installed."
+            "Please install it with `pip install infinopy`"
         )
     return InfinoClient()
 
@@ -22,52 +22,100 @@ def import_infino() -> Any:
 class InfinoCallbackHandler(BaseCallbackHandler):
     """Callback Handler that logs to infino."""
 
-    def __init__(self) -> None:
+    def __init__(
+        self,
+        model_id: Optional[str] = None,
+        model_version: Optional[str] = None,
+        verbose=False,
+    ) -> None:
         # Set infino client
         self.client = import_infino()
+        self.model_id = model_id
+        self.model_version = model_version
+        self.verbose = verbose
+
+    def _send_to_infino(self, key, value, is_ts=True):
+        """Send the key-value to Infino.
+
+        Parameters:
+        key (str): the key to send to Infino.
+        value (Any): the value to send to Infino.
+        is_ts (bool): is True, the value is part of a time series, else it is sent as a log message.
+        """
+        payload = {
+            "date": int(time.time()),
+            key: value,
+            "labels": {
+                "model_id": self.model_id,
+                "model_version": self.model_version,
+            },
+        }
+        if self.verbose:
+            print(f"Tracking {key} with Infino: {payload}")
+
+        # Append to Infino time series only if is_ts is True, otherwise append to Infino log.
+        if is_ts:
+            self.client.append_ts(payload)
+        else:
+            self.client.append_log(payload)
 
     def on_llm_start(
         self, serialized: Dict[str, Any], prompts: List[str], **kwargs: Any
     ) -> None:
-        """Log the prompts to infino."""
-        self.tokens_stream = []
+        """Log the prompts to infino. and set start time and error flag"""
+
         for prompt in prompts:
-            payload = {"date": int(time.time()), "prompt_question": prompt}
-            self.client.append_log(payload)
+            self._send_to_infino("prompt", prompt, is_ts=False)
+
+        # Set the error flag to indicate no error (this will get overridden in on_llm_error if an error occurs).
+        self.error = 0
+
+        # Set the start time (so that we can calculate the request duration in on_llm_end).
+        self.start_time = time.time()
 
     def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
-        """Need to register new token"""
-        self.tokens_stream += token
+        """Do nothing when a new token is generated."""
+        pass
 
     def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
-        """Log the response to infino."""
-        payload = {
-            "date": int(time.time()),
-            "prompt_response": self.tokens_stream.join(" "),
-        }
-        self.client.append_log(payload)
-        pass
+        """Log the latency, error, token usage and response to Infino."""
+
+        # Calculate and track the request latency.
+        self.end_time = time.time()
+        duration = self.end_time - self.start_time
+        self._send_to_infino("latency", duration)
+
+        # Track success or error flag.
+        self._send_to_infino("error", self.error)
+
+        # Track token usage.
+        token_usage = response.llm_output["token_usage"]
+        total_tokens = response.llm_output["token_usage"]["total_tokens"]
+        completion_tokens = response.llm_output["token_usage"]["completion_tokens"]
+        self._send_to_infino("prompt_tokens", token_usage["prompt_tokens"])
+        self._send_to_infino("total_tokens", token_usage["total_tokens"])
+        self._send_to_infino("completion_tokens", token_usage["completion_tokens"])
+
+        # Track prompt reponse.
+        for generations in response.generations:
+            for generation in generations:
+                self._send_to_infino("prompt_response", generation.text, is_ts=False)
 
     def on_llm_error(
         self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
     ) -> None:
-        """Need to log the error."""
-        payload = {
-            "date": int(time.time()),
-            "error": error,
-            "labels": {"model": self.chain_name},
-        }
-        self.client.append_ts(payload)
+        """Set the error flag."""
+        self.error = 1
 
     def on_chain_start(
         self, serialized: Dict[str, Any], inputs: Dict[str, Any], **kwargs: Any
     ) -> None:
-        """Need to store the chain name."""
-        class_name = serialized["name"]
-        self.chain_name = class_name
+        """Do nothing when LLM chain starts."""
+        pass
 
     def on_chain_end(self, outputs: Dict[str, Any], **kwargs: Any) -> None:
-        """Print out that we finished a chain."""
+        """Do nothing when LLM chain ends."""
+        pass
 
     def on_chain_error(
         self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
@@ -81,11 +129,11 @@ class InfinoCallbackHandler(BaseCallbackHandler):
         input_str: str,
         **kwargs: Any,
     ) -> None:
-        """Print out the log in specified color."""
+        """Do nothing when tool starts."""
         pass
 
     def on_agent_action(self, action: AgentAction, **kwargs: Any) -> Any:
-        """Do nothing."""
+        """Do nothing when agent takes a specific action."""
         pass
 
     def on_tool_end(
@@ -95,13 +143,13 @@ class InfinoCallbackHandler(BaseCallbackHandler):
         llm_prefix: Optional[str] = None,
         **kwargs: Any,
     ) -> None:
-        """Do nothing."""
+        """Do nothing when tool ends."""
         pass
 
     def on_tool_error(
         self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
     ) -> None:
-        """Do nothing."""
+        """Do nothing when tool outputs an error."""
         pass
 
     def on_text(self, text: str, **kwargs: Any) -> None:
