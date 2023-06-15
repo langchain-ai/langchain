24684c4572e21a4a5373326ceb21cc5e7b424414
Harrison Chase <hw.chase.17@gmail.com>
2023-06-14 22:36:45 -0700

    cr

--- langchain/chains/openai_functions.py
+++ langchain/chains/openai_functions.py
@@ -1,3 +1,4 @@
+import json
 from typing import Any, Dict, List, Optional
 
 from langchain.base_language import BaseLanguageModel
@@ -6,14 +7,28 @@ from langchain.callbacks.manager import (
     CallbackManagerForChainRun,
 )
 from langchain.chains.base import Chain
+from langchain.chains.sequential import SimpleSequentialChain
+from langchain.chains.transform import TransformChain
 from langchain.prompts.base import BasePromptTemplate
+from langchain.prompts.chat import ChatPromptTemplate
+
+
+def _parse_tag(inputs: dict) -> dict:
+    message = inputs["input"]
+    args = message.additional_kwargs["function_call"]["arguments"]
+    return {"output": json.loads(args)}
+
+
+def _parse_entities(inputs: dict) -> dict:
+    message = inputs["input"]
+    args = message.additional_kwargs["function_call"]["arguments"]
+    return {"output": json.loads(args)["info"]}
 
 
 class OpenAIFunctionsChain(Chain):
     prompt: BasePromptTemplate
     llm: BaseLanguageModel
-    entity_schema: Dict[Any, Any]
-    tagging: bool = True
+    functions: List[Dict]
 
     @property
     def input_keys(self) -> List[str]:
@@ -22,36 +37,6 @@ class OpenAIFunctionsChain(Chain):
     @property
     def output_keys(self) -> List[str]:
         return ["output"]
-
-    def _get_functions(self):
-        properties = self.entity_schema["properties"]
-        required = self.entity_schema["required"]
-
-        func = {}
-        if self.tagging:
-            func["name"] = "entity_tagging"
-            func[
-                "description"
-            ] = "Saves the relevant information for each entity that was mentioned in the passage."
-        else:
-            func["name"] = "information_extraction"
-            func["description"] = "Extracts the relevant information from the passage."
-
-        parameters = {"type": "object"}
-        parameters["required"] = required
-        parameters["properties"] = {}
-
-        for k, v in properties.items():
-            if self.tagging and not isinstance(v, list):
-                # tag all the ocurrences it finds
-                parameters["properties"][k] = {"title": k, "type": [v]}
-            else:
-                # extract only one occurrence
-                parameters["properties"][k] = {"title": k, "type": v}
-
-        func["parameters"] = parameters
-
-        return func
 
     def _call(
         self,
@@ -63,9 +48,8 @@ class OpenAIFunctionsChain(Chain):
         messages = prompt.to_messages()
         _run_manager = run_manager or CallbackManagerForChainRun.get_noop_manager()
         callbacks = _run_manager.get_child()
-        function = self._get_functions()
         predicted_message = self.llm.predict_messages(
-            messages, functions=[function], callbacks=callbacks
+            messages, functions=self.functions, callbacks=callbacks
         )
         return {"output": predicted_message}
 
@@ -79,8 +63,79 @@ class OpenAIFunctionsChain(Chain):
         messages = prompt.to_messages()
         _run_manager = run_manager or AsyncCallbackManagerForChainRun.get_noop_manager()
         callbacks = _run_manager.get_child()
-        function = self._get_functions()
         predicted_message = await self.llm.apredict_messages(
-            messages, functions=[function], callbacks=callbacks
+            messages, functions=self.functions, callbacks=callbacks
         )
         return {"output": predicted_message}
+
+
+def _convert_schema(schema: dict) -> dict:
+    props = {k: {"title": k, "type": v} for k, v in schema["properties"].items()}
+    return {
+        "type": "object",
+        "properties": props,
+        "required": schema.get("required", []),
+    }
+
+
+def _get_extraction_functions(entity_schema: dict) -> List[dict]:
+    return [
+        {
+            "name": "information_extraction",
+            "description": "Extracts the relevant information from the passage.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "info": {"type": "array", "items": _convert_schema(entity_schema)}
+                },
+                "required": ["info"],
+            },
+        }
+    ]
+
+
+def _get_tagging_functions(schema: dict) -> List[dict]:
+    return [
+        {
+            "name": "information_extraction",
+            "description": "Extracts the relevant information from the passage.",
+            "parameters": _convert_schema(schema),
+        }
+    ]
+
+
+_EXTRACTION_TEMPLATE = """Extract and save the relevant entities mentioned\
+ in the following passage together with their properties.
+
+Passage:
+{input}
+"""
+
+
+def create_extraction_chain(schema: dict, llm: BaseLanguageModel) -> Chain:
+    functions = _get_extraction_functions(schema)
+    prompt = ChatPromptTemplate.from_template(_EXTRACTION_TEMPLATE)
+    chain = OpenAIFunctionsChain(llm=llm, prompt=prompt, functions=functions)
+    parsing_chain = TransformChain(
+        transform=_parse_entities,
+        input_variables=["input"],
+        output_variables=["output"],
+    )
+    return SimpleSequentialChain(chains=[chain, parsing_chain])
+
+
+_TAGGING_TEMPLATE = """Extract the desired information from the following passage.
+
+Passage:
+{input}
+"""
+
+
+def create_tagging_chain(schema: dict, llm: BaseLanguageModel) -> Chain:
+    functions = _get_tagging_functions(schema)
+    prompt = ChatPromptTemplate.from_template(_TAGGING_TEMPLATE)
+    chain = OpenAIFunctionsChain(llm=llm, prompt=prompt, functions=functions)
+    parsing_chain = TransformChain(
+        transform=_parse_tag, input_variables=["input"], output_variables=["output"]
+    )
+    return SimpleSequentialChain(chains=[chain, parsing_chain])
