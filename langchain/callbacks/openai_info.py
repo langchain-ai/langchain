13e5de3e80f68e035f655eb6e0b5196fdc11a5b7
rajib76 <rajib76@yahoo.com>
2023-06-15 23:46:48 -0700

    Added gpt-35-turbo-base model for Azure

--- langchain/callbacks/openai_info.py
+++ langchain/callbacks/openai_info.py
@@ -4,7 +4,6 @@ from typing import Any, Dict, List, Optional, Union
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain.schema import AgentAction, AgentFinish, LLMResult
 
-# Added the model gpt-35-turbo. Azure Open AI base model name is gpt-35-turbo
 MODEL_COST_PER_1K_TOKENS = {
     "gpt-4": 0.03,
     "gpt-4-0314": 0.03,
