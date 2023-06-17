c45ead3f7a62f0ef35389fdee9f8d73a658137bb
Harrison Chase <hw.chase.17@gmail.com>
2023-06-17 09:43:25 -0700

    cr

--- tests/unit_tests/agents/test_serialization.py
+++ tests/unit_tests/agents/test_serialization.py
@@ -3,8 +3,8 @@ from tempfile import TemporaryDirectory
 
 from langchain.agents.agent_types import AgentType
 from langchain.agents.initialize import initialize_agent, load_agent
-from langchain.llms.fake import FakeListLLM
 from langchain.agents.tools import Tool
+from langchain.llms.fake import FakeListLLM
 
 
 def test_mrkl_serialization() -> None:
