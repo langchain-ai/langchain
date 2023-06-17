8cba8211b68ff8e323ab9193e8cdc345e9c8fa2a
rajib76 <rajib76@yahoo.com>
2023-06-16 18:38:29 -0700

    added gpt-35-turbo for Azure Open AI in the opean ai callback

--- langchain/callbacks/openai_info.py
+++ langchain/callbacks/openai_info.py
@@ -15,6 +15,7 @@ MODEL_COST_PER_1K_TOKENS = {
     "gpt-4-32k-0314-completion": 0.12,
     "gpt-3.5-turbo": 0.002,
     "gpt-3.5-turbo-0301": 0.002,
+    "gpt-35-turbo": 0.002,
     "text-ada-001": 0.0004,
     "ada": 0.0004,
     "text-babbage-001": 0.0005,
