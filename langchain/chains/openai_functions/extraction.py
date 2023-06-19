de8f66d9ecefc92f6605eb5f942e8857702aa9f6
Harrison Chase <hw.chase.17@gmail.com>
2023-06-18 23:13:42 -0700

    Harrison/refactor functions (#6408)

--- langchain/chains/openai_functions/extraction.py
+++ langchain/chains/openai_functions/extraction.py
@@ -8,6 +8,7 @@ from langchain.chains.llm import LLMChain
 from langchain.chains.openai_functions.utils import (
     _convert_schema,
     _resolve_schema_references,
+    get_llm_kwargs,
 )
 from langchain.output_parsers.openai_functions import (
     JsonKeyOutputFunctionsParser,
@@ -15,24 +16,19 @@ from langchain.output_parsers.openai_functions import (
 )
 from langchain.prompts import ChatPromptTemplate
 
-EXTRACTION_NAME = "information_extraction"
-EXTRACTION_KWARGS = {"function_call": {"name": "information_extraction"}}
 
-
-def _get_extraction_functions(entity_schema: dict) -> List[dict]:
-    return [
-        {
-            "name": EXTRACTION_NAME,
-            "description": "Extracts the relevant information from the passage.",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "info": {"type": "array", "items": _convert_schema(entity_schema)}
-                },
-                "required": ["info"],
+def _get_extraction_function(entity_schema: dict) -> dict:
+    return {
+        "name": "information_extraction",
+        "description": "Extracts the relevant information from the passage.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "info": {"type": "array", "items": _convert_schema(entity_schema)}
             },
-        }
-    ]
+            "required": ["info"],
+        },
+    }
 
 
 _EXTRACTION_TEMPLATE = """Extract and save the relevant entities mentioned\
@@ -44,13 +40,14 @@ Passage:
 
 
 def create_extraction_chain(schema: dict, llm: BaseLanguageModel) -> Chain:
-    functions = _get_extraction_functions(schema)
+    function = _get_extraction_function(schema)
     prompt = ChatPromptTemplate.from_template(_EXTRACTION_TEMPLATE)
     output_parser = JsonKeyOutputFunctionsParser(key_name="info")
+    llm_kwargs = get_llm_kwargs(function)
     chain = LLMChain(
         llm=llm,
         prompt=prompt,
-        llm_kwargs={**{"functions": functions}, **EXTRACTION_KWARGS},
+        llm_kwargs=llm_kwargs,
         output_parser=output_parser,
     )
     return chain
@@ -67,15 +64,16 @@ def create_extraction_chain_pydantic(
         openai_schema, openai_schema["definitions"]
     )
 
-    functions = _get_extraction_functions(openai_schema)
+    function = _get_extraction_function(openai_schema)
     prompt = ChatPromptTemplate.from_template(_EXTRACTION_TEMPLATE)
     output_parser = PydanticAttrOutputFunctionsParser(
         pydantic_schema=PydanticSchema, attr_name="info"
     )
+    llm_kwargs = get_llm_kwargs(function)
     chain = LLMChain(
         llm=llm,
         prompt=prompt,
-        llm_kwargs={**{"functions": functions}, **EXTRACTION_KWARGS},
+        llm_kwargs=llm_kwargs,
         output_parser=output_parser,
     )
     return chain
