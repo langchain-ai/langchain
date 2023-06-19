a6d582d9012787843519e772eb7164abf01c952a
Hakan Tekgul <tekgul2@illinois.edu>
2023-06-19 10:16:22 -0700

    Update arize_callback.py

--- langchain/callbacks/arize_callback.py
+++ langchain/callbacks/arize_callback.py
@@ -21,7 +21,7 @@ class ArizeCallbackHandler(BaseCallbackHandler):
         API_KEY: Optional[str] = None,
     ) -> None:
         """Initialize callback handler."""
-        
+
         super().__init__()
         self.model_id = model_id
         self.model_version = model_version
@@ -31,20 +31,26 @@ class ArizeCallbackHandler(BaseCallbackHandler):
         self.response_records: List[str] = []
         self.prediction_ids: List[str] = []
         self.pred_timestamps: List[int] = []
-        self.response_embeddings: List[float] = []  
-        self.prompt_embeddings: List[float] = [] 
-        self.prompt_tokens = 0 
-        self.completion_tokens = 0 
+        self.response_embeddings: List[float] = []
+        self.prompt_embeddings: List[float] = []
+        self.prompt_tokens = 0
+        self.completion_tokens = 0
         self.total_tokens = 0
 
         from arize.pandas.logger import Client
         from arize.pandas.embeddings import EmbeddingGenerator, UseCases
 
-
-        self.generator = EmbeddingGenerator.from_use_case(use_case=UseCases.NLP.SEQUENCE_CLASSIFICATION,model_name="distilbert-base-uncased",tokenizer_max_length=512,batch_size=256)
+        self.generator = EmbeddingGenerator.from_use_case(
+            use_case=UseCases.NLP.SEQUENCE_CLASSIFICATION,
+            model_name="distilbert-base-uncased",
+            tokenizer_max_length=512,
+            batch_size=256,
+        )
         self.arize_client = Client(space_key=SPACE_KEY, api_key=API_KEY)
-        if SPACE_KEY == "SPACE_KEY" or API_KEY == "API_KEY": raise ValueError("❌ CHANGE SPACE AND API KEYS")
-        else: print("✅ Arize client setup done! Now you can start using Arize!")
+        if SPACE_KEY == "SPACE_KEY" or API_KEY == "API_KEY":
+            raise ValueError("❌ CHANGE SPACE AND API KEYS")
+        else:
+            print("✅ Arize client setup done! Now you can start using Arize!")
 
     def on_llm_start(
         self, serialized: Dict[str, Any], prompts: List[str], **kwargs: Any
@@ -57,54 +63,96 @@ class ArizeCallbackHandler(BaseCallbackHandler):
         pass
 
     def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
-
         pd = import_pandas()
-        from arize.utils.types import EmbeddingColumnNames, Environments, ModelTypes, Schema
+        from arize.utils.types import (
+            EmbeddingColumnNames,
+            Environments,
+            ModelTypes,
+            Schema,
+        )
 
         # Safe check if 'llm_output' and 'token_usage' exist
-        if response.llm_output and 'token_usage' in response.llm_output:
-            self.prompt_tokens = response.llm_output['token_usage'].get('prompt_tokens', 0)
-            self.total_tokens = response.llm_output['token_usage'].get('total_tokens', 0)
-            self.completion_tokens = response.llm_output['token_usage'].get('completion_tokens', 0)
+        if response.llm_output and "token_usage" in response.llm_output:
+            self.prompt_tokens = response.llm_output["token_usage"].get(
+                "prompt_tokens", 0
+            )
+            self.total_tokens = response.llm_output["token_usage"].get(
+                "total_tokens", 0
+            )
+            self.completion_tokens = response.llm_output["token_usage"].get(
+                "completion_tokens", 0
+            )
         else:
-            self.prompt_tokens = self.total_tokens = self.completion_tokens = 0  # assign default value
+            self.prompt_tokens = (
+                self.total_tokens
+            ) = self.completion_tokens = 0  # assign default value
 
         i = 0
 
         for generations in response.generations:
             for generation in generations:
                 prompt = self.prompt_records[i]
-                prompt_embedding = pd.Series(self.generator.generate_embeddings(text_col=pd.Series(prompt.replace("\n", " "))).reset_index(drop=True))
+                prompt_embedding = pd.Series(
+                    self.generator.generate_embeddings(
+                        text_col=pd.Series(prompt.replace("\n", " "))
+                    ).reset_index(drop=True)
+                )
 
                 # Assigning text to response_text instead of response
                 response_text = generation.text.replace("\n", " ")
-                response_embedding = pd.Series(self.generator.generate_embeddings(text_col=pd.Series(generation.text.replace("\n", " "))).reset_index(drop=True))
+                response_embedding = pd.Series(
+                    self.generator.generate_embeddings(
+                        text_col=pd.Series(generation.text.replace("\n", " "))
+                    ).reset_index(drop=True)
+                )
                 pred_id = str(uuid.uuid4())
                 pred_timestamp = datetime.now().timestamp()
 
                 # Define the columns and data
-                columns = ['prediction_ts', 'response', 'prompt', 'response_vector', 'prompt_vector', 'prompt_token', 'completion_token', 'total_token']
-                data = [[pred_timestamp, response_text, prompt, response_embedding[0], prompt_embedding[0], self.prompt_tokens, self.total_tokens, self.completion_tokens]]
+                columns = [
+                    "prediction_ts",
+                    "response",
+                    "prompt",
+                    "response_vector",
+                    "prompt_vector",
+                    "prompt_token",
+                    "completion_token",
+                    "total_token",
+                ]
+                data = [
+                    [
+                        pred_timestamp,
+                        response_text,
+                        prompt,
+                        response_embedding[0],
+                        prompt_embedding[0],
+                        self.prompt_tokens,
+                        self.total_tokens,
+                        self.completion_tokens,
+                    ]
+                ]
 
                 # Create the DataFrame
                 df = pd.DataFrame(data, columns=columns)
 
                 # Declare prompt and response columns
-                prompt_columns=EmbeddingColumnNames(
-                    vector_column_name="prompt_vector",
-                    data_column_name="prompt"
+                prompt_columns = EmbeddingColumnNames(
+                    vector_column_name="prompt_vector", data_column_name="prompt"
                 )
 
-                response_columns=EmbeddingColumnNames(
-                    vector_column_name="response_vector",
-                    data_column_name="response"
+                response_columns = EmbeddingColumnNames(
+                    vector_column_name="response_vector", data_column_name="response"
                 )
 
                 schema = Schema(
-                timestamp_column_name="prediction_ts",
-                tag_column_names=["prompt_token", "completion_token","total_token"],
-                prompt_column_names=prompt_columns,
-                response_column_names=response_columns,
+                    timestamp_column_name="prediction_ts",
+                    tag_column_names=[
+                        "prompt_token",
+                        "completion_token",
+                        "total_token",
+                    ],
+                    prompt_column_names=prompt_columns,
+                    response_column_names=response_columns,
                 )
 
                 response_from_arize = self.arize_client.log(
@@ -113,7 +161,7 @@ class ArizeCallbackHandler(BaseCallbackHandler):
                     model_id=self.model_id,
                     model_version=self.model_version,
                     model_type=ModelTypes.GENERATIVE_LLM,
-                    environment=Environments.PRODUCTION
+                    environment=Environments.PRODUCTION,
                 )
                 if response_from_arize.status_code == 200:
                     print(f"✅ Successfully logged data to Arize!")
@@ -121,9 +169,8 @@ class ArizeCallbackHandler(BaseCallbackHandler):
                     print(
                         f'❌ Logging failed with status code {response_from_arize.status_code} and message "{response_from_arize.text}"'
                     )
-                
-                i = i + 1
 
+                i = i + 1
 
     def on_llm_error(
         self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
