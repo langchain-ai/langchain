371852156f9026ab01124cb5c8a16489c4e2e417
Dev 2049 <dev.dev2049@gmail.com>
2023-06-21 12:23:09 -0700

    wip

--- docs/api_reference/conf.py
+++ docs/api_reference/conf.py
@@ -75,7 +75,7 @@ autodoc_default_options = {
 }
 autodoc_typehints = "description"
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+templates_path = ["templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
---
+++ docs/api_reference/create_api_rst.py
@@ -0,0 +1,53 @@
+import glob
+import re
+
+classes = {}
+for py in glob.glob("../../langchain/**/*.py", recursive=True):
+    mod = py[len("../../langchain/"):].split(".")[0].replace("/", ".")
+    first = mod.split(".")[0]
+    if first not in classes:
+        classes[first] = []
+    with open(py) as f:
+        for l in f.readlines():
+            found = re.findall(r"class (.*)\(", l)
+            classes[first].extend([mod + "." + c for c in found])
+
+
+full_doc = """\
+.. _api_ref:
+
+=============
+API Reference
+=============
+
+""" 
+for mod, clist in classes.items():
+    if not clist:
+        continue
+    cstring = "\n    ".join(clist)
+    mod_title = mod.replace("_", " ").title()
+    section = f":mod:`langchain.{mod}`: {mod_title}"
+    doc = f"""\
+{section}
+{'=' * (len(section) + 1)}
+
+.. automodule:: langchain.{mod}
+    :no-members:
+    :no-inherited-members:
+
+Classes
+--------------
+.. currentmodule:: langchain
+
+.. autosummary::
+    :toctree: {mod}
+    :template: class.rst
+
+    {cstring}
+
+"""
+    full_doc += doc
+    
+
+with open("./api_ref.rst", "w") as f:
+    f.write(full_doc)
