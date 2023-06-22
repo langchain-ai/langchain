52045d22a51b9b063d794344016afec48ad3f85b
Piyush Jain <piyushjain@duck.com>
2023-06-21 19:30:25 -0700

    Ran format and lint

--- langchain/retrievers/kendra.py
+++ langchain/retrievers/kendra.py
@@ -1,14 +1,16 @@
 import re
-from langchain.docstore.document import Document
 from typing import Any, Dict, List, Literal, Optional, Union
+
 from pydantic import BaseModel, Extra
 
+from langchain.docstore.document import Document
 from langchain.schema import BaseRetriever
 
 
 def clean_excerpt(res_text):
-    res = re.sub("\s+", " ", res_text).replace("...","")
+    res = re.sub("\s+", " ", res_text).replace("...", "")
     return res
+
 
 def combined_text(title: str, excerpt: str) -> str:
     return f"Document Title: {title} \nDocument Excerpt: \n{excerpt}\n"
@@ -44,26 +46,24 @@ class QueryResultItem(BaseModel, extra=Extra.allow):
     DocumentExcerpt: Optional[TextWithHighLights]
 
     def get_excerpt(self) -> str:
-        if(self.AdditionalAttributes and self.AdditionalAttributes[0].Key == "AnswerText"):
+        if (
+            self.AdditionalAttributes
+            and self.AdditionalAttributes[0].Key == "AnswerText"
+        ):
             excerpt = self.AdditionalAttributes[0].Value.Text
         else:
             excerpt = self.DocumentExcerpt.Text
 
         return clean_excerpt(excerpt)
-    
+
     def to_doc(self) -> Document:
         title = self.DocumentTitle.Text
         source = self.DocumentURI
         excerpt = self.get_excerpt()
         type = self.Type
         page_content = combined_text(title, excerpt)
-        metadata = {
-            "source": source, 
-            "title": title, 
-            "excerpt": excerpt, 
-            "type": type
-        }
-        return Document(page_content = page_content, metadata = metadata)
+        metadata = {"source": source, "title": title, "excerpt": excerpt, "type": type}
+        return Document(page_content=page_content, metadata=metadata)
 
 
 class QueryResult(BaseModel, extra=Extra.allow):
@@ -75,13 +75,14 @@ class QueryResult(BaseModel, extra=Extra.allow):
         docs = [self.ResultItems[i].to_doc() for i in range(0, count)]
 
         return docs
-    
+
 
 class DocumentAttributeValue(BaseModel, extra=Extra.allow):
     DateValue: Optional[str]
     LongValue: Optional[int]
     StringListValue: Optional[List[str]]
     StringValue: Optional[str]
+
 
 class DocumentAttribute(BaseModel, extra=Extra.allow):
     Key: str
@@ -104,12 +105,8 @@ class RetrieveResultItem(BaseModel, extra=Extra.allow):
         source = self.DocumentURI
         excerpt = self.get_excerpt()
         page_content = combined_text(title, excerpt)
-        metadata = {
-            "source": source, 
-            "title": title, 
-            "excerpt": excerpt
-        }
-        return Document(page_content = page_content, metadata = metadata)
+        metadata = {"source": source, "title": title, "excerpt": excerpt}
+        return Document(page_content=page_content, metadata=metadata)
 
 
 class RetrieveResult(BaseModel, extra=Extra.allow):
@@ -122,7 +119,7 @@ class RetrieveResult(BaseModel, extra=Extra.allow):
         docs = [self.ResultItems[i].to_doc() for i in range(0, count)]
 
         return docs
-    
+
 
 class AmazonKendraRetriever(BaseRetriever):
     """Retriever to query documents from Amazon Kendra Index.
@@ -156,27 +153,27 @@ class AmazonKendraRetriever(BaseRetriever):
     """Language code that the documents are indexed with.
     See: https://docs.aws.amazon.com/kendra/latest/dg/in-adding-languages.html
     """
-    
+
     client: Optional[Any] = None
     """boto3 client for Kendra."""
-    
+
     def __init__(
-            self, 
-            index_id, 
-            region_name=None, 
-            credentials_profile_name=None,
-            language_code=None,
-            client=None
-        ):
+        self,
+        index_id,
+        region_name=None,
+        credentials_profile_name=None,
+        language_code=None,
+        client=None,
+    ):
         self.index_id = index_id
         self.language_code = language_code
 
         if client is not None:
             self.client = client
             return
-        
+
         try:
-            import boto3 
+            import boto3
 
             if self.credentials_profile_name is not None:
                 session = boto3.Session(profile_name=credentials_profile_name)
@@ -186,9 +183,9 @@ class AmazonKendraRetriever(BaseRetriever):
 
             client_params = {}
             if region_name is not None:
-                client_params['region_name'] = region_name
+                client_params["region_name"] = region_name
 
-            self.client = session.client('kendra', **client_params)
+            self.client = session.client("kendra", **client_params)
         except ImportError:
             raise ModuleNotFoundError(
                 "Could not import boto3 python package. "
@@ -201,12 +198,7 @@ class AmazonKendraRetriever(BaseRetriever):
                 "profile name are valid."
             ) from e
 
-    def _kendra_query(
-            self, 
-            query, 
-            top_k: int = 3
-        ) -> List[Document]:
-
+    def _kendra_query(self, query, top_k: int = 3) -> List[Document]:
         if self.language_code is not None:
             attribute_filter = {
                 "AndAllFilters": [
@@ -224,28 +216,27 @@ class AmazonKendraRetriever(BaseRetriever):
             attribute_filter = None
 
         response = self.client.retrieve(
-            IndexId=self.index_id, 
-            QueryText=query.strip(), 
+            IndexId=self.index_id,
+            QueryText=query.strip(),
             PageSize=top_k,
-            AttributeFilter=attribute_filter
+            AttributeFilter=attribute_filter,
         )
         r_result = RetrieveResult.parse_obj(response)
         result_len = len(r_result.ResultItems)
-        
+
         if result_len == 0:
-            #retrieve API returned 0 results, call query API
+            # retrieve API returned 0 results, call query API
             response = self.client.query(
-                IndexId=self.index_id, 
-                QueryText=query.strip(), 
+                IndexId=self.index_id,
+                QueryText=query.strip(),
                 PageSize=top_k,
-                AttributeFilter=attribute_filter
+                AttributeFilter=attribute_filter,
             )
             q_result = QueryResult.parse_obj(response)
             docs = q_result.get_top_n_docs(top_k)
         else:
             docs = r_result.get_top_n_docs(top_k)
         return docs
-
 
     def get_relevant_documents(self, query: str, top_k: int = 3) -> List[Document]:
         """Run search on Kendra index and get top k documents
@@ -254,10 +245,10 @@ class AmazonKendraRetriever(BaseRetriever):
         .. code-block:: python
 
             docs = retriever.get_relevant_documents('This is my query')
-        
+
         """
         docs = self._kendra_query(query, top_k)
         return docs
-    
+
     async def aget_relevant_documents(self, query: str) -> List[Document]:
         raise NotImplementedError("Async version is not implemented for Kendra yet.")
