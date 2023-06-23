de64b10c001905cdece76f98737a05b3c1cefc32
Lance Martin <122662504+rlancemartin@users.noreply.github.com>
2023-06-23 13:02:48 -0700

    Create merge loader that combines documents from a set of loaders (#6659)
    Simple utility loader that combines documents from a set of specified
    loaders.

--- langchain/document_loaders/__init__.py
+++ langchain/document_loaders/__init__.py
@@ -67,6 +67,7 @@ from langchain.document_loaders.markdown import UnstructuredMarkdownLoader
 from langchain.document_loaders.mastodon import MastodonTootsLoader
 from langchain.document_loaders.max_compute import MaxComputeLoader
 from langchain.document_loaders.mediawikidump import MWDumpLoader
+from langchain.document_loaders.merge import MergedDataLoader
 from langchain.document_loaders.modern_treasury import ModernTreasuryLoader
 from langchain.document_loaders.notebook import NotebookLoader
 from langchain.document_loaders.notion import NotionDirectoryLoader
@@ -201,6 +202,7 @@ __all__ = [
     "MastodonTootsLoader",
     "MathpixPDFLoader",
     "MaxComputeLoader",
+    "MergedDataLoader",
     "ModernTreasuryLoader",
     "NotebookLoader",
     "NotionDBLoader",
