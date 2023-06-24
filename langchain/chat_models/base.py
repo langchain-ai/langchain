cd0c228a7f83b97d789586e72fb6fae411fa9570
Harrison Chase <hw.chase.17@gmail.com>
2023-06-23 21:49:21 -0700

    cr

--- langchain/chat_models/base.py
+++ langchain/chat_models/base.py
@@ -1,9 +1,10 @@
 import asyncio
 import inspect
+import json
 import warnings
 from abc import ABC, abstractmethod
 from functools import partial
-from typing import Any, Dict, List, Mapping, Optional, Sequence, Tuple
+from typing import Any, Dict, List, Mapping, Optional, Sequence
 
 from pydantic import Field, root_validator
 
@@ -27,6 +28,7 @@ from langchain.schema import (
     LLMResult,
     PromptValue,
     RunInfo,
+    messages_to_dict,
 )
 
 
@@ -35,53 +37,7 @@ def _get_verbosity() -> bool:
 
 
 def chat_history_as_string(messages: List[BaseMessage]) -> str:
-    return "\n".join([f"{m.type}: {m.content}" for m in messages])
-
-
-def get_prompts(
-    params: Dict[str, Any], prompts: List[List[BaseMessage]]
-) -> Tuple[Dict[int, ChatResult], str, List[int], List[List[BaseMessage]]]:
-    """Get prompts that are already cached."""
-    llm_string = str(sorted([(k, v) for k, v in params.items()]))
-    missing_prompts = []
-    missing_prompt_idxs = []
-    existing_prompts = {}
-    for i, prompt in enumerate(prompts):
-        if langchain.llm_cache is not None:
-            cache_val = langchain.llm_cache.lookup(
-                chat_history_as_string(prompt), llm_string
-            )
-            if isinstance(cache_val, list):
-                # the cache stores List[Generation]; we parse it to List[ChatGeneration]
-                cache_val_for_chat = [
-                    ChatGeneration(text=gen.text, message=AIMessage(content=gen.text))
-                    for gen in cache_val
-                ]
-
-                existing_prompts[i] = ChatResult(generations=cache_val_for_chat)
-            else:
-                missing_prompts.append(prompt)
-                missing_prompt_idxs.append(i)
-    return existing_prompts, llm_string, missing_prompt_idxs, missing_prompts
-
-
-def update_cache(
-    existing_prompts: Dict[int, ChatResult],
-    llm_string: str,
-    missing_prompt_idxs: List[int],
-    new_results: List[ChatResult],
-    prompts: List[List[BaseMessage]],
-) -> List[Optional[dict]]:
-    """Update the cache and get the LLM output."""
-    for i, result in enumerate(new_results):
-        existing_prompts[missing_prompt_idxs[i]] = result
-        prompt = prompts[missing_prompt_idxs[i]]
-        if langchain.llm_cache is not None:
-            langchain.llm_cache.update(
-                chat_history_as_string(prompt), llm_string, result.generations
-            )
-    llm_output = [results.llm_output for results in new_results]
-    return llm_output
+    return json.dumps(messages_to_dict(messages))
 
 
 class BaseChatModel(BaseLanguageModel, ABC):
@@ -110,24 +66,15 @@ class BaseChatModel(BaseLanguageModel, ABC):
         arbitrary_types_allowed = True
 
     def _combine_llm_outputs(self, llm_outputs: List[Optional[dict]]) -> dict:
-        """Combine general llm outputs by aggregating them into lists
-        e.g. [{"token_usage": 12}, {"token_usage": 18}] -> {"token_usage": [12, 18]}
+        return {}
 
-        Subclasses can override this function if there's a semantically better way to
-        combine llm outputs
-        e.g. [{"token_usage": 12}, {"token_usage": 18}] -> {"token_usage": 30}
-        """
-        combined_outputs = {}
-
-        for llm_output in llm_outputs:
-            if llm_output is not None:
-                for key, value in llm_output.items():
-                    if key not in combined_outputs:
-                        combined_outputs[key] = [value]
-                    else:
-                        combined_outputs[key].append(value)
-
-        return combined_outputs
+    def _get_invocation_params(
+        self,
+        stop: Optional[List[str]] = None,
+    ) -> dict:
+        params = self.dict()
+        params["stop"] = stop
+        return params
 
     def generate(
         self,
@@ -139,15 +86,7 @@ class BaseChatModel(BaseLanguageModel, ABC):
         **kwargs: Any,
     ) -> LLMResult:
         """Top Level call"""
-        params = self.dict()
-        params["stop"] = stop
-        (
-            existing_prompts,
-            llm_string,
-            missing_prompt_idxs,
-            missing_prompts,
-        ) = get_prompts(params, messages)
-        disregard_cache = self.cache is not None and not self.cache
+        params = self._get_invocation_params(stop=stop)
         options = {"stop": stop}
 
         callback_manager = CallbackManager.configure(
@@ -160,63 +99,25 @@ class BaseChatModel(BaseLanguageModel, ABC):
         run_manager = callback_manager.on_chat_model_start(
             dumpd(self), messages, invocation_params=params, options=options
         )
+
         new_arg_supported = inspect.signature(self._generate).parameters.get(
             "run_manager"
         )
-        if langchain.llm_cache is None or disregard_cache:
-            # This happens when langchain.cache is None, but self.cache is True
-            if self.cache is not None and self.cache:
-                raise ValueError(
-                    "Asked to cache, but no cache found at `langchain.cache`."
+        try:
+            results = [
+                self._generate_with_cache(
+                    m, stop=stop, run_manager=run_manager, **kwargs
                 )
-            try:
-                results: List[ChatResult] = [
-                    self._generate(m, stop=stop, run_manager=run_manager)
-                    if new_arg_supported
-                    else self._generate(m, stop=stop)
-                    for m in messages
-                ]
-                llm_outputs = [res.llm_output for res in results]
-            except (KeyboardInterrupt, Exception) as e:
-                run_manager.on_llm_error(e)
-                raise e
-        else:
-            # use cache
-            if len(missing_prompts) > 0:
-                try:
-                    new_results = [
-                        self._generate(m, stop=stop, run_manager=run_manager)
-                        if new_arg_supported
-                        else self._generate(m, stop=stop)
-                        for m in missing_prompts
-                    ]
-                except (KeyboardInterrupt, Exception) as e:
-                    run_manager.on_llm_error(e)
-                    raise e
-                llm_outputs = update_cache(
-                    existing_prompts,
-                    llm_string,
-                    missing_prompt_idxs,
-                    new_results,
-                    messages,
-                )
-                # Combine cached results and new results
-                results_dict = {
-                    **existing_prompts,
-                    **dict(zip(missing_prompt_idxs, new_results)),
-                }
-                results = [result for _, result in sorted(results_dict.items())]
-            else:
-                llm_outputs = []
-                # All prompts were caches, so we construct results solely from cache
-                results = [r for _, r in existing_prompts.items()]
-        llm_output = self._combine_llm_outputs(llm_outputs)
+                if new_arg_supported
+                else self._generate_with_cache(m, stop=stop)
+                for m in messages
+            ]
+        except (KeyboardInterrupt, Exception) as e:
+            run_manager.on_llm_error(e)
+            raise e
+        llm_output = self._combine_llm_outputs([res.llm_output for res in results])
         generations = [res.generations for res in results]
-        # We ignore type as List[List[Generation]] is expected instead of
-        # List[List[ChatGeneration]], but ChatGeneration is subclass of Generation
-        output = LLMResult(
-            generations=generations, llm_output=llm_output  # type: ignore
-        )
+        output = LLMResult(generations=generations, llm_output=llm_output)
         run_manager.on_llm_end(output)
         if run_manager:
             output.run = RunInfo(run_id=run_manager.run_id)
@@ -232,15 +133,7 @@ class BaseChatModel(BaseLanguageModel, ABC):
         **kwargs: Any,
     ) -> LLMResult:
         """Top Level call"""
-        params = self.dict()
-        params["stop"] = stop
-        (
-            existing_prompts,
-            llm_string,
-            missing_prompt_idxs,
-            missing_prompts,
-        ) = get_prompts(params, messages)
-        disregard_cache = self.cache is not None and not self.cache
+        params = self._get_invocation_params(stop=stop)
         options = {"stop": stop}
 
         callback_manager = AsyncCallbackManager.configure(
@@ -253,66 +146,25 @@ class BaseChatModel(BaseLanguageModel, ABC):
         run_manager = await callback_manager.on_chat_model_start(
             dumpd(self), messages, invocation_params=params, options=options
         )
+
         new_arg_supported = inspect.signature(self._agenerate).parameters.get(
             "run_manager"
         )
-        if langchain.llm_cache is None or disregard_cache:
-            # This happens when langchain.cache is None, but self.cache is True
-            if self.cache is not None and self.cache:
-                raise ValueError(
-                    "Asked to cache, but no cache found at `langchain.cache`."
-                )
-            try:
-                results = await asyncio.gather(
-                    *[
-                        self._agenerate(m, stop=stop, run_manager=run_manager)
-                        if new_arg_supported
-                        else self._agenerate(m, stop=stop)
-                        for m in messages
-                    ]
-                )
-            except (KeyboardInterrupt, Exception) as e:
-                await run_manager.on_llm_error(e)
-                raise e
-        else:
-            # use cache
-            if len(missing_prompts) > 0:
-                try:
-                    new_results = await asyncio.gather(
-                        *[
-                            self._agenerate(m, stop=stop, run_manager=run_manager)
-                            if new_arg_supported
-                            else self._agenerate(m, stop=stop)
-                            for m in missing_prompts
-                        ]
-                    )
-                except (KeyboardInterrupt, Exception) as e:
-                    run_manager.on_llm_error(e)
-                    raise e
-                llm_outputs = update_cache(
-                    existing_prompts,
-                    llm_string,
-                    missing_prompt_idxs,
-                    new_results,
-                    messages,
-                )
-                # Combine cached results and new results
-                results_dict = {
-                    **existing_prompts,
-                    **dict(zip(missing_prompt_idxs, new_results)),
-                }
-                results = [result for _, result in sorted(results_dict.items())]
-            else:
-                llm_outputs = []
-                # All prompts were caches, so we construct results solely from cache
-                results = [r for _, r in existing_prompts.items()]
-        llm_output = self._combine_llm_outputs(llm_outputs)
+        try:
+            results = await asyncio.gather(
+                *[
+                    self._agenerate(m, stop=stop, run_manager=run_manager, **kwargs)
+                    if new_arg_supported
+                    else self._agenerate(m, stop=stop)
+                    for m in messages
+                ]
+            )
+        except (KeyboardInterrupt, Exception) as e:
+            await run_manager.on_llm_error(e)
+            raise e
+        llm_output = self._combine_llm_outputs([res.llm_output for res in results])
         generations = [res.generations for res in results]
-        # We ignore type as List[List[Generation]] is expected instead of
-        # List[List[ChatGeneration]], but ChatGeneration is subclass of Generation
-        output = LLMResult(
-            generations=generations, llm_output=llm_output  # type: ignore
-        )
+        output = LLMResult(generations=generations, llm_output=llm_output)
         await run_manager.on_llm_end(output)
         if run_manager:
             output.run = RunInfo(run_id=run_manager.run_id)
@@ -339,6 +191,78 @@ class BaseChatModel(BaseLanguageModel, ABC):
         return await self.agenerate(
             prompt_messages, stop=stop, callbacks=callbacks, **kwargs
         )
+
+    def _generate_with_cache(
+        self,
+        messages: List[BaseMessage],
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+        **kwargs: Any,
+    ) -> ChatResult:
+        disregard_cache = self.cache is not None and not self.cache
+        if langchain.llm_cache is None or disregard_cache:
+            # This happens when langchain.cache is None, but self.cache is True
+            if self.cache is not None and self.cache:
+                raise ValueError(
+                    "Asked to cache, but no cache found at `langchain.cache`."
+                )
+            return self._generate(
+                messages, stop=stop, run_manager=run_manager, **kwargs
+            )
+        else:
+            params = self._get_invocation_params(stop=stop)
+            llm_string = str(sorted([(k, v) for k, v in params.items()]))
+            prompt = chat_history_as_string(messages)
+            cache_val = langchain.llm_cache.lookup(prompt, llm_string)
+            if isinstance(cache_val, list):
+                # the cache stores List[Generation]; we parse it to List[ChatGeneration]
+                cache_val_for_chat = [
+                    ChatGeneration(text=gen.text, message=AIMessage(content=gen.text))
+                    for gen in cache_val
+                ]
+                return ChatResult(generations=cache_val_for_chat)
+            else:
+                result = self._generate(
+                    messages, stop=stop, run_manager=run_manager, **kwargs
+                )
+                langchain.llm_cache.update(prompt, llm_string, result.generations)
+                return result
+
+    async def _agenerate_with_cache(
+        self,
+        messages: List[BaseMessage],
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[AsyncCallbackManagerForLLMRun] = None,
+        **kwargs: Any,
+    ) -> ChatResult:
+        disregard_cache = self.cache is not None and not self.cache
+        if langchain.llm_cache is None or disregard_cache:
+            # This happens when langchain.cache is None, but self.cache is True
+            if self.cache is not None and self.cache:
+                raise ValueError(
+                    "Asked to cache, but no cache found at `langchain.cache`."
+                )
+            return await self._agenerate(
+                messages, stop=stop, run_manager=run_manager, **kwargs
+            )
+        else:
+            params = self._get_invocation_params(stop=stop)
+            llm_string = str(sorted([(k, v) for k, v in params.items()]))
+            prompt = chat_history_as_string(messages)
+            cache_val = langchain.llm_cache.lookup(prompt, llm_string)
+            if isinstance(cache_val, list):
+                # the cache stores List[Generation]; we parse it to List[ChatGeneration]
+                cache_val_for_chat = [
+                    ChatGeneration(text=gen.text, message=AIMessage(content=gen.text))
+                    for gen in cache_val
+                ]
+                return ChatResult(generations=cache_val_for_chat)
+            else:
+                result = await self._agenerate(
+                    messages, stop=stop, run_manager=run_manager, **kwargs
+                )
+                langchain.llm_cache.update(prompt, llm_string, result.generations)
+                return result
 
     @abstractmethod
     def _generate(
