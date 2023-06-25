fbbe1e832372e6c72b62c078e86b3e55de1049bc
sudolong <abc115105@163.com>
2023-06-26 02:53:42 +0800

    fix chroma _similarity_search_with_relevance_scores missing `kwargs` … (#6708)
    Issue: https://github.com/hwchase17/langchain/issues/6707

--- langchain/vectorstores/chroma.py
+++ langchain/vectorstores/chroma.py
@@ -228,7 +228,7 @@ class Chroma(VectorStore):
         k: int = 4,
         **kwargs: Any,
     ) -> List[Tuple[Document, float]]:
-        return self.similarity_search_with_score(query, k)
+        return self.similarity_search_with_score(query, k, **kwargs)
 
     def max_marginal_relevance_search_by_vector(
         self,
