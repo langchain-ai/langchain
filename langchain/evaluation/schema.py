cc60fed3be30d28be05d67c4a9b1b5dcc058aa3d
Zander Chase <130414180+vowelparrot@users.noreply.github.com>
2023-06-26 20:47:41 -0700

    Add a Pairwise Comparison Chain (#6703)
    Notebook shows preference scoring between two chains and reports wilson
    score interval + p value
    
    I think I'll add the option to insert ground truth labels but doesn't
    have to be in this PR

--- langchain/evaluation/schema.py
+++ langchain/evaluation/schema.py
@@ -14,7 +14,7 @@ class StringEvaluator(Protocol):
         prediction: str,
         reference: Optional[str] = None,
         input: Optional[str] = None,
-        **kwargs: Any
+        **kwargs: Any,
     ) -> dict:
         """Evaluate Chain or LLM output, based on optional input and label.
 
@@ -34,7 +34,7 @@ class StringEvaluator(Protocol):
         prediction: str,
         reference: Optional[str] = None,
         input: Optional[str] = None,
-        **kwargs: Any
+        **kwargs: Any,
     ) -> dict:
         """Asynchronously evaluate Chain or LLM output, based on optional
           input and label.
@@ -48,6 +48,66 @@ class StringEvaluator(Protocol):
         Returns:
             dict: The evaluation results containing the score or value.
         """
-        return self.evaluate_strings(
-            prediction=prediction, reference=reference, input=input, **kwargs
+        raise NotImplementedError(
+            f"{self.__class__.__name__} hasn't implemented an "
+            "async aevaluate_strings method."
+        )
+
+
+@runtime_checkable
+class PairwiseStringEvaluator(Protocol):
+    """A protocol for comparing the output of two models."""
+
+    @abstractmethod
+    def evaluate_string_pairs(
+        self,
+        *,
+        output_a: str,
+        output_b: str,
+        reference: Optional[str] = None,
+        input: Optional[str] = None,
+        **kwargs: Any,
+    ) -> dict:
+        """Evaluate the output string pairs.
+
+        Args:
+            output_a (str): The output string from the first model.
+            output_b (str): The output string from the second model.
+            reference (str, optional): The expected output / reference
+                string. Defaults to None.
+            input (str, optional): The input string. Defaults to None.
+            **kwargs (Any): Additional keyword arguments, such
+                as callbacks and optional reference strings.
+
+        Returns:
+            dict: A dictionary containing the preference, scores, and/or
+                other information.
+        """
+
+    async def aevaluate_string_pairs(
+        self,
+        output_a: str,
+        output_b: str,
+        reference: Optional[str] = None,
+        input: Optional[str] = None,
+        **kwargs: Any,
+    ) -> dict:
+        """Evaluate the output string pairs.
+
+        Args:
+            output_a (str): The output string from the first model.
+            output_b (str): The output string from the second model.
+            reference (str, optional): The expected output / reference
+                string. Defaults to None.
+            input (str, optional): The input string. Defaults to None.
+            **kwargs (Any): Additional keyword arguments, such
+                as callbacks and optional reference strings.
+
+        Returns:
+            dict: A dictionary containing the preference, scores, and/or
+                other information.
+        """
+        raise NotImplementedError(
+            f"{self.__class__.__name__} hasn't implemented an async "
+            "aevaluate_string_pairs method."
         )
