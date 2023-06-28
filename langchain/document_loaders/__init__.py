9d1bd1859666fea1515f0ab06dee0af8f7fb3bbe
Yaohui Wang <wangyaohuicn@gmail.com>
2023-06-28 14:08:05 +0800

    feat (documents): add LarkSuite document loader (#6420)
    <!--
    Thank you for contributing to LangChain! Your PR will appear in our
    release under the title you set. Please make sure it highlights your
    valuable contribution.
    
    Replace this with a description of the change, the issue it fixes (if
    applicable), and relevant context. List any dependencies required for
    this change.
    
    After you're done, someone will review your PR. They may suggest
    improvements. If no one reviews your PR within a few days, feel free to
    @-mention the same people again, as notifications can get lost.
    
    Finally, we'd love to show appreciation for your contribution - if you'd
    like us to shout you out on Twitter, please also include your handle!
    -->
    
    <!-- Remove if not applicable -->
    
    ### Summary
    
    This PR adds a LarkSuite (FeiShu) document loader. 
    > [LarkSuite](https://www.larksuite.com/) is an enterprise collaboration
    platform developed by ByteDance.
    
    ### Tests
    
    - an integration test case is added
    - an example notebook showing usage is added. [Notebook
    preview](https://github.com/yaohui-wyh/langchain/blob/master/docs/extras/modules/data_connection/document_loaders/integrations/larksuite.ipynb)
    
    <!-- If you're adding a new integration, please include:
    
    1. a test for the integration - favor unit tests that does not rely on
    network access.
    2. an example notebook showing its use
    
    
    See contribution guidelines for more information on how to write tests,
    lint
    etc:
    
    
    https://github.com/hwchase17/langchain/blob/master/.github/CONTRIBUTING.md
    -->
    
    ### Who can review?
    
    - PTAL @eyurtsev @hwchase17
    
    <!-- For a quicker response, figure out the right person to tag with @
    
      @hwchase17 - project lead
    
      Tracing / Callbacks
      - @agola11
    
      Async
      - @agola11
    
      DataLoaders
      - @eyurtsev
    
      Models
      - @hwchase17
      - @agola11
    
      Agents / Tools / Toolkits
      - @hwchase17
    
      VectorStores / Retrievers / Memory
      - @dev2049
    
     -->
    
    ---------
    
    Co-authored-by: Yaohui Wang <wangyaohui.01@bytedance.com>

--- langchain/document_loaders/__init__.py
+++ langchain/document_loaders/__init__.py
@@ -63,6 +63,7 @@ from langchain.document_loaders.imsdb import IMSDbLoader
 from langchain.document_loaders.iugu import IuguLoader
 from langchain.document_loaders.joplin import JoplinLoader
 from langchain.document_loaders.json_loader import JSONLoader
+from langchain.document_loaders.larksuite import LarkSuiteDocLoader
 from langchain.document_loaders.markdown import UnstructuredMarkdownLoader
 from langchain.document_loaders.mastodon import MastodonTootsLoader
 from langchain.document_loaders.max_compute import MaxComputeLoader
@@ -204,6 +205,7 @@ __all__ = [
     "IuguLoader",
     "JSONLoader",
     "JoplinLoader",
+    "LarkSuiteDocLoader",
     "MWDumpLoader",
     "MastodonTootsLoader",
     "MathpixPDFLoader",
