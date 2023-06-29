e71d37078a1d77e2d75c72b494125a49b18e9a07
Dev 2049 <dev.dev2049@gmail.com>
2023-06-28 19:47:50 -0700

    test

--- tests/integration_tests/vectorstores/test_faiss.py
+++ tests/integration_tests/vectorstores/test_faiss.py
@@ -48,16 +48,17 @@ def test_faiss_vector_sim() -> None:
 
 
 def test_faiss_mmr() -> None:
-    texts = ["foo", "bar", "baz"]
+    texts = ["foo", "foo", "fou", "foy"]
     docsearch = FAISS.from_texts(texts, FakeEmbeddings())
     query_vec = FakeEmbeddings().embed_query(text="foo")
     # make sure we can have k > docstore size
     output = docsearch.max_marginal_relevance_search_with_score_by_vector(
-        query_vec, k=10
+        query_vec, k=10, lambda_mult=0.1
     )
     assert len(output) == len(texts)
     assert output[0][0] == Document(page_content="foo")
     assert output[0][1] == 0.0
+    assert output[1][0] != Document(page_content="foo")
 
 
 def test_faiss_with_metadatas() -> None:
