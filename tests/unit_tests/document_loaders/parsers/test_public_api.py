20c6ade2fc24c458a080eaa468add7c61dc8dbc3
corranmac <corranmac@hotmail.co.uk>
2023-06-29 22:29:29 +0100

    Grobid parser for Scientific Articles from PDF (#6729)
    ### Scientific Article PDF Parsing via Grobid
    
    `Description:`
    This change adds the GrobidParser class, which uses the Grobid library
    to parse scientific articles into a universal XML format containing the
    article title, references, sections, section text etc. The GrobidParser
    uses a local Grobid server to return PDFs document as XML and parses the
    XML to optionally produce documents of individual sentences or of whole
    paragraphs. Metadata includes the text, paragraph number, pdf relative
    bboxes, pages (text may overlap over two pages), section title
    (Introduction, Methodology etc), section_number (i.e 1.1, 2.3), the
    title of the paper and finally the file path.
          
    Grobid parsing is useful beyond standard pdf parsing as it accurately
    outputs sections and paragraphs within them. This allows for
    post-fitering of results for specific sections i.e. limiting results to
    the methodology section or results. While sections are split via
    headings, ideally they could be classified specifically into
    introduction, methodology, results, discussion, conclusion. I'm
    currently experimenting with chatgpt-3.5 for this function, which could
    later be implemented as a textsplitter.
    
    `Dependencies:`
    For use, the grobid repo must be cloned and Java must be installed, for
    colab this is:
    
    ```
    !apt-get install -y openjdk-11-jdk -q
    !update-alternatives --set java /usr/lib/jvm/java-11-openjdk-amd64/bin/java
    !git clone https://github.com/kermitt2/grobid.git
    os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-11-openjdk-amd64"
    os.chdir('grobid')
    !./gradlew clean install
    ```
    
    Once installed the server is ran on localhost:8070 via
    ```
    get_ipython().system_raw('nohup ./gradlew run > grobid.log 2>&1 &')
    ```
    
    @rlancemartin, @eyurtsev
    
    Twitter Handle: @Corranmac
    
    Grobid Demo Notebook is
    [here](https://colab.research.google.com/drive/1X-St_mQRmmm8YWtct_tcJNtoktbdGBmd?usp=sharing).
    
    ---------
    
    Co-authored-by: rlm <pexpresss31@gmail.com>

--- tests/unit_tests/document_loaders/parsers/test_public_api.py
+++ tests/unit_tests/document_loaders/parsers/test_public_api.py
@@ -5,6 +5,7 @@ def test_parsers_public_api_correct() -> None:
     """Test public API of parsers for breaking changes."""
     assert set(__all__) == {
         "BS4HTMLParser",
+        "GrobidParser",
         "LanguageParser",
         "OpenAIWhisperParser",
         "PyPDFParser",
