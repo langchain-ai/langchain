4150cfe3b6e99d8e3e2c6bf03e83877a13677339
Bagatur <baskaryan@gmail.com>
2023-06-30 00:17:08 -0700

    conf

--- docs/api_reference/conf.py
+++ docs/api_reference/conf.py
@@ -11,9 +11,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 import toml
 
