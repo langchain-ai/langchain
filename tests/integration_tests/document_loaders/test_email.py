0b1fa454ee97cbc8b6916e7da521c42c4b88011e
Matt Robinson <mrobinson@unstructuredai.io>
2023-06-30 11:53:14 -0400

    fix typing in email test

--- tests/integration_tests/document_loaders/test_email.py
+++ tests/integration_tests/document_loaders/test_email.py
@@ -23,10 +23,10 @@ def test_outlook_message_loader() -> None:
 def test_unstructured_email_loader_with_attachments() -> None:
     file_path = Path(__file__).parent.parent / "examples/fake-email-attachment.eml"
     loader = UnstructuredEmailLoader(
-        file_path, mode="elements", process_attachments=True
+        str(file_path), mode="elements", process_attachments=True
     )
     docs = loader.load()
 
     assert docs[-1].page_content == "Hey this is a fake attachment!"
     assert docs[-1].metadata["filename"] == "fake-attachment.txt"
-    assert docs[-1].metadata["source"].name == "fake-email-attachment.eml"
+    assert docs[-1].metadata["source"].endswith("fake-email-attachment.eml")
