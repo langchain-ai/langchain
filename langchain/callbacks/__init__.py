3498ca7eb341667bc3c3f22eee30060483edc40e
William FH <13333726+hinthornw@users.noreply.github.com>
2023-06-30 08:32:42 -0700

    Promptlayer Callback (#6975)
    Co-authored-by: Saleh Hindi <saleh.hindi.one@gmail.com>
    Co-authored-by: jped <jonathanped@gmail.com>

--- langchain/callbacks/__init__.py
+++ langchain/callbacks/__init__.py
@@ -16,6 +16,7 @@ from langchain.callbacks.manager import (
 )
 from langchain.callbacks.mlflow_callback import MlflowCallbackHandler
 from langchain.callbacks.openai_info import OpenAICallbackHandler
+from langchain.callbacks.promptlayer_callback import PromptLayerCallbackHandler
 from langchain.callbacks.stdout import StdOutCallbackHandler
 from langchain.callbacks.streaming_aiter import AsyncIteratorCallbackHandler
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
@@ -30,6 +31,7 @@ __all__ = [
     "AimCallbackHandler",
     "ArgillaCallbackHandler",
     "ArizeCallbackHandler",
+    "PromptLayerCallbackHandler",
     "ArthurCallbackHandler",
     "ClearMLCallbackHandler",
     "CometCallbackHandler",
