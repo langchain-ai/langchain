53aec6a54bc9dc511db5c3ec60f7b5010058ec39
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2023-06-30 06:24:26 -0700

    lint

--- langchain/callbacks/arthur_callback.py
+++ langchain/callbacks/arthur_callback.py
@@ -1,10 +1,13 @@
+"""ArthurAI's Callback Handler."""
+import os
+import uuid
 from collections import defaultdict
 from datetime import datetime
+from time import time
+from typing import Any, DefaultDict, Dict, List, Optional, Union
+
 import numpy as np
 import pytz
-from time import time
-from typing import Any, Dict, List, Optional, Union
-import uuid
 
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain.schema import AgentAction, AgentFinish, LLMResult
@@ -18,11 +21,15 @@ DURATION = "duration"
 
 class ArthurCallbackHandler(BaseCallbackHandler):
     """Callback Handler that logs to Arthur platform.
-    
-    Arthur helps enterprise teams optimize model operations and performance at scale. 
-    The Arthur API tracks model performance, explainability, and fairness across tabular, NLP, and CV models.
-    Our API is model- and platform-agnostic, and continuously scales with complex and dynamic enterprise needs.
-    To learn more about Arthur, visit our website at https://www.arthur.ai/ or read the Arthur docs at https://docs.arthur.ai/
+
+    Arthur helps enterprise teams optimize model operations
+    and performance at scale. The Arthur API tracks model
+    performance, explainability, and fairness across tabular,
+    NLP, and CV models. Our API is model- and platform-agnostic,
+    and continuously scales with complex and dynamic enterprise needs.
+    To learn more about Arthur, visit our website at
+    https://www.arthur.ai/ or read the Arthur docs at
+    https://docs.arthur.ai/
     """
 
     def __init__(
@@ -30,116 +37,167 @@ class ArthurCallbackHandler(BaseCallbackHandler):
         model_id: str,
         arthur_url: Optional[str] = "https://app.arthur.ai",
         arthur_login: Optional[str] = None,
-        arthur_password: Optional[str] = None
+        arthur_password: Optional[str] = None,
     ) -> None:
         """Initialize callback handler."""
 
         super().__init__()
         from arthurai import ArthurAI
-        from arthurai.common.constants import InputType, OutputType, Stage, ValueType
+        from arthurai.common.constants import Stage, ValueType
         from arthurai.common.exceptions import ResponseClientError
-                
-        # connect to Arthur               
+
+        # connect to Arthur
         if arthur_login is None:
             try:
-                arthur_api_key = os.environ['ARTHUR_API_KEY']
+                arthur_api_key = os.environ["ARTHUR_API_KEY"]
             except KeyError:
-                raise ValueError('No Arthur authentication provided. Either give a login to the ArthurCallbackHandler \
-                    or set an ARTHUR_API_KEY as an environment variable.')
+                raise ValueError(
+                    "No Arthur authentication provided. Either give"
+                    " a login to the ArthurCallbackHandler"
+                    " or set an ARTHUR_API_KEY as an environment variable."
+                )
             arthur = ArthurAI(url=arthur_url, access_key=arthur_api_key)
         else:
             if arthur_password is None:
                 arthur = ArthurAI(url=arthur_url, login=arthur_login)
             else:
-                arthur = ArthurAI(url=arthur_url, login=arthur_login, password=arthur_password)
-                
+                arthur = ArthurAI(
+                    url=arthur_url, login=arthur_login, password=arthur_password
+                )
+
         # get model from Arthur by the provided model ID
         try:
             self.arthur_model = arthur.get_model(model_id)
         except ResponseClientError:
-            raise ValueError(f"Was unable to retrieve model with id {model_id} from Arthur. \
-                Make sure the ID corresponds to a model that is currently registered with your Arthur account.")
-            
-        # save the attributes of this model to be used when preparing inferences to log to Arthur in on_llm_end()
+            raise ValueError(
+                f"Was unable to retrieve model with id {model_id} from Arthur."
+                " Make sure the ID corresponds to a model that is currently"
+                " registered with your Arthur account."
+            )
+
+        # save the attributes of this model to be used when preparing
+        # inferences to log to Arthur in on_llm_end()
         self.attr_names = set([a.name for a in self.arthur_model.get_attributes()])
-        self.input_attr = [x for x in self.arthur_model.get_attributes()
-                           if x.stage==Stage.ModelPipelineInput and x.value_type==ValueType.Unstructured_Text][0].name
-        self.output_attr = [x for x in self.arthur_model.get_attributes()
-                           if x.stage==Stage.PredictedValue and x.value_type==ValueType.Unstructured_Text][0].name
+        self.input_attr = [
+            x
+            for x in self.arthur_model.get_attributes()
+            if x.stage == Stage.ModelPipelineInput
+            and x.value_type == ValueType.Unstructured_Text
+        ][0].name
+        self.output_attr = [
+            x
+            for x in self.arthur_model.get_attributes()
+            if x.stage == Stage.PredictedValue
+            and x.value_type == ValueType.Unstructured_Text
+        ][0].name
         self.token_likelihood_attr = None
-        if len([x for x in self.arthur_model.get_attributes() if x.value_type==ValueType.TokenLikelihoods]) > 0:
-            self.token_likelihood_attr = [x for x in self.arthur_model.get_attributes()
-                           if x.value_type==ValueType.TokenLikelihoods][0].name
-        
-        # prepare callback data defaults to be updated in on_llm_start() and on_llm_end()
-        self.input_texts: List = []
-        self.on_llm_start_time: float = -1.0
-        self.run_map = defaultdict(dict)
-        
+        if (
+            len(
+                [
+                    x
+                    for x in self.arthur_model.get_attributes()
+                    if x.value_type == ValueType.TokenLikelihoods
+                ]
+            )
+            > 0
+        ):
+            self.token_likelihood_attr = [
+                x
+                for x in self.arthur_model.get_attributes()
+                if x.value_type == ValueType.TokenLikelihoods
+            ][0].name
+
+        self.run_map: DefaultDict[str, Any] = defaultdict(dict)
 
     def on_llm_start(
         self, serialized: Dict[str, Any], prompts: List[str], **kwargs: Any
     ) -> None:
         """On LLM start, save the input prompts"""
-        run_id = kwargs['run_id']
-        self.run_map[run_id]['input_texts'] = prompts
-        self.run_map[run_id]['start_time'] = time()
+        run_id = kwargs["run_id"]
+        self.run_map[run_id]["input_texts"] = prompts
+        self.run_map[run_id]["start_time"] = time()
 
     def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
         """On LLM end, send data to Arthur."""
 
-        run_id = kwargs['run_id']
+        run_id = kwargs["run_id"]
 
-        # get the run params from this run ID, 
+        # get the run params from this run ID,
         # or raise an error if this run ID has no corresponding metadata in self.run_map
         try:
             run_map_data = self.run_map[run_id]
         except KeyError as e:
-            raise KeyError("This function has been called with a run_id that was never registered in on_llm_start(). Restart and try running the LLM again") from e
+            raise KeyError(
+                "This function has been called with a run_id"
+                " that was never registered in on_llm_start()."
+                " Restart and try running the LLM again"
+            ) from e
 
         # mark the duration time between on_llm_start() and on_llm_end()
-        time_from_start_to_end = time() - run_map_data['start_time']
-        
+        time_from_start_to_end = time() - run_map_data["start_time"]
+
         # create inferences to log to Arthur
         inferences = []
         for i, generations in enumerate(response.generations):
             for generation in generations:
-
                 inference = {
-                    'partner_inference_id': str(uuid.uuid4()),
-                    'inference_timestamp': datetime.now(tz=pytz.UTC),
-                    self.input_attr: run_map_data['input_texts'][i], 
+                    "partner_inference_id": str(uuid.uuid4()),
+                    "inference_timestamp": datetime.now(tz=pytz.UTC),
+                    self.input_attr: run_map_data["input_texts"][i],
                     self.output_attr: generation.text,
                 }
 
                 if generation.generation_info is not None:
-                    
-                    # add finish reason to the inference 
-                    # if generation info contains a finish reason and if the ArthurModel was registered to monitor finish_reason
-                    if FINISH_REASON in generation.generation_info and FINISH_REASON in self.attr_names:
-                        inference[FINISH_REASON] = generation.generation_info[FINISH_REASON]
-                    
-                    # add token likelihoods data to the inference if the ArthurModel was registered to monitor token likelihoods
+                    # add finish reason to the inference
+                    # if generation info contains a finish reason and
+                    # if the ArthurModel was registered to monitor finish_reason
+                    if (
+                        FINISH_REASON in generation.generation_info
+                        and FINISH_REASON in self.attr_names
+                    ):
+                        inference[FINISH_REASON] = generation.generation_info[
+                            FINISH_REASON
+                        ]
+
+                    # add token likelihoods data to the inference if the ArthurModel
+                    # was registered to monitor token likelihoods
                     logprobs_data = generation.generation_info["logprobs"]
-                    if logprobs_data is not None and self.token_likelihood_attr is not None:
+                    if (
+                        logprobs_data is not None
+                        and self.token_likelihood_attr is not None
+                    ):
                         logprobs = logprobs_data["top_logprobs"]
-                        likelihoods = [{k: np.exp(v) for k,v in logprobs[i].items()} for i in range(len(logprobs))]
+                        likelihoods = [
+                            {k: np.exp(v) for k, v in logprobs[i].items()}
+                            for i in range(len(logprobs))
+                        ]
                         inference[self.token_likelihood_attr] = likelihoods
-                
-                # add token usage counts to the inference if the ArthurModel was registered to monitor token usage
-                if isinstance(response.llm_output, dict) and TOKEN_USAGE in response.llm_output:
+
+                # add token usage counts to the inference if the
+                # ArthurModel was registered to monitor token usage
+                if (
+                    isinstance(response.llm_output, dict)
+                    and TOKEN_USAGE in response.llm_output
+                ):
                     token_usage = response.llm_output[TOKEN_USAGE]
-                    if PROMPT_TOKENS in token_usage and PROMPT_TOKENS in self.attr_names:
+                    if (
+                        PROMPT_TOKENS in token_usage
+                        and PROMPT_TOKENS in self.attr_names
+                    ):
                         inference[PROMPT_TOKENS] = token_usage[PROMPT_TOKENS]
-                    if COMPLETION_TOKENS in token_usage and COMPLETION_TOKENS in self.attr_names:
+                    if (
+                        COMPLETION_TOKENS in token_usage
+                        and COMPLETION_TOKENS in self.attr_names
+                    ):
                         inference[COMPLETION_TOKENS] = token_usage[COMPLETION_TOKENS]
-                
-                # add inference duration to the inference if the ArthurModel was registered to monitor inference duration
+
+                # add inference duration to the inference if the ArthurModel
+                # was registered to monitor inference duration
                 if DURATION in self.attr_names:
                     inference[DURATION] = time_from_start_to_end
-                
+
                 inferences.append(inference)
-                
+
         # send inferences to arthur
         self.arthur_model.send_inferences(inferences)
 
