696886f3979009af77c3254c44aca35fa0755c62
Nuno Campos <nuno@boringbits.io>
2023-07-04 10:19:08 +0100

    Use serialized format for messages in tracer (#6827)
    <!-- Thank you for contributing to LangChain!
    
    Replace this comment with:
      - Description: a description of the change, 
      - Issue: the issue # it fixes (if applicable),
      - Dependencies: any dependencies required for this change,
    - Tag maintainer: for a quicker response, tag the relevant maintainer
    (see below),
    - Twitter handle: we announce bigger features on Twitter. If your PR
    gets announced and you'd like a mention, we'll gladly shout you out!
    
    If you're adding a new integration, please include:
    1. a test for the integration, preferably unit tests that do not rely on
    network access,
      2. an example notebook showing its use.
    
    Maintainer responsibilities:
      - General / Misc / if you don't know who to tag: @dev2049
      - DataLoaders / VectorStores / Retrievers: @rlancemartin, @eyurtsev
      - Models / Prompts: @hwchase17, @dev2049
      - Memory: @hwchase17
      - Agents / Tools / Toolkits: @vowelparrot
      - Tracing / Callbacks: @agola11
      - Async: @agola11
    
    If no one reviews your PR within a few days, feel free to @-mention the
    same people again.
    
    See contribution guidelines for more information on how to write/run
    tests, lint, etc:
    https://github.com/hwchase17/langchain/blob/master/.github/CONTRIBUTING.md
     -->

--- langchain/callbacks/tracers/langchain.py
+++ langchain/callbacks/tracers/langchain.py
@@ -11,13 +11,10 @@ from uuid import UUID
 from langchainplus_sdk import LangChainPlusClient
 
 from langchain.callbacks.tracers.base import BaseTracer
-from langchain.callbacks.tracers.schemas import (
-    Run,
-    RunTypeEnum,
-    TracerSession,
-)
+from langchain.callbacks.tracers.schemas import Run, RunTypeEnum, TracerSession
 from langchain.env import get_runtime_environment
-from langchain.schema.messages import BaseMessage, messages_to_dict
+from langchain.load.dump import dumpd
+from langchain.schema.messages import BaseMessage
 
 logger = logging.getLogger(__name__)
 _LOGGED = set()
@@ -83,7 +80,7 @@ class LangChainTracer(BaseTracer):
             id=run_id,
             parent_run_id=parent_run_id,
             serialized=serialized,
-            inputs={"messages": [messages_to_dict(batch) for batch in messages]},
+            inputs={"messages": [[dumpd(msg) for msg in batch] for batch in messages]},
             extra=kwargs,
             events=[{"name": "start", "time": start_time}],
             start_time=start_time,
