a56b7f630e83aed1d6a63fd608ead698666279d8
Harrison Chase <hw.chase.17@gmail.com>
2023-07-05 12:45:22 -0400

    cr

--- langchain/callbacks/streaming_aiter.py
+++ langchain/callbacks/streaming_aiter.py
@@ -31,7 +31,7 @@ class AsyncIteratorCallbackHandler(AsyncCallbackHandler):
         self.done.clear()
 
     async def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
-        if token is not None and token != '':
+        if token is not None and token != "":
             self.queue.put_nowait(token)
 
     async def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
