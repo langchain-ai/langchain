ea767aab1029aa6ef6e9824a83731d6f08c775bd
Fangrui Liu <fangruil@moqi.ai>
2023-07-05 14:58:57 +0800

    extensive for other VectorSQL

--- langchain/chains/sql_database/parser.py
+++ langchain/chains/sql_database/parser.py
@@ -4,9 +4,9 @@ from langchain.schema import BaseOutputParser
 
 class VectorSQLOutputParser(BaseOutputParser):
     model: BaseModel
-    
-    def from_embeddings(cls, model: BaseModel, **kwargs: Any):
-        return cls(model, **kwargs)
+    distance_func_name: str = 'distance'
+    def from_embeddings(cls, model: BaseModel, distance_func_name: str ='distane', **kwargs: Any):
+        return cls(model=model, distance_func_name=distance_func_name, **kwargs)
     
     def parse(self, text: str):
         start = text.find('NeuralArray(')
@@ -16,7 +16,7 @@ class VectorSQLOutputParser(BaseOutputParser):
             entity = _matched[:_matched.find(')')]
             vecs = self.model.embed_query(entity)
             vecs_str = '[' + ','.join(map(str, vecs)) + ']'
-            _sql_str_compl = text.replace('DISTANCE', 'distance').replace(text[start:end], vecs_str)
+            _sql_str_compl = text.replace('DISTANCE', self.distance_func_name).replace(text[start:end], vecs_str)
             if _sql_str_compl[-1] == ';':
                 _sql_str_compl = _sql_str_compl[:-1]
             text = _sql_str_compl
