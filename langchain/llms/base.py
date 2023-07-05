52d32969115c518bbd8ac8c06c2477ed685442d3
Leonid Kuligin <kuligin@google.com>
2023-07-05 20:57:36 +0200

    Added retries for Vertex LLM

--- langchain/llms/base.py
+++ langchain/llms/base.py
@@ -1,14 +1,35 @@
 """Base interface for large language models to expose."""
+from __future__ import annotations
+
 import asyncio
+import functools
 import inspect
 import json
 import warnings
 from abc import ABC, abstractmethod
 from pathlib import Path
-from typing import Any, Dict, List, Mapping, Optional, Sequence, Tuple, Union
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    List,
+    Mapping,
+    Optional,
+    Sequence,
+    Tuple,
+    Type,
+    Union,
+)
 
 import yaml
 from pydantic import Field, root_validator, validator
+from tenacity import (
+    before_sleep_log,
+    retry,
+    retry_if_exception_type,
+    stop_after_attempt,
+    wait_exponential,
+)
 
 import langchain
 from langchain.base_language import BaseLanguageModel
@@ -32,6 +53,38 @@ from langchain.schema.messages import AIMessage, BaseMessage, get_buffer_string
 
 def _get_verbosity() -> bool:
     return langchain.verbose
+
+
+def create_base_retry_decorator(
+    error_types: List[Type[BaseException]], max_retries: int = 1
+) -> Callable[[Any], Any]:
+    """Create a retry decorator for a given LLM and provided list of error types."""
+    import logging
+
+    logger = logging.getLogger(__name__)
+
+    min_seconds = 4
+    max_seconds = 10
+    # Wait 2^x * 1 second between each retry starting with
+    # 4 seconds, then up to 10 seconds, then 10 seconds afterwards
+    if len(error_types) == 0:
+        return lambda x: x
+    elif len(error_types) == 1:
+        retries = retry_if_exception_type(error_types[0])
+    else:
+        init = retry_if_exception_type(error_types[0])
+        retries = functools.reduce(
+            lambda x, y: x | retry_if_exception_type(y),  # type: ignore
+            error_types[1:],
+            init,
+        )
+    return retry(
+        reraise=True,
+        stop=stop_after_attempt(max_retries),
+        wait=wait_exponential(multiplier=1, min=min_seconds, max=max_seconds),
+        retry=retries,
+        before_sleep=before_sleep_log(logger, logging.WARNING),
+    )
 
 
 def get_prompts(
