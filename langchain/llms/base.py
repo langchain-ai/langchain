e4088b026f0154a6ba2844af405135b06500cbae
Leonid Kuligin <kuligin@google.com>
2023-07-06 07:42:20 +0200

    fixes after review

--- langchain/llms/base.py
+++ langchain/llms/base.py
@@ -2,9 +2,9 @@
 from __future__ import annotations
 
 import asyncio
-import functools
 import inspect
 import json
+import logging
 import warnings
 from abc import ABC, abstractmethod
 from pathlib import Path
@@ -26,6 +26,7 @@ from pydantic import Field, root_validator, validator
 from tenacity import (
     before_sleep_log,
     retry,
+    retry_base,
     retry_if_exception_type,
     stop_after_attempt,
     wait_exponential,
@@ -50,6 +51,8 @@ from langchain.schema import (
 )
 from langchain.schema.messages import AIMessage, BaseMessage, get_buffer_string
 
+logger = logging.getLogger(__name__)
+
 
 def _get_verbosity() -> bool:
     return langchain.verbose
@@ -59,30 +62,19 @@ def create_base_retry_decorator(
     error_types: List[Type[BaseException]], max_retries: int = 1
 ) -> Callable[[Any], Any]:
     """Create a retry decorator for a given LLM and provided list of error types."""
-    import logging
-
-    logger = logging.getLogger(__name__)
 
     min_seconds = 4
     max_seconds = 10
     # Wait 2^x * 1 second between each retry starting with
     # 4 seconds, then up to 10 seconds, then 10 seconds afterwards
-    if len(error_types) == 0:
-        return lambda x: x
-    elif len(error_types) == 1:
-        retries = retry_if_exception_type(error_types[0])
-    else:
-        init = retry_if_exception_type(error_types[0])
-        retries = functools.reduce(
-            lambda x, y: x | retry_if_exception_type(y),  # type: ignore
-            error_types[1:],
-            init,
-        )
+    retry_instance: "retry_base" = retry_if_exception_type(error_types[0])
+    for error in error_types[1:]:
+        retry_instance = retry_instance | retry_if_exception_type(error)
     return retry(
         reraise=True,
         stop=stop_after_attempt(max_retries),
         wait=wait_exponential(multiplier=1, min=min_seconds, max=max_seconds),
-        retry=retries,
+        retry=retry_instance,
         before_sleep=before_sleep_log(logger, logging.WARNING),
     )
 
