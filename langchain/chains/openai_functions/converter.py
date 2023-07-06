046a3f654165b7eee25365926b9d937169cd2980
Eric <enjui.chang@gmail.com>
2023-07-06 11:35:58 -0700

    Add dictionary support

--- langchain/chains/openai_functions/converter.py
+++ langchain/chains/openai_functions/converter.py
@@ -29,9 +29,8 @@ def from_pydantic_create_openai_function_dict(
     return llm_kwargs
 
 
-
 def from_json_create_openai_function_dict(
-        json_schema: Any, func_name: str, func_description: str
+        json_schema: str, func_name: str, func_description: str
     ) -> dict:
     """
     Converts a json schema and func property to a dict that is a correct function calling format
@@ -52,6 +51,34 @@ def from_json_create_openai_function_dict(
         "name": func_name,
         "description": func_description,
         "parameters": json_dict,
+    }
+
+    llm_kwargs = get_llm_kwargs(function_props)
+
+    return llm_kwargs
+
+
+def from_dict_create_openai_function_dict(
+        dict_schema: dict, func_name: str, func_description: str
+    ) -> dict:
+    """
+    Converts a dictionary and func property to a dict that is a correct function calling format
+    for the llm_kwargs prop for LLMChain
+
+    Args:
+    dict_schema: dictionary schema of output
+    func_name: str
+    func_description: str
+
+    Returns:
+    The kwargs for the LLMChain constructor.
+    """
+
+    dict_schema["type"] = "object"
+    function_props = {
+        "name": func_name,
+        "description": func_description,
+        "parameters": dict_schema,
     }
 
     llm_kwargs = get_llm_kwargs(function_props)
