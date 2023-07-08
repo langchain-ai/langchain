e169b4c37c38554b5c1c09ec4cc819e727d19ab8
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2023-07-07 21:42:48 -0700

    use patch

--- tests/unit_tests/callbacks/tracers/test_langchain.py
+++ tests/unit_tests/callbacks/tracers/test_langchain.py
@@ -29,22 +29,23 @@ def test_example_id_assignment_threadsafe() -> None:
             time.sleep(0.01)
             old_persist_run_single(run)
 
-        tracer._persist_run_single = new_persist_run_single
-
-        run_id_1 = UUID("9d878ab3-e5ca-4218-aef6-44cbdc90160a")
-        run_id_2 = UUID("f1f9fa53-8b2f-4742-bdbc-38215f7bd1e1")
-        example_id_1 = UUID("57e42c57-8c79-4d9f-8765-bf6cd3a98055")
-        tracer.example_id = example_id_1
-        tracer.on_llm_start({"name": "example_1"}, ["foo"], run_id=run_id_1)
-        tracer.on_llm_end(LLMResult(generations=[], llm_output={}), run_id=run_id_1)
-        example_id_2 = UUID("4f31216e-7c26-4027-a5fd-0bbf9ace17dc")
-        tracer.example_id = example_id_2
-        tracer.on_llm_start({"name": "example_2"}, ["foo"], run_id=run_id_2)
-        tracer.on_llm_end(LLMResult(generations=[], llm_output={}), run_id=run_id_2)
-        tracer.example_id = None
-        expected_example_ids = {
-            run_id_1: example_id_1,
-            run_id_2: example_id_2,
-        }
-        tracer.wait_for_futures()
-        assert example_ids == expected_example_ids
+        with unittest.mock.patch.object(
+            tracer, "_persist_run_single", new=new_persist_run_single
+        ):
+            run_id_1 = UUID("9d878ab3-e5ca-4218-aef6-44cbdc90160a")
+            run_id_2 = UUID("f1f9fa53-8b2f-4742-bdbc-38215f7bd1e1")
+            example_id_1 = UUID("57e42c57-8c79-4d9f-8765-bf6cd3a98055")
+            tracer.example_id = example_id_1
+            tracer.on_llm_start({"name": "example_1"}, ["foo"], run_id=run_id_1)
+            tracer.on_llm_end(LLMResult(generations=[], llm_output={}), run_id=run_id_1)
+            example_id_2 = UUID("4f31216e-7c26-4027-a5fd-0bbf9ace17dc")
+            tracer.example_id = example_id_2
+            tracer.on_llm_start({"name": "example_2"}, ["foo"], run_id=run_id_2)
+            tracer.on_llm_end(LLMResult(generations=[], llm_output={}), run_id=run_id_2)
+            tracer.example_id = None
+            expected_example_ids = {
+                run_id_1: example_id_1,
+                run_id_2: example_id_2,
+            }
+            tracer.wait_for_futures()
+            assert example_ids == expected_example_ids
