c1bde33bac2f904588c476b9de48fd8de50fa6eb
keenborder786 <21110290@lums.edu.pk>
2023-07-09 22:06:41 +0500

    [feat]: Added a utils module which validates the sql-chain memory i.e the input and memory keys

--- langchain/chains/sql_database/utils.py
+++ langchain/chains/sql_database/utils.py
@@ -1,6 +1,12 @@
 from langchain.schema import BaseMemory
 
 
-def validate_sql_chain_memory(memory:BaseMemory) -> None:
+def validate_sql_chain_memory(memory: BaseMemory) -> None:
     if memory.input_key is None:
         raise ValueError("You need to provide an input_key for SQLDatabaseChain Memory")
+    if memory.input_key != "input":
+        raise ValueError(f"Your memory_key should be input and not {memory.input_key}")
+    if memory.memory_key != "history":
+        raise ValueError(
+            f"Your memory_key should be history and not {memory.memory_key}"
+        )
