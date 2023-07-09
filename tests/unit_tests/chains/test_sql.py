cdfd3379f92cfe03dc53d7814b0848cf695e59cf
keenborder786 <21110290@lums.edu.pk>
2023-07-09 22:08:51 +0500

    [chore]: Reformatted test_sql.py module by running the linter

--- tests/unit_tests/chains/test_sql.py
+++ tests/unit_tests/chains/test_sql.py
@@ -6,23 +6,21 @@ from langchain.memory import ConversationBufferMemory
 from langchain.sql_database import SQLDatabase
 from tests.unit_tests.llms.fake_llm import FakeLLM
 
+
 def test_sql_chain_without_memory() -> None:
     db = SQLDatabase.from_uri("sqlite:///:memory:")
     queries = {"foo": "Final Answer: baz"}
     llm = FakeLLM(queries=queries, sequential_responses=True)
-    db_chain = SQLDatabaseChain.from_llm(
-        llm, db, verbose=True
-    )
+    db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
     assert db_chain.run("hello") == "Final Answer: baz"
+
 
 def test_sql_chain_with_valid_memory() -> None:
     db = SQLDatabase.from_uri("sqlite:///:memory:")
     queries = {"foo": "Final Answer: baz"}
     llm = FakeLLM(queries=queries, sequential_responses=True)
     memory = ConversationBufferMemory(memory_key="history", input_key="input")
-    db_chain = SQLDatabaseChain.from_llm(
-        llm, db, verbose=True
-    )
+    db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
     assert db_chain.run("hello") == "Final Answer: baz"
 
 
@@ -32,9 +30,7 @@ def test_sql_chain_invalid_memory_key() -> None:
     llm = FakeLLM(queries=queries, sequential_responses=True)
     with pytest.raises(ValueError):
         memory = ConversationBufferMemory(memory_key="chat_history", input_key="input")
-        db_chain = SQLDatabaseChain.from_llm(
-            llm, db, memory = memory , verbose=True
-        )
+        db_chain = SQLDatabaseChain.from_llm(llm, db, memory=memory, verbose=True)
 
 
 def test_sql_chain_invalid_memory_input_key() -> None:
@@ -43,9 +39,4 @@ def test_sql_chain_invalid_memory_input_key() -> None:
     llm = FakeLLM(queries=queries, sequential_responses=True)
     with pytest.raises(ValueError):
         memory = ConversationBufferMemory(memory_key="history", input_key="query")
-        db_chain = SQLDatabaseChain.from_llm(
-            llm, db, memory = memory , verbose=True
-        )
-
-
-    
+        db_chain = SQLDatabaseChain.from_llm(llm, db, memory=memory, verbose=True)
