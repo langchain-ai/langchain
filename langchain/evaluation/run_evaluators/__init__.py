4ecbb3aeac2519fa7d5b0936a76f33cb5fb8a46a
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2023-07-10 10:52:19 -0700

    Delete deprecated run evaluator loaders

--- langchain/evaluation/run_evaluators/__init__.py
+++ langchain/evaluation/run_evaluators/__init__.py
@@ -4,13 +4,6 @@ from langchain.evaluation.run_evaluators.base import (
     RunEvaluatorInputMapper,
     RunEvaluatorOutputParser,
 )
-from langchain.evaluation.run_evaluators.implementations import (
-    ChoicesOutputParser,
-    StringRunEvaluatorInputMapper,
-    get_criteria_evaluator,
-    get_qa_evaluator,
-    get_trajectory_evaluator,
-)
 from langchain.evaluation.run_evaluators.loading import (
     load_run_evaluator_for_model,
     load_run_evaluators_for_model,
