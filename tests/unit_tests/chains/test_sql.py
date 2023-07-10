e948de1506fa011cf75ebdb0bdfa668cf4c71507
keenborder786 <21110290@lums.edu.pk>
2023-07-10 18:53:29 +0500

    [tests]: Changed the test to account for the new flow with respect to sql-chain memory

--- tests/unit_tests/chains/test_sql.py
+++ tests/unit_tests/chains/test_sql.py
@@ -4,6 +4,7 @@ from langchain import SQLDatabaseChain
 from langchain.chains import SQLDatabaseSequentialChain
 from langchain.memory import ConversationBufferMemory
 from langchain.sql_database import SQLDatabase
+from langchain.prompts import PromptTemplate
 from tests.unit_tests.llms.fake_llm import FakeLLM
 
 
@@ -16,27 +17,45 @@ def test_sql_chain_without_memory() -> None:
 
 
 def test_sql_chain_with_valid_memory() -> None:
+    valid_prompt_with_history = """
+    Only use the following tables:
+    {table_info}
+    Question: {input}
+    
+    Given an input question, first create a syntactically correct {dialect} query to run.
+    Always limit your query to at most {top_k} results.
+
+    Relevant pieces of previous conversation:
+    {history}
+
+    (You do not need to use these pieces of information if not relevant)
+    """
+    prompt = PromptTemplate(
+    input_variables=["input", "table_info", "dialect", "top_k", "history"],
+    template=valid_prompt_with_history)
     db = SQLDatabase.from_uri("sqlite:///:memory:")
     queries = {"foo": "Final Answer: baz"}
     llm = FakeLLM(queries=queries, sequential_responses=True)
     memory = ConversationBufferMemory(memory_key="history", input_key="input")
-    db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
+    db_chain = SQLDatabaseChain.from_llm(llm, db, memory = memory , prompt = prompt, verbose=True)
     assert db_chain.run("hello") == "Final Answer: baz"
 
 
-def test_sql_chain_invalid_memory_key() -> None:
+def test_sql_chain_invalid_memory() -> None:
+    invalid_prompt_without_history = """
+    Only use the following tables:
+    {table_info}
+    Question: {input}
+    
+    Given an input question, first create a syntactically correct {dialect} query to run.
+    Always limit your query to at most {top_k} results.
+    """
+    prompt = PromptTemplate(
+    input_variables=["input", "table_info", "dialect", "top_k"],
+    template=invalid_prompt_without_history)
     db = SQLDatabase.from_uri("sqlite:///:memory:")
     queries = {"foo": "Final Answer: baz"}
     llm = FakeLLM(queries=queries, sequential_responses=True)
+    memory = ConversationBufferMemory(memory_key="history", input_key="input")
     with pytest.raises(ValueError):
-        memory = ConversationBufferMemory(memory_key="chat_history", input_key="input")
-        db_chain = SQLDatabaseChain.from_llm(llm, db, memory=memory, verbose=True)
-
-
-def test_sql_chain_invalid_memory_input_key() -> None:
-    db = SQLDatabase.from_uri("sqlite:///:memory:")
-    queries = {"foo": "Final Answer: baz"}
-    llm = FakeLLM(queries=queries, sequential_responses=True)
-    with pytest.raises(ValueError):
-        memory = ConversationBufferMemory(memory_key="history", input_key="query")
-        db_chain = SQLDatabaseChain.from_llm(llm, db, memory=memory, verbose=True)
+        db_chain = SQLDatabaseChain.from_llm(llm, db, memory = memory, prompt = prompt, verbose=True)
