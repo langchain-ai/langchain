92b4418c8c03a7737764af6c7f19549297ded977
Kazuki Maeda <kzk.maeda0711@gmail.com>
2023-07-10 17:27:55 +0900

    Datadog logs loader (#7356)
    ### Description
    Created a Loader to get a list of specific logs from Datadog Logs.
    
    ### Dependencies
    `datadog_api_client` is required.
    
    ### Twitter handle
    [kzk_maeda](https://twitter.com/kzk_maeda)
    
    ---------
    
    Co-authored-by: Bagatur <baskaryan@gmail.com>

--- langchain/document_loaders/__init__.py
+++ langchain/document_loaders/__init__.py
@@ -30,6 +30,7 @@ from langchain.document_loaders.confluence import ConfluenceLoader
 from langchain.document_loaders.conllu import CoNLLULoader
 from langchain.document_loaders.csv_loader import CSVLoader, UnstructuredCSVLoader
 from langchain.document_loaders.cube_semantic import CubeSemanticLoader
+from langchain.document_loaders.datadog_logs import DatadogLogsLoader
 from langchain.document_loaders.dataframe import DataFrameLoader
 from langchain.document_loaders.diffbot import DiffbotLoader
 from langchain.document_loaders.directory import DirectoryLoader
@@ -179,6 +180,7 @@ __all__ = [
     "CollegeConfidentialLoader",
     "ConfluenceLoader",
     "CubeSemanticLoader",
+    "DatadogLogsLoader",
     "DataFrameLoader",
     "DiffbotLoader",
     "DirectoryLoader",
