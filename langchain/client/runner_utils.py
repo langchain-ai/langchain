fbfa2f9d100d55386ca97cb08a924ad9bce51bcb
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2023-07-10 09:23:30 -0700

    Rm create_project line

--- langchain/client/runner_utils.py
+++ langchain/client/runner_utils.py
@@ -384,8 +384,6 @@ async def arun_on_examples(
     """
     project_name = _get_project_name(project_name, llm_or_chain_factory, None)
     client_ = client or LangChainPlusClient()
-    client_.create_project(project_name)
-
     results: Dict[str, List[Any]] = {}
 
     async def process_example(
@@ -584,7 +582,6 @@ def run_on_examples(
     results: Dict[str, Any] = {}
     project_name = _get_project_name(project_name, llm_or_chain_factory, None)
     client_ = client or LangChainPlusClient()
-    client_.create_project(project_name)
     tracer = LangChainTracer(project_name=project_name)
     evaluator_project_name = f"{project_name}-evaluators"
     evalution_handler = EvaluatorCallbackHandler(
