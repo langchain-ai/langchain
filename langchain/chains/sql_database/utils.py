160c1cf666b44acdd1b9eab56b99f08b0809b0ae
keenborder786 <21110290@lums.edu.pk>
2023-07-10 18:50:51 +0500

    [refactor]: Changed validation function by making sure users provided memory key is in prompt template rather than restricting the memory_key itself as I did previously. This removes the need of changing default prompt of sql_database package to use history as input variable

--- langchain/chains/sql_database/utils.py
+++ langchain/chains/sql_database/utils.py
@@ -1,12 +1,11 @@
-from langchain.schema import BaseMemory
+"""utils module consist of function which validates memory passed to sql agent/chain"""
+
+from langchain.schema import BaseMemory, BasePromptTemplate
 
 
-def validate_sql_chain_memory(memory: BaseMemory) -> None:
-    if memory.input_key is None:
-        raise ValueError("You need to provide an input_key for SQLDatabaseChain Memory")
-    if memory.input_key != "input":
-        raise ValueError(f"Your memory_key should be input and not {memory.input_key}")
-    if memory.memory_key != "history":
-        raise ValueError(
-            f"Your memory_key should be history and not {memory.memory_key}"
-        )
+def validate_sql_chain_memory(memory: BaseMemory, prompt: BasePromptTemplate) -> None:
+    if not memory.input_key:
+        raise ValueError("You need to provide an input_key for memory to work with sql-database chain/agent")
+    if memory.memory_key not in prompt.input_variables:
+        raise ValueError(f"You need to provide {memory.memory_key} as input variable in your PromptTemplate")
+
