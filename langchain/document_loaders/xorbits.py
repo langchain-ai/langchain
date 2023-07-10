4774bdad100fc482c1c901e3359d4606008c22ea
Bagatur <baskaryan@gmail.com>
2023-07-10 04:17:31 -0400

    cr

--- langchain/document_loaders/xorbits.py
+++ langchain/document_loaders/xorbits.py
@@ -10,12 +10,20 @@ class XorbitsLoader(BaseLoader):
     def __init__(self, data_frame: Any, page_content_column: str = "text"):
         """Initialize with dataframe object.
 
+        Requirements:
+            Must have xorbits installed. You can install with `pip install xorbits`.
+
         Args:
             data_frame: Xorbits DataFrame object.
             page_content_column: Name of the column containing the page content.
               Defaults to "text".
         """
-        import xorbits.pandas as pd
+        try:
+            import xorbits.pandas as pd
+        except ImportError as e:
+            raise ImportError(
+                "Cannot import xorbits, please install with 'pip install xorbits'."
+            ) from e
 
         if not isinstance(data_frame, pd.DataFrame):
             raise ValueError(
