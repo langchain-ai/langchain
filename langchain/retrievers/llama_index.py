eb2c8c180c9b9df87eb2f790b2f763f6c99c8819
Víctor Navarro Aránguiz <victornavarrokp47@gmail.com>
2023-07-11 10:11:01 -0400

    Update llama_index.py to new versions of llama-index
    Llama-index updated its files and the retrievers no longer worked. I also added the async version of `get_relevant_documents`
    @rlancemartin, @eyurtsev

--- langchain/retrievers/llama_index.py
+++ langchain/retrievers/llama_index.py
@@ -2,10 +2,6 @@ from typing import Any, Dict, List, cast
 
 from pydantic import Field
 
-from langchain.callbacks.manager import (
-    AsyncCallbackManagerForRetrieverRun,
-    CallbackManagerForRetrieverRun,
-)
 from langchain.schema import BaseRetriever, Document
 
 
@@ -15,9 +11,7 @@ class LlamaIndexRetriever(BaseRetriever):
     index: Any
     query_kwargs: Dict = Field(default_factory=dict)
 
-    def _get_relevant_documents(
-        self, query: str, *, run_manager: CallbackManagerForRetrieverRun
-    ) -> List[Document]:
+    def get_relevant_documents(self, query: str) -> List[Document]:
         """Get documents relevant for a query."""
         try:
             from llama_index.indices.base import BaseGPTIndex
@@ -28,38 +22,50 @@ class LlamaIndexRetriever(BaseRetriever):
             )
         index = cast(BaseGPTIndex, self.index)
 
-        response = index.query(query, response_mode="no_text", **self.query_kwargs)
+        response = index.as_query_engine(response_mode="no_text", **self.query_kwargs).query(query)
         response = cast(Response, response)
         # parse source nodes
         docs = []
         for source_node in response.source_nodes:
-            metadata = source_node.extra_info or {}
+            metadata = source_node.node.metadata or {}
             docs.append(
-                Document(page_content=source_node.source_text, metadata=metadata)
+                Document(page_content=source_node.node.text, metadata=metadata)
             )
         return docs
 
-    async def _aget_relevant_documents(
-        self, query: str, *, run_manager: AsyncCallbackManagerForRetrieverRun
-    ) -> List[Document]:
-        raise NotImplementedError("LlamaIndexRetriever does not support async")
+    async def aget_relevant_documents(self, query: str) -> List[Document]:
+        """Get documents relevant for a query using async."""
+        try:
+            from llama_index.indices.base import BaseGPTIndex
+            from llama_index.response.schema import Response
+        except ImportError:
+            raise ImportError(
+                "You need to install `pip install llama-index` to use this retriever."
+            )
+        index = cast(BaseGPTIndex, self.index)
+
+        response = await index.as_query_engine(response_mode="no_text", **self.query_kwargs).aquery(query)
+        response = cast(Response, response)
+        # parse source nodes
+        docs = []
+        for source_node in response.source_nodes:
+            metadata = source_node.node.metadata or {}
+            docs.append(
+                Document(page_content=source_node.node.text, metadata=metadata)
+            )
+        return docs
 
 
 class LlamaIndexGraphRetriever(BaseRetriever):
     """Question-answering with sources over an LlamaIndex graph data structure."""
 
     graph: Any
-    query_configs: List[Dict] = Field(default_factory=list)
+    query_kwargs: Dict = Field(default_factory=dict)
 
-    def _get_relevant_documents(
-        self, query: str, *, run_manager: CallbackManagerForRetrieverRun
-    ) -> List[Document]:
+    def get_relevant_documents(self, query: str) -> List[Document]:
         """Get documents relevant for a query."""
         try:
-            from llama_index.composability.graph import (
-                QUERY_CONFIG_TYPE,
-                ComposableGraph,
-            )
+            from llama_index.composability import ComposableGraph
             from llama_index.response.schema import Response
         except ImportError:
             raise ImportError(
@@ -67,23 +73,41 @@ class LlamaIndexGraphRetriever(BaseRetriever):
             )
         graph = cast(ComposableGraph, self.graph)
 
-        # for now, inject response_mode="no_text" into query configs
-        for query_config in self.query_configs:
-            query_config["response_mode"] = "no_text"
-        query_configs = cast(List[QUERY_CONFIG_TYPE], self.query_configs)
-        response = graph.query(query, query_configs=query_configs)
+        custom_query_engines = self.query_kwargs
+        custom_query_engines['response_mode'] = "no_text"
+        response = graph.as_query_engine(custom_query_engines=custom_query_engines).query(query)
         response = cast(Response, response)
 
         # parse source nodes
         docs = []
         for source_node in response.source_nodes:
-            metadata = source_node.extra_info or {}
+            metadata = source_node.node.metadata or {}
             docs.append(
-                Document(page_content=source_node.source_text, metadata=metadata)
+                Document(page_content=source_node.node.text, metadata=metadata)
             )
         return docs
 
-    async def _aget_relevant_documents(
-        self, query: str, *, run_manager: AsyncCallbackManagerForRetrieverRun
-    ) -> List[Document]:
-        raise NotImplementedError("LlamaIndexGraphRetriever does not support async")
+    async def aget_relevant_documents(self, query: str) -> List[Document]:
+        """Get documents relevant for a query using async."""
+        try:
+            from llama_index.composability import ComposableGraph
+            from llama_index.response.schema import Response
+        except ImportError:
+            raise ImportError(
+                "You need to install `pip install llama-index` to use this retriever."
+            )
+        graph = cast(ComposableGraph, self.graph)
+
+        custom_query_engines = self.query_kwargs
+        custom_query_engines['response_mode'] = "no_text"
+        response = await graph.as_query_engine(custom_query_engines=custom_query_engines).aquery(query)
+        response = cast(Response, response)
+
+        # parse source nodes
+        docs = []
+        for source_node in response.source_nodes:
+            metadata = source_node.node.metadata or {}
+            docs.append(
+                Document(page_content=source_node.node.text, metadata=metadata)
+            )
+        return docs
