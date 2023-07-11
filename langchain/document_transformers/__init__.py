2fb865b12867a3fa436da8cebdf902a525c0dda8
jasonwcfan <jason.wc.fan@gmail.com>
2023-07-11 14:09:20 -0700

    add additional import

--- langchain/document_transformers/__init__.py
+++ langchain/document_transformers/__init__.py
@@ -1,1 +1,1 @@
-from .embeddings_redundant_filter import EmbeddingsRedundantFilter, EmbeddingsClusteringFilter, _DocumentWithState, _filter_similar_embeddings, _get_embeddings_from_stateful_docs, get_stateful_documents
+from .embeddings_redundant_filter import EmbeddingsRedundantFilter, EmbeddingsClusteringFilter, _DocumentWithState, _filter_similar_embeddings, _get_embeddings_from_stateful_docs, get_stateful_documents, _filter_cluster_embeddingsgit 
