ca27dce0474c5aa894d1316c2d4c2408ee188b30
Lance Martin <lance@langchain.dev>
2023-07-12 16:30:39 -0700

    Upddate interface

--- langchain/document_transformers/__init__.py
+++ langchain/document_transformers/__init__.py
@@ -2,9 +2,6 @@ from langchain.document_transformers.embeddings_redundant_filter import (
     EmbeddingsClusteringFilter,
     EmbeddingsRedundantFilter,
     _DocumentWithState,
-    _filter_similar_embeddings,
-    _get_embeddings_from_stateful_docs,
-    get_stateful_documents,
 )
 from langchain.document_transformers.text_extract import DoctranPropertyExtractor
 from langchain.document_transformers.text_qa import DoctranQATransformer
@@ -17,7 +14,4 @@ __all__ = [
     "EmbeddingsClusteringFilter",
     "EmbeddingsRedundantFilter",
     "_DocumentWithState",
-    "_filter_similar_embeddings",
-    "_get_embeddings_from_stateful_docs",
-    "get_stateful_documents",
 ]
