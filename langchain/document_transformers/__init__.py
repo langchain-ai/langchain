4b2d0212f80bd2e3d5b99c517a4bbc9bbf982641
Lance Martin <lance@langchain.dev>
2023-07-12 13:47:42 -0700

    Need to move document_transformers.py due to redundant naming w new subdir

--- langchain/document_transformers/__init__.py
+++ langchain/document_transformers/__init__.py
@@ -1,3 +1,11 @@
 from langchain.document_transformers.text_qa import DoctranQATransformer
 from langchain.document_transformers.text_translate import DoctranTextTranslator
 from langchain.document_transformers.text_extract import DoctranPropertyExtractor
+from langchain.document_transformers.embeddings_redundant_filter import (
+    EmbeddingsClusteringFilter,
+    EmbeddingsRedundantFilter,
+    _DocumentWithState,
+    _filter_similar_embeddings,
+    _get_embeddings_from_stateful_docs,
+    get_stateful_documents,
+)
