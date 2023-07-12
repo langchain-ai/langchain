8b6a3dd010745fead83814118d9fb66cb7d3ff24
Piyush Jain <piyushjain@duck.com>
2023-07-11 20:27:04 -0700

    Updated the prompt

--- langchain/chains/graph_qa/prompts.py
+++ langchain/chains/graph_qa/prompts.py
@@ -200,10 +200,10 @@ SPARQL_QA_PROMPT = PromptTemplate(
 
 NEPTUNE_CYPHER_EXTRA_INSTRUCTIONS = """
 Instructions:
-Generate the query in openCypher format that follows these rules:
-1. Do not use `NONE`, `ALL` or `ANY` predicate functions, rather use list comprehensions.
-2. Do not use `REDUCE` function. Rather use a combination of list comprehension and the `UNWIND` clause to achieve similar results.
-3. Do not use `FOREACH` clause. Rather use a combination of `WITH` and `UNWIND` clauses to achieve similar results.
+Generate the query in openCypher format and follow these rules:
+Do not use `NONE`, `ALL` or `ANY` predicate functions, rather use list comprehensions.
+Do not use `REDUCE` function. Rather use a combination of list comprehension and the `UNWIND` clause to achieve similar results.
+Do not use `FOREACH` clause. Rather use a combination of `WITH` and `UNWIND` clauses to achieve similar results.
 \n"""
 
 NEPTUNE_CYPHER_GENERATION_TEMPLATE = CYPHER_GENERATION_TEMPLATE.replace("Instructions:", NEPTUNE_CYPHER_EXTRA_INSTRUCTIONS)
