91c2a5925e7a242531486c0aaf096f8c1a6b7209
Bagatur <baskaryan@gmail.com>
2023-07-12 23:44:22 -0400

    final

--- langchain/document_transformers/__init__.py
+++ langchain/document_transformers/__init__.py
@@ -1,11 +1,13 @@
+from langchain.document_transformers.doctran_text_extract import (
+    DoctranPropertyExtractor,
+)
+from langchain.document_transformers.doctran_text_qa import DoctranQATransformer
+from langchain.document_transformers.doctran_text_translate import DoctranTextTranslator
 from langchain.document_transformers.embeddings_redundant_filter import (
     EmbeddingsClusteringFilter,
     EmbeddingsRedundantFilter,
     get_stateful_documents,
 )
-from langchain.document_transformers.text_extract import DoctranPropertyExtractor
-from langchain.document_transformers.text_qa import DoctranQATransformer
-from langchain.document_transformers.text_translate import DoctranTextTranslator
 
 __all__ = [
     "DoctranQATransformer",
