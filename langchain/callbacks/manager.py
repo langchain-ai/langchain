1e40427755f3034c5c411c1d0a921cdb3e13849d
Nuno Campos <nuno@boringbits.io>
2023-07-14 10:03:16 +0100

    Enabled nesting chain group (#7697)
    <!-- Thank you for contributing to LangChain!
    
    Replace this comment with:
      - Description: a description of the change, 
      - Issue: the issue # it fixes (if applicable),
      - Dependencies: any dependencies required for this change,
    - Tag maintainer: for a quicker response, tag the relevant maintainer
    (see below),
    - Twitter handle: we announce bigger features on Twitter. If your PR
    gets announced and you'd like a mention, we'll gladly shout you out!
    
    If you're adding a new integration, please include:
    1. a test for the integration, preferably unit tests that do not rely on
    network access,
      2. an example notebook showing its use.
    
    Maintainer responsibilities:
      - General / Misc / if you don't know who to tag: @baskaryan
      - DataLoaders / VectorStores / Retrievers: @rlancemartin, @eyurtsev
      - Models / Prompts: @hwchase17, @baskaryan
      - Memory: @hwchase17
      - Agents / Tools / Toolkits: @hinthornw
      - Tracing / Callbacks: @agola11
      - Async: @agola11
    
    If no one reviews your PR within a few days, feel free to @-mention the
    same people again.
    
    See contribution guidelines for more information on how to write/run
    tests, lint, etc:
    https://github.com/hwchase17/langchain/blob/master/.github/CONTRIBUTING.md
     -->

--- langchain/callbacks/manager.py
+++ langchain/callbacks/manager.py
@@ -177,6 +177,7 @@ def tracing_v2_enabled(
 @contextmanager
 def trace_as_chain_group(
     group_name: str,
+    callback_manager: Optional[CallbackManager] = None,
     *,
     project_name: Optional[str] = None,
     example_id: Optional[Union[str, UUID]] = None,
@@ -203,12 +204,19 @@ def trace_as_chain_group(
         ...     # Use the callback manager for the chain group
         ...     llm.predict("Foo", callbacks=manager)
     """
-    cb = LangChainTracer(
-        project_name=project_name,
-        example_id=example_id,
+    cb = cast(
+        Callbacks,
+        [
+            LangChainTracer(
+                project_name=project_name,
+                example_id=example_id,
+            )
+        ]
+        if callback_manager is None
+        else callback_manager,
     )
     cm = CallbackManager.configure(
-        inheritable_callbacks=[cb],
+        inheritable_callbacks=cb,
         inheritable_tags=tags,
     )
 
@@ -220,6 +228,7 @@ def trace_as_chain_group(
 @asynccontextmanager
 async def atrace_as_chain_group(
     group_name: str,
+    callback_manager: Optional[AsyncCallbackManager] = None,
     *,
     project_name: Optional[str] = None,
     example_id: Optional[Union[str, UUID]] = None,
@@ -245,13 +254,18 @@ async def atrace_as_chain_group(
         ...     # Use the async callback manager for the chain group
         ...     await llm.apredict("Foo", callbacks=manager)
     """
-    cb = LangChainTracer(
-        project_name=project_name,
-        example_id=example_id,
+    cb = cast(
+        Callbacks,
+        [
+            LangChainTracer(
+                project_name=project_name,
+                example_id=example_id,
+            )
+        ]
+        if callback_manager is None
+        else callback_manager,
     )
-    cm = AsyncCallbackManager.configure(
-        inheritable_callbacks=[cb], inheritable_tags=tags
-    )
+    cm = AsyncCallbackManager.configure(inheritable_callbacks=cb, inheritable_tags=tags)
 
     run_manager = await cm.on_chain_start({"name": group_name}, {})
     try:
