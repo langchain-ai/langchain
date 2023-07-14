c66e1b411b7b6b72185fd04448495bdc628d61aa
Bagatur <baskaryan@gmail.com>
2023-07-14 10:18:01 -0400

    type

--- langchain/output_parsers/json.py
+++ langchain/output_parsers/json.py
@@ -69,3 +69,6 @@ class SimpleJsonOutputParser(BaseOutputParser[Any]):
             return json.loads(text)
         except JSONDecodeError as e:
             raise OutputParserException(f"Invalid json output: {text}") from e
+
+    def _type(self) -> str:
+        return "simple_json_output_parser"
