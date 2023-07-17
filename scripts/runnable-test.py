8539d4455e312dc9dff7835e612f3a783384677a
Nuno Campos <nuno@boringbits.io>
2023-07-17 12:17:21 +0100

    Lint

--- scripts/runnable-test.py
+++ scripts/runnable-test.py
@@ -23,13 +23,13 @@ fake = FakeRunnable()
 llm = OpenAI()
 
 print("invoke fake", fake.invoke("hello"))
-for token in fake.stream("hello"):
-    print("stream fake", token)
+for ftoken in fake.stream("hello"):
+    print("stream fake", ftoken)
 print("batch fake", fake.batch(["hello", "world"]))
 
 print("invoke llm", llm.invoke("say hi"))
-for token in llm.stream("say hi"):
-    print("stream llm", token)
+for ltoken in llm.stream("say hi"):
+    print("stream llm", ltoken)
 print(
     "batch llm",
     llm.batch(["say hi", StringPromptValue(text="say hello")]),
@@ -38,13 +38,13 @@ print(
 
 async def test_async() -> None:
     print("ainvoke", await fake.ainvoke("say hi"))
-    async for token in fake.astream("say hi"):
-        print("astream", token)
+    async for ftoken in fake.astream("say hi"):
+        print("astream", ftoken)
     print("abatch", await fake.abatch(["say hi", "say hello"]))
 
     print("ainvoke llm", await llm.ainvoke(StringPromptValue(text="say hi")))
-    async for token in llm.astream(StringPromptValue(text="say hi")):
-        print("astream llm", token)
+    async for ltoken in llm.astream(StringPromptValue(text="say hi")):
+        print("astream llm", ltoken)
     print(
         "abatch llm",
         await llm.abatch(
