27009849ce319e0fdbb5c7bfa150715e4ea9b04d
Bagatur <baskaryan@gmail.com>
2023-07-18 09:44:51 -0700

    cr

--- tests/unit_tests/retrievers/test_knn.py
+++ tests/unit_tests/retrievers/test_knn.py
@@ -1,5 +1,6 @@
 from typing import List
 
+import numpy as np
 import pytest
 
 from langchain.embeddings.base import Embeddings
@@ -9,18 +10,17 @@ from langchain.retrievers.knn import KNNRetriever
 class TestKNNRetriever:
     class MockEmbeddings(Embeddings):
         def embed_documents(self, texts: List[str]) -> List[List[float]]:
-            return []
+            return [np.random.random(size=100).tolist() for _ in texts]
 
         def embed_query(self, text: str) -> List[float]:
-            return []
+            return np.random.random(size=100).tolist()
 
         async def aembed_documents(self, texts: List[str]) -> List[List[float]]:
-            return []
+            NotImplemented
 
         async def aembed_query(self, text: str) -> List[float]:
-            return []
+            NotImplemented
 
-    @pytest.mark.requires("sklearn")
     def test_from_texts(self) -> None:
         input_texts = ["I have a pen.", "Do you have a pen?", "I have a bag."]
         knn_retriever = KNNRetriever.from_texts(
