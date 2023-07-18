4b9917587c3df1fd8296b53146557e62e832e0e6
Bagatur <baskaryan@gmail.com>
2023-07-18 09:46:49 -0700

    cr

--- tests/unit_tests/retrievers/test_knn.py
+++ tests/unit_tests/retrievers/test_knn.py
@@ -1,29 +1,11 @@
-from typing import List
-
-import numpy as np
-import pytest
-
-from langchain.embeddings.base import Embeddings
+from langchain.embeddings import FakeEmbeddings
 from langchain.retrievers.knn import KNNRetriever
 
 
 class TestKNNRetriever:
-    class MockEmbeddings(Embeddings):
-        def embed_documents(self, texts: List[str]) -> List[List[float]]:
-            return [np.random.random(size=100).tolist() for _ in texts]
-
-        def embed_query(self, text: str) -> List[float]:
-            return np.random.random(size=100).tolist()
-
-        async def aembed_documents(self, texts: List[str]) -> List[List[float]]:
-            NotImplemented
-
-        async def aembed_query(self, text: str) -> List[float]:
-            NotImplemented
-
     def test_from_texts(self) -> None:
         input_texts = ["I have a pen.", "Do you have a pen?", "I have a bag."]
         knn_retriever = KNNRetriever.from_texts(
-            texts=input_texts, embeddings=self.MockEmbeddings()
+            texts=input_texts, embeddings=FakeEmbeddings(size=100)
         )
         assert len(knn_retriever.texts) == 3
