b1d5fc40a7bc24eb109960ed01580b6350caef15
Harrison Chase <hw.chase.17@gmail.com>
2023-07-19 13:49:41 -0700

    set up experimental

---
+++ libs/langchain/langchain/tools/openweathermap/tool.py
@@ -0,0 +1,41 @@
+"""Tool for the OpenWeatherMap API."""
+
+from typing import Optional
+
+from pydantic import Field
+
+from langchain.callbacks.manager import (
+    AsyncCallbackManagerForToolRun,
+    CallbackManagerForToolRun,
+)
+from langchain.tools.base import BaseTool
+from langchain.utilities import OpenWeatherMapAPIWrapper
+
+
+class OpenWeatherMapQueryRun(BaseTool):
+    """Tool that adds the capability to query using the OpenWeatherMap API."""
+
+    api_wrapper: OpenWeatherMapAPIWrapper = Field(
+        default_factory=OpenWeatherMapAPIWrapper
+    )
+
+    name = "OpenWeatherMap"
+    description = (
+        "A wrapper around OpenWeatherMap API. "
+        "Useful for fetching current weather information for a specified location. "
+        "Input should be a location string (e.g. London,GB)."
+    )
+
+    def _run(
+        self, location: str, run_manager: Optional[CallbackManagerForToolRun] = None
+    ) -> str:
+        """Use the OpenWeatherMap tool."""
+        return self.api_wrapper.run(location)
+
+    async def _arun(
+        self,
+        location: str,
+        run_manager: Optional[AsyncCallbackManagerForToolRun] = None,
+    ) -> str:
+        """Use the OpenWeatherMap tool asynchronously."""
+        raise NotImplementedError("OpenWeatherMapQueryRun does not support async")
