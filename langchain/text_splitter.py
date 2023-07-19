062421038c82dd1e27cc75c581ea28e78880788a
ilya.michlin <ilya.michlin@lemonade.com>
2023-07-19 13:29:14 +0300

    black

--- langchain/text_splitter.py
+++ langchain/text_splitter.py
@@ -261,7 +261,9 @@ class TextSplitter(BaseDocumentTransformer, ABC):
 class CharacterTextSplitter(TextSplitter):
     """Implementation of splitting text that looks at characters."""
 
-    def __init__(self, separator: str = "\n\n", is_separator_regex: bool = False, **kwargs: Any) -> None:
+    def __init__(
+        self, separator: str = "\n\n", is_separator_regex: bool = False, **kwargs: Any
+    ) -> None:
         """Create a new TextSplitter."""
         super().__init__(**kwargs)
         self._separator = separator
@@ -270,7 +272,9 @@ class CharacterTextSplitter(TextSplitter):
     def split_text(self, text: str) -> List[str]:
         """Split incoming text and return chunks."""
         # First we naively split the large input into a bunch of smaller ones.
-        separator = self._separator if self._is_separator_regex else re.escape(self._separator)
+        separator = (
+            self._separator if self._is_separator_regex else re.escape(self._separator)
+        )
         splits = _split_text_with_regex(text, separator, self._keep_separator)
         _separator = "" if self._keep_separator else self._separator
         return self._merge_splits(splits, _separator)
