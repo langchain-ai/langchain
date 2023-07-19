7cc0faea93ae59d3af806aebe8d2eb31bb37080e
shelbyjenkins <shelbyjenkins@gmail.com>
2023-07-19 00:35:22 +0000

    Adds BalancedRecursiveCharacterTextSplitter to text_splitter.py

--- langchain/text_splitter.py
+++ langchain/text_splitter.py
@@ -1003,6 +1003,123 @@ class RecursiveCharacterTextSplitter(TextSplitter):
             )
 
 
+class BalancedRecursiveCharacterTextSplitter(TextSplitter):
+    """Implementation of splitting text that looks at characters.
+
+    Recursively tries to split by different characters to find one
+    that works. It retries if the chunk sizes do not meet the input requirements.
+    """
+
+    def __init__(
+        self,
+        goal_length=None,
+        max_length=None,
+        chunk_overlap=None,
+        separators: Optional[List[str]] = None,
+        keep_separator=True,
+        **kwargs: Any,
+    ) -> None:
+        """Create a new TextSplitter."""
+        super().__init__(keep_separator=keep_separator, **kwargs)
+        self._separators = separators or ["\n\n", "\n", " ", ""]
+
+        self.goal_length = goal_length or self._chunk_size
+        self.max_length = max_length or (self._chunk_size * 1.25)
+
+        # Chunk size logic
+        if chunk_overlap is not None:
+            # There must be at least some chunk overlap for this to function
+            if chunk_overlap < 100:
+                self._chunk_overlap = 100
+            else:
+                self._chunk_overlap = chunk_overlap
+        else:
+            self._chunk_overlap = self._chunk_overlap
+
+    def _split_text(
+        self, text: str, separators: List[str], goal_length: None
+    ) -> List[str]:
+        """Split incoming text and return chunks."""
+
+        # Have to define here initially so it can be redefined for each recursion
+        if goal_length is None:
+            goal_length = self.goal_length
+        # Get appropriate separator to use
+        separator = separators[-1]
+        new_separators = []
+        for i, _s in enumerate(separators):
+            if _s == "":
+                separator = _s
+                break
+            if re.search(_s, text):
+                separator = _s
+                new_separators = separators[i + 1 :]
+                break
+
+        # Use the current separator to split the text
+        splits = _split_text_with_regex(text, separator, self._keep_separator)
+        final_combos = self.distribute_splits(splits, goal_length)
+
+        # If any split was larger than the max size final_combos will be returned empty from distribute_splits
+        if final_combos:
+            for combo in final_combos:
+                # If a combo of splits is too small, we adjust the goal_length and retry separator
+                combo_token_count = self._length_function("".join(combo))
+                if (
+                    combo_token_count < self.goal_length * 0.75
+                    and len(final_combos) > 1
+                ):
+                    new_goal_length = goal_length + (
+                        combo_token_count / (len(final_combos) - 1)
+                    )
+                    final_combos = self._split_text(text, separators, new_goal_length)
+                # If a combo of splits is too large, we retry with new separator
+                elif combo_token_count > self.max_length and new_separators:
+                    final_combos = self._split_text(text, new_separators, goal_length)
+        else:
+            # In the case distribute_splits returned None continue to next separator
+            final_combos = self._split_text(text, new_separators, goal_length)
+
+        # All combos satisfy requirements
+        return final_combos
+
+    def distribute_splits(self, splits, goal_length):
+        # Build initial combos
+        combos = []
+        current_combo = []
+        combo_token_count = 0
+        for split in splits:
+            split_token_count = self._length_function(split)
+            # If too big skip to next separator
+            if split_token_count > self.max_length:
+                combos = []
+                return combos
+            if goal_length > (combo_token_count + split_token_count):
+                current_combo.append(split)
+                combo_token_count = self._length_function("".join(current_combo))
+            # Combo larger than goal_length
+            else:
+                current_combo.append(split)
+                combos.append(current_combo)
+                # Create a new combo and add the current split so there is overlap
+                if split_token_count < self._chunk_overlap:
+                    current_combo = []
+                    current_combo.append(split)
+                    combo_token_count = self._length_function("".join(current_combo))
+                # If the overlap chunk is larger than overlap size continue to next separator
+                else:
+                    combos = []
+                    return combos
+        # Add the last combo if it has more than just the overlap chunk
+        if len(current_combo) > 1:
+            combos.append(current_combo)
+        return combos
+
+    def split_text(self, text: str) -> List[str]:
+        final_combos = self._split_text(text, self._separators, self.goal_length)
+        return ["".join(combo) for combo in final_combos]
+
+
 class NLTKTextSplitter(TextSplitter):
     """Implementation of splitting text that looks at sentences using NLTK."""
 
