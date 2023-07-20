ca11d4d144ed6b27a39242223cab22a67914f6a3
Julien Salinas <all@juliensalinas.com>
2023-07-20 09:48:19 +0200

    Make it possible to modify the GPU and lang parameters for LLMs

--- langchain/llms/nlpcloud.py
+++ langchain/llms/nlpcloud.py
@@ -24,6 +24,10 @@ class NLPCloud(LLM):
     client: Any  #: :meta private:
     model_name: str = "finetuned-gpt-neox-20b"
     """Model name to use."""
+    gpu: bool = True
+    """Whether to use a GPU or not"""
+    lang: str = "en"
+    """Language to use (multilingual addon)"""
     temperature: float = 0.7
     """What sampling temperature to use."""
     min_length: int = 1
@@ -72,7 +76,7 @@ class NLPCloud(LLM):
             import nlpcloud
 
             values["client"] = nlpcloud.Client(
-                values["model_name"], nlpcloud_api_key, gpu=True, lang="en"
+                values["model_name"], nlpcloud_api_key, gpu=values["gpu"], lang=values["lang"]
             )
         except ImportError:
             raise ImportError(
@@ -105,7 +109,7 @@ class NLPCloud(LLM):
     @property
     def _identifying_params(self) -> Mapping[str, Any]:
         """Get the identifying parameters."""
-        return {**{"model_name": self.model_name}, **self._default_params}
+        return {**{"model_name": self.model_name}, **{"gpu": self.gpu}, **{"lang": self.lang}, **self._default_params}
 
     @property
     def _llm_type(self) -> str:
