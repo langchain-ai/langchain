1aa49df52f189442e5871fe4b975795c9752f192
Julien Salinas <all@juliensalinas.com>
2023-07-20 09:44:04 +0200

    Add GPU as a class attribute for embeddings

--- langchain/embeddings/nlpcloud.py
+++ langchain/embeddings/nlpcloud.py
@@ -21,6 +21,7 @@ class NLPCloudEmbeddings(BaseModel, Embeddings):
     """
 
     model_name: str  # Define model_name as a class attribute
+    gpu: bool  # Define gpu as a class attribut
     client: Any  #: :meta private:
 
     def __init__(
