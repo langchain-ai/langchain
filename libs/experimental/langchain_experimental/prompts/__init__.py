9dcd34e5863ab1b5e0e694aaecfb0b477948aca9
Harrison Chase <hw.chase.17@gmail.com>
2023-07-21 12:10:30 -0700

    cr

---
+++ libs/experimental/langchain_experimental/prompts/__init__.py
@@ -0,0 +1,1 @@
+from langchain.experimental.prompts.load import load_prompt
--- libs/experimental/langchain_experimental/sql/__init__.py
+++ libs/experimental/langchain_experimental/sql/__init__.py
@@ -1,1 +1,2 @@
 """Chain for interacting with SQL Database."""
+from langchain.experimental.sql.base import SQLDatabaseChain
