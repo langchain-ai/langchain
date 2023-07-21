b65a25c25ffb14c77849fdc09199de73ab8db3f2
Bagatur <22008038+baskaryan@users.noreply.github.com>
2023-07-21 12:37:21 -0700

    fix api ref (#8083)

--- docs/api_reference/create_api_rst.py
+++ docs/api_reference/create_api_rst.py
@@ -4,7 +4,7 @@ import re
 from pathlib import Path
 
 ROOT_DIR = Path(__file__).parents[2].absolute()
-PKG_DIR = ROOT_DIR / "libs" / "langchain"
+PKG_DIR = ROOT_DIR / "libs" / "langchain" / "langchain"
 WRITE_FILE = Path(__file__).parent / "api_reference.rst"
 
 
