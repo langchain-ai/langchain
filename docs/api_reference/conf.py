facd7e0b7e2da9577a126dd30b0144ce0c6c2cab
Bagatur <baskaryan@gmail.com>
2023-07-21 10:56:53 -0700

    fix

--- docs/api_reference/conf.py
+++ docs/api_reference/conf.py
@@ -17,6 +17,7 @@ import sys
 import toml
 
 sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("libs/langchain"))
 
 with open("../../libs/langchain/pyproject.toml") as f:
     data = toml.load(f)
