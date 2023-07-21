0e6e9bfee534e65b16a6b872d3e0ed14fd59425a
Bagatur <baskaryan@gmail.com>
2023-07-21 11:15:05 -0700

    undo

--- docs/api_reference/conf.py
+++ docs/api_reference/conf.py
@@ -17,7 +17,6 @@ import sys
 import toml
 
 sys.path.insert(0, os.path.abspath("."))
-sys.path.insert(0, os.path.abspath("../libs/langchain"))
 sys.path.insert(0, os.path.abspath("../../libs/langchain"))
 
 with open("../../libs/langchain/pyproject.toml") as f:
