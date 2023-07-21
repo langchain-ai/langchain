67d6cb1ef550dc9fe7b4447b796c4a0596c40c28
Bagatur <22008038+baskaryan@users.noreply.github.com>
2023-07-21 13:52:03 -0700

    use top nav docs (#8090)

--- docs/docs_skeleton/vercel_build.sh
+++ docs/docs_skeleton/vercel_build.sh
@@ -4,7 +4,7 @@ cd ..
 python3 --version
 python3 -m venv .venv
 source .venv/bin/activate
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r vercel_requirements.txt
 cp -r extras/* docs_skeleton/docs
 cd docs_skeleton
 nbdoc_build
