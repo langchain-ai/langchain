f7b66d3597eb9acd8974f1e26b314e7b4140d0c7
Gordon Clark <gordon.clark@botpress.com>
2023-07-22 18:18:25 +0800

    Fixed error in Github tool, added docstrings, tests

---
+++ libs/langchain/tests/integration_tests/utilities/test_github.py
@@ -0,0 +1,19 @@
+"""Integration test for Github Wrapper."""
+from typing import Any, List, Dict
+import pytest
+from langchain.utilities.github import GitHubAPIWrapper
+
+# Make sure you have set the following env variables:
+# GITHUB_REPOSITORY
+# GITHUB_BRANCH
+# GITHUB_APP_ID
+# GITHUB_PRIVATE_KEY
+
+@pytest.fixture
+def api_client() -> GitHubAPIWrapper:
+    return GitHubAPIWrapper()
+
+def test_get_open_issues(api_client: GitHubAPIWrapper) -> None:
+    """Basic test to fetch issues"""
+    issues = api_client.get_issues()
+    assert len(issues) != 0
--- tests/integration_tests/utilities/test_github.py
+++
@@ -1,5 +0,0 @@
-"""Integration test for Github Wrapper."""
-from typing import Any, List
-import pytest
-
-#TODO: Add the tests here
