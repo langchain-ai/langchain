f794f9d70f9cb45b01d6ae8a93f63b0a46a8a93a
Harrison Chase <hw.chase.17@gmail.com>
2023-07-22 08:09:58 -0700

    add back in experimental

---
+++ libs/langchain/langchain/experimental/generative_agents/__init__.py
@@ -0,0 +1,5 @@
+"""Generative Agents primitives."""
+from langchain.experimental.generative_agents.generative_agent import GenerativeAgent
+from langchain.experimental.generative_agents.memory import GenerativeAgentMemory
+
+__all__ = ["GenerativeAgent", "GenerativeAgentMemory"]
