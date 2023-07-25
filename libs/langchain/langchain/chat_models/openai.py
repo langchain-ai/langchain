66e305039b57cb61546535e79e705eadef5ae30b
Bagatur <baskaryan@gmail.com>
2023-07-25 11:08:04 -0700

    fmt

--- libs/langchain/langchain/chat_models/openai.py
+++ libs/langchain/langchain/chat_models/openai.py
@@ -334,7 +334,9 @@ class ChatOpenAI(BaseChatModel):
                     role = stream_resp["choices"][0]["delta"].get("role", role)
                     token = stream_resp["choices"][0]["delta"].get("content") or ""
                     inner_completion += token
-                    _function_call = stream_resp["choices"][0]["delta"].get("function_call")
+                    _function_call = stream_resp["choices"][0]["delta"].get(
+                        "function_call"
+                    )
                     if _function_call:
                         if function_call is None:
                             function_call = _function_call
@@ -398,7 +400,9 @@ class ChatOpenAI(BaseChatModel):
                     role = stream_resp["choices"][0]["delta"].get("role", role)
                     token = stream_resp["choices"][0]["delta"].get("content", "")
                     inner_completion += token or ""
-                    _function_call = stream_resp["choices"][0]["delta"].get("function_call")
+                    _function_call = stream_resp["choices"][0]["delta"].get(
+                        "function_call"
+                    )
                     if _function_call:
                         if function_call is None:
                             function_call = _function_call
