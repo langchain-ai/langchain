6851b59ff3bfee2404356dba84fb0c67b030ffde
Nuno Campos <nuno@boringbits.io>
2023-07-25 10:23:42 +0100

    Add unit tests for message chunks

---
+++ libs/langchain/tests/unit_tests/schema/test_messages.py
@@ -0,0 +1,48 @@
+from langchain.prompts.chat import ChatPromptTemplate
+from langchain.schema.messages import AIMessageChunk, HumanMessage, HumanMessageChunk
+
+
+def test_message_chunks():
+    assert HumanMessage(content="You are a nice assistant") + AIMessageChunk(
+        content="I am indeed."
+    ) == ChatPromptTemplate(
+        messages=[
+            HumanMessage(content="You are a nice assistant"),
+            AIMessageChunk(content="I am indeed."),
+        ]
+    ), "Message + MessageChunk should be a ChatPromptTemplate"
+
+    assert AIMessageChunk(content="I am") + AIMessageChunk(
+        content=" indeed."
+    ) == AIMessageChunk(
+        content="I am indeed."
+    ), "MessageChunk + MessageChunk should be a MessageChunk"
+
+    assert AIMessageChunk(content="I am") + HumanMessageChunk(
+        content=" indeed."
+    ) == AIMessageChunk(
+        content="I am indeed."
+    ), "MessageChunk + MessageChunk should be a MessageChunk of same class as the left side"
+
+    assert AIMessageChunk(
+        content="", additional_kwargs={"foo": "bar"}
+    ) + AIMessageChunk(content="", additional_kwargs={"baz": "foo"}) == AIMessageChunk(
+        content="", additional_kwargs={"foo": "bar", "baz": "foo"}
+    ), "MessageChunk + MessageChunk should be a MessageChunk with merged additional_kwargs"
+
+    assert AIMessageChunk(
+        content="", additional_kwargs={"function_call": {"name": "web_search"}}
+    ) + AIMessageChunk(
+        content="", additional_kwargs={"function_call": {"arguments": "{\n"}}
+    ) + AIMessageChunk(
+        content="",
+        additional_kwargs={"function_call": {"arguments": '  "query": "turtles"\n}'}},
+    ) == AIMessageChunk(
+        content="",
+        additional_kwargs={
+            "function_call": {
+                "name": "web_search",
+                "arguments": '{\n  "query": "turtles"\n}',
+            }
+        },
+    ), "MessageChunk + MessageChunk should be a MessageChunk with merged additional_kwargs"
