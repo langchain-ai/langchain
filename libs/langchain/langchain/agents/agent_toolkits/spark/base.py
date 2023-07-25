654557c98aa1ecd29d3f8141715822729e3b8f45
subashkonar13 <subashkonar13@gmail.com>
2023-07-26 04:21:12 +0530

    Update base.py to support multiple dataframes

--- libs/langchain/langchain/agents/agent_toolkits/spark/base.py
+++ libs/langchain/langchain/agents/agent_toolkits/spark/base.py
@@ -14,7 +14,10 @@ def _validate_spark_df(df: Any) -> bool:
     try:
         from pyspark.sql import DataFrame as SparkLocalDataFrame
 
-        return isinstance(df, SparkLocalDataFrame)
+        if isinstance(df, list):
+            return all(isinstance(d, SparkLocalDataFrame) for d in df)
+        else:
+            return isinstance(df, SparkLocalDataFrame)
     except ImportError:
         return False
 
@@ -23,14 +26,17 @@ def _validate_spark_connect_df(df: Any) -> bool:
     try:
         from pyspark.sql.connect.dataframe import DataFrame as SparkConnectDataFrame
 
-        return isinstance(df, SparkConnectDataFrame)
+        if isinstance(df, list):
+            return all(isinstance(d, SparkConnectDataFrame) for d in df)
+        else:
+            return isinstance(df, SparkConnectDataFrame)
     except ImportError:
         return False
 
 
 def create_spark_dataframe_agent(
     llm: BaseLLM,
-    df: Any,
+    dfs: Any,  # Accept multiple DataFrames as input
     callback_manager: Optional[BaseCallbackManager] = None,
     prefix: str = PREFIX,
     suffix: str = SUFFIX,
@@ -43,18 +49,33 @@ def create_spark_dataframe_agent(
     agent_executor_kwargs: Optional[Dict[str, Any]] = None,
     **kwargs: Dict[str, Any],
 ) -> AgentExecutor:
-    """Construct a Spark agent from an LLM and dataframe."""
+    """Construct a Spark agent from an LLM and DataFrame(s)."""
 
-    if not _validate_spark_df(df) and not _validate_spark_connect_df(df):
+    if not _validate_spark_df(dfs) and not _validate_spark_connect_df(dfs):
         raise ValueError("Spark is not installed. run `pip install pyspark`.")
 
     if input_variables is None:
-        input_variables = ["df", "input", "agent_scratchpad"]
-    tools = [PythonAstREPLTool(locals={"df": df})]
-    prompt = ZeroShotAgent.create_prompt(
-        tools, prefix=prefix, suffix=suffix, input_variables=input_variables
-    )
-    partial_prompt = prompt.partial(df=str(df.first()))
+        input_variables = ["dfs", "input", "agent_scratchpad"]
+    tools = [PythonAstREPLTool(locals={"dfs": dfs})]  # Update the locals variable
+
+    # Determine whether the input contains single or multiple DataFrames
+    if isinstance(dfs, list):
+        prompt = ZeroShotAgent.create_prompt(
+            tools,
+            prefix=prefix,
+            suffix=suffix,
+            input_variables=input_variables + ["num_dfs"],  # Add 'num_dfs' to input variables
+        )
+        partial_prompt = prompt.partial(num_dfs=str(len(dfs)))  # Add 'num_dfs' to partial_prompt
+    else:
+        prompt = ZeroShotAgent.create_prompt(
+            tools,
+            prefix=prefix,
+            suffix=suffix,
+            input_variables=input_variables,
+        )
+        partial_prompt = prompt.partial(dfs=str(dfs.first()))
+
     llm_chain = LLMChain(
         llm=llm,
         prompt=partial_prompt,
@@ -78,3 +99,5 @@ def create_spark_dataframe_agent(
         early_stopping_method=early_stopping_method,
         **(agent_executor_kwargs or {}),
     )
+
+
