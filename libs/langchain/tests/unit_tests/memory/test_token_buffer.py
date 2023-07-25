b9c7140a55716918698ba36158df078686d13159
yakigac <10434946+yakigac@users.noreply.github.com>
2023-07-25 21:45:42 +0900

    Format the file

--- libs/langchain/tests/unit_tests/memory/test_token_buffer.py
+++ libs/langchain/tests/unit_tests/memory/test_token_buffer.py
@@ -2,6 +2,7 @@
 from langchain.memory.token_buffer import ConversationTokenBufferMemory
 from tests.unit_tests.llms.fake_llm import FakeLLM
 
+
 def test_token_buffer_memory() -> None:
     """Test ConversationTokenBufferMemory with a max_token_limit of 6."""
     memory = ConversationTokenBufferMemory(
@@ -10,8 +11,8 @@ def test_token_buffer_memory() -> None:
     memory.save_context({"input": "bar"}, {"output": "foo"})
     output = memory.load_memory_variables({})
     # Check the initial conversation.
-    assert output == {'baz': 'Human: bar\nAI: foo'}
+    assert output == {"baz": "Human: bar\nAI: foo"}
     memory.save_context({"input": "bar1"}, {"output": "foo1"})
     # Check the pruned conversation after exceeding the token limit.
     output = memory.load_memory_variables({})
-    assert output == {'baz': 'AI: foo\nHuman: bar1\nAI: foo1'}
+    assert output == {"baz": "AI: foo\nHuman: bar1\nAI: foo1"}
