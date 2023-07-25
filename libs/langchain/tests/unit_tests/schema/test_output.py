c7b319cbab41b1cf34f50114916fbf657dd5dbaf
Nuno Campos <nuno@boringbits.io>
2023-07-25 12:18:36 +0100

    Lint

--- libs/langchain/tests/unit_tests/schema/test_output.py
+++ libs/langchain/tests/unit_tests/schema/test_output.py
@@ -1,4 +1,5 @@
-from langchain.schema.output import GenerationChunk
+from langchain.schema.messages import HumanMessageChunk
+from langchain.schema.output import ChatGenerationChunk, GenerationChunk
 
 
 def test_generation_chunk() -> None:
@@ -18,4 +19,34 @@ def test_generation_chunk() -> None:
         text="world!", generation_info={"foo": "bar"}
     ) + GenerationChunk(text="!", generation_info={"baz": "foo"}) == GenerationChunk(
         text="Hello, world!!", generation_info={"foo": "bar", "baz": "foo"}
+    ), "GenerationChunk + GenerationChunk should be a GenerationChunk with merged generation_info"  # noqa: E501
+
+
+def test_chat_generation_chunk() -> None:
+    assert ChatGenerationChunk(
+        message=HumanMessageChunk(content="Hello, ")
+    ) + ChatGenerationChunk(
+        message=HumanMessageChunk(content="world!")
+    ) == ChatGenerationChunk(
+        message=HumanMessageChunk(content="Hello, world!")
+    ), "ChatGenerationChunk + ChatGenerationChunk should be a ChatGenerationChunk"
+
+    assert ChatGenerationChunk(
+        message=HumanMessageChunk(content="Hello, ")
+    ) + ChatGenerationChunk(
+        message=HumanMessageChunk(content="world!"), generation_info={"foo": "bar"}
+    ) == ChatGenerationChunk(
+        message=HumanMessageChunk(content="Hello, world!"),
+        generation_info={"foo": "bar"},
+    ), "GenerationChunk + GenerationChunk should be a GenerationChunk with merged generation_info"  # noqa: E501
+
+    assert ChatGenerationChunk(
+        message=HumanMessageChunk(content="Hello, ")
+    ) + ChatGenerationChunk(
+        message=HumanMessageChunk(content="world!"), generation_info={"foo": "bar"}
+    ) + ChatGenerationChunk(
+        message=HumanMessageChunk(content="!"), generation_info={"baz": "foo"}
+    ) == ChatGenerationChunk(
+        message=HumanMessageChunk(content="Hello, world!!"),
+        generation_info={"foo": "bar", "baz": "foo"},
     ), "GenerationChunk + GenerationChunk should be a GenerationChunk with merged generation_info"  # noqa: E501
