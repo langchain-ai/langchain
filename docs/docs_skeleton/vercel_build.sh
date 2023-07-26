01790b97112d70a249524ae43c76b4295e7d95e0
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2023-07-25 23:32:55 -0700

    311

--- docs/docs_skeleton/vercel_build.sh
+++ docs/docs_skeleton/vercel_build.sh
@@ -2,20 +2,18 @@
 
 yum remove openssl-devel -y
 yum install openssl11 openssl11-devel -y
-# Remove python 3.9 and install python 3.10
-yum list
-yum remove python39 -y
+yum update -y
 yum install python3.11 -y
-python3 --version
+python3.11 --version
 
 cd ..
-python3 --version
-python3 -m venv .venv
+python3.11 --version
+python3.11 -m venv .venv
 source .venv/bin/activate
-python3 -m pip install --upgrade pip
-python3 -m pip install -r vercel_requirements.txt
-python3 -c "from requests import HTTPError, Response; print(HTTPError, Response)"
+python3.11 -m pip install --upgrade pip
+python3.11 -m pip install -r vercel_requirements.txt
+python3.11 -c "from requests import HTTPError, Response; print(HTTPError, Response)"
 cp -r extras/* docs_skeleton/docs
 cd docs_skeleton
 nbdoc_build
-python3 generate_api_reference_links.py
+python3.11 generate_api_reference_links.py
