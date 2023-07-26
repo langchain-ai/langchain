f55d02a9e053db80bfe9a9393efbd8a8df7146b3
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2023-07-26 00:23:13 -0700

    openssl version

--- docs/docs_skeleton/vercel_build.sh
+++ docs/docs_skeleton/vercel_build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 yum -y update
+yum remove openssl-devel -y
 yum install gcc bzip2-devel libffi-devel zlib-devel wget tar -y
 # Make sure openssl-devel is installed before Python compilation
 yum install openssl11 -y
-yum install wget tar -y
+echo "OPENSSL VERSION"
+openssl version 
 
 # Install python 3.11 to connect with openSSL 1.1.1
 wget https://www.python.org/ftp/python/3.11.4/Python-3.11.4.tgz 
