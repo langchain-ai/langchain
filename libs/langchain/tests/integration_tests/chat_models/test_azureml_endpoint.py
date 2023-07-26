e8a03a0cd2bfe7c4fad55a059c9f3799c951e9c1
matthewdeguzman <matthew.deguzman0613@gmail.com>
2023-07-26 09:04:41 -0700

    Assert proper instance

--- libs/langchain/tests/integration_tests/chat_models/test_azureml_endpoint.py
+++ libs/langchain/tests/integration_tests/chat_models/test_azureml_endpoint.py
@@ -1,19 +1,24 @@
 """Test AzureML Endpoint wrapper."""
+import os
+
 import pytest
 
 from langchain.chat_models.azureml_endpoint import (
     LlamaContentFormatter,
     AzureMLChatOnlineEndpoint
 )
-from langchain.schema import ChatResult, HumanMessage, SystemMessage
+from langchain.schema import BaseMessage, HumanMessage
 
 
 def test_llama_call() -> None:
     """Test valid call to Open Source Foundation Model."""
-    llm = AzureMLChatOnlineEndpoint(content_formatter=LlamaContentFormatter())
+    llm = AzureMLChatOnlineEndpoint(
+        endpoint_api_key=os.getenv("AZUREML_ENDPOINT_API_KEY"),
+        endpoint_url=os.getenv("AZUREML_ENDPOINT_URL"),
+        content_formatter=LlamaContentFormatter()
+    )
     output = llm(messages=[
-                SystemMessage(content="You are a helpful assistant."),
                 HumanMessage(content="Why is the sky blue?")
             ]
         )
-    assert isinstance(output, ChatResult)
+    assert isinstance(output, BaseMessage)
