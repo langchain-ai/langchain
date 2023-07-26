6316e11524d6192c164b9957b001e9b1af82ecd8
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2023-07-25 22:46:31 -0700

    pip upgrade

--- docs/docs_skeleton/vercel_build.sh
+++ docs/docs_skeleton/vercel_build.sh
@@ -4,6 +4,7 @@ cd ..
 python3 --version
 python3 -m venv .venv
 source .venv/bin/activate
+python3 -m pip install --upgrade pip
 python3 -m pip install -r vercel_requirements.txt
 cp -r extras/* docs_skeleton/docs
 cd docs_skeleton
