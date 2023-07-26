ebb89d9385f7e363d243b23a4630faa85f5dd90b
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2023-07-25 23:30:51 -0700

    yum list

--- docs/docs_skeleton/vercel_build.sh
+++ docs/docs_skeleton/vercel_build.sh
@@ -3,6 +3,7 @@
 yum remove openssl-devel -y
 yum install openssl11 openssl11-devel -y
 # Remove python 3.9 and install python 3.10
+yum list
 yum remove python39 -y
 yum install python310 -y
 python3 --version
@@ -13,7 +14,7 @@ python3 -m venv .venv
 source .venv/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install -r vercel_requirements.txt
-python3 -m "from requests import HTTPError, Response; print(HTTPError, Response)"
+python3 -c "from requests import HTTPError, Response; print(HTTPError, Response)"
 cp -r extras/* docs_skeleton/docs
 cd docs_skeleton
 nbdoc_build
