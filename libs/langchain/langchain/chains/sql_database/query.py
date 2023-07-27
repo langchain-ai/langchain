7b27a63a4a1f29b474a28ae59b587bb9c4c0102f
Harrison Chase <hw.chase.17@gmail.com>
2023-07-27 14:58:45 -0700

    cr

--- libs/langchain/langchain/chains/sql_database/query.py
+++ libs/langchain/langchain/chains/sql_database/query.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import List, Optional, TypedDict, Union
 
 from langchain.chains.sql_database.prompt import PROMPT, SQL_PROMPTS
 from langchain.schema.language_model import BaseLanguageModel
@@ -12,12 +12,38 @@ def _strip(text: str) -> str:
     return text.strip()
 
 
+class SQLInput(TypedDict):
+    """Input for a SQL Chain."""
+
+    question: str
+
+
+class SQLInputWithTables(TypedDict):
+    """Input for a SQL Chain."""
+
+    question: str
+    table_names_to_use: List[str]
+
+
 def create_sql_query_chain(
     llm: BaseLanguageModel,
     db: SQLDatabase,
     prompt: Optional[BasePromptTemplate] = None,
     k: int = 5,
-) -> RunnableSequence:
+) -> RunnableSequence[Union[SQLInput, SQLInputWithTables], str]:
+    """Create a chain that generates SQL queries.
+
+    Args:
+        llm: The language model to use
+        db: The SQLDatabase to generate the query for
+        prompt: The prompt to use. If none is provided, will choose one
+            based on dialect. Defaults to None.
+        k: The number of results per select statement to return. Defaults to 5.
+
+    Returns:
+        A chain that takes in a question and generates a SQL query that answers
+        that question.
+    """
     if prompt is not None:
         prompt_to_use = prompt
     elif db.dialect in SQL_PROMPTS:
