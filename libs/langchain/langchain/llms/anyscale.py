fd65290b576979c4b8863742e1a634be5fa17153
Bagatur <baskaryan@gmail.com>
2023-07-28 14:48:02 -0700

    fmt

--- libs/langchain/langchain/llms/anyscale.py
+++ libs/langchain/langchain/llms/anyscale.py
@@ -8,7 +8,9 @@ from langchain.llms.base import LLM
 from langchain.llms.utils import enforce_stop_tokens
 from langchain.utils import get_from_dict_or_env
 
-ANYSCLAE_ENDPOINT = 'https://console.endpoints.anyscale.com'
+ANYSCLAE_ENDPOINT = "https://console.endpoints.anyscale.com"
+
+
 class Anyscale(LLM):
     """Anyscale Service models.
 
@@ -113,16 +115,18 @@ class Anyscale(LLM):
 
         if self.anyscale_service_url.startswith(ANYSCLAE_ENDPOINT):
             body = {
-              "model": "meta-llama/Llama-2-70b-chat-hf",
-              "messages": [{"role": "system", "content": "You are a helpful assistant."}, 
-                           {"role": "user", "content": prompt}],
-              "temperature": 0.7
+                "model": "meta-llama/Llama-2-70b-chat-hf",
+                "messages": [
+                    {"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": prompt},
+                ],
+                "temperature": 0.7,
             }
             resp = requests.post(anyscale_service_endpoint, headers=headers, json=body)
             if resp.status_code != 200:
                 raise ValueError(f"Error returned by service")
             resp_json = resp.json()
-            text = resp_json['choices'][0]['message']['content']
+            text = resp_json["choices"][0]["message"]["content"]
 
         else:
             body = {"prompt": prompt}
