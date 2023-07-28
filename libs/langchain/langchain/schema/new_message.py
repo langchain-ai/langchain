ba06d0656ff8c63b11ec55629bc1ce9e36dd9b2b
Bagatur <baskaryan@gmail.com>
2023-07-28 13:20:36 -0700

    wip

--- libs/langchain/langchain/schema/new_message.py
+++ libs/langchain/langchain/schema/new_message.py
@@ -1,9 +1,13 @@
-class PromptInterface(ABC):
+T = TypeVar("T")
 
+
+class PromptInterface(Generic[T], ABC):
     @abstractmethod
-    def format(self, **kwargs: Any) -> Any:
+    def format(self, **kwargs: Any) -> T:
+        ...
 
-class Prompt(PromptInterface):
+
+class Prompt(PromptInterface[str]):
     def __init__(
         self,
         content: Optional[str] = None,
@@ -14,7 +18,7 @@ class Prompt(PromptInterface):
         self.input_variables = input_variables or infer_inputs(content, formatter)
         self.formatter = formatter
 
-    def format(self, **kwargs: Any) -> Any:
+    def format(self, **kwargs: Any) -> str:
         if not self.input_variables:
             return self.content
         elif self.formatter == "f-string":
@@ -25,31 +29,54 @@ class Prompt(PromptInterface):
             raise ValueError
 
 
-class Message(Prompt):
+class Message(PromptInterface["Message"]):
     def __init__(
         self,
+        prompt: Prompt,
         role: Optional[str] = None,
         type: Optional[Literal["human", "ai", "system", "function"]] = None,
         additional_data: Optional[Mapping[str, Any]] = None,
-        **kwargs: Any,
     ) -> None:
-        super().__init__(**kwargs)
+        self.prompt = prompt
         self.role = role
         self.type = type
         self.additional_data = additional_data
 
-    def __add__(self, other) -> MessageCollection:
-        return MessageCollection((self, other))
+    @classmethod
+    def from_content(
+        cls,
+        content: Optional[str] = None,
+        input_variables: Optional[Mapping[str, Any]] = None,
+        formatter: Literal["f-string", "jinja2"] = "f-string",
+        **kwargs: Any,
+    ) -> Message:
+        prompt = Prompt(
+            content=content, input_variables=input_variables, formatter=formatter
+        )
+        return cls(prompt, **kwargs)
+
+    def format(self, **kwargs: Any) -> Message:
+        content = self.prompt.format(**kwargs)
+        return self.from_content(
+            content=content,
+            role=self.role,
+            type=self.type,
+            additional_data=self.additional_data,
+        )
+
+    def __add__(self, other) -> MessageSequence:
+        return MessageSequence((self, other))
 
 
-class MessageSequence(PromptInterface):
+class MessageSequence(PromptInterface[List[Message]]):
     def __init__(self, messages: Sequence[Message]):
         self.messages = messages
 
     def format(self, **kwargs) -> List[Message]:
         return [m.format(**kwargs) for m in self.messages]
 
-    def __add__(self, other) -> MessageCollection:
-        return MessageCollection(list(self.messages) + list(other.messages))
+    def __add__(self, other) -> MessageSequence:
+        return MessageSequence(list(self.messages) + list(other.messages))
 
-
+    def __getitem__(self, item):
+        return self.messages.__getitem__(item)
