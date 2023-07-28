b9588be9321ccc9a98dcc70824b4cc14e965de41
Bagatur <baskaryan@gmail.com>
2023-07-28 13:09:13 -0700

    update

--- libs/langchain/langchain/schema/new_message.py
+++ libs/langchain/langchain/schema/new_message.py
@@ -1,4 +1,9 @@
-class Prompt:
+class PromptInterface(ABC):
+
+    @abstractmethod
+    def format(self, **kwargs: Any) -> Any:
+
+class Prompt(PromptInterface):
     def __init__(
         self,
         content: Optional[str] = None,
@@ -9,9 +14,9 @@ class Prompt:
         self.input_variables = input_variables or infer_inputs(content, formatter)
         self.formatter = formatter
 
-    def format(self: T, **kwargs: Any) -> T:
+    def format(self, **kwargs: Any) -> Any:
         if not self.input_variables:
-            return self
+            return self.content
         elif self.formatter == "f-string":
             return self.content.format(**kwargs)
         elif self.formatter == "jinja":
@@ -20,14 +25,13 @@ class Prompt:
             raise ValueError
 
 
-@dataclasses.dataclass(frozen=True)
 class Message(Prompt):
     def __init__(
         self,
         role: Optional[str] = None,
         type: Optional[Literal["human", "ai", "system", "function"]] = None,
         additional_data: Optional[Mapping[str, Any]] = None,
-        **kwargs: Any
+        **kwargs: Any,
     ) -> None:
         super().__init__(**kwargs)
         self.role = role
@@ -35,9 +39,17 @@ class Message(Prompt):
         self.additional_data = additional_data
 
     def __add__(self, other) -> MessageCollection:
-        return MessageCollection(self, other)
+        return MessageCollection((self, other))
 
 
-@dataclasses.dataclass(frozen=True)
-class MessageCollection(Prompt):
-    messages: Sequence[Message]
+class MessageSequence(PromptInterface):
+    def __init__(self, messages: Sequence[Message]):
+        self.messages = messages
+
+    def format(self, **kwargs) -> List[Message]:
+        return [m.format(**kwargs) for m in self.messages]
+
+    def __add__(self, other) -> MessageCollection:
+        return MessageCollection(list(self.messages) + list(other.messages))
+
+
