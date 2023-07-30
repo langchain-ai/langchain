d9699075e5555532ccb28a0840cc75da1a8bf4fe
Joshua Carroll <joshua.carroll@snowflake.com>
2023-07-30 14:38:42 -0700

    Move back to unit tests and make it compatible across various Streamlit versions

--- libs/langchain/tests/unit_tests/memory/chat_message_histories/test_streamlit.py
+++ libs/langchain/tests/unit_tests/memory/chat_message_histories/test_streamlit.py
@@ -1,4 +1,4 @@
-"""Integration tests for StreamlitChatMessageHistory functionality."""
+"""Unit tests for StreamlitChatMessageHistory functionality."""
 import pytest
 
 test_script = """
@@ -16,42 +16,49 @@ test_script = """
         memory.chat_memory.add_ai_message("This is me, the AI")
         memory.chat_memory.add_user_message("This is me, the human")
     else:
-        st.text("Skipped add")
+        st.markdown("Skipped add")
 
     # Clear messages if checked
     if st.checkbox("clear messages"):
-        st.text("Cleared!")
+        st.markdown("Cleared!")
         memory.chat_memory.clear()
 
     # Write the output to st.code as a json blob for inspection
     messages = memory.chat_memory.messages
     messages_json = json.dumps([_message_to_dict(msg) for msg in messages])
-    st.code(messages_json)
+    st.text(messages_json)
 """
 
 
 @pytest.mark.requires("streamlit")
 def test_memory_with_message_store() -> None:
-    from streamlit.testing.script_interactions import InteractiveScriptTests
+    try:
+        from streamlit.testing.script_interactions import InteractiveScriptTests
+    except ModuleNotFoundError:
+        pytest.skip("Incorrect version of Streamlit installed")
 
     test_handler = InteractiveScriptTests()
     test_handler.setUp()
-    sr = test_handler.script_from_string(test_script).run()
+    try:
+        sr = test_handler.script_from_string(test_script).run()
+    except TypeError:
+        # Earlier version expected 2 arguments
+        sr = test_handler.script_from_string("memory_test.py", test_script).run()
 
     # Initial run should write two messages
-    messages_json = sr.code[0].value
+    messages_json = sr.get("text")[-1].value
     assert "This is me, the AI" in messages_json
     assert "This is me, the human" in messages_json
 
     # Uncheck the initial write, they should persist in session_state
-    sr = sr.checkbox[0].uncheck().run()
-    assert sr.text[0].value == "Skipped add"
-    messages_json = sr.code[0].value
+    sr = sr.get("checkbox")[0].uncheck().run()
+    assert sr.get("markdown")[0].value == "Skipped add"
+    messages_json = sr.get("text")[-1].value
     assert "This is me, the AI" in messages_json
     assert "This is me, the human" in messages_json
 
     # Clear the message history
-    sr = sr.checkbox[1].check().run()
-    assert sr.text[1].value == "Cleared!"
-    messages_json = sr.code[0].value
-    assert sr.code[0].value == "[]"
+    sr = sr.get("checkbox")[1].check().run()
+    assert sr.get("markdown")[1].value == "Cleared!"
+    messages_json = sr.get("text")[-1].value
+    assert messages_json == "[]"
