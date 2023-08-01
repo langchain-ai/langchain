bab1bcdc9eac0bb523ae855e41b2714683fae59b
Jerzy Czopek <jerzy.czopek@avanade.com>
2023-08-02 00:27:31 +0200

    made some cleanup in models to make sure gpt-35-16k and 0613 models have split between input and output tokens prices.

--- libs/langchain/tests/unit_tests/callbacks/test_openai_info.py
+++ libs/langchain/tests/unit_tests/callbacks/test_openai_info.py
@@ -60,3 +60,50 @@ def test_on_llm_end_finetuned_model(handler: OpenAICallbackHandler) -> None:
     )
     handler.on_llm_end(response)
     assert handler.total_cost > 0
+
+@pytest.mark.parametrize(
+        "model_name,expected_cost", 
+        [
+            ("gpt-35-turbo", 0.004), # 0.002 per 1k tokens both input and output
+            ("gpt-35-turbo-0301", 0.004), # 0.002 per 1k tokens both input and output
+            ("gpt-35-turbo-0613", 0.0035), # 0.0015 per 1k input tokens; 0.002 per 1k output tokens
+            ("gpt-35-turbo-16k-0613", 0.007), # 0.003 per 1k input tokens; 0.004 per 1k output tokens
+            ("gpt-35-turbo-16k", 0.007), # 0.003 per 1k input tokens; 0.004 per 1k output tokens
+            ("gpt-4", 0.009), # 0,003 per 1k input tokens; 0.006 per 1k output tokens
+            ("gpt-4-0314", 0.009), # 0,003 per 1k input tokens; 0.006 per 1k output tokens
+            ("gpt-4-0613", 0.009), # 0,003 per 1k input tokens; 0.006 per 1k output tokens
+            ("gpt-4-32k", 0.126), # 0,006 per 1k input tokens; 0.12 per 1k output tokens
+            ("gpt-4-32k-0314", 0.126), # 0,006 per 1k input tokens; 0.12 per 1k output tokens
+            ("gpt-4-32k-0613", 0.126), # 0,006 per 1k input tokens; 0.12 per 1k output tokens
+        ]
+    )
+def test_on_llm_end_azure_openai(handler: OpenAICallbackHandler, model_name, expected_cost) -> None:
+    response = LLMResult(
+        generations=[],
+        llm_output={
+            "token_usage": {
+                "prompt_tokens": 1000,
+                "completion_tokens": 1000,
+                "total_tokens": 2000,
+            },
+            "model_name": model_name,
+        },        
+    )
+    handler.on_llm_end(response)
+    assert handler.total_cost == expected_cost
+
+@pytest.mark.parametrize("model_name", ["gpt-35-turbo-16k-0301", "gpt-4-0301", "gpt-4-32k-0301"])
+def test_on_llm_end_no_cost_invalid_model(handler: OpenAICallbackHandler, model_name) -> None:
+    response = LLMResult(
+        generations=[],
+        llm_output={
+            "token_usage": {
+                "prompt_tokens": 1000,
+                "completion_tokens": 1000,
+                "total_tokens": 2000,
+            },
+            "model_name": model_name,
+        },        
+    )
+    handler.on_llm_end(response)
+    assert handler.total_cost == 0
