9e56d24cff837b4312fb56c0062d6c64c46fdce2
Bagatur <baskaryan@gmail.com>
2023-08-01 13:36:50 -0700

    cr

--- libs/langchain/langchain/graphs/__init__.py
+++ libs/langchain/langchain/graphs/__init__.py
@@ -3,6 +3,7 @@
 from langchain.graphs.arangodb_graph import ArangoGraph
 from langchain.graphs.hugegraph import HugeGraph
 from langchain.graphs.kuzu_graph import KuzuGraph
+from langchain.graphs.memgraph_graph import MemgraphGraph
 from langchain.graphs.nebula_graph import NebulaGraph
 from langchain.graphs.neo4j_graph import Neo4jGraph
 from langchain.graphs.neptune_graph import NeptuneGraph
@@ -10,6 +11,7 @@ from langchain.graphs.networkx_graph import NetworkxEntityGraph
 from langchain.graphs.rdf_graph import RdfGraph
 
 __all__ = [
+    "MemgraphGraph",
     "NetworkxEntityGraph",
     "Neo4jGraph",
     "NebulaGraph",
