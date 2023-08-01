ed9a0f8185248c57da1d6d11bab2cd8d8dae38b6
Leonid Ganeline <leo.gan.57@gmail.com>
2023-08-01 09:12:32 -0700

    Docstrings: Module descriptions (#8262)
    Added/changed the module descriptions (the firs-line docstrings in the
    `__init__` files).
    Added class hierarchy info.
     @baskaryan

--- libs/langchain/langchain/document_loaders/__init__.py
+++ libs/langchain/langchain/document_loaders/__init__.py
@@ -1,4 +1,19 @@
-"""All different types of document loaders."""
+"""**Document Loaders**  are classes to load Documents.
+
+**Document Loaders** are usually used to load a lot of Documents in a single run.
+
+**Class hierarchy:**
+
+.. code-block::
+
+    BaseLoader --> <name>Loader  # Examples: TextLoader, UnstructuredFileLoader
+
+**Main helpers:**
+
+.. code-block::
+
+    Document, <name>TextSplitter
+"""
 
 from langchain.document_loaders.acreom import AcreomLoader
 from langchain.document_loaders.airbyte_json import AirbyteJSONLoader
--- libs/langchain/langchain/memory/__init__.py
+++ libs/langchain/langchain/memory/__init__.py
@@ -1,4 +1,31 @@
-"""Memory maintains Chain state, incorporating context from past runs."""
+"""**Memory** maintains Chain state, incorporating context from past runs.
+
+**Class hierarchy for Memory:**
+
+.. code-block::
+
+    BaseMemory --> BaseChatMemory --> <name>Memory  # Examples: ZepMemory, MotorheadMemory
+
+**Main helpers:**
+
+.. code-block::
+
+    BaseChatMessageHistory
+
+**Chat Message History** stores the chat message history in different stores.
+
+**Class hierarchy for ChatMessageHistory:**
+
+.. code-block::
+
+    BaseChatMessageHistory --> <name>ChatMessageHistory  # Example: ZepChatMessageHistory
+
+**Main helpers:**
+
+.. code-block::
+
+    AIMessage, BaseMessage, HumanMessage
+"""  # noqa: E501
 from langchain.memory.buffer import (
     ConversationBufferMemory,
     ConversationStringBufferMemory,
