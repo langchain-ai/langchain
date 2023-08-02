42f05f4669db895e2ea512712428f9e43217eca0
schadem <45048633+schadem@users.noreply.github.com>
2023-08-02 22:49:04 +0200

    Amazon Textract as document loader added

--- libs/langchain/tests/integration_tests/document_loaders/test_pdf.py
+++ libs/langchain/tests/integration_tests/document_loaders/test_pdf.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 
+import pytest
+
 from langchain.document_loaders import (
+    AmazonTextractPDFLoader,
     MathpixPDFLoader,
     PDFMinerLoader,
     PDFMinerPDFasHTMLLoader,
@@ -136,3 +139,33 @@ def test_mathpix_loader() -> None:
     docs = loader.load()
     assert len(docs) == 1
     print(docs[0].page_content)
+
+
+@pytest.mark.skip()
+def test_amazontextract_loader() -> None:
+    http_file_path = (
+        "https://amazon-textract-public-content.s3.us-east-2.amazonaws.com"
+        "/langchain/alejandro_rosalez_sample_1.jpg"
+    )
+    loader = AmazonTextractPDFLoader(http_file_path)
+    docs = loader.load()
+
+    assert len(docs) == 1
+
+    local_file_path: Path = Path(__file__).parent.parent / "examples/hello.pdf"
+    loader = AmazonTextractPDFLoader(str(local_file_path))
+    docs = loader.load()
+
+    assert len(docs) == 1
+
+    import boto3
+
+    textract_client = boto3.client("textract", region_name="us-east-2")
+
+    s3_file_path = (
+        "s3://amazon-textract-public-content/langchain/layout-parser-paper.pdf"
+    )
+    loader = AmazonTextractPDFLoader(s3_file_path, client=textract_client)
+
+    docs = loader.load()
+    assert len(docs) == 16
