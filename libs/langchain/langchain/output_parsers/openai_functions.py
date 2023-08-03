c1cc4ea39c98e15e5f62b1b99717e00711a83e47
bhavish.pahwa <bhavish.pahwa@mindtickle.com>
2023-08-03 09:58:18 +0530

    Keep only the relevant KeyError handling

--- libs/langchain/langchain/output_parsers/openai_functions.py
+++ libs/langchain/langchain/output_parsers/openai_functions.py
@@ -26,10 +26,10 @@ class OutputFunctionsParser(BaseGenerationOutputParser[Any]):
         message = generation.message
         try:
             func_call = message.additional_kwargs["function_call"]
-        except ValueError as exc:
-            raise OutputParserException(f"Could not parse function call: {exc}")
         except KeyError as key:
-            raise OutputParserException(f"Could not find the following key in additional_kwargs: {key}")
+            raise OutputParserException(
+                f"Could not find the following key in additional_kwargs: {key}"
+            )
 
         if self.args_only:
             return func_call["arguments"]
