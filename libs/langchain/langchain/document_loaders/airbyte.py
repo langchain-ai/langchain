02de90f7d604930f8519d04b41afb5b465c82c67
Joe Reuter <joe@airbyte.io>
2023-08-04 12:05:20 +0200

    review comments

--- libs/langchain/langchain/document_loaders/airbyte.py
+++ libs/langchain/langchain/document_loaders/airbyte.py
@@ -13,26 +13,43 @@ RecordHandler = Callable[[Any, Optional[str]], Document]
 class AirbyteCDKLoader(BaseLoader):
     """Loads records using an Airbyte source connector implemented using the CDK."""
 
-    def __init__(self, config: Mapping[str, Any], source_class: Any, stream_name: str, record_handler: Optional[RecordHandler] = None, state: Optional[Any] = None) -> None:
-        from airbyte_cdk.sources.embedded.base_integration import BaseEmbeddedIntegration
+    def __init__(
+        self,
+        config: Mapping[str, Any],
+        source_class: Any,
+        stream_name: str,
+        record_handler: Optional[RecordHandler] = None,
+        state: Optional[Any] = None,
+    ) -> None:
+        from airbyte_cdk.sources.embedded.base_integration import (
+            BaseEmbeddedIntegration,
+        )
         from airbyte_cdk.sources.embedded.runner import CDKRunner
         from airbyte_cdk.models.airbyte_protocol import AirbyteRecordMessage
+
         class CDKIntegration(BaseEmbeddedIntegration):
-            def _handle_record(self, record: AirbyteRecordMessage, id: Optional[str]) -> Document:
+            def _handle_record(
+                self, record: AirbyteRecordMessage, id: Optional[str]
+            ) -> Document:
                 if record_handler:
                     return record_handler(record, id)
                 return Document(page_content="", metadata=record.data)
-        
-        self._integration = CDKIntegration(config=config, runner=CDKRunner(source=source_class(), name=source_class.__name__))
+
+        self._integration = CDKIntegration(
+            config=config,
+            runner=CDKRunner(source=source_class(), name=source_class.__name__),
+        )
         self._stream_name = stream_name
         self._state = state
-
 
     def load(self) -> List[Document]:
         return list(self.lazy_load())
 
     def lazy_load(self) -> Iterator[Document]:
-        return self._integration._load_data(stream_name=self._stream_name, state=self._state)
+        return self._integration._load_data(
+            stream_name=self._stream_name, state=self._state
+        )
+
 
 class AirbyteHubspotLoader(AirbyteCDKLoader):
     def __init__(
@@ -42,7 +59,9 @@ class AirbyteHubspotLoader(AirbyteCDKLoader):
         record_handler: Optional[RecordHandler] = None,
         state: Optional[Any] = None,
     ) -> None:
-        source_class = guard_import("source_hubspot", "airbyte-source-hubspot").SourceHubspot
+        source_class = guard_import(
+            "source_hubspot", "airbyte-source-hubspot"
+        ).SourceHubspot
         super().__init__(
             config=config,
             source_class=source_class,
@@ -50,6 +69,7 @@ class AirbyteHubspotLoader(AirbyteCDKLoader):
             record_handler=record_handler,
             state=state,
         )
+
 
 class AirbyteStripeLoader(AirbyteCDKLoader):
     def __init__(
@@ -59,7 +79,9 @@ class AirbyteStripeLoader(AirbyteCDKLoader):
         record_handler: Optional[RecordHandler] = None,
         state: Optional[Any] = None,
     ) -> None:
-        source_class = guard_import("source_stripe", "airbyte-source-stripe").SourceStripe
+        source_class = guard_import(
+            "source_stripe", "airbyte-source-stripe"
+        ).SourceStripe
         super().__init__(
             config=config,
             source_class=source_class,
@@ -67,6 +89,7 @@ class AirbyteStripeLoader(AirbyteCDKLoader):
             record_handler=record_handler,
             state=state,
         )
+
 
 class AirbyteTypeformLoader(AirbyteCDKLoader):
     def __init__(
@@ -76,7 +99,9 @@ class AirbyteTypeformLoader(AirbyteCDKLoader):
         record_handler: Optional[RecordHandler] = None,
         state: Optional[Any] = None,
     ) -> None:
-        source_class = guard_import("source_typeform", "airbyte-source-typeform").SourceTypeform
+        source_class = guard_import(
+            "source_typeform", "airbyte-source-typeform"
+        ).SourceTypeform
         super().__init__(
             config=config,
             source_class=source_class,
@@ -84,6 +109,7 @@ class AirbyteTypeformLoader(AirbyteCDKLoader):
             record_handler=record_handler,
             state=state,
         )
+
 
 class AirbyteZendeskSupportLoader(AirbyteCDKLoader):
     def __init__(
@@ -93,7 +119,9 @@ class AirbyteZendeskSupportLoader(AirbyteCDKLoader):
         record_handler: Optional[RecordHandler] = None,
         state: Optional[Any] = None,
     ) -> None:
-        source_class = guard_import("source_zendesk_support", "airbyte-source-zendesk-support").SourceZendeskSupport
+        source_class = guard_import(
+            "source_zendesk_support", "airbyte-source-zendesk-support"
+        ).SourceZendeskSupport
         super().__init__(
             config=config,
             source_class=source_class,
@@ -101,6 +129,7 @@ class AirbyteZendeskSupportLoader(AirbyteCDKLoader):
             record_handler=record_handler,
             state=state,
         )
+
 
 class AirbyteShopifyLoader(AirbyteCDKLoader):
     def __init__(
@@ -110,7 +139,9 @@ class AirbyteShopifyLoader(AirbyteCDKLoader):
         record_handler: Optional[RecordHandler] = None,
         state: Optional[Any] = None,
     ) -> None:
-        source_class = guard_import("source_shopify", "airbyte-source-shopify").SourceShopify
+        source_class = guard_import(
+            "source_shopify", "airbyte-source-shopify"
+        ).SourceShopify
         super().__init__(
             config=config,
             source_class=source_class,
@@ -118,6 +149,7 @@ class AirbyteShopifyLoader(AirbyteCDKLoader):
             record_handler=record_handler,
             state=state,
         )
+
 
 class AirbyteSalesforceLoader(AirbyteCDKLoader):
     def __init__(
@@ -127,7 +159,9 @@ class AirbyteSalesforceLoader(AirbyteCDKLoader):
         record_handler: Optional[RecordHandler] = None,
         state: Optional[Any] = None,
     ) -> None:
-        source_class = guard_import("source_salesforce", "airbyte-source-salesforce").SourceSalesforce
+        source_class = guard_import(
+            "source_salesforce", "airbyte-source-salesforce"
+        ).SourceSalesforce
         super().__init__(
             config=config,
             source_class=source_class,
