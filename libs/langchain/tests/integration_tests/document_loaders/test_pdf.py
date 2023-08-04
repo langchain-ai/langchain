7b0791865807bc708c9bb745ac719dd61b9dd221
Piyush Jain <piyushjain@duck.com>
2023-08-04 12:13:52 -0700

    Update test_pdf.py
    Adding a skip marker for the integration test.

--- libs/langchain/tests/integration_tests/document_loaders/test_pdf.py
+++ libs/langchain/tests/integration_tests/document_loaders/test_pdf.py
@@ -183,6 +183,7 @@ def test_amazontextract_loader(
 
     assert len(docs) == docs_length
 
+@pytest.mark.skip(reason="Requires AWS credentials to run")
 def test_amazontextract_loader_failures() -> None:
     # 2-page PDF local file system
     two_page_pdf = str(Path(__file__).parent.parent / "examples/multi-page-forms-sample-2-page.pdf")
