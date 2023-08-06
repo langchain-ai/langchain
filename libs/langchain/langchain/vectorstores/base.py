5e43a160e93cba15825f251a328046169c53fe04
Harrison Chase <hw.chase.17@gmail.com>
2023-08-06 16:31:11 -0700

    cr

--- libs/langchain/langchain/vectorstores/base.py
+++ libs/langchain/langchain/vectorstores/base.py
@@ -464,13 +464,19 @@ class VectorStore(ABC):
         """Return VectorStoreRetriever initialized from this VectorStore.
 
         Args:
-            search_type (Optional[str]): Defines the type of search that the Retriever should perform. Can be "similarity" (default), "mmr", or "similarity_score_threshold".
-            search_kwargs (Optional[Dict]): Keyword arguments to pass to the search function.
-                k: Amount of documents to return (Default: 4)
-                score_threshold: Minimum relevance threshold for similarity_score_threshold
-                fetch_k: Amount of documents to pass to MMR algorithm (Default: 20)
-                lambda_mult: Diversity of results returned by MMR; 1 for minimum diversity and 0 for maximum. (Default: 0.5)
-                filter: Filter by document metadata
+            search_type (Optional[str]): Defines the type of search that
+                the Retriever should perform.
+                Can be "similarity" (default), "mmr", or
+                "similarity_score_threshold".
+            search_kwargs (Optional[Dict]): Keyword arguments to pass to the
+                search function. Can include things like:
+                    k: Amount of documents to return (Default: 4)
+                    score_threshold: Minimum relevance threshold
+                        for similarity_score_threshold
+                    fetch_k: Amount of documents to pass to MMR algorithm (Default: 20)
+                    lambda_mult: Diversity of results returned by MMR;
+                        1 for minimum diversity and 0 for maximum. (Default: 0.5)
+                    filter: Filter by document metadata
 
         Returns:
             VectorStoreRetriever: Retriever class for VectorStore.
