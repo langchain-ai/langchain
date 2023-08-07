1d032578993cdfad841a96e1cba2663a235f16b5
RedhaWassim <rwasssim@gmail.com>
2023-08-07 21:21:03 +0200

    Add:Eden ai tool explicit content detection text

---
+++ libs/langchain/langchain/tools/edenai/__init__.py
@@ -0,0 +1,34 @@
+"""Edenai Tools."""
+from langchain.tools.edenai.explicit_content_detection_image import (
+    EdenAiExplicitImage,
+)
+from langchain.tools.edenai.explicit_content_detection_text import (
+    EdenAiExplicitTextDetection,
+)
+from langchain.tools.edenai.object_detection import (
+    EdenAiObjectDetectionTool,
+)
+from langchain.tools.edenai.parsing_ID import (
+    EdenAiParsingIDTool,
+)
+from langchain.tools.edenai.parsing_invoice import (
+    EdenAiParsingInvoice,
+)
+
+from langchain.tools.edenai.speech_to_text import (
+    EdenAiSpeechToText,
+)
+
+from langchain.tools.edenai.text_to_speech import (
+    EdenAiTextToSpeech,
+)
+
+__all__ = [
+    "EdenAiExplicitImage",
+    "EdenAiExplicitTextDetection",
+    "EdenAiObjectDetectionTool",
+    "EdenAiParsingIDTool",
+    "EdenAiParsingInvoice",
+    "EdenAiSpeechToText",
+    "EdenAiTextToSpeech",
+]
---
+++ libs/langchain/langchain/tools/edenai/explicit_content_detection_image.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+import logging
+
+from langchain.tools.base import BaseTool
+
+logger = logging.getLogger(__name__)
+
+class EdenAiExplicitImage(BaseTool):
+    
+    pass
