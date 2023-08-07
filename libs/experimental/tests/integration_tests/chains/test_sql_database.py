a69f4c9226890b3006a8ec674ef99367419c8bc6
Bagatur <baskaryan@gmail.com>
2023-08-07 12:53:00 -0700

    fmt

--- libs/experimental/tests/integration_tests/chains/test_sql_database.py
+++ libs/experimental/tests/integration_tests/chains/test_sql_database.py
@@ -1,10 +1,11 @@
 """Test SQL Database Chain."""
-from sqlalchemy import Column, Integer, MetaData, String, Table, create_engine, insert
-
 from langchain.llms.openai import OpenAI
 from langchain.utilities.sql_database import SQLDatabase
-from libs.experimental.langchain_experimental.sql.base import SQLDatabaseChain, \
-    SQLDatabaseSequentialChain
+from libs.experimental.langchain_experimental.sql.base import (
+    SQLDatabaseChain,
+    SQLDatabaseSequentialChain,
+)
+from sqlalchemy import Column, Integer, MetaData, String, Table, create_engine, insert
 
 metadata_obj = MetaData()
 
