a3358d832a0b49777fc7817da6b99e5b4cf073ea
Andrew White <white.d.andrew@gmail.com>
2023-08-07 14:54:17 -0700

    Ran black

--- libs/langchain/tests/integration_tests/vectorstores/qdrant/test_max_marginal_relevance.py
+++ libs/langchain/tests/integration_tests/vectorstores/qdrant/test_max_marginal_relevance.py
@@ -10,27 +10,33 @@ from tests.integration_tests.vectorstores.fake_embeddings import (
 )
 from qdrant_client import models
 
+
 @pytest.mark.parametrize("batch_size", [1, 64])
 @pytest.mark.parametrize("content_payload_key", [Qdrant.CONTENT_KEY, "test_content"])
 @pytest.mark.parametrize("metadata_payload_key", [Qdrant.METADATA_KEY, "test_metadata"])
 @pytest.mark.parametrize("vector_name", [None, "my-vector"])
-@pytest.mark.parametrize("filter", [None, 
-                                    models.Filter(
-                must=[
-                    models.FieldCondition(
-                        key="metadata.page",
-                        match=models.MatchValue(
-                            value=2,
-                        ),
+@pytest.mark.parametrize(
+    "filter",
+    [
+        None,
+        models.Filter(
+            must=[
+                models.FieldCondition(
+                    key="metadata.page",
+                    match=models.MatchValue(
+                        value=2,
                     ),
-                ],
-            ),])
+                ),
+            ],
+        ),
+    ],
+)
 def test_qdrant_max_marginal_relevance_search(
     batch_size: int,
     content_payload_key: str,
     metadata_payload_key: str,
     vector_name: Optional[str],
-    filter: Optional[MetadataFilter]
+    filter: Optional[MetadataFilter],
 ) -> None:
     """Test end to end construction and MRR search."""
     texts = ["foo", "bar", "baz"]
@@ -55,8 +61,7 @@ def test_qdrant_max_marginal_relevance_search(
     ]
 
     output = docsearch.max_marginal_relevance_search(
-        "foo", k=2, fetch_k=3, lambda_mult=0.0,
-        filter=filter
+        "foo", k=2, fetch_k=3, lambda_mult=0.0, filter=filter
     )
     assert output == [
         Document(page_content="baz", metadata={"page": 2}),
