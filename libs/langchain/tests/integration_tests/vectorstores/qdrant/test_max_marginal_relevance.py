e3137266270d8679b84d099cf8cf72908c501712
Andrew White <white.d.andrew@gmail.com>
2023-08-07 14:44:40 -0700

    Added unit test for filter

--- libs/langchain/tests/integration_tests/vectorstores/qdrant/test_max_marginal_relevance.py
+++ libs/langchain/tests/integration_tests/vectorstores/qdrant/test_max_marginal_relevance.py
@@ -4,20 +4,33 @@ import pytest
 
 from langchain.schema import Document
 from langchain.vectorstores import Qdrant
+from langchain.vectorstores.qdrant import MetadataFilter
 from tests.integration_tests.vectorstores.fake_embeddings import (
     ConsistentFakeEmbeddings,
 )
-
+from qdrant_client import models
 
 @pytest.mark.parametrize("batch_size", [1, 64])
 @pytest.mark.parametrize("content_payload_key", [Qdrant.CONTENT_KEY, "test_content"])
 @pytest.mark.parametrize("metadata_payload_key", [Qdrant.METADATA_KEY, "test_metadata"])
 @pytest.mark.parametrize("vector_name", [None, "my-vector"])
+@pytest.mark.parametrize("filter", [None, 
+                                    models.Filter(
+                must=[
+                    models.FieldCondition(
+                        key="metadata.page",
+                        match=models.MatchValue(
+                            value=2,
+                        ),
+                    ),
+                ],
+            ),])
 def test_qdrant_max_marginal_relevance_search(
     batch_size: int,
     content_payload_key: str,
     metadata_payload_key: str,
     vector_name: Optional[str],
+    filter: Optional[MetadataFilter]
 ) -> None:
     """Test end to end construction and MRR search."""
     texts = ["foo", "bar", "baz"]
@@ -38,5 +51,13 @@ def test_qdrant_max_marginal_relevance_search(
     )
     assert output == [
         Document(page_content="foo", metadata={"page": 0}),
+        Document(page_content="baz", metadata={"page": 2}),
+    ]
+
+    output = docsearch.max_marginal_relevance_search(
+        "foo", k=2, fetch_k=3, lambda_mult=0.0,
+        filter=filter
+    )
+    assert output == [
         Document(page_content="baz", metadata={"page": 2}),
     ]
