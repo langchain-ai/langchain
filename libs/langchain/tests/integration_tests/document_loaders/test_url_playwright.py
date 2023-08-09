04fcd2d2e0aee574a0a26b416b4caa0610786b01
Youngwook Kim <youngwook.kim@gmail.com>
2023-08-09 14:14:59 +0900

    refactor(document_loaders): introduce PlaywrightEvaluator abstract base class for custom evalutors and add tests

--- libs/langchain/tests/integration_tests/document_loaders/test_url_playwright.py
+++ libs/langchain/tests/integration_tests/document_loaders/test_url_playwright.py
@@ -4,6 +4,16 @@ import pytest
 from langchain.document_loaders import PlaywrightURLLoader
 
 
+class TestEvaluator(PageEvaluator):
+    """A simple evaluator for testing purposes."""
+
+    def evaluate(self, page, browser, response):
+        return "test"
+
+    async def evaluate_async(self, page, browser, response):
+        return "test"
+
+
 def test_playwright_url_loader() -> None:
     """Test Playwright URL loader."""
     urls = [
@@ -39,3 +49,32 @@ async def test_playwright_async_url_loader() -> None:
     )
     docs = await loader.aload()
     assert len(docs) > 0
+
+
+def test_playwright_url_loader_with_custom_evaluator() -> None:
+    """Test Playwright URL loader with a custom evaluator."""
+    urls = ["https://www.youtube.com/watch?v=dQw4w9WgXcQ"]
+    loader = PlaywrightURLLoader(
+        urls=urls,
+        page_evaluator=TestEvaluator(),
+        continue_on_failure=False,
+        headless=True,
+    )
+    docs = loader.load()
+    assert len(docs) == 1
+    assert docs[0].page_content == "test-"
+
+
+@pytest.mark.asyncio
+async def test_playwright_async_url_loader_with_custom_evaluator() -> None:
+    """Test Playwright async URL loader with a custom evaluator."""
+    urls = ["https://www.youtube.com/watch?v=dQw4w9WgXcQ"]
+    loader = PlaywrightURLLoader(
+        urls=urls,
+        page_evaluator=TestEvaluator(),
+        continue_on_failure=False,
+        headless=True,
+    )
+    docs = await loader.aload()
+    assert len(docs) == 2
+    assert docs[0].page_content == "test"
