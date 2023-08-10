35023e1ae9bfdbb8b3c22a2aa65de0ae5d2621b9
Blake Ho <ycho527@hotmail.com>
2023-08-10 10:32:26 +0100

    Added integration test

---
+++ libs/langchain/tests/integration_tests/llms/test_titan_takeoff.py
@@ -0,0 +1,17 @@
+"""Test Titan Takeoff wrapper."""
+
+from typing import Any, List, Optional
+from langchain.llms.titan_takeoff import TitanTakeoff
+import requests
+import responses
+
+@responses.activate
+def test_titan_takeoff_call() -> None:
+    """Test valid call to Titan Takeoff."""
+    url="http://localhost:8000/generate"
+    responses.add(responses.POST, url, json={"message": "2 + 2 is 4"}, status=200)
+
+    # response = requests.post(url)
+    llm = TitanTakeoff()
+    output = llm("What is 2 + 2?")
+    assert isinstance(output, str)
