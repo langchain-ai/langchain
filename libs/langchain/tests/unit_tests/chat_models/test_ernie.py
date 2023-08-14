a45f19d6559e0b2437aa5fa731ebf4f1e4f8514d
axiangcoding <49201354+axiangcoding@users.noreply.github.com>
2023-08-14 01:50:34 +0000

    lint(tests): format test_ernie.py

--- libs/langchain/tests/unit_tests/chat_models/test_ernie.py
+++ libs/langchain/tests/unit_tests/chat_models/test_ernie.py
@@ -1,22 +1,23 @@
-from langchain.schema.messages import HumanMessage, AIMessage, SystemMessage
 from langchain.chat_models.ernie import _convert_message_to_dict
+from langchain.schema.messages import AIMessage, HumanMessage, SystemMessage
 
 
 def test__convert_dict_to_message_human() -> None:
     message = HumanMessage(content="foo")
     result = _convert_message_to_dict(message)
-    expected_output = {"role":"user", "content":"foo"}
+    expected_output = {"role": "user", "content": "foo"}
     assert result == expected_output
 
 
 def test__convert_dict_to_message_ai() -> None:
     message = AIMessage(content="foo")
     result = _convert_message_to_dict(message)
-    expected_output = {"role":"assistant", "content":"foo"}
+    expected_output = {"role": "assistant", "content": "foo"}
     assert result == expected_output
+
 
 def test__convert_dict_to_message_system() -> None:
     message = SystemMessage(content="foo")
     result = _convert_message_to_dict(message)
-    expected_output = {"role":"system", "content":"foo"}
+    expected_output = {"role": "system", "content": "foo"}
     assert result == expected_output
