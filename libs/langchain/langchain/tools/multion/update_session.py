9bcbbf9439a0433ec1046a94127f59542cad1248
Divyansh Garg <Div99@users.noreply.github.com>
2023-08-14 03:43:09 -0700

    Update update_session.py

--- libs/langchain/langchain/tools/multion/update_session.py
+++ libs/langchain/langchain/tools/multion/update_session.py
@@ -16,56 +16,43 @@ else:
         pass
 
 
-class UpdateSessionSchema(BaseModel):
-    """Input for UpdateSessionTool."""
+class CreateSessionSchema(BaseModel):
+    """Input for CreateSessionTool."""
 
-    tabId: str = Field(
-        ..., description="The tabID, received from one of the createSessions run before"
-    )
     query: str = Field(
         ...,
         description="The query to run in multion agent.",
     )
     url: str = Field(
         "https://www.google.com/",
-        description="""The Url to run the agent at. \
-        Note: accepts only secure links having https://""",
+        description="""The Url to run the agent at. Note: accepts only secure \
+            links having https://""",
     )
 
 
-class MultionUpdateSession(BaseTool):
-    """Tool that updates an existing Multion Browser Window with provided fields.
+class MultionCreateSession(BaseTool):
+    """Tool that creates a new Multion Browser Window with provided fields.
 
     Attributes:
-        name: The name of the tool. Default: "update_multion_session"
+        name: The name of the tool. Default: "create_multion_session"
         description: The description of the tool.
-        args_schema: The schema for the tool's arguments. Default: UpdateSessionSchema
+        args_schema: The schema for the tool's arguments.
     """
 
-    name: str = "update_multion_session"
-    description: str = """Use this tool to update \
-        a existing corresponding \
-    Multion Browser Window with provided fields. \
-        Note:TabId is got from one of the previous Browser window creation."""
-    args_schema: Type[UpdateSessionSchema] = UpdateSessionSchema
+    name: str = "create_multion_session"
+    description: str = """Create a new web browsing session based on a user's command or request. The command should include the full info required for the session. Also include an url (defaults to google.com if no better option) to start the session. \
+Use this tool to create a new Browser Window with provided fields. Always the first step to run \
+any activities that can be done using browser."""
+    args_schema: Type[CreateSessionSchema] = CreateSessionSchema
 
     def _run(
         self,
-        tabId: str,
         query: str,
         url: Optional[str] = "https://www.google.com/",
         run_manager: Optional[CallbackManagerForToolRun] = None,
     ) -> dict:
         try:
-            try:
-                response = multion.update_session(tabId, {"input": query, "url": url})
-                content = {"tabId": tabId, "Response": response["message"]}
-                self.tabId = tabId
-                return content
-            except Exception as e:
-                print(f"{e}, creating a new session")
-                response = multion.new_session({"input": query, "url": url})
-                self.tabID = response["tabId"]
-                return {"tabId": response["tabId"], "Response": response["message"]}
+            response = multion.new_session({"input": query, "url": url})
+            return {"tabId": response["tabId"], "Response": response["message"]}
         except Exception as e:
             raise Exception(f"An error occurred: {e}")
