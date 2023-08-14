6e6cce7d8209aa6707409d7625ab284f3bf6133c
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2023-08-14 16:35:58 -0700

    Update base callbacks

--- libs/langchain/langchain/callbacks/base.py
+++ libs/langchain/langchain/callbacks/base.py
@@ -4,6 +4,8 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Any, Dict, List, Optional, Sequence, Union
 from uuid import UUID
 
+from tenacity import RetryCallState
+
 if TYPE_CHECKING:
     from langchain.schema.agent import AgentAction, AgentFinish
     from langchain.schema.document import Document
@@ -111,13 +113,6 @@ class ChainManagerMixin:
         **kwargs: Any,
     ) -> Any:
         """Run on agent end."""
-
-    def on_retry(
-        self,
-        *args: Any,
-        **kwargs: Any,
-    ) -> Any:
-        """Run on agent retry."""
 
 
 class ToolManagerMixin:
@@ -228,6 +223,16 @@ class RunManagerMixin:
         **kwargs: Any,
     ) -> Any:
         """Run on arbitrary text."""
+
+    def on_retry(
+        self,
+        retry_state: RetryCallState,
+        *,
+        run_id: UUID,
+        parent_run_id: Optional[UUID] = None,
+        **kwargs: Any,
+    ) -> Any:
+        """Run on a retry event."""
 
 
 class BaseCallbackHandler(
@@ -420,6 +425,16 @@ class AsyncCallbackHandler(BaseCallbackHandler):
         **kwargs: Any,
     ) -> None:
         """Run on arbitrary text."""
+
+    async def on_retry(
+        self,
+        retry_state: RetryCallState,
+        *,
+        run_id: UUID,
+        parent_run_id: Optional[UUID] = None,
+        **kwargs: Any,
+    ) -> Any:
+        """Run on a retry event."""
 
     async def on_agent_action(
         self,
