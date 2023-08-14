c2437a27d29803c910c4eb8d68d5e89a8b5291b8
Michael Goin <michael@neuralmagic.com>
2023-08-13 20:19:23 -0400

    Fix test

--- libs/langchain/tests/integration_tests/llms/test_deepsparse.py
+++ libs/langchain/tests/integration_tests/llms/test_deepsparse.py
@@ -6,16 +6,14 @@ from langchain.llms import DeepSparse
 
 def test_deepsparse_call() -> None:
     """Test valid call to DeepSparse."""
-    config = {"max_generated_tokens": 5}
+    config = {"max_generated_tokens": 5, "use_deepsparse_cache": False}
 
     llm = DeepSparse(
         model="zoo:nlg/text_generation/codegen_mono-350m/pytorch/huggingface/bigpython_bigquery_thepile/base-none",
         config=config,
     )
 
-    output = llm("Say foo:")
+    output = llm("def ")
     assert isinstance(output, str)
     assert len(output) > 1
-    assert 0 < len(output) <= config["max_generated_tokens"]
-
-test_deepsparse_call()
+    assert output == "ids_to_names"
