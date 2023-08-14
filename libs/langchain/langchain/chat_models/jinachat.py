64f896e0efbe091bac16fa716e4c8179501e0f85
Clark <wqj97@126.com>
2023-08-15 02:04:20 +0800

    fix(jinachat): related to #9197 (#9200)
    related to: https://github.com/langchain-ai/langchain/issues/9197
    
    ---------
    
    Co-authored-by: qianjun.wqj <qianjun.wqj@alibaba-inc.com>
    Co-authored-by: Bagatur <baskaryan@gmail.com>

--- libs/langchain/langchain/chat_models/jinachat.py
+++ libs/langchain/langchain/chat_models/jinachat.py
@@ -35,6 +35,7 @@ from langchain.schema import (
     ChatGeneration,
     ChatMessage,
     ChatResult,
+    FunctionMessage,
     HumanMessage,
     SystemMessage,
 )
@@ -125,6 +126,12 @@ def _convert_message_to_dict(message: BaseMessage) -> dict:
         message_dict = {"role": "assistant", "content": message.content}
     elif isinstance(message, SystemMessage):
         message_dict = {"role": "system", "content": message.content}
+    elif isinstance(message, FunctionMessage):
+        message_dict = {
+            "role": "function",
+            "name": message.name,
+            "content": message.content,
+        }
     else:
         raise ValueError(f"Got unknown type {message}")
     if "name" in message.additional_kwargs:
