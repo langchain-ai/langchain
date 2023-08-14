2cc23ca3dd9471f6333d51b8ac02113164484d79
Eugene Yurtsev <eyurtsev@gmail.com>
2023-08-14 10:05:24 -0400

    x

--- libs/langchain/langchain/__init__.py
+++ libs/langchain/langchain/__init__.py
@@ -23,6 +23,12 @@ except ImportError:
 
 sys.modules["pydantic_v1"] = pydantic_v1
 
+try:
+    _PYDANTIC_MAJOR_VERSION = int(metadata.version("pydantic").split(".")[0])
+except metadata.PackageNotFoundError:
+    _PYDANTIC_MAJOR_VERSION = 0
+
+
 from langchain.agents import MRKLChain, ReActChain, SelfAskWithSearchChain
 from langchain.cache import BaseCache
 from langchain.chains import (
