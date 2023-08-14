5ed303eb984371c4898de0075e7b3383db008c82
Eugene Yurtsev <eyurtsev@gmail.com>
2023-08-14 13:42:33 -0400

    x

---
+++ libs/langchain/langchain/tools/openapi/utils/_api_models_p2.py
@@ -0,0 +1,26 @@
+from typing import Any
+
+
+class APIPropertyLocation:
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        raise NotImplementedError("Only supported for pydantic v1")
+
+
+class APIProperty:
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        raise NotImplementedError("Only supported for pydantic v1")
+
+
+class APIRequestBodyProperty:
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        raise NotImplementedError("Only supported for pydantic v1")
+
+
+class APIRequestBody:
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        raise NotImplementedError("Only supported for pydantic v1")
+
+
+class APIOperation:
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        raise NotImplementedError("Only supported for pydantic v1")
