b6f342c1a762e9056b1b8b7a8f135e773b40861b
Erick Friis <erick@langchain.dev>
2023-08-14 17:14:47 -0700

    bagatur code review

--- libs/langchain/langchain/hub.py
+++ libs/langchain/langchain/hub.py
@@ -7,15 +7,10 @@ from langchain.load.load import loads
 from langchain.utils import get_from_dict_or_env, get_from_env
 
 
-def _get_client(kwargs: Dict[str, Any]) -> Client:
-    api_url = get_from_dict_or_env(kwargs, "api_url", "LANGCHAIN_HUB_API_URL")
-
-    # read hub-specific api key
-    api_key = get_from_dict_or_env(kwargs, "api_key", "LANGCHAIN_HUB_API_KEY", None)
-
-    # use default api key as fallback
-    if api_key is None:
-        api_key = get_from_env("api_key", "LANGCHAIN_API_KEY")
+def _get_client(api_url: Optional[str] = None, api_key: Optional[str] = None) -> Client:
+    api_url = api_url or get_from_env("api_url", "LANGCHAIN_HUB_API_URL")
+    api_key = api_key or get_from_env("api_key", "LANGCHAIN_HUB_API_KEY", default="")
+    api_key = api_key or get_from_env("api_key", "LANGCHAIN_API_KEY")
     return Client(api_url, api_key=api_key)
 
 
@@ -23,22 +18,32 @@ def push(
     repo_full_name: str,
     parent_commit_hash: Optional[str],
     object: Any,
+    *,
+    api_url: Optional[str] = None,
+    api_key: Optional[str] = None,
     **kwargs: Dict[str, Any]
 ) -> str:
     """
     Pushes an object to the hub and returns the URL.
     """
-    client = _get_client(kwargs)
+    client = _get_client(api_url, api_key)
     manifest_json = dumps(object)
     resp = client.push(repo_full_name, parent_commit_hash, manifest_json)
     commit_hash: str = resp["commit"]["commit_hash"]
     return commit_hash
 
 
-def pull(repo_full_name: str, commit_hash: str, **kwargs: Dict[str, Any]) -> Any:
+def pull(
+    repo_full_name: str,
+    commit_hash: str,
+    *,
+    api_url: Optional[str] = None,
+    api_key: Optional[str] = None,
+    **kwargs: Dict[str, Any]
+) -> Any:
     """
     Pulls an object from the hub and returns it.
     """
-    client = _get_client(kwargs)
+    client = _get_client(api_url, api_key)
     resp: str = client.pull(repo_full_name, commit_hash)
     return loads(resp)
