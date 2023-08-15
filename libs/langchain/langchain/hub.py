fd192496202120aeebb61ef27a9922eeef6e6363
Bagatur <baskaryan@gmail.com>
2023-08-15 13:09:09 -0700

    cr

--- libs/langchain/langchain/hub.py
+++ libs/langchain/langchain/hub.py
@@ -1,4 +1,7 @@
-from typing import TYPE_CHECKING, Any, Dict, Optional
+"""Push and pull to the LangChain Hub."""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, Optional
 
 from langchain.load.dump import dumps
 from langchain.load.load import loads
@@ -9,7 +12,13 @@ if TYPE_CHECKING:
 
 
 def _get_client(api_url: Optional[str] = None, api_key: Optional[str] = None) -> Client:
-    from langchainhub import Client
+    try:
+        from langchainhub import Client
+    except ImportError as e:
+        raise ImportError(
+            "Could not import langchainhub, please install with `pip install "
+            "langchainhub`."
+        ) from e
 
     api_url = api_url or get_from_env("api_url", "LANGCHAIN_HUB_API_URL")
     api_key = api_key or get_from_env("api_key", "LANGCHAIN_HUB_API_KEY", default="")
@@ -24,12 +33,11 @@ def push(
     api_url: Optional[str] = None,
     api_key: Optional[str] = None,
     parent_commit_hash: Optional[str] = "latest",
-    **kwargs: Dict[str, Any]
 ) -> str:
     """
     Pushes an object to the hub and returns the URL.
     """
-    client = _get_client(api_url, api_key)
+    client = _get_client(api_url=api_url, api_key=api_key)
     manifest_json = dumps(object)
     resp = client.push(
         repo_full_name, manifest_json, parent_commit_hash=parent_commit_hash
@@ -43,11 +51,10 @@ def pull(
     *,
     api_url: Optional[str] = None,
     api_key: Optional[str] = None,
-    **kwargs: Dict[str, Any]
 ) -> Any:
     """
     Pulls an object from the hub and returns it.
     """
-    client = _get_client(api_url, api_key)
+    client = _get_client(api_url=api_url, api_key=api_key)
     resp: str = client.pull(owner_repo_commit)
     return loads(resp)
