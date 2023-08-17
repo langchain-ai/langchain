b035ce9059cb5cfad3e1b7807025ae058199aefe
RedhaWassim <rwasssim@gmail.com>
2023-08-17 23:06:51 +0200

    added : eden ai Parsing id tool

--- libs/langchain/langchain/tools/edenai/parsing_ID.py
+++ libs/langchain/langchain/tools/edenai/parsing_ID.py
@@ -8,7 +8,7 @@ from langchain.utils import get_from_dict_or_env
 logger = logging.getLogger(__name__)
 
 class EdenAiParsingIDTool(EdenaiTool):
-    """Tool that queries the Eden AI  Identity parsingAPI.
+    """Tool that queries the Eden AI  Identity parsing API.
 
     for api reference check edenai documentation: https://docs.edenai.co/reference/ocr_identity_parser_create.
     
@@ -27,49 +27,59 @@ class EdenAiParsingIDTool(EdenaiTool):
         "Input should be the string url of the document to parse."
     )
     
-    base_url = "https://api.edenai.run/v2/ocr/identity_parser"
+    feature="ocr"
+    subfeature="identity_parser"    
     
     language: Optional[str] = None
     """
     language of the text passed to the model.
     """    
     
-    provider: str
-    """ provider to use (amazon,base64,microsoft,mindee,klippa )"""
     params : Optional[Dict[str,Any]] = Field(default_factory=dict)
 
     
-    
-    @root_validator()
-    def validate_environment(cls, values: Dict) -> Dict:
-        """Validate that api key exists in environment."""
-        values["edenai_api_key"] = get_from_dict_or_env(
-            values, "edenai_api_key", "EDENAI_API_KEY"
-        )
-        return values
-    
-    def _format_value(self,value):
-        if isinstance(value, dict):
-            if "value" in value:
-                return value["value"]
-            elif "name" in value:
-                return value["name"]
-        elif isinstance(value, list):
-            return " ".join([self.format_value(item) for item in value])
-        return value
 
-    
-    
-    def _format_id_parsing_result(self, id_parsing_result: list) -> str:
-        formatted_text = ""
+    def _parse_json(self,extracted_data : dict , formatted_list : list, level: int =0   ):
         
-        extracted_data = id_parsing_result["extracted_data"][0]
-        for key, value in extracted_data.items():
-            formatted_value = self._format_value(value)
-            formatted_text += f"{key} : {formatted_value}\n"
-        
-        return formatted_text
+        for section, subsections in extracted_data.items():
+            
+            indentation = "  " * level
+            if isinstance(subsections, str):
+                subsections = subsections.replace('\n', ',')
+                formatted_list.append(f"{indentation}{section} : {subsections}")
+                
+            elif isinstance(subsections, list):
+                formatted_list.append(f"{indentation}{section} : ")
+                self._list_handling(subsections, formatted_list, level + 1)
+                
+            elif isinstance(subsections, dict):
+                formatted_list.append(f"{indentation}{section} : ")
+                self._parse_json(subsections, formatted_list, level + 1)
+            
     
+    def _list_handling(self,subsection_list : list, formatted_list : list, level : int):
+        
+        for list_item in subsection_list:
+            if isinstance(list_item, dict):
+                self._parse_json(list_item, formatted_list, level)
+            
+            elif isinstance(list_item, list):
+                self._list_handling(list_item, formatted_list, level + 1)
+                
+            else:
+                formatted_list.append(f"{'  ' * level}{list_item}")
+        
+    def _format_id_parsing(self,json_data : list )->str:
+        formatted_list = []
+
+        if len(json_data) == 1 :
+            result=self._parse_json(json_data[0]["extracted_data"][0],formatted_list)
+        else:
+            for entry in json_data:
+                if entry.get("provider") == "eden-ai":                    
+                    result=self._parse_json(entry["extracted_data"][0],formatted_list)
+    
+        return "\n".join(formatted_list)
     
     
     def _run(
@@ -82,11 +92,10 @@ class EdenAiParsingIDTool(EdenaiTool):
             query_params = {"file_url": query,
                             "language": self.language,
                             **self.params,
-                            "response_as_dict": True,
                             "attributes_as_list": False}
-            text_analysis_result = self._call_eden_ai(query_params)
-            text_analysis_result=text_analysis_result.json()
-            return self._format_id_parsing_result(text_analysis_result)
+            image_analysis_result = self._call_eden_ai(query_params)
+            image_analysis_result=image_analysis_result.json()
+            return self._format_id_parsing(image_analysis_result)
 
         except Exception as e:
             raise RuntimeError(f"Error while running EdenAiExplicitText: {e}")
