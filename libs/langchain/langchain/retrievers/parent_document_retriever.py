554c13f88b03e3dd052227add7ce1e201a7eac42
Harrison Chase <hw.chase.17@gmail.com>
2023-08-21 13:22:33 -0700

    stash

--- libs/langchain/langchain/retrievers/parent_document_retriever.py
+++ libs/langchain/langchain/retrievers/parent_document_retriever.py
@@ -10,7 +10,38 @@ from langchain.text_splitter import TextSplitter
 from langchain.vectorstores.base import VectorStore
 
 
-class ParentDocumentRetriever(BaseRetriever):
+class BaseParentDocumentRetriever(BaseRetriever):
+    """Retrieve from a set of multiple embeddings for the same document."""
+    vectorstore: VectorStore
+    """The underlying vectorstore to use to store small chunks
+    and their embedding vectors"""
+    docstore: BaseStore[str, Document]
+    """The storage layer for the parent documents"""
+    id_key: str = "doc_id"
+    search_kwargs: dict = Field(default_factory=dict)
+    """Keyword arguments to pass to the search function."""
+
+    def _get_relevant_documents(
+        self, query: str, *, run_manager: CallbackManagerForRetrieverRun
+    ) -> List[Document]:
+        """Get documents relevant to a query.
+        Args:
+            query: String to find relevant documents for
+            run_manager: The callbacks handler to use
+        Returns:
+            List of relevant documents
+        """
+        sub_docs = self.vectorstore.similarity_search(query, **self.search_kwargs)
+        # We do this to maintain the order of the ids that are returned
+        ids = []
+        for d in sub_docs:
+            if d.metadata[self.id_key] not in ids:
+                ids.append(d.metadata[self.id_key])
+        docs = self.docstore.mget(ids)
+        return [d for d in docs if d is not None]
+
+
+class ParentDocumentRetriever(BaseParentDocumentRetriever):
     """Retrieve small chunks then retrieve their parent documents.
 
     When splitting documents for retrieval, there are often conflicting desires:
@@ -59,40 +90,18 @@ class ParentDocumentRetriever(BaseRetriever):
             )
     """
 
-    vectorstore: VectorStore
-    """The underlying vectorstore to use to store small chunks
-    and their embedding vectors"""
-    docstore: BaseStore[str, Document]
-    """The storage layer for the parent documents"""
+
     child_splitter: TextSplitter
     """The text splitter to use to create child documents."""
-    id_key: str = "doc_id"
+
     """The key to use to track the parent id. This will be stored in the
     metadata of child documents."""
     parent_splitter: Optional[TextSplitter] = None
     """The text splitter to use to create parent documents.
     If none, then the parent documents will be the raw documents passed in."""
-    search_kwargs: dict = Field(default_factory=dict)
-    """Keyword arguments to pass to the search function."""
 
-    def _get_relevant_documents(
-        self, query: str, *, run_manager: CallbackManagerForRetrieverRun
-    ) -> List[Document]:
-        """Get documents relevant to a query.
-        Args:
-            query: String to find relevant documents for
-            run_manager: The callbacks handler to use
-        Returns:
-            List of relevant documents
-        """
-        sub_docs = self.vectorstore.similarity_search(query, **self.search_kwargs)
-        # We do this to maintain the order of the ids that are returned
-        ids = []
-        for d in sub_docs:
-            if d.metadata[self.id_key] not in ids:
-                ids.append(d.metadata[self.id_key])
-        docs = self.docstore.mget(ids)
-        return [d for d in docs if d is not None]
+
+
 
     def add_documents(
         self,
@@ -142,3 +151,6 @@ class ParentDocumentRetriever(BaseRetriever):
         self.vectorstore.add_documents(docs)
         if add_to_docstore:
             self.docstore.mset(full_docs)
+
+
+class HypotheticalQueryRetriever(BaseParentDocumentRetriever):
