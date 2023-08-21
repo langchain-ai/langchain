c1bf14995120a726033e508478bca2464ba76d50
Крестников Константин Николаевич <KNKrestnikov@sberbank.ru>
2023-08-21 15:36:52 +0300

    Switch to black formatter

--- libs/langchain/langchain/chat_models/gigachat.py
+++ libs/langchain/langchain/chat_models/gigachat.py
@@ -1,4 +1,3 @@
-# flake8: noqa
 """
 GigaChatModel for GigaChat.
 """
@@ -8,12 +7,20 @@ import os
 from typing import Any, AsyncIterator, Dict, Iterator, List, Optional, Union
 
 import requests
-from langchain.callbacks.manager import (AsyncCallbackManagerForLLMRun,
-                                         CallbackManagerForLLMRun)
+from langchain.callbacks.manager import (
+    AsyncCallbackManagerForLLMRun,
+    CallbackManagerForLLMRun,
+)
 from langchain.chat_models.base import SimpleChatModel
-from langchain.schema.messages import (AIMessage, AIMessageChunk, BaseMessage,
-                                       ChatMessage, FunctionMessage,
-                                       HumanMessage, SystemMessage)
+from langchain.schema.messages import (
+    AIMessage,
+    AIMessageChunk,
+    BaseMessage,
+    ChatMessage,
+    FunctionMessage,
+    HumanMessage,
+    SystemMessage,
+)
 from langchain.schema.output import ChatGenerationChunk
 
 
@@ -36,7 +43,7 @@ class GigaChat(SimpleChatModel):
     @classmethod
     def transform_output(cls, response: Any) -> str:
         """Transforms API response to extract desired output."""
-        return response.json()["choices"][0]['message']['content']
+        return response.json()["choices"][0]["message"]["content"]
 
     @classmethod
     def convert_message_to_dict(cls, message: BaseMessage) -> dict:
@@ -47,14 +54,19 @@ class GigaChat(SimpleChatModel):
         elif isinstance(message, AIMessage):
             message_dict = {"role": "assistant", "content": message.content}
             if "function_call" in message.additional_kwargs:
-                message_dict["function_call"] = message.additional_kwargs["function_call"]
+                message_dict["function_call"] = message.additional_kwargs[
+                    "function_call"
+                ]
                 if message_dict["content"] == "":
                     message_dict["content"] = None
         elif isinstance(message, SystemMessage):
             message_dict = {"role": "system", "content": message.content}
         elif isinstance(message, FunctionMessage):
-            message_dict = {"role": "function",
-                            "content": message.content, "name": message.name}
+            message_dict = {
+                "role": "function",
+                "content": message.content,
+                "name": message.name,
+            }
         else:
             raise TypeError(f"Got unknown type {message}")
 
@@ -66,17 +78,30 @@ class GigaChat(SimpleChatModel):
     def _authorize(self):
         if self.user is None or self.password is None:
             raise ValueError(
-                "Please provide GIGA_USER and GIGA_PASSWORD environment variables.")
+                "Please provide GIGA_USER and GIGA_PASSWORD environment variables."
+            )
 
         response = requests.request(
-            "POST", f"{self.api_url}/v1/token", auth=(self.user, self.password), data=[], timeout=3)
+            "POST",
+            f"{self.api_url}/v1/token",
+            auth=(self.user, self.password),
+            data=[],
+            timeout=3,
+        )
         if not response.ok:
             raise ValueError(
-                f"Can't authorize to GigaChat. Error code: {response.status_code}")
+                f"Can't authorize to GigaChat. Error code: {response.status_code}"
+            )
 
         self.token = response.json()["tok"]
 
-    def _call(self, messages: List[BaseMessage], stop: Optional[List[str]] = None, run_manager: Optional[CallbackManagerForLLMRun] = None, **kwargs: Any) -> str:
+    def _call(
+        self,
+        messages: List[BaseMessage],
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+        **kwargs: Any,
+    ) -> str:
         """
         Calls the GigaChat API to get the response.
         """
@@ -84,22 +109,26 @@ class GigaChat(SimpleChatModel):
             self._authorize()
 
         headers = {
-            'Content-Type': 'application/json',
-            'Authorization': f'Bearer {self.token}'
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.token}",
         }
 
         message_dicts = [self.convert_message_to_dict(m) for m in messages]
         payload = {
             "model": self.model,
             "profanity_check": self.profanity,
-            "messages": message_dicts
+            "messages": message_dicts,
         }
 
         if self.verbose:
             self.logger.warning(f"Giga request: {payload}")
 
         response = requests.post(
-            f"{self.api_url}/v1/chat/completions", headers=headers, json=payload, timeout=600)
+            f"{self.api_url}/v1/chat/completions",
+            headers=headers,
+            json=payload,
+            timeout=600,
+        )
         text = self.transform_output(response)
 
         if self.verbose:
@@ -107,11 +136,27 @@ class GigaChat(SimpleChatModel):
 
         return text
 
-    def _stream(self, messages: List[BaseMessage], stop: Union[List[str], None] = None, run_manager: Union[CallbackManagerForLLMRun, None] = None, **kwargs: Any) -> Iterator[ChatGenerationChunk]:
-        yield ChatGenerationChunk(message=AIMessageChunk(content="Async is not supported yet"))
+    def _stream(
+        self,
+        messages: List[BaseMessage],
+        stop: Union[List[str], None] = None,
+        run_manager: Union[CallbackManagerForLLMRun, None] = None,
+        **kwargs: Any,
+    ) -> Iterator[ChatGenerationChunk]:
+        yield ChatGenerationChunk(
+            message=AIMessageChunk(content="Async is not supported yet")
+        )
 
-    async def _astream(self, messages: List[BaseMessage], stop: Union[List[str], None] = None, run_manager: Union[AsyncCallbackManagerForLLMRun, None] = None, **kwargs: Any) -> AsyncIterator[ChatGenerationChunk]:
-        yield ChatGenerationChunk(message=AIMessageChunk(content="Async is not supported yet"))
+    async def _astream(
+        self,
+        messages: List[BaseMessage],
+        stop: Union[List[str], None] = None,
+        run_manager: Union[AsyncCallbackManagerForLLMRun, None] = None,
+        **kwargs: Any,
+    ) -> AsyncIterator[ChatGenerationChunk]:
+        yield ChatGenerationChunk(
+            message=AIMessageChunk(content="Async is not supported yet")
+        )
 
     @property
     def _identifying_params(self) -> Dict[str, Any]:
