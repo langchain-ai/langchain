83627f176be2cb423facbf8fe5cf2adc7a3752d3
priez <jean-baptiste.priez@msd.com>
2023-08-22 08:57:26 +0200

    agents/self_ask_with_search/output_parser fixed on multiline output and single answer (without intermediate) + unit tests

--- libs/langchain/langchain/agents/self_ask_with_search/output_parser.py
+++ libs/langchain/langchain/agents/self_ask_with_search/output_parser.py
@@ -1,3 +1,4 @@
+import re
 from typing import Sequence, Union
 
 from langchain.agents.agent import AgentOutputParser
@@ -8,17 +9,21 @@ class SelfAskOutputParser(AgentOutputParser):
     """Output parser for the self-ask agent."""
 
     followups: Sequence[str] = ("Follow up:", "Followup:")
-    finish_string: str = "So the final answer is: "
+    finish_string: str = "So the final answer is:"
 
     def parse(self, text: str) -> Union[AgentAction, AgentFinish]:
-        last_line = text.split("\n")[-1]
-        if not any([follow in last_line for follow in self.followups]):
-            if self.finish_string not in last_line:
-                raise OutputParserException(f"Could not parse output: {text}")
-            return AgentFinish({"output": last_line[len(self.finish_string) :]}, text)
+        followup_pattern = "|".join(map(lambda followup: f"{followup}", self.followups))
+        followup = re.findall(rf"({followup_pattern})(?ms:(.*))", text)
+        end = re.findall(rf"({self.finish_string})(?ms:(.*))$", text)
 
-        after_colon = text.split(":")[-1].strip()
-        return AgentAction("Intermediate Answer", after_colon, text)
+        # if follow up
+        if len(followup) > 0 and followup[-1][0] in self.followups:
+            return AgentAction("Intermediate Answer", followup[-1][1].strip(), text)
+        # if final output
+        if len(end) > 0 and end[-1][0] == self.finish_string:
+            return AgentFinish({"output": end[-1][1].strip()}, text)
+
+        raise OutputParserException(f"Could not parse output: {text}")
 
     @property
     def _type(self) -> str:
