569e2c205f7e5540eaeb3fa4ede381efc941008e
Eugene Yurtsev <eyurtsev@gmail.com>
2023-08-22 10:53:08 -0400

    x

--- libs/langchain/langchain/indexing/__init__.py
+++ libs/langchain/langchain/indexing/__init__.py
@@ -1,3 +1,14 @@
+"""Code to support indexing workflows with LangChain data loaders and vectorstores.
+
+Use to:
+
+1. Avoid writing duplicated content into the vectostore
+2. Avoid over-writing content if it's unchanged
+
+Importantly, this keeps on working even if the content being written is derived
+via a set of transformations from some source content (e.g., indexing children
+documents that were derived from parent documents by chunking.)
+"""
 from langchain.indexing._sql_record_manager import SQLRecordManager
 from langchain.indexing.api import IndexingResult, index
 
