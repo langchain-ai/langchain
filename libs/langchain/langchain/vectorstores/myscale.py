0cfa38dc3bbb8d3567aa5a40ea48be3db2845d24
刘 方瑞 <fangrui.liu@outlook.com>
2023-08-23 15:37:03 +0800

    Update myscale.py

--- libs/langchain/langchain/vectorstores/myscale.py
+++ libs/langchain/langchain/vectorstores/myscale.py
@@ -46,7 +46,7 @@ class MyScaleSettings(BaseSettings):
         table (str) : Table name to operate on.
                       Defaults to 'vector_table'.
         metric (str) : Metric to compute distance,
-                       supported are ('l2', 'cosine', 'ip'). Defaults to 'cosine'.
+                       supported are ('L2', 'Cosine', 'IP'). Defaults to 'Cosine'.
         column_map (Dict) : Column type map to project column name onto langchain
                             semantics. Must have keys: `text`, `id`, `vector`,
                             must be same size to number of columns. For example:
