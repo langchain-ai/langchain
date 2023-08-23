406c7ff778f60019fed149bf771c54c3363016b1
Leonid Kuligin <kuligin@google.com>
2023-08-23 07:38:25 +0200

    Fixes after review

--- libs/langchain/langchain/document_loaders/parsers/pdf_docai.py
+++ libs/langchain/langchain/document_loaders/parsers/pdf_docai.py
@@ -4,18 +4,22 @@ You need to install two libraries to use this parser:
 pip install google-cloud-documentai
 pip install google-cloud-documentai-toolbox
 """
+import logging
 import time
 from dataclasses import dataclass
-from typing import TYPE_CHECKING, Iterator, List, Optional
-
-from google.api_core.operation import Operation
+from typing import TYPE_CHECKING, Any, Iterator, List, Optional, Sequence
 
 from langchain.docstore.document import Document
 from langchain.document_loaders.base import BaseBlobParser
 from langchain.document_loaders.blob_loaders import Blob
+from langchain.utils.iter import batch_iterate
 
 if TYPE_CHECKING:
+    from google.api_core.operation import Operation
     from google.cloud.documentai import DocumentProcessorServiceClient
+
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -29,39 +33,45 @@ class DocAIParsingResults:
 class DocAIPdfLoader(BaseBlobParser):
     def __init__(
         self,
-        project: str,
         *,
         location: str = "us",
         processor_name: Optional[str] = None,
-        gcs_output_path: Optional[str] = None,
     ):
+        """Initializes the parser.
+
+        Args:
+            location: a GCP location where a DOcAI parser is located
+            processor_name: name of a processor
+        """
         from google.api_core.client_options import ClientOptions
 
         self._options = ClientOptions(
             api_endpoint=f"{location}-documentai.googleapis.com"
         )
         self._processor_name = processor_name
-        self._name = self.client.common_location_path(project=project, location="us")
-        self._gcs_output_path = gcs_output_path
+        self._client: Optional["DocumentProcessorServiceClient"] = None
 
-    def lazy_parse(self, blob: Blob) -> Iterator[Document]:
+    def lazy_parse(self, blob: Blob, **kwargs: Any) -> Iterator[Document]:
         """Parses a blob lazily.
+
+        Args:
+            blobs: a Blob to parse
 
         This is a long-running operations! A recommended way is to batch
             documents together and use `batch_parse` method.
         """
-        yield from self.batch_parse(
-            [blob], self._gcs_output_path  # type: ignore[arg-type]
-        )
+        gcs_output_path: str = kwargs["gcs_output_path"]
+        yield from self.batch_parse([blob], gcs_output_path=gcs_output_path)
 
     def batch_parse(
-        self, blobs: Sequence[Blob], gcs_output_path: str
+        self, blobs: Sequence[Blob], gcs_output_path: str, timeout_min: int = 60
     ) -> Iterator[Document]:
         """Parses a list of blobs lazily.
 
         Args:
             blobs: a list of blobs to parse
             gcs_output_path: a path on GCS to store parsing results
+            timeout_min: a timeout to wait for DocAI to complete, in minutes
         This is a long-running operations! A recommended way is to decouple
             parsing from creating Langchain Documents:
             >>> operations = parser.docai_parse(blobs, gcs_path)
@@ -71,60 +81,85 @@ class DocAIPdfLoader(BaseBlobParser):
             And when all operations are finished, you can use their results:
             >>> operations = parser.operations_from_names(operation_names)
             >>> results = parser.get_results(operations)
-            >>> docs = parser.batch_parse_from_results(results)
+            >>> docs = parser.parse_from_results(results)
         """
         operations = self.docai_parse(blobs, gcs_output_path=gcs_output_path)
-        is_running = self.is_running(operations)
+        operation_names = [op.name for op in operations]  # type: ignore[attr-defined]
+        logger.debug(
+            f"Started parsing with DocAI, submitted operations {operation_names}"
+        )
+        is_running, time_elapsed = True, 0
         while is_running:
             is_running = self.is_running(operations)
             if not is_running:
                 break
             time.sleep(60)
-            print(".", end="")
+            time_elapsed += 1
+            if time_elapsed > timeout_min:
+                raise ValueError(
+                    "Timeout exceeded! Check operations " f"{operation_names} later!"
+                )
+            logger.debug(".")
 
         results = self.get_results(operations=operations)
-        yield from self.batch_parse_from_results(results)
+        yield from self.parse_from_results(results)
 
-    def batch_parse_from_results(
+    def parse_from_results(
         self, results: List[DocAIParsingResults]
     ) -> Iterator[Document]:
+        try:
+            from google.cloud.documentai_toolbox.wrappers.document import _get_shards
+            from google.cloud.documentai_toolbox.wrappers.page import _text_from_layout
+        except ImportError:
+            raise ImportError(
+                "documentai_toolbox package not found, please install it with"
+                " `pip install google-cloud-documentai-toolbox`"
+            )
         for result in results:
-            for doc in self.parse_from_result(result):
-                yield doc
-
-    def parse_from_result(self, result: DocAIParsingResults) -> List[Document]:
-        """Creates a list of Documents from Google DocAI parsing results."""
-        from google.cloud.documentai_toolbox.wrappers.document import _get_shards
-        from google.cloud.documentai_toolbox.wrappers.page import _text_from_layout
-
-        output_gcs = result.parsed_path.split("/")
-        gcs_bucket_name = output_gcs[2]
-        gcs_prefix = "/".join(output_gcs[3:]) + "/"
-        shards = _get_shards(gcs_bucket_name, gcs_prefix)
-        docs, i = [], 0
-        for shard in shards:
-            for page in shard.pages:
-                docs.append(
-                    Document(
-                        page_content=_text_from_layout(page.layout, shard.text),
-                        metadata={"page": i + 1, "source": result.source_path},
+            output_gcs = result.parsed_path.split("/")
+            gcs_bucket_name = output_gcs[2]
+            gcs_prefix = "/".join(output_gcs[3:]) + "/"
+            shards = _get_shards(gcs_bucket_name, gcs_prefix)
+            docs, page_number = [], 1
+            for shard in shards:
+                for page in shard.pages:
+                    docs.append(
+                        Document(
+                            page_content=_text_from_layout(page.layout, shard.text),
+                            metadata={
+                                "page": page_number,
+                                "source": result.source_path,
+                            },
+                        )
                     )
-                )
-                i += 1
-        return docs
+                    page_number += 1
+            yield from docs
 
     @property
     def client(self) -> "DocumentProcessorServiceClient":
         """Returns a docai client to be used by the parser."""
-        from google.cloud.documentai import DocumentProcessorServiceClient
-
+        try:
+            from google.cloud.documentai import DocumentProcessorServiceClient
+        except ImportError:
+            raise ImportError(
+                "documentai package not found, please install it with"
+                " `pip install google-cloud-documentai`"
+            )
+        if self._client:
+            return self._client
         return DocumentProcessorServiceClient(client_options=self._options)
 
-    def operations_from_names(self, operation_names: List[str]) -> List[Operation]:
+    def operations_from_names(self, operation_names: List[str]) -> List["Operation"]:
         """Initializes Long-Running Operations from their names."""
-        from google.longrunning.operations_pb2 import (
-            GetOperationRequest,  # type: ignore
-        )
+        try:
+            from google.longrunning.operations_pb2 import (
+                GetOperationRequest,  # type: ignore
+            )
+        except ImportError:
+            raise ImportError(
+                "documentai package not found, please install it with"
+                " `pip install gapic-google-longrunning`"
+            )
 
         operations = []
         for name in operation_names:
@@ -132,35 +167,90 @@ class DocAIPdfLoader(BaseBlobParser):
             operations.append(self.client.get_operation(request=request))
         return operations
 
-    def is_running(self, operations: List[Operation]) -> bool:
+    def is_running(self, operations: List["Operation"]) -> bool:
         for op in operations:
             if not op.done:  # type: ignore[truthy-function]
                 return False
         return True
 
-    def docai_parse(self, blobs: List[Blob], gcs_output_path: str) -> List[Operation]:
+    def docai_parse(
+        self,
+        blobs: Sequence[Blob],
+        gcs_output_path: str,
+        *,
+        batch_size: int = 4000,
+        enable_native_pdf_parsing: bool = True,
+    ) -> List["Operation"]:
         """Runs Google DocAI PDF parser on a list of blobs.
 
         Args:
             blobs: a list of blobs to be parsed
             gcs_output_path: a path (folder) on GCS to store results
+            batch_size: amount of documents per batch
+            enable_native_pdf_parsing: a config option for the parser
 
         DocAI has a limit on the amount of documents per batch, that's why split a
             batch into mini-batches. Parsing is an async long-running operation
             on Google Cloud and results are stored in a output GCS bucket.
         """
-        i = 0
-        operations = []
-        while i < len(blobs):
-            op = self._docai_parse(
-                blobs[i : (i + 4000)], gcs_output_path=gcs_output_path
+        try:
+            from google.cloud import documentai
+            from google.cloud.documentai_v1.types import OcrConfig, ProcessOptions
+        except ImportError:
+            raise ImportError(
+                "documentai package not found, please install it with"
+                " `pip install google-cloud-documentai`"
             )
-            operations.append(op)
-            i += 4000
+
+        if not self._processor_name:
+            raise ValueError("Processor name is not defined, aborting!")
+
+        operations = []
+        for batch in batch_iterate(size=batch_size, iterable=blobs):
+            documents = []
+            for blob in batch:
+                gcs_document = documentai.GcsDocument(
+                    gcs_uri=blob.path, mime_type="application/pdf"
+                )
+                documents.append(gcs_document)
+            gcs_documents = documentai.GcsDocuments(documents=documents)
+
+            input_config = documentai.BatchDocumentsInputConfig(
+                gcs_documents=gcs_documents
+            )
+
+            gcs_output_config = documentai.DocumentOutputConfig.GcsOutputConfig(
+                gcs_uri=gcs_output_path, field_mask=None
+            )
+            output_config = documentai.DocumentOutputConfig(
+                gcs_output_config=gcs_output_config
+            )
+
+            if enable_native_pdf_parsing:
+                process_options = ProcessOptions(
+                    ocr_config=OcrConfig(
+                        enable_native_pdf_parsing=enable_native_pdf_parsing
+                    )
+                )
+            else:
+                process_options = ProcessOptions()
+            request = documentai.BatchProcessRequest(
+                name=self._processor_name,
+                input_documents=input_config,
+                document_output_config=output_config,
+                process_options=process_options,
+            )
+            operations.append(self.client.batch_process_documents(request))
         return operations
 
-    def get_results(self, operations: List[Operation]) -> List[DocAIParsingResults]:
-        from google.cloud.documentai_v1 import BatchProcessMetadata
+    def get_results(self, operations: List["Operation"]) -> List[DocAIParsingResults]:
+        try:
+            from google.cloud.documentai_v1 import BatchProcessMetadata
+        except ImportError:
+            raise ImportError(
+                "documentai package not found, please install it with"
+                " `pip install google-cloud-documentai`"
+            )
 
         results = []
         for op in operations:
@@ -173,47 +263,11 @@ class DocAIPdfLoader(BaseBlobParser):
                 )
         return results
 
-    def _docai_parse(
-        self,
-        blobs: List[Blob],
-        gcs_output_path: str,
-        *,
-        enable_native_pdf_parsing: bool = True,
-    ) -> Operation:
-        from google.cloud import documentai
-        from google.cloud.documentai_v1.types import OcrConfig, ProcessOptions
-
-        if not self._processor_name:
-            raise ValueError("Processor name is not defined, aborting!")
-
-        documents = []
-        for blob in blobs:
-            gcs_document = documentai.GcsDocument(
-                gcs_uri=blob.path, mime_type="application/pdf"
-            )
-            documents.append(gcs_document)
-        gcs_documents = documentai.GcsDocuments(documents=documents)
-
-        input_config = documentai.BatchDocumentsInputConfig(gcs_documents=gcs_documents)
-
-        gcs_output_config = documentai.DocumentOutputConfig.GcsOutputConfig(
-            gcs_uri=gcs_output_path, field_mask=None
-        )
-        output_config = documentai.DocumentOutputConfig(
-            gcs_output_config=gcs_output_config
-        )
-
-        if enable_native_pdf_parsing:
-            process_options = ProcessOptions(
-                ocr_config=OcrConfig(enable_native_pdf_parsing=enable_native_pdf_parsing)
-            )
-        else:
-            process_options = ProcessOptions()
-        request = documentai.BatchProcessRequest(
-            name=self._processor_name,
-            input_documents=input_config,
-            document_output_config=output_config,
-            process_options=process_options,
-        )
-        operation = self.client.batch_process_documents(request)
-        return operation
+    @classmethod
+    def from_components(
+        cls, *, client: "DocumentProcessorServiceClient", processor_name: str
+    ) -> "DocumentProcessorServiceClient":
+        """Creates a DocumentProcessorServiceClient with a pre-existing client."""
+        parser = cls(processor_name=processor_name)
+        parser._client = client
+        return parser
