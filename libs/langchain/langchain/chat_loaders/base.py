e023ac98bb6dfd6f606e959475bec29bc4b72a5f
William FH <13333726+hinthornw@users.noreply.github.com>
2023-08-23 10:10:47 -0700

    Wfh/whatsapp loader (#9660)
    <!-- Thank you for contributing to LangChain!
    
    Replace this entire comment with:
      - Description: a description of the change, 
      - Issue: the issue # it fixes (if applicable),
      - Dependencies: any dependencies required for this change,
    - Tag maintainer: for a quicker response, tag the relevant maintainer
    (see below),
    - Twitter handle: we announce bigger features on Twitter. If your PR
    gets announced and you'd like a mention, we'll gladly shout you out!
    
    Please make sure your PR is passing linting and testing before
    submitting. Run `make format`, `make lint` and `make test` to check this
    locally.
    
    See contribution guidelines for more information on how to write/run
    tests, lint, etc:
    
    https://github.com/hwchase17/langchain/blob/master/.github/CONTRIBUTING.md
    
    If you're adding a new integration, please include:
    1. a test for the integration, preferably unit tests that do not rely on
    network access,
    2. an example notebook showing its use. These live is docs/extras
    directory.
    
    If no one reviews your PR within a few days, please @-mention one of
    @baskaryan, @eyurtsev, @hwchase17, @rlancemartin.
     -->
    
    ---------
    
    Co-authored-by: Harrison Chase <hw.chase.17@gmail.com>

--- libs/langchain/langchain/chat_loaders/base.py
+++ libs/langchain/langchain/chat_loaders/base.py
@@ -1,13 +1,20 @@
 from abc import ABC, abstractmethod
-from typing import List, Iterator
+from typing import Iterator, List, TypedDict
+
 from langchain.schema.messages import BaseMessage
-from pydantic import BaseModel
 
-class BaseChatLoader(BaseModel, ABC):
 
-    def load_messages(self) -> List[List[BaseMessage]]:
-        return list(self.lazy_load_messages())
+class ChatSession(TypedDict):
+    """A chat session is a sequence of messages."""
+
+    messages: List[BaseMessage]
+
+class BaseChatLoader(ABC):
 
     @abstractmethod
-    def lazy_load_messages(self) -> Iterator[List[BaseMessage]]:
-        """Return iterator of messages."""
+    def lazy_load(self) -> Iterator[ChatSession]:
+        """Lazy load the messages from the chat file and yield them in the required format."""
+    
+    def load(self) -> List[ChatSession]:
+        return list(self.lazy_load())
+
