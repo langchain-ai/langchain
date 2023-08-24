3609d5dbcd5bea0aa9c762441ed501fe69bc7211
Piotr Gródek <piotr.grodek@deepsense.ai>
2023-08-24 17:51:41 +0200

    Add utility to wrap docker API
    Goal is to execute code and commands in isolated env.

---
+++ libs/langchain/langchain/utilities/docker_containers.py
@@ -0,0 +1,236 @@
+from functools import lru_cache
+from pathlib import Path
+from types import TracebackType
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Callable,
+    Dict,
+    Generator,
+    List,
+    Optional,
+    Tuple,
+    Type,
+    Union,
+)
+
+import docker
+import docker.errors
+
+if TYPE_CHECKING:
+    from docker.models.containers import Container
+
+
+@lru_cache(maxsize=1)
+def get_docker_client(**kwargs: Any) -> docker.DockerClient:
+    """cached version to retrieve docker client. By default it will use environment
+    variables to connect to docker daemon.
+    """
+    return docker.from_env(**kwargs)
+
+
+def generate_langchain_container_tag() -> str:
+    """Generates a random tag for a docker container."""
+    import uuid
+    from datetime import datetime
+
+    timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    return f"langchain_runner:{timestamp}-{uuid.uuid4().hex[:8]}"
+
+
+class DockerImage:
+    """Represents a locally available docker image as a tag.
+    You can either use existing docker image or build a new one from Dockerfile.
+
+    Examples:
+    >>> image = DockerImage.from_tag("alpine")
+    >>> image = DockerImage.from_tag("python", tag="3.9-slim")
+    >>> image = DockerImage.from_dockerfile("example/Dockerfile")
+    >>> image = DockerImage.from_dockerfile("path/to/dir_with_Dockerfile/", name="cow")
+    """
+
+    def __init__(self, name: str):
+        """Note that it does not pull the image from the internet.
+        It only represents a tag so it must exist on your system.
+        """
+        self.name = name
+        # check if image exists
+        docker_client = get_docker_client()
+        if len(docker_client.images.list(name=name)) < 1:
+            raise ValueError(
+                f"Invalid value: name={name} does not exist on your system."
+                "Use DockerImage.from_tag() to pull it."
+            )
+
+    def __repr__(self) -> str:
+        return f"DockerImage(name={self.name})"
+
+    @staticmethod
+    def from_tag(
+        repository: str,
+        tag: str = "latest",
+        auth_config: Optional[Dict[str, str]] = None,
+    ) -> "DockerImage":
+        """Use image with a given repository and tag. It is going to pull it if it is
+        not present on the system.
+        Example: repository = "alpine" (will get "latest" tag)
+        Example: repository = "python" tag = "3.9-slim"
+        """
+        docker_client = get_docker_client()
+        docker_client.images.pull(
+            repository=repository, tag=tag, auth_config=auth_config
+        )
+        return DockerImage(name=f"{repository}:{tag}")
+
+    @staticmethod
+    def from_dockerfile(
+        dockerfile_path: Union[Path, str],
+        name: Union[str, Callable[[], str]] = generate_langchain_container_tag,
+        **kwargs: Any,
+    ) -> "DockerImage":
+        """Build a new image from Dockerfile given its file path."""
+
+        img_name = (
+            name
+            if isinstance(name, str) and name
+            else generate_langchain_container_tag()
+        )
+        dockerfile = Path(dockerfile_path)
+
+        docker_client = get_docker_client()
+
+        if dockerfile.is_dir():
+            if not (dockerfile / "Dockerfile").exists():
+                raise ValueError(
+                    f"Directory {dockerfile} does not contain a Dockerfile."
+                )
+            docker_client.images.build(
+                path=str(dockerfile), tag=img_name, rm=True, **kwargs
+            )
+        elif dockerfile.name == "Dockerfile" and dockerfile.is_file():
+            docker_client.images.build(
+                fileobj=dockerfile.open("rb"), tag=img_name, rm=True, **kwargs
+            )
+        else:
+            raise ValueError(f"Invalid parameter: dockerfile: {dockerfile}")
+
+        return DockerImage(name=img_name)
+
+
+class DockerContainer:
+    """An isolated environment for running commands, based on docker container.
+
+    Examples:
+    If you need to run container for a single job:
+    >>> container = DockerContainer(DockerImage.from_tag("alpine"))
+    >>> status_code, logs = container.spawn_run("echo hello world")
+
+    To run a container in background and execute commands:
+    >>> with DockerContainer(DockerImage.from_tag("alpine")) as container:
+    >>>     status_code, logs = container.run("echo hello world")
+    """
+
+    def __init__(self, image: DockerImage, **kwargs: Any):
+        """Wraps docker image to control container interaction.
+        NOTE: **kwargs are passed to docker client containers.run() method so you can
+        use them as you wish.
+        """
+        self.image = image
+        self._client = get_docker_client()
+        self._container = None
+        self._run_kwargs = kwargs
+
+    def __enter__(self) -> "DockerContainer":
+        """Enters container context. It means that container is started and you can
+        execute commands inside it.
+        """
+        assert self._container is None, "You cannot re-entry container"
+        # tty=True is required to keep container alive
+        self._container = self._client.containers.run(
+            self.image.name,
+            detach=True,
+            tty=True,
+            **self._run_kwargs,
+        )
+        return self
+
+    def __exit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc: Optional[BaseException],
+        traceback: Optional[TracebackType],
+    ) -> bool:
+        """Cleanup container on exit."""
+        if exc_type is not None:
+            # re-throw exception. try to stop container and remove it
+            try:
+                self._cleanup()
+            except Exception as e:
+                print("Failed to stop and remove container to cleanup exception.", e)
+            return False
+        else:
+            self._cleanup()
+            self._container = None
+            return True
+
+    def spawn_run(
+        self, command: Union[str, List[str]], **kwargs: Any
+    ) -> Tuple[int, bytes]:
+        """Run a script in the isolated environment which is docker container with the
+        same lifetime as this function call.
+
+        You can also pass all arguments that docker client containers.run() accepts.
+        It blocks till command is finished.
+        """
+        # we can update here kwargs with self._run_kwargs so user can override them
+        custom_kwargs = (
+            self._run_kwargs.copy().update(kwargs) if kwargs else self._run_kwargs
+        )
+        # There is a known issue with auto_remove=True and docker-py:
+        # https://github.com/docker/docker-py/issues/1813
+        # so as workaround we detach, wait & and remove container manually
+        container = self._client.containers.run(
+            self.image.name, command=command, detach=True, **custom_kwargs
+        )
+        status_code = container.wait().get("StatusCode", 1)
+        logs = container.logs()
+        container.remove()
+        return status_code, logs
+
+    @property
+    def docker_container(self) -> Container:
+        """Returns docker container object."""
+        assert (
+            self._container is not None
+        ), "You cannot access container that was not entered"
+        return self._container
+
+    @property
+    def name(self) -> str:
+        """Name of the container if it exists, empty string otherwise."""
+        if self._container:
+            return self._container.name
+        return ""
+
+    def run(
+        self, command: Union[str, List[str]], **kwargs: Any
+    ) -> Tuple[int, Union[bytes, Tuple[bytes, bytes], Generator[bytes, None, None]]]:
+        """Run a script in the isolated environment which is docker container.
+        You can send any args which docker-py exec_run accepts:
+        https://docker-py.readthedocs.io/en/stable/containers.html#docker.models.containers.Container.exec_run
+        Return is a tuple of exit code and output which is controlled by arguments:
+        stream, socket and demux.
+        """
+        assert (
+            self._container is not None
+        ), "You cannot execute command in container that was not entered"
+
+        exit_code, output = self._container.exec_run(cmd=command, **kwargs)
+        return exit_code, output
+
+    def _cleanup(self) -> None:
+        """Stops and removes container."""
+        if self._container is None:
+            return
+        self._container.stop()
+        self._container.remove()
---
+++ libs/langchain/tests/integration_tests/utilities/test_docker_containers.py
@@ -0,0 +1,85 @@
+from pathlib import Path
+from typing import cast
+
+import pytest
+from docker_containers.docker_containers import (
+    DockerContainer,
+    DockerImage,
+    generate_langchain_container_tag,
+    get_docker_client,
+)
+
+
+def test_generate_langchain_container_tag() -> None:
+    tag = generate_langchain_container_tag()
+    assert tag.startswith("langchain")
+    assert len(tag) > len("langchain")
+    new_tag = generate_langchain_container_tag()
+    assert tag != new_tag, "Tags should be different"
+
+
+def test_docker_image_throws_for_bad_name() -> None:
+    with pytest.raises(ValueError):
+        DockerImage(name="riddiculus_docker_image_which_should_not_exist_42")
+
+
+def run_container_cowsay(image: DockerImage) -> None:
+    """Helper for testing - runs cowsay command and verifies it works."""
+    # note that our cowsay adds moo prefix as commands are executed
+    # by ENTRYPOINT defined in dockerfile.
+    try:
+        container = DockerContainer(image)
+        ret_code, log = container.spawn_run("I like langchain!")
+        assert ret_code == 0, "Cowsay should return 0"
+        assert (
+            log.find(b"moo I like langchain") >= 0
+        ), "Cowsay should say same words with moo"
+    finally:
+        docker_client = get_docker_client()
+        docker_client.images.remove(image.name)
+
+
+def test_build_image_from_dockerfile() -> None:
+    dockerfile_path = Path(__file__).parent / "docker_test_data/Dockerfile"
+    image = DockerImage.from_dockerfile(dockerfile_path, name="cow")
+    run_container_cowsay(image)
+
+
+def test_build_image_from_dockerfile_dirpath() -> None:
+    dockerfile_dir = Path(__file__).parent / "docker_test_data/"
+    image = DockerImage.from_dockerfile(dockerfile_dir)
+    run_container_cowsay(image)
+
+
+def test_docker_spawn_run_works() -> None:
+    container = DockerContainer(DockerImage.from_tag("alpine"))
+    status_code, logs = container.spawn_run(["echo", "hello", "world"])
+    assert status_code == 0
+    assert logs.find(b"hello world") >= 0
+
+    status_code, logs = container.spawn_run("echo good bye")
+    assert status_code == 0
+    assert logs.find(b"good bye") >= 0
+
+
+def test_docker_spawn_run_return_nonzero_status_code() -> None:
+    container = DockerContainer(DockerImage.from_tag("alpine"))
+    status_code, logs = container.spawn_run("sh -c 'echo langusta && exit 1'")
+    assert status_code == 1
+    assert logs.find(b"langusta") >= 0
+
+
+def test_docker_container_background_run_works() -> None:
+    client = get_docker_client()
+    container_name: str
+    with DockerContainer(DockerImage.from_tag("alpine")) as container:
+        container_name = container.name
+        assert len(client.containers.list(filters={"name": container_name})) == 1
+        ret_code, output = container.run("touch /animal.txt")
+        assert ret_code == 0
+
+        ret_code, output = container.run("ls /")
+        assert ret_code == 0
+        assert cast(bytes, output).find(b"animal.txt") >= 0
+
+    assert len(client.containers.list(filters={"name": container_name})) == 0
