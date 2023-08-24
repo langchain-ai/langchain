b510f026e43a145f534d53b7bede1bc8da2d0da6
Anjan Biswas <anjanavb@amazon.com>
2023-08-24 12:04:37 -0700

    Removed BaseModerationException and updated Intent Label name

--- libs/langchain/langchain/chains/comprehend_moderation/base_moderation_enums.py
+++ libs/langchain/langchain/chains/comprehend_moderation/base_moderation_enums.py
@@ -1,11 +1,9 @@
 from enum import Enum
 
-
 class BaseModerationActions(Enum):
     STOP = 1
     ALLOW = 2
-
-
+    
 class BaseModerationFilters(str, Enum):
     PII = "pii"
     TOXICITY = "toxicity"
