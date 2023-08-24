420e69a0d4da8509301a34a94d513128b34cb3df
Tudor Golubenco <tudor@xata.io>
2023-08-24 23:13:31 +0200

    lint fixes

--- libs/langchain/tests/integration_tests/memory/test_xata.py
+++ libs/langchain/tests/integration_tests/memory/test_xata.py
@@ -3,8 +3,8 @@
 Before running this test, please create a Xata database.
 """
 
-import os
 import json
+import os
 
 from langchain.memory import ConversationBufferMemory
 from langchain.memory.chat_message_histories import XataChatMessageHistory
@@ -19,9 +19,9 @@ class TestXata:
 
     def test_xata_chat_memory(self) -> None:
         message_history = XataChatMessageHistory(
-            api_key=os.getenv("XATA_API_KEY"), 
-            db_url=os.getenv("XATA_DB_URL"),
-            session_id="integration-test-session"
+            api_key=os.getenv("XATA_API_KEY", ""),
+            db_url=os.getenv("XATA_DB_URL", ""),
+            session_id="integration-test-session",
         )
         memory = ConversationBufferMemory(
             memory_key="baz", chat_memory=message_history, return_messages=True
@@ -39,4 +39,3 @@ class TestXata:
 
         # remove the record from Redis, so the next test run won't pick it up
         memory.chat_memory.clear()
-
