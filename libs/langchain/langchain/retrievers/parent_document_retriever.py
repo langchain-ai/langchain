38926625efa3f51e9333d17f6c8989e8aa55cfe7
Harrison Chase <hw.chase.17@gmail.com>
2023-08-24 06:18:36 -0700

    cr

--- libs/langchain/langchain/retrievers/parent_document_retriever.py
+++ libs/langchain/langchain/retrievers/parent_document_retriever.py
@@ -1,49 +1,12 @@
 import uuid
-from typing import List, Optional, Callable, Union
+from typing import List, Optional
 
-from langchain.callbacks.manager import CallbackManagerForRetrieverRun
-from langchain.pydantic_v1 import Field
+from langchain.retrievers import MultiVectorRetriever
 from langchain.schema.document import Document
-from langchain.schema.retriever import BaseRetriever
-from langchain.schema.storage import BaseStore
 from langchain.text_splitter import TextSplitter
-from langchain.vectorstores.base import VectorStore
-from langchain.chains.llm import LLMChain
-from langchain.schema.runnable import Runnable
 
 
-class BaseParentDocumentRetriever(BaseRetriever):
-    """Retrieve from a set of multiple embeddings for the same document."""
-    vectorstore: VectorStore
-    """The underlying vectorstore to use to store small chunks
-    and their embedding vectors"""
-    docstore: BaseStore[str, Document]
-    """The storage layer for the parent documents"""
-    id_key: str = "doc_id"
-    search_kwargs: dict = Field(default_factory=dict)
-    """Keyword arguments to pass to the search function."""
-
-    def _get_relevant_documents(
-        self, query: str, *, run_manager: CallbackManagerForRetrieverRun
-    ) -> List[Document]:
-        """Get documents relevant to a query.
-        Args:
-            query: String to find relevant documents for
-            run_manager: The callbacks handler to use
-        Returns:
-            List of relevant documents
-        """
-        sub_docs = self.vectorstore.similarity_search(query, **self.search_kwargs)
-        # We do this to maintain the order of the ids that are returned
-        ids = []
-        for d in sub_docs:
-            if d.metadata[self.id_key] not in ids:
-                ids.append(d.metadata[self.id_key])
-        docs = self.docstore.mget(ids)
-        return [d for d in docs if d is not None]
-
-
-class ParentDocumentRetriever(BaseParentDocumentRetriever):
+class ParentDocumentRetriever(MultiVectorRetriever):
     """Retrieve small chunks then retrieve their parent documents.
 
     When splitting documents for retrieval, there are often conflicting desires:
@@ -91,7 +54,6 @@ class ParentDocumentRetriever(BaseParentDocumentRetriever):
                 parent_splitter=parent_splitter,
             )
     """
-
 
     child_splitter: TextSplitter
     """The text splitter to use to create child documents."""
@@ -143,86 +105,6 @@ class ParentDocumentRetriever(BaseParentDocumentRetriever):
         for i, doc in enumerate(documents):
             _id = doc_ids[i]
             sub_docs = self.child_splitter.split_documents([doc])
-            for _doc in sub_docs:
-                _doc.metadata[self.id_key] = _id
-            docs.extend(sub_docs)
-            full_docs.append((_id, doc))
-        self.vectorstore.add_documents(docs)
-        if add_to_docstore:
-            self.docstore.mset(full_docs)
-
-
-class MultiVectorRetriever(BaseParentDocumentRetriever):
-    operators: List[Union[TextSplitter, Callable, LLMChain, Runnable]]
-
-    def _get_sub_docs(self, doc: Document) -> List[Document]:
-        """Create a bunch of sub documents for one parent document."""
-        sub_docs = []
-        for op in self.operators:
-            if isinstance(op, TextSplitter):
-                sub_docs.extend(op.split_documents([doc]))
-            elif isinstance(op, Callable):
-                docs = op(doc)
-                sub_docs.extend(docs)
-            else:
-                if isinstance(op, LLMChain):
-                    result = op.run(doc)
-                elif isinstance(op, Runnable):
-                    result = op.invoke(doc)
-                else:
-                    raise ValueError(
-                        f"Got unexpected type of operator: {op}"
-                    )
-                if isinstance(result, str):
-                    sub_docs.append(Document(page_content=result))
-                elif isinstance(result, list):
-                    sub_docs.extend([Document(page_content=r) for r in result])
-                else:
-                    raise ValueError(
-                        f"LLMChain returned unexpected result "
-                        f"- got type {type(result)}, expected string or list"
-                    )
-        return sub_docs
-
-    def add_documents(
-        self,
-        documents: List[Document],
-        ids: Optional[List[str]] = None,
-        add_to_docstore: bool = True,
-    ) -> None:
-        """Adds documents to the docstore and vectorstores.
-
-        Args:
-            documents: List of documents to add
-            ids: Optional list of ids for documents. If provided should be the same
-                length as the list of documents. Can provided if parent documents
-                are already in the document store and you don't want to re-add
-                to the docstore. If not provided, random UUIDs will be used as
-                ids.
-            add_to_docstore: Boolean of whether to add documents to docstore.
-                This can be false if and only if `ids` are provided. You may want
-                to set this to False if the documents are already in the docstore
-                and you don't want to re-add them.
-        """
-        if ids is None:
-            doc_ids = [str(uuid.uuid4()) for _ in documents]
-            if not add_to_docstore:
-                raise ValueError(
-                    "If ids are not passed in, `add_to_docstore` MUST be True"
-                )
-        else:
-            if len(documents) != len(ids):
-                raise ValueError(
-                    "Got uneven list of documents and ids. "
-                    "If `ids` is provided, should be same length as `documents`."
-                )
-            doc_ids = ids
-
-        docs = []
-        full_docs = []
-        for i, doc in enumerate(documents):
-            _id = doc_ids[i]
-            sub_docs = self._get_sub_docs(doc)
             for _doc in sub_docs:
                 _doc.metadata[self.id_key] = _id
             docs.extend(sub_docs)
