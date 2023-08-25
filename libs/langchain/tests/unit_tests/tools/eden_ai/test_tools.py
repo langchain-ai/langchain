ba9e4b6c64a986e809ca1aad7b1ecdcd01024976
RedhaWassim <rwasssim@gmail.com>
2023-08-25 12:14:55 +0200

    fix  : unit test mock response

--- libs/langchain/tests/unit_tests/tools/eden_ai/test_tools.py
+++ libs/langchain/tests/unit_tests/tools/eden_ai/test_tools.py
@@ -18,8 +18,20 @@ def test_provider_not_available(mock_post: MagicMock) -> None:
     mock_response = MagicMock()
     mock_response.status_code = 200
     mock_response.json.return_value = [
-        {"status": "fail", "openai": {"error": {"message": "Provider not available"}}}
-    ]
+                {
+                  "error": {
+                    "message": """Amazon has returned an error: An error occurred (TextSizeLimitExceededException) 
+                    when calling the DetectTargetedSentiment operation: Input text size exceeds limit. 
+                    Max length of request text allowed is 5000 bytes while in this request the text size is 47380 bytes""",
+                    "type": "ProviderInvalidInputTextLengthError"
+                    
+                  },
+                  "status": "fail",
+                  "provider": "amazon",
+                  "provider_status_code": 400,
+                  "cost": 0.0
+                }
+]           
     mock_post.return_value = mock_response
 
     with pytest.raises(ValueError):
@@ -30,8 +42,10 @@ def test_unexpected_response(mock_post: MagicMock) -> None:
     mock_response = MagicMock()
     mock_response.status_code = 200
     mock_response.json.return_value = [
-        {"status": "success", "openai": {"data": "unexpected data"}}
-    ]
+                {
+                "status": "success",
+                }
+            ]
     mock_post.return_value = mock_response
     with pytest.raises(RuntimeError):
         tool._run("some query")
@@ -41,15 +55,14 @@ def test_parsing_response(mock_post: MagicMock) -> None:
     mock_response = MagicMock()
     mock_response.status_code = 200
     mock_response.json.return_value = [
-        {
-            "status": "success",
-            "openai": {
-                "nsfw_likelihood": 3,
-                "label": "az",
-                "likelihood": [4, 5],
-            },
-        }
-    ]
+                {
+                "status": "success",
+                "provider": "microsoft",
+                "nsfw_likelihood": 5,
+                "cost": 0.001,
+                "label": ["sexually explicit","sexually suggestive","offensive"],
+                }
+            ]
 
     mock_post.return_value = mock_response
     with pytest.raises(RuntimeError):
