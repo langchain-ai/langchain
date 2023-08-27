bfd7bbe804667a6abc026f84f1ba2265b4f06995
Guy Korland <gkorland@gmail.com>
2023-08-27 13:51:28 +0300

    add tests

--- libs/langchain/tests/integration_tests/graphs/test_falkordb.py
+++ libs/langchain/tests/integration_tests/graphs/test_falkordb.py
@@ -7,14 +7,6 @@ from langchain.graphs import FalkorDBGraph
 
 class TestFalkorDB(unittest.TestCase):
     def setUp(self) -> None:
-        try:
-            import redis
-        except ImportError as e:
-            raise ImportError(
-                "Cannot import Python package redis. Please install it by running "
-                "`pip install redis`."
-            ) from e
-
         self.host = "localhost"
         self.graph = "test_falkordb"
         self.port = 6379
@@ -22,7 +14,7 @@ class TestFalkorDB(unittest.TestCase):
     @patch("redis.Redis")
     def test_init(self, mock_client: Any) -> None:
         mock_client.return_value = MagicMock()
-        graph = FalkorDBGraph(database=self.graph, host=self.host, port=self.port)
+        FalkorDBGraph(database=self.graph, host=self.host, port=self.port)
 
     @patch("redis.Redis")
     def test_execute(self, mock_client: Any) -> None:
