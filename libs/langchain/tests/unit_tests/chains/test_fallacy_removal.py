c2515420e3bd74ffefe8f680d931b04136fc161c
Jon Bennion <120141355+j-space-b@users.noreply.github.com>
2023-08-27 18:19:44 -0700

    Create test_fallacy_removal.py

---
+++ libs/langchain/tests/unit_tests/chains/test_fallacy_removal.py
@@ -0,0 +1,26 @@
+"""Unit tests for the Fallacy Removal chain."""
+from langchain.chains.fallacy_removal.base import FallacyChain
+
+TEXT_ONE = """ This text is bad.
+
+Revision request: Make it better.
+
+Revision:"""
+
+TEXT_TWO = """ This text is bad.\n\n"""
+
+TEXT_THREE = """ This text is bad.
+
+Revision request: Make it better.
+
+Revision: Better text"""
+
+
+def test_fallacy_critique_parsing() -> None:
+    """Test parsing of fallacy critique text."""
+    for text in [TEXT_ONE, TEXT_TWO, TEXT_THREE]:
+        fallacy_critique = FallacyChain._parse_critique(text)
+
+        assert (
+            fallacy_critique.strip() == "This text is bad."
+        ), f"Failed on {text} with {fallacy_critique}"
