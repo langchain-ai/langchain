79066a736ab1bfed4b194df4977582f4b46b730a
Fangrui Liu <fangrui.liu@outlook.com>
2023-08-28 20:19:34 +0800

    update unittest

--- libs/langchain/tests/unit_tests/retrievers/self_query/test_myscale.py
+++ libs/langchain/tests/unit_tests/retrievers/self_query/test_myscale.py
@@ -9,9 +9,28 @@ from langchain.chains.query_constructor.ir import (
     Operator,
     StructuredQuery,
 )
+from langchain.chains.query_constructor.schema import VirtualColumnName
 from langchain.retrievers.self_query.myscale import MyScaleTranslator
 
 DEFAULT_TRANSLATOR = MyScaleTranslator()
+
+
+@pytest.mark.parametrize(
+    "triplet",
+    [
+        (Comparator.LT, 2, "foo < 2"),
+        (Comparator.LTE, 2, "foo <= 2"),
+        (Comparator.GT, 2, "foo > 2"),
+        (Comparator.GTE, 2, "foo >= 2"),
+        (Comparator.CONTAIN, 2, "has(foo,2)"),
+        (Comparator.LIKE, "bar", "foo ILIKE '%bar%'"),
+    ],
+)
+def test_visit_comparison(triplet: Tuple[Comparator, Any, str]) -> None:
+    comparator, value, expected = triplet
+    comp = Comparison(comparator=comparator, attribute="foo", value=value)
+    actual = DEFAULT_TRANSLATOR.visit_comparison(comp)
+    assert expected == actual
 
 
 @pytest.mark.parametrize(
@@ -25,9 +44,39 @@ DEFAULT_TRANSLATOR = MyScaleTranslator()
         (Comparator.LIKE, "bar", "metadata.foo ILIKE '%bar%'"),
     ],
 )
-def test_visit_comparison(triplet: Tuple[Comparator, Any, str]) -> None:
+def test_visit_comparison_with_virt_col_name(
+    triplet: Tuple[Comparator, Any, str]
+) -> None:
     comparator, value, expected = triplet
-    comp = Comparison(comparator=comparator, attribute="foo", value=value)
+    comp = Comparison(
+        comparator=comparator,
+        attribute=VirtualColumnName(name="foo", column="metadata.foo"),
+        value=value,
+    )
+    actual = DEFAULT_TRANSLATOR.visit_comparison(comp)
+    assert expected == actual
+
+
+@pytest.mark.parametrize(
+    "triplet",
+    [
+        (Comparator.LT, 2, "metadata.foo < 2"),
+        (Comparator.LTE, 2, "metadata.foo <= 2"),
+        (Comparator.GT, 2, "metadata.foo > 2"),
+        (Comparator.GTE, 2, "metadata.foo >= 2"),
+        (Comparator.CONTAIN, 2, "has(metadata.foo,2)"),
+        (Comparator.LIKE, "bar", "metadata.foo ILIKE '%bar%'"),
+    ],
+)
+def test_visit_comparison_with_virt_col_name_func(
+    triplet: Tuple[Comparator, Any, str]
+) -> None:
+    comparator, value, expected = triplet
+    comp = Comparison(
+        comparator=comparator,
+        attribute=VirtualColumnName(name="foo", func=lambda x: f"metadata.{x}"),
+        value=value,
+    )
     actual = DEFAULT_TRANSLATOR.visit_comparison(comp)
     assert expected == actual
 
@@ -40,7 +89,7 @@ def test_visit_operation() -> None:
             Comparison(comparator=Comparator.EQ, attribute="bar", value="baz"),
         ],
     )
-    expected = "metadata.foo < 2 AND metadata.bar = 'baz'"
+    expected = "foo < 2 AND bar = 'baz'"
     actual = DEFAULT_TRANSLATOR.visit_operation(op)
     assert expected == actual
 
@@ -50,6 +99,7 @@ def test_visit_structured_query() -> None:
     structured_query = StructuredQuery(
         query=query,
         filter=None,
+        limit=4,
     )
     expected: Tuple[str, Dict] = (query, {})
     actual = DEFAULT_TRANSLATOR.visit_structured_query(structured_query)
@@ -59,10 +109,11 @@ def test_visit_structured_query() -> None:
     structured_query = StructuredQuery(
         query=query,
         filter=comp,
+        limit=4,
     )
     expected = (
         query,
-        {"where_str": "metadata.foo < ['1', '2']"},
+        {"where_str": "foo < ['1', '2']"},
     )
     actual = DEFAULT_TRANSLATOR.visit_structured_query(structured_query)
     assert expected == actual
@@ -77,6 +128,34 @@ def test_visit_structured_query() -> None:
     structured_query = StructuredQuery(
         query=query,
         filter=op,
+        limit=4,
+    )
+    expected = (
+        query,
+        {"where_str": "foo < 2 AND bar = 'baz'"},
+    )
+    actual = DEFAULT_TRANSLATOR.visit_structured_query(structured_query)
+    assert expected == actual
+
+    op = Operation(
+        operator=Operator.AND,
+        arguments=[
+            Comparison(
+                comparator=Comparator.LT,
+                attribute=VirtualColumnName(name="foo", column="metadata.foo"),
+                value=2,
+            ),
+            Comparison(
+                comparator=Comparator.EQ,
+                attribute=VirtualColumnName(name="bar", column="metadata.bar"),
+                value="baz",
+            ),
+        ],
+    )
+    structured_query = StructuredQuery(
+        query=query,
+        filter=op,
+        limit=4,
     )
     expected = (
         query,
