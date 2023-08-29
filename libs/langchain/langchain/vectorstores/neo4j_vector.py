9daa63fc28abed66f676f4049fcfec63e5d34827
Tomaz Bratanic <bratanic.tomaz@gmail.com>
2023-08-29 09:57:29 +0200

    I suck at type hinting

--- libs/langchain/langchain/vectorstores/neo4j_vector.py
+++ libs/langchain/langchain/vectorstores/neo4j_vector.py
@@ -34,8 +34,8 @@ def check_if_not_null(props: List[str], values: List[Any]) -> None:
 
 
 def sort_by_index_name(
-    lst: List[Dict[str, str]], index_name: str
-) -> List[Dict[str, str]]:
+    lst: List[Dict[str, Any]], index_name: str
+) -> List[Dict[str, Any]]:
     """Sort first element to match the index_name if exists"""
     return sorted(lst, key=lambda x: x.get("index_name") != index_name)
 
