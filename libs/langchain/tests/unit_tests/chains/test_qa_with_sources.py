dd10cf945c9d2d823fee1b77665252d03eb665b9
Nikhil Suresh <nikhilsuresh097@gmail.com>
2023-08-29 14:15:59 +0000

    fixed minor linting issues

--- libs/langchain/tests/unit_tests/chains/test_qa_with_sources.py
+++ libs/langchain/tests/unit_tests/chains/test_qa_with_sources.py
@@ -28,7 +28,8 @@ from tests.unit_tests.llms.fake_llm import FakeLLM
             "28-pl",
         ),
         (
-            "According to the sources, the agreement is governed by English law.\nSource: 28-pl",
+            "According to the sources, the agreement is governed by English law.\n"
+            "Source: 28-pl",
             "According to the sources, the agreement is governed by English law.\n",
             "28-pl",
         ),
