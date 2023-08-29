01de4be338c0751c0422426193432808ad097686
Donatas Remeika <donatas.remeika@gmail.com>
2023-08-29 08:50:47 +0300

    SearchApi integration

---
+++ libs/langchain/langchain/tools/searchapi/__init__.py
@@ -0,0 +1,6 @@
+from langchain.tools.searchapi.tool import SearchAPIResults, SearchAPIRun
+
+"""SearchApi.io API Toolkit."""
+"""Tool for the SearchApi.io Google SERP API."""
+
+__all__ = ["SearchAPIResults", "SearchAPIRun"]
