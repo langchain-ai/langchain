f76aa5ff15e6bb2f67c79c2b025c3b35b3769735
maks-operlejn-ds <maksymilian.operlejn@deepsense.ai>
2023-08-30 14:59:50 +0000

    CR

--- libs/experimental/langchain_experimental/data_anonymizer/presidio.py
+++ libs/experimental/langchain_experimental/data_anonymizer/presidio.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 
+from collections import defaultdict
 from typing import TYPE_CHECKING, Dict, List, Optional
-
-from presidio_analyzer import RecognizerResult
 
 from langchain_experimental.data_anonymizer.base import (
     AnonymizerBase,
@@ -12,14 +11,30 @@ from langchain_experimental.data_anonymizer.faker_presidio_mapping import (
     get_pseudoanonymizer_mapping,
 )
 
+try:
+    from presidio_analyzer import AnalyzerEngine
+except ImportError as e:
+    raise ImportError(
+        "Could not import presidio_analyzer, please install with "
+        "`pip install presidio-analyzer`. You will also need to download a "
+        "spaCy model to use the analyzer, e.g. "
+        "`python -m spacy download en_core_web_lg`."
+    ) from e
+try:
+    from presidio_anonymizer import AnonymizerEngine
+    from presidio_anonymizer.entities import OperatorConfig
+except ImportError as e:
+    raise ImportError(
+        "Could not import presidio_anonymizer, please install with "
+        "`pip install presidio-anonymizer`."
+    ) from e
+
 if TYPE_CHECKING:
-    from presidio_analyzer import EntityRecognizer
-    from presidio_anonymizer.entities import EngineResult, OperatorConfig
+    from presidio_analyzer import EntityRecognizer, RecognizerResult
+    from presidio_anonymizer.entities import EngineResult
 
 
-class PresidioAnonymizer(AnonymizerBase):
-    """Anonymizer using Microsoft Presidio."""
-
+class PresidioAnonymizerBase(AnonymizerBase):
     def __init__(
         self,
         analyzed_fields: Optional[List[str]] = None,
@@ -36,24 +51,6 @@ class PresidioAnonymizer(AnonymizerBase):
                 Learn more:
                 https://microsoft.github.io/presidio/tutorial/10_simple_anonymization/
         """
-        try:
-            from presidio_analyzer import AnalyzerEngine
-        except ImportError as e:
-            raise ImportError(
-                "Could not import presidio_analyzer, please install with "
-                "`pip install presidio-analyzer`. You will also need to download a "
-                "spaCy model to use the analyzer, e.g. "
-                "`python -m spacy download en_core_web_lg`."
-            ) from e
-        try:
-            from presidio_anonymizer import AnonymizerEngine
-            from presidio_anonymizer.entities import OperatorConfig
-        except ImportError as e:
-            raise ImportError(
-                "Could not import presidio_anonymizer, please install with "
-                "`pip install presidio-anonymizer`."
-            ) from e
-
         self.analyzed_fields = (
             analyzed_fields
             if analyzed_fields is not None
@@ -73,7 +70,22 @@ class PresidioAnonymizer(AnonymizerBase):
         self._analyzer = AnalyzerEngine()
         self._anonymizer = AnonymizerEngine()
 
+    def add_recognizer(self, recognizer: EntityRecognizer) -> None:
+        """Add a recognizer to the analyzer"""
+        self._analyzer.registry.add_recognizer(recognizer)
+        self.analyzed_fields.extend(recognizer.supported_entities)
+
+    def add_operators(self, operators: Dict[str, OperatorConfig]) -> None:
+        """Add operators to the anonymizer"""
+        self.operators.update(operators)
+
+
+class PresidioAnonymizer(PresidioAnonymizerBase):
     def _anonymize(self, text: str) -> str:
+        """Anonymize text.
+        Each PII entity is replaced with a fake value.
+        Each time fake values will be different, as they are generated randomly.
+        """
         results = self._analyzer.analyze(
             text,
             entities=self.analyzed_fields,
@@ -86,72 +98,23 @@ class PresidioAnonymizer(AnonymizerBase):
             operators=self.operators,
         ).text
 
-    def add_recognizer(self, recognizer: EntityRecognizer) -> None:
-        """Add a recognizer to the analyzer"""
-        self._analyzer.registry.add_recognizer(recognizer)
-        self.analyzed_fields.extend(recognizer.supported_entities)
 
-    def add_operators(self, operators: Dict[str, OperatorConfig]) -> None:
-        """Add operators to the anonymizer"""
-        self.operators.update(operators)
-
-
-class PresidioReversibleAnonymizer(ReversibleAnonymizerBase):
-    """Reversible Anonymizer using Microsoft Presidio."""
-
+class PresidioReversibleAnonymizer(PresidioAnonymizerBase, ReversibleAnonymizerBase):
     def __init__(
         self,
         analyzed_fields: Optional[List[str]] = None,
         language: str = "en",
         operators: Optional[Dict[str, OperatorConfig]] = None,
     ):
-        """
-        Args:
-            analyzed_fields: List of fields to detect and then anonymize.
-                Defaults to all entities supported by Microsoft Presidio.
-            language: Language to use for analysis. Defaults to english.
-            operators: Operators to use for anonymization.
-                Operators allow for custom anonymization of detected PII.
-                Learn more:
-                https://microsoft.github.io/presidio/tutorial/10_simple_anonymization/
-        """
-        try:
-            from presidio_analyzer import AnalyzerEngine
-        except ImportError as e:
-            raise ImportError(
-                "Could not import presidio_analyzer, please install with "
-                "`pip install presidio-analyzer`. You will also need to download a "
-                "spaCy model to use the analyzer, e.g. "
-                "`python -m spacy download en_core_web_lg`."
-            ) from e
-        try:
-            from presidio_anonymizer import AnonymizerEngine
-            from presidio_anonymizer.entities import OperatorConfig
-        except ImportError as e:
-            raise ImportError(
-                "Could not import presidio_anonymizer, please install with "
-                "`pip install presidio-anonymizer`."
-            ) from e
+        super().__init__(analyzed_fields, language, operators)
+        self._deanonymizer_mapping: Dict[str, Dict[str, str]] = defaultdict(
+            lambda: defaultdict(str)
+        )
 
-        self.analyzed_fields = (
-            analyzed_fields
-            if analyzed_fields is not None
-            else list(get_pseudoanonymizer_mapping().keys())
-        )
-        self.language = language
-        self.operators = (
-            operators
-            if operators is not None
-            else {
-                field: OperatorConfig(
-                    operator_name="custom", params={"lambda": faker_function}
-                )
-                for field, faker_function in get_pseudoanonymizer_mapping().items()
-            }
-        )
-        self._analyzer = AnalyzerEngine()
-        self._anonymizer = AnonymizerEngine()
-        self.deanonymizer_mapping: Dict[str, Dict[str, str]] = dict()
+    @property
+    def deanonymizer_mapping(self) -> Dict[str, Dict[str, str]]:
+        """Return the deanonymizer mapping"""
+        return {k: dict(v) for k, v in self._deanonymizer_mapping.items()}
 
     def _create_update_deanonymizer_mapping(
         self,
@@ -159,33 +122,60 @@ class PresidioReversibleAnonymizer(ReversibleAnonymizerBase):
         analyzer_results: List[RecognizerResult],
         anonymizer_results: EngineResult,
     ) -> None:
-        """Create a mapping from anonymized text to original text"""
+        """Creates or updates the mapping used to de-anonymize text.
+
+        This method exploits the results returned by the
+        analysis and anonymization processes.
+
+        It constructs a mapping from each anonymized entity
+        back to its original text value.
+
+        Mapping will be stored as "deanonymizer_mapping" property.
+
+        Example of "deanonymizer_mapping":
+        {
+            "PERSON": {
+                "<anonymized>": "<original>",
+                "John Doe": "Slim Shady"
+            },
+            "PHONE_NUMBER": {
+                "111-111-1111": "555-555-5555"
+            }
+            ...
+        }
+        """
+
+        # We are able to zip and loop through both lists because we expect
+        # them to return corresponding entities for each identified piece
+        # of analyzable data from our input.
+
+        # We sort them by their 'start' attribute because it allows us to
+        # match corresponding entities by their position in the input text.
         analyzer_results = sorted(analyzer_results, key=lambda d: d.start)
         anonymizer_results.items = sorted(
             anonymizer_results.items, key=lambda d: d.start
         )
 
-        new_deanonymizer_mapping: Dict[str, Dict[str, str]] = {}
+        new_deanonymizer_mapping: defaultdict[str, Dict[str, str]] = defaultdict(dict)
 
         for analyzed_entity, anonymized_entity in zip(
             analyzer_results, anonymizer_results.items
         ):
             original_value = original_text[analyzed_entity.start : analyzed_entity.end]
-
-            if anonymized_entity.entity_type not in new_deanonymizer_mapping:
-                new_deanonymizer_mapping[anonymized_entity.entity_type] = {}
-
             new_deanonymizer_mapping[anonymized_entity.entity_type][
                 anonymized_entity.text
             ] = original_value
 
         for entity_type, values in new_deanonymizer_mapping.items():
-            if entity_type not in self.deanonymizer_mapping:
-                self.deanonymizer_mapping[entity_type] = {}
-
-            self.deanonymizer_mapping[entity_type].update(values)
+            self._deanonymizer_mapping[entity_type].update(values)
 
     def _anonymize(self, text: str) -> str:
+        """Anonymize text.
+        Each PII entity is replaced with a fake value.
+        Each time fake values will be different, as they are generated randomly.
+        At the same time, we will create a mapping from each anonymized entity
+        back to its original text value.
+        """
         analyzer_results = self._analyzer.analyze(
             text,
             entities=self.analyzed_fields,
@@ -211,16 +201,17 @@ class PresidioReversibleAnonymizer(ReversibleAnonymizerBase):
         return anonymizer_results.text
 
     def _deanonymize(self, text_to_deanonymize: str) -> str:
-        for entity_type in self.deanonymizer_mapping:
-            for anonymized, original in self.deanonymizer_mapping[entity_type].items():
+        """Deanonymize text.
+        Each anonymized entity is replaced with its original value.
+        This method exploits the mapping created during the anonymization process.
+        """
+        if not self._deanonymizer_mapping:
+            raise ValueError(
+                "Deanonymizer mapping is empty.",
+                "Please call anonymize() and anonymize some text first.",
+            )
+
+        for entity_type in self._deanonymizer_mapping:
+            for anonymized, original in self._deanonymizer_mapping[entity_type].items():
                 text_to_deanonymize = text_to_deanonymize.replace(anonymized, original)
         return text_to_deanonymize
-
-    def add_recognizer(self, recognizer: EntityRecognizer) -> None:
-        """Add a recognizer to the analyzer"""
-        self._analyzer.registry.add_recognizer(recognizer)
-        self.analyzed_fields.extend(recognizer.supported_entities)
-
-    def add_operators(self, operators: Dict[str, OperatorConfig]) -> None:
-        """Add operators to the anonymizer"""
-        self.operators.update(operators)
