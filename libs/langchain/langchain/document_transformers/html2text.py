a78a1e790d6115ba017d5cc62f02e25616138e73
taamedag <Davide.Menini@swisscom.com>
2023-08-30 11:25:13 +0200

    fix (Html2TextTransformer): instantiate HTML2Text inside transform_documents

--- libs/langchain/langchain/document_transformers/html2text.py
+++ libs/langchain/langchain/document_transformers/html2text.py
@@ -13,11 +13,19 @@ class Html2TextTransformer(BaseDocumentTransformer):
     Example:
         .. code-block:: python
             from langchain.document_transformers import Html2TextTransformer
-            html2text=Html2TextTransformer()
-            docs_transform=html2text.transform_documents(docs)
+            html2text = Html2TextTransformer()
+            docs_transform = html2text.transform_documents(docs)
     """
 
     def __init__(self, ignore_links: bool = True, ignore_images: bool = True) -> None:
+        self.ignore_links = ignore_links
+        self.ignore_images = ignore_images
+
+    def transform_documents(
+        self,
+        documents: Sequence[Document],
+        **kwargs: Any,
+    ) -> Sequence[Document]:
         try:
             import html2text
         except ImportError:
@@ -28,18 +36,11 @@ class Html2TextTransformer(BaseDocumentTransformer):
 
         # Create a html2text.HTML2Text object and override some properties
         h = html2text.HTML2Text()
-        h.ignore_links = ignore_links
-        h.ignore_images = ignore_images
+        h.ignore_links = self.ignore_links
+        h.ignore_images = self.ignore_images
 
-        self.html2text = h
-
-    def transform_documents(
-        self,
-        documents: Sequence[Document],
-        **kwargs: Any,
-    ) -> Sequence[Document]:
         for d in documents:
-            d.page_content = self.html2text.handle(d.page_content)
+            d.page_content = h.handle(d.page_content)
         return documents
 
     async def atransform_documents(
