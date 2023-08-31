19a84ed661ee287b3f46ab63a518d5a6b642e716
Wrick Talukdar <wrick.talukdar@gmail.com>
2023-08-31 14:03:22 -0600

    added base_moderation_config and added __init__

---
+++ libs/experimental/langchain_experimental/comprehend_moderation/base_moderation_config.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+from typing import List, Optional, Any
+
+class ModerationPiiConfig(BaseModel):
+    """Threshold for PII confidence score, defaults to 0.5 i.e. 50%"""
+    threshold: Optional[float] = 0.5
+    """List of PII Universal Labels. Refer - https://docs.aws.amazon.com/comprehend/latest/dg/how-pii.html#how-pii-types. Defaults to `list[]`"""
+    labels: Optional[List[str]] = []
+    """Whether to perfrom redaction of detected PII entities"""
+    redact: Optional[bool] = False
+    """Redaction mask character in case redact=True, defaults to asterisk (*)"""
+    mask_character: Optional[str] = "*"
+
+class ModerationToxicityConfig(BaseModel):
+    """Threshold for Toxic label confidence score, defaults to 0.5 i.e. 50%"""
+    threshold: Optional[float] = 0.5
+    """List of toxic labels, defaults to `list[]`"""
+    labels: Optional[List[str]] = []
+
+class ModerationIntentConfig(BaseModel):
+    """Threshold for Intent classification confidence score, defaults to 0.5 i.e. 50%"""
+    threshold: Optional[float] = 0.5
+
+class BaseModerationConfig(BaseModel):
+    """Filters applied to the moderation chain, defaults to `[ModerationPiiConfig(), ModerationToxicityConfig(), ModerationIntentConfig()]`"""
+    filters: Optional[List[Any]] = [ModerationPiiConfig(), 
+                                    ModerationToxicityConfig(),
+                                    ModerationIntentConfig()]
