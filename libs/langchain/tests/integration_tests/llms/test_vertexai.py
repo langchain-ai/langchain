30239b302599716bd06bc9f8e68d5b16059f5d15
Leonid Kuligin <lkuligin@yandex.ru>
2023-09-02 00:58:21 +0200

    added support for inference from Model Garden (#9367)
    #8850
    
    ---------
    
    Co-authored-by: Leonid Kuligin <kuligin@google.com>

--- libs/langchain/tests/integration_tests/llms/test_vertexai.py
+++ libs/langchain/tests/integration_tests/llms/test_vertexai.py
@@ -7,7 +7,10 @@ pip install google-cloud-aiplatform>=1.25.0
 Your end-user credentials would be used to make the calls (make sure you've run 
 `gcloud auth login` first).
 """
-from langchain.llms import VertexAI
+import os
+
+from langchain.llms import VertexAI, VertexAIModelGarden
+from langchain.schema import LLMResult
 
 
 def test_vertex_call() -> None:
@@ -16,3 +19,34 @@ def test_vertex_call() -> None:
     assert isinstance(output, str)
     assert llm._llm_type == "vertexai"
     assert llm.model_name == llm.client._model_id
+
+
+def test_model_garden() -> None:
+    """In order to run this test, you should provide an endpoint name.
+
+    Example:
+    export ENDPOINT_ID=...
+    export PROJECT=...
+    """
+    endpoint_id = os.environ["ENDPOINT_ID"]
+    project = os.environ["PROJECT"]
+    llm = VertexAIModelGarden(endpoint_id=endpoint_id, project=project)
+    output = llm("What is the meaning of life?")
+    print(output)
+    assert isinstance(output, str)
+    assert llm._llm_type == "vertexai_model_garden"
+
+
+def test_model_garden_batch() -> None:
+    """In order to run this test, you should provide an endpoint name.
+
+    Example:
+    export ENDPOINT_ID=...
+    export PROJECT=...
+    """
+    endpoint_id = os.environ["ENDPOINT_ID"]
+    project = os.environ["PROJECT"]
+    llm = VertexAIModelGarden(endpoint_id=endpoint_id, project=project)
+    output = llm._generate(["What is the meaning of life?", "How much is 2+2"])
+    assert isinstance(output, LLMResult)
+    assert len(output.generations) == 2
