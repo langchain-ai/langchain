37a433fc8438754a452867f6496983eb7ad41c88
valkryhx <283306449@qq.com>
2023-09-01 09:50:59 +0800

    Update chroma.py

--- libs/langchain/langchain/vectorstores/chroma.py
+++ libs/langchain/langchain/vectorstores/chroma.py
@@ -40,7 +40,9 @@ def _results_to_docs_and_scores(results: Any) -> List[Tuple[Document, float]]:
         # TODO: Chroma can do batch querying,
         # we shouldn't hard code to the 1st result
         # minor fix :metadata may contain distances dict by merge two dicts
-        (Document(page_content=result[0], metadata={**result[1],**{"distances":result[2]} } or {}), result[2])
+        (Document(page_content=result[0], 
+         metadata={**result[1],**{"distances":result[2]} } or {}), 
+         result[2])
         for result in zip(
             results["documents"][0],
             results["metadatas"][0],
