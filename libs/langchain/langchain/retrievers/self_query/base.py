e7c5bcdebce9f0fbbfe19a30af4f6d8f3f82442b
Viktor Zhemchuzhnikov <asai5909@gmail.com>
2023-09-04 17:10:56 +0800

    feat: add async support to self query retrievers

--- libs/langchain/langchain/retrievers/self_query/base.py
+++ libs/langchain/langchain/retrievers/self_query/base.py
@@ -1,9 +1,12 @@
 """Retriever that generates and executes structured queries over its own data source."""
-
-from typing import Any, Dict, List, Optional, Type, cast
+import logging
+from typing import Any, Dict, List, Optional, Tuple, Type, cast
 
 from langchain import LLMChain
-from langchain.callbacks.manager import CallbackManagerForRetrieverRun
+from langchain.callbacks.manager import (
+    AsyncCallbackManagerForRetrieverRun,
+    CallbackManagerForRetrieverRun,
+)
 from langchain.chains.query_constructor.base import load_query_constructor_chain
 from langchain.chains.query_constructor.ir import StructuredQuery, Visitor
 from langchain.chains.query_constructor.schema import AttributeInfo
@@ -31,6 +34,8 @@ from langchain.vectorstores import (
     VectorStore,
     Weaviate,
 )
+
+logger = logging.getLogger(__name__)
 
 
 def _get_builtin_translator(vectorstore: VectorStore) -> Visitor:
@@ -91,6 +96,49 @@ class SelfQueryRetriever(BaseRetriever, BaseModel):
             )
         return values
 
+    def _get_structured_query(
+        self, inputs: Dict[str, Any], run_manager: CallbackManagerForRetrieverRun
+    ) -> StructuredQuery:
+        structured_query = cast(
+            StructuredQuery,
+            self.llm_chain.predict(callbacks=run_manager.get_child(), **inputs),
+        )
+        return structured_query
+
+    async def _aget_structured_query(
+        self, inputs: Dict[str, Any], run_manager: AsyncCallbackManagerForRetrieverRun
+    ) -> StructuredQuery:
+        structured_query = cast(
+            StructuredQuery,
+            await self.llm_chain.apredict(callbacks=run_manager.get_child(), **inputs),
+        )
+        return structured_query
+
+    def _prepare_query(
+        self, query: str, structured_query: StructuredQuery
+    ) -> Tuple[str, Dict[str, Any]]:
+        new_query, new_kwargs = self.structured_query_translator.visit_structured_query(
+            structured_query
+        )
+        if structured_query.limit is not None:
+            new_kwargs["k"] = structured_query.limit
+        if self.use_original_query:
+            new_query = query
+        search_kwargs = {**self.search_kwargs, **new_kwargs}
+        return new_query, search_kwargs
+
+    def _get_docs_with_query(
+        self, query: str, search_kwargs: Dict[str, Any]
+    ) -> List[Document]:
+        docs = self.vectorstore.search(query, self.search_type, **search_kwargs)
+        return docs
+
+    async def _aget_docs_with_query(
+        self, query: str, search_kwargs: Dict[str, Any]
+    ) -> List[Document]:
+        docs = await self.vectorstore.asearch(query, self.search_type, **search_kwargs)
+        return docs
+
     def _get_relevant_documents(
         self, query: str, *, run_manager: CallbackManagerForRetrieverRun
     ) -> List[Document]:
@@ -103,25 +151,30 @@ class SelfQueryRetriever(BaseRetriever, BaseModel):
             List of relevant documents
         """
         inputs = self.llm_chain.prep_inputs({"query": query})
-        structured_query = cast(
-            StructuredQuery,
-            self.llm_chain.predict_and_parse(
-                callbacks=run_manager.get_child(), **inputs
-            ),
-        )
+        structured_query = self._get_structured_query(inputs, run_manager)
         if self.verbose:
-            print(structured_query)
-        new_query, new_kwargs = self.structured_query_translator.visit_structured_query(
-            structured_query
-        )
-        if structured_query.limit is not None:
-            new_kwargs["k"] = structured_query.limit
+            logger.info(f"Generated Query: {structured_query}")
+        new_query, search_kwargs = self._prepare_query(query, structured_query)
+        docs = self._get_docs_with_query(new_query, search_kwargs)
+        return docs
 
-        if self.use_original_query:
-            new_query = query
+    async def _aget_relevant_documents(
+        self, query: str, *, run_manager: AsyncCallbackManagerForRetrieverRun
+    ) -> List[Document]:
+        """Get documents relevant for a query.
 
-        search_kwargs = {**self.search_kwargs, **new_kwargs}
-        docs = self.vectorstore.search(new_query, self.search_type, **search_kwargs)
+        Args:
+            query: string to find relevant documents for
+
+        Returns:
+            List of relevant documents
+        """
+        inputs = self.llm_chain.prep_inputs({"query": query})
+        structured_query = await self._aget_structured_query(inputs, run_manager)
+        if self.verbose:
+            logger.info(f"Generated Query: {structured_query}")
+        new_query, search_kwargs = self._prepare_query(query, structured_query)
+        docs = await self._aget_docs_with_query(new_query, search_kwargs)
         return docs
 
     @classmethod
