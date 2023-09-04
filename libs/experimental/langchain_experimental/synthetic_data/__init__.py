ea22643470e78600fbb06b93c35f20c9ab9c5311
mateusz.wosinski <mateusz.wosinski@deepsense.ai>
2023-09-04 09:08:56 +0200

    Moved data generation to experimental

--- libs/experimental/langchain_experimental/synthetic_data/__init__.py
+++ libs/experimental/langchain_experimental/synthetic_data/__init__.py
@@ -1,7 +1,7 @@
 from typing import TYPE_CHECKING, Any, Dict, List, Optional
 
 from langchain.chains.llm import LLMChain
-from langchain.chains.synthetic_data.prompts import SENTENCE_PROMPT
+from langchain_experimental.synthetic_data.prompts import SENTENCE_PROMPT
 
 if TYPE_CHECKING:
     from langchain.chains.base import Chain
