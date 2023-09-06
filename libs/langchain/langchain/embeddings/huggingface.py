7669253b016bded6138147778bfcad54c14f8e8c
unknown <20CS021@charusat.edu.in>
2023-09-06 15:47:11 +0530

    Added Hugging face inference api to embed documents without locally downloading the model

--- libs/langchain/langchain/embeddings/huggingface.py
+++ libs/langchain/langchain/embeddings/huggingface.py
@@ -266,3 +266,62 @@ class HuggingFaceBgeEmbeddings(BaseModel, Embeddings):
             self.query_instruction + text, **self.encode_kwargs
         )
         return embedding.tolist()
+
+
+class  HuggingFaceInferenceAPI(BaseModel, Embeddings):
+    """
+    This class is used to get embeddings for a list of texts using the HuggingFace API.
+    It requires an API key and a model name. The default model name is "sentence-transformers/all-MiniLM-L6-v2".
+    """
+
+    def __init__(
+        self, api_key: str, model_name: str = "sentence-transformers/all-MiniLM-L6-v2"
+    ):
+        """
+        Initialize the HuggingFaceEmbeddingFunction.
+
+        Args:
+            api_key (str): Your API key for the HuggingFace API.
+            model_name (str, optional): The name of the model to use for text embeddings. Defaults to "sentence-transformers/all-MiniLM-L6-v2".
+        """
+        try:
+            import requests
+        except ImportError:
+            raise ValueError(
+                "The requests python package is not installed. Please install it with `pip install requests`"
+            )
+        self._api_url = f"https://api-inference.huggingface.co/pipeline/feature-extraction/{model_name}"
+        self._session = requests.Session()
+        self._session.headers.update({"Authorization": f"Bearer {api_key}"})
+
+    def embed_documents(self, texts: List[str]) -> List[List[float]]:
+        """
+        Get the embeddings for a list of texts.
+
+        Args:
+            texts (Documents): A list of texts to get embeddings for.
+
+        Returns:
+            List[List[float]]
+
+        Example:
+            >>> hugging_face = HuggingFaceEmbeddingFunction(api_key="your_api_key")
+            >>> texts = ["Hello, world!", "How are you?"]
+            >>> embeddings = hugging_face(texts)
+        """
+        # Call HuggingFace Embedding API for each document
+        return self._session.post(  # type: ignore
+            self._api_url, json={"inputs": texts, "options": {"wait_for_model": True, "use_cache": True}}
+        ).json()
+    
+    def embed_query(self, text: str) -> List[float]:
+        """Compute query embeddings using a HuggingFace transformer model.
+
+        Args:
+            text: The text to embed.
+
+        Returns:
+            Embeddings for the text.
+        """
+        return self.embed_documents([text])[0]
+        
