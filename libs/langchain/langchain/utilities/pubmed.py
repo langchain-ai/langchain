3a56a73118c7b2e8dac8c6201bca90b6d5512d9f
Florian <133657389+FlorianH5@users.noreply.github.com>
2023-09-07 16:07:57 +0200

    Extended parsing to abstracts from books
    Without these changes, the _parse_article method throws a KeyError when a book is given as input.

--- libs/langchain/langchain/utilities/pubmed.py
+++ libs/langchain/langchain/utilities/pubmed.py
@@ -158,16 +158,27 @@ class PubMedAPIWrapper(BaseModel):
         return self._parse_article(uid, text_dict)
 
     def _parse_article(self, uid: str, text_dict: dict) -> dict:
-        ar = text_dict["PubmedArticleSet"]["PubmedArticle"]["MedlineCitation"][
-            "Article"
-        ]
+        try:
+            ar = text_dict["PubmedArticleSet"]["PubmedArticle"]["MedlineCitation"][
+                "Article"
+            ]
+        except KeyError:
+            ar = text_dict["PubmedArticleSet"]["PubmedBookArticle"]["BookDocument"]
         abstract_text = ar.get("Abstract", {}).get("AbstractText", [])
         summaries = [
             f"{txt['@Label']}: {txt['#text']}"
             for txt in abstract_text
             if "#text" in txt and "@Label" in txt
         ]
-        summary = "\n".join(summaries) if summaries else abstract_text
+        summary = (
+            "\n".join(summaries)
+            if summaries
+            else (
+                abstract_text
+                if isinstance(abstract_text, str)
+                else ("\n".join(str(value) for value in abstract_text.values()))
+            )
+        )
         a_d = ar.get("ArticleDate", {})
         pub_date = "-".join(
             [a_d.get("Year", ""), a_d.get("Month", ""), a_d.get("Day", "")]
