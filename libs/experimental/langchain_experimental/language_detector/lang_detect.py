b9462bb059f091a2d9339be35efeca19c090944f
mateusz.wosinski <mateusz.wosinski@deepsense.ai>
2023-09-08 14:21:20 +0200

    Fix tests and linters

--- libs/experimental/langchain_experimental/language_detector/lang_detect.py
+++ libs/experimental/langchain_experimental/language_detector/lang_detect.py
@@ -5,14 +5,14 @@ from langchain_experimental.language_detector.base import LanguageDetectorBase
 try:
     import langdetect
 except ImportError:
-    raise ImportError("Could not import langdetect, please install with "
-        "`pip install langdetect`."
+    raise ImportError(
+        "Could not import langdetect, please install with " "`pip install langdetect`."
     )
 
 
 if TYPE_CHECKING:
     import langdetect
-    
+
 
 class LangDetector(LanguageDetectorBase):
     """
@@ -20,13 +20,16 @@ class LangDetector(LanguageDetectorBase):
     Warning: Results are not deterministic!
     It supports 55 languages out of the box.
     """
+
     def __init__(self, threshold: float = 0.1):
-        self.threshold = threshold  # Minimum probability to consider a language as detected.
-    
+        self.threshold = (
+            threshold  # Minimum probability to consider a language as detected.
+        )
+
     def _detect_single(self, text: str) -> str:
         """Detects the most probable language of a single text."""
         return langdetect.detect(text)
-    
+
     def _detect_many(self, text: str) -> List[Tuple[str, float]]:
         """Detects all languages of a single text with a score bigger than threshold.
         Returns them sorted on score in descending order.
