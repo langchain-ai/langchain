5e3b37157a96d65c0ebab13f2f33a93d1255484d
mateusz.wosinski <mateusz.wosinski@deepsense.ai>
2023-09-08 13:49:37 +0200

    Initial commit for language detection

---
+++ libs/experimental/langchain_experimental/language_detector/base.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+from typing import List, Tuple
+
+class LanguageDetectorBase(ABC):
+    """
+    Base abstract class for language detectors.
+    """
+    
+    def detect_single_language(self, text: str) -> str:
+        """Detect language of a single text"""
+        return self._detect_single(text)
+    
+    @abstractmethod
+    def _detect_single(self, text: str) -> str:
+        """Abstract method to detect language of a single text"""
+
+    def detect_many_languages(self, text: str) -> List[Tuple[str, float]]:
+        """Detect languages of a single text"""
+        return self._detect_many(text)
+    
+    @abstractmethod
+    def _detect_many(self, text: str) -> List[Tuple[str, float]]:
+        """Abstract method to detect languages of a single text"""
---
+++ libs/experimental/langchain_experimental/language_detector/lang_detect.py
@@ -0,0 +1,35 @@
+from typing import TYPE_CHECKING, List, Tuple
+
+from langchain_experimental.language_detector.base import LanguageDetectorBase
+
+try:
+    import langdetect
+except ImportError:
+    raise ImportError("Could not import langdetect, please install with "
+        "`pip install langdetect`."
+    )
+
+
+if TYPE_CHECKING:
+    import langdetect
+    
+
+class LangDetector(LanguageDetectorBase):
+    """
+    Language detector based on langdetect package.
+    Warning: Results are not deterministic!
+    It supports 55 languages out of the box.
+    """
+    def __init__(self, threshold: float = 0.1):
+        self.threshold = threshold  # Minimum probability to consider a language as detected.
+    
+    def _detect_single(self, text: str) -> str:
+        """Detects the most probable language of a single text."""
+        return langdetect.detect(text)
+    
+    def _detect_many(self, text: str) -> List[Tuple[str, float]]:
+        """Detects all languages of a single text with a score bigger than threshold.
+        Returns them sorted on score in descending order.
+        """
+        predictions = langdetect.detect_langs(text)
+        return [(x.lang, x.prob) for x in predictions if x.prob > self.threshold]
