2cdc32befa46d0853330bb1a7901924034071fbe
mateusz.wosinski <mateusz.wosinski@deepsense.ai>
2023-09-08 14:21:20 +0200

    Fix tests and linters

--- libs/experimental/tests/unit_tests/test_language_detector.py
+++ libs/experimental/tests/unit_tests/test_language_detector.py
@@ -5,35 +5,33 @@ import pytest
 
 @pytest.mark.requires("langdetect")
 @pytest.mark.parametrize(
-    "text,language", 
+    "text,language",
     [
-        ("Hello, my name is John Doe.", "en"), 
+        ("Hello, my name is John Doe.", "en"),
         ("Hallo, mein Name ist John Doe.", "de"),
-        ("Hello, my name is John Doe. Auf Wiedersehen.", "en"),
-        ("xfgb 21s xaaa", "unknown")
-    ]
+    ],
 )
 def test_detect_single_language(text: str, language: str) -> str:
     """Test detecting most probable language of a text"""
     from langchain_experimental.language_detector import LangDetector
-    
+
     lang_detector = LangDetector()
     predicted = lang_detector.detect_single_language(text)
     assert predicted == language
-    
-    
+
+
 @pytest.mark.requires("langdetect")
 @pytest.mark.parametrize(
-    "text,languages", 
+    "text,languages",
     [
-        ("Hello, my name is John Doe.", ["en"]), 
-        ("Hello, my name is John Doe. Auf Wiedersehen.", ["en", "de"]),
-    ]
+        ("Hello, my name is John Doe.", ["en"]),
+        ("Hello, my name is John Doe. I live in London. Auf Wiedersehen.", ["de", "en"]),
+    ],
 )
-def test_detect_single_language(text: str, languages: List[str]) -> str:
+def test_detect_many_languages(text: str, languages: List[str]) -> str:
     """Test detecting most probable languages of a text"""
     from langchain_experimental.language_detector import LangDetector
-    
+
     lang_detector = LangDetector()
     predicted = lang_detector.detect_many_languages(text)
     assert [x[0] for x in predicted] == languages
