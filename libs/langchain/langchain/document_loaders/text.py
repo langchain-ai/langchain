2e1d32323d57b6556c34924841c5a50d38ddfa13
OinkinB <123262276+OinkinB@users.noreply.github.com>
2023-09-09 17:58:03 -0400

    Update text.py
    Changed textLoader to be a string loader instead of document

--- libs/langchain/langchain/document_loaders/text.py
+++ libs/langchain/langchain/document_loaders/text.py
@@ -3,57 +3,21 @@ from typing import List, Optional
 
 from langchain.docstore.document import Document
 from langchain.document_loaders.base import BaseLoader
-from langchain.document_loaders.helpers import detect_file_encodings
 
 logger = logging.getLogger(__name__)
 
-
 class TextLoader(BaseLoader):
-    """Load text file.
-
+    """Load text content from a string.
 
     Args:
-        file_path: Path to the file to load.
-
-        encoding: File encoding to use. If `None`, the file will be loaded
-        with the default system encoding.
-
-        autodetect_encoding: Whether to try to autodetect the file encoding
-            if the specified encoding fails.
+        text_content (str): The text content to load.
     """
 
-    def __init__(
-        self,
-        file_path: str,
-        encoding: Optional[str] = None,
-        autodetect_encoding: bool = False,
-    ):
-        """Initialize with file path."""
-        self.file_path = file_path
-        self.encoding = encoding
-        self.autodetect_encoding = autodetect_encoding
+    def __init__(self, text_content: str):
+        """Initialize with text content."""
+        self.text_content = text_content
 
     def load(self) -> List[Document]:
-        """Load from file path."""
-        text = ""
-        try:
-            with open(self.file_path, encoding=self.encoding) as f:
-                text = f.read()
-        except UnicodeDecodeError as e:
-            if self.autodetect_encoding:
-                detected_encodings = detect_file_encodings(self.file_path)
-                for encoding in detected_encodings:
-                    logger.debug(f"Trying encoding: {encoding.encoding}")
-                    try:
-                        with open(self.file_path, encoding=encoding.encoding) as f:
-                            text = f.read()
-                        break
-                    except UnicodeDecodeError:
-                        continue
-            else:
-                raise RuntimeError(f"Error loading {self.file_path}") from e
-        except Exception as e:
-            raise RuntimeError(f"Error loading {self.file_path}") from e
-
-        metadata = {"source": self.file_path}
-        return [Document(page_content=text, metadata=metadata)]
+        """Load text content."""
+        metadata = {"source": "text_loader"}
+        return [Document(page_content=self.text_content, metadata=metadata)]
