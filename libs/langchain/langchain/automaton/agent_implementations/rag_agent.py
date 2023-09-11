9933c6c44c09bf7cd3385e3c7d4dfbbef7a4cf65
Eugene Yurtsev <eyurtsev@gmail.com>
2023-09-11 12:50:10 -0400

    x

--- libs/langchain/langchain/automaton/agent_implementations/rag_agent.py
+++ libs/langchain/langchain/automaton/agent_implementations/rag_agent.py
@@ -2,34 +2,22 @@
 from __future__ import annotations
 
 from typing import (
-    Callable,
-    Iterator,
-    Optional,
-    Sequence,
-    TypeVar,
-    Union,
-    List,
+    Callable, Iterator, Optional, Sequence, TypeVar, Union, List,
 )
 
+from langchain.automaton.processors import WorkingMemoryManager
 from langchain.automaton.runnables import create_llm_program, create_retriever
 from langchain.automaton.typedefs import (
-    Agent,
-    AgentFinish,
-    MessageLike,
-    RetrievalRequest,
-    RetrievalResponse,
+    Agent, AgentFinish, MessageLike, RetrievalRequest, RetrievalResponse,
 )
 from langchain.schema import PromptValue, BaseRetriever
 from langchain.schema.language_model import (
-    BaseLanguageModel,
-    LanguageModelInput,
-    LanguageModelOutput,
+    BaseLanguageModel, LanguageModelInput, LanguageModelOutput,
 )
 from langchain.schema.messages import BaseMessage, HumanMessage, AIMessage
 from langchain.schema.output_parser import BaseOutputParser
 from langchain.schema.runnable import (
-    Runnable,
-    RunnableConfig,
+    Runnable, RunnableConfig,
 )
 from langchain.tools import BaseTool
 
@@ -89,6 +77,7 @@ class RagAgent(Agent):
             None,
         ] = None,
         retriever: Optional[BaseRetriever] = None,
+        memory_manager: WorkingMemoryManager = None,
     ) -> None:
         """Initialize the chat agent."""
         invoke_tools = bool(tools)
@@ -101,6 +90,7 @@ class RagAgent(Agent):
             invoke_tools=invoke_tools,
         )
         self.retriever = create_retriever(retriever)
+        self.memory_manager = memory_manager
 
     def step(
         self,
@@ -123,3 +113,20 @@ class RagAgent(Agent):
                 return [self.retriever.invoke(last_message, config=config)]
             case _:
                 return self.llm_program.invoke(messages, config=config)
+
+    def run(
+        self,
+        messages: Sequence[MessageLike],
+        *,
+        config: Optional[dict] = None,
+        max_iterations: int = 100,
+    ) -> Iterator[MessageLike]:
+        """Run the agent."""
+        all_messages = list(messages)
+        for _ in range(max_iterations):
+            all_messages = self.memory_manager.process(all_messages)
+            new_messages = self.step(all_messages, config=config)
+            if not new_messages:
+                break
+            yield from new_messages
+            all_messages.extend(new_messages)
--- libs/langchain/langchain/automaton/processors.py
+++ libs/langchain/langchain/automaton/processors.py
@@ -14,23 +14,6 @@ class WorkingMemoryManager(abc.ABC):
         """Process the working memory returning a potentially new working memory."""
 
 
-def trim_messages(
-    messages: Sequence[MessageLike],
-    max_token_limit: int,
-    token_counter: Callable[[str], int],
-) -> List[MessageLike]:
-    """Trim the working memory to a maximum context length (aka token limit)."""
-    # Work backwards from the end of the buffer dropping messages until
-    messages = list(messages)
-    curr_buffer_length = sum(token_counter(message) for message in messages)
-    while curr_buffer_length > max_token_limit:
-        messages.pop(0)  # Drop the first message
-        if not messages:
-            raise AssertionError("No messages left in buffer")
-        curr_buffer_length = sum(token_counter(message) for message in messages)
-    return messages
-
-
 class TokenBufferProcessor(WorkingMemoryManager):
     """Trim the working memory to a maximum context length (aka token limit)."""
 
@@ -59,6 +42,9 @@ class TokenBufferProcessor(WorkingMemoryManager):
 
     def count_across_messages(self, messages: Sequence[MessageLike]) -> int:
         """Count the number of tokens across messages."""
-        base_messages = self.prompt_generator.to_messages(messages)
+        if isinstance(self.prompt_generator, PromptGenerator):
+            base_messages = self.prompt_generator.to_messages(messages)
+        else:
+            base_messages = self.prompt_generator(messages)
         buffer_string = get_buffer_string(base_messages)
         return self.token_counter(buffer_string)
