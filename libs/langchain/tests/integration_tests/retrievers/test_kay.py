145c513046755459b66c48c44314f0136a70f762
Paul Vorobyev <plv@kay.ai>
2023-09-11 16:38:40 -0400

    Added kay.ai retriever integration

---
+++ libs/langchain/tests/integration_tests/retrievers/test_kay.py
@@ -0,0 +1,27 @@
+"""Integration test for Kay.ai API Wrapper."""
+from typing import List
+
+import pytest
+
+from langchain.retrievers import KayAiRetriever
+from langchain.schema import Document
+
+
+@pytest.mark.requires("kay")
+def test_kay_retriever() -> None:
+    retriever = KayAiRetriever.create(
+        dataset_id="company", 
+        data_sources=["10-K", "10-Q", "8-K", "PressRelease"],
+        num_contexts=3
+    )
+    docs = retriever.get_relevant_documents(
+        "What were the biggest strategy changes and partnerships made by Roku "
+        "in 2023?",
+    )
+    assert len(docs) == 3
+    for doc in docs:
+        assert isinstance(doc, Document)
+        assert doc.page_content
+        assert doc.metadata
+        assert len(list(doc.metadata.items())) > 0
+
