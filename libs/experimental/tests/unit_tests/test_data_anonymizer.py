11c30383e8223b0d0d81dddaf7e13c35af678407
olgavrou <olgavrou@gmail.com>
2023-09-11 13:26:41 -0400

    check if libcublas is available before running extended tests

--- libs/experimental/tests/unit_tests/test_data_anonymizer.py
+++ libs/experimental/tests/unit_tests/test_data_anonymizer.py
@@ -1,6 +1,7 @@
 from typing import Iterator, List
 
 import pytest
+from . import is_libcublas_available
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -10,6 +11,13 @@ def check_spacy_model() -> Iterator[None]:
     if not spacy.util.is_package("en_core_web_lg"):
         pytest.skip(reason="Spacy model 'en_core_web_lg' not installed")
     yield
+
+@pytest.fixture(scope="module", autouse=True)
+def check_libcublas() -> Iterator[None]:
+    if not is_libcublas_available():
+        pytest.skip(reason="libcublas.so is not available")
+    yield
+
 
 
 @pytest.mark.requires("presidio_analyzer", "presidio_anonymizer", "faker")
--- libs/experimental/tests/unit_tests/test_reversible_data_anonymizer.py
+++ libs/experimental/tests/unit_tests/test_reversible_data_anonymizer.py
@@ -2,6 +2,7 @@ import os
 from typing import Iterator, List
 
 import pytest
+from . import is_libcublas_available
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -10,6 +11,12 @@ def check_spacy_model() -> Iterator[None]:
 
     if not spacy.util.is_package("en_core_web_lg"):
         pytest.skip(reason="Spacy model 'en_core_web_lg' not installed")
+    yield
+
+@pytest.fixture(scope="module", autouse=True)
+def check_libcublas() -> Iterator[None]:
+    if not is_libcublas_available():
+        pytest.skip(reason="libcublas.so is not available")
     yield
 
 
