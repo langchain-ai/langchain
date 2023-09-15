78c49c2247ee046e7b0c64b9ef0df789304538cc
anexplore <caoliuyi@yeah.net>
2023-09-15 15:24:27 +0800

    format code

--- libs/langchain/tests/unit_tests/schema/test_messages.py
+++ libs/langchain/tests/unit_tests/schema/test_messages.py
@@ -1,4 +1,8 @@
-from langchain.schema.messages import AIMessageChunk, HumanMessageChunk, ChatMessageChunk
+from langchain.schema.messages import (
+    AIMessageChunk,
+    HumanMessageChunk,
+    ChatMessageChunk,
+)
 
 
 def test_message_chunks() -> None:
@@ -37,25 +41,32 @@ def test_message_chunks() -> None:
         },
     ), "MessageChunk + MessageChunk should be a MessageChunk with merged additional_kwargs"  # noqa: E501
 
-    assert (ChatMessageChunk(content="Hello ", role="user")
-            + ChatMessageChunk(content="World", role="user")
-            == ChatMessageChunk(content="Hello World", role="user")), \
-        "ChatMessageChunk + ChatMessageChunk should be a ChatMessageChunk"
+    assert ChatMessageChunk(content="Hello ", role="user") + ChatMessageChunk(
+        content="World", role="user"
+    ) == ChatMessageChunk(
+        content="Hello World", role="user"
+    ), "ChatMessageChunk + ChatMessageChunk should be a ChatMessageChunk"
 
-    assert (ChatMessageChunk(content="I am ", role="ai")
-            + HumanMessageChunk(content="user")
-            == ChatMessageChunk(content="I am user", role="ai")), \
-        "Message Chunk + Message Chunk should be a MessageChunk of same class as the left side"
+    assert ChatMessageChunk(content="I am ", role="ai") + HumanMessageChunk(
+        content="user"
+    ) == ChatMessageChunk(
+        content="I am user", role="ai"
+    ), "Message Chunk + Message Chunk should be a MessageChunk of same class as the left side"
 
-    assert (ChatMessageChunk(content="", role="ai")
-            + ChatMessageChunk(content="", role="user")
-            == ChatMessageChunk(content="", role="ai")), \
-        "ChatMessageChunk + ChatMessageChunk should be a ChatMessageChunk of same role field as left side"
+    assert ChatMessageChunk(content="", role="ai") + ChatMessageChunk(
+        content="", role="user"
+    ) == ChatMessageChunk(
+        content="", role="ai"
+    ), "ChatMessageChunk + ChatMessageChunk should be a ChatMessageChunk of same role field as left side"
 
     assert ChatMessageChunk(
-        content="", role="user", additional_kwargs={"function_call": {"name": "web_search"}}
+        content="",
+        role="user",
+        additional_kwargs={"function_call": {"name": "web_search"}},
     ) + ChatMessageChunk(
-        content="", role="user", additional_kwargs={"function_call": {"arguments": "{\n"}}
+        content="",
+        role="user",
+        additional_kwargs={"function_call": {"arguments": "{\n"}},
     ) + ChatMessageChunk(
         content="",
         role="user",
