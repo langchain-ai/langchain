0ad6646d8070b7422d2b8ceb2a43ed5837c04168
Nuno Campos <nuno@boringbits.io>
2023-09-15 14:56:43 +0100

    Add AsyncExecutor class

---
+++ libs/langchain/langchain/utils/aexecutor.py
@@ -0,0 +1,108 @@
+from __future__ import annotations
+
+import os
+from types import TracebackType
+from typing import (
+    Any,
+    Awaitable,
+    Callable,
+    Dict,
+    Generator,
+    Generic,
+    List,
+    Optional,
+    TypeVar,
+    Union,
+    cast,
+)
+
+from anyio import CapacityLimiter, Event, create_task_group
+
+T = TypeVar("T")
+Results = Dict[Event, Any]
+
+
+async def execute(
+    fn: Callable[..., Awaitable[T]],
+    args: Any,
+    kwargs: Any,
+    limiter: CapacityLimiter,
+    results: Results,
+    done: Event,
+) -> None:
+    async with limiter:
+        results[done] = await fn(*args, **kwargs)
+        done.set()
+
+
+class AsyncExecutorAwaitable(Generic[T]):
+    def __init__(self, done: Event, results: Results) -> None:
+        self.done = done
+        self.results = results
+
+    def __await__(self) -> Generator[Any, Any, T]:
+        # wait for completion
+        yield from self.done.wait().__await__()
+        # return the result
+        return self.results.pop(self.done)
+
+
+class AsyncExecutor:
+    """This is an asyncio equivalent of concurrent.futures.Executor."""
+
+    def __init__(self, max_workers: Optional[int] = None) -> None:
+        self.task_group = create_task_group()
+        self.limiter = CapacityLimiter(
+            max_workers or min(32, (os.cpu_count() or 1) + 4)
+        )
+        self.results = cast(Results, {})
+
+    def submit(
+        self, fn: Callable[..., Awaitable[T]], /, *args: Any, **kwargs: Any
+    ) -> AsyncExecutorAwaitable[T]:
+        """Submits a callable to be executed with the given arguments.
+
+        Schedules the async callable to be executed as fn(*args, **kwargs), returning
+        an awaitable that will return the result of the call.
+
+        Returns:
+            An awaitable that will return the result of the call.
+        """
+        done = Event()
+
+        # start the task
+        self.task_group.start_soon(
+            execute, fn, args, kwargs, self.limiter, self.results, done
+        )
+        # return an awaitable for result
+        return AsyncExecutorAwaitable[T](done, self.results)
+
+    async def map(
+        self, fn: Callable[..., Awaitable[T]], *iterables: Any
+    ) -> List[AsyncExecutorAwaitable[T]]:
+        """Returns an iterator equivalent to map(fn, iter).
+
+        Args:
+            fn: A callable that will take as many arguments as there are
+                passed iterables.
+
+        Returns:
+            A list of awaitables that return the result of each call.
+
+        Raises:
+            Exception: If fn(*args) raises for any values.
+        """
+
+        return [self.submit(fn, *args) for args in zip(*iterables)]
+
+    async def __aenter__(self) -> AsyncExecutor:
+        await self.task_group.__aenter__()
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: Optional[type[BaseException]],
+        exc_val: Optional[BaseException],
+        exc_tb: Optional[TracebackType],
+    ) -> Union[bool, None]:
+        return await self.task_group.__aexit__(exc_type, exc_val, exc_tb)
---
+++ libs/langchain/tests/unit_tests/utils/test_aexecutor.py
@@ -0,0 +1,73 @@
+import asyncio
+
+import pytest
+
+from langchain.utils.aexecutor import AsyncExecutor
+
+
+@pytest.mark.asyncio
+async def test_aexecutor_one_task() -> None:
+    """Test that we can run a single task."""
+
+    async def return_one() -> int:
+        return 1
+
+    async with AsyncExecutor() as executor:
+        assert await executor.submit(return_one) == 1
+
+    with pytest.raises(RuntimeError):
+        executor.submit(return_one)
+
+
+@pytest.mark.asyncio
+async def test_aexecutor_several_tasks_sequence() -> None:
+    """Test that we can run several tasks in sequence."""
+
+    async def identity(arg: int) -> int:
+        return arg
+
+    async with AsyncExecutor() as executor:
+        results = []
+        results.append(await executor.submit(identity, 1))
+        results.append(await executor.submit(identity, 2))
+        results.append(await executor.submit(identity, 3))
+        results.append(await executor.submit(identity, 4))
+        results.append(await executor.submit(identity, 5))
+        assert results == [1, 2, 3, 4, 5]
+
+    with pytest.raises(RuntimeError):
+        executor.submit(identity, 1)
+
+
+@pytest.mark.asyncio
+@pytest.mark.timeout(2)
+async def test_aexecutor_several_tasks_concurrent() -> None:
+    """Test that we can run submit several tasks, and then await them afterwards."""
+
+    async def identity(arg: int) -> int:
+        await asyncio.sleep(1)
+        return arg
+
+    async with AsyncExecutor() as executor:
+        tasks = [executor.submit(identity, i) for i in range(1, 6)]
+        assert [await task for task in tasks] == [1, 2, 3, 4, 5]
+
+    with pytest.raises(RuntimeError):
+        executor.submit(identity, 1)
+
+
+@pytest.mark.asyncio
+@pytest.mark.timeout(2)
+async def test_aexecutor_several_tasks_exception() -> None:
+    """Test that we can run submit several tasks, and then await them afterwards."""
+
+    async def identity(arg: int) -> int:
+        if arg == 3:
+            raise ValueError("3 is not allowed")
+        await asyncio.sleep(1)
+        return arg
+
+    with pytest.raises(Exception):  # ExceptionGroup in >=3.11
+        async with AsyncExecutor() as executor:
+            tasks = [executor.submit(identity, i) for i in range(1, 6)]
+            assert [await task for task in tasks] == [1, 2, 3, 4, 5]
