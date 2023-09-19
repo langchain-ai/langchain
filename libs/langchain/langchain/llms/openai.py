a8060a4955c5f58ca073de0c7d1e17cd359ba0d7
Jacob Lee <jacoblee93@gmail.com>
2023-09-19 15:50:06 -0700

    Fix for versioned OpenAI instruct models (#10788)
    Versioned OpenAI instruct models may end with numbers, e.g.
    `gpt-3.5-turbo-instruct-0914`.
    
    Fixes https://github.com/langchain-ai/langchainjs/issues/2669 in Python

--- libs/langchain/langchain/llms/openai.py
+++ libs/langchain/langchain/llms/openai.py
@@ -198,7 +198,7 @@ class BaseOpenAI(BaseLLM):
         model_name = data.get("model_name", "")
         if (
             model_name.startswith("gpt-3.5-turbo") or model_name.startswith("gpt-4")
-        ) and not model_name.endswith("-instruct"):
+        ) and "-instruct" not in model_name:
             warnings.warn(
                 "You are trying to use a chat model. This way of initializing it is "
                 "no longer supported. Instead, please use: "
