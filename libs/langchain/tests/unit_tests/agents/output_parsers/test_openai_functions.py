9c7fb2c902d2c459ca3f39bc8da9cca74b4bb0c4
Harrison Chase <hw.chase.17@gmail.com>
2023-09-19 10:12:52 -0700

    cr

--- libs/langchain/tests/unit_tests/agents/output_parsers/test_openai_functions.py
+++ libs/langchain/tests/unit_tests/agents/output_parsers/test_openai_functions.py
@@ -1,30 +1,80 @@
-import json
+import pytest
 
 from langchain.agents.output_parsers.openai_functions import (
     OpenAIFunctionsAgentOutputParser,
+    _FunctionsAgentAction,
 )
-from langchain.schema.messages import AIMessage
+from langchain.schema import AgentFinish, OutputParserException
+from langchain.schema.messages import AIMessage, SystemMessage
 
 
-def test_tool_usage() -> None:
+def test_not_an_ai() -> None:
     parser = OpenAIFunctionsAgentOutputParser()
-    function_call = json.dumps({"query": "hi"})
-    _input = AIMessage(
-        content="hi",
+    err = f"Expected an AI message got {str(SystemMessage)}"
+    with pytest.raises(TypeError, match=err):
+        parser.invoke(SystemMessage(content="x"))
+
+
+# Test: Model response (not a function call).
+def test_model_response() -> None:
+    parser = OpenAIFunctionsAgentOutputParser()
+    msg = AIMessage(content="Model response.")
+    result = parser.invoke(msg)
+
+    assert isinstance(result, AgentFinish)
+    assert result.return_values == {"output": "Model response."}
+    assert result.log == "Model response."
+
+
+# Test: Model response with a function call.
+def test_func_call() -> None:
+    parser = OpenAIFunctionsAgentOutputParser()
+    msg = AIMessage(
+        content="LLM thoughts.",
         additional_kwargs={
-            "function_call": {"name": "search", "arguments": function_call}
+            "function_call": {"name": "foo", "arguments": '{"param": 42}'}
         },
     )
-    output = parser.invoke(_input)
-    # Test this way, because we don't care about testing `log`
-    assert output.tool == "search"
-    assert output.tool_input == {"query": "hi"}
-    assert output.message_log == [_input]
+    result = parser.invoke(msg)
+
+    assert isinstance(result, _FunctionsAgentAction)
+    assert result.tool == "foo"
+    assert result.tool_input == {"param": 42}
+    assert result.log == (
+        "\nInvoking: `foo` with `{'param': 42}`\nresponded: LLM thoughts.\n\n"
+    )
+    assert result.message_log == [msg]
 
 
-def test_finish() -> None:
+# Test: Model response with a function call (old style tools).
+def test_func_call_oldstyle() -> None:
     parser = OpenAIFunctionsAgentOutputParser()
-    _input = AIMessage(content="hi")
-    output = parser.invoke(_input)
-    # Test this way, because we don't care about testing `log`
-    assert output.return_values == {"output": "hi"}
+    msg = AIMessage(
+        content="LLM thoughts.",
+        additional_kwargs={
+            "function_call": {"name": "foo", "arguments": '{"__arg1": "42"}'}
+        },
+    )
+    result = parser.invoke(msg)
+
+    assert isinstance(result, _FunctionsAgentAction)
+    assert result.tool == "foo"
+    assert result.tool_input == "42"
+    assert result.log == ("\nInvoking: `foo` with `42`\nresponded: LLM thoughts.\n\n")
+    assert result.message_log == [msg]
+
+
+# Test: Invalid function call args.
+def test_func_call_invalid() -> None:
+    parser = OpenAIFunctionsAgentOutputParser()
+    msg = AIMessage(
+        content="LLM thoughts.",
+        additional_kwargs={"function_call": {"name": "foo", "arguments": "{42]"}},
+    )
+
+    err = (
+        "Could not parse tool input: {'name': 'foo', 'arguments': '{42]'} "
+        "because the `arguments` is not valid JSON."
+    )
+    with pytest.raises(OutputParserException, match=err):
+        parser.invoke(msg)
