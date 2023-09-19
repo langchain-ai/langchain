eaf7baeea2a2958bf27369235f9b1d2c4902ce0b
etveritas <bravewavy@gmail.com>
2023-09-19 15:15:47 +0800

    modify import

--- libs/langchain/langchain/chat_models/__init__.py
+++ libs/langchain/langchain/chat_models/__init__.py
@@ -22,7 +22,6 @@ from langchain.chat_models.anyscale import ChatAnyscale
 from langchain.chat_models.azure_openai import AzureChatOpenAI
 from langchain.chat_models.baidu_qianfan_endpoint import QianfanChatEndpoint
 from langchain.chat_models.bedrock import BedrockChat
-from langchain.chat_models.bedrock import BedrockChat
 from langchain.chat_models.chatchatglm import ChatChatGLM
 from langchain.chat_models.ernie import ErnieBotChat
 from langchain.chat_models.fake import FakeListChatModel
