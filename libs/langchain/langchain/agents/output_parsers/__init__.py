3aa4215ebc0e05830dbbeeef0104fdf4b360a398
Harrison Chase <hw.chase.17@gmail.com>
2023-09-19 08:49:48 -0700

    add react single input output parser

---
+++ libs/langchain/langchain/agents/output_parsers/__init__.py
@@ -0,0 +1,6 @@
+"""Parsing utils to go from string to AgentAction or Agent Finish."""
+from langchain.agents.output_parsers.react_single_input import (
+    ReActSingleInputOutputParser,
+)
+
+__all__ = ["ReActSingleInputOutputParser"]
