68aec5f7bd558dd840997464e3a0965e637c54d6
Cory Zue <cory@coryzue.com>
2023-09-19 13:31:12 +0200

    allow explicitly setting the encoding

--- libs/langchain/langchain/document_loaders/web_base.py
+++ libs/langchain/langchain/document_loaders/web_base.py
@@ -64,6 +64,7 @@ class WebBaseLoader(BaseLoader):
         proxies: Optional[dict] = None,
         continue_on_failure: Optional[bool] = False,
         autoset_encoding: Optional[bool] = True,
+        encoding: Optional[str] = None,
     ):
         """Initialize with webpage path."""
 
@@ -100,6 +101,7 @@ class WebBaseLoader(BaseLoader):
         self.session.verify = verify_ssl
         self.continue_on_failure = continue_on_failure
         self.autoset_encoding = autoset_encoding
+        self.encoding = encoding
         if proxies:
             self.session.proxies.update(proxies)
 
@@ -210,7 +212,9 @@ class WebBaseLoader(BaseLoader):
         if self.raise_for_status:
             html_doc.raise_for_status()
 
-        if self.autoset_encoding:
+        if self.encoding is not None:
+            html_doc.encoding = self.encoding
+        elif self.autoset_encoding:
             html_doc.encoding = html_doc.apparent_encoding
         return BeautifulSoup(html_doc.text, parser)
 
