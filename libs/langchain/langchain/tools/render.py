808caca6075a261038ae55ebf0337a4dfc9b7795
Harrison Chase <hw.chase.17@gmail.com>
2023-09-20 23:09:58 -0700

    beef up agent docs (#10866)

--- libs/langchain/langchain/tools/render.py
+++ libs/langchain/langchain/tools/render.py
@@ -1,6 +1,10 @@
 from typing import List
 
 from langchain.tools.base import BaseTool
+from langchain.utils.openai_functions import (
+    FunctionDescription,
+    convert_pydantic_to_openai_function,
+)
 
 
 def render_text_description(tools: List[BaseTool]) -> str:
@@ -32,3 +36,28 @@ args: {"expression": {"type": "string"}}
         args_schema = str(tool.args)
         tool_strings.append(f"{tool.name}: {tool.description}, args: {args_schema}")
     return "\n".join(tool_strings)
+
+
+def format_tool_to_openai_function(tool: BaseTool) -> FunctionDescription:
+    """Format tool into the OpenAI function API."""
+    if tool.args_schema:
+        return convert_pydantic_to_openai_function(
+            tool.args_schema, name=tool.name, description=tool.description
+        )
+    else:
+        return {
+            "name": tool.name,
+            "description": tool.description,
+            "parameters": {
+                # This is a hack to get around the fact that some tools
+                # do not expose an args_schema, and expect an argument
+                # which is a string.
+                # And Open AI does not support an array type for the
+                # parameters.
+                "properties": {
+                    "__arg1": {"title": "__arg1", "type": "string"},
+                },
+                "required": ["__arg1"],
+                "type": "object",
+            },
+        }
