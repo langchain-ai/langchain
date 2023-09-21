ea85b43d3170a18046f2815822ae457c1b6523bd
Bagatur <baskaryan@gmail.com>
2023-09-21 12:47:40 -0700

    cr

--- libs/langchain/langchain/document_loaders/arxiv.py
+++ libs/langchain/langchain/document_loaders/arxiv.py
@@ -1,4 +1,4 @@
-from typing import Any, List
+from typing import Any, List, Optional
 
 from langchain.docstore.document import Document
 from langchain.document_loaders.base import BaseLoader
@@ -9,11 +9,18 @@ class ArxivLoader(BaseLoader):
     """Load a query result from `Arxiv`.
 
     The loader converts the original PDF format into the text.
+
+    Args:
+        Supports all arguments of `ArxivAPIWrapper`.
     """
 
-    def __init__(self, query: str, **kwargs: Any):
+    def __init__(
+        self, query: str, doc_content_chars_max: Optional[int] = None, **kwargs: Any
+    ):
         self.query = query
-        self.client = ArxivAPIWrapper(**kwargs)
+        self.client = ArxivAPIWrapper(
+            doc_content_chars_max=doc_content_chars_max, **kwargs
+        )
 
     def load(self) -> List[Document]:
         return self.client.load(self.query)
