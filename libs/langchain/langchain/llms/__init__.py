231dfcd8cd086bee3269a967256815c993d0d9e1
Cheng, Penghui <penghui.cheng@intel.com>
2023-09-22 16:16:22 +0800

    fixed typo
    Signed-off-by: Cheng, Penghui <penghui.cheng@intel.com>

--- libs/langchain/langchain/llms/__init__.py
+++ libs/langchain/langchain/llms/__init__.py
@@ -662,7 +662,7 @@ __all__ = [
     "HuggingFacePipeline",
     "HuggingFaceTextGenInference",
     "HumanInputLLM",
-    "ITREXWrightOnlyQuantPipeline",
+    "WrightOnlyQuantPipeline",
     "KoboldApiLLM",
     "LlamaCpp",
     "TextGen",
