6a2f9d04a8115902b20759c6d677069342f95313
xuqi.wxq <xuqi.wxq@alibaba-inc.com>
2023-09-22 10:38:40 +0800

    Implement llm stream api of Alibaba Tongyi model.

--- libs/langchain/langchain/llms/tongyi.py
+++ libs/langchain/langchain/llms/tongyi.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 
 import logging
-from typing import Any, Callable, Dict, List, Optional
+from typing import Any, Callable, Dict, Iterator, List, Optional
 
 from requests.exceptions import HTTPError
 from tenacity import (
-    before_sleep_log,
+    RetryCallState,
     retry,
     retry_if_exception_type,
     stop_after_attempt,
@@ -13,15 +13,57 @@ from tenacity import (
 )
 
 from langchain.callbacks.manager import CallbackManagerForLLMRun
-from langchain.llms.base import LLM
+from langchain.llms.base import BaseLLM
 from langchain.pydantic_v1 import Field, root_validator
 from langchain.schema import Generation, LLMResult
+from langchain.schema.output import GenerationChunk
 from langchain.utils import get_from_dict_or_env
 
 logger = logging.getLogger(__name__)
 
 
-def _create_retry_decorator(llm: Tongyi) -> Callable[[Any], Any]:
+def _stream_response_to_generation_chunk(
+    stream_response: Dict[str, Any],
+    length: int,
+) -> GenerationChunk:
+    """Convert a stream response to a generation chunk.
+
+    As the low level API implement is different from openai and other llm.
+    Stream response of Tongyi is not split into chunks, but all data generated before.
+    For example, the answer 'Hi Pickle Rick! How can I assist you today?'
+    Other llm will stream answer:
+    'Hi Pickle',
+    ' Rick!',
+    ' How can I assist you today?'.
+
+    Tongyi answer:
+    'Hi Pickle',
+    'Hi Pickle Rick!',
+    'Hi Pickle Rick! How can I assist you today?'.
+
+    As the GenerationChunk is implemented with chunks. Only return full_text[length:]
+    for new chunk.
+    """
+    full_text = stream_response["output"]["text"]
+    text = full_text[length:]
+    finish_reason = stream_response["output"].get("finish_reason", None)
+
+    return GenerationChunk(
+        text=text,
+        generation_info=dict(
+            finish_reason=finish_reason,
+        ),
+    )
+
+
+def _create_retry_decorator(
+    llm: Tongyi, run_manager: Optional[CallbackManagerForLLMRun]
+) -> Callable[[Any], Any]:
+    def _before_sleep(retry_state: RetryCallState) -> None:
+        if run_manager:
+            run_manager.on_retry(retry_state)
+        return None
+
     min_seconds = 1
     max_seconds = 4
     # Wait 2^x * 1 second between each retry starting with
@@ -31,16 +73,18 @@ def _create_retry_decorator(llm: Tongyi) -> Callable[[Any], Any]:
         stop=stop_after_attempt(llm.max_retries),
         wait=wait_exponential(multiplier=1, min=min_seconds, max=max_seconds),
         retry=(retry_if_exception_type(HTTPError)),
-        before_sleep=before_sleep_log(logger, logging.WARNING),
+        before_sleep=_before_sleep,
     )
 
 
-def generate_with_retry(llm: Tongyi, **kwargs: Any) -> Any:
+def completion_with_retry(
+    llm: Tongyi, run_manager: Optional[CallbackManagerForLLMRun] = None, **kwargs: Any
+) -> Any:
     """Use tenacity to retry the completion call."""
-    retry_decorator = _create_retry_decorator(llm)
+    retry_decorator = _create_retry_decorator(llm, run_manager=run_manager)
 
     @retry_decorator
-    def _generate_with_retry(**_kwargs: Any) -> Any:
+    def _completion_with_retry(**_kwargs: Any) -> Any:
         resp = llm.client.call(**_kwargs)
         if resp.status_code == 200:
             return resp
@@ -55,40 +99,27 @@ def generate_with_retry(llm: Tongyi, **kwargs: Any) -> Any:
                 f"code: {resp.code} \n message: {resp.message}"
             )
 
-    return _generate_with_retry(**kwargs)
+    return _completion_with_retry(**kwargs)
 
 
-def stream_generate_with_retry(llm: Tongyi, **kwargs: Any) -> Any:
+def stream_completion_with_retry(
+    llm: Tongyi, run_manager: Optional[CallbackManagerForLLMRun] = None, **kwargs: Any
+) -> Any:
     """Use tenacity to retry the completion call."""
-    retry_decorator = _create_retry_decorator(llm)
+    retry_decorator = _create_retry_decorator(llm, run_manager=run_manager)
 
     @retry_decorator
-    def _stream_generate_with_retry(**_kwargs: Any) -> Any:
-        stream_resps = []
-        resps = llm.client.call(**_kwargs)
-        for resp in resps:
-            if resp.status_code == 200:
-                stream_resps.append(resp)
-            elif resp.status_code in [400, 401]:
-                raise ValueError(
-                    f"status_code: {resp.status_code} \n "
-                    f"code: {resp.code} \n message: {resp.message}"
-                )
-            else:
-                raise HTTPError(
-                    f"HTTP error occurred: status_code: {resp.status_code} \n "
-                    f"code: {resp.code} \n message: {resp.message}"
-                )
-        return stream_resps
+    def _stream_completion_with_retry(**_kwargs: Any) -> Any:
+        return llm.client.call(**_kwargs)
 
-    return _stream_generate_with_retry(**kwargs)
+    return _stream_completion_with_retry(**kwargs)
 
 
-class Tongyi(LLM):
+class Tongyi(BaseLLM):
     """Tongyi Qwen large language models.
 
-    To use, you should have the ``dashscope`` python package installed, and the
-    environment variable ``DASHSCOPE_API_KEY`` set with your API key, or pass
+    To use, you should have the ``dashscope`` python package installed,
+    and set env ``DASHSCOPE_API_KEY`` with your API key, or pass
     it as a named parameter to the constructor.
 
     Example:
@@ -107,7 +138,7 @@ class Tongyi(LLM):
         return True
 
     client: Any  #: :meta private:
-    model_name: str = "qwen-plus-v1"
+    model_name: str = Field(default="qwen-turbo", alias="model")
 
     """Model name to use."""
     model_kwargs: Dict[str, Any] = Field(default_factory=dict)
@@ -144,7 +175,7 @@ class Tongyi(LLM):
         except ImportError:
             raise ImportError(
                 "Could not import dashscope python package. "
-                "Please install it with `pip install dashscope`."
+                "Please install it with `pip install dashscope --upgrade`."
             )
         try:
             values["client"] = dashscope.Generation
@@ -166,39 +197,6 @@ class Tongyi(LLM):
 
         return {**normal_params, **self.model_kwargs}
 
-    def _call(
-        self,
-        prompt: str,
-        stop: Optional[List[str]] = None,
-        run_manager: Optional[CallbackManagerForLLMRun] = None,
-        **kwargs: Any,
-    ) -> str:
-        """Call out to Tongyi's generate endpoint.
-
-        Args:
-            prompt: The prompt to pass into the model.
-
-        Returns:
-            The string generated by the model.
-
-        Example:
-            .. code-block:: python
-
-                response = tongyi("Tell me a joke.")
-        """
-        params: Dict[str, Any] = {
-            **{"model": self.model_name},
-            **self._default_params,
-            **kwargs,
-        }
-
-        completion = generate_with_retry(
-            self,
-            prompt=prompt,
-            **params,
-        )
-        return completion["output"]["text"]
-
     def _generate(
         self,
         prompts: List[str],
@@ -216,34 +214,59 @@ class Tongyi(LLM):
             if len(prompts) > 1:
                 raise ValueError("Cannot stream results with multiple prompts.")
             params["stream"] = True
-            for stream_resp in stream_generate_with_retry(
+
+            # Mark current chunk total length
+            length = 0
+            for stream_resp in stream_completion_with_retry(
                 self, prompt=prompts[0], **params
             ):
+                full_text = stream_resp["output"]["text"]
+                text = full_text[length:]
                 generations.append(
                     [
                         Generation(
-                            text=stream_resp["output"]["text"],
+                            text=text,
                             generation_info=dict(
                                 finish_reason=stream_resp["output"]["finish_reason"],
                             ),
                         )
                     ]
                 )
+                if run_manager:
+                    run_manager.on_llm_new_token(text)
+                length = len(stream_resp["output"]["text"])
         else:
             for prompt in prompts:
-                completion = generate_with_retry(
-                    self,
-                    prompt=prompt,
-                    **params,
-                )
+                completion = completion_with_retry(self, prompt=prompt, **params)
                 generations.append(
                     [
                         Generation(
                             text=completion["output"]["text"],
-                            generation_info=dict(
-                                finish_reason=completion["output"]["finish_reason"],
-                            ),
                         )
                     ]
                 )
         return LLMResult(generations=generations)
+
+    def _stream(
+        self,
+        prompt: str,
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+        **kwargs: Any,
+    ) -> Iterator[GenerationChunk]:
+        params: Dict[str, Any] = {
+            **{"model": self.model_name},
+            **self._default_params,
+            **kwargs,
+            "stream": True,
+        }
+        # Mark current chunk total length
+        length = 0
+        for stream_resp in stream_completion_with_retry(
+            self, prompt=prompt, run_manager=run_manager, **params
+        ):
+            chunk = _stream_response_to_generation_chunk(stream_resp, length)
+            length += len(chunk.text)
+            yield chunk
+            if run_manager:
+                run_manager.on_llm_new_token(chunk.text)
--- libs/langchain/tests/integration_tests/llms/test_tongyi.py
+++ libs/langchain/tests/integration_tests/llms/test_tongyi.py
@@ -1,6 +1,15 @@
 """Test Tongyi API wrapper."""
+from typing import Any, Generator
+
+from langchain.callbacks.base import BaseCallbackHandler
 from langchain.llms.tongyi import Tongyi
 from langchain.schema import LLMResult
+
+
+class TestCallbackHandler(BaseCallbackHandler):
+    def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
+        """Run when LLM generates a new token."""
+        assert isinstance(token, str)
 
 
 def test_tongyi_call() -> None:
@@ -8,6 +17,12 @@ def test_tongyi_call() -> None:
     llm = Tongyi()
     output = llm("who are you")
     assert isinstance(output, str)
+
+
+def test_tongyi_stream_call() -> None:
+    """Test valid call to tongyi."""
+    llm = Tongyi(streaming=True, callbacks=[TestCallbackHandler()])
+    llm("who are you")
 
 
 def test_tongyi_generate() -> None:
@@ -22,6 +37,16 @@ def test_tongyi_generate_stream() -> None:
     """Test valid call to tongyi."""
     llm = Tongyi(streaming=True)
     output = llm.generate(["who are you"])
-    print(output)
     assert isinstance(output, LLMResult)
     assert isinstance(output.generations, list)
+
+
+def test_tongyi_stream() -> None:
+    """Test valid call to tongyi."""
+    llm = Tongyi()
+    generator = llm.stream("who are you")
+
+    assert isinstance(generator, Generator)
+
+    for token in generator:
+        assert isinstance(token, str)
