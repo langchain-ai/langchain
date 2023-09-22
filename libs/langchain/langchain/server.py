606ced18d6a4e61cc305836463f933f6d6727afc
Carl Peng <miaow.xd@gmail.com>
2023-09-22 16:41:17 +0800

    20230922 fix wrong import when launch langchain server with docker-compose since langsmith-0.0.26

--- libs/langchain/langchain/server.py
+++ libs/langchain/langchain/server.py
@@ -2,7 +2,7 @@
 import subprocess
 from pathlib import Path
 
-from langsmith.cli.main import get_docker_compose_command
+from langsmith.utils import get_docker_compose_command
 
 
 def main() -> None:
