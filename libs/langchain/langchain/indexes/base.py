5c6dd63c37bad0b7f6abef89a283c0084f028548
Jesús Vélez Santiago <jvelezmagic@gmail.com>
2023-09-22 22:03:57 -0600

    refactor: :recycle: Unify async versions with RecordManager abstractclass and SQLRecordManager

--- libs/langchain/langchain/indexes/base.py
+++ libs/langchain/langchain/indexes/base.py
@@ -26,7 +26,22 @@ class RecordManager(ABC):
         """Create the database schema for the record manager."""
 
     @abstractmethod
+    async def acreate_schema(self) -> None:
+        """Create the database schema for the record manager."""
+
+    @abstractmethod
     def get_time(self) -> float:
+        """Get the current server time as a high resolution timestamp!
+
+        It's important to get this from the server to ensure a monotonic clock,
+        otherwise there may be data loss when cleaning up old documents!
+
+        Returns:
+            The current server time as a float timestamp.
+        """
+
+    @abstractmethod
+    async def aget_time(self) -> float:
         """Get the current server time as a high resolution timestamp!
 
         It's important to get this from the server to ensure a monotonic clock,
@@ -57,7 +72,38 @@ class RecordManager(ABC):
         """
 
     @abstractmethod
+    async def aupdate(
+        self,
+        keys: Sequence[str],
+        *,
+        group_ids: Optional[Sequence[Optional[str]]] = None,
+        time_at_least: Optional[float] = None,
+    ) -> None:
+        """Upsert records into the database.
+
+        Args:
+            keys: A list of record keys to upsert.
+            group_ids: A list of group IDs corresponding to the keys.
+            time_at_least: if provided, updates should only happen if the
+              updated_at field is at least this time.
+
+        Raises:
+            ValueError: If the length of keys doesn't match the length of group_ids.
+        """
+
+    @abstractmethod
     def exists(self, keys: Sequence[str]) -> List[bool]:
+        """Check if the provided keys exist in the database.
+
+        Args:
+            keys: A list of keys to check.
+
+        Returns:
+            A list of boolean values indicating the existence of each key.
+        """
+
+    @abstractmethod
+    async def aexists(self, keys: Sequence[str]) -> List[bool]:
         """Check if the provided keys exist in the database.
 
         Args:
@@ -87,76 +133,7 @@ class RecordManager(ABC):
         """
 
     @abstractmethod
-    def delete_keys(self, keys: Sequence[str]) -> None:
-        """Delete specified records from the database.
-
-        Args:
-            keys: A list of keys to delete.
-        """
-
-
-class RecordManagerAsync(ABC):
-    """An abstract base class representing the interface for a record manager."""
-
-    def __init__(
-        self,
-        namespace: str,
-    ) -> None:
-        """Initialize the record manager.
-
-        Args:
-            namespace (str): The namespace for the record manager.
-        """
-        self.namespace = namespace
-
-    @abstractmethod
-    async def create_schema(self) -> None:
-        """Create the database schema for the record manager."""
-
-    @abstractmethod
-    async def get_time(self) -> float:
-        """Get the current server time as a high resolution timestamp!
-
-        It's important to get this from the server to ensure a monotonic clock,
-        otherwise there may be data loss when cleaning up old documents!
-
-        Returns:
-            The current server time as a float timestamp.
-        """
-
-    @abstractmethod
-    async def update(
-        self,
-        keys: Sequence[str],
-        *,
-        group_ids: Optional[Sequence[Optional[str]]] = None,
-        time_at_least: Optional[float] = None,
-    ) -> None:
-        """Upsert records into the database.
-
-        Args:
-            keys: A list of record keys to upsert.
-            group_ids: A list of group IDs corresponding to the keys.
-            time_at_least: if provided, updates should only happen if the
-              updated_at field is at least this time.
-
-        Raises:
-            ValueError: If the length of keys doesn't match the length of group_ids.
-        """
-
-    @abstractmethod
-    async def exists(self, keys: Sequence[str]) -> List[bool]:
-        """Check if the provided keys exist in the database.
-
-        Args:
-            keys: A list of keys to check.
-
-        Returns:
-            A list of boolean values indicating the existence of each key.
-        """
-
-    @abstractmethod
-    async def list_keys(
+    async def alist_keys(
         self,
         *,
         before: Optional[float] = None,
@@ -175,7 +152,15 @@ class RecordManagerAsync(ABC):
         """
 
     @abstractmethod
-    async def delete_keys(self, keys: Sequence[str]) -> None:
+    def delete_keys(self, keys: Sequence[str]) -> None:
+        """Delete specified records from the database.
+
+        Args:
+            keys: A list of keys to delete.
+        """
+
+    @abstractmethod
+    async def adelete_keys(self, keys: Sequence[str]) -> None:
         """Delete specified records from the database.
 
         Args:
