2f970c58e804478f4534c31e155d3eff7a3e554b
Harrison Chase <hw.chase.17@gmail.com>
2023-09-25 13:49:21 -0700

    cr

--- libs/langchain/scripts/check_imports.sh
+++ libs/langchain/scripts/check_imports.sh
@@ -7,7 +7,7 @@ errors=0
 git grep 'from langchain import' langchain | grep -vE 'from langchain import (__version__|hub)' && errors=$((errors+1))
 git grep 'from langchain ' langchain/pydantic_v1 && errors=$((errors+1))
 git grep 'from langchain' langchain/load | grep -vE 'from langchain.(pydantic_v1|load)' && errors=$((errors+1))
-git grep 'from langchain' langchain/utils | grep -vE 'from langchain.(pydantic_v1)' && errors=$((errors+1))
+git grep 'from langchain' langchain/utils | grep -vE 'from langchain.(pydantic_v1|utils)' && errors=$((errors+1))
 
 # Decide on an exit status based on the errors
 if [ "$errors" -gt 0 ]; then
