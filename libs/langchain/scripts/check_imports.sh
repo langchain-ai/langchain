a7842eeb6e13c90c6e03708635e7f90f8f197813
Harrison Chase <hw.chase.17@gmail.com>
2023-09-25 13:46:42 -0700

    add more import checks

--- libs/langchain/scripts/check_imports.sh
+++ libs/langchain/scripts/check_imports.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-set -eu
+# Initialize a variable to keep track of errors
+errors=0
 
-git grep 'from langchain import' langchain | grep -vE 'from langchain import (__version__|hub)' && exit 1 || exit 0
+# Check the conditions
+git grep 'from langchain import' langchain | grep -vE 'from langchain import (__version__|hub)' && errors=$((errors+1))
+git grep 'from langchain ' langchain/pydantic_v1 && errors=$((errors+1))
+git grep 'from langchain' langchain/load | grep -vE 'from langchain.(pydantic_v1|load)' && errors=$((errors+1))
+git grep 'from langchain' langchain/utils | grep -vE 'from langchain.(pydantic_v1)' && errors=$((errors+1))
 
-# Pydantic bridge should not import from any other module
-git grep 'from langchain ' langchain/pydantic_v1 && exit 1 || exit 0
-
-# load should not import from anything except itself and pydantic_v1
-git grep 'from langchain' langchain/load | grep -vE 'from langchain.(pydantic_v1)' && exit 1 || exit 0
-
-# utils should not import from anything except itself and pydantic_v1
-git grep 'from langchain' langchain/utils | grep -vE 'from langchain.(pydantic_v1|utils)' && exit 1 || exit 0
+# Decide on an exit status based on the errors
+if [ "$errors" -gt 0 ]; then
+    exit 1
+else
+    exit 0
+fi
