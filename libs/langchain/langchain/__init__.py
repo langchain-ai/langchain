8f1e5987cd5bffec59bfcf58c910975f9f519384
Harrison Chase <hw.chase.17@gmail.com>
2023-09-25 13:17:39 -0700

    cr

--- libs/langchain/langchain/__init__.py
+++ libs/langchain/langchain/__init__.py
@@ -2,7 +2,7 @@
 """Main entrypoint into package."""
 import warnings
 from importlib import metadata
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING, Any, Optional
 
 if TYPE_CHECKING:
     from langchain.schema import BaseCache
@@ -20,153 +20,188 @@ debug: bool = False
 llm_cache: Optional["BaseCache"] = None
 
 
-def __getattr__(name):
+def __getattr__(name: str) -> Any:
     warnings.warn(f"Importing {name} is no longer supported.")
-    if name in ["MRKLChain", "ReActChain", "SelfAskWithSearchChain"]:
-        from langchain.agents import MRKLChain, ReActChain, SelfAskWithSearchChain
+    if name == "MRKLChain":
+        from langchain.agents import MRKLChain
 
-        # Add the imported attribute to the module's namespace
-        # This avoids re-entering this in the future
-        globals()[name] = locals()[name]
-        return locals()[name]
-    elif name in [
-        "ConversationChain",
-        "LLMBashChain",
-        "LLMChain",
-        "LLMCheckerChain",
-        "LLMMathChain",
-        "QAWithSourcesChain",
-        "VectorDBQA",
-        "VectorDBQAWithSourcesChain",
-    ]:
-        from langchain.chains import (
-            ConversationChain,
-            LLMBashChain,
-            LLMChain,
-            LLMCheckerChain,
-            LLMMathChain,
-            QAWithSourcesChain,
-            VectorDBQA,
-            VectorDBQAWithSourcesChain,
-        )
+        return MRKLChain
+    elif name == "ReActChain":
+        from langchain.agents import ReActChain
 
-        # Add the imported attribute to the module's namespace
-        # This avoids re-entering this in the future
-        globals()[name] = locals()[name]
-        return locals()[name]
-    elif name in ["InMemoryDocstore", "Wikipedia"]:
-        from langchain.docstore import InMemoryDocstore, Wikipedia
+        return ReActChain
+    elif name == "SelfAskWithSearchChain":
+        from langchain.agents import SelfAskWithSearchChain
 
-        # Add the imported attribute to the module's namespace
-        # This avoids re-entering this in the future
-        globals()[name] = locals()[name]
-        return locals()[name]
-    elif name in [
-        "Anthropic",
-        "Banana",
-        "CerebriumAI",
-        "Cohere",
-        "ForefrontAI",
-        "GooseAI",
-        "HuggingFaceHub",
-        "HuggingFaceTextGenInference",
-        "LlamaCpp",
-        "Modal",
-        "OpenAI",
-        "Petals",
-        "PipelineAI",
-        "SagemakerEndpoint",
-        "StochasticAI",
-        "Writer",
-    ]:
-        from langchain.llms import (
-            Anthropic,
-            Banana,
-            CerebriumAI,
-            Cohere,
-            ForefrontAI,
-            GooseAI,
-            HuggingFaceHub,
-            HuggingFaceTextGenInference,
-            LlamaCpp,
-            Modal,
-            OpenAI,
-            Petals,
-            PipelineAI,
-            SagemakerEndpoint,
-            StochasticAI,
-            Writer,
-        )
+        return SelfAskWithSearchChain
+    elif name == "ConversationChain":
+        from langchain.chains import ConversationChain
 
-        # Add the imported attribute to the module's namespace
-        # This avoids re-entering this in the future
-        globals()[name] = locals()[name]
-        return locals()[name]
-    elif name in ["HuggingFacePipeline"]:
+        return ConversationChain
+    elif name == "LLMBashChain":
+        from langchain.chains import LLMBashChain
+
+        return LLMBashChain
+    elif name == "LLMChain":
+        from langchain.chains import LLMChain
+
+        return LLMChain
+    elif name == "LLMCheckerChain":
+        from langchain.chains import LLMCheckerChain
+
+        return LLMCheckerChain
+    elif name == "LLMMathChain":
+        from langchain.chains import LLMMathChain
+
+        return LLMMathChain
+    elif name == "QAWithSourcesChain":
+        from langchain.chains import QAWithSourcesChain
+
+        return QAWithSourcesChain
+    elif name == "VectorDBQA":
+        from langchain.chains import VectorDBQA
+
+        return VectorDBQA
+    elif name == "VectorDBQAWithSourcesChain":
+        from langchain.chains import VectorDBQAWithSourcesChain
+
+        return VectorDBQAWithSourcesChain
+    elif name == "InMemoryDocstore":
+        from langchain.docstore import InMemoryDocstore
+
+        return InMemoryDocstore
+    elif name == "Wikipedia":
+        from langchain.docstore import Wikipedia
+
+        return Wikipedia
+    elif name == "Anthropic":
+        from langchain.llms import Anthropic
+
+        return Anthropic
+    elif name == "Banana":
+        from langchain.llms import Banana
+
+        return Banana
+    elif name == "CerebriumAI":
+        from langchain.llms import CerebriumAI
+
+        return CerebriumAI
+    elif name == "Cohere":
+        from langchain.llms import Cohere
+
+        return Cohere
+    elif name == "ForefrontAI":
+        from langchain.llms import ForefrontAI
+
+        return ForefrontAI
+    elif name == "GooseAI":
+        from langchain.llms import GooseAI
+
+        return GooseAI
+    elif name == "HuggingFaceHub":
+        from langchain.llms import HuggingFaceHub
+
+        return HuggingFaceHub
+    elif name == "HuggingFaceTextGenInference":
+        from langchain.llms import HuggingFaceTextGenInference
+
+        return HuggingFaceTextGenInference
+    elif name == "LlamaCpp":
+        from langchain.llms import LlamaCpp
+
+        return LlamaCpp
+    elif name == "Modal":
+        from langchain.llms import Modal
+
+        return Modal
+    elif name == "OpenAI":
+        from langchain.llms import OpenAI
+
+        return OpenAI
+    elif name == "Petals":
+        from langchain.llms import Petals
+
+        return Petals
+    elif name == "PipelineAI":
+        from langchain.llms import PipelineAI
+
+        return PipelineAI
+    elif name == "SagemakerEndpoint":
+        from langchain.llms import SagemakerEndpoint
+
+        return SagemakerEndpoint
+    elif name == "StochasticAI":
+        from langchain.llms import StochasticAI
+
+        return StochasticAI
+    elif name == "Writer":
+        from langchain.llms import Writer
+
+        return Writer
+    elif name == "HuggingFacePipeline":
         from langchain.llms.huggingface_pipeline import HuggingFacePipeline
 
-        # Add the imported attribute to the module's namespace
-        # This avoids re-entering this in the future
-        globals()[name] = locals()[name]
-        return locals()[name]
-    elif name in [
-        "FewShotPromptTemplate",
-        "Prompt",
-        "PromptTemplate",
-    ]:
-        from langchain.prompts import (
-            FewShotPromptTemplate,
-            Prompt,
-            PromptTemplate,
-        )
+        return HuggingFacePipeline
+    elif name == "FewShotPromptTemplate":
+        from langchain.prompts import FewShotPromptTemplate
 
-        # Add the imported attribute to the module's namespace
-        # This avoids re-entering this in the future
-        globals()[name] = locals()[name]
-        return locals()[name]
-    elif name in ["BasePromptTemplate"]:
+        return FewShotPromptTemplate
+    elif name == "Prompt":
+        from langchain.prompts import Prompt
+
+        return Prompt
+    elif name == "PromptTemplate":
+        from langchain.prompts import PromptTemplate
+
+        return PromptTemplate
+    elif name == "BasePromptTemplate":
         from langchain.schema.prompt_template import BasePromptTemplate
 
-        # Add the imported attribute to the module's namespace
-        # This avoids re-entering this in the future
-        globals()[name] = locals()[name]
-        return locals()[name]
-    elif name in [
-        "ArxivAPIWrapper",
-        "GoldenQueryAPIWrapper",
-        "GoogleSearchAPIWrapper",
-        "GoogleSerperAPIWrapper",
-        "PowerBIDataset",
-        "SearxSearchWrapper",
-        "SerpAPIWrapper",
-        "WikipediaAPIWrapper",
-        "WolframAlphaAPIWrapper",
-        "SQLDatabase",
-    ]:
-        from langchain.utilities import (
-            ArxivAPIWrapper,
-            GoldenQueryAPIWrapper,
-            GoogleSearchAPIWrapper,
-            GoogleSerperAPIWrapper,
-            PowerBIDataset,
-            SearxSearchWrapper,
-            SerpAPIWrapper,
-            SQLDatabase,
-            WikipediaAPIWrapper,
-            WolframAlphaAPIWrapper,
-        )
+        return BasePromptTemplate
+    elif name == "ArxivAPIWrapper":
+        from langchain.utilities import ArxivAPIWrapper
 
-        # Add the imported attribute to the module's namespace
-        # This avoids re-entering this in the future
-        globals()[name] = locals()[name]
-        return locals()[name]
-    elif name in ["FAISS", "ElasticVectorSearch"]:
-        from langchain.vectorstores import FAISS, ElasticVectorSearch
+        return ArxivAPIWrapper
+    elif name == "GoldenQueryAPIWrapper":
+        from langchain.utilities import GoldenQueryAPIWrapper
 
-        # Add the imported attribute to the module's namespace
-        # This avoids re-entering this in the future
-        globals()[name] = locals()[name]
-        return locals()[name]
+        return GoldenQueryAPIWrapper
+    elif name == "GoogleSearchAPIWrapper":
+        from langchain.utilities import GoogleSearchAPIWrapper
+
+        return GoogleSearchAPIWrapper
+    elif name == "GoogleSerperAPIWrapper":
+        from langchain.utilities import GoogleSerperAPIWrapper
+
+        return GoogleSerperAPIWrapper
+    elif name == "PowerBIDataset":
+        from langchain.utilities import PowerBIDataset
+
+        return PowerBIDataset
+    elif name == "SearxSearchWrapper":
+        from langchain.utilities import SearxSearchWrapper
+
+        return SearxSearchWrapper
+    elif name == "WikipediaAPIWrapper":
+        from langchain.utilities import WikipediaAPIWrapper
+
+        return WikipediaAPIWrapper
+    elif name == "WolframAlphaAPIWrapper":
+        from langchain.utilities import WolframAlphaAPIWrapper
+
+        return WolframAlphaAPIWrapper
+    elif name == "SQLDatabase":
+        from langchain.utilities import SQLDatabase
+
+        return SQLDatabase
+    elif name == "FAISS":
+        from langchain.vectorstores import FAISS
+
+        return FAISS
+    elif name == "ElasticVectorSearch":
+        from langchain.vectorstores import ElasticVectorSearch
+
+        return ElasticVectorSearch
     # For backwards compatibility
     elif name == "SerpAPIChain":
         from langchain.utilities import SerpAPIWrapper
