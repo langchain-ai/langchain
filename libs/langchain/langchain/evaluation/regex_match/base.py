37aa5ac04d3fd1faa24df4505e9b785b5d07050f
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2023-09-27 13:32:05 -0700

    Add actual files

---
+++ libs/langchain/langchain/evaluation/regex_match/base.py
@@ -0,0 +1,85 @@
+import re
+from typing import Any, List, Optional
+
+from langchain.evaluation.schema import StringEvaluator
+
+
+class RegexMatchStringEvaluator(StringEvaluator):
+    """Compute a regex match between the prediction and the reference.
+
+    Examples
+    ----------
+    >>> evaluator = RegexMatchStringEvaluator(flags=re.IGNORECASE)
+    >>> evaluator.evaluate_strings(
+            prediction="Mindy is the CTO",
+            reference="^mindy.*cto$",
+        )  # This will return {'score': 1.0} due to the IGNORECASE flag
+
+    >>> evaluator = RegexMatchStringEvaluator()
+    >>> evaluator.evaluate_strings(
+            prediction="Mindy is the CTO",
+            reference="^Mike.*CEO$",
+        )  # This will return {'score': 0.0}
+
+    >>> evaluator.evaluate_strings(
+            prediction="Mindy is the CTO",
+            reference="^Mike.*CEO$|^Mindy.*CTO$",
+        )  # This will return {'score': 1.0} as the prediction matches the second pattern in the union
+    """
+
+    def __init__(self, flags=0):  # Default is no flags
+        self.flags = flags
+
+    @property
+    def requires_input(self) -> bool:
+        """
+        This evaluator does not require input.
+        """
+        return False
+
+    @property
+    def requires_reference(self) -> bool:
+        """
+        This evaluator requires a reference.
+        """
+        return True
+
+    @property
+    def input_keys(self) -> List[str]:
+        """
+        Get the input keys.
+
+        Returns:
+            List[str]: The input keys.
+        """
+        return ["reference", "prediction"]
+
+    @property
+    def evaluation_name(self) -> str:
+        """
+        Get the evaluation name.
+
+        Returns:
+            str: The evaluation name.
+        """
+        return "regex_match"
+
+    def _evaluate_strings(
+        self,
+        *,
+        prediction: str,
+        reference: Optional[str] = None,
+        **kwargs: Any,
+    ) -> dict:
+        """
+        Evaluate the regex match between the prediction and the reference.
+
+        Args:
+            prediction (str): The prediction string.
+            reference (Optional[str], optional): The reference regex pattern.
+
+        Returns:
+            dict: The evaluation results containing the score.
+        """
+        match = re.match(reference, prediction, flags=self.flags)
+        return {"score": int(bool(match))}
