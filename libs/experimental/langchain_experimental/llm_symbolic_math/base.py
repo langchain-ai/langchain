8f7f21d1a1f96d3a8cb0837a01ce13058e9843a2
Eugene Yurtsev <eyurtsev@gmail.com>
2023-09-27 15:06:06 -0400

    x

--- libs/experimental/langchain_experimental/llm_symbolic_math/base.py
+++ libs/experimental/langchain_experimental/llm_symbolic_math/base.py
@@ -12,9 +12,9 @@ from langchain.callbacks.manager import (
 from langchain.chains.base import Chain
 from langchain.chains.llm import LLMChain
 from langchain.prompts.base import BasePromptTemplate
-from langchain.pydantic_v1 import Extra
 
 from langchain_experimental.llm_symbolic_math.prompt import PROMPT
+from langchain_experimental.pydantic_v1 import Extra
 
 
 class LLMSymbolicMathChain(Chain):
