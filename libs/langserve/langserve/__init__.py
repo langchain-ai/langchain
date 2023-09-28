3e4b6e34394f71fe9cbdedab7f90e034de8ae441
Eugene Yurtsev <eyurtsev@gmail.com>
2023-09-28 11:10:42 -0400

    LangServe: Clean up init files (#11174)
    Clean up init files

--- libs/langserve/langserve/__init__.py
+++ libs/langserve/langserve/__init__.py
@@ -1,4 +1,4 @@
-from .client import RemoteRunnable
-from .server import add_routes
+from langserve.client import RemoteRunnable
+from langserve.server import add_routes
 
 __all__ = ["RemoteRunnable", "add_routes"]
