dba01d9d10c614907efe4701c89487b7eafcd2cb
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2023-09-28 16:27:57 -0700

    format

--- libs/experimental/tests/integration_tests/chains/test_synthetic_data_openai.py
+++ libs/experimental/tests/integration_tests/chains/test_synthetic_data_openai.py
@@ -1,15 +1,17 @@
 import pytest
 
 from langchain import FewShotPromptTemplate
-
 from langchain.chat_models import ChatOpenAI
 from langchain.pydantic_v1 import BaseModel
-from langchain_experimental.tabular_synthetic_data.base import \
-    SyntheticDataGenerator
-from langchain_experimental.tabular_synthetic_data.openai import \
-    create_openai_data_generator, OPENAI_TEMPLATE
-from langchain_experimental.tabular_synthetic_data.prompts import \
-    SYNTHETIC_FEW_SHOT_SUFFIX, SYNTHETIC_FEW_SHOT_PREFIX
+from langchain_experimental.tabular_synthetic_data.base import SyntheticDataGenerator
+from langchain_experimental.tabular_synthetic_data.openai import (
+    OPENAI_TEMPLATE,
+    create_openai_data_generator,
+)
+from langchain_experimental.tabular_synthetic_data.prompts import (
+    SYNTHETIC_FEW_SHOT_PREFIX,
+    SYNTHETIC_FEW_SHOT_SUFFIX,
+)
 
 
 # Define the desired output schema for individual medical billing record
