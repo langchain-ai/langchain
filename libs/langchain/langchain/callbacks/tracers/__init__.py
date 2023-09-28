80dd05b802fa640da5f58a489c0bda645404109f
Ankush Gola <ankush.gola@gmail.com>
2023-09-28 16:05:29 -0700

    delete v1 tracer

--- libs/langchain/langchain/callbacks/tracers/__init__.py
+++ libs/langchain/langchain/callbacks/tracers/__init__.py
@@ -1,7 +1,6 @@
 """Tracers that record execution of LangChain runs."""
 
 from langchain.callbacks.tracers.langchain import LangChainTracer
-from langchain.callbacks.tracers.langchain_v1 import LangChainTracerV1
 from langchain.callbacks.tracers.stdout import (
     ConsoleCallbackHandler,
     FunctionCallbackHandler,
@@ -10,7 +9,6 @@ from langchain.callbacks.tracers.wandb import WandbTracer
 
 __all__ = [
     "LangChainTracer",
-    "LangChainTracerV1",
     "FunctionCallbackHandler",
     "ConsoleCallbackHandler",
     "WandbTracer",
