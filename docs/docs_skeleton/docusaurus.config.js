f38c92ec173f0d12b11a0653037ef20474cc2df7
Bagatur <baskaryan@gmail.com>
2023-09-27 19:09:35 -0700

    update

--- docs/docs_skeleton/docusaurus.config.js
+++ docs/docs_skeleton/docusaurus.config.js
@@ -170,9 +170,19 @@ const config = {
             label: "Integrations",
           },
           {
-            href: "https://api.python.langchain.com",
+            to: "https://api.python.langchain.com",
             label: "API",
             position: "left",
+          },
+          {
+            to: "/docs/community",
+            label: "Community",
+            position: "left",
+          },
+          {
+            to: "https://chat.langchain.com",
+            label: "Chat LC",
+            position: "right",
           },
           {
             to: "https://smith.langchain.com",
