29c96a0e31909b63ad70c31c4dc78cfae085cc7f
Nuno Campos <nuno@boringbits.io>
2023-09-28 12:45:14 +0100

    Lint

--- libs/langchain/langchain/cli/create_repo/templates/repo/tests/test_chain.py
+++ libs/langchain/langchain/cli/create_repo/templates/repo/tests/test_chain.py
@@ -1,10 +1,10 @@
-from langchain.llms import HumanInputLLM
-
 from ____project_name_identifier import get_chain
 
 
 def test_my_chain() -> None:
     """Edit this test to test your chain."""
+    from langchain.llms import HumanInputLLM
+
     llm = HumanInputLLM(input_func=lambda: "baz")
     chain = get_chain(llm)
     chain.invoke({"text": "foo"})
