f6f1a9f0485811163830eea42914626265c31e03
Trayan Azarov <trayan.azarov@amikos.tech>
2023-09-29 04:55:07 +0300

    feat: ChromaDB Batching Support
    Note: As of 0.4.10 chroma introduced checks for max_batch_size and will throw an error if the underlying SQLite does not support the size of user's batch. This PR checks for chroma version and if >=0.4.10 adds batch splitting support

--- libs/langchain/langchain/vectorstores/chroma.py
+++ libs/langchain/langchain/vectorstores/chroma.py
@@ -558,14 +558,17 @@ class Chroma(VectorStore):
             )
         embeddings = self._embedding_function.embed_documents(text)
 
-        if hasattr(self._collection._client, "max_batch_size"): # for Chroma 0.4.10 and above
+        if hasattr(
+            self._collection._client, "max_batch_size"
+        ):  # for Chroma 0.4.10 and above
             from chromadb.utils.batch_utils import create_batches
+
             for batch in create_batches(
-                    api=self._collection._client,
-                    ids=ids,
-                    metadatas=metadata,
-                    documents=text,
-                    embeddings=embeddings,
+                api=self._collection._client,
+                ids=ids,
+                metadatas=metadata,
+                documents=text,
+                embeddings=embeddings,
             ):
                 self._collection.update(
                     ids=batch[0],
@@ -623,15 +626,22 @@ class Chroma(VectorStore):
             collection_metadata=collection_metadata,
             **kwargs,
         )
-        if hasattr(chroma_collection._client, "max_batch_size"): # for Chroma 0.4.10 and above
+        if hasattr(
+            chroma_collection._client, "max_batch_size"
+        ):  # for Chroma 0.4.10 and above
             from chromadb.utils.batch_utils import create_batches
+
             for batch in create_batches(
-                    api=chroma_collection._client,
-                    ids=ids,
-                    metadatas=metadatas,
-                    documents=texts,
+                api=chroma_collection._client,
+                ids=ids,
+                metadatas=metadatas,
+                documents=texts,
             ):
-                chroma_collection.add_texts(texts=batch[3], metadatas=batch[2], ids=batch[0])
+                chroma_collection.add_texts(
+                    texts=batch[3] if batch[3] else [],
+                    metadatas=batch[2] if batch[2] else None,
+                    ids=batch[0],
+                )
         else:
             chroma_collection.add_texts(texts=texts, metadatas=metadatas, ids=ids)
         return chroma_collection
