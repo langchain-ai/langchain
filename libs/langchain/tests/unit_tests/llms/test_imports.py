534fc79b5cce71f5efbd33214ce00ae022d806f7
Harrison Chase <hw.chase.17@gmail.com>
2023-09-29 13:48:48 -0700

    make llm imports optional

--- libs/langchain/tests/unit_tests/llms/test_imports.py
+++ libs/langchain/tests/unit_tests/llms/test_imports.py
@@ -1,6 +1,7 @@
 from langchain import llms
 from langchain.llms.base import BaseLLM
 
+
 def test_all_imports() -> None:
     """Simple test to make sure all things can be imported."""
     for cls in llms.__all__:
