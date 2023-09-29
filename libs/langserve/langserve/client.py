2d2ad5df4826f8d96d9b3c4e76e4d13eb3184970
Eugene Yurtsev <eyurtsev@gmail.com>
2023-09-29 10:27:50 -0400

    x

--- libs/langserve/langserve/client.py
+++ libs/langserve/langserve/client.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import asyncio
-import json
 import weakref
 from concurrent.futures import ThreadPoolExecutor
 from typing import Any, AsyncIterator, Iterator, List, Optional, Sequence, Union
