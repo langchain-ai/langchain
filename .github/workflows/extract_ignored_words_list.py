b54f2e8368a39123d61ac9f8f5670721190f72b4
CG80499 <cg80492@gmail.com>
2023-10-02 21:56:29 +0000

    test ci

--- .github/workflows/extract_ignored_words_list.py
+++ .github/workflows/extract_ignored_words_list.py
@@ -1,6 +1,6 @@
 import toml
 
-pyproject_toml = toml.load("pyproject.toml")
+pyproject_toml = toml.load("lib/langchain/pyproject.toml")
 
 # Extract the ignore words list (adjust the key as per your TOML structure)
 ignore_words_list = pyproject_toml.get("tool", {}).get("codespell", {}).get("ignore-words-list")
