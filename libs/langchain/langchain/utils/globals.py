30f8dbc0d5cd7144c7d89ac023c0d2a40cad697b
Predrag Gruevski <obi1kenobi82@gmail.com>
2023-10-02 22:14:59 +0000

    Add warnings to the repo.

--- libs/langchain/langchain/utils/globals.py
+++ libs/langchain/langchain/utils/globals.py
@@ -14,6 +14,12 @@ def set_verbose(value: bool) -> None:
     """Set a new value for the `verbose` global setting."""
     import langchain
 
+    # N.B.: This is a workaround for an unfortunate quirk of Python's
+    #       module-level `__getattr__()` implementation:
+    # https://github.com/langchain-ai/langchain/pull/11311#issuecomment-1743780004
+    #
+    # Remove it once `langchain.verbose` is no longer supported, and once all users
+    # have migrated to using `set_verbose()` here.
     langchain.verbose = value
 
     global verbose
@@ -24,6 +30,12 @@ def set_debug(value: bool) -> None:
     """Set a new value for the `debug` global setting."""
     import langchain
 
+    # N.B.: This is a workaround for an unfortunate quirk of Python's
+    #       module-level `__getattr__()` implementation:
+    # https://github.com/langchain-ai/langchain/pull/11311#issuecomment-1743780004
+    #
+    # Remove it once `langchain.debug` is no longer supported, and once all users
+    # have migrated to using `set_debug()` here.
     langchain.debug = value
 
     global debug
@@ -34,6 +46,12 @@ def set_llm_cache(value: "BaseCache") -> None:
     """Set a new LLM cache, overwriting the previous value, if any."""
     import langchain
 
+    # N.B.: This is a workaround for an unfortunate quirk of Python's
+    #       module-level `__getattr__()` implementation:
+    # https://github.com/langchain-ai/langchain/pull/11311#issuecomment-1743780004
+    #
+    # Remove it once `langchain.llm_cache` is no longer supported, and once all users
+    # have migrated to using `set_llm_cache()` here.
     langchain.llm_cache = value
 
     global llm_cache
