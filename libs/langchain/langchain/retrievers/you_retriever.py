6b00b35736027abf9c35b2093dcf364e775a27b9
mrbean <sam@you.com>
2023-10-02 17:21:07 -0400

    more comment updates

--- libs/langchain/langchain/retrievers/you_retriever.py
+++ libs/langchain/langchain/retrievers/you_retriever.py
@@ -10,8 +10,8 @@ class YouRetriever(BaseRetriever):
     """`You` retriever that uses You.com's search API.
 
     To connect to the You.com api requires an API key which
-    you can get at https://documentation.you.com.
-    You can also find our swagger docs there!
+    you can get by emailing api@you.com.
+    You can check out our docs at https://documentation.you.com.
 
     You need to set the environment variable `YDC_API_KEY` for retriever to operate.
     """
