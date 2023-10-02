f266fba7bb9bcf57f5aa282e11317737fe926f7c
Predrag Gruevski <obi1kenobi82@gmail.com>
2023-10-02 21:43:12 +0000

    Workaround for duplicated attributes depending on how they are imported.

--- libs/langchain/langchain/utils/globals.py
+++ libs/langchain/langchain/utils/globals.py
@@ -12,17 +12,26 @@ llm_cache: Optional["BaseCache"] = None
 
 def set_verbose(value: bool) -> None:
     """Set a new value for the `verbose` global setting."""
+    import langchain
+    langchain.verbose = value
+
     global verbose
     verbose = value
 
 
 def set_debug(value: bool) -> None:
     """Set a new value for the `debug` global setting."""
+    import langchain
+    langchain.debug = value
+
     global debug
     debug = value
 
 
-def replace_llm_cache(value: "BaseCache") -> None:
+def set_llm_cache(value: "BaseCache") -> None:
     """Set a new LLM cache, overwriting the previous value, if any."""
+    import langchain
+    langchain.llm_cache = value
+
     global llm_cache
     llm_cache = value
