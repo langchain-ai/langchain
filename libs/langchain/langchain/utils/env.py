119dd34738cc26d9f7ecbcb27ad61541e0ce7bc9
Erlich Sefi <erlichsefi@gmail.com>
2023-10-03 06:58:21 +0000

    fix bug when data[key] is False

--- libs/langchain/langchain/utils/env.py
+++ libs/langchain/langchain/utils/env.py
@@ -6,7 +6,7 @@ def get_from_dict_or_env(
     data: Dict[str, Any], key: str, env_key: str, default: Optional[str] = None
 ) -> str:
     """Get a value from a dictionary or an environment variable."""
-    if key in data and data[key]:
+    if key in data and data[key] is not None:
         return data[key]
     else:
         return get_from_env(key, env_key, default=default)
