5c85e61f00051fbb0e97d38398087570694dd3f7
Kastan Day <kastanvday@gmail.com>
2023-10-03 14:47:30 -0700

    further revert changes to human tool

--- libs/langchain/langchain/tools/human/tool.py
+++ libs/langchain/langchain/tools/human/tool.py
@@ -5,7 +5,6 @@ from typing import Callable, Optional
 from langchain.callbacks.manager import CallbackManagerForToolRun
 from langchain.pydantic_v1 import Field
 from langchain.tools.base import BaseTool
-from pydantic import Field
 
 
 def _print_func(text: str) -> None:
