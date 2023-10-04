1888ddce6a817418c9e4d337864955c0def7ca10
jacoblee93 <jacoblee93@gmail.com>
2023-10-04 16:56:08 -0700

    Add default async implementation for document compressors

--- libs/langchain/langchain/retrievers/document_compressors/base.py
+++ libs/langchain/langchain/retrievers/document_compressors/base.py
@@ -1,4 +1,6 @@
+import asyncio
 from abc import ABC, abstractmethod
+from functools import partial
 from inspect import signature
 from typing import List, Optional, Sequence, Union
 
@@ -19,7 +21,6 @@ class BaseDocumentCompressor(BaseModel, ABC):
     ) -> Sequence[Document]:
         """Compress retrieved documents given the query context."""
 
-    @abstractmethod
     async def acompress_documents(
         self,
         documents: Sequence[Document],
@@ -27,6 +28,9 @@ class BaseDocumentCompressor(BaseModel, ABC):
         callbacks: Optional[Callbacks] = None,
     ) -> Sequence[Document]:
         """Compress retrieved documents given the query context."""
+        return await asyncio.get_running_loop().run_in_executor(
+            None, partial(self.compress_documents), documents, query, callbacks
+        )
 
 
 class DocumentCompressorPipeline(BaseDocumentCompressor):
