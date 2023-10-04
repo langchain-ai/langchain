8e84714840f5d8783d9caece77015918dd729a2d
Eugene Yurtsev <eyurtsev@gmail.com>
2023-10-04 13:41:14 -0400

    x

--- libs/langchain/tests/unit_tests/test_dependencies.py
+++ libs/langchain/tests/unit_tests/test_dependencies.py
@@ -49,7 +49,6 @@ def test_required_dependencies(poetry_conf: Mapping[str, Any]) -> None:
         "python",
         "requests",
         "tenacity",
-        "typer",
     ]
 
     unrequired_dependencies = [
