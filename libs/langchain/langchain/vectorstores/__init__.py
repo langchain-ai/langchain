22afa3e0550d38f699ac0e44e67c4672023473d9
nuric <nuric@users.noreply.github.com>
2023-10-06 15:35:29 +0000

    Add SemaDB VST wrapper

--- libs/langchain/langchain/vectorstores/__init__.py
+++ libs/langchain/langchain/vectorstores/__init__.py
@@ -283,6 +283,12 @@ def _import_scann() -> Any:
     return ScaNN
 
 
+def _import_semadb() -> Any:
+    from langchain.vectorstores.semadb import SemaDB
+
+    return SemaDB
+
+
 def _import_singlestoredb() -> Any:
     from langchain.vectorstores.singlestoredb import SingleStoreDB
 
@@ -470,6 +476,8 @@ def __getattr__(name: str) -> Any:
         return _import_rocksetdb()
     elif name == "ScaNN":
         return _import_scann()
+    elif name == "SemaDB":
+        return _import_semadb()
     elif name == "SingleStoreDB":
         return _import_singlestoredb()
     elif name == "SKLearnVectorStore":
@@ -559,6 +567,7 @@ __all__ = [
     "Rockset",
     "SKLearnVectorStore",
     "ScaNN",
+    "SemaDB",
     "SingleStoreDB",
     "SingleStoreDB",
     "SQLiteVSS",
