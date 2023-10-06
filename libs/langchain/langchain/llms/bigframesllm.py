c5c97481aa077095e58e1b5ec7ede3db8eb37ecf
Ashley Xu <139821907+ashleyxuu@users.noreply.github.com>
2023-10-06 21:57:04 +0000

    add DataFrame support and BigFramesChain

--- libs/langchain/langchain/llms/bigframesllm.py
+++ libs/langchain/langchain/llms/bigframesllm.py
@@ -155,189 +155,202 @@ class BigFramesLLM(BaseLLM):
         for result in results_pd:
             generations.append([Generation(text=result)])
         return LLMResult(generations=generations)
+
+
+    def _generate_df(
+        self,
+        prompts: Union[List[str], bf.DataFrame, bf.Series],
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+        **kwargs: Any,
+    ) -> bf.DataFrame:
+        """Run the LLM on the given prompt and input."""
+        if isinstance(prompts, List):
+            prompts_df = bigframes.pandas.DataFrame({"index": prompts})
+        else:
+            prompts_df = prompts
+        responses_df = self.client.predict(
+            X=prompts_df,
+            temperature=self.temperature,
+            max_output_tokens=self.max_output_tokens,
+            top_k=self.top_k,
+            top_p=self.top_p,
+        )
+        return responses_df
     
 
-    # def generate_prompt(
-    #     self,
-    #     prompts: List[PromptValue],
-    #     stop: Optional[List[str]] = None,
-    #     callbacks: Optional[Union[Callbacks, List[Callbacks]]] = None,
-    #     **kwargs: Any,
-    # ) -> bf.DataFrame:
-    #     prompt_strings = [p.to_string() for p in prompts]
-    #     return self.generate(prompt_strings, stop=stop, callbacks=callbacks, **kwargs)
+    def generate_df_prompt(
+        self,
+        prompts: Union[List[PromptValue], bf.DataFrame, bf.Series],
+        stop: Optional[List[str]] = None,
+        callbacks: Optional[Union[Callbacks, List[Callbacks]]] = None,
+        **kwargs: Any,
+    ) -> bf.DataFrame:
+        if not isinstance(prompts, (bf.DataFrame, bf.Series)):
+            prompt_strings = [p.to_string() for p in prompts]
+            return self.generate_df(prompt_strings, stop=stop, callbacks=callbacks, **kwargs)
+        return self._generate_df(prompts=prompts)
     
 
-    # def _generate_helper(
-    #     self,
-    #     prompts: List[str],
-    #     stop: Optional[List[str]],
-    #     run_managers: List[CallbackManagerForLLMRun],
-    #     new_arg_supported: bool,
-    #     **kwargs: Any,
-    # ) -> bf.DataFrame:
-    #     try:
-    #         output = (
-    #             self._generate(
-    #                 prompts,
-    #                 stop=stop,
-    #                 # TODO: support multiple run managers
-    #                 run_manager=run_managers[0] if run_managers else None,
-    #                 **kwargs,
-    #             )
-    #             if new_arg_supported
-    #             else self._generate(prompts, stop=stop)
-    #         )
-    #     except BaseException as e:
-    #         for run_manager in run_managers:
-    #             run_manager.on_llm_error(e)
-    #         raise e
-    #     # flattened_outputs = output.flatten()
-    #     # for manager, flattened_output in zip(run_managers, flattened_outputs):
-    #     #     manager.on_llm_end(flattened_output)
-    #     if run_managers:
-    #         output.run = [
-    #             RunInfo(run_id=run_manager.run_id) for run_manager in run_managers
-    #         ]
-    #     return output
+    def _generate_df_helper(
+        self,
+        prompts: List[str],
+        stop: Optional[List[str]],
+        run_managers: List[CallbackManagerForLLMRun],
+        new_arg_supported: bool,
+        **kwargs: Any,
+    ) -> bf.DataFrame:
+        try:
+            output = (
+                self._generate_df(
+                    prompts,
+                    stop=stop,
+                    # TODO: support multiple run managers
+                    run_manager=run_managers[0] if run_managers else None,
+                    **kwargs,
+                )
+                if new_arg_supported
+                else self._generate_df(prompts, stop=stop)
+            )
+        except BaseException as e:
+            for run_manager in run_managers:
+                run_manager.on_llm_error(e)
+            raise e
+        if run_managers:
+            output.run = [
+                RunInfo(run_id=run_manager.run_id) for run_manager in run_managers
+            ]
+        return output
 
-    # def generate(
-    #     self,
-    #     prompts: List[str],
-    #     stop: Optional[List[str]] = None,
-    #     callbacks: Optional[Union[Callbacks, List[Callbacks]]] = None,
-    #     *,
-    #     tags: Optional[Union[List[str], List[List[str]]]] = None,
-    #     metadata: Optional[Union[Dict[str, Any], List[Dict[str, Any]]]] = None,
-    #     run_name: Optional[Union[str, List[str]]] = None,
-    #     **kwargs: Any,
-    # ) -> bf.DataFrame:
-    #     """Run the LLM on the given prompt and input."""
-    #     if not isinstance(prompts, list):
-    #         raise ValueError(
-    #             "Argument 'prompts' is expected to be of type List[str], received"
-    #             f" argument of type {type(prompts)}."
-    #         )
-    #     # Create callback managers
-    #     if (
-    #         isinstance(callbacks, list)
-    #         and callbacks
-    #         and (
-    #             isinstance(callbacks[0], (list, BaseCallbackManager))
-    #             or callbacks[0] is None
-    #         )
-    #     ):
-    #         # We've received a list of callbacks args to apply to each input
-    #         assert len(callbacks) == len(prompts)
-    #         assert tags is None or (
-    #             isinstance(tags, list) and len(tags) == len(prompts)
-    #         )
-    #         assert metadata is None or (
-    #             isinstance(metadata, list) and len(metadata) == len(prompts)
-    #         )
-    #         assert run_name is None or (
-    #             isinstance(run_name, list) and len(run_name) == len(prompts)
-    #         )
-    #         callbacks = cast(List[Callbacks], callbacks)
-    #         tags_list = cast(List[Optional[List[str]]], tags or ([None] * len(prompts)))
-    #         metadata_list = cast(
-    #             List[Optional[Dict[str, Any]]], metadata or ([{}] * len(prompts))
-    #         )
-    #         run_name_list = run_name or cast(
-    #             List[Optional[str]], ([None] * len(prompts))
-    #         )
-    #         callback_managers = [
-    #             CallbackManager.configure(
-    #                 callback,
-    #                 self.callbacks,
-    #                 self.verbose,
-    #                 tag,
-    #                 self.tags,
-    #                 meta,
-    #                 self.metadata,
-    #             )
-    #             for callback, tag, meta in zip(callbacks, tags_list, metadata_list)
-    #         ]
-    #     else:
-    #         # We've received a single callbacks arg to apply to all inputs
-    #         callback_managers = [
-    #             CallbackManager.configure(
-    #                 cast(Callbacks, callbacks),
-    #                 self.callbacks,
-    #                 self.verbose,
-    #                 cast(List[str], tags),
-    #                 self.tags,
-    #                 cast(Dict[str, Any], metadata),
-    #                 self.metadata,
-    #             )
-    #         ] * len(prompts)
-    #         run_name_list = [cast(Optional[str], run_name)] * len(prompts)
 
-    #     params = self.dict()
-    #     params["stop"] = stop
-    #     options = {"stop": stop}
-    #     (
-    #         existing_prompts,
-    #         llm_string,
-    #         missing_prompt_idxs,
-    #         missing_prompts,
-    #     ) = get_prompts(params, prompts)
-    #     disregard_cache = self.cache is not None and not self.cache
-    #     new_arg_supported = inspect.signature(self._generate).parameters.get(
-    #         "run_manager"
-    #     )
-    #     new_arg_supported = False
+    def generate_df(
+        self,
+        prompts: Union[List[str], bf.DataFrame, bf.Series],
+        stop: Optional[List[str]] = None,
+        callbacks: Optional[Union[Callbacks, List[Callbacks]]] = None,
+        *,
+        tags: Optional[Union[List[str], List[List[str]]]] = None,
+        metadata: Optional[Union[Dict[str, Any], List[Dict[str, Any]]]] = None,
+        run_name: Optional[Union[str, List[str]]] = None,
+        **kwargs: Any,
+    ) -> bf.DataFrame:
+        """Run the LLM on the given prompt and input."""
+        if not isinstance(prompts, (list, bf.DataFrame, bf.Series)):
+            raise ValueError(
+                "Argument 'prompts' is expected to be of type List[str], bf.DataFrame"
+                f", bf.Series, but received"
+                f" argument of type {type(prompts)}."
+            )
+        # Create callback managers
+        if (
+            isinstance(callbacks, list)
+            and callbacks
+            and (
+                isinstance(callbacks[0], (list, BaseCallbackManager))
+                or callbacks[0] is None
+            )
+        ):
+            # We've received a list of callbacks args to apply to each input
+            assert len(callbacks) == len(prompts)
+            assert tags is None or (
+                isinstance(tags, list) and len(tags) == len(prompts)
+            )
+            assert metadata is None or (
+                isinstance(metadata, list) and len(metadata) == len(prompts)
+            )
+            assert run_name is None or (
+                isinstance(run_name, list) and len(run_name) == len(prompts)
+            )
+            callbacks = cast(List[Callbacks], callbacks)
+            tags_list = cast(List[Optional[List[str]]], tags or ([None] * len(prompts)))
+            metadata_list = cast(
+                List[Optional[Dict[str, Any]]], metadata or ([{}] * len(prompts))
+            )
+            run_name_list = run_name or cast(
+                List[Optional[str]], ([None] * len(prompts))
+            )
+            callback_managers = [
+                CallbackManager.configure(
+                    callback,
+                    self.callbacks,
+                    self.verbose,
+                    tag,
+                    self.tags,
+                    meta,
+                    self.metadata,
+                )
+                for callback, tag, meta in zip(callbacks, tags_list, metadata_list)
+            ]
+        else:
+            # We've received a single callbacks arg to apply to all inputs
+            callback_managers = [
+                CallbackManager.configure(
+                    cast(Callbacks, callbacks),
+                    self.callbacks,
+                    self.verbose,
+                    cast(List[str], tags),
+                    self.tags,
+                    cast(Dict[str, Any], metadata),
+                    self.metadata,
+                )
+            ] * len(prompts)
+            run_name_list = [cast(Optional[str], run_name)] * len(prompts)
 
-    #     if langchain.llm_cache is None or disregard_cache:
-    #         if self.cache is not None and self.cache:
-    #             raise ValueError(
-    #                 "Asked to cache, but no cache found at `langchain.cache`."
-    #             )
-    #         run_managers = [
-    #             callback_manager.on_llm_start(
-    #                 dumpd(self),
-    #                 [prompt],
-    #                 invocation_params=params,
-    #                 options=options,
-    #                 name=run_name,
-    #             )[0]
-    #             for callback_manager, prompt, run_name in zip(
-    #                 callback_managers, prompts, run_name_list
-    #             )
-    #         ]
-    #         output = self._generate_helper(
-    #             prompts, stop, run_managers, bool(new_arg_supported), **kwargs
-    #         )
-    #         return output
-    #     if len(missing_prompts) > 0:
-    #         run_managers = [
-    #             callback_managers[idx].on_llm_start(
-    #                 dumpd(self),
-    #                 [prompts[idx]],
-    #                 invocation_params=params,
-    #                 options=options,
-    #                 name=run_name_list[idx],
-    #             )[0]
-    #             for idx in missing_prompt_idxs
-    #         ]
-    #         results = self._generate_helper(
-    #             missing_prompts, stop, run_managers, bool(new_arg_supported), **kwargs
-    #         )
-    #         # llm_output = update_cache(
-    #         #     existing_prompts, llm_string, missing_prompt_idxs, new_results, prompts
-    #         # )
-    #         # run_info = (
-    #         #     [RunInfo(run_id=run_manager.run_id) for run_manager in run_managers]
-    #         #     if run_managers
-    #         #     else None
-    #         # )
-    #     else:
-    #         llm_output = {}
-    #         # run_info = None
-    #         results = bf.DataFrame()
-    #     return results
-    #     # generations = [existing_prompts[i] for i in range(len(prompts))]
-    #     # return LLMResult(generations=generations, llm_output=llm_output, run=run_info)
+        params = self.dict()
+        params["stop"] = stop
+        options = {"stop": stop}
+        (
+            existing_prompts,
+            llm_string,
+            missing_prompt_idxs,
+            missing_prompts,
+        ) = get_prompts(params, prompts)
+        disregard_cache = self.cache is not None and not self.cache
+        new_arg_supported = inspect.signature(self._generate).parameters.get(
+            "run_manager"
+        )
+        new_arg_supported = False
+
+        if langchain.llm_cache is None or disregard_cache:
+            if self.cache is not None and self.cache:
+                raise ValueError(
+                    "Asked to cache, but no cache found at `langchain.cache`."
+                )
+            run_managers = [
+                callback_manager.on_llm_start(
+                    dumpd(self),
+                    [prompt],
+                    invocation_params=params,
+                    options=options,
+                    name=run_name,
+                )[0]
+                for callback_manager, prompt, run_name in zip(
+                    callback_managers, prompts, run_name_list
+                )
+            ]
+            output = self._generate_df_helper(
+                prompts, stop, run_managers, bool(new_arg_supported), **kwargs
+            )
+            return output
+        if len(missing_prompts) > 0:
+            run_managers = [
+                callback_managers[idx].on_llm_start(
+                    dumpd(self),
+                    [prompts[idx]],
+                    invocation_params=params,
+                    options=options,
+                    name=run_name_list[idx],
+                )[0]
+                for idx in missing_prompt_idxs
+            ]
+            results = self._generate_df_helper(
+                missing_prompts, stop, run_managers, bool(new_arg_supported), **kwargs
+            )
+        else:
+            llm_output = {}
+            results = bf.DataFrame()
+        return results
+
 
     @property
     def _llm_type(self) -> str:
