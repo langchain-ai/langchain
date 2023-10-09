1af7da576faeb95903e531f9c15659f74619cc20
Predrag Gruevski <obi1kenobi82@gmail.com>
2023-10-09 15:21:21 +0000

    Fix type hint.

--- libs/langchain/langchain/globals/__init__.py
+++ libs/langchain/langchain/globals/__init__.py
@@ -146,7 +146,7 @@ def set_llm_cache(value: "BaseCache") -> None:
     _llm_cache = value
 
 
-def get_llm_cache() -> bool:
+def get_llm_cache() -> "BaseCache":
     """Get the value of the `llm_cache` global setting."""
     import langchain
 
