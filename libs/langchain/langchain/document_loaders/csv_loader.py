fb6c1d5edbd97f6d91a57c1cb7c52a32b2df71cf
Ben Chello <bchello@dropbox.com>
2023-10-09 16:43:17 -0400

    Rebase and update

--- libs/langchain/langchain/document_loaders/csv_loader.py
+++ libs/langchain/langchain/document_loaders/csv_loader.py
@@ -89,9 +89,9 @@ class CSVLoader(BaseLoader):
 
     def __read_file(self, csvfile: TextIOWrapper) -> List[Document]:
         docs = []
+
         csv_reader = csv.DictReader(csvfile, **self.csv_args)  # type: ignore
         for i, row in enumerate(csv_reader):
-            content = "\n".join(f"{k.strip()}: {v.strip()}" for k, v in row.items())
             try:
                 source = (
                     row[self.source_column]
@@ -103,39 +103,23 @@ class CSVLoader(BaseLoader):
                     f"Source column '{self.source_column}' not found in CSV file."
                 )
             metadata = {"source": source, "row": i}
+            if self.metadata_columns is not None:
+                contents = {
+                    k: v for k, v in row.items() if k not in self.metadata_columns
+                }.items()
+                for col in self.metadata_columns:
+                    try:
+                        metadata[col] = row[col]
+                    except KeyError:
+                        raise ValueError(
+                            f"Metadata column '{col}' not found in CSV file."
+                        )
+            else:
+                contents = row.items()
+
+            content = "\n".join(f"{k.strip()}: {v.strip()}" for k, v in contents)
             doc = Document(page_content=content, metadata=metadata)
             docs.append(doc)
-        with open(self.file_path, newline="", encoding=self.encoding) as csvfile:
-            csv_reader = csv.DictReader(csvfile, **self.csv_args)  # type: ignore
-            for i, row in enumerate(csv_reader):
-                try:
-                    source = (
-                        row[self.source_column]
-                        if self.source_column is not None
-                        else self.file_path
-                    )
-                except KeyError:
-                    raise ValueError(
-                        f"Source column '{self.source_column}' not found in CSV file."
-                    )
-                metadata = {"source": source, "row": i}
-                if self.metadata_columns is not None:
-                    contents = {
-                        k: v for k, v in row.items() if k not in self.metadata_columns
-                    }.items()
-                    for col in self.metadata_columns:
-                        try:
-                            metadata[col] = row[col]
-                        except KeyError:
-                            raise ValueError(
-                                f"Metadata column '{col}' not found in CSV file."
-                            )
-                else:
-                    contents = row.items()
-                content = "\n".join(f"{k.strip()}: {v.strip()}" for k, v in contents)
-
-                doc = Document(page_content=content, metadata=metadata)
-                docs.append(doc)
 
         return docs
 
