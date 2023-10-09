5a428de73ddf1562c74ae47d93ac7d0488607ccb
Taikono-Himazin <kazu@po.harenet.ne.jp>
2023-10-10 00:06:43 +0900

    Added autodetect_encoding option to csvLoader (#11327)

--- libs/langchain/tests/unit_tests/document_loaders/test_detect_encoding.py
+++ libs/langchain/tests/unit_tests/document_loaders/test_detect_encoding.py
@@ -2,12 +2,12 @@ from pathlib import Path
 
 import pytest
 
-from langchain.document_loaders import DirectoryLoader, TextLoader
+from langchain.document_loaders import CSVLoader, DirectoryLoader, TextLoader
 from langchain.document_loaders.helpers import detect_file_encodings
 
 
 @pytest.mark.requires("chardet")
-def test_loader_detect_encoding() -> None:
+def test_loader_detect_encoding_text() -> None:
     """Test text loader."""
     path = Path(__file__).parent.parent / "examples"
     files = path.glob("**/*.txt")
@@ -16,7 +16,7 @@ def test_loader_detect_encoding() -> None:
         str(path),
         glob="**/*.txt",
         loader_kwargs={"autodetect_encoding": True},
-        loader_cls=TextLoader,
+        loader_cls=TextLoader,  # type: ignore
     )
 
     with pytest.raises((UnicodeDecodeError, RuntimeError)):
@@ -24,6 +24,43 @@ def test_loader_detect_encoding() -> None:
 
     docs = loader_detect_encoding.load()
     assert len(docs) == len(list(files))
+
+
+@pytest.mark.requires("chardet")
+def test_loader_detect_encoding_csv() -> None:
+    """Test csv loader."""
+    path = Path(__file__).parent.parent / "examples"
+    files = path.glob("**/*.csv")
+
+    # Count the number of lines.
+    row_count = 0
+    for file in files:
+        encodings = detect_file_encodings(str(file))
+        for encoding in encodings:
+            try:
+                row_count += sum(1 for line in open(file, encoding=encoding.encoding))
+                break
+            except UnicodeDecodeError:
+                continue
+        # CSVLoader uses DictReader, and one line per file is a header,
+        # so subtract the number of files.
+        row_count -= 1
+
+    loader = DirectoryLoader(
+        str(path), glob="**/*.csv", loader_cls=CSVLoader  # type: ignore
+    )
+    loader_detect_encoding = DirectoryLoader(
+        str(path),
+        glob="**/*.csv",
+        loader_kwargs={"autodetect_encoding": True},
+        loader_cls=CSVLoader,  # type: ignore
+    )
+
+    with pytest.raises((UnicodeDecodeError, RuntimeError)):
+        loader.load()
+
+    docs = loader_detect_encoding.load()
+    assert len(docs) == row_count
 
 
 @pytest.mark.skip(reason="slow test")
