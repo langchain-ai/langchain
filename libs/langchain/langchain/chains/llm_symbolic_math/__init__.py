00407adf9a200f19795099fac5a282eb57480cc0
Eugene Yurtsev <eyurtsev@gmail.com>
2023-10-10 12:33:51 -0400

    Deprecate LLMSymbolicMath from langchain core (#11615)
    Deprecate LLMSymbolicMath from langchain core package.

--- libs/langchain/langchain/chains/llm_symbolic_math/__init__.py
+++ libs/langchain/langchain/chains/llm_symbolic_math/__init__.py
@@ -1,14 +1,12 @@
-"""Chain that interprets a prompt and executes python code to do math.
+def raise_on_import() -> None:
+    """Raise an error on import since is deprecated."""
+    raise ImportError(
+        "This module has been moved to langchain-experimental. "
+        "For more details: https://github.com/langchain-ai/langchain/discussions/11352."
+        "To access this code, install it with `pip install langchain-experimental`."
+        "`from langchain_experimental.llm_symbolic_math.base "
+        "import LLMSymbolicMathChain`"
+    )
 
-Heavily borrowed from llm_math, wrapper for SymPy
-"""
-from langchain._api import warn_deprecated
 
-warn_deprecated(
-    since="0.0.304",
-    message=(
-        "On 2023-10-06 this module will be moved to langchain-experimental as "
-        "it relies on sympy https://github.com/sympy/sympy/issues/10805"
-    ),
-    pending=True,
-)
+raise_on_import()
