3757faef3251a84311182836c24f8317a137ead1
Eric Hartford <ehartford@gmail.com>
2023-10-11 14:33:16 -0500

    fix style error

--- libs/langchain/langchain/document_loaders/parsers/language/language_parser.py
+++ libs/langchain/langchain/document_loaders/parsers/language/language_parser.py
@@ -3,9 +3,9 @@ from typing import Any, Dict, Iterator, Optional
 from langchain.docstore.document import Document
 from langchain.document_loaders.base import BaseBlobParser
 from langchain.document_loaders.blob_loaders import Blob
+from langchain.document_loaders.parsers.language.cobol import CobolSegmenter
 from langchain.document_loaders.parsers.language.javascript import JavaScriptSegmenter
 from langchain.document_loaders.parsers.language.python import PythonSegmenter
-from langchain.document_loaders.parsers.language.cobol import CobolSegmenter
 from langchain.text_splitter import Language
 
 LANGUAGE_EXTENSIONS: Dict[str, str] = {
