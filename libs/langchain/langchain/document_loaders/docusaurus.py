865617d2ad6983a008ecb5d7bd4042fda52ef79f
leedotpang <leedotpang@gmail.com>
2023-10-13 18:24:44 -0400

    Lint and deps fix

--- libs/langchain/langchain/document_loaders/docusaurus.py
+++ libs/langchain/langchain/document_loaders/docusaurus.py
@@ -1,19 +1,19 @@
 """Load Documents from Docusarus Documentation"""
 from typing import Any, List, Optional
+
 from bs4 import BeautifulSoup
 
 from langchain.docstore.document import Document
-from langchain.document_loaders import SitemapLoader
 from langchain.document_loaders.base import BaseLoader
 
 
 class DocusaurusLoader(BaseLoader):
     """
     Loader that leverages the SitemapLoader to loop through the generated pages of a
-    Docusaurus Documentation website and extracts the content by looking for specific HTML tags.
-    By default, the parse searches for the main content of the docusaurus page which is normally
-    the <article>. You also have the option to define your own custom HTML tags by providing them
-    as a list, for example: ["div", ".main", "a"].
+    Docusaurus Documentation website and extracts the content by looking for specific
+    HTML tags. By default, the parser searches for the main content of the Docusaurus
+    page, which is normally the <article>. You also have the option to define your own
+    custom HTML tags by providing them as a list, for example: ["div", ".main", "a"].
     """
 
     def __init__(
@@ -26,9 +26,9 @@ class DocusaurusLoader(BaseLoader):
         Initialize DocusaurusLoader
         Args:
             url: The base URL of the Docusaurus website.
-            custom_html_tags: Optional custom html tag to retrieve the content from pages.
-            kwargs: Additional args to extend SitemapLoader
-                (i.e. filter_urls, blocksize, blocknum, meta_function, is_local, continue_on_failure)
+            custom_html_tags: Optional custom html tags to extract content from pages.
+            kwargs: Additional args to extend the underlying SitemapLoader, for example:
+                filter_urls, blocksize, meta_function, is_local, continue_on_failure
         """
         self.url = url
         self.custom_html_tags = custom_html_tags
@@ -36,12 +36,18 @@ class DocusaurusLoader(BaseLoader):
 
     def load(self) -> List[Document]:
         """Load documents."""
-        loader = SitemapLoader(f"{self.url}/sitemap.xml", parsing_function=self._parsing_function, **self.kwargs)
+        from langchain.document_loaders.sitemap import SitemapLoader
+
+        loader = SitemapLoader(
+            f"{self.url}/sitemap.xml",
+            parsing_function=self._parsing_function,
+            **self.kwargs,
+        )
         return loader.load()
 
     def _parsing_function(self, content: BeautifulSoup) -> str:
         """Parses specific elements from a Docusarus page."""
-        relevant_elements = content.select(','.join(self.custom_html_tags))
+        relevant_elements = content.select(",".join(self.custom_html_tags))
 
         for element in relevant_elements:
             if element not in relevant_elements:
