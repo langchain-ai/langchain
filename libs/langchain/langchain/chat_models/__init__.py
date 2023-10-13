56653c53aae55f8638a673764c8784dca93718da
cloudscool <37284105+cloudscool@users.noreply.github.com>
2023-10-13 01:04:28 -0500

    New add Baichuan Model (#11714)
    Motivation and Context
    At present, the Baichuan Large Language Model is relatively popular and
    efficient in performance. Due to widespread market recognition, this
    model has been added to enhance the scalability of Langchain's ability
    to access the big language model, so as to facilitate application access
    and usage for interested users.
    
    System Info
    langchain： 0.0.295
    python：3.8.3
    IDE：vs code
    
    Description
    Add the following files:
    
    1. Add baichuan_baichuaninc_endpoint.py in the
    libs/langchain/langchain/chat_models
    2. Modify the __init__.py file,which is located in the
    libs/langchain/langchain/chat_models/__init__.py：
    a. Add "from langchain.chat_models.baichuan_baichuaninc_endpoint import
    BaichuanChatEndpoint"
        b. Add "BaichuanChatEndpoint" In the file's __ All__  method
    
    Your contribution
    I am willing to help implement this feature and submit a PR, but I would
    appreciate guidance from the maintainers or community to ensure the
    changes are made correctly and in line with the project's standards and
    practices.

--- libs/langchain/langchain/chat_models/__init__.py
+++ libs/langchain/langchain/chat_models/__init__.py
@@ -21,6 +21,7 @@ from langchain.chat_models.anthropic import ChatAnthropic
 from langchain.chat_models.anyscale import ChatAnyscale
 from langchain.chat_models.azure_openai import AzureChatOpenAI
 from langchain.chat_models.baidu_qianfan_endpoint import QianfanChatEndpoint
+from langchain.chat_models.baichuan_baichuaninc_endpoint import BaichuanChatEndpoint
 from langchain.chat_models.bedrock import BedrockChat
 from langchain.chat_models.cohere import ChatCohere
 from langchain.chat_models.ernie import ErnieBotChat
@@ -61,4 +62,5 @@ __all__ = [
     "ChatKonko",
     "QianfanChatEndpoint",
     "ChatFireworks",
+    "BaichuanChatEndpoint",
 ]
