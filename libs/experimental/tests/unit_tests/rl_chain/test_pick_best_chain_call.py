2ab00020c40871eba338441d286fe3062cc869b8
Predrag Gruevski <obi1kenobi82@gmail.com>
2023-10-17 20:52:52 +0000

    Fix invalid kwarg error pointed out by mypy.

--- libs/experimental/tests/unit_tests/rl_chain/test_pick_best_chain_call.py
+++ libs/experimental/tests/unit_tests/rl_chain/test_pick_best_chain_call.py
@@ -80,7 +80,7 @@ def test_update_with_delayed_score_with_auto_validator_throws() -> None:
     chain = pick_best_chain.PickBest.from_llm(
         llm=llm,
         prompt=PROMPT,
-        selection_scorer=rl_chain.AutoSelectionScorer(llm=auto_val_llm),
+        selection_scorer=rl_chain.AutoSelectionScorer(llm_chain=auto_val_llm),
         feature_embedder=pick_best_chain.PickBestFeatureEmbedder(
             auto_embed=False, model=MockEncoder()
         ),
@@ -107,7 +107,7 @@ def test_update_with_delayed_score_force() -> None:
     chain = pick_best_chain.PickBest.from_llm(
         llm=llm,
         prompt=PROMPT,
-        selection_scorer=rl_chain.AutoSelectionScorer(llm=auto_val_llm),
+        selection_scorer=rl_chain.AutoSelectionScorer(llm_chain=auto_val_llm),
         feature_embedder=pick_best_chain.PickBestFeatureEmbedder(
             auto_embed=False, model=MockEncoder()
         ),
@@ -380,7 +380,7 @@ def test_auto_scorer_with_user_defined_llm() -> None:
     chain = pick_best_chain.PickBest.from_llm(
         llm=llm,
         prompt=PROMPT,
-        selection_scorer=rl_chain.AutoSelectionScorer(llm=scorer_llm),
+        selection_scorer=rl_chain.AutoSelectionScorer(llm_chain=scorer_llm),
         feature_embedder=pick_best_chain.PickBestFeatureEmbedder(
             auto_embed=False, model=MockEncoder()
         ),
@@ -426,7 +426,7 @@ def test_activate_and_deactivate_scorer() -> None:
     chain = pick_best_chain.PickBest.from_llm(
         llm=llm,
         prompt=PROMPT,
-        selection_scorer=pick_best_chain.base.AutoSelectionScorer(llm=scorer_llm),
+        selection_scorer=pick_best_chain.base.AutoSelectionScorer(llm_chain=scorer_llm),
         feature_embedder=pick_best_chain.PickBestFeatureEmbedder(
             auto_embed=False, model=MockEncoder()
         ),
