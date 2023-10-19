f12c908134bc4db77d0ea40b774d7e664ed801d4
esiiol <ehsan.olfat@gmail.com>
2023-10-19 17:00:03 +0200

    Added new unit_tests for the new feature i.e. PDF bytes buffer parser

--- libs/langchain/tests/unit_tests/document_loaders/parsers/test_pdf_parsers.py
+++ libs/langchain/tests/unit_tests/document_loaders/parsers/test_pdf_parsers.py
@@ -1,12 +1,13 @@
 """Tests for the various PDF parsers."""
-from typing import Iterator
+from typing import Any, Iterator
 
 import pytest
 
-from langchain.document_loaders.base import BaseBlobParser
+from langchain.document_loaders.base import BaseBlobParser, BaseBytesParser
 from langchain.document_loaders.blob_loaders import Blob
 from langchain.document_loaders.parsers.pdf import (
     PDFMinerParser,
+    PyMuPDFBytesParser,
     PyMuPDFParser,
     PyPDFium2Parser,
     PyPDFParser,
@@ -53,6 +54,49 @@ def _assert_with_parser(parser: BaseBlobParser, splits_by_page: bool = True) -> 
         assert int(metadata["page"]) == 0
 
 
+def _assert_with_bytes_parser(parser: BaseBytesParser, splits_by_page: bool = True) -> None:
+    """Standard tests to verify that the given bytes parser works.
+
+    Args:
+        parser (BaseBytesParser): The parser to test.
+        splits_by_page (bool): Whether the parser splits by page or not by default.
+    """
+    import io
+
+    with open(HELLO_PDF, "rb") as f:
+        doc_generator = parser.lazy_parse(io.BytesIO(f.read()))
+
+    assert isinstance(doc_generator, Iterator)
+    docs = list(doc_generator)
+    assert len(docs) == 1
+    page_content = docs[0].page_content
+    assert isinstance(page_content, str)
+    # The different parsers return different amount of whitespace, so using
+    # startswith instead of equals.
+    assert docs[0].page_content.startswith("Hello world!")
+
+    with open(LAYOUT_PARSER_PAPER_PDF, "rb") as f:
+        doc_generator = parser.lazy_parse(io.BytesIO(f.read()))
+
+    assert isinstance(doc_generator, Iterator)
+    docs = list(doc_generator)
+
+    if splits_by_page:
+        assert len(docs) == 16
+    else:
+        assert len(docs) == 1
+    # Test is imprecise since the parsers yield different parse information depending
+    # on configuration. Each parser seems to yield a slightly different result
+    # for this page!
+    assert "LayoutParser" in docs[0].page_content
+    metadata = docs[0].metadata
+
+    assert metadata["total_pages"] == 16
+
+    if splits_by_page:
+        assert int(metadata["page"]) == 0
+
+
 @pytest.mark.requires("pypdf")
 def test_pypdf_parser() -> None:
     """Test PyPDF parser."""
@@ -72,6 +116,12 @@ def test_pymupdf_loader() -> None:
     _assert_with_parser(PyMuPDFParser())
 
 
+@pytest.mark.requires("fitz")  # package is PyMuPDF
+def test_pymupdfbytes_loader() -> None:
+    """Test PyMuPDF loader."""
+    _assert_with_bytes_parser(PyMuPDFBytesParser())
+
+
 @pytest.mark.requires("pypdfium2")
 def test_pypdfium2_parser() -> None:
     """Test PyPDFium2 parser."""
@@ -86,3 +136,9 @@ def test_extract_images_text_from_pdf() -> None:
     _assert_with_parser(PDFMinerParser(extract_images=True))
     _assert_with_parser(PyMuPDFParser(extract_images=True))
     _assert_with_parser(PyPDFium2Parser(extract_images=True))
+
+
+@pytest.mark.requires("rapidocr_onnxruntime")
+def test_extract_images_text_from_pdf_bytes() -> None:
+    """Test extract image from pdf bytes and recognize text with rapid ocr"""
+    _assert_with_bytes_parser(PyMuPDFBytesParser(extract_images=True))
