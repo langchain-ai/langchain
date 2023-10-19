29c0d099abeec68d89c0e188e8ff37ecd4e5758b
esiiol <ehsan.olfat@gmail.com>
2023-10-19 16:57:16 +0200

    Added PyMuPDFBytesParser
    This class is the parser for `PDF` bytes using `PyMuPDF`

--- libs/langchain/langchain/document_loaders/parsers/pdf.py
+++ libs/langchain/langchain/document_loaders/parsers/pdf.py
@@ -1,6 +1,7 @@
 """Module contains common parsers for PDFs."""
 from __future__ import annotations
 
+import io
 import warnings
 from typing import (
     TYPE_CHECKING,
@@ -16,7 +17,7 @@ from urllib.parse import urlparse
 
 import numpy as np
 
-from langchain.document_loaders.base import BaseBlobParser
+from langchain.document_loaders.base import BaseBlobParser, BaseBytesParser
 from langchain.document_loaders.blob_loaders import Blob
 from langchain.schema import Document
 
@@ -497,3 +498,65 @@ class DocumentIntelligenceParser(BaseBlobParser):
             docs = self._generate_docs(blob, result)
 
             yield from docs
+
+
+class PyMuPDFBytesParser(BaseBytesParser):
+    """Parse `PDF` bytes using `PyMuPDF`."""
+
+    def __init__(
+        self,
+        text_kwargs: Optional[Mapping[str, Any]] = None,
+        extract_images: bool = False,
+    ) -> None:
+        """Initialize the parser.
+
+        Args:
+            text_kwargs: Keyword arguments to pass to ``fitz.Page.get_text()``.
+        """
+        self.text_kwargs = text_kwargs or {}
+        self.extract_images = extract_images
+
+    def lazy_parse(self, stream: io.BytesIO) -> Iterator[Document]:
+        """Lazily parse the byte buffer."""
+        import fitz
+
+        doc = fitz.open(stream=stream)  # open document
+
+        yield from [
+            Document(
+                page_content=page.get_text(**self.text_kwargs)
+                + self._extract_images_from_page(doc, page),
+                metadata=dict(
+                    {
+                        "page": page.number,
+                        "total_pages": len(doc),
+                    },
+                    **{
+                        k: doc.metadata[k]
+                        for k in doc.metadata
+                        if type(doc.metadata[k]) in [str, int]
+                    },
+                ),
+            )
+            for page in doc
+        ]
+
+    def _extract_images_from_page(
+        self, doc: fitz.fitz.Document, page: fitz.fitz.Page
+    ) -> str:
+        """Extract images from page and get the text with RapidOCR."""
+        if not self.extract_images:
+            return ""
+        import fitz
+
+        img_list = page.get_images()
+        imgs = []
+        for img in img_list:
+            xref = img[0]
+            pix = fitz.Pixmap(doc, xref)
+            imgs.append(
+                np.frombuffer(pix.samples, dtype=np.uint8).reshape(
+                    pix.height, pix.width, -1
+                )
+            )
+        return extract_from_images_with_rapidocr(imgs)
