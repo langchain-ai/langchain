7c6cec5585498b21423badf2988bd93ff92d1b1a
Bagatur <baskaryan@gmail.com>
2023-10-20 17:16:01 -0400

    marker

--- libs/langchain/tests/integration_tests/__init__.py
+++ libs/langchain/tests/integration_tests/__init__.py
@@ -1,8 +1,1 @@
 """All integration tests (tests that call out to an external API)."""
-import pytest
-
-
-@pytest.mark.compile
-def test_placeholder() -> None:
-    """Used for compiling integration tests without running any real tests."""
-    pass
