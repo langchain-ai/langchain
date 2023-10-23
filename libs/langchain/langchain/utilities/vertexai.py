79ac065929bd6e210f5c3c26d53e1fca01be021e
Holt Skinner <holtskinner@google.com>
2023-10-23 13:34:18 -0500

    Change LangChain Version import to use `importlib`

--- libs/langchain/langchain/utilities/vertexai.py
+++ libs/langchain/langchain/utilities/vertexai.py
@@ -1,7 +1,7 @@
 """Utilities to init Vertex AI."""
+from importlib import metadata
 from typing import TYPE_CHECKING, Optional
 
-from langchain import __version__
 
 if TYPE_CHECKING:
     from google.auth.credentials import Credentials
@@ -68,7 +68,10 @@ def get_client_info(module: Optional[str] = None) -> "ClientInfo":
             "pip install google-api-core"
         ) from exc
 
-    client_library_version = f"{__version__}-{module}" if module else __version__
+    langchain_version = metadata.version("langchain")
+    client_library_version = (
+        f"{langchain_version}-{module}" if module else langchain_version
+    )
     return ClientInfo(
         client_library_version=client_library_version,
         user_agent=f"langchain/{client_library_version}",
