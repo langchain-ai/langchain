57f056b1582b4f63e434ab4682626090bdbcd87e
Mateusz Szewczyk <Mateusz.Szewczyk@ibm.com>
2023-10-24 13:04:23 +0200

    add watsonx large language models

--- libs/langchain/langchain/llms/__init__.py
+++ libs/langchain/langchain/llms/__init__.py
@@ -468,6 +468,12 @@ def _import_vllm_openai() -> Any:
     return VLLMOpenAI
 
 
+def _import_watsonxllm() -> Any:
+    from langchain.llms.watsonxllm import WatsonxLLM
+
+    return WatsonxLLM
+
+
 def _import_writer() -> Any:
     from langchain.llms.writer import Writer
 
@@ -635,6 +641,8 @@ def __getattr__(name: str) -> Any:
         return _import_vllm()
     elif name == "VLLMOpenAI":
         return _import_vllm_openai()
+    elif name == "WatsonxLLM":
+        return _import_watsonxllm()
     elif name == "Writer":
         return _import_writer()
     elif name == "Xinference":
@@ -722,6 +730,7 @@ __all__ = [
     "VertexAIModelGarden",
     "VLLM",
     "VLLMOpenAI",
+    "WatsonxLLM",
     "Writer",
     "OctoAIEndpoint",
     "Xinference",
@@ -799,6 +808,7 @@ def get_type_to_cls_dict() -> Dict[str, Callable[[], Type[BaseLLM]]]:
         "openllm_client": _import_openllm,
         "vllm": _import_vllm,
         "vllm_openai": _import_vllm_openai,
+        "watsonxllm": _import_watsonxllm(),
         "writer": _import_writer,
         "xinference": _import_xinference,
         "javelin-ai-gateway": _import_javelin_ai_gateway,
---
+++ libs/langchain/langchain/llms/watsonxllm.py
@@ -0,0 +1,145 @@
+import logging
+import os
+from typing import Any, List, Mapping, Optional, Dict, Union
+
+from langchain.llms.base import LLM
+from langchain.utils import get_from_dict_or_env
+from langchain.llms.utils import enforce_stop_tokens
+from langchain.pydantic_v1 import Extra, root_validator
+from langchain.callbacks.manager import CallbackManagerForLLMRun
+
+logger = logging.getLogger(__name__)
+
+
+class WatsonxLLM(LLM):
+    """
+    Watsonx large language models.
+
+    To use, you should have the ``ibm_watson_machine_learning`` python package installed,
+    and the environment variable ``WATSONX_APIKEY`` set with your API key, or pass
+    it as a named parameter to the constructor.
+
+
+    Example:
+        .. code-block:: python
+
+            from langchain.llms import WatsonxLLM
+            llm = WatsonxLLM(
+                model_id='google/flan-ul2',
+                credentials = {"url": "https://us-south.ml.cloud.ibm.com",
+                                   "apikey": "*****"},
+                project_id="*****"
+            )
+    """
+    model_id: str = None
+    """Type of model to use."""
+
+    project_id: str = None
+    """ID of the Watson Studio project."""
+
+    space_id: str = None
+    """ID of the Watson Studio space."""
+
+    credentials: dict = {}
+    """Credentials to Watson Machine Learning instance"""
+
+    params: dict = None
+    """Model parameters to use during generate requests."""
+
+    verify: Union[str, bool] = None
+    """User can pass as verify one of following:
+        the path to a CA_BUNDLE file
+        the path of directory with certificates of trusted CAs
+        True - default path to truststore will be taken
+        False - no verification will be made"""
+
+    llm_type: str = "IBM watsonx.ai"
+    """Type of llm."""
+
+    class Config:
+        """Configuration for this pydantic object."""
+
+        extra = Extra.forbid
+
+    @root_validator()
+    def validate_environment(cls, values: Dict) -> Dict:
+        """Validate that api key, project_id or space_id and python package exists in environment."""
+        if 'cloud.ibm.com' in values['credentials']['url']:
+            values['credentials']['apikey'] = get_from_dict_or_env(values['credentials'], "apikey", "WATSONX_APIKEY")
+        else:
+            if "token" not in values['credentials'] and "TOKEN" not in os.environ and "password" not in values['credentials'] and "PASSWORD" not in os.environ and "apikey" not in values['credentials'] and "APIKEY" not in os.environ:
+                raise ValueError(
+                    f"Did not find 'token', 'password' or 'apikey', please add an environment variable"
+                    f" `TOKEN`, 'PASSWORD' or 'APIKEY' which contains it, or pass"
+                    f" 'token', 'password' or 'apikey' as a named parameter in `credentials`."
+                )
+            elif "token" in values['credentials'] or "TOKEN" in os.environ:
+                values['credentials']['token'] = get_from_dict_or_env(values['credentials'], "token", "TOKEN")
+            elif "password" in values['credentials'] or "PASSWORD" in os.environ:
+                values['credentials']['password'] = get_from_dict_or_env(values['credentials'], "password", "PASSWORD")
+                values['credentials']['username'] = get_from_dict_or_env(values['credentials'], "username", "USERNAME")
+            elif "apikey" in values['credentials'] or "APIKEY" in os.environ:
+                values['credentials']['apikey'] = get_from_dict_or_env(values['credentials'], "apikey", "APIKEY")
+                values['credentials']['username'] = get_from_dict_or_env(values['credentials'], "username", "USERNAME")
+
+        try:
+            from ibm_watson_machine_learning.foundation_models import Model
+
+            model = Model(
+                model_id=values["model_id"],
+                credentials=values['credentials'],
+                params=values["params"],
+                project_id=values['project_id'],
+                space_id=values['space_id'],
+                verify=values["verify"])
+            values["model"] = model
+
+        except ImportError:
+            raise ImportError(
+                "Could not import ibm_watson_machine_learning python package. "
+                "Please install it with `pip install ibm_watson_machine_learning`."
+            )
+        return values
+
+    @property
+    def _identifying_params(self) -> Mapping[str, Any]:
+        """Get the identifying parameters."""
+        return {
+            **{"model_id": self.model_id},
+            **{"params": self.params},
+            **{"project_id": self.project_id},
+            **{"space_id": self.space_id},
+        }
+
+    @property
+    def _llm_type(self) -> str:
+        """Return type of llm."""
+        return self.llm_type
+
+    def _call(
+        self,
+        prompt: str,
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+        **kwargs: Any,
+    ) -> str:
+        """Call the IBM watsonx.ai inference endpoint.
+
+        Args:
+            prompt: The prompt to pass into the model.
+            stop: Optional list of stop words to use when generating.
+
+        Returns:
+            The string generated by the model.
+
+        Example:
+            .. code-block:: python
+
+                response = watsonxllm("What is a molecule")
+        """
+
+        text = self.model.generate_text(prompt=prompt)
+        logger.info("Output of watsonx.ai call: {}".format(text))
+        if stop is not None:
+            text = enforce_stop_tokens(text, stop)
+        return text
