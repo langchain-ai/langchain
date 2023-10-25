21a4438251886ef505e80084d6c8ba630a19c4e3
Harrison Chase <hw.chase.17@gmail.com>
2023-10-25 15:09:59 -0700

    cr

--- templates/openai-functions-agent/README.md
+++ templates/openai-functions-agent/README.md
@@ -1,3 +1,4 @@
 # OpenAI Functions Agent
 
-We can create an agent that uses OpenAI function calling to respond.
+This template creates an agent that uses OpenAI function calling to communicate its decisions of what actions to take.
+This example creates an agent that can optionally look up things on the internet using Tavily's search engine.
--- templates/openai-functions-agent/openai_functions_agent/agent.py
+++ templates/openai-functions-agent/openai_functions_agent/agent.py
@@ -1,4 +1,5 @@
-from langchain.agents import tool
+from typing import List, Tuple
+from langchain.schema.messages import HumanMessage, AIMessage
 from langchain.chat_models import ChatOpenAI
 from langchain.agents import AgentExecutor
 from langchain.utilities.tavily_search import TavilySearchAPIWrapper
@@ -19,6 +20,7 @@ tools = [tavily_tool]
 llm = ChatOpenAI(temperature=0)
 prompt = ChatPromptTemplate.from_messages([
     ("system", "You are very powerful assistant, but bad at calculating lengths of words."),
+    MessagesPlaceholder(variable_name="chat_history"),
     ("user", "{input}"),
     MessagesPlaceholder(variable_name="agent_scratchpad"),
 ])
@@ -26,13 +28,24 @@ prompt = ChatPromptTemplate.from_messages([
 llm_with_tools = llm.bind(
     functions=[format_tool_to_openai_function(t) for t in tools]
 )
+
+def _format_chat_history(chat_history: List[Tuple[str, str]]):
+    buffer = []
+    for human, ai in chat_history:
+        buffer.append(HumanMessage(content=human))
+        buffer.append(AIMessage(content=ai))
+    return buffer
+
+
 agent = {
     "input": lambda x: x["input"],
-    "agent_scratchpad": lambda x: format_to_openai_functions(x['intermediate_steps'])
+    "chat_history": lambda x: _format_chat_history(x['chat_history']),
+    "agent_scratchpad": lambda x: format_to_openai_functions(x['intermediate_steps']),
 } | prompt | llm_with_tools | OpenAIFunctionsAgentOutputParser()
 
 class AgentInput(BaseModel):
     input: str
+    chat_history: List[Tuple[str, str]]
 
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True).with_types(
     input_type=AgentInput
