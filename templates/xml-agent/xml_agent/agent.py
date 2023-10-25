7415c31c3be19eddcdef52b62b1bb6fafe463a58
Harrison Chase <hw.chase.17@gmail.com>
2023-10-25 13:25:22 -0700

    cr

--- templates/xml-agent/xml_agent/agent.py
+++ templates/xml-agent/xml_agent/agent.py
@@ -1,6 +1,4 @@
-from langchain.agents import tool
 from langchain.chat_models import ChatAnthropic
-model = ChatAnthropic(model="claude-2")
 from langchain.tools.render import render_text_description
 from langchain.agents.output_parsers import XMLAgentOutputParser
 from langchain.agents.format_scratchpad import format_xml
@@ -8,6 +6,10 @@ from langchain import hub
 from langchain.agents import AgentExecutor
 from langchain.utilities.tavily_search import TavilySearchAPIWrapper
 from langchain.tools.tavily_search import TavilySearchResults
+from langchain.pydantic_v1 import BaseModel
+
+
+model = ChatAnthropic(model="claude-2")
 
 # Fake Tool
 search = TavilySearchAPIWrapper()
@@ -26,4 +28,11 @@ agent = {
     "agent_scratchpad": lambda x: format_xml(x['intermediate_steps']),
 } | prompt | llm_with_stop | XMLAgentOutputParser()
 
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+class AgentInput(BaseModel):
+    question: str
+
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True).with_types(
+    input_type=AgentInput
+)
+
+agent_executor = agent_executor | (lambda x: x["output"])
