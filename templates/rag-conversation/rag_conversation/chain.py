65bf4be6c3060395e95d6c08781f8cdc9ec5322a
Harrison Chase <hw.chase.17@gmail.com>
2023-10-25 15:58:56 -0700

    cr

--- templates/rag-conversation/rag_conversation/chain.py
+++ templates/rag-conversation/rag_conversation/chain.py
@@ -1,33 +1,36 @@
 from typing import Tuple, List
 from pydantic import BaseModel
 from operator import itemgetter
-from langchain.vectorstores import Chroma
+from langchain.vectorstores import Pinecone
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.schema import format_document
+from langchain.schema import format_document, AIMessage, HumanMessage
 from langchain.prompts.prompt import PromptTemplate
 from langchain.schema.output_parser import StrOutputParser
-from langchain.schema.runnable import RunnablePassthrough, RunnableParallel, RunnableMap, RunnableBranch, RunnableLambda
+from langchain.schema.output_parser import StrOutputParser
+from langchain.schema.runnable import RunnablePassthrough, RunnableBranch, RunnableLambda, RunnableMap
 
 # Load
 from langchain.document_loaders import WebBaseLoader
 loader = WebBaseLoader("https://lilianweng.github.io/posts/2023-06-23-agent/")
 data = loader.load()
 
-# Split
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
-all_splits = text_splitter.split_documents(data)
+# # Split
+# from langchain.text_splitter import RecursiveCharacterTextSplitter
+# text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+# all_splits = text_splitter.split_documents(data)
+#
+# # Add to vectorDB
+# vectorstore = Pinecone.from_documents(
+#     documents=all_splits, embedding=OpenAIEmbeddings(), index_name='langchain-test'
+# )
+# retriever = vectorstore.as_retriever()
 
-# Add to vectorDB
-vectorstore = Chroma.from_documents(documents=all_splits, 
-                                    collection_name="rag-conversation",
-                                    embedding=OpenAIEmbeddings(),
-                                    )
+vectorstore = Pinecone.from_existing_index("langchain-test", OpenAIEmbeddings())
 retriever = vectorstore.as_retriever()
 
-# Condense a a chat history and follow-up question into a standalone question
+# Condense a chat history and follow-up question into a standalone question
 _template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
 Chat History:
 {chat_history}
@@ -37,11 +40,14 @@ CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
 
 # RAG answer synthesis prompt
 template = """Answer the question based only on the following context:
+<context>
 {context}
-
-Question: {question}
-"""
-ANSWER_PROMPT = ChatPromptTemplate.from_template(template)
+</context>"""
+ANSWER_PROMPT = ChatPromptTemplate.from_messages([
+    ("system",template),
+    MessagesPlaceholder(variable_name="chat_history"),
+    ("user", "{question}")
+])
 
 # Conversational Retrieval Chain
 DEFAULT_DOCUMENT_PROMPT = PromptTemplate.from_template(template="{page_content}")
@@ -49,12 +55,11 @@ def _combine_documents(docs, document_prompt = DEFAULT_DOCUMENT_PROMPT, document
     doc_strings = [format_document(doc, document_prompt) for doc in docs]
     return document_separator.join(doc_strings)
 
-def _format_chat_history(chat_history: List[Tuple]) -> str:
-    buffer = ""
-    for dialogue_turn in chat_history:
-        human = "Human: " + dialogue_turn[0]
-        ai = "Assistant: " + dialogue_turn[1]
-        buffer += "\n" + "\n".join([human, ai])
+def _format_chat_history(chat_history: List[Tuple[str, str]]) -> List:
+    buffer = []
+    for human, ai in chat_history:
+        buffer.append(HumanMessage(content=human))
+        buffer.append(AIMessage(content=ai))
     return buffer
 
 # User input 
@@ -62,8 +67,8 @@ class ChatHistory(BaseModel):
     chat_history: List[Tuple[str, str]] 
     question: str
 
-_inputs = RunnableMap(
-    standalone_question=RunnableBranch(
+
+_search_query = RunnableBranch(
         # If input includes chat_history, we condense it with the follow-up question
         (
             RunnableLambda(lambda x: bool(x.get("chat_history"))).with_config(
@@ -73,16 +78,17 @@ _inputs = RunnableMap(
                 chat_history=lambda x: _format_chat_history(x['chat_history'])
             ) | CONDENSE_QUESTION_PROMPT | ChatOpenAI(temperature=0) | StrOutputParser(),
         ),
-        # Else, we have no chat history, so just pass throgh the question
+        # Else, we have no chat history, so just pass through the question
         RunnableLambda(itemgetter("question"))
-        
+
     )
-).with_types(input_type=ChatHistory)
 
-# Context includes retrieved docs and the current question
-_context = {
-    "context": itemgetter("standalone_question") | retriever | _combine_documents,
-    "question": lambda x: x["standalone_question"]
-}
+_inputs = RunnableMap({
+    "question": lambda x: x["question"],
+    "chat_history": lambda x: _format_chat_history(x['chat_history']),
+    "context": _search_query | retriever | _combine_documents
+}).with_types(input_type=ChatHistory)
 
-chain = _inputs | _context | ANSWER_PROMPT | ChatOpenAI()
+chain = _inputs | ANSWER_PROMPT | ChatOpenAI() | StrOutputParser()
+
+print(chain.invoke({"question": "hi!", "chat_history": [("hi", "bye")]}))
