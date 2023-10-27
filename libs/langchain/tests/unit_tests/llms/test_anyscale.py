6ddc0b2f229ebd915f6b8dda66400bc28e114a54
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-10-27 12:03:42 +0600

    Rename unit tests for better clarity

--- libs/langchain/tests/unit_tests/llms/test_anyscale.py
+++ libs/langchain/tests/unit_tests/llms/test_anyscale.py
@@ -15,7 +15,7 @@ def test_api_key_is_secret_string() -> None:
 
 
 @pytest.mark.requires("openai")
-def test_initialization_with_api_key_as_env_var(
+def test_api_key_masked_when_passed_from_env(
     monkeypatch: MonkeyPatch, capsys: CaptureFixture
 ) -> None:
     """Test initialization with an API key provided via an env variable"""
@@ -28,7 +28,7 @@ def test_initialization_with_api_key_as_env_var(
 
 
 @pytest.mark.requires("openai")
-def test_initialization_with_api_key_as_initializer_argument(
+def test_api_key_masked_when_passed_via_constructor(
     capsys: CaptureFixture,
 ) -> None:
     """Test initialization with an API key provided via the initializer"""
