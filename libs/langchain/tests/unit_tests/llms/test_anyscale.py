06d188069a0eed2ef9d63db8632907800bd658b6
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-10-27 11:58:50 +0600

    Refactor anyscale llm unit tests

--- libs/langchain/tests/unit_tests/llms/test_anyscale.py
+++ libs/langchain/tests/unit_tests/llms/test_anyscale.py
@@ -1,36 +1,41 @@
-import os
-
+"""Test Anyscale llm"""
 import pytest
+from pytest import CaptureFixture, MonkeyPatch
 
 from langchain.llms.anyscale import Anyscale
-
-os.environ["ANYSCALE_API_KEY"] = SECRET_ANYSCALE_API_KEY = "SECRET_ANYSCALE_API_KEY"
+from langchain.pydantic_v1 import SecretStr
 
 
 @pytest.mark.requires("openai")
-def test_initialization_with_api_key_as_env_var() -> None:
-    """Test initialization with an API key provided via an env variable"""
+def test_api_key_is_secret_string() -> None:
     llm = Anyscale(
-        anyscale_api_base="test",
-        model_name="test",
+        anyscale_api_key="secret-api-key", anyscale_api_base="test", model_name="test"
     )
-    _string = str(llm)
-    _repr = repr(llm)
-
-    assert SECRET_ANYSCALE_API_KEY not in _string
-    assert SECRET_ANYSCALE_API_KEY not in _repr
+    assert isinstance(llm.anyscale_api_key, SecretStr)
 
 
 @pytest.mark.requires("openai")
-def test_initialization_with_api_key_as_initializer_argument() -> None:
+def test_initialization_with_api_key_as_env_var(
+    monkeypatch: MonkeyPatch, capsys: CaptureFixture
+) -> None:
+    """Test initialization with an API key provided via an env variable"""
+    monkeypatch.setenv("ANYSCALE_API_KEY", "secret-api-key")
+    llm = Anyscale(anyscale_api_base="test", model_name="test")
+    print(llm.anyscale_api_key, end="")
+    captured = capsys.readouterr()
+
+    assert captured.out == "**********"
+
+
+@pytest.mark.requires("openai")
+def test_initialization_with_api_key_as_initializer_argument(
+    capsys: CaptureFixture,
+) -> None:
     """Test initialization with an API key provided via the initializer"""
     llm = Anyscale(
-        anyscale_api_base="test",
-        anyscale_api_key=os.environ["ANYSCALE_API_KEY"],
-        model_name="test",
+        anyscale_api_key="secret-api-key", anyscale_api_base="test", model_name="test"
     )
-    _string = str(llm)
-    _repr = repr(llm)
+    print(llm.anyscale_api_key, end="")
+    captured = capsys.readouterr()
 
-    assert SECRET_ANYSCALE_API_KEY not in _string
-    assert SECRET_ANYSCALE_API_KEY not in _repr
+    assert captured.out == "**********"
