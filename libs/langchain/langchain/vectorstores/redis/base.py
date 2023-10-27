e53c10e64ba745a64fddc4cae2970816d3acb29a
Tyler Hutcherson <tyler.hutcherson@redis.com>
2023-10-27 15:16:26 -0400

    fix linting/formatting

--- libs/langchain/langchain/vectorstores/redis/base.py
+++ libs/langchain/langchain/vectorstores/redis/base.py
@@ -24,10 +24,7 @@ import numpy as np
 import yaml
 
 from langchain._api import deprecated
-from langchain.callbacks.manager import (
-    AsyncCallbackManagerForRetrieverRun,
-    CallbackManagerForRetrieverRun,
-)
+from langchain.callbacks.manager import CallbackManagerForRetrieverRun
 from langchain.docstore.document import Document
 from langchain.schema.embeddings import Embeddings
 from langchain.schema.vectorstore import VectorStore, VectorStoreRetriever
