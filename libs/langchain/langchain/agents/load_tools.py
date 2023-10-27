f65a45c963905a3e8c3e9083a470892ced9eae70
Eugene Yurtsev <eyurtsev@gmail.com>
2023-10-27 11:28:36 -0400

    x

--- libs/langchain/langchain/agents/load_tools.py
+++ libs/langchain/langchain/agents/load_tools.py
@@ -79,9 +79,14 @@ from langchain.utilities.dataforseo_api_search import DataForSeoAPIWrapper
 
 
 def _get_python_repl() -> BaseTool:
-    from langchain.tools.python import tool  # Will raise on import
-
-    raise AssertionError("Unreachable code")
+    raise ImportError(
+        "This tool has been moved to langchain experiment. "
+        "This tool has access to a python REPL. "
+        "For best practices make sure to sandbox this tool. "
+        "Read https://github.com/langchain-ai/langchain/blob/master/SECURITY.md "
+        "To keep using this code as is, install langchain experimental and "
+        "update relevant imports replacing 'langchain' with 'langchain_experimental'"
+    )
 
 
 def _get_tools_requests_get() -> BaseTool:
