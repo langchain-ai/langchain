d45c0880741f70382cf6335d6087c17ffaedfbab
Sam Partee <sam.partee@redis.com>
2023-10-28 00:49:29 -0700

    Touchups

--- templates/rag-redis/ingest.py
+++ templates/rag-redis/ingest.py
@@ -1,34 +1,31 @@
 import os
 
-from langchain.vectorstores import Redis
-from langchain.embeddings import HuggingFaceEmbeddings
-
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import UnstructuredFileLoader
-
-
-from rag_redis.redis import (
-    REDIS_URL,
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Redis
+from rag_redis.config import (
     INDEX_NAME,
     INDEX_SCHEMA,
+    REDIS_URL,
+    EMBED_MODEL
 )
 
 
 def ingest_documents():
     """
-    Ingest PDF to Redis from the data/ directory that contains Edgar 10k filings data for Nike.
+    Ingest PDF to Redis from the data/ directory that
+    contains Edgar 10k filings data for Nike.
     """
     # Load list of pdfs
-    data_path = "data/"
-    doc = [os.path.join(data_path, file) for file in os.listdir(data_path)][0]
     company_name = "Nike"
+    data_path = "data/"
+    doc = [
+        os.path.join(data_path, file) for file in os.listdir(data_path)
+    ][0]
+
     print("Parsing 10k filing doc for NIKE", doc)
 
-    # For simplicity, we will just work with one of the 10k files. This will take some time still.
-    # To Note: the UnstructuredFileLoader is not the only document loader type that LangChain provides
-    # To Note: the RecursiveCharacterTextSplitter is what we use to create smaller chunks of text from the doc.
-    # Docs: https://python.langchain.com/docs/integrations/document_loaders/unstructured_file
-    # Docs: https://python.langchain.com/docs/modules/data_connection/document_transformers/text_splitters/recursive_text_splitter
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1500, chunk_overlap=100, add_start_index=True
     )
@@ -37,10 +34,13 @@ def ingest_documents():
 
     print("Done preprocessing. Created", len(chunks), "chunks of the original pdf")
     # Create vectorstore
-    embedder = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    embedder = HuggingFaceEmbeddings(
+        model_name=EMBED_MODEL
+    )
 
     _ = Redis.from_texts(
-        # appending this little bit can sometimes help with semantic retrieval -- especially with multiple companies
+        # appending this little bit can sometimes help with semantic retrieval
+        # especially with multiple companies
         texts=[f"Company: {company_name}. " + chunk.page_content for chunk in chunks],
         metadatas=[chunk.metadata for chunk in chunks],
         embedding=embedder,
--- templates/rag-redis/rag_redis/chain.py
+++ templates/rag-redis/rag_redis/chain.py
@@ -6,34 +6,23 @@ from langchain.llms import OpenAI
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.schema.output_parser import StrOutputParser
 from langchain.schema.runnable import RunnablePassthrough, RunnableParallel
+from langchain.pydantic_v1 import BaseModel
 
-from .redis import (
+from rag_redis.config import (
     REDIS_URL,
     INDEX_NAME,
     INDEX_SCHEMA,
     DEBUG,
+    EMBED_MODEL,
 )
 
-
-# Check for openai API key
-if "OPENAI_API_KEY" not in os.environ:
-    raise Exception("Must provide an OPENAI_API_KEY as an env var.")
+# Make this look better in the docs.
+class Question(BaseModel):
+    __root__: str
 
 
 # Init Embeddings
-embedder = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-
-
-# Set DEBUG env var to "true" if you wish to enable LC debugging module
-if DEBUG:
-    import langchain
-    langchain.debug=True
-
-
-# Check for openai API key
-if "OPENAI_API_KEY" not in os.environ:
-    raise Exception("Must provide an OPENAI_API_KEY as an env var.")
-
+embedder = HuggingFaceEmbeddings(model_name=EMBED_MODEL)
 
 # Connect to pre-loaded vectorstore -- run the ingest.py script to populate this
 vectorstore = Redis.from_existing_index(
@@ -42,11 +31,14 @@ vectorstore = Redis.from_existing_index(
     schema=INDEX_SCHEMA,
     redis_url=REDIS_URL
 )
+# TODO set parameters
 retriever = vectorstore.as_retriever()
 
 
 # Define our prompt
-template = """Use the following pieces of context from financial 10k filings data to answer the user question at the end. If you don't know the answer, say that you don't know, don't try to make up an answer.
+template = """
+Use the following pieces of context from financial 10k filings data to answer the user question.
+If you don't know the answer, say that you don't know, don't try to make up an answer.
 
 This should be in the following format:
 
@@ -64,7 +56,6 @@ Answer:"""
 
 prompt = PromptTemplate.from_template(template)
 
-
 # RAG Chain
 model = OpenAI()
 chain = (
@@ -72,4 +63,4 @@ chain = (
     | prompt
     | model
     | StrOutputParser()
-)
+).with_types(input_type=Question)
