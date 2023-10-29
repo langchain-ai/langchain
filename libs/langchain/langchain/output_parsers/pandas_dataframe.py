78e1abad6a151039a9f11fd49948c6b5595361ec
Rohan Dey <58871401+RohanDey02@users.noreply.github.com>
2023-10-29 16:40:10 -0400

    [LC-11532] Array detection and parsing completed

--- libs/langchain/langchain/output_parsers/pandas_dataframe.py
+++ libs/langchain/langchain/output_parsers/pandas_dataframe.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 from typing import Any, Dict, List, Tuple
 from langchain.pydantic_v1 import root_validator
 from langchain.schema import BaseOutputParser, OutputParserException
@@ -19,6 +20,18 @@ class PandasDataFrameOutputParser(BaseOutputParser):
         if dataframe.empty:
             raise ValueError("DataFrame cannot be empty.")
         return values
+    
+    def parse_array(self, array: str) -> List[int]:
+        # Check if the format is [1,3,5]
+        if re.match(r'\[\d+(,\s*\d+)*\]', array):
+            return [int(i) for i in re.findall(r'\d+', array)]
+        # Check if the format is [1..5]
+        elif re.match(r'\[(\d+)\.\.(\d+)\]', array):
+            match = re.match(r'\[(\d+)\.\.(\d+)\]', array)
+            start, end = map(int, match.groups())
+            return list(range(start, end + 1))
+
+        return []
 
     # NOTE: LLM will use format instructions to generate query in correct format.
     #       parse() function will then take the output and apply it to the DataFrame
@@ -38,8 +51,15 @@ class PandasDataFrameOutputParser(BaseOutputParser):
                 case 'row':
                     print(request_type)
                 case _:
-                    # Call getattr. If it fails, then except, otherwise, do the stuff.
-                    result[request_type] = getattr(self.dataframe[request_params], request_type)()
+                    array_exists = re.search(r'(\[.*?\])', request_params)
+                    if array_exists:
+                        parsed_array = self.parse_array(array_exists.group(1))
+                        if parsed_array == []:
+                            raise OutputParserException(
+                                f"The array provided is not correctly defined. Please refer to the format instructions."
+                            )
+                    else:
+                        result[request_type] = getattr(self.dataframe[request_params], request_type)()
         except AttributeError:
             raise OutputParserException(
                 f"Request type '{request_type}' is possibly not supported. Please refer to the format instructions."
