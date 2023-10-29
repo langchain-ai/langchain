69044bfb8cf54835ffb485009cbb2dbf481e25f6
Rohan Dey <58871401+RohanDey02@users.noreply.github.com>
2023-10-29 13:16:36 -0400

    [LC-11532] Added some comments for parse() implementation

--- libs/langchain/langchain/output_parsers/pandas_dataframe.py
+++ libs/langchain/langchain/output_parsers/pandas_dataframe.py
@@ -1,20 +1,48 @@
 import pandas as pd
-from typing import Any, Dict, List, Type
+from typing import Any, Dict, List, Tuple
 from langchain.pydantic_v1 import root_validator
 from langchain.schema import BaseOutputParser, OutputParserException
 
 
 class PandasDataFrameOutputParser(BaseOutputParser):
-    """Parse an output that is one of a set of values."""
+    """Parse an output using Pandas DataFrame format."""
 
+    """The Pandas DataFrame to parse."""
     dataframe: pd.DataFrame
 
-    @property
-    def _valid_values(self) -> List[str]:
-        # TODO: Do we want something like this?
-        return []
+    @root_validator()
+    def check_dataframe(cls, values: Dict) -> Dict:
+        dataframe = values.get('dataframe')
+        if not isinstance(dataframe, pd.DataFrame):
+            raise ValueError("Input must be a Pandas DataFrame.")
+        if dataframe.empty:
+            raise ValueError("DataFrame cannot be empty.")
+        return values
 
-    def parse(self, request: str) -> Any:
+    # NOTE: LLM will use format instructions to generate query in correct format.
+    #       parse() function will then take the output and apply it to the DataFrame
+    def parse(self, request: str) -> Dict[str, Any]:
+        splitted_request: Tuple[str, str] = request.strip().split(':')
+        if len(splitted_request) != 2:
+            raise OutputParserException(
+                f"Request '{request}' is not correctly formatted. Please refer to the format instructions."
+            )
+        try:
+            # NOTE: Can probably simplify using getattr(df, function_name)()
+            request_type, request_params = splitted_request
+            match request_type:
+                case 'column':
+                    print(request_type)
+                case 'row':
+                    print(request_type)
+                case _:
+                    # Call getattr. If it fails, then except, otherwise, do the stuff.
+                    print(request_type)
+        except:
+            raise OutputParserException(
+                f"Request type '{request_type}' is not correctly formatted. Please refer to the format instructions."
+            )
+
         return
 
     def get_format_instructions(self) -> str:
