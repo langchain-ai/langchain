ab18b2c87f98a7bfc362d8f165098f5cfcd182ed
Harrison Chase <hw.chase.17@gmail.com>
2023-10-29 13:34:27 -0700

    cr

--- templates/pii-protected-chatbot/pii_protected_chatbot/chain.py
+++ templates/pii-protected-chatbot/pii_protected_chatbot/chain.py
@@ -8,8 +8,7 @@ from langchain.schema.output_parser import StrOutputParser
 from langchain.schema.runnable import RunnablePassthrough
 from presidio_analyzer import AnalyzerEngine
 
-analyzer = AnalyzerEngine()
-
+# Formatting for chat history
 def _format_chat_history(chat_history: List[Tuple[str, str]]):
     buffer = []
     for human, ai in chat_history:
@@ -17,6 +16,8 @@ def _format_chat_history(chat_history: List[Tuple[str, str]]):
         buffer.append(AIMessage(content=ai))
     return buffer
 
+
+# Prompt we will use
 _prompt = ChatPromptTemplate.from_messages(
     [
         (
@@ -27,34 +28,50 @@ _prompt = ChatPromptTemplate.from_messages(
         ("human", "{text}"),
     ]
 )
+
+# Model we will use
 _model = ChatOpenAI()
 
+# Standard conversation chain.
 chat_chain = {
     "chat_history": lambda x: _format_chat_history(x["chat_history"]),
     "text": lambda x: x["text"],
 } | _prompt | _model | StrOutputParser()
 
-
+# PII Detection logic
+analyzer = AnalyzerEngine()
 # You can customize this to detect any PII
 def _detect_pii(inputs: dict) -> bool:
     analyzer_results = analyzer.analyze(text=inputs["text"], language="en")
     return bool(analyzer_results)
 
 
+# Add logic to route on whether PII has been detected
 def _route_on_pii(inputs: dict):
     if inputs["pii_detected"]:
+        # Response if PII is detected
         return "Sorry, I can't answer questions that involve PII"
     else:
         return chat_chain
 
+
+# Final chain
+chain = RunnablePassthrough.assign(
+    # First detect PII
+    pii_detected=_detect_pii
+) | {
+    # Then use this information to generate the response
+    "response": _route_on_pii,
+    # Return boolean of whether PII is detected so client can decided
+    # whether or not to include in chat history
+    "pii_detected": lambda x: x["pii_detected"]
+}
+
+
+# Add typing for playground
 class ChainInput(BaseModel):
     text: str
     chat_history: List[Tuple[str, str]]
 
 
-chain = RunnablePassthrough.assign(
-    pii_detected=_detect_pii
-).with_types(input_type=ChainInput) | {
-    "response": _route_on_pii,
-    "pii_detected": lambda x: x["pii_detected"]
-}
+chain = chain.with_types(input_type=ChainInput)
