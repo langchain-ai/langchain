b058954a8afaddb220105ab244d5e3a1e7571757
Samad Koita <>
2023-10-29 15:11:45 +0530

    fix typing

--- libs/langchain/langchain/llms/gooseai.py
+++ libs/langchain/langchain/llms/gooseai.py
@@ -67,7 +67,7 @@ class GooseAI(LLM):
     logit_bias: Optional[Dict[str, float]] = Field(default_factory=dict)
     """Adjust the probability of specific tokens being generated."""
 
-    gooseai_api_key: Optional[Union[str, SecretStr]] = None
+    gooseai_api_key: Optional[SecretStr] = None
 
     class Config:
         """Configuration for this pydantic config."""
