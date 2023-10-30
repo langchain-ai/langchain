51af71020f9cd783e412e701536a36549ffa5abe
Erika Cardenas <110841617+erika-cardenas@users.noreply.github.com>
2023-10-30 18:48:14 -0400

    add hybrid search template

--- templates/hybrid-search-weaviate/hybrid_search_weaviate/chain.py
+++ templates/hybrid-search-weaviate/hybrid_search_weaviate/chain.py
@@ -9,6 +9,8 @@ from langchain.schema.runnable import RunnableParallel, RunnablePassthrough
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Weaviate
 
+from langchain.retrievers.weaviate_hybrid_search import WeaviateHybridSearchRetriever
+
 if os.environ.get("WEAVIATE_API_KEY", None) is None:
     raise Exception("Missing `WEAVIATE_API_KEY` environment variable.")
 
@@ -26,27 +28,19 @@ data = loader.load()
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
 all_splits = text_splitter.split_documents(data)
 
-# # Add to vectorDB
-# vectorstore = Weaviate.from_documents(
-#     documents=all_splits, embedding=OpenAIEmbeddings(), index_name=WEAVIATE_INDEX_NAME
-# )
-# retriever = vectorstore.as_retriever()
-
-vectorstore = Weaviate.from_existing_index(WEAVIATE_INDEX_NAME, OpenAIEmbeddings())
-retriever = vectorstore.as_retriever()
-
-# RAG prompt
-template = """Answer the question based only on the following context:
-{context}
-Question: {question}
-"""
-prompt = ChatPromptTemplate.from_template(template)
-
-# RAG
-model = ChatOpenAI()
-chain = (
-    RunnableParallel({"context": retriever, "question": RunnablePassthrough()})
-    | prompt
-    | model
-    | StrOutputParser()
+# # Build retriever 
+retriever = WeaviateHybridSearchRetriever = (
+    client=client, index_name=WEAVIATE_INDEX_NAME, text_key="text", attributes=[], create_schema_if_missing=True
 )
+
+# # Add to vectorDB
+retriever.add_documents(all_splits)
+
+
+# Hybrid search
+retriever.get_relevant_documents("agents short-term memory")
+
+# Hybrid search with scores
+retriever.get_relevant_documents("short-term memory", score=True)
+
+
