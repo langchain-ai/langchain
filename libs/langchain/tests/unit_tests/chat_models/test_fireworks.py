4e9097cd3636f17543906ffa72575a3483479274
Bagatur <baskaryan@gmail.com>
2023-10-30 16:26:47 -0700

    cr

--- libs/langchain/tests/unit_tests/chat_models/test_fireworks.py
+++ libs/langchain/tests/unit_tests/chat_models/test_fireworks.py
@@ -2,7 +2,7 @@
 import sys
 
 import pytest
-from pytest import CaptureFixture, MonkeyPatch
+from pytest import CaptureFixture
 
 from langchain.chat_models import ChatFireworks
 from langchain.pydantic_v1 import SecretStr
@@ -15,18 +15,6 @@ if sys.version_info < (3, 9):
 def test_api_key_is_string() -> None:
     llm = ChatFireworks(fireworks_api_key="secret-api-key")
     assert isinstance(llm.fireworks_api_key, SecretStr)
-
-
-@pytest.mark.requires("fireworks")
-def test_api_key_masked_when_passed_from_env(
-    monkeypatch: MonkeyPatch, capsys: CaptureFixture
-) -> None:
-    monkeypatch.setenv("FIREWORKS_API_KEY", "secret-api-key")
-    llm = ChatFireworks()
-    print(llm.fireworks_api_key, end="")
-    captured = capsys.readouterr()
-
-    assert captured.out == "**********"
 
 
 @pytest.mark.requires("fireworks")
