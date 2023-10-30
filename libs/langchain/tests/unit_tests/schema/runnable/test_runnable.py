34390b3f202dca250cc3763e05539df15cd8dc9a
Predrag Gruevski <obi1kenobi82@gmail.com>
2023-10-30 21:18:25 +0000

    `poetry run ruff format .` over merge commit changes.

--- libs/langchain/tests/unit_tests/schema/runnable/test_runnable.py
+++ libs/langchain/tests/unit_tests/schema/runnable/test_runnable.py
@@ -570,10 +570,12 @@ def test_with_types_with_type_generics() -> None:
 
     # Try specifying some
     RunnableLambda(foo).with_types(
-        output_type=List[int], input_type=List[int]  # type: ignore
+        output_type=List[int],
+        input_type=List[int],  # type: ignore
     )
     RunnableLambda(foo).with_types(
-        output_type=Sequence[int], input_type=Sequence[int]  # type: ignore[arg-type]
+        output_type=Sequence[int],
+        input_type=Sequence[int],  # type: ignore[arg-type]
     )
 
 
