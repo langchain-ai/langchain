f4b250238cb9ac2f9b871435c6e74b9418f5bea6
Erick Friis <erick@langchain.dev>
2023-10-30 13:13:30 -0700

    template name

--- libs/cli/langchain_cli/cli.py
+++ libs/cli/langchain_cli/cli.py
@@ -4,11 +4,11 @@ import typer
 from typing_extensions import Annotated
 
 from langchain_cli.namespaces import app as app_namespace
-from langchain_cli.namespaces import package as package_namespace
+from langchain_cli.namespaces import template as template_namespace
 
 app = typer.Typer(no_args_is_help=True, add_completion=False)
 app.add_typer(
-    package_namespace.package_cli, name="package", help=package_namespace.__doc__
+    template_namespace.package_cli, name="template", help=template_namespace.__doc__
 )
 app.add_typer(app_namespace.app_cli, name="app", help=app_namespace.__doc__)
 
@@ -24,12 +24,12 @@ def start(
     ] = None,
 ) -> None:
     """
-    Start the LangServe instance, whether it's a hub package or a serve project.
+    Start the LangServe instance, whether it's a template package or an app.
     """
 
-    # try starting hub package, if error, try langserve
+    # try starting template package, if error, try langserve
     try:
-        package_namespace.start(port=port, host=host)
+        template_namespace.start(port=port, host=host)
     except KeyError:
         app_namespace.start(port=port, host=host)
 
