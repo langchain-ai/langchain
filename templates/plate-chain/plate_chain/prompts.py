6dbfc19eb3337e35a184533348c17c0353922cd9
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2023-10-31 11:51:06 +1300

    Fixed missing return type, and over long arguments

--- templates/plate-chain/plate_chain/prompts.py
+++ templates/plate-chain/plate_chain/prompts.py
@@ -56,7 +56,11 @@ AI_REPONSE_DICT = {
 }
 
 
-def create_prompt(num_plates: Optional[int] = None, num_rows: Optional[int] = None, num_cols: Optional[int] = None):
+def create_prompt(
+    num_plates: Optional[int] = None,
+    num_rows: Optional[int] = None,
+    num_cols: Optional[int] = None,
+) -> str:
     additional_prompts = []
     if num_plates:
         num_plates_str = f"are {num_plates} plates" if num_plates > 1 else "is 1 plate"
