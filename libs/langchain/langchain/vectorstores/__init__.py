49e95dbb9722d2cd0ae71d6a001ebee6e5b6a8a1
Nikos Papailiou <nikolaos.papailiou@tiledb.com>
2023-10-31 11:28:13 +0200

    Add TileDB vector store

--- libs/langchain/langchain/vectorstores/__init__.py
+++ libs/langchain/langchain/vectorstores/__init__.py
@@ -348,6 +348,12 @@ def _import_tencentvectordb() -> Any:
     return TencentVectorDB
 
 
+def _import_tiledb() -> Any:
+    from langchain.vectorstores.tiledb import TileDB
+
+    return TileDB
+
+
 def _import_tigris() -> Any:
     from langchain.vectorstores.tigris import Tigris
 
@@ -515,6 +521,8 @@ def __getattr__(name: str) -> Any:
         return _import_tair()
     elif name == "TencentVectorDB":
         return _import_tencentvectordb()
+    elif name == "TileDB":
+        return _import_tiledb()
     elif name == "Tigris":
         return _import_tigris()
     elif name == "TimescaleVector":
@@ -592,6 +600,7 @@ __all__ = [
     "StarRocks",
     "SupabaseVectorStore",
     "Tair",
+    "TileDB",
     "Tigris",
     "TimescaleVector",
     "Typesense",
