a7fe34274bb4a800d7f8d11975f1b7175e237157
jacoblee93 <jacoblee93@gmail.com>
2023-10-30 21:59:15 -0700

    Adds version CLI command

--- libs/cli/langchain_cli/cli.py
+++ libs/cli/langchain_cli/cli.py
@@ -6,12 +6,35 @@ from typing_extensions import Annotated
 from langchain_cli.namespaces import app as app_namespace
 from langchain_cli.namespaces import template as template_namespace
 
+__version__ = "0.0.9"
+
 app = typer.Typer(no_args_is_help=True, add_completion=False)
 app.add_typer(
     template_namespace.package_cli, name="template", help=template_namespace.__doc__
 )
 app.add_typer(app_namespace.app_cli, name="app", help=app_namespace.__doc__)
 
+
+def version_callback(show_version: bool) -> None:
+    if show_version:
+        typer.echo(f"LangChain {__version__}")
+        raise typer.Exit()
+
+
+@app.callback()
+def main(
+    version: bool = typer.Option(
+        False,
+        "--version",
+        "-v",
+        help="Print the current CLI version.",
+        callback=version_callback,
+        is_eager=True,
+    ),
+):
+    pass
+
+main()
 
 @app.command()
 def serve(
