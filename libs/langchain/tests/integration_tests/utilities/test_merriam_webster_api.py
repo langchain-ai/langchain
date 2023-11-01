8848645f4572251d71b83267bd23fca8e1b1d2da
Josef Zoller <josef@walterzollerpiano.com>
2023-10-31 21:36:38 -0400

    Fix test documentation

--- libs/langchain/tests/integration_tests/utilities/test_merriam_webster_api.py
+++ libs/langchain/tests/integration_tests/utilities/test_merriam_webster_api.py
@@ -22,7 +22,10 @@ def test_call_no_result(api_client: MerriamWebsterAPIWrapper) -> None:
 
 
 def test_call_alternatives(api_client: MerriamWebsterAPIWrapper) -> None:
-    """Test that non-existent words return proper result."""
+    """
+    Test that non-existent queries that are close to an
+    existing definition return proper result.
+    """
     output = api_client.run("It's raining cats and dogs")
     assert "No Merriam-Webster definition was found for query" in output
     assert "You can try one of the following alternative queries" in output
