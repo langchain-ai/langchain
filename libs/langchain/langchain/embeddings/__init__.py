71ac4d0ca5dd61f2bf6f4452cd6af4365703c339
Lance Martin <lance@langchain.dev>
2023-11-01 16:50:35 -0700

    Lint

--- libs/langchain/langchain/embeddings/__init__.py
+++ libs/langchain/langchain/embeddings/__init__.py
@@ -53,8 +53,8 @@ from langchain.embeddings.mosaicml import MosaicMLInstructorEmbeddings
 from langchain.embeddings.nlpcloud import NLPCloudEmbeddings
 from langchain.embeddings.octoai_embeddings import OctoAIEmbeddings
 from langchain.embeddings.ollama import OllamaEmbeddings
+from langchain.embeddings.open_clip import OpenCLIPEmbeddings
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.embeddings.open_clip  import OpenCLIPEmbeddings
 from langchain.embeddings.sagemaker_endpoint import SagemakerEndpointEmbeddings
 from langchain.embeddings.self_hosted import SelfHostedEmbeddings
 from langchain.embeddings.self_hosted_hugging_face import (
--- libs/langchain/langchain/embeddings/open_clip.py
+++ libs/langchain/langchain/embeddings/open_clip.py
@@ -3,8 +3,8 @@ from typing import Any, Dict, List
 from langchain.pydantic_v1 import BaseModel, root_validator
 from langchain.schema.embeddings import Embeddings
 
-class OpenCLIPEmbeddings(BaseModel, Embeddings):
 
+class OpenCLIPEmbeddings(BaseModel, Embeddings):
     model: Any
     preprocess: Any
     tokenizer: Any
@@ -14,11 +14,12 @@ class OpenCLIPEmbeddings(BaseModel, Embeddings):
         """Validate that open_clip and torch libraries are installed."""
         try:
             import open_clip
-            import torch
 
-            model_name = 'ViT-B-32'
-            checkpoint = 'laion2b_s34b_b79k'
-            model, _, preprocess = open_clip.create_model_and_transforms(model_name=model_name, pretrained=checkpoint)
+            model_name = "ViT-B-32"
+            checkpoint = "laion2b_s34b_b79k"
+            model, _, preprocess = open_clip.create_model_and_transforms(
+                model_name=model_name, pretrained=checkpoint
+            )
             tokenizer = open_clip.get_tokenizer(model_name)
             values["model"] = model
             values["preprocess"] = preprocess
@@ -32,7 +33,9 @@ class OpenCLIPEmbeddings(BaseModel, Embeddings):
         return values
 
     def embed_documents(self, texts: List[str]) -> List[List[float]]:
-        text_features = [self.model.encode_text(self.tokenizer(text)).tolist() for text in texts]
+        text_features = [
+            self.model.encode_text(self.tokenizer(text)).tolist() for text in texts
+        ]
         return text_features
 
     def embed_query(self, text: str) -> List[float]:
@@ -44,5 +47,8 @@ class OpenCLIPEmbeddings(BaseModel, Embeddings):
         except ImportError:
             raise ImportError("Please install the PIL library: pip install pillow")
         pil_images = [_PILImage.fromarray(image) for image in images]
-        image_features = [self.model.encode_image(self.preprocess(pil_image).unsqueeze(0)).tolist() for pil_image in pil_images]
+        image_features = [
+            self.model.encode_image(self.preprocess(pil_image).unsqueeze(0)).tolist()
+            for pil_image in pil_images
+        ]
         return image_features
