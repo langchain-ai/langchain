1f14c7385dc4b812f41e9be87c715c2b31da003a
Lara <63805048+larkgz@users.noreply.github.com>
2023-10-31 20:29:01 -0400

    Add tests for Merriam-Webster API

--- libs/langchain/tests/integration_tests/utilities/test_merriam_webster_api.py
+++ libs/langchain/tests/integration_tests/utilities/test_merriam_webster_api.py
@@ -1,7 +1,7 @@
-"""Integration test for Wikipedia API Wrapper."""
+"""Integration test for Merriam Webster API Wrapper."""
 import pytest
 
-from langchain.utilities import MerriamWebsterAPIWrapper
+from langchain.utilities.merriam_webster import MerriamWebsterAPIWrapper
 
 
 @pytest.fixture
@@ -9,6 +9,10 @@ def api_client() -> MerriamWebsterAPIWrapper:
     return MerriamWebsterAPIWrapper()
 
 
-def test_run_llm(api_client: MerriamWebsterAPIWrapper) -> None:
+def test_call(api_client: MerriamWebsterAPIWrapper) -> None:
     output = api_client.run("LLM")
     assert "large language model" in output
+
+def test_call_no_result(api_client: MerriamWebsterAPIWrapper) -> None:
+    output = api_client.run("NO_RESULT_NO_RESULT_NO_RESULT")
+    assert "No Merriam-Webster definition was found for query" in output
