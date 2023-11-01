0375d89a4f6d515e47e32a6ed2a85335d1e9497a
Josef Zoller <josef@walterzollerpiano.com>
2023-10-31 21:32:08 -0400

    Add test case for alternatives

--- libs/langchain/tests/integration_tests/utilities/test_merriam_webster_api.py
+++ libs/langchain/tests/integration_tests/utilities/test_merriam_webster_api.py
@@ -19,3 +19,11 @@ def test_call_no_result(api_client: MerriamWebsterAPIWrapper) -> None:
     """Test that non-existent words return proper result."""
     output = api_client.run("NO_RESULT_NO_RESULT_NO_RESULT")
     assert "No Merriam-Webster definition was found for query" in output
+
+
+def test_call_alternatives(api_client: MerriamWebsterAPIWrapper) -> None:
+    """Test that non-existent words return proper result."""
+    output = api_client.run("It's raining cats and dogs")
+    assert "No Merriam-Webster definition was found for query" in output
+    assert "You can try one of the following alternative queries" in output
+    assert "raining cats and dogs" in output
