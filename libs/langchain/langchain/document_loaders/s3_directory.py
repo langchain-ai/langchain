17eed79f704bcd470e7082af2ebe0665edadc8ac
V.Prasanna kumar <v.prasannakumar1998@gmail.com>
2023-11-03 17:02:58 +0530

    Update s3_directory.py
    read all the files inside the sub-directory as well

--- libs/langchain/langchain/document_loaders/s3_directory.py
+++ libs/langchain/langchain/document_loaders/s3_directory.py
@@ -119,18 +119,21 @@ class S3DirectoryLoader(BaseLoader):
         bucket = s3.Bucket(self.bucket)
         docs = []
         for obj in bucket.objects.filter(Prefix=self.prefix):
-            loader = S3FileLoader(
-                self.bucket,
-                obj.key,
-                region_name=self.region_name,
-                api_version=self.api_version,
-                use_ssl=self.use_ssl,
-                verify=self.verify,
-                endpoint_url=self.endpoint_url,
-                aws_access_key_id=self.aws_access_key_id,
-                aws_secret_access_key=self.aws_secret_access_key,
-                aws_session_token=self.aws_session_token,
-                boto_config=self.boto_config,
-            )
-            docs.extend(loader.load())
+            if obj.key.endswith("/"):
+                continue
+            else:
+                loader = S3FileLoader(
+                    self.bucket,
+                    obj.key,
+                    region_name=self.region_name,
+                    api_version=self.api_version,
+                    use_ssl=self.use_ssl,
+                    verify=self.verify,
+                    endpoint_url=self.endpoint_url,
+                    aws_access_key_id=self.aws_access_key_id,
+                    aws_secret_access_key=self.aws_secret_access_key,
+                    aws_session_token=self.aws_session_token,
+                    boto_config=self.boto_config,
+                )
+                docs.extend(loader.load())
         return docs
