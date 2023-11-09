a2bcae62cb4bbc05854910b0afa3143011777ffa
Erick Friis <erick@langchain.dev>
2023-11-08 18:19:03 -0800

    format

--- templates/openai-functions-tool-retrieval-agent/openai_functions_tool_retrieval_agent/agent.py
+++ templates/openai-functions-tool-retrieval-agent/openai_functions_tool_retrieval_agent/agent.py
@@ -1,9 +1,7 @@
-import re
-from typing import Dict, List, Tuple, Union
+from typing import Dict, List, Tuple
 
 from langchain.agents import (
     AgentExecutor,
-    AgentOutputParser,
     Tool,
 )
 from langchain.agents.format_scratchpad import format_to_openai_functions
@@ -15,7 +13,7 @@ from langchain.prompts import (
     MessagesPlaceholder,
 )
 from langchain.pydantic_v1 import BaseModel, Field
-from langchain.schema import AgentAction, AgentFinish, Document
+from langchain.schema import Document
 from langchain.schema.messages import AIMessage, HumanMessage
 from langchain.schema.runnable import Runnable, RunnableLambda, RunnableParallel
 from langchain.tools.base import BaseTool
@@ -61,32 +59,6 @@ def get_tools(query: str) -> List[Tool]:
     return [ALL_TOOLS[d.metadata["index"]] for d in docs]
 
 
-class CustomOutputParser(AgentOutputParser):
-    def parse(self, llm_output: str) -> Union[AgentAction, AgentFinish]:
-        # Check if agent should finish
-        if "Final Answer:" in llm_output:
-            return AgentFinish(
-                # Return values is generally always a dictionary with a single `output` key
-                # It is not recommended to try anything else at the moment :)
-                return_values={"output": llm_output.split("Final Answer:")[-1].strip()},
-                log=llm_output,
-            )
-        # Parse out the action and action input
-        regex = r"Action\s*\d*\s*:(.*?)\nAction\s*\d*\s*Input\s*\d*\s*:[\s]*(.*)"
-        match = re.search(regex, llm_output, re.DOTALL)
-        if not match:
-            raise ValueError(f"Could not parse LLM output: `{llm_output}`")
-        action = match.group(1).strip()
-        action_input = match.group(2)
-        # Return the action and action input
-        return AgentAction(
-            tool=action, tool_input=action_input.strip(" ").strip('"'), log=llm_output
-        )
-
-
-output_parser = CustomOutputParser()
-
-llm = ChatOpenAI(temperature=0)
 assistant_system_message = """You are a helpful assistant. \
 Use tools (only if necessary) to best answer the users questions."""
 assistant_system_message = """You are a helpful assistant. \
