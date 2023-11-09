e8ed60cffba8dd5eb0454526ba8d39b4e902f2ad
Christophe Bornet <cbornet@hotmail.com>
2023-11-09 13:12:55 +0100

    Expose chat loaders objects in top-level package

--- libs/langchain/langchain/chat_loaders/__init__.py
+++ libs/langchain/langchain/chat_loaders/__init__.py
@@ -17,3 +17,42 @@ WhatsApp. The loaded chat messages can be used for fine-tuning models.
     ChatSession
 
 """  # noqa: E501
+
+from langchain.chat_loaders.base import BaseChatLoader, ChatSession
+from langchain.chat_loaders.facebook_messenger import (
+    FolderFacebookMessengerChatLoader,
+    SingleFileFacebookMessengerChatLoader,
+)
+from langchain.chat_loaders.gmail import GMailLoader
+from langchain.chat_loaders.imessage import IMessageChatLoader
+from langchain.chat_loaders.langsmith import (
+    LangSmithDatasetChatLoader,
+    LangSmithRunChatLoader,
+)
+from langchain.chat_loaders.slack import SlackChatLoader
+from langchain.chat_loaders.telegram import TelegramChatLoader
+from langchain.chat_loaders.utils import (
+    map_ai_messages,
+    map_ai_messages_in_session,
+    merge_chat_runs,
+    merge_chat_runs_in_session,
+)
+from langchain.chat_loaders.whatsapp import WhatsAppChatLoader
+
+__all__ = [
+    "BaseChatLoader",
+    "ChatSession",
+    "FolderFacebookMessengerChatLoader",
+    "GMailLoader",
+    "IMessageChatLoader",
+    "LangSmithDatasetChatLoader",
+    "LangSmithRunChatLoader",
+    "merge_chat_runs",
+    "merge_chat_runs_in_session",
+    "map_ai_messages",
+    "map_ai_messages_in_session",
+    "SingleFileFacebookMessengerChatLoader",
+    "SlackChatLoader",
+    "TelegramChatLoader",
+    "WhatsAppChatLoader",
+]
