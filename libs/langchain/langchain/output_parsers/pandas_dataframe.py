7366168c56c3d818ec945e91b64a1d1f448c9d8a
Rohan Dey <58871401+RohanDey02@users.noreply.github.com>
2023-11-09 22:16:02 -0500

    [LC-11532] Passed linter and test cases

--- libs/langchain/langchain/output_parsers/pandas_dataframe.py
+++ libs/langchain/langchain/output_parsers/pandas_dataframe.py
@@ -1,9 +1,14 @@
-import pandas as pd
 import re
-from typing import Any, Dict, List, Tuple
+from typing import Any, Dict, List, Tuple, Union
+
+import pandas as pd
+
+from langchain.output_parsers.format_instructions import (
+    PANDAS_DATAFRAME_FORMAT_INSTRUCTIONS,
+)
 from langchain.pydantic_v1 import validator
 from langchain.schema import BaseOutputParser, OutputParserException
-from langchain.output_parsers.format_instructions import PANDAS_DATAFRAME_FORMAT_INSTRUCTIONS
+
 
 class PandasDataFrameOutputParser(BaseOutputParser):
     """Parse an output using Pandas DataFrame format."""
@@ -12,104 +17,141 @@ class PandasDataFrameOutputParser(BaseOutputParser):
     dataframe: Any
 
     @validator("dataframe")
-    def validate_dataframe(cls, val):
+    def validate_dataframe(cls, val: Any) -> Any:
         if issubclass(type(val), pd.DataFrame):
             return val
         if pd.DataFrame(val).empty:
             raise ValueError("DataFrame cannot be empty.")
 
-        raise TypeError("Wrong type for 'dataframe', must be subclass of Pandas DataFrame (pd.DataFrame)")
-    
-    def parse_array(self, array: str, original_request_params: str) -> Tuple[List[int], str]:
-        parsed_array = []
+        raise TypeError(
+            "Wrong type for 'dataframe', must be a subclass \
+                of Pandas DataFrame (pd.DataFrame)"
+        )
+
+    def parse_array(
+        self, array: str, original_request_params: str
+    ) -> Tuple[List[Union[int, str]], str]:
+        parsed_array: List[Union[int, str]] = []
 
         # Check if the format is [1,3,5]
-        if re.match(r'\[\d+(,\s*\d+)*\]', array):
-            parsed_array = [int(i) for i in re.findall(r'\d+', array)]
+        if re.match(r"\[\d+(,\s*\d+)*\]", array):
+            parsed_array = [int(i) for i in re.findall(r"\d+", array)]
         # Check if the format is [1..5]
-        elif re.match(r'\[(\d+)\.\.(\d+)\]', array):
-            match = re.match(r'\[(\d+)\.\.(\d+)\]', array)
-            start, end = map(int, match.groups())
-            parsed_array = list(range(start, end + 1))
+        elif re.match(r"\[(\d+)\.\.(\d+)\]", array):
+            match = re.match(r"\[(\d+)\.\.(\d+)\]", array)
+            if match:
+                start, end = map(int, match.groups())
+                parsed_array = list(range(start, end + 1))
+            else:
+                raise OutputParserException(
+                    f"Unable to parse the array provided in {array}. \
+                        Please check the format instructions."
+                )
         # Check if the format is ["column_name"]
-        elif re.match(r'\[[a-zA-Z0-9_]+(?:,[a-zA-Z0-9_]+)*\]', array):
-            match = re.match(r'\[[a-zA-Z0-9_]+(?:,[a-zA-Z0-9_]+)*\]', array)
-            parsed_array = match.group().strip("[]").split(",")
+        elif re.match(r"\[[a-zA-Z0-9_]+(?:,[a-zA-Z0-9_]+)*\]", array):
+            match = re.match(r"\[[a-zA-Z0-9_]+(?:,[a-zA-Z0-9_]+)*\]", array)
+            if match:
+                parsed_array = list(map(str, match.group().strip("[]").split(",")))
+            else:
+                raise OutputParserException(
+                    f"Unable to parse the array provided in {array}. \
+                        Please check the format instructions."
+                )
 
         # Validate the array
-        if parsed_array == []:
+        if not parsed_array:
             raise OutputParserException(
-                f"Request parameter '{original_request_params}' has an invalid array format. Please refer to the format instructions."
+                f"Invalid array format in '{original_request_params}'. \
+                    Please check the format instructions."
             )
-        elif type(parsed_array[0]) == int and parsed_array[-1] > self.dataframe.index.max():
+        elif (
+            isinstance(parsed_array[0], int)
+            and parsed_array[-1] > self.dataframe.index.max()
+        ):
             raise OutputParserException(
-                f"The specified maximum index {parsed_array[-1]} exceeds the maximum index of the Pandas DataFrame {self.dataframe.index.max()}"
+                f"The maximum index {parsed_array[-1]} exceeds the maximum index of \
+                    the Pandas DataFrame {self.dataframe.index.max()}."
             )
 
-        return parsed_array, original_request_params.split('[')[0]
+        return parsed_array, original_request_params.split("[")[0]
 
     def parse(self, request: str) -> Dict[str, Any]:
         stripped_request_params = None
-        splitted_request: Tuple[str, str] = request.strip().split(':')
+        splitted_request = request.strip().split(":")
         if len(splitted_request) != 2:
             raise OutputParserException(
-                f"Request '{request}' is not correctly formatted. Please refer to the format instructions."
+                f"Request '{request}' is not correctly formatted. \
+                    Please refer to the format instructions."
             )
         result = {}
         try:
             request_type, request_params = splitted_request
-            match request_type:
-                case 'Invalid column', 'Invalid operation':
-                    raise OutputParserException(
-                        f"{request}. Please refer to the format instructions."
-                    )
-                case 'column':
-                    array_exists = re.search(r'(\[.*?\])', request_params)
-                    if array_exists:
-                        parsed_array, stripped_request_params = self.parse_array(array_exists.group(1), request_params)
-                        filtered_df = self.dataframe[self.dataframe.index.isin(parsed_array)]
-                        if len(parsed_array) == 1:
-                            result[stripped_request_params] = filtered_df[stripped_request_params].iloc[parsed_array[0]]
-                        else:
-                            result[stripped_request_params] = filtered_df[stripped_request_params]
+            if request_type in {"Invalid column", "Invalid operation"}:
+                raise OutputParserException(
+                    f"{request}. Please check the format instructions."
+                )
+            array_exists = re.search(r"(\[.*?\])", request_params)
+            if array_exists:
+                parsed_array, stripped_request_params = self.parse_array(
+                    array_exists.group(1), request_params
+                )
+                if request_type == "column":
+                    filtered_df = self.dataframe[
+                        self.dataframe.index.isin(parsed_array)
+                    ]
+                    if len(parsed_array) == 1:
+                        result[stripped_request_params] = filtered_df[
+                            stripped_request_params
+                        ].iloc[parsed_array[0]]
                     else:
-                        result[request_params] = self.dataframe[request_params]
-                case 'row':
-                    array_exists = re.search(r'(\[.*?\])', request_params)
-                    if array_exists:
-                        parsed_array, stripped_request_params = self.parse_array(array_exists.group(1), request_params)
-                        filtered_df = self.dataframe[self.dataframe.columns.intersection(parsed_array)]
-                        if len(parsed_array) == 1:
-                            result[stripped_request_params] = filtered_df.iloc[int(stripped_request_params)][parsed_array[0]]
-                        else:
-                            result[stripped_request_params] = filtered_df.iloc[int(stripped_request_params)]
+                        result[stripped_request_params] = filtered_df[
+                            stripped_request_params
+                        ]
+                elif request_type == "row":
+                    filtered_df = self.dataframe[
+                        self.dataframe.columns.intersection(parsed_array)
+                    ]
+                    if len(parsed_array) == 1:
+                        result[stripped_request_params] = filtered_df.iloc[
+                            int(stripped_request_params)
+                        ][parsed_array[0]]
                     else:
-                        result[request_params] = self.dataframe.iloc[int(request_params)]
-                case _:
-                    array_exists = re.search(r'(\[.*?\])', request_params)
-                    if array_exists:
-                        parsed_array, stripped_request_params = self.parse_array(array_exists.group(1), request_params)
-                        filtered_df = self.dataframe[self.dataframe.index.isin(parsed_array)]
-                        result[request_type] = getattr(filtered_df[stripped_request_params], request_type)()
-                    else:
-                        result[request_type] = getattr(self.dataframe[request_params], request_type)()
-        except AttributeError:
+                        result[stripped_request_params] = filtered_df.iloc[
+                            int(stripped_request_params)
+                        ]
+                else:
+                    filtered_df = self.dataframe[
+                        self.dataframe.index.isin(parsed_array)
+                    ]
+                    result[request_type] = getattr(
+                        filtered_df[stripped_request_params], request_type
+                    )()
+            else:
+                if request_type == "column":
+                    result[request_params] = self.dataframe[request_params]
+                elif request_type == "row":
+                    result[request_params] = self.dataframe.iloc[int(request_params)]
+                else:
+                    result[request_type] = getattr(
+                        self.dataframe[request_params], request_type
+                    )()
+        except (AttributeError, IndexError, KeyError):
+            if request_type not in {"column", "row"}:
+                raise OutputParserException(
+                    f"Unsupported request type '{request_type}'. \
+                        Please check the format instructions."
+                )
             raise OutputParserException(
-                f"Request type '{request_type}' is possibly not supported. Please refer to the format instructions."
-            )
-        except IndexError:
-            raise OutputParserException(
-                f"""Requested index 
-                    {request_params if stripped_request_params is None else stripped_request_params} 
-                    out of bounds. Please refer to the format instructions."""
-            )
-        except KeyError:
-            raise OutputParserException(
-                f"""Requested row or column from {request_params} is not in the provided DataFrame.
-                    Please verify that it is in the DataFrame."""
+                f"""Requested index {
+                    request_params
+                    if stripped_request_params is None
+                    else stripped_request_params
+                } is out of bounds."""
             )
 
         return result
 
     def get_format_instructions(self) -> str:
-        return PANDAS_DATAFRAME_FORMAT_INSTRUCTIONS.format(columns=', '.join(self.dataframe.columns))
+        return PANDAS_DATAFRAME_FORMAT_INSTRUCTIONS.format(
+            columns=", ".join(self.dataframe.columns)
+        )
--- libs/langchain/tests/unit_tests/output_parsers/test_pandas_dataframe_parser.py
+++ libs/langchain/tests/unit_tests/output_parsers/test_pandas_dataframe_parser.py
@@ -1,15 +1,15 @@
 """Test PandasDataframeParser"""
 import pandas as pd
+
 from langchain.output_parsers.pandas_dataframe import PandasDataFrameOutputParser
 from langchain.schema import OutputParserException
 
-df = pd.DataFrame({
-    'chicken': [1, 2, 3, 4],
-    'veggies': [5, 4, 3, 2],
-    'steak':  [9, 8, 7, 6]
-})
+df = pd.DataFrame(
+    {"chicken": [1, 2, 3, 4], "veggies": [5, 4, 3, 2], "steak": [9, 8, 7, 6]}
+)
 
-parser = PandasDataFrameOutputParser(dataframe = df)
+parser = PandasDataFrameOutputParser(dataframe=df)
+
 
 # Test Invalid Column
 def test_pandas_output_parser_col_no_array() -> None:
@@ -19,6 +19,7 @@ def test_pandas_output_parser_col_no_array() -> None:
     except OutputParserException:
         assert True
 
+
 # Test Column with invalid array (above DataFrame max index)
 def test_pandas_output_parser_col_oob() -> None:
     try:
@@ -27,57 +28,64 @@ def test_pandas_output_parser_col_oob() -> None:
     except OutputParserException:
         assert True
 
+
 # Test Column with array [x]
 def test_pandas_output_parser_col_first_elem() -> None:
-    expected_output = {'chicken': 1}
+    expected_output = {"chicken": 1}
     actual_output = parser.parse("column:chicken[0]")
     assert actual_output == expected_output
 
+
 # Test Column with array [x,y,z]
 def test_pandas_output_parser_col_multi_elem() -> None:
-    expected_output = {'chicken': pd.Series([1, 2], name='chicken', dtype='int64')}
+    expected_output = {"chicken": pd.Series([1, 2], name="chicken", dtype="int64")}
     actual_output = parser.parse("column:chicken[0, 1]")
     for key in actual_output.keys():
-        assert expected_output['chicken'].equals(actual_output[key])
+        assert expected_output["chicken"].equals(actual_output[key])
+
 
 # Test Row with invalid row entry
-def test_pandas_output_parser_row_no_array():
+def test_pandas_output_parser_row_no_array() -> None:
     try:
         parser.parse("row:5")
         assert False, "Should have raised OutputParserException"
     except OutputParserException:
         assert True
 
+
 # Test Row with valid row entry
-def test_pandas_output_parser_row_first():
-    expected_output = {'1': pd.Series({'chicken': 2, 'veggies': 4, 'steak': 8})}
+def test_pandas_output_parser_row_first() -> None:
+    expected_output = {"1": pd.Series({"chicken": 2, "veggies": 4, "steak": 8})}
     actual_output = parser.parse("row:1")
-    assert actual_output['1'].equals(expected_output['1'])
+    assert actual_output["1"].equals(expected_output["1"])
+
 
 # Test Row with invalid col entry
-def test_pandas_output_parser_row_no_column():
+def test_pandas_output_parser_row_no_column() -> None:
     try:
         parser.parse("row:1[num_legs]")
         assert False, "Should have raised OutputParserException"
     except OutputParserException:
         assert True
 
+
 # Test Row with valid col entry
-def test_pandas_output_parser_row_col_1():
-    expected_output = {'1': 2}
+def test_pandas_output_parser_row_col_1() -> None:
+    expected_output = {"1": 2}
     actual_output = parser.parse("row:1[chicken]")
     assert actual_output == expected_output
 
-def test_pandas_output_parser_special_ops():
+
+def test_pandas_output_parser_special_ops() -> None:
     actual_output = [
-        {'mean': 3.0},
-        {'median': 3.0},
-        {'min': 2},
-        {'max': 4},
-        {'var': 1.0},
-        {'std': 1.0},
-        {'count': 3},
-        {'quantile': 3.0}
+        {"mean": 3.0},
+        {"median": 3.0},
+        {"min": 2},
+        {"max": 4},
+        {"var": 1.0},
+        {"std": 1.0},
+        {"count": 3},
+        {"quantile": 3.0},
     ]
 
     expected_output = [
@@ -88,12 +96,13 @@ def test_pandas_output_parser_special_ops():
         parser.parse("var:chicken[1..3]"),
         parser.parse("std:chicken[1..3]"),
         parser.parse("count:chicken[1..3]"),
-        parser.parse("quantile:chicken[1..3]")
+        parser.parse("quantile:chicken[1..3]"),
     ]
 
     assert actual_output == expected_output
 
-def test_pandas_output_parser_invalid_special_op():
+
+def test_pandas_output_parser_invalid_special_op() -> None:
     try:
         parser.parse("riemann_sum:chicken")
         assert False, "Should have raised OutputParserException"
