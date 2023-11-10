aa84e5c7df58dac44c1bcf79d000ae34ae62a0da
Bagatur <baskaryan@gmail.com>
2023-11-09 23:34:34 -0800

    nit

--- docs/scripts/copy_templates.py
+++ docs/scripts/copy_templates.py
@@ -30,7 +30,7 @@ TEMPLATES_INDEX_DESTINATION = DOCS_TEMPLATES_DIR / "index.md"
 with open(TEMPLATES_INDEX_DESTINATION, "r") as f:
     content = f.read()
 # replace relative links
-content = re.sub("\]\(\.\.\/", "\]\(\/docs\/templates\/", content)
+content = re.sub("\]\(\.\.\/", "](/docs/templates/", content)
 with open(TEMPLATES_INDEX_DESTINATION, "w") as f:
     f.write(sidebar_hidden + content)
 
