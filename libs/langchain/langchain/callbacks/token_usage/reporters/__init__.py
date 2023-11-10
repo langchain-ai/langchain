5c7b10f0dcdb36dbd0b5dbaa51b304506d0a0b62
Janos Tolgyesi <janos.tolgyesi@neosperience.com>
2023-11-10 18:48:20 +0100

    Correct spelling

--- libs/langchain/langchain/callbacks/token_usage/reporters/__init__.py
+++ libs/langchain/langchain/callbacks/token_usage/reporters/__init__.py
@@ -1,4 +1,4 @@
-"""This package contains reporters that send toke usage reports to a metrics
+"""This package contains reporters that send token usage reports to a metrics
 repository."""
 
 from .base import TokenUsageReport, TokenUsageReporter
