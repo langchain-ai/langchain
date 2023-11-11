cd2e2e864bfdd169e79562e578a469bc2a87f8a4
Erick Friis <erick@langchain.dev>
2023-11-10 16:49:27 -0800

    wip

--- templates/research-assistant/research_assistant/__init__.py
+++ templates/research-assistant/research_assistant/__init__.py
@@ -1,5 +1,3 @@
 from research_assistant.chain import chain
 
-chain.invoke("what is langchain")
-
 __all__ = ["chain"]
--- templates/research-assistant/research_assistant/search/web.py
+++ templates/research-assistant/research_assistant/search/web.py
@@ -1,4 +1,5 @@
 import json
+from typing import Any
 
 import requests
 from bs4 import BeautifulSoup
@@ -6,15 +7,15 @@ from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema.messages import SystemMessage
 from langchain.schema.output_parser import StrOutputParser
-from langchain.schema.runnable import RunnableMap
+from langchain.schema.runnable import (
+    Runnable,
+    RunnableLambda,
+    RunnableParallel,
+    RunnablePassthrough,
+)
 from langchain.utilities import DuckDuckGoSearchAPIWrapper
 
 ddg_search = DuckDuckGoSearchAPIWrapper()
-
-
-
-
-
 
 
 def scrape_text(url: str):
@@ -24,37 +25,36 @@ def scrape_text(url: str):
     # Check if the request was successful
     if response.status_code == 200:
         # Parse the content of the request with BeautifulSoup
-        soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response.text, "html.parser")
 
         # Extract all text from the webpage
-        page_text = soup.get_text(separator=' ', strip=True)
+        page_text = soup.get_text(separator=" ", strip=True)
 
         # Print the extracted text
         return page_text
     else:
-        return  f"Failed to retrieve the webpage: Status code {response.status_code}"
+        return f"Failed to retrieve the webpage: Status code {response.status_code}"
 
-def web_search(query:str , num_results: int):
+
+def web_search(query: str, num_results: int):
     results = ddg_search.results(query, num_results)
-    return [r['link'] for r in results]
-
-def generate_search_queries_prompt(question):
-    """ Generates the search queries prompt for the given question.
-    Args: question (str): The question to generate the search queries prompt for
-    Returns: str: The search queries prompt for the given question
-    """
-
-    return f'Write 3 google search queries to search online that form an objective opinion from the following: "{question}"'\
-           f'You must respond with a list of strings in the following format: ["query 1", "query 2", "query 3"].'
+    return [r["link"] for r in results]
 
 
-search_message = (generate_search_queries_prompt("{question}"))
-SEARCH_PROMPT = ChatPromptTemplate.from_messages([
-    ("system", "{agent_prompt}"),
-    ("user", search_message)
-])
+SEARCH_PROMPT = ChatPromptTemplate.from_messages(
+    [
+        ("system", "{agent_prompt}"),
+        (
+            "user",
+            "Write 3 google search queries to search online that form an "
+            "objective opinion from the following: {question}\n"
+            "You must respond with a list of strings in the following format: "
+            '["query 1", "query 2", "query 3"].',
+        ),
+    ]
+)
 
-AUTO_AGENT_INSTRUCTIONS =  """
+AUTO_AGENT_INSTRUCTIONS = """
 This task involves researching a given topic, regardless of its complexity or the availability of a definitive answer. The research is conducted by a specific agent, defined by its type and role, with each agent requiring distinct instructions.
 Agent
 The agent is determined by the field of the topic and the specific name of the agent that could be utilized to research the topic provided. Agents are categorized by their area of expertise, and each agent type is associated with a corresponding emoji.
@@ -79,10 +79,9 @@ response:
     "agent_role_prompt": "You are a world-travelled AI tour guide assistant. Your main purpose is to draft engaging, insightful, unbiased, and well-structured travel reports on given locations, including history, attractions, and cultural insights."
 }
 """
-CHOOSE_AGENT_PROMPT = ChatPromptTemplate.from_messages([
-    SystemMessage(content=AUTO_AGENT_INSTRUCTIONS),
-    ("user", "task: {task}")
-])
+CHOOSE_AGENT_PROMPT = ChatPromptTemplate.from_messages(
+    [SystemMessage(content=AUTO_AGENT_INSTRUCTIONS), ("user", "task: {task}")]
+)
 
 SUMMARY_TEMPLATE = """{text} 
 
@@ -96,29 +95,49 @@ Using the above text, answer in short the following question:
 if the question cannot be answered using the text, imply summarize the text. Include all factual information, numbers, stats etc if available."""
 SUMMARY_PROMPT = ChatPromptTemplate.from_template(SUMMARY_TEMPLATE)
 
-scrape_and_summarize = {
-    "question": lambda x: x["question"],
-    "text": lambda x: scrape_text(x['url'])[:10000],
-    "url": lambda x: x['url']
-} | RunnableMap({
-        "summary": SUMMARY_PROMPT | ChatOpenAI(temperature=0) | StrOutputParser(),
-        "url": lambda x: x['url']
-}) | (lambda x: f"Source Url: {x['url']}\nSummary: {x['summary']}")
+scrape_and_summarize: Runnable[Any, Any] = (
+    RunnableParallel(
+        {
+            "question": lambda x: x["question"],
+            "text": lambda x: scrape_text(x["url"])[:10000],
+            "url": lambda x: x["url"],
+        }
+    )
+    | RunnableParallel(
+        {
+            "summary": SUMMARY_PROMPT | ChatOpenAI(temperature=0) | StrOutputParser(),
+            "url": lambda x: x["url"],
+        }
+    )
+    | RunnableLambda(lambda x: f"Source Url: {x['url']}\nSummary: {x['summary']}")
+)
 
 multi_search = (
-    lambda x: [
-        {"url": url, "question": x["question"]}
-        for url in web_search(query=x["question"], num_results=3)
-   ]
-) | scrape_and_summarize.map() | (lambda x: "\n".join(x))
+    (
+        lambda x: [
+            {"url": url, "question": x["question"]}
+            for url in web_search(query=x["question"], num_results=3)
+        ]
+    )
+    | scrape_and_summarize.map()
+    | (lambda x: "\n".join(x))
+)
 
-search_query = SEARCH_PROMPT | ChatOpenAI(temperature=0) | StrOutputParser() | json.loads
-choose_agent = CHOOSE_AGENT_PROMPT | ChatOpenAI(temperature=0) | StrOutputParser() | json.loads
+search_query = (
+    SEARCH_PROMPT | ChatOpenAI(temperature=0) | StrOutputParser() | json.loads
+)
+choose_agent = (
+    CHOOSE_AGENT_PROMPT | ChatOpenAI(temperature=0) | StrOutputParser() | json.loads
+)
 
-get_search_queries = {
-    "question": lambda x: x,
-    "agent_prompt": {"task": lambda x: x} | choose_agent | (lambda x: x["agent_role_prompt"])
-} | search_query
+get_search_queries = (
+    RunnablePassthrough().assign(
+        agent_prompt=RunnableParallel({"task": lambda x: x})
+        | choose_agent
+        | (lambda x: x["agent_role_prompt"])
+    )
+    | search_query
+)
 
 
 chain = (
@@ -127,4 +146,3 @@ chain = (
     | multi_search.map()
     | (lambda x: "\n\n".join(x))
 )
-
