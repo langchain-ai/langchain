ee86a5474785d41d3709e75dee9bcae1a1b54602
Taqi Jaffri <tjaffri@docugami.com>
2023-11-11 22:57:14 -0800

    use dgml-utils 0.1.0

--- libs/langchain/langchain/document_loaders/docugami.py
+++ libs/langchain/langchain/document_loaders/docugami.py
@@ -22,7 +22,7 @@ PROJECTS_KEY = "projects"
 
 DEFAULT_API_ENDPOINT = "https://api.docugami.com/v1preview1"
 DEFAULT_MAX_TEXT_LENGTH = 1024
-DEFAULT_MIN_CHUNK_SIZE = 32
+DEFAULT_MIN_TEXT_LENGTH = 32
 
 logger = logging.getLogger(__name__)
 
@@ -42,14 +42,14 @@ class DocugamiLoader(BaseLoader, BaseModel):
     max_text_length = DEFAULT_MAX_TEXT_LENGTH
     """Max length of chunk and metadata values."""
 
-    min_chunk_size: int = DEFAULT_MIN_CHUNK_SIZE
+    min_text_length: int = DEFAULT_MIN_TEXT_LENGTH
     """Threshold under which chunks are appended to next chunk to avoid over-chunking."""
 
-    include_xml_tags: bool = False
+    xml_mode: bool = False
     """Set to true for XML tags in chunk output text."""
 
-    xml_hierarchy_levels: int = 0
-    """Set appropriately to get parent chunks using the XML hierarchy. Must be 0 if include_xml_tags is False."""
+    parent_hierarchy_levels: int = 0
+    """Set appropriately to get parent chunks using the chunk hierarchy."""
 
     sub_chunk_tables: bool = False
     """Set to True to return sub-chunks within tables."""
@@ -104,7 +104,7 @@ class DocugamiLoader(BaseLoader, BaseModel):
 
         try:
             from dgml_utils.models import Chunk
-            from dgml_utils.segmentation import get_leaf_structural_chunks
+            from dgml_utils.segmentation import get_chunks
         except ImportError:
             raise ValueError(
                 "Could not import from dgml-utils python package. "
@@ -129,19 +129,19 @@ class DocugamiLoader(BaseLoader, BaseModel):
                 metadata=metadata,
             )
 
-        # parse the tree and return chunks
+        # Parse the tree and return chunks
         tree = etree.parse(io.BytesIO(content))
         root = tree.getroot()
 
         framework_chunks: List[Document] = []
-        dg_chunks = get_leaf_structural_chunks(
+        dg_chunks = get_chunks(
             root,
-            min_chunk_size=self.min_chunk_size,
+            min_text_length=self.min_text_length,
+            max_text_length=self.max_text_length,
             whitespace_normalize_text=self.whitespace_normalize_text,
             sub_chunk_tables=self.sub_chunk_tables,
-            include_xml_tags=self.include_xml_tags,
-            xml_hierarchy_levels=self.xml_hierarchy_levels,
-            max_text_size=self.max_text_length,
+            xml_mode=self.xml_mode,
+            parent_hierarchy_levels=self.parent_hierarchy_levels
         )
 
         for dg_chunk in dg_chunks:
@@ -232,7 +232,7 @@ class DocugamiLoader(BaseLoader, BaseModel):
                 doc_id = artifact_doc["id"]
                 metadata: Dict = {}
 
-                # the evaluated XML for each document is named after the project
+                # The evaluated XML for each document is named after the project
                 response = requests.request(
                     "GET",
                     f"{artifact_url}/content",
@@ -314,7 +314,7 @@ class DocugamiLoader(BaseLoader, BaseModel):
                     self.docset_id, doc, doc_metadata
                 )
         elif self.file_paths:
-            # local mode (for integration testing, or pre-downloaded XML)
+            # Local mode (for integration testing, or pre-downloaded XML)
             for path in self.file_paths:
                 path = Path(path)
                 with open(path, "rb") as file:
@@ -327,106 +327,3 @@ class DocugamiLoader(BaseLoader, BaseModel):
                     )
 
         return chunks
-
-    from langchain.chat_models import ChatOpenAI
-
-    def get_chain(
-        self,
-        llm=ChatOpenAI(temperature=0, model="gpt-4"),
-    ):
-        if not self.access_token and self.docset_id:
-            raise Exception(f"Please specify a docset ID to use agent executor")
-
-        from langchain.sql_database import SQLDatabase
-        from langchain.agents.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
-        from langchain.agents.agent_toolkits.sql.base import create_sql_agent
-        from langchain.agents.agent_types import AgentType
-
-        import sqlite3
-        import pandas as pd
-        import tempfile
-
-        response = requests.request(
-            "GET",
-            f"{self.api}/projects/?docset.id={self.docset_id}",
-            headers={"Authorization": f"Bearer {self.access_token}"},
-            data={},
-        )
-        if response.ok:
-            response_json = response.json()
-            projects = response_json["projects"]
-            if not projects or not projects[0]["id"]:
-                raise Exception(
-                    f"Could not access published projects for docset: {self.docset_id}"
-                )
-
-            project_id = projects[0]["id"]
-            project_name = projects[0].get("name") or "Report"
-            response = requests.request(
-                "GET",
-                f"{self.api}/projects/{project_id}/artifacts/latest?name=spreadsheet.xlsx",
-                headers={"Authorization": f"Bearer {self.access_token}"},
-                data={},
-            )
-            if response.ok:
-                response_json = response.json()
-                artifacts = response_json["artifacts"]
-                if not artifacts or not artifacts[0]["downloadUrl"]:
-                    raise Exception(
-                        f"Could not find download URL for latest artifact in project: {project_id}"
-                    )
-
-                download_url = artifacts[0]["downloadUrl"]
-
-                response = requests.request(
-                    "GET",
-                    download_url,
-                    headers={"Authorization": f"Bearer {self.access_token}"},
-                    data={},
-                )
-                if response.ok:
-                    temp_xlsx_file = tempfile.NamedTemporaryFile(
-                        suffix=".xlsx", delete=False
-                    )
-                    temp_xlsx_path = temp_xlsx_file.name
-                    temp_xlsx_file.close()
-
-                    with open(temp_xlsx_path, "wb") as f:
-                        f.write(response.content)
-                        f.flush()
-
-                    # Create a temporary SQLite database in memory
-                    in_memory_sqlite_connection = sqlite3.connect(":memory:")
-
-                    # Read the Excel file using pandas (only the first sheet)
-                    df = pd.read_excel(temp_xlsx_path, sheet_name=0)
-
-                    # Write the Excel file from pandas to the in-memory sqlite connection
-                    df.to_sql(
-                        project_name,
-                        in_memory_sqlite_connection,
-                        if_exists="replace",
-                        index=False,
-                    )
-
-                    # Dump the in-memory sqlite connection to a db file on disk
-                    temp_db_file = tempfile.NamedTemporaryFile(suffix=".sqlite")
-                    with sqlite3.connect(temp_db_file.name) as disk_conn:
-                        in_memory_sqlite_connection.backup(disk_conn)
-
-                    # Connect to the db file on disk
-                    db = SQLDatabase.from_uri(
-                        f"sqlite:///{temp_db_file.name}", sample_rows_in_table_info=3
-                    )
-
-                    toolkit = SQLDatabaseToolkit(db=db, llm=llm)
-                    return create_sql_agent(
-                        llm=llm,
-                        toolkit=toolkit,
-                        verbose=True,
-                        agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-                    )
-                else:
-                    raise Exception(f"Failed to download XLSX for {project_id}")
-            else:
-                raise Exception("Failed to get docsets.")
