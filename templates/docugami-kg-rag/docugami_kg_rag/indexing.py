521731717dcdacab6ac00732e32d7a924cf7522a
Taqi Jaffri <tjaffri@gmail.com>
2023-11-12 15:17:37 -0800

    add system prompt and other cleanup

--- templates/docugami-kg-rag/docugami_kg_rag/indexing.py
+++ templates/docugami-kg-rag/docugami_kg_rag/indexing.py
@@ -7,23 +7,23 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
 
 if __name__ == "__main__":
+    EMBEDDINGS=OpenAIEmbeddings()
     DOCUGAMI_API_KEY = os.environ.get("DOCUGAMI_API_KEY")
     PINECONE_INDEX_NAME = os.environ.get("PINECONE_INDEX", "langchain-test")
+    DOCSET_ID = "fi6vi49cmeac"
 
     loader = DocugamiLoader(
-        docset_id="4eik4b8kuoup",
-        document_ids=["4vevuqrux0yb"],
+        docset_id=DOCSET_ID,
         min_text_length=32,
         max_text_length=1024 * 8,
         sub_chunk_tables=False,
         xml_mode=True,
-        parent_hierarchy_levels=2
+        parent_hierarchy_levels=0
     )
 
     data = loader.load()
 
     # Add to vectorDB
-    vectorstore = Pinecone.from_documents(
-        documents=data, embedding=OpenAIEmbeddings(), index_name=PINECONE_INDEX_NAME
+    Pinecone.from_documents(
+        documents=data, embedding=EMBEDDINGS, index_name=PINECONE_INDEX_NAME,
     )
-    retriever = vectorstore.as_retriever()
