4020d149706534d393a70f89c3429f66c45905c1
Bagatur <baskaryan@gmail.com>
2023-11-14 11:52:17 -0800

    fmt

--- libs/langchain/tests/integration_tests/llms/test_azure_openai.py
+++ libs/langchain/tests/integration_tests/llms/test_azure_openai.py
@@ -14,12 +14,12 @@ from tests.unit_tests.callbacks.fake_callback_handler import FakeCallbackHandler
 OPENAI_API_VERSION = os.environ.get("AZURE_OPENAI_API_VERSION", "")
 OPENAI_API_BASE = os.environ.get("AZURE_OPENAI_API_BASE", "")
 OPENAI_API_KEY = os.environ.get("AZURE_OPENAI_API_KEY", "")
-DEPLOYMENT_NAME = os.environ.get("AZURE_OPENAI_DEPLOYMENT_NAME", "td2")
+DEPLOYMENT_NAME = os.environ.get("AZURE_OPENAI_DEPLOYMENT_NAME", "")
 
 
 def _get_llm(**kwargs: Any) -> AzureOpenAI:
     return AzureOpenAI(
-        deployment_name="td2",
+        deployment_name=DEPLOYMENT_NAME,
         openai_api_version=OPENAI_API_VERSION,
         openai_api_base=OPENAI_API_BASE,
         openai_api_key=OPENAI_API_KEY,
