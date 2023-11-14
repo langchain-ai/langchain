25375085278d2494f1c63a8ec890d69789d95291
Wang Wei <wangwei1237@gmail.com>
2023-11-14 11:49:49 +0800

    fix: fixed the lint errors

--- libs/langchain/langchain/output_parsers/ernie_functions.py
+++ libs/langchain/langchain/output_parsers/ernie_functions.py
@@ -74,7 +74,7 @@ class JsonOutputFunctionsParser(BaseCumulativeTransformOutputParser[Any]):
         message = generation.message
         message.additional_kwargs["function_call"] = {}
         if "function_call" in message.content:
-            function_call = json.loads(message.content)
+            function_call = json.loads(str(message.content))
             if "function_call" in function_call:
                 fc = function_call["function_call"]
                 message.additional_kwargs["function_call"] = fc
