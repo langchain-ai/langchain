b05991a0260016e0ffdf4c91b73ad0520aed686c
Nuno Campos <nuno@boringbits.io>
2023-11-15 12:41:13 +0000

    Lint

--- libs/langchain/langchain/callbacks/tracers/langchain.py
+++ libs/langchain/langchain/callbacks/tracers/langchain.py
@@ -1,5 +1,8 @@
 """A Tracer implementation that records to LangChain endpoint."""
 
-from langchain.schema.callbacks.tracers.langchain import LangChainTracer
+from langchain.schema.callbacks.tracers.langchain import (
+    LangChainTracer,
+    wait_for_all_tracers,
+)
 
-__all__ = ["LangChainTracer"]
+__all__ = ["LangChainTracer", "wait_for_all_tracers"]
