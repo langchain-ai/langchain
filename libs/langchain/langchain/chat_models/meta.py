60859e198cd85a61f108f4606bbf22a5286884ec
wouter.durnez <wouter.durnez@showpad.com>
2023-11-15 15:31:30 +0100

    🩹 Fix for method

--- libs/langchain/langchain/chat_models/meta.py
+++ libs/langchain/langchain/chat_models/meta.py
@@ -8,7 +8,7 @@ from langchain.schema.messages import (
 )
 
 
-def _convert_one_message_to_text_llama(self, message: BaseMessage) -> str:
+def _convert_one_message_to_text_llama(message: BaseMessage) -> str:
     if isinstance(message, ChatMessage):
         message_text = f"\n\n{message.role.capitalize()}: {message.content}"
     elif isinstance(message, HumanMessage):
@@ -21,7 +21,7 @@ def _convert_one_message_to_text_llama(self, message: BaseMessage) -> str:
         raise ValueError(f"Got unknown type {message}")
     return message_text
 
-def convert_messages_to_prompt_llama(self, messages: List[BaseMessage]) -> str:
+def convert_messages_to_prompt_llama(messages: List[BaseMessage]) -> str:
     return "\n".join(
         [_convert_one_message_to_text_llama(message) for message in messages]
     )
