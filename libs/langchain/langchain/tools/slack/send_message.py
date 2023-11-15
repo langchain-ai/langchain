14b45813113f34e2ec351fe5152e7ec9273cf38c
gzyJoy <joy.gong@mail.utoronto.ca>
2023-11-15 16:02:12 +0000

    token unauthned

--- libs/langchain/langchain/tools/slack/send_message.py
+++ libs/langchain/langchain/tools/slack/send_message.py
@@ -3,8 +3,8 @@ from typing import List, Optional, Type
 from langchain.callbacks.manager import CallbackManagerForToolRun
 from langchain.pydantic_v1 import BaseModel, Field
 from langchain.tools.slack.base import SlackBaseTool
-
-
+from slack_sdk.errors import SlackApiError
+import logging
 class SendMessageSchema(BaseModel):
     """Input for SendMessageTool."""
 
@@ -33,7 +33,19 @@ class SlackSendMessage(SlackBaseTool):
         channel: str,
         run_manager: Optional[CallbackManagerForToolRun] = None,
     ) -> str:
+        logger = logging.getLogger(__name__)
+        logger.error("Message: %s", message)
+        logger.error("Channel: %s", channel)
         result = self.client.chat_postMessage(channel=channel, text=message)
-
         output = "Message sent: " + str(result)
+        # output = "msg sent"
         return output
+        # try:
+        #     result = self.client.chat_postMessage(channel=channel, text=message)
+        #     output = "Message sent: " + str(result)
+        #     return output
+        # except SlackApiError as e:
+        #     logger.error("Error: %s", e)
+        #     logger.error("Failed to send message")
+        #     return "Failed to send message"
+        
--- libs/langchain/langchain/tools/slack/utils.py
+++ libs/langchain/langchain/tools/slack/utils.py
@@ -24,16 +24,30 @@ def login() -> WebClient:
     if "SLACK_BOT_TOKEN" in os.environ:
         token = os.environ["SLACK_BOT_TOKEN"]
         client = WebClient(token=token)
+        logger.info("slack login success")
         return client
     elif "SLACK_USER_TOKEN" in os.environ:
         token = os.environ["SLACK_USER_TOKEN"]
         client = WebClient(token=token)
+        logger.info("slack login success")
         return client
     else:
         logger.error(
             "Error: The SLACK_BOT_TOKEN or SLACK_USER_TOKEN environment variable have not "
             "been set."
         )
+
+# def login() -> WebClient:
+#     try:
+#         from slack_sdk import WebClient
+#     except ImportError as e:
+#         raise ImportError(
+#             "Cannot import slack_sdk. Please install the package with `pip install slack_sdk`."
+#         ) from e
+        
+#     client = WebClient(token="xoxp-6040211441731-6052945665441-6048335428100-5909cdff7e6e11a55541834dc69dd9b6")
+#     return client
+
 def authenticate() -> WebClient:
     """Authenticate using the Microsoft Grah API"""
     try:
