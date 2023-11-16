2fc8300c974640c944cb33c930f3732b254502a8
Sam Kemp <samkemp@microsoft.com>
2023-11-16 16:27:22 +0000

    Revert "rebase"
    This reverts commit 9a2ad3dfc4ce2f71de4a548a907b3105c7ce9b98.

--- .devcontainer/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-poetry completions bash >> ~/.bash_completion
-cd libs/langchain
-# Installs main, test, codespell, lint, typing (all non-optional) and dev dependencies
-poetry install --no-interaction --no-ansi --with dev
