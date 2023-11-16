dda28b1602af76ae8b8c76a1e3ec63914f050b39
umair <umairapi1995@gmail.com>
2023-11-16 21:08:25 +0500

    fix llm stop word escape regex

--- libs/langchain/langchain/llms/utils.py
+++ libs/langchain/langchain/llms/utils.py
@@ -5,4 +5,8 @@ from typing import List
 
 def enforce_stop_tokens(text: str, stop: List[str]) -> str:
     """Cut off the text as soon as any stop words occur."""
-    return re.split("|".join(stop), text, maxsplit=1)[0]
+
+    escaped_stop = [re.escape(word) for word in stop]
+    pattern = "|".join(escaped_stop)
+
+    return re.split(pattern, text, maxsplit=1)[0]
