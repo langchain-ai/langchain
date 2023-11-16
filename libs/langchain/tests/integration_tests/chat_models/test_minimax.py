ae040ce695f3353105ca16f6f3aa6485a7f75530
Peanut <vvanglro@gmail.com>
2023-11-16 07:29:40 +0000

    style: format

--- libs/langchain/tests/integration_tests/chat_models/test_minimax.py
+++ libs/langchain/tests/integration_tests/chat_models/test_minimax.py
@@ -2,7 +2,11 @@ from langchain.chat_models import MiniMaxChat
 
 
 def test_minimaxchat_init_client():
-    chat = MiniMaxChat(minimax_api_host="test_host", minimax_api_key="test_api_key", minimax_group_id="test_group_id")
+    chat = MiniMaxChat(
+        minimax_api_host="test_host",
+        minimax_api_key="test_api_key",
+        minimax_group_id="test_group_id",
+    )
     assert chat._client
     assert chat._client.host == "test_host"
     assert chat._client.group_id == "test_group_id"
