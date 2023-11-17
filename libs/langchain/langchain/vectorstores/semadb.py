FIX: normalization in relevance functions; include cosine similarity and cosine distance strategies and relevance functions

libs/langchain/langchain/vectorstores/semadb.py
@@ -21,7 +21,7 @@ class SemaDB(VectorStore):
 
             from langchain.vectorstores import SemaDB
 
-            db = SemaDB('mycollection', 768, embeddings, DistanceStrategy.COSINE)
+            db = SemaDB('mycollection', 768, embeddings, DistanceStrategy.COSINE_DISTANCE)
 
     """
 
@@ -62,8 +62,10 @@ class SemaDB(VectorStore):
             return "dot"
         elif self.distance_strategy == DistanceStrategy.JACCARD:
             raise ValueError("Max inner product is not supported by SemaDB")
-        elif self.distance_strategy == DistanceStrategy.COSINE:
+        elif self.distance_strategy == DistanceStrategy.COSINE_DISTANCE:
             return "cosine"
+        elif self.distance_strategy == DistanceStrategy.COSINE_SIMILARITY:
+            raise ValueError("Cosine similarity is not directly supported by SemaDB.")
         else:
             raise ValueError(f"Unknown distance strategy {self.distance_strategy}")
 
@@ -106,7 +108,7 @@ class SemaDB(VectorStore):
                 f"Embedding size mismatch {len(embeddings[0])} != {self.vector_size}"
             )
         # Normalise if needed
-        if self.distance_strategy == DistanceStrategy.COSINE:
+        if self.distance_strategy == DistanceStrategy.COSINE_DISTANCE:
             embed_matrix = np.array(embeddings)
             embed_matrix = embed_matrix / np.linalg.norm(
                 embed_matrix, axis=1, keepdims=True
@@ -183,7 +185,7 @@ class SemaDB(VectorStore):
     def _search_points(self, embedding: List[float], k: int = 4) -> List[dict]:
         """Search points."""
         # Normalise if needed
-        if self.distance_strategy == DistanceStrategy.COSINE:
+        if self.distance_strategy == DistanceStrategy.COSINE_DISTANCE:
             vec = np.array(embedding)
             vec = vec / np.linalg.norm(vec)
             embedding = vec.tolist()