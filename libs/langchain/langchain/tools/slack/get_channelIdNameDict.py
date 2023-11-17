3c761fe98fa1accaf1d6dd667deff6d41158da72
gzyJoy <joy.gong@mail.utoronto.ca>
2023-11-17 01:40:04 +0000

    lint fix, hopefully last time

--- libs/langchain/langchain/tools/slack/get_channelIdNameDict.py
+++ libs/langchain/langchain/tools/slack/get_channelIdNameDict.py
@@ -20,7 +20,6 @@ class SlackGetChannelIdNameDict(SlackBaseTool):
 
     def _run(
         self,
-        # channelid_name: dict,
         run_manager: Optional[CallbackManagerForToolRun] = None,
     ) -> str:
         logging.getLogger(__name__)
@@ -30,15 +29,7 @@ class SlackGetChannelIdNameDict(SlackBaseTool):
         self.save_conversations(result["channels"], channelId_Name)
 
         json.dumps(channelId_Name)
-        # print(channelId_Name_json)
-        # return channelId_Name_json
         return json.dumps(result["channels"])
-
-        # except SlackApiError as e:
-        #     logger.error("Error fetching conversations: {}".format(e))
-
-    # You probably want to use a database to store any conversations information ;)
-    # channelId_Name = {}
 
     @classmethod
     def save_conversations(self, conversations, channelId_Name)->None:
