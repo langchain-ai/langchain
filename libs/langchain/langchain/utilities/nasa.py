e4eb9781a65634106b42420e796901cc1ab6802e
Karam Daid <karam.daid@mail.utoronto.ca>
2023-11-17 16:17:00 -0500

    added nasa api wrapper

---
+++ libs/langchain/langchain/utilities/nasa.py
@@ -0,0 +1,44 @@
+"""Util that calls several NASA APIs."""
+import json
+import warnings
+from dataclasses import asdict, dataclass, fields
+from typing import Any, Dict, List, Mapping, Optional, Tuple, Type, Union
+
+import requests
+
+from langchain.pydantic_v1 import BaseModel, Extra, root_validator
+from langchain.utils import get_from_dict_or_env
+
+IMAGE_AND_VIDEO_LIBRARY_URL = "https://images-api.nasa.gov"
+EXOPLANETS_URL = "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?"
+
+
+class NasaAPIWrapper(BaseModel):
+
+    def get_image_or_video(self, query: str) -> str:
+        params = json.loads(query)
+        response = requests.get(IMAGE_AND_VIDEO_LIBRARY_URL, params=params)
+        data = response.json()
+        return data
+    
+
+
+    def get_expoplanet_info(self, query: str) -> str:
+        params = json.loads(query)
+        response = requests.get(EXOPLANETS_URL, params=params)
+        data = response.json()
+        return data
+    
+
+    def run(self, mode: str, query: str) -> str:
+        if mode == 'image_and_video_library':
+            output = self.get_image_or_video(query)
+        elif mode == 'exoplanet_database':
+            output = self.get_expoplanet_info(query)
+        else:
+            output = {"ModeError": f"Got unexpected mode {mode}."}
+
+        try:
+            return json.dumps(output)
+        except Exception:
+            return str(output)
