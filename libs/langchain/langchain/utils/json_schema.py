2259e99df08c3bc9ee82e13274a77f024b5b20b4
Bagatur <baskaryan@gmail.com>
2023-11-18 12:17:08 -0800

    bump 338, exp 42

--- libs/langchain/langchain/utils/json_schema.py
+++ libs/langchain/langchain/utils/json_schema.py
@@ -4,7 +4,7 @@ from copy import deepcopy
 from typing import Any, List, Optional, Sequence
 
 
-def _retrieve_ref(path: str, schema: dict) -> dict:
+def _retrieve_ref(path: str, schema: dict, *, ignore_errors: bool = False) -> dict:
     components = path.split("/")
     if components[0] != "#":
         raise ValueError(
@@ -12,13 +12,22 @@ def _retrieve_ref(path: str, schema: dict) -> dict:
             "with #."
         )
     out = schema
-    for component in components[1:]:
-        out = out[component]
+    try:
+        for component in components[1:]:
+            out = out[component]
+    except KeyError as e:
+        if ignore_errors:
+            return {"$ref": path}
+        raise e
     return deepcopy(out)
 
 
 def _dereference_refs_helper(
-    obj: Any, full_schema: dict, skip_keys: Sequence[str]
+    obj: Any,
+    full_schema: dict,
+    skip_keys: Sequence[str],
+    *,
+    ignore_errors: bool = False,
 ) -> Any:
     if isinstance(obj, dict):
         obj_out = {}
@@ -26,32 +35,45 @@ def _dereference_refs_helper(
             if k in skip_keys:
                 obj_out[k] = v
             elif k == "$ref":
-                ref = _retrieve_ref(v, full_schema)
-                return _dereference_refs_helper(ref, full_schema, skip_keys)
+                ref = _retrieve_ref(v, full_schema, ignore_errors=ignore_errors)
+                if ref == {k: v}:
+                    return ref
+                return _dereference_refs_helper(
+                    ref, full_schema, skip_keys, ignore_errors=ignore_errors
+                )
             elif isinstance(v, (list, dict)):
-                obj_out[k] = _dereference_refs_helper(v, full_schema, skip_keys)
+                obj_out[k] = _dereference_refs_helper(
+                    v, full_schema, skip_keys, ignore_errors=ignore_errors
+                )
             else:
                 obj_out[k] = v
         return obj_out
     elif isinstance(obj, list):
-        return [_dereference_refs_helper(el, full_schema, skip_keys) for el in obj]
+        return [
+            _dereference_refs_helper(
+                el, full_schema, skip_keys, ignore_errors=ignore_errors
+            )
+            for el in obj
+        ]
     else:
         return obj
 
 
-def _infer_skip_keys(obj: Any, full_schema: dict) -> List[str]:
+def _infer_skip_keys(obj: Any, full_schema: dict, ignore_errors: bool=False) -> List[str]:
     keys = []
     if isinstance(obj, dict):
         for k, v in obj.items():
             if k == "$ref":
-                ref = _retrieve_ref(v, full_schema)
+                ref = _retrieve_ref(v, full_schema, ignore_errors=ignore_errors)
+                if ref == {k: v}:
+                    continue
                 keys.append(v.split("/")[1])
-                keys += _infer_skip_keys(ref, full_schema)
+                keys += _infer_skip_keys(ref, full_schema, ignore_errors=ignore_errors)
             elif isinstance(v, (list, dict)):
-                keys += _infer_skip_keys(v, full_schema)
+                keys += _infer_skip_keys(v, full_schema, ignore_errors=ignore_errors)
     elif isinstance(obj, list):
         for el in obj:
-            keys += _infer_skip_keys(el, full_schema)
+            keys += _infer_skip_keys(el, full_schema, ignore_errors=ignore_errors)
     return keys
 
 
@@ -60,6 +82,7 @@ def dereference_refs(
     *,
     full_schema: Optional[dict] = None,
     skip_keys: Optional[Sequence[str]] = None,
+    ignore_errors: bool = False,
 ) -> dict:
     """Try to substitute $refs in JSON Schema."""
 
@@ -67,6 +90,8 @@ def dereference_refs(
     skip_keys = (
         skip_keys
         if skip_keys is not None
-        else _infer_skip_keys(schema_obj, full_schema)
+        else _infer_skip_keys(schema_obj, full_schema, ignore_errors=ignore_errors)
     )
-    return _dereference_refs_helper(schema_obj, full_schema, skip_keys)
+    return _dereference_refs_helper(
+        schema_obj, full_schema, skip_keys, ignore_errors=ignore_errors
+    )
