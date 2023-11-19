0afa6af748aa5ab2d64ce61c50ebbbd13f913403
Nuno Campos <nuno@boringbits.io>
2023-11-19 08:16:28 +0000

    Add one more export

--- libs/langchain/langchain/chat_models/base.py
+++ libs/langchain/langchain/chat_models/base.py
@@ -1,3 +1,13 @@
-from langchain_core.chat_model import BaseChatModel
-from langchain_core.chat_model import SimpleChatModel
-__all__ = ['BaseChatModel', 'SimpleChatModel']
+from langchain_core.chat_model import (
+    BaseChatModel,
+    SimpleChatModel,
+    _agenerate_from_stream,
+    _generate_from_stream,
+)
+
+__all__ = [
+    "BaseChatModel",
+    "SimpleChatModel",
+    "_generate_from_stream",
+    "_agenerate_from_stream",
+]
