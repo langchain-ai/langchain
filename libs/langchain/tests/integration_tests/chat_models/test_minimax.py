bc6ff7b0b38a4b87de1cfe9ae37aa2c3c00786bb
Peanut <vvanglro@gmail.com>
2023-11-20 02:51:15 +0000

    style: mypy

--- libs/langchain/tests/integration_tests/chat_models/test_minimax.py
+++ libs/langchain/tests/integration_tests/chat_models/test_minimax.py
@@ -1,7 +1,9 @@
 from langchain.chat_models import MiniMaxChat
+from langchain.llms.minimax import MinimaxCommon
 
 
-def test_minimaxchat_init_client():
+def test_minimaxchat_init_client() -> None:
+    "Test MiniMaxChat and MinimaxCommon model init _client attribute"
     chat = MiniMaxChat(
         minimax_api_host="test_host",
         minimax_api_key="test_api_key",
@@ -11,3 +13,13 @@ def test_minimaxchat_init_client():
     assert chat._client.host == "test_host"
     assert chat._client.group_id == "test_group_id"
     assert chat._client.api_key == "test_api_key"
+
+    chat2 = MinimaxCommon(
+        minimax_api_host="test_host",
+        minimax_api_key="test_api_key",
+        minimax_group_id="test_group_id",
+    )
+    assert chat2._client
+    assert chat2._client.host == "test_host"
+    assert chat2._client.group_id == "test_group_id"
+    assert chat2._client.api_key == "test_api_key"
