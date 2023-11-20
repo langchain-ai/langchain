c43a627a51999e76f494429e3c60603d944a29fb
David Ruan <ruanwz@gmail.com>
2023-11-20 12:51:51 +0800

    Update mongodb_atlas.py to fix ImportError: cannot import name 'DriverInfo' from 'pymongo'

--- libs/langchain/langchain/vectorstores/mongodb_atlas.py
+++ libs/langchain/langchain/vectorstores/mongodb_atlas.py
@@ -104,7 +104,8 @@ class MongoDBAtlasVectorSearch(VectorStore):
         try:
             from importlib.metadata import version
 
-            from pymongo import DriverInfo, MongoClient
+            from pymongo import MongoClient
+            from pymongo.driver_info import DriverInfo
         except ImportError:
             raise ImportError(
                 "Could not import pymongo, please install it with "
