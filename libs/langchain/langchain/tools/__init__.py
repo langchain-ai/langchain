af234b5adf2647c8f737c9456cb3d4d18ae59310
Bagatur <baskaryan@gmail.com>
2023-11-19 21:18:41 -0800

    fix

--- libs/langchain/langchain/tools/__init__.py
+++ libs/langchain/langchain/tools/__init__.py
@@ -19,109 +19,6 @@ tool for the job.
 from typing import Any
 
 from langchain.tools.base import BaseTool, StructuredTool, Tool, tool
-from langchain.tools.bing_search.tool import BingSearchResults, BingSearchRun
-from langchain.tools.brave_search.tool import BraveSearch
-from langchain.tools.cogniswitch.tool import CogniswitchAnswerTool, CogniswitchStoreTool
-from langchain.tools.ddg_search.tool import DuckDuckGoSearchResults, DuckDuckGoSearchRun
-from langchain.tools.edenai import (
-    EdenAiExplicitImageTool,
-    EdenAiObjectDetectionTool,
-    EdenAiParsingIDTool,
-    EdenAiParsingInvoiceTool,
-    EdenAiSpeechToTextTool,
-    EdenAiTextModerationTool,
-    EdenAiTextToSpeechTool,
-    EdenaiTool,
-)
-from langchain.tools.eleven_labs.text2speech import ElevenLabsText2SpeechTool
-from langchain.tools.file_management import (
-    CopyFileTool,
-    DeleteFileTool,
-    FileSearchTool,
-    ListDirectoryTool,
-    MoveFileTool,
-    ReadFileTool,
-    WriteFileTool,
-)
-from langchain.tools.gmail import (
-    GmailCreateDraft,
-    GmailGetMessage,
-    GmailGetThread,
-    GmailSearch,
-    GmailSendMessage,
-)
-from langchain.tools.google_places.tool import GooglePlacesTool
-from langchain.tools.google_search.tool import GoogleSearchResults, GoogleSearchRun
-from langchain.tools.google_serper.tool import GoogleSerperResults, GoogleSerperRun
-from langchain.tools.graphql.tool import BaseGraphQLTool
-from langchain.tools.human.tool import HumanInputRun
-from langchain.tools.ifttt import IFTTTWebhook
-from langchain.tools.interaction.tool import StdInInquireTool
-from langchain.tools.jira.tool import JiraAction
-from langchain.tools.json.tool import JsonGetValueTool, JsonListKeysTool
-from langchain.tools.metaphor_search import MetaphorSearchResults
-from langchain.tools.office365.create_draft_message import O365CreateDraftMessage
-from langchain.tools.office365.events_search import O365SearchEvents
-from langchain.tools.office365.messages_search import O365SearchEmails
-from langchain.tools.office365.send_event import O365SendEvent
-from langchain.tools.office365.send_message import O365SendMessage
-from langchain.tools.office365.utils import authenticate
-from langchain.tools.openapi.utils.api_models import APIOperation
-from langchain.tools.openapi.utils.openapi_utils import OpenAPISpec
-from langchain.tools.openweathermap.tool import OpenWeatherMapQueryRun
-from langchain.tools.playwright import (
-    ClickTool,
-    CurrentWebPageTool,
-    ExtractHyperlinksTool,
-    ExtractTextTool,
-    GetElementsTool,
-    NavigateBackTool,
-    NavigateTool,
-)
-from langchain.tools.plugin import AIPluginTool
-from langchain.tools.powerbi.tool import (
-    InfoPowerBITool,
-    ListPowerBITool,
-    QueryPowerBITool,
-)
-from langchain.tools.pubmed.tool import PubmedQueryRun
-from langchain.tools.python.tool import PythonAstREPLTool, PythonREPLTool
-from langchain.tools.render import format_tool_to_openai_function
-from langchain.tools.requests.tool import (
-    BaseRequestsTool,
-    RequestsDeleteTool,
-    RequestsGetTool,
-    RequestsPatchTool,
-    RequestsPostTool,
-    RequestsPutTool,
-)
-from langchain.tools.scenexplain.tool import SceneXplainTool
-from langchain.tools.searx_search.tool import SearxSearchResults, SearxSearchRun
-from langchain.tools.shell.tool import ShellTool
-from langchain.tools.sleep.tool import SleepTool
-from langchain.tools.spark_sql.tool import (
-    BaseSparkSQLTool,
-    InfoSparkSQLTool,
-    ListSparkSQLTool,
-    QueryCheckerTool,
-    QuerySparkSQLTool,
-)
-from langchain.tools.sql_database.tool import (
-    BaseSQLDatabaseTool,
-    InfoSQLDatabaseTool,
-    ListSQLDatabaseTool,
-    QuerySQLCheckerTool,
-    QuerySQLDataBaseTool,
-)
-from langchain.tools.steamship_image_generation import SteamshipImageGenerationTool
-from langchain.tools.vectorstore.tool import (
-    VectorStoreQATool,
-    VectorStoreQAWithSourcesTool,
-)
-from langchain.tools.wikipedia.tool import WikipediaQueryRun
-from langchain.tools.wolfram_alpha.tool import WolframAlphaQueryRun
-from langchain.tools.youtube.search import YouTubeSearchTool
-from langchain.tools.zapier.tool import ZapierNLAListActions, ZapierNLARunAction
 
 # Used for internal purposes
 _DEPRECATED_TOOLS = {"PythonAstREPLTool", "PythonREPLTool"}
@@ -211,6 +108,18 @@ def _import_brave_search_tool() -> Any:
     from langchain.tools.brave_search.tool import BraveSearch
 
     return BraveSearch
+
+
+def _import_cogniswitch_answer_tool() -> Any:
+    from langchain.tools.cogniswitch.tool import CogniswitchAnswerTool
+
+    return CogniswitchAnswerTool
+
+
+def _import_cogniswitch_store_tool() -> Any:
+    from langchain.tools.cogniswitch.tool import CogniswitchStoreTool
+
+    return CogniswitchStoreTool
 
 
 def _import_ddg_search_tool_DuckDuckGoSearchResults() -> Any:
@@ -998,6 +907,7 @@ def __getattr__(name: str) -> Any:
         return _import_e2b_data_analysis()
     else:
         raise AttributeError(f"Could not find: {name}")
+
 
 __all__ = [
     "AINAppOps",
