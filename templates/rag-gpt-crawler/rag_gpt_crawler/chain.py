a6bb9babae7a532fa851cc690a736c2e6791e903
Lance Martin <lance@langchain.dev>
2023-11-20 14:19:45 -0800

    fmt

--- templates/rag-gpt-crawler/rag_gpt_crawler/chain.py
+++ templates/rag-gpt-crawler/rag_gpt_crawler/chain.py
@@ -14,19 +14,24 @@ from langchain.vectorstores import Chroma
 # Load output from gpt crawler
 path_to_gptcrawler = "/Users/rlm/Desktop/Code/gpt-crawler/output.json"
 data = json.loads(Path(path_to_gptcrawler).read_text())
-docs = [Document(page_content=dict_['html'], 
-                 metadata={'title': dict_['title'],
-                 'url': dict_['url']}) for dict_ in data]
+docs = [
+    Document(
+        page_content=dict_["html"],
+        metadata={"title": dict_["title"], "url": dict_["url"]},
+    )
+    for dict_ in data
+]
 
 # Split
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
 all_splits = text_splitter.split_documents(docs)
 
 # Add to vectorDB
-vectorstore = Chroma.from_documents(documents=all_splits, 
-                                    collection_name="rag-gpt-builder",
-                                    embedding=OpenAIEmbeddings(),
-                                    )
+vectorstore = Chroma.from_documents(
+    documents=all_splits,
+    collection_name="rag-gpt-builder",
+    embedding=OpenAIEmbeddings(),
+)
 retriever = vectorstore.as_retriever()
 
 # RAG prompt
@@ -48,8 +53,10 @@ chain = (
     | StrOutputParser()
 )
 
+
 # Add typing for input
 class Question(BaseModel):
     __root__: str
+
 
 chain = chain.with_types(input_type=Question)
