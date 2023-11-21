cd3a454a25dba5566e0fa26b7faf1d0147471812
Mogith PN <mogith.nirmala@clarifai.com>
2023-11-21 13:24:49 +0530

    resolved conflicts

--- libs/langchain/langchain/vectorstores/clarifai.py
+++ libs/langchain/langchain/vectorstores/clarifai.py
@@ -9,9 +9,9 @@ from typing import Any, Iterable, List, Optional, Tuple
 
 import requests
 from google.protobuf.struct_pb2 import Struct
-
 from langchain_core.schema.embeddings import Embeddings
 from langchain_core.schema.vectorstore import VectorStore
+
 from langchain.docstore.document import Document
 
 logger = logging.getLogger(__name__)
