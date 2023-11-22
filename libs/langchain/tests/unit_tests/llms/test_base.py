49ec1ba6e4752d91b6f5bd7fa0b05cce275e9b5f
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2023-11-22 14:26:44 -0800

    rm

--- libs/langchain/tests/unit_tests/llms/test_base.py
+++ libs/langchain/tests/unit_tests/llms/test_base.py
@@ -7,7 +7,6 @@ except ImportError:
     from sqlalchemy.ext.declarative import declarative_base
 
 from langchain_core.outputs import Generation, LLMResult
-from langchain_core.tracers.context import collect_runs
 
 from langchain.cache import InMemoryCache, SQLAlchemyCache
 from langchain.globals import get_llm_cache, set_llm_cache
@@ -90,71 +89,3 @@ def test_custom_caching() -> None:
         llm_output=None,
     )
     assert output == expected_output
-
-
-def test_batch() -> None:
-    llm = FakeLLM()
-    output = llm.batch(["foo", "bar", "foo"])
-    assert output == ["foo"] * 3
-
-    output = llm.batch(["foo", "bar", "foo"], config={"max_concurrency": 2})
-    assert output == ["foo"] * 3
-
-
-async def test_abatch() -> None:
-    llm = FakeLLM()
-    output = await llm.abatch(["foo", "bar", "foo"])
-    assert output == ["foo"] * 3
-
-    output = await llm.abatch(["foo", "bar", "foo"], config={"max_concurrency": 2})
-    assert output == ["foo"] * 3
-
-
-def test_batch_size() -> None:
-    llm = FakeLLM()
-    with collect_runs() as cb:
-        llm.batch(["foo", "bar", "foo"], {"callbacks": [cb]})
-        assert all([(r.extra or {}).get("batch_size") == 3 for r in cb.traced_runs])
-        assert len(cb.traced_runs) == 3
-    with collect_runs() as cb:
-        llm.batch(["foo"], {"callbacks": [cb]})
-        assert all([(r.extra or {}).get("batch_size") == 1 for r in cb.traced_runs])
-        assert len(cb.traced_runs) == 1
-
-    with collect_runs() as cb:
-        llm.invoke("foo")
-        assert len(cb.traced_runs) == 1
-        assert (cb.traced_runs[0].extra or {}).get("batch_size") == 1
-
-    with collect_runs() as cb:
-        list(llm.stream("foo"))
-        assert len(cb.traced_runs) == 1
-        assert (cb.traced_runs[0].extra or {}).get("batch_size") == 1
-
-    with collect_runs() as cb:
-        llm.predict("foo")
-        assert len(cb.traced_runs) == 1
-        assert (cb.traced_runs[0].extra or {}).get("batch_size") == 1
-
-
-async def test_async_batch_size() -> None:
-    llm = FakeLLM()
-    with collect_runs() as cb:
-        await llm.abatch(["foo", "bar", "foo"], {"callbacks": [cb]})
-        assert all([(r.extra or {}).get("batch_size") == 3 for r in cb.traced_runs])
-        assert len(cb.traced_runs) == 3
-    with collect_runs() as cb:
-        await llm.abatch(["foo"], {"callbacks": [cb]})
-        assert all([(r.extra or {}).get("batch_size") == 1 for r in cb.traced_runs])
-        assert len(cb.traced_runs) == 1
-
-    with collect_runs() as cb:
-        await llm.ainvoke("foo")
-        assert len(cb.traced_runs) == 1
-        assert (cb.traced_runs[0].extra or {}).get("batch_size") == 1
-
-    with collect_runs() as cb:
-        async for _ in llm.astream("foo"):
-            pass
-        assert len(cb.traced_runs) == 1
-        assert (cb.traced_runs[0].extra or {}).get("batch_size") == 1
