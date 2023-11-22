6c59db482b6b447f37289598ba7830486be298ff
Erick Friis <erick@langchain.dev>
2023-11-22 13:38:00 -0500

    unit test for core

---
+++ libs/langchain/tests/unit_tests/load/test_core.py
@@ -0,0 +1,84 @@
+import importlib
+from typing import Any, cast
+
+from langchain_core.load import Serializable
+
+core_modules = [
+    "agents",
+    "caches",
+    "callbacks",
+    "chat_history",
+    "chat_sessions",
+    "document_transformers",
+    "documents",
+    "embeddings",
+    "env",
+    "example_selectors",
+    "exceptions",
+    "globals",
+    "language_models",
+    "load",
+    "memory",
+    "messages",
+    "output_parsers",
+    "outputs",
+    "prompt_values",
+    "prompts",
+    "pydantic_v1",
+    "retrievers",
+    "runnables",
+    "stores",
+    "tools",
+    "tracers",
+    "utils",
+    "vectorstores",
+]
+
+
+def test_core_exported_from_langchain() -> None:
+    # iterate through core modules and get exported names that inherit from serializable
+    # and are not private
+    wrong_module = []
+    does_not_exist = []
+
+    for module_name in core_modules:
+        module = importlib.import_module(f"langchain_core.{module_name}")
+        for name in dir(module):
+            if name.startswith("_"):
+                continue
+            obj: Any = getattr(module, name)
+            if not isinstance(obj, type) or not issubclass(obj, Serializable):
+                continue
+            obj_name = f"langchain_core.{module_name}.{name}"
+            lc_id = cast(obj, Any).lc_id()
+            if not lc_id[0] == "langchain":
+                wrong_module.append(f"{obj_name} -> {lc_id}")
+                continue
+            # see if importable
+            [*id_namespace, id_name] = lc_id
+            import_name = ".".join(id_namespace)
+            try:
+                import_module = importlib.import_module(import_name)
+                import_obj = getattr(import_module, id_name)
+            except (ImportError, AttributeError):
+                does_not_exist.append(f"{obj_name} -> {lc_id}")
+                continue
+
+            # assert same id
+            assert import_obj.lc_id() == lc_id, f"{obj_name} -> {lc_id}"
+            # assert serializable
+            assert issubclass(
+                import_obj, Serializable
+            ), f"Referenced object not serializable: {obj_name} -> {lc_id}"
+    if len(wrong_module) == 0 and len(does_not_exist) == 0:
+        return
+
+    wrong_modules = "\n".join(f"- {m}" for m in wrong_module) or "None! Passed"
+    does_not_exist_message = (
+        "\n".join(f"- {m}" for m in does_not_exist) or "None! Passed"
+    )
+    assert False, f"""LC ID must be from langchain.x:
+{wrong_modules}
+
+The following LC IDs do not exist:
+{does_not_exist_message}"""
