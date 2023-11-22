7e761ebc86b66df8ec6b167643562ccb637f9533
Smit-Parmar <smitraj333@gmail.com>
2023-11-22 22:52:52 +0530

    Resolved conflict

--- libs/langchain/langchain/retrievers/kendra.py
+++ libs/langchain/langchain/retrievers/kendra.py
@@ -19,7 +19,7 @@ from langchain_core.pydantic_v1 import (
     root_validator,
     validator,
 )
-from langchain_core.schema import BaseRetriever
+from langchain_core.retrievers import BaseRetriever
 
 from langchain.callbacks.manager import CallbackManagerForRetrieverRun
 from langchain.docstore.document import Document
