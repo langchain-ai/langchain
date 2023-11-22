32d087fcb89393a0ae587fbfbd0604888f114006
Bagatur <22008038+baskaryan@users.noreply.github.com>
2023-11-22 10:10:26 -0800

    REFACTOR: combine core documents files (#13733)

--- libs/langchain/langchain/document_transformers/google_translate.py
+++ libs/langchain/langchain/document_transformers/google_translate.py
@@ -1,7 +1,6 @@
 from typing import Any, Optional, Sequence
 
-from langchain_core.document_transformers import BaseDocumentTransformer
-from langchain_core.documents import Document
+from langchain_core.documents import BaseDocumentTransformer, Document
 
 from langchain.utilities.vertexai import get_client_info
 
