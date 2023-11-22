2c566bb08106d5bd358c253ec5d6fa8e6ed11658
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2023-11-22 14:29:26 -0800

    snapshot

--- libs/core/tests/unit_tests/language_models/llms/test_base.py
+++ libs/core/tests/unit_tests/language_models/llms/test_base.py
@@ -21,26 +21,30 @@ async def test_abatch() -> None:
 
 
 def test_batch_size() -> None:
-    llm = FakeListLLM()
+    llm = FakeListLLM(responses=["foo"] * 3)
     with collect_runs() as cb:
         llm.batch(["foo", "bar", "foo"], {"callbacks": [cb]})
         assert all([(r.extra or {}).get("batch_size") == 3 for r in cb.traced_runs])
         assert len(cb.traced_runs) == 3
+    llm = FakeListLLM(responses=["foo"])
     with collect_runs() as cb:
         llm.batch(["foo"], {"callbacks": [cb]})
         assert all([(r.extra or {}).get("batch_size") == 1 for r in cb.traced_runs])
         assert len(cb.traced_runs) == 1
 
+    llm = FakeListLLM(responses=["foo"])
     with collect_runs() as cb:
         llm.invoke("foo")
         assert len(cb.traced_runs) == 1
         assert (cb.traced_runs[0].extra or {}).get("batch_size") == 1
 
+    llm = FakeListLLM(responses=["foo"])
     with collect_runs() as cb:
         list(llm.stream("foo"))
         assert len(cb.traced_runs) == 1
         assert (cb.traced_runs[0].extra or {}).get("batch_size") == 1
 
+    llm = FakeListLLM(responses=["foo"] * 1)
     with collect_runs() as cb:
         llm.predict("foo")
         assert len(cb.traced_runs) == 1
@@ -48,21 +52,24 @@ def test_batch_size() -> None:
 
 
 async def test_async_batch_size() -> None:
-    llm = FakeListLLM()
+    llm = FakeListLLM(responses=["foo"] * 3)
     with collect_runs() as cb:
         await llm.abatch(["foo", "bar", "foo"], {"callbacks": [cb]})
         assert all([(r.extra or {}).get("batch_size") == 3 for r in cb.traced_runs])
         assert len(cb.traced_runs) == 3
+    llm = FakeListLLM(responses=["foo"])
     with collect_runs() as cb:
         await llm.abatch(["foo"], {"callbacks": [cb]})
         assert all([(r.extra or {}).get("batch_size") == 1 for r in cb.traced_runs])
         assert len(cb.traced_runs) == 1
 
+    llm = FakeListLLM(responses=["foo"])
     with collect_runs() as cb:
         await llm.ainvoke("foo")
         assert len(cb.traced_runs) == 1
         assert (cb.traced_runs[0].extra or {}).get("batch_size") == 1
 
+    llm = FakeListLLM(responses=["foo"])
     with collect_runs() as cb:
         async for _ in llm.astream("foo"):
             pass
