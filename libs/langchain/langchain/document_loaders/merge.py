7717d1e63128e0601bdc3b5485f97e2d3f1c2ff5
chyroc <chyroc@qq.com>
2023-11-22 22:24:00 +0800

    fix: MergedDataLoader is compatible with load with parameters such as ConfluenceLoader

--- libs/langchain/langchain/document_loaders/merge.py
+++ libs/langchain/langchain/document_loaders/merge.py
@@ -1,3 +1,4 @@
+import inspect
 from typing import Iterator, List
 
 from langchain.docstore.document import Document
@@ -7,21 +8,23 @@ from langchain.document_loaders.base import BaseLoader
 class MergedDataLoader(BaseLoader):
     """Merge documents from a list of loaders"""
 
-    def __init__(self, loaders: List):
+    def __init__(self, loaders: List, **kwargs):
         """Initialize with a list of loaders"""
         self.loaders = loaders
 
-    def lazy_load(self) -> Iterator[Document]:
+    def lazy_load(self, **kwargs) -> Iterator[Document]:
         """Lazy load docs from each individual loader."""
         for loader in self.loaders:
+            param_keys = inspect.signature(loader.load).parameters.keys()
+            param = {k: v for k, v in kwargs.items() if k in param_keys}
             # Check if lazy_load is implemented
             try:
-                data = loader.lazy_load()
+                data = loader.lazy_load(**param)
             except NotImplementedError:
-                data = loader.load()
+                data = loader.load(**param)
             for document in data:
                 yield document
 
-    def load(self) -> List[Document]:
+    def load(self, **kwargs) -> List[Document]:
         """Load docs."""
-        return list(self.lazy_load())
+        return list(self.lazy_load(**kwargs))
