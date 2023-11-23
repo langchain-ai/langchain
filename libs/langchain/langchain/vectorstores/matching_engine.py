1caf76cac4951caf8591c82a067427d336b80b3a
saiwas <johnnysaiwas@gmail.com>
2023-11-23 11:41:52 +0800

    small fix matching engine AttributeError - object has no attribute

--- libs/langchain/langchain/vectorstores/matching_engine.py
+++ libs/langchain/langchain/vectorstores/matching_engine.py
@@ -232,7 +232,7 @@ class MatchingEngine(VectorStore):
         filter = filter or []
 
         # If the endpoint is public we use the find_neighbors function.
-        if self.endpoint._public_match_client:
+        if hasattr(self.endpoint, "_public_match_client"):
             response = self.endpoint.find_neighbors(
                 deployed_index_id=self._get_index_id(),
                 queries=[embedding],
