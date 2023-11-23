e8944fb5a2aab387cc00e353e438a3ef24388b58
What-Is-A-Username <49571870+What-Is-A-Username@users.noreply.github.com>
2023-11-23 15:21:57 -0500

    Fix descriptions of tool - tool now accepts limit on number of results - tool now has complete descriptions for input parameters

--- libs/langchain/langchain/tools/reddit_search/tool.py
+++ libs/langchain/langchain/tools/reddit_search/tool.py
@@ -1,5 +1,5 @@
 
-"""Tool for the Google search API."""
+"""Tool for the Reddit search API."""
 
 from typing import Optional, Type
 
@@ -9,21 +9,21 @@ from langchain.pydantic_v1 import Field, BaseModel
 from langchain.utilities.reddit_search import RedditSearchAPIWrapper
 
 class RedditSearchSchema(BaseModel):
-    query: str = Field(description='should be query string that post title should contain')
+    query: str = Field(description="should be query string that post title should contain, or '*' if anything is allowed.")
     sort: str = Field(description='should be sort method, which is one of: "relevance", "hot", "top", "new", or "comments".')
     time_filter: str = Field(description='should be time period to filter by, which is one of "all", "day", "hour", "month", "week", or "year"')
     subreddit: str = Field(description='should be name of subreddit, like "all" for r/all')
-
+    limit: str = Field(description='a positive integer indicating the maximum number of results to return')
+    
 class RedditSearchRun(BaseTool):
-    """Tool that queries the Google search API."""
+    """Tool that queries for posts on a subreddit."""
 
-    name: str = "google_search"
+    name: str = "reddit_search"
     description: str = (
-        "A tool for searching Reddit. "
-        "Useful for when you need to search for a post in a subreddit. "
-        "Input should be search query, sort method, time filter, and subreddit name."
+        "A tool that searches for posts on Reddit.",
+        "Useful when you need to know post information on a subreddit, like post titles, text, score, and authors.",
     )
-    api_wrapper: RedditSearchAPIWrapper
+    api_wrapper: RedditSearchAPIWrapper = Field(default_factory=RedditSearchAPIWrapper)
     args_schema: Type[BaseModel] = RedditSearchSchema
 
     def _run(self,
@@ -31,13 +31,15 @@ class RedditSearchRun(BaseTool):
         sort: str, 
         time_filter: str,
         subreddit: str,
+        limit: str,
         run_manager: Optional[CallbackManagerForToolRun] = None,
     ) -> str:
         """Use the tool."""
         return self.api_wrapper.run(
-            query=query, 
-            sort=sort, 
-            time_filter=time_filter, 
-            subreddit=subreddit
+            query=query,
+            sort=sort,
+            time_filter=time_filter,
+            subreddit=subreddit,
+            limit=int(limit)
         )
 
