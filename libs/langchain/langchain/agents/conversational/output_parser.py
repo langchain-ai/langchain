26ed72b3f65d1eda1d7956f21546a638d9706bbc
lilong669 <53218099+lilong669@users.noreply.github.com>
2023-11-23 11:48:38 +0800

    Update output_parser.py
    The regular expression  regex = r"Action: (.*?)[\n]*Action Input: (.*)", with .* being the default greedy match.This results in the action_input variable not including the part after the newline character.

--- libs/langchain/langchain/agents/conversational/output_parser.py
+++ libs/langchain/langchain/agents/conversational/output_parser.py
@@ -22,7 +22,7 @@ class ConvoOutputParser(AgentOutputParser):
             return AgentFinish(
                 {"output": text.split(f"{self.ai_prefix}:")[-1].strip()}, text
             )
-        regex = r"Action: (.*?)[\n]*Action Input: (.*)"
+        regex = r"Action: (.*?)[\n]*Action Input: ([\s\S]*)"
         match = re.search(regex, text)
         if not match:
             raise OutputParserException(f"Could not parse LLM output: `{text}`")
