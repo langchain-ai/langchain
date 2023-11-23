1aa89d7788bb7915e07936d0017a3c48ba79975b
Крестников Константин Николаевич <KNKrestnikov@sberbank.ru>
2023-11-23 16:45:24 +0300

    Sync with upstream fix

--- libs/langchain/tests/unit_tests/tools/test_exported.py
+++ libs/langchain/tests/unit_tests/tools/test_exported.py
@@ -21,12 +21,11 @@ def _get_tool_classes(skip_tools_without_default_names: bool) -> List[Type[BaseT
         if isinstance(tool_class, type) and issubclass(tool_class, BaseTool):
             if tool_class in _EXCLUDE:
                 continue
-            if skip_tools_without_default_names and tool_class.__fields__[
-                "name"
-            ].default in [  # type: ignore
-                None,
-                "",
-            ]:
+            if (
+                skip_tools_without_default_names
+                and tool_class.__fields__["name"].default  # type: ignore
+                in [None, ""]
+            ):
                 continue
             results.append(tool_class)
     return results
