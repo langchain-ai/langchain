9e11b8e2079f92ff781935bb6c6eb99a87c504fe
raelix <raelix@hotmail.it>
2023-11-25 03:53:55 +0100

    IMPROVEMENT: Added title metadata to GoogleDriveLoader for optional File Loaders (#13832)
    - **Description:** Simple change, I just added title metadata to
    GoogleDriveLoader for optional File Loaders
      - **Dependencies:** no dependencies
      - **Tag maintainer:** @hwchase17
    
    ---------
    
    Co-authored-by: Bagatur <baskaryan@gmail.com>

--- libs/langchain/langchain/document_loaders/googledrive.py
+++ libs/langchain/langchain/document_loaders/googledrive.py
@@ -317,6 +317,8 @@ class GoogleDriveLoader(BaseLoader, BaseModel):
             docs = loader.load()
             for doc in docs:
                 doc.metadata["source"] = f"https://drive.google.com/file/d/{id}/view"
+                if "title" not in doc.metadata:
+                    doc.metadata["title"] = f"{file.get('name')}"
             return docs
 
         else:
