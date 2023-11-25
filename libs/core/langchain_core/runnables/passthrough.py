5d9cfe29bf2920691c25b1c11b1a5b2acba609c7
hyungjoo.song <hyungjoo.song@gmail.com>
2023-11-25 18:46:21 +0900

    DOCS: fixed error in the docstring of RunnablePassthrough class

--- libs/core/langchain_core/runnables/passthrough.py
+++ libs/core/langchain_core/runnables/passthrough.py
@@ -63,6 +63,7 @@ class RunnablePassthrough(RunnableSerializable[Other, Other]):
         .. code-block:: python
 
             from langchain_core.runnables import RunnablePassthrough, RunnableParallel
+            from langchain_core.runnables import RunnableLambda
 
             runnable = RunnableParallel(
                 origin=RunnablePassthrough(),
@@ -72,7 +73,7 @@ class RunnablePassthrough(RunnableSerializable[Other, Other]):
             runnable.invoke(1) # {'origin': 1, 'modified': 2}
 
 
-             def fake_llm(prompt: str) -> str: # Fake LLM for the example
+            def fake_llm(prompt: str) -> str: # Fake LLM for the example
                 return "completion"
 
             chain = RunnableLambda(fake_llm) | {
@@ -89,7 +90,7 @@ class RunnablePassthrough(RunnableSerializable[Other, Other]):
 
             from langchain_core.runnables import RunnablePassthrough, RunnableParallel
 
-             def fake_llm(prompt: str) -> str: # Fake LLM for the example
+            def fake_llm(prompt: str) -> str: # Fake LLM for the example
                 return "completion"
 
             runnable = {
