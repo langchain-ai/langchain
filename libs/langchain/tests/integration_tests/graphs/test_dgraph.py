e1603cba536d90b7a7af29d37fb2d1e33ba6fb22
Porom <porom.kamal@gmail.com>
2023-11-27 17:56:25 -0500

    [LC-11533] Added DGraph graph operation wrapper integration tests

---
+++ libs/langchain/tests/integration_tests/graphs/test_dgraph.py
@@ -0,0 +1,49 @@
+
+import unittest
+from typing import Any
+from unittest.mock import MagicMock, patch
+from langchain.graphs import DGraph
+
+class TestDGraphDB(unittest.TestCase):
+  def setUp(self) -> None:
+    self.host = "localhost:9080"
+  
+  @patch("pydgraph.DgraphClientStub")
+  def test_init(self, mock_client: Any) -> None:
+    mock_client.return_value = MagicMock()
+    dgraph = DGraph(clientUrl=self.host)
+    self.assertEqual(dgraph.clientUrl, self.host)
+  
+  @patch("pydgraph.DgraphClientStub")
+  def test_execute(self, mock_client: Any) -> None:
+    mock_client.return_value = MagicMock()
+    dgraph = DGraph(clientUrl=self.host)
+    query = "{ me(func: has(name)) { name }}}"
+    result = dgraph.query(query)
+    self.assertIsInstance(result, MagicMock)
+  
+  @patch("pydgraph.DgraphClientStub")
+  def test_get_schema(self, mock_client: Any) -> None:
+    mock_client.return_value = MagicMock()
+    dgraph = DGraph(clientUrl=self.host)
+    schema = dgraph.get_schema()
+    self.assertIsInstance(schema, dict)
+  
+  @patch("pydgraph.DgraphClientStub")
+  def test_add_schema(self, mock_client: Any) -> None:
+    mock_client.return_value = MagicMock()
+    dgraph = DGraph(clientUrl=self.host)
+    schema = "name: string @index(exact) ."
+    dgraph.add_schema(schema)
+    self.assertEqual(dgraph.get_schema(), 
+                     {"name": [{"predicate": "name", "type": "string", "index": "exact"}]})
+  
+  @patch("pydgraph.DgraphClientStub")
+  def test_add_node(self, mock_client: Any) -> None:
+    mock_client.return_value = MagicMock()
+    dgraph = DGraph(clientUrl=self.host)
+    data = {"name": "Alice"}
+    dgraph.add_node(data)
+    query = "{ me(func: has(name)) { name }}}"
+    result = dgraph.query(query)
+    self.assertEqual(result[0], {"me": [{"name": "Alice"}]})
