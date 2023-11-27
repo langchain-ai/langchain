2a4522a0c64a84c3bcd3e63d55b6fd974aeee503
Shroominic <db2002dominic@gmail.com>
2023-11-27 23:19:53 +0100

    use create_chat_completion

--- libs/langchain/langchain/chat_models/llamacpp.py
+++ libs/langchain/langchain/chat_models/llamacpp.py
@@ -7,15 +7,9 @@ from langchain.callbacks.manager import CallbackManagerForLLMRun
 from langchain.chat_models.base import BaseChatModel
 from langchain.llms.llamacpp import _LlamaCppCommon
 from langchain.schema import ChatResult
-from langchain.schema.messages import (
-    AIMessage,
-    AIMessageChunk,
-    BaseMessage,
-    ChatMessage,
-    HumanMessage,
-    SystemMessage,
-)
+from langchain.schema.messages import AIMessage, AIMessageChunk, BaseMessage
 from langchain.schema.output import ChatGeneration, ChatGenerationChunk, GenerationChunk
+from langchain_core.schema.messages import messages_to_dict
 
 logger = logging.getLogger(__name__)
 
@@ -38,24 +32,6 @@ class ChatLlamaCpp(BaseChatModel, _LlamaCppCommon):
     def _llm_type(self) -> str:
         """Return type of chat model."""
         return "llamacpp-chat"
-
-    def _format_message_as_text(self, message: BaseMessage) -> str:
-        if isinstance(message, ChatMessage):
-            message_text = f"\n\n{message.role.capitalize()}: {message.content}"
-        elif isinstance(message, HumanMessage):
-            message_text = f"[INST] {message.content} [/INST]"
-        elif isinstance(message, AIMessage):
-            message_text = f"{message.content}"
-        elif isinstance(message, SystemMessage):
-            message_text = f"<<SYS>> {message.content} <</SYS>>"
-        else:
-            raise ValueError(f"Got unknown type {message}")
-        return message_text
-
-    def _format_messages_as_text(self, messages: List[BaseMessage]) -> str:
-        return "\n".join(
-            [self._format_message_as_text(message) for message in messages]
-        )
 
     def _stream_with_aggregation(
         self,
@@ -121,8 +97,9 @@ class ChatLlamaCpp(BaseChatModel, _LlamaCppCommon):
         **kwargs: Any,
     ) -> Iterator[ChatGenerationChunk]:
         params = {**self._get_parameters(stop), **kwargs}
-        prompt = self._format_messages_as_text(messages)
-        result = self.client(prompt=prompt, stream=True, **params)
+        result = self.client.create_chat_completion(
+            messages=messages_to_dict(messages), stream=True, **params
+        )
         for part in result:
             logprobs = part["choices"][0].get("logprobs", None)
             chunk = ChatGenerationChunk(
