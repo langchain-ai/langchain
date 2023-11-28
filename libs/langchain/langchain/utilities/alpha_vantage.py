50db214f354969d5405deca0e253bdfab6822ab1
anshaneel <68671970+anshaneel@users.noreply.github.com>
2023-11-28 03:55:19 -0500

    feat: immplemented function to get news and sentiment for an asset using the alpha vantage NEWS_SENTIMENT function in api

--- libs/langchain/langchain/utilities/alpha_vantage.py
+++ libs/langchain/langchain/utilities/alpha_vantage.py
@@ -49,6 +49,25 @@ class AlphaVantageAPIWrapper(BaseModel):
 
         return data
 
+    def _get_market_news_sentiment(self, symbol: str) -> Dict[str, Any]:
+        """Make a request to the AlphaVantage API to get market news sentiment for a given symbol."""
+        response = requests.get(
+            "https://www.alphavantage.co/query/",
+            params={
+                "function": "NEWS_SENTIMENT",
+                "symbol": symbol,
+                "apikey": self.alphavantage_api_key,
+            },
+        )
+        response.raise_for_status()
+        data = response.json()
+
+        if "Error Message" in data:
+            raise ValueError(f"API Error: {data['Error Message']}")
+
+        return data
+
+
     def _get_exchange_rate(
         self, from_currency: str, to_currency: str
     ) -> Dict[str, Any]:
