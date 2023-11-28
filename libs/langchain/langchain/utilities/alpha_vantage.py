5a3a2a0dc3f494b4612b8072d5021eda196479ba
anshaneel <68671970+anshaneel@users.noreply.github.com>
2023-11-28 04:04:27 -0500

    feat: added function to fetch Top Gainers, Losers, and Most Active Tickers in US market using Alpha Vantage API

--- libs/langchain/langchain/utilities/alpha_vantage.py
+++ libs/langchain/langchain/utilities/alpha_vantage.py
@@ -126,6 +126,23 @@ class AlphaVantageAPIWrapper(BaseModel):
 
         return data
 
+    def _get_top_gainers_losers(self) -> Dict[str, Any]:
+        """Make a request to the AlphaVantage API to get the top gainers, losers, and most actively traded tickers in the US market."""
+        response = requests.get(
+            "https://www.alphavantage.co/query/",
+            params={
+                "function": "TOP_GAINERS_LOSERS",
+                "apikey": self.alphavantage_api_key,
+            },
+        )
+        response.raise_for_status()
+        data = response.json()
+
+        if "Error Message" in data:
+            raise ValueError(f"API Error: {data['Error Message']}")
+
+        return data
+
     def _get_exchange_rate(
         self, from_currency: str, to_currency: str
     ) -> Dict[str, Any]:
