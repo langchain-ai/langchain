00a2f7a08ecaf0a006aa25b069a415b1e3f99340
anshaneel <68671970+anshaneel@users.noreply.github.com>
2023-11-28 02:16:36 -0500

    feat: added function to make request to search endpoint in Alphavantage api

--- libs/langchain/langchain/utilities/alpha_vantage.py
+++ libs/langchain/langchain/utilities/alpha_vantage.py
@@ -31,6 +31,24 @@ class AlphaVantageAPIWrapper(BaseModel):
         )
         return values
 
+    def search_symbols(self, keywords: str) -> Dict[str, Any]:
+        """Make a request to the AlphaVantage API to search for symbols."""
+        response = requests.get(
+            "https://www.alphavantage.co/query/",
+            params={
+                "function": "SYMBOL_SEARCH",
+                "keywords": keywords,
+                "apikey": self.alphavantage_api_key,
+            },
+        )
+        response.raise_for_status()
+        data = response.json()
+
+        if "Error Message" in data:
+            raise ValueError(f"API Error: {data['Error Message']}")
+
+        return data
+
     def _get_exchange_rate(
         self, from_currency: str, to_currency: str
     ) -> Dict[str, Any]:
