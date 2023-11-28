ff0dd2245c16e7370133dc01951b18633fe949fe
Adam Badar <94140103+adam-badar@users.noreply.github.com>
2023-11-28 18:49:33 -0500

    feat: :sparkles: added time_series_weekly method
    used an api call to alphavantage

--- libs/langchain/langchain/utilities/alpha_vantage.py
+++ libs/langchain/langchain/utilities/alpha_vantage.py
@@ -106,6 +106,26 @@ class AlphaVantageAPIWrapper(BaseModel):
 
         return data
 
+    def _get_time_series_weekly(
+        self, symbol: str
+    ) -> Dict[str, Any]:
+        """Make a request to the AlphaVantage API to get the Weekly Time Series."""
+        response = requests.get(
+            "https://www.alphavantage.co/query/",
+            params={
+                "function": "TIME_SERIES_WEEKLY",
+                "symbol": symbol,
+                "apikey": self.alphavantage_api_key,
+            },
+        )
+        response.raise_for_status()
+        data = response.json()
+
+        if "Error Message" in data:
+            raise ValueError(f"API Error: {data['Error Message']}")
+
+        return data
+
     def _get_exchange_rate(
         self, from_currency: str, to_currency: str
     ) -> Dict[str, Any]:
