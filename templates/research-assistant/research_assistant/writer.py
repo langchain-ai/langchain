d4b4307adbe35e80f12665d5435ddbf65e0df79d
Pannanja <72413871+Pannanja@users.noreply.github.com>
2023-11-28 16:28:21 -0500

    Update writer.py
    fixed typos

--- templates/research-assistant/research_assistant/writer.py
+++ templates/research-assistant/research_assistant/writer.py
@@ -16,10 +16,10 @@ Using the above information, answer the following question or topic: "{question}
 The report should focus on the answer to the question, should be well structured, informative, \
 in depth, with facts and numbers if available and a minimum of 1,200 words.
 
-You should strive to write the report as long as you can using all relevant and necessary information provided.
+You should strive to make the report as long as you can using all relevant and necessary information provided.
 You must write the report with markdown syntax.
-You MUST determine your own concrete and valid opinion based on the given information. Do NOT deter to general and meaningless conclusions.
-Write all used source urls at the end of the report, and make sure to not add duplicated sources, but only one reference for each.
+You MUST determine your own concrete and valid opinion based on the given information. Do NOT defer to general and meaningless conclusions.
+Write all used sources at the end of the report, and make sure to not add duplicated sources, but only one reference for each.
 You must write the report in apa format.
 Please do your best, this is very important to my career."""  # noqa: E501
 
