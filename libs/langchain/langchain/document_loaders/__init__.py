aca2ac729f5d5dba1aff66e34f8e3060f5f6e1c8
Nithish Raghunandanan <nithish.raghunandanan@couchbase.com>
2023-11-28 19:13:39 +0100

    Add the document loader for Couchbase

--- libs/langchain/langchain/document_loaders/__init__.py
+++ libs/langchain/langchain/document_loaders/__init__.py
@@ -59,6 +59,7 @@ from langchain.document_loaders.college_confidential import CollegeConfidentialL
 from langchain.document_loaders.concurrent import ConcurrentLoader
 from langchain.document_loaders.confluence import ConfluenceLoader
 from langchain.document_loaders.conllu import CoNLLULoader
+from langchain.document_loaders.couchbase import CouchbaseLoader
 from langchain.document_loaders.csv_loader import CSVLoader, UnstructuredCSVLoader
 from langchain.document_loaders.cube_semantic import CubeSemanticLoader
 from langchain.document_loaders.datadog_logs import DatadogLogsLoader
@@ -244,6 +245,7 @@ __all__ = [
     "CollegeConfidentialLoader",
     "ConcurrentLoader",
     "ConfluenceLoader",
+    "CouchbaseLoader",
     "CubeSemanticLoader",
     "DataFrameLoader",
     "DatadogLogsLoader",
