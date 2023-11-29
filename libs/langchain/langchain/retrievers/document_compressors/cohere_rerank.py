897718c700c168414dd57b1e1896ecd379adf481
yoch <yoch.melka@gmail.com>
2023-11-29 17:53:51 +0200

    fix #12899

--- libs/langchain/langchain/retrievers/document_compressors/cohere_rerank.py
+++ libs/langchain/langchain/retrievers/document_compressors/cohere_rerank.py
@@ -40,7 +40,7 @@ class CohereRerank(BaseDocumentCompressor):
         extra = Extra.forbid
         arbitrary_types_allowed = True
 
-    @root_validator(pre=True)
+    @root_validator(pre=False)
     def validate_environment(cls, values: Dict) -> Dict:
         """Validate that api key and python package exists in environment."""
         try:
