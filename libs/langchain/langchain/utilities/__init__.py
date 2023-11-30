c2e3963da4b7c6650fc37acfa8ea39a355e7dae9
Josef Zoller <josef@walterzollerpiano.com>
2023-11-29 20:28:29 -0500

    Merriam-Webster Dictionary Tool (#12044)
    # Description
    
    We implemented a simple tool for accessing the Merriam-Webster
    Collegiate Dictionary API
    (https://dictionaryapi.com/products/api-collegiate-dictionary).
    
    Here's a simple usage example:
    
    ```py
    from langchain.llms import OpenAI
    from langchain.agents import load_tools, initialize_agent, AgentType
    
    llm = OpenAI()
    tools = load_tools(["serpapi", "merriam-webster"], llm=llm) # Serp API gives our agent access to Google
    agent = initialize_agent(
      tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
    )
    agent.run("What is the english word for the german word Himbeere? Define that word.")
    ```
    
    Sample output:
    
    ```
    > Entering new AgentExecutor chain...
     I need to find the english word for Himbeere and then get the definition of that word.
    Action: Search
    Action Input: "English word for Himbeere"
    Observation: {'type': 'translation_result'}
    Thought: Now I have the english word, I can look up the definition.
    Action: MerriamWebster
    Action Input: raspberry
    Observation: Definitions of 'raspberry':
    
    1. rasp-ber-ry, noun: any of various usually black or red edible berries that are aggregate fruits consisting of numerous small drupes on a fleshy receptacle and that are usually rounder and smaller than the closely related blackberries
    2. rasp-ber-ry, noun: a perennial plant (genus Rubus) of the rose family that bears raspberries
    3. rasp-ber-ry, noun: a sound of contempt made by protruding the tongue between the lips and expelling air forcibly to produce a vibration; broadly : an expression of disapproval or contempt
    4. black raspberry, noun: a raspberry (Rubus occidentalis) of eastern North America that has a purplish-black fruit and is the source of several cultivated varieties —called also blackcap
    
    Thought: I now know the final answer.
    Final Answer: Raspberry is an english word for Himbeere and it is defined as any of various usually black or red edible berries that are aggregate fruits consisting of numerous small drupes on a fleshy receptacle and that are usually rounder and smaller than the closely related blackberries.
    
    > Finished chain.
    ```
    
    # Issue
    
    This closes #12039.
    
    # Dependencies
    
    We added no extra dependencies.
    
    <!-- Thank you for contributing to LangChain!
    
    Replace this entire comment with:
      - **Description:** a description of the change, 
      - **Issue:** the issue # it fixes (if applicable),
      - **Dependencies:** any dependencies required for this change,
    - **Tag maintainer:** for a quicker response, tag the relevant
    maintainer (see below),
    - **Twitter handle:** we announce bigger features on Twitter. If your PR
    gets announced, and you'd like a mention, we'll gladly shout you out!
    
    Please make sure your PR is passing linting and testing before
    submitting. Run `make format`, `make lint` and `make test` to check this
    locally.
    
    See contribution guidelines for more information on how to write/run
    tests, lint, etc:
    
    https://github.com/langchain-ai/langchain/blob/master/.github/CONTRIBUTING.md
    
    If you're adding a new integration, please include:
    1. a test for the integration, preferably unit tests that do not rely on
    network access,
    2. an example notebook showing its use. It lives in `docs/extras`
    directory.
    
    If no one reviews your PR within a few days, please @-mention one of
    @baskaryan, @eyurtsev, @hwchase17.
     -->
    
    ---------
    
    Co-authored-by: Lara <63805048+larkgz@users.noreply.github.com>
    Co-authored-by: Harrison Chase <hw.chase.17@gmail.com>

--- libs/langchain/langchain/utilities/__init__.py
+++ libs/langchain/langchain/utilities/__init__.py
@@ -134,6 +134,12 @@ def _import_max_compute() -> Any:
     return MaxComputeAPIWrapper
 
 
+def _import_merriam_webster() -> Any:
+    from langchain.utilities.merriam_webster import MerriamWebsterAPIWrapper
+
+    return MerriamWebsterAPIWrapper
+
+
 def _import_metaphor_search() -> Any:
     from langchain.utilities.metaphor_search import MetaphorSearchAPIWrapper
 
@@ -291,6 +297,8 @@ def __getattr__(name: str) -> Any:
         return _import_jira()
     elif name == "MaxComputeAPIWrapper":
         return _import_max_compute()
+    elif name == "MerriamWebsterAPIWrapper":
+        return _import_merriam_webster()
     elif name == "MetaphorSearchAPIWrapper":
         return _import_metaphor_search()
     elif name == "OpenWeatherMapAPIWrapper":
@@ -355,6 +363,7 @@ __all__ = [
     "JiraAPIWrapper",
     "LambdaWrapper",
     "MaxComputeAPIWrapper",
+    "MerriamWebsterAPIWrapper",
     "MetaphorSearchAPIWrapper",
     "OpenWeatherMapAPIWrapper",
     "OutlineAPIWrapper",
