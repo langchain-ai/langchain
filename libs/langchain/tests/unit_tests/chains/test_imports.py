5bc03869d66d2dbf27fca5611d4f8aeb68a60a5c
LunarECL <jkjk98765@naver.com>
2023-11-29 21:26:56 -0500

    fix: changed import

--- libs/langchain/tests/unit_tests/chains/test_imports.py
+++ libs/langchain/tests/unit_tests/chains/test_imports.py
@@ -46,7 +46,6 @@ EXPECTED_ALL = [
     "TransformChain",
     "VectorDBQA",
     "VectorDBQAWithSourcesChain",
-    "VideoCaptioningChain",
     "create_citation_fuzzy_match_chain",
     "create_extraction_chain",
     "create_extraction_chain_pydantic",
