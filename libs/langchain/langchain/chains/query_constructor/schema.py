42787ff4b2b70c91b9929bb537dffe6f45ea4f13
Fangrui Liu <fangruil@moqi.ai>
2023-11-30 13:18:45 +0800

    fix lint

--- libs/langchain/langchain/chains/query_constructor/schema.py
+++ libs/langchain/langchain/chains/query_constructor/schema.py
@@ -1,6 +1,6 @@
 from typing import Callable, Optional, Union
-from langchain_core.pydantic_v1 import BaseModel
 
+from langchain_core.pydantic_v1 import BaseModel
 
 
 class VirtualColumnName(BaseModel):
