68b8da6190c009cbaa5c387ef40a4b4e1e642e20
Erick Friis <erick@langchain.dev>
2023-11-30 16:39:40 -0800

    local docs build <5s

--- docs/.local_build.sh
+++ docs/.local_build.sh
@@ -9,10 +9,10 @@ SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
 cd "${SCRIPT_DIR}"
 
 mkdir -p ../_dist
-cp -r . ../_dist
+rsync -ruv . ../_dist
 cd ../_dist
 poetry run python scripts/model_feat_table.py
-poetry run nbdoc_build --srcdir docs
+poetry run nbdoc_build --srcdir docs --pause 0
 cp ../cookbook/README.md src/pages/cookbook.mdx
 cp ../.github/CONTRIBUTING.md docs/contributing.md
 wget https://raw.githubusercontent.com/langchain-ai/langserve/main/README.md -O docs/langserve.md
