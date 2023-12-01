75056f2efb9393f77ab58a9a8e95165c82644348
Lance Martin <lance@langchain.dev>
2023-11-30 16:12:32 -0800

    Expose query for OSS LLM

--- templates/rag-ollama-multi-query/rag_ollama_multi_query/chain.py
+++ templates/rag-ollama-multi-query/rag_ollama_multi_query/chain.py
@@ -1,9 +1,13 @@
 
+from typing import List
+
+from langchain.chains import LLMChain
 from langchain.chat_models import ChatOllama, ChatOpenAI
 from langchain.document_loaders import WebBaseLoader
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.prompts import ChatPromptTemplate
-from langchain.pydantic_v1 import BaseModel
+from langchain.output_parsers import PydanticOutputParser
+from langchain.prompts import ChatPromptTemplate, PromptTemplate
+from langchain.pydantic_v1 import BaseModel, Field
 from langchain.retrievers.multi_query import MultiQueryRetriever
 from langchain.schema.output_parser import StrOutputParser
 from langchain.schema.runnable import RunnableParallel, RunnablePassthrough
@@ -25,12 +29,42 @@ vectorstore = Chroma.from_documents(
     embedding=OpenAIEmbeddings(),
 )
 
+# Output parser will split the LLM result into a list of queries
+class LineList(BaseModel):
+    # "lines" is the key (attribute name) of the parsed output
+    lines: List[str] = Field(description="Lines of text")
+
+class LineListOutputParser(PydanticOutputParser):
+    def __init__(self) -> None:
+        super().__init__(pydantic_object=LineList)
+
+    def parse(self, text: str) -> LineList:
+        lines = text.strip().split("\n")
+        return LineList(lines=lines)
+
+output_parser = LineListOutputParser()
+
+QUERY_PROMPT = PromptTemplate(
+    input_variables=["question"],
+    template="""You are an AI language model assistant. Your task is to generate five
+    different versions of the given user question to retrieve relevant documents from
+    a vector database. By generating multiple perspectives on the user question, your
+    goal is to help the user overcome some of the limitations of the distance-based
+    similarity search. Provide these alternative questions separated by newlines.
+    Original question: {question}""",
+)
+
 # Add the LLM downloaded from Ollama
 ollama_llm = "zephyr"
-model = ChatOllama(model=ollama_llm)
-retriever = MultiQueryRetriever.from_llm(
-    retriever=vectorstore.as_retriever(), llm=model
-)
+llm = ChatOllama(model=ollama_llm)
+
+# Chain
+llm_chain = LLMChain(llm=llm, prompt=QUERY_PROMPT, output_parser=output_parser)
+
+# Run
+retriever = MultiQueryRetriever(
+    retriever=vectorstore.as_retriever(), llm_chain=llm_chain, parser_key="lines"
+)  # "lines" is the key (attribute name) of the parsed output
 
 # RAG prompt
 template = """Answer the question based only on the following context:
@@ -48,10 +82,8 @@ chain = (
     | StrOutputParser()
 )
 
-
 # Add typing for input
 class Question(BaseModel):
     __root__: str
-
 
 chain = chain.with_types(input_type=Question)
