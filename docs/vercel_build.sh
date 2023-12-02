b449e553087157336e967217d0c40e1d55886cb4
Erick Friis <erick@langchain.dev>
2023-12-01 16:39:38 -0800

    py3.8

--- docs/vercel_build.sh
+++ docs/vercel_build.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-### See: https://github.com/urllib3/urllib3/issues/2168
-# Requests lib breaks for old SSL versions,
-# which are defaults on Amazon Linux 2 (which Vercel uses for builds)
 yum -y update
-yum remove openssl-devel -y
 yum install gcc bzip2-devel libffi-devel zlib-devel wget tar gzip -y
 amazon-linux-extras install python3.8 -y
-yum install openssl11 -y
-yum install openssl11-devel -y
 
 # install quarto
 wget -q https://github.com/quarto-dev/quarto-cli/releases/download/v1.3.450/quarto-1.3.450-linux-amd64.tar.gz
