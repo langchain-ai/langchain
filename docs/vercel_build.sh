781b5b19337714e0f394c0a470cdcb0eb70c06ab
Erick Friis <erick@langchain.dev>
2023-12-01 16:27:33 -0800

    quieter

--- docs/vercel_build.sh
+++ docs/vercel_build.sh
@@ -12,7 +12,7 @@ yum install openssl11-devel -y
 
 # install quarto
 wget -q https://github.com/quarto-dev/quarto-cli/releases/download/v1.3.450/quarto-1.3.450-linux-amd64.tar.gz
-tar -xvzf quarto-1.3.450-linux-amd64.tar.gz
+tar -xzf quarto-1.3.450-linux-amd64.tar.gz
 export PATH=$PATH:$(pwd)/quarto-1.3.450/bin/
 
 
