b951119c1a68d970079c75fb49d5c710d0cd9a7a
Bagatur <baskaryan@gmail.com>
2023-12-05 12:16:40 -0800

    cr

--- .github/scripts/check_diff.py
+++ .github/scripts/check_diff.py
@@ -5,8 +5,6 @@ ALL_DIRS = {
     "libs/core",
     "libs/langchain",
     "libs/experimental",
-    # "libs/community",
-    # "libs/partners/openai",
 }
 
 if __name__ == "__main__":
@@ -14,18 +12,34 @@ if __name__ == "__main__":
     dirs_to_run = set()
 
     for file in files:
-        if any(dir_ in file for dir_ in (".github/workflows", ".github/tools", ".github/actions", "libs/core", ".github/scripts/check_diff.py")):
+        if any(
+            file.startswith(dir_)
+            for dir_ in (
+                ".github/workflows",
+                ".github/tools",
+                ".github/actions",
+                "libs/core",
+                ".github/scripts/check_diff.py",
+            )
+        ):
             dirs_to_run = ALL_DIRS
             break
         elif "libs/community" in file:
-            dirs_to_run.update(("libs/community", "libs/langchain", "libs/experimental"))
+            dirs_to_run.update(
+                ("libs/community", "libs/langchain", "libs/experimental")
+            )
         elif "libs/partners" in file:
             partner_dir = file.split("/")[2]
-            dirs_to_run.update((f"libs/partners/{partner_dir}", "libs/langchain", "libs/experimental"))
+            dirs_to_run.update(
+                (f"libs/partners/{partner_dir}", "libs/langchain", "libs/experimental")
+            )
         elif "libs/langchain" in file:
             dirs_to_run.update(("libs/langchain", "libs/experimental"))
         elif "libs/experimental" in file:
             dirs_to_run.add("libs/experimental")
+        elif file.startswith("libs/"):
+            dirs_to_run = ALL_DIRS
+            break
         else:
             pass
     print(json.dumps(list(dirs_to_run)))
