2ae61b5608874ed1410a5b69125adf93a36e5373
Bagatur <baskaryan@gmail.com>
2023-12-05 11:10:52 -0800

    more

--- .github/scripts/check_diff.py
+++ .github/scripts/check_diff.py
@@ -3,10 +3,10 @@ import sys
 
 ALL_DIRS = {
     "libs/core",
-    "libs/community",
     "libs/langchain",
     "libs/experimental",
-    "libs/partners/openai",
+    # "libs/community",
+    # "libs/partners/openai",
 }
 
 if __name__ == "__main__":
@@ -14,14 +14,18 @@ if __name__ == "__main__":
     dirs_to_run = set()
 
     for file in files:
-        if any(dir_ in file for dir_ in (".github/workflows", ".github/tools", ".github/actions", "libs/core")):
+        if any(dir_ in file for dir_ in (".github/workflows", ".github/tools", ".github/actions", "libs/core", ".github/scripts/check_diff.py")):
             dirs_to_run = ALL_DIRS
             break
         elif "libs/community" in file:
             dirs_to_run.update(("libs/community", "libs/langchain", "libs/experimental"))
         elif "libs/partners" in file:
             partner_dir = file.split("/")[2]
-            dirs_to_run.add(f"libs/partners/{partner_dir}")
+            dirs_to_run.update((f"libs/partners/{partner_dir}", "libs/langchain", "libs/experimental"))
+        elif "libs/langchain" in file:
+            dirs_to_run.update(("libs/langchain", "libs/experimental"))
+        elif "libs/experimental" in file:
+            dirs_to_run.add("libs/experimental")
         else:
             pass
     print(json.dumps(list(dirs_to_run)))
