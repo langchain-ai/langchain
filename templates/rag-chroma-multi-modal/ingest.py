758792acde8c9c5ccf361cfd6320306e7e4ba0eb
Lance Martin <lance@langchain.dev>
2023-12-05 12:55:05 -0800

    fmt

--- templates/rag-chroma-multi-modal/ingest.py
+++ templates/rag-chroma-multi-modal/ingest.py
@@ -34,7 +34,7 @@ vectorstore = Path(__file__).parent / "chroma_db_multi_modal"
 re_vectorstore_path = vectorstore.relative_to(Path.cwd())
 
 # Load embedding function
-print('Loading embedding function')
+print("Loading embedding function")
 embedding = OpenCLIPEmbeddings(model_name="ViT-H-14", checkpoint="laion2b_s32b_b79k")
 
 # Create chroma
@@ -54,5 +54,5 @@ image_uris = sorted(
 )
 
 # Add images
-print('Embedding images')
+print("Embedding images")
 vectorstore_mmembd.add_images(uris=image_uris)
