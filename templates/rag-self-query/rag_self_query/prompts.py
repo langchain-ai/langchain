06459448fe67fdb59cbcb11529401d5b1730cb09
Juan Sebastian Vargas Torres <52805660+Sebas102507@users.noreply.github.com>
2023-12-07 15:52:31 -0500

    Update prompts.py

--- templates/rag-self-query/rag_self_query/prompts.py
+++ templates/rag-self-query/rag_self_query/prompts.py
@@ -14,7 +14,7 @@ CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(
 # RAG Prompt to provide the context and question for LLM to answer
 # We also ask the LLM to cite the source of the passage it is answering from
 llm_context_prompt_template = """
-Use the following passages to answer the user's question.
+I'M SEBASTIAN ! Use the following passages to answer the user's question.
 Each passage has a SOURCE which is the title of the document. When answering, cite source name of the passages you are answering from below the answer in a unique bullet point list.
 
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
