408f64d93dc6cd2cf043b82432d03606cf357b95
Eugene Yurtsev <eyurtsev@gmail.com>
2023-12-08 12:41:13 -0500

    x

--- libs/core/tests/unit_tests/runnables/test_history.py
+++ libs/core/tests/unit_tests/runnables/test_history.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable, Sequence, Union
+from typing import Any, Callable, Sequence, Union, Tuple, Optional, Dict
 
 from langchain_core.messages import AIMessage, BaseMessage, HumanMessage, SystemMessage
 from langchain_core.pydantic_v1 import BaseModel
@@ -18,8 +18,10 @@ def test_interfaces() -> None:
     assert str(history) == "System: system\nHuman: human 1\nAI: ai\nHuman: human 2"
 
 
-def _get_get_session_history() -> Callable[..., ChatMessageHistory]:
-    chat_history_store = {}
+def _get_get_session_history(
+    *, store: Optional[Dict[str, Any]] = None
+) -> Callable[..., ChatMessageHistory]:
+    chat_history_store = store if store is not None else {}
 
     def get_session_history(session_id: str, **kwargs: Any) -> ChatMessageHistory:
         if session_id not in chat_history_store:
@@ -34,13 +36,15 @@ def test_input_messages() -> None:
         lambda messages: "you said: "
         + "\n".join(str(m.content) for m in messages if isinstance(m, HumanMessage))
     )
-    get_session_history = _get_get_session_history()
+    store = {}
+    get_session_history = _get_get_session_history(store=store)
     with_history = RunnableWithMessageHistory(runnable, get_session_history)
     config: RunnableConfig = {"configurable": {"session_id": "1"}}
     output = with_history.invoke([HumanMessage(content="hello")], config)
     assert output == "you said: hello"
     output = with_history.invoke([HumanMessage(content="good bye")], config)
     assert output == "you said: hello\ngood bye"
+    assert store == {}
 
 
 def test_input_dict() -> None:
@@ -271,7 +275,6 @@ def test_using_custom_config_specs() -> None:
     with_message_history = RunnableWithMessageHistory(
         runnable,
         get_session_history=get_session_history,
-        input_messages_key="input",
         session_history_config_specs=[
             ConfigurableFieldSpec(
                 id="user_id",
@@ -300,12 +303,30 @@ def test_using_custom_config_specs() -> None:
             AIMessage(content="you said: hello"),
         ]
     }
-    with_message_history.invoke(
-        {"input": "meow"}, {"configurable": {"user_id": "user1", "conversation_id": "1"}}
-    )
-
-    with_message_history.invoke(
-        {"input": "goodbye"}, {"configurable": {"user_id": "user2", "conversation_id": "1"}}
-    )
     assert store == {
+        ("user1", "1"): ChatMessageHistory(
+            messages=[
+                HumanMessage(content="hello"),
+                AIMessage(content="you said: hello"),
+            ]
+        )
+    }
+
+    result = with_message_history.invoke(
+        "goodbye", {"configurable": {"user_id": "user1", "conversation_id": "1"}}
+    )
+    assert result == {
+        "output": [
+            AIMessage(content="you said: hello\ngoodbye"),
+        ]
+    }
+    assert store == {
+        ("user1", "1"): ChatMessageHistory(
+            messages=[
+                HumanMessage(content="hello"),
+                AIMessage(content="you said: hello"),
+                HumanMessage(content="goodbye"),
+                AIMessage(content="you said: hello\ngoodbye"),
+            ]
+        )
     }
