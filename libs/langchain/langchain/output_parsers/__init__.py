77af120aa446d198fbc8fc02ad06ad5339a65030
TomTom101 <thobra@gmail.com>
2023-12-08 23:49:13 +0100

    feat: Yaml output parser

--- libs/langchain/langchain/output_parsers/__init__.py
+++ libs/langchain/langchain/output_parsers/__init__.py
@@ -35,6 +35,7 @@ from langchain.output_parsers.regex import RegexParser
 from langchain.output_parsers.regex_dict import RegexDictParser
 from langchain.output_parsers.retry import RetryOutputParser, RetryWithErrorOutputParser
 from langchain.output_parsers.structured import ResponseSchema, StructuredOutputParser
+from langchain.output_parsers.yaml import YamlOutputParser
 from langchain.output_parsers.xml import XMLOutputParser
 
 __all__ = [
@@ -60,4 +61,5 @@ __all__ = [
     "JsonOutputToolsParser",
     "PydanticToolsParser",
     "JsonOutputKeyToolsParser",
+    "YamlOutputParser",
 ]
