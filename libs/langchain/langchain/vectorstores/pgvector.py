6ceb8e2ad43856a23b07dcc5b2f780504e28d614
Erick Friis <erick@langchain.dev>
2023-12-07 16:14:48 -0800

    simplify

--- libs/langchain/langchain/vectorstores/pgvector.py
+++ libs/langchain/langchain/vectorstores/pgvector.py
@@ -7,7 +7,6 @@ import logging
 import uuid
 from functools import partial
 from typing import (
-    TYPE_CHECKING,
     Any,
     Callable,
     Dict,
@@ -61,50 +60,45 @@ class BaseModel(Base):
     uuid = sqlalchemy.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
 
 
-def _get_collection_store() -> Any:
-    class CollectionStore(BaseModel):
-        """Collection store."""
+class CollectionStore(BaseModel):
+    """Collection store."""
 
-        __tablename__ = "langchain_pg_collection"
+    __tablename__ = "langchain_pg_collection"
 
-        name = sqlalchemy.Column(sqlalchemy.String)
-        cmetadata = sqlalchemy.Column(JSON)
+    name = sqlalchemy.Column(sqlalchemy.String)
+    cmetadata = sqlalchemy.Column(JSON)
 
-        embeddings = relationship(
-            "EmbeddingStore",
-            back_populates="collection",
-            passive_deletes=True,
-        )
+    embeddings = relationship(
+        "EmbeddingStore",
+        back_populates="collection",
+        passive_deletes=True,
+    )
 
-        @classmethod
-        def get_by_name(
-            cls, session: Session, name: str
-        ) -> Optional["CollectionStore"]:
-            return session.query(cls).filter(cls.name == name).first()  # type: ignore
+    @classmethod
+    def get_by_name(cls, session: Session, name: str) -> Optional["CollectionStore"]:
+        return session.query(cls).filter(cls.name == name).first()  # type: ignore
 
-        @classmethod
-        def get_or_create(
-            cls,
-            session: Session,
-            name: str,
-            cmetadata: Optional[dict] = None,
-        ) -> Tuple["CollectionStore", bool]:
-            """
-            Get or create a collection.
-            Returns [Collection, bool] where the bool is True if the collection was created.
-            """
-            created = False
-            collection = cls.get_by_name(session, name)
-            if collection:
-                return collection, created
-
-            collection = cls(name=name, cmetadata=cmetadata)
-            session.add(collection)
-            session.commit()
-            created = True
+    @classmethod
+    def get_or_create(
+        cls,
+        session: Session,
+        name: str,
+        cmetadata: Optional[dict] = None,
+    ) -> Tuple["CollectionStore", bool]:
+        """
+        Get or create a collection.
+        Returns [Collection, bool] where the bool is True if the collection was created.
+        """
+        created = False
+        collection = cls.get_by_name(session, name)
+        if collection:
             return collection, created
 
-    return CollectionStore
+        collection = cls(name=name, cmetadata=cmetadata)
+        session.add(collection)
+        session.commit()
+        created = True
+        return collection, created
 
 
 def _get_embedding_store() -> Any:
@@ -209,7 +203,7 @@ class PGVector(VectorStore):
         """Initialize the store."""
         self.create_vector_extension()
 
-        self.CollectionStore = _get_collection_store()
+        self.CollectionStore = CollectionStore
         self.EmbeddingStore = _get_embedding_store()
         self.create_tables_if_not_exists()
         self.create_collection()
