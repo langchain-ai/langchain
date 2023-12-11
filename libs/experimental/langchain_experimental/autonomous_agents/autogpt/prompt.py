4f795065c029310a59a5828ae6a496b424aadc82
Крестников Константин Николаевич <KNKrestnikov@sberbank.ru>
2023-12-11 17:44:09 +0300

    AutoGPT fix

--- libs/experimental/langchain_experimental/autonomous_agents/autogpt/prompt.py
+++ libs/experimental/langchain_experimental/autonomous_agents/autogpt/prompt.py
@@ -95,7 +95,7 @@ class AutoGPTPrompt(BaseChatPromptTemplate, BaseModel):  # type: ignore[misc]
             historical_messages = [message] + historical_messages
             used_tokens += message_tokens
         input_message = HumanMessage(content=kwargs["user_input"])
-        messages: List[BaseMessage] = [SystemMessage(content=system_content)]
+        messages: List[BaseMessage] = [base_prompt, time_prompt, memory_message]
         messages += historical_messages
         messages.append(input_message)
         return messages
