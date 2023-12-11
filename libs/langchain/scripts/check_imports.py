dc253020451be0a9e21c3e6698211df6813ae794
Erick Friis <erick@langchain.dev>
2023-12-11 15:50:01 -0800

    need unique module names for pydantic

--- libs/langchain/scripts/check_imports.py
+++ libs/langchain/scripts/check_imports.py
@@ -1,3 +1,5 @@
+import random
+import string
 import sys
 import traceback
 from importlib.machinery import SourceFileLoader
@@ -7,7 +9,10 @@ if __name__ == "__main__":
     has_failure = False
     for file in files:
         try:
-            SourceFileLoader("x", file).load_module()
+            module_name = "".join(
+                random.choice(string.ascii_letters) for _ in range(20)
+            )
+            SourceFileLoader(module_name, file).load_module()
         except Exception:
             has_failure = True
             print(file)
