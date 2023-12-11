34fa1d938da4c3181f8e99f08b1a492af8d1cb32
shrinitgoyal <shrinit.goyal@engati.com>
2023-12-11 18:26:57 +0530

    updated streaming chunk on_llm_new_token in vertexai

--- libs/langchain/langchain/chat_models/vertexai.py
+++ libs/langchain/langchain/chat_models/vertexai.py
@@ -255,9 +255,10 @@ class ChatVertexAI(_VertexAICommon, BaseChatModel):
         chat = self._start_chat(history, **params)
         responses = chat.send_message_streaming(question.content, **params)
         for response in responses:
+            chunk = ChatGenerationChunk(message=AIMessageChunk(content=response.text))
+            yield chunk
             if run_manager:
-                run_manager.on_llm_new_token(response.text)
-            yield ChatGenerationChunk(message=AIMessageChunk(content=response.text))
+                run_manager.on_llm_new_token(token=response.text, chunk=chunk)
 
     def _start_chat(
         self, history: _ChatHistory, **kwargs: Any
