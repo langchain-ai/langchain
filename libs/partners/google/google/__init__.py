fc30c95c3a5720990883ac7b23be21f4f831c7d9
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2023-12-12 12:02:20 -0800

    del

--- libs/partners/google/google/__init__.py
+++ libs/partners/google/google/__init__.py
@@ -1,5 +1,3 @@
 from google.chat_models import ChatGoogleGenerativeAIChat
-from google.llms import GoogleGenerativeAIChatLLM
-from google.vectorstores import GoogleGenerativeAIChatVectorStore
 
-__all__ = ["GoogleGenerativeAIChatLLM", "ChatGoogleGenerativeAIChat", "GoogleGenerativeAIChatVectorStore"]
+__all__ = ["ChatGoogleGenerativeAIChat"]
