2336f0d8e61fa0039d74c09ea914eec4b6a5ae46
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2023-12-13 10:18:50 -0800

    keep imports

--- libs/langchain/langchain/llms/vertexai.py
+++ libs/langchain/langchain/llms/vertexai.py
@@ -1,13 +1,19 @@
 from langchain_community.llms.vertexai import (
     VertexAI,
     VertexAIModelGarden,
+    _response_to_generation,
     _VertexAIBase,
     _VertexAICommon,
+    completion_with_retry,
     is_codey_model,
+    stream_completion_with_retry,
 )
 
 __all__ = [
+    "_response_to_generation",
     "is_codey_model",
+    "completion_with_retry",
+    "stream_completion_with_retry",
     "_VertexAIBase",
     "_VertexAICommon",
     "VertexAI",
