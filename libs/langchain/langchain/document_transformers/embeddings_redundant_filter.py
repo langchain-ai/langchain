ba238c0befa836186d5e56756e7f0b6ec8c95c33
Harrison Chase <hw.chase.17@gmail.com>
2023-12-13 14:19:41 -0800

    cr

--- libs/langchain/langchain/document_transformers/embeddings_redundant_filter.py
+++ libs/langchain/langchain/document_transformers/embeddings_redundant_filter.py
@@ -1,9 +1,11 @@
 from langchain_community.document_transformers.embeddings_redundant_filter import (
     EmbeddingsClusteringFilter,
     EmbeddingsRedundantFilter,
+    _DocumentWithState,
 )
 
 __all__ = [
     "EmbeddingsRedundantFilter",
     "EmbeddingsClusteringFilter",
+    "_DocumentWithState",
 ]
