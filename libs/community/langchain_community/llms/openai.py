4bd54d77adc85e2938325f948a98e9305bf76e80
ggeutzzang <hyungjoo.song@gmail.com>
2023-12-13 17:42:27 +0900

    Update OpenAI Default Model to 'gpt-3.5-turbo-instruct'

--- libs/community/langchain_community/llms/openai.py
+++ libs/community/langchain_community/llms/openai.py
@@ -173,7 +173,7 @@ class BaseOpenAI(BaseLLM):
 
     client: Any = Field(default=None, exclude=True)  #: :meta private:
     async_client: Any = Field(default=None, exclude=True)  #: :meta private:
-    model_name: str = Field(default="text-davinci-003", alias="model")
+    model_name: str = Field(default="gpt-3.5-turbo-instruct", alias="model")
     """Model name to use."""
     temperature: float = 0.7
     """What sampling temperature to use."""
@@ -666,9 +666,11 @@ class BaseOpenAI(BaseLLM):
             "gpt-4-32k": 32768,
             "gpt-4-32k-0314": 32768,
             "gpt-4-32k-0613": 32768,
+            "gpt-4-1106-preview": 128000,
             "gpt-3.5-turbo": 4096,
             "gpt-3.5-turbo-0301": 4096,
             "gpt-3.5-turbo-0613": 4096,
+            "gpt-3.5-turbo-1106": 16385,
             "gpt-3.5-turbo-16k": 16385,
             "gpt-3.5-turbo-16k-0613": 16385,
             "gpt-3.5-turbo-instruct": 4096,
