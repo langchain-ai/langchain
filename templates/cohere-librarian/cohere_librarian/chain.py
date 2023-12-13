234efa43699e0414b5b1bfe67311b5e111d5719f
Erick Friis <erick@langchain.dev>
2023-12-13 11:54:42 -0800

    cr

--- templates/cohere-librarian/cohere_librarian/chain.py
+++ templates/cohere-librarian/cohere_librarian/chain.py
@@ -1,6 +1,3 @@
-# from .router import branch
-# # from .library_info import chain
-# from .blurb_matcher import book_rec_chain
 from langchain.pydantic_v1 import BaseModel
 
 from .router import branched_chain
