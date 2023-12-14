2dd743551897a691ddc4bb0ae6342c49ea3a780c
lujingxuansc <lujingxuansc@bytedance.com>
2023-12-14 11:57:35 +0800

    fix import

--- libs/community/langchain_community/embeddings/bytedance_volcano.py
+++ libs/community/langchain_community/embeddings/bytedance_volcano.py
@@ -5,8 +5,7 @@ from typing import Any, Dict, List, Optional
 
 from langchain_core.embeddings import Embeddings
 from langchain_core.pydantic_v1 import BaseModel, root_validator
-
-from langchain.utils import get_from_dict_or_env
+from langchain_core.utils import get_from_dict_or_env
 
 logger = logging.getLogger(__name__)
 
