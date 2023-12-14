cf3b827278ea5206d9fd5dbf7b74a0c1b3727fbe
vsxd <sun.xd@hotmail.com>
2023-12-14 10:36:58 +0800

    fix: only export main class in langchain/chat_models

--- libs/langchain/langchain/chat_models/sparkllm.py
+++ libs/langchain/langchain/chat_models/sparkllm.py
@@ -1,13 +1,7 @@
 from langchain_community.chat_models.sparkllm import (
     ChatSparkLLM,
-    _convert_delta_to_message_chunk,
-    _convert_dict_to_message,
-    _convert_message_to_dict,
 )
 
 __all__ = [
     "ChatSparkLLM",
-    "_convert_delta_to_message_chunk",
-    "_convert_dict_to_message",
-    "_convert_message_to_dict",
 ]
