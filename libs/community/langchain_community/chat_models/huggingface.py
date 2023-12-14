7989ca091c7608fb159518f6327f2ce7cf9c5041
jacoblee93 <jacoblee93@gmail.com>
2023-12-14 15:10:33 -0800

    Format please

--- libs/community/langchain_community/chat_models/huggingface.py
+++ libs/community/langchain_community/chat_models/huggingface.py
@@ -3,7 +3,9 @@ from typing import Any, List, Optional, Union
 
 from langchain_community.llms.huggingface_endpoint import HuggingFaceEndpoint
 from langchain_community.llms.huggingface_hub import HuggingFaceHub
-from langchain_community.llms.huggingface_text_gen_inference import HuggingFaceTextGenInference
+from langchain_community.llms.huggingface_text_gen_inference import (
+  HuggingFaceTextGenInference
+)
 from langchain_core.callbacks.manager import (
     AsyncCallbackManagerForLLMRun,
     CallbackManagerForLLMRun,
