a587357a553354738eda83231eba10e8a05a6a4f
Harrison Chase <hw.chase.17@gmail.com>
2023-12-15 14:12:02 -0800

    langchain[patch]: remove unused imports (#14680)
    Co-authored-by: Bagatur <baskaryan@gmail.com>

--- libs/langchain/langchain/agents/agent_toolkits/file_management/toolkit.py
+++ libs/langchain/langchain/agents/agent_toolkits/file_management/toolkit.py
@@ -1,6 +1,5 @@
 from langchain_community.agent_toolkits.file_management.toolkit import (
-    _FILE_TOOLS,
     FileManagementToolkit,
 )
 
-__all__ = ["_FILE_TOOLS", "FileManagementToolkit"]
+__all__ = ["FileManagementToolkit"]
--- libs/langchain/langchain/llms/huggingface_pipeline.py
+++ libs/langchain/langchain/llms/huggingface_pipeline.py
@@ -1,15 +1,7 @@
 from langchain_community.llms.huggingface_pipeline import (
-    DEFAULT_BATCH_SIZE,
-    DEFAULT_MODEL_ID,
-    DEFAULT_TASK,
-    VALID_TASKS,
     HuggingFacePipeline,
 )
 
 __all__ = [
-    "DEFAULT_MODEL_ID",
-    "DEFAULT_TASK",
-    "VALID_TASKS",
-    "DEFAULT_BATCH_SIZE",
     "HuggingFacePipeline",
 ]
