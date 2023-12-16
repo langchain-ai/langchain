ed2d07ded77986e21e049a12bf8fe06ca720fef0
MING KANG <ming.kang@oracle.com>
2023-12-15 17:30:39 -0800

    fixed failed tests

--- libs/langchain/langchain/llms/oci_data_science_model_deployment_endpoint.py
+++ libs/langchain/langchain/llms/oci_data_science_model_deployment_endpoint.py
@@ -1,7 +1,6 @@
 from langchain_community.llms.oci_data_science_model_deployment_endpoint import (
     OCIModelDeploymentTGI,
     OCIModelDeploymentVLLM,
-    OCIModelDeploymentLLM,
 )
 
-__all__ = ["OCIModelDeploymentTGI", "OCIModelDeploymentVLLM", "OCIModelDeploymentLLM"]
+__all__ = ["OCIModelDeploymentTGI", "OCIModelDeploymentVLLM"]
