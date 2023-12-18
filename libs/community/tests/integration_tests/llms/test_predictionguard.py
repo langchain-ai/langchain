063a86bc60d275f3ed212be3dd4a2ac626e39062
Erick Friis <erick@langchain.dev>
2023-12-18 11:16:29 -0800

    community: replace deprecated davinci models

--- libs/community/tests/integration_tests/llms/test_predictionguard.py
+++ libs/community/tests/integration_tests/llms/test_predictionguard.py
@@ -5,6 +5,6 @@ from langchain_community.llms.predictionguard import PredictionGuard
 
 def test_predictionguard_call() -> None:
     """Test valid call to prediction guard."""
-    llm = PredictionGuard(model="OpenAI-text-davinci-003")
+    llm = PredictionGuard(model="OpenAI-gpt-3.5-turbo-instruct")
     output = llm("Say foo:")
     assert isinstance(output, str)
