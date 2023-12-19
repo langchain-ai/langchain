1b4e5846708b86920a236304d28ee8878c7b1952
Mikko Korpela <mikko@robocorp.com>
2023-12-19 22:56:49 +0200

    ConvoOutputParser: Handle multiline Action Input + potential hallucinated Observation
    Co-authored-by: Rihards Gravis <rihards@gravis.lv>

--- libs/langchain/langchain/agents/conversational/output_parser.py
+++ libs/langchain/langchain/agents/conversational/output_parser.py
@@ -22,12 +22,22 @@ class ConvoOutputParser(AgentOutputParser):
             return AgentFinish(
                 {"output": text.split(f"{self.ai_prefix}:")[-1].strip()}, text
             )
-        regex = r"Action: (.*?)[\n]*Action Input: (.*)"
+        regex = r"Action: (.*?)[\n]*Action Input: ([\s\S]*)"
         match = re.search(regex, text)
         if not match:
             raise OutputParserException(f"Could not parse LLM output: `{text}`")
         action = match.group(1)
         action_input = match.group(2)
+        # Generate all partials of "\nObservation: " down to "\nO"
+        observation_base = "\nObservation: "
+        parts = action_input.rsplit(observation_base, 1)
+        if len(parts) == 2:
+            action_input = parts[0]
+        for i in range(len(observation_base) - 1, 1, -1):
+            partial = observation_base[:i]
+            if action_input.endswith(partial):
+                action_input = action_input.rsplit(partial, 1)[0]
+                break
         return AgentAction(action.strip(), action_input.strip(" ").strip('"'), text)
 
     @property
