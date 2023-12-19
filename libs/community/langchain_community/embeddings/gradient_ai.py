7ba83e5e0ff8fb9f1e3f154bae74790c78335b07
michaelfeil <me@michaelfeil.eu>
2023-12-19 13:43:48 +0100

    add compatability TinyEmbeddingClient

--- libs/community/langchain_community/embeddings/gradient_ai.py
+++ libs/community/langchain_community/embeddings/gradient_ai.py
@@ -152,3 +152,14 @@ class GradientEmbeddings(BaseModel, Embeddings):
         )
         embeddings = await self.aembed_documents([query])
         return embeddings[0]
+
+
+class TinyAsyncGradientEmbeddingClient:  #: :meta private:
+    """Deprecated, TinyAsyncGradientEmbeddingClient was removed.
+    
+    This class is just for backwards compatibility with older versions of langchain_community.
+    It might be entirely removed in the future.
+    """
+
+    def __init__(self, *args, **kwargs) -> None:
+        raise ValueError("Deprecated,TinyAsyncGradientEmbeddingClient was removed.")
