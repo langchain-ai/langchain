c5d72c599311723a4eb0ad4b2f73d5c1587e0b72
MING KANG <ming.kang@oracle.com>
2023-12-18 16:43:02 -0800

    reverted change in libs/langchain

--- libs/langchain/langchain/llms/oci_data_science_model_deployment_endpoint.py
+++
@@ -1,6 +0,0 @@
-from langchain_community.llms.oci_data_science_model_deployment_endpoint import (
-    OCIModelDeploymentTGI,
-    OCIModelDeploymentVLLM,
-)
-
-__all__ = ["OCIModelDeploymentTGI", "OCIModelDeploymentVLLM"]
