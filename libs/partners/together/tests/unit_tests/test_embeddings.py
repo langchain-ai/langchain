25f2b77f16afdf2a26ea3597b6345d170e6fe213
Erick Friis <erick@langchain.dev>
2023-12-19 18:26:37 -0800

    x

--- libs/partners/together/tests/unit_tests/test_embeddings.py
+++ libs/partners/together/tests/unit_tests/test_embeddings.py
@@ -6,4 +6,4 @@ from langchain_together.embeddings import TogetherEmbeddings
 
 def test_initialization() -> None:
     """Test embedding model initialization."""
-    TogetherEmbeddings()
+    TogetherEmbeddings(model="togethercomputer/m2-bert-80M-8k-retrieval")
