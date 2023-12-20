279ce84ede1d523431b9b2911e5de9c34fb285a1
Mikko Korpela <mikko@robocorp.com>
2023-12-20 17:50:11 +0200

    Test cleanup
    Co-authored-by: Rihards Gravis <rihards@gravis.lv>

--- libs/langchain/tests/unit_tests/agents/output_parsers/test_convo_output_parser.py
+++ libs/langchain/tests/unit_tests/agents/output_parsers/test_convo_output_parser.py
@@ -1,19 +1,21 @@
+from langchain_core.agents import AgentAction
+
 from langchain.agents.conversational.output_parser import ConvoOutputParser
 
 
-def test_normal_output_parsing():
-    result = ConvoOutputParser().parse(
+def test_normal_output_parsing() -> None:
+    _test_convo_output(
         """
 Action: my_action
 Action Input: my action input
-""".strip()
+""",
+        "my_action",
+        "my action input",
     )
-    assert result.tool == "my_action"
-    assert result.tool_input == "my action input"
 
 
-def test_multiline_output_parsing():
-    result = ConvoOutputParser().parse(
+def test_multiline_output_parsing() -> None:
+    _test_convo_output(
         """
 Thought: Do I need to use a tool? Yes
 Action: evaluate_code
@@ -21,39 +23,45 @@ Action Input: Evaluate Code with the following Python content:
 ```python
 print("Hello fifty shades of gray mans!"[::-1])
 ```
-""".strip()
-    )
-    assert result.tool == "evaluate_code"
-    assert (
-        result.tool_input
-        == """
+""",
+        "evaluate_code",
+        """
 Evaluate Code with the following Python content:
 ```python
 print("Hello fifty shades of gray mans!"[::-1])
-```""".lstrip()
+```""".lstrip(),
     )
 
 
-def test_output_parsing_with_observation():
-    result = ConvoOutputParser().parse(
+def test_output_parsing_with_observation() -> None:
+    _test_convo_output(
         """
 Thought: Do I need to use a tool? Yes
 Action: my_tool
 Action Input: the input to the action
 Observation: the result of the action
-""".strip()
+""",
+        "my_tool",
+        "the input to the action",
     )
-    assert result.tool == "my_tool"
-    assert result.tool_input == "the input to the action"
 
 
-def test_output_parsing_with_partial_observation():
-    result = ConvoOutputParser().parse(
+def test_output_parsing_with_partial_observation() -> None:
+    _test_convo_output(
         """
 Thought: Do I need to use a tool? Yes
 Action: my_tool
 Action Input: the input to the action
-Obs""".strip()
+Obs""",
+        "my_tool",
+        "the input to the action",
     )
-    assert result.tool == "my_tool"
-    assert result.tool_input == "the input to the action"
+
+
+def _test_convo_output(
+    input: str, expected_tool: str, expected_tool_input: str
+) -> None:
+    result = ConvoOutputParser().parse(input.strip())
+    assert isinstance(result, AgentAction)
+    assert result.tool == expected_tool
+    assert result.tool_input == expected_tool_input
