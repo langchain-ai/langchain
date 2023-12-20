8c7ce4e5090fb95b034ea0c4a88d8576aaeb28a6
Erick Friis <erick@langchain.dev>
2023-12-19 17:15:55 -0800

    remove chat

--- libs/partners/together/tests/unit_tests/test_chat_models.py
+++
@@ -1,9 +0,0 @@
-"""Test chat model integration."""
-
-
-from langchain_together.chat_models import ChatTogether
-
-
-def test_initialization() -> None:
-    """Test chat model initialization."""
-    ChatTogether()
