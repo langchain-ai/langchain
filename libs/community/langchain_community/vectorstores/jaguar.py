5500891f8398a90bbb07967ed85a0ec00cdd59b4
JY <jyjy@jaguardb>
2023-12-21 02:02:50 +0000

    Fixed line spacing in langchain_community/vectorstores/jaguar.py

--- libs/community/langchain_community/vectorstores/jaguar.py
+++ libs/community/langchain_community/vectorstores/jaguar.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import json
 import logging
 from typing import Any, List, Optional, Tuple
+
 from langchain_core.documents import Document
 from langchain_core.embeddings import Embeddings
 from langchain_core.vectorstores import VectorStore
