ae1ecc93db2cb7f5762673016fe4d1b5eeb829fd
hyy1987 <779003812@qq.com>
2023-12-22 10:27:02 +0000

    update zhipuai

--- libs/community/tests/unit_tests/chat_models/test_zhipuai.py
+++ libs/community/tests/unit_tests/chat_models/test_zhipuai.py
@@ -2,64 +2,79 @@ from typing import Any
 from unittest.mock import MagicMock, patch
 
 import pytest
-from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 
 from langchain_community.chat_models.zhipuai import ChatZhipuAI
 
 
 @pytest.mark.requires("zhipuai")
 def test_chat_zhipuai_model_param() -> None:
-    chat = ChatZhipuAI(api_key="your_api_key", model="chatglm_turbo", streaming=False)
+    chat = ChatZhipuAI(
+        zhipuai_api_key="your_zhipuai_api_key", model="chatglm_turbo", streaming=False
+    )
     assert chat.model == "chatglm_turbo"
-    assert chat.api_key == "your_api_key"
+    assert chat.zhipuai_api_key == "your_zhipuai_api_key"
     assert chat.streaming is False
 
 
 @pytest.fixture
-def mock_chat() -> dict:
+def mock_completion() -> dict:
     return {
-        "code": 200,
-        "msg": "",
-        "success": True,
-        "data": {
-            "task_id": "75931252186628016897601864755556524089",
-            "request_id": "123445676789",
-            "task_status": "SUCCESS",
-            "choices": [{"role": "assistant", "content": "我爱编程"}],
-            "usage": {
-                "prompt_tokens": 215,
-                "completion_tokens": 302,
-                "total_tokens": 517,
-            },
-        },
+        "id": "zhipuai-test-123",
+        "object": "chat.completion",
+        "created": 1703222377,
+        "model": "chatglm_turbo",
+        "choices": [
+            {
+                "index": 0,
+                "message": {
+                    "role": "assistant",
+                    "content": "Bar Baz",
+                },
+                "finish_reason": "stop",
+            }
+        ],
     }
 
 
 @pytest.mark.requires("zhipuai")
-def test_zhipuai_chat(mock_chat: dict) -> None:
-    chat = ChatZhipuAI(api_key="your_api_key", model="chatglm_turbo", streaming=False)
+def test_zhipuai_predict(mock_completion: dict) -> None:
+    llm = ChatZhipuAI()
     mock_client = MagicMock()
     completed = False
 
     def mock_create(*args: Any, **kwargs: Any) -> Any:
         nonlocal completed
         completed = True
-        return mock_chat
+        return mock_completion
 
     mock_client.create = mock_create
     with patch.object(
-        chat,
+        llm,
         "client",
         mock_client,
     ):
-        messages = [
-            AIMessage(content="Hi."),
-            SystemMessage(content="You are a helpful assistant as a translator."),
-            HumanMessage(
-                content="""Translate this sentence from English to Chinese:
-    I love programming."""
-            ),
-        ]
-        res = chat(messages)
-        assert res == "我爱编程"
+        res = llm.predict("bar")
+        assert res == "Bar Baz"
+    assert completed
+
+
+@pytest.mark.requires("zhipuai")
+async def test_zhipuai_apredict(mock_completion: dict) -> None:
+    llm = ChatZhipuAI()
+    mock_client = MagicMock()
+    completed = False
+
+    def mock_create(*args: Any, **kwargs: Any) -> Any:
+        nonlocal completed
+        completed = True
+        return mock_completion
+
+    mock_client.create = mock_create
+    with patch.object(
+        llm,
+        "client",
+        mock_client,
+    ):
+        res = llm.predict("bar")
+        assert res == "Bar Baz"
     assert completed
