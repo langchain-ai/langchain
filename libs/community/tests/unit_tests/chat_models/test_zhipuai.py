314d0c343c0629bac77fd0ca3c15e11c5e826887
nanli <linanenv@gmail.com>
2023-12-23 15:06:13 +0000

    fix test

--- libs/community/tests/unit_tests/chat_models/test_zhipuai.py
+++ libs/community/tests/unit_tests/chat_models/test_zhipuai.py
@@ -5,78 +5,9 @@ import pytest
 
 from langchain_community.chat_models.zhipuai import ChatZhipuAI
 
-zhipuai_api_key = None
-
 
 @pytest.mark.requires("zhipuai")
-def test_chat_zhipuai_model_param() -> None:
-    chat = ChatZhipuAI(
-        zhipuai_api_key=zhipuai_api_key, model="chatglm_turbo", streaming=False
-    )
+def test_integration_initialization() -> None:
+    chat = ChatZhipuAI(model="chatglm_turbo", streaming=False)
     assert chat.model == "chatglm_turbo"
-    assert chat.zhipuai_api_key is None
     assert chat.streaming is False
-
-
-@pytest.fixture
-def mock_completion() -> dict:
-    return {
-        "id": "zhipuai-test-123",
-        "object": "chat.completion",
-        "created": 1703222377,
-        "model": "chatglm_turbo",
-        "choices": [
-            {
-                "index": 0,
-                "message": {
-                    "role": "assistant",
-                    "content": "Bar Baz",
-                },
-                "finish_reason": "stop",
-            }
-        ],
-    }
-
-
-@pytest.mark.requires("zhipuai")
-def test_zhipuai_predict(mock_completion: dict) -> None:
-    llm = ChatZhipuAI()
-    mock_client = MagicMock()
-    completed = False
-
-    def mock_create(*args: Any, **kwargs: Any) -> Any:
-        nonlocal completed
-        completed = True
-        return mock_completion
-
-    mock_client.create = mock_create
-    with patch.object(
-        llm,
-        "invoke",
-        mock_client,
-    ):
-        res = llm.predict("bar")
-        assert res == "Bar Baz"
-    assert completed
-
-
-@pytest.mark.requires("zhipuai")
-async def test_zhipuai_apredict(mock_completion: dict) -> None:
-    llm = ChatZhipuAI()
-    mock_client = MagicMock()
-    completed = False
-
-    def mock_create(*args: Any, **kwargs: Any) -> Any:
-        nonlocal completed
-        completed = True
-        return mock_completion
-
-    mock_client.create = mock_create
-    with patch.object(
-        llm,
-        "async_invoke",
-        mock_client,
-    ):
-        res = llm.predict("bar")
-        assert res == "Bar Baz"
-    assert completed
