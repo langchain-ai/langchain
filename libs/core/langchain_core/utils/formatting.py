c70c92dde68485b0c87f0565d8716c6b98d96593
Harrison Chase <hw.chase.17@gmail.com>
2023-12-26 15:26:53 -0800

    prompt changes

--- libs/core/langchain_core/utils/formatting.py
+++ libs/core/langchain_core/utils/formatting.py
@@ -6,17 +6,6 @@ from typing import Any, List, Mapping, Sequence, Union
 class StrictFormatter(Formatter):
     """A subclass of formatter that checks for extra keys."""
 
-    def check_unused_args(
-        self,
-        used_args: Sequence[Union[int, str]],
-        args: Sequence,
-        kwargs: Mapping[str, Any],
-    ) -> None:
-        """Check to see if extra parameters are passed."""
-        extra = set(kwargs).difference(used_args)
-        if extra:
-            raise KeyError(extra)
-
     def vformat(
         self, format_string: str, args: Sequence, kwargs: Mapping[str, Any]
     ) -> str:
