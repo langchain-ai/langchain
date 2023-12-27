50ef9708abed4ae7d3a83707e46c30dd869159c2
leo-gan <leo.gan.57@gmail.com>
2023-12-27 13:09:21 -0800

    fixed dependency

--- libs/community/langchain_community/output_parsers/ernie_functions.py
+++ libs/community/langchain_community/output_parsers/ernie_functions.py
@@ -3,12 +3,12 @@ import json
 from typing import Any, Dict, List, Optional, Type, Union
 
 import jsonpatch
-from langchain.output_parsers.json import parse_partial_json
 from langchain_core.exceptions import OutputParserException
 from langchain_core.output_parsers import (
     BaseCumulativeTransformOutputParser,
     BaseGenerationOutputParser,
 )
+from langchain_core.output_parsers.json import parse_partial_json
 from langchain_core.outputs import ChatGeneration, Generation
 from langchain_core.pydantic_v1 import BaseModel, root_validator
 
