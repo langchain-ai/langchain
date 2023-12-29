7013395ae03e6a67575ec81ecc33b790a54e5591
Hao Fan <h_fan@apple.com>
2023-12-29 16:12:09 +0800

    add: GitHubIssuesLoader into __init__.py to fix PRB issue

--- libs/community/langchain_community/document_loaders/__init__.py
+++ libs/community/langchain_community/document_loaders/__init__.py
@@ -103,6 +103,7 @@ from langchain_community.document_loaders.geodataframe import GeoDataFrameLoader
 from langchain_community.document_loaders.git import GitLoader
 from langchain_community.document_loaders.gitbook import GitbookLoader
 from langchain_community.document_loaders.github import GitHubIssuesLoader
+from langchain_community.document_loaders.github import GithubFileLoader
 from langchain_community.document_loaders.google_speech_to_text import (
     GoogleSpeechToTextLoader,
 )
@@ -296,6 +297,7 @@ __all__ = [
     "GCSDirectoryLoader",
     "GCSFileLoader",
     "GeoDataFrameLoader",
+    "GithubFileLoader",
     "GitHubIssuesLoader",
     "GitLoader",
     "GitbookLoader",
