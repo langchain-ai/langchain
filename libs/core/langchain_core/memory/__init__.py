6ecd4964869394ab806db5b090f24caf46186ba1
leo-gan <leo.gan.57@gmail.com>
2023-12-29 17:22:26 -0800

    fix ref

--- libs/core/langchain_core/memory/__init__.py
+++ libs/core/langchain_core/memory/__init__.py
@@ -1,11 +1,19 @@
 from langchain_core.memory.base import BaseMemory
+from langchain_core.memory.buffer import (
+    ConversationBufferMemory,
+    ConversationStringBufferMemory,
+)
+from langchain_core.memory.chat_history import BaseChatMessageHistory
 from langchain_core.memory.chat_memory import BaseChatMemory
 from langchain_core.memory.chat_message_history import ChatMessageHistory
 from langchain_core.memory.utils import get_prompt_input_key
 
 __all__ = [
     "BaseChatMemory",
+    "BaseChatMessageHistory",
     "BaseMemory",
     "ChatMessageHistory",
+    "ConversationBufferMemory",
+    "ConversationStringBufferMemory",
     "get_prompt_input_key",
 ]
