3cf7bed382e1ccc65956fb49dfd9b91c26e8f964
Rihards Gravis <rihards@gravis.lv>
2024-01-03 11:20:40 +0200

    Fix linting

--- templates/action_server/action_server/agent.py
+++ templates/action_server/action_server/agent.py
@@ -12,11 +12,13 @@ toolkit = ActionServerToolkit(url="http://localhost:8080", llm=llm)
 tools = toolkit.get_tools()
 
 # Initialize Agent
-system_message = SystemMessage(
-    content="You are a helpful assistant"
-)
+system_message = SystemMessage(content="You are a helpful assistant")
 prompt = OpenAIFunctionsAgent.create_prompt(system_message)
-agent = OpenAIFunctionsAgent(llm=llm,prompt=prompt,tools=tools, )
+agent = OpenAIFunctionsAgent(
+    llm=llm,
+    prompt=prompt,
+    tools=tools,
+)
 
 # Initialize Agent executor
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
@@ -26,7 +28,9 @@ agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 class Input(BaseModel):
     input: str
 
+
 class Output(BaseModel):
     output: str
-    
-agent_executor = agent_executor.with_types(input_type=Input, output_type=Output) # type: ignore[arg-type, assignment]
+
+
+agent_executor = agent_executor.with_types(input_type=Input, output_type=Output)  # type: ignore[arg-type, assignment]
