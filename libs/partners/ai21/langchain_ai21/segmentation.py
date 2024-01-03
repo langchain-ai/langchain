cc26486a29b0bf4c9ced2152da7b4e7bd839d4e2
etang <etang@ai21.com>
2024-01-03 20:41:44 +0200

    feat: wip segmentation

--- libs/partners/ai21/langchain_ai21/segmentation.py
+++ libs/partners/ai21/langchain_ai21/segmentation.py
@@ -1,16 +1,8 @@
-from typing import Sequence, Any
+from typing import List
 
-from langchain_ai21.ai21_base import AI21Base
-from langchain_core.documents import BaseDocumentTransformer, Document
+from langchain.text_splitter import TextSplitter
 
 
-class AI21Segmentation(BaseDocumentTransformer, AI21Base):
-    def transform_documents(self, documents: Sequence[Document], **kwargs: Any) -> Sequence[Document]:
-        new_documents = []
-
-        for document in documents:
-            segments = self.client.segmentation.create(source=document.page_content)
-            for segment in segments.segments:
-                new_documents.append(Document(page_content=segment, metadata={"segment_type": segment.segment_type}))
-
-        return new_documents
+class AI21TextSplitter(TextSplitter):
+    def split_text(self, text: str) -> List[str]:
+        pass
