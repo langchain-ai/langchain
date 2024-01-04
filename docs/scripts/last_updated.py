04ea42c723a9f3dd9042a474e56163ace821464f
Bagatur <baskaryan@gmail.com>
2024-01-04 12:45:18 -0500

    fmt

--- docs/scripts/last_updated.py
+++ docs/scripts/last_updated.py
@@ -8,11 +8,14 @@ _DOCS_DIR = str(Path(__file__).parent.absolute() / ".." / "docs")
 
 
 def update_contents(md, last_updated):
-    if "\n" not in md:
-        md = "\n" + md
     first_header = md.find("#")
     if first_header >= 0:
-        body = md[first_header:].replace("\n", f"\nLast updated: {last_updated}\n", 1)
+        if "\n" in md[first_header:]:
+            body = md[first_header:].replace(
+                "\n", f"\nLast updated: {last_updated}\n", 1
+            )
+        else:
+            body = md[first_header:] + f"\nLast updated: {last_updated}"
         return md[:first_header] + body
     else:
         return f"Last updated: {last_updated}\n{md}"
