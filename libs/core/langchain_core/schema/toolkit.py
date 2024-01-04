0cac115d4fcd8db5326f206b20e168f7f0fcaba3
leo-gan <leo.gan.57@gmail.com>
2024-01-04 14:52:04 -0800

    fix a bug

--- libs/core/langchain_core/schema/toolkit.py
+++ libs/core/langchain_core/schema/toolkit.py
@@ -1,12 +1,12 @@
 from abc import ABC, abstractmethod
 from typing import List
 
-from langchain_core.schema.tool import BaseToolInterface
+from langchain_core.schema.tool import ToolInterface
 
 
 class ToolkitInterface(ABC):
     """Toolkit Interface is a collection of related tools."""
 
     @abstractmethod
-    def get_tools(self) -> List[BaseToolInterface]:
+    def get_tools(self) -> List[ToolInterface]:
         """Get the tools in the toolkit."""
