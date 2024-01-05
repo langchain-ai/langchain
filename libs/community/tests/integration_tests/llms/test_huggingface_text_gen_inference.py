02f6745e71e2a4262b7b596e26728aa2e9db2e0e
minhna4 <minhna4@fpt.com>
2024-01-05 18:42:43 +0700

    Add integration test for HuggingFace TGI

--- libs/community/tests/integration_tests/llms/test_huggingface_text_gen_inference.py
+++ libs/community/tests/integration_tests/llms/test_huggingface_text_gen_inference.py
@@ -1,3 +1,5 @@
+from text_generation import AsyncClient, Client
+
 from langchain_community.llms import HuggingFaceTextGenInference
 
 
@@ -17,3 +19,23 @@ def test_invocation_params_stop_sequences() -> None:
     runtime_stop = ["stop"]
     assert llm._invocation_params(runtime_stop)["stop_sequences"] == [".", "stop"]
     assert llm._default_params["stop_sequences"] == ["."]
+
+
+def test_client_type() -> None:
+    llm = HuggingFaceTextGenInference()
+
+    assert isinstance(llm.client, Client)
+    assert isinstance(llm.async_client, AsyncClient)
+
+
+def test_bearer_api() -> None:
+    llm = HuggingFaceTextGenInference()
+    # If called from a self-hosted TGI server,
+    assert not llm.client.headers
+    assert not llm.async_client.headers
+
+    BEARER_TOKEN = "abcdef1230"
+    llm = HuggingFaceTextGenInference(huggingfacehub_api_token=BEARER_TOKEN)
+    # If called from a self-hosted TGI server,
+    assert llm.client.headers["Authorization"] == f"Bearer {BEARER_TOKEN}"
+    assert llm.async_client.headers["Authorization"] == f"Bearer {BEARER_TOKEN}"
