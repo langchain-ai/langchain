0f7b35f28c24b9a5483f63ca9a832120f743ec74
Erick Friis <erick@langchain.dev>
2024-01-04 17:52:46 -0800

    format

--- libs/community/langchain_community/embeddings/azure_openai.py
+++ libs/community/langchain_community/embeddings/azure_openai.py
@@ -5,9 +5,9 @@ import os
 import warnings
 from typing import Callable, Dict, Optional, Union
 
+from langchain_core._api.deprecation import deprecated
 from langchain_core.pydantic_v1 import Field, root_validator
 from langchain_core.utils import get_from_dict_or_env
-from langchain_core._api.deprecation import deprecated
 
 from langchain_community.embeddings.openai import OpenAIEmbeddings
 from langchain_community.utils.openai import is_openai_v1
