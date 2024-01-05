fefec8f77f01d68244f435230f6f231607147a5f
Erick Friis <erick@langchain.dev>
2024-01-04 19:27:01 -0800

    tests

--- libs/langchain/tests/unit_tests/load/test_load.py
+++ libs/langchain/tests/unit_tests/load/test_load.py
@@ -16,7 +16,7 @@ class NotSerializable:
 
 @pytest.mark.requires("openai")
 def test_loads_openai_llm() -> None:
-    llm = OpenAI(model="davinci", temperature=0.5, openai_api_key="hello")
+    llm = CommunityOpenAI(model="davinci", temperature=0.5, openai_api_key="hello")
     llm_string = dumps(llm)
     llm2 = loads(llm_string, secrets_map={"OPENAI_API_KEY": "hello"})
 
