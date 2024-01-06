8e56e539254181965fcf9e398fb960206a732f60
Nuno Campos <nuno@langchain.dev>
2024-01-05 17:51:24 -0800

    Fix history runnable when called with messages as input

--- libs/core/langchain_core/runnables/history.py
+++ libs/core/langchain_core/runnables/history.py
@@ -283,6 +283,9 @@ class RunnableWithMessageHistory(RunnableBindingBase):
     ) -> List[BaseMessage]:
         from langchain_core.messages import BaseMessage
 
+        if isinstance(input_val, dict):
+            input_val = input_val[self.input_messages_key or "input"]
+
         if isinstance(input_val, str):
             from langchain_core.messages import HumanMessage
 
@@ -337,8 +340,7 @@ class RunnableWithMessageHistory(RunnableBindingBase):
         hist = config["configurable"]["message_history"]
 
         # Get the input messages
-        inputs = _load_suppress_warning(run.inputs)
-        input_val = inputs[self.input_messages_key or "input"]
+        input_val = _load_suppress_warning(run.inputs)
         input_messages = self._get_input_messages(input_val)
 
         # If historic messages were prepended to the input messages, remove them to
