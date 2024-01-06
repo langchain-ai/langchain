4ac61670b283897dca3ff9aaba0a5eed80389b3d
Bagatur <22008038+baskaryan@users.noreply.github.com>
2024-01-05 20:14:22 -0500

    infra: fix langchain openai test dep (#15620)

--- libs/langchain/tests/unit_tests/test_dependencies.py
+++ libs/langchain/tests/unit_tests/test_dependencies.py
@@ -77,6 +77,7 @@ def test_test_group_dependencies(poetry_conf: Mapping[str, Any]) -> None:
             "duckdb-engine",
             "freezegun",
             "langchain-core",
+            "langchain-openai",
             "lark",
             "pandas",
             "pytest",
