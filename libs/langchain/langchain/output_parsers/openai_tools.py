26924e04c7166e2638a4b8eba5b32cd4767ce394
Tat Dat Duong <david@duong.cz>
2024-01-08 17:19:18 +0100

    Mark JsonOutputKeyToolsParser as serializable, fix bug

--- libs/langchain/langchain/output_parsers/openai_tools.py
+++ libs/langchain/langchain/output_parsers/openai_tools.py
@@ -50,9 +50,14 @@ class JsonOutputKeyToolsParser(JsonOutputToolsParser):
     key_name: str
     """The type of tools to return."""
 
+    @classmethod
+    def is_lc_serializable(cls) -> bool:
+        """Return whether this model can be serialized by Langchain."""
+        return True
+
     def parse_result(self, result: List[Generation], *, partial: bool = False) -> Any:
         results = super().parse_result(result)
-        return [res["args"] for res in results if results["type"] == self.key_name]
+        return [res["args"] for res in results if res["type"] == self.key_name]
 
 
 class PydanticToolsParser(JsonOutputToolsParser):
