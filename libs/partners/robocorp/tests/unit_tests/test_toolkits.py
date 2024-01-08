2a93d1240e5755ec3ee8e3f06f7f67d0e4e57ed8
Rihards Gravis <rihards@gravis.lv>
2024-01-08 10:28:15 +0200

    Leave template only

--- libs/partners/robocorp/tests/unit_tests/test_toolkits.py
+++
@@ -1,9 +0,0 @@
-"""Test toolkit integration."""
-from langchain_robocorp.toolkits import ActionServerToolkit
-
-from ._fixtures import FakeChatLLMT
-
-
-def test_initialization() -> None:
-    """Test toolkit initialization."""
-    ActionServerToolkit(url="http://localhost", llm=FakeChatLLMT())
