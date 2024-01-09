581a27d67d4dc9c180febce853c71253d7378a89
Xin Liu <sam@secondstate.io>
2024-01-10 00:19:01 +0800

    feat: support `stream` in `WasmChatService`
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- libs/community/tests/integration_tests/chat_models/test_wasm_chat.py
+++ libs/community/tests/integration_tests/chat_models/test_wasm_chat.py
@@ -26,3 +26,29 @@ def test_chat_wasm_service() -> None:
     assert isinstance(response, AIMessage)
     assert isinstance(response.content, str)
     assert "Paris" in response.content
+
+
+@pytest.mark.enable_socket
+def test_chat_wasm_service_streaming() -> None:
+    """This test requires the port 8080 is not occupied."""
+
+    # service url
+    service_url = "https://f370-50-112-58-64.ngrok-free.app"
+
+    # create wasm-chat service instance
+    chat = WasmChatService(service_url=service_url, streaming=True)
+
+    # create message sequence
+    user_message = HumanMessage(content="What is the capital of France?")
+    messages = [
+        user_message,
+    ]
+
+    output = ""
+    for chunk in chat.stream(messages):
+        # print(f"[DEBUG] result chunk: {chunk}")
+        print(chunk.content, end="", flush=True)
+        output += chunk.content
+
+    print(f"\n\n[DEBUG] output:\n\n{output}")
+    assert "Paris" in output
