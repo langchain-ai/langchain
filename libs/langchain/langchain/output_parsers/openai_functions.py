00465dee9959c4c6e1f3922db07edd58c9df1ffd
Eugene Yurtsev <eyurtsev@gmail.com>
2024-01-09 11:03:47 -0500

    x

--- libs/langchain/langchain/output_parsers/openai_functions.py
+++ libs/langchain/langchain/output_parsers/openai_functions.py
@@ -1,6 +1,6 @@
 import copy
 import json
-from typing import Any, Callable, Dict, List, Optional, Type, Union
+from typing import Any, Dict, List, Optional, Type, Union
 
 import jsonpatch
 from langchain_core.exceptions import OutputParserException
