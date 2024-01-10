6bfcc8f009e132490552614269e0de1923d2feb4
Bagatur <baskaryan@gmail.com>
2024-01-09 21:44:23 -0500

    wip

---
+++ libs/langchain/langchain/chains/tool_use/__init__.py
@@ -0,0 +1,3 @@
+from langchain.chains.tool_use.openai import create_openai_tools_chain, create_openai_functions_chain
+
+__all__ = ["create_openai_functions_chain", "create_openai_tools_chain"]
---
+++ libs/langchain/langchain/chains/tool_use/openai.py
@@ -0,0 +1,79 @@
+from operator import itemgetter
+from typing import Optional, Sequence
+
+from langchain_community.tools.convert_to_openai import (
+    format_tool_to_openai_function,
+    format_tool_to_openai_tool,
+)
+from langchain_core.language_models import BaseLanguageModel
+from langchain_core.prompts import BasePromptTemplate
+from langchain_core.runnables import Runnable, RunnableLambda, RunnableMap
+from langchain_core.tools import Tool
+
+from langchain.output_parsers import JsonOutputToolsParser
+from langchain.output_parsers.ernie_functions import JsonOutputFunctionsParser
+
+
+def _call_tool(tool_api_output: list, tools: Sequence[Tool]) -> Runnable:
+    tool_map = {tool.name: tool for tool in tools}
+    chosen = {}
+    for i, tool_call in enumerate(tool_api_output):
+        chosen[tool_call["type"]] = RunnableLambda(itemgetter(i)) | RunnableLambda(itemgetter("args")) | tool_map[tool_call["type"]]
+    return RunnableMap(chosen)
+
+
+def create_openai_tools_chain(
+    llm: BaseLanguageModel,
+    tools: Sequence[Tool],
+    *,
+    prompt: Optional[BasePromptTemplate] = None,
+    enforce_tool_usage: bool = False,
+) -> Runnable:
+    """"""
+    if enforce_tool_usage and len(tools) > 1:
+        raise ValueError("Cannot enforce tool usage with more than one tool.")
+    llm_with_tools = llm.bind(tools=[format_tool_to_openai_tool(t) for t in tools])
+    if enforce_tool_usage:
+        llm_with_tools = llm_with_tools.bind(
+            tool_choice={"type": "function", "function": {"name": tools[0].name}}
+        )
+    call_tool = RunnableLambda(_call_tool).bind(tools=tools)
+    chain = llm_with_tools | JsonOutputToolsParser() | call_tool
+    if prompt:
+        chain = prompt | chain
+    if enforce_tool_usage:
+        chain = chain | itemgetter(tools[0].name)
+    return chain
+
+
+def _call_tool_from_function(
+    function_api_output: dict, tools: Sequence[Tool]
+) -> Runnable:
+    tool_map = {tool.name: tool for tool in tools}
+    chosen = tool_map[function_api_output["type"]]
+    return itemgetter("args") | chosen
+
+
+def create_openai_functions_chain(
+    llm: BaseLanguageModel,
+    tools: Sequence[Tool],
+    *,
+    prompt: Optional[BasePromptTemplate] = None,
+    enforce_tool_usage: bool = False,
+) -> Runnable:
+    """"""
+    if enforce_tool_usage and len(tools) > 1:
+        raise ValueError()
+
+    llm_with_functions = llm.bind(
+        tools=[format_tool_to_openai_function(t) for t in tools]
+    )
+    if enforce_tool_usage:
+        llm_with_functions = llm_with_functions.bind(
+            function_call={"name": tools[0].name}
+        )
+    call_tool = RunnableLambda(_call_tool_from_function).bind(tools=tools)
+    chain = llm_with_functions | JsonOutputFunctionsParser() | call_tool
+    if prompt:
+        chain = prompt | chain
+    return chain
