102a715b8fcf74e174263e0b963d9d8a62ca47da
mspronesti <massimiliano.pronesti@gmail.com>
2024-01-11 15:20:05 +0100

    wrong import

--- libs/community/langchain_community/tools/amadeus/closest_airport.py
+++ libs/community/langchain_community/tools/amadeus/closest_airport.py
@@ -1,6 +1,5 @@
-from typing import Dict, Optional, Type
+from typing import Any, Dict, Optional, Type
 
-from black import Any
 from langchain_core.callbacks import CallbackManagerForToolRun
 from langchain_core.language_models import BaseLanguageModel
 from langchain_core.pydantic_v1 import BaseModel, Field, root_validator
