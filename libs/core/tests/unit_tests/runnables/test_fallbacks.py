ef1da35280f8d40a46de1b86c8f143cca473d7af
Bagatur <baskaryan@gmail.com>
2024-01-15 15:00:39 -0800

    add tests

---
+++ libs/core/tests/unit_tests/runnables/test_fallbacks.py
@@ -0,0 +1,80 @@
+import sys
+from typing import Any
+
+import pytest
+from syrupy import SnapshotAssertion
+
+from langchain_core.load import dumps
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import (
+    Runnable,
+    RunnableLambda,
+    RunnableParallel,
+    RunnablePassthrough,
+    RunnableWithFallbacks,
+)
+from tests.unit_tests.fake.llm import FakeListLLM
+
+
+@pytest.fixture()
+def llm() -> RunnableWithFallbacks:
+    error_llm = FakeListLLM(responses=["foo"], i=1)
+    pass_llm = FakeListLLM(responses=["bar"])
+
+    return error_llm.with_fallbacks([pass_llm])
+
+
+@pytest.fixture()
+def llm_multi() -> RunnableWithFallbacks:
+    error_llm = FakeListLLM(responses=["foo"], i=1)
+    error_llm_2 = FakeListLLM(responses=["baz"], i=1)
+    pass_llm = FakeListLLM(responses=["bar"])
+
+    return error_llm.with_fallbacks([error_llm_2, pass_llm])
+
+
+@pytest.fixture()
+def chain() -> Runnable:
+    error_llm = FakeListLLM(responses=["foo"], i=1)
+    pass_llm = FakeListLLM(responses=["bar"])
+
+    prompt = PromptTemplate.from_template("what did baz say to {buz}")
+    return RunnableParallel({"buz": lambda x: x}) | (prompt | error_llm).with_fallbacks(
+        [prompt | pass_llm]
+    )
+
+
+def _raise_error(inputs: dict) -> str:
+    raise ValueError()
+
+
+def _dont_raise_error(inputs: dict) -> str:
+    if "exception" in inputs:
+        return "bar"
+    raise ValueError()
+
+
+@pytest.fixture()
+def chain_pass_exceptions() -> Runnable:
+    fallback = RunnableLambda(_dont_raise_error)
+    return {"text": RunnablePassthrough()} | RunnableLambda(
+        _raise_error
+    ).with_fallbacks([fallback], exception_key="exception")
+
+
+@pytest.mark.parametrize(
+    "runnable",
+    ["llm", "llm_multi", "chain", "chain_pass_exceptions"],
+)
+async def test_fallbacks(
+    runnable: RunnableWithFallbacks, request: Any, snapshot: SnapshotAssertion
+) -> None:
+    runnable = request.getfixturevalue(runnable)
+    assert runnable.invoke("hello") == "bar"
+    assert runnable.batch(["hi", "hey", "bye"]) == ["bar"] * 3
+    assert list(runnable.stream("hello")) == ["bar"]
+    assert await runnable.ainvoke("hello") == "bar"
+    assert await runnable.abatch(["hi", "hey", "bye"]) == ["bar"] * 3
+    assert list(await runnable.ainvoke("hello")) == list("bar")
+    if sys.version_info >= (3, 9):
+        assert dumps(runnable, pretty=True) == snapshot
