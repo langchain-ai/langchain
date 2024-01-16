9c7e36cfeb51d5c5f3be3c5f268188c41aef27c8
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2024-01-16 12:50:17 -0800

    lint

--- libs/community/langchain_community/retrievers/bm25.py
+++ libs/community/langchain_community/retrievers/bm25.py
@@ -4,8 +4,8 @@ from typing import Any, Callable, Dict, Iterable, List, Optional
 
 from langchain_core.callbacks import CallbackManagerForRetrieverRun
 from langchain_core.documents import Document
-from langchain_core.retrievers import BaseRetriever
 from langchain_core.pydantic_v1 import Field
+from langchain_core.retrievers import BaseRetriever
 
 
 def default_preprocessing_func(text: str) -> List[str]:
