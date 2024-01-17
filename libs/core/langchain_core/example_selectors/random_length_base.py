25ae3c77fa346657858cb265336b078902e2fce9
Dmaturana81 <elmaturana@gmail.com>
2024-01-17 18:06:40 -0300

    test, format and lint done

--- libs/core/langchain_core/example_selectors/random_length_base.py
+++ libs/core/langchain_core/example_selectors/random_length_base.py
@@ -1,13 +1,24 @@
+"""Select random examples based on length."""
 import random
-from typing import Dict, List
+import re
+from typing import Callable, Dict, List
 
 from langchain_core.example_selectors.length_based import LengthBasedExampleSelector
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts.prompt import PromptTemplate
+from langchain_core.pydantic_v1 import BaseModel
 
 
-class RandomLenghtExampleSelector(LengthBasedExampleSelector):
-    example_prompt: ChatPromptTemplate
+def _get_length_based(text: str) -> int:
+    return len(re.split("\n| ", text))
+
+
+class RandomLenghtExampleSelector(LengthBasedExampleSelector, BaseModel):
+    """Select random examples based on length."""
+
+    example_prompt: PromptTemplate
     min_remaining: int = 30
+    get_text_length: Callable[[str], int] = _get_length_based
+    """Function to measure prompt length. Defaults to word count."""
 
     def select_examples(self, input_variables: Dict[str, str]) -> List[dict]:
         """Select random examples to use based on the input lengths."""
