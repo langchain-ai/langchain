36889fa3f7cca8fe701dfb8739e1e1b75c0d4700
Nisarg Trivedi <nisarg.trivedi@juspay.in>
2024-01-18 03:26:41 +0530

    Resolve flat-layout error

---
+++ setup.py
@@ -0,0 +1,10 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="langchain",
+    version="0.0.329",
+    packages=find_packages(),
+    py_modules=[]
+    # ... other configurations ...
+)
