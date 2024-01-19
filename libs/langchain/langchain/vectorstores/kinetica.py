e72aea17db6bf795a36f7c3a6c5a88251edd09d7
anindyam1969 <amukherjee@kinetica.com>
2024-01-19 16:43:53 +0530

    Deleted Kinetica exports from langchain main

--- libs/langchain/langchain/vectorstores/kinetica.py
+++
@@ -1,7 +0,0 @@
-from langchain_community.vectorstores.kinetica import (
-    DistanceStrategy,
-    Kinetica,
-    KineticaSettings,
-)
-
-__all__ = ["DistanceStrategy", "KineticaSettings", "Kinetica"]
