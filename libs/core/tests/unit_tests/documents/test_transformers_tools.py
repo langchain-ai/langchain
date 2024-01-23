b02ba277438b50bc7ae5ec0f935717dcb7b5869d
Philippe Prados <github@prados.fr>
2024-01-23 11:00:27 +0100

    Remove tools for tests

--- libs/core/tests/unit_tests/documents/test_transformers_tools.py
+++ libs/core/tests/unit_tests/documents/test_transformers_tools.py
@@ -96,7 +96,7 @@ def init_llm(
         #     # cache=False,
         # )
         # return llm
-        assert (False, "set FAKE_LLM=True")
+        assert (FAKE_LLM, "set FAKE_LLM=True")
 
 
 # %% copy_transformer
