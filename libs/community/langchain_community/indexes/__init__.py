37099c88365862106923398e77877fd83532bc14
Philippe Prados <github@prados.fr>
2024-01-24 09:32:30 +0100

    Add __init__.py

--- libs/community/langchain_community/indexes/__init__.py
+++ libs/community/langchain_community/indexes/__init__.py
@@ -1,4 +1,4 @@
-from langchain.indexes import SQLRecordManager
+from langchain_community.indexes._sql_record_manager import SQLRecordManager
 from langchain_community.indexes.memory_recordmanager import MemoryRecordManager
 
 __all__ = ["SQLRecordManager", "MemoryRecordManager"]
