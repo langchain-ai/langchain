eac2b79d633056f54a6cfda7d1bbf94dca5515c8
Praveen Nakshatrala <pnakshatrala@gmail.com>
2024-01-23 23:34:28 -0600

    Added documentation. Cleaned up code from langchain. Moved to langchain_community.

--- libs/community/langchain_community/embeddings/__init__.py
+++ libs/community/langchain_community/embeddings/__init__.py
@@ -85,6 +85,7 @@ from langchain_community.embeddings.vertexai import VertexAIEmbeddings
 from langchain_community.embeddings.volcengine import VolcanoEmbeddings
 from langchain_community.embeddings.voyageai import VoyageEmbeddings
 from langchain_community.embeddings.xinference import XinferenceEmbeddings
+from langchain_community.embeddings.nemo import NeMoEmbeddings
 
 logger = logging.getLogger(__name__)
 
