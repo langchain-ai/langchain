8d78d3e2669f4e7360ca9b32540f20f5912a4f54
mhavey <mchavey@gmail.com>
2024-01-26 07:02:42 -0500

    Update neptune_sparql.py
    updated doc

--- libs/langchain/langchain/chains/graph_qa/neptune_sparql.py
+++ libs/langchain/langchain/chains/graph_qa/neptune_sparql.py
@@ -66,8 +66,28 @@ def extract_sparql(query: str) -> str:
     return query
 
 class NeptuneSparqlQAChain(Chain):
-    """
-    Question-answering against an RDF or OWL graph by generating SPARQL statements.
+    """Chain for question-answering against a Neptune graph
+    by generating SPARQL statements.
+
+    *Security note*: Make sure that the database connection uses credentials
+        that are narrowly-scoped to only include necessary permissions.
+        Failure to do so may result in data corruption or loss, since the calling
+        code may attempt commands that would result in deletion, mutation
+        of data if appropriately prompted or reading sensitive data if such
+        data is present in the database.
+        The best way to guard against such negative outcomes is to (as appropriate)
+        limit the permissions granted to the credentials used with this tool.
+
+        See https://python.langchain.com/docs/security for more information.
+
+    Example:
+        .. code-block:: python
+
+        chain = NeptuneSparqlQAChain.from_llm(
+            llm=llm,
+            graph=graph
+        )
+        response = chain.invoke(query)
     """
 
     graph: NeptuneRdfGraph = Field(exclude=True)
