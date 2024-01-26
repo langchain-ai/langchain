1d2901ec1a9fd09eb8fb37df5674d20001feb276
Jarod Stewart <jarod@jarod.is>
2024-01-26 15:04:14 -0700

    lint

--- templates/shopping-assistant/shopping_assistant/agent.py
+++ templates/shopping-assistant/shopping_assistant/agent.py
@@ -1,22 +1,28 @@
 from typing import List, Tuple
 
-from langchain import hub
+from ionic_langchain.tool import IonicTool
 from langchain.agents import AgentExecutor, create_openai_tools_agent
 from langchain_core.messages import AIMessage, SystemMessage
-from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder
+from langchain_core.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder,
+)
 from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_openai import ChatOpenAI
-
-from ionic_langchain.tool import IonicTool
 
 tools = [IonicTool().tool()]
 
 llm = ChatOpenAI(temperature=0.5, model_name="gpt-3.5-turbo-1106", streaming=True)
 
 # You can modify these!
-AI_CONTENT = """I should use the full pdp url that the tool provides me. Always include query parameters: tag, veh, sourceid"""
+AI_CONTENT = """
+I should use the full pdp url that the tool provides me. 
+Always include query parameters
+"""
 SYSTEM_CONTENT = """
-You are a shopping assistant. You help humans find the best product given their {input}. 
+You are a shopping assistant. 
+You help humans find the best product given their {input}. 
 """
 messages = [
     SystemMessage(content=SYSTEM_CONTENT),
@@ -27,6 +33,7 @@ messages = [
 
 prompt = ChatPromptTemplate.from_messages(messages)
 agent = create_openai_tools_agent(llm, tools, prompt)
+
 
 class AgentInput(BaseModel):
     input: str
