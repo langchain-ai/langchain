301870440e11a906d620a0f87bf144b2c9772695
Chenhui Zhang <zhang.chenhui@outlook.com>
2024-01-28 21:06:40 +0800

    update-chatzhipuai

--- libs/community/tests/unit_tests/chat_models/test_zhipuai.py
+++ libs/community/tests/unit_tests/chat_models/test_zhipuai.py
@@ -5,6 +5,6 @@ from langchain_community.chat_models.zhipuai import ChatZhipuAI
 
 @pytest.mark.requires("zhipuai")
 def test_integration_initialization() -> None:
-    chat = ChatZhipuAI(model="chatglm_turbo", streaming=False)
-    assert chat.model == "chatglm_turbo"
+    chat = ChatZhipuAI(model="glm-4", streaming=False)
+    assert chat.model == "glm-4"
     assert chat.streaming is False
