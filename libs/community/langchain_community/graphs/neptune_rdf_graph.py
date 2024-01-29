5be36367bfe1319300de1c865dc547878869193f
Piyush Jain <piyushjain@duck.com>
2024-01-29 13:26:58 -0800

    Fixed lint errors.

--- libs/community/langchain_community/graphs/neptune_rdf_graph.py
+++ libs/community/langchain_community/graphs/neptune_rdf_graph.py
@@ -1,18 +1,11 @@
-import boto3
 import json
-import requests
-import urllib.parse
+from types import SimpleNamespace
+from typing import Optional
 
+import boto3
+import requests
 from botocore.auth import SigV4Auth
 from botocore.awsrequest import AWSRequest
-from botocore.credentials import ReadOnlyCredentials
-from types import SimpleNamespace
-
-from typing import (
-    TYPE_CHECKING,
-    List,
-    Optional,
-)
 
 CLASS_QUERY = """
 SELECT DISTINCT ?elem ?com
@@ -61,11 +54,12 @@ WHERE {
 """
 
 ELEM_TYPES = {
-	'classes': CLASS_QUERY, 
-	'rels': REL_QUERY, 
-	'dtprops': DTPROP_QUERY, 
-	'oprops': OPROP_QUERY
+    "classes": CLASS_QUERY,
+    "rels": REL_QUERY,
+    "dtprops": DTPROP_QUERY,
+    "oprops": OPROP_QUERY,
 }
+
 
 class NeptuneRdfGraph:
     """Neptune wrapper for RDF graph operations.
@@ -91,7 +85,7 @@ class NeptuneRdfGraph:
             use_iam_auth=False
         )
         schema_elem = graph.get_schema_elements()
-        ... change schema_elements ... 
+        ... change schema_elements ...
         graph.load_from_schema_elements(schema_elem)
         schema = graph.get_schema()
 
@@ -106,12 +100,13 @@ class NeptuneRdfGraph:
 
         See https://python.langchain.com/docs/security for more information.
     """
+
     def __init__(
         self,
         query_endpoint: str,
         use_iam_auth: bool = False,
         region_name: Optional[str] = None,
-        hide_comments: bool = False # we introspect comments, but they might bloat the prompt
+        hide_comments: bool = False,
     ) -> None:
         self.use_iam_auth = use_iam_auth
         self.region_name = region_name
@@ -122,7 +117,7 @@ class NeptuneRdfGraph:
         self.schema = ""
         self.schema_elements = {}
         self.load_schema()
-        
+
     @property
     def get_schema(self) -> str:
         """
@@ -133,18 +128,17 @@ class NeptuneRdfGraph:
     @property
     def get_schema_elements(self):
         return self.schema_elements
- 
-    '''
+
+    """
     Run Neptune query.
-    '''
+    """
+
     def query(
         self,
         query: str,
     ):
         session = boto3.Session()
-        request_data = {
-            "query": query
-        }
+        request_data = {"query": query}
         data = request_data
         request_hdr = None
 
@@ -153,54 +147,66 @@ class NeptuneRdfGraph:
             credentials = credentials.get_frozen_credentials()
             access_key = credentials.access_key
             secret_key = credentials.secret_key
-            service = 'neptune-db'
+            service = "neptune-db"
             session_token = credentials.token
-            params=None
+            params = None
             creds = SimpleNamespace(
-                access_key=access_key, secret_key=secret_key, token=session_token, region=self.region_name)
-            request = AWSRequest(method='POST', url=self.query_endpoint, data=data, params=params)
+                access_key=access_key,
+                secret_key=secret_key,
+                token=session_token,
+                region=self.region_name,
+            )
+            request = AWSRequest(
+                method="POST", url=self.query_endpoint, data=data, params=params
+            )
             SigV4Auth(creds, service, self.region_name).add_auth(request)
-            request.headers['Content-Type']= 'application/x-www-form-urlencoded'
+            request.headers["Content-Type"] = "application/x-www-form-urlencoded"
             request_hdr = request.headers
         else:
             request_hdr = {}
-            request_hdr['Content-Type']= 'application/x-www-form-urlencoded'
+            request_hdr["Content-Type"] = "application/x-www-form-urlencoded"
 
-        queryres = requests.request(method='POST', url=self.query_endpoint, headers=request_hdr, data=data)
+        queryres = requests.request(
+            method="POST", url=self.query_endpoint, headers=request_hdr, data=data
+        )
         json_resp = json.loads(queryres.text)
         return json_resp
 
-    
     def load_from_schema_elements(self, schema_elements):
         """
         Create schema from schema_elements. Helpful in cases where
         introspected schema needs pruning.
         """
 
-        elemstr={}
+        elemstr = {}
         for elem in ELEM_TYPES:
             reslist = []
             for elemrec in self.schema_elements[elem]:
-                uri = elemrec['uri']
-                local = elemrec['local']
+                uri = elemrec["uri"]
+                local = elemrec["local"]
                 str = f"<{uri}> ({local})"
                 if self.hide_comments is False:
-                    str = str + f", {comment}"
+                    str = str + f", {elemrec['comment']}"
                 reslist.append(str)
             elemstr[elem] = ", ".join(reslist)
 
-        self.schema = "".join([
-            f"In the following, each IRI is followed by the local name and ", 
-            f"optionally its description in parentheses. \n",
-            f"The graph supports the following node types:\n", elemstr['classes'],
-            f"The graph supports the following relationships:\n", elemstr['rels'],
-            f"The graph supports the following OWL object properties, ", elemstr['dtprops'],
-            "The graph supports the following OWL data properties, ", elemstr['oprops']
-        ])
+        self.schema = (
+            "In the following, each IRI is followed by the local name and "
+            "optionally its description in parentheses. \n"
+            "The graph supports the following node types:\n"
+            f"{elemstr['classes']}"
+            "The graph supports the following relationships:\n"
+            f"{elemstr['rels']}"
+            "The graph supports the following OWL object properties, "
+            f"{elemstr['dtprops']}"
+            "The graph supports the following OWL data properties, "
+            f"{elemstr['oprops']}"
+        )
 
-    '''
+    """
     Private method split URI into prefix and local
-    '''
+    """
+
     @staticmethod
     def _get_local_name(iri: str):
         if "#" in iri:
@@ -211,28 +217,27 @@ class NeptuneRdfGraph:
             return [f"{'/'.join(toks[0:len(toks)-1])}/", toks[-1]]
         else:
             raise ValueError(f"Unexpected IRI '{iri}', contains neither '#' nor '/'.")
-        
-    '''
+
+    """
     Query Neptune to introspect schema.
-    '''      
+    """
+
     def load_schema(self) -> None:
-        self.schema_elements['distinct_prefixes'] = {}
+        self.schema_elements["distinct_prefixes"] = {}
 
         for elem in ELEM_TYPES:
             items = self.query(ELEM_TYPES[elem])
             reslist = []
-            for r in items['results']['bindings']:
-                uri = r['elem']['value']
+            for r in items["results"]["bindings"]:
+                uri = r["elem"]["value"]
                 toks = self._get_local_name(uri)
-                elem_record = {'uri': uri, 'local': toks[1]}
-                if self.hide_comments == False:
-                    elem_record['comment'] = r['com']['value'] if 'com' in r else ""
+                elem_record = {"uri": uri, "local": toks[1]}
+                if not self.hide_comments:
+                    elem_record["comment"] = r["com"]["value"] if "com" in r else ""
                 reslist.append(elem_record)
-                if not(toks[0] in self.schema_elements['distinct_prefixes']):
-                    self.schema_elements['distinct_prefixes'][toks[0]] = "y"
-                       
+                if toks[0] not in self.schema_elements["distinct_prefixes"]:
+                    self.schema_elements["distinct_prefixes"][toks[0]] = "y"
+
             self.schema_elements[elem] = reslist
 
         self.load_from_schema_elements(self.schema_elements)
-        
-        
