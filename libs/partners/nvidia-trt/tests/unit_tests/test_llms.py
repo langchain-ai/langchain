20f7d5fc353f13678841c563e449a9bfe924700f
Mikhail Khludnev <mkhl@apache.org>
2024-02-01 00:44:15 +0300

    add TritonTensorRTLLM(verbose=False)

--- libs/partners/nvidia-trt/tests/unit_tests/test_llms.py
+++ libs/partners/nvidia-trt/tests/unit_tests/test_llms.py
@@ -1,7 +1,33 @@
 """Test TritonTensorRT Chat API wrapper."""
+import sys
+from io import StringIO
+
+import pytest
+from tritonclient.utils import InferenceServerException
+
 from langchain_nvidia_trt import TritonTensorRTLLM
 
 
 def test_initialization() -> None:
     """Test integration initialization."""
     TritonTensorRTLLM(model_name="ensemble", server_url="http://localhost:8001")
+
+def test_default_verbose() -> None:
+    llm=TritonTensorRTLLM(server_url="http://localhost:8001", model_name="ensemble")
+    captured = StringIO()
+    sys.stdout = captured
+    with pytest.raises(InferenceServerException):
+        llm.client.is_server_live()
+    sys.stdout = sys.__stdout__
+    assert not "is_server_live" in captured.getvalue()
+
+def test_verbose() -> None:
+    llm = TritonTensorRTLLM(server_url="http://localhost:8001", model_name="ensemble",  verbose=True)
+    captured = StringIO()
+    sys.stdout = captured
+    with pytest.raises(InferenceServerException):
+        llm.client.is_server_live()
+    sys.stdout = sys.__stdout__
+    assert "is_server_live" in captured.getvalue()
+
+
