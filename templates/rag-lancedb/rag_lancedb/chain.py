0159139c8ea41001b76f1aeface88723feb15a0f
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2024-02-03 19:18:41 +0530

    Update chain.py
    example added

--- templates/rag-lancedb/rag_lancedb/chain.py
+++ templates/rag-lancedb/rag_lancedb/chain.py
@@ -7,6 +7,42 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.pydantic_v1 import BaseModel
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough
 
+
+# Example for document loading (from url), splitting, and creating vectostore
+"""
+# Load
+from langchain_community.document_loaders import WebBaseLoader
+loader = WebBaseLoader("https://lilianweng.github.io/posts/2023-06-23-agent/")
+data = loader.load()
+
+# Split
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+all_splits = text_splitter.split_documents(data)
+
+# embedding
+embedding = OpenAIEmbeddings()
+
+# Add to vectorDB
+db = lancedb.connect("/tmp/lancedb")
+table = db.create_table(
+    "my_table",
+    data=[
+        {
+            "vector": embeddings.embed_query("Hello World"),
+            "text": "Hello World",
+            "id": "1",
+        }
+    ],
+    mode="overwrite",
+)
+
+vectorstore = LanceDB.from_documents(all_splits, embeddings, connection=table)
+
+retriever = vectorstore.as_retriever()
+
+"""
+
 # Embedding model, DB setting, and embedding a single document
 embedding = OpenAIEmbeddings()
 
