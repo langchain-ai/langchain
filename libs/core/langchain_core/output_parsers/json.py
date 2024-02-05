c14a8d47de976d861724cddecb5133b059619806
tcramer <tania7cramer@gmail.com>
2024-02-05 22:34:01 +0000

    Use commit reference

--- libs/core/langchain_core/output_parsers/json.py
+++ libs/core/langchain_core/output_parsers/json.py
@@ -44,8 +44,8 @@ def _custom_parser(multiline_string: str) -> str:
     return multiline_string
 
 
-# Adapted from https://github.com/KillianLucas/open-interpreter/blob/main/interpreter/core/llm/utils/parse_partial_json.py
-# AGPL License
+# Adapted from https://github.com/KillianLucas/open-interpreter/blob/5b6080fae1f8c68938a1e4fa8667e3744084ee21/interpreter/utils/parse_partial_json.py
+# MIT License
 def parse_partial_json(s: str, *, strict: bool = False) -> Any:
     """Parse a JSON string that may be missing closing braces.
 
