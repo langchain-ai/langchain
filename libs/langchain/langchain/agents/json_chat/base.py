c4f9d68febb5859f30f06d83108428f0867eb61b
Harrison Chase <hw.chase.17@gmail.com>
2024-02-05 12:34:44 -0800

    cr

--- libs/langchain/langchain/agents/json_chat/base.py
+++ libs/langchain/langchain/agents/json_chat/base.py
@@ -12,7 +12,10 @@ from langchain.tools.render import render_text_description
 
 
 def create_json_chat_agent(
-    llm: BaseLanguageModel, tools: Sequence[BaseTool], prompt: ChatPromptTemplate, stop_sequene: bool = True
+    llm: BaseLanguageModel,
+    tools: Sequence[BaseTool],
+    prompt: ChatPromptTemplate,
+    stop_sequence: bool = True,
 ) -> Runnable:
     """Create an agent that uses JSON to format its logic, build for Chat Models.
 
@@ -20,7 +23,9 @@ def create_json_chat_agent(
         llm: LLM to use as the agent.
         tools: Tools this agent has access to.
         prompt: The prompt to use. See Prompt section below for more.
-        stop_sequene: Stop on "Observation:" to avoid hallucinates, default is True
+        stop_sequence: Adds a stop token of "Observation:" to avoid hallucinates. 
+            Default is True. You may to set this to False if the LLM you are using
+            does not support stop sequences.
     Returns:
         A Runnable sequence representing an agent. It takes as input all the same input
         variables as the prompt passed in does. It returns as output either an
@@ -148,9 +153,9 @@ def create_json_chat_agent(
         tools=render_text_description(list(tools)),
         tool_names=", ".join([t.name for t in tools]),
     )
-    if stop_sequene:
+    if stop_sequence:
         llm = llm.bind(stop=["\nObservation"])
-        
+
     agent = (
         RunnablePassthrough.assign(
             agent_scratchpad=lambda x: format_log_to_messages(
