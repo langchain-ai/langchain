b3491d9ef65db1e8340e6feeff61bc6200d4cffe
root <tkhabouss@leyton.com>
2024-02-05 17:05:47 +0100

    Ensure that the Elasticsearch Query Translator functions accurately with Date

--- libs/langchain/langchain/retrievers/self_query/elasticsearch.py
+++ libs/langchain/langchain/retrievers/self_query/elasticsearch.py
@@ -61,11 +61,25 @@ class ElasticsearchTranslator(Visitor):
         ]
 
         if is_range_comparator:
-            return {
-                "range": {
-                    field: {self._format_func(comparison.comparator): comparison.value}
+            if isinstance(comparison.value, dict):
+                if "date" in comparison.value:
+                    return {
+                        "range": {
+                            field: {
+                                self._format_func(
+                                    comparison.comparator
+                                ): comparison.value["date"]
+                            }
+                        }
+                    }
+            else:
+                return {
+                    "range": {
+                        field: {
+                            self._format_func(comparison.comparator): comparison.value
+                        }
+                    }
                 }
-            }
 
         if comparison.comparator == Comparator.CONTAIN:
             return {
@@ -84,6 +98,10 @@ class ElasticsearchTranslator(Visitor):
         # we assume that if the value is a string,
         # we want to use the keyword field
         field = f"{field}.keyword" if isinstance(comparison.value, str) else field
+
+        if isinstance(comparison.value, dict):
+            if "date" in comparison.value:
+                comparison.value = comparison.value["date"]
 
         return {self._format_func(comparison.comparator): {field: comparison.value}}
 
