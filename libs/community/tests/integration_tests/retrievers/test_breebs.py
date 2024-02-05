334b6ebdf34ae3f99a4f7c2ee9a37bc7e58ba66b
Alex Boury <55697630+Poissecaille@users.noreply.github.com>
2024-02-06 00:51:08 +0100

    community[minor]: Breebs docs retriever (#16578)
    - **Description:** Implementation of breeb retriever with integration
    tests ->
    libs/community/tests/integration_tests/retrievers/test_breebs.py and
    documentation (notebook) ->
    docs/docs/integrations/retrievers/breebs.ipynb.
      - **Dependencies:** None

---
+++ libs/community/tests/integration_tests/retrievers/test_breebs.py
@@ -0,0 +1,21 @@
+from typing import List
+
+from langchain_core.callbacks.manager import CallbackManagerForRetrieverRun
+from langchain_core.documents import Document
+
+from langchain_community.retrievers.breebs import BreebsRetriever
+
+
+class TestBreebsRetriever:
+    def test_breeb_query(self) -> None:
+        breeb_key = "Parivoyage"
+        query = "What are the best churches to visit in Paris?"
+        breeb_retriever = BreebsRetriever(breeb_key)
+        documents: List[Document] = breeb_retriever._get_relevant_documents(
+            query, run_manager=CallbackManagerForRetrieverRun
+        )
+        assert isinstance(documents, list), "Documents should be a list"
+        for doc in documents:
+            assert doc.page_content, "Document page_content should not be None"
+            assert doc.metadata["source"], "Document metadata should contain 'source'"
+            assert doc.metadata["score"] == 1, "Document score should be equal to 1"
