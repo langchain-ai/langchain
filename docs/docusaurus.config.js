91ae66975ec81e740f05436f879999e53572c58f
Bagatur <baskaryan@gmail.com>
2024-02-05 13:58:36 -0800

    fix

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -302,7 +302,6 @@ const config = {
                 label: "YouTube",
                 href: "https://www.youtube.com/@LangChain",
               },
-              }
             ],
           },
         ],
