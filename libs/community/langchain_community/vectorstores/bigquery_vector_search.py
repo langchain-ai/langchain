dd320f7338ab6f8cef8b97fcbbccf3f109f1ac13
Ashley Xu <139821907+ashleyxuu@users.noreply.github.com>
2024-02-06 20:04:55 +0000

    fix lint

--- libs/community/langchain_community/vectorstores/bigquery_vector_search.py
+++ libs/community/langchain_community/vectorstores/bigquery_vector_search.py
@@ -12,10 +12,10 @@ from threading import Lock, Thread
 from typing import Any, Callable, Dict, List, Optional, Tuple, Type
 
 import numpy as np
-from langchain_core import __version__
 from langchain_core.documents import Document
 from langchain_core.embeddings import Embeddings
 from langchain_core.vectorstores import VectorStore
+from langchain_core import __version__
 
 from langchain_community.vectorstores.utils import (
     DistanceStrategy,
