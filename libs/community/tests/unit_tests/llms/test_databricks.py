53ea2369fbf7db86afef83432173a29aef554f15
Liang Zhang <liang.zhang@databricks.com>
2024-02-07 18:46:05 -0800

    add test

---
+++ libs/community/tests/unit_tests/llms/test_databricks.py
@@ -0,0 +1,36 @@
+"""test Databricks LLM"""
+from pytest import MonkeyPatch
+import pickle
+
+from langchain_community.llms.databricks import Databricks
+
+class MockDatabricksServingEndpointClient:
+    def __init__(self, host, api_token, endpoint_name, databricks_uri, task):
+        self.host = host
+        self.api_token = api_token
+        self.endpoint_name = endpoint_name
+        self.databricks_uri = databricks_uri
+        self.task = task
+
+def transform_input(**request):
+    request["messages"] = [
+        {
+            "role": "user",
+            "content": request["prompt"]
+        }
+    ]
+    del request["prompt"]
+    return request
+
+def test_serde_transform_input_fn(monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setattr(
+        'langchain_community.llms.databricks._DatabricksServingEndpointClient',
+        MockDatabricksServingEndpointClient
+    )
+    monkeypatch.setenv("DATABRICKS_HOST", "my-default-host")
+    monkeypatch.setenv("DATABRICKS_TOKEN", "my-default-token")
+
+    llm = Databricks(endpoint_name="databricks-mixtral-8x7b-instruct", transform_input_fn=transform_input)
+    params = llm._default_params
+    pickled_string = pickle.dumps(transform_input).hex()
+    assert params["transform_input_fn"] == pickled_string
