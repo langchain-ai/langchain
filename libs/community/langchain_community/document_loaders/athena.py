f7e453971dd56288b2d84dc7d78f390cc78f7aba
Bagatur <22008038+baskaryan@users.noreply.github.com>
2024-02-12 15:21:38 -0800

    community[patch]: remove print (#17435)

--- libs/community/langchain_community/document_loaders/athena.py
+++ libs/community/langchain_community/document_loaders/athena.py
@@ -82,14 +82,12 @@ class AthenaLoader(BaseLoader):
             ResultConfiguration={"OutputLocation": self.s3_output_uri},
         )
         query_execution_id = response["QueryExecutionId"]
-        print(f"Query : {self.query}")
         while True:
             response = self.athena_client.get_query_execution(
                 QueryExecutionId=query_execution_id
             )
             state = response["QueryExecution"]["Status"]["State"]
             if state == "SUCCEEDED":
-                print(f"State : {state}")
                 break
             elif state == "FAILED":
                 resp_status = response["QueryExecution"]["Status"]
@@ -98,8 +96,6 @@ class AthenaLoader(BaseLoader):
                 raise Exception(err)
             elif state == "CANCELLED":
                 raise Exception("Query was cancelled by the user.")
-            else:
-                print(f"State : {state}")
             time.sleep(1)
 
         result_set = self._get_result_set(query_execution_id)
