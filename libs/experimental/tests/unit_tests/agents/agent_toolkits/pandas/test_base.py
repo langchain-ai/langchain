e2d93e46c6016af949e48935f4297cb26a65b41e
Bagatur <baskaryan@gmail.com>
2024-02-12 21:12:21 -0800

    fmt

--- libs/experimental/tests/unit_tests/agents/agent_toolkits/pandas/test_base.py
+++ libs/experimental/tests/unit_tests/agents/agent_toolkits/pandas/test_base.py
@@ -1,3 +1,5 @@
+import sys
+
 import pytest
 
 from langchain_experimental.agents import create_pandas_dataframe_agent
@@ -5,6 +7,7 @@ from tests.unit_tests.fake_llm import FakeLLM
 
 
 @pytest.mark.requires("pandas", "tabulate")
+@pytest.mark.skipif(sys.version_info < (3, 9), reason="requires python3.9 or higher")
 def test_create_pandas_dataframe_agent() -> None:
     import pandas as pd
 
