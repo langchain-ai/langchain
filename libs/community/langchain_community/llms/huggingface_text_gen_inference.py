7566d610f4679a338540910f149faf66f4450985
A-Roucher <aymeric.roucher@gmail.com>
2024-02-13 19:11:44 +0000

    Resolve import lint error

--- libs/community/langchain_community/llms/huggingface_text_gen_inference.py
+++ libs/community/langchain_community/llms/huggingface_text_gen_inference.py
@@ -1,11 +1,11 @@
 import logging
 from typing import Any, AsyncIterator, Dict, Iterator, List, Optional
 
+from langchain_core._api.deprecation import deprecated
 from langchain_core.callbacks import (
     AsyncCallbackManagerForLLMRun,
     CallbackManagerForLLMRun,
 )
-from langchain_core._api.deprecation import deprecated
 from langchain_core.language_models.llms import LLM
 from langchain_core.outputs import GenerationChunk
 from langchain_core.pydantic_v1 import Extra, Field, root_validator
