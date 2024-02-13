7feea66862bea18c4633a27ef5dd9464b1e4951a
Eugene Yurtsev <eyurtsev@gmail.com>
2024-02-12 22:33:52 -0500

    Update libs/community/langchain_community/llms/huggingface_hub.py

--- libs/community/langchain_community/llms/huggingface_hub.py
+++ libs/community/langchain_community/llms/huggingface_hub.py
@@ -127,7 +127,7 @@ class HuggingFaceHub(LLM):
                 response = hf("Tell me a joke.")
         """
         _model_kwargs = self.model_kwargs or {}
-        parameters = {"return_full_text": False, **_model_kwargs, **kwargs, "return_full_text": False}
+        parameters = {"return_full_text": False, **_model_kwargs, **kwargs}
 
         response = self.client.post(
             json={"inputs": prompt, "parameters": parameters}, task=self.task
