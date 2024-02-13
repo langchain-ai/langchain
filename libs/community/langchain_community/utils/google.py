6f0e783002ea558d3dc5adf21a887aa3a181db0c
Erick Friis <erick@langchain.dev>
2024-02-13 14:20:15 -0800

    delete unused retry decorator

--- libs/community/langchain_community/utils/google.py
+++ libs/community/langchain_community/utils/google.py
@@ -1,42 +1,7 @@
 """Utilities to use Google provided components."""
 
 from importlib import metadata
-from typing import Any, Callable, Optional, Union
-
-from langchain_core.callbacks import (
-    AsyncCallbackManagerForLLMRun,
-    CallbackManagerForLLMRun,
-)
-from langchain_core.language_models.llms import create_base_retry_decorator
-
-
-def create_retry_decorator(
-    *,
-    max_retries: int = 1,
-    run_manager: Optional[
-        Union[AsyncCallbackManagerForLLMRun, CallbackManagerForLLMRun]
-    ] = None,
-) -> Callable[[Any], Any]:
-    """Creates a retry decorator for Vertex / Palm LLMs."""
-    from google.api_core.exceptions import (
-        Aborted,
-        DeadlineExceeded,
-        GoogleAPIError,
-        ResourceExhausted,
-        ServiceUnavailable,
-    )
-
-    errors = [
-        ResourceExhausted,
-        ServiceUnavailable,
-        Aborted,
-        DeadlineExceeded,
-        GoogleAPIError,
-    ]
-    decorator = create_base_retry_decorator(
-        error_types=errors, max_retries=max_retries, run_manager=run_manager
-    )
-    return decorator
+from typing import Any, Optional
 
 
 def get_client_info(module: Optional[str] = None) -> Any:
