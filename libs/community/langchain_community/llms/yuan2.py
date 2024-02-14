cfeae31324e3113f6c4ccffc983e15a2ce03b369
wulixuan <cauwulixuan@163.com>
2024-02-14 11:18:18 +0800

    Update yuan2.py

--- libs/community/langchain_community/llms/yuan2.py
+++ libs/community/langchain_community/llms/yuan2.py
@@ -3,6 +3,7 @@ import logging
 from typing import Any, Dict, List, Mapping, Optional, Set
 
 import requests
+from langchain_core.callbacks import CallbackManagerForLLMRun
 from langchain_core.language_models.llms import LLM
 from langchain_core.pydantic_v1 import Field
 
