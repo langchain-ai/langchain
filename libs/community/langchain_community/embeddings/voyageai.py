4de355353b04d2e66131b8cd1980e4c926895ba7
fodizoltan <zoltan@conway.expert>
2024-02-16 22:08:08 +0100

    - model:str  -make batch_size optional

--- libs/community/langchain_community/embeddings/voyageai.py
+++ libs/community/langchain_community/embeddings/voyageai.py
@@ -74,9 +74,10 @@ class VoyageEmbeddings(BaseModel, Embeddings):
             query_result = voyage.embed_query(text)
     """
 
+    model: str
     voyage_api_base: str = "https://api.voyageai.com/v1/embeddings"
     voyage_api_key: Optional[SecretStr] = None
-    batch_size: int = 8
+    batch_size: Optional[int] = None
     """Maximum number of texts to embed in each API request."""
     max_retries: int = 6
     """Maximum number of retries to make when generating."""
