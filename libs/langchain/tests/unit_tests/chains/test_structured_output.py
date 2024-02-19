670943d84ff028d7e44eed2f099791f3819809b6
Chester Curme <chester.curme@gmail.com>
2024-02-19 16:20:19 -0500

    lint

--- libs/langchain/tests/unit_tests/chains/test_structured_output.py
+++ libs/langchain/tests/unit_tests/chains/test_structured_output.py
@@ -1,21 +1,44 @@
 from typing import Optional
 
-from langchain.chains import create_structured_output_runnable
 from langchain_community.chat_models.fake import FakeMessagesListChatModel
 from langchain_core.messages import AIMessage
 from langchain_core.pydantic_v1 import BaseModel, Field
 
+from langchain.chains import create_structured_output_runnable
 
-def test_structured_output_tools():
+
+def test_structured_output_tools() -> None:
     class Dog(BaseModel):
-        '''Identifying information about a dog.'''
+        """Identifying information about a dog."""
 
         name: str = Field(..., description="The dog's name")
         color: str = Field(..., description="The dog's color")
         fav_food: Optional[str] = Field(None, description="The dog's favorite food")
 
     responses = [
-        AIMessage(content='', additional_kwargs={'tool_calls': [{'id': 'call_qtDBDM46DpUsPA3BC0hucEUi', 'function': {'arguments': '{"output": {"name": "Harry", "color": "brown", "fav_food": "chicken"}}', 'name': '_OutputFormatter'}, 'type': 'function'}, {'id': 'call_kIPim7iMCL1Ekk61jCenP5Kd', 'function': {'arguments': '{"output": {"name": "Joe", "color": "black", "fav_food": "dog food"}}', 'name': '_OutputFormatter'}, 'type': 'function'}]})
+        AIMessage(
+            content="",
+            additional_kwargs={
+                "tool_calls": [
+                    {
+                        "id": "call_qtDBDM46DpUsPA3BC0hucEUi",
+                        "function": {
+                            "arguments": '{"output": {"name": "Harry", "color": "brown", "fav_food": "chicken"}}',  # noqa: E501
+                            "name": "_OutputFormatter",
+                        },
+                        "type": "function",
+                    },
+                    {
+                        "id": "call_kIPim7iMCL1Ekk61jCenP5Kd",
+                        "function": {
+                            "arguments": '{"output": {"name": "Joe", "color": "black", "fav_food": "dog food"}}',  # noqa: E501
+                            "name": "_OutputFormatter",
+                        },
+                        "type": "function",
+                    },
+                ]
+            },
+        )
     ]
     llm = FakeMessagesListChatModel(responses=responses)
     structured_llm = create_structured_output_runnable(Dog, llm, mode="openai-tools")
@@ -23,4 +46,7 @@ def test_structured_output_tools():
         "Harry was a chubby brown beagle who loved chicken. "
         "Joe was a black lab who loved dog food."
     )
-    assert [Dog(name='Harry', color='brown', fav_food='chicken'), Dog(name='Joe', color='black', fav_food='dog food')] == result
+    assert [
+        Dog(name="Harry", color="brown", fav_food="chicken"),
+        Dog(name="Joe", color="black", fav_food="dog food"),
+    ] == result
