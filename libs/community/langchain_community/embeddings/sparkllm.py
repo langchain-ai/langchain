4fe2c1946954ab835a063425e3ef87cea8b1ffef
liugddx <liugddx@gmail.com>
2024-02-20 16:32:50 +0800

    fmt

--- libs/community/langchain_community/embeddings/sparkllm.py
+++ libs/community/langchain_community/embeddings/sparkllm.py
@@ -11,11 +11,10 @@ from wsgiref.handlers import format_date_time
 
 import numpy as np
 import requests
-from numpy import ndarray
-
 from langchain_core.embeddings import Embeddings
 from langchain_core.pydantic_v1 import BaseModel, SecretStr, root_validator
 from langchain_core.utils import convert_to_secret_str, get_from_dict_or_env
+from numpy import ndarray
 
 # Used for document and knowledge embedding
 EMBEDDING_P_API_URL: str = "https://cn-huabei-1.xf-yun.com/v1/private/sa8a05c27"
