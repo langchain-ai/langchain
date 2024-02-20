139864f8c6993a6e717bfe46041c6d7b16c4287b
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2024-02-20 20:30:48 +0530

    Update chain.py with new lancedb format

--- templates/rag-lancedb/rag_lancedb/chain.py
+++ templates/rag-lancedb/rag_lancedb/chain.py
@@ -1,4 +1,3 @@
-import lancedb
 from langchain_community.chat_models import ChatOpenAI
 from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores import LanceDB
@@ -8,7 +7,8 @@ from langchain_core.pydantic_v1 import BaseModel
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough
 
 # Example for document loading (from url), splitting, and creating vectostore
-"""
+
+""" 
 # Load
 from langchain_community.document_loaders import WebBaseLoader
 loader = WebBaseLoader("https://lilianweng.github.io/posts/2023-06-23-agent/")
@@ -19,57 +19,33 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
 all_splits = text_splitter.split_documents(data)
 
-# embedding
-embedding = OpenAIEmbeddings()
-
 # Add to vectorDB
-db = lancedb.connect("/tmp/lancedb")
-table = db.create_table(
-    "my_table",
-    data=[
-        {
-            "vector": embeddings.embed_query("Hello World"),
-            "text": "Hello World",
-            "id": "1",
-        }
-    ],
-    mode="overwrite",
-)
-
-vectorstore = LanceDB.from_documents(all_splits, embeddings, connection=table)
-
+vectorstore = LanceDB.from_documents(documents=all_splits, 
+                                    collection_name="rag-chroma",
+                                    embedding=OpenAIEmbeddings(),
+                                    )
 retriever = vectorstore.as_retriever()
-
 """
 
-# Embedding model, DB setting, and embedding a single document
-embedding = OpenAIEmbeddings()
-
-db = lancedb.connect("/tmp/lancedb")
-table = db.create_table(
-    "my_table",
-    data=[
-        {
-            "vector": embedding.embed_query("Hello World"),
-            "text": "Hello World",
-            "id": "1",
-        }
-    ],
-    mode="overwrite",
-)
-
+# Embed a single document for test
 vectorstore = LanceDB.from_texts(
-    ["harrison worked at kensho"], embedding, connection=table
+    ["harrison worked at kensho"], embedding=OpenAIEmbeddings()
 )
+
 retriever = vectorstore.as_retriever()
-# Template, LLM, RAG chain, and typing for input
+
+# RAG prompt
 template = """Answer the question based only on the following context:
 {context}
 
 Question: {question}
 """
 prompt = ChatPromptTemplate.from_template(template)
+
+# LLM
 model = ChatOpenAI()
+
+# RAG chain
 chain = (
     RunnableParallel({"context": retriever, "question": RunnablePassthrough()})
     | prompt
