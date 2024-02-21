97c338ac3e5f498a92867b8c2dc8e36a7dc10f08
leo-gan <leo.gan.57@gmail.com>
2024-02-20 19:28:31 -0800

    added module descriptions

--- libs/core/langchain_core/documents/__init__.py
+++ libs/core/langchain_core/documents/__init__.py
@@ -1,3 +1,7 @@
+"""**Document** module is a collection of classes that handle documents
+and their transformations.
+
+"""
 from langchain_core.documents.base import Document
 from langchain_core.documents.transformers import BaseDocumentTransformer
 
