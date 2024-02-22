9502485295fe784dc70fd8b4ee4f3df3fea9ff72
Shengsheng Huang <shengsheng.huang@intel.com>
2024-02-23 05:58:02 +0800

    revise tests to make it run faster

--- libs/community/tests/integration_tests/llms/test_bigdl.py
+++ libs/community/tests/integration_tests/llms/test_bigdl.py
@@ -5,14 +5,18 @@ from langchain_community.llms.bigdl import BigdlLLM
 
 def test_call() -> None:
     """Test valid call to baichuan."""
-    llm = BigdlLLM()
-    output = llm("Who won the second world war?")
+    llm = BigdlLLM.from_model_id(
+        model_id="lmsys/vicuna-7b-v1.5",
+        model_kwargs={"temperature": 0, "max_length": 16, "trust_remote_code": True})
+    output = llm("Hello!")
     assert isinstance(output, str)
 
 
 def test_generate() -> None:
     """Test valid call to baichuan."""
-    llm = BigdlLLM()
-    output = llm.generate(["Who won the second world war?"])
+    llm = BigdlLLM.from_model_id(
+        model_id="lmsys/vicuna-7b-v1.5",
+        model_kwargs={"temperature": 0, "max_length": 16, "trust_remote_code": True})
+    output = llm.generate(["Hello!"])
     assert isinstance(output, LLMResult)
     assert isinstance(output.generations, list)
