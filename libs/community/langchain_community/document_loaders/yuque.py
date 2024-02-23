19d09374bbee319aa127638e89686329e01df1a0
Dounx <imdounx@gmail.com>
2024-02-23 21:58:03 +0800

    fix lint

--- libs/community/langchain_community/document_loaders/yuque.py
+++ libs/community/langchain_community/document_loaders/yuque.py
@@ -1,7 +1,7 @@
 import re
-from typing import List, Dict, Iterator
-import requests
+from typing import Dict, Iterator, List
 
+import requests
 from langchain_core.documents import Document
 
 from langchain_community.document_loaders.base import BaseLoader
