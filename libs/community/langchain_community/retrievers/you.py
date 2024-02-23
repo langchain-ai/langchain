5a7c889ee45713fd722b541304ea865bbbf935af
Scott Nath <github@scottnath.com>
2024-02-23 12:22:00 -0500

    Update you.py

--- libs/community/langchain_community/retrievers/you.py
+++ libs/community/langchain_community/retrievers/you.py
@@ -10,7 +10,7 @@ from langchain_core.retrievers import BaseRetriever
 from langchain_community.utilities import YouSearchAPIWrapper
 
 
-class YouRetriever(BaseRetriever, YouSearchAPIWrapper): # type: ignore[valid-type,misc]
+class YouRetriever(BaseRetriever, YouSearchAPIWrapper):  # type: ignore[valid-type,misc]
     """`You` retriever that uses You.com's search API.
     It wraps results() to get_relevant_documents
     It uses all YouSearchAPIWrapper arguments without any change.
