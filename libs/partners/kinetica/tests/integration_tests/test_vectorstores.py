418e8253d4a13e5b3715850ca6a292ac36c0e2c4
Chad Juliano <cjuliano@kinetica.com>
2024-02-26 20:48:42 -0600

    Add working integration tests.

---
+++ libs/partners/kinetica/tests/integration_tests/test_vectorstores.py
@@ -0,0 +1,5 @@
+from langchain_kinetica.vectorstores import KineticaVectorStore
+
+def test_initialization() -> None:
+    """Test integration vectorstore initialization."""
+    KineticaVectorStore()
