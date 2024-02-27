8e7ddae181a87473747e987b6d6de476167e4604
JAlexMcGraw <jamcgraw21@gmail.com>
2024-02-27 15:59:21 -0600

    Add GoogleWeatherServiceAPI Class
    Added the Google/NationalWeatherService weather API and a test file. Need to make sure it all works, and add the rest of the files needed for pushing to Langchain repo. Checking this all against the OpenWeatherMap class.

---
+++ libs/community/tests/integration_tests/utilities/test_googleweatherservice.py
@@ -0,0 +1,13 @@
+from langchain_community.utilities.googleweatherservice import GoogleNationalWeatherAPI
+
+
+def test_googlenationalweatherapi() -> None:
+    """Test that the GoogleNationalWeatherAPI returns correct data for Chicago, IL"""
+
+    weather = GoogleNationalWeatherAPI()
+    weather_data = weather._run('Chicago')
+
+    assert weather_data is not None
+    assert "Chicago" in weather_data
+    assert "Fahrenheit" in weather_data
+
