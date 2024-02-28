dbdc50fd47de35745e8f6439ed6b36b1d4d3a8f5
Erick Friis <erick@langchain.dev>
2024-02-28 13:48:59 -0800

    format

--- libs/community/langchain_community/vectorstores/mongodb_atlas.py
+++ libs/community/langchain_community/vectorstores/mongodb_atlas.py
@@ -16,10 +16,10 @@ from typing import (
 )
 
 import numpy as np
+from langchain_core._api.deprecation import deprecated
 from langchain_core.documents import Document
 from langchain_core.embeddings import Embeddings
 from langchain_core.vectorstores import VectorStore
-from langchain_core._api.deprecation import deprecated
 
 from langchain_community.vectorstores.utils import maximal_marginal_relevance
 
