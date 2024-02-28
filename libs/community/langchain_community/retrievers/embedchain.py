f1639040fb066775eade010140ab5a4aa2203ad7
KKUGLER <kai.kugler@mercedes-benz.com>
2024-02-28 11:40:10 +0100

    community [patch]: Fixing embedchain document mapping

--- libs/community/langchain_community/retrievers/embedchain.py
+++ libs/community/langchain_community/retrievers/embedchain.py
@@ -65,7 +65,7 @@ class EmbedchainRetriever(BaseRetriever):
             docs.append(
                 Document(
                     page_content=r["context"],
-                    metadata={"source": r["source"], "document_id": r["document_id"]},
+                    metadata={"score": r["metadata"]["score"], "document_id": r["metadata"]["doc_id"]},
                 )
             )
         return docs
