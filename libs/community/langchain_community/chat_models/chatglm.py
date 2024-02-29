c2061763d2134ec1a45b2a708660b1d66b004602
hulitaitai <146365078+hulitaitai@users.noreply.github.com>
2024-02-29 22:40:02 +0800

    Update chatglm.py

--- libs/community/langchain_community/chat_models/chatglm.py
+++ libs/community/langchain_community/chat_models/chatglm.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
-from typing import Any, List, Tuple
-
+from typing import Any, List, Tuple, Optional
+import logging
+import requests
+from langchain_core.callbacks import CallbackManagerForLLMRun
 from langchain_core.language_models.chat_models import BaseChatModel
 from langchain_core.messages import (
     AIMessage,
@@ -11,11 +13,12 @@ from langchain_core.messages import (
     SystemMessage,
 )
 from langchain_core.outputs import ChatGeneration, ChatResult
+from langchain_core.pydantic_v1 import BaseModel
+from langchain_community.llms.utils import enforce_stop_tokens
 
-from langchain_community.llms.chatglm import ChatGLM
+logger = logging.getLogger(__name__)
 
-
-class ChatGLM_ChatModel(BaseChatModel, ChatGLM):
+class ChatGLM_ChatModel(BaseChatModel, BaseModel):
 
     """ChatGLM LLM ChatModel.
     Example:
@@ -24,6 +27,20 @@ class ChatGLM_ChatModel(BaseChatModel, ChatGLM):
             from langchain_community.chat_models.chatglm import ChatGLM_ChatModel
             llm = ChatGLM_ChatModel(temperature=0.9, top_p=0.9)
     """
+    streaming: bool = False
+    """Whether to stream the results."""
+    endpoint_url: str = "http://127.0.0.1:8000"
+    """Endpoint URL to use."""
+    max_token: int = 20000
+    """Max token allowed to pass to the model."""
+    temperature: float = 0.1
+    """LLM model temperature from 0 to 10."""
+    history: List[List] = []
+    """History of the conversation"""
+    top_p: float = 0.7
+    """Top P for nucleus sampling from 0 to 1"""
+    with_history: bool = False
+    """Whether to use history or not"""
 
     @property
     def _llm_type(self) -> str:
@@ -31,32 +48,93 @@ class ChatGLM_ChatModel(BaseChatModel, ChatGLM):
         return "ChatGLM"
 
     def _convert_messages(self, messages: List[BaseMessage]) -> Tuple[List, str]:
+        """Convert the list of messages into a history and a prompt to feed the ChatGLM."""
         if len(messages) == 0:
             raise ValueError(f"Got no message in {messages}")
         elif len(messages) == 1:
-            history: List[List] = []
+            self.history = []
             prompt: str = str(messages[0].content)
         else:
-            history = []
+            self.history = []
             prompt = str(messages[-1].content)
             for index, message in enumerate(messages):
                 if index == len(messages) - 1:
                     break
                 if isinstance(message, SystemMessage):
-                    history.append([message.content, "知道了。"])
+                    self.history.append([message.content, "知道了。"])
                 elif isinstance(message, HumanMessage):
-                    history.append([message.content, ""])
+                    self.history.append([message.content, ""])
                 elif isinstance(message, AIMessage):
-                    history.append(["", message.content])
+                    self.history.append(["", message.content])
                 elif isinstance(message, FunctionMessage):
-                    history.append([message.content, "知道了。"])
+                    self.history.append([message.content, "知道了。"])
                 else:
                     raise ValueError(f"Got unknown type {message}")
-        return history, prompt
+        return self.history, prompt
+
+    def _call(
+        self,
+        prompt: str,
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+        **kwargs: Any,
+    ) -> str:
+        """Call the ChatGLM to generalize the response."""
+
+        headers = {"Content-Type": "application/json"}
+        payload = {
+            "prompt": prompt,
+            "temperature": self.temperature,
+            "history": self.history,
+            "max_length": self.max_token,
+            "top_p": self.top_p,
+            "endpoint_url": self.endpoint_url,
+        }
+
+        logger.debug(f"ChatGLM payload: {payload}")
+        try:
+            response = requests.post(self.endpoint_url, headers=headers, json=payload)
+        except requests.exceptions.RequestException as e:
+            raise ValueError(f"Error raised by inference endpoint: {e}")
+        logger.debug(f"ChatGLM response: {response}")
+
+        if response.status_code != 200:
+            raise ValueError(f"Failed with response: {response}")
+
+        try:
+            parsed_response = response.json()
+
+            # Check if response content does exists
+            if isinstance(parsed_response, dict):
+                content_keys = "response"
+                if content_keys in parsed_response:
+                    text = parsed_response[content_keys]
+                else:
+                    raise ValueError(f"No content in response : {parsed_response}")
+            else:
+                raise ValueError(f"Unexpected response type: {parsed_response}")
+
+        except requests.exceptions.JSONDecodeError as e:
+            raise ValueError(
+                f"Error raised during decoding response from inference endpoint: {e}."
+                f"\nResponse: {response.text}"
+            )
+
+        if stop is not None:
+            text = enforce_stop_tokens(text, stop)
+
+        if self.with_history:
+            self.history = parsed_response["history"]
+        else:
+            self.history = []
+
+        return text
 
     def _generate(
         self,
         messages: List[BaseMessage],
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
         **kwargs: Any,
     ) -> ChatResult:
         history, prompt = self._convert_messages(messages)
