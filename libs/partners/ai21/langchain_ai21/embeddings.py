9b1799d26db41b9d68d2ec27b9b7367a39fcb11f
Asaf Gardin <asafg@ai21.com>
2024-02-29 13:18:26 +0200

    feat: Added chunking to embedding

--- libs/partners/ai21/langchain_ai21/embeddings.py
+++ libs/partners/ai21/langchain_ai21/embeddings.py
@@ -20,22 +20,46 @@ class AI21Embeddings(Embeddings, AI21Base):
             query_result = embeddings.embed_query("Hello embeddings world!")
     """
 
-    def embed_documents(self, texts: List[str], **kwargs: Any) -> List[List[float]]:
+    chunk_size: int = 1000
+    """Maximum number of texts to embed in each batch"""
+
+    def embed_documents(
+        self, texts: List[str], chunk_size: int = 0, **kwargs: Any
+    ) -> List[List[float]]:
         """Embed search docs."""
-        response = self.client.embed.create(
-            texts=texts,
-            type=EmbedType.SEGMENT,
-            **kwargs,
+        return self._send_embeddings(
+            texts=texts, chunk_size=chunk_size, embed_type=EmbedType.SEGMENT, **kwargs
         )
 
-        return [result.embedding for result in response.results]
-
-    def embed_query(self, text: str, **kwargs: Any) -> List[float]:
+    def embed_query(self, text: str, chunk_size: int = 0, **kwargs: Any) -> List[float]:
         """Embed query text."""
-        response = self.client.embed.create(
-            texts=[text],
-            type=EmbedType.QUERY,
-            **kwargs,
-        )
+        return self._send_embeddings(
+            texts=[text], chunk_size=chunk_size, embed_type=EmbedType.QUERY, **kwargs
+        )[0]
 
-        return [result.embedding for result in response.results][0]
+    def _send_embeddings(
+        self, texts: List[str], chunk_size: int, embed_type: EmbedType, **kwargs: Any
+    ) -> List[List[float]]:
+        chunks = self._get_len_safe_embeddings(texts, chunk_size)
+        responses = [
+            self.client.embed.create(
+                texts=chunk,
+                type=embed_type,
+                **kwargs,
+            )
+            for chunk in chunks
+        ]
+
+        return [
+            result.embedding for response in responses for result in response.results
+        ]
+
+    def _get_len_safe_embeddings(
+        self, texts: List[str], chunk_size: int
+    ) -> List[List[str]]:
+        result = []
+
+        for i in range(0, len(texts), chunk_size):
+            result.append(texts[i : i + chunk_size])
+
+        return result
