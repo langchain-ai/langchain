588ff5e7e98f349717cf5f63443747dab67bd5f6
Christophe Bornet <cbornet@hotmail.com>
2024-03-01 16:28:38 +0100

    Move document loader interfaces to core

--- libs/core/langchain_core/document_loaders/__init__.py
+++ libs/core/langchain_core/document_loaders/__init__.py
@@ -1,6 +1,9 @@
+from langchain_core.document_loaders.base import BaseBlobParser, BaseLoader
 from langchain_core.document_loaders.blob_loaders import Blob, BlobLoader, PathLike
 
 __all__ = [
+    "BaseBlobParser",
+    "BaseLoader",
     "Blob",
     "BlobLoader",
     "PathLike",
