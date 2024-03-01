b831dd825869bf546006e91a501201e91184b6e3
Erick Friis <erick@langchain.dev>
2024-03-01 10:50:13 -0800

    cr

--- templates/rag-lancedb/rag_lancedb/chain.py
+++ templates/rag-lancedb/rag_lancedb/chain.py
@@ -1,10 +1,9 @@
-from langchain_community.chat_models import ChatOpenAI
-from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores import LanceDB
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.pydantic_v1 import BaseModel
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
 # Example for document loading (from url), splitting, and creating vectostore
 
