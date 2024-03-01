519c752f987d238a2aa26bf1014fd741a373689a
Erick Friis <erick@langchain.dev>
2024-03-01 13:53:12 -0800

    remove main

--- templates/gemini-functions-agent/main.py
+++
@@ -1,5 +0,0 @@
-from gemini_functions_agent import agent_executor # from current folder 
-# Issue Not solved 
-if __name__ == "__main__":
-    question = "who won the womens world cup in 2023?"
-    print(agent_executor.invoke({"input": question, "chat_history": []}))  # noqa: T201 
