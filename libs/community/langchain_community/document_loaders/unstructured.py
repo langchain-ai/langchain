6ce52e2bd2870c371c20afdeefee99472d5add2d
mmajewsk <maciej.w.majewski@gmail.com>
2024-03-02 11:45:16 +0100

    fix type incompatibility in unstructured

--- libs/community/langchain_community/document_loaders/unstructured.py
+++ libs/community/langchain_community/document_loaders/unstructured.py
@@ -195,10 +195,13 @@ def get_elements_from_api(
     **unstructured_kwargs: Any,
 ) -> List:
     """Retrieve a list of elements from the `Unstructured API`."""
-    if isinstance(file, collections.abc.Sequence) or isinstance(file_path, list):
+    if isinstance(file, collections.abc.Sequence) or (
+        is_list := isinstance(file_path, list)
+    ):
         from unstructured.partition.api import partition_multiple_via_api
 
-        file_path = [str(path) for path in file_path]
+        if is_list:
+            file_path = [str(path) for path in file_path]
 
         _doc_elements = partition_multiple_via_api(
             filenames=file_path,
