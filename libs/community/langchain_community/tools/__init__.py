57e3d8186f65b733ca57d8c500d98da65789c850
Scott Nath <github@scottnath.com>
2024-03-03 17:30:05 -0500

    community: Add you.com tool, add async to retriever, add async testing, add You tool doc (#18032)
    - **Description:** finishes adding the you.com functionality including:
        - add async functions to utility and retriever
        - add the You.com Tool
        - add async testing for utility, retriever, and tool
        - add a tool integration notebook page
    - **Dependencies:** any dependencies required for this change
    - **Twitter handle:** @scottnath

--- libs/community/langchain_community/tools/__init__.py
+++ libs/community/langchain_community/tools/__init__.py
@@ -782,6 +782,12 @@ def _import_yahoo_finance_news() -> Any:
     return YahooFinanceNewsTool
 
 
+def _import_you_tool() -> Any:
+    from langchain_community.tools.you.tool import YouSearchTool
+
+    return YouSearchTool
+
+
 def _import_youtube_search() -> Any:
     from langchain_community.tools.youtube.search import YouTubeSearchTool
 
@@ -1055,6 +1061,8 @@ def __getattr__(name: str) -> Any:
         return _import_wolfram_alpha_tool()
     elif name == "YahooFinanceNewsTool":
         return _import_yahoo_finance_news()
+    elif name == "YouSearchTool":
+        return _import_you_tool()
     elif name == "YouTubeSearchTool":
         return _import_youtube_search()
     elif name == "ZapierNLAListActions":
@@ -1192,6 +1200,7 @@ __all__ = [
     "WolframAlphaQueryRun",
     "WriteFileTool",
     "YahooFinanceNewsTool",
+    "YouSearchTool",
     "YouTubeSearchTool",
     "ZapierNLAListActions",
     "ZapierNLARunAction",
