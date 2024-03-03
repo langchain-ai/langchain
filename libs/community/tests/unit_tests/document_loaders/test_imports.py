8a46b1e6c3cbcf6efc1a93d250e4457b69995d93
Fabrice Pont <fa.pont@gmail.com>
2024-03-03 19:28:09 +0100

    fix: batch loader import test

--- libs/community/tests/unit_tests/document_loaders/test_imports.py
+++ libs/community/tests/unit_tests/document_loaders/test_imports.py
@@ -29,6 +29,7 @@ EXPECTED_ALL = [
     "AzureBlobStorageContainerLoader",
     "AzureBlobStorageFileLoader",
     "BSHTMLLoader",
+    "BatchLoader",
     "BibtexLoader",
     "BigQueryLoader",
     "BiliBiliLoader",
