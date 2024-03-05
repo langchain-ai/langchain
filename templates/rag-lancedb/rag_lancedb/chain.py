1f7e25cd1db95d571fe9c687a56ae8e9e10c0565
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2024-03-05 08:26:16 +0530

    templates: fix rag-lancedb template (#18551)

--- templates/rag-lancedb/rag_lancedb/chain.py
+++ templates/rag-lancedb/rag_lancedb/chain.py
@@ -19,10 +19,8 @@ text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
 all_splits = text_splitter.split_documents(data)
 
 # Add to vectorDB
-vectorstore = LanceDB.from_documents(documents=all_splits, 
-                                    collection_name="rag-chroma",
-                                    embedding=OpenAIEmbeddings(),
-                                    )
+vectorstore = LanceDB.from_documents(documents=all_splits,
+                             embedding=OpenAIEmbeddings())
 retriever = vectorstore.as_retriever()
 """
 
