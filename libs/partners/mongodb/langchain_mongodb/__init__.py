19270afabd26fe379d348ea9b37b573b1c5b569b
Jib <jib.adegunloye@mongodb.com>
2024-03-05 13:53:02 -0500

    mongodb[patch]: Migrate MongoDBChatMessageHistory (#18590)
    ## **Description** 
    Migrate the `MongoDBChatMessageHistory` to the managed
    `langchain-mongodb` partner-package
    ## **Dependencies**
    None
    ## **Twitter handle**
    @mongodb
    
    ## **tests and docs**
    - [x] Migrate existing integration test
    - [x ]~ Convert existing integration test to a unit test~ Creation is
    out of scope for this ticket
    - [x ] ~Considering delaying work until #17470 merges to leverage the
    `MockCollection` object. ~
    - [x] **Lint and test**: Run `make format`, `make lint` and `make test`
    from the root of the package(s) you've modified. See contribution
    guidelines for more: https://python.langchain.com/docs/contributing/
    
    ---------
    
    Co-authored-by: Erick Friis <erick@langchain.dev>

--- libs/partners/mongodb/langchain_mongodb/__init__.py
+++ libs/partners/mongodb/langchain_mongodb/__init__.py
@@ -1,7 +1,9 @@
+from langchain_mongodb.chat_message_histories import MongoDBChatMessageHistory
 from langchain_mongodb.vectorstores import (
     MongoDBAtlasVectorSearch,
 )
 
 __all__ = [
     "MongoDBAtlasVectorSearch",
+    "MongoDBChatMessageHistory",
 ]
--- libs/partners/mongodb/langchain_mongodb/chat_message_histories.py
+++ libs/partners/mongodb/langchain_mongodb/chat_message_histories.py
@@ -2,13 +2,13 @@ import json
 import logging
 from typing import List
 
-from langchain_core._api.deprecation import deprecated
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.messages import (
     BaseMessage,
     message_to_dict,
     messages_from_dict,
 )
+from pymongo import MongoClient, errors
 
 logger = logging.getLogger(__name__)
 
@@ -16,11 +16,6 @@ DEFAULT_DBNAME = "chat_history"
 DEFAULT_COLLECTION_NAME = "message_store"
 
 
-@deprecated(
-    since="0.0.25",
-    removal="0.2.0",
-    alternative_import="langchain_mongodb.MongoDBChatMessageHistory",
-)
 class MongoDBChatMessageHistory(BaseChatMessageHistory):
     """Chat message history that stores history in MongoDB.
 
@@ -39,8 +34,6 @@ class MongoDBChatMessageHistory(BaseChatMessageHistory):
         database_name: str = DEFAULT_DBNAME,
         collection_name: str = DEFAULT_COLLECTION_NAME,
     ):
-        from pymongo import MongoClient, errors
-
         self.connection_string = connection_string
         self.session_id = session_id
         self.database_name = database_name
@@ -58,8 +51,6 @@ class MongoDBChatMessageHistory(BaseChatMessageHistory):
     @property
     def messages(self) -> List[BaseMessage]:  # type: ignore
         """Retrieve the messages from MongoDB"""
-        from pymongo import errors
-
         try:
             cursor = self.collection.find({"SessionId": self.session_id})
         except errors.OperationFailure as error:
@@ -75,8 +66,6 @@ class MongoDBChatMessageHistory(BaseChatMessageHistory):
 
     def add_message(self, message: BaseMessage) -> None:
         """Append the message to the record in MongoDB"""
-        from pymongo import errors
-
         try:
             self.collection.insert_one(
                 {
@@ -89,8 +78,6 @@ class MongoDBChatMessageHistory(BaseChatMessageHistory):
 
     def clear(self) -> None:
         """Clear session memory from MongoDB"""
-        from pymongo import errors
-
         try:
             self.collection.delete_many({"SessionId": self.session_id})
         except errors.WriteError as err:
