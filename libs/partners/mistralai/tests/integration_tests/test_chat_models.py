6b7ffb96777158c0bea6ea1f34c5f96cfb2c7c49
Pierre Véron <pierre.veron@outlook.fr>
2024-03-05 22:11:17 +0100

    feat: add tests to assert model_name presence in llm_output

--- libs/partners/mistralai/tests/integration_tests/test_chat_models.py
+++ libs/partners/mistralai/tests/integration_tests/test_chat_models.py
@@ -1,4 +1,6 @@
 """Test ChatMistral chat model."""
+from langchain_core.messages import HumanMessage
+
 from langchain_mistralai.chat_models import ChatMistralAI
 
 
@@ -61,3 +63,22 @@ def test_invoke() -> None:
 
     result = llm.invoke("I'm Pickle Rick", config=dict(tags=["foo"]))
     assert isinstance(result.content, str)
+
+
+def test_chat_mistralai_llm_output_contains_model_name() -> None:
+    """Test llm_output contains model_name."""
+    chat = ChatMistralAI(max_tokens=10)
+    message = HumanMessage(content="Hello")
+    llm_result = chat.generate([[message]])
+    assert llm_result.llm_output is not None
+    assert llm_result.llm_output["model_name"] == chat.model
+
+
+def test_chat_mistralai_streaming_llm_output_contains_model_name() -> None:
+    """Test llm_output contains model_name."""
+    chat = ChatMistralAI(max_tokens=10, streaming=True)
+    message = HumanMessage(content="Hello")
+    llm_result = chat.generate([[message]])
+    assert llm_result.llm_output is not None
+    assert llm_result.llm_output["model_name"] == chat.model
+
