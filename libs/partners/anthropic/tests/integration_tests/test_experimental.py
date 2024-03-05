494a9f110c531678dbbc3ba560b2c3c44a0a1c0b
Erick Friis <erick@langchain.dev>
2024-03-05 14:19:40 -0800

    anthropic[patch]: handle lists in function calling (#18609)

--- libs/partners/anthropic/tests/integration_tests/test_experimental.py
+++ libs/partners/anthropic/tests/integration_tests/test_experimental.py
@@ -1,9 +1,11 @@
 """Test ChatAnthropic chat model."""
 
 import json
+from enum import Enum
+from typing import List, Optional
 
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.pydantic_v1 import BaseModel
+from langchain_core.pydantic_v1 import BaseModel, Field
 
 from langchain_anthropic.experimental import ChatAnthropicTools
 
@@ -129,3 +131,49 @@ def test_with_structured_output() -> None:
     assert isinstance(result, Person)
     assert result.name == "Erick"
     assert result.age == 27
+
+
+def test_anthropic_complex_structured_output() -> None:
+    class ToneEnum(str, Enum):
+        positive = "positive"
+        negative = "negative"
+
+    class Email(BaseModel):
+        """Relevant information about an email."""
+
+        sender: Optional[str] = Field(
+            None, description="The sender's name, if available"
+        )
+        sender_phone_number: Optional[str] = Field(
+            None, description="The sender's phone number, if available"
+        )
+        sender_address: Optional[str] = Field(
+            None, description="The sender's address, if available"
+        )
+        action_items: List[str] = Field(
+            ..., description="A list of action items requested by the email"
+        )
+        topic: str = Field(
+            ..., description="High level description of what the email is about"
+        )
+        tone: ToneEnum = Field(..., description="The tone of the email.")
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "human",
+                "What can you tell me about the following email? Make sure to answer in the correct format: {email}",  # noqa: E501
+            ),
+        ]
+    )
+
+    llm = ChatAnthropicTools(temperature=0, model_name="claude-3-sonnet-20240229")
+
+    extraction_chain = prompt | llm.with_structured_output(Email)
+
+    response = extraction_chain.invoke(
+        {
+            "email": "From: Erick. The email is about the new project. The tone is positive. The action items are to send the report and to schedule a meeting."  # noqa: E501
+        }
+    )  # noqa: E501
+    assert isinstance(response, Email)
