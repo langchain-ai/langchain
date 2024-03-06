24663280e0070739b13ea00b282d5e93dd0b0155
barneyjm <jamesbarney71@gmail.com>
2024-03-05 20:19:16 -0500

    safe access

--- libs/community/langchain_community/llms/bedrock.py
+++ libs/community/langchain_community/llms/bedrock.py
@@ -88,7 +88,7 @@ def _stream_response_to_generation_chunk(
     if not stream_response["delta"]:
         return GenerationChunk(text="")
     return GenerationChunk(
-        text=stream_response.get("delta").get("text"),
+        text=stream_response["delta"]["text"],
         generation_info=dict(
             finish_reason=stream_response.get("stop_reason", None),
         ),
