ba6c179752c18d0344d5de5f42adbba7bf2d7699
Douglas Trajano <douglas.trajano@outlook.com>
2024-03-05 22:28:15 -0300

    Add backwards compatibility with default type as str

--- libs/core/langchain_core/prompts/loading.py
+++ libs/core/langchain_core/prompts/loading.py
@@ -91,6 +91,8 @@ def _load_output_parser(config: dict) -> dict:
         output_parser_type = _config.pop("_type")
         if output_parser_type in output_parsers_map:
             output_parser = output_parsers_map[output_parser_type](**_config)
+        elif output_parser_type == "default":
+            output_parser = StrOutputParser(**_config)
         else:
             raise ValueError(f"Unsupported output parser {output_parser_type}")
         config["output_parser"] = output_parser
