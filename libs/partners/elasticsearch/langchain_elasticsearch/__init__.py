dce96738502063960acd1c2a745dda96c0e1bb5e
Max Jakob <max.jakob@gmail.com>
2024-03-08 04:38:31 +0100

    elasticsearch[patch]: add top-level import, remove obsolete dependency (#18644)
    Make `ElasticsearchRetriever` available as top-level import.
    
    The `langchain` package depends on `langchain-community` so we do not
    need to depend on it explicitly.

--- libs/partners/elasticsearch/langchain_elasticsearch/__init__.py
+++ libs/partners/elasticsearch/langchain_elasticsearch/__init__.py
@@ -1,5 +1,6 @@
 from langchain_elasticsearch.chat_history import ElasticsearchChatMessageHistory
 from langchain_elasticsearch.embeddings import ElasticsearchEmbeddings
+from langchain_elasticsearch.retrievers import ElasticsearchRetriever
 from langchain_elasticsearch.vectorstores import (
     ApproxRetrievalStrategy,
     ElasticsearchStore,
@@ -11,6 +12,7 @@ __all__ = [
     "ApproxRetrievalStrategy",
     "ElasticsearchChatMessageHistory",
     "ElasticsearchEmbeddings",
+    "ElasticsearchRetriever",
     "ElasticsearchStore",
     "ExactRetrievalStrategy",
     "SparseRetrievalStrategy",
