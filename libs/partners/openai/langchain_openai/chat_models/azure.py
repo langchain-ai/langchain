edca1b8df05cfbf254f991a4fb9ca8d0c1becb06
Dmitry Kankalovich <dmitry.kankalovich@gmail.com>
2024-03-09 01:52:55 +0100

    openai[patch]: Proper example for AzureOpenAI usage in error message (#17798)
    # Proper example for AzureOpenAI usage in error message
    
    The original error message is wrong in part of a usage example it gives.
    Corrected to the right one.
    
    Co-authored-by: Dzmitry Kankalovich <dzmitry_kankalovich@epam.com>
    Co-authored-by: Bagatur <baskaryan@gmail.com>

--- libs/partners/openai/langchain_openai/chat_models/azure.py
+++ libs/partners/openai/langchain_openai/chat_models/azure.py
@@ -164,7 +164,7 @@ class AzureChatOpenAI(ChatOpenAI):
                     "`azure_endpoint` instead of `base_url`.\n\n"
                     "For example, you could specify:\n\n"
                     'azure_endpoint="https://xxx.openai.azure.com/", '
-                    'deployment_name="my-deployment"\n\n'
+                    'azure_deployment="my-deployment"\n\n'
                     "Or you can equivalently specify:\n\n"
                     'base_url="https://xxx.openai.azure.com/openai/deployments/my-deployment"'  # noqa: E501
                 )
