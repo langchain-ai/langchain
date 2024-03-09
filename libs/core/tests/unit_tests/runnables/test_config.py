5f0121ebaaa0ae30c34655edee840414835dfe28
Erick Friis <erick@langchain.dev>
2024-03-08 17:17:09 -0800

    core[patch]: fix arbitrary config keys

--- libs/core/tests/unit_tests/runnables/test_config.py
+++ libs/core/tests/unit_tests/runnables/test_config.py
@@ -1,6 +1,7 @@
 from langchain_core.callbacks.manager import CallbackManager
 from langchain_core.callbacks.stdout import StdOutCallbackHandler
 from langchain_core.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain_core.runnables import RunnablePassthrough
 from langchain_core.runnables.config import RunnableConfig, merge_configs
 from langchain_core.tracers.stdout import ConsoleCallbackHandler
 
@@ -32,3 +33,11 @@ def test_merge_config_callbacks() -> None:
     assert len(merged) == 2
     assert isinstance(merged[0], ConsoleCallbackHandler)
     assert isinstance(merged[1], StreamingStdOutCallbackHandler)
+
+
+def test_config_arbitrary_keys() -> None:
+    base = RunnablePassthrough()
+    bound = base.with_config(my_custom_key="my custom value")
+    config = bound.config
+
+    assert config.get("my_custom_key") == "my custom value"
