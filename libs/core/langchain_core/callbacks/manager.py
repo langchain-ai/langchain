langchain-ai/langchain

commit 43db4cd20e0e718f368267528706f92bf604bac9

core[major]: On Tool End Observation Casting Fix (#18798)
This PR updates the on_tool_end handlers to return the raw output from the tool instead of casting it to a string. 

This is technically a breaking change, though it's impact is expected to be somewhat minimal. It will fix behavior in `astream_events` as well.

Fixes the following issue #18760 raised by @eyurtsev

---------

Co-authored-by: Eugene Yurtsev <eyurtsev@gmail.com>

libs/core/langchain_core/callbacks/manager.py
@@ -976,14 +976,15 @@ class CallbackManagerForToolRun(ParentRunManager, ToolManagerMixin):
 
     def on_tool_end(
         self,
-        output: str,
+        output: Any,
         **kwargs: Any,
     ) -> None:
         """Run when tool ends running.
 
         Args:
-            output (str): The output of the tool.
+            output (Any): The output of the tool.
         """
+        output = str(output)
         handle_event(
             self.handlers,
             "on_tool_end",
@@ -1038,12 +1039,13 @@ class AsyncCallbackManagerForToolRun(AsyncParentRunManager, ToolManagerMixin):
         )
 
     @shielded
-    async def on_tool_end(self, output: str, **kwargs: Any) -> None:
+    async def on_tool_end(self, output: Any, **kwargs: Any) -> None:
         """Run when tool ends running.
 
         Args:
-            output (str): The output of the tool.
+            output (Any): The output of the tool.
         """
+        output = str(output)
         await ahandle_event(
             self.handlers,
             "on_tool_end",

docs/docs/modules/agents/how_to/streaming.ipynb
@@ -1068,7 +1068,7 @@
     "\n",
     "    def on_tool_end(\n",
     "        self,\n",
-    "        output: str,\n",
+    "        output: Any,\n",
     "        *,\n",
     "        run_id: UUID,\n",
     "        parent_run_id: Optional[UUID] = None,\n",
@@ -1076,7 +1076,7 @@
     "    ) -> Any:\n",
     "        \"\"\"Run when tool ends running.\"\"\"\n",
     "        print(\"Tool end\")\n",
-    "        print(output)\n",
+    "        print(str(output))\n",
     "\n",
     "    async def on_llm_end(\n",
     "        self,\n",

docs/docs/modules/callbacks/index.mdx
@@ -59,7 +59,7 @@ class BaseCallbackHandler:
     ) -> Any:
         """Run when tool starts running."""
 
-    def on_tool_end(self, output: str, **kwargs: Any) -> Any:
+    def on_tool_end(self, output: Any, **kwargs: Any) -> Any:
         """Run when tool ends running."""
 
     def on_tool_error(

libs/community/langchain_community/callbacks/aim_callback.py
@@ -314,8 +314,9 @@ class AimCallbackHandler(BaseMetadataCallbackHandler, BaseCallbackHandler):
 
         self._run.track(aim.Text(input_str), name="on_tool_start", context=resp)
 
-    def on_tool_end(self, output: str, **kwargs: Any) -> None:
+    def on_tool_end(self, output: Any, **kwargs: Any) -> None:
         """Run when tool ends running."""
+        output = str(output)
         aim = import_aim()
         self.step += 1
         self.tool_ends += 1

libs/community/langchain_community/callbacks/argilla_callback.py
@@ -328,7 +328,7 @@ class ArgillaCallbackHandler(BaseCallbackHandler):
 
     def on_tool_end(
         self,
-        output: str,
+        output: Any,
         observation_prefix: Optional[str] = None,
         llm_prefix: Optional[str] = None,
         **kwargs: Any,

libs/community/langchain_community/callbacks/arize_callback.py
@@ -196,7 +196,7 @@ class ArizeCallbackHandler(BaseCallbackHandler):
 
     def on_tool_end(
         self,
-        output: str,
+        output: Any,
         observation_prefix: Optional[str] = None,
         llm_prefix: Optional[str] = None,
         **kwargs: Any,

libs/community/langchain_community/callbacks/arthur_callback.py
@@ -279,7 +279,7 @@ class ArthurCallbackHandler(BaseCallbackHandler):
 
     def on_tool_end(
         self,
-        output: str,
+        output: Any,
         observation_prefix: Optional[str] = None,
         llm_prefix: Optional[str] = None,
         **kwargs: Any,

libs/community/langchain_community/callbacks/clearml_callback.py
@@ -243,8 +243,9 @@ class ClearMLCallbackHandler(BaseMetadataCallbackHandler, BaseCallbackHandler):
         if self.stream_logs:
             self.logger.report_text(resp)
 
-    def on_tool_end(self, output: str, **kwargs: Any) -> None:
+    def on_tool_end(self, output: Any, **kwargs: Any) -> None:
         """Run when tool ends running."""
+        output = str(output)
         self.step += 1
         self.tool_ends += 1
         self.ends += 1

commit 98435a4b33603ea60c99be8f03a153ebe3a480b2

[fix]: CallbackManager Casting Revert. Bug introduced in PR #18798

libs/core/langchain_core/callbacks/manager.py
@@ -984,7 +984,6 @@ class CallbackManagerForToolRun(ParentRunManager, ToolManagerMixin):
         Args:
             output (Any): The output of the tool.
         """
-        output = str(output)
         handle_event(
             self.handlers,
             "on_tool_end",