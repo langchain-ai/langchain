c7e6e85dc2fe5bae04155a270e69d79f7b506797
karthikravi <tkgravikarthik@gmail.com>
2024-03-12 00:49:45 -0400

    Fixed lint errors and add doc string.

--- libs/experimental/langchain_experimental/rephrase_using_translation/__init__.py
+++ libs/experimental/langchain_experimental/rephrase_using_translation/__init__.py
@@ -1,6 +1,4 @@
 from langchain_experimental.rephrase_using_translation.\
-    rephrase_using_translation import (
-    LTORQueryExpansion,
-)
+    rephrase_using_translation import LTORQueryExpansion
 
 __all__ = ["LTORQueryExpansion"]
