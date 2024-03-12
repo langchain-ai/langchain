65c09f1b0034263da41b3b514243645bd6213482
Sanjaypranav V M <sanjaypriya195@gmail.com>
2024-03-12 15:02:42 +0530

    renamed pincone ==> pinecone import

--- libs/community/langchain_community/tools/pinecone/tool.py
+++ libs/community/langchain_community/tools/pinecone/tool.py
@@ -2,21 +2,20 @@
 import os
 from typing import Optional
 
-from langchain_core.callbacks import (
-    AsyncCallbackManagerForToolRun,
-    CallbackManagerForToolRun,
-)
-from langchain_core.pydantic_v1 import Field
-from langchain_core.tools import BaseTool
+from langchain_core.callbacks import (AsyncCallbackManagerForToolRun,
+                                      CallbackManagerForToolRun)
 from langchain_core.pydantic_v1 import BaseModel, Field, root_validator
-from langchain_pinecone import PineconeVectorStore
+from langchain_core.tools import BaseTool
 from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
 
 class BasePineconeTool(BaseModel):
     """Base tool for interacting with a pinecone vector database."""
 
     class _QueryPinconeBaseToolInput(BaseModel):
         query: str = Field(..., description="A natural language question")
+
 
 class PineconeTool(BasePineconeTool, BaseTool):
     """Tool that queries the Pinecone API."""
@@ -30,11 +29,14 @@ class PineconeTool(BasePineconeTool, BaseTool):
 
     vectorstore: PineconeVectorStore = Field(
         default_factory=lambda: PineconeVectorStore.from_existing_index(
-            index_name=os.environ["PINECONE_INDEX"],
-            embedding=OpenAIEmbeddings()
+            index_name=os.environ["PINECONE_INDEX"], embedding=OpenAIEmbeddings()
         ),
-        description="The Pinecone vector store"
+        description="The Pinecone vector store",
     )
+
+    @classmethod
+    def create(cls, **kwargs) -> "PineconeTool":
+        return cls()
 
     def _run(
         self,
