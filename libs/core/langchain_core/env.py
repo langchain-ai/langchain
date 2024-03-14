0f4fe5b55e18a93e58b3a90494d69ffd4b05911d
Eugene Yurtsev <eyurtsev@gmail.com>
2024-03-14 14:22:39 -0400

    x

--- libs/core/langchain_core/env.py
+++ libs/core/langchain_core/env.py
@@ -3,7 +3,7 @@ from functools import lru_cache
 
 
 @lru_cache(maxsize=1)
-def get_runtime_environment() -> dict:
+def get_runtime_environment() -> dict: # type: ignore
     """Get information about the LangChain runtime environment."""
     # Lazy import to avoid circular imports
     from langchain_core import __version__
