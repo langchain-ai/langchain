869b8df7bae79525d62d8ee7eb886d4d4c9a895a
Josha van Houdt <josha.van.houdt@sap.com>
2024-03-14 13:13:30 +0000

    Fix docstring

--- libs/community/langchain_community/tools/sql_database/tool.py
+++ libs/community/langchain_community/tools/sql_database/tool.py
@@ -81,14 +81,14 @@ class ListSQLDatabaseTool(BaseSQLDatabaseTool, BaseTool):
     """Tool for getting tables names."""
 
     name: str = "sql_db_list_tables"
-    description: str = "Input is an empty string, output is a comma separated list of tables in the database."
+    description: str = "Input is an empty string, output is a comma-separated list of tables in the database."
 
     def _run(
         self,
         tool_input: str = "",
         run_manager: Optional[CallbackManagerForToolRun] = None,
     ) -> str:
-        """Get the schema for a specific table."""
+        """Get a comma-separated list of table names."""
         return ", ".join(self.db.get_usable_table_names())
 
 
