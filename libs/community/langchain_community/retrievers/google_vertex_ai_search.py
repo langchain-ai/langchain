f5828c1811c0106e4e120d6ece56d61f5f866859
Holt Skinner <holtskinner@google.com>
2024-03-14 12:03:35 -0500

    Fix lint errors

--- libs/community/langchain_community/retrievers/google_vertex_ai_search.py
+++ libs/community/langchain_community/retrievers/google_vertex_ai_search.py
@@ -6,7 +6,7 @@ from typing import TYPE_CHECKING, Any, Dict, List, Optional, Sequence
 
 from langchain_core.callbacks import CallbackManagerForRetrieverRun
 from langchain_core.documents import Document
-from langchain_core.pydantic_v1 import BaseModel, Extra, Field, root_validator, ValidationError
+from langchain_core.pydantic_v1 import BaseModel, Extra, Field, root_validator
 from langchain_core.retrievers import BaseRetriever
 from langchain_core.utils import get_from_dict_or_env
 
@@ -271,7 +271,7 @@ class GoogleVertexAISearchRetriever(BaseRetriever, _BaseGoogleVertexAISearchRetr
             )
 
         if self.search_engine_id:
-            self._serving_config = f"projects/{self.project_id}/locations/{self.location_id}/collections/default_collection/engines/{self.search_engine_id}/servingConfigs/default_config"
+            self._serving_config = f"projects/{self.project_id}/locations/{self.location_id}/collections/default_collection/engines/{self.search_engine_id}/servingConfigs/default_config"  # noqa: E501
         elif self.data_store_id:
             self._serving_config = self._client.serving_config_path(
                 project=self.project_id,
