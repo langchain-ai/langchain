a5bb732464939573c80cabc955a237c66ef91aaf
Erick Friis <erick@langchain.dev>
2024-03-14 17:47:46 -0700

    fix integration test

--- libs/partners/voyageai/tests/integration_tests/test_embeddings.py
+++ libs/partners/voyageai/tests/integration_tests/test_embeddings.py
@@ -18,7 +18,7 @@ def test_langchain_voyageai_embedding_documents() -> None:
 def test_langchain_voyageai_embedding_documents_multiple() -> None:
     """Test voyage embeddings."""
     documents = ["foo bar", "bar foo", "foo"]
-    embedding = VoyageAIEmbeddings(model=MODEL, embed_batch_size=2)
+    embedding = VoyageAIEmbeddings(model=MODEL, batch_size=2)
     output = embedding.embed_documents(documents)
     assert len(output) == 3
     assert len(output[0]) == 1024
@@ -37,7 +37,7 @@ def test_langchain_voyageai_embedding_query() -> None:
 async def test_langchain_voyageai_async_embedding_documents_multiple() -> None:
     """Test voyage embeddings."""
     documents = ["foo bar", "bar foo", "foo"]
-    embedding = VoyageAIEmbeddings(model=MODEL, embed_batch_size=2)
+    embedding = VoyageAIEmbeddings(model=MODEL, batch_size=2)
     output = await embedding.aembed_documents(documents)
     assert len(output) == 3
     assert len(output[0]) == 1024
