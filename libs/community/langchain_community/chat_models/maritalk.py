1337b9b65d599ef4b31cfea53571f5374059e6cd
rodrigo-f-nogueira <rodrigo@maritaca.ai>
2024-03-16 15:23:53 +0000

    fix lint

--- libs/community/langchain_community/chat_models/maritalk.py
+++ libs/community/langchain_community/chat_models/maritalk.py
@@ -6,27 +6,28 @@ from langchain_core.callbacks import CallbackManagerForLLMRun
 from langchain_core.language_models.chat_models import SimpleChatModel
 from langchain_core.messages import AIMessage, BaseMessage, HumanMessage, SystemMessage
 from langchain_core.pydantic_v1 import Field
+from requests import Response
 from requests.exceptions import HTTPError
 
 
 class MaritalkHTTPError(HTTPError):
-    def __init__(self, request_obj):
+    def __init__(self, request_obj: Response) -> None:
         self.request_obj = request_obj
         try:
             response_json = request_obj.json()
             if "detail" in response_json:
-                api_message = response_json["detail"]
+                api_message: str = response_json["detail"]
             elif "message" in response_json:
-                api_message = response_json["message"]
+                api_message: str = response_json["message"]
             else:
-                api_message = response_json
+                api_message: Any = response_json
         except Exception:
-            api_message = request_obj.text
+            api_message: str = request_obj.text
 
         self.message = api_message
         self.status_code = request_obj.status_code
 
-    def __str__(self):
+    def __str__(self) -> str:
         status_code_meaning = HTTPStatus(self.status_code).phrase
         formatted_message = f"HTTP Error: {self.status_code} - {status_code_meaning}"
         formatted_message += f"\nDetail: {self.message}"
