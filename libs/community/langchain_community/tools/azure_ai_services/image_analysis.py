8f5fc98f232ea4775cde3c8e6bded32b75d3f1dd
Christian Galo <cgalo@pm.me>
2024-03-17 02:28:06 -0500

    Refactor Azure Cognitive Services to Azure AI Services in image_analysis.py

--- libs/community/langchain_community/tools/azure_ai_services/image_analysis.py
+++ libs/community/langchain_community/tools/azure_ai_services/image_analysis.py
@@ -8,28 +8,28 @@ from langchain_core.pydantic_v1 import root_validator
 from langchain_core.tools import BaseTool
 from langchain_core.utils import get_from_dict_or_env
 
-from langchain_community.tools.azure_cognitive_services.utils import (
+from langchain_community.tools.azure_ai_services.utils import (
     detect_file_src_type,
 )
 
 logger = logging.getLogger(__name__)
 
 
-class AzureCogsImageAnalysisTool(BaseTool):
-    """Tool that queries the Azure Cognitive Services Image Analysis API.
+class AzureAiServicesImageAnalysisTool(BaseTool):
+    """Tool that queries the Azure AI Services Image Analysis API.
 
     In order to set this up, follow instructions at:
-    https://learn.microsoft.com/en-us/azure/cognitive-services/computer-vision/quickstarts-sdk/image-analysis-client-library-40
+    https://learn.microsoft.com/en-us/azure/ai-services/computer-vision/quickstarts-sdk/image-analysis-client-library-40
     """
 
-    azure_cogs_key: str = ""  #: :meta private:
-    azure_cogs_endpoint: str = ""  #: :meta private:
-    vision_service: Any  #: :meta private:
-    analysis_options: Any  #: :meta private:
+    azure_ai_services_key: str = ""  #: :meta private:
+    azure_ai_services_endpoint: str = ""  #: :meta private:
+    image_analysis_client: Any  #: :meta private:
+    visual_features: Any  #: :meta private:
 
-    name: str = "azure_cognitive_services_image_analysis"
+    name: str = "azure_ai_services_image_analysis"
     description: str = (
-        "A wrapper around Azure Cognitive Services Image Analysis. "
+        "A wrapper around Azure AI Services Image Analysis. "
         "Useful for when you need to analyze images. "
         "Input should be a url to an image."
     )
@@ -37,76 +37,80 @@ class AzureCogsImageAnalysisTool(BaseTool):
     @root_validator(pre=True)
     def validate_environment(cls, values: Dict) -> Dict:
         """Validate that api key and endpoint exists in environment."""
-        azure_cogs_key = get_from_dict_or_env(
-            values, "azure_cogs_key", "AZURE_COGS_KEY"
+        azure_ai_services_key = get_from_dict_or_env(
+            values, "azure_ai_services_key", "AZURE_AI_SERVICES_KEY"
         )
 
-        azure_cogs_endpoint = get_from_dict_or_env(
-            values, "azure_cogs_endpoint", "AZURE_COGS_ENDPOINT"
+        azure_ai_services_endpoint = get_from_dict_or_env(
+            values, "azure_ai_services_endpoint", "AZURE_AI_SERVICES_ENDPOINT"
         )
-
+        
+        """Validate that azure-ai-vision-imageanalysis is installed."""
         try:
-            import azure.ai.vision as sdk
-
-            values["vision_service"] = sdk.VisionServiceOptions(
-                endpoint=azure_cogs_endpoint, key=azure_cogs_key
-            )
-
-            values["analysis_options"] = sdk.ImageAnalysisOptions()
-            values["analysis_options"].features = (
-                sdk.ImageAnalysisFeature.CAPTION
-                | sdk.ImageAnalysisFeature.OBJECTS
-                | sdk.ImageAnalysisFeature.TAGS
-                | sdk.ImageAnalysisFeature.TEXT
-            )
+            from azure.ai.vision.imageanalysis import ImageAnalysisClient
+            from azure.core.credentials import AzureKeyCredential
+            from azure.ai.vision.imageanalysis.models import VisualFeatures
         except ImportError:
             raise ImportError(
-                "azure-ai-vision is not installed. "
-                "Run `pip install azure-ai-vision` to install."
+                "azure-ai-vision-imageanalysis is not installed. "
+                "Run `pip install azure-ai-vision-imageanalysis` to install. "
             )
+        
+        """Validate that the Azure AI Vision Image Analysis client can be initialized."""
+        try:
+            values["image_analysis_client"] = ImageAnalysisClient(
+                endpoint=azure_ai_services_endpoint,
+                credential=AzureKeyCredential(azure_ai_services_key)
+            )
+        except Exception as e:
+            raise RuntimeError(f"Initialization of Azure AI Vision Image Analysis client failed: {e}")
+        
+        values["visual_features"] = [
+            VisualFeatures.TAGS,
+            VisualFeatures.OBJECTS,
+            VisualFeatures.CAPTION,
+            VisualFeatures.READ,
+        ]
 
         return values
 
     def _image_analysis(self, image_path: str) -> Dict:
         try:
-            import azure.ai.vision as sdk
+            from azure.ai.vision.imageanalysis import ImageAnalysisClient
         except ImportError:
             pass
 
+        self.image_analysis_client: ImageAnalysisClient
+
         image_src_type = detect_file_src_type(image_path)
         if image_src_type == "local":
-            vision_source = sdk.VisionSource(filename=image_path)
+            with open(image_path, "rb") as image_file:
+                image_data = image_file.read()
+            result = self.image_analysis_client.analyze(
+                image_data=image_data,
+                visual_features=self.visual_features,
+            )
         elif image_src_type == "remote":
-            vision_source = sdk.VisionSource(url=image_path)
+            result = self.image_analysis_client.analyze_from_url(
+                image_url=image_path,
+                visual_features=self.visual_features,
+            )
         else:
             raise ValueError(f"Invalid image path: {image_path}")
 
-        image_analyzer = sdk.ImageAnalyzer(
-            self.vision_service, vision_source, self.analysis_options
-        )
-        result = image_analyzer.analyze()
-
         res_dict = {}
-        if result.reason == sdk.ImageAnalysisResultReason.ANALYZED:
+        if result:
             if result.caption is not None:
-                res_dict["caption"] = result.caption.content
+                res_dict["caption"] = result.caption.text
 
             if result.objects is not None:
-                res_dict["objects"] = [obj.name for obj in result.objects]
+                res_dict["objects"] = [obj.tags[0].name for obj in result.objects.list]
 
             if result.tags is not None:
-                res_dict["tags"] = [tag.name for tag in result.tags]
+                res_dict["tags"] = [tag.name for tag in result.tags.list]
 
-            if result.text is not None:
-                res_dict["text"] = [line.content for line in result.text.lines]
-
-        else:
-            error_details = sdk.ImageAnalysisErrorDetails.from_result(result)
-            raise RuntimeError(
-                f"Image analysis failed.\n"
-                f"Reason: {error_details.reason}\n"
-                f"Details: {error_details.message}"
-            )
+            if result.read is not None:
+                res_dict["text"] = [line.text for line in result.read.blocks[0].lines]
 
         return res_dict
 
@@ -115,13 +119,8 @@ class AzureCogsImageAnalysisTool(BaseTool):
         if "caption" in image_analysis_result:
             formatted_result.append("Caption: " + image_analysis_result["caption"])
 
-        if (
-            "objects" in image_analysis_result
-            and len(image_analysis_result["objects"]) > 0
-        ):
-            formatted_result.append(
-                "Objects: " + ", ".join(image_analysis_result["objects"])
-            )
+        if "objects" in image_analysis_result and len(image_analysis_result["objects"]) > 0:
+            formatted_result.append("Objects: " + ", ".join(image_analysis_result["objects"]))
 
         if "tags" in image_analysis_result and len(image_analysis_result["tags"]) > 0:
             formatted_result.append("Tags: " + ", ".join(image_analysis_result["tags"]))
@@ -144,4 +143,4 @@ class AzureCogsImageAnalysisTool(BaseTool):
 
             return self._format_image_analysis_result(image_analysis_result)
         except Exception as e:
-            raise RuntimeError(f"Error while running AzureCogsImageAnalysisTool: {e}")
+            raise RuntimeError(f"Error while running AzureAiImageAnalysisTool: {e}")
