cfe57667f40ae509a9ece8e9b4addfc16699fe1a
rodrigo-f-nogueira <rodrigo@maritaca.ai>
2024-03-17 14:56:17 +0000

    fix typo

--- libs/community/langchain_community/chat_models/maritalk.py
+++ libs/community/langchain_community/chat_models/maritalk.py
@@ -1,5 +1,5 @@
 from http import HTTPStatus
-from typing import Any, Dict, List, Optional, Unionm, AsyncIterator
+from typing import Any, Dict, List, Optional, Union, AsyncIterator
 
 import httpx
 
