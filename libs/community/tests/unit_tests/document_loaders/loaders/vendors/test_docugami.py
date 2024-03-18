879175b7e05be49a88365689e303f98815cad3d1
Kenzie Mihardja <kenzie28@cs.washington.edu>
2024-03-18 04:27:04 -0700

    deprecate community docugami loader

--- libs/community/tests/unit_tests/document_loaders/loaders/vendors/test_docugami.py
+++ libs/community/tests/unit_tests/document_loaders/loaders/vendors/test_docugami.py
@@ -3,8 +3,7 @@ from pathlib import Path
 
 import pytest
 
-from langchain_community.document_loaders import DocugamiLoader
-
+from docugami_langchain.document_loaders import DocugamiLoader
 DOCUGAMI_XML_PATH = Path(__file__).parent / "test_data" / "docugami-example.xml"
 
 
