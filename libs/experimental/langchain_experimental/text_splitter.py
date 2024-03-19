14677babe102921d770f630ae2d6417abcd3f4a4
Zihong <zihch@outlook.com>
2024-03-19 19:24:51 +0800

    experimental: update the notebook link of semantic chunk. (#19253)
    update the notebook link of semantic chunk.

--- libs/experimental/langchain_experimental/text_splitter.py
+++ libs/experimental/langchain_experimental/text_splitter.py
@@ -95,7 +95,7 @@ class SemanticChunker(BaseDocumentTransformer):
     """Split the text based on semantic similarity.
 
     Taken from Greg Kamradt's wonderful notebook:
-    https://github.com/FullStackRetrieval-com/RetrievalTutorials/blob/main/5_Levels_Of_Text_Splitting.ipynb
+    https://github.com/FullStackRetrieval-com/RetrievalTutorials/blob/main/tutorials/LevelsOfTextSplitting/5_Levels_Of_Text_Splitting.ipynb
 
     All credits to him.
 
