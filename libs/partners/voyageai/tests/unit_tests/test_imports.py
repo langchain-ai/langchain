4d5c6e3ac150a99de78e216a5179335a3c012fbc
fodizoltan <zoltan@conway.expert>
2024-03-20 10:56:56 +0100

    Code refactoring

--- libs/partners/voyageai/tests/unit_tests/test_imports.py
+++ libs/partners/voyageai/tests/unit_tests/test_imports.py
@@ -1,9 +1,6 @@
 from langchain_voyageai import __all__
 
-EXPECTED_ALL = [
-    "VoyageAIEmbeddings",
-    "VoyageAIRerank"
-]
+EXPECTED_ALL = ["VoyageAIEmbeddings", "VoyageAIRerank"]
 
 
 def test_all_imports() -> None:
