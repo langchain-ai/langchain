a29c7733ca158826805c415a8b3473d151b3b4dd
Alexandra Belousov <sashabelousovrh@Alexandras-MacBook-Pro.local>
2024-03-24 19:44:46 +0200

    docs, community: update runhouse integration

--- libs/community/tests/unit_tests/test_imports.py
+++ libs/community/tests/unit_tests/test_imports.py
@@ -9,6 +9,8 @@ def test_importable_all() -> None:
         if relative_path.endswith(".typed"):
             continue
         module_name = relative_path.split(".")[0]
+        if module_name == "runhouse":
+            continue
 
         module = importlib.import_module("langchain_community." + module_name)
         all_ = getattr(module, "__all__", [])
