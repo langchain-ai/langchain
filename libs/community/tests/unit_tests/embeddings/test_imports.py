0424bb9e3411632c7568130b8c14dd3005e38171
Mikelarg <murtazkorpaz@gmail.com>
2024-03-26 02:08:37 +0300

    community[minor]: Added GigaChat Embeddings support + updated previous GigaChat integration (#19516)
    - **Description:** Added integration with
    [GigaChat](https://developers.sber.ru/portal/products/gigachat)
    embeddings. Also added support for extra fields in GigaChat LLM and
    fixed docs.

--- libs/community/tests/unit_tests/embeddings/test_imports.py
+++ libs/community/tests/unit_tests/embeddings/test_imports.py
@@ -48,6 +48,7 @@ EXPECTED_ALL = [
     "SpacyEmbeddings",
     "NLPCloudEmbeddings",
     "GPT4AllEmbeddings",
+    "GigaChatEmbeddings",
     "XinferenceEmbeddings",
     "LocalAIEmbeddings",
     "AwaEmbeddings",
