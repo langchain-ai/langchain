2d91752b15382eb782268f56de486b704ca5641a
billytrend-cohere <144115527+billytrend-cohere@users.noreply.github.com>
2024-03-25 11:03:59 -0500

    Update __init__.py

--- libs/partners/cohere/langchain_cohere/__init__.py
+++ libs/partners/cohere/langchain_cohere/__init__.py
@@ -1,10 +1,8 @@
 from langchain_cohere.chat_models import ChatCohere
 from langchain_cohere.embeddings import CohereEmbeddings
-from langchain_cohere.llms import BaseCohere
 from langchain_cohere.rag_retrievers import CohereRagRetriever
 
 __all__ = [
-    "BaseCohere",
     "ChatCohere",
     "CohereVectorStore",
     "CohereEmbeddings",
