ada3f8cdad75c6b72be0ef45721094b9906cb398
BeatrixCohere <128378696+BeatrixCohere@users.noreply.github.com>
2024-03-26 07:52:42 +0000

    Upadte

--- libs/partners/cohere/langchain_cohere/__init__.py
+++ libs/partners/cohere/langchain_cohere/__init__.py
@@ -1,6 +1,7 @@
 from langchain_cohere.chat_models import ChatCohere
 from langchain_cohere.embeddings import CohereEmbeddings
 from langchain_cohere.rag_retrievers import CohereRagRetriever
+from langchain_cohere.rerank import CohereRerank
 from langchain_cohere.cohere_agent import create_cohere_tools_agent
 
 __all__ = [
@@ -8,5 +9,6 @@ __all__ = [
     "CohereVectorStore",
     "CohereEmbeddings",
     "CohereRagRetriever",
+    "CohereRerank",
     "create_cohere_tools_agent",
 ]
