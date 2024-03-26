34e7a344b8c2fc85528d80092ad638b9b9545a40
Christophe Bornet <cbornet@hotmail.com>
2024-03-26 15:06:43 +0100

    core: Add async methods to BaseExampleSelector and SemanticSimilarityExampleSelector (#19399)
    Few-Shot prompt template may use a `SemanticSimilarityExampleSelector`
    that in turn uses a `VectorStore` that does I/O operations.
    So to work correctly on the event loop, we need:
    * async methods for the `VectorStore` (OK)
    * async methods for the `SemanticSimilarityExampleSelector` (this PR)
    * async methods for `BasePromptTemplate` and `BaseChatPromptTemplate`
    (future work)

--- libs/core/langchain_core/example_selectors/semantic_similarity.py
+++ libs/core/langchain_core/example_selectors/semantic_similarity.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Any, Dict, List, Optional, Type
 
+from langchain_core.documents import Document
 from langchain_core.example_selectors.base import BaseExampleSelector
 from langchain_core.pydantic_v1 import BaseModel, Extra
 from langchain_core.vectorstores import VectorStore
@@ -37,34 +38,59 @@ class SemanticSimilarityExampleSelector(BaseExampleSelector, BaseModel):
         extra = Extra.forbid
         arbitrary_types_allowed = True
 
+    @staticmethod
+    def _example_to_text(
+        example: Dict[str, str], input_keys: Optional[List[str]]
+    ) -> str:
+        if input_keys:
+            return " ".join(sorted_values({key: example[key] for key in input_keys}))
+        else:
+            return " ".join(sorted_values(example))
+
+    def _documents_to_examples(self, documents: List[Document]) -> List[dict]:
+        # Get the examples from the metadata.
+        # This assumes that examples are stored in metadata.
+        examples = [dict(e.metadata) for e in documents]
+        # If example keys are provided, filter examples to those keys.
+        if self.example_keys:
+            examples = [{k: eg[k] for k in self.example_keys} for eg in examples]
+        return examples
+
     def add_example(self, example: Dict[str, str]) -> str:
         """Add new example to vectorstore."""
-        if self.input_keys:
-            string_example = " ".join(
-                sorted_values({key: example[key] for key in self.input_keys})
-            )
-        else:
-            string_example = " ".join(sorted_values(example))
-        ids = self.vectorstore.add_texts([string_example], metadatas=[example])
+        ids = self.vectorstore.add_texts(
+            [self._example_to_text(example, self.input_keys)], metadatas=[example]
+        )
+        return ids[0]
+
+    async def aadd_example(self, example: Dict[str, str]) -> str:
+        """Add new example to vectorstore."""
+        ids = await self.vectorstore.aadd_texts(
+            [self._example_to_text(example, self.input_keys)], metadatas=[example]
+        )
         return ids[0]
 
     def select_examples(self, input_variables: Dict[str, str]) -> List[dict]:
         """Select which examples to use based on semantic similarity."""
         # Get the docs with the highest similarity.
-        if self.input_keys:
-            input_variables = {key: input_variables[key] for key in self.input_keys}
         vectorstore_kwargs = self.vectorstore_kwargs or {}
-        query = " ".join(sorted_values(input_variables))
         example_docs = self.vectorstore.similarity_search(
-            query, k=self.k, **vectorstore_kwargs
+            self._example_to_text(input_variables, self.input_keys),
+            k=self.k,
+            **vectorstore_kwargs,
         )
-        # Get the examples from the metadata.
-        # This assumes that examples are stored in metadata.
-        examples = [dict(e.metadata) for e in example_docs]
-        # If example keys are provided, filter examples to those keys.
-        if self.example_keys:
-            examples = [{k: eg[k] for k in self.example_keys} for eg in examples]
-        return examples
+        return self._documents_to_examples(example_docs)
+
+    async def aselect_examples(self, input_variables: Dict[str, str]) -> List[dict]:
+        """Select which examples to use based on semantic similarity."""
+        # Get the docs with the highest similarity.
+        vectorstore_kwargs = self.vectorstore_kwargs or {}
+        example_docs = await self.vectorstore.asimilarity_search(
+            self._example_to_text(input_variables, self.input_keys),
+            k=self.k,
+            **vectorstore_kwargs,
+        )
+        return self._documents_to_examples(example_docs)
 
     @classmethod
     def from_examples(
@@ -95,14 +121,49 @@ class SemanticSimilarityExampleSelector(BaseExampleSelector, BaseModel):
         Returns:
             The ExampleSelector instantiated, backed by a vector store.
         """
-        if input_keys:
-            string_examples = [
-                " ".join(sorted_values({k: eg[k] for k in input_keys}))
-                for eg in examples
-            ]
-        else:
-            string_examples = [" ".join(sorted_values(eg)) for eg in examples]
+        string_examples = [cls._example_to_text(eg, input_keys) for eg in examples]
         vectorstore = vectorstore_cls.from_texts(
+            string_examples, embeddings, metadatas=examples, **vectorstore_cls_kwargs
+        )
+        return cls(
+            vectorstore=vectorstore,
+            k=k,
+            input_keys=input_keys,
+            example_keys=example_keys,
+            vectorstore_kwargs=vectorstore_kwargs,
+        )
+
+    @classmethod
+    async def afrom_examples(
+        cls,
+        examples: List[dict],
+        embeddings: Embeddings,
+        vectorstore_cls: Type[VectorStore],
+        k: int = 4,
+        input_keys: Optional[List[str]] = None,
+        *,
+        example_keys: Optional[List[str]] = None,
+        vectorstore_kwargs: Optional[dict] = None,
+        **vectorstore_cls_kwargs: Any,
+    ) -> SemanticSimilarityExampleSelector:
+        """Create k-shot example selector using example list and embeddings.
+
+        Reshuffles examples dynamically based on query similarity.
+
+        Args:
+            examples: List of examples to use in the prompt.
+            embeddings: An initialized embedding API interface, e.g. OpenAIEmbeddings().
+            vectorstore_cls: A vector store DB interface class, e.g. FAISS.
+            k: Number of examples to select
+            input_keys: If provided, the search is based on the input variables
+                instead of all variables.
+            vectorstore_cls_kwargs: optional kwargs containing url for vector store
+
+        Returns:
+            The ExampleSelector instantiated, backed by a vector store.
+        """
+        string_examples = [cls._example_to_text(eg, input_keys) for eg in examples]
+        vectorstore = await vectorstore_cls.afrom_texts(
             string_examples, embeddings, metadatas=examples, **vectorstore_cls_kwargs
         )
         return cls(
