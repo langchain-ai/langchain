ab6a3bd2cbcbb165c239591a28c3c9eda38a008b
Jaid <siddiquizaid683@gmail.com>
2024-03-27 16:17:21 +0545

    Update __init__.py
    I think the previous version didn't had  Voyage rerank in the init file

--- libs/partners/voyageai/langchain_voyageai/__init__.py
+++ libs/partners/voyageai/langchain_voyageai/__init__.py
@@ -1,5 +1,7 @@
 from langchain_voyageai.embeddings import VoyageAIEmbeddings
+from langchain_voyageai.rerank import  VoyageAIRerank
 
 __all__ = [
     "VoyageAIEmbeddings",
+    " VoyageAIRerank"
 ]
