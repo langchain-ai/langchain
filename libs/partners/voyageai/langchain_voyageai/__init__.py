562053d83e524510cc224842ffa807401c273a11
Bagatur <baskaryan@gmail.com>
2024-03-27 23:35:49 -0700

    fmt

--- libs/partners/voyageai/langchain_voyageai/__init__.py
+++ libs/partners/voyageai/langchain_voyageai/__init__.py
@@ -1,7 +1,4 @@
 from langchain_voyageai.embeddings import VoyageAIEmbeddings
-from langchain_voyageai.rerank import  VoyageAIRerank
+from langchain_voyageai.rerank import VoyageAIRerank
 
-__all__ = [
-    "VoyageAIEmbeddings",
-    "VoyageAIRerank"
-]
+__all__ = ["VoyageAIEmbeddings", "VoyageAIRerank"]
