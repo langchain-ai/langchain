8d2e5c602afda459274ba69ff64837c98da3fd9d
HuangZiy <38649663+HuangZiy@users.noreply.github.com>
2024-03-29 05:46:27 +0800

    openai[patch]: perform judgment processing on chat model streaming delta (#18983)
    **PR title:** partners: openai chat model
    **PR message:** perform judgment processing on chat model streaming
    delta
    Closes #18977
    
    Co-authored-by: Bagatur <22008038+baskaryan@users.noreply.github.com>

--- libs/partners/openai/langchain_openai/chat_models/base.py
+++ libs/partners/openai/langchain_openai/chat_models/base.py
@@ -463,6 +463,8 @@ class ChatOpenAI(BaseChatModel):
             if len(chunk["choices"]) == 0:
                 continue
             choice = chunk["choices"][0]
+            if choice["delta"] is None:
+                continue
             chunk = _convert_delta_to_message_chunk(
                 choice["delta"], default_chunk_class
             )
@@ -557,6 +559,8 @@ class ChatOpenAI(BaseChatModel):
             if len(chunk["choices"]) == 0:
                 continue
             choice = chunk["choices"][0]
+            if choice["delta"] is None:
+                continue
             chunk = _convert_delta_to_message_chunk(
                 choice["delta"], default_chunk_class
             )
