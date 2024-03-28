be7162b73e3219b26473e58a485e2e480a43c70d
Erick Friis <erick@langchain.dev>
2024-03-28 16:16:02 -0700

    pinecone[patch]: source tag

--- libs/partners/pinecone/langchain_pinecone/vectorstores.py
+++ libs/partners/pinecone/langchain_pinecone/vectorstores.py
@@ -100,7 +100,7 @@ class PineconeVectorStore(VectorStore):
                 )
 
             # needs
-            client = PineconeClient(api_key=_pinecone_api_key)
+            client = PineconeClient(api_key=_pinecone_api_key, source_tag="langchain")
             self._index = client.Index(_index_name)
 
     @property
@@ -370,7 +370,9 @@ class PineconeVectorStore(VectorStore):
         Returns:
             Pinecone Index instance."""
         _pinecone_api_key = pinecone_api_key or os.environ.get("PINECONE_API_KEY") or ""
-        client = PineconeClient(api_key=_pinecone_api_key, pool_threads=pool_threads)
+        client = PineconeClient(
+            api_key=_pinecone_api_key, pool_threads=pool_threads, source_tag="langchain"
+        )
         indexes = client.list_indexes()
         index_names = [i.name for i in indexes.index_list["indexes"]]
 
