d4fb723429fa9934abf0282473bd2a1ebf59558c
Tomaz Bratanic <bratanic.tomaz@gmail.com>
2024-03-29 16:33:48 +0100

    community[patch]: Add the ability to pass maps to neo4j retrieval query (#19758)
    Makes it easier to flatten complex values to text, so you don't have to
    use a lot of Cypher to do it.

--- libs/community/tests/unit_tests/vectorstores/test_neo4j.py
+++ libs/community/tests/unit_tests/vectorstores/test_neo4j.py
@@ -1,6 +1,9 @@
 """Test Neo4j functionality."""
 
-from langchain_community.vectorstores.neo4j_vector import remove_lucene_chars
+from langchain_community.vectorstores.neo4j_vector import (
+    dict_to_yaml_str,
+    remove_lucene_chars,
+)
 
 
 def test_escaping_lucene() -> None:
@@ -43,3 +46,22 @@ def test_escaping_lucene() -> None:
         remove_lucene_chars("It is the end of the world. Take shelter~")
         == "It is the end of the world. Take shelter"
     )
+
+
+def test_converting_to_yaml() -> None:
+    example_dict = {
+        "name": "John Doe",
+        "age": 30,
+        "skills": ["Python", "Data Analysis", "Machine Learning"],
+        "location": {"city": "Ljubljana", "country": "Slovenia"},
+    }
+
+    yaml_str = dict_to_yaml_str(example_dict)
+
+    expected_output = (
+        "name: John Doe\nage: 30\nskills:\n- Python\n- "
+        "Data Analysis\n- Machine Learning\nlocation:\n  city: Ljubljana\n"
+        "  country: Slovenia\n"
+    )
+
+    assert yaml_str == expected_output
