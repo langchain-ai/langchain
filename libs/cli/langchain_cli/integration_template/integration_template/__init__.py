ae910b5be60caf585a42b5fa9d4ee2c1c665908f
Eugene Yurtsev <eyurtsev@gmail.com>
2024-04-01 14:10:12 -0400

    Delete metadata

--- libs/cli/langchain_cli/integration_template/integration_template/__init__.py
+++ libs/cli/langchain_cli/integration_template/integration_template/__init__.py
@@ -10,6 +10,7 @@ try:
 except metadata.PackageNotFoundError:
     # Case where package metadata is not available.
     __version__ = ""
+del metadata  # optional, avoids polluting the results of dir(__package__)
 
 __all__ = [
     "Chat__ModuleName__",
