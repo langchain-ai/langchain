d9d8e0a8d544b0ff04b57149ea051dc28f470821
Eugene Yurtsev <eyurtsev@gmail.com>
2024-04-02 15:17:08 -0400

    x

--- libs/langchain/tests/unit_tests/storage/test_filesystem.py
+++ libs/langchain/tests/unit_tests/storage/test_filesystem.py
@@ -29,8 +29,8 @@ def test_mset_and_mget(file_store: LocalFileStore) -> None:
     assert values == [b"value1", b"value2"]
 
 
-@pytest.mark.parametrize("chmod_dir, chmod_file",
-    [ ("777", "666"), ("770", "660"), ("700", "600") ]
+@pytest.mark.parametrize(
+    "chmod_dir, chmod_file", [("777", "666"), ("770", "660"), ("700", "600")]
 )
 def test_mset_chmod(chmod_dir, chmod_file) -> None:
     chmod_dir = int(chmod_dir, base=8)
@@ -40,7 +40,7 @@ def test_mset_chmod(chmod_dir, chmod_file) -> None:
     with tempfile.TemporaryDirectory() as temp_dir:
         # Instantiate the LocalFileStore with a directory inside the temporary directory
         # as the root path
-        temp_dir = os.path.join(temp_dir, 'store_dir')
+        temp_dir = os.path.join(temp_dir, "store_dir")
         file_store = LocalFileStore(
             temp_dir, chmod_dir=chmod_dir, chmod_file=chmod_file
         )
@@ -52,9 +52,9 @@ def test_mset_chmod(chmod_dir, chmod_file) -> None:
         # verify the permissions are set correctly
         # (test only the standard user/group/other bits)
         dir_path = str(file_store.root_path)
-        file_path = os.path.join(dir_path, 'key1')
+        file_path = os.path.join(dir_path, "key1")
         assert (os.stat(dir_path).st_mode & 0o777) == chmod_dir
-        assert (os.stat(file_path).st_mode & 0o777)  == chmod_file
+        assert (os.stat(file_path).st_mode & 0o777) == chmod_file
 
 
 def test_mdelete(file_store: LocalFileStore) -> None:
