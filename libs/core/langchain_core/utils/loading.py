c5011b19ae0924a3c696c5a398ef783cfd20205b
Erick Friis <erick@langchain.dev>
2024-04-03 09:54:33 -0700

    x

--- libs/core/langchain_core/utils/loading.py
+++ libs/core/langchain_core/utils/loading.py
@@ -3,7 +3,18 @@
 import warnings
 from typing import Any
 
+from langchain_core._api.deprecation import deprecated
 
+
+@deprecated(
+    since="0.1.30",
+    removal="0.2",
+    message=(
+        "Using the hwchase17/langchain-hub "
+        "repo for prompts is deprecated. Please use "
+        "https://smith.langchain.com/hub instead."
+    ),
+)
 def try_load_from_hub(
     *args: Any,
     **kwargs: Any,
@@ -12,4 +23,6 @@ def try_load_from_hub(
         "Loading from the deprecated github-based Hub is no longer supported. "
         "Please use the new LangChain Hub at https://smith.langchain.com/hub instead."
     )
+    # return None, which indicates that we shouldn't load from old hub
+    # and might just be a filepath for e.g. load_chain
     return None
