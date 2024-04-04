8ca41dbfecee79460e79d1d0c9ed455713820899
Jacob Lee <jacoblee93@gmail.com>
2024-04-04 12:49:15 -0700

    docs[patch]: Make Docusaurus and Vercel add trailing slashes when navigating by default (#20014)
    Should hopefully avoid weird broken link edge cases.
    
    Relative links now trip up the Docusaurus broken link checker, so this
    PR also removes them.
    
    Also snuck in a small addition about asyncio

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
-
+  trailingSlash: true,
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "throw",
 
