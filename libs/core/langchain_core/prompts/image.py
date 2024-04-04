f97de4e2750f5292042dad109d5ca9515ac94124
Christophe Bornet <cbornet@hotmail.com>
2024-04-04 16:24:55 +0200

    core[minor]: Add aformat to FewShotPromptTemplate (#19652)

--- libs/core/langchain_core/prompts/image.py
+++ libs/core/langchain_core/prompts/image.py
@@ -3,6 +3,7 @@ from typing import Any, List
 from langchain_core.prompt_values import ImagePromptValue, ImageURL, PromptValue
 from langchain_core.prompts.base import BasePromptTemplate
 from langchain_core.pydantic_v1 import Field
+from langchain_core.runnables import run_in_executor
 from langchain_core.utils import image as image_utils
 
 
@@ -79,6 +80,9 @@ class ImagePromptTemplate(BasePromptTemplate[ImageURL]):
             # Don't check literal values here: let the API check them
             output["detail"] = detail  # type: ignore[typeddict-item]
         return output
+
+    async def aformat(self, **kwargs: Any) -> ImageURL:
+        return await run_in_executor(None, self.format, **kwargs)
 
     def pretty_repr(self, html: bool = False) -> str:
         raise NotImplementedError()
