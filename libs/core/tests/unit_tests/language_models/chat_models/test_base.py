9280fc45e5e34ccc2b5240c6ba3cd13490d10907
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2024-04-05 12:52:05 -0700

    lint..

--- libs/core/tests/unit_tests/language_models/chat_models/test_base.py
+++ libs/core/tests/unit_tests/language_models/chat_models/test_base.py
@@ -234,7 +234,7 @@ async def test_astream_implementation_uses_astream() -> None:
 
 
 class FakeTracer(BaseTracer):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         self.traced_run_ids = []
 
