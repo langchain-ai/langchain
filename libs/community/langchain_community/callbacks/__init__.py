01b829539710f1dfb4cd5fe9c15a851cd5834f68
Eugene Yurtsev <eyurtsev@gmail.com>
2024-04-04 22:50:55 -0400

    x

--- libs/community/langchain_community/callbacks/__init__.py
+++ libs/community/langchain_community/callbacks/__init__.py
@@ -7,9 +7,7 @@
     BaseCallbackHandler --> <name>CallbackHandler  # Example: AimCallbackHandler
 """
 import importlib
-from typing import Any
-
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
     from langchain_community.callbacks.aim_callback import AimCallbackHandler
@@ -23,18 +21,31 @@ if TYPE_CHECKING:
     from langchain_community.callbacks.flyte_callback import FlyteCallbackHandler
     from langchain_community.callbacks.human import HumanApprovalCallbackHandler
     from langchain_community.callbacks.infino_callback import InfinoCallbackHandler
-    from langchain_community.callbacks.llmonitor_callback import LLMonitorCallbackHandler
-    from langchain_community.callbacks.labelstudio_callback import LabelStudioCallbackHandler
+    from langchain_community.callbacks.labelstudio_callback import (
+        LabelStudioCallbackHandler,
+    )
+    from langchain_community.callbacks.llmonitor_callback import (
+        LLMonitorCallbackHandler,
+    )
+    from langchain_community.callbacks.manager import (
+        get_openai_callback,
+        wandb_tracing_enabled,
+    )
     from langchain_community.callbacks.mlflow_callback import MlflowCallbackHandler
     from langchain_community.callbacks.openai_info import OpenAICallbackHandler
-    from langchain_community.callbacks.promptlayer_callback import PromptLayerCallbackHandler
-    from langchain_community.callbacks.sagemaker_callback import SageMakerCallbackHandler
-    from langchain_community.callbacks.streamlit import LLMThoughtLabeler, StreamlitCallbackHandler
+    from langchain_community.callbacks.promptlayer_callback import (
+        PromptLayerCallbackHandler,
+    )
+    from langchain_community.callbacks.sagemaker_callback import (
+        SageMakerCallbackHandler,
+    )
+    from langchain_community.callbacks.streamlit import (
+        LLMThoughtLabeler,
+        StreamlitCallbackHandler,
+    )
     from langchain_community.callbacks.trubrics_callback import TrubricsCallbackHandler
     from langchain_community.callbacks.wandb_callback import WandbCallbackHandler
     from langchain_community.callbacks.whylabs_callback import WhyLabsCallbackHandler
-    from langchain_community.callbacks.manager import get_openai_callback, wandb_tracing_enabled
-
 
 
 _module_lookup = {
