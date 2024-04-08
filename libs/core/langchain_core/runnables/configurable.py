ffbe622ce81e9495183ea67b1dd3c5b65a497083
Bagatur <baskaryan@gmail.com>
2024-04-08 11:05:08 -0500

    fmt

--- libs/core/langchain_core/runnables/configurable.py
+++ libs/core/langchain_core/runnables/configurable.py
@@ -346,7 +346,9 @@ class RunnableConfigurableFields(DynamicRunnable[Input, Output]):
 
         if configurable:
             init_params = {
-                k: v for k, v in self.default.__dict__ if k in self.default.__fields__
+                k: v
+                for k, v in self.default.__dict__.items()
+                if k in self.default.__fields__
             }
             return (
                 self.default.__class__(**init_params, **configurable),
