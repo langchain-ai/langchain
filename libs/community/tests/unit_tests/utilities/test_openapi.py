306ea3f47de80af08ef8768c34b2012e13f7134a
anujmehta <anujmehta@clearwateranalytics.com>
2024-04-08 10:58:39 +0530

    lint fix

--- libs/community/tests/unit_tests/utilities/test_openapi.py
+++ libs/community/tests/unit_tests/utilities/test_openapi.py
@@ -12,37 +12,40 @@ class OpenAPISpecTestCase(unittest.TestCase):
     def setUp(self):
         self.instance = OpenAPISpec(info=Info(title="test", version="1.0.0"))
 
-    @patch('langchain_community.utilities.openapi.OpenAPISpec.get_referenced_schema')
+    @patch("langchain_community.utilities.openapi.OpenAPISpec.get_referenced_schema")
     @pytest.mark.requires("openapi-pydantic")
     def test_get_root_referenced_schema_with_obj_ref(self, mock_get_referenced_schema):
         address_schema = Schema()
-        address_schema.properties = { 
-                            'address': Reference(ref='#/components/schemas/Address')}
-        
+        address_schema.properties = {
+            "address": Reference(ref="#/components/schemas/Address")
+        }
+
         address_ref_schema = Schema()
-        address_ref_schema.properties = {'street': Schema(), 'city': Schema(), 
-                                         'state': Schema()}
-    
+        address_ref_schema.properties = {
+            "street": Schema(),
+            "city": Schema(),
+            "state": Schema(),
+        }
+
         mock_get_referenced_schema.side_effect = [address_schema, address_ref_schema]
-        ref = Reference(ref='#/components/schemas/Pet')
+        ref = Reference(ref="#/components/schemas/Pet")
         result = self.instance._get_root_referenced_schema(ref)
-        self.assertTrue(isinstance(result.properties['address'], Schema))
+        self.assertIsInstance(result.properties["address"], Schema)
 
-
-    @patch('langchain_community.utilities.openapi.OpenAPISpec.get_referenced_schema')
+    @patch("langchain_community.utilities.openapi.OpenAPISpec.get_referenced_schema")
     @pytest.mark.requires("openapi-pydantic")
     def test_get_root_referenced_schema_obj_arr_ref(self, mock_get_referenced_schema):
         hobby_schema = Schema()
-        hobby_schema.properties = { 
-                            'hobby': Reference(ref='#/components/schemas/Hobby')}
-        
-        hobby_ref_schema = Schema()
-        hobby_ref_schema.properties = {'name': Schema()}
-    
-        mock_get_referenced_schema.side_effect = [hobby_schema, hobby_ref_schema]
-        ref = Reference(ref='#/components/schemas/Pet')
-        result = self.instance._get_root_referenced_schema(ref)
-        self.assertTrue(isinstance(result.properties['hobby'], Schema))
+        hobby_schema.properties = {"hobby": Reference(ref="#/components/schemas/Hobby")}
 
-if __name__ == '__main__':
+        hobby_ref_schema = Schema()
+        hobby_ref_schema.properties = {"name": Schema()}
+
+        mock_get_referenced_schema.side_effect = [hobby_schema, hobby_ref_schema]
+        ref = Reference(ref="#/components/schemas/Pet")
+        result = self.instance._get_root_referenced_schema(ref)
+        self.assertIsInstance(result.properties["hobby"], Schema)
+
+
+if __name__ == "__main__":
     unittest.main()
