c09c2ebd5cd2909589fcb6261391f9644d2af268
Surya <git@sdan.io>
2024-04-09 15:15:56 -0700

    Add VLite tests and indexing/public api

--- libs/community/tests/unit_tests/vectorstores/test_indexing_docs.py
+++ libs/community/tests/unit_tests/vectorstores/test_indexing_docs.py
@@ -87,6 +87,7 @@ def test_compatible_vectorstore_documentation() -> None:
         "VDMS",
         "Vearch",
         "VespaStore",
+        "VLite",
         "Weaviate",
         "ZepVectorStore",
         "Zilliz",
