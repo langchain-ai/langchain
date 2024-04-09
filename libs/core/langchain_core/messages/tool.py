0cc3142eba25612643d5938188a2504428ba4851
ccurme <chester.curme@gmail.com>
2024-04-09 14:02:48 -0400

    update (#20215)

--- libs/core/langchain_core/messages/tool.py
+++ libs/core/langchain_core/messages/tool.py
@@ -1,5 +1,5 @@
 import json
-from typing import Any, Dict, List, Literal, Optional
+from typing import Any, Dict, List, Literal, Optional, Tuple
 
 from typing_extensions import TypedDict
 
@@ -124,23 +124,35 @@ class InvalidToolCall(TypedDict):
     error: Optional[str]
 
 
-def default_tool_parser(raw_tool_calls: List[dict]) -> List[ToolCall]:
+def default_tool_parser(
+    raw_tool_calls: List[dict],
+) -> Tuple[List[ToolCall], List[InvalidToolCall]]:
     """Best-effort parsing of tools."""
     tool_calls = []
+    invalid_tool_calls = []
     for tool_call in raw_tool_calls:
         if "function" not in tool_call:
-            function_args = None
-            function_name = None
+            continue
         else:
-            function_args = json.loads(tool_call["function"]["arguments"])
             function_name = tool_call["function"]["name"]
-        parsed = ToolCall(
-            name=function_name or "",
-            args=function_args or {},
-            id=tool_call.get("id"),
-        )
-        tool_calls.append(parsed)
-    return tool_calls
+            try:
+                function_args = json.loads(tool_call["function"]["arguments"])
+                parsed = ToolCall(
+                    name=function_name or "",
+                    args=function_args or {},
+                    id=tool_call.get("id"),
+                )
+                tool_calls.append(parsed)
+            except json.JSONDecodeError:
+                invalid_tool_calls.append(
+                    InvalidToolCall(
+                        name=function_name,
+                        args=tool_call["function"]["arguments"],
+                        id=tool_call.get("id"),
+                        error="Malformed args.",
+                    )
+                )
+    return tool_calls, invalid_tool_calls
 
 
 def default_tool_chunk_parser(raw_tool_calls: List[dict]) -> List[ToolCallChunk]:
