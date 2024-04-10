5133b56cf7fd4664f0402331b75b3a6cb12164ed
Leonid Ganeline <leo.gan.57@gmail.com>
2024-04-10 10:01:19 -0700

    community[patch]: import flattening fix  (#20110)
    This PR should make it easier for linters to do type checking and for IDEs to jump to definition of code.
    
    See #20050 as a template for this PR.
    - As a byproduct: Added 3 missed `test_imports`.
    - Added missed `SolarChat` in to __init___.py Added it into test_import
    ut.
    - Added `# type: ignore` to fix linting. It is not clear, why linting
    errors appear after ^ changes.
    
    ---------
    
    Co-authored-by: Eugene Yurtsev <eyurtsev@gmail.com>

--- libs/community/langchain_community/chat_models/moonshot.py
+++ libs/community/langchain_community/chat_models/moonshot.py
@@ -8,7 +8,7 @@ from langchain_community.chat_models import ChatOpenAI
 from langchain_community.llms.moonshot import MOONSHOT_SERVICE_URL_BASE, MoonshotCommon
 
 
-class MoonshotChat(MoonshotCommon, ChatOpenAI):
+class MoonshotChat(MoonshotCommon, ChatOpenAI):  # type: ignore[misc]
     """Wrapper around Moonshot large language models.
 
     To use, you should have the ``openai`` python package installed, and the
