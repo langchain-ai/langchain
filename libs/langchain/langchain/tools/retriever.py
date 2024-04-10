57aa8a0c5750d6e9ff68587441fafee0b58c1780
leo-gan <leo.gan.57@gmail.com>
2024-04-10 12:33:43 -0700

    merging conflicts

--- libs/langchain/langchain/tools/retriever.py
+++ libs/langchain/langchain/tools/retriever.py
@@ -1,3 +1,15 @@
-from langchain_core.tools import RetrieverInput, create_retriever_tool
+from langchain_core.tools import (
+    RetrieverInput,
+    create_retriever_tool,
+    ToolsRenderer,
+    render_text_description,
+    render_text_description_and_arg
+)
 
-__all__ = ["RetrieverInput", "create_retriever_tool"]
+__all__ = [
+    "RetrieverInput",
+    "ToolsRenderer",
+    "create_retriever_tool",
+    "render_text_description",
+    "render_text_description_and_args"
+]
