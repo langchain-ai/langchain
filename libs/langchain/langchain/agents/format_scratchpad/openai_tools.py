aa8cfa0ebc4edc7f4351ba1122ed5cfc23e1c61a
Bagatur <baskaryan@gmail.com>
2024-04-10 06:35:00 -0500

    fmt

--- libs/langchain/langchain/agents/format_scratchpad/openai_tools.py
+++ libs/langchain/langchain/agents/format_scratchpad/openai_tools.py
@@ -1,3 +1,3 @@
-from langchain.agents.format_scratchpad.tools import format_to_tool_messages
+from langchain.agents.format_scratchpad.tools import format_to_tool_messages as format_to_openai_tool_messages
 
-format_to_openai_tool_messages = format_to_tool_messages
+__all__ = ["format_to_openai_tool_messages"]
