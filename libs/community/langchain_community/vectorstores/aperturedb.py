ecf2ace456c84b3bc8e7036982cae966254a6f94
Gavin Matthews <gavin@aperturedata.io>
2024-04-11 01:03:58 +0000

    flesh out more implementation

--- libs/community/langchain_community/vectorstores/aperturedb.py
+++ libs/community/langchain_community/vectorstores/aperturedb.py
@@ -1,7 +1,7 @@
 # System imports
 from itertools import cycle, islice, repeat
 import logging
-from typing import Any, Iterable, List, Optional, Type
+from typing import Any, Iterable, List, Optional, Tuple, Type
 
 # Third-party imports
 import numpy as np
@@ -29,6 +29,8 @@ class ApertureDB(VectorStore):
         self.embeddings = embeddings
         self.descriptor_set = descriptor_set
         self.dimensions = dimensions
+        self.engine = engine
+        self.metric = metric
 
         try:
             import aperturedb
@@ -46,6 +48,20 @@ class ApertureDB(VectorStore):
         self.utils = aperturedb.Utils.Utils(self.connection)
 
         self._find_or_add_descriptor_set(descriptor_set, dimensions)
+
+    def _select_relevance_score_fn(self) -> Callable[[float], float]:
+        """Selects the relevance score function based on the metric.
+        
+        This method is defined in order to make the base class implementation of similarity_search_with_relevance_scores work.
+        """
+        if self.metric == "L2":
+            return VectorStore._euclidean_relevance_score_fn
+        elif self.metric == "IP":
+            return VectorStore._max_inner_product_relevance_score_fn
+        elif self.metric == "CS":
+            return VectorStore._cosine_relevance_score_fn
+        else:
+            raise ValueError(f"Unknown metric: {self.metric}")
 
     def _find_or_add_descriptor_set(self, descriptor_set:str, dimensions:Optional[int]=None):
         """Checks if the descriptor set exists, if not, creates it"""
@@ -134,23 +150,57 @@ class ApertureDB(VectorStore):
         results = [r["DeleteDescriptor"]["results"]["count"] == 1 for r in responses[1::3]]
         return results
     
-    def search(self, query: str, search_type: str, **kwargs: Any) -> List[Document]:
-        raise NotImplementedError # TODO
-
     def similarity_search(
         self, query: str, k: int = 4, **kwargs: Any
     ) -> List[Document]:
-        raise NotImplementedError
+        embedding = self.embeddings.embed_query(query)
+        return self.similarity_search_by_vector(embedding, k, **kwargs)
 
     def similarity_search_with_score(
-        self, *args: Any, **kwargs: Any
+        self, query: str, **kwargs: Any
     ) -> List[Tuple[Document, float]]:
-        raise NotImplementedError
+        embedding = self.embeddings.embed_query(query)
+        self._similarity_search_with_score_by_vector(embedding, **kwargs)
 
+    def _similarity_search_with_score_by_vector(
+        self, embedding: List[float], k: int = 4
+    ) -> List[Tuple[Document, float]]:
+        query = [ {
+            "FindDescriptor": {
+                "set": self.descriptor_set,
+                "k_neighbors": k,
+                "_ref": 1,
+                "results": { "list": ["_uniqueid"] },
+                "distances": True,
+            }
+        }, {
+            "FindBlob": {
+                "is_connected_to": {
+                    "ref": 1
+                }
+                "results": { "all_properties": True },
+                "blobs": True,
+            }
+        }]   
+        blobs_in = [np.array(embedding).tobytes()]
+        responses, blobs_out = self.connection.query(query, blobs_in) 
+        assert self.connection.last_query_ok(), self.connection.last_error()
+        results = []
+        for i in range(0, len(responses), 2):
+            unique_ids = responses[i]["FindDescriptor"]["results"]["_uniqueid"]
+            metadata = responses[i+1]["FindBlob"]["entities"][0]
+            distance = metadata["_distance"]
+            blob_index = metadata["_blob_index"]
+            text = blobs_out[blob_index].decode()
+            # consider filtering internal properties out
+            results.append((Document(page_content=text, _uniqueid=unique_ids, **metadata), distance))
+        return results
+    
     def similarity_search_by_vector(
         self, embedding: List[float], k: int = 4, **kwargs: Any
     ) -> List[Document]:
-        raise NotImplementedError
+        results = self._similarity_search_with_score_by_vector(embedding, k)
+        return [document for document, score in results]    
 
     def max_marginal_relevance_search(
         self,
