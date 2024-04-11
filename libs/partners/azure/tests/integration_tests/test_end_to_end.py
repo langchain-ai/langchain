d86b53398886d702f8d0f9ddc876d04b2955f2df
Anthony Chu <anthony@anthonychu.ca>
2024-04-11 11:01:04 -0700

    Fix integration tests

--- libs/partners/azure/tests/integration_tests/test_end_to_end.py
+++ libs/partners/azure/tests/integration_tests/test_end_to_end.py
@@ -21,33 +21,31 @@ def test_end_to_end():
     assert uploaded_file1_metadata.filename == "test1.txt"
     assert uploaded_file1_metadata.size_in_bytes == 16
     assert uploaded_file1_metadata.full_path == "/mnt/data/test1.txt"
-    downloaded_file1 = tool.download_file(remote_filename="test1.txt")
+    downloaded_file1 = tool.download_file(remote_file_path="test1.txt")
     assert downloaded_file1.read() == b"hello world!!!!!"
 
     # upload file from buffer
-    with NamedTemporaryFile() as f:
-        f.write(b"file 2 content")
-        f.seek(0)
+    with open(TEST_DATA_PATH, "rb") as f:
         uploaded_file2_metadata = tool.upload_file(remote_file_path="test2.txt", data=f)
         assert uploaded_file2_metadata.filename == "test2.txt"
-        downloaded_file2 = tool.download_file(remote_filename="test2.txt")
-        assert downloaded_file2.read() == b"file 2 content"
+        downloaded_file2 = tool.download_file(remote_file_path="test2.txt")
+        assert downloaded_file2.read() == TEST_DATA_CONTENT
 
     # upload file from disk, specifying remote file path
     uploaded_file3_metadata = tool.upload_file(remote_file_path="test3.txt", local_file_path=TEST_DATA_PATH)
     assert uploaded_file3_metadata.filename == "test3.txt"
-    downloaded_file3 = tool.download_file(remote_filename="test3.txt")
+    downloaded_file3 = tool.download_file(remote_file_path="test3.txt")
     assert downloaded_file3.read() == TEST_DATA_CONTENT
 
     # upload file from disk, without specifying remote file path
     uploaded_file4_metadata = tool.upload_file(local_file_path=TEST_DATA_PATH)
     assert uploaded_file4_metadata.filename == os.path.basename(TEST_DATA_PATH)
-    downloaded_file4 = tool.download_file(remote_filename=uploaded_file4_metadata.filename)
+    downloaded_file4 = tool.download_file(remote_file_path=uploaded_file4_metadata.filename)
     assert downloaded_file4.read() == TEST_DATA_CONTENT
 
     # list files
     remote_files_metadata = tool.list_files()
     assert len(remote_files_metadata) == 4
-    remote_filenames = [metadata.filename for metadata in remote_files_metadata]
+    remote_file_paths = [metadata.filename for metadata in remote_files_metadata]
     expected_filenames = ["test1.txt", "test2.txt", "test3.txt", os.path.basename(TEST_DATA_PATH)]
-    assert set(remote_filenames) == set(expected_filenames)
+    assert set(remote_file_paths) == set(expected_filenames)
