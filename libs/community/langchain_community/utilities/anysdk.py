5aa4289e36bb83c770df269951b1221cef466e0c
James Barney <Jamesbarney71@gmail.com>
2024-04-12 22:02:18 +0000

    stashing

--- libs/community/langchain_community/utilities/anysdk.py
+++ libs/community/langchain_community/utilities/anysdk.py
@@ -1,16 +1,103 @@
 import json
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
-from langchain_core.pydantic_v1 import BaseModel, Extra
+from langchain_core.pydantic_v1 import BaseModel, Extra, root_validator
+from langchain_core.utils import get_from_dict_or_env
 
+ANYSDK_CRUD_CONTROLS_CREATE = False
+ANYSDK_CRUD_CONTROLS_READ = True
+ANYSDK_CRUD_CONTROLS_UPDATE = False
+ANYSDK_CRUD_CONTROLS_DELETE = False
+
+ANYSDK_CRUD_CONTROLS_CREATE_LIST = "create"
+ANYSDK_CRUD_CONTROLS_READ_LIST = "get,read,list"
+ANYSDK_CRUD_CONTROLS_UPDATE_LIST = "update,put,post"
+ANYSDK_CRUD_CONTROLS_DELETE_LIST = "delete,destroy,remove"
 
 class AnySdkWrapper(BaseModel):
     client: Any
     operations: List[Dict] = []
+    crud_controls_create: Optional[bool] = ANYSDK_CRUD_CONTROLS_CREATE
+    crud_controls_create_list: Optional[str]
+    crud_controls_read: Optional[bool] = ANYSDK_CRUD_CONTROLS_READ
+    crud_controls_read_list: Optional[str]
+    crud_controls_update: Optional[bool] = ANYSDK_CRUD_CONTROLS_UPDATE
+    crud_controls_update_list: Optional[str]
+    crud_controls_delete: Optional[bool] = ANYSDK_CRUD_CONTROLS_DELETE
+    crud_controls_delete_list: Optional[str]
 
     class Config:
         extra = Extra.allow
     
+    @root_validator
+    def validate_environment(cls, values: Dict) -> Dict:
+        crud_controls_create = get_from_dict_or_env(
+            values,
+            "crud_controls_create",
+            "ANYSDK_CRUD_CONTROLS_CREATE",
+            default=ANYSDK_CRUD_CONTROLS_CREATE
+        )
+        values["crud_controls_create"] = crud_controls_create
+
+        crud_controls_create_list = get_from_dict_or_env(
+            values,
+            "crud_controls_create_list",
+            "ANYSDK_CRUD_CONTROLS_CREATE_LIST",
+            default=ANYSDK_CRUD_CONTROLS_CREATE_LIST
+        )
+        values["crud_controls_create_list"] = crud_controls_create_list.split(',')
+
+        crud_controls_read = get_from_dict_or_env(
+            values,
+            "crud_controls_read",
+            "ANYSDK_CRUD_CONTROLS_READ",
+            default=ANYSDK_CRUD_CONTROLS_READ
+        )
+        values["crud_controls_read"] = crud_controls_read
+
+        crud_controls_read_list = get_from_dict_or_env(
+            values,
+            "crud_controls_read_list",
+            "ANYSDK_CRUD_CONTROLS_READ_LIST",
+            default=ANYSDK_CRUD_CONTROLS_READ_LIST
+        )
+        values["crud_controls_read_list"] = crud_controls_read_list.split(',')
+
+        crud_controls_update = get_from_dict_or_env(
+            values,
+            "crud_controls_update",
+            "ANYSDK_CRUD_CONTROLS_UPDATE",
+            default=ANYSDK_CRUD_CONTROLS_UPDATE
+        )
+        values["crud_controls_update"] = crud_controls_update
+
+        crud_controls_update_list = get_from_dict_or_env(
+            values,
+            "crud_controls_update_list",
+            "ANYSDK_CRUD_CONTROLS_UPDATE_LIST",
+            default=ANYSDK_CRUD_CONTROLS_UPDATE_LIST
+        )
+        values["crud_controls_update_list"] = crud_controls_update_list.split(',')
+
+        crud_controls_delete = get_from_dict_or_env(
+            values,
+            "crud_controls_delete",
+            "ANYSDK_CRUD_CONTROLS_DELETE",
+            default=ANYSDK_CRUD_CONTROLS_DELETE
+        )
+        values["crud_controls_delete"] = crud_controls_delete
+
+        crud_controls_delete_list = get_from_dict_or_env(
+            values,
+            "crud_controls_delete_list",
+            "ANYSDK_CRUD_CONTROLS_DELETE_LIST",
+            default=ANYSDK_CRUD_CONTROLS_DELETE_LIST
+        )
+        values["crud_controls_delete_list"] = crud_controls_delete_list.split(',')
+
+        return values
+
+
     def __init__(self, **data):
         super().__init__(**data)
         self.operations = self._build_operations()
@@ -25,6 +112,16 @@ class AnySdkWrapper(BaseModel):
 
         for func_name in sdk_functions:
             func = getattr(self.client, func_name)
+
+            if any(word.lower() in func_name.lower() for word in self.crud_controls_create):
+                continue
+            if any(word.lower() in func_name.lower() for word in self.crud_controls_read):
+                continue
+            if any(word.lower() in func_name.lower() for word in self.crud_controls_update):
+                continue
+            if any(word.lower() in func_name.lower() for word in self.crud_controls_delete):
+                continue
+
             operation = {
                 "mode": func_name,
                 "name": func.__name__.replace("_", " ").title(),
--- libs/community/tests/unit_tests/utilities/test_anysdk.py
+++ libs/community/tests/unit_tests/utilities/test_anysdk.py
@@ -7,44 +7,51 @@ from langchain_community.utilities.anysdk import AnySdkWrapper
 
 class FakeSdk():
     def get_thing(self, thing_id):
-        return {"id": thing_id}
+        """Gets Things"""
+        return self._dummy_return(thing_id)
     
     def post_thing(self, thing_id):
+        """Posts Things"""
         return self._dummy_return(thing_id)
     
     def put_thing(self, thing_id):
+        """Puts Things"""
         return self._dummy_return(thing_id)
     
     def delete_thing(self, thing_id):
+        """Deletes Things"""
         return self._dummy_return(thing_id)
     
     def _dummy_return(self, thing_id):
+        """Hidden, never called directly. Does Things"""
         return {"status": 200, "response": {"id": thing_id}}
 
 client = FakeSdk()
 
 anysdk = AnySdkWrapper(client=client)
 
+def test_operations_is_populated():
+    assert len(anysdk.operations) != 0
+
 def test_get_thing():
     assert json.loads(anysdk.run('get_thing', json.dumps({
             "thing_id": 123
-        })))["id"] == 123
+        })))["response"]["id"] == 123
 
 def test_post_thing():
     assert json.loads(anysdk.run('post_thing', json.dumps({
             "thing_id": 123
-        })))["id"] == 123
+        })))["response"]["id"] == 123
 
 def test_put_thing():
     assert json.loads(anysdk.run('put_thing', json.dumps({
             "thing_id": 123
-        })))["id"] == 123
+        })))["response"]["id"] == 123
 
 def test_delete_thing():
     assert json.loads(anysdk.run('delete_thing', json.dumps({
             "thing_id": 123
-        })))["id"] == 123
+        })))["response"]["id"] == 123
 
 def test_no_hidden_methods():
-    with pytest.raises(AttributeError):
-        anysdk.run('_dummy_return', json.dumps({"thing_id": 123}))
+    assert not any(op['mode'].startswith('_') for op in anysdk.operations)
