e87a36d60706bbf737a46840f75d596256684cf0
Erick Friis <erick@langchain.dev>
2024-04-12 14:11:33 -0700

    x

--- libs/core/tests/unit_tests/runnables/test_configurable.py
+++ libs/core/tests/unit_tests/runnables/test_configurable.py
@@ -1,6 +1,6 @@
 from typing import Any, Dict, Optional
 
-from langchain_core.pydantic_v1 import root_validator
+from langchain_core.pydantic_v1 import Field, root_validator
 from langchain_core.runnables import (
     ConfigurableField,
     RunnableConfig,
@@ -9,8 +9,11 @@ from langchain_core.runnables import (
 
 
 class MyRunnable(RunnableSerializable[str, str]):
-    my_property: str
+    my_property: str = Field(alias="my_property_alias")
     _my_hidden_property: str = ""
+
+    class Config:
+        allow_population_by_field_name = True
 
     @root_validator(pre=True)
     def my_error(cls, values: Dict[str, Any]) -> Dict[str, Any]:
@@ -38,7 +41,42 @@ def test_doubly_set_configurable() -> None:
         )
     )
 
-    assert runnable.invoke("b") == "ba"
+    assert (
+        configurable_runnable.invoke(
+            "d", config=RunnableConfig(configurable={"my_property": "c"})
+        )
+        == "dc"
+    )
+
+
+def test_alias_set_configurable() -> None:
+    runnable = MyRunnable(my_property="a")
+    configurable_runnable = runnable.configurable_fields(
+        my_property=ConfigurableField(
+            id="my_property_alias",
+            name="My property alias",
+            description="The property to test alias",
+        )
+    )
+
+    assert (
+        configurable_runnable.invoke(
+            "d", config=RunnableConfig(configurable={"my_property_alias": "c"})
+        )
+        == "dc"
+    )
+
+
+def test_field_alias_set_configurable() -> None:
+    runnable = MyRunnable(my_property_alias="a")
+    configurable_runnable = runnable.configurable_fields(
+        my_property=ConfigurableField(
+            id="my_property",
+            name="My property alias",
+            description="The property to test alias",
+        )
+    )
+
     assert (
         configurable_runnable.invoke(
             "d", config=RunnableConfig(configurable={"my_property": "c"})
