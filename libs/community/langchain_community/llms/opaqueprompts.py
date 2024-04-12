0f836647054eb0e4a34aa3b3c55cdb5d013754cd
Chester Curme <chester.curme@gmail.com>
2024-04-12 14:27:38 -0400

    fix stop tokens

--- libs/community/langchain_community/llms/opaqueprompts.py
+++ libs/community/langchain_community/llms/opaqueprompts.py
@@ -95,9 +95,8 @@ class OpaquePrompts(LLM):
 
         # TODO: Add in callbacks once child runs for LLMs are supported by LangSmith.
         # call the LLM with the sanitized prompt and get the response
-        llm_response = self.base_llm.invoke(
+        llm_response = self.base_llm.bind(stop=stop).invoke(
             sanitized_prompt_value_str,
-            stop=stop,
         )
 
         # desanitize the response by restoring the original sensitive information
