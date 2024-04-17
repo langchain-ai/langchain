65ed13fbba416f5744baf6531c5f88580c09489a
Erick Friis <erick@langchain.dev>
2024-04-17 15:38:21 -0700

    x

--- libs/partners/upstage/tests/integration_tests/test_chat_models_standard.py
+++ libs/partners/upstage/tests/integration_tests/test_chat_models_standard.py
@@ -1,4 +1,5 @@
 """Standard LangChain interface tests"""
+
 from typing import Type
 
 import pytest
@@ -18,3 +19,14 @@ class TestUpstageStandard(ChatModelIntegrationTests):
         return {
             "model": "solar-1-mini-chat",
         }
+
+    @pytest.mark.xfail(reason="400s with tool calling currently")
+    def test_tool_message_histories(
+        self,
+        chat_model_class: Type[BaseChatModel],
+        chat_model_params: dict,
+        chat_model_has_tool_calling: bool,
+    ) -> None:
+        super().test_tool_message_histories(
+            chat_model_class, chat_model_params, chat_model_has_tool_calling
+        )
