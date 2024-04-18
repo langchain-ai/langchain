bb9ac827bb19625f5e070b0290af8eb1f66a2763
Patrick McFadin <pmcfadin@gmail.com>
2024-04-18 08:10:37 -0700

    Removed unused QUERY_CHECKER prompt for now.

--- libs/community/langchain_community/tools/cassandra_database/prompt.py
+++ libs/community/langchain_community/tools/cassandra_database/prompt.py
@@ -1,22 +1,5 @@
 """Tools for interacting with an Apache Cassandra database."""
 
-QUERY_CHECKER = """
-{query}
-Double check the CQL query above for common mistakes, including:
-- Using incorrect keyspace or table names
-- Using incorrect data types in the query
-- Using the wrong column names
-- Improper use of CQL statements like SELECT, INSERT, UPDATE, DELETE
-- Incorrect use of allow filtering and ordering
-
-If there are any of the above mistakes, rewrite the query. If there are no mistakes, 
-just reproduce the original query.
-
-Output the final CQL query only.
-
-CQL Query: """
-
-
 QUERY_PATH_PROMPT = """"
 You are an Apache Cassandra expert query analysis bot with the following features 
 and rules:
