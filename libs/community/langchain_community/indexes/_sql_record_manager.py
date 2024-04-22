1909eb312adabcc3e897921d40a6732202ee4183
Philippe Prados <github@prados.fr>
2024-04-22 12:09:35 +0200

    Fix _amake_session()

--- libs/community/langchain_community/indexes/_sql_record_manager.py
+++ libs/community/langchain_community/indexes/_sql_record_manager.py
@@ -175,7 +175,7 @@ class SQLRecordManager(RecordManager):
     async def _amake_session(self) -> AsyncGenerator[AsyncSession, None]:
         """Create a session and close it after use."""
 
-        if not isinstance(self.session_factory, async_sessionmaker):
+        if not isinstance(self.engine, AsyncEngine):
             raise AssertionError("This method is not supported for sync engines.")
 
         async with self.session_factory() as session:
