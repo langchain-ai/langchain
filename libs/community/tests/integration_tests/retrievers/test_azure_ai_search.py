c010ec8b71771dc3f54dc148475c90070b6a7c0b
ccurme <chester.curme@gmail.com>
2024-04-22 11:14:53 -0400

    patch: deprecate (a)get_relevant_documents (#20477)
    - `.get_relevant_documents(query)` -> `.invoke(query)`
    - `.get_relevant_documents(query=query)` -> `.invoke(query)`
    - `.get_relevant_documents(query, callbacks=callbacks)` ->
    `.invoke(query, config={"callbacks": callbacks})`
    - `.get_relevant_documents(query, **kwargs)` -> `.invoke(query,
    **kwargs)`
    
    ---------
    
    Co-authored-by: Erick Friis <erick@langchain.dev>

--- libs/community/tests/integration_tests/retrievers/test_azure_ai_search.py
+++ libs/community/tests/integration_tests/retrievers/test_azure_ai_search.py
@@ -7,7 +7,7 @@ from langchain_community.retrievers.azure_ai_search import (
 )
 
 
-def test_azure_ai_search_get_relevant_documents() -> None:
+def test_azure_ai_search_invoke() -> None:
     """Test valid call to Azure AI Search.
 
     In order to run this test, you should provide
@@ -17,17 +17,17 @@ def test_azure_ai_search_get_relevant_documents() -> None:
     """
     retriever = AzureAISearchRetriever()
 
-    documents = retriever.get_relevant_documents("what is langchain?")
+    documents = retriever.invoke("what is langchain?")
     for doc in documents:
         assert isinstance(doc, Document)
         assert doc.page_content
 
     retriever = AzureAISearchRetriever(top_k=1)
-    documents = retriever.get_relevant_documents("what is langchain?")
+    documents = retriever.invoke("what is langchain?")
     assert len(documents) <= 1
 
 
-async def test_azure_ai_search_aget_relevant_documents() -> None:
+async def test_azure_ai_search_ainvoke() -> None:
     """Test valid async call to Azure AI Search.
 
     In order to run this test, you should provide
@@ -35,36 +35,36 @@ async def test_azure_ai_search_aget_relevant_documents() -> None:
     as arguments for the AzureAISearchRetriever.
     """
     retriever = AzureAISearchRetriever()
-    documents = await retriever.aget_relevant_documents("what is langchain?")
+    documents = await retriever.ainvoke("what is langchain?")
     for doc in documents:
         assert isinstance(doc, Document)
         assert doc.page_content
 
 
-def test_azure_cognitive_search_get_relevant_documents() -> None:
+def test_azure_cognitive_search_invoke() -> None:
     """Test valid call to Azure Cognitive Search.
 
     This is to test backwards compatibility of the retriever
     """
     retriever = AzureCognitiveSearchRetriever()
 
-    documents = retriever.get_relevant_documents("what is langchain?")
+    documents = retriever.invoke("what is langchain?")
     for doc in documents:
         assert isinstance(doc, Document)
         assert doc.page_content
 
     retriever = AzureCognitiveSearchRetriever(top_k=1)
-    documents = retriever.get_relevant_documents("what is langchain?")
+    documents = retriever.invoke("what is langchain?")
     assert len(documents) <= 1
 
 
-async def test_azure_cognitive_search_aget_relevant_documents() -> None:
+async def test_azure_cognitive_search_ainvoke() -> None:
     """Test valid async call to Azure Cognitive Search.
 
     This is to test backwards compatibility of the retriever
     """
     retriever = AzureCognitiveSearchRetriever()
-    documents = await retriever.aget_relevant_documents("what is langchain?")
+    documents = await retriever.ainvoke("what is langchain?")
     for doc in documents:
         assert isinstance(doc, Document)
         assert doc.page_content
