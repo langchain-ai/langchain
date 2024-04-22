53196bafde858bb9a7b2043a613e20bec8cbff61
Eugene Yurtsev <eyurtsev@gmail.com>
2024-04-22 14:07:41 -0400

    x

--- libs/core/tests/unit_tests/indexing/test_indexing.py
+++ libs/core/tests/unit_tests/indexing/test_indexing.py
@@ -14,8 +14,8 @@ from unittest.mock import patch
 
 import pytest
 import pytest_asyncio
-from langchain_community.document_loaders.base import BaseLoader
 
+from langchain_core.document_loaders.base import BaseLoader
 from langchain_core.documents import Document
 from langchain_core.embeddings import Embeddings
 from langchain_core.indexing import aindex, index
