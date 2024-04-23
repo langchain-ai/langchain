3e52bd78055d29ae28584fbdcbd5758b77400c11
Eugene Yurtsev <eyurtsev@gmail.com>
2024-04-23 12:36:40 -0400

    x

--- libs/langchain/langchain/indexes/prompts/__init__.py
+++ libs/langchain/langchain/indexes/prompts/__init__.py
@@ -5,7 +5,8 @@ warn_deprecated(
     since="0.1.46",
     message=(
         "langchain.indexes.prompts will likely be removed in the future."
-        "If you're relying on these prompts, please open an issue on GitHub to explain your use case."
+        "If you're relying on these prompts, please open an issue on "
+        "GitHub to explain your use case."
     ),
     pending=True,
 )
