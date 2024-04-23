7a78068cd4b30301c31178270d52febf812d8746
Chester Curme <chester.curme@gmail.com>
2024-04-23 15:02:02 -0400

    fix test

--- libs/core/tests/unit_tests/documents/test_imports.py
+++ libs/core/tests/unit_tests/documents/test_imports.py
@@ -1,6 +1,11 @@
 from langchain_core.documents import __all__
 
-EXPECTED_ALL = ["Document", "BaseDocumentTransformer", "BaseDocumentCompressor"]
+EXPECTED_ALL = [
+    "Document",
+    "DocumentSearchHit",
+    "BaseDocumentTransformer",
+    "BaseDocumentCompressor",
+]
 
 
 def test_all_imports() -> None:
