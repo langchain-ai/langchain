fd11b244880042737030e0800fcd5d650bf3c508
Eugene Yurtsev <eyurtsev@gmail.com>
2024-04-23 12:08:39 -0400

    x

---
+++ libs/langchain/langchain/indexes/prompts/__init__.py
@@ -0,0 +1,11 @@
+"""Relevant prompts for constructing indexes."""
+from langchain_core._api import warn_deprecated
+
+warn_deprecated(
+    since="0.1.46",
+    message=(
+        "langchain.indexes.prompts will likely be removed in the future."
+        "If you're relying on these prompts, please open an issue on GitHub to explain your use case."
+    ),
+    pending=True,
+)
