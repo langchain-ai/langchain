04939a8bf9b112b77fda20cc43499c21e293516e
Erick Friis <erick@langchain.dev>
2024-04-24 16:07:23 -0700

    x

--- libs/partners/ai21/tests/integration_tests/test_standard.py
+++ libs/partners/ai21/tests/integration_tests/test_standard.py
@@ -1,5 +1,6 @@
 """Standard LangChain interface tests"""
 
+import time
 from typing import Type
 
 import pytest
@@ -9,7 +10,11 @@ from langchain_standard_tests.integration_tests import ChatModelIntegrationTests
 from langchain_ai21 import ChatAI21
 
 
-class TestAI21Standard(ChatModelIntegrationTests):
+class TestAI21J2(ChatModelIntegrationTests):
+    def teardown(self) -> None:
+        # avoid getting rate limited
+        time.sleep(1)
+
     @pytest.fixture
     def chat_model_class(self) -> Type[BaseChatModel]:
         return ChatAI21
@@ -20,24 +25,18 @@ class TestAI21Standard(ChatModelIntegrationTests):
             "model": "j2-ultra",
         }
 
-    @pytest.mark.xfail(reason="Emits AIMessage instead of AIMessageChunk.")
-    def test_stream(
-        self,
-        chat_model_class: Type[BaseChatModel],
-        chat_model_params: dict,
-    ) -> None:
-        super().test_stream(
-            chat_model_class,
-            chat_model_params,
-        )
 
-    @pytest.mark.xfail(reason="Emits AIMessage instead of AIMessageChunk.")
-    async def test_astream(
-        self,
-        chat_model_class: Type[BaseChatModel],
-        chat_model_params: dict,
-    ) -> None:
-        await super().test_astream(
-            chat_model_class,
-            chat_model_params,
-        )
+class TestAI21Jamba(ChatModelIntegrationTests):
+    def teardown(self) -> None:
+        # avoid getting rate limited
+        time.sleep(1)
+
+    @pytest.fixture
+    def chat_model_class(self) -> Type[BaseChatModel]:
+        return ChatAI21
+
+    @pytest.fixture
+    def chat_model_params(self) -> dict:
+        return {
+            "model": "jamba-instruct",
+        }
--- libs/partners/ai21/tests/unit_tests/test_standard.py
+++ libs/partners/ai21/tests/unit_tests/test_standard.py
@@ -9,7 +9,7 @@ from langchain_standard_tests.unit_tests import ChatModelUnitTests
 from langchain_ai21 import ChatAI21
 
 
-class TestAI21Standard(ChatModelUnitTests):
+class TestAI21J2(ChatModelUnitTests):
     @pytest.fixture
     def chat_model_class(self) -> Type[BaseChatModel]:
         return ChatAI21
@@ -18,5 +18,18 @@ class TestAI21Standard(ChatModelUnitTests):
     def chat_model_params(self) -> dict:
         return {
             "model": "j2-ultra",
+            "api_key": "test_api_key",
+        }
+
+
+class TestAI21Jamba(ChatModelUnitTests):
+    @pytest.fixture
+    def chat_model_class(self) -> Type[BaseChatModel]:
+        return ChatAI21
+
+    @pytest.fixture
+    def chat_model_params(self) -> dict:
+        return {
+            "model": "jamba-instruct",
             "api_key": "test_api_key",
         }
