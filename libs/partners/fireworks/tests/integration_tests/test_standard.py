d4216930197ed0b797d93c18f63fbabad5a8b8d4
Chester Curme <chester.curme@gmail.com>
2024-04-27 15:45:48 -0400

    xfail anthropic style messages

--- libs/partners/fireworks/tests/integration_tests/test_standard.py
+++ libs/partners/fireworks/tests/integration_tests/test_standard.py
@@ -21,3 +21,13 @@ class TestFireworksStandard(ChatModelIntegrationTests):
             "temperature": 0,
         }
 
+    @pytest.mark.xfail(reason="Not yet implemented.")
+    def test_tool_message_histories_list_content(
+        self,
+        chat_model_class: Type[BaseChatModel],
+        chat_model_params: dict,
+        chat_model_has_tool_calling: bool,
+    ) -> None:
+        super().test_tool_message_histories_list_content(
+            chat_model_class, chat_model_params, chat_model_has_tool_calling
+        )
