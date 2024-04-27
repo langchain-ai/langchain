52efccf2e9a3526537640ce06859e7fb7ed62464
Karim Lalani <karim.lalani@gmx.com>
2024-04-27 14:48:45 -0500

    fix for make api_docs_build

--- docs/api_reference/create_api_rst.py
+++ docs/api_reference/create_api_rst.py
@@ -359,7 +359,7 @@ def main(dirs: Optional[list] = None) -> None:
         dirs = [
             dir_
             for dir_ in os.listdir(ROOT_DIR / "libs")
-            if dir_ not in ("cli", "partners")
+            if dir_ not in ("cli", "partners", "standard-tests")
         ]
         dirs += os.listdir(ROOT_DIR / "libs" / "partners")
     for dir_ in dirs:
