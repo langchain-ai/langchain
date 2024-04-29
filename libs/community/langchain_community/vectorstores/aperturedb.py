022225a496bd7cee007d6d3f86b4bd58174df677
Gavin Matthews <gavin@aperturedata.io>
2024-04-29 02:40:45 +0000

    many tweaks

--- libs/community/langchain_community/vectorstores/aperturedb.py
+++ libs/community/langchain_community/vectorstores/aperturedb.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from itertools import cycle, islice, repeat
 import logging
 from typing import Any, Callable, Iterable, List, Optional, Tuple, Type
+import time
 
 # Third-party imports
 import aperturedb.Utils
@@ -17,13 +18,14 @@ ENGINE = "HNSW"
 METRIC = "L2"
 DESCRIPTOR_SET = "langchain"
 
+
 class ApertureDB(VectorStore):
     def __init__(self,
-                 embeddings:Embeddings,
-                 descriptor_set:str=DESCRIPTOR_SET,
-                 dimensions:Optional[int]=None,
-                 engine:str=ENGINE,
-                 metric:str=METRIC,
+                 embeddings: Embeddings,
+                 descriptor_set: str = DESCRIPTOR_SET,
+                 dimensions: Optional[int] = None,
+                 engine: str = ENGINE,
+                 metric: str = METRIC,
                  **kwargs):
         super().__init__(**kwargs)
         self.logger = logging.getLogger(__name__)
@@ -37,7 +39,8 @@ class ApertureDB(VectorStore):
         try:
             import aperturedb
         except ImportError:
-            self.logger.exception("ApertureDB is not installed. Please install it using 'pip install aperturedb'")
+            self.logger.exception(
+                "ApertureDB is not installed. Please install it using 'pip install aperturedb'")
             raise
 
         self.connection = aperturedb.Utils.create_connector()
@@ -66,7 +69,7 @@ class ApertureDB(VectorStore):
         else:
             raise ValueError(f"Unknown metric: {self.metric}")
 
-    def _find_or_add_descriptor_set(self, descriptor_set:str, dimensions:Optional[int]=None):
+    def _find_or_add_descriptor_set(self, descriptor_set: str, dimensions: Optional[int] = None):
         """Checks if the descriptor set exists, if not, creates it"""
         find_ds_query = [{
             "FindDescriptorSet": {
@@ -77,26 +80,29 @@ class ApertureDB(VectorStore):
             }
 
         }]
-        r,b = self.connection.query(find_ds_query)
+        r, b = self.connection.query(find_ds_query)
         assert self.connection.last_query_ok(), self.connection.get_last_response_str()
         # TODO: Could check that dimensions, engine and metric are the same
         if r[0]["FindDescriptorSet"]["count"] == 0:
             if self.dimensions is None:
-                self.dimensions = len(self.embedding_function.embed_query("test"))
-            success = self.utils.add_descriptorset(descriptor_set, self.dimensions, engine=self.engine, metric=[self.metric])
+                self.dimensions = len(
+                    self.embedding_function.embed_query("test"))
+            success = self.utils.add_descriptorset(
+                descriptor_set, self.dimensions, engine=self.engine, metric=[self.metric])
             assert success, self.connection.get_last_response_str()
             self.utils.create_entity_index("_Descriptor", "_uniqueid")
 
     def add_texts(self, texts: Iterable[str],
-        metadatas: Optional[List[dict]] = None,
-    ) -> List[str]:
+                  metadatas: Optional[List[dict]] = None,
+                  ) -> List[str]:
         if metadatas is not None:
-            assert len(texts) == len(metadatas), "Length of texts and metadatas should be the same"
+            assert len(texts) == len(
+                metadatas), "Length of texts and metadatas should be the same"
 
         embeddings = self.embedding_function.embed_documents(texts)
         if metadatas is None:
             metadatas = repeat({})
-        refs = cycle(range(1,100000,2))
+        refs = cycle(range(1, 100000, 2))
         data = zip(texts, embeddings, metadatas, refs)
 
         commands = []
@@ -135,13 +141,15 @@ class ApertureDB(VectorStore):
                 }
             })
 
-        status, responses, blobs = aperturedb.ParallelQuery.execute_batch(q=commands, blobs=blobs, db=self.connection, commands_per_query=3, blobs_per_query=2)
+        status, responses, blobs = aperturedb.ParallelQuery.execute_batch(
+            q=commands, blobs=blobs, db=self.connection, commands_per_query=3, blobs_per_query=2)
         assert status == 0, responses
-        unique_ids = [r["_uniqueid"] for r in responses[2::3]["FindDescriptor"]["entities"]]
+        unique_ids = [r["_uniqueid"]
+                      for r in responses[2::3]["FindDescriptor"]["entities"]]
         return unique_ids
 
     def delete(self, ids: List[str]) -> Optional[bool]:
-        refs = cycle(range(1,100000))
+        refs = cycle(range(1, 100000))
         commands = []
         for id, ref in zip(ids, refs):
             commands.extend([{
@@ -150,7 +158,7 @@ class ApertureDB(VectorStore):
                     "set": self.descriptor_set,
                     "unique": True,
                     "constraints": {
-                        "_uniqueid": [ "==", id ]
+                        "_uniqueid": ["==", id]
                     }
                 }
             }, {
@@ -166,15 +174,18 @@ class ApertureDB(VectorStore):
                 }
             }])
 
-        status, responses, blobs = aperturedb.ParallelQuery.execute_batch(q=commands, blobs=blobs, db=self.connection, commands_per_query=3, blobs_per_query=0)
+        status, responses, blobs = aperturedb.ParallelQuery.execute_batch(
+            q=commands, blobs=blobs, db=self.connection, commands_per_query=3, blobs_per_query=0)
         assert status == 0, responses
-        results = [r["DeleteDescriptor"]["results"]["count"] == 1 for r in responses[1::3]]
+        results = [r["DeleteDescriptor"]["results"]
+                   ["count"] == 1 for r in responses[1::3]]
         return results
 
     def similarity_search(
         self, query: str, k: int = 4, *args: Any, **kwargs: Any
     ) -> List[Document]:
         embedding = self.embedding_function.embed_query(query)
+        # print(f"query: {query}, embedding: {embedding}")
         return self.similarity_search_by_vector(embedding, k, *args, **kwargs)
 
     def similarity_search_with_score(
@@ -186,12 +197,12 @@ class ApertureDB(VectorStore):
     def _similarity_search_with_score_by_vector(
         self, embedding: List[float], k: int = 4, vectors=False
     ) -> List[Tuple[Document, float]]:
-        query = [ {
+        query = [{
             "FindDescriptor": {
                 "set": self.descriptor_set,
                 "k_neighbors": k,
                 "_ref": 1,
-                "results": { "list": ["_uniqueid"] },
+                "results": {"list": ["_uniqueid"]},
                 "blobs": vectors,
                 "distances": True,
             }
@@ -200,27 +211,36 @@ class ApertureDB(VectorStore):
                 "is_connected_to": {
                     "ref": 1
                 },
-                "results": { "all_properties": True },
+                "results": {"all_properties": True},
                 "blobs": True,
             }
         }]
+        # print(f"quey: {query}")
         blobs_in = [np.array(embedding, dtype=np.float32).tobytes()]
+        start_time = time.time()
         responses, blobs_out = self.connection.query(query, blobs_in)
+        #print(f"ApertureDB query completed in {time.time() - start_time:.1f}s")
+        # print(f"responses: {responses}")
         assert self.connection.last_query_ok(), self.connection.get_last_response_str()
         results = []
-        for i in range(0, len(responses), 2):
-            unique_ids = [d["_uniqueid"] for d in responses[i]["FindDescriptor"]["entities"]]
-            metadata = responses[i+1]["FindBlob"]["entities"][0]
-            distance = responses[i]["FindDescriptor"]["entities"][0]["_distance"]
-            blob_index = metadata["_blob_index"]
+        descriptors = responses[0]["FindDescriptor"]["entities"]
+        blobs = responses[1]["FindBlob"]["entities"]
+        for d, b in zip(descriptors, blobs):
+            unique_id = d["_uniqueid"]
+            distance = d["_distance"]
+            blob_index = b["_blob_index"]
             text = blobs_out[blob_index].decode()
             # consider filtering internal properties out of metadata
             if vectors:
-                vector_blob_index = responses[i]["FindDescriptor"]["entities"][0]["_blob_index"]
-                vector = np.frombuffer(blobs_out[vector_blob_index], dtype=np.float32)
-                results.append((Document(page_content=text, _uniqueid=unique_ids, **metadata), distance, vector))
+                vector_blob_index = d["_blob_index"]
+                vector = np.frombuffer(
+                    blobs_out[vector_blob_index], dtype=np.float32)
+                results.append(
+                    (Document(page_content=text, **d), distance, vector))
             else:
-                results.append((Document(page_content=text, **metadata), distance))
+                results.append(
+                    (Document(page_content=text, **d), distance))
+        # print(f"results: {results}")
         return results
 
     def similarity_search_by_vector(
@@ -238,6 +258,7 @@ class ApertureDB(VectorStore):
         **kwargs: Any,
     ) -> List[Document]:
         embedding = self.embedding_function.embed_query(query)
+        # print(f"query-{query}, embedding: {embedding}")
         return self.max_marginal_relevance_search_by_vector(embedding, k, fetch_k, lambda_mult, **kwargs)
 
     def _vector_similarity(self, vector1: List[float], vector2: List[float]) -> float:
@@ -246,7 +267,8 @@ class ApertureDB(VectorStore):
         elif self.metric == "IP":
             distance = np.dot(vector1, vector2)
         elif self.metric == "CS":
-            distance = np.dot(vector1, vector2) / (np.linalg.norm(vector1) * np.linalg.norm(vector2))
+            distance = np.dot(vector1, vector2) / \
+                (np.linalg.norm(vector1) * np.linalg.norm(vector2))
         else:
             raise ValueError(f"Unknown metric: {self.metric}")
         similarity = self._select_relevance_score_fn()(distance)
@@ -260,14 +282,16 @@ class ApertureDB(VectorStore):
         lambda_mult: float = 0.5,
         **kwargs: Any,
     ) -> List[Document]:
-        results = self._similarity_search_with_score_by_vector(embedding, fetch_k, vectors=True)
-        query_similarity = [ self._vector_similarity(embedding, vector) for _, _, vector in results]
+        results = self._similarity_search_with_score_by_vector(
+            embedding, fetch_k, vectors=True)
+        query_similarity = [self._vector_similarity(
+            embedding, vector) for _, _, vector in results]
         document_similarity = {}
         for i, (_, _, vector) in enumerate(results):
-            for j, (_, _, vector2) in enumerate(results[i+1:]):
+            for j, (_, _, vector2) in enumerate(results[i+1:], i+1):
                 similarity = self._vector_similarity(vector, vector2)
-                document_similarity[(i,j)] = similarity
-                document_similarity[(j,i)] = similarity
+                document_similarity[(i, j)] = similarity
+                document_similarity[(j, i)] = similarity
 
         selected = []
         unselected = list(range(len(results)))
@@ -277,11 +301,15 @@ class ApertureDB(VectorStore):
                 selected.append(0)
                 unselected.remove(0)
             else:
-                selected_unselected_similarity = np.array([[document_similarity[(i,j)] for j in unselected] for i in selected])
-                worst_similarity = np.max(selected_unselected_similarity, axis=0)
-                relevance_scores = [query_similarity[i] for i in unselected]
-                scores = (1 - lambda_mult) * worst_similarity + lambda_mult * relevance_scores
-                max_index = np.argmax(scores)
+                selected_unselected_similarity = np.array(
+                    [[document_similarity[(i, j)] for j in unselected] for i in selected])
+                worst_similarity = np.max(
+                    selected_unselected_similarity, axis=0)
+                relevance_scores = np.array([query_similarity[i] for i in unselected])
+                scores = (1 - lambda_mult) * worst_similarity + \
+                    lambda_mult * relevance_scores
+                max_index = unselected[np.argmax(scores)]
+                #print(f"max_index: {max_index}, selected: {selected}, unselected: {unselected}")
                 selected.append(max_index)
                 unselected.remove(max_index)
         results2 = [results[i][0] for i in selected]
