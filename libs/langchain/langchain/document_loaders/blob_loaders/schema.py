298779a8b86c138c4825cb2b2798289aa630628c
Eugene Yurtsev <eyurtsev@gmail.com>
2024-04-29 13:01:31 -0400

    x

--- libs/langchain/langchain/document_loaders/blob_loaders/schema.py
+++ libs/langchain/langchain/document_loaders/blob_loaders/schema.py
@@ -1,7 +1,4 @@
-from langchain_community.document_loaders.blob_loaders.schema import (
-    Blob,
-    BlobLoader,
-    PathLike,
-)
+from langchain_community.document_loaders.blob_loaders.schema import PathLike
+from langchain_core.document_loaders import Blob, BlobLoader
 
 __all__ = ["PathLike", "Blob", "BlobLoader"]
