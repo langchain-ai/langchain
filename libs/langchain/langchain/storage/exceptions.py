afa3797b3dedc106fc8cc1d4ed883211e41acf3b
Eugene Yurtsev <eyurtsev@gmail.com>
2024-04-30 13:58:12 -0400

    x

--- libs/langchain/langchain/storage/exceptions.py
+++ libs/langchain/langchain/storage/exceptions.py
@@ -1,3 +1,25 @@
+from typing import TYPE_CHECKING, Any
+
 from langchain_core.stores import InvalidKeyException
 
-__all__ = ["InvalidKeyException"]
+from langchain._api import create_importer
+
+if TYPE_CHECKING:
+    from langchain_community.storage.exceptions import InvalidKeyException
+
+# Create a way to dynamically look up deprecated imports.
+# Used to consolidate logic for raising deprecation warnings and
+# handling optional imports.
+DEPRECATED_LOOKUP = {"InvalidKeyException": "langchain_community.storage.exceptions"}
+
+_import_attribute = create_importer(__package__, deprecated_lookups=DEPRECATED_LOOKUP)
+
+
+def __getattr__(name: str) -> Any:
+    """Look up attributes dynamically."""
+    return _import_attribute(name)
+
+
+__all__ = [
+    "InvalidKeyException",
+]
