dd1121df719c899d2b9d6d4a34496c7221696695
Eugene Yurtsev <eyurtsev@gmail.com>
2024-04-30 13:15:18 -0400

    Update libs/langchain/langchain/indexes/prompts/__init__.py

--- libs/langchain/langchain/indexes/prompts/__init__.py
+++ libs/langchain/langchain/indexes/prompts/__init__.py
@@ -2,7 +2,7 @@
 from langchain_core._api import warn_deprecated
 
 warn_deprecated(
-    since="0.1.46",
+    since="0.1.47",
     message=(
         "langchain.indexes.prompts will likely be removed in the future."
         "If you're relying on these prompts, please open an issue on "
