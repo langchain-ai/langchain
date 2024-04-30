2c2ee15c3f4bd4041c46f81484acd5ecf978fb28
Eugene Yurtsev <eyurtsev@gmail.com>
2024-04-30 12:46:15 -0400

    x

--- libs/langchain/langchain/embeddings/__init__.py
+++ libs/langchain/langchain/embeddings/__init__.py
@@ -12,14 +12,10 @@ from different APIs and services.
 
 
 import logging
-import warnings
 from typing import TYPE_CHECKING, Any
-
-from langchain_core._api import LangChainDeprecationWarning
 
 from langchain._api import create_importer
 from langchain.embeddings.cache import CacheBackedEmbeddings
-from langchain.utils.interactive_env import is_interactive_env
 
 if TYPE_CHECKING:
     from langchain_community.embeddings import (
