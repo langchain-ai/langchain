f1a496dd26805259e1ed61021e2b2b60aa22393a
Eugene Yurtsev <eyurtsev@gmail.com>
2024-05-01 16:51:00 -0400

    x

--- libs/langchain/langchain/llms/titan_takeoff_pro.py
+++ libs/langchain/langchain/llms/titan_takeoff_pro.py
@@ -1,3 +1,24 @@
-from langchain_community.llms.titan_takeoff import TitanTakeoff as TitanTakeoffPro
+from typing import TYPE_CHECKING, Any, Type
+
+from langchain._api import warn_deprecated
+
+if TYPE_CHECKING:
+    from langchain_community.llms.titan_takeoff import TitanTakeoff as TitanTakeoffPro
+
+
+def _get_titan_pro() -> Type[TitanTakeoffPro]:
+    from langchain_community.llms.titan_takeoff import TitanTakeoff as TitanTakeoffPro
+
+    warn_deprecated(
+        "0.1.0", "Deprecated in favor of langchain_community.llms.TitanTakeoff."
+    )
+
+    return TitanTakeoffPro
+
+
+def __getattr__(name: str) -> Any:
+    if name == "TitanTakeoff":
+        return _get_titan_pro()
+
 
 __all__ = ["TitanTakeoffPro"]
