3551a2d9aaff218cd2e14c7865655a47222010b8
Mark Jan van Kampen <m.vankampen@freeday.ai>
2024-05-01 09:09:28 +0200

    Refactor to cleaner code

--- libs/partners/openai/langchain_openai/chat_models/base.py
+++ libs/partners/openai/langchain_openai/chat_models/base.py
@@ -719,12 +719,14 @@ class ChatOpenAI(BaseChatModel):
         if sys.version_info[1] <= 7:
             return super().get_num_tokens_from_messages(messages)
         model, encoding = self._get_encoding_model()
-        if model.startswith("gpt-3.5-turbo-0301") or model.startswith("gpt-35-turbo-0301"):
+        # Azure OpenAI models do not have dots in their names, so we remove them for consistency
+        model = model.replace(".", "")
+        if model.startswith("gpt-35-turbo-0301"):
             # every message follows <im_start>{role/name}\n{content}<im_end>\n
             tokens_per_message = 4
             # if there's a name, the role is omitted
             tokens_per_name = -1
-        elif model.startswith("gpt-3.5-turbo") or model.startswith("gpt-35-turbo") or model.startswith("gpt-4"):
+        elif model.startswith("gpt-35-turbo") or model.startswith("gpt-4"):
             tokens_per_message = 3
             tokens_per_name = 1
         else:
