152c5751728169d04a9ad001549e4a79716840da
Anush008 <anushshetty90@gmail.com>
2024-05-02 21:58:56 +0530

    chore: Happy linter

--- libs/partners/qdrant/tests/integration_tests/conftest.py
+++ libs/partners/qdrant/tests/integration_tests/conftest.py
@@ -5,7 +5,7 @@ from qdrant_client import QdrantClient
 from tests.integration_tests.fixtures import qdrant_locations
 
 
-def pytest_sessionfinish(session, exitstatus):
+def pytest_sessionfinish() -> None:
     """Clean up all collections after the test session."""
     for location in qdrant_locations():
         client = QdrantClient(location=location, api_key=os.getenv("QDRANT_API_KEY"))
