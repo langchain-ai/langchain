89172aad27a006d5f83bee497530c6960a78cbd8
Eugene Yurtsev <eyurtsev@gmail.com>
2024-05-02 10:03:13 -0400

    x

--- libs/langchain/langchain/memory/chat_message_histories/in_memory.py
+++ libs/langchain/langchain/memory/chat_message_histories/in_memory.py
@@ -1,3 +1,21 @@
-from langchain_community.chat_message_histories.in_memory import ChatMessageHistory
+from typing import TYPE_CHECKING, Any
+
+from langchain._api import create_importer
+
+if TYPE_CHECKING:
+    from langchain_community.chat_message_histories.in_memory import ChatMessageHistory
+
+# Create a way to dynamically look up deprecated imports.
+# Used to consolidate logic for raising deprecation warnings and
+# handling optional imports.
+DEPRECATED_LOOKUP = {"ChatMessageHistory": "langchain_community.chat_message_histories"}
+
+_import_attribute = create_importer(__package__, deprecated_lookups=DEPRECATED_LOOKUP)
+
+
+def __getattr__(name: str) -> Any:
+    """Look up attributes dynamically."""
+    return _import_attribute(name)
+
 
 __all__ = ["ChatMessageHistory"]
