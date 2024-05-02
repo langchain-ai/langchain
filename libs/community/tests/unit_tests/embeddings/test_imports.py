4a70448f1552edcd78dddeb4661b2c670bf13109
jorge piedrahita ortiz <jhpiedrahitao97@gmail.com>
2024-05-02 16:55:19 -0500

    embeddings imports  unitest updated

--- libs/community/tests/unit_tests/embeddings/test_imports.py
+++ libs/community/tests/unit_tests/embeddings/test_imports.py
@@ -66,6 +66,7 @@ EXPECTED_ALL = [
     "QuantizedBiEncoderEmbeddings",
     "NeMoEmbeddings",
     "SparkLLMTextEmbeddings",
+    "SambaStudioEmbeddings",
     "TitanTakeoffEmbed",
     "QuantizedBgeEmbeddings",
     "PremAIEmbeddings",
