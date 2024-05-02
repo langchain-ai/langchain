e209307375072a90a94c4660651bb67e4a186590
Eugene Yurtsev <eyurtsev@gmail.com>
2024-05-02 10:57:24 -0400

    x

--- libs/langchain/langchain/memory/motorhead_memory.py
+++ libs/langchain/langchain/memory/motorhead_memory.py
@@ -1,3 +1,23 @@
-from langchain_community.memory.motorhead_memory import MotorheadMemory
+from typing import TYPE_CHECKING, Any
 
-__all__ = ["MotorheadMemory"]
+from langchain._api import create_importer
+
+if TYPE_CHECKING:
+    from langchain_community.memory.motorhead_memory import MotorheadMemory
+
+# Create a way to dynamically look up deprecated imports.
+# Used to consolidate logic for raising deprecation warnings and
+# handling optional imports.
+DEPRECATED_LOOKUP = {"MotorheadMemory": "langchain_community.memory.motorhead_memory"}
+
+_import_attribute = create_importer(__package__, deprecated_lookups=DEPRECATED_LOOKUP)
+
+
+def __getattr__(name: str) -> Any:
+    """Look up attributes dynamically."""
+    return _import_attribute(name)
+
+
+__all__ = [
+    "MotorheadMemory",
+]
