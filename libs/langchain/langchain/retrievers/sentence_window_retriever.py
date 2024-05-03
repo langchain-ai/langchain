a77318faf8276d8dd208dc7827a54b6422ca5b4a
Roshan Santhosh <roshan.santhosh@gmail.com>
2024-05-03 09:09:45 -0700

    Update sentence_window_retriever.py

--- libs/langchain/langchain/retrievers/sentence_window_retriever.py
+++ libs/langchain/langchain/retrievers/sentence_window_retriever.py
@@ -1,3 +1,5 @@
-from langchain_community.retrievers.sentence_window_retriever import SentenceWindowRetriever
+from langchain_community.retrievers.sentence_window_retriever import (
+    SentenceWindowRetriever,
+)
 
 __all__ = ["SentenceWindowRetriever"]
