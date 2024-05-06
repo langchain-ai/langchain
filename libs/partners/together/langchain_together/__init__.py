72079d089b4474398a8948f5198cf418b453e44d
Hassan El Mghari <hassan4709@gmail.com>
2024-05-06 12:40:07 -0700

    changed completions to llms

--- libs/partners/together/langchain_together/__init__.py
+++ libs/partners/together/langchain_together/__init__.py
@@ -1,5 +1,5 @@
 from langchain_together.chat_models import ChatTogether
-from langchain_together.completions import Together
+from langchain_together.llms import Together
 from langchain_together.embeddings import TogetherEmbeddings
 
 __all__ = ["ChatTogether", "Together", "TogetherEmbeddings"]
