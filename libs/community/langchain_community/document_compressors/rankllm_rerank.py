ebe38dd5da6c5385d748ce16ce5581c2b15f4642
Eric Z <ericzh1616@gmail.com>
2024-05-08 01:35:27 +0000

    move test and format

--- libs/community/langchain_community/document_compressors/rankllm_rerank.py
+++ libs/community/langchain_community/document_compressors/rankllm_rerank.py
@@ -4,12 +4,11 @@ from copy import deepcopy
 from enum import Enum
 from typing import TYPE_CHECKING, Any, Dict, Optional, Sequence
 
+from langchain.retrievers.document_compressors.base import BaseDocumentCompressor
+from langchain_core.callbacks.manager import Callbacks
 from langchain_core.documents import Document
 from langchain_core.pydantic_v1 import Extra, Field, PrivateAttr, root_validator
-from langchain_core.callbacks.manager import Callbacks
 from langchain_core.utils import get_from_dict_or_env
-
-from langchain.retrievers.document_compressors.base import BaseDocumentCompressor
 
 if TYPE_CHECKING:
     from rank_llm.data import Candidate, Query, Request
