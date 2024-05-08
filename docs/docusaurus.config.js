bbeda7a8715843c445acc1cf4f216b7ea5a8ade7
Erick Friis <erick@langchain.dev>
2024-05-07 21:33:20 -0700

    build dir

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: "https://python.langchain.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/v0.2",
+  baseUrl: "/v0.2/",
   trailingSlash: true,
   onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
