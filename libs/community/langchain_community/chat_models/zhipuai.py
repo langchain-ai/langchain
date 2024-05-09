1d658c0a8c4f1ccb7ff60ba72254b417765ec406
GUOHUI_WU <wuguohui@winsing.net>
2024-05-09 10:04:59 +0800

    fix Import block is un-sorted or un-formatted

--- libs/community/langchain_community/chat_models/zhipuai.py
+++ libs/community/langchain_community/chat_models/zhipuai.py
@@ -30,7 +30,7 @@ from langchain_core.messages import (
     SystemMessage,
     SystemMessageChunk,
     ToolMessage,
-    ToolMessageChunk
+    ToolMessageChunk,
 )
 from langchain_core.outputs import ChatGeneration, ChatGenerationChunk, ChatResult
 from langchain_core.pydantic_v1 import BaseModel, Field, root_validator
