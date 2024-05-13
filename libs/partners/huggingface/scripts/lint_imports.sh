9b51ca08bcc3b74341e2257e5a57f717af719d21
Erick Friis <erick@langchain.dev>
2024-05-13 14:52:18 -0700

    huggingface: fix community dep checking (#21628)

--- libs/partners/huggingface/scripts/lint_imports.sh
+++ libs/partners/huggingface/scripts/lint_imports.sh
@@ -8,7 +8,7 @@ errors=0
 # make sure not importing from langchain or langchain_experimental
 git --no-pager grep '^from langchain\.' . && errors=$((errors+1))
 git --no-pager grep '^from langchain_experimental\.' . && errors=$((errors+1))
-# git --no-pager grep '^from langchain_community\.' . && errors=$((errors+1))
+git --no-pager grep '^from langchain_community\.' . && errors=$((errors+1))
 
 # Decide on an exit status based on the errors
 if [ "$errors" -gt 0 ]; then
