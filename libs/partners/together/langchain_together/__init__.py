e958add55a4af1e4f71c5ac0fa4765bcd3dbb5c1
MAC <mac@MACs-MacBook-Pro.local>
2024-05-14 02:08:44 +0500

    [chore]

--- libs/partners/together/langchain_together/__init__.py
+++ libs/partners/together/langchain_together/__init__.py
@@ -1,5 +1,6 @@
 from langchain_together.chat_models import ChatTogether
 from langchain_together.embeddings import TogetherEmbeddings
 from langchain_together.llms import Together
+from langchain_together.output_parsers import JsonOutputFunctionsParser
 
-__all__ = ["ChatTogether", "Together", "TogetherEmbeddings"]
+__all__ = ["ChatTogether", "Together", "TogetherEmbeddings","JsonOutputFunctionsParser"]
