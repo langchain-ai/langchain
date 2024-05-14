971e8f22fc22d7a7c2d30bce20cb35f0c6926146
Eugene Yurtsev <eyurtsev@gmail.com>
2024-05-14 16:47:32 -0400

    x

--- libs/langchain/tests/unit_tests/llms/test_base.py
+++ libs/langchain/tests/unit_tests/llms/test_base.py
@@ -1,8 +1,9 @@
 """Test base LLM functionality."""
-from langchain.globals import get_llm_cache, set_llm_cache
-from langchain.llms.base import __all__
 from langchain_core.caches import InMemoryCache
 from langchain_core.outputs import Generation, LLMResult
+
+from langchain.globals import get_llm_cache, set_llm_cache
+from langchain.llms.base import __all__
 from tests.unit_tests.llms.fake_llm import FakeLLM
 
 EXPECTED_ALL = [
