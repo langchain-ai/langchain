264d23e6517d19952a7da4b47d82ec8982fce38b
Chad Juliano <cjuliano@kinetica.com>
2024-05-14 19:37:13 +0000

    copy latest changes from community

--- libs/partners/kinetica/langchain_kinetica/vectorstores.py
+++ libs/partners/kinetica/langchain_kinetica/vectorstores.py
@@ -1,179 +1,300 @@
-from __future__ import annotations
+import os
+from typing import List
 
-import asyncio
-from functools import partial
-from typing import (
-    TYPE_CHECKING,
-    Any,
-    Callable,
-    Iterable,
-    List,
-    Optional,
-    Tuple,
-    Type,
-    TypeVar,
+import pytest
+from langchain_core.documents import Document
+
+from langchain_community.embeddings.openai import OpenAIEmbeddings
+from langchain_community.vectorstores import (
+    DistanceStrategy,
+    Kinetica,
+    KineticaSettings,
 )
+from tests.integration_tests.vectorstores.fake_embeddings import FakeEmbeddings
 
-from langchain_core.embeddings import Embeddings
-from langchain_core.vectorstores import VectorStore
-
-if TYPE_CHECKING:
-    from langchain_core.documents import Document
-
-VST = TypeVar("VST", bound=VectorStore)
+DIMENSIONS = 3
+HOST = os.getenv("KINETICA_HOST", "http://127.0.0.1:9191")
+USERNAME = os.getenv("KINETICA_USERNAME", "")
+PASSWORD = os.getenv("KINETICA_PASSWORD", "")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
 
 
-class KineticaVectorStore(VectorStore):
-    """Interface for vector store.
+class FakeEmbeddingsWithAdaDimension(FakeEmbeddings):
+    """Fake embeddings functionality for testing."""
 
-    Example:
-        .. code-block:: python
+    def embed_documents(self, texts: List[str]) -> List[List[float]]:
+        """Return simple embeddings."""
+        return [[float(1.0)] * (DIMENSIONS - 1) + [float(i)] for i in range(len(texts))]
 
-            from langchain_kinetica.vectorstores import KineticaVectorStore
+    def embed_query(self, text: str) -> List[float]:
+        """Return simple embeddings."""
+        return [float(1.0)] * (DIMENSIONS - 1) + [float(0.0)]
 
-            vectorstore = KineticaVectorStore()
-    """
 
-    def add_texts(
-        self,
-        texts: Iterable[str],
-        metadatas: Optional[List[dict]] = None,
-        **kwargs: Any,
-    ) -> List[str]:
-        raise NotImplementedError
+@pytest.fixture
+def create_config() -> KineticaSettings:
+    return KineticaSettings(host=HOST, username=USERNAME, password=PASSWORD)
 
-    async def aadd_texts(
-        self,
-        texts: Iterable[str],
-        metadatas: Optional[List[dict]] = None,
-        **kwargs: Any,
-    ) -> List[str]:
-        return await asyncio.get_running_loop().run_in_executor(
-            None, partial(self.add_texts, **kwargs), texts, metadatas
-        )
 
-    def delete(self, ids: Optional[List[str]] = None, **kwargs: Any) -> Optional[bool]:
-        raise NotImplementedError
+@pytest.mark.requires("gpudb")
+def test_kinetica(create_config: KineticaSettings) -> None:
+    """Test end to end construction and search."""
+    texts = ["foo", "bar", "baz"]
+    metadatas = [{"text": text} for text in texts]
+    docsearch = Kinetica.from_texts(
+        config=create_config,
+        texts=texts,
+        metadatas=metadatas,
+        embedding=FakeEmbeddingsWithAdaDimension(),
+        collection_name="test_kinetica",
+        pre_delete_collection=True,
+    )
+    output = docsearch.similarity_search("foo", k=1)
+    assert output[0].page_content == "foo"
 
-    async def adelete(
-        self, ids: Optional[List[str]] = None, **kwargs: Any
-    ) -> Optional[bool]:
-        raise NotImplementedError
 
-    def similarity_search(
-        self, query: str, k: int = 4, **kwargs: Any
-    ) -> List[Document]:
-        raise NotImplementedError
+@pytest.mark.requires("gpudb")
+def test_kinetica_embeddings(create_config: KineticaSettings) -> None:
+    """Test end to end construction with embeddings and search."""
+    texts = ["foo", "bar", "baz"]
+    text_embeddings = FakeEmbeddingsWithAdaDimension().embed_documents(texts)
+    text_embedding_pairs = list(zip(texts, text_embeddings))
+    docsearch = Kinetica.from_embeddings(
+        config=create_config,
+        text_embeddings=text_embedding_pairs,
+        embedding=FakeEmbeddingsWithAdaDimension(),
+        collection_name="test_kinetica_embeddings",
+        pre_delete_collection=False,
+    )
+    output = docsearch.similarity_search("foo", k=1)
+    assert output == [Document(page_content="foo")]
 
-    async def asimilarity_search(
-        self, query: str, k: int = 4, **kwargs: Any
-    ) -> List[Document]:
-        # This is a temporary workaround to make the similarity search
-        # asynchronous. The proper solution is to make the similarity search
-        # asynchronous in the vector store implementations.
-        func = partial(self.similarity_search, query, k=k, **kwargs)
-        return await asyncio.get_event_loop().run_in_executor(None, func)
 
-    def similarity_search_with_score(
-        self, *args: Any, **kwargs: Any
-    ) -> List[Tuple[Document, float]]:
-        raise NotImplementedError
+@pytest.mark.requires("gpudb")
+def test_kinetica_with_metadatas(create_config: KineticaSettings) -> None:
+    """Test end to end construction and search."""
+    texts = ["foo", "bar", "baz"]
+    metadatas = [{"page": str(i)} for i in range(len(texts))]
+    docsearch = Kinetica.from_texts(
+        config=create_config,
+        texts=texts,
+        metadatas=metadatas,
+        embedding=FakeEmbeddingsWithAdaDimension(),
+        collection_name="test_kinetica_with_metadatas",
+        pre_delete_collection=False,
+    )
 
-    async def asimilarity_search_with_score(
-        self, *args: Any, **kwargs: Any
-    ) -> List[Tuple[Document, float]]:
-        # This is a temporary workaround to make the similarity search
-        # asynchronous. The proper solution is to make the similarity search
-        # asynchronous in the vector store implementations.
-        func = partial(self.similarity_search_with_score, *args, **kwargs)
-        return await asyncio.get_event_loop().run_in_executor(None, func)
+    output = docsearch.similarity_search("foo", k=1)
+    assert output == [Document(page_content="foo", metadata={"page": "0"})]
 
-    def similarity_search_by_vector(
-        self, embedding: List[float], k: int = 4, **kwargs: Any
-    ) -> List[Document]:
-        raise NotImplementedError
 
-    async def asimilarity_search_by_vector(
-        self, embedding: List[float], k: int = 4, **kwargs: Any
-    ) -> List[Document]:
-        # This is a temporary workaround to make the similarity search
-        # asynchronous. The proper solution is to make the similarity search
-        # asynchronous in the vector store implementations.
-        func = partial(self.similarity_search_by_vector, embedding, k=k, **kwargs)
-        return await asyncio.get_event_loop().run_in_executor(None, func)
+@pytest.mark.requires("gpudb")
+def test_kinetica_with_metadatas_with_scores(create_config: KineticaSettings) -> None:
+    """Test end to end construction and search."""
+    texts = ["foo", "bar", "baz"]
+    metadatas = [{"page": str(i)} for i in range(len(texts))]
+    docsearch = Kinetica.from_texts(
+        config=create_config,
+        texts=texts,
+        metadatas=metadatas,
+        embedding=FakeEmbeddingsWithAdaDimension(),
+        collection_name="test_kinetica_with_metadatas_with_scores",
+        pre_delete_collection=False,
+    )
 
-    def max_marginal_relevance_search(
-        self,
-        query: str,
-        k: int = 4,
-        fetch_k: int = 20,
-        lambda_mult: float = 0.5,
-        **kwargs: Any,
-    ) -> List[Document]:
-        raise NotImplementedError
+    output = docsearch.similarity_search_with_score("foo", k=1)
+    assert output == [(Document(page_content="foo", metadata={"page": "0"}), 0.0)]
 
-    async def amax_marginal_relevance_search(
-        self,
-        query: str,
-        k: int = 4,
-        fetch_k: int = 20,
-        lambda_mult: float = 0.5,
-        **kwargs: Any,
-    ) -> List[Document]:
-        # This is a temporary workaround to make the similarity search
-        # asynchronous. The proper solution is to make the similarity search
-        # asynchronous in the vector store implementations.
-        func = partial(
-            self.max_marginal_relevance_search,
-            query,
-            k=k,
-            fetch_k=fetch_k,
-            lambda_mult=lambda_mult,
-            **kwargs,
-        )
-        return await asyncio.get_event_loop().run_in_executor(None, func)
 
-    def max_marginal_relevance_search_by_vector(
-        self,
-        embedding: List[float],
-        k: int = 4,
-        fetch_k: int = 20,
-        lambda_mult: float = 0.5,
-        **kwargs: Any,
-    ) -> List[Document]:
-        raise NotImplementedError
+@pytest.mark.requires("gpudb")
+def test_kinetica_with_filter_match(create_config: KineticaSettings) -> None:
+    """Test end to end construction and search."""
+    texts = ["foo", "bar", "baz"]
+    metadatas = [{"page": str(i)} for i in range(len(texts))]
+    docsearch = Kinetica.from_texts(
+        config=create_config,
+        texts=texts,
+        metadatas=metadatas,
+        embedding=FakeEmbeddingsWithAdaDimension(),
+        collection_name="test_kinetica_with_filter_match",
+        pre_delete_collection=False,
+    )
 
-    async def amax_marginal_relevance_search_by_vector(
-        self,
-        embedding: List[float],
-        k: int = 4,
-        fetch_k: int = 20,
-        lambda_mult: float = 0.5,
-        **kwargs: Any,
-    ) -> List[Document]:
-        raise NotImplementedError
+    output = docsearch.similarity_search_with_score("foo", k=1, filter={"page": "0"})
+    assert output == [(Document(page_content="foo", metadata={"page": "0"}), 0.0)]
 
-    @classmethod
-    def from_texts(
-        cls: Type[VST],
-        texts: List[str],
-        embedding: Embeddings,
-        metadatas: Optional[List[dict]] = None,
-        **kwargs: Any,
-    ) -> VST:
-        raise NotImplementedError
 
-    @classmethod
-    async def afrom_texts(
-        cls: Type[VST],
-        texts: List[str],
-        embedding: Embeddings,
-        metadatas: Optional[List[dict]] = None,
-        **kwargs: Any,
-    ) -> VST:
-        return await asyncio.get_running_loop().run_in_executor(
-            None, partial(cls.from_texts, **kwargs), texts, embedding, metadatas
-        )
+@pytest.mark.requires("gpudb")
+def test_kinetica_with_filter_distant_match(create_config: KineticaSettings) -> None:
+    """Test end to end construction and search."""
+    texts = ["foo", "bar", "baz"]
+    metadatas = [{"page": str(i)} for i in range(len(texts))]
+    docsearch = Kinetica.from_texts(
+        config=create_config,
+        texts=texts,
+        metadatas=metadatas,
+        embedding=FakeEmbeddingsWithAdaDimension(),
+        collection_name="test_kinetica_with_filter_distant_match",
+        pre_delete_collection=False,
+    )
 
-    def _select_relevance_score_fn(self) -> Callable[[float], float]:
-        raise NotImplementedError
+    output = docsearch.similarity_search_with_score("foo", k=1, filter={"page": "2"})
+    assert output == [(Document(page_content="baz", metadata={"page": "2"}), 2.0)]
+
+
+@pytest.mark.skip(reason="Filter condition has IN clause")
+@pytest.mark.requires("gpudb")
+def test_kinetica_with_filter_in_set(create_config: KineticaSettings) -> None:
+    """Test end to end construction and search."""
+    texts = ["foo", "bar", "baz"]
+    metadatas = [{"page": str(i)} for i in range(len(texts))]
+    docsearch = Kinetica.from_texts(
+        config=create_config,
+        texts=texts,
+        metadatas=metadatas,
+        embedding=FakeEmbeddingsWithAdaDimension(),
+        collection_name="test_kinetica_with_filter_in_set",
+        pre_delete_collection=False,
+    )
+
+    output = docsearch.similarity_search_with_score(
+        "foo", k=2, filter={"page": {"IN": ["0", "2"]}}
+    )
+    assert output == [
+        (Document(page_content="foo", metadata={"page": "0"}), 0.0),
+        (Document(page_content="baz", metadata={"page": "2"}), 0.0013003906671379406),
+    ]
+
+
+@pytest.mark.requires("gpudb")
+def test_kinetica_relevance_score(create_config: KineticaSettings) -> None:
+    """Test end to end construction and search."""
+    texts = ["foo", "bar", "baz"]
+    metadatas = [{"page": str(i)} for i in range(len(texts))]
+    docsearch = Kinetica.from_texts(
+        config=create_config,
+        texts=texts,
+        metadatas=metadatas,
+        embedding=FakeEmbeddingsWithAdaDimension(),
+        collection_name="test_kinetica_relevance_score",
+        pre_delete_collection=False,
+    )
+
+    output = docsearch.similarity_search_with_relevance_scores("foo", k=3)
+    assert output == [
+        (Document(page_content="foo", metadata={"page": "0"}), 1.0),
+        (Document(page_content="bar", metadata={"page": "1"}), 0.29289321881345254),
+        (Document(page_content="baz", metadata={"page": "2"}), -0.4142135623730949),
+    ]
+
+
+@pytest.mark.requires("openai", "gpudb")
+def test_kinetica_max_marginal_relevance_search(
+    create_config: KineticaSettings,
+) -> None:
+    """Test end to end construction and search."""
+    openai = OpenAIEmbeddings(api_key=OPENAI_API_KEY)
+    texts = ["foo", "bar", "baz"]
+    docsearch = Kinetica.from_texts(
+        config=create_config,
+        texts=texts,
+        embedding=openai,
+        distance_strategy=DistanceStrategy.COSINE,
+        collection_name="test_kinetica_max_marginal_relevance_search",
+        pre_delete_collection=False,
+    )
+
+    output = docsearch.max_marginal_relevance_search("foo", k=1, fetch_k=3)
+    assert output == [Document(page_content="foo")]
+
+
+@pytest.mark.requires("gpudb")
+def test_kinetica_max_marginal_relevance_search_with_score(
+    create_config: KineticaSettings,
+) -> None:
+    """Test end to end construction and search."""
+    texts = ["foo", "bar", "baz"]
+    docsearch = Kinetica.from_texts(
+        config=create_config,
+        texts=texts,
+        embedding=FakeEmbeddingsWithAdaDimension(),
+        distance_strategy=DistanceStrategy.EUCLIDEAN,
+        collection_name="test_kinetica_max_marginal_relevance_search_with_score",
+        pre_delete_collection=False,
+    )
+
+    output = docsearch.max_marginal_relevance_search_with_score("foo", k=1, fetch_k=3)
+    assert output == [(Document(page_content="foo"), 0.0)]
+
+
+@pytest.mark.requires("openai", "gpudb")
+def test_kinetica_with_openai_embeddings(create_config: KineticaSettings) -> None:
+    """Test end to end construction and search."""
+    if OPENAI_API_KEY == "":
+        assert False
+
+    openai = OpenAIEmbeddings(api_key=OPENAI_API_KEY)
+    texts = ["foo", "bar", "baz"]
+    metadatas = [{"text": text} for text in texts]
+    docsearch = Kinetica.from_texts(
+        config=create_config,
+        texts=texts,
+        metadatas=metadatas,
+        embedding=openai,
+        collection_name="kinetica_openai_test",
+        pre_delete_collection=False,
+    )
+
+    output = docsearch.similarity_search("foo", k=1)
+    assert output == [Document(page_content="foo", metadata={"text": "foo"})]
+
+
+@pytest.mark.requires("gpudb")
+def test_kinetica_retriever_search_threshold(create_config: KineticaSettings) -> None:
+    """Test using retriever for searching with threshold."""
+    texts = ["foo", "bar", "baz"]
+    metadatas = [{"page": str(i)} for i in range(len(texts))]
+    docsearch = Kinetica.from_texts(
+        config=create_config,
+        texts=texts,
+        metadatas=metadatas,
+        embedding=FakeEmbeddingsWithAdaDimension(),
+        distance_strategy=DistanceStrategy.EUCLIDEAN,
+        collection_name="test_kinetica_retriever_search_threshold",
+        pre_delete_collection=False,
+    )
+
+    retriever = docsearch.as_retriever(
+        search_type="similarity_score_threshold",
+        search_kwargs={"k": 3, "score_threshold": 0.999},
+    )
+    output = retriever.get_relevant_documents("summer")
+    assert output == [
+        Document(page_content="foo", metadata={"page": "0"}),
+    ]
+
+
+@pytest.mark.requires("gpudb")
+def test_kinetica_retriever_search_threshold_custom_normalization_fn(
+    create_config: KineticaSettings,
+) -> None:
+    """Test searching with threshold and custom normalization function"""
+    texts = ["foo", "bar", "baz"]
+    metadatas = [{"page": str(i)} for i in range(len(texts))]
+    docsearch = Kinetica.from_texts(
+        config=create_config,
+        texts=texts,
+        metadatas=metadatas,
+        embedding=FakeEmbeddingsWithAdaDimension(),
+        distance_strategy=DistanceStrategy.EUCLIDEAN,
+        collection_name="test_kinetica_retriever_search_threshold_custom_normalization_fn",
+        pre_delete_collection=False,
+        relevance_score_fn=lambda d: d * 0,
+    )
+
+    retriever = docsearch.as_retriever(
+        search_type="similarity_score_threshold",
+        search_kwargs={"k": 3, "score_threshold": 0.5},
+    )
+    output = retriever.get_relevant_documents("foo")
+    assert output == []
--- libs/partners/kinetica/tests/integration_tests/test_vectorstores.py
+++ libs/partners/kinetica/tests/integration_tests/test_vectorstores.py
@@ -1,5 +1,6 @@
 from langchain_kinetica.vectorstores import KineticaVectorStore
 
+
 def test_initialization() -> None:
     """Test integration vectorstore initialization."""
     KineticaVectorStore()
