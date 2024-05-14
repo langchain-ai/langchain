dfa3ea5563bb6c08186afbf3780a4197aff3a357
Chad Juliano <cjuliano@kinetica.com>
2024-05-14 20:35:57 +0000

    lint fixes

--- libs/partners/kinetica/langchain_kinetica/vectorstores.py
+++ libs/partners/kinetica/langchain_kinetica/vectorstores.py
@@ -12,12 +12,11 @@ from functools import partial
 from typing import Any, Callable, Dict, Iterable, List, Optional, Tuple, Type
 
 import numpy as np
+from langchain_community.vectorstores.utils import maximal_marginal_relevance
 from langchain_core.documents import Document
 from langchain_core.embeddings import Embeddings
 from langchain_core.pydantic_v1 import BaseSettings
 from langchain_core.vectorstores import VectorStore
-
-from langchain_community.vectorstores.utils import maximal_marginal_relevance
 
 
 class DistanceStrategy(str, enum.Enum):
