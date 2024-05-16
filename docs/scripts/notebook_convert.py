743d8e5fc92c969834174906dbba1b3bdf55c9f3
Erick Friis <erick@langchain.dev>
2024-05-16 11:09:38 -0700

    docs: dont rewrite ipynb links that have double slash (v0.1)

--- docs/scripts/notebook_convert.py
+++ docs/scripts/notebook_convert.py
@@ -27,7 +27,7 @@ class EscapePreprocessor(Preprocessor):
                 )
             # rewrite .ipynb links to .md
             cell.source = re.sub(
-                r"\[([^\]]*)\]\(([^)]*).ipynb\)", r"[\1](\2.md)", cell.source
+                r"\[([^\]]*)\]\((?!.*//)([^)]*).ipynb\)", r"[\1](\2.md)", cell.source
             )
         return cell, resources
 
