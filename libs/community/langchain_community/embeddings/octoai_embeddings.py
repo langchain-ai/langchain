00f3ba64d8cc843a497843d7527853fc0e87a918
Sevin F. Varoglu <sfvaroglu@octoml.ai>
2024-05-17 10:10:07 +0300

    Fix import error

--- libs/community/langchain_community/embeddings/octoai_embeddings.py
+++ libs/community/langchain_community/embeddings/octoai_embeddings.py
@@ -2,8 +2,8 @@ from typing import Dict
 
 from langchain_core.pydantic_v1 import Field, SecretStr, root_validator
 from langchain_core.utils import convert_to_secret_str, get_from_dict_or_env
-from langchain_openai import OpenAIEmbeddings
 
+from langchain_community.embeddings.openai import OpenAIEmbeddings
 from langchain_community.utils.openai import is_openai_v1
 
 DEFAULT_API_BASE = "https://text.octoai.run/v1/"
