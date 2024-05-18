c334d478bad89a8e3a59eb6a895b06f926c68244
Ofer Mendelevitch <ofermend@gmail.com>
2024-05-17 17:05:04 -0700

    fixed ruff comments

--- libs/langchain/langchain/vectorstores/vectara.py
+++ libs/langchain/langchain/vectorstores/vectara.py
@@ -4,7 +4,7 @@ from langchain._api import create_importer
 
 if TYPE_CHECKING:
     from langchain_community.vectorstores import Vectara
-    from langchain_community.vectorstores.vectara import VectaraRetriever, VectaraRag
+    from langchain_community.vectorstores.vectara import VectaraRetriever, VectaraRAG
 
 # Create a way to dynamically look up deprecated imports.
 # Used to consolidate logic for raising deprecation warnings and
