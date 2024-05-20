53bd6d4e76a252e6417627ebe1c5bf1beed8d791
Anwaar Khalid <shahanwaar.khalid8@gmail.com>
2024-05-20 09:50:09 +0000

    Added standard integration test

--- libs/partners/unify/tests/integration_tests/test_standard.py
+++ libs/partners/unify/tests/integration_tests/test_standard.py
@@ -1,15 +1,16 @@
 """Standard LangChain interface tests"""
 
+import time
 from typing import Type
 
 import pytest
 from langchain_core.language_models import BaseChatModel
-from langchain_standard_tests.unit_tests import ChatModelUnitTests
+from langchain_standard_tests.integration_tests import ChatModelIntegrationTests
 
 from langchain_unify import ChatUnify
 
 
-class TestUnify(ChatModelUnitTests):
+class TestUnify(ChatModelIntegrationTests):
     @pytest.fixture
     def chat_model_class(self) -> Type[BaseChatModel]:
         return ChatUnify
@@ -18,16 +19,27 @@ class TestUnify(ChatModelUnitTests):
     def chat_model_params(self) -> dict:
         return {
             "model": "test-model",
-            "unify_api_key": "test_api_key",
+            "unify_api_key": "test-api-key",
         }
 
-    @pytest.mark.xfail(reason="Not implemented.")
-    def test_standard_params(
+    @pytest.fixture
+    def test_stream(
         self,
         chat_model_class: Type[BaseChatModel],
         chat_model_params: dict,
     ) -> None:
-        super().test_standard_params(
+        super().test_stream(
+            chat_model_class,
+            chat_model_params,
+        )
+
+    @pytest.fixture
+    async def test_astream(
+        self,
+        chat_model_class: Type[BaseChatModel],
+        chat_model_params: dict,
+    ) -> None:
+        await super().test_astream(
             chat_model_class,
             chat_model_params,
         )
