e11c962122f5f2875d6879b76a8e5f7b4ff8ad09
ccurme <chester.curme@gmail.com>
2024-05-20 14:49:48 -0400

    mistral: implement ls_params (#21867)

--- libs/partners/mistralai/langchain_mistralai/chat_models.py
+++ libs/partners/mistralai/langchain_mistralai/chat_models.py
@@ -29,6 +29,7 @@ from langchain_core.callbacks import (
 from langchain_core.language_models import LanguageModelInput
 from langchain_core.language_models.chat_models import (
     BaseChatModel,
+    LangSmithParams,
     agenerate_from_stream,
     generate_from_stream,
 )
@@ -342,6 +343,23 @@ class ChatMistralAI(BaseChatModel):
         }
         filtered = {k: v for k, v in defaults.items() if v is not None}
         return filtered
+
+    def _get_ls_params(
+        self, stop: Optional[List[str]] = None, **kwargs: Any
+    ) -> LangSmithParams:
+        """Get standard params for tracing."""
+        params = self._get_invocation_params(stop=stop, **kwargs)
+        ls_params = LangSmithParams(
+            ls_provider="mistral",
+            ls_model_name=self.model,
+            ls_model_type="chat",
+            ls_temperature=params.get("temperature", self.temperature),
+        )
+        if ls_max_tokens := params.get("max_tokens", self.max_tokens):
+            ls_params["ls_max_tokens"] = ls_max_tokens
+        if ls_stop := stop or params.get("stop", None):
+            ls_params["ls_stop"] = ls_stop
+        return ls_params
 
     @property
     def _client_params(self) -> Dict[str, Any]:
