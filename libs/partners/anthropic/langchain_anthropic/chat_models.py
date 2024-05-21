117053b43d3a3d343218f934d9a56e2153f55e55
Chester Curme <chester.curme@gmail.com>
2024-05-21 10:19:43 -0700

    set default model

--- libs/partners/anthropic/langchain_anthropic/chat_models.py
+++ libs/partners/anthropic/langchain_anthropic/chat_models.py
@@ -259,7 +259,7 @@ class ChatAnthropic(BaseChatModel):
     _client: anthropic.Client = Field(default=None)
     _async_client: anthropic.AsyncClient = Field(default=None)
 
-    model: str = Field(alias="model_name")
+    model: str = Field(default="claude-3-haiku-20240307", alias="model_name")
     """Model name to use."""
 
     max_tokens: int = Field(default=1024, alias="max_tokens_to_sample")
