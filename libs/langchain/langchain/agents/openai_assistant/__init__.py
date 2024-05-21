f4180945349f7d6778b6650cb416d9988e36a568
Surya Rath <suryakantarath02@gmail.com>
2024-05-22 04:02:29 +0530

    OpenAI Assistants v2 api support for OpenAIAssistantRunnable (#21484)
    **Title**: "langchain: OpenAI Assistants v2 api support"
    
    ***Descriptions*** 
    - [x] "attachments" support added along with backward compatibility of
    "file_ids"
    - [x]  "tool_resources" support added while creating new assistant
    
    - [ ] "tool_choice" parameter support
    - [ ]  Streaming support
    
    
    - **Dependencies:** OpenAI v2 API (openai>=1.23.0)
    - **Twitter handle:** @skanta_rath
    
    ---------
    
    Co-authored-by: Chester Curme <chester.curme@gmail.com>

--- libs/langchain/langchain/agents/openai_assistant/__init__.py
+++ libs/langchain/langchain/agents/openai_assistant/__init__.py
@@ -1,3 +1,4 @@
 from langchain.agents.openai_assistant.base import OpenAIAssistantRunnable
+from langchain.agents.openai_assistant.base_v2 import OpenAIAssistantV2Runnable
 
-__all__ = ["OpenAIAssistantRunnable"]
+__all__ = ["OpenAIAssistantRunnable", "OpenAIAssistantV2Runnable"]
