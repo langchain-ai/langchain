506595372b40cdd2d97fef763a1b67752fad3f2f
StreetLamb <l.y.z.jerron@gmail.com>
2024-05-22 15:43:52 +0800

    add args_schema to WikipediaQueryRun

--- libs/community/langchain_community/tools/wikipedia/tool.py
+++ libs/community/langchain_community/tools/wikipedia/tool.py
@@ -1,11 +1,18 @@
 """Tool for the Wikipedia API."""
 
-from typing import Optional
+from typing import Optional, Type
 
 from langchain_core.callbacks import CallbackManagerForToolRun
+from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_core.tools import BaseTool
 
 from langchain_community.utilities.wikipedia import WikipediaAPIWrapper
+
+
+class WikipediaQueryInput(BaseModel):
+    """Input for the WikipediaQuery tool."""
+
+    query: str = Field(description="query to look up on wikipedia")
 
 
 class WikipediaQueryRun(BaseTool):
@@ -20,6 +27,8 @@ class WikipediaQueryRun(BaseTool):
     )
     api_wrapper: WikipediaAPIWrapper
 
+    args_schema: Type[BaseModel] = WikipediaQueryInput
+
     def _run(
         self,
         query: str,
@@ -27,3 +36,4 @@ class WikipediaQueryRun(BaseTool):
     ) -> str:
         """Use the Wikipedia tool."""
         return self.api_wrapper.run(query)
+
