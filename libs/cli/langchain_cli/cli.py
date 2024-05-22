8b5f4cc0a4c05f7ddd6df14408bde3fe84badc15
Morgante Pell <morgantep@google.com>
2024-05-22 16:59:04 -0700

    feat: migration via gritql

--- libs/cli/langchain_cli/cli.py
+++ libs/cli/langchain_cli/cli.py
@@ -1,12 +1,13 @@
-import importlib
 from typing import Optional
 
 import typer
+from gritql import run
 from typing_extensions import Annotated
 
 from langchain_cli.namespaces import app as app_namespace
 from langchain_cli.namespaces import integration as integration_namespace
 from langchain_cli.namespaces import template as template_namespace
+from langchain_cli.namespaces.migrate import main as migrate_namespace
 from langchain_cli.utils.packages import get_langserve_export, get_package_root
 
 __version__ = "0.0.22rc0"
@@ -23,11 +24,23 @@ app.add_typer(
 )
 
 
-# If libcst is installed, add the migrate namespace
-if importlib.util.find_spec("libcst"):
-    from langchain_cli.namespaces.migrate import main as migrate_namespace
+@app.command(
+    context_settings={
+        # Let Grit handle the arguments
+        "allow_extra_args": True,
+        "ignore_unknown_options": True,
+    },
+    # Grit embeds its own help
+    add_help_option=False,
+)
+def migrate(ctx: typer.Context):
+    final_code = run.apply_pattern(
+        "langchain_all_migrations()",
+        ctx.args,
+        grit_dir=migrate_namespace.get_gritdir_path(),
+    )
 
-    app.add_typer(migrate_namespace.app, name="migrate", help=migrate_namespace.__doc__)
+    raise typer.Exit(code=final_code)
 
 
 def version_callback(show_version: bool) -> None:
