fbaf72094b0898eb16af16ce454b94187aaac148
maang-h <maang_h@163.com>
2024-05-22 23:20:14 +0800

    Improve exception information prompts

--- libs/community/langchain_community/embeddings/baichuan.py
+++ libs/community/langchain_community/embeddings/baichuan.py
@@ -1,10 +1,10 @@
-import warnings
 from typing import Any, Dict, List, Optional
 
 import requests
 from langchain_core.embeddings import Embeddings
 from langchain_core.pydantic_v1 import BaseModel, SecretStr, root_validator
 from langchain_core.utils import convert_to_secret_str, get_from_dict_or_env
+from requests import RequestException
 
 BAICHUAN_API_URL: str = "http://api.baichuan-ai.com/v1/embeddings"
 
@@ -70,6 +70,8 @@ class BaichuanTextEmbeddings(BaseModel, Embeddings):
             response = self.session.post(
                 BAICHUAN_API_URL, json={"input": texts, "model": self.model_name}
             )
+            # Raise exception if response status code from 400 to 600
+            response.raise_for_status()
             # Check if the response status code indicates success
             if response.status_code == 200:
                 resp = response.json()
@@ -78,42 +80,16 @@ class BaichuanTextEmbeddings(BaseModel, Embeddings):
                 sorted_embeddings = sorted(embeddings, key=lambda e: e.get("index", 0))
                 # Return just the embeddings
                 return [result.get("embedding", []) for result in sorted_embeddings]
-            # Common Error 1: Invalid `api_key`
-            elif response.status_code == 401:
-                warnings.warn(
-                    "401 Unauthorized: Access denied for `BaichuanEmbedding`."
-                    "Please check your `api_key`."
-                )
-            # Common Error 2: Incorrect proxy settings
-            elif response.status_code == 407:
-                warnings.warn(
-                    "407 Proxy Authentication Required: Please check your proxy "
-                    "setting and provide valid credentials."
-                )
-            # Client Error
-            elif response.status_code >= 400:
-                warnings.warn(
-                    f"{response.status_code} Client Error: Please verify your "
-                    f"input and try again."
-                )
-            # Server Error
-            elif response.status_code >= 500:
-                warnings.warn(
-                    f"{response.status_code} Service Error: An internal error "
-                    f"has occurred on the `BaichuanEmbedding` server."
-                )
             else:
                 # Log error or handle unsuccessful response appropriately
-                warnings.warn(  # noqa: T201
+                # Handle 100 <= status_code < 400, not include 200
+                raise RequestException(
                     f"Error: Received status code {response.status_code} from "
                     "`BaichuanEmbedding` API"
                 )
-            return None
-        except Exception as e:
+        except Exception:
             # Log the exception or handle it as needed
-            raise Exception(  # noqa: T201
-                f"Exception occurred while trying to get embeddings: {str(e)}"
-            )
+            raise
 
     def embed_documents(self, texts: List[str]) -> Optional[List[List[float]]]:  # type: ignore[override]
         """Public method to get embeddings for a list of documents.
