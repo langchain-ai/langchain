e4c02725e3b2668a3b539ab9a980059a06d7dee3
Bagatur <baskaryan@gmail.com>
2024-05-22 14:10:26 -0700

    fmt

--- libs/community/langchain_community/vectorstores/azuresearch.py
+++ libs/community/langchain_community/vectorstores/azuresearch.py
@@ -27,7 +27,7 @@ from langchain_core.callbacks import (
 )
 from langchain_core.documents import Document
 from langchain_core.embeddings import Embeddings
-from langchain_core.pydantic_v1 import Field, root_validator
+from langchain_core.pydantic_v1 import root_validator
 from langchain_core.retrievers import BaseRetriever
 from langchain_core.utils import get_from_env
 from langchain_core.vectorstores import VectorStore
