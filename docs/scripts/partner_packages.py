f12fc6232f96dc4daee4904a0720075700cc350c
leo-gan <leo.gan.57@gmail.com>
2024-05-21 18:42:21 -0700

    added external package logic

--- docs/scripts/partner_packages.py
+++ docs/scripts/partner_packages.py
@@ -9,6 +9,7 @@ from dataclasses import dataclass
 from pathlib import Path
 
 import tomli
+import yaml
 
 logger = logging.getLogger(__name__)
 
@@ -29,9 +30,8 @@ class PartnerPackage:
     description: str
     url: str
     authors: list[str] = None
+    license: str = None
     external_dependencies: list[str] = None
-    api_ref_link: str = None
-    implemented_component2member_links: dict[str, dict[set[str]]] = None
 
 
 def get_partner_dirs(partner_packages_dir: Path) -> list[Path]:
@@ -51,6 +51,7 @@ def parse_toml_metadata(metadata):
     version = tool_poetry["version"] if "version" in tool_poetry else ""
     description = tool_poetry["description"] if "description" in tool_poetry else ""
     authors = tool_poetry["authors"] if "authors" in tool_poetry else []
+    license_ = tool_poetry["license"] if "license" in tool_poetry else ""
     if "urls" not in tool_poetry or "Source Code" not in tool_poetry["urls"]:
         url = ""
     else:
@@ -62,53 +63,56 @@ def parse_toml_metadata(metadata):
         description=description,
         authors=authors,
         url=url,
+        license=license_,
     )
 
 
-def is_module_loaded(module_name):
-    try:
-        importlib.import_module(module_name)
-        return True
-    except ModuleNotFoundError:
-        return False
+def get_external_package_info():
+    external_package_info_file = PARTNER_PACKAGES_DIR / "external_packages.yaml"
+    if not external_package_info_file.exists():
+        logger.warning(
+            f"The external packages file {external_package_info_file} does not exist."
+        )
+        return []
+
+    with open(external_package_info_file, "r") as f:
+        data = yaml.safe_load(f)
+
+        if data["kind"] != "Package discovery":
+            raise ValueError(
+                f"The kind of the external packages file should be 'Package discovery' but it is {data['kind']}."
+            )
+        if data["version"] != "v1":
+            raise ValueError(
+                f"The version of the external packages file should be v1 but it is {data['version']}."
+            )
+
+        external_packages = [
+            PartnerPackage(
+                name=package["name"],
+                version=package["version"],
+                description=package.get("description", ""),
+                authors=package.get("authors", []),
+                url=package.get("source_code", ""),
+                license=package.get("license", ""),
+            )
+            for package in data["external_packages"]
+        ]
+        return external_packages
 
 
-def get_module_info(module_name: str) -> PartnerPackage:
-    """Retrieve meta-information about a loaded module/package.
-
-    Args:
-      module_name: The name of the loaded module/package.
-
-    Returns:
-        PartnerPackage: The meta information about the module/package.
-    """
-    package_dist = importlib.metadata.distribution(module_name)
-    if hasattr(package_dist, "authors"):
-        authors = package_dist.authors
-    else:
-        authors = []
-    if (
-        hasattr(package_dist, "metadata")
-        and hasattr(package_dist.metadata, "json")
-        and "project_url" in package_dist.metadata.json
-        and len(package_dist.metadata.json["project_url"]) > 1
-        and ", " in package_dist.metadata.json["project_url"][1]
-    ):
-        url = package_dist.metadata.json["project_url"][1].split(",")[1].strip()
-    else:
-        url = ""
-    info = PartnerPackage(
-        name=package_dist.name,
-        version=package_dist.version,
-        description=f"{package_dist.metadata['summary']}",
-        url=url,
-        authors=authors,
-    )
-    return info
+def _compound_partner_metadatas(partner_metadatas, external_packages):
+    # check for duplicate package names
+    partner_metadatas_names = [partner.name for partner in partner_metadatas]
+    external_packages_names = [package.name for package in external_packages]
+    if names_in_both := set(partner_metadatas_names) & set(external_packages_names):
+        raise ValueError(f"Duplicate package names: {names_in_both}")
+    return partner_metadatas + external_packages
 
 
 def get_partner_metadatas(partner_dirs: list[Path]) -> list[PartnerPackage]:
     """Get the partner package metadata."""
+    # parse the metadata from the `partners/{partner}/pyproject.toml` files
     partner_metadatas = []
     for partner_dir in partner_dirs:
         metadata_file = partner_dir / "pyproject.toml"
@@ -119,9 +123,6 @@ def get_partner_metadatas(partner_dirs: list[Path]) -> list[PartnerPackage]:
                 content = f.read()
                 metadata = tomli.loads(content)
                 partner_package = parse_toml_metadata(metadata)
-        # parse the module metadata, if module is loaded as an external package
-        if not partner_package and is_module_loaded(module_name):
-            partner_package = get_module_info(module_name)
 
         if not partner_package:
             logger.warning(
@@ -130,14 +131,22 @@ def get_partner_metadatas(partner_dirs: list[Path]) -> list[PartnerPackage]:
             continue
 
         partner_metadatas.append(partner_package)
-    return partner_metadatas
+
+    # parse the metadata for external packages
+    external_packages = get_external_package_info()
+
+    # compound the metadata
+    all_partner_metadatas = _compound_partner_metadatas(
+        partner_metadatas, external_packages
+    )
+    return all_partner_metadatas
 
 
 def generate_table(packages: list[PartnerPackage]) -> str:
     lines = []
     table_header = """
-| Package 🔻 | Version | Authors | Description |
-|------------------|---------|-------------------|-------------------------|
+| Package 🔻 | Version | License | Authors | Description |
+|------------|---------|---------|---------|-------------|
 """
     lines.append(table_header)
     for package in sorted(packages, key=lambda x: x.name):
@@ -146,6 +155,7 @@ def generate_table(packages: list[PartnerPackage]) -> str:
             [
                 title_link,
                 package.version,
+                package.license,
                 ", ".join(package.authors),
                 package.description,
             ]
@@ -178,23 +188,19 @@ LangChain integrates with many providers.
 
 ## Partner Packages
 
-These providers have standalone `langchain-{provider}` packages for improved versioning, dependency management and testing.
+These providers have separate `langchain-{provider}` packages for improved versioning, dependency management and testing.
 
 {table}
 
-## Featured Community Providers
-
-- [Hugging Face](/docs/integrations/platforms/huggingface)
-- [Microsoft](/docs/integrations/platforms/microsoft)
 
 ## All Providers
 
-Click [here](/docs/integrations/providers/) to see all providers.
+[A full list of all providers](/docs/integrations/providers/).
 """
     index_file_content = index_file_content.replace("{table}", table_str)
     with open(output_file, "w") as f:
         f.write(index_file_content)
-        logger.info(f"{output_file} file updated with the package table.")
+        logger.warning(f"{output_file} file updated with the package table.")
 
 
 def main():
