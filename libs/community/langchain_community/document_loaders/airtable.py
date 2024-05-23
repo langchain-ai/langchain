3bc9cc1476d9b83d051cb45e91e6868df9175df7
Bagatur <baskaryan@gmail.com>
2024-05-23 15:50:56 -0700

    fmt

--- libs/community/langchain_community/document_loaders/airtable.py
+++ libs/community/langchain_community/document_loaders/airtable.py
@@ -1,30 +1,28 @@
-from typing import Iterator
+from typing import Any, Iterator
 
+from langchain_core.document_loaders import BaseLoader
 from langchain_core.documents import Document
-
-from langchain_community.document_loaders.base import BaseLoader
 
 
 class AirtableLoader(BaseLoader):
     """Load the `Airtable` tables."""
 
-    def __init__(self,
-            api_token: str,
-            table_id: str,
-            base_id: str,
-            **kwargs
-        ):
-        """Initialize with API token and the IDs for table and base"""
+    def __init__(
+        self, api_token: str, table_id: str, base_id: str, **kwargs: Any
+    ) -> None:
+        """Initialize with API token and the IDs for table and base.
+
+        Args:
+            api_token: Airtable API token.
+            table_id: Airtable table ID.
+            base_id:
+            **kwargs: Additional parameters to pass to Table.all(). Refer to the
+                pyairtable documentation for available options:
+                https://pyairtable.readthedocs.io/en/latest/api.html#pyairtable.Table.all
+        """  # noqa: E501
         self.api_token = api_token
-        """Airtable API token."""
         self.table_id = table_id
-        """Airtable table ID."""
         self.base_id = base_id
-        """
-            Additional parameters to pass to Table.all(). Refer to the 
-            pyairtable documentation for available options:
-            https://pyairtable.readthedocs.io/en/latest/api.html#pyairtable.Table.all
-        """
         self.kwargs = kwargs
 
     def lazy_load(self) -> Iterator[Document]:
@@ -43,7 +41,4 @@ class AirtableLoader(BaseLoader):
             if "view" in self.kwargs:
                 metadata["view"] = self.kwargs["view"]
             # Need to convert record from dict to str
-            yield Document(
-                page_content=str(record),
-                metadata=metadata,
-            )
+            yield Document(page_content=str(record), metadata=metadata)
