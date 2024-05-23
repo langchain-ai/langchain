bc3532420571b0a4910e7480896fe922d9111fbf
Cole Enyart <letchro@gmail.com>
2024-05-23 18:09:20 -0500

    Update ValidationError to import from pydantic_v1 to pass lint check

--- libs/community/tests/unit_tests/chat_models/test_everlyai.py
+++ libs/community/tests/unit_tests/chat_models/test_everlyai.py
@@ -2,8 +2,7 @@
 import os
 
 import pytest
-from langchain_core.pydantic_v1 import SecretStr
-from pydantic import ValidationError
+from langchain_core.pydantic_v1 import SecretStr, ValidationError
 
 from langchain_community.chat_models import ChatEverlyAI
 
