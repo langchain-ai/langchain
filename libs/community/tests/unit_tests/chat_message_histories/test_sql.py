31dc9c07121b7c8db7d4b5bb2cfb198a0297a9c0
Philippe Prados <github@prados.fr>
2024-05-23 14:10:36 +0200

    Real async version of SQLChatMessageHistory

--- libs/community/tests/unit_tests/chat_message_histories/test_sql.py
+++ libs/community/tests/unit_tests/chat_message_histories/test_sql.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Any, Generator, Tuple
+from typing import Any, AsyncGenerator, Generator, Tuple
 
 import pytest
 from langchain_core.messages import AIMessage, HumanMessage
@@ -18,20 +18,52 @@ def con_str(tmp_path: Path) -> str:
 
 
 @pytest.fixture()
+def acon_str(tmp_path: Path) -> str:
+    file_path = tmp_path / "adb.sqlite3"
+    con_str = f"sqlite+aiosqlite:///{file_path}"
+    return con_str
+
+
+@pytest.fixture()
 def sql_histories(
     con_str: str,
 ) -> Generator[Tuple[SQLChatMessageHistory, SQLChatMessageHistory], None, None]:
     message_history = SQLChatMessageHistory(
-        session_id="123", connection_string=con_str, table_name="test_table"
+        session_id="123", connection=con_str, table_name="test_table"
     )
     # Create history for other session
     other_history = SQLChatMessageHistory(
-        session_id="456", connection_string=con_str, table_name="test_table"
+        session_id="456", connection=con_str, table_name="test_table"
     )
 
     yield message_history, other_history
     message_history.clear()
     other_history.clear()
+
+
+@pytest.fixture()
+async def asql_histories(
+    acon_str: str,
+) -> AsyncGenerator[Tuple[SQLChatMessageHistory, SQLChatMessageHistory], None]:
+    message_history = SQLChatMessageHistory(
+        session_id="123",
+        connection=acon_str,
+        table_name="test_table",
+        async_mode=True,
+        engine_args={"echo": True},
+    )
+    # Create history for other session
+    other_history = SQLChatMessageHistory(
+        session_id="456",
+        connection=acon_str,
+        table_name="test_table",
+        async_mode=True,
+        engine_args={"echo": True},
+    )
+
+    yield message_history, other_history
+    await message_history.aclear()
+    await other_history.aclear()
 
 
 def test_add_messages(
@@ -49,6 +81,22 @@ def test_add_messages(
     assert messages[1].content == "Hi there!"
 
 
+@pytest.mark.asyncio
+async def test_async_add_messages(
+    asql_histories: Tuple[SQLChatMessageHistory, SQLChatMessageHistory],
+) -> None:
+    sql_history, other_history = asql_histories
+    await sql_history.aadd_user_message("Hello!")
+    await sql_history.aadd_ai_message("Hi there!")
+
+    messages = await sql_history.aget_messages()
+    assert len(messages) == 2
+    assert isinstance(messages[0], HumanMessage)
+    assert isinstance(messages[1], AIMessage)
+    assert messages[0].content == "Hello!"
+    assert messages[1].content == "Hi there!"
+
+
 def test_multiple_sessions(
     sql_histories: Tuple[SQLChatMessageHistory, SQLChatMessageHistory],
 ) -> None:
@@ -58,19 +106,48 @@ def test_multiple_sessions(
     sql_history.add_user_message("Whats cracking?")
 
     # Ensure the messages are added correctly in the first session
-    assert len(sql_history.messages) == 3, "waat"
-    assert sql_history.messages[0].content == "Hello!"
-    assert sql_history.messages[1].content == "Hi there!"
-    assert sql_history.messages[2].content == "Whats cracking?"
+    messages = sql_history.messages
+    assert len(messages) == 3, "waat"
+    assert messages[0].content == "Hello!"
+    assert messages[1].content == "Hi there!"
+    assert messages[2].content == "Whats cracking?"
 
     # second session
     other_history.add_user_message("Hellox")
     assert len(other_history.messages) == 1
-    assert len(sql_history.messages) == 3
+    messages = sql_history.messages
+    assert len(messages) == 3
     assert other_history.messages[0].content == "Hellox"
-    assert sql_history.messages[0].content == "Hello!"
-    assert sql_history.messages[1].content == "Hi there!"
-    assert sql_history.messages[2].content == "Whats cracking?"
+    assert messages[0].content == "Hello!"
+    assert messages[1].content == "Hi there!"
+    assert messages[2].content == "Whats cracking?"
+
+
+@pytest.mark.asyncio
+async def test_async_multiple_sessions(
+    asql_histories: Tuple[SQLChatMessageHistory, SQLChatMessageHistory],
+) -> None:
+    sql_history, other_history = asql_histories
+    await sql_history.aadd_user_message("Hello!")
+    await sql_history.aadd_ai_message("Hi there!")
+    await sql_history.aadd_user_message("Whats cracking?")
+
+    # Ensure the messages are added correctly in the first session
+    messages = await sql_history.aget_messages()
+    assert len(messages) == 3, "waat"
+    assert messages[0].content == "Hello!"
+    assert messages[1].content == "Hi there!"
+    assert messages[2].content == "Whats cracking?"
+
+    # second session
+    await other_history.aadd_user_message("Hellox")
+    messages = await sql_history.aget_messages()
+    assert len(await other_history.aget_messages()) == 1
+    assert len(messages) == 3
+    assert (await other_history.aget_messages())[0].content == "Hellox"
+    assert messages[0].content == "Hello!"
+    assert messages[1].content == "Hi there!"
+    assert messages[2].content == "Whats cracking?"
 
 
 def test_clear_messages(
@@ -88,6 +165,24 @@ def test_clear_messages(
     sql_history.clear()
     assert len(sql_history.messages) == 0
     assert len(other_history.messages) == 1
+
+
+@pytest.mark.asyncio
+async def test_async_clear_messages(
+    asql_histories: Tuple[SQLChatMessageHistory, SQLChatMessageHistory],
+) -> None:
+    sql_history, other_history = asql_histories
+    await sql_history.aadd_user_message("Hello!")
+    await sql_history.aadd_ai_message("Hi there!")
+    assert len(await sql_history.aget_messages()) == 2
+    # Now create another history with different session id
+    await other_history.aadd_user_message("Hellox")
+    assert len(await other_history.aget_messages()) == 1
+    assert len(await sql_history.aget_messages()) == 2
+    # Now clear the first history
+    await sql_history.aclear()
+    assert len(await sql_history.aget_messages()) == 0
+    assert len(await other_history.aget_messages()) == 1
 
 
 def test_model_no_session_id_field_error(con_str: str) -> None:
