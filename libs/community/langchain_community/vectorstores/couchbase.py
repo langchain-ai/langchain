fb91b3d4fd99d4fdcc9edf4d57e71422208dfe39
Nithish Raghunandanan <nithishr@users.noreply.github.com>
2024-05-24 17:26:41 +0200

    Fix linting error

--- libs/community/langchain_community/vectorstores/couchbase.py
+++ libs/community/langchain_community/vectorstores/couchbase.py
@@ -3,11 +3,10 @@ from __future__ import annotations
 import uuid
 from typing import TYPE_CHECKING, Any, Dict, Iterable, List, Optional, Tuple, Type
 
+from langchain_core._api.deprecation import deprecated
 from langchain_core.documents import Document
 from langchain_core.embeddings import Embeddings
 from langchain_core.vectorstores import VectorStore
-from langchain_core._api.deprecation import deprecated
-
 
 if TYPE_CHECKING:
     from couchbase.cluster import Cluster
