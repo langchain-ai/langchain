66fe5f7350eaa5fb6fd7eafff4af38fa955cfa92
Prithvi Kannan <prithvi.kannan@databricks.com>
2024-05-31 12:17:49 -0700

    update
    Signed-off-by: Prithvi Kannan <prithvi.kannan@databricks.com>

--- libs/community/langchain_community/output_parsers/mlflow_rag_parsers.py
+++ libs/community/langchain_community/output_parsers/mlflow_rag_parsers.py
@@ -1,70 +1,39 @@
-from typing import Any, Dict, List
-
+from dataclasses import asdict
+from typing import Any, Dict
 from langchain_core.output_parsers.transform import BaseTransformOutputParser
-
+from langchain_core.messages import BaseMessage
+from mlflow.models.rag_signatures import StringResponse, ChatCompletionResponse, ChainCompletionChoice, Message
 
 class ChatCompletionsOutputParser(BaseTransformOutputParser[Dict[str, Any]]):
-    """
-    OutputParser that wraps the string output into a ChatCompletions
-    structured format.
-    """
+    """OutputParser that wraps the string output into a dictionary representation of an MLflow ChatCompletionResponse"""
 
     @classmethod
     def is_lc_serializable(cls) -> bool:
         """Return whether this class is serializable."""
         return True
 
-    @classmethod
-    def get_lc_namespace(cls) -> List[str]:
-        """Get the namespace of the langchain object."""
-        return ["langchain", "schema", "output_parser"]
-
     @property
     def _type(self) -> str:
         """Return the output parser type for serialization."""
-        return "openai_style"
+        return "mlflow_simplified_chat_completions"
 
-    def parse(self, text: str) -> Dict[str, Any]:
-        """Returns the input text wrapped in an OpenAI-like response structure."""
-        try:
-            from mlflow.models.rag_signatures import (
-                ChainCompletionChoice,
-                ChatCompletionResponse,
-                Message,
-            )
-        except ImportError:
-            raise ImportError(
-                "mlflow 2.13.0 or higher is required to use this output parser. "
-            )
-        return ChatCompletionResponse(
-            choices=[ChainCompletionChoice(message=Message(content=text))]
-        ).asdict()
-
-
-class StrObjOutputParser(BaseTransformOutputParser[Dict[str, Any]]):
-    """OutputParser that wraps the string output into a structured format."""
+    def parse(self, text: BaseMessage) -> Dict[str, Any]:
+        return asdict(ChatCompletionResponse(
+            choices=[ChainCompletionChoice(message=Message(content=text.content))]
+        ))
+    
+class StringResponseOutputParser(BaseTransformOutputParser[Dict[str, Any]]):
+    """OutputParser that wraps the string output into an dictionary representation of a MLflow StringResponse"""
 
     @classmethod
     def is_lc_serializable(cls) -> bool:
         """Return whether this class is serializable."""
         return True
 
-    @classmethod
-    def get_lc_namespace(cls) -> List[str]:
-        """Get the namespace of the langchain object."""
-        return ["langchain", "schema", "output_parser"]
-
     @property
     def _type(self) -> str:
         """Return the output parser type for serialization."""
-        return "openai_style"
+        return "mlflow_simplified_str_object"
 
-    def parse(self, text: str) -> Dict[str, Any]:
-        """Returns the input text wrapped in a response structure with a string."""
-        try:
-            from mlflow.models.rag_signatures import StringResponse
-        except ImportError:
-            raise ImportError(
-                "mlflow 2.13.0 or higher is required to use this output parser. "
-            )
-        return StringResponse(text=text).asdict()
+    def parse(self, text: BaseMessage) -> Dict[str, Any]:
+        return asdict(StringResponse(content=text.content))
