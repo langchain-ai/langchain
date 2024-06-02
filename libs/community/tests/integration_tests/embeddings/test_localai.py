6772a82030ff3c8605a528f90b1a87786bfc9afa
mikhail-khludnev <mikhail_khludnev@rntgroup.com>
2024-06-02 21:14:14 +0300

    migrate async embeds as well, covering by test.

--- libs/community/tests/integration_tests/embeddings/test_localai.py
+++ libs/community/tests/integration_tests/embeddings/test_localai.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import pytest
 
 from langchain_community.embeddings import LocalAIEmbeddings
@@ -9,5 +11,16 @@ def test_localai_embed() -> None:
                          openai_api_key="foo", model="bge-m3",
                         openai_api_base="https://foo.bar/v1")
    eqq=llm.embed_query("foo bar")
+   assert len(eqq) > 100
+   assert eqq[0] != eqq[1]
+
+
+@pytest.mark.requires("openai")
+@pytest.mark.vcr
+async def test_localai_aembed() -> None:
+   llm=LocalAIEmbeddings(
+                         openai_api_key="foo", model="bge-m3",
+                        openai_api_base="https://foo.bar/v1")
+   eqq=await llm.aembed_query("foo bar")
    assert len(eqq) > 100
    assert eqq[0] != eqq[1]
