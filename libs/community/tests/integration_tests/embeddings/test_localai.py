081ea467b87e778a90d043f75c5e486d359f6606
mikhail-khludnev <mikhail_khludnev@rntgroup.com>
2024-06-02 11:41:12 +0300

    Adjust LocalAI embeds for OpenAI 1.30

--- libs/community/tests/integration_tests/embeddings/test_localai.py
+++ libs/community/tests/integration_tests/embeddings/test_localai.py
@@ -9,6 +9,5 @@ def test_localai_embed() -> None:
                          openai_api_key="foo", model="bge-m3",
                         openai_api_base="https://foo.bar/v1")
    eqq=llm.embed_query("foo bar")
-   //print(eqq)
    assert len(eqq) > 100
    assert eqq[0] != eqq[1]
