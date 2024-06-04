975ab3f2db26947b3a54e2b8fe873ac32f0d7943
MarceloCorreiaData <marcelocorreiausp@gmail.com>
2024-06-04 13:00:06 -0300

    Update test_anzographdbgraph.py

--- libs/community/tests/unit_tests/graphs/test_anzographdbgraph.py
+++ libs/community/tests/unit_tests/graphs/test_anzographdbgraph.py
@@ -1,3 +1,5 @@
+@pytest.mark.requires(rdflib)
+@pytest.mark.requires(sparqlwrapper)
 def test_import() -> None:
     from langchain_community.graphs.anzograph_graph import (
         AnzoGraphDBGraph,
