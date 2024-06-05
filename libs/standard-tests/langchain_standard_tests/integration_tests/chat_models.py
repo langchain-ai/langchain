a3857de12b7100336bdf8dd8a8b291af676f10c5
Chester Curme <chester.curme@gmail.com>
2024-06-05 16:38:57 -0400

    add standard test

--- libs/standard-tests/langchain_standard_tests/integration_tests/chat_models.py
+++ libs/standard-tests/langchain_standard_tests/integration_tests/chat_models.py
@@ -144,6 +144,19 @@ class ChatModelIntegrationTests(ABC):
         assert isinstance(result.usage_metadata["output_tokens"], int)
         assert isinstance(result.usage_metadata["total_tokens"], int)
 
+    def test_stop_sequence(
+        self, chat_model_class: Type[BaseChatModel], chat_model_params: dict
+    ) -> None:
+        model = chat_model_class(**chat_model_params)
+        result = model.invoke("hi", stop=["you"])
+        assert isinstance(result, AIMessage)
+        assert "you" not in result.content
+
+        model = chat_model_class(**chat_model_params, stop=["you"])
+        result = model.invoke("hi")
+        assert isinstance(result, AIMessage)
+        assert "you" not in result.content
+
     def test_tool_message_histories_string_content(
         self,
         chat_model_class: Type[BaseChatModel],
