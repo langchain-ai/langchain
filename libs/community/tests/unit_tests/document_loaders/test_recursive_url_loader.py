8151792619ba4894b3bbc83ee88792b19d78f026
isaac hershenson <ihershenson@hmc.edu>
2024-06-05 12:15:19 -0700

    formatting

--- libs/community/tests/unit_tests/document_loaders/test_recursive_url_loader.py
+++ libs/community/tests/unit_tests/document_loaders/test_recursive_url_loader.py
@@ -1,6 +1,8 @@
-import requests_mock
-from langchain_community.document_loaders.recursive_url_loader import RecursiveUrlLoader
 import aiohttp
+import requests_mock
+
+from langchain_community.document_loaders.recursive_url_loader import RecursiveUrlLoader
+
 
 def mock_requests(loader):
     html1 = (
@@ -9,13 +11,13 @@ def mock_requests(loader):
     )
     html2 = '<div><a class="first" href="../three">buhbye</a></div>'
     html3 = '<div><a class="second" href="../three">buhbye</a></div>'
-    html4 = '<p>the end<p>'
+    html4 = "<p>the end<p>"
 
     MOCK_DEFINITIONS = [
-    ('http://test.com', html1),
-    ('http://test.com/one', html2),
-    ('http://test.com/two', html3),
-    ('http://test.com/three', html4),
+        ("http://test.com", html1),
+        ("http://test.com/one", html2),
+        ("http://test.com/two", html3),
+        ("http://test.com/three", html4),
     ]
 
     with requests_mock.Mocker() as m:
@@ -24,6 +26,7 @@ def mock_requests(loader):
         docs = loader.load()
     return docs
 
+
 class MockGet:
     def __init__(self, url):
         if "one" in url:
@@ -31,12 +34,12 @@ class MockGet:
         elif "two" in url:
             self._text = '<div><a class="second" href="../three">buhbye</a></div>'
         elif "three" in url:
-            self._text = '<p>the end<p>'
+            self._text = "<p>the end<p>"
         else:
             self._text = (
-                            '<div><a class="blah" href="/one">hullo</a></div>'
-                            '<div><a class="bleh" href="/two">buhbye</a></div>'
-                        )
+                '<div><a class="blah" href="/one">hullo</a></div>'
+                '<div><a class="bleh" href="/two">buhbye</a></div>'
+            )
         self.headers = {}
 
     async def text(self):
@@ -48,35 +51,41 @@ class MockGet:
     async def __aenter__(self):
         return self
 
+
 def test_sync__init__():
-    loader = RecursiveUrlLoader("http://test.com",max_depth=1)
+    loader = RecursiveUrlLoader("http://test.com", max_depth=1)
     docs = mock_requests(loader)
     assert len(docs) == 1
 
+
 def test_async__init__(mocker):
-    mocker.patch.object(aiohttp.ClientSession, 'get', new=MockGet)
-    loader = RecursiveUrlLoader("http://test.com",max_depth=1, use_async=True)
+    mocker.patch.object(aiohttp.ClientSession, "get", new=MockGet)
+    loader = RecursiveUrlLoader("http://test.com", max_depth=1, use_async=True)
     docs = loader.load()
     assert len(docs) == 1
+
 
 def test_sync_default_depth():
     loader = RecursiveUrlLoader("http://test.com")
     docs = mock_requests(loader)
     assert len(docs) == 3
 
+
 def test_async_default_depth(mocker):
-    mocker.patch.object(aiohttp.ClientSession, 'get', new=MockGet)
+    mocker.patch.object(aiohttp.ClientSession, "get", new=MockGet)
     loader = RecursiveUrlLoader("http://test.com", use_async=True)
     docs = loader.load()
     assert len(docs) == 3
-    
+
+
 def test_sync_deduplication():
-    loader = RecursiveUrlLoader("http://test.com",max_depth=3)
+    loader = RecursiveUrlLoader("http://test.com", max_depth=3)
     docs = mock_requests(loader)
     assert len(docs) == 4
 
+
 def test_async_deduplication(mocker):
-    mocker.patch.object(aiohttp.ClientSession, 'get', new=MockGet)
-    loader = RecursiveUrlLoader("http://test.com",max_depth=3, use_async=True)
+    mocker.patch.object(aiohttp.ClientSession, "get", new=MockGet)
+    loader = RecursiveUrlLoader("http://test.com", max_depth=3, use_async=True)
     docs = loader.load()
     assert len(docs) == 4
