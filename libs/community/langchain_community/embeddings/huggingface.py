478a8e69f5d433b170f66a3394e5256c3804d291
Jon Zeolla <zeolla@gmail.com>
2024-06-06 13:27:00 -0400

    Update bge-large-en references to v1.5

--- libs/community/langchain_community/embeddings/huggingface.py
+++ libs/community/langchain_community/embeddings/huggingface.py
@@ -210,7 +210,7 @@ class HuggingFaceBgeEmbeddings(BaseModel, Embeddings):
 
             from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 
-            model_name = "BAAI/bge-large-en"
+            model_name = "BAAI/bge-large-en-v1.5"
             model_kwargs = {'device': 'cpu'}
             encode_kwargs = {'normalize_embeddings': True}
             hf = HuggingFaceBgeEmbeddings(
