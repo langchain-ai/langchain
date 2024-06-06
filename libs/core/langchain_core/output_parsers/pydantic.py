20be038db7da0bd7b15e09b2d1625063066f88bb
Erick Friis <erick@langchain.dev>
2024-06-06 13:22:19 -0700

    x

--- libs/core/langchain_core/output_parsers/pydantic.py
+++ libs/core/langchain_core/output_parsers/pydantic.py
@@ -30,7 +30,7 @@ class PydanticOutputParser(JsonOutputParser, Generic[TBaseModel]):
         if PYDANTIC_MAJOR_VERSION == 2:
             try:
                 if issubclass(self.pydantic_object, pydantic.BaseModel):
-                    return self.pydantic_object.model_validate(obj)
+                    return self.pydantic_object.model_validate(obj)  # type: ignore
                 elif issubclass(self.pydantic_object, pydantic.v1.BaseModel):
                     return self.pydantic_object.parse_obj(obj)
                 else:
