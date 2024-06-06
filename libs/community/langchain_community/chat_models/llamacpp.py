cf3fc57b43bbcbe74d8483be6bfc5e06d7121de5
Bagatur <baskaryan@gmail.com>
2024-06-06 08:49:53 -0700

    fmt

--- libs/community/langchain_community/chat_models/llamacpp.py
+++ libs/community/langchain_community/chat_models/llamacpp.py
@@ -1,49 +1,44 @@
+from operator import itemgetter
+from pathlib import Path
 from typing import (
-    TYPE_CHECKING,
     Any,
     Callable,
     Dict,
-    List,
     Iterator,
-    Literal,
+    List,
     Optional,
-    Tuple,
+    Sequence,
     Type,
-    TypedDict,
     Union,
     cast,
-    Mapping
 )
-if TYPE_CHECKING:
-    from langchain_core.tools import BaseTool
 
-from pathlib import Path
 from langchain_core.callbacks import CallbackManagerForLLMRun
-
+from langchain_core.language_models import LanguageModelInput
 from langchain_core.language_models.chat_models import (
     BaseChatModel,
     generate_from_stream,
 )
-
 from langchain_core.messages import (
     AIMessageChunk,
     BaseMessage,
 )
+from langchain_core.output_parsers.base import OutputParserLike
+from langchain_core.output_parsers.openai_tools import (
+    JsonOutputKeyToolsParser,
+    PydanticToolsParser,
+)
+from langchain_core.outputs import ChatGeneration, ChatGenerationChunk, ChatResult
+from langchain_core.pydantic_v1 import BaseModel, Field, root_validator
+from langchain_core.runnables import Runnable, RunnableMap, RunnablePassthrough
+from langchain_core.tools import BaseTool
+from langchain_core.utils.function_calling import convert_to_openai_tool
 
-from langchain_openai.chat_models.base import (
+from langchain_community.chat_models.chat_models.base import (
     _convert_delta_to_message_chunk,
     _convert_dict_to_message,
-    _convert_message_to_dict
+    _convert_message_to_dict,
 )
-
-
-from langchain_core.language_models import BaseChatModel
-from langchain_core.outputs import ChatGeneration, ChatGenerationChunk, ChatResult
-from langchain_core.pydantic_v1 import Field, root_validator, BaseModel
-from langchain_core.runnables import Runnable
-from langchain_core.language_models import LanguageModelInput
-from langchain_core.callbacks import CallbackManagerForLLMRun
-from langchain_core.utils.function_calling import convert_to_openai_tool
 
 
 class ChatLlamaCpp(BaseChatModel):
@@ -221,7 +216,6 @@ class ChatLlamaCpp(BaseChatModel):
         else:
             pass
         return values
-    
 
     def _get_parameters(self) -> Dict[str, Any]:
         """
@@ -241,19 +235,14 @@ class ChatLlamaCpp(BaseChatModel):
 
         return params
 
-
     def _create_message_dicts(
         self, messages: List[BaseMessage]
     ) -> List[Dict[str, Any]]:
-        
         message_dicts = [_convert_message_to_dict(m) for m in messages]
 
         return message_dicts
 
-
-    def _create_chat_result(
-        self, response: dict
-    ) -> ChatResult:
+    def _create_chat_result(self, response: dict) -> ChatResult:
         generations = []
 
         for res in response["choices"]:
@@ -269,45 +258,41 @@ class ChatLlamaCpp(BaseChatModel):
         token_usage = response.get("usage", {})
         llm_output = {
             "token_usage": token_usage,
-            "model_name": self.model_name,
-            "system_fingerprint": response.get("system_fingerprint", ""),
         }
         return ChatResult(generations=generations, llm_output=llm_output)
-
 
     def _generate(
         self,
         messages: List[BaseMessage],
+        stop: Optional[List[str]] = None,
         run_manager: Optional[CallbackManagerForLLMRun] = None,
         **kwargs: Any,
     ) -> ChatResult:
-        
         if self.streaming:
-            stream_iter = self._stream(
-                messages, run_manager=run_manager, **kwargs
-            )
+            stream_iter = self._stream(messages, run_manager=run_manager, **kwargs)
             return generate_from_stream(stream_iter)
-        
+
         params = {**self._get_parameters(), **kwargs}
         message_dicts = self._create_message_dicts(messages)
 
         response = self.client.create_chat_completion(messages=message_dicts, **params)
 
         return self._create_chat_result(response)
-    
 
     def _stream(
         self,
         messages: List[BaseMessage],
+        stop: Optional[List[str]] = None,
         run_manager: Optional[CallbackManagerForLLMRun] = None,
         **kwargs: Any,
     ) -> Iterator[ChatGenerationChunk]:
-        
         params = {**self._get_parameters(), **kwargs}
         message_dicts = self._create_message_dicts(messages)
 
-        result = self.client.create_chat_completion(messages=message_dicts, stream=True, **params)
-        
+        result = self.client.create_chat_completion(
+            messages=message_dicts, stream=True, **params
+        )
+
         default_chunk_class = AIMessageChunk
         count = 0
         for chunk in result:
@@ -333,42 +318,53 @@ class ChatLlamaCpp(BaseChatModel):
                 message=chunk, generation_info=generation_info or None
             )
             if run_manager:
-                run_manager.on_llm_new_token(
-                    chunk.text, chunk=chunk, logprobs=logprobs
-                )
+                run_manager.on_llm_new_token(chunk.text, chunk=chunk, logprobs=logprobs)
             yield chunk
 
     def bind_tools(
         self,
-        tools,
+        tools: Sequence[Union[Dict[str, Any], Type[BaseModel], Callable, BaseTool]],
         *,
-        tool_choice: Optional[Dict[str, str]]= None,
+        tool_choice: Optional[Union[Dict[str, Dict], bool, str]] = None,
         **kwargs: Any,
     ) -> Runnable[LanguageModelInput, BaseMessage]:
         """Bind tool-like objects to this chat model
-        
-        tool_choice: temporarily not support for "any", "auto" choices like OpenAI tool-calling API.
-                    should be a dict of the form to force this tool
-                    {"type": "function", "function": {"name": <<tool_name>>}}.
+
+        tool_choice: does not currently support "any", "auto" choices like OpenAI
+            tool-calling API. should be a dict of the form to force this tool
+            {"type": "function", "function": {"name": <<tool_name>>}}.
         """
         formatted_tools = [convert_to_openai_tool(tool) for tool in tools]
+        tool_names = [ft["function"]["name"] for ft in formatted_tools]
         if tool_choice:
             if isinstance(tool_choice, dict):
-                tool_names = [
-                    formatted_tool["function"]["name"]
-                    for formatted_tool in formatted_tools
-                ]
                 if not any(
-                    tool_name == tool_choice["function"]["name"]
-                    for tool_name in tool_names
+                    tool_choice["function"]["name"] == name for name in tool_names
                 ):
                     raise ValueError(
-                        f"Tool choice {tool_choice} was specified, but the only "
+                        f"Tool choice {tool_choice=} was specified, but the only "
                         f"provided tools were {tool_names}."
                     )
+            elif isinstance(tool_choice, str):
+                chosen = [
+                    f for f in formatted_tools if f["function"]["name"] == tool_choice
+                ]
+                if not chosen:
+                    raise ValueError(
+                        f"Tool choice {tool_choice=} was specified, but the only "
+                        f"provided tools were {tool_names}."
+                    )
+            elif isinstance(tool_choice, bool):
+                if len(formatted_tools) > 1:
+                    raise ValueError(
+                        "tool_choice=True can only be specified when a single tool is "
+                        f"passed in. Received {len(tools)} tools."
+                    )
+                tool_choice = formatted_tools[0]
             else:
                 raise ValueError(
-                    """Unrecognized tool_choice type. Expected dict having format like this {"type": "function", "function": {"name": <<tool_name>>}}"""
+                    """Unrecognized tool_choice type. Expected dict having format like 
+                    this {"type": "function", "function": {"name": <<tool_name>>}}"""
                     f"Received: {tool_choice}"
                 )
 
@@ -376,7 +372,175 @@ class ChatLlamaCpp(BaseChatModel):
         formatted_tools = [convert_to_openai_tool(tool) for tool in tools]
         return super().bind(tools=formatted_tools, **kwargs)
 
+    def with_structured_output(
+        self,
+        schema: Optional[Union[Dict, Type[BaseModel]]] = None,
+        *,
+        include_raw: bool = False,
+        **kwargs: Any,
+    ) -> Runnable[LanguageModelInput, Union[Dict, BaseModel]]:
+        """Model wrapper that returns outputs formatted to match the given schema.
 
+        Args:
+            schema: The output schema as a dict or a Pydantic class. If a Pydantic class
+                then the model output will be an object of that class. If a dict then
+                the model output will be a dict. With a Pydantic class the returned
+                attributes will be validated, whereas with a dict they will not be. If
+                `method` is "function_calling" and `schema` is a dict, then the dict
+                must match the OpenAI function-calling spec or be a valid JSON schema
+                with top level 'title' and 'description' keys specified.
+            include_raw: If False then only the parsed structured output is returned. If
+                an error occurs during model output parsing it will be raised. If True
+                then both the raw model response (a BaseMessage) and the parsed model
+                response will be returned. If an error occurs during output parsing it
+                will be caught and returned as well. The final output is always a dict
+                with keys "raw", "parsed", and "parsing_error".
+            kwargs: Any other args to bind to model, ``self.bind(..., **kwargs)``.
+
+        Returns:
+            A Runnable that takes any ChatModel input and returns as output:
+
+                If include_raw is True then a dict with keys:
+                    raw: BaseMessage
+                    parsed: Optional[_DictOrPydantic]
+                    parsing_error: Optional[BaseException]
+
+                If include_raw is False then just _DictOrPydantic is returned,
+                where _DictOrPydantic depends on the schema:
+
+                If schema is a Pydantic class then _DictOrPydantic is the Pydantic
+                    class.
+
+                If schema is a dict then _DictOrPydantic is a dict.
+
+        Example: Pydantic schema (include_raw=False):
+            .. code-block:: python
+
+                from langchain_community.chat_models import ChatLlamaCpp
+                from langchain_core.pydantic_v1 import BaseModel
+
+                class AnswerWithJustification(BaseModel):
+                    '''An answer to the user question along with justification for the answer.'''
+                    answer: str
+                    justification: str
+
+                llm = ChatLlamaCpp(
+                    temperature=0.,
+                    model_path="./SanctumAI-meta-llama-3-8b-instruct.Q8_0.gguf",
+                    n_ctx=10000,
+                    n_gpu_layers=4,
+                    n_batch=200,
+                    max_tokens=512,
+                    n_threads=multiprocessing.cpu_count() - 1,
+                    repeat_penalty=1.5,
+                    top_p=0.5,
+                    stop=["<|end_of_text|>", "<|eot_id|>"],
+                )
+                structured_llm = llm.with_structured_output(AnswerWithJustification)
+
+                structured_llm.invoke("What weighs more a pound of bricks or a pound of feathers")
+
+                # -> AnswerWithJustification(
+                #     answer='They weigh the same',
+                #     justification='Both a pound of bricks and a pound of feathers weigh one pound. The weight is the same, but the volume or density of the objects may differ.'
+                # )
+
+        Example: Pydantic schema (include_raw=True):
+            .. code-block:: python
+
+                from langchain_community.chat_models import ChatLlamaCpp
+                from langchain_core.pydantic_v1 import BaseModel
+
+                class AnswerWithJustification(BaseModel):
+                    '''An answer to the user question along with justification for the answer.'''
+                    answer: str
+                    justification: str
+
+                llm = ChatLlamaCpp(
+                    temperature=0.,
+                    model_path="./SanctumAI-meta-llama-3-8b-instruct.Q8_0.gguf",
+                    n_ctx=10000,
+                    n_gpu_layers=4,
+                    n_batch=200,
+                    max_tokens=512,
+                    n_threads=multiprocessing.cpu_count() - 1,
+                    repeat_penalty=1.5,
+                    top_p=0.5,
+                    stop=["<|end_of_text|>", "<|eot_id|>"],
+                )
+                structured_llm = llm.with_structured_output(AnswerWithJustification, include_raw=True)
+
+                structured_llm.invoke("What weighs more a pound of bricks or a pound of feathers")
+                # -> {
+                #     'raw': AIMessage(content='', additional_kwargs={'tool_calls': [{'id': 'call_Ao02pnFYXD6GN1yzc0uXPsvF', 'function': {'arguments': '{"answer":"They weigh the same.","justification":"Both a pound of bricks and a pound of feathers weigh one pound. The weight is the same, but the volume or density of the objects may differ."}', 'name': 'AnswerWithJustification'}, 'type': 'function'}]}),
+                #     'parsed': AnswerWithJustification(answer='They weigh the same.', justification='Both a pound of bricks and a pound of feathers weigh one pound. The weight is the same, but the volume or density of the objects may differ.'),
+                #     'parsing_error': None
+                # }
+
+        Example: dict schema (include_raw=False):
+            .. code-block:: python
+
+                from langchain_community.chat_models import ChatLlamaCpp
+                from langchain_core.pydantic_v1 import BaseModel
+                from langchain_core.utils.function_calling import convert_to_openai_tool
+
+                class AnswerWithJustification(BaseModel):
+                    '''An answer to the user question along with justification for the answer.'''
+                    answer: str
+                    justification: str
+
+                dict_schema = convert_to_openai_tool(AnswerWithJustification)
+                llm = ChatLlamaCpp(
+                    temperature=0.,
+                    model_path="./SanctumAI-meta-llama-3-8b-instruct.Q8_0.gguf",
+                    n_ctx=10000,
+                    n_gpu_layers=4,
+                    n_batch=200,
+                    max_tokens=512,
+                    n_threads=multiprocessing.cpu_count() - 1,
+                    repeat_penalty=1.5,
+                    top_p=0.5,
+                    stop=["<|end_of_text|>", "<|eot_id|>"],
+                )
+                structured_llm = llm.with_structured_output(dict_schema)
+
+                structured_llm.invoke("What weighs more a pound of bricks or a pound of feathers")
+                # -> {
+                #     'answer': 'They weigh the same',
+                #     'justification': 'Both a pound of bricks and a pound of feathers weigh one pound. The weight is the same, but the volume and density of the two substances differ.'
+                # }
+
+        """  # noqa: E501
+        if kwargs:
+            raise ValueError(f"Received unsupported arguments {kwargs}")
+        is_pydantic_schema = isinstance(schema, type) and issubclass(schema, BaseModel)
+        if schema is None:
+            raise ValueError(
+                "schema must be specified when method is 'function_calling'. "
+                "Received None."
+            )
+        llm = self.bind_tools([schema], tool_choice=True)
+        if is_pydantic_schema:
+            output_parser: OutputParserLike = PydanticToolsParser(
+                tools=[cast(Type, schema)], first_tool_only=True
+            )
+        else:
+            key_name = convert_to_openai_tool(schema)["function"]["name"]
+            output_parser = JsonOutputKeyToolsParser(
+                key_name=key_name, first_tool_only=True
+            )
+
+        if include_raw:
+            parser_assign = RunnablePassthrough.assign(
+                parsed=itemgetter("raw") | output_parser, parsing_error=lambda _: None
+            )
+            parser_none = RunnablePassthrough.assign(parsed=lambda _: None)
+            parser_with_fallback = parser_assign.with_fallbacks(
+                [parser_none], exception_key="parsing_error"
+            )
+            return RunnableMap(raw=llm) | parser_with_fallback
+        else:
+            return llm | output_parser
 
     @property
     def _identifying_params(self) -> Dict[str, Any]:
@@ -390,18 +554,19 @@ class ChatLlamaCpp(BaseChatModel):
             # rules in LLM monitoring applications (e.g., in LangSmith users
             # can provide per token pricing for their model and monitor
             # costs for the given LLM.)
-            **{"model_path": self.model_path}, **self._default_params
+            **{"model_path": self.model_path},
+            **self._default_params,
         }
 
     @property
     def _llm_type(self) -> str:
         """Get the type of language model used by this chat model."""
         return "llama-cpp-python"
-    
+
     @property
     def _default_params(self) -> Dict[str, Any]:
         """Get the default parameters for calling create_chat_completion."""
-        params = {
+        params: Dict = {
             "max_tokens": self.max_tokens,
             "temperature": self.temperature,
             "top_p": self.top_p,
@@ -409,10 +574,7 @@ class ChatLlamaCpp(BaseChatModel):
             "logprobs": self.logprobs,
             "stop_sequences": self.stop,  # key here is convention among LLM classes
             "repeat_penalty": self.repeat_penalty,
-            "top_k": self.top_k,
         }
         if self.grammar:
             params["grammar"] = self.grammar
         return params
-
-
