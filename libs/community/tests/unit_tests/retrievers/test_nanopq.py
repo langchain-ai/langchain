9a5db8704c038a66a44117bd7d826cb1e9bff0df
Vishnu Nandakumar <38393302+Vishnunkumar@users.noreply.github.com>
2024-06-09 10:10:04 +0530

    fix(nanopq): fixed code for unit tests for nanopq

--- libs/community/tests/unit_tests/retrievers/test_nanopq.py
+++ libs/community/tests/unit_tests/retrievers/test_nanopq.py
@@ -2,7 +2,7 @@ import pytest
 from langchain_core.documents import Document
 
 from langchain_community.embeddings import FakeEmbeddings
-from langchain_community.retrievers.product_quantization import NanoPQRetriever
+from langchain_community.retrievers.nanopq import NanoPQRetriever
 
 
 class TestPQRetriever:
