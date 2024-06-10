36e713c927bba229f234b8ebc3414618a3e16ebd
marlenezw <marlenemhangami@gmail.com>
2024-06-10 10:29:38 +0100

    renaming credential in azure search to azure_credential and updating docs.

--- libs/community/langchain_community/vectorstores/azuresearch.py
+++ libs/community/langchain_community/vectorstores/azuresearch.py
@@ -74,7 +74,7 @@ def _get_search_client(
     endpoint: str,
     key: str,
     index_name: str,
-    credential: Optional[Callable],
+    azure_credential: Optional[Callable],
     semantic_configuration_name: Optional[str] = None,
     fields: Optional[List[SearchField]] = None,
     vector_search: Optional[VectorSearch] = None,
@@ -111,8 +111,8 @@ def _get_search_client(
     )
 
     default_fields = default_fields or []
-    if credential:
-        credential = credential
+    if azure_credential:
+        credential = azure_credential
     elif key is None:
         credential = DefaultAzureCredential()
     elif key.upper() == "INTERACTIVE":
@@ -246,7 +246,7 @@ class AzureSearch(VectorStore):
         azure_search_endpoint: str,
         index_name: str,
         embedding_function: Union[Callable, Embeddings],
-        credential: Optional[Callable],
+        azure_credential: Optional[Callable],
         azure_search_key: Optional[str] = None,
         search_type: str = "hybrid",
         semantic_configuration_name: Optional[str] = None,
@@ -315,7 +315,7 @@ class AzureSearch(VectorStore):
             azure_search_endpoint,
             azure_search_key,
             index_name,
-            credential=credential,
+            azure_credential=azure_credential,
             semantic_configuration_name=semantic_configuration_name,
             fields=fields,
             vector_search=vector_search,
