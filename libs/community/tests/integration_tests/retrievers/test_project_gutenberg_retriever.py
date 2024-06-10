89587780209e1666b0ad65aafbe1c50e40921d89
Oguz Vuruskaner <ovuruska@outlook.com>
2024-06-10 06:46:24 +0300

    lint

--- libs/community/tests/integration_tests/retrievers/test_project_gutenberg_retriever.py
+++ libs/community/tests/integration_tests/retrievers/test_project_gutenberg_retriever.py
@@ -5,11 +5,8 @@ from langchain_community.retrievers import ProjectGutenbergRetriever
 
 
 def test_project_gutenberg_retriever() -> None:
-
     retrievers = ProjectGutenbergRetriever()
-    documents = retrievers.invoke(
-        "Crime and Punishment"
-    )
+    documents = retrievers.invoke("Crime and Punishment")
     assert len(documents) == 1
     for doc in documents:
         assert isinstance(doc, Document)
