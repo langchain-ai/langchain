69799ef5969bfe2472a501d4b376985cb75eb292
Marlene <57748216+marlenezw@users.noreply.github.com>
2024-06-10 15:05:03 +0000

    adding tests, updating vector store notebook and fixing minor error in retriever notebook.

--- libs/community/tests/integration_tests/retrievers/test_azure_ai_search.py
+++ libs/community/tests/integration_tests/retrievers/test_azure_ai_search.py
@@ -68,3 +68,32 @@ async def test_azure_cognitive_search_ainvoke() -> None:
     for doc in documents:
         assert isinstance(doc, Document)
         assert doc.page_content
+
+
+def test_azure_ai_search_invoke_managed_identity() -> None:
+    """Test valid call to Azure AI Search using managed identity.
+
+    In order to run this test, you should provide
+    `service_name`,index_name`, and azure_ad_token_provider,
+    as arguments for the AzureAISearchRetriever in both tests.
+    api_version, aiosession and topk_k are optional parameters.
+    """
+    from azure.identity import DefaultAzureCredential, get_bearer_token_provider
+
+    bearer_token_provider = get_bearer_token_provider(
+        DefaultAzureCredential(), "https://search.azure.com/.default"
+    )
+
+    retriever = AzureAISearchRetriever(azure_ad_token_provider=bearer_token_provider)
+
+    documents = retriever.invoke("what is langchain?")
+    for doc in documents:
+        assert isinstance(doc, Document)
+        assert doc.page_content
+
+    retriever = AzureAISearchRetriever(
+        azure_ad_token_provider=bearer_token_provider, top_k=1
+    )
+
+    documents = retriever.invoke("what is langchain?")
+    assert len(documents) <= 1
