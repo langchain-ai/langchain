f3e41d5818a203068ec668bb3a05a6d673cab0e6
Bagatur <baskaryan@gmail.com>
2024-06-12 11:27:56 -0700

    fmt

--- libs/core/tests/unit_tests/messages/test_utils.py
+++ libs/core/tests/unit_tests/messages/test_utils.py
@@ -1,4 +1,13 @@
-from langchain_core.messages import AIMessage
+from typing import Type
+
+import pytest
+
+from langchain_core.messages import (
+    AIMessage,
+    BaseMessage,
+    HumanMessage,
+    SystemMessage,
+)
 from langchain_core.messages.utils import (
     filter_messages,
     merge_message_runs,
@@ -6,29 +15,35 @@ from langchain_core.messages.utils import (
 )
 
 
-def test_merge_message_runs_str() -> None:
-    messages = [AIMessage("foo"), AIMessage("bar"), AIMessage("baz")]
-    expected = [AIMessage("foobar")]
+@pytest.mark.parametrize("msg_cls", [HumanMessage, AIMessage, SystemMessage])
+def test_merge_message_runs_str(msg_cls: Type[BaseMessage]) -> None:
+    messages = [msg_cls("foo"), msg_cls("bar"), msg_cls("baz")]
+    expected = [msg_cls("foo\nbar\nbaz")]
     actual = merge_message_runs(messages)
-    actual == expected
+    assert actual == expected
 
 
 def test_merge_message_runs_content() -> None:
     messages = [
-        AIMessage("foo"),
-        AIMessage(
-            [{"text": "bar", "type": "text"}, {"image_url": "...", "type": "image_url"}]
+        HumanMessage("foo", id="1"),
+        HumanMessage(
+            [
+                {"text": "bar", "type": "text"},
+                {"image_url": "...", "type": "image_url"},
+            ],
+            id="2",
         ),
-        AIMessage("baz"),
+        HumanMessage("baz", id="3"),
     ]
     expected = [
-        AIMessage(
+        HumanMessage(
             [
                 "foo",
                 {"text": "bar", "type": "text"},
                 {"image_url": "...", "type": "image_url"},
                 "baz",
-            ]
+            ],
+            id="1",
         ),
     ]
     actual = merge_message_runs(messages)
