caef566f3c0b9b11cba597867d62834114028980
isaac hershenson <ihershenson@hmc.edu>
2024-06-13 16:47:32 -0700

    fmt

--- docs/scripts/document_loader_feat_table.py
+++ docs/scripts/document_loader_feat_table.py
@@ -23,6 +23,7 @@ The following table shows the feature support for all document loaders.
 
 """
 
+
 def get_document_loader_table() -> str:
     """Get the table of document loaders."""
 
@@ -31,27 +32,24 @@ def get_document_loader_table() -> str:
         doc_loaders_feat_table[cm] = {}
         cls = getattr(document_loaders, cm)
         if issubclass(cls, BaseLoader):
-            for feat in ("aload", "alazy_load", ("lazy_load","lazy_loading")):
+            for feat in ("aload", "alazy_load", ("lazy_load", "lazy_loading")):
                 if isinstance(feat, tuple):
                     feat, name = feat
                 else:
                     feat, name = feat, feat
-                doc_loaders_feat_table[cm][name] = getattr(cls, feat) != getattr(BaseLoader, feat)
-            native_async = doc_loaders_feat_table[cm]["aload"] or doc_loaders_feat_table[cm]["alazy_load"]
+                doc_loaders_feat_table[cm][name] = getattr(cls, feat) != getattr(
+                    BaseLoader, feat
+                )
+            native_async = (
+                doc_loaders_feat_table[cm]["aload"]
+                or doc_loaders_feat_table[cm]["alazy_load"]
+            )
             del doc_loaders_feat_table[cm]["aload"]
             del doc_loaders_feat_table[cm]["alazy_load"]
             doc_loaders_feat_table[cm]["native_async"] = native_async
 
-    header = [
-        "loader",
-        "lazy_loading",
-        "native_async"
-    ]
-    title = [
-        "Document Loader",
-        "Document Lazy Loading",
-        "Native Async Support"
-    ]
+    header = ["loader", "lazy_loading", "native_async"]
+    title = ["Document Loader", "Document Lazy Loading", "Native Async Support"]
     rows = [title, [":-"] + [":-:"] * (len(title) - 1)]
     for llm, feats in sorted(doc_loaders_feat_table.items()):
         rows += [[llm] + ["✅" if feats.get(h) else "❌" for h in header[1:]]]
@@ -64,6 +62,8 @@ if __name__ == "__main__":
     output_integrations_dir_doc_loaders = output_integrations_dir / "document_loaders"
     output_integrations_dir_doc_loaders.mkdir(parents=True, exist_ok=True)
 
-    document_loader_page = DOCUMENT_LOADER_TEMPLATE.format(table=get_document_loader_table())
+    document_loader_page = DOCUMENT_LOADER_TEMPLATE.format(
+        table=get_document_loader_table()
+    )
     with open(output_integrations_dir / "document_loaders" / "index.mdx", "w") as f:
         f.write(document_loader_page)
