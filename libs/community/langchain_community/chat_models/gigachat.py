6b17346444d534c131d4d67eebe88e93077e04f7
Mikhail Murtazin <murtazkorpaz@gmail.com>
2024-06-13 16:05:13 +0300

    fix

--- libs/community/langchain_community/chat_models/gigachat.py
+++ libs/community/langchain_community/chat_models/gigachat.py
@@ -502,7 +502,8 @@ class GigaChat(_BaseGigaChat, BaseChatModel):
         method: Literal["function_calling", "json_mode"] = "function_calling",
         include_raw: Literal[True] = True,
         **kwargs: Any,
-    ) -> Runnable[LanguageModelInput, _AllReturnType]: ...
+    ) -> Runnable[LanguageModelInput, _AllReturnType]:
+        ...
 
     @overload
     def with_structured_output(
@@ -512,7 +513,8 @@ class GigaChat(_BaseGigaChat, BaseChatModel):
         method: Literal["function_calling", "json_mode"] = "function_calling",
         include_raw: Literal[False] = False,
         **kwargs: Any,
-    ) -> Runnable[LanguageModelInput, _DictOrPydantic]: ...
+    ) -> Runnable[LanguageModelInput, _DictOrPydantic]:
+        ...
 
     @beta()
     def with_structured_output(
