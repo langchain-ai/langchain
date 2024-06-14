50d6a4548493a453b6853ddfebac5365b54f4fae
vbarda <vadym@langchain.dev>
2024-06-14 16:23:36 -0400

    update naming & remove chunk modifier

--- libs/core/langchain_core/messages/modifiers.py
+++ libs/core/langchain_core/messages/modifiers.py
@@ -3,14 +3,14 @@ from typing import Any, List, Literal
 from langchain_core.messages.base import BaseMessage, BaseMessageChunk
 
 
-class ModifierMessage(BaseMessage):
+class MessageModifier(BaseMessage):
     """Message responsible for modifying other messages (deleting / updating.)"""
 
-    def __init__(self, **kwargs: Any) -> None:
-        return super().__init__("modifier", **kwargs)
+    def __init__(self, id: str) -> None:
+        return super().__init__("modifier", id=id)
 
 
-class RemoveMessage(ModifierMessage):
+class RemoveMessageModifier(MessageModifier):
     """Message responsible for deleting other messages."""
 
     type: Literal["remove"] = "remove"
@@ -21,18 +21,4 @@ class RemoveMessage(ModifierMessage):
         return ["langchain", "schema", "messages"]
 
 
-RemoveMessage.update_forward_refs()
-
-
-class RemoveMessageChunk(RemoveMessage, BaseMessageChunk):
-    """Remove Message chunk."""
-
-    # Ignoring mypy re-assignment here since we're overriding the value
-    # to make sure that the chunk variant can be discriminated from the
-    # non-chunk variant.
-    type: Literal["RemoveMessageChunk"] = "RemoveMessageChunk"  # type: ignore[assignment]
-
-    @classmethod
-    def get_lc_namespace(cls) -> List[str]:
-        """Get the namespace of the langchain object."""
-        return ["langchain", "schema", "messages"]
+RemoveMessageModifier.update_forward_refs()
