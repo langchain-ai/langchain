69d12eea2692e7291adf9356764eeffe23436548
Jiro Shimaya <shimaya.jiro@irl.sys.es.osaka-u.ac.jp>
2024-06-16 13:12:34 +0900

    make format

--- libs/langchain/tests/unit_tests/retrievers/test_ensemble.py
+++ libs/langchain/tests/unit_tests/retrievers/test_ensemble.py
@@ -1,13 +1,11 @@
 from typing import Any, List
 
+import pytest
 from langchain_core.documents import Document
 from langchain_core.retrievers import BaseRetriever
 
 from langchain.retrievers.ensemble import EnsembleRetriever
 
-import pytest
-
-from typing import List
 
 class MockRetriever(BaseRetriever):
     docs: List[Document]
@@ -18,32 +16,40 @@ class MockRetriever(BaseRetriever):
 
 
 def test_invoke() -> None:
-    documents1 = [Document(page_content = "a", metadata = {"id": 1}),
-                  Document(page_content = "b", metadata = {"id": 2}),
-                  Document(page_content = "c", metadata = {"id": 3})]
-    documents2 = [Document(page_content = "b")]
+    documents1 = [
+        Document(page_content="a", metadata={"id": 1}),
+        Document(page_content="b", metadata={"id": 2}),
+        Document(page_content="c", metadata={"id": 3}),
+    ]
+    documents2 = [Document(page_content="b")]
 
-    retriever1 = MockRetriever(docs = documents1)
-    retriever2 = MockRetriever(docs = documents2)
+    retriever1 = MockRetriever(docs=documents1)
+    retriever2 = MockRetriever(docs=documents2)
 
-    ensemble_retriever = EnsembleRetriever(retrievers = [retriever1, retriever2], id_key = None)
+    ensemble_retriever = EnsembleRetriever(
+        retrievers=[retriever1, retriever2], id_key=None
+    )
     ranked_documents = ensemble_retriever.invoke("_")
 
-    # The document with page_content "b" in documents2 will be merged with the document 
+    # The document with page_content "b" in documents2 will be merged with the document
     # with page_content "b" in documents1, so the length of ranked_documents should be 3.
     # Additionally, the document with page_content "b" will be ranked 1st.
     assert len(ranked_documents) == 3
     assert ranked_documents[0].page_content == "b"
 
-    documents1 = [Document(page_content = "a", metadata = {"id": 1}),
-                  Document(page_content = "b", metadata = {"id": 2}),
-                  Document(page_content = "c", metadata = {"id": 3})]
-    documents2 = [Document(page_content = "d")]
+    documents1 = [
+        Document(page_content="a", metadata={"id": 1}),
+        Document(page_content="b", metadata={"id": 2}),
+        Document(page_content="c", metadata={"id": 3}),
+    ]
+    documents2 = [Document(page_content="d")]
 
-    retriever1 = MockRetriever(docs = documents1)
-    retriever2 = MockRetriever(docs = documents2)
+    retriever1 = MockRetriever(docs=documents1)
+    retriever2 = MockRetriever(docs=documents2)
 
-    ensemble_retriever = EnsembleRetriever(retrievers = [retriever1, retriever2], id_key = None)
+    ensemble_retriever = EnsembleRetriever(
+        retrievers=[retriever1, retriever2], id_key=None
+    )
     ranked_documents = ensemble_retriever.invoke("_")
 
     # The document with page_content "d" in documents2 will not be merged with any document in documents1,
@@ -53,15 +59,19 @@ def test_invoke() -> None:
     assert len(ranked_documents) == 4
     assert ranked_documents[0].page_content == "a"
 
-    documents1 = [Document(page_content = "a", metadata = {"id": 1}),
-                  Document(page_content = "b", metadata = {"id": 2}),
-                  Document(page_content = "c", metadata = {"id": 3})]
-    documents2 = [Document(page_content = "d", metadata = {"id": 2})]
+    documents1 = [
+        Document(page_content="a", metadata={"id": 1}),
+        Document(page_content="b", metadata={"id": 2}),
+        Document(page_content="c", metadata={"id": 3}),
+    ]
+    documents2 = [Document(page_content="d", metadata={"id": 2})]
 
-    retriever1 = MockRetriever(docs = documents1)
-    retriever2 = MockRetriever(docs = documents2)
+    retriever1 = MockRetriever(docs=documents1)
+    retriever2 = MockRetriever(docs=documents2)
 
-    ensemble_retriever = EnsembleRetriever(retrievers = [retriever1, retriever2], id_key = "id")
+    ensemble_retriever = EnsembleRetriever(
+        retrievers=[retriever1, retriever2], id_key="id"
+    )
     ranked_documents = ensemble_retriever.invoke("_")
 
     # Since id_key is specified, the document with id 2 will be merged.
@@ -69,7 +79,3 @@ def test_invoke() -> None:
     # Additionally, the document with page_content "b" will be ranked 1st.
     assert len(ranked_documents) == 3
     assert ranked_documents[0].page_content == "b"
-
-
-
-
