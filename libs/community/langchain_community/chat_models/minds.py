659b67e057acd90ad4469b11855257d189dad8b2
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-06-17 21:34:53 +0530

    implemented the method for validating the environment

--- libs/community/langchain_community/chat_models/minds.py
+++ libs/community/langchain_community/chat_models/minds.py
@@ -3,6 +3,7 @@ from langchain_core.pydantic_v1 import Field, SecretStr, root_validator
 from langchain_community.chat_models.openai import (
     ChatOpenAI,
 )
+from langchain_core.utils import convert_to_secret_str, get_from_dict_or_env
 
 DEFAULT_API_BASE = "https://llm.mdb.ai"
 DEFAULT_MODEL = "gpt-3.5-turbo"
@@ -37,4 +38,75 @@ class ChatMinds(ChatOpenAI):
 
     @root_validator()
     def validate_environment(cls, values: Dict) -> Dict:
-        pass
+        """
+        Validate that the MindsDB API key and the OpenAI exists in the environment.
+        """
+        # Validate that the API key and base URL are available.
+        values["mindsdb_api_key"] = convert_to_secret_str(
+            get_from_dict_or_env(
+                values,
+                "mindsdb_api_key",
+                "MINDSDB_API_KEY",
+            )
+        )
+        values["mindsdb_api_base"] = get_from_dict_or_env(
+            values,
+            "mindsdb_api_base",
+            "MINDSDB_API_BASE",
+            default=DEFAULT_API_BASE,
+        )
+
+        # Validate that the `openai` can be imported.
+        try:
+            import openai
+
+        except ImportError as e:
+            raise ImportError(
+                "Could not import openai python package. "
+                "Please install it with `pip install openai`.",
+            ) from e
+
+        # Set the client based on the version of the `openai` package that is being used.
+        try:
+            if is_openai_v1():
+                client_params = {
+                    "api_key": values["mindsdb_api_key"].get_secret_value(),
+                    "base_url": values["mindsdb_api_base"],
+                    # TODO: Add other parameters
+                }
+                if not values.get("client"):
+                    values["client"] = openai.OpenAI(**client_params).chat.completions
+                if not values.get("async_client"):
+                    values["async_client"] = openai.AsyncOpenAI(
+                        **client_params
+                    ).chat.completions
+            else:
+                values["openai_api_base"] = values["mindsdb_api_base"]
+                values["openai_api_key"] = values["mindsdb_api_key"].get_secret_value()
+                values["client"] = openai.ChatCompletion
+        except AttributeError as exc:
+            raise ValueError(
+                "`openai` has no `ChatCompletion` attribute, this is likely "
+                "due to an old version of the openai package. Try upgrading it "
+                "with `pip install --upgrade openai`.",
+            ) from exc
+
+        # Validate that the chosen model provided is supported.
+        if "model_name" not in values.keys():
+            values["model_name"] = DEFAULT_MODEL
+
+        model_name = values["model_name"]
+        available_models = cls.get_available_models(
+            values["mindsdb_api_key"].get_secret_value(),
+            values["mindsdb_api_base"],
+        )
+
+        if model_name not in available_models:
+            raise ValueError(
+                f"Model name {model_name} not found in available models: "
+                f"{available_models}.",
+            )
+
+        values["available_models"] = available_models
+
+        return values
