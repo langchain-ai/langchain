00c3f5f39d2fa8f76d951392224981285b35c433
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-06-18 23:35:46 +0530

    added a few simple validations

--- libs/community/langchain_community/utilities/mindsdb.py
+++ libs/community/langchain_community/utilities/mindsdb.py
@@ -1,5 +1,5 @@
 from openai import OpenAI
-from typing import Text, Dict, Optional
+from typing import Text, Dict, Literal
 from mindsdb_sdk.utils.mind import Mind, create_mind
 
 from langchain_core.pydantic_v1 import BaseModel, Field, SecretStr, PrivateAttr, root_validator
@@ -8,20 +8,52 @@ DEFAULT_API_BASE = "https://llm.mdb.ai"
 DEFAULT_MODEL = "gpt-3.5-turbo"
 
 
+class PostgresConnection(BaseModel):
+    user: Text
+    password: Text
+    host: Text
+    port: Text = Field(default=5432)
+    database: Text
+    schema: Text
+
+
 class DatabaseMindWrapper(BaseModel):
     mindsdb_api_key: SecretStr = Field(default=None)
     mindsdb_api_base: Text = Field(default=DEFAULT_API_BASE)
     model: Text = Field(default=DEFAULT_MODEL)
     name: Text
     description: Text
-    data_source_type: Text
+    data_source_type: Text = Literal['postgres']
     data_source_connection_args: Dict
 
     _mind: Mind = PrivateAttr()
 
     @root_validator()
     def validate_environment(cls, values: Dict) -> Dict:
-        pass
+        # Validate that the `mindsdb_sdk` and `openai' packages can be imported.
+        try:
+            import openai
+
+        except ImportError as e:
+            raise ImportError(
+                "Could not import openai python package. "
+                "Please install it with `pip install openai`.",
+            ) from e
+
+        try:
+            import mindsdb_sdk
+
+        except ImportError as e:
+            raise ImportError(
+                "Could not import mindsdb_sdk python package. "
+                "Please install it with `pip install mindsdb_sdk`.",
+            ) from e
+
+        # Validate that the correct connection arguments are provided for the chosen data source.
+        if values['data_source_type'] == 'postgres':
+            PostgresConnection(**values['data_source_connection_args'])
+
+        return values
 
     def _create_mind(self) -> Mind:
         pass
