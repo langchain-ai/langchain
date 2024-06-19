2b08e9e26561c7cfa94037b0d5e88ed63071986f
ccurme <chester.curme@gmail.com>
2024-06-19 11:24:38 -0400

    core[patch]: update test to catch circular imports (#23172)
    This raises ImportError due to a circular import:
    ```python
    from langchain_core import chat_history
    ```
    
    This does not:
    ```python
    from langchain_core import runnables
    from langchain_core import chat_history
    ```
    
    Here we update `test_imports` to run each import in a separate
    subprocess. Open to other ways of doing this!

--- libs/core/langchain_core/runnables/__init__.py
+++ libs/core/langchain_core/runnables/__init__.py
@@ -36,7 +36,6 @@ from langchain_core.runnables.config import (
     run_in_executor,
 )
 from langchain_core.runnables.fallbacks import RunnableWithFallbacks
-from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.runnables.passthrough import (
     RunnableAssign,
     RunnablePassthrough,
@@ -79,7 +78,6 @@ __all__ = [
     "RunnablePick",
     "RunnableSequence",
     "RunnableWithFallbacks",
-    "RunnableWithMessageHistory",
     "get_config_list",
     "aadd",
     "add",
