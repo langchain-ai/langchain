f92c1332520ddcdccd4ec9ce5e00c046aba56b37
Chester Curme <chester.curme@gmail.com>
2024-06-19 11:04:07 -0400

    add comment to test

--- libs/core/tests/unit_tests/test_imports.py
+++ libs/core/tests/unit_tests/test_imports.py
@@ -12,12 +12,16 @@ def test_importable_all() -> None:
         if relative_path.endswith(".typed"):
             continue
         module_name = relative_path.split(".")[0]
+        module = importlib.import_module("langchain_core." + module_name)
+        all_ = getattr(module, "__all__", [])
+        for cls_ in all_:
+            getattr(module, cls_)
+
+        # Test import in isolation
+        # Note: ImportErrors due to circular imports can be raised
+        # for one sequence of imports but not another.
         result = subprocess.run(
             ["python", "-c", f"import langchain_core.{module_name}"],
         )
         if result.returncode != 0:
             pytest.fail(f"Failed to import {module_name}.")
-        module = importlib.import_module("langchain_core." + module_name)
-        all_ = getattr(module, "__all__", [])
-        for cls_ in all_:
-            getattr(module, cls_)
