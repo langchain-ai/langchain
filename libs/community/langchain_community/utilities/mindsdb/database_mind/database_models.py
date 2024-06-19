3e81b530c3f3daf4765131e9a6da2276fe4bfbbd
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-06-19 20:28:24 +0530

    added the model for MySQL

--- libs/community/langchain_community/utilities/mindsdb/database_mind/database_models.py
+++ libs/community/langchain_community/utilities/mindsdb/database_mind/database_models.py
@@ -9,3 +9,11 @@ class PostgresModel(BaseModel):
     port: Text = Field(default=5432)
     database: Text
     database_schema: Text = Field(alias='schema')
+
+
+class MySQLModel(BaseModel):
+    user: Text
+    password: Text
+    host: Text
+    port: Text = Field(default=5432)
+    database: Text
