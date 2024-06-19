b7adfbdbce850bb3528fb012f304017338721ef1
Bagatur <baskaryan@gmail.com>
2024-06-19 11:56:37 -0700

    fmt

--- libs/cli/langchain_cli/namespaces/integration.py
+++ libs/cli/langchain_cli/namespaces/integration.py
@@ -154,7 +154,8 @@ def create_doc(
         str,
         typer.Option(
             help=(
-                "The type of component. Currently only 'ChatModel', 'DocumentLoader' supported."
+                "The type of component. Currently only 'ChatModel', 'DocumentLoader' "
+                "supported."
             )
         ),
     ] = "ChatModel",
