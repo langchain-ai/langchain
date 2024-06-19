81caa61f3cf5e9785aea3a8a0303e230249d17a1
Eugene Yurtsev <eyurtsev@gmail.com>
2024-06-19 10:49:25 -0400

    Update test_ovhcloud.py

--- libs/community/tests/unit_tests/embeddings/test_ovhcloud.py
+++ libs/community/tests/unit_tests/embeddings/test_ovhcloud.py
@@ -23,10 +23,4 @@ def test_ovhcloud_empty_access_token_should_not_raise_error() -> None:
         model_name="multilingual-e5-base", region="kepler", access_token=""
     )
     assert isinstance(llm, OVHCloudEmbeddings)
-
-@pytest.mark.xfail(reason="Attempts to make network requests.")
-def test_ovhcloud_embed_documents() -> None:
-    llm = OVHCloudEmbeddings(model_name="multilingual-e5-base")
-    docs = ["Hello", "World"]
-    output = llm.embed_documents(docs)
-    assert len(output) == len(docs)
+    
