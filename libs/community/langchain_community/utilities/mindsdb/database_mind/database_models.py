99a6f03fec47e00b864b9acfc501bc55b7196a72
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-06-20 00:13:30 +0530

    added funcs to get supported data sources and validate creds

--- libs/community/langchain_community/utilities/mindsdb/database_mind/database_models.py
+++ libs/community/langchain_community/utilities/mindsdb/database_mind/database_models.py
@@ -2,14 +2,23 @@ from typing import Text, Dict
 from langchain_core.pydantic_v1 import BaseModel, Field
 
 
-def validate_data_source_connection_args(data_source_type: Text, data_source_connection_args: Dict):
-    if data_source_type == 'postgres':
-        return PostgresModel(**data_source_connection_args)
+def validate_data_source_connection_args(data_source_type: Text, data_source_connection_args: Dict) -> None:
+    supported_data_sources = get_supported_data_sources()
 
-    elif data_source_type in ['mysql', 'mariadb']:
-        return MySQLModel(**data_source_connection_args)
+    model = supported_data_sources[data_source_type]
+    model(**data_source_connection_args)
 
 
+# TODO: Improve this method to generate the mapping dynamically.
+def get_supported_data_sources() -> Dict[Text: BaseModel]:
+    return {
+        "postgres": PostgresModel,
+        "mysql": MySQLModel,
+        "mariadb": MySQLModel
+    }
+
+
+# TODO: Allow credentials to be specified as environment variables.
 class PostgresModel(BaseModel):
     user: Text
     password: Text
