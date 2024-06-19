f4b7a7e20ba6e6e0b526b8d9b233d7ca68d3b3d5
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-06-19 20:27:55 +0530

    moved PostgresConnection to database_models.py

---
+++ libs/community/langchain_community/utilities/mindsdb/database_mind/database_models.py
@@ -0,0 +1,11 @@
+from typing import Text
+from langchain_core.pydantic_v1 import BaseModel, Field
+
+
+class PostgresModel(BaseModel):
+    user: Text
+    password: Text
+    host: Text
+    port: Text = Field(default=5432)
+    database: Text
+    database_schema: Text = Field(alias='schema')
