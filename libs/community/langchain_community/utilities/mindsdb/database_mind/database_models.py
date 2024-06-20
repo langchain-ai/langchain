1af662654ab68b005c050d8e1595f615ed78a730
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-06-20 17:39:21 +0530

    allowed database creds to be passed as env vars

--- libs/community/langchain_community/utilities/mindsdb/database_mind/database_models.py
+++ libs/community/langchain_community/utilities/mindsdb/database_mind/database_models.py
@@ -1,12 +1,7 @@
 from typing import Text, Dict
-from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_core.pydantic_v1 import BaseModel, Field, SecretStr, root_validator
 
-
-def validate_data_source_connection_args(data_source_type: Text, data_source_connection_args: Dict) -> None:
-    supported_data_sources = get_supported_data_sources()
-
-    model = supported_data_sources[data_source_type]
-    model(**data_source_connection_args)
+from langchain_core.utils import convert_to_secret_str, get_from_dict_or_env
 
 
 # TODO: Improve this method to generate the mapping dynamically.
@@ -18,19 +13,95 @@ def get_supported_data_sources() -> Dict:
     }
 
 
-# TODO: Allow credentials to be specified as environment variables.
 class PostgresModel(BaseModel):
-    user: Text
-    password: Text
-    host: Text
-    port: Text = Field(default=5432)
-    database: Text
-    database_schema: Text = Field(alias='schema')
+    user: Text = Field(default=None)
+    password: SecretStr = Field(default=None)
+    host: Text = Field(default=None)
+    port: int = Field(default=5432)
+    database: Text = Field(default=None)
+    database_schema: Text = Field(alias='schema', default=None)
+
+    @root_validator()
+    def validate_environment(cls, values: Dict) -> Dict:
+        values["user"] = get_from_dict_or_env(
+            values,
+            "user",
+            "DATABASE_USER",
+        )
+        values["password"] = convert_to_secret_str(
+            get_from_dict_or_env(
+                values,
+                "password",
+                "DATABASE_PASSWORD",
+            )
+        )
+        values["host"] = get_from_dict_or_env(
+            values,
+            "host",
+            "DATABASE_HOST",
+        )
+        values["port"] = get_from_dict_or_env(
+            values,
+            "port",
+            "DATABASE_PORT",
+            default=5432,
+        )
+        values["database"] = get_from_dict_or_env(
+            values,
+            "database",
+            "DATABASE_DATABASE",
+        )
+        values["schema"] = get_from_dict_or_env(
+            values,
+            "database_schema",
+            "DATABASE_SCHEMA",
+        )
+        del values["database_schema"]
+
+        return values
+    
+    def dict(self, **kwargs):
+        base_dict = super().dict(**kwargs)
+
+        # Convert the secret password to a string.
+        base_dict["password"] = base_dict["password"].get_secret_value()
+        return base_dict
 
 
 class MySQLModel(BaseModel):
-    user: Text
-    password: Text
-    host: Text
-    port: Text = Field(default=5432)
-    database: Text
+    user: Text = Field(default=None)
+    password: SecretStr = Field(default=None)
+    host: Text = Field(default=None)
+    port: int = Field(default=3306)
+    database: Text = Field(default=None)
+
+    @root_validator()
+    def validate_environment(cls, values: Dict) -> Dict:
+        values["user"] = get_from_dict_or_env(
+            values,
+            "user",
+            "DATABASE_USER",
+        )
+        values["password"] = convert_to_secret_str(
+            get_from_dict_or_env(
+                values,
+                "password",
+                "DATABASE_PASSWORD",
+            )
+        )
+        values["host"] = get_from_dict_or_env(
+            values,
+            "host",
+            "DATABASE_HOST",
+        )
+        values["port"] = get_from_dict_or_env(
+            values,
+            "port",
+            "DATABASE_PORT",
+            default=5432,
+        )
+        values["database"] = get_from_dict_or_env(
+            values,
+            "database",
+            "DATABASE_DATABASE",
+        )
