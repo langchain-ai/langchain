a8f2c6aa022e1e8938e1d26ca889b90f3490ad8c
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-06-20 16:26:34 +0530

    added common validations to base mind wrapper

--- libs/community/langchain_community/utilities/mindsdb/base_mind_wrapper.py
+++ libs/community/langchain_community/utilities/mindsdb/base_mind_wrapper.py
@@ -1,12 +1,79 @@
-from typing import Text
-from langchain_core.pydantic_v1 import BaseModel, Field, SecretStr
+from typing import Text, Dict, Any
+
+from langchain_community.utils.openai import is_openai_v1
+
+from langchain_core.utils import convert_to_secret_str, get_from_dict_or_env
+from langchain_core.pydantic_v1 import BaseModel, Field, PrivateAttr, SecretStr, root_validator
 
 DEFAULT_API_BASE = "https://llm.mdb.ai"
 DEFAULT_MODEL = "gpt-3.5-turbo"
 
 
 class BaseMindWrapper(BaseModel):
-    mindsdb_api_key: SecretStr = Field(default=None)
+    mindsdb_api_key: SecretStr = Field(default=None, alias="api_key")
     mindsdb_api_base: Text = Field(default=DEFAULT_API_BASE)
     model: Text = Field(default=DEFAULT_MODEL)
     name: Text
+
+    client: Any = Field(default=None, exclude=True)
+
+    @root_validator()
+    def validate_environment(cls, values: Dict) -> Dict:
+        """
+        Validate that the MindsDB API credentials are provided and that the required packages are installed.
+        Further, validate that the chosen model is supported by the MindsDB API.
+        """
+        # Validate that the API key and base URL are available.
+        values["mindsdb_api_key"] = convert_to_secret_str(
+            get_from_dict_or_env(
+                values,
+                "mindsdb_api_key",
+                "MINDSDB_API_KEY",
+            )
+        )
+        values["mindsdb_api_base"] = get_from_dict_or_env(
+            values,
+            "mindsdb_api_base",
+            "MINDSDB_API_BASE",
+            default=DEFAULT_API_BASE,
+        )
+
+        # Validate that the `mindsdb_sdk` and `openai' packages can be imported.
+        try:
+            import openai
+
+        except ImportError as e:
+            raise ImportError(
+                "Could not import openai python package. "
+                "Please install it with `pip install openai`.",
+            ) from e
+
+        try:
+            import mindsdb_sdk
+
+        except ImportError as e:
+            raise ImportError(
+                "Could not import mindsdb_sdk python package. "
+                "Please install it with `pip install mindsdb_sdk`.",
+            ) from e
+
+        # Set the client based on the version of the `openai` package that is being used.
+        try:
+            if is_openai_v1():
+                client_params = {
+                    "api_key": values["mindsdb_api_key"].get_secret_value(),
+                    "base_url": values["mindsdb_api_base"],
+                }
+                if not values.get("client"):
+                    values["client"] = openai.OpenAI(**client_params).chat.completions
+
+            else:
+                values["openai_api_base"] = values["mindsdb_api_base"]
+                values["openai_api_key"] = values["mindsdb_api_key"].get_secret_value()
+                values["client"] = openai.ChatCompletion
+        except AttributeError as exc:
+            raise ValueError(
+                "`openai` has no `ChatCompletion` attribute, this is likely "
+                "due to an old version of the openai package. Try upgrading it "
+                "with `pip install --upgrade openai`.",
+            ) from exc
