1fbf44a1a128edd945ec5cadd18a960c16a05d83
Alexander Dicke <adicke.math@gmail.com>
2024-06-22 10:23:00 +0200

    ignore linting in test

--- libs/community/tests/unit_tests/llms/test_huggingface_endpoint.py
+++ libs/community/tests/unit_tests/llms/test_huggingface_endpoint.py
@@ -9,6 +9,6 @@ from langchain_community.llms.huggingface_endpoint import HuggingFaceEndpoint
 @pytest.mark.requires("huggingface_hub")
 def test_disable_api_token_check() -> None:
     with pytest.raises(ValidationError):
-        HuggingFaceEndpoint(endpoint_url="some-url")
+        HuggingFaceEndpoint(endpoint_url="some-url")  # type: ignore[call-arg]
 
-    HuggingFaceEndpoint(endpoint_url="some-url", validate_api_token=False)
+    HuggingFaceEndpoint(endpoint_url="some-url", validate_api_token=False)  # type: ignore[call-arg]
