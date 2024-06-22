f3a15e85bd5f91d8a4354a133667ab809ff133a3
Alexander Dicke <adicke.math@gmail.com>
2024-06-22 10:15:25 +0200

    adds test decorator

--- libs/community/tests/unit_tests/llms/test_huggingface_endpoint.py
+++ libs/community/tests/unit_tests/llms/test_huggingface_endpoint.py
@@ -6,6 +6,7 @@ from langchain_core.pydantic_v1 import ValidationError
 from langchain_community.llms.huggingface_endpoint import HuggingFaceEndpoint
 
 
+@pytest.mark.requires("huggingface_hub")
 def test_disable_api_token_check():
     with pytest.raises(ValidationError):
         HuggingFaceEndpoint(endpoint_url="some-url")
