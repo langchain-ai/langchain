aa6a7bda8767c71e5c260465551a6e9725905874
gongwn1 <gongwn1@lenovo.com>
2024-06-23 11:09:13 +0800

    fmt

--- libs/partners/huggingface/langchain_huggingface/llms/huggingface_endpoint.py
+++ libs/partners/huggingface/langchain_huggingface/llms/huggingface_endpoint.py
@@ -1,6 +1,5 @@
 import json
 import logging
-import os
 from typing import Any, AsyncIterator, Dict, Iterator, List, Mapping, Optional
 
 from langchain_core.callbacks import (
@@ -10,8 +9,7 @@ from langchain_core.callbacks import (
 from langchain_core.language_models.llms import LLM
 from langchain_core.outputs import GenerationChunk
 from langchain_core.pydantic_v1 import Extra, Field, root_validator
-from langchain_core.utils import get_pydantic_field_names
-from langchain_core.utils import get_from_dict_or_env
+from langchain_core.utils import get_from_dict_or_env, get_pydantic_field_names
 
 logger = logging.getLogger(__name__)
 
