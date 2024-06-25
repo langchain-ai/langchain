011dc53239a8f64600d71ac22aa8be7720160506
Eugene Yurtsev <eyurtsev@gmail.com>
2024-06-25 13:26:06 -0400

    x

--- libs/core/langchain_core/documents/base.py
+++ libs/core/langchain_core/documents/base.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, List, Literal
+from typing import Any, List, Literal, Optional
 
 from langchain_core.load.serializable import Serializable
 from langchain_core.pydantic_v1 import Field
@@ -19,6 +19,16 @@ class Document(Serializable):
                 page_content="Hello, world!",
                 metadata={"source": "https://example.com"}
             )
+    """
+
+    id: Optional[str] = None
+    """An optional identifier for the document.
+    
+    Ideally this should be unique across the document collection and formatted 
+    as a UUID, but this will not be enforced.
+    
+    This field is optional at the moment, but may become a required field 
+    in the future (wil be assigned automatically if not provided).
     """
 
     page_content: str
@@ -42,3 +52,17 @@ class Document(Serializable):
     def get_lc_namespace(cls) -> List[str]:
         """Get the namespace of the langchain object."""
         return ["langchain", "schema", "document"]
+
+    def __str__(self) -> str:
+        """Override __str__ to restrict it to page_content and metadata.
+
+        The format matches pydantic format for __str__.
+
+        The purpose of this change is to make sure that user code that
+        feeds Document objects directly into prompts remains unchanged
+        due to the addition of the id field (or any other fields in the future).
+
+        This override will likely be removed in the future in favor of
+        a more general solution of formatting content directly inside the prompts.
+        """
+        return f"page_content='{self.page_content}', metadata={self.metadata}"
