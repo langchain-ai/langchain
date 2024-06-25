2729ea761b129de9bec56fd23e186bfb36b2a195
Eugene Yurtsev <eyurtsev@gmail.com>
2024-06-25 09:25:02 -0400

    x

--- libs/core/tests/unit_tests/runnables/test_runnable.py
+++ libs/core/tests/unit_tests/runnables/test_runnable.py
@@ -5703,17 +5703,20 @@ async def test_listeners_async() -> None:
     assert value1 in shared_state.values(), "Value not found in the dictionary."
     assert value2 in shared_state.values(), "Value not found in the dictionary."
 
+
 async def test_closing_iterator_doesnt_raise_error() -> None:
     """Test that closing an iterator calls on_chain_end rather than on_chain_error."""
-    from langchain_core.output_parsers import StrOutputParser
-    from langchain_core.callbacks import BaseCallbackHandler
-    from langchain_core.language_models.fake_chat_models import GenericFakeChatModel
     import time
 
+    from langchain_core.callbacks import BaseCallbackHandler
+    from langchain_core.language_models.fake_chat_models import GenericFakeChatModel
+    from langchain_core.output_parsers import StrOutputParser
+
     on_chain_error_triggered = False
+
     class MyHandler(BaseCallbackHandler):
         def on_chain_error(
-                self, error: BaseException, *, run_id, parent_run_id=None, **kwargs
+            self, error: BaseException, *, run_id, parent_run_id=None, **kwargs
         ):
             nonlocal on_chain_error_triggered
             on_chain_error_triggered = True
