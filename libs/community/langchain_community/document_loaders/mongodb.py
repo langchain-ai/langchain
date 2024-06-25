0d474dfcc7a8d40cbdb07bc2b419d03a81fb670b
RUO <61719257+comsa33@users.noreply.github.com>
2024-06-25 14:02:44 +0900

    update: change single quote to double

--- libs/community/langchain_community/document_loaders/mongodb.py
+++ libs/community/langchain_community/document_loaders/mongodb.py
@@ -124,6 +124,6 @@ class MongodbLoader(BaseLoader):
                 value = value.get(key, default)
                 if value == default:
                     break
-            new_field_name = field.replace('.', '_')
+            new_field_name = field.replace(".", "_")
             extracted[new_field_name] = value
         return extracted
