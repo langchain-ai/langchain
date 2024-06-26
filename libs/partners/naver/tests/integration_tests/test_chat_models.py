154b14783c8d559132f1c969cdf3d555976000ea
Pilhwan Kim <dl_hyperclova_dev@navercorp.com>
2024-06-26 11:29:07 +0900

    partner: Initialize Naver(hyperclova) package

---
+++ libs/partners/naver/tests/integration_tests/test_chat_models.py
@@ -0,0 +1,63 @@
+"""Test ChatNaver chat model."""
+from langchain_naver.chat_models import ChatNaver
+
+
+def test_stream() -> None:
+    """Test streaming tokens from OpenAI."""
+    llm = ChatNaver()
+
+    for token in llm.stream("I'm Pickle Rick"):
+        assert isinstance(token.content, str)
+
+
+async def test_astream() -> None:
+    """Test streaming tokens from OpenAI."""
+    llm = ChatNaver()
+
+    async for token in llm.astream("I'm Pickle Rick"):
+        assert isinstance(token.content, str)
+
+
+async def test_abatch() -> None:
+    """Test streaming tokens from ChatNaver."""
+    llm = ChatNaver()
+
+    result = await llm.abatch(["I'm Pickle Rick", "I'm not Pickle Rick"])
+    for token in result:
+        assert isinstance(token.content, str)
+
+
+async def test_abatch_tags() -> None:
+    """Test batch tokens from ChatNaver."""
+    llm = ChatNaver()
+
+    result = await llm.abatch(
+        ["I'm Pickle Rick", "I'm not Pickle Rick"], config={"tags": ["foo"]}
+    )
+    for token in result:
+        assert isinstance(token.content, str)
+
+
+def test_batch() -> None:
+    """Test batch tokens from ChatNaver."""
+    llm = ChatNaver()
+
+    result = llm.batch(["I'm Pickle Rick", "I'm not Pickle Rick"])
+    for token in result:
+        assert isinstance(token.content, str)
+
+
+async def test_ainvoke() -> None:
+    """Test invoke tokens from ChatNaver."""
+    llm = ChatNaver()
+
+    result = await llm.ainvoke("I'm Pickle Rick", config={"tags": ["foo"]})
+    assert isinstance(result.content, str)
+
+
+def test_invoke() -> None:
+    """Test invoke tokens from ChatNaver."""
+    llm = ChatNaver()
+
+    result = llm.invoke("I'm Pickle Rick", config=dict(tags=["foo"]))
+    assert isinstance(result.content, str)
---
+++ libs/partners/naver/tests/integration_tests/test_embeddings.py
@@ -0,0 +1,19 @@
+"""Test Naver embeddings."""
+from langchain_naver.embeddings import NaverEmbeddings
+
+
+def test_langchain_naver_embedding_documents() -> None:
+    """Test cohere embeddings."""
+    documents = ["foo bar"]
+    embedding = NaverEmbeddings()
+    output = embedding.embed_documents(documents)
+    assert len(output) == 1
+    assert len(output[0]) > 0
+
+
+def test_langchain_naver_embedding_query() -> None:
+    """Test cohere embeddings."""
+    document = "foo bar"
+    embedding = NaverEmbeddings()
+    output = embedding.embed_query(document)
+    assert len(output) > 0
