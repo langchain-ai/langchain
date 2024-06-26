353f33e56b401d25f9ca86a46e8c977ba21b4343
gongwn1 <gongwn1@lenovo.com>
2024-06-26 23:33:03 +0800

    add _select_relevance_score_fn to test

--- libs/langchain/tests/unit_tests/retrievers/test_multi_vector.py
+++ libs/langchain/tests/unit_tests/retrievers/test_multi_vector.py
@@ -1,4 +1,4 @@
-from typing import Any, List, Tuple
+from typing import Any, List, Tuple, Callable
 
 from langchain_core.documents import Document
 
@@ -8,6 +8,13 @@ from tests.unit_tests.indexes.test_indexing import InMemoryVectorStore
 
 
 class InMemoryVectorstoreWithSearch(InMemoryVectorStore):
+    @staticmethod
+    def _identity_fn(score: float) -> float:
+        return score
+
+    def _select_relevance_score_fn(self) -> Callable[[float], float]:
+        return self._identity_fn
+
     def similarity_search(
         self, query: str, k: int = 4, **kwargs: Any
     ) -> List[Document]:
@@ -43,8 +50,8 @@ def test_multi_vector_retriever_similarity_search_with_score() -> None:
     documents = [Document(page_content="test document", metadata={"doc_id": "1"})]
 
     retriever = MultiVectorRetriever(  # type: ignore[call-arg]
-        vectorstore=vectorstore, docstore=InMemoryStore(), doc_id="doc_id", search_kwargs={"score_threshold": 0.5},
-        search_type="similarity_score_threshold"
+        vectorstore=vectorstore, docstore=InMemoryStore(), doc_id="doc_id",
+        search_kwargs={"score_threshold": 0.5}, search_type="similarity_score_threshold"
     )
     retriever.vectorstore.add_documents(documents, ids=["1"])
     retriever.docstore.mset(list(zip(["1"], documents)))
@@ -53,8 +60,8 @@ def test_multi_vector_retriever_similarity_search_with_score() -> None:
     assert results[0].page_content == "test document"
 
     retriever = MultiVectorRetriever(  # type: ignore[call-arg]
-        vectorstore=vectorstore, docstore=InMemoryStore(), doc_id="doc_id", search_kwargs={"score_threshold": 0.9},
-        search_type="similarity_score_threshold"
+        vectorstore=vectorstore, docstore=InMemoryStore(), doc_id="doc_id",
+        search_kwargs={"score_threshold": 0.9}, search_type="similarity_score_threshold"
     )
     retriever.docstore.mset(list(zip(["1"], documents)))
     results = retriever.invoke("1")
