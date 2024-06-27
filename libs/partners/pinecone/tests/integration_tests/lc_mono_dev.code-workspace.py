8af224c4311f3850c46ef7545eae0d2b75eee0da
ceciliawong15 <ceciliawong15@gmail.com>
2024-06-27 01:59:52 -0400

    gwip

---
+++ libs/partners/pinecone/tests/integration_tests/lc_mono_dev.code-workspace.py
@@ -0,0 +1,129 @@
+{
+    "folders": [
+        {
+            "path": "."
+        },
+        {
+            "path": "../ect_dev"
+        }
+    ],
+    "settings": {
+        "workbench.colorCustomizations": {
+            "[Monokai Dimmed]": {
+                "activityBar.activeBackground": "#4b0cb6",
+                "activityBar.background": "#4b0cb6",
+                "activityBar.inactiveForeground": "#e7e7e799",
+                "activityBarBadge.background": "#000000",
+                "commandCenter.border": "#e7e7e799",
+                "sash.hoverBorder": "#4b0cb6",
+                "statusBar.background": "#370986",
+                "statusBarItem.hoverBackground": "#4b0cb6",
+                "statusBarItem.remoteBackground": "#370986",
+                "tab.activeBorderTop": "#232729",
+                "tab.activeForeground": "#232729",
+                "tab.border": "#232729",
+                "tab.inactiveBackground": "#67625aa8",
+                "tab.inactiveForeground": "#3f3f3f",
+                "titleBar.activeBackground": "#370986",
+                "titleBar.activeForeground": "#e7e7e7"
+            },
+            "[paddy-genmaicha]": {
+                "activityBar.activeBackground": "#2f7c47",
+                "activityBar.background": "#2f7c47",
+                "activityBar.foreground": "#e7e7e7",
+                "activityBar.inactiveForeground": "#e7e7e799",
+                "activityBarBadge.background": "#422c74",
+                "activityBarBadge.foreground": "#e7e7e7",
+                "commandCenter.border": "#e7e7e799",
+                "editor.background": "#f7e6ce7b",
+                "menu.border": "#f1b36c",
+                "panel.border": "#f1b36c",
+                "sash.hoverBorder": "#2f7c47",
+                "sideBar.background": "#fbe5bc",
+                "sideBar.border": "#f1b36c",
+                "sideBarSectionHeader.border": "#f1b36c",
+                "statusBar.background": "#215732",
+                "statusBar.border": "#f1b36c",
+                "statusBar.foreground": "#e7e7e7",
+                "statusBarItem.hoverBackground": "#2f7c47",
+                "statusBarItem.remoteBackground": "#215732",
+                "statusBarItem.remoteForeground": "#e7e7e7",
+                "tab.activeBackground": "#ebddc2",
+                "tab.activeBorderTop": "#232729",
+                "tab.activeForeground": "#232729",
+                "tab.border": "#232729",
+                "tab.inactiveBackground": "#67625aa8",
+                "tab.inactiveForeground": "#3f3f3f",
+                "titleBar.activeBackground": "#215732",
+                "titleBar.activeForeground": "#e7e7e7",
+                "titleBar.inactiveBackground": "#21573299",
+                "titleBar.inactiveForeground": "#e7e7e799",
+                "notebook.cellEditorBackground": "#f04d4d08",
+                "notebook.editorBackground": "#f7e6ce7b"
+            },
+            "[Tomorrow Night Blue]": {
+                "terminal.background": "#000000",
+                "tab.inactiveBackground": "#5b717cf3"
+            },
+            "[paddy-citadel]": {
+                "tab.activeBackground": "#092630",
+                "tab.inactiveBackground": "#104354"
+            },
+            "[Cute]": {
+                "editor.background": "#ffdfe8"
+            },
+            "titleBar.activeBackground": "#5aaba0",
+            "titleBar.activeForeground": "#15202b",
+            "commandCenter.border": "#15202b99",
+            "sash.hoverBorder": "#7cbcb3",
+            "statusBar.background": "#5aaba0",
+            "statusBar.foreground": "#15202b",
+            "statusBarItem.hoverBackground": "#478b82",
+            "statusBarItem.remoteBackground": "#5aaba0",
+            "statusBarItem.remoteForeground": "#15202b",
+            "titleBar.inactiveBackground": "#5aaba099",
+            "titleBar.inactiveForeground": "#15202b99",
+            "editorGroup.border": "#7cbcb3",
+            "panel.border": "#7cbcb3",
+            "sideBar.border": "#7cbcb3",
+            "statusBar.border": "#5aaba0",
+            "titleBar.border": "#5aaba0",
+            "tab.activeBorder": "#7cbcb3"
+        },
+        "peacock.color": "#5aaba0",
+        "terminal.integrated.cwd": "/Users/ceciliawong/Desktop/lc_mono_dev/libs/partners/pinecone",
+        "window.title": "${separator}[Branch: feature/pinecone-partner-hybrid-vectorstore]${rootName}${separator}${seperator}${activeRepositoryBranchName}"
+        // "workbench.colorCustomizations": {
+        //     "activityBar.activeBackground": "#829de8",
+        //     "activityBar.background": "#829de8",
+        //     "activityBar.foreground": "#15202b",
+        //     "activityBar.inactiveForeground": "#15202b99",
+        //     "activityBarBadge.background": "#fae4ea",
+        //     "activityBarBadge.foreground": "#15202b",
+        //     "commandCenter.border": "#e7e7e799",
+        //     "sash.hoverBorder": "#829de8",
+        //     "statusBar.background": "#577be0",
+        //     "statusBar.foreground": "#e7e7e7",
+        //     "statusBarItem.hoverBackground": "#829de8",
+        //     "statusBarItem.remoteBackground": "#577be0",
+        //     "statusBarItem.remoteForeground": "#e7e7e7",
+        //     "titleBar.activeBackground": "#577be0",
+        //     "titleBar.activeForeground": "#e7e7e7",
+        //     "titleBar.inactiveBackground": "#577be099",
+        //     "titleBar.inactiveForeground": "#e7e7e799"
+        },
+
+        // "terminal.integrated.cwd": "/Users/ceciliawong/Desktop/lc_mono_dev/libs/partners/pinecone",
+        // "debugpy.debugJustMyCode": true,
+        // "debug.confirmOnExit": "always",
+        // "debug.autoExpandLazyVariables": true,
+        // "debug.toolBarLocation": "docked",
+        // "debug.allowBreakpointsEverywhere": true,
+        // "debug.showBreakpointsInOverviewRuler": true,
+        // "debug.terminal.clearBeforeReusing": true,
+        // "debug.showSubSessionsInToolBar": true,
+        // "peacock.color": "#577be0",
+        // "terminal.integrated.environmentChangesIndicator": "on",
+        // "autopep8.importStrategy": "useBundled",
+        
+    }
