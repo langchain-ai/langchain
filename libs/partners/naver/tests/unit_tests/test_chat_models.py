bde3b0426805d1c3d4a876345db9bc7472594653
Pilhwan Kim <dl_hyperclova_dev@navercorp.com>
2024-06-27 14:32:15 +0900

    partners: add unit test on NaverChat

--- libs/partners/naver/tests/unit_tests/test_chat_models.py
+++ libs/partners/naver/tests/unit_tests/test_chat_models.py
@@ -61,101 +61,70 @@ def test_convert_dict_to_message_system() -> None:
     assert _convert_message_to_naver_chat_message(expected_output) == message
 
 
-def test_convert_dict_to_message_system_with_name() -> None:
-    message = {"role": "system", "content": "foo"}
-    result = _convert_naver_chat_message_to_message(message)
-    expected_output = SystemMessage(content="foo")
-    assert result == expected_output
-    assert _convert_message_to_naver_chat_message(expected_output) == message
-
-
-
 @pytest.fixture
-def mock_completion() -> dict:
+def mock_chat_completion_response() -> dict:
     return {
-        "id": "chatcmpl-7fcZavknQda3SQ",
-        "object": "chat.completion",
-        "created": 1689989000,
-        "model": "HCX-003",
-        "choices": [
-            {
-                "index": 0,
-                "message": {
-                    "role": "assistant",
-                    "content": "Bab",
-                    "name": "KimSolar",
+        "status": {
+            "code": "20000",
+            "message": "OK"
+        },
+        "result": {
+            "message": {
+                "role": "assistant",
+                "content": "Phrases: Record what happened today and prepare for tomorrow. "
+                           "The diary will make your life richer."
+            },
+            "stopReason": "LENGTH",
+            "inputLength": 100,
+            "outputLength": 10,
+            "aiFilter": [
+                {
+                    "groupName": "curse",
+                    "name": "insult",
+                    "score": "1"
                 },
-                "finish_reason": "stop",
-            }
-        ],
+                {
+                    "groupName": "curse",
+                    "name": "discrimination",
+                    "score": "0"
+                },
+                {
+                    "groupName": "unsafeContents",
+                    "name": "sexualHarassment",
+                    "score": "2"
+                }
+            ]
+        }
     }
 
 
-def test_naver_invoke(mock_completion: dict) -> None:
+def test_naver_invoke(mock_chat_completion_response: dict) -> None:
     llm = ChatNaver()
-    mock_client = MagicMock()
     completed = False
 
-    def mock_create(*args: Any, **kwargs: Any) -> Any:
+    def mock_completion_with_retry(*args: Any, **kwargs: Any) -> Any:
         nonlocal completed
         completed = True
-        return mock_completion
+        return mock_chat_completion_response
 
-    mock_client.create = mock_create
-    with patch.object(
-        llm,
-        "client",
-        mock_client,
-    ):
-        res = llm.invoke("bab")
-        assert res.content == "Bab"
+    with (patch.object(ChatNaver, '_completion_with_retry', mock_completion_with_retry)):
+        res = llm.invoke("Let's test it.")
+        assert res.content == \
+               "Phrases: Record what happened today and prepare for tomorrow. The diary will make your life richer."
     assert completed
 
 
-async def test_naver_ainvoke(mock_completion: dict) -> None:
+async def test_naver_ainvoke(mock_chat_completion_response: dict) -> None:
     llm = ChatNaver()
-    mock_client = AsyncMock()
     completed = False
 
-    async def mock_create(*args: Any, **kwargs: Any) -> Any:
+    async def mock_acompletion_with_retry(*args: Any, **kwargs: Any) -> Any:
         nonlocal completed
         completed = True
-        return mock_completion
+        return mock_chat_completion_response
 
-    mock_client.create = mock_create
-    with patch.object(
-        llm,
-        "async_client",
-        mock_client,
-    ):
-        res = await llm.ainvoke("bab")
-        assert res.content == "Bab"
+    with patch.object(ChatNaver, "_acompletion_with_retry", mock_acompletion_with_retry):
+        res = await llm.ainvoke("Let's test it.")
+        assert res.content == \
+               "Phrases: Record what happened today and prepare for tomorrow. The diary will make your life richer."
     assert completed
-
-
-def test_naver_invoke_name(mock_completion: dict) -> None:
-    llm = ChatNaver()
-
-    mock_client = MagicMock()
-    mock_client.create.return_value = mock_completion
-
-    with patch.object(
-        llm,
-        "client",
-        mock_client,
-    ):
-        messages = [
-            HumanMessage(content="Foo", name="Zorba"),
-        ]
-        res = llm.invoke(messages)
-        call_args, call_kwargs = mock_client.create.call_args
-        assert len(call_args) == 0  # no positional args
-        call_messages = call_kwargs["messages"]
-        assert len(call_messages) == 1
-        assert call_messages[0]["role"] == "user"
-        assert call_messages[0]["content"] == "Foo"
-        assert call_messages[0]["name"] == "Zorba"
-
-        # check return type has name
-        assert res.content == "Bab"
-        assert res.name == "KimSolar"
