7306dce74cf8632af534d930a455a3f1033f799b
Pilhwan Kim <dl_hyperclova_dev@navercorp.com>
2024-06-27 14:32:15 +0900

    partners: add unit test on NaverChat

--- libs/partners/naver/tests/integration_tests/test_chat_models.py
+++ libs/partners/naver/tests/integration_tests/test_chat_models.py
@@ -6,7 +6,7 @@ def test_stream() -> None:
     """Test streaming tokens from ChatNaver."""
     llm = ChatNaver()
 
-    for token in llm.stream("I'm Pickle Rick"):
+    for token in llm.stream("I'm Clova"):
         assert isinstance(token.content, str)
 
 
@@ -14,7 +14,7 @@ async def test_astream() -> None:
     """Test streaming tokens from ChatNaver."""
     llm = ChatNaver()
 
-    async for token in llm.astream("I'm Pickle Rick"):
+    async for token in llm.astream("I'm Clova"):
         assert isinstance(token.content, str)
 
 
@@ -22,7 +22,7 @@ async def test_abatch() -> None:
     """Test streaming tokens from ChatNaver."""
     llm = ChatNaver()
 
-    result = await llm.abatch(["I'm Pickle Rick", "I'm not Pickle Rick"])
+    result = await llm.abatch(["I'm Clova", "I'm not Clova"])
     for token in result:
         assert isinstance(token.content, str)
 
@@ -32,7 +32,7 @@ async def test_abatch_tags() -> None:
     llm = ChatNaver()
 
     result = await llm.abatch(
-        ["I'm Pickle Rick", "I'm not Pickle Rick"], config={"tags": ["foo"]}
+        ["I'm Clova", "I'm not Clova"], config={"tags": ["foo"]}
     )
     for token in result:
         assert isinstance(token.content, str)
@@ -42,7 +42,7 @@ def test_batch() -> None:
     """Test batch tokens from ChatNaver."""
     llm = ChatNaver()
 
-    result = llm.batch(["I'm Pickle Rick", "I'm not Pickle Rick"])
+    result = llm.batch(["I'm Clova", "I'm not Clova"])
     for token in result:
         assert isinstance(token.content, str)
 
@@ -51,7 +51,7 @@ async def test_ainvoke() -> None:
     """Test invoke tokens from ChatNaver."""
     llm = ChatNaver()
 
-    result = await llm.ainvoke("I'm Pickle Rick", config={"tags": ["foo"]})
+    result = await llm.ainvoke("I'm Clova", config={"tags": ["foo"]})
     assert isinstance(result.content, str)
 
 
@@ -59,5 +59,5 @@ def test_invoke() -> None:
     """Test invoke tokens from ChatNaver."""
     llm = ChatNaver()
 
-    result = llm.invoke("I'm Pickle Rick", config=dict(tags=["foo"]))
+    result = llm.invoke("I'm Clova", config=dict(tags=["foo"]))
     assert isinstance(result.content, str)
