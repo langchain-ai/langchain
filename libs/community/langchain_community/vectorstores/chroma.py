60428032fbe6fee786a13644c96458abc84787d8
Mrugank Wadekar <mrugank9009@gmail.com>
2024-07-02 10:42:33 +0530

    fix linting errors for CI

--- libs/community/langchain_community/vectorstores/chroma.py
+++ libs/community/langchain_community/vectorstores/chroma.py
@@ -485,20 +485,20 @@ class Chroma(VectorStore):
         filter: Optional[Dict[str, str]] = None,
         **kwargs: Any,
     ) -> List[Document]:
-        """
-        Search for similar images based on the given image URI.
+        """Search for similar images based on the given image URI.
 
         Args:
             uri (str): URI of the image to search for.
             k (int, optional): Number of results to return. Defaults to DEFAULT_K.
             filter (Optional[Dict[str, str]], optional): Filter by metadata.
-            Defaults to None.
+            **kwargs (Any): Additional arguments to pass to function.
 
         Returns:
             List of Images most similar to the provided image.
             Each element in list is a Langchain Document Object.
             The page content is b64 encoded image, metadata is default or
             as defined by user.
+
         Raises:
             ValueError: If the embedding function does not support image embeddings.
         """
@@ -528,20 +528,21 @@ class Chroma(VectorStore):
         filter: Optional[Dict[str, str]] = None,
         **kwargs: Any,
     ) -> List[Tuple[Document, float]]:
-        """
-        Search for similar images based on the given image URI.
+        """Search for similar images based on the given image URI.
 
         Args:
             uri (str): URI of the image to search for.
             k (int, optional): Number of results to return.
             Defaults to DEFAULT_K.
             filter (Optional[Dict[str, str]], optional): Filter by metadata.
+            **kwargs (Any): Additional arguments to pass to function.
 
         Returns:
             List[Tuple[Document, float]]: List of tuples containing documents similar
             to the query image and their similarity scores.
             0th element in each tuple is a Langchain Document Object.
             The page content is b64 encoded img, metadata is default or defined by user.
+        
         Raises:
             ValueError: If the embedding function does not support image embeddings.
         """
