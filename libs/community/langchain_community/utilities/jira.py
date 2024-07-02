338cef35b479b47bd4c3d9b3a24739a62d4af1e2
Eugene Yurtsev <eyurtsev@gmail.com>
2024-07-02 10:33:01 -0400

    community[patch]: update @root_validator in utilities namespace (#23768)
    Update all utilities to use `pre=True` or `pre=False`
    
    https://github.com/langchain-ai/langchain/issues/22819

--- libs/community/langchain_community/utilities/jira.py
+++ libs/community/langchain_community/utilities/jira.py
@@ -21,7 +21,7 @@ class JiraAPIWrapper(BaseModel):
 
         extra = Extra.forbid
 
-    @root_validator()
+    @root_validator(pre=True)
     def validate_environment(cls, values: Dict) -> Dict:
         """Validate that api key and python package exists in environment."""
         jira_username = get_from_dict_or_env(
