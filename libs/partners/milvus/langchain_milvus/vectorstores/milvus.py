7719a0dc257bdb041b21068ff5bd95bc41079eba
r0g0bum <rohit.gupta2@walmart.com>
2024-07-03 10:50:54 +0530

    add array data type for milvus vector store collection create

--- libs/partners/milvus/langchain_milvus/vectorstores/milvus.py
+++ libs/partners/milvus/langchain_milvus/vectorstores/milvus.py
@@ -225,7 +225,7 @@ class Milvus(VectorStore):
         replica_number: int = 1,
         timeout: Optional[float] = None,
         num_shards: Optional[int] = None,
-        field_schema: Optional[dict[str, Any]] = dict(),
+        field_schema: Optional[dict[str, Any]] = None,
     ):
         """Initialize the Milvus vector store."""
         try:
