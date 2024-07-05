d7d4328d2d3b00c2a87968dd758d606059c06949
hyper-clova <dl_hyperclova_dev@navercorp.com>
2024-07-05 10:23:40 +0900

    partners: add naver embedding

--- libs/partners/naver/tests/integration_tests/test_embeddings.py
+++ libs/partners/naver/tests/integration_tests/test_embeddings.py
@@ -1,11 +1,11 @@
 """Test Naver embeddings."""
-from langchain_naver.embeddings import NaverEmbeddings
+from langchain_naver.embeddings import ClovaStudioEmbeddings
 
 
 def test_langchain_naver_embedding_documents() -> None:
     """Test cohere embeddings."""
     documents = ["foo bar"]
-    embedding = NaverEmbeddings()
+    embedding = ClovaStudioEmbeddings()
     output = embedding.embed_documents(documents)
     assert len(output) == 1
     assert len(output[0]) > 0
@@ -14,6 +14,6 @@ def test_langchain_naver_embedding_documents() -> None:
 def test_langchain_naver_embedding_query() -> None:
     """Test cohere embeddings."""
     document = "foo bar"
-    embedding = NaverEmbeddings()
+    embedding = ClovaStudioEmbeddings()
     output = embedding.embed_query(document)
     assert len(output) > 0
--- libs/partners/naver/tests/unit_tests/test_embeddings.py
+++ libs/partners/naver/tests/unit_tests/test_embeddings.py
@@ -1,9 +1,18 @@
 """Test embedding model integration."""
 
+import os
+from typing import cast
 
-from langchain_naver.embeddings import NaverEmbeddings
+from pydantic.v1 import SecretStr
+
+from langchain_naver.embeddings import ClovaStudioEmbeddings
+
+os.environ["NCP_CLOVASTUDIO_API_KEY"] = "test_api_key"
+os.environ["NCP_APIGW_API_KEY"] = "test_gw_key"
+os.environ["NCP_CLOVADTUDIO_APP_ID"] = "test_app_id"
 
 
-def test_initialization() -> None:
-    """Test embedding model initialization."""
-    NaverEmbeddings()
+def test_initialization_api_key() -> None:
+    chat_model = ClovaStudioEmbeddings(clovastudio_api_key="foo", apigw_api_key="bar")
+    assert cast(SecretStr, chat_model.ncp_clovastudio_api_key).get_secret_value() == "foo"
+    assert cast(SecretStr, chat_model.ncp_apigw_api_key).get_secret_value() == "bar"
