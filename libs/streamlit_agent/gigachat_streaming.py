16fa5038874746096bf1b56c172ff22d860fa7d0
Mikhail Murtazin <murtazkorpaz@gmail.com>
2024-07-05 19:22:15 +0300

    Добавил возможность получать сгенерированные изображения в демо

--- libs/streamlit_agent/gigachat_streaming.py
+++ libs/streamlit_agent/gigachat_streaming.py
@@ -1,4 +1,5 @@
 """Пример работы с чатом через gigachain """
+
 import streamlit as st
 
 # Try demo - https://gigachat-streaming.streamlit.app/
@@ -10,6 +11,15 @@ st.title("GigaChain Bot")
 
 with st.sidebar:
     st.title("GIGACHAT API")
+    model = st.selectbox(
+        "GIGACHAT_MODEL",
+        (
+            "GigaChat",
+            "GigaChat-Pro",
+            "GigaChat-Plus",
+        ),
+    )
+    st.title("GIGACHAT API")
     base_url = st.selectbox(
         "GIGACHAT_BASE_URL",
         (
@@ -18,6 +28,13 @@ with st.sidebar:
         ),
     )
     st.title("Авторизационные данные")
+    scope = st.selectbox(
+        "GIGACHAT_SCOPE",
+        (
+            "GIGACHAT_API_PERS",
+            "GIGACHAT_API_CORP",
+        ),
+    )
     credentials = st.text_input("GIGACHAT_CREDENTIALS", type="password")
     st.title("OR")
     access_token = st.text_input("GIGACHAT_ACCESS_TOKEN", type="password")
@@ -40,7 +57,7 @@ if "messages" not in st.session_state:
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message.role):
-        st.markdown(message.content)
+        st.markdown(message.content, True)
 
 
 if prompt := st.chat_input():
@@ -51,12 +68,14 @@ if prompt := st.chat_input():
     chat = GigaChat(
         base_url=base_url,
         credentials=credentials,
+        model=model,
         access_token=st.session_state.get("token")
         or access_token,  # Переиспользуем токен
         user=user,
         password=password,
+        scope=scope,
         verify_ssl_certs=False,
-    )
+    ).bind_tools(tools=[], tool_choice="auto")
 
     message = ChatMessage(role="user", content=prompt)
     st.session_state.messages.append(message)
@@ -69,10 +88,30 @@ if prompt := st.chat_input():
 
     with st.chat_message(message.role):
         message_placeholder = st.empty()
+        spinner = None
         for chunk in chat.stream(st.session_state.messages):
-            message.content += chunk.content
-            message_placeholder.markdown(message.content + "▌")
-        message_placeholder.markdown(message.content)
+            if chunk.type == "FunctionInProgressMessage":
+                if spinner is None:
+                    spinner = st.spinner(text="")
+                    spinner.__enter__()  # Тут хак для показа спинера в streamlit
+                    # не знаю долго ли он проработает, но он здесь просто для красоты
+                continue
+            else:
+                if spinner is not None:
+                    spinner.__exit__(None, None, None)
+                    spinner = None
+                if chunk.additional_kwargs.get("image_uuid"):
+                    image_uuid = chunk.additional_kwargs.get("image_uuid")
+                    message.content += f"""<img src="data:png;base64,{chat.get_file(image_uuid).content}" style="width: 450px; display: block; border-radius: 10px;" >"""
+                else:
+                    message.content += chunk.content
+            message.additional_kwargs = {
+                **message.additional_kwargs,
+                **chunk.additional_kwargs,
+            }
+
+            message_placeholder.markdown(message.content + "▌", True)
+        message_placeholder.markdown(message.content, True)
 
     # Каждый раз, когда пользователь нажимает что-то в интерфейсе весь скрипт выполняется заново.
     # Сохраняем токен и закрываем соединения
