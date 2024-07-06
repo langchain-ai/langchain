8693dcb62e121704bb0030f4e7abe0a3a1ad4c78
Lilly Guo <reubenh14@tutanota.com>
2024-07-07 01:29:47 +0200

    integration tests all work

--- libs/community/tests/integration_tests/chat_models/test_straico.py
+++ libs/community/tests/integration_tests/chat_models/test_straico.py
@@ -1,21 +1,13 @@
 """Test ChatStraico wrapper."""
 
-from typing import Any, Optional
-
 import pytest
-from langchain_core.callbacks import CallbackManager
-from langchain_core.messages import AIMessage, BaseMessage, HumanMessage, SystemMessage
+from langchain_core.messages import BaseMessage, HumanMessage, SystemMessage
 from langchain_core.outputs import (
     ChatGeneration,
-    ChatResult,
     LLMResult,
 )
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.pydantic_v1 import BaseModel, Field
-
+import os
 from langchain_community.chat_models import ChatStraico
-from tests.unit_tests.callbacks.fake_callback_handler import FakeCallbackHandler
-
 
 @pytest.mark.scheduled
 def test_chat_straico() -> None:
@@ -50,60 +42,44 @@ def test_chat_straico_generate() -> None:
     """Test ChatStraico wrapper with generate."""
     chat = ChatStraico(model="google/gemini-pro")
     message = HumanMessage(content="Hello")
-    response = chat.generate([[message], [message]])
+    message2 = HumanMessage(content="Hello2")
+    response = chat.generate([[message], [message2]])
     assert isinstance(response, LLMResult)
     assert len(response.generations) == 2
-    # assert response.llm_output
+    assert response.llm_output
     for generations in response.generations:
-        print(type(generations))
-        # assert len(generations) == 2
         for generation in generations:
-            print("GENERATION", generation)
             assert isinstance(generation, ChatGeneration)
             assert isinstance(generation.text, str)
             assert generation.text == generation.message.content
 
 
-@pytest.mark.scheduled
-def test_chat_straico_multiple_completions() -> None:
-    """Test ChatStraico wrapper with multiple completions."""
-    chat = ChatStraico(max_tokens=10, n=5)
-    message = HumanMessage(content="Hello")
-    response = chat._generate([message])
-    assert isinstance(response, ChatResult)
-    assert len(response.generations) == 5
-    for generation in response.generations:
-        assert isinstance(generation.message, BaseMessage)
-        assert isinstance(generation.message.content, str)
-
-
 def test_chat_straico_llm_output_contains_model_name() -> None:
     """Test llm_output contains model_name."""
-    chat = ChatStraico(max_tokens=10)
+    chat = ChatStraico()
     message = HumanMessage(content="Hello")
     llm_result = chat.generate([[message]])
     assert llm_result.llm_output is not None
-    assert llm_result.llm_output["model_name"] == chat.model_name
+    assert llm_result.llm_output["model_name"] == chat.model
 
 
-@pytest.mark.scheduled
+@pytest.mark.asyncio
 async def test_async_chat_straico() -> None:
     """Test async generation."""
-    chat = ChatStraico(max_tokens=10, n=2)
+    chat = ChatStraico(model="google/gemini-pro")
     message = HumanMessage(content="Hello")
     response = await chat.agenerate([[message], [message]])
     assert isinstance(response, LLMResult)
     assert len(response.generations) == 2
     assert response.llm_output
     for generations in response.generations:
-        assert len(generations) == 2
         for generation in generations:
             assert isinstance(generation, ChatGeneration)
             assert isinstance(generation.text, str)
             assert generation.text == generation.message.content
 
 
-@pytest.mark.scheduled
+@pytest.mark.asyncio
 async def test_openai_abatch_tags() -> None:
     """Test batch tokens from ChatStraico."""
     llm = ChatStraico(max_tokens=10)
@@ -118,14 +94,13 @@ async def test_openai_abatch_tags() -> None:
 @pytest.mark.scheduled
 def test_openai_batch() -> None:
     """Test batch tokens from ChatStraico."""
-    llm = ChatStraico(max_tokens=10)
-
+    llm = ChatStraico()
     result = llm.batch(["I'm Pickle Rick", "I'm not Pickle Rick"])
     for token in result:
         assert isinstance(token.content, str)
 
 
-@pytest.mark.scheduled
+@pytest.mark.asyncio
 async def test_openai_ainvoke() -> None:
     """Test invoke tokens from ChatStraico."""
     llm = ChatStraico(max_tokens=10)
