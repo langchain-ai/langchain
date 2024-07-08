81960961a9ccc6e77f38189e0211362c3f8788c7
Bagatur <baskaryan@gmail.com>
2024-07-07 19:41:15 -0600

    test

--- libs/langchain/tests/integration_tests/chat_models/test_base.py
+++ libs/langchain/tests/integration_tests/chat_models/test_base.py
@@ -1,15 +1,21 @@
 from langchain_core.messages import AIMessage
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.pydantic_v1 import BaseModel
 from langchain_core.runnables import RunnableConfig
 
 from langchain.chat_models import init_chat_model
 
 
+class multiply(BaseModel):
+    """Product of two ints."""
+
+    x: int
+    y: int
+
+
 def test_init_chat_model() -> None:
     model = init_chat_model("gpt-4o", configurable_fields="any", config_prefix="bar")
-    model_with_tools = model.bind_tools(
-        [{"name": "foo", "description": "foo", "parameters": {}}]
-    )
+    model_with_tools = model.bind_tools([multiply])
 
     model_with_config = model_with_tools.with_config(
         RunnableConfig(tags=["foo"]),
