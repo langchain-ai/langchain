518fb36aaf80f3674454f79c11478cd3603dc4fc
Erick Friis <erick@langchain.dev>
2024-07-08 12:39:13 -0700

    x

--- libs/core/langchain_core/vectorstores/__init__.py
+++ libs/core/langchain_core/vectorstores/__init__.py
@@ -1,4 +1,8 @@
 from langchain_core.vectorstores.fake import FakeVectorStore
-from langchain_core.vectorstores.vectorstores import VectorStore
+from langchain_core.vectorstores.vectorstores import (
+    VST,
+    VectorStore,
+    VectorStoreRetriever,
+)
 
-__all__ = ["VectorStore", "FakeVectorStore"]
+__all__ = ["VectorStore", "FakeVectorStore", "VST", "VectorStoreRetriever"]
