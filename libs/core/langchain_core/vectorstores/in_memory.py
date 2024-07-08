47e437af9497e0b6be5833c3e9e9297c5751eca5
Eugene Yurtsev <eyurtsev@gmail.com>
2024-07-08 16:55:32 -0400

    x

--- libs/core/langchain_core/vectorstores/in_memory.py
+++ libs/core/langchain_core/vectorstores/in_memory.py
@@ -1,17 +1,31 @@
+from __future__ import annotations
+
 import json
 import uuid
 from pathlib import Path
-from typing import Any, Callable, Dict, Iterable, List, Optional, Sequence, Tuple
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Iterable,
+    List,
+    Optional,
+    Sequence,
+    Tuple,
+    TYPE_CHECKING,
+)
 
 from langchain_core.documents import Document
 from langchain_core.embeddings import Embeddings
-from langchain_core.indexing import UpsertResponse
 from langchain_core.load import dumpd, load
 from langchain_core.vectorstores import VectorStore
 from langchain_core.vectorstores.utils import _cosine_similarity as cosine_similarity
 from langchain_core.vectorstores.utils import (
     _maximal_marginal_relevance as maximal_marginal_relevance,
 )
+
+if TYPE_CHECKING:
+    from langchain_core.indexing import UpsertResponse
 
 
 class InMemoryVectorStore(VectorStore):
