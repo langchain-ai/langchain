d7b593541e4528aad4cd0b43e635c97d3f465f06
Anush008 <anushshetty90@gmail.com>
2024-07-09 09:49:38 +0530

    lint

--- libs/partners/qdrant/langchain_qdrant/fastembed_sparse.py
+++ libs/partners/qdrant/langchain_qdrant/fastembed_sparse.py
@@ -8,7 +8,7 @@ class FastEmbedSparse(SparseEmbeddings):
 
     def __init__(
         self,
-        model_name: str = "Qdrant/bm42-all-minilm-l6-v2-attentions",
+        model_name: str = "Qdrant/bm25",
         batch_size: int = 256,
         cache_dir: Optional[str] = None,
         threads: Optional[int] = None,
@@ -21,23 +21,25 @@ class FastEmbedSparse(SparseEmbeddings):
         For a list of available models, see https://qdrant.github.io/fastembed/examples/Supported_Models/
 
         Args:
-            model_name (str): The name of the model to use. Defaults to `"Qdrant/bm42-all-minilm-l6-v2-attentions"`.
-            batch_size (int): Batch size for encoding. Higher values will use more memory, but will be faster.\
-                                        Defaults to 256.
+            model_name (str): The name of the model to use. Defaults to `"Qdrant/bm25"`.
+            batch_size (int): Batch size for encoding. Defaults to 256.
             cache_dir (str, optional): The path to the model cache directory.\
-                                       Can also be set using the `FASTEMBED_CACHE_PATH` env variable.
-            threads (int, optional): The number of threads single onnxruntime session can use.
-            providers (Sequence[Any], optional): A list of ONNX execution providers to use.\
-            parallel (int, optional): If `>1`, data-parallel encoding will be used, recommended for offline encoding of large datasets.\
+                                       Can also be set using the\
+                                       `FASTEMBED_CACHE_PATH` env variable.
+            threads (int, optional): The number of threads onnxruntime session can use.
+            providers (Sequence[Any], optional): List of ONNX execution providers.\
+            parallel (int, optional): If `>1`, data-parallel encoding will be used, r\
+                                      Recommended for encoding of large datasets.\
                                       If `0`, use all available cores.\
-                                      If `None`, don't use data-parallel processing, use default onnxruntime threading instead.\
+                                      If `None`, don't use data-parallel processing,\
+                                      use default onnxruntime threading instead.\
                                       Defaults to None.
             **kwargs: Additional options to pass to fastembed.SparseTextEmbedding
         Raises:
             ValueError: If the model_name is not supported in SparseTextEmbedding.
         """
         try:
-            from fastembed import SparseTextEmbedding
+            from fastembed import SparseTextEmbedding  # type: ignore
         except ImportError:
             raise ValueError(
                 "The 'fastembed' package is not installed. "
--- libs/partners/qdrant/tests/integration_tests/fastembed/test_fastembed_sparse.py
+++ libs/partners/qdrant/tests/integration_tests/fastembed/test_fastembed_sparse.py
@@ -7,21 +7,21 @@ from langchain_qdrant import FastEmbedSparse
 @pytest.mark.parametrize(
     "model_name", ["Qdrant/bm25", "Qdrant/bm42-all-minilm-l6-v2-attentions"]
 )
-def test_attention_embeddings(model_name):
+def test_attention_embeddings(model_name: str) -> None:
     model = FastEmbedSparse(model_name=model_name)
 
-    output = model.embed_query("Stay, steady and sprint.")
+    query_output = model.embed_query("Stay, steady and sprint.")
 
-    assert len(output.indices) == len(output.values)
-    assert np.allclose(output.values, np.ones(len(output.values)))
+    assert len(query_output.indices) == len(query_output.values)
+    assert np.allclose(query_output.values, np.ones(len(query_output.values)))
 
     texts = [
         "The journey of a thousand miles begins with a single step.",
-        "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+        "Be yourself in a world that is constantly trying to make you something else",
         "In the end, we only regret the chances we didn't take.",
         "Every moment is a fresh beginning.",
         "Not all those who wander are lost.",
-        "Do not go where the path may lead, go instead where there is no path and leave a trail.",
+        "Do not go where the path may lead, go elsewhere and leave a trail.",
         "Life is what happens when you're busy making other plans.",
         "The only limit to our realization of tomorrow is our doubts of today.",
     ]
