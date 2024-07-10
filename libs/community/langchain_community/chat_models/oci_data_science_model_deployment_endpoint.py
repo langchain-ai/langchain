bec5e09add76e6b64f52ef84bb9dc015c3a6fc70
MING KANG <ming.kang@oracle.com>
2024-07-10 18:40:26 -0400

    poc tool calling

--- libs/community/langchain_community/chat_models/oci_data_science_model_deployment_endpoint.py
+++ libs/community/langchain_community/chat_models/oci_data_science_model_deployment_endpoint.py
@@ -207,7 +207,6 @@ class ChatOCIModelDeploymentEndpoint(BaseChatModel):
         params = self._default_params
         # stop pased to _generate has higher priority
         params["stop"] = stop or params["stop"]
-
         return {**params, **kwargs}
 
     def _construct_json_body(self, messages: list, params: dict) -> dict:
@@ -344,6 +343,7 @@ class ChatOCIModelDeploymentEndpoint(BaseChatModel):
                 request = Requests(
                     headers=self._headers(), auth=self.auth.get("signer")
                 )
+                print(data)
                 response = request.post(
                     url=self.endpoint,
                     data=data,
@@ -455,7 +455,7 @@ class ChatOCIModelDeploymentEndpoint(BaseChatModel):
         res = self.completion_with_retry(
             data=body,
             run_manager=run_manager,
-            **kwargs,
+            # **kwargs,
         )
         return self._process_response(res.json())
 
@@ -592,41 +592,42 @@ class ChatOCIModelDeploymentEndpoint(BaseChatModel):
             **kwargs: Any additional parameters to pass to the
                 :class:`~langchain.runnable.Runnable` constructor.
         """
-        formatted_tools = [convert_to_openai_tool(tool) for tool in tools]
-        if tool_choice:
-            if isinstance(tool_choice, str):
-                # tool_choice is a tool/function name
-                if tool_choice not in ("auto", "none", "any", "required"):
-                    tool_choice = {
-                        "type": "function",
-                        "function": {"name": tool_choice},
-                    }
-                # 'any' is not natively supported by OpenAI API.
-                # We support 'any' since other models use this instead of 'required'.
-                if tool_choice == "any":
-                    tool_choice = "required"
-            elif isinstance(tool_choice, bool):
-                tool_choice = "required"
-            elif isinstance(tool_choice, dict):
-                tool_names = [
-                    formatted_tool["function"]["name"]
-                    for formatted_tool in formatted_tools
-                ]
-                if not any(
-                    tool_name == tool_choice["function"]["name"]
-                    for tool_name in tool_names
-                ):
-                    raise ValueError(
-                        f"Tool choice {tool_choice} was specified, but the only "
-                        f"provided tools were {tool_names}."
-                    )
-            else:
-                raise ValueError(
-                    f"Unrecognized tool_choice type. Expected str, bool or dict. "
-                    f"Received: {tool_choice}"
-                )
-            kwargs["tool_choice"] = tool_choice
-        return super().bind(tools=formatted_tools, **kwargs)
+        pass
+        # formatted_tools = [convert_to_openai_tool(tool) for tool in tools]
+        # if tool_choice:
+        #     if isinstance(tool_choice, str):
+        #         # tool_choice is a tool/function name
+        #         if tool_choice not in ("auto", "none", "any", "required"):
+        #             tool_choice = {
+        #                 "type": "function",
+        #                 "function": {"name": tool_choice},
+        #             }
+        #         # 'any' is not natively supported by OpenAI API.
+        #         # We support 'any' since other models use this instead of 'required'.
+        #         if tool_choice == "any":
+        #             tool_choice = "required"
+        #     elif isinstance(tool_choice, bool):
+        #         tool_choice = "required"
+        #     elif isinstance(tool_choice, dict):
+        #         tool_names = [
+        #             formatted_tool["function"]["name"]
+        #             for formatted_tool in formatted_tools
+        #         ]
+        #         if not any(
+        #             tool_name == tool_choice["function"]["name"]
+        #             for tool_name in tool_names
+        #         ):
+        #             raise ValueError(
+        #                 f"Tool choice {tool_choice} was specified, but the only "
+        #                 f"provided tools were {tool_names}."
+        #             )
+        #     else:
+        #         raise ValueError(
+        #             f"Unrecognized tool_choice type. Expected str, bool or dict. "
+        #             f"Received: {tool_choice}"
+        #         )
+        #     kwargs["tool_choice"] = tool_choice
+        # return super().bind(tools=formatted_tools, **kwargs)
 
     def _parse_stream(self, lines: Iterator[bytes]) -> Iterator[str]:
         for line in lines:
