5f8858a490347dddb680fa40f877228ef0dbd325
Eugene Yurtsev <eyurtsev@gmail.com>
2024-07-11 12:49:55 -0400

    x

--- libs/community/langchain_community/graph_vectorstores/cassandra.py
+++ libs/community/langchain_community/graph_vectorstores/cassandra.py
@@ -9,14 +9,15 @@ from typing import (
     Type,
 )
 
+from langchain_core.documents import Document
+from langchain_core.embeddings import Embeddings
+
 from langchain_community.graph_vectorstores.base import (
     GraphVectorStore,
     Node,
     nodes_to_documents,
 )
 from langchain_community.utilities.cassandra import SetupMode
-from langchain_core.documents import Document
-from langchain_core.embeddings import Embeddings
 
 if TYPE_CHECKING:
     from cassandra.cluster import Session
