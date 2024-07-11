432ccd686d45de97eba19573e36b7a8c6caf25b6
jacoblee93 <jacoblee93@gmail.com>
2024-07-11 11:19:51 -0700

    Adds curry utils function

---
+++ libs/core/langchain_core/utils/curry.py
@@ -0,0 +1,39 @@
+import asyncio
+import inspect
+from functools import wraps
+from typing import Any, Callable
+
+
+def curry(func: Callable[..., Any], **curried_kwargs: Any) -> Callable[..., Any]:
+    """Util that wraps a function and partially applies kwargs to it.
+    Returns a new function whose signature omits the curried variables.
+
+    Args:
+        func: The function to curry.
+        curried_kwargs: Arguments to apply to the function.
+
+    Returns:
+        A new function with curried arguments applied.
+
+    .. versionadded:: 0.2.14"""
+
+    @wraps(func)
+    async def async_wrapper(*args: Any, **kwargs: Any) -> Any:
+        new_kwargs = {**curried_kwargs, **kwargs}
+        return await func(*args, **new_kwargs)
+
+    @wraps(func)
+    def sync_wrapper(*args: Any, **kwargs: Any) -> Any:
+        new_kwargs = {**curried_kwargs, **kwargs}
+        return func(*args, **new_kwargs)
+
+    sig = inspect.signature(func)
+    # Create a new signature without the curried parameters
+    new_params = [p for name, p in sig.parameters.items() if name not in curried_kwargs]
+
+    if asyncio.iscoroutinefunction(func):
+        async_wrapper.__signature__ = sig.replace(parameters=new_params)
+        return async_wrapper
+    else:
+        sync_wrapper.__signature__ = sig.replace(parameters=new_params)
+        return sync_wrapper
---
+++ libs/core/tests/unit_tests/utils/test_curry.py
@@ -0,0 +1,53 @@
+from typing import Any
+
+import pytest
+
+from langchain_core.utils.curry import curry
+
+
+def test_curry() -> None:
+    def test_fn(a: str, b: str):
+        return a + b
+
+    curried = curry(test_fn, a="hey")
+    assert curried(b=" you") == "hey you"
+
+
+def test_curry_with_kwargs_values() -> None:
+    def test_fn(a: str, b: str, **kwargs: Any):
+        return a + b + kwargs["c"]
+
+    curried = curry(test_fn, c=" you you")
+    assert curried(a="hey", b=" hey") == "hey hey you you"
+
+
+def test_noop_curry() -> None:
+    def test_fn(a: str, b: str):
+        return a + b
+
+    curried = curry(test_fn)
+    assert curried(a="bye", b=" you") == "bye you"
+
+
+async def test_async_curry() -> None:
+    async def test_fn(a: str, b: str):
+        return a + b
+
+    curried = curry(test_fn, a="hey")
+    assert await curried(b=" you") == "hey you"
+
+
+async def test_curry_with_kwargs_values() -> None:
+    async def test_fn(a: str, b: str, **kwargs: Any):
+        return a + b + kwargs["c"]
+
+    curried = curry(test_fn, c=" you you")
+    assert await curried(a="hey", b=" hey") == "hey hey you you"
+
+
+async def test_noop_async_curry() -> None:
+    async def test_fn(a: str, b: str):
+        return a + b
+
+    curried = curry(test_fn)
+    assert await curried(a="bye", b=" you") == "bye you"
