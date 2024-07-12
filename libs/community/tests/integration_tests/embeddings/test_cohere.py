a2c6fc7641894c4211afc4299569d0f9892fb252
Erick Friis <erick@langchain.dev>
2024-07-12 15:49:45 -0700

    x

--- libs/community/tests/integration_tests/embeddings/test_cohere.py
+++ libs/community/tests/integration_tests/embeddings/test_cohere.py
@@ -6,7 +6,7 @@ from langchain_community.embeddings.cohere import CohereEmbeddings
 def test_cohere_embedding_documents() -> None:
     """Test cohere embeddings."""
     documents = ["foo bar"]
-    embedding = CohereEmbeddings(model="embed-english-v3.0")  # type: ignore[call-arg]
+    embedding = CohereEmbeddings()  # type: ignore[call-arg]
     output = embedding.embed_documents(documents)
     assert len(output) == 1
     assert len(output[0]) == 2048
@@ -15,6 +15,6 @@ def test_cohere_embedding_documents() -> None:
 def test_cohere_embedding_query() -> None:
     """Test cohere embeddings."""
     document = "foo bar"
-    embedding = CohereEmbeddings(model="embed-english-v3.0")  # type: ignore[call-arg]
+    embedding = CohereEmbeddings()  # type: ignore[call-arg]
     output = embedding.embed_query(document)
     assert len(output) == 2048
