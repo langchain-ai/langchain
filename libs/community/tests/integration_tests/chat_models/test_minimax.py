2abdb6b6256e1148644926f72459eff31c0c9ac1
maang-h <maang_h@163.com>
2024-07-16 23:46:58 +0800

    fmt

--- libs/community/tests/integration_tests/chat_models/test_minimax.py
+++ libs/community/tests/integration_tests/chat_models/test_minimax.py
@@ -49,11 +49,11 @@ def test_chat_minimax_with_tool() -> None:
     assert len(ai_msg.tool_calls) == 1
     tool_call = ai_msg.tool_calls[0]
     assert "args" in tool_call
-    messages.append(ai_msg)
+    messages.append(ai_msg)  # type: ignore[arg-type]
     for tool_call in ai_msg.tool_calls:
         selected_tool = {"add": add, "multiply": multiply}[tool_call["name"].lower()]
-        tool_output = selected_tool.invoke(tool_call["args"])
-        messages.append(ToolMessage(tool_output, tool_call_id=tool_call["id"]))
+        tool_output = selected_tool.invoke(tool_call["args"])  # type: ignore[attr-defined]
+        messages.append(ToolMessage(tool_output, tool_call_id=tool_call["id"]))  # type: ignore[arg-type]
     response = chat_with_tools.invoke(messages)
     assert isinstance(response, AIMessage)
 
