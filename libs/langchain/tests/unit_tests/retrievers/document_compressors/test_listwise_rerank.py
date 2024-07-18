1efa986e325527f6a85d1d2f0bb5919c4f3b90de
Chester Curme <chester.curme@gmail.com>
2024-07-18 15:29:11 -0400

    typing

--- libs/langchain/tests/unit_tests/retrievers/document_compressors/test_listwise_rerank.py
+++ libs/langchain/tests/unit_tests/retrievers/document_compressors/test_listwise_rerank.py
@@ -7,4 +7,7 @@ from langchain.retrievers.document_compressors.listwise_rerank import LLMListwis
 def test__list_rerank_init() -> None:
     from langchain_openai import ChatOpenAI
 
-    LLMListwiseRerank.from_llm(llm=ChatOpenAI(api_key="foo"), top_n=10)
+    LLMListwiseRerank.from_llm(
+        llm=ChatOpenAI(api_key="foo"),  # type: ignore[arg-type]
+        top_n=10,
+    )
