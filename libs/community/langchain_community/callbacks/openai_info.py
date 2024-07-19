dda9438e879ded0b156739e419345c6d98215bf8
Bagatur <22008038+baskaryan@users.noreply.github.com>
2024-07-19 12:02:44 -0700

    community[patch]: gpt-4o-mini costs (#24421)

--- libs/community/langchain_community/callbacks/openai_info.py
+++ libs/community/langchain_community/callbacks/openai_info.py
@@ -8,6 +8,12 @@ from langchain_core.messages import AIMessage
 from langchain_core.outputs import ChatGeneration, LLMResult
 
 MODEL_COST_PER_1K_TOKENS = {
+    # GPT-4o-mini input
+    "gpt-4o-mini": 0.00015,
+    "gpt-4o-mini-2024-07-18": 0.00015,
+    # GPT-4o-mini output
+    "gpt-4o-mini-completion": 0.0006,
+    "gpt-4o-mini-2024-07-18-completion": 0.0006,
     # GPT-4o input
     "gpt-4o": 0.005,
     "gpt-4o-2024-05-13": 0.005,
