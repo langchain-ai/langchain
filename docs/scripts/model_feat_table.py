50cb0a03bc59787fc094aba96604e51afe32a402
Erick Friis <erick@langchain.dev>
2024-07-19 13:05:59 -0700

    docs: advanced feature note (#24456)
    fixes #24430

--- docs/scripts/model_feat_table.py
+++ docs/scripts/model_feat_table.py
@@ -150,7 +150,13 @@ hide_table_of_contents: true
 
 ## Advanced features
 
-The following table shows all the chat models that support one or more advanced features.
+The following table shows all the chat model classes that support one or more advanced features.
+
+:::info
+While all these LangChain classes support the indicated advanced feature, you may have
+to open the provider-specific documentation to learn which hosted models or backends support
+the feature.
+:::
 
 {table}
 
