916f3e7bf7e2fd5d6ddc01e4b7a902116f9fd805
Joel Akeret <joel.akeret@ti&m.com>
2024-07-22 14:18:49 +0200

    linting

--- libs/experimental/tests/unit_tests/test_ollama_functions.py
+++ libs/experimental/tests/unit_tests/test_ollama_functions.py
@@ -10,15 +10,17 @@ class Schema(BaseModel):
     pass
 
 
-@patch.object(OllamaFunctions, '_create_stream')
+@patch.object(OllamaFunctions, "_create_stream")
 def test_convert_image_prompt(_create_stream_mock):
-    response = {'message': {'content': '{"tool": "Schema", "tool_input": {}}'}}
+    response = {"message": {"content": '{"tool": "Schema", "tool_input": {}}'}}
     _create_stream_mock.return_value = [json.dumps(response)]
 
-    prompt = ChatPromptTemplate.from_messages([("human", [{"image_url": "data:image/jpeg;base64,{image_url}"}])])
+    prompt = ChatPromptTemplate.from_messages(
+        [("human", [{"image_url": "data:image/jpeg;base64,{image_url}"}])]
+    )
 
     lmm = prompt | OllamaFunctions().with_structured_output(schema=Schema)
 
-    schema_instance = lmm.invoke(dict(image_url=''))
+    schema_instance = lmm.invoke(dict(image_url=""))
 
     assert schema_instance is not None
