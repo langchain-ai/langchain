3848f86f057934a44ab2f7eef4b214ac3f322ef4
gdj0nes <gareth@pinecone.io>
2024-07-23 12:27:08 -0400

    Add back 5.0.0 version check

--- libs/partners/pinecone/langchain_pinecone/embeddings.py
+++ libs/partners/pinecone/langchain_pinecone/embeddings.py
@@ -13,12 +13,10 @@ from langchain_core.pydantic_v1 import (
 )
 from langchain_core.utils import convert_to_secret_str
 from pinecone import Pinecone as PineconeClient  # type: ignore
-from pinecone import __version__ as pinecone_version
 
 logger = logging.getLogger(__name__)
 
 DEFAULT_BATCH_SIZE = 64
-MIN_PINECONE_VERSION = "5.0.0"
 
 
 class PineconeEmbeddings(BaseModel, Embeddings):
@@ -75,14 +73,6 @@ class PineconeEmbeddings(BaseModel, Embeddings):
     @root_validator()
     def validate_environment(cls, values: dict) -> dict:
         """Validate that Pinecone version and credentials exist in environment."""
-        # Embeddings new in 5.0.0
-        if int(pinecone_version.split(".")[0]) < int(
-            MIN_PINECONE_VERSION.split(".")[0]
-        ):
-            raise ValueError(
-                f"Pinecone client version must be >= {MIN_PINECONE_VERSION}."
-                f" Found version: {pinecone_version}"
-            )
 
         pinecone_api_key = values.get("pinecone_api_key") or os.getenv(
             "PINECONE_API_KEY", None
@@ -143,7 +133,7 @@ class PineconeEmbeddings(BaseModel, Embeddings):
             response = self._client.inference.embed(
                 model=self.model,
                 parameters=self.document_params,
-                inputs=texts[i : i + self.batch_size],
+                inputs=texts[i: i + self.batch_size],
             )
             embeddings.extend([r["values"] for r in response])
 
@@ -156,7 +146,7 @@ class PineconeEmbeddings(BaseModel, Embeddings):
             response = await self._aembed_texts(
                 model=self.model,
                 parameters=self.document_params,
-                texts=texts[i : i + self.batch_size],
+                texts=texts[i: i + self.batch_size],
             )
             embeddings.extend([r["values"] for r in response["data"]])
         return embeddings
@@ -177,7 +167,7 @@ class PineconeEmbeddings(BaseModel, Embeddings):
         return response["data"][0]["values"]
 
     async def _aembed_texts(
-        self, texts: List[str], model: str, parameters: dict
+            self, texts: List[str], model: str, parameters: dict
     ) -> Dict:
         data = {
             "model": model,
@@ -185,7 +175,7 @@ class PineconeEmbeddings(BaseModel, Embeddings):
             "parameters": parameters,
         }
         async with self._async_client.post(
-            "https://api.pinecone.io/embed", json=data
+                "https://api.pinecone.io/embed", json=data
         ) as response:
             response_data = await response.json(content_type=None)
             return response_data
