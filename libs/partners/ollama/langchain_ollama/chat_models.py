e4bea8e5403cedbe01e92d6c42fab016348665f3
Bagatur <baskaryan@gmail.com>
2024-07-27 15:40:53 -0700

    fmt

--- libs/partners/ollama/langchain_ollama/chat_models.py
+++ libs/partners/ollama/langchain_ollama/chat_models.py
@@ -36,7 +36,6 @@ from langchain_core.messages import (
 from langchain_core.messages.ai import UsageMetadata
 from langchain_core.messages.tool import tool_call
 from langchain_core.outputs import ChatGeneration, ChatGenerationChunk, ChatResult
-from langchain_core.pydantic_v1 import BaseModel
 from langchain_core.runnables import Runnable
 from langchain_core.tools import BaseTool
 from langchain_core.utils.function_calling import convert_to_openai_tool
