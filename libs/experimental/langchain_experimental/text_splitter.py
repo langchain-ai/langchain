647c3e36e94c0cf97cb6074c79f75b0b17ff34a3
ZhangShenao <15201440436@163.com>
2024-07-29 10:14:44 +0800

    patch[experimental] Fix start_index in `SemanticChunker`

--- libs/experimental/langchain_experimental/text_splitter.py
+++ libs/experimental/langchain_experimental/text_splitter.py
@@ -258,12 +258,10 @@ class SemanticChunker(BaseDocumentTransformer):
         _metadatas = metadatas or [{}] * len(texts)
         documents = []
         for i, text in enumerate(texts):
-            index = -1
-            for chunk in self.split_text(text):
+            for start_index, chunk in enumerate(self.split_text(text)):
                 metadata = copy.deepcopy(_metadatas[i])
                 if self._add_start_index:
-                    index = text.find(chunk, index + 1)
-                    metadata["start_index"] = index
+                    metadata["start_index"] = start_index
                 new_doc = Document(page_content=chunk, metadata=metadata)
                 documents.append(new_doc)
         return documents
