33814a17526fa74c80ee21def30f7a267177f675
Erick Friis <erick@langchain.dev>
2024-07-31 14:08:07 -0700

    x

--- libs/community/tests/unit_tests/llms/test_yandex.py
+++ libs/community/tests/unit_tests/llms/test_yandex.py
@@ -5,7 +5,9 @@ from langchain_community.llms.yandex import YandexGPT
 
 def test_yandexgpt_initialization() -> None:
     llm = YandexGPT(
-        iam_token="your_iam_token", api_key="your_api_key", folder_id="your_folder_id"
+        iam_token="your_iam_token",
+        api_key="your_api_key",  # type: ignore[arg-type]
+        folder_id="your_folder_id",
     )
     assert llm.model_name == "yandexgpt-lite"
     assert llm.model_uri.startswith("gpt://your_folder_id/yandexgpt-lite/")
@@ -15,8 +17,8 @@ def test_yandexgpt_model_params() -> None:
     llm = YandexGPT(
         model_name="custom-model",
         model_version="v1",
-        iam_token="your_iam_token",
-        api_key="your_api_key",
+        iam_token="your_iam_token",  # type: ignore[arg-type]
+        api_key="your_api_key",  # type: ignore[arg-type]
         folder_id="your_folder_id",
     )
     assert llm.model_name == "custom-model"
@@ -27,7 +29,10 @@ def test_yandexgpt_model_params() -> None:
 
 def test_yandexgpt_invalid_model_params() -> None:
     with pytest.raises(ValueError):
-        YandexGPT(model_uri="", iam_token="your_iam_token")
-
+        YandexGPT(model_uri="", iam_token="your_iam_token")  # type: ignore[arg-type]
     with pytest.raises(ValueError):
-        YandexGPT(iam_token="", api_key="your_api_key", model_uri="")
+        YandexGPT(
+            iam_token="",  # type: ignore[arg-type]
+            api_key="your_api_key",  # type: ignore[arg-type]
+            model_uri="",
+        )
