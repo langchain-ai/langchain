00576dcee360066d4735f593181a46813c305216
Eugene Yurtsev <eyurtsev@gmail.com>
2024-07-31 13:08:10 -0400

    MANUAL: FIX get_pydantic_field_names to work with pydantic 2

--- libs/core/langchain_core/utils/utils.py
+++ libs/core/langchain_core/utils/utils.py
@@ -191,9 +191,9 @@ def get_pydantic_field_names(pydantic_cls: Any) -> Set[str]:
         Set[str]: Field names.
     """
     all_required_field_names = set()
-    for field in pydantic_cls.__fields__.values():
-        all_required_field_names.add(field.name)
-        if field.has_alias:
+    for name, field in pydantic_cls.__fields__.items():
+        all_required_field_names.add(name)
+        if field.alias:
             all_required_field_names.add(field.alias)
     return all_required_field_names
 
