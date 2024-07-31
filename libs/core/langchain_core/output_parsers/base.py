1100b82a49d8c0efa1d237415d41f44d1e54d015
Eugene Yurtsev <eyurtsev@gmail.com>
2024-07-31 18:04:06 -0400

    MANUAL: Fix OutputType inference of OutputParsers

--- libs/core/langchain_core/output_parsers/base.py
+++ libs/core/langchain_core/output_parsers/base.py
@@ -166,10 +166,11 @@ class BaseOutputParser(
         Raises:
             TypeError: If the class doesn't have an inferable OutputType.
         """
-        for cls in self.__class__.__orig_bases__:  # type: ignore[attr-defined]
-            type_args = get_args(cls)
-            if type_args and len(type_args) == 1:
-                return type_args[0]
+        for base in self.__class__.__bases__:
+            if hasattr(base, "__pydantic_generic_metadata__"):
+                metadata = base.__pydantic_generic_metadata__
+                if "args" in metadata:
+                    return metadata["args"][0]
 
         raise TypeError(
             f"Runnable {self.__class__.__name__} doesn't have an inferable OutputType. "
