e17a67d6799e9c9b44cd07a7f31d88ca01342322
Eugene Yurtsev <eyurtsev@gmail.com>
2024-08-01 10:54:46 -0400

    x

--- libs/core/langchain_core/outputs/llm_result.py
+++ libs/core/langchain_core/outputs/llm_result.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from copy import deepcopy
 from typing import List, Optional, Union
 
+from langchain_core.outputs.chat_generation import ChatGeneration
 from langchain_core.outputs.generation import Generation
 from langchain_core.outputs.run_info import RunInfo
 from langchain_core.pydantic_v1 import BaseModel
