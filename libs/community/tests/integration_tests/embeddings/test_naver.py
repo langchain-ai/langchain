fd27e6b7fe12069e4ae7153ac60182a12a91b82b
Pilhwan Kim <dl_hyperclova_dev@navercorp.com>
2024-08-01 18:44:27 +0900

    package: move partner package to community package

--- libs/community/tests/integration_tests/embeddings/test_naver.py
+++ libs/community/tests/integration_tests/embeddings/test_naver.py
@@ -1,5 +1,5 @@
 """Test Naver embeddings."""
-from langchain_naver.embeddings import ClovaXEmbeddings
+from langchain_community.embeddings.naver import ClovaXEmbeddings
 
 
 def test_langchain_naver_embedding_documents() -> None:
