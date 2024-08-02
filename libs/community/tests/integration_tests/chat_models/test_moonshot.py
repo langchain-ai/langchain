ca75b53585b57e27a790500204534c9793482658
ZhangShenao <15201440436@163.com>
2024-08-02 14:14:27 +0800

    format code

--- libs/community/tests/integration_tests/chat_models/test_moonshot.py
+++ libs/community/tests/integration_tests/chat_models/test_moonshot.py
@@ -7,7 +7,7 @@ from langchain_community.chat_models.moonshot import MoonshotChat
 
 def test_default_call() -> None:
     """Test default model call."""
-    chat = MoonshotChat()   # type: ignore[call-arg]
+    chat = MoonshotChat()  # type: ignore[call-arg]
     response = chat.invoke([HumanMessage(content="How are you?")])
     assert isinstance(response, BaseMessage)
     assert isinstance(response.content, str)
@@ -15,7 +15,7 @@ def test_default_call() -> None:
 
 def test_model() -> None:
     """Test model kwarg works."""
-    chat = MoonshotChat(model="moonshot-v1-32k")    # type: ignore[call-arg]
+    chat = MoonshotChat(model="moonshot-v1-32k")  # type: ignore[call-arg]
     response = chat.invoke([HumanMessage(content="How are you?")])
     assert isinstance(response, BaseMessage)
     assert isinstance(response.content, str)
@@ -23,7 +23,7 @@ def test_model() -> None:
 
 def test_multiple_history() -> None:
     """Tests multiple history works."""
-    chat = MoonshotChat()   # type: ignore[call-arg]
+    chat = MoonshotChat()  # type: ignore[call-arg]
 
     response = chat.invoke(
         [
