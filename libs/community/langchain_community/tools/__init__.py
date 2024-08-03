9fd9b974ce0a3639420d7af59170032eec868d3d
virattt <virat.dot@gmail.com>
2024-08-03 14:51:14 -0400

    community: Add IncomeStatements, BalanceSheets, and CashFlowStatements tools from financialdatasets.ai

--- libs/community/langchain_community/tools/__init__.py
+++ libs/community/langchain_community/tools/__init__.py
@@ -211,6 +211,15 @@ if TYPE_CHECKING:
     from langchain_community.tools.plugin import (
         AIPluginTool,
     )
+    from langchain_community.tools.financial_datasets.balance_sheets import (
+        BalanceSheets,
+    )
+    from langchain_community.tools.financial_datasets.cash_flow_statements import (
+        CashFlowStatements,
+    )
+    from langchain_community.tools.financial_datasets.income_statements import (
+        IncomeStatements,
+    )
     from langchain_community.tools.polygon.aggregates import (
         PolygonAggregates,
     )
