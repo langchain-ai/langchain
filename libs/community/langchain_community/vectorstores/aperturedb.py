5fe450a11683dd7ff1fa90688cd30e8665ac7cb4
Eugene Yurtsev <eyurtsev@gmail.com>
2024-08-06 08:45:04 -0400

    remove @override

--- libs/community/langchain_community/vectorstores/aperturedb.py
+++ libs/community/langchain_community/vectorstores/aperturedb.py
@@ -499,7 +499,6 @@ class ApertureDB(VectorStore):
         # If upsert has been implemented, we can use it to add documents
         return self.upsert(documents_, **kwargs)["succeeded"]
 
-    @override
     def upsert(self, items: Sequence[Document], /, **kwargs: Any) -> UpsertResponse:
         """Insert or update items
 
