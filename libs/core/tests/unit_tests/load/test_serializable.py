ece0ab85392bf6573070e96ff8330b550f98f82d
Eugene Yurtsev <eyurtsev@gmail.com>
2024-08-07 15:18:16 -0400

    Add more tests for serializable

---
+++ libs/core/tests/unit_tests/load/test_serializable.py
@@ -0,0 +1,61 @@
+from typing import List, Dict
+
+from langchain_core.load import Serializable, dumpd
+
+
+def test_simple_serialization() -> None:
+    class Foo(Serializable):
+        bar: int
+        baz: str
+
+    foo = Foo(bar=1, baz="hello")
+    assert dumpd(foo) == {
+        "id": ["tests", "unit_tests", "load", "test_serializable", "Foo"],
+        "lc": 1,
+        "repr": "Foo(bar=1, baz='hello')",
+        "type": "not_implemented",
+    }
+
+
+def test_simple_serialization_is_serializable() -> None:
+    class Foo(Serializable):
+        bar: int
+        baz: str
+
+        @classmethod
+        def is_lc_serializable(cls) -> bool:
+            return True
+
+    foo = Foo(bar=1, baz="hello")
+    assert foo.lc_id() == ["tests", "unit_tests", "load", "test_serializable", "Foo"]
+    assert dumpd(foo) == {
+        "id": ["tests", "unit_tests", "load", "test_serializable", "Foo"],
+        "kwargs": {"bar": 1, "baz": "hello"},
+        "lc": 1,
+        "type": "constructor",
+    }
+
+
+def test_simple_serialization_secret() -> None:
+    """Test handling of secrets."""
+    from pydantic import SecretStr
+
+    class Foo(Serializable):
+        bar: int
+        baz: str
+        secret1: str
+        secret2: SecretStr
+
+        @classmethod
+        def is_lc_serializable(cls) -> bool:
+            return True
+
+        @classmethod
+        def lc_secrets(self) -> Dict[str, str]:
+            return {
+                "secret1": "MY_SECRET_1",
+                "secret2": "MY_SECRET_2",
+            }
+
+    foo = Foo(bar=1, baz="baz", secret1="secret", secret2="meow")
+    assert dumpd(foo) == {}
