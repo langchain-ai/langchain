5bef45b34168bcabae3c79454e382a7acba625a6
Bagatur <baskaryan@gmail.com>
2024-08-08 00:48:58 -0700

    fix

--- docs/vercel_build.sh
+++ docs/vercel_build.sh
@@ -9,9 +9,9 @@ make build
 rm -rf docs
 mv build/output-new/docs ./
 
-mkdir docs/static/api_reference
+mkdir static/api_reference
 
 git clone --depth=1 https://github.com/baskaryan/langchain-api-docs-build.git
 
-mv -r langchain-api-docs-build/api_reference_build/html/* docs/static/api_reference/
+mv -r langchain-api-docs-build/api_reference_build/html/* static/api_reference/
 
