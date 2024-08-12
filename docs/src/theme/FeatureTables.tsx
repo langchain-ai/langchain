e98d29979f7986eabd44563590356a9eea2e0dab
Erick Friis <erick@langchain.dev>
2024-08-12 13:11:53 -0700

    docs: retrievers index page

--- docs/src/theme/FeatureTables.tsx
+++ docs/src/theme/FeatureTables.tsx
@@ -39,6 +39,93 @@ const FeatureTables: Record<string, Category> = {
             }
         ]
     },
+    document_retrievers: {
+        link: 'docs/integrations/retrievers',
+        columns: [
+            {title: "Retriever", formatter: (item) => <a href={item.link}>{item.name}</a>},
+            {title: "Self-host", formatter: (item) => item.selfHost ? "✅" : "❌"},
+            {title: "Cloud offering", formatter: (item) => item.cloudOffering ? "✅" : "❌"},
+            {title: "Package", formatter: (item) => <a href={item.apiLink}>{item.package}</a>},
+        ],
+        items: [
+            {
+                name: "AmazonKnowledgeBasesRetriever",
+                link: "bedrock",
+                selfHost: false,
+                cloudOffering: true,
+                apiLink: "https://api.python.langchain.com/en/latest/retrievers/langchain_aws.retrievers.bedrock.AmazonKnowledgeBasesRetriever.html",
+                package: "langchain_aws"
+            },
+            {
+                name: "AzureAISearchRetriever",
+                link: "azure_ai_search",
+                selfHost: false,
+                cloudOffering: true,
+                apiLink: "https://api.python.langchain.com/en/latest/retrievers/langchain_community.retrievers.azure_ai_search.AzureAISearchRetriever.html",
+                package: "langchain_community"
+            },
+            {
+                name: "ElasticsearchRetriever",
+                link: "elasticsearch_retriever",
+                selfHost: true,
+                cloudOffering: true,
+                apiLink: "https://api.python.langchain.com/en/latest/retrievers/langchain_elasticsearch.retrievers.ElasticsearchRetriever.html",
+                package: "langchain_elasticsearch"
+            },
+            {
+                name: "MilvusCollectionHybridSearchRetriever",
+                link: "milvus_hybrid_search",
+                selfHost: true,
+                cloudOffering: false,
+                apiLink: "https://api.python.langchain.com/en/latest/retrievers/langchain_milvus.retrievers.milvus_hybrid_search.MilvusCollectionHybridSearchRetriever.html",
+                package: "langchain_milvus"
+            },
+            {
+                name: "VertexAISearchRetriever",
+                link: "google_vertex_ai_search",
+                selfHost: false,
+                cloudOffering: true,
+                apiLink: "https://api.python.langchain.com/en/latest/vertex_ai_search/langchain_google_community.vertex_ai_search.VertexAISearchRetriever.html",
+                package: "langchain_google_community"
+            }
+        ],
+    },
+    external_retrievers: {
+        link: 'docs/integrations/retrievers',
+        columns: [
+            {title: "Retriever", formatter: (item) => <a href={
+                item.link
+            }>{item.name}</a>},
+            {title: "Source", formatter: (item) => item.source},
+            {title: "Package", formatter: (item) => <a href={
+                item.apiLink
+            }>{item.package}</a>},
+        ],
+        items: [
+            {
+                name: "ArxivRetriever",
+                link: "arxiv",
+                source: (<>Scholarly articles on <a href="https://arxiv.org/">arxiv.org</a></>),
+                apiLink: "https://api.python.langchain.com/en/latest/retrievers/langchain_community.retrievers.arxiv.ArxivRetriever.html",
+                package: "langchain_community"
+            },
+            {
+                name: "TavilySearchAPIRetriever",
+                link: "tavily",
+                source: "Internet search",
+                apiLink: "https://api.python.langchain.com/en/latest/retrievers/langchain_community.retrievers.tavily_search_api.TavilySearchAPIRetriever.html",
+                package: "langchain_community"
+            },
+            {
+                name: "WikipediaRetriever",
+                link: "wikipedia",
+                source: (<><a href="https://www.wikipedia.org/">Wikipedia</a> articles</>),
+                apiLink: "https://api.python.langchain.com/en/latest/retrievers/langchain_community.retrievers.wikipedia.WikipediaRetriever.html",
+                package: "langchain_community"
+            }
+        ]
+
+    }
 };
 
 function toTable(columns: Column[], items: any[]) {
