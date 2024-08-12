e77eeee6ee8253710b9427565d3c3b987827cccf
ccurme <chester.curme@gmail.com>
2024-08-12 10:51:59 -0400

    core[patch]: add standard tracing params for retrievers (#25240)

--- libs/core/langchain_core/vectorstores/base.py
+++ libs/core/langchain_core/vectorstores/base.py
@@ -44,7 +44,7 @@ from typing import (
 
 from langchain_core.embeddings import Embeddings
 from langchain_core.pydantic_v1 import Field, root_validator
-from langchain_core.retrievers import BaseRetriever
+from langchain_core.retrievers import BaseRetriever, LangSmithRetrieverParams
 from langchain_core.runnables.config import run_in_executor
 
 if TYPE_CHECKING:
@@ -1013,6 +1013,25 @@ class VectorStoreRetriever(BaseRetriever):
                     "in `search_kwargs`."
                 )
         return values
+
+    def _get_ls_params(self, **kwargs: Any) -> LangSmithRetrieverParams:
+        """Get standard params for tracing."""
+
+        ls_params = super()._get_ls_params(**kwargs)
+        ls_params["ls_vector_store_provider"] = self.vectorstore.__class__.__name__
+
+        if self.vectorstore.embeddings:
+            ls_params["ls_embedding_provider"] = (
+                self.vectorstore.embeddings.__class__.__name__
+            )
+        elif hasattr(self.vectorstore, "embedding") and isinstance(
+            self.vectorstore.embedding, Embeddings
+        ):
+            ls_params["ls_embedding_provider"] = (
+                self.vectorstore.embedding.__class__.__name__
+            )
+
+        return ls_params
 
     def _get_relevant_documents(
         self, query: str, *, run_manager: CallbackManagerForRetrieverRun
