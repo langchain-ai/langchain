d52232aa752fa576bd80b123962a735b620e2126
Eugene Yurtsev <eyurtsev@gmail.com>
2024-08-12 16:41:39 -0400

    x

--- libs/partners/ollama/langchain_ollama/embeddings.py
+++ libs/partners/ollama/langchain_ollama/embeddings.py
@@ -11,21 +11,50 @@ from ollama import AsyncClient, Client
 class OllamaEmbeddings(BaseModel, Embeddings):
     """Ollama embedding model integration.
 
-    Setup:
-    * Follow the instructions here: https://github.com/ollama/ollama to set up and
-      run a local Ollama instance.
-    * Fetch available LLM model via `ollama pull <name-of-model>`.
-        * View a list of available models via the model library (https://ollama.com/library)
-        * For example, `ollama pull llama3`
-    * This will download the default tagged version of the model. Typically, the default points to the latest, smallest sized-parameter model.
+    Set up a local Ollama instance:
+        Install the Ollama package and set up a local Ollama instance 
+        using the instructions here: https://github.com/ollama/ollama .
+        
+        You will need to choose a model to serve.
+        
+        You can view a list of available models via the model library (https://ollama.com/library).
+        
+        To fetch a model from the Ollama model library use ``ollama pull <name-of-model>``.
+        
+        For example, to pull the llama3 model:
+        
+        .. code-block:: bash
+        
+            ollama pull llama3
+            
+        This will download the default tagged version of the model. 
+        Typically, the default points to the latest, smallest sized-parameter model.
+        
         * On Mac, the models will be downloaded to ~/.ollama/models
         * On Linux (or WSL), the models will be stored at /usr/share/ollama/.ollama/models
-    * Specify the exact version of the model of interest as such ollama pull vicuna:13b-v1.5-16k-q4_0 (View the various tags for the Vicuna model in this instance)
-    * To view all pulled models, use `ollama list`.
-    * View the Ollama documentation for more commands. https://github.com/ollama/ollama
-    * Run `ollama help` in the terminal to see available commands too.
-
-    Setup:
+        
+        You can specify the exact version of the model of interest 
+        as such ``ollama pull vicuna:13b-v1.5-16k-q4_0``.
+        
+        To view pulled models:
+        
+        .. code-block:: bash
+        
+            ollama list
+        
+        To start serving:
+        
+        .. code-block:: bash
+        
+            ollama serve
+            
+        View the Ollama documentation for more commands.
+        
+        .. code-block:: bash
+        
+            ollama help
+            
+    Install the langchain-ollama integration package:
         .. code-block:: bash
 
             pip install -U langchain_ollama
