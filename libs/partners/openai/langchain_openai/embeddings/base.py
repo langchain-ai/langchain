130481e951a2a6db41fdc599878bebaaa657541b
Eugene Yurtsev <eyurtsev@gmail.com>
2024-08-14 09:29:15 -0400

    x

--- libs/partners/openai/langchain_openai/embeddings/base.py
+++ libs/partners/openai/langchain_openai/embeddings/base.py
@@ -21,12 +21,7 @@ from typing import (
 import openai
 import tiktoken
 from langchain_core.embeddings import Embeddings
-from langchain_core.pydantic_v1 import (
-    BaseModel,
-    Field,
-    SecretStr,
-    root_validator,
-)
+from langchain_core.pydantic_v1 import BaseModel, Field, SecretStr, root_validator
 from langchain_core.utils import (
     convert_to_secret_str,
     get_from_dict_or_env,
