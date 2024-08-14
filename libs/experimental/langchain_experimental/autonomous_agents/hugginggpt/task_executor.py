493e474063817b9a4c2521586b2dbc34d20b4cf1
Bagatur <22008038+baskaryan@users.noreply.github.com>
2024-08-14 07:00:17 -0700

    docs: udpated api reference (#25172)
    - Move the API reference into the vercel build
    - Update api reference organization and styling

--- libs/experimental/langchain_experimental/autonomous_agents/hugginggpt/task_executor.py
+++ libs/experimental/langchain_experimental/autonomous_agents/hugginggpt/task_executor.py
@@ -3,7 +3,7 @@ import uuid
 from typing import Dict, List
 
 import numpy as np
-from langchain.tools.base import BaseTool
+from langchain_core.tools import BaseTool
 
 from langchain_experimental.autonomous_agents.hugginggpt.task_planner import Plan
 
