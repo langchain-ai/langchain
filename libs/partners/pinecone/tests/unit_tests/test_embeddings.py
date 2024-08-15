f26b0e6bef144b03aac83ebebe07ab0468430e5e
Eugene Yurtsev <eyurtsev@gmail.com>
2024-08-15 15:43:15 -0400

    x

--- libs/partners/pinecone/tests/unit_tests/test_embeddings.py
+++ libs/partners/pinecone/tests/unit_tests/test_embeddings.py
@@ -7,7 +7,10 @@ MODEL_NAME = "multilingual-e5-large"
 
 
 def test_default_config() -> None:
-    e = PineconeEmbeddings(pinecone_api_key=API_KEY, model=MODEL_NAME)  # type: ignore
+    e = PineconeEmbeddings(
+        pinecone_api_key=API_KEY,  # type: ignore[call-arg]
+        model=MODEL_NAME,
+    )
     assert e.batch_size == 96
 
 
@@ -17,5 +20,9 @@ def test_default_config_with_api_key() -> None:
 
 
 def test_custom_config() -> None:
-    e = PineconeEmbeddings(pinecone_api_key=API_KEY, model=MODEL_NAME, batch_size=128)
+    e = PineconeEmbeddings(
+        pinecone_api_key=API_KEY,  # type: ignore[call-arg]
+        model=MODEL_NAME,
+        batch_size=128,
+    )
     assert e.batch_size == 128
