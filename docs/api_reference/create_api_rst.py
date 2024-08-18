7be4dde1c0a1ef87af866ba36c9c35f849112ef2
Bagatur <baskaryan@gmail.com>
2024-08-18 12:02:28 -0700

    fmt

--- docs/api_reference/create_api_rst.py
+++ docs/api_reference/create_api_rst.py
@@ -529,7 +529,7 @@ def _build_index(dirs: List[str]) -> None:
     ordered = ["core", "langchain", "text-splitters", "community", "experimental"]
     main_ = [dir_ for dir_ in ordered if dir_ in dirs]
     integrations = sorted(dir_ for dir_ in dirs if dir_ not in main_)
-    doc = f"""# LangChain Python API Reference
+    doc = """# LangChain Python API Reference
 
 Welcome to the LangChain Python API reference. This is a reference for all 
 `langchain-x` packages. 
