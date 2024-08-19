b5474eab371f4191c8e4bc390653570651d0c646
leo-gan <leo.gan.57@gmail.com>
2024-08-19 12:56:29 -0700

    added references

--- libs/langchain/langchain/chains/constitutional_ai/models.py
+++ libs/langchain/langchain/chains/constitutional_ai/models.py
@@ -4,7 +4,11 @@ from langchain_core.pydantic_v1 import BaseModel
 
 
 class ConstitutionalPrinciple(BaseModel):
-    """Class for a constitutional principle."""
+    """Class for a constitutional principle.
+
+    `Constitutional AI principles` are based on the
+    [Constitutional AI: Harmlessness from AI Feedback](https://arxiv.org/pdf/2212.08073) paper.
+    """  # noqa: E501
 
     critique_request: str
     revision_request: str
