3b089a53775c7a5920340333973b8bce463a029b
istrebitel-1 <istrebitel.3.12@gmail.com>
2024-08-19 17:04:35 +0300

    Fix UnionType type var replacement
    - Added types.UnionType to typing.Union mapping

--- libs/core/langchain_core/utils/function_calling.py
+++ libs/core/langchain_core/utils/function_calling.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 import collections
 import inspect
 import logging
+import types
 import typing
 import uuid
 from typing import (
@@ -575,6 +576,7 @@ def _parse_google_docstring(
 
 
 def _py_38_safe_origin(origin: Type) -> Type:
+    union_type_origin = types.UnionType if hasattr(types, "UnionType") else Union
     origin_map: Dict[Type, Any] = {
         dict: Dict,
         list: List,
@@ -584,5 +586,6 @@ def _py_38_safe_origin(origin: Type) -> Type:
         collections.abc.Mapping: typing.Mapping,
         collections.abc.Sequence: typing.Sequence,
         collections.abc.MutableMapping: typing.MutableMapping,
+        union_type_origin: Union,
     }
     return cast(Type, origin_map.get(origin, origin))
