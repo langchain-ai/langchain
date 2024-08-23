b1929681f5fd86833a118329325e45ea21a46774
Erick Friis <erick@langchain.dev>
2024-08-22 17:58:43 -0700

    core: mmr util

--- libs/core/langchain_core/vectorstores/in_memory.py
+++ libs/core/langchain_core/vectorstores/in_memory.py
@@ -21,9 +21,7 @@ from langchain_core.embeddings import Embeddings
 from langchain_core.load import dumpd, load
 from langchain_core.vectorstores import VectorStore
 from langchain_core.vectorstores.utils import _cosine_similarity as cosine_similarity
-from langchain_core.vectorstores.utils import (
-    _maximal_marginal_relevance as maximal_marginal_relevance,
-)
+from langchain_core.vectorstores.utils import maximal_marginal_relevance
 
 if TYPE_CHECKING:
     from langchain_core.indexing import UpsertResponse
