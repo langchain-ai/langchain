469b40c5e31919ceeff1cdbe06913c293868f9a0
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-08-24 13:19:34 +0530

    removed the use of root_validator in the mind wrapper and data source config

--- libs/community/langchain_community/utilities/mindsdb/ai_data_mind/ai_data_mind_wrapper.py
+++ libs/community/langchain_community/utilities/mindsdb/ai_data_mind/ai_data_mind_wrapper.py
@@ -13,22 +13,20 @@ class DataSourceConfig(BaseModel):
     connection_args: Dict[Text, Any]
     tables: Optional[List[Text]] = Field(default=[])
 
-    @root_validator()
-    def validate_environment(cls, values: Dict) -> Dict:
-        supported_data_sources = get_supported_data_sources()
+    def __init__(self, **data):
+        super().__init__(**data)
 
-        if values['type'] not in supported_data_sources.keys():
+        supported_data_sources = get_supported_data_sources()
+        if self.type not in supported_data_sources.keys():
             raise ValueError(
-                f"Data source type '{values['type']}' is not supported. "
+                f"Data source type '{self.type}' is not supported. "
                 f"Supported data source types are: {supported_data_sources}."
             )
 
-        model_cls = supported_data_sources[values['type']]
-        model_obj = model_cls(**values['connection_args'])
-        values['connection_args'] = model_obj.dict()
+        model_cls = supported_data_sources[self.type]
+        model_obj = model_cls(**self.connection_args)
+        self.connection_args = model_obj.dict()
 
-        return values
-    
     def to_database_config(self) -> DatabaseConfig:
         return DatabaseConfig(
             type=self.type,
@@ -40,39 +38,34 @@ class DataSourceConfig(BaseModel):
 
 class AIDataMindWrapper(BaseMindWrapper):
     data_source_configs: List[Dict[Text, Any]] = Field(default=None)
-
     mind: Any = Field(default=None, exclude=True)
 
-    @root_validator()
-    def validate_environment(cls, values: Dict) -> Dict:
-        # Validate that the `mindsdb_sdk` package can be imported.
-        super().validate_environment(values)
+    def __init__(self, **data):
+        super().__init__(**data)
 
+        # Validate that the `mindsdb_sdk` package can be imported.
         try:
             from mindsdb_sdk.utils.mind import create_mind
-
         except ImportError as e:
             raise ImportError(
                 "Could not import mindsdb_sdk python package. "
                 "Please install it with `pip install mindsdb_sdk`.",
             ) from e
-        
+
         # Validate that the correct connection arguments are provided for the chosen data sources.
         data_source_config_objs = []
-        for data_source_config in values['data_source_configs']:
+        for data_source_config in self.data_source_configs:
             data_source_config_obj = DataSourceConfig(**data_source_config)
             data_source_config_objs.append(data_source_config_obj.to_database_config())
 
         # Create the MindsDB mind object.
-        values['mind'] = create_mind(
-            name=values['name'],
-            base_url=values['mindsdb_api_base'],
-            api_key=values['mindsdb_api_key'].get_secret_value(),
-            model=values['model'],
+        self.mind = create_mind(
+            name=self.name,
+            base_url=self.mindsdb_api_base,
+            api_key=self.mindsdb_api_key.get_secret_value(),
+            model=self.model,
             data_source_configs=data_source_config_objs,
         )
-
-        return values
 
     def run(self, query: Text) -> Text:
         completion = self.client.create(
--- libs/community/langchain_community/utilities/mindsdb/base_mind_wrapper.py
+++ libs/community/langchain_community/utilities/mindsdb/base_mind_wrapper.py
@@ -15,38 +15,33 @@ class BaseMindWrapper(BaseModel):
     mindsdb_api_base: Text = Field(default=DEFAULT_API_BASE)
     model: Text = Field(default=DEFAULT_MODEL)
     name: Text = Field(default=None)
-
     client: Any = Field(default=None, exclude=True)
 
-    @root_validator()
-    def validate_environment(cls, values: Dict) -> Dict:
-        """
-        Validate that the MindsDB API credentials are provided and that the required packages are installed.
-        Further, validate that the chosen model is supported by the MindsDB API.
-        """
+    def __init__(self, **data):
+        super().__init__(**data)
+
         # Validate that the API key and base URL are available.
-        values["mindsdb_api_key"] = convert_to_secret_str(
+        self.mindsdb_api_key = convert_to_secret_str(
             get_from_dict_or_env(
-                values,
+                data,
                 "mindsdb_api_key",
                 "MINDSDB_API_KEY",
             )
         )
-        values["mindsdb_api_base"] = get_from_dict_or_env(
-            values,
+        self.mindsdb_api_base = get_from_dict_or_env(
+            data,
             "mindsdb_api_base",
             "MINDSDB_API_BASE",
             default=DEFAULT_API_BASE,
         )
 
         # If a name is not provided, generate a random one.
-        if not values.get("name"):
-            values["name"] = f"lc_mind_{secrets.token_hex(5)}"
+        if not self.name:
+            self.name = f"lc_mind_{secrets.token_hex(5)}"
 
-        # Validate that the `openai' packages can be imported.
+        # Validate that the `openai` package can be imported.
         try:
             import openai
-
         except ImportError as e:
             raise ImportError(
                 "Could not import openai python package. "
@@ -57,16 +52,15 @@ class BaseMindWrapper(BaseModel):
         try:
             if is_openai_v1():
                 client_params = {
-                    "api_key": values["mindsdb_api_key"].get_secret_value(),
-                    "base_url": values["mindsdb_api_base"],
+                    "api_key": self.mindsdb_api_key.get_secret_value(),
+                    "base_url": self.mindsdb_api_base,
                 }
-                if not values.get("client"):
-                    values["client"] = openai.OpenAI(**client_params).chat.completions
-
+                if not self.client:
+                    self.client = openai.OpenAI(**client_params).chat.completions
             else:
-                values["openai_api_base"] = values["mindsdb_api_base"]
-                values["openai_api_key"] = values["mindsdb_api_key"].get_secret_value()
-                values["client"] = openai.ChatCompletion
+                self.openai_api_base = self.mindsdb_api_base
+                self.openai_api_key = self.mindsdb_api_key.get_secret_value()
+                self.client = openai.ChatCompletion
         except AttributeError as exc:
             raise ValueError(
                 "`openai` has no `ChatCompletion` attribute, this is likely "
