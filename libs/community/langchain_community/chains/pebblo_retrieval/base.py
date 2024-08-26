58a98c7d8a2bfd094a306dec4f121eab6dd99a84
Rajendra Kadam <raj.725@outlook.com>
2024-08-26 16:57:05 +0530

    community: [PebbloRetrievalQA] Implemented Async support for prompt APIs (#25748)
    - **Description:** PebbloRetrievalQA: Implemented Async support for
    prompt APIs (classification and governance)
    - **Issue:** NA
    - **Dependencies:** NA

--- libs/community/langchain_community/chains/pebblo_retrieval/base.py
+++ libs/community/langchain_community/chains/pebblo_retrieval/base.py
@@ -149,6 +149,7 @@ class PebbloRetrievalQA(Chain):
         res = indexqa({'query': 'This is my query'})
         answer, docs = res['result'], res['source_documents']
         """
+        prompt_time = datetime.datetime.now().isoformat()
         _run_manager = run_manager or AsyncCallbackManagerForChainRun.get_noop_manager()
         question = inputs[self.input_key]
         auth_context = inputs.get(self.auth_context_key)
@@ -157,7 +158,7 @@ class PebbloRetrievalQA(Chain):
             "run_manager" in inspect.signature(self._aget_docs).parameters
         )
 
-        _, prompt_entities = self.pb_client.check_prompt_validity(question)
+        _, prompt_entities = await self.pb_client.acheck_prompt_validity(question)
 
         if accepts_run_manager:
             docs = await self._aget_docs(
@@ -167,6 +168,18 @@ class PebbloRetrievalQA(Chain):
             docs = await self._aget_docs(question, auth_context, semantic_context)  # type: ignore[call-arg]
         answer = await self.combine_documents_chain.arun(
             input_documents=docs, question=question, callbacks=_run_manager.get_child()
+        )
+
+        await self.pb_client.asend_prompt(
+            self.app_name,
+            self.retriever,
+            question,
+            answer,
+            auth_context,
+            docs,
+            prompt_entities,
+            prompt_time,
+            self.enable_prompt_gov,
         )
 
         if self.return_source_documents:
