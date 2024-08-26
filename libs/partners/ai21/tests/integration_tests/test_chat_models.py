17dffd974154c6ab7db0c35f1a537a271abd2345
amirai21 <89905406+amirai21@users.noreply.github.com>
2024-08-26 20:50:30 +0300

    AI21: tools calling support in Langchain (#25635)
    This pull request introduces support for the AI21 tools calling feature,
    available by the Jamba-1.5 models. When Jamba-1.5 detects the necessity
    to invoke a provided tool, as indicated by the 'tools' parameter passed
    to the model:
    
    ```
    class ToolDefinition(TypedDict, total=False):
        type: Required[Literal["function"]]
        function: Required[FunctionToolDefinition]
    
    class FunctionToolDefinition(TypedDict, total=False):
        name: Required[str]
        description: str
        parameters: ToolParameters
    
    class ToolParameters(TypedDict, total=False):
        type: Literal["object"]
        properties: Required[Dict[str, Any]]
        required: List[str]
    ```
    
    It will respond with a list of tool calls structured as follows:
    
    ```
    class ToolCall(AI21BaseModel):
        id: str
        function: ToolFunction
        type: Literal["function"] = "function"
    
    class ToolFunction(AI21BaseModel):
        name: str
        arguments: str
    ```
    
    This pull request incorporates the necessary modifications to integrate
    this functionality into the ai21-langchain library.
    
    ---------
    
    Co-authored-by: asafg <asafg@ai21.com>
    Co-authored-by: pazshalev <111360591+pazshalev@users.noreply.github.com>
    Co-authored-by: Paz Shalev <pazs@ai21.com>
    Co-authored-by: Erick Friis <erick@langchain.dev>

--- libs/partners/ai21/tests/integration_tests/test_chat_models.py
+++ libs/partners/ai21/tests/integration_tests/test_chat_models.py
@@ -1,12 +1,25 @@
 """Test ChatAI21 chat model."""
 
 import pytest
-from langchain_core.messages import AIMessageChunk, HumanMessage
+from langchain_core.messages import (
+    AIMessageChunk,
+    HumanMessage,
+    SystemMessage,
+    ToolMessage,
+)
 from langchain_core.outputs import ChatGeneration
 from langchain_core.rate_limiters import InMemoryRateLimiter
+from langchain_core.tools import tool
+from langchain_core.utils.function_calling import convert_to_openai_tool
 
 from langchain_ai21.chat_models import ChatAI21
-from tests.unit_tests.conftest import J2_CHAT_MODEL_NAME, JAMBA_CHAT_MODEL_NAME
+from tests.unit_tests.conftest import (
+    J2_CHAT_MODEL_NAME,
+    JAMBA_1_5_LARGE_CHAT_MODEL_NAME,
+    JAMBA_1_5_MINI_CHAT_MODEL_NAME,
+    JAMBA_CHAT_MODEL_NAME,
+    JAMBA_FAMILY_MODEL_NAMES,
+)
 
 rate_limiter = InMemoryRateLimiter(requests_per_second=0.5)
 
@@ -15,11 +28,15 @@ rate_limiter = InMemoryRateLimiter(requests_per_second=0.5)
     ids=[
         "when_j2_model",
         "when_jamba_model",
+        "when_jamba1.5-mini_model",
+        "when_jamba1.5-large_model",
     ],
     argnames=["model"],
     argvalues=[
         (J2_CHAT_MODEL_NAME,),
         (JAMBA_CHAT_MODEL_NAME,),
+        (JAMBA_1_5_MINI_CHAT_MODEL_NAME,),
+        (JAMBA_1_5_LARGE_CHAT_MODEL_NAME,),
     ],
 )
 def test_invoke(model: str) -> None:
@@ -36,6 +53,10 @@ def test_invoke(model: str) -> None:
         "when_j2_model_num_results_is_3",
         "when_jamba_model_n_is_1",
         "when_jamba_model_n_is_3",
+        "when_jamba1.5_mini_model_n_is_1",
+        "when_jamba1.5_mini_model_n_is_3",
+        "when_jamba1.5_large_model_n_is_1",
+        "when_jamba1.5_large_model_n_is_3",
     ],
     argnames=["model", "num_results"],
     argvalues=[
@@ -43,12 +64,16 @@ def test_invoke(model: str) -> None:
         (J2_CHAT_MODEL_NAME, 3),
         (JAMBA_CHAT_MODEL_NAME, 1),
         (JAMBA_CHAT_MODEL_NAME, 3),
+        (JAMBA_1_5_MINI_CHAT_MODEL_NAME, 1),
+        (JAMBA_1_5_MINI_CHAT_MODEL_NAME, 3),
+        (JAMBA_1_5_LARGE_CHAT_MODEL_NAME, 1),
+        (JAMBA_1_5_LARGE_CHAT_MODEL_NAME, 3),
     ],
 )
 def test_generation(model: str, num_results: int) -> None:
     """Test generation with multiple models and different result counts."""
     # Determine the configuration key based on the model type
-    config_key = "n" if model == JAMBA_CHAT_MODEL_NAME else "num_results"
+    config_key = "n" if model in JAMBA_FAMILY_MODEL_NAMES else "num_results"
 
     # Create the model instance using the appropriate key for the result count
     llm = ChatAI21(model=model, rate_limiter=rate_limiter, **{config_key: num_results})  # type: ignore[arg-type, arg-type, arg-type, arg-type, arg-type, arg-type, arg-type, arg-type, arg-type, arg-type]
@@ -69,11 +94,15 @@ def test_generation(model: str, num_results: int) -> None:
     ids=[
         "when_j2_model",
         "when_jamba_model",
+        "when_jamba1.5_mini_model",
+        "when_jamba1.5_large_model",
     ],
     argnames=["model"],
     argvalues=[
         (J2_CHAT_MODEL_NAME,),
         (JAMBA_CHAT_MODEL_NAME,),
+        (JAMBA_1_5_MINI_CHAT_MODEL_NAME,),
+        (JAMBA_1_5_LARGE_CHAT_MODEL_NAME,),
     ],
 )
 async def test_ageneration(model: str) -> None:
@@ -92,7 +121,7 @@ async def test_ageneration(model: str) -> None:
 
 
 def test__chat_stream() -> None:
-    llm = ChatAI21(model="jamba-instruct")  # type: ignore[call-arg]
+    llm = ChatAI21(model="jamba-1.5-mini")  # type: ignore[call-arg]
     message = HumanMessage(content="What is the meaning of life?")
 
     for chunk in llm.stream([message]):
@@ -107,3 +136,53 @@ def test__j2_chat_stream__should_raise_error() -> None:
     with pytest.raises(NotImplementedError):
         for _ in llm.stream([message]):
             pass
+
+
+@pytest.mark.parametrize(
+    ids=[
+        "when_jamba1.5_mini_model",
+        "when_jamba1.5_large_model",
+    ],
+    argnames=["model"],
+    argvalues=[
+        (JAMBA_1_5_MINI_CHAT_MODEL_NAME,),
+        (JAMBA_1_5_LARGE_CHAT_MODEL_NAME,),
+    ],
+)
+def test_tool_calls(model: str) -> None:
+    @tool
+    def get_weather(location: str, date: str) -> str:
+        """“Provide the weather for the specified location on the given date.”"""
+        if location == "New York" and date == "2024-12-05":
+            return "25 celsius"
+        return "32 celsius"
+
+    llm = ChatAI21(model=model, temperature=0)  # type: ignore[call-arg]
+    llm_with_tools = llm.bind_tools([convert_to_openai_tool(get_weather)])
+
+    chat_messages = [
+        SystemMessage(
+            content="You are a helpful assistant. "
+            "You can use the provided tools "
+            "to assist with various tasks and provide "
+            "accurate information"
+        ),
+        HumanMessage(
+            content="What is the forecast for the weather "
+            "in New York on December 5, 2024?"
+        ),
+    ]
+
+    response = llm_with_tools.invoke(chat_messages)
+    chat_messages.append(response)
+    assert response.tool_calls is not None  # type: ignore[attr-defined]
+    tool_call = response.tool_calls[0]  # type: ignore[attr-defined]
+    assert tool_call["name"] == "get_weather"
+
+    weather = get_weather.invoke(  # type: ignore[attr-defined]
+        {"location": tool_call["args"]["location"], "date": tool_call["args"]["date"]}
+    )
+    chat_messages.append(ToolMessage(content=weather, tool_call_id=tool_call["id"]))
+    llm_answer = llm_with_tools.invoke(chat_messages)
+    content = llm_answer.content.lower()  # type: ignore[union-attr]
+    assert "new york" in content and "25" in content and "celsius" in content
