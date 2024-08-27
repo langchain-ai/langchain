8c066dfdb4c6499e47eceb21e34f8ffa1dfe6960
Paz Shalev <pazs@ai21.com>
2024-08-27 13:57:45 +0300

    fix: fix xfail test names

--- libs/partners/ai21/tests/integration_tests/test_standard.py
+++ libs/partners/ai21/tests/integration_tests/test_standard.py
@@ -78,12 +78,12 @@ class TestAI21Jamba1_5(BaseTestAI21):
 
     @pytest.mark.xfail(reason="Prompt doesn't generate tool calls for Jamba 1.5.")
     def test_tool_calling_with_no_arguments(self, model: BaseChatModel) -> None:
-        super().test_tool_calling(model)
+        super().test_tool_calling_with_no_arguments(model)
 
     @pytest.mark.xfail(reason="Requires tool calling & stream - still WIP")
     def test_structured_output(self, model: BaseChatModel) -> None:
-        super().test_tool_calling(model)
+        super().test_structured_output(model)
 
     @pytest.mark.xfail(reason="Requires tool calling & stream - still WIP")
     def test_structured_output_pydantic_2_v1(self, model: BaseChatModel) -> None:
-        super().test_tool_calling(model)
+        super().test_structured_output_pydantic_2_v1(model)
