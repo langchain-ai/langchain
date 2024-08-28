5ecfe230f0e49b1cc3dad8b7d58896ad0750df8e
Bagatur <baskaryan@gmail.com>
2024-08-28 01:11:07 -0700

    fmt

--- libs/community/tests/unit_tests/chat_models/test_deepinfra.py
+++ libs/community/tests/unit_tests/chat_models/test_deepinfra.py
@@ -2,10 +2,10 @@ from langchain_community.chat_models import ChatDeepInfra
 
 
 def test_deepinfra_model_name_param() -> None:
-    llm = ChatDeepInfra(model_name="foo")
+    llm = ChatDeepInfra(model_name="foo")  # type: ignore[call-arg]
     assert llm.model_name == "foo"
 
 
 def test_deepinfra_model_param() -> None:
-    llm = ChatDeepInfra(model="foo")  # type: ignore[call-arg]
+    llm = ChatDeepInfra(model="foo")
     assert llm.model_name == "foo"
