7c6c232d9eca5744d01c84aee24875eb6ccff551
Martin Tříska <martin.triska@gmail.com>
2024-08-29 17:11:55 +0200

    formatting: literally added a space before comment :-/

--- libs/community/tests/unit_tests/vectorstores/test_azure_search.py
+++ libs/community/tests/unit_tests/vectorstores/test_azure_search.py
@@ -201,7 +201,7 @@ def test_ids_used_correctly() -> None:
         def __init__(self) -> None:
             self.succeeded: bool = True
 
-    def mock_upload_documents(self, documents: List[object]) -> List[Response]: # type: ignore[no-untyped-def]
+    def mock_upload_documents(self, documents: List[object]) -> List[Response]:  # type: ignore[no-untyped-def]
         # assume all documents uploaded successfuly
         response = [Response() for _ in documents]
         return response
