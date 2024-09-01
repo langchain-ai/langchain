ec0d1c3f947fc1778aaee03d37dc49626b376a1b
keenborder786 <21110290@lums.edu.pk>
2024-09-02 01:34:12 +0500

    [tests]: Added test for cache_text_only

--- libs/core/tests/unit_tests/language_models/chat_models/test_cache.py
+++ libs/core/tests/unit_tests/language_models/chat_models/test_cache.py
@@ -402,3 +402,36 @@ def test_cleanup_serialized() -> None:
         "name": "CustomChat",
         "type": "constructor",
     }
+
+
+def test_cache_text_only() -> None:
+    """Test that the llm representation of a serializable chat model is correct."""
+        
+    global_cache = InMemoryCache()
+    local_cache = InMemoryCache()
+    try:
+        set_llm_cache(global_cache)
+        chat_model = FakeListChatModel(
+            cache=local_cache, responses=["hello", "goodbye"],
+            cache_text_only = True
+        )
+        assert chat_model.invoke("How are you?").content == "hello"
+        # If the cache works we should get the same response since
+        # the prompt is the same
+        assert chat_model.invoke("How are you?").content == "hello"
+        # The global cache should be empty
+        assert global_cache._cache == {}
+        # The local cache should be populated
+        assert len(local_cache._cache) == 1
+        llm_result = list(local_cache._cache.values())
+        chat_generation = llm_result[0][0]
+        assert isinstance(chat_generation, ChatGeneration)
+        assert chat_generation.message.content == "hello"
+        # Verify that another prompt will trigger the call to the model
+        assert chat_model.invoke("meow?").content == "goodbye"
+        # The global cache should be empty
+        assert global_cache._cache == {}
+        # The local cache should be populated
+        assert len(local_cache._cache) == 2
+    finally:
+        set_llm_cache(None)
