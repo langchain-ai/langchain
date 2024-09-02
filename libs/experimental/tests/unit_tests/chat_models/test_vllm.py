a62224bd2cb8a41b510322290f53da5edd3fa0d2
Bagatur <baskaryan@gmail.com>
2024-09-02 11:59:38 -0700

    fmt

--- libs/experimental/tests/unit_tests/chat_models/test_vllm.py
+++ libs/experimental/tests/unit_tests/chat_models/test_vllm.py
@@ -3,37 +3,38 @@
 from importlib import import_module
 
 import pytest
-from langchain_community.chat_models.vllm import ChatVLLMOpenAI
 from langchain_core.pydantic_v1 import BaseModel
 
+from langchain_experimental.chat_models.vllm import ChatVLLMStructured
 
-class TestChatVLLMOpenAI:
+
+class TestChatVLLMStructured:
     def test_import_class(self) -> None:
         """Test that the class can be imported."""
         module_name = "langchain_community.chat_models.vllm"
-        class_name = "ChatVLLMOpenAI"
+        class_name = "ChatVLLMStructured"
 
         module = import_module(module_name)
         assert hasattr(module, class_name)
 
     def test_default_openai_api_base(self) -> None:
-        chat = ChatVLLMOpenAI()
+        chat = ChatVLLMStructured()
         assert chat.openai_api_base == "http://localhost:8000/v1"
 
     def test_default_params(self) -> None:
-        chat = ChatVLLMOpenAI()
+        chat = ChatVLLMStructured()
         assert "extra_body" in chat._default_params
 
     def test_extra_body(self) -> None:
-        chat = ChatVLLMOpenAI(extra_body={"test": "test"})
+        chat = ChatVLLMStructured(extra_body={"test": "test"})
         assert chat.extra_body == {"test": "test"}
 
     def test_llm_type(self) -> None:
-        chat = ChatVLLMOpenAI()
+        chat = ChatVLLMStructured()
         assert chat._llm_type == "vllm-openai"
 
     def test_get_instructions(self) -> None:
-        chat = ChatVLLMOpenAI()
+        chat = ChatVLLMStructured()
         for mode in ["guided_json", "guided_regex", "guided_choice", "guided_grammar"]:
             assert isinstance(chat._get_instructions("schema", mode), str)
         with pytest.raises(ValueError):
@@ -43,7 +44,7 @@ class TestChatVLLMOpenAI:
         class TestSchema(BaseModel):
             test: str
 
-        chat = ChatVLLMOpenAI()
+        chat = ChatVLLMStructured()
         with pytest.raises(ValueError):
             chat.with_structured_output(schema=123)  # type: ignore[arg-type]
 
