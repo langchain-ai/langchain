bc3b02651c4b77e18be30a2e7cfb9252903c15c7
Bagatur <22008038+baskaryan@users.noreply.github.com>
2024-09-03 12:05:39 -0700

    standard-tests[patch]: test init from env vars (#25983)

--- libs/standard-tests/langchain_standard_tests/unit_tests/chat_models.py
+++ libs/standard-tests/langchain_standard_tests/unit_tests/chat_models.py
@@ -1,11 +1,12 @@
 """Unit tests for chat models."""
-
+import os
 from abc import abstractmethod
-from typing import Any, List, Literal, Optional, Type
+from typing import Any, List, Literal, Optional, Tuple, Type
+from unittest import mock
 
 import pytest
 from langchain_core.language_models import BaseChatModel
-from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_core.pydantic_v1 import BaseModel, Field, SecretStr
 from langchain_core.runnables import RunnableBinding
 from langchain_core.tools import tool
 
@@ -132,11 +133,29 @@ class ChatModelUnitTests(ChatModelTests):
         params["api_key"] = "test"
         return params
 
+    @property
+    def init_from_env_params(self) -> Tuple[dict, dict, dict]:
+        """Return env vars, init args, and expected instance attrs for initializing
+        from env vars."""
+        return {}, {}, {}
+
     def test_init(self) -> None:
         model = self.chat_model_class(
             **{**self.standard_chat_model_params, **self.chat_model_params}
         )
         assert model is not None
+
+    def test_init_from_env(self) -> None:
+        env_params, model_params, expected_attrs = self.init_from_env_params
+        if env_params:
+            with mock.patch.dict(os.environ, env_params):
+                model = self.chat_model_class(**model_params)
+            assert model is not None
+            for k, expected in expected_attrs.items():
+                actual = getattr(model, k)
+                if isinstance(actual, SecretStr):
+                    actual = actual.get_secret_value()
+                assert actual == expected
 
     def test_init_streaming(
         self,
