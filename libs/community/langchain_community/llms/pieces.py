e008fae4eb15f03e866938426e4801988747dc4d
Sambhav Dixit <94298612+sambhavnoobcoder@users.noreply.github.com>
2024-09-03 23:14:00 +0530

    Final Refactor
    - Final Refactor of the PR making some basic changes to it , in terms of style and formatting

--- libs/community/langchain_community/llms/pieces.py
+++ libs/community/langchain_community/llms/pieces.py
@@ -1,7 +1,3 @@
-# Accomplish the following: 
-# Implement method to pass model name from the list of supported models 
-# Implement a method to stream the response from Pieces OS
-
 from __future__ import annotations
 from typing import Any, List, Mapping, Optional, Iterator
 from langchain_core.callbacks import CallbackManagerForLLMRun
@@ -9,6 +5,7 @@ from langchain_core.language_models.llms import BaseLLM
 from langchain_core.outputs import GenerationChunk, LLMResult
 from pieces_os_client.wrapper import PiecesClient
 from pieces_os_client.models import ModelFoundationEnum
+
 
 class PiecesOSLLM(BaseLLM):
     """Pieces OS language model."""
@@ -23,24 +20,22 @@ class PiecesOSLLM(BaseLLM):
     @property
     def _identifying_params(self) -> Mapping[str, Any]:
         """Get the identifying parameters."""
-        return {
-            "model": self.model,
-        }
+        return {"model": self.model}
 
     def _call(
-            self,
-            prompt: str,
-            stop: Optional[List[str]] = None,
-            run_manager: Optional[CallbackManagerForLLMRun] = None,
-            **kwargs: Any,
-        ) -> str:
-            """Call the Pieces OS model."""
-            try:
-                response = self.client.copilot.ask_question(prompt)
-                return response.question.answers[0].text if response.question and response.question.answers else ""
-            except Exception as error:
-                print(f'Error asking question: {error}')
-                return 'Error asking question'
+        self,
+        prompt: str,
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+        **kwargs: Any,
+    ) -> str:
+        """Call the Pieces OS model."""
+        try:
+            response = self.client.copilot.ask_question(prompt)
+            return response.question.answers[0].text if response.question and response.question.answers else ""
+        except Exception as error:
+            print(f'Error asking question: {error}')
+            return 'Error asking question'
 
     def _generate(
         self,
@@ -57,22 +52,22 @@ class PiecesOSLLM(BaseLLM):
         return LLMResult(generations=generations)
 
     def stream(
-            self,
-            prompt: str,
-            stop: Optional[List[str]] = None,
-            run_manager: Optional[CallbackManagerForLLMRun] = None,
-            **kwargs: Any,
-        ) -> Iterator[GenerationChunk]:
-            """Stream the response from Pieces OS model."""
-            try:
-                for response in self.client.copilot.stream_question(prompt):
-                    if response.question:
-                        answers = response.question.answers.iterable
-                        for answer in answers:
-                            yield GenerationChunk(text=answer.text)
-            except Exception as error:
-                print(f'Error streaming question: {error}')
-                yield GenerationChunk(text='Error streaming question')
+        self,
+        prompt: str,
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+        **kwargs: Any,
+    ) -> Iterator[GenerationChunk]:
+        """Stream the response from Pieces OS model."""
+        try:
+            for response in self.client.copilot.stream_question(prompt):
+                if response.question:
+                    answers = response.question.answers.iterable
+                    for answer in answers:
+                        yield GenerationChunk(text=answer.text)
+        except Exception as error:
+            print(f'Error streaming question: {error}')
+            yield GenerationChunk(text='Error streaming question')
 
     def get_supported_models(self) -> List[str]:
         """Get the list of supported models."""
