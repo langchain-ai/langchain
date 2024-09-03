d4c18887b7ac26eff48e40d947eccb765941295b
Christophe Bornet <cbornet@hotmail.com>
2024-09-03 15:22:56 +0200

    Import extractors into graph_vectorstore

--- libs/community/langchain_community/graph_vectorstores/__init__.py
+++ libs/community/langchain_community/graph_vectorstores/__init__.py
@@ -50,7 +50,7 @@ create links between documents.
 
 .. code-block:: python
 
-    from langchain_community.graph_vectorstores.extractors import KeybertLinkExtractor
+    from langchain_community.graph_vectorstores import KeybertLinkExtractor
     from langchain_community.graph_vectorstores.links import add_links
 
     extractor = KeybertLinkExtractor()
@@ -148,6 +148,31 @@ from langchain_community.graph_vectorstores.base import (
     Node,
 )
 from langchain_community.graph_vectorstores.cassandra import CassandraGraphVectorStore
+from langchain_community.graph_vectorstores.extractors.gliner_link_extractor import (
+    GLiNERInput,
+    GLiNERLinkExtractor,
+)
+from langchain_community.graph_vectorstores.extractors.hierarchy_link_extractor import (
+    HierarchyInput,
+    HierarchyLinkExtractor,
+)
+from langchain_community.graph_vectorstores.extractors.html_link_extractor import (
+    HtmlInput,
+    HtmlLinkExtractor,
+)
+from langchain_community.graph_vectorstores.extractors.keybert_link_extractor import (
+    KeybertInput,
+    KeybertLinkExtractor,
+)
+from langchain_community.graph_vectorstores.extractors.link_extractor import (
+    LinkExtractor,
+)
+from langchain_community.graph_vectorstores.extractors.link_extractor_adapter import (
+    LinkExtractorAdapter,
+)
+from langchain_community.graph_vectorstores.extractors.link_extractor_transformer import (  # noqa: E501
+    LinkExtractorTransformer,
+)
 from langchain_community.graph_vectorstores.links import (
     Link,
 )
@@ -158,4 +183,17 @@ __all__ = [
     "Node",
     "Link",
     "CassandraGraphVectorStore",
+    "GLiNERInput",
+    "GLiNERLinkExtractor",
+    "HierarchyInput",
+    "HierarchyLinkExtractor",
+    "HtmlInput",
+    "HtmlLinkExtractor",
+    "KeybertInput",
+    "KeybertLinkExtractor",
+    "LinkExtractor",
+    "LinkExtractor",
+    "LinkExtractorAdapter",
+    "LinkExtractorAdapter",
+    "LinkExtractorTransformer",
 ]
