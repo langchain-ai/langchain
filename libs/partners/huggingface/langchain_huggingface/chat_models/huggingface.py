8ad73d441a5b360190c39f4ffc0a58551e7e3ffb
Chester Curme <chester.curme@gmail.com>
2024-09-04 14:22:22 -0400

    update docstring

--- libs/partners/huggingface/langchain_huggingface/chat_models/huggingface.py
+++ libs/partners/huggingface/langchain_huggingface/chat_models/huggingface.py
@@ -265,7 +265,7 @@ class ChatHuggingFace(BaseChatModel):
     Tool calling:
         .. code-block:: python
 
-            from langchain_core.pydantic_v1 import BaseModel, Field
+            from pydantic import BaseModel, Field
 
             class GetWeather(BaseModel):
                 '''Get the current weather in a given location'''
