5199555c0d0d20ef2bb35ced28f77ad941d552c8
Bagatur <22008038+baskaryan@users.noreply.github.com>
2024-09-03 23:35:26 -0700

    Update libs/partners/openai/langchain_openai/chat_models/base.py

--- libs/partners/openai/langchain_openai/chat_models/base.py
+++ libs/partners/openai/langchain_openai/chat_models/base.py
@@ -476,7 +476,7 @@ class BaseChatOpenAI(BaseChatModel):
                 "'http_client'/'http_async_client' is already specified. Received:\n"
                 f"{openai_proxy=}\n{http_client=}\n{http_async_client=}"
             )
-        if not (self.client or None):
+        if not self.client:
             if self.openai_proxy and not self.http_client:
                 try:
                     import httpx
