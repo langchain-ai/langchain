4c7afb0d6c68b22963055d5fc85d09ec84b758cd
Bagatur <22008038+baskaryan@users.noreply.github.com>
2024-09-03 23:36:19 -0700

    Update libs/partners/openai/langchain_openai/llms/base.py

--- libs/partners/openai/langchain_openai/llms/base.py
+++ libs/partners/openai/langchain_openai/llms/base.py
@@ -190,7 +190,7 @@ class BaseOpenAI(BaseLLM):
         if not self.client:
             sync_specific = {"http_client": self.http_client}
             self.client = openai.OpenAI(**client_params, **sync_specific).completions  # type: ignore[arg-type]
-        if not (self.async_client or None):
+        if not self.async_client:
             async_specific = {"http_client": self.http_async_client}
             self.async_client = openai.AsyncOpenAI(
                 **client_params,
