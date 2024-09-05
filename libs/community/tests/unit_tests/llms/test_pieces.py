97361f56dcb92beb6fd4fec0e986ebd2be43807a
Sambhav Dixit <94298612+sambhavnoobcoder@users.noreply.github.com>
2024-09-05 16:22:49 +0530

    Add unit test setup for PiecesOSLLM class
    - Introduced a `TestPiecesOSLLM` class inheriting from `unittest.TestCase`.
    - Implemented `setUp` method to initialize mocks for `PiecesClient` and `copilot`.
    - Added a list of mock available model names to `mock_client`.
    - Instantiated the `PiecesOSLLM` class with the mock client.

--- libs/community/tests/unit_tests/llms/test_pieces.py
+++ libs/community/tests/unit_tests/llms/test_pieces.py
@@ -6,9 +6,23 @@ from typing import Any, List, Mapping, Optional, Iterator
 from langchain_core.callbacks import CallbackManagerForLLMRun
 from langchain_core.language_models.llms import BaseLLM
 from langchain_core.outputs import GenerationChunk, LLMResult
+from langchain_community.llms.pieces import PiecesOSLLM  
 from pieces_copilot_sdk import PiecesClient
 
+class TestPiecesOSLLM(unittest.TestCase):
 
+    def setUp(self):
+        self.mock_copilot = Mock()
+        self.mock_client = Mock(spec=PiecesClient)
+        self.mock_client.copilot = self.mock_copilot
+        self.mock_client.available_models_names = [
+            '(Gemini) Chat Model', '(PaLM2) Chat Model', 'GPT-3.5-turbo-16k Chat Model',
+            'Claude 3 Sonnet Chat Model', 'Gemini-1.5 Flash Chat Model', 'Claude 3.5 Sonnet Chat Model',
+            'GPT-3.5-turbo Chat Model', 'GPT-4 Turbo Chat Model', 'Codey (PaLM2) Chat Model',
+            'Claude 3 Opus Chat Model', 'GPT-4 Chat Model', 'Gemini-1.5 Pro Chat Model',
+            'Claude 3 Haiku Chat Model', 'GPT-4o Mini Chat Model', 'GPT-4o Chat Model'
+        ]
+        self.llm = PiecesOSLLM(client=self.mock_client)
 def mock_function_name(args):
     # Define the mock behavior for the function being mocked
     pass
