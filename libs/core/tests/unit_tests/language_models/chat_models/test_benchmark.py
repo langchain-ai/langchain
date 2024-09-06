519265f2fe6c1484bf468a321a561c1aa6b4b105
Eugene Yurtsev <eyurtsev@gmail.com>
2024-09-06 17:50:19 -0400

    x

--- libs/core/tests/unit_tests/language_models/chat_models/test_benchmark.py
+++ libs/core/tests/unit_tests/language_models/chat_models/test_benchmark.py
@@ -1,23 +1,21 @@
 import time
-from typing import Optional as Optional
+from itertools import cycle
 
-from langchain_core.caches import InMemoryCache
 from langchain_core.language_models import GenericFakeChatModel
-from langchain_core.rate_limiters import InMemoryRateLimiter
 
 
-async def test_rate_limit_ainvoke() -> None:
+def test_benchamrk_model() -> None:
     """Add rate limiter."""
     tic = time.time()
 
     model = GenericFakeChatModel(
-        messages=iter(["hello", "world", "!"]),
+        messages=cycle(["hello", "world", "!"]),
     )
 
     for _ in range(1_000):
-        await model.ainvoke("foo")
+        model.invoke("foo")
     toc = time.time()
 
     # Verify that the time taken to run the loop is less than 0.5 seconds
 
-    assert (toc - tic) < 0.5
+    assert (toc - tic) < 0.6
