6a5fe1b59c77d06475f6969b27461e77341c4adc
Bagatur <baskaryan@gmail.com>
2024-09-07 15:14:27 -0400

    standard-tests[patch]: resolve pydantic warnings

--- libs/standard-tests/langchain_standard_tests/unit_tests/chat_models.py
+++ libs/standard-tests/langchain_standard_tests/unit_tests/chat_models.py
@@ -79,7 +79,8 @@ def my_adder(a: int, b: int) -> int:
 class ChatModelTests(BaseStandardTests):
     @property
     @abstractmethod
-    def chat_model_class(self) -> Type[BaseChatModel]: ...
+    def chat_model_class(self) -> Type[BaseChatModel]:
+        ...
 
     @property
     def chat_model_params(self) -> dict:
@@ -187,7 +188,12 @@ class ChatModelUnitTests(ChatModelTests):
         tools = [my_adder_tool, my_adder]
 
         for pydantic_model in TEST_PYDANTIC_MODELS:
-            tools.extend([pydantic_model, pydantic_model.schema()])
+            model_schema = (
+                pydantic_model.model_json_schema()
+                if hasattr(pydantic_model, "model_json_schema")
+                else pydantic_model.schema()
+            )
+            tools.extend([pydantic_model, model_schema])
 
         # Doing a mypy ignore here since some of the tools are from pydantic
         # BaseModel 2 which isn't typed properly yet. This will need to be fixed
