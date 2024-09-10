e2c8690e7fe1a96eb0d49fa443de5b451d8c5cd4
ccurme <chester.curme@gmail.com>
2024-09-10 18:15:39 -0400

    community[patch]: fix huggingface pydantic 2 init (#26286)

--- libs/community/langchain_community/embeddings/huggingface_hub.py
+++ libs/community/langchain_community/embeddings/huggingface_hub.py
@@ -35,8 +35,8 @@ class HuggingFaceHubEmbeddings(BaseModel, Embeddings):
             )
     """
 
-    client: Any  #: :meta private:
-    async_client: Any  #: :meta private:
+    client: Any = None  #: :meta private:
+    async_client: Any = None  #: :meta private:
     model: Optional[str] = None
     """Model name to use."""
     repo_id: Optional[str] = None
