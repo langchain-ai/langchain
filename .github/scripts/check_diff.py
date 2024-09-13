71cc6973349e82d9340717ac2edae21f540bad64
Bagatur <baskaryan@gmail.com>
2024-09-12 18:19:24 -0700

    fix

--- .github/scripts/check_diff.py
+++ .github/scripts/check_diff.py
@@ -106,44 +106,7 @@ def add_dependents(dirs_to_eval: Set[str], dependents: dict) -> List[str]:
 
 def _get_configs_for_single_dir(job: str, dir_: str) -> List[Dict[str, str]]:
     if job == "test-pydantic":
-        with open("./libs/core/poetry.lock", "rb") as f:
-            core_poetry_lock_data = tomllib.load(f)
-        for package in core_poetry_lock_data["package"]:
-            if package["name"] == "pydantic":
-                core_max_pydantic_minor = package["version"].split(".")[1]
-                break
-
-        with open(f"./{dir_}/poetry.lock", "rb") as f:
-            dir_poetry_lock_data = tomllib.load(f)
-
-        for package in dir_poetry_lock_data["package"]:
-            if package["name"] == "pydantic":
-                dir_max_pydantic_minor = package["version"].split(".")[1]
-                break
-
-        core_min_pydantic_minor = get_min_version_from_toml(
-            "./libs/core/pyproject.toml", "release", include=["pydantic"]
-        )["pydantic"].split(".")[1]
-        dir_min_pydantic_minor = (
-            get_min_version_from_toml(
-                f"./{dir_}/pyproject.toml", "release", include=["pydantic"]
-            )
-            .get("pydantic", "0.0.0")
-            .split(".")[1]
-        )
-
-        max_pydantic_minor = min(
-            int(dir_max_pydantic_minor), int(core_max_pydantic_minor)
-        )
-        min_pydantic_minor = max(
-            int(dir_min_pydantic_minor), int(core_min_pydantic_minor)
-        )
-
-        configs = [
-            {"working-directory": dir_, "pydantic-version": f"2.{v}.0"}
-            for v in range(min_pydantic_minor, max_pydantic_minor + 1)
-        ]
-        return configs
+        return _get_pydantic_test_configs(dir_)
 
     if dir_ == "libs/core":
         py_versions = ["3.9", "3.10", "3.11", "3.12"]
@@ -165,6 +128,52 @@ def _get_configs_for_single_dir(job: str, dir_: str) -> List[Dict[str, str]]:
         py_versions = ["3.9", "3.12"]
 
     return [{"working-directory": dir_, "python-version": py_v} for py_v in py_versions]
+
+
+def _get_pydantic_test_configs(dir_: str, *, python_version: str = "3.11") -> List[Dict[str, str]]:
+    with open("./libs/core/poetry.lock", "rb") as f:
+        core_poetry_lock_data = tomllib.load(f)
+    for package in core_poetry_lock_data["package"]:
+        if package["name"] == "pydantic":
+            core_max_pydantic_minor = package["version"].split(".")[1]
+            break
+
+    with open(f"./{dir_}/poetry.lock", "rb") as f:
+        dir_poetry_lock_data = tomllib.load(f)
+
+    for package in dir_poetry_lock_data["package"]:
+        if package["name"] == "pydantic":
+            dir_max_pydantic_minor = package["version"].split(".")[1]
+            break
+
+    core_min_pydantic_minor = get_min_version_from_toml(
+        "./libs/core/pyproject.toml", "release", python_version, include=["pydantic"]
+    )["pydantic"].split(".")[1]
+    dir_min_pydantic_minor = (
+        get_min_version_from_toml(
+            f"./{dir_}/pyproject.toml", "release", python_version, include=["pydantic"]
+        )
+        .get("pydantic", "0.0.0")
+        .split(".")[1]
+    )
+
+    custom_mins = {
+        # depends on pydantic-settings 2.4 which requires pydantic 2.7
+        "libs/community": "7",
+    }
+
+    max_pydantic_minor = min(
+        int(dir_max_pydantic_minor), int(core_max_pydantic_minor),
+    )
+    min_pydantic_minor = max(
+        int(dir_min_pydantic_minor), int(core_min_pydantic_minor), *custom_mins.get(dir_, [])
+    )
+
+    configs = [
+        {"working-directory": dir_, "pydantic-version": f"2.{v}.0", "python-version": python_version}
+        for v in range(min_pydantic_minor, max_pydantic_minor + 1)
+    ]
+    return configs
 
 
 def _get_configs_for_multi_dirs(
--- .github/scripts/get_min_versions.py
+++ .github/scripts/get_min_versions.py
@@ -8,6 +8,9 @@ else:
     import tomli as tomllib
 
 from packaging.version import parse as parse_version
+from packaging.specifiers import SpecifierSet
+from packaging.version import Version
+
 import re
 
 MIN_VERSION_LIBS = [
@@ -48,7 +51,7 @@ def get_min_version(version: str) -> str:
 
 
 def get_min_version_from_toml(
-    toml_path: str, versions_for: str, *, include: Optional[list] = None
+    toml_path: str, versions_for: str, python_version: str, *, include: Optional[list] = None
 ):
     # Parse the TOML file
     with open(toml_path, "rb") as file:
@@ -75,6 +78,8 @@ def get_min_version_from_toml(
 
             if isinstance(version_string, dict):
                 version_string = version_string["version"]
+            if isinstance(version_string, list):
+                version_string = [vs for vs in version_string if check_python_version(python_version, version_string["python"])][0]["version"]
 
             # Use parse_version to get the minimum supported version from version_string
             min_version = get_min_version(version_string)
@@ -84,14 +89,31 @@ def get_min_version_from_toml(
 
     return min_versions
 
+def check_python_version(version_string, constraint_string):
+    """
+    Check if the given Python version matches the given constraints.
+
+    :param version_string: A string representing the Python version (e.g. "3.8.5").
+    :param constraint_string: A string representing the package's Python version constraints (e.g. ">=3.6, <4.0").
+    :return: True if the version matches the constraints, False otherwise.
+    """
+    try:
+        version = Version(version_string)
+        constraints = SpecifierSet(constraint_string)
+        return version in constraints
+    except Exception as e:
+        print(f"Error: {e}")
+        return False
+
 
 if __name__ == "__main__":
     # Get the TOML file path from the command line argument
     toml_file = sys.argv[1]
     versions_for = sys.argv[2]
+    python_version = sys.argv[3]
     assert versions_for in ["release", "pull_request"]
 
     # Call the function to get the minimum versions
-    min_versions = get_min_version_from_toml(toml_file, versions_for)
+    min_versions = get_min_version_from_toml(toml_file, versions_for, python_version)
 
     print(" ".join([f"{lib}=={version}" for lib, version in min_versions.items()]))
