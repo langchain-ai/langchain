aff94c91c5f1d11d2e3133fee979440faa3a2e15
Bagatur <baskaryan@gmail.com>
2024-09-13 11:34:32 -0700

    fix

--- libs/partners/openai/tests/unit_tests/chat_models/test_base.py
+++ libs/partners/openai/tests/unit_tests/chat_models/test_base.py
@@ -18,6 +18,7 @@ from langchain_core.messages import (
 )
 from langchain_core.messages.ai import UsageMetadata
 from langchain_core.pydantic_v1 import BaseModel
+from pydantic import BaseModel as BaseModelV2
 
 from langchain_openai import ChatOpenAI
 from langchain_openai.chat_models.base import (
@@ -696,18 +697,29 @@ def test_get_num_tokens_from_messages() -> None:
     assert expected == actual
 
 
-def test_schema_from_with_structured_output() -> None:
+class Foo(BaseModel):
+    bar: int
+
+
+class FooV2(BaseModelV2):
+    bar: int
+
+
+@pytest.mark.parametrize("schema", [Foo, FooV2])
+def test_schema_from_with_structured_output(schema: Type) -> None:
     """Test schema from with_structured_output."""
 
-    class Foo(BaseModel):
-        bar: Any
+    llm = ChatOpenAI()
 
-    model_chat = ChatOpenAI(api_key="foo")
+    structured_llm = llm.with_structured_output(
+        schema, method="json_schema", strict=True
+    )
 
-    model = model_chat.with_structured_output(Foo, method="json_schema", strict=True)
-
-    assert model.get_output_schema().schema() == {
-        "properties": {"bar": {"title": "Bar"}},
-        "title": "Foo",
+    expected = {
+        "properties": {"bar": {"title": "Bar", "type": "integer"}},
+        "required": ["bar"],
+        "title": schema.__name__,
         "type": "object",
     }
+    actual = structured_llm.get_output_schema().schema()
+    assert actual == expected
