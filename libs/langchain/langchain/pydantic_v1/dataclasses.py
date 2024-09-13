1bf9a04f268cbd72cf97f9dfed8f972237ba2d4a
Eugene Yurtsev <eyurtsev@gmail.com>
2024-09-13 12:50:45 -0400

    x

--- libs/langchain/langchain/pydantic_v1/dataclasses.py
+++ libs/langchain/langchain/pydantic_v1/dataclasses.py
@@ -1,24 +1,14 @@
-from langchain_core._api import warn_deprecated
-
-try:
-    from pydantic.v1.dataclasses import *  # noqa: F403
-except ImportError:
-    from pydantic.dataclasses import *  # type: ignore # noqa: F403
-
-warn_deprecated(
-    "0.3.0",
-    removal="1.0.0",
-    alternative="pydantic.v1 or pydantic",
-    message=(
-        "As of langchain-core 0.3.0, LangChain uses pydantic v2 internally. "
-        "The langchain.pydantic_v1 module was a "
-        "compatibility shim for pydantic v1, and should no longer be used. "
-        "Please update the code to import from Pydantic directly.\n\n"
+def _raise_import_error() -> None:
+    """Raise ImportError with a helpful message."""
+    raise ImportError(
+        "Please do not import from langchain.pydantic_v1.*."
+        "This module was a compatibility shim for pydantic v1, and should "
+        "no longer be used "
+        "Please update the code to import from Pydantic directly. "
         "For example, replace imports like: "
         "`from langchain.pydantic_v1 import BaseModel`\n"
         "with: `from pydantic import BaseModel`\n"
-        "or the v1 compatibility namespace if you are working in a code base "
-        "that has not been fully upgraded to pydantic 2 yet. "
-        "\tfrom pydantic.v1 import BaseModel\n"
-    ),
-)
+    )
+
+
+_raise_import_error()
