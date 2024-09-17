a6b12d2a41b7bad478fe5d0deaa9c0243d5a0b7a
V.Prasanna kumar <v.prasannakumar1998@gmail.com>
2024-09-17 09:52:00 +0530

    added relevant import

--- libs/community/langchain_community/chat_message_histories/dynamodb.py
+++ libs/community/langchain_community/chat_message_histories/dynamodb.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import logging
 from typing import TYPE_CHECKING, Dict, List, Optional
+from decimal import Decimal
 
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.messages import (
