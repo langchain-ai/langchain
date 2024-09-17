c528292da57b86ab964f431f426d7aab009bf934
ZeevRispler <zeev_rispler@mckinsey.com>
2024-09-17 10:57:48 +0300

    mlrun llm contribution

---
+++ libs/community/langchain_community/llms/mlrun.py
@@ -0,0 +1,170 @@
+from collections.abc import AsyncIterator, Iterator
+from typing import Any, List, Optional, Union
+
+import mlrun
+from langchain_core.language_models.base import LanguageModelInput
+from langchain_core.language_models.llms import LLM
+from langchain_core.pydantic_v1 import Field
+from langchain_core.runnables import RunnableConfig
+
+
+class Mlrun(LLM):
+    """A class for interacting with Langchain, while using models served by mlrun."""
+
+    llm: Any = Field(default=None)
+    name: str = Field(default=None)
+
+    def __init__(
+        self,
+        nuclio_function: Any,
+        name: str,
+        **kwargs: dict,
+    ):
+        super().__init__(**kwargs)
+        self.llm = nuclio_function
+        self.name = name
+
+    @property
+    def _llm_type(self) -> str:
+        """Get the type of language model used by this chat model.
+        Used for logging purposes only."""
+        return "Mlrun"
+
+    def _call(
+        self,
+        prompt: str,
+        **generate_kwargs: dict,
+    ) -> str:
+        pass
+
+    def invoke(
+        self,
+        input: LanguageModelInput,
+        config: Optional[RunnableConfig] = None,
+        *,
+        stop: Optional[List[str]] = None,
+        **kwargs: Any,
+    ) -> str:
+        """
+        call the llm invoke function, routed through mlrun model server.
+        """
+        # Convert the input to the correct format
+        input = self._convert_input(input)
+        # Check if the model server is a mock server or a real server
+        # and call the correct function
+        if isinstance(self.llm, mlrun.serving.server.GraphServer):
+            response = self.llm.test(
+                path=f"/v2/models/{self.name}/predict",
+                body={
+                    "inputs": [input],
+                    "stop": stop,
+                    "config": config,
+                    "usage": "invoke",
+                    **kwargs,
+                },
+            )
+            return response if type(response) is str else response["outputs"]
+        else:
+            response = self.llm.invoke(
+                path=f"/v2/models/{self.name}/predict",
+                body={
+                    "inputs": [input],
+                    "stop": stop,
+                    "config": config,
+                    "usage": "invoke",
+                    **kwargs,
+                },
+            )
+            return response if type(response) is str else response["outputs"]
+
+    async def ainvoke(
+        self,
+        input: LanguageModelInput,
+        config: Optional[RunnableConfig] = None,
+        *,
+        stop: Optional[List[str]] = None,
+        **kwargs: Any,
+    ) -> str:
+        """
+        call the llm ainvoke function, routed through mlrun model server.
+        """
+        raise NotImplementedError("ainvoke is not supported by mlrun yet.")
+
+    def batch(
+        self,
+        inputs: List[LanguageModelInput],
+        config: Optional[Union[RunnableConfig, List[RunnableConfig]]] = None,
+        *,
+        return_exceptions: bool = False,
+        **kwargs: Any,
+    ) -> List[str]:
+        """
+        call the llm batch function, routed through mlrun model server.
+        """
+        # Convert all inputs to the correct format and list them
+        inputs = [self._convert_input(input) for input in inputs]
+        # Check if the model server is a mock server or a real server
+        # and call the correct function
+        if isinstance(self.llm, mlrun.serving.server.GraphServer):
+            response = self.llm.test(
+                path=f"/v2/models/{self.name}/predict",
+                body={
+                    "inputs": inputs,
+                    "config": config,
+                    "return_exceptions": return_exceptions,
+                    "usage": "batch",
+                    **kwargs,
+                },
+            )
+            return response
+        else:
+            response = self.llm.invoke(
+                path=f"/v2/models/{self.name}/predict",
+                body={
+                    "inputs": inputs,
+                    "config": config,
+                    "return_exceptions": return_exceptions,
+                    "usage": "batch",
+                    **kwargs,
+                },
+            )
+            return response
+
+    async def abatch(
+        self,
+        inputs: List[LanguageModelInput],
+        config: Optional[Union[RunnableConfig, List[RunnableConfig]]] = None,
+        *,
+        return_exceptions: bool = False,
+        **kwargs: Any,
+    ) -> List[str]:
+        """
+        call the llm abatch function, routed through mlrun model server.
+        """
+        raise NotImplementedError("abatch is not supported by mlrun yet.")
+
+    def stream(
+        self,
+        input: LanguageModelInput,
+        config: Optional[RunnableConfig] = None,
+        *,
+        stop: Optional[List[str]] = None,
+        **kwargs: Any,
+    ) -> Iterator[str]:
+        """
+        call the llm stream function, routed through mlrun model server.
+        """
+        raise NotImplementedError("Stream is not supported by mlrun yet.")
+
+    def astream(
+        self,
+        input: LanguageModelInput,
+        config: Optional[RunnableConfig] = None,
+        *,
+        stop: Optional[List[str]] = None,
+        **kwargs: Any,
+    ) -> AsyncIterator[str]:
+        """
+        call the llm astream function, routed through mlrun model server.
+        """
+        raise NotImplementedError("Stream is not supported by mlrun yet.")
---
+++ libs/community/tests/unit_tests/llms/test_mlrun.py
@@ -0,0 +1,272 @@
+import os
+import pathlib
+import subprocess
+from subprocess import PIPE, Popen
+from typing import Any, Dict, Union
+
+import mlrun
+import pytest
+from langchain_core.language_models.llms import LLM
+from libs.community.langchain_community.llms.mlrun import Mlrun
+from mlrun.serving.v2_serving import V2ModelServer
+
+import langchain_community.llms
+
+_HUGGINGFACE_MODEL = "Qwen/Qwen2-0.5B-Instruct"
+_OPENAI_MODEL = "gpt-3.5-turbo-instruct"
+_OLLAMA_MODEL = "qwen:0.5b"
+_OLLAMA_DELETE_MODEL_POST_TEST = False
+
+langchain_model_server_path = str(
+    pathlib.Path(__file__).parent.parent.parent.parent.parent
+    / "mlrun/mlrun/frameworks/langchain/model_server.py"
+)
+
+
+# Until the full model server will be available in an official mlrun release
+class LangChainModelServer(V2ModelServer):
+    """
+    LangChain Model serving class, short testing version
+    """
+
+    def __init__(
+        self,
+        context: mlrun.MLClientCtx = None,
+        llm: Union[str, LLM] = None,
+        init_kwargs: Dict[str, Any] = None,
+        generation_kwargs: Dict[str, Any] = None,
+        name: str = "",
+        model_path: str = "",
+    ):
+        """
+        Initialize a serving class for general llm usage.
+        :param context:           The mlrun context to use.
+        :param llm:               The llm object itself in case of local usage or the
+                                    name of the llm.
+        :param init_kwargs:       The initialization arguments to use while initializing
+                                    the llm.
+        :param generation_kwargs: The generation arguments to use while generating text.
+        :param name:              The name of this server to be initialized.
+        :param model_path:        Not in use. When adding a model pass any string value
+        """
+        super().__init__(name=name, context=context, model_path=model_path)
+        self.llm = llm
+        self.init_kwargs = init_kwargs or {}
+        self.generation_kwargs = generation_kwargs or {}
+
+    def load(self):
+        """
+        loads the model.
+        """
+        # If the llm is a string (or not given, then we take default model),
+        # load the llm from langchain.
+        model_class = getattr(langchain_community.llms, self.llm)
+        if self.init_method:
+            self.model = getattr(model_class, self.init_method)(**self.init_kwargs)
+        else:
+            self.model = model_class(**self.init_kwargs)
+
+    def predict(
+        self,
+        request: Dict[str, Any],
+    ) -> str:
+        """
+        Predict the output of the model, can use the following usages
+        """
+        inputs = request.get("inputs", [])
+        usage = request.get("usage", "predict")
+        generation_kwargs = (
+            request.get("generation_kwargs", None) or self.generation_kwargs
+        )
+        # Both predict and invoke are the using the same method,
+        # invoke has more generation options to comply with langchain's invoke method.
+        if usage == "predict":
+            return self.model.invoke(input=inputs[0], config=generation_kwargs)
+        elif usage == "invoke":
+            config = request.get("config", None)
+            stop = request.get("stop", None)
+            answer = self.model.invoke(
+                input=inputs[0], config=config, stop=stop, **generation_kwargs
+            )
+            return answer
+        elif usage == "batch":
+            config = request.get("config", None)
+            return_exceptions = request.get("return_exceptions", None)
+            return self.model.batch(
+                inputs=inputs,
+                config=config,
+                return_exceptions=return_exceptions,
+                **generation_kwargs,
+            )
+
+
+@pytest.mark.requires("mlrun")
+def test_huggingface() -> None:
+    """
+    Test the langchain model server with a huggingface model
+    """
+    from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+
+    prompt = "How far is the moon"
+    question_len = len(prompt.split(" "))
+    max_tokens = 10
+    model_id = _HUGGINGFACE_MODEL
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    model = AutoModelForCausalLM.from_pretrained(model_id)
+    pipe = pipeline(
+        "text-generation", model=model, tokenizer=tokenizer, max_new_tokens=max_tokens
+    )
+
+    project = mlrun.get_or_create_project(
+        name="huggingface-model-server-example", context="./"
+    )
+    serving_func = project.set_function(
+        func=__file__,
+        name="huggingface-langchain-model-server",
+        kind="serving",
+        image="mlrun/mlrun",
+    )
+    serving_func.add_model(
+        "huggingface-langchain-model",
+        class_name="LangChainModelServer",
+        llm="HuggingFacePipeline",
+        init_kwargs={"pipeline": pipe},
+        model_path=".",
+    )
+    server = serving_func.to_mock_server()
+    llm = Mlrun(server, "huggingface-langchain-model")
+    invoke_result = llm.invoke("how old are you?")
+    assert invoke_result
+    invoke_result_params = llm.invoke(
+        "how old are you?",
+        stop=["<eos>"],
+        generation_kwargs={"return_full_text": False},
+    )
+    assert invoke_result_params
+    assert len(invoke_result_params.lstrip().split(" ")) <= max_tokens + question_len
+    batch_result = llm.batch(["how old are you?", "what is your name?"])
+    assert batch_result
+    batch_result_params = llm.batch(
+        ["how old are you?", "what is your name?"],
+        generation_kwargs={"return_full_text": False},
+    )
+    assert batch_result_params
+
+
+def skip_openai() -> bool:
+    """
+    Check if the OpenAI API credentials are set
+    """
+    return not os.environ.get("OPENAI_API_KEY") or not os.environ.get("OPENAI_BASE_URL")
+
+
+@pytest.mark.requires("mlrun")
+@pytest.mark.skipif(skip_openai(), reason="OpenAI API credentials not set")
+def test_openai() -> None:
+    """
+    Test the langchain model server with an openai model
+    """
+    prompt = "How far is the moon"
+    question_len = len(prompt.split(" "))
+    max_tokens = 10
+    project = mlrun.get_or_create_project(
+        name="openai-model-server-example", context="./"
+    )
+    serving_func = project.set_function(
+        func=__file__,
+        name="openai-langchain-model-server",
+        kind="serving",
+        image="mlrun/mlrun",
+    )
+    serving_func.add_model(
+        "openai-langchain-model",
+        llm="OpenAI",
+        class_name="LangChainModelServer",
+        init_kwargs={"model": _OPENAI_MODEL},
+        model_path=".",
+    )
+    server = serving_func.to_mock_server()
+    llm = Mlrun(server, "openai-langchain-model")
+    invoke_result = llm.invoke(prompt)
+    assert invoke_result
+    invoke_result_params = llm.invoke(
+        prompt, generation_kwargs={"max_tokens": max_tokens}
+    )
+    assert invoke_result_params
+    assert len(invoke_result_params.split(" ")) <= max_tokens + question_len
+    batch_result = llm.batch([prompt, "what is your name?"])
+    assert batch_result
+
+
+def ollama_check_skip() -> bool:
+    """
+    Check if ollama is installed
+    """
+    try:
+        result = subprocess.run(["ollama", "--help"], stdout=PIPE)
+    except Exception:
+        return True
+    return result.returncode != 0
+
+
+@pytest.fixture
+def ollama_fixture() -> None:
+    """
+    Do the setup and cleanup for the ollama test
+    """
+    # pull or make sure the model is available
+    subprocess.run(["ollama", "pull", _OLLAMA_MODEL], stdout=PIPE)
+
+    # start the ollama server
+    ollama_serve_process = Popen(["ollama", "serve"], stdout=PIPE)
+
+    yield
+
+    ollama_serve_process.kill()
+    # delete the model after the test if requested
+    global _OLLAMA_DELETE_MODEL_POST_TEST
+    if _OLLAMA_DELETE_MODEL_POST_TEST:
+        subprocess.run(["ollama", "rm", _OLLAMA_MODEL], stdout=PIPE)
+
+
+@pytest.mark.requires("mlrun")
+@pytest.mark.skipif(ollama_check_skip(), reason="Ollama not installed")
+def test_ollama(ollama_fixture: Any) -> None:
+    """
+    Test the langchain model server with an ollama model
+    """
+    prompt = "How far is the moon"
+    question_len = len(prompt.split(" "))
+    max_tokens = 10
+    project = mlrun.get_or_create_project(
+        name="ollama-model-server-example", context="./"
+    )
+    serving_func = project.set_function(
+        func=__file__,
+        name="ollama-langchain-model-server",
+        kind="serving",
+        image="mlrun/mlrun",
+    )
+    serving_func.add_model(
+        "ollama-langchain-model",
+        llm="Ollama",
+        class_name="LangChainModelServer",
+        init_kwargs={"model": _OLLAMA_MODEL},
+        model_path=".",
+    )
+    server = serving_func.to_mock_server()
+    llm = Mlrun(server, "ollama-langchain-model")
+    invoke_result = llm.invoke(prompt)
+    assert invoke_result
+    invoke_result_params = llm.invoke(
+        prompt, generation_kwargs={"num_predict": max_tokens}, stop=["<eos>"]
+    )
+    assert invoke_result_params
+    assert len(invoke_result_params.split(" ")) <= max_tokens + question_len
+    batch_result = llm.batch([prompt, "what is your name?"])
+    assert batch_result
+    batch_result_params = llm.batch(
+        [prompt, "what is your name?"],
+        generation_kwargs={"num_predict": max_tokens},
+    )
+    assert batch_result_params
