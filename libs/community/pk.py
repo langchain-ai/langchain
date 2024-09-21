9d00973f30d1d1be78c000fdd2bbcfb8a9187e5a
Paras Madan <parasmadan@Parass-MacBook-Pro.local>
2024-09-21 13:29:39 +0530

    Corrections

--- libs/community/pk.py
+++ libs/community/pk.py
@@ -1,11 +1,10 @@
 
 import os
-os.environ["UNIFY_API_KEY"] = "1a2Yi8+xTGIsQ8bwxgSUhOvztnIhLPgJALzg5Ys98lI="
+os.environ["UNIFY_API_KEY"] = ""
 
 from langchain_community.chat_models.unify import ChatUnify
 #from langchain_community.chat_models.unifyai import UnifyChat
 chat = ChatUnify(model="gpt-3.5-turbo@openai")
 print(chat.invoke("Hello! How are you?"))
 
-#print()
 
