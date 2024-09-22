fff06388c46c2deca13019b590621cb1a2d8b60e
V.Prasanna kumar <v.prasannakumar1998@gmail.com>
2024-09-22 21:50:38 +0530

    Lint issue resolved

--- libs/community/langchain_community/chat_message_histories/dynamodb.py
+++ libs/community/langchain_community/chat_message_histories/dynamodb.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
-
+from decimal import Decimal
 import logging
 from typing import TYPE_CHECKING, Dict, List, Optional
-from decimal import Decimal
 
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.messages import (
