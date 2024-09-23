2754786b354b13de7b5d508601afdbc4a086a815
Tibor Reiss <tibor.reiss@gmail.com>
2024-09-23 21:24:54 +0200

    Remove pandas dependency

--- libs/core/tests/unit_tests/load/test_serializable.py
+++ libs/core/tests/unit_tests/load/test_serializable.py
@@ -1,8 +1,21 @@
-import pandas as pd
 from pydantic import ConfigDict, Field
 
 from langchain_core.load import Serializable, dumpd, load
 from langchain_core.load.serializable import _is_field_useful
+
+
+class NonBoolObj:
+    def __bool__(self) -> bool:
+        raise ValueError("Truthiness can't be determined")
+
+    def __eq__(self, other: object) -> bool:
+        raise ValueError("Equality can't be determined")
+
+    def __str__(self) -> str:
+        return self.__class__.__name__
+
+    def __repr__(self) -> str:
+        return self.__class__.__name__
 
 
 def test_simple_serialization() -> None:
@@ -81,13 +94,6 @@ def test__is_field_useful() -> None:
 
         def __eq__(self, other: object) -> bool:
             return self  # type: ignore[return-value]
-
-    class NonBoolObj:
-        def __bool__(self) -> bool:
-            raise ValueError("Truthiness can't be determined")
-
-        def __eq__(self, other: object) -> bool:
-            raise ValueError("Equality can't be determined")
 
     default_x = ArrayObj()
     default_y = NonBoolObj()
@@ -173,7 +179,7 @@ class Foo3(Serializable):
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
     content: str
-    data_frame: pd.DataFrame
+    non_bool: NonBoolObj
 
     @classmethod
     def is_lc_serializable(cls) -> bool:
@@ -182,21 +188,15 @@ class Foo3(Serializable):
 
 def test_repr() -> None:
     foo = Foo3(
-        content="test repr",
-        data_frame=pd.DataFrame({"name": ["Alice", "Bob"], "age": [17, 19]}),
+        content="repr",
+        non_bool=NonBoolObj(),
     )
-    assert (
-        repr(foo) == "Foo3(content='test repr', data_frame=    "
-        "name  age\n0  Alice   17\n1    Bob   19)"
-    )
+    assert repr(foo) == "Foo3(content='repr', non_bool=NonBoolObj)"
 
 
 def test_str() -> None:
     foo = Foo3(
-        content="test repr",
-        data_frame=pd.DataFrame({"name": ["Alice", "Bob"], "age": [17, 19]}),
+        content="str",
+        non_bool=NonBoolObj(),
     )
-    assert (
-        str(foo) == "content='test repr' data_frame=    "
-        "name  age\n0  Alice   17\n1    Bob   19"
-    )
+    assert str(foo) == "content='str' non_bool=NonBoolObj"
