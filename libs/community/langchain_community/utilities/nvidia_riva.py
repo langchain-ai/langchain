7ebe4f5902b14d1a82fe9ca4744e3532c2f47547
Tibor Reiss <tibor.reiss@gmail.com>
2024-09-24 21:29:57 +0200

    Fix mypy error

--- libs/community/langchain_community/utilities/nvidia_riva.py
+++ libs/community/langchain_community/utilities/nvidia_riva.py
@@ -469,7 +469,7 @@ class RivaASR(
             ) from err
 
     def invoke(
-        self, input: ASRInputType, _: Optional[RunnableConfig] = None, **kwargs: Any
+        self, input: ASRInputType, config: Optional[RunnableConfig] = None, **kwargs: Any
     ) -> ASROutputType:
         """Transcribe the audio bytes into a string with Riva."""
         # create an output text generator with Riva
@@ -565,7 +565,7 @@ class RivaTTS(
             ) from err
 
     def invoke(
-        self, input: TTSInputType, _: Union[RunnableConfig, None] = None, **kwargs: Any
+        self, input: TTSInputType, config: Optional[RunnableConfig] = None, **kwargs: Any
     ) -> TTSOutputType:
         """Perform TTS by taking a string and outputting the entire audio file."""
         return b"".join(self.transform(iter([input])))
