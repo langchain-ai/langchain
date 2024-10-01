929e0e31f15ac9c1eafd605b662485f5494e8ccb
rafaelsideguide <150964962+rafaelsideguide@users.noreply.github.com>
2024-10-01 16:45:21 -0300

    fix: added adapters and warnings for breaking changes

--- libs/community/langchain_community/document_loaders/firecrawl.py
+++ libs/community/langchain_community/document_loaders/firecrawl.py
@@ -3,7 +3,7 @@ from typing import Iterator, Literal, Optional
 from langchain_core.document_loaders import BaseLoader
 from langchain_core.documents import Document
 from langchain_core.utils import get_from_env
-
+import warnings
 
 class FireCrawlLoader(BaseLoader):
     """
@@ -48,7 +48,6 @@ class FireCrawlLoader(BaseLoader):
              Join the waitlist to turn any web
             {'ogUrl': 'https://www.firecrawl.dev/', 'title': 'Home - Firecrawl', 'robots': 'follow, index', 'ogImage': 'https://www.firecrawl.dev/og.png?123', 'ogTitle': 'Firecrawl', 'sitemap': {'lastmod': '2024-08-12T00:28:16.681Z', 'changefreq': 'weekly'}, 'keywords': 'Firecrawl,Markdown,Data,Mendable,Langchain', 'sourceURL': 'https://www.firecrawl.dev/', 'ogSiteName': 'Firecrawl', 'description': 'Firecrawl crawls and converts any website into clean markdown.', 'ogDescription': 'Turn any website into LLM-ready data.', 'pageStatusCode': 200, 'ogLocaleAlternate': []}
 
-
     Async load:
         .. code-block:: python
 
@@ -63,6 +62,129 @@ class FireCrawlLoader(BaseLoader):
             {'ogUrl': 'https://www.firecrawl.dev/', 'title': 'Home - Firecrawl', 'robots': 'follow, index', 'ogImage': 'https://www.firecrawl.dev/og.png?123', 'ogTitle': 'Firecrawl', 'sitemap': {'lastmod': '2024-08-12T00:28:16.681Z', 'changefreq': 'weekly'}, 'keywords': 'Firecrawl,Markdown,Data,Mendable,Langchain', 'sourceURL': 'https://www.firecrawl.dev/', 'ogSiteName': 'Firecrawl', 'description': 'Firecrawl crawls and converts any website into clean markdown.', 'ogDescription': 'Turn any website into LLM-ready data.', 'pageStatusCode': 200, 'ogLocaleAlternate': []}
 
     """  # noqa: E501
+
+    def legacy_crawler_options_adapter(self, params):
+        use_legacy_options = False
+        legacy_keys = ['includes', 'excludes', 'allowBackwardCrawling', 'allowExternalContentLinks', 'pageOptions']
+        for key in legacy_keys:
+            if params.get(key):
+                use_legacy_options = True
+                break
+
+        if use_legacy_options:
+            warnings.warn("Some of the used parameters are going to be deprecated in the next versions. Please check the FireCrawl documentation about v1 for updated usage.", DeprecationWarning)
+            if 'includes' in params:
+                if params['includes'] is True:
+                    params['includePaths'] = params['includes']
+                del params['includes']
+
+            if 'excludes' in params:
+                if params['excludes'] is True:
+                    params['excludePaths'] = params['excludes']
+                del params['excludes']
+
+            if 'allowBackwardCrawling' in params:
+                if params['allowBackwardCrawling'] is True:
+                    params['allowBackwardLinks'] = params['allowBackwardCrawling']
+                del params['allowBackwardCrawling']
+
+            if 'allowExternalContentLinks' in params:
+                if params['allowExternalContentLinks'] is True:
+                    params['allowExternalLinks'] = params['allowExternalContentLinks']
+                del params['allowExternalContentLinks']
+
+            if 'pageOptions' in params:
+                if params['pageOptions'] is True:
+                    params['scrapeOptions'] = self.legacy_scrape_options_adapter(params['pageOptions'])
+                del params['pageOptions']
+        
+        return params
+
+    def legacy_scrape_options_adapter(self, params):
+        use_legacy_options = False
+        formats = ["markdown"]
+
+        if 'extractorOptions' in params:
+            if 'mode' in params['extractorOptions']:
+              if params['extractorOptions']['mode'] == 'llm-extraction' or \
+                params['extractorOptions']['mode'] == 'llm-extraction-from-raw-html' or \
+                params['extractorOptions']['mode'] == 'llm-extraction-from-markdown':
+
+                use_legacy_options = True
+                if 'extractionPrompt' in params['extractorOptions']:
+                    if params['extractorOptions']['extractionPrompt']:
+                        params['prompt'] = params['extractorOptions']['extractionPrompt']
+                    else:
+                        params['prompt'] = params['extractorOptions'].get('extractionPrompt', "Based on the information on the page, extract the information from the schema.")
+                        
+                if 'extractionSchema' in params['extractorOptions']:
+                    if params['extractorOptions']['extractionSchema']:
+                        params['schema'] = params['extractorOptions']['extractionSchema']
+
+                if 'userPrompt' in params['extractorOptions']:
+                    if params['extractorOptions']['userPrompt']:
+                        params['prompt'] = params['extractorOptions']['userPrompt']
+
+                del params['extractorOptions']
+
+        scrape_keys = ['includeMarkdown', 'includeHtml', 'includeRawHtml', 'includeExtract', 'includeLinks', 'screenshot', 'fullPageScreenshot', 'onlyIncludeTags', 'removeTags']
+        for key in scrape_keys:
+            if params.get(key):
+                use_legacy_options = True
+                break
+
+        if use_legacy_options:
+            warnings.warn("Some of the used parameters are going to be deprecated in the next versions. Please check the FireCrawl documentation about v1 for updated usage.", DeprecationWarning)
+            if 'includeMarkdown' in params:
+                if params['includeMarkdown'] is False:
+                    formats.remove("markdown")
+                del params['includeMarkdown']
+
+            if 'includeHtml' in params:
+                if params['includeHtml'] is True:
+                    formats.append("html")
+                del params['includeHtml']
+
+            if 'includeRawHtml' in params:
+                if params['includeRawHtml'] is True:
+                    formats.append("rawHtml")
+                del params['includeRawHtml']
+
+            if 'includeExtract' in params:
+                if params['includeExtract'] is True:
+                    formats.append("extract")
+                del params['includeExtract']
+
+            if 'includeLinks' in params:
+                if params['includeLinks'] is True:
+                    formats.append("links")
+                del params['includeLinks']
+
+            if 'screenshot' in params:
+                if params['screenshot'] is True:
+                    formats.append("screenshot")
+                del params['screenshot']
+
+            if 'fullPageScreenshot' in params:
+                if params['fullPageScreenshot'] is True:
+                    formats.append("screenshot@fullPage")
+                del params['fullPageScreenshot']
+
+            if 'onlyIncludeTags' in params:
+                if params['onlyIncludeTags'] is True:
+                    params['includeTags'] = params['onlyIncludeTags']
+                del params['onlyIncludeTags']
+
+            if 'removeTags' in params:
+                if params['removeTags'] is True:
+                    params['excludeTags'] = params['removeTags']
+                del params['removeTags']
+
+        if 'formats' not in params:
+            params['formats'] = formats
+        print(params)
+        
+        return params
 
     def __init__(
         self,
@@ -110,11 +232,11 @@ class FireCrawlLoader(BaseLoader):
 
     def lazy_load(self) -> Iterator[Document]:
         if self.mode == "scrape":
-            firecrawl_docs = [self.firecrawl.scrape_url(self.url, params=self.params)]
+            firecrawl_docs = [self.firecrawl.scrape_url(self.url, params=self.legacy_scrape_options_adapter(self.params))]
         elif self.mode == "crawl":
             if not self.url:
                 raise ValueError("URL is required for crawl mode")
-            crawl_response = self.firecrawl.crawl_url(self.url, params=self.params)
+            crawl_response = self.firecrawl.crawl_url(self.url, params=self.legacy_crawler_options_adapter(self.params))
             firecrawl_docs = crawl_response.get("data", [])
         elif self.mode == "map":
             if not self.url:
