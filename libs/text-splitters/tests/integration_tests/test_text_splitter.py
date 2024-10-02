6888d300a08e1cac9bafa64b680ac7d8916ad204
keenborder786 <21110290@lums.edu.pk>
2024-10-02 05:12:41 +0500

    [chore]: Linting issue in tests

--- libs/text-splitters/tests/integration_tests/test_text_splitter.py
+++ libs/text-splitters/tests/integration_tests/test_text_splitter.py
@@ -31,15 +31,15 @@ def test_huggingface_tokenizer() -> None:
     from transformers import AutoTokenizer
 
     tokenizer_2 = AutoTokenizer.from_pretrained("cmarkea/bloomz-3b-retriever-v2")
-    splitter = TokenTextSplitter(tokenizer=tokenizer_2, chunk_size=1, chunk_overlap=0)
-    output = splitter.split_text("foo bar")
+    splitter_2 = TokenTextSplitter(tokenizer=tokenizer_2, chunk_size=1, chunk_overlap=0)
+    output = splitter_2.split_text("foo bar")
     assert output == ["<s>", "foo", " bar", "</s>"]
 
     tokenizer_3 = AutoTokenizer.from_pretrained("cmarkea/bloomz-3b-retriever-v2")
-    splitter = TokenTextSplitter.from_huggingface_tokenizer(
+    splitter_3 = TokenTextSplitter.from_huggingface_tokenizer(
         tokenizer_3, chunk_size=1, chunk_overlap=0
     )
-    output = splitter.split_text("foo bar")
+    output = splitter_3.split_text("foo bar")
     assert output == ["<s>", "foo", " bar", "</s>"]
 
 
