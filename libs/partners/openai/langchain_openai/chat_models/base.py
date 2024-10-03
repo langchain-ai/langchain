78161ad9117a05a16255e37897cb359829d45361
Bagatur <baskaryan@gmail.com>
2024-10-03 13:11:34 -0700

    fmt

--- libs/partners/openai/langchain_openai/chat_models/base.py
+++ libs/partners/openai/langchain_openai/chat_models/base.py
@@ -2160,17 +2160,17 @@ def _create_usage_metadata(oai_token_usage: dict) -> UsageMetadata:
     input_tokens = oai_token_usage.get("prompt_tokens", 0)
     output_tokens = oai_token_usage.get("completion_tokens", 0)
     total_tokens = oai_token_usage.get("total_tokens", input_tokens + output_tokens)
-    input_token_details = {
+    input_token_details: dict = {
         "audio": oai_token_usage.get("prompt_tokens_details", {}).get("audio_tokens"),
         "cache_read": oai_token_usage.get("prompt_tokens_details", {}).get(
             "cached_tokens"
         ),
     }
-    output_token_details = {
+    output_token_details: dict = {
         "audio": oai_token_usage.get("completion_tokens_details", {}).get(
             "audio_tokens"
         ),
-        "reasoning": oai_token_usage.get("completion_token_details", {}).get(
+        "reasoning": oai_token_usage.get("completion_tokens_details", {}).get(
             "reasoning_tokens"
         ),
     }
