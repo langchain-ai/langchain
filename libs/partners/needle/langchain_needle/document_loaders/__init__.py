e7202ba87070e84eaf97f959ff707b5902084f09
Jan Heimes <jan@kooperativ.net>
2024-10-04 16:43:21 +0200

    techdebt: remove partner package

--- libs/partners/needle/langchain_needle/document_loaders/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Needle Document Loader."""
-
-from langchain_needle.document_loaders.needle import NeedleLoader
-
-__all__ = ["NeedleLoader"]
--- libs/partners/needle/langchain_needle/retrievers/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Needle Document Retriever."""
-
-from langchain_needle.retrievers.needle import NeedleRetriever
-
-__all__ = ["NeedleRetriever"]
