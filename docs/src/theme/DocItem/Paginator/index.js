a91832d7305110423b4763068c12cf3880e98391
Erick Friis <erick@langchain.dev>
2024-10-07 12:51:35 -0700

    docs: add discussions with giscus

--- docs/src/theme/DocItem/Paginator/index.js
+++ docs/src/theme/DocItem/Paginator/index.js
@@ -6,6 +6,22 @@ export default function PaginatorWrapper(props) {
   return (
     <>
       <Feedback />
+      <script src="https://giscus.app/client.js"
+        data-repo="langchain-ai/langchain"
+        data-repo-id="R_kgDOIPDwlg"
+        data-category="Docs Discussions"
+        data-category-id="DIC_kwDOIPDwls4CjJYb"
+        data-mapping="pathname"
+        data-strict="0"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="bottom"
+        data-theme="preferred_color_scheme"
+        data-lang="en"
+        data-loading="lazy"
+        crossorigin="anonymous"
+        async>
+      </script>
       <Paginator {...props} />
     </>
   );
