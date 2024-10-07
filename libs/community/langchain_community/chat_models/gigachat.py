42b4f2bfc678ba1c002c0d5dcb8505d0263a9c32
Konstantin Krestnikov <rai220@gmail.com>
2024-10-07 17:54:21 +0300

    Fixing versions

--- libs/community/langchain_community/chat_models/gigachat.py
+++ libs/community/langchain_community/chat_models/gigachat.py
@@ -1,3 +1,9 @@
 def __getattr__(name: str = "") -> None:
     """Raise an error on import since is deprecated."""
     raise AttributeError("This module has been moved to langchain_gigachat module")
+
+
+class GigaChat:
+    def __init___(self) -> None:
+        """Raise an error on import since is deprecated."""
+        raise AttributeError("This module has been moved to langchain_gigachat module")
