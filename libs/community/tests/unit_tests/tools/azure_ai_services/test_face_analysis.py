229b933e18cda8afc202dfb71042ffd265cea826
Sheepsta300 <128811766+Sheepsta300@users.noreply.github.com>
2024-10-08 19:50:54 +1300

    Update test file and create docs

--- libs/community/tests/unit_tests/tools/azure_ai_services/test_face_analysis.py
+++ libs/community/tests/unit_tests/tools/azure_ai_services/test_face_analysis.py
@@ -31,7 +31,7 @@ def test_face_analysis(mocker: Any) -> None:
     assert tool.azure_ai_face_endpoint == endpoint
 
 
-@pytest.mark.requires("azure.ai.contentsafety")
+@pytest.mark.requires("azure.ai.vision.face")
 def test_faces_detected(mocker: Any) -> None:
     key = "key"
     endpoint = "endpoint"
@@ -49,14 +49,14 @@ def test_faces_detected(mocker: Any) -> None:
 
     tool.face_client = mock_content_client
 
-    input = FACE
+    input: str = FACE
     output = "FACE: 1\nfaceId: 69d017e8-464e-42c1-adfb-5b7fec0526ea\n"
 
     result = tool._run(input)
     assert result == output
 
 
-@pytest.mark.requires("azure.ai.contentsafety")
+@pytest.mark.requires("azure.ai.vision.face")
 def test_no_faces_detected(mocker: Any) -> None:
     key = "key"
     endpoint = "endpoint"
@@ -72,7 +72,7 @@ def test_no_faces_detected(mocker: Any) -> None:
 
     tool.face_client = mock_content_client
 
-    input = NO_FACE
+    input: str = NO_FACE
     output = "No faces found"
 
     result = tool._run(input)
