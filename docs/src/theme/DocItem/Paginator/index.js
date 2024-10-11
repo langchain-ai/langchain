0695aba8983f73cfca95fd574e669264340858df
Erick Friis <erick@langchain.dev>
2024-10-11 12:37:23 -0400

    x

--- docs/src/theme/DocItem/Paginator/index.js
+++ docs/src/theme/DocItem/Paginator/index.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import Paginator from '@theme-original/DocItem/Paginator';
 import Feedback from "@theme/Feedback";
+import Giscus from "@giscus/react";
 
 export default function PaginatorWrapper(props) {
   return (
     <>
       <Feedback />
-      <script src="https://giscus.app/client.js"
-        data-repo="langchain-ai/langchain"
-        data-repo-id="R_kgDOIPDwlg"
-        data-category="Docs Discussions"
-        data-category-id="DIC_kwDOIPDwls4CjJYb"
-        data-mapping="pathname"
-        data-strict="0"
-        data-reactions-enabled="1"
-        data-emit-metadata="0"
-        data-input-position="bottom"
-        data-theme="preferred_color_scheme"
-        data-lang="en"
-        data-loading="lazy"
-        crossorigin="anonymous"
-        async>
-      </script>
+      <Giscus
+        repo="langchain-ai/langchain"
+        repoId="R_kgDOIPDwlg"
+        category="Docs Discussions"
+        categoryId="DIC_kwDOIPDwls4CjJYb"
+        mapping="pathname"
+        strict="0"
+        reactionsEnabled="1"
+        emitMetadata="0"
+        inputPosition="bottom"
+        theme="preferred_color_scheme"
+        lang="en"
+        loading="lazy" />
       <Paginator {...props} />
     </>
   );
