761780f95d3d6457ed2f4539e02f1239d1eb646f
Saad Makrod <saad.makrod@gmail.com>
2024-10-12 11:24:32 -0400

    adding wrapper

--- libs/community/langchain_community/utilities/google_books.py
+++ libs/community/langchain_community/utilities/google_books.py
@@ -1,16 +1,36 @@
-from typing import Optional
+"""Util that calls Google Books."""
+
+from typing import Dict, Optional, List
 
 import requests
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict, model_validator
+
+from langchain_core.utils import get_from_dict_or_env
+
+GOOGLE_BOOKS_MAX_ITEM_SIZE = 5
+GOOGLE_BOOKS_API_URL = 'https://www.googleapis.com/books/v1/volumes'
 
 class GoogleBooksAPIWrapper(BaseModel):
+    """Wrapper around Google Books API.
+
+    To use, you should have a Google Books API key available.
+    This wrapper will use the Google Books API to conduct searches and
+    fetch books based on a query passed in by the agents. By default,
+    it will return the top-k results.
+
+    The response for each book will contain the book title, author name, summary, and
+    a source link.
+    """
+
     google_books_api_key: Optional[str] = None
+    top_k_results: int = GOOGLE_BOOKS_MAX_ITEM_SIZE
 
     model_config = ConfigDict(
         extra="forbid",
     )
 
-    @root_validator()
+    @model_validator(mode="before")
+    @classmethod
     def validate_environment(cls, values: Dict) -> Dict:
         """Validate that api key exists in environment."""
         google_books_api_key = get_from_dict_or_env(
@@ -22,14 +42,45 @@ class GoogleBooksAPIWrapper(BaseModel):
 
     def run(self, query: str) -> str:
         # build Url based on API key and query
-        request_url = ...
+        params = {
+            'q': query,
+            'maxResults': self.top_k_results,
+            'key': self.google_books_api_key
+        }
 
         # send request
-        response = requests.get(request_url)
+        response = requests.get(GOOGLE_BOOKS_API_URL, params=params)
+        json = response.json()
 
         # some error handeling
         if response.status_code != 200:
-            return response.text
+            return f'Unable to retrieve books got status code ${response.status_code}: {json.get('error', {}).get('message', 'Internal failure')}'
 
         # send back data (format style tbd)
-        return self.format(query, response)
+        return self._format(query, json.get('items', []))
+
+    def _format(self, query: str, books: List) -> str:
+        if not books:
+            return f'Sorry no books could be found for your query: {query}'
+
+        results = [f'Here are {len(books)} suggestions based off your search for books related to {query}']
+        i = 1
+
+        for book in books:
+            info = book['volumeInfo']
+            title = info['title']
+            authors = self._format_authors(info['authors'])
+            summary = info['description']
+            source = info['infoLink']
+
+            results.append(f'{i}. "{title}" by {authors}: {summary}\nYou can read more at {source}')
+
+            i += 1
+
+        return '\n\n'.join(results)
+
+    def _format_authors(self, authors: List) -> str:
+        if len(authors) == 1:
+            return authors[0]
+        return '{} and {}'.format(', '.join(authors[:-1]), authors[-1])
+
