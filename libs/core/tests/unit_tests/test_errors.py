a488f4ea0f82ed12425e026f2fe5712b866491f6
Erick Friis <erick@langchain.dev>
2024-10-16 16:21:54 -0400

    x

--- libs/core/tests/unit_tests/test_errors.py
+++ libs/core/tests/unit_tests/test_errors.py
@@ -1,7 +1,18 @@
-import langchain_core.errors as errors
+from langchain_core.errors import LangChainException, ErrorCode
 import pytest
 
 
 @pytest.fixture
-def error_classes() -> list[type[errors.LangChainException]]:
-    return list(errors.LangChainException.__subclasses__())
+def error_classes() -> list[type[LangChainException]]:
+    return list(LangChainException.__subclasses__())
+
+
+def test_error_classes_codes_one_one_mapping(
+    error_classes: list[type[LangChainException]],
+) -> None:
+    all_error_codes_from_classes = {
+        error_class("message").error_code for error_class in error_classes  # type: ignore
+    }
+    all_error_codes_from_enum = set(ErrorCode)
+
+    assert all_error_codes_from_classes == all_error_codes_from_enum
