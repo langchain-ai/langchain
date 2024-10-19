1544e9038af7f29ecafa6a0982d3cab7962bb1ca
KevinZJN <kwei.zhang@mail.utoronto.ca>
2024-10-19 03:50:43 -0400

    add standard test and fix corresponding issue TODO: fix order issue for last 3 tests

--- libs/partners/chroma/tests/unit_tests/test_vectorstores.py
+++ libs/partners/chroma/tests/unit_tests/test_vectorstores.py
@@ -1,37 +1,12 @@
-from langchain_core.embeddings.fake import (
-    FakeEmbeddings,
-)
-from langchain_chroma.vectorstores import Chroma
+import pytest
 from langchain_core.documents import Document
-from typing import (
-    TYPE_CHECKING,
-    Any,
-    Callable,
-    Dict,
-    Iterable,
-    List,
-    Optional,
-    Tuple,
-    Type,
-)
+from langchain_chroma.vectorstores import Chroma
+from langchain_core.embeddings.fake import (FakeEmbeddings)
 
+
+# Write our own test for the Chroma vectorstore
 class TestChromaSearch:
-
-    def validate_search_results(self, vectorstore, results) -> None:
-        id = results[0].id
-        assert results is not None, "The similarity_search method returned None"
-        assert len(results) > 0, "The similarity_search method returned an empty list"
-        assert results[0] is not None, "The first result is None"
-        assert id is not None, "The id of the first result is None"
-
-    def validate_Document_search_results(self, vectorstore, results) -> None:
-        """Validate search results."""
-        id = results[0].id
-        assert vectorstore.get(id)['documents'][0] == results[0].page_content, "The first result is not the same as the result from get"
-        newDoc = Document(page_content="This is a new document.", metadata={"x": 1})
-        vectorstore.update_document(id, newDoc)
-        assert vectorstore.get(id)['documents'][0] == newDoc.page_content, "The first result is not the same as the updated document"
-
+    @pytest.fixture
     def test_from_documents(self) -> None:
         """Test end to end construction and search."""
         documents = [
@@ -47,8 +22,11 @@ class TestChromaSearch:
         )
 
         results = vectorstore.similarity_search("What is a sandwich?")
-        self.validate_search_results(vectorstore, results)
-        self.validate_Document_search_results(vectorstore, results)
+        id = results[0].id
+        assert vectorstore.get(id)['documents'][0] == results[0].page_content, "The first result is not the same as the result from get"
+        newDoc = Document(page_content="This is a new document.", metadata={"x": 1})
+        vectorstore.update_document(id, newDoc)
+        assert vectorstore.get(id)['documents'][0] == newDoc.page_content, "The first result is not the same as the updated document"
 
 
 
@@ -67,4 +45,9 @@ class TestChromaSearch:
         )
 
         results = vectorstore.similarity_search("sandwich")
-        self.validate_search_results(vectorstore, results)
+        assert results is not None, "The similarity_search method returned None"
+        assert len(results) > 0, "The similarity_search method returned an empty list"
+        assert results[0] is not None, "The first result is None"
+        assert results[0].id is not None, "The id of the first result is None"
+
+        print("Results: ", results)
