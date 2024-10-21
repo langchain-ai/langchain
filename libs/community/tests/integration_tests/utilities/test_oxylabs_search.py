a1eaa76e2c0a3ade7e2e48903fe3a42641a1315f
karksm <karolis.kesminas@telesoftas.com>
2024-10-21 15:27:47 +0300

    chore: add wrapper class tests

---
+++ libs/community/tests/integration_tests/utilities/test_oxylabs_search.py
@@ -0,0 +1,38 @@
+"""Integration test for OxylabsSearchAPIWrapper."""
+import pytest
+
+from langchain_community.utilities import OxylabsSearchAPIWrapper
+
+
+def oxylabs_installed() -> bool:
+    try:
+        from oxylabs import RealtimeClient
+
+        return True
+
+    except Exception as e:
+        print(f"oxylabs not installed, skipping test {e}")
+
+        return False
+
+
+@pytest.mark.skipif(not oxylabs_installed(), reason="requires oxylabs package - `pip install oxylabs`")
+def test_call() -> None:
+    """Test that call gives the correct answer."""
+    chain = OxylabsSearchAPIWrapper()  # type: ignore[call-arg]
+    output = chain.run("Python programming language")
+    assert "high-level, general-purpose programming language" in output
+    assert "extensions" in output
+    assert ".py" in output
+    assert "Guido van Rossum" in output
+
+
+@pytest.mark.skipif(not oxylabs_installed(), reason="requires oxylabs package - `pip install oxylabs`")
+async def test_async_call() -> None:
+    """Test that call gives the correct answer."""
+    chain = OxylabsSearchAPIWrapper()  # type: ignore[call-arg]
+    output = chain.run("Python programming language")
+    assert "high-level, general-purpose programming language" in output
+    assert "extensions" in output
+    assert ".py" in output
+    assert "Guido van Rossum" in output
