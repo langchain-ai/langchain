46524505fbdc1b5239e25d9a980fa714bf98f2ed
Mateusz Szewczyk <Mateusz.Szewczyk@ibm.com>
2024-10-30 14:19:44 +0100

    show WATSONX_API_KEY

--- docs/src/theme/ChatModelTabs.js
+++ docs/src/theme/ChatModelTabs.js
@@ -180,15 +180,6 @@ export default function ChatModelTabs(props) {
       shouldHide: hideTogether,
     },
     {
-      value: "AWS",
-      label: "AWS",
-      text: `from langchain_aws import ChatBedrock\n\n${llmVarName} = ChatBedrock(${awsBedrockParamsOrDefault})`,
-      apiKeyText: "# Ensure your AWS credentials are configured",
-      packageName: "langchain-aws",
-      default: false,
-      shouldHide: hideAWS,
-    },
-    {
       value: "IBM",
       label: "IBM",
       text: `from langchain_ibm import ChatWatsonx\n\n${llmVarName} = ChatWatsonx(${ibmParamsOrDefault})`,
@@ -196,6 +187,15 @@ export default function ChatModelTabs(props) {
       packageName: "langchain-ibm",
       default: false,
       shouldHide: hideIBM,
+    },
+    {
+      value: "AWS",
+      label: "AWS",
+      text: `from langchain_aws import ChatBedrock\n\n${llmVarName} = ChatBedrock(${awsBedrockParamsOrDefault})`,
+      apiKeyText: "# Ensure your AWS credentials are configured",
+      packageName: "langchain-aws",
+      default: false,
+      shouldHide: hideAWS,
     },
   ];
 
