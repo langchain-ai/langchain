65d2fc3fa5607072b0849b56f7bcd985a2a9546a
Erick Friis <erick@langchain.dev>
2024-10-30 15:45:38 -0700

    x

--- libs/community/tests/unit_tests/tools/playwright/test_all.py
+++ libs/community/tests/unit_tests/tools/playwright/test_all.py
@@ -1,9 +1,12 @@
 """Test Playwright's Tools."""
 
+import pytest
+
 from langchain_community.agent_toolkits import PlayWrightBrowserToolkit
 from langchain_community.tools.playwright.utils import create_sync_playwright_browser
 
 
+@pytest.mark.requires("playwright")
 def test_playwright_tools_schemas() -> None:
     """Test calling 'tool_call_schema' for every tool to check to init issues."""
 
