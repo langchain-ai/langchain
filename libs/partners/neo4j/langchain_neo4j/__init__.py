a50bf2196271b468ddf50207373f5b1363847fdd
Alex Thomas <alexthomas93@users.noreply.github.com>
2024-10-31 14:41:17 +0000

    Updated base level imports

--- libs/partners/neo4j/langchain_neo4j/__init__.py
+++ libs/partners/neo4j/langchain_neo4j/__init__.py
@@ -1,8 +1,8 @@
 from importlib import metadata
 
+from langchain_neo4j.chains.graph_qa.cypher import GraphCypherQAChain
 from langchain_neo4j.chat_message_histories.neo4j import Neo4jChatMessageHistory
 from langchain_neo4j.graphs.neo4j_graph import Neo4jGraph
-from langchain_neo4j.query_constructors.neo4j import Neo4jTranslator
 from langchain_neo4j.vectorstores.neo4j_vector import Neo4jVector
 
 try:
@@ -13,9 +13,9 @@ except metadata.PackageNotFoundError:
 del metadata  # optional, avoids polluting the results of dir(__package__)
 
 __all__ = [
+    "GraphCypherQAChain",
     "Neo4jChatMessageHistory",
     "Neo4jGraph",
-    "Neo4jTranslator",
     "Neo4jVector",
     "__version__",
 ]
