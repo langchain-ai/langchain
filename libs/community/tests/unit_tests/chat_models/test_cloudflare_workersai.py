c38745838d736773bfeab3c4814d4b186b16fb75
AkshataDM <oxta28@gmail.com>
2024-11-01 11:08:57 -0500

    Add unit tests

---
+++ libs/community/tests/unit_tests/chat_models/test_cloudflare_workersai.py
@@ -0,0 +1,72 @@
+"""Test CloudflareWorkersAI Chat API wrapper."""
+
+from typing import Any, Dict, List
+
+import pytest
+from langchain_core.messages import (
+    AIMessage,
+    BaseMessage,
+    HumanMessage,
+    SystemMessage,
+    ToolMessage,
+)
+
+from langchain_community.chat_models.cloudflare_workersai import (
+    ChatCloudflareWorkersAI,
+    _convert_messages_to_cloudflare_messages,
+)
+
+
+def test_standard_params() -> None:
+    llm = ChatCloudflareWorkersAI(
+        account_id="my_account_id",
+        api_token="my_api_token",
+        model="@hf/nousresearch/hermes-2-pro-mistral-7b",
+    )
+
+    assert llm.model == "@hf/nousresearch/hermes-2-pro-mistral-7b"
+
+
+@pytest.mark.parametrize(
+    ("messages", "expected"),
+    [
+        # Test case with a single HumanMessage
+        (
+            [HumanMessage(content="Hello, AI!")],
+            [{"role": "user", "content": "Hello, AI!"}],
+        ),
+        # Test case with SystemMessage, HumanMessage, and AIMessage without tool calls
+        (
+            [
+                SystemMessage(content="System initialized."),
+                HumanMessage(content="Hello, AI!"),
+                AIMessage(content="Response from AI"),
+            ],
+            [
+                {"role": "system", "content": "System initialized."},
+                {"role": "user", "content": "Hello, AI!"},
+                {"role": "assistant", "content": "Response from AI"},
+            ],
+        ),
+        # Test case with ToolMessage and tool_call_id
+        (
+            [
+                ToolMessage(
+                    content="Tool message content", tool_call_id="tool_call_123"
+                ),
+            ],
+            [
+                {
+                    "role": "tool",
+                    "content": "Tool message content",
+                    "tool_call_id": "tool_call_123",
+                }
+            ],
+        ),
+    ],
+)
+def test_convert_messages_to_cloudflare_format(
+    messages: List[BaseMessage], expected: List[Dict[str, Any]]
+) -> None:
+    result = _convert_messages_to_cloudflare_messages(messages)
+    assert result == expected
