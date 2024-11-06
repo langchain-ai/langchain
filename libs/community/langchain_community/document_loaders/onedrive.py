d19e7c51c4a1dfdb976a360c4c53d5a5c5baafd4
Martin Tříska <martin.triska@gmail.com>
2024-11-06 09:25:55 +0100

    line too long fix

--- libs/community/langchain_community/document_loaders/onedrive.py
+++ libs/community/langchain_community/document_loaders/onedrive.py
@@ -6,7 +6,10 @@ from langchain_community.document_loaders import SharePointLoader
 
 
 class OneDriveLoader(SharePointLoader):
-    """Load documents from Microsoft OneDrive. Uses `SharePointLoader` under the hood."""
+    """
+    Load documents from Microsoft OneDrive.
+    Uses `SharePointLoader` under the hood.
+    """
 
     drive_id: str = Field(...)
     """The ID of the OneDrive drive to load data from."""
