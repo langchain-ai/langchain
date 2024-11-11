b3d87fba83183cd0594906e83c6086e1f62508bb
jhpiedrahitao <jhpiedrahitao97@gmail.com>
2024-11-11 11:20:10 -0500

    fmt

--- libs/community/langchain_community/llms/sambanova.py
+++ libs/community/langchain_community/llms/sambanova.py
@@ -56,7 +56,7 @@ class SambaStudio(LLM):
                 do_sample: bool
                     wether to do sample
                 process_prompt:
-                    wether to process prompt 
+                    wether to process prompt
                     (set for Bundle generic v1 and v2 endpoints)
     Key init args — client params:
         sambastudio_url: str
