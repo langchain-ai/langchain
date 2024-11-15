796da053c6e6bba1b5ab738fd958311bf7e59274
Erick Friis <erick@langchain.dev>
2024-11-15 10:06:24 -0800

    x

--- libs/cli/langchain_cli/integration_template/tests/unit_tests/test_chat_models.py
+++ libs/cli/langchain_cli/integration_template/tests/unit_tests/test_chat_models.py
@@ -1,8 +1,23 @@
 """Test chat model integration."""
 
+from langchain_standard_tests.unit_tests import ChatModelUnitTests
+from typing import Tuple, Type
+
+from langchain_core.language_models import BaseChatModel
+
 from __module_name__.chat_models import Chat__ModuleName__
 
 
-def test_initialization() -> None:
-    """Test chat model initialization."""
-    Chat__ModuleName__()
+class TestChat__ModuleName__StandardUnitTests(ChatModelUnitTests):
+    """Standard LangChain interface tests, applied to __ModuleName__."""
+
+    @property
+    def chat_model_class(self) -> Type[BaseChatModel]:
+        """Return chat model class."""
+        return Chat__ModuleName__
+
+    @property
+    def chat_model_params(self) -> dict:
+        # TODO: Update with chat model parameters
+        # e.g. return {"model": "claude-3-haiku-20240307"} for ChatAnthropic
+        return {}
--- libs/cli/langchain_cli/integration_template/tests/unit_tests/test_embeddings.py
+++ libs/cli/langchain_cli/integration_template/tests/unit_tests/test_embeddings.py
@@ -1,6 +1,41 @@
 """Test embedding model integration."""
 
+from typing import Tuple, Type
+
+from langchain_core.embeddings import Embeddings
+from langchain_standard_tests.unit_tests.embeddings import EmbeddingsUnitTests
+
 from __module_name__.embeddings import __ModuleName__Embeddings
+
+
+class TestFireworksStandard(EmbeddingsUnitTests):
+    @property
+    def embeddings_class(self) -> Type[Embeddings]:
+        return __ModuleName__Embeddings
+
+    @property
+    def embeddings_params(self) -> dict:
+        return {"api_key": "test api key"}
+
+    @property
+    def init_from_env_params(self) -> Tuple[dict, dict, dict]:
+        """Return env vars, init args, and expected instance attrs for initializing
+        from env vars.
+
+        This powers tests for initializing from environment variables."""
+        return (
+            # env vars
+            {
+                "__MODULE_NAME___API_KEY": "test api key",
+            },
+            # init vars - only pass things that are required and CAN'T be set
+            # via env vars
+            {},
+            # expected attributes once the object has been constructed
+            {
+                "api_key": "test api key",
+            },
+        )
 
 
 def test_initialization() -> None:
