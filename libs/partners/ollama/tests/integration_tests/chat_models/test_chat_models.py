1c15e7013a789c75483a3615c30eba14117c4efd
Daniel Grishanov <daniel.grishanov@mail.utoronto.ca>
2024-11-19 23:45:57 +0000

    Fix issue #25343, add  testing

---
+++ libs/partners/ollama/tests/integration_tests/chat_models/test_chat_models.py
@@ -0,0 +1,36 @@
+""" Ollama specific chat model integration tests """
+
+from typing import List, Optional
+from langchain_core.language_models import BaseChatModel
+from langchain_ollama import ChatOllama
+from pydantic import BaseModel, Field
+import pytest
+
+@pytest.mark.parametrize(
+    ("model"), [("llama3.1")]
+)
+def test_structured_output_deeply_nested(model: BaseChatModel) -> None:
+    """ Test to verify structured output with a nested objects. """
+    llm = ChatOllama(model=model, temperature=0)
+
+    class Person(BaseModel):
+        """Information about a person."""
+        name: Optional[str] = Field(default=None, description="The name of the person")
+        hair_color: Optional[str] = Field(
+            default=None, description="The color of the person's hair if known"
+        )
+        height_in_meters: Optional[str] = Field(
+            default=None, description="Height measured in meters"
+        )
+
+    class Data(BaseModel):
+        """Extracted data about people."""
+        people: List[Person]
+
+    chat = llm.with_structured_output(Data)  # type: ignore[arg-type]
+    text = "Alan Smith is 6 feet tall and has blond hair. Alan Poe is 3 feet tall and has grey hair."
+    result = chat.invoke(text)
+    assert isinstance(result, Data)
+
+    for chunk in chat.stream(text):
+        assert isinstance(chunk, Data)
