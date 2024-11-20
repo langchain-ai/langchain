d6f8872e0516b5ac8ca25fcecf9ba0c8880b3c2d
Daniel Grishanov <daniel.grishanov@mail.utoronto.ca>
2024-11-20 02:00:47 +0000

    Fix linting issues in test_chat_models.py

--- libs/partners/ollama/tests/integration_tests/chat_models/test_chat_models.py
+++ libs/partners/ollama/tests/integration_tests/chat_models/test_chat_models.py
@@ -1,20 +1,21 @@
-""" Ollama specific chat model integration tests """
+"""Ollama specific chat model integration tests"""
 
 from typing import List, Optional
-from langchain_core.language_models import BaseChatModel
-from langchain_ollama import ChatOllama
-from pydantic import BaseModel, Field
-import pytest
 
-@pytest.mark.parametrize(
-    ("model"), [("llama3.1")]
-)
-def test_structured_output_deeply_nested(model: BaseChatModel) -> None:
-    """ Test to verify structured output with a nested objects. """
+import pytest
+from pydantic import BaseModel, Field
+
+from langchain_ollama import ChatOllama
+
+
+@pytest.mark.parametrize(("model"), [("llama3.1")])
+def test_structured_output_deeply_nested(model: str) -> None:
+    """Test to verify structured output with a nested objects."""
     llm = ChatOllama(model=model, temperature=0)
 
     class Person(BaseModel):
         """Information about a person."""
+
         name: Optional[str] = Field(default=None, description="The name of the person")
         hair_color: Optional[str] = Field(
             default=None, description="The color of the person's hair if known"
@@ -25,10 +26,14 @@ def test_structured_output_deeply_nested(model: BaseChatModel) -> None:
 
     class Data(BaseModel):
         """Extracted data about people."""
+
         people: List[Person]
 
     chat = llm.with_structured_output(Data)  # type: ignore[arg-type]
-    text = "Alan Smith is 6 feet tall and has blond hair. Alan Poe is 3 feet tall and has grey hair."
+    text = (
+        "Alan Smith is 6 feet tall and has blond hair."
+        "Alan Poe is 3 feet tall and has grey hair."
+    )
     result = chat.invoke(text)
     assert isinstance(result, Data)
 
