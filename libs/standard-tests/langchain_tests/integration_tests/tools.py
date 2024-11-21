94ed2fed3e723593a167aa14a8c3938e515c5607
Erick Friis <erick@langchain.dev>
2024-11-20 16:34:26 -0800

    x

--- libs/standard-tests/langchain_tests/integration_tests/tools.py
+++ libs/standard-tests/langchain_tests/integration_tests/tools.py
@@ -12,26 +12,26 @@ class ToolsIntegrationTests(ToolsTests):
         tool_call = ToolCall(
             name=tool.name,
             args=self.tool_invoke_params_example,
-            id=None,
+            id="123",
             type="tool_call",
         )
         result = tool.invoke(tool_call)
 
         if tool.response_format == "content":
-            content = result
+            tool_message = result
         elif tool.response_format == "content_and_artifact":
             # should be (content, artifact)
             assert isinstance(result, tuple)
             assert len(result) == 2
-            content, artifact = result
+            tool_message, artifact = result
 
             assert artifact  # artifact can be anything, but shouldn't be none
 
         # check content is a valid ToolMessage content
-        assert isinstance(content, (str, list))
-        if isinstance(content, list):
+        assert isinstance(tool_message.content, (str, list))
+        if isinstance(tool_message.content, list):
             # content blocks must be str or dict
-            assert all(isinstance(c, (str, dict)) for c in content)
+            assert all(isinstance(c, (str, dict)) for c in tool_message.content)
 
     async def test_async_invoke_matches_output_schema(self, tool: BaseTool) -> None:
         """
@@ -40,26 +40,26 @@ class ToolsIntegrationTests(ToolsTests):
         tool_call = ToolCall(
             name=tool.name,
             args=self.tool_invoke_params_example,
-            id=None,
+            id="123",
             type="tool_call",
         )
         result = await tool.ainvoke(tool_call)
 
         if tool.response_format == "content":
-            content = result
+            tool_message = result
         elif tool.response_format == "content_and_artifact":
             # should be (content, artifact)
             assert isinstance(result, tuple)
             assert len(result) == 2
-            content, artifact = result
+            tool_message, artifact = result
 
             assert artifact  # artifact can be anything, but shouldn't be none
 
         # check content is a valid ToolMessage content
-        assert isinstance(content, (str, list))
-        if isinstance(content, list):
+        assert isinstance(tool_message.content, (str, list))
+        if isinstance(tool_message.content, list):
             # content blocks must be str or dict
-            assert all(isinstance(c, (str, dict)) for c in content)
+            assert all(isinstance(c, (str, dict)) for c in tool_message.content)
 
     def test_invoke_no_tool_call(self, tool: BaseTool) -> None:
         """
