dda18303ed91d575a97449d49d8de5296067774f
PiotrBLL <piotr.sobieszczyk@bluelabellabs.com>
2024-11-29 02:40:59 +0100

    Add HPU tests: summarization

--- libs/community/tests/integration_tests/llms/test_huggingface_pipeline_hpu.py
+++ libs/community/tests/integration_tests/llms/test_huggingface_pipeline_hpu.py
@@ -36,3 +36,14 @@ def test_huggingface_pipeline_invalid_hpu_and_openvino_backend() -> None:
         )
     except ValueError as e:
         assert "Cannot specify `model_kwargs{'device': 'hpu'}` and `backend=openvino` at the same time." in str(e)
+
+
+def test_huggingface_pipeline_summarization_on_hpu() -> None:
+    """Test valid call to HuggingFace summarization model."""
+    llm = HuggingFacePipeline.from_model_id(
+        model_id="facebook/bart-large-cnn",
+        task="summarization",
+        model_kwargs={"device": "hpu"},
+    )
+    output = llm.invoke("Say foo:")
+    assert isinstance(output, str)
