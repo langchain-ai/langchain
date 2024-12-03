f00171783835b0ef177aab43a24192bfd8eecddd
wassim-mechergui-shift <wassim.mechergui@shift-technology.com>
2024-12-03 17:21:35 +0100

    fix linting

--- libs/community/langchain_community/vectorstores/azure_cosmos_db_no_sql.py
+++ libs/community/langchain_community/vectorstores/azure_cosmos_db_no_sql.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 
 import uuid
 import warnings
-from typing import TYPE_CHECKING, Any, Dict, Iterable, List, Optional, Tuple, Callable
+from typing import (TYPE_CHECKING, Any, Callable, Dict,
+                    Iterable, List, Optional, Tuple)
 
 import numpy as np
 from langchain_core.documents import Document
