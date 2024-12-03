d3ffdb42c22c86a802e8244c513e53cbe5854b3b
PiotrBLL <piotr.sobieszczyk@bluelabellabs.com>
2024-12-03 21:46:32 +0100

    Split hpu llms test files

--- libs/partners/huggingface/tests/integration_tests/test_llms_hpu.py
+++ libs/partners/huggingface/tests/integration_tests/test_llms_hpu.py
@@ -3,10 +3,13 @@ from typing import Generator
 from langchain_huggingface.llms import HuggingFacePipeline
 
 
-def test_huggingface_pipeline_streaming() -> None:
-    """Test streaming tokens from huggingface_pipeline."""
+def test_huggingface_pipeline_streaming_on_hpu() -> None:
+    """Test streaming tokens from huggingface_pipeline on HPU."""
     llm = HuggingFacePipeline.from_model_id(
-        model_id="gpt2", task="text-generation", pipeline_kwargs={"max_new_tokens": 10}
+        model_id="gpt2",
+        task="text-generation",
+        pipeline_kwargs={"max_new_tokens": 10},
+        model_kwargs={"device": "hpu"},
     )
     generator = llm.stream("Q: How do you say 'hello' in German? A:'", stop=["."])
     stream_results_string = ""
