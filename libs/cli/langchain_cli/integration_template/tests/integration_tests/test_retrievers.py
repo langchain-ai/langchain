63150792d568a6da3c87c766c3aac5ab08e1505a
Erick Friis <erick@langchain.dev>
2024-12-04 23:30:34 -0800

    x

---
+++ libs/cli/langchain_cli/integration_template/tests/integration_tests/test_retrievers.py
@@ -0,0 +1,20 @@
+from typing import Type
+
+from __module_name__.retrievers import __ModuleName__Retriever
+from langchain_standard_tests.integration_tests import (
+    RetrieversIntegrationTests,
+)
+
+
+class Test__ModuleName__VectorStoreSync(RetrieversIntegrationTests):
+    @property
+    def retriever_constructor(self) -> Type[__ModuleName__Retriever]:
+        """Get an empty vectorstore for unit tests."""
+        return __ModuleName__Retriever
+
+    @property
+    def retriever_query_example(self) -> str:
+        """
+        Returns a dictionary representing the "args" of an example retriever call.
+        """
+        return "example query"
