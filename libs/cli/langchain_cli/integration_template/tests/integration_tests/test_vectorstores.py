f3dc142d3cb4103375ce33c17ad740110abb8b04
ccurme <chester.curme@gmail.com>
2024-12-06 13:10:22 -0500

    cli[patch]: implement minimal starter vector store (#28577)
    Basically the same as core's in-memory vector store. Removed some
    optional methods.

--- libs/cli/langchain_cli/integration_template/tests/integration_tests/test_vectorstores.py
+++ libs/cli/langchain_cli/integration_template/tests/integration_tests/test_vectorstores.py
@@ -13,7 +13,7 @@ class Test__ModuleName__VectorStoreSync(ReadWriteTestSuite):
     @pytest.fixture()
     def vectorstore(self) -> Generator[VectorStore, None, None]:  # type: ignore
         """Get an empty vectorstore for unit tests."""
-        store = __ModuleName__VectorStore()
+        store = __ModuleName__VectorStore(self.get_embeddings())
         # note: store should be EMPTY at this point
         # if you need to delete data, you may do so here
         try:
@@ -27,7 +27,7 @@ class Test__ModuleName__VectorStoreAsync(AsyncReadWriteTestSuite):
     @pytest.fixture()
     async def vectorstore(self) -> AsyncGenerator[VectorStore, None]:  # type: ignore
         """Get an empty vectorstore for unit tests."""
-        store = __ModuleName__VectorStore()
+        store = __ModuleName__VectorStore(self.get_embeddings())
         # note: store should be EMPTY at this point
         # if you need to delete data, you may do so here
         try:
