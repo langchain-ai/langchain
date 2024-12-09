59290be862baa465aa390a92df341d27db14957f
Chester Curme <chester.curme@gmail.com>
2024-12-09 10:34:53 -0500

    update

--- libs/partners/ollama/tests/integration_tests/chat_models/test_chat_models.py
+++ libs/partners/ollama/tests/integration_tests/chat_models/test_chat_models.py
@@ -4,8 +4,59 @@ from typing import List, Optional
 
 import pytest
 from pydantic import BaseModel, Field
+from typing_extensions import Annotated, TypedDict
 
 from langchain_ollama import ChatOllama
+
+
+@pytest.mark.parametrize(("method"), [("function_calling"), ("json_schema")])
+def test_structured_output(method: str) -> None:
+    """Test to verify structured output via tool calling and ``format`` parameter."""
+
+    class Joke(BaseModel):
+        """Joke to tell user."""
+
+        setup: str = Field(description="question to set up a joke")
+        punchline: str = Field(description="answer to resolve the joke")
+
+    llm = ChatOllama(model="llama3.1", temperature=0)
+    query = "Tell me a joke about cats."
+
+    # Pydantic
+    structured_llm = llm.with_structured_output(Joke, method=method)  # type: ignore[arg-type]
+    result = structured_llm.invoke(query)
+    assert isinstance(result, Joke)
+
+    for chunk in structured_llm.stream(query):
+        assert isinstance(chunk, Joke)
+
+    # JSON Schema
+    structured_llm = llm.with_structured_output(Joke.model_json_schema(), method=method)  # type: ignore[arg-type]
+    result = structured_llm.invoke(query)
+    assert isinstance(result, dict)
+    assert set(result.keys()) == {"setup", "punchline"}
+
+    for chunk in structured_llm.stream(query):
+        assert isinstance(chunk, dict)
+    assert isinstance(chunk, dict)  # for mypy
+    assert set(chunk.keys()) == {"setup", "punchline"}
+
+    # Typed Dict
+    class JokeSchema(TypedDict):
+        """Joke to tell user."""
+
+        setup: Annotated[str, "question to set up a joke"]
+        punchline: Annotated[str, "answer to resolve the joke"]
+
+    structured_llm = llm.with_structured_output(JokeSchema, method=method)  # type: ignore[arg-type]
+    result = structured_llm.invoke(query)
+    assert isinstance(result, dict)
+    assert set(result.keys()) == {"setup", "punchline"}
+
+    for chunk in structured_llm.stream(query):
+        assert isinstance(chunk, dict)
+    assert isinstance(chunk, dict)  # for mypy
+    assert set(chunk.keys()) == {"setup", "punchline"}
 
 
 @pytest.mark.parametrize(("model"), [("llama3.1")])
