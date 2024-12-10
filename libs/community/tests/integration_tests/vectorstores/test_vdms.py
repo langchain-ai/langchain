76ea1b37d8f14ee9072d8863ec5884881a121e18
Lacewell, Chaunte W <chaunte.w.lacewell@intel.com>
2024-12-10 15:12:56 -0800

    Update VDMS test tp use new VectorStoreIntegrationTests

--- libs/community/tests/integration_tests/vectorstores/test_vdms.py
+++ libs/community/tests/integration_tests/vectorstores/test_vdms.py
@@ -7,10 +7,7 @@ import os
 import uuid
 
 import pytest
-from langchain_tests.integration_tests.vectorstores import (
-    AsyncReadWriteTestSuite,
-    ReadWriteTestSuite,
-)
+from langchain_tests.integration_tests.vectorstores import VectorStoreIntegrationTests
 
 from langchain_community.vectorstores.vdms import VDMS, VDMS_Client
 
@@ -22,24 +19,9 @@ logging.basicConfig(level=logging.DEBUG)
 # docker run -d -p $VDMS_DBPORT:55555 intellabs/vdms:latest
 
 
-class TestVDMSReadWriteTestSuite(ReadWriteTestSuite):
+class TestVDMSStandard(VectorStoreIntegrationTests):
     @pytest.fixture
     def vectorstore(self) -> VDMS:
-        test_name = uuid.uuid4().hex
-        client = VDMS_Client(
-            host=os.getenv("VDMS_DBHOST", "localhost"),
-            port=int(os.getenv("VDMS_DBPORT", 6025)),
-        )
-        return VDMS(
-            client=client,
-            embedding=self.get_embeddings(),
-            collection_name=test_name,
-        )
-
-
-class TestAsyncVDMSReadWriteTestSuite(AsyncReadWriteTestSuite):
-    @pytest.fixture
-    async def vectorstore(self) -> VDMS:
         test_name = uuid.uuid4().hex
         client = VDMS_Client(
             host=os.getenv("VDMS_DBHOST", "localhost"),
