482f73824cad17ee64a6d6fd662f877ace33940e
Martin Tříska <martin.triska@gmail.com>
2024-12-13 11:58:37 +0100

    linting

--- libs/community/langchain_community/document_loaders/base_o365.py
+++ libs/community/langchain_community/document_loaders/base_o365.py
@@ -13,7 +13,6 @@ from datetime import datetime
 from pathlib import Path, PurePath
 from typing import TYPE_CHECKING, Any, Dict, Iterable, List, Optional, Sequence, Union
 
-from dateutil.parser import parse
 from pydantic import (
     BaseModel,
     Field,
