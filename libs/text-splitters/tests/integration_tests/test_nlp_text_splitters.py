0f9ba47f78013eca8ecafb2a8290d3f2a7d5c2cd
Erick Friis <erick@langchain.dev>
2024-12-16 11:40:56 -0800

    x

--- libs/text-splitters/tests/integration_tests/test_nlp_text_splitters.py
+++ libs/text-splitters/tests/integration_tests/test_nlp_text_splitters.py
@@ -1,8 +1,10 @@
 """Test text splitting functionality using NLTK and Spacy based sentence splitters."""
 
+import importlib
+from typing import Any
+
 import nltk
 import pytest
-import spacy
 from langchain_core.documents import Document
 
 from langchain_text_splitters.nltk import NLTKTextSplitter
@@ -10,8 +12,16 @@ from langchain_text_splitters.spacy import SpacyTextSplitter
 
 
 def setup_module() -> None:
-    spacy.cli.download("en_core_web_sm")  # type: ignore
     nltk.download("punkt_tab")
+
+@pytest.fixture()
+def spacy() -> Any:
+    try:
+        import spacy
+    except ImportError:
+        pytest.skip("Spacy not installed.")
+    spacy.cli.download("en_core_web_sm")  # type: ignore
+    return spacy
 
 
 def test_nltk_text_splitting_args() -> None:
@@ -20,7 +30,7 @@ def test_nltk_text_splitting_args() -> None:
         NLTKTextSplitter(chunk_size=2, chunk_overlap=4)
 
 
-def test_spacy_text_splitting_args() -> None:
+def test_spacy_text_splitting_args(spacy: Any) -> None:
     """Test invalid arguments."""
     with pytest.raises(ValueError):
         SpacyTextSplitter(chunk_size=2, chunk_overlap=4)
@@ -35,9 +45,8 @@ def test_nltk_text_splitter() -> None:
     expected_output = [f"This is sentence one.{separator}And this is sentence two."]
     assert output == expected_output
 
-
 @pytest.mark.parametrize("pipeline", ["sentencizer", "en_core_web_sm"])
-def test_spacy_text_splitter(pipeline: str) -> None:
+def test_spacy_text_splitter(pipeline: str, spacy: Any) -> None:
     """Test splitting by sentence using Spacy."""
     text = "This is sentence one. And this is sentence two."
     separator = "|||"
@@ -46,9 +55,8 @@ def test_spacy_text_splitter(pipeline: str) -> None:
     expected_output = [f"This is sentence one.{separator}And this is sentence two."]
     assert output == expected_output
 
-
 @pytest.mark.parametrize("pipeline", ["sentencizer", "en_core_web_sm"])
-def test_spacy_text_splitter_strip_whitespace(pipeline: str) -> None:
+def test_spacy_text_splitter_strip_whitespace(pipeline: str, spacy: Any) -> None:
     """Test splitting by sentence using Spacy."""
     text = "This is sentence one. And this is sentence two."
     separator = "|||"
