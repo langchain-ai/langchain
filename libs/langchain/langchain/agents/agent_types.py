49ca6b340b74f1d03b41878b3b3d1e120534830e
Hrishikesh Kalola <97169623+hrishi-008@users.noreply.github.com>
2025-01-02 12:57:42 +0530

    Update agent_types.py
    Updated the correct link to migration page.

--- libs/langchain/langchain/agents/agent_types.py
+++ libs/langchain/langchain/agents/agent_types.py
@@ -15,7 +15,10 @@ from langchain._api.deprecation import AGENT_DEPRECATION_WARNING
 class AgentType(str, Enum):
     """An enum for agent types.
 
-    See documentation: https://python.langchain.com/docs/modules/agents/agent_types/
+    This feature has been depricated.
+    For detailed guidance on transitioning from LangChain's `AgentType` to LangGraph agents, 
+    refer to the documentation: https://python.langchain.com/docs/how_to/migrate_agent/
+    This page provides step-by-step instructions for the migration process.
     """
 
     ZERO_SHOT_REACT_DESCRIPTION = "zero-shot-react-description"
