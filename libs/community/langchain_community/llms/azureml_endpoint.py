e09064a47501b2072e752e5e9910e3638155dca5
ashvin <ashvinanilkumarsh@gmail.com>
2025-01-04 13:16:25 +0530

    Remove unused endpoint from message thrown as exception.

--- libs/community/langchain_community/llms/azureml_endpoint.py
+++ libs/community/langchain_community/llms/azureml_endpoint.py
@@ -434,7 +434,7 @@ class AzureMLBaseEndpoint(BaseModel):
             raise ValueError(
                 "`endpoint_url` should contain the full invocation URL including "
                 "`/score` for `endpoint_api_type='dedicated'` or `/completions` "
-                "or `/chat/completions` or `/models/chat/completions` "
+                "or `/models/chat/completions` "
                 "for `endpoint_api_type='serverless'`"
             )
         return field_value
@@ -456,7 +456,7 @@ class AzureMLBaseEndpoint(BaseModel):
                 "Endpoints of type `dedicated` should follow the format "
                 "`https://<your-endpoint>.<your_region>.inference.ml.azure.com/score`."
                 " If your endpoint URL ends with `/completions` or"
-                "`/chat/completions` or `/models/chat/completions`,"
+                "`/models/chat/completions`,"
                 "use `endpoint_api_type='serverless'` instead."
             )
         if field_value == AzureMLEndpointApiType.serverless and not (
@@ -466,7 +466,6 @@ class AzureMLBaseEndpoint(BaseModel):
             raise ValueError(
                 "Endpoints of type `serverless` should follow the format "
                 "`https://<your-endpoint>.<your_region>.inference.ml.azure.com/completions`"
-                " or `https://<your-endpoint>.<your_region>.inference.ml.azure.com/chat/completions`"
                 " or `https://<your-endpoint>.<your_region>.inference.ml.azure.com/models/chat/completions`"
             )
 
