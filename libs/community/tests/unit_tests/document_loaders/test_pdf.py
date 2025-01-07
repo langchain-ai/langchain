3beda82cdca13e7f0a8d52a991f160a7f0bb49da
Philippe Prados <github@prados.fr>
2025-01-07 10:01:05 +0100

    Fix tu

--- libs/community/tests/unit_tests/document_loaders/test_pdf.py
+++ libs/community/tests/unit_tests/document_loaders/test_pdf.py
@@ -59,4 +59,4 @@ def test_pypdf_loader_with_layout() -> None:
     expected = path_to_layout_pdf_txt.read_text(encoding="utf-8")
     cleaned_first_page = re.sub(r"\x00", "", first_page)
     cleaned_expected = re.sub(r"\x00", "", expected)
-    assert cleaned_first_page == cleaned_expected.strip()
+    assert cleaned_first_page == cleaned_expected
