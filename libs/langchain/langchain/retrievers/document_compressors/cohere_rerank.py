b07acd0d47e402aa46ae322a1a8da1f2f95dbaf5
Christophe Bornet <cbornet@hotmail.com>
2025-01-14 18:49:57 +0100

    langchain: Bump ruff version to 0.9

--- libs/langchain/langchain/retrievers/document_compressors/cohere_rerank.py
+++ libs/langchain/langchain/retrievers/document_compressors/cohere_rerank.py
@@ -92,7 +92,10 @@ class CohereRerank(BaseDocumentCompressor):
         result_dicts = []
         for res in results:
             result_dicts.append(
-                {"index": res.index, "relevance_score": res.relevance_score}
+                {
+                    "index": res.index,
+                    "relevance_score": res.relevance_score,
+                }
             )
         return result_dicts
 
