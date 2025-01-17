66e5ba2ea1b91092c5a29648d2ad8a3cb6a401b8
TheSongg <145535169+TheSongg@users.noreply.github.com>
2025-01-17 10:18:16 +0800

    add type and value check

--- libs/community/langchain_community/llms/xinference.py
+++ libs/community/langchain_community/llms/xinference.py
@@ -281,7 +281,7 @@ class Xinference(LLM):
 
     @staticmethod
     def _stream_response_to_generation_chunk(
-        stream_response: Optional[Dict],
+        stream_response: Optional[Dict[Any, Any]],
     ) -> GenerationChunk:
         """Convert a stream response to a generation chunk."""
         token = ""
@@ -299,6 +299,8 @@ class Xinference(LLM):
                             logprobs=choice.get("logprobs", None),
                         ),
                     )
+                else:
+                    raise TypeError("choice type error!")
             else:
                 return GenerationChunk(text=token)
         else:
