96c4dd6468e1dc1bf8d1f7644ae9b56970e8498a
Aayush Kataria <aayushkataria3011@gmail.com>
2025-02-06 12:52:14 -0800

    linting

--- libs/community/tests/integration_tests/vectorstores/test_azure_cosmos_db_no_sql.py
+++ libs/community/tests/integration_tests/vectorstores/test_azure_cosmos_db_no_sql.py
@@ -7,8 +7,8 @@ from typing import Any, Dict, List, Tuple
 
 import pytest
 from langchain_core.documents import Document
-from langchain_community.embeddings import AzureOpenAIEmbeddings
 
+from langchain_community.embeddings import AzureOpenAIEmbeddings
 from langchain_community.vectorstores.azure_cosmos_db_no_sql import (
     AzureCosmosDBNoSqlVectorSearch,
     Condition,
