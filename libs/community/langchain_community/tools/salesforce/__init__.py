58847da2ae3d68a3d6f02cdd5aa73e90bf8f8339
colesmcintosh <colemcintosh6@gmail.com>
2025-02-11 23:41:20 -0700

    community: export Salesforce tool classes
    Add exports for Salesforce tool classes in the __init__.py file, making them easily importable from the package

--- libs/community/langchain_community/tools/salesforce/__init__.py
+++ libs/community/langchain_community/tools/salesforce/__init__.py
@@ -1,1 +1,15 @@
 """Salesforce API toolkit."""
+
+from langchain_community.tools.salesforce.tool import (
+    BaseSalesforceTool,
+    InfoSalesforceTool,
+    ListSalesforceTool,
+    QuerySalesforceTool,
+)
+
+__all__ = [
+    "BaseSalesforceTool",
+    "InfoSalesforceTool",
+    "ListSalesforceTool",
+    "QuerySalesforceTool",
+]
