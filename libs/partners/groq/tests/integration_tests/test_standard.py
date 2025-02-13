6d51a8e0b6773b3841b5fd63416cd826b6b8f21b
ccurme <chester.curme@gmail.com>
2025-02-13 15:49:50 -0800

    groq[patch]: remove xfails (#29794)
    These appear to pass.

--- libs/partners/groq/tests/integration_tests/test_standard.py
+++ libs/partners/groq/tests/integration_tests/test_standard.py
@@ -48,26 +48,3 @@ class TestGroqLlama(BaseTestGroq):
     @property
     def supports_json_mode(self) -> bool:
         return False  # Not supported in streaming mode
-
-    @pytest.mark.xfail(
-        reason=("Fails with 'Failed to call a function. Please adjust your prompt.'")
-    )
-    def test_tool_calling_with_no_arguments(self, model: BaseChatModel) -> None:
-        super().test_tool_calling_with_no_arguments(model)
-
-    @pytest.mark.xfail(
-        reason=("Fails with 'Failed to call a function. Please adjust your prompt.'")
-    )
-    def test_tool_message_histories_string_content(
-        self, model: BaseChatModel, my_adder_tool: BaseTool
-    ) -> None:
-        super().test_tool_message_histories_string_content(model, my_adder_tool)
-
-    @pytest.mark.xfail(
-        reason=(
-            "Sometimes fails with 'Failed to call a function. "
-            "Please adjust your prompt.'"
-        )
-    )
-    def test_bind_runnables_as_tools(self, model: BaseChatModel) -> None:
-        super().test_bind_runnables_as_tools(model)
