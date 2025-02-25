738a22de15cd2962d45b03457a111021c0ded0f4
1ndigo <1090891928@qq.com>
2025-02-25 18:42:58 +0800

    Create test_openwebui.py

---
+++ libs/community/tests/integration_tests/chat_models/test_openwebui.py
@@ -0,0 +1,15 @@
+"""Test OpenWebUI Chat Model."""
+
+from langchain_core.messages import BaseMessage, HumanMessage
+
+from langchain_community.chat_models.openwebui import OpenWebUIAI
+
+def test_default_call() -> None:
+    """Test default model call."""
+    llm = OpenWebUIAI(
+        temperature=0.1,
+        model_name="gemini-2.0-pro-exp-02-05",
+    )
+    response = llm.invoke(input=[HumanMessage(content="Hello")])
+    assert isinstance(response, BaseMessage)
+    assert isinstance(response.content, str)
