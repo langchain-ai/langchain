68eb8a8e6978cd2b38dffabb9ac4c7e570439ab8
Really Him <hesereallyhim@proton.me>
2025-03-07 03:13:08 -0500

    fix: remove components from MDXComponents if not used in ipynb

--- docs/src/theme/MDXComponents.js
+++ docs/src/theme/MDXComponents.js
@@ -3,14 +3,9 @@ import MDXComponents from "@theme-original/MDXComponents";
 import ChatModelTabs from "./ChatModelTabs.js"
 import CodeBlock from "./CodeBlock";
 import Compatibility from "./Compatibility.js";
-import DocPaginator from "./DocPaginator";
 import Div from "./Div.js";
 import EmbeddingTabs from "./EmbeddingTabs.js";
-import FeatureTables from "./FeatureTables.js";
-import People from "./People.js";
 import Prerequisites from "./Prerequisites.js";
-import SearchBar from "./SearchBar";
-import SearchPage from "./SearchPage";
 import Tabs from "@theme-original/Tabs";
 import TabItem from "@theme-original/TabItem";
 import VectorStoreTabs from "./VectorStoreTabs.js";
@@ -21,15 +16,10 @@ export default {
   ChatModelTabs,
   CodeBlock,
   Compatibility,
-  DocPaginator,
   Div,
   EmbeddingTabs,
-  FeatureTables,
   Fragment,
-  People,
   Prerequisites,
-  SearchBar,
-  SearchPage,
   TabItem,
   Tabs,
   VectorStoreTabs,
