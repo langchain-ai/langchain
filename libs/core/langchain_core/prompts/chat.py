d2746a49017023678ab7cede8f40cfa7770edefe
Thommy257 <37929273+Thommy257@users.noreply.github.com>
2025-03-12 10:03:43 +0100

    remove attribute check

--- libs/core/langchain_core/prompts/chat.py
+++ libs/core/langchain_core/prompts/chat.py
@@ -2,7 +2,6 @@
 
 from __future__ import annotations
 
-import contextlib
 from abc import ABC, abstractmethod
 from pathlib import Path
 from typing import (
@@ -1042,17 +1041,17 @@ class ChatPromptTemplate(BaseChatPromptTemplate):
             Combined prompt template.
         """
         partials = {**self.partial_variables}
-        with contextlib.suppress(AttributeError):
-            partials = {**partials, **other.partial_variables}
 
         # Allow for easy combining
         if isinstance(other, ChatPromptTemplate):
+            partials = {**partials, **other.partial_variables}
             return ChatPromptTemplate(messages=self.messages + other.messages).partial(
                 **partials
             )  # type: ignore[call-arg]
         elif isinstance(
             other, (BaseMessagePromptTemplate, BaseMessage, BaseChatPromptTemplate)
         ):
+            partials = {**partials, **other.partial_variables}
             return ChatPromptTemplate(messages=self.messages + [other]).partial(
                 **partials
             )  # type: ignore[call-arg]
