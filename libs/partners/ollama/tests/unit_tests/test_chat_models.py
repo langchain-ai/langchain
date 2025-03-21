b30b1a8545958f5e1366180fd000df7eafe29dd4
Ryan Stewart <ryanstewart@Ryans-MacBook-Pro.local>
2025-03-20 23:46:12 -0400

    add test to ensure arbitrary roles accepted

--- libs/partners/ollama/tests/unit_tests/test_chat_models.py
+++ libs/partners/ollama/tests/unit_tests/test_chat_models.py
@@ -1,12 +1,16 @@
 """Test chat model integration."""
 
-from typing import Dict, Type
+from contextlib import contextmanager
+from typing import Any, Dict, Type
 
 from langchain_tests.unit_tests import ChatModelUnitTests
 
+from httpx import Request, Response
+import httpx
 from langchain_ollama.chat_models import ChatOllama
+from langchain_core.messages import ChatMessage
 
-
+import pytest
 class TestChatOllama(ChatModelUnitTests):
     @property
     def chat_model_class(self) -> Type[ChatOllama]:
@@ -15,3 +19,24 @@ class TestChatOllama(ChatModelUnitTests):
     @property
     def chat_model_params(self) -> Dict:
         return {"model": "llama3-groq-tool-use"}
+
+@contextmanager
+def _mock_httpx_client_stream(*args: Any, **kwargs: Any):
+  yield Response(status_code=200, content='{"message": {"role":"assistant","content":"The meaning of life is..."}}', request=Request(method="POST", url="http://whocares:11434")) 
+
+def test_arbitrary_roles_accepted_in_chatmessages(monkeypatch: pytest.MonkeyPatch):
+    monkeypatch.setattr(httpx.Client, 'stream', _mock_httpx_client_stream)
+
+    llm = ChatOllama(
+        base_url    = "http://whocares:11434",
+        model       = "granite3.2",
+        verbose     = True,
+    )
+
+    messages = [
+                    ChatMessage(role="somerandomrole", content="I'm ok with you adding any role message now!"),
+                    ChatMessage(role="control", content="thinking"),
+                    ChatMessage(role="user",content="What is the meaning of life?")
+                ]
+
+    llm.invoke(messages)
