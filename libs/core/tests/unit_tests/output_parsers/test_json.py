9870c04b5119a4e0b1a6ce9cbd0acdf05665c672
dongfangduoshou123 <471747996@qq.com>
2025-04-01 11:00:27 +0800

    add unit test sample for json output parser bug fix
    add unit test sample for json output parser bug fix

--- libs/core/tests/unit_tests/output_parsers/test_json.py
+++ libs/core/tests/unit_tests/output_parsers/test_json.py
@@ -243,6 +243,7 @@ TEST_CASES_PARTIAL = [
     ('{"foo": "bar", "bar"', '{"foo": "bar"}'),
     ('{"foo": "bar", ', '{"foo": "bar"}'),
     ('{"foo":"bar\\', '{"foo": "bar"}'),
+    (']{"foo":"bar}', '{"foo": "bar"}'),
 ]
 
 
