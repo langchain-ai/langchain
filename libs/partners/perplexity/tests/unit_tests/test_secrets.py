f52a5fa7a5e5574736d39bf924cfd5bdf90a3703
Sydney Runkle <54324534+sydney-runkle@users.noreply.github.com>
2025-04-02 11:05:17 -0400

    Apply suggestions from code review
    Co-authored-by: Eugene Yurtsev <eyurtsev@gmail.com>

--- libs/partners/perplexity/tests/unit_tests/test_secrets.py
+++ libs/partners/perplexity/tests/unit_tests/test_secrets.py
@@ -2,6 +2,6 @@ from langchain_perplexity import ChatPerplexity
 
 
 def test_chat_perplexity_secrets() -> None:
-    o = ChatPerplexity(model="llama-3.1-sonar-small-128k-online", pplx_api_key="foo")  # type: ignore[call-arg]
-    s = str(o)
-    assert "foo" not in s
+    model = ChatPerplexity(model="llama-3.1-sonar-small-128k-online", pplx_api_key="foo")  # type: ignore[call-arg]
+    model_str = str(model)
+    assert "foo" not in model_str
