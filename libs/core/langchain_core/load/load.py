3814bd1ea78348d9617c84e12c5c78356cb17c3f
Sydney Runkle <54324534+sydney-runkle@users.noreply.github.com>
2025-04-03 12:09:14 -0400

    partners: Add Perplexity Chat Integration (#30618)
    Perplexity's importance in the space has been growing, so we think it's
    time to add an official integration!
    
    Note: following the release of `langchain-perplexity` to `pypi`, we
    should be able to add `perplexity` as an extra in
    `libs/langchain/pyproject.toml`, but we're blocked by a circular import
    for now.
    
    ---------
    
    Co-authored-by: Eugene Yurtsev <eyurtsev@gmail.com>
    Co-authored-by: Chester Curme <chester.curme@gmail.com>

--- libs/core/langchain_core/load/load.py
+++ libs/core/langchain_core/load/load.py
@@ -28,6 +28,7 @@ DEFAULT_NAMESPACES = [
     "langchain_fireworks",
     "langchain_xai",
     "langchain_sambanova",
+    "langchain_perplexity",
 ]
 # Namespaces for which only deserializing via the SERIALIZABLE_MAPPING is allowed.
 # Load by path is not allowed.
