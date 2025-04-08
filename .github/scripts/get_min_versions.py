c575a2dad9a937d5c3cbae9287901f14683370b6
Sydney Runkle <sydneymarierunkle@gmail.com>
2025-04-08 08:57:18 -0400

    revert CI change

--- .github/scripts/get_min_versions.py
+++ .github/scripts/get_min_versions.py
@@ -103,7 +103,6 @@ def _check_python_version_from_requirement(
         return True
     else:
         marker_str = str(requirement.marker)
-        
         if "python_version" or "python_full_version" in marker_str:
             python_version_str = "".join(
                 char
