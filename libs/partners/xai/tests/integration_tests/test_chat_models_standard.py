1383c1736a92704e553b0633b6607a5fcdce9941
Chester Curme <chester.curme@gmail.com>
2025-04-09 19:38:07 -0400

    support reasoning content

--- libs/partners/xai/tests/integration_tests/test_chat_models_standard.py
+++ libs/partners/xai/tests/integration_tests/test_chat_models_standard.py
@@ -1,9 +1,10 @@
 """Standard LangChain interface tests"""
 
-from typing import Type
+from typing import Optional, Type
 
 import pytest  # type: ignore[import-not-found]
 from langchain_core.language_models import BaseChatModel
+from langchain_core.messages import AIMessageChunk, BaseMessageChunk
 from langchain_core.rate_limiters import InMemoryRateLimiter
 from langchain_tests.integration_tests import (  # type: ignore[import-not-found]
     ChatModelIntegrationTests,  # type: ignore[import-not-found]
@@ -26,10 +27,28 @@ class TestXAIStandard(ChatModelIntegrationTests):
     @property
     def chat_model_params(self) -> dict:
         return {
-            "model": "grok-2",
+            "model": "grok-3",
             "rate_limiter": rate_limiter,
         }
 
     @pytest.mark.xfail(reason="Not yet supported.")
     def test_usage_metadata_streaming(self, model: BaseChatModel) -> None:
         super().test_usage_metadata_streaming(model)
+
+
+def test_reasoning_content() -> None:
+    """Test reasoning content."""
+    chat_model = ChatXAI(
+        model="grok-3-mini-beta",
+        reasoning_effort="low",
+    )
+    response = chat_model.invoke("What is 3^3?")
+    assert response.content
+    assert response.additional_kwargs["reasoning_content"]
+
+    # Test streaming
+    full: Optional[BaseMessageChunk] = None
+    for chunk in chat_model.stream("What is 3^3?"):
+        full = chunk if full is None else full + chunk
+    assert isinstance(full, AIMessageChunk)
+    assert full.additional_kwargs["reasoning_content"]
