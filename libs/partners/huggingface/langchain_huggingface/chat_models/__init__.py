4787d9eaa7db4b1143ef63aa7308019c4a4e2322
hanouticelina <hanouticelina@gmail.com>
2025-04-09 11:16:28 +0200

    format

--- libs/partners/huggingface/langchain_huggingface/chat_models/__init__.py
+++ libs/partners/huggingface/langchain_huggingface/chat_models/__init__.py
@@ -1,9 +1,6 @@
 from langchain_huggingface.chat_models.huggingface import (  # type: ignore[import-not-found]
     ChatHuggingFace,
-    _convert_dict_to_message
+    _convert_dict_to_message,
 )
 
-__all__ = [
-    "ChatHuggingFace",
-    "_convert_dict_to_message"
-]
+__all__ = ["ChatHuggingFace", "_convert_dict_to_message"]
