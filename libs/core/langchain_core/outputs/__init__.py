e90de1897a0990dc4398f9ea1ad624a36e2f8bea
Sydney Runkle <sydneymarierunkle@gmail.com>
2025-04-11 15:34:11 -0400

    linting

--- libs/core/langchain_core/outputs/__init__.py
+++ libs/core/langchain_core/outputs/__init__.py
@@ -57,10 +57,11 @@ _dynamic_imports = {
 
 def __getattr__(attr_name: str) -> object:
     module_name = _dynamic_imports.get(attr_name)
+    package = __spec__.parent  # type: ignore[name-defined]
     if module_name == "__module__" or module_name is None:
-        result = import_module(f".{attr_name}", package=__spec__.parent)
+        result = import_module(f".{attr_name}", package=package)
     else:
-        module = import_module(f".{module_name}", package=__spec__.parent)
+        module = import_module(f".{module_name}", package=package)
         result = getattr(module, attr_name)
     globals()[attr_name] = result
     return result
