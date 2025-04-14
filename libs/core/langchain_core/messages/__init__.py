4f69094b51bd1d637c15433050b6a793202354c4
Sydney Runkle <54324534+sydney-runkle@users.noreply.github.com>
2025-04-14 08:57:54 -0400

    core[performance]: use custom `__getattr__` in `__init__.py` files for lazy imports (#30769)
    Most easily reviewed with the "hide whitespace" option toggled.
    
    Seeing 10-50% speed ups in import time for common structures 🚀 
    
    The general purpose of this PR is to lazily import structures within
    `langchain_core.XXX_module.__init__.py` so that we're not eagerly
    importing expensive dependencies (`pydantic`, `requests`, etc).
    
    Analysis of flamegraphs generated with `importtime` motivated these
    changes. For example, the one below demonstrates that importing
    `HumanMessage` accidentally triggered imports for `importlib.metadata`,
    `requests`, etc.
    
    There's still much more to do on this front, and we can start digging
    into our own internal code for optimizations now that we're less
    concerned about external imports.
    
    <img width="1210" alt="Screenshot 2025-04-11 at 1 10 54 PM"
    src="https://github.com/user-attachments/assets/112a3fe7-24a9-4294-92c1-d5ae64df839e"
    />
    
    I've tracked the improvements with some local benchmarks:
    
    ## `pytest-benchmark` results
    
    | Name | Before (s) | After (s) | Delta (s) | % Change |
    
    |-----------------------------|------------|-----------|-----------|----------|
    | Document | 2.8683 | 1.2775 | -1.5908 | -55.46% |
    | HumanMessage | 2.2358 | 1.1673 | -1.0685 | -47.79% |
    | ChatPromptTemplate | 5.5235 | 2.9709 | -2.5526 | -46.22% |
    | Runnable | 2.9423 | 1.7793 | -1.163 | -39.53% |
    | InMemoryVectorStore | 3.1180 | 1.8417 | -1.2763 | -40.93% |
    | RunnableLambda | 2.7385 | 1.8745 | -0.864 | -31.55% |
    | tool | 5.1231 | 4.0771 | -1.046 | -20.42% |
    | CallbackManager | 4.2263 | 3.4099 | -0.8164 | -19.32% |
    | LangChainTracer | 3.8394 | 3.3101 | -0.5293 | -13.79% |
    | BaseChatModel | 4.3317 | 3.8806 | -0.4511 | -10.41% |
    | PydanticOutputParser | 3.2036 | 3.2995 | 0.0959 | 2.99% |
    | InMemoryRateLimiter | 0.5311 | 0.5995 | 0.0684 | 12.88% |
    
    Note the lack of change for `InMemoryRateLimiter` and
    `PydanticOutputParser` is just random noise, I'm getting comparable
    numbers locally.
    
    ## Local CodSpeed results
    
    We're still working on configuring CodSpeed on CI. The local usage
    produced similar results.

--- libs/core/langchain_core/messages/__init__.py
+++ libs/core/langchain_core/messages/__init__.py
@@ -15,42 +15,46 @@
 
 """  # noqa: E501
 
-from langchain_core.messages.ai import (
-    AIMessage,
-    AIMessageChunk,
-)
-from langchain_core.messages.base import (
-    BaseMessage,
-    BaseMessageChunk,
-    merge_content,
-    message_to_dict,
-    messages_to_dict,
-)
-from langchain_core.messages.chat import ChatMessage, ChatMessageChunk
-from langchain_core.messages.function import FunctionMessage, FunctionMessageChunk
-from langchain_core.messages.human import HumanMessage, HumanMessageChunk
-from langchain_core.messages.modifier import RemoveMessage
-from langchain_core.messages.system import SystemMessage, SystemMessageChunk
-from langchain_core.messages.tool import (
-    InvalidToolCall,
-    ToolCall,
-    ToolCallChunk,
-    ToolMessage,
-    ToolMessageChunk,
-)
-from langchain_core.messages.utils import (
-    AnyMessage,
-    MessageLikeRepresentation,
-    _message_from_dict,
-    convert_to_messages,
-    convert_to_openai_messages,
-    filter_messages,
-    get_buffer_string,
-    merge_message_runs,
-    message_chunk_to_message,
-    messages_from_dict,
-    trim_messages,
-)
+from importlib import import_module
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from langchain_core.messages.ai import (
+        AIMessage,
+        AIMessageChunk,
+    )
+    from langchain_core.messages.base import (
+        BaseMessage,
+        BaseMessageChunk,
+        merge_content,
+        message_to_dict,
+        messages_to_dict,
+    )
+    from langchain_core.messages.chat import ChatMessage, ChatMessageChunk
+    from langchain_core.messages.function import FunctionMessage, FunctionMessageChunk
+    from langchain_core.messages.human import HumanMessage, HumanMessageChunk
+    from langchain_core.messages.modifier import RemoveMessage
+    from langchain_core.messages.system import SystemMessage, SystemMessageChunk
+    from langchain_core.messages.tool import (
+        InvalidToolCall,
+        ToolCall,
+        ToolCallChunk,
+        ToolMessage,
+        ToolMessageChunk,
+    )
+    from langchain_core.messages.utils import (
+        AnyMessage,
+        MessageLikeRepresentation,
+        _message_from_dict,
+        convert_to_messages,
+        convert_to_openai_messages,
+        filter_messages,
+        get_buffer_string,
+        merge_message_runs,
+        message_chunk_to_message,
+        messages_from_dict,
+        trim_messages,
+    )
 
 __all__ = [
     "AIMessage",
@@ -86,3 +90,54 @@ __all__ = [
     "trim_messages",
     "convert_to_openai_messages",
 ]
+
+_dynamic_imports = {
+    "AIMessage": "ai",
+    "AIMessageChunk": "ai",
+    "BaseMessage": "base",
+    "BaseMessageChunk": "base",
+    "merge_content": "base",
+    "message_to_dict": "base",
+    "messages_to_dict": "base",
+    "ChatMessage": "chat",
+    "ChatMessageChunk": "chat",
+    "FunctionMessage": "function",
+    "FunctionMessageChunk": "function",
+    "HumanMessage": "human",
+    "HumanMessageChunk": "human",
+    "RemoveMessage": "modifier",
+    "SystemMessage": "system",
+    "SystemMessageChunk": "system",
+    "InvalidToolCall": "tool",
+    "ToolCall": "tool",
+    "ToolCallChunk": "tool",
+    "ToolMessage": "tool",
+    "ToolMessageChunk": "tool",
+    "AnyMessage": "utils",
+    "MessageLikeRepresentation": "utils",
+    "_message_from_dict": "utils",
+    "convert_to_messages": "utils",
+    "convert_to_openai_messages": "utils",
+    "filter_messages": "utils",
+    "get_buffer_string": "utils",
+    "merge_message_runs": "utils",
+    "message_chunk_to_message": "utils",
+    "messages_from_dict": "utils",
+    "trim_messages": "utils",
+}
+
+
+def __getattr__(attr_name: str) -> object:
+    module_name = _dynamic_imports.get(attr_name)
+    package = __spec__.parent  # type: ignore[name-defined]
+    if module_name == "__module__" or module_name is None:
+        result = import_module(f".{attr_name}", package=package)
+    else:
+        module = import_module(f".{module_name}", package=package)
+        result = getattr(module, attr_name)
+    globals()[attr_name] = result
+    return result
+
+
+def __dir__() -> list[str]:
+    return list(__all__)
--- libs/core/langchain_core/outputs/__init__.py
+++ libs/core/langchain_core/outputs/__init__.py
@@ -21,11 +21,18 @@ in the AIMessage object, it is recommended to access it from there rather than
 from the `LLMResult` object.
 """
 
-from langchain_core.outputs.chat_generation import ChatGeneration, ChatGenerationChunk
-from langchain_core.outputs.chat_result import ChatResult
-from langchain_core.outputs.generation import Generation, GenerationChunk
-from langchain_core.outputs.llm_result import LLMResult
-from langchain_core.outputs.run_info import RunInfo
+from importlib import import_module
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from langchain_core.outputs.chat_generation import (
+        ChatGeneration,
+        ChatGenerationChunk,
+    )
+    from langchain_core.outputs.chat_result import ChatResult
+    from langchain_core.outputs.generation import Generation, GenerationChunk
+    from langchain_core.outputs.llm_result import LLMResult
+    from langchain_core.outputs.run_info import RunInfo
 
 __all__ = [
     "ChatGeneration",
@@ -36,3 +43,29 @@ __all__ = [
     "LLMResult",
     "RunInfo",
 ]
+
+_dynamic_imports = {
+    "ChatGeneration": "chat_generation",
+    "ChatGenerationChunk": "chat_generation",
+    "ChatResult": "chat_result",
+    "Generation": "generation",
+    "GenerationChunk": "generation",
+    "LLMResult": "llm_result",
+    "RunInfo": "run_info",
+}
+
+
+def __getattr__(attr_name: str) -> object:
+    module_name = _dynamic_imports.get(attr_name)
+    package = __spec__.parent  # type: ignore[name-defined]
+    if module_name == "__module__" or module_name is None:
+        result = import_module(f".{attr_name}", package=package)
+    else:
+        module = import_module(f".{module_name}", package=package)
+        result = getattr(module, attr_name)
+    globals()[attr_name] = result
+    return result
+
+
+def __dir__() -> list[str]:
+    return list(__all__)
