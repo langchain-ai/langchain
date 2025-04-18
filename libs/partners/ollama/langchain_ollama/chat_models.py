42c9d0426f679a9ca155aa90e87f4941d844aa87
Chester Curme <chester.curme@gmail.com>
2025-04-18 09:37:20 -0400

    revert changes to structured output

--- libs/partners/ollama/langchain_ollama/chat_models.py
+++ libs/partners/ollama/langchain_ollama/chat_models.py
@@ -896,7 +896,6 @@ class ChatOllama(BaseChatModel):
         *,
         method: Literal["function_calling", "json_mode", "json_schema"] = "json_schema",
         include_raw: bool = False,
-        strict: Optional[bool] = None,
         **kwargs: Any,
     ) -> Runnable[LanguageModelInput, Union[dict, BaseModel]]:
         """Model wrapper that returns outputs formatted to match the given schema.
@@ -935,22 +934,6 @@ class ChatOllama(BaseChatModel):
                 response will be returned. If an error occurs during output parsing it
                 will be caught and returned as well. The final output is always a dict
                 with keys "raw", "parsed", and "parsing_error".
-
-            strict:
-                (This parameter is currently unsupported by Ollama and has been added for future compatibility)
-                - True:
-                    Model output is guaranteed to exactly match the schema.
-                - False:
-                    Input schema will not be validated and model output will not be
-                    validated.
-                - None:
-                    ``strict`` argument will not be passed to the model.
-
-                If schema is specified via TypedDict or JSON schema, ``strict`` is not
-                enabled by default. Pass ``strict=True`` to enable it.
-
-                Note: ``strict`` can only be non-null if ``method`` is
-                ``"json_schema"`` or ``"function_calling"``.
 
             kwargs: Additional keyword args aren't supported.
 
@@ -1177,7 +1160,6 @@ class ChatOllama(BaseChatModel):
                     "kwargs": {"method": method},
                     "schema": formatted_tool,
                 },
-                strict=strict,
             )
             if is_pydantic_schema:
                 output_parser: Runnable = PydanticToolsParser(
