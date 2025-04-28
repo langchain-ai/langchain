ee5103613832c4990239edc0b551b588f6d19e02
Chester Curme <chester.curme@gmail.com>
2025-04-28 15:38:22 -0400

    delete community and update readme

--- libs/community/langchain_community/embeddings/sentence_transformer.py
+++
@@ -1,5 +0,0 @@
-"""HuggingFace sentence_transformer embedding models."""
-
-from langchain_community.embeddings.huggingface import HuggingFaceEmbeddings
-
-SentenceTransformerEmbeddings = HuggingFaceEmbeddings
