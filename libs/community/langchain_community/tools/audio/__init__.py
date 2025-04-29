9ff5b5d2824794c82f3df9032f09a31fe007eb19
ccurme <chester.curme@gmail.com>
2025-04-29 09:22:04 -0400

    community: move to separate repo (#31060)
    langchain-community is moving to
    https://github.com/langchain-ai/langchain-community

--- libs/community/langchain_community/tools/audio/__init__.py
+++
@@ -1,7 +0,0 @@
-from langchain_community.tools.audio.huggingface_text_to_speech_inference import (
-    HuggingFaceTextToSpeechModelInference,
-)
-
-__all__ = [
-    "HuggingFaceTextToSpeechModelInference",
-]
--- libs/community/langchain_community/tools/bing_search/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Bing Search API toolkit."""
-
-from langchain_community.tools.bing_search.tool import BingSearchResults, BingSearchRun
-
-__all__ = ["BingSearchRun", "BingSearchResults"]
