dd73e444dfb2cf103923eaebf805e83ba2b7a5b4
CtrlMj <Alikhanimjd@gmail.com>
2025-05-03 18:40:52 -0400

    run make format

--- libs/core/langchain_core/tools/base.py
+++ libs/core/langchain_core/tools/base.py
@@ -1078,9 +1078,11 @@ def get_all_basemodel_annotations(
     # cls has no subscript: cls = FooBar
     if isinstance(cls, type):
         annotations: dict[str, type] = {}
-        fields = getattr(cls, "model_fields", {}) or getattr(cls, "__fields__", {}) # pydantic v2+ or pydantic v1
+        fields = getattr(cls, "model_fields", {}) or getattr(
+            cls, "__fields__", {}
+        )  # pydantic v2+ or pydantic v1
         # create map from aliases to the original variable name (key)
-        field_names_map = {} 
+        field_names_map = {}
         for key, field in fields.items():
             if field.alias:
                 field_names_map[field.alias] = key
@@ -1090,7 +1092,9 @@ def get_all_basemodel_annotations(
         for name, param in inspect.signature(cls).parameters.items():
             # Exclude hidden init args added by pydantic Config. For example if
             # BaseModel(extra="allow") then "extra_data" will part of init sig.
-            if name not in field_names_map: # using the {alias: key} map to check for fields
+            if (
+                name not in field_names_map
+            ):  # using the {alias: key} map to check for fields
                 continue
             annotations[field_names_map[name]] = param.annotation
         orig_bases: tuple = getattr(cls, "__orig_bases__", ())
