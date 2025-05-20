0f28c5ca1876d1f1c785ebc335374ba1b6e3faef
Chester Curme <chester.curme@gmail.com>
2025-05-20 11:13:16 -0400

    lint

--- libs/partners/openai/tests/conftest.py
+++ libs/partners/openai/tests/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from langchain_tests.conftest import _base_vcr_config
+from langchain_tests.conftest import _base_vcr_config as _base_vcr_config
 
 _EXTRA_HEADERS = [
     ("openai-organization", "PLACEHOLDER"),
@@ -7,13 +7,13 @@ _EXTRA_HEADERS = [
 ]
 
 
-def remove_response_headers(response):
+def remove_response_headers(response: dict) -> dict:
     response["headers"] = {}
     return response
 
 
 @pytest.fixture(scope="session")
-def vcr_config(_base_vcr_config: dict) -> dict:
+def vcr_config(_base_vcr_config: dict) -> dict:  # noqa: F811
     """
     Extend the default configuration coming from langchain_tests.
     """
