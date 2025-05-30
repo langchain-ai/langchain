05b2d28752b1292eaac62ffadc3ab7ac3dd8a005
keenborder786 <21110290@lums.edu.pk>
2025-05-31 02:05:09 +0500

    [fix]: model name

--- libs/partners/openai/langchain_openai/chat_models/base.py
+++ libs/partners/openai/langchain_openai/chat_models/base.py
@@ -1238,7 +1238,7 @@ class BaseChatOpenAI(BaseChatModel):
             encoding = tiktoken.encoding_for_model(model)
         except KeyError:
             encoder = "cl100k_base"
-            if self.model_name.startswith("gpt-4o"):
+            if self.model_name.startswith("gpt-4o") or self.model_name.startswith("gpt-4.1"):
                 encoder = "o200k_base"
             encoding = tiktoken.get_encoding(encoder)
         return model, encoding
