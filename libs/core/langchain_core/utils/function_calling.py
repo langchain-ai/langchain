1a055806d3bcecf9885d9d805aebd3aa67014c4b
Mason Daugherty <github@mdrxy.com>
2025-06-20 15:22:01 -0700

    fix: "ToolMessage: contains..." was incorrectly bolded due to whitespace issue

--- libs/core/langchain_core/utils/function_calling.py
+++ libs/core/langchain_core/utils/function_calling.py
@@ -634,7 +634,7 @@ def tool_example_to_messages(
     1) HumanMessage: contains the content from which content should be extracted.
     2) AIMessage: contains the extracted information from the model
     3) ToolMessage: contains confirmation to the model that the model requested a tool
-        correctly.
+       correctly.
 
     If `ai_response` is specified, there will be a final AIMessage with that response.
 
