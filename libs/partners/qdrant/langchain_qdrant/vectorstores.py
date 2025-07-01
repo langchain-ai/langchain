6cadb9a0365f7a28fd680d671536a84d63385f0e
Mason Daugherty <github@mdrxy.com>
2025-07-01 14:40:58 -0400

    format

--- libs/partners/qdrant/langchain_qdrant/vectorstores.py
+++ libs/partners/qdrant/langchain_qdrant/vectorstores.py
@@ -105,7 +105,7 @@ class Qdrant(VectorStore):
 
         if embeddings is None and embedding_function is None:
             raise ValueError(
-                "`embeddings` value can't be None. Pass `Embeddings` instance."
+                "`embeddings` value can't be None. Pass `embeddings` instance."
             )
 
         if embeddings is not None and embedding_function is not None:
@@ -161,7 +161,7 @@ class Qdrant(VectorStore):
                 uuid-like strings.
             batch_size:
                 How many vectors upload per-request.
-                Default: 64
+                Default: ``64``
 
         Returns:
             List of ids from adding the texts into the vectorstore.
@@ -196,7 +196,7 @@ class Qdrant(VectorStore):
                 uuid-like strings.
             batch_size:
                 How many vectors upload per-request.
-                Default: 64
+                Default: ``64``
 
         Returns:
             List of ids from adding the texts into the vectorstore.
