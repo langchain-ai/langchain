4215261be1cdb7df3bf74d1f291977fff486b1a3
Christophe Bornet <cbornet@hotmail.com>
2025-07-07 19:30:48 +0200

    core: Cleanup pyproject (#31857)
    * Reorganize some toml properties
    * Fix some E501: line too long
    
    Co-authored-by: Mason Daugherty <mason@langchain.dev>

--- libs/core/tests/unit_tests/prompts/test_chat.py
+++ libs/core/tests/unit_tests/prompts/test_chat.py
@@ -781,7 +781,8 @@ async def test_chat_tmpl_from_messages_multipart_formatting_with_path() -> None:
     )
     with pytest.raises(
         ValueError,
-        match="Loading images from 'path' has been removed as of 0.3.15 for security reasons.",
+        match="Loading images from 'path' has been removed "
+        "as of 0.3.15 for security reasons.",
     ):
         template.format_messages(
             name="R2D2",
@@ -791,7 +792,8 @@ async def test_chat_tmpl_from_messages_multipart_formatting_with_path() -> None:
 
     with pytest.raises(
         ValueError,
-        match="Loading images from 'path' has been removed as of 0.3.15 for security reasons.",
+        match="Loading images from 'path' has been removed "
+        "as of 0.3.15 for security reasons.",
     ):
         await template.aformat_messages(
             name="R2D2",
