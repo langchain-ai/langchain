d75d2d43e9b2bf0a0ce86309259204d98b8adbe5
Ahmed Tammaa <ahmed@graph8.com>
2025-07-13 02:03:13 +0300

    [lint]

--- libs/partners/openai/langchain_openai/chat_models/base.py
+++ libs/partners/openai/langchain_openai/chat_models/base.py
@@ -1873,7 +1873,8 @@ class BaseChatOpenAI(BaseChatModel):
             
             tool_name = convert_to_openai_tool(schema)["function"]["name"]
             base_url = (
-                getattr(self, "openai_api_base", "") or getattr(self, "api_base", "") or ""
+                getattr(self, "openai_api_base", "")
+                or getattr(self, "api_base", "") or ""
             ).lower()
             # The Azure-Hosted Models routes to this function
             # so we need to set the tool_choice to required
