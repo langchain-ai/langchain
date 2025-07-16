9d232b4bf9a5e549da9e4f77754bd8fbf41b4331
Mason Daugherty <github@mdrxy.com>
2025-07-16 11:45:20 -0400

    lint

--- libs/partners/huggingface/langchain_huggingface/llms/huggingface_pipeline.py
+++ libs/partners/huggingface/langchain_huggingface/llms/huggingface_pipeline.py
@@ -445,10 +445,7 @@ class HuggingFacePipeline(BaseLLM):
                 scores: torch.FloatTensor,
                 **kwargs: Any,
             ) -> bool:
-                for stop_id in stopping_ids_list:
-                    if input_ids[0][-1] == stop_id:
-                        return True
-                return False
+                return any(input_ids[0][-1] == stop_id for stop_id in stopping_ids_list)
 
         stopping_criteria = StoppingCriteriaList([StopOnTokens()])
 
