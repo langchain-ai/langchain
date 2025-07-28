2b51bfe08035c6db59a737e12a62f7a61597a8de
Chester Curme <chester.curme@gmail.com>
2025-07-28 18:23:17 -0400

    add ChatOpenAIV1 to init

--- libs/partners/openai/langchain_openai/__init__.py
+++ libs/partners/openai/langchain_openai/__init__.py
@@ -1,10 +1,11 @@
-from langchain_openai.chat_models import AzureChatOpenAI, ChatOpenAI
+from langchain_openai.chat_models import AzureChatOpenAI, ChatOpenAI, ChatOpenAIV1
 from langchain_openai.embeddings import AzureOpenAIEmbeddings, OpenAIEmbeddings
 from langchain_openai.llms import AzureOpenAI, OpenAI
 
 __all__ = [
     "OpenAI",
     "ChatOpenAI",
+    "ChatOpenAIV1",
     "OpenAIEmbeddings",
     "AzureOpenAI",
     "AzureChatOpenAI",
--- libs/partners/openai/langchain_openai/chat_models/__init__.py
+++ libs/partners/openai/langchain_openai/chat_models/__init__.py
@@ -1,4 +1,5 @@
 from langchain_openai.chat_models.azure import AzureChatOpenAI
 from langchain_openai.chat_models.base import ChatOpenAI
+from langchain_openai.chat_models.base_v1 import ChatOpenAI as ChatOpenAIV1
 
-__all__ = ["ChatOpenAI", "AzureChatOpenAI"]
+__all__ = ["ChatOpenAI", "AzureChatOpenAI", "ChatOpenAIV1"]
