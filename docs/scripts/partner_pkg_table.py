d1930d89ac265e1713b892055263e2f79af4fdee
Chester Curme <chester.curme@gmail.com>
2025-08-10 18:20:53 -0400

    fix badges

--- docs/scripts/partner_pkg_table.py
+++ docs/scripts/partner_pkg_table.py
@@ -103,14 +103,14 @@ def package_row(p: dict) -> str:
     provider = f"[{title}]({link})" if link else title
     return (
         f"| {provider} | [{p['name']}]({p['package_url']}) | "
-        f"![Downloads](https://static.pepy.tech/personalized-badge/{p['name']}?period=month&units=abbreviation&left_color=grey&right_color=blue&left_text=%20) | "
+        f"![Downloads](https://static.pepy.tech/badge/{p['name']}/month) | "
         f"![PyPI - Version](https://img.shields.io/pypi/v/{p['name']}?style=flat-square&label=%20&color=orange) | "
         f"{js} |"
     )
 
 
 def table() -> str:
-    header = """| Provider | Package | Downloads (monthly) | Latest | [JS](https://js.langchain.com/docs/integrations/platforms/) |
+    header = """| Provider | Package | Downloads | Latest | [JS](https://js.langchain.com/docs/integrations/platforms/) |
 | :--- | :---: | :---: | :---: | :---: |
 """
     return header + "\n".join(package_row(p) for p in packages_sorted)
