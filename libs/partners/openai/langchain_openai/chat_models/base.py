671f970a695f7c7253b96ed26d35fe881300be55
sa411022 <sa411022@gmail.com>
2025-08-15 16:36:44 +0800

    Update base.py

--- libs/partners/openai/langchain_openai/chat_models/base.py
+++ libs/partners/openai/langchain_openai/chat_models/base.py
@@ -3749,11 +3749,13 @@ def _construct_responses_api_input(messages: Sequence[BaseMessage]) -> list:
                     {
                         "type": "message",
                         "role": "assistant",
-                        "content": [{
-                            "type": "output_text",
-                            "text": msg["content"],
-                            "annotations": [],
-                        }],
+                        "content": [
+                            {
+                                "type": "output_text",
+                                "text": msg["content"],
+                                "annotations": [],
+                            }
+                        ],
                     }
                 )
 
