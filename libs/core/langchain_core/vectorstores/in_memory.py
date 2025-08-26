da64ee54a326a7ebe47950d662661fa75a0540b0
PieterKok-jaam <pieter.kok@jaamautomation.com>
2025-08-26 14:30:33 +0200

    Update in_memory.py
    Added id field to Document passed to filter for InMemoryVectorStore similarity search.

--- libs/core/langchain_core/vectorstores/in_memory.py
+++ libs/core/langchain_core/vectorstores/in_memory.py
@@ -376,7 +376,7 @@ class InMemoryVectorStore(VectorStore):
             docs = [
                 doc
                 for doc in docs
-                if filter(Document(page_content=doc["text"], metadata=doc["metadata"]))
+                if filter(Document(id=doc["id"], page_content=doc["text"], metadata=doc["metadata"]))
             ]
 
         if not docs:
