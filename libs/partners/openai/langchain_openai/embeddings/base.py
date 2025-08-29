ad55c25561643b669e2138a45f89a756fd954813
zhangchao <zhangchao@feiersport.com>
2025-08-29 10:41:55 +0800

    To facilitate local embedding, make parameters 'tiktoken_enabled' and 'embedding_ctx_length' configurable

--- libs/partners/openai/langchain_openai/embeddings/base.py
+++ libs/partners/openai/langchain_openai/embeddings/base.py
@@ -215,7 +215,11 @@ class OpenAIEmbeddings(BaseModel, Embeddings):
     """Timeout for requests to OpenAI completion API. Can be float, ``httpx.Timeout`` or
         None."""
     headers: Any = None
-    tiktoken_enabled: bool = True
+    tiktoken_enabled: Optional[bool] = Field(
+        default_factory=from_env(
+            ["OPENAI_TIKTOKEN_ENABLED"], default=True
+        ),
+    )
     """Set this to False for non-OpenAI implementations of the embeddings API, e.g.
     the ``--extensions openai`` extension for ``text-generation-webui``"""
     tiktoken_model_name: Optional[str] = None
@@ -251,7 +255,11 @@ class OpenAIEmbeddings(BaseModel, Embeddings):
     http_async_client: Union[Any, None] = None
     """Optional ``httpx.AsyncClient``. Only used for async invocations. Must specify
         ``http_client`` as well if you'd like a custom client for sync invocations."""
-    check_embedding_ctx_length: bool = True
+    check_embedding_ctx_length: Optional[bool] = Field(
+        default_factory=from_env(
+            ["OPENAI_CHECK_EMBEDDING_CTX_LENGTH"], default=True
+        ),
+    )
     """Whether to check the token length of inputs and automatically split inputs
         longer than embedding_ctx_length."""
 
