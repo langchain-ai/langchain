d9c3db0ae3b1b5fcdb9247f99aa5a86a57a1970d
Sydney Runkle <sydneymarierunkle@gmail.com>
2025-09-07 21:05:36 -0400

    linting

--- libs/langchain_v1/langchain/agents/middleware/__init__.py
+++ libs/langchain_v1/langchain/agents/middleware/__init__.py
@@ -3,13 +3,13 @@
 from .human_in_the_loop import HumanInTheLoopMiddleware
 from .prompt_caching import AnthropicPromptCachingMiddleware
 from .summarization import SummarizationMiddleware
-from .types import AgentMiddleware, ModelRequest, AgentState
+from .types import AgentMiddleware, AgentState, ModelRequest
 
 __all__ = [
     "AgentMiddleware",
+    "AgentState",
     "AnthropicPromptCachingMiddleware",
     "HumanInTheLoopMiddleware",
     "ModelRequest",
-    "AgentState",
     "SummarizationMiddleware",
 ]
