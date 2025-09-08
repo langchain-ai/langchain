8e7f4161b4dd8f1b895d317205306df9089954ae
Sydney Runkle <sydneymarierunkle@gmail.com>
2025-09-07 21:03:17 -0400

    import top level

--- libs/langchain_v1/langchain/agents/middleware/__init__.py
+++ libs/langchain_v1/langchain/agents/middleware/__init__.py
@@ -3,12 +3,13 @@
 from .human_in_the_loop import HumanInTheLoopMiddleware
 from .prompt_caching import AnthropicPromptCachingMiddleware
 from .summarization import SummarizationMiddleware
-from .types import AgentMiddleware, ModelRequest
+from .types import AgentMiddleware, ModelRequest, AgentState
 
 __all__ = [
     "AgentMiddleware",
     "AnthropicPromptCachingMiddleware",
     "HumanInTheLoopMiddleware",
     "ModelRequest",
+    "AgentState",
     "SummarizationMiddleware",
 ]
