3da0377c02e12700b69379700dd567254dbf7ff4
Mason Daugherty <mason@langchain.dev>
2025-09-10 16:44:04 -0400

    fix(anthropic): update `ChatAnthropic` model in tests/doc (#32892)
    from `'claude-3-5-sonnet-latest'` to `'claude-3-5-haiku-latest'` since
    sonnet is deprecated

--- libs/partners/anthropic/tests/integration_tests/test_chat_models.py
+++ libs/partners/anthropic/tests/integration_tests/test_chat_models.py
@@ -895,13 +895,13 @@ def test_image_tool_calling() -> None:
             ],
         ),
     ]
-    llm = ChatAnthropic(model="claude-3-5-sonnet-latest")  # type: ignore[call-arg]
+    llm = ChatAnthropic(model="claude-3-5-haiku-latest")  # type: ignore[call-arg]
     llm.bind_tools([color_picker]).invoke(messages)
 
 
 @pytest.mark.vcr
 def test_web_search() -> None:
-    llm = ChatAnthropic(model="claude-3-5-sonnet-latest")  # type: ignore[call-arg]
+    llm = ChatAnthropic(model="claude-3-5-haiku-latest")  # type: ignore[call-arg]
 
     tool = {"type": "web_search_20250305", "name": "web_search", "max_uses": 1}
     llm_with_tools = llm.bind_tools([tool])
