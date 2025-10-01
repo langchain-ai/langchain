7e6bbefcc8efc81caba5ef5046ed6cb2734ebcb7
Mason Daugherty <github@mdrxy.com>
2025-10-01 18:59:11 -0400

    undo vertex

--- libs/core/langchain_core/messages/base.py
+++ libs/core/langchain_core/messages/base.py
@@ -211,9 +211,6 @@ class BaseMessage(Serializable):
         from langchain_core.messages.block_translators.google_genai import (  # noqa: PLC0415
             _convert_to_v1_from_genai_input,
         )
-        from langchain_core.messages.block_translators.google_vertexai import (  # noqa: PLC0415
-            _convert_to_v1_from_vertex_input,
-        )
         from langchain_core.messages.block_translators.langchain_v0 import (  # noqa: PLC0415
             _convert_v0_multimodal_input_to_v1,
         )
@@ -255,7 +252,6 @@ class BaseMessage(Serializable):
             _convert_to_v1_from_chat_completions_input,
             _convert_to_v1_from_anthropic_input,
             _convert_to_v1_from_genai_input,
-            _convert_to_v1_from_vertex_input,
             _convert_to_v1_from_converse_input,
         ]:
             blocks = parsing_step(blocks)
--- libs/core/langchain_core/messages/block_translators/google_vertexai.py
+++ libs/core/langchain_core/messages/block_translators/google_vertexai.py
@@ -1,7 +1,6 @@
 """Derivations of standard content blocks from Google (VertexAI) content."""
 
-from collections.abc import Iterable
-from typing import Any, cast
+import warnings
 
 from langchain_core.messages import AIMessage, AIMessageChunk
 from langchain_core.messages import content as types
@@ -9,76 +8,30 @@ from langchain_core.messages import content as types
 WARNED = False
 
 
-def _convert_to_v1_from_vertex_input(
-    content: list[types.ContentBlock],
-) -> list[types.ContentBlock]:
-    """Attempt to unpack non-standard blocks."""
-
-    def _iter_blocks() -> Iterable[types.ContentBlock]:
-        blocks: list[dict[str, Any]] = [
-            cast("dict[str, Any]", block)
-            if block.get("type") != "non_standard"
-            else block["value"]  # type: ignore[typeddict-item]  # this is only non-standard blocks
-            for block in content
-        ]
-        for block in blocks:
-            num_keys = len(block)
-
-            if num_keys == 1 and (text := block.get("text")):
-                # This is probably a TextContentBlock
-                yield {"type": "text", "text": text}
-
-            elif (
-                num_keys == 1
-                and (document := block.get("document"))
-                and isinstance(document, dict)
-                and "format" in document
-            ):
-                # Probably a document of some kind - TODO
-                yield {"type": "non_standard", "value": block}
-
-            elif (
-                num_keys == 1
-                and (image := block.get("image"))
-                and isinstance(image, dict)
-                and "format" in image
-            ):
-                # Probably an image of some kind - TODO
-                yield {"type": "non_standard", "value": block}
-
-            elif block.get("type") in types.KNOWN_BLOCK_TYPES:
-                # We see a standard block type, so we just cast it, even if
-                # we don't fully understand it. This may be dangerous, but
-                # it's better than losing information.
-                yield cast("types.ContentBlock", block)
-
-            else:
-                # We don't understand this block at all.
-                yield {"type": "non_standard", "value": block}
-
-    return list(_iter_blocks())
-
-
-def _convert_to_v1_from_vertex(message: AIMessage) -> list[types.ContentBlock]:
-    """Convert Google (VertexAI) input message to v1 format.
-
-    Args:
-        message: The input message in Google (VertexAI) format.
-
-    Returns:
-        List of content blocks in v1 format.
-    """
-    return message  # type: ignore[return-value]
-
-
-def translate_content(message: AIMessage) -> list[types.ContentBlock]:
+def translate_content(message: AIMessage) -> list[types.ContentBlock]:  # noqa: ARG001
     """Derive standard content blocks from a message with Google (VertexAI) content."""
-    return _convert_to_v1_from_vertex(message)
+    global WARNED  # noqa: PLW0603
+    if not WARNED:
+        warning_message = (
+            "Content block standardization is not yet fully supported for Google "
+            "VertexAI."
+        )
+        warnings.warn(warning_message, stacklevel=2)
+        WARNED = True
+    raise NotImplementedError
 
 
-def translate_content_chunk(message: AIMessageChunk) -> list[types.ContentBlock]:
+def translate_content_chunk(message: AIMessageChunk) -> list[types.ContentBlock]:  # noqa: ARG001
     """Derive standard content blocks from a chunk with Google (VertexAI) content."""
-    return _convert_to_v1_from_vertex(message)
+    global WARNED  # noqa: PLW0603
+    if not WARNED:
+        warning_message = (
+            "Content block standardization is not yet fully supported for Google "
+            "VertexAI."
+        )
+        warnings.warn(warning_message, stacklevel=2)
+        WARNED = True
+    raise NotImplementedError
 
 
 def _register_google_vertexai_translator() -> None:
