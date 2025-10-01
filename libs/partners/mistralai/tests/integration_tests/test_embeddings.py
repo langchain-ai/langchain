f7cef24449f25781b8ad5cb11a48178993f08d27
noeliecherrier <noelie@hellocarbo.com>
2025-10-01 16:25:04 +0200

    fix(mistralai): aembed_documents fails in case of HTTP error

--- libs/partners/mistralai/tests/integration_tests/test_embeddings.py
+++ libs/partners/mistralai/tests/integration_tests/test_embeddings.py
@@ -1,5 +1,11 @@
 """Test MistralAI Embedding."""
 
+from unittest.mock import patch
+
+import httpx
+import pytest
+import tenacity
+
 from langchain_mistralai import MistralAIEmbeddings
 
 
@@ -27,6 +33,21 @@ async def test_mistralai_embedding_documents_async() -> None:
     output = await embedding.aembed_documents(documents)
     assert len(output) == 2
     assert len(output[0]) == 1024
+
+
+async def test_mistralai_embedding_documents_http_error_async() -> None:
+    """Test MistralAI embeddings for documents."""
+    documents = ["foo bar", "test document"]
+    embedding = MistralAIEmbeddings(max_retries=0)
+    mock_response = httpx.Response(
+        status_code=400,
+        request=httpx.Request("POST", url=embedding.async_client.base_url),
+    )
+    with (
+        patch.object(embedding.async_client, "post", return_value=mock_response),
+        pytest.raises(tenacity.RetryError),
+    ):
+        await embedding.aembed_documents(documents)
 
 
 async def test_mistralai_embedding_query_async() -> None:
