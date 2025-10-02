d7cce2f469a0272f1ef3cdaee4a732ef9e76fec1
ccurme <chester.curme@gmail.com>
2025-10-02 10:35:00 -0400

    feat(langchain_v1): update messages namespace (#33207)

--- libs/langchain_v1/langchain/messages/__init__.py
+++ libs/langchain_v1/langchain/messages/__init__.py
@@ -3,15 +3,27 @@
 from langchain_core.messages import (
     AIMessage,
     AIMessageChunk,
+    AnyMessage,
     HumanMessage,
+    InvalidToolCall,
+    MessageLikeRepresentation,
     SystemMessage,
+    ToolCall,
+    ToolCallChunk,
     ToolMessage,
+    trim_messages,
 )
 
 __all__ = [
     "AIMessage",
     "AIMessageChunk",
+    "AnyMessage",
     "HumanMessage",
+    "InvalidToolCall",
+    "MessageLikeRepresentation",
     "SystemMessage",
+    "ToolCall",
+    "ToolCallChunk",
     "ToolMessage",
+    "trim_messages",
 ]
