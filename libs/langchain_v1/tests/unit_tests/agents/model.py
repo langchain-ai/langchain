e529445b29ca8872fdae0075bece74084c2eaee6
Sydney Runkle <sydneymarierunkle@gmail.com>
2025-10-03 11:12:57 -0400

    no printing

--- libs/langchain_v1/tests/unit_tests/agents/model.py
+++ libs/langchain_v1/tests/unit_tests/agents/model.py
@@ -58,7 +58,6 @@ class FakeToolCallingModel(BaseChatModel, Generic[StructuredResponseT]):
                 content_obj = asdict(self.structured_response)
             elif isinstance(self.structured_response, dict):
                 content_obj = self.structured_response
-            print(content_obj)
             message = AIMessage(content=json.dumps(content_obj), id=str(self.index))
         else:
             messages_string = "-".join([m.content for m in messages])
