d3fe0655dc8d9672725e61ffc85640ce21d1b3ce
thdev01 <thdev01@gmail.com>
2025-10-03 12:47:11 -0300

    style(langchain): normalize sentence spacing in langchain library

--- libs/langchain/langchain/chains/llm_summarization_checker/__init__.py
+++ libs/langchain/langchain/chains/llm_summarization_checker/__init__.py
@@ -2,6 +2,6 @@
 
 Chain that tries to verify the accuracy of text generation by splitting it into a
 list of facts, then checking if those facts are true or not, and rewriting
-the text to make it more truth-ful.  It will repeat this loop until it hits `max_tries`
+the text to make it more truth-ful. It will repeat this loop until it hits `max_tries`
 or gets to a "true" output.
 """
