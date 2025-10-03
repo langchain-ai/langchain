1074ce5fe522de87c7819fc5b54244a5c8b3ab4e
Eugene Yurtsev <eyurtsev@gmail.com>
2025-10-03 10:57:54 -0400

    chore(langchain_v1)!: Remove ToolNode from agents (#33250)
    Remove ToolNode from agents namespace. It should only be present in tools

--- libs/langchain_v1/langchain/agents/__init__.py
+++ libs/langchain_v1/langchain/agents/__init__.py
@@ -1,10 +1,8 @@
 """langgraph.prebuilt exposes a higher-level API for creating and executing agents and tools."""
 
 from langchain.agents.react_agent import AgentState, create_agent
-from langchain.tools import ToolNode
 
 __all__ = [
     "AgentState",
-    "ToolNode",
     "create_agent",
 ]
