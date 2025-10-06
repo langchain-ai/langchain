99d8504731c558041270053a9402d5cde9706f70
Mason Daugherty <mason@langchain.dev>
2025-10-05 22:40:34 -0400

    chore(core): docstring nits (#33285)

--- libs/core/langchain_core/_api/beta_decorator.py
+++ libs/core/langchain_core/_api/beta_decorator.py
@@ -6,8 +6,8 @@ https://github.com/matplotlib/matplotlib/blob/main/lib/matplotlib/_api/deprecati
 
 !!! warning
 
-    This module is for internal use only.  Do not use it in your own code.
-    We may change the API at any time with no warning.
+    This module is for internal use only. Do not use it in your own code. We may change
+    the API at any time with no warning.
 """
 
 import contextlib
