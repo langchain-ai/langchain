5e038274a87a18fa69998f708735ad973b298003
ads2280 <anika.somaia@langchain.dev>
2025-10-07 17:39:57 -0700

    skip pydantic<2.10

--- libs/core/tests/unit_tests/prompts/test_string.py
+++ libs/core/tests/unit_tests/prompts/test_string.py
@@ -1,6 +1,19 @@
+import pytest
+from packaging import version
+
 from langchain_core.prompts.string import mustache_schema
+from langchain_core.utils.pydantic import PYDANTIC_VERSION
+
+PYDANTIC_VERSION_AT_LEAST_210 = version.parse("2.10") <= PYDANTIC_VERSION
 
 
+@pytest.mark.skipif(
+    PYDANTIC_VERSION_AT_LEAST_210,
+    reason=(
+        "Only test with most recent version of pydantic. "
+        "Pydantic introduced small fixes to generated JSONSchema on minor versions."
+    ),
+)
 def test_mustache_schema_parent_child() -> None:
     template = "{{x.y}} {{x}}"
     expected = {
