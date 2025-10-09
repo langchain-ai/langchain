0e80291804f0746ff6616675d9b597822c41ff3c
Anika <anika.somaia@columbia.edu>
2025-10-09 15:45:32 -0700

    fix(core): handle parent/child mustache vars (#33345)
    **Description:**
    
    currently `mustache_schema("{{x.y}} {{x}}")` will error. pr fixes
    
    **Issue:** na
    **Dependencies:**na
    
    ---------
    
    Co-authored-by: Bagatur <baskaryan@gmail.com>

---
+++ libs/core/tests/unit_tests/prompts/test_string.py
@@ -0,0 +1,32 @@
+import pytest
+from packaging import version
+
+from langchain_core.prompts.string import mustache_schema
+from langchain_core.utils.pydantic import PYDANTIC_VERSION
+
+PYDANTIC_VERSION_AT_LEAST_29 = version.parse("2.9") <= PYDANTIC_VERSION
+
+
+@pytest.mark.skipif(
+    not PYDANTIC_VERSION_AT_LEAST_29,
+    reason=(
+        "Only test with most recent version of pydantic. "
+        "Pydantic introduced small fixes to generated JSONSchema on minor versions."
+    ),
+)
+def test_mustache_schema_parent_child() -> None:
+    template = "{{x.y}} {{x}}"
+    expected = {
+        "$defs": {
+            "x": {
+                "properties": {"y": {"default": None, "title": "Y", "type": "string"}},
+                "title": "x",
+                "type": "object",
+            }
+        },
+        "properties": {"x": {"$ref": "#/$defs/x", "default": None}},
+        "title": "PromptInput",
+        "type": "object",
+    }
+    actual = mustache_schema(template).model_json_schema()
+    assert expected == actual
