c5b29610d7fe2d70ce69c6152264ebbe2e78f82a
Eugene Yurtsev <eyurtsev@gmail.com>
2025-10-09 21:37:42 -0400

    Apply suggestion from @eyurtsev

--- libs/langchain_v1/langchain/agents/middleware/__init__.py
+++ libs/langchain_v1/langchain/agents/middleware/__init__.py
@@ -39,6 +39,7 @@ __all__ = [
     "ContextEditingMiddleware",
     "HumanInTheLoopMiddleware",
     "InterruptOnConfig",
+    "LLMToolEmulator",
     "LLMToolSelectorMiddleware",
     "ModelCallLimitMiddleware",
     "ModelFallbackMiddleware",
