c97e649bd3147d072282b3a7de4b7a41549356c6
Sydney Runkle <sydneymarierunkle@gmail.com>
2025-10-16 12:44:44 -0400

    linting, ofc

--- libs/langchain_v1/langchain/agents/factory.py
+++ libs/langchain_v1/langchain/agents/factory.py
@@ -958,9 +958,7 @@ def create_agent(  # noqa: PLR0915
             # Use provider-specific structured output
             kwargs = effective_response_format.to_model_kwargs(model=request.model)
             return (
-                request.model.bind_tools(
-                    final_tools, **kwargs, **request.model_settings
-                ),
+                request.model.bind_tools(final_tools, **kwargs, **request.model_settings),
                 effective_response_format,
             )
 
