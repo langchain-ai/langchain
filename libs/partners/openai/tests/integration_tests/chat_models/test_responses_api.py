4c2a93176a8b957b5128eacf3639b2222f07245f
Chester Curme <chester.curme@gmail.com>
2025-10-24 10:52:10 -0400

    x

--- libs/partners/openai/tests/integration_tests/chat_models/test_responses_api.py
+++ libs/partners/openai/tests/integration_tests/chat_models/test_responses_api.py
@@ -254,7 +254,7 @@ def test_parsed_strict() -> None:
 
     schema = _convert_to_openai_response_format(Joke)
     invalid_schema = cast(dict, _convert_to_openai_response_format(Joke, strict=True))
-    invalid_schema["json_schema"]["schema"]["required"] = ["setup"]
+    invalid_schema["json_schema"]["schema"]["required"] = ["setup"]  # make invalid
 
     # Test not strict
     response = llm.invoke("Tell me a joke", response_format=schema)
