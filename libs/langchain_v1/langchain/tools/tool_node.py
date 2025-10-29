19e6cb1203c6e6266b64241dcb01839f528a6760
Sydney Runkle <sydneymarierunkle@gmail.com>
2025-10-29 10:49:29 -0700

    backwards compat

--- libs/langchain_v1/langchain/tools/tool_node.py
+++ libs/langchain_v1/langchain/tools/tool_node.py
@@ -1,9 +1,14 @@
 """Utils file included for backwards compat imports."""
 
 from langgraph.prebuilt import InjectedState, InjectedStore, ToolRuntime
+from langgraph.prebuilt.tool_node import ToolCallWithContext, ToolNode as _ToolNode, ToolCallRequest, ToolCallWrapper
 
 __all__ = [
     "InjectedState",
     "InjectedStore",
     "ToolRuntime",
+    "ToolCallWithContext",
+    "ToolNode",
+    "ToolCallRequest",
+    "ToolCallWrapper",
 ]
