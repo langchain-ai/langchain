493be259c3609b6b950636bc394317272bc96bc7
ccurme <chester.curme@gmail.com>
2025-10-31 09:44:46 -0400

    feat(core): mint langchain-model-profiles and add profile property to `BaseChatModel` (#33728)

---
+++ libs/model-profiles/tests/unit_tests/test_chat_model.py
@@ -0,0 +1,18 @@
+"""End to end test for fetching model profiles from a chat model."""
+
+import pytest
+from langchain.chat_models import init_chat_model
+
+
+def test_chat_model() -> None:
+    """Test that chat model gets profile data correctly."""
+    model = init_chat_model("openai:gpt-5", api_key="foo")
+    assert model.profile
+    assert model.profile["max_input_tokens"] == 400000
+    assert model.profile["structured_output"]
+
+
+def test_chat_model_no_data(monkeypatch: pytest.MonkeyPatch) -> None:
+    """Test that chat model handles missing profile data."""
+    model = init_chat_model("openai:gpt-fake", api_key="foo")
+    assert model.profile == {}
