3dfea96ec1d2dac4e506d287860ee943c183c9f1
Mason Daugherty <mason@langchain.dev>
2025-11-10 22:51:35 -0500

    chore: update `README.md` files (#33919)

---
+++ libs/partners/ollama/tests/__init__.py
@@ -0,0 +1,12 @@
+# Running Tests
+#
+# To run integration tests (`make integration_tests`), you will need the following
+# models installed in your Ollama server:
+#
+# - `llama3.1`
+# - `deepseek-r1:1.5b`
+# - `gpt-oss:20b`
+#
+# Install these models by running:
+#
+# ollama pull <name-of-model>
