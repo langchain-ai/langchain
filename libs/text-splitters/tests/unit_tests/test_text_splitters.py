9f181b7e777f3d346fb01be1b00d1e4ef55dd0b9
Emmanuel-Arokiaraj <aemmanuel685210@gmail.com>
2025-11-23 21:22:49 +0530

    reduced the test script lines to less than 88

--- libs/text-splitters/tests/unit_tests/test_text_splitters.py
+++ libs/text-splitters/tests/unit_tests/test_text_splitters.py
@@ -1168,31 +1168,31 @@ def test_mysql_query_splits() -> None:
         Document(
             page_content=(
                 "CREATE TABLE users (\n"
-                "        id INT AUTO_INCREMENT PRIMARY KEY,\n"
-                "        username VARCHAR(50) NOT NULL,\n"
-                "        email VARCHAR(100) NOT NULL\n"
-                "    );"
+                "    id INT AUTO_INCREMENT PRIMARY KEY,\n"
+                "    username VARCHAR(50) NOT NULL,\n"
+                "    email VARCHAR(100) NOT NULL\n"
+                ");"
             ),
             metadata={"source": "source-1"},
         ),
         Document(
             page_content=(
                 "CREATE PROCEDURE GetUser(IN userId INT)\n"
-                "    BEGIN\n"
-                "        SELECT * FROM users WHERE id = userId;\n"
-                "    END;\n\n"
-                "    INSERT INTO users (username, email) VALUES ('testuser', 'test@example.com');\n\n"
-                "    DELIMITER //\n"
-                "    CREATE TRIGGER before_insert_users\n"
-                "    BEFORE INSERT ON users\n"
-                "    FOR EACH ROW\n"
-                "    BEGIN\n"
-                "        IF NEW.username IS NULL THEN\n"
-                "            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Username cannot be null';\n"
-                "        END IF;\n"
-                "    END;\n"
-                "    //\n"
-                "    DELIMITER ;"
+                "BEGIN\n"
+                "    SELECT * FROM users WHERE id = userId;\n"
+                "END;\n\n"
+                "INSERT INTO users (username, email) VALUES ('testuser', 'test@example.com');\n\n"
+                "DELIMITER //\n"
+                "CREATE TRIGGER before_insert_users\n"
+                "BEFORE INSERT ON users\n"
+                "FOR EACH ROW\n"
+                "BEGIN\n"
+                "    IF NEW.username IS NULL THEN\n"
+                "        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Username cannot be null';\n"
+                "    END IF;\n"
+                "END;\n"
+                "//\n"
+                "DELIMITER ;"
             ),
             metadata={"source": "source-1"},
         ),
