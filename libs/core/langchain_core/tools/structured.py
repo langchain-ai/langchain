9a976d510dd993398a7c08428a96add808a003c6
PrinceThummar011 <princethummar011@gmail.com>
2025-12-12 22:32:14 +0530

    fix: format code to resolve linting error

--- libs/core/langchain_core/tools/structured.py
+++ libs/core/langchain_core/tools/structured.py
@@ -204,7 +204,10 @@ class StructuredTool(BaseTool):
             )
         description_ = description
         if description is None and not parse_docstring:
-            if isinstance(source_function, type) and "__doc__" not in source_function.__dict__:
+            if (
+                isinstance(source_function, type)
+                and "__doc__" not in source_function.__dict__
+            ):
                 description_ = ""
             else:
                 description_ = source_function.__doc__ or None
