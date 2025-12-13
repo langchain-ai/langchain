19e337a6da7a1134caf4c5e5732d7d25255919d9
isatyamks <isatyamks@gmail.com>
2025-12-13 14:16:13 +0530

    style(tests): format assertion for verbose mode in test_pretty_repr_verbose_mode

--- libs/core/tests/unit_tests/test_messages.py
+++ libs/core/tests/unit_tests/test_messages.py
@@ -1515,6 +1515,9 @@ def test_pretty_repr_verbose_mode() -> None:
     result_verbose = msg.pretty_repr(verbose=True)
     result_normal = msg.pretty_repr(verbose=False)
     # Verbose mode should include more details
-    assert "data: detailed information" in result_verbose or "count: 5" in result_verbose
+    assert (
+        "data: detailed information" in result_verbose
+        or "count: 5" in result_verbose
+    )
     # Normal mode should be more concise
     assert "Server Tool Result" in result_normal
