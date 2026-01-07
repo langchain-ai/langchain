244f020a71b437930bac9e327512e39bf9990141
Chester Curme <chester.curme@gmail.com>
2026-01-07 09:54:33 -0500

    check matching model provider

--- libs/langchain_v1/langchain/agents/middleware/summarization.py
+++ libs/langchain_v1/langchain/agents/middleware/summarization.py
@@ -333,7 +333,8 @@ class SummarizationMiddleware(AgentMiddleware):
             and last_ai_message.usage_metadata is not None
             and (reported_tokens := last_ai_message.usage_metadata.get("total_tokens", -1))
             and reported_tokens >= threshold
-            # add check for same provider as self.model
+            and (message_provider := last_ai_message.response_metadata.get("model_provider"))
+            and message_provider == self.model._get_ls_params().get("ls_provider")  # noqa: SLF001
         ):
             return True
         return False
