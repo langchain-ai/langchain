40b9d8653f9cfbbb1a1d9918605a5b0b46e04ecc
北渚 <worship76@foxmail.com>
2026-01-14 15:01:15 +0800

    fix(openai): add reasoning_content for stream&astream funciton

--- libs/partners/openai/langchain_openai/chat_models/base.py
+++ libs/partners/openai/langchain_openai/chat_models/base.py
@@ -373,6 +373,8 @@ def _convert_delta_to_message_chunk(
     role = cast(str, _dict.get("role"))
     content = cast(str, _dict.get("content") or "")
     additional_kwargs: dict = {}
+    if _dict.get("reasoning_content"):
+        additional_kwargs["reasoning_content"] = _dict["reasoning_content"]
     if _dict.get("function_call"):
         function_call = dict(_dict["function_call"])
         if "name" in function_call and function_call["name"] is None:
