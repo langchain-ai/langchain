15030d4c42440da502a5e581edd04d33db1b3beb
Mason Daugherty <github@mdrxy.com>
2026-04-06 15:15:07 -0400

    nit

--- libs/partners/ollama/langchain_ollama/chat_models.py
+++ libs/partners/ollama/langchain_ollama/chat_models.py
@@ -630,9 +630,11 @@ class ChatOllama(BaseChatModel):
     """Whether to return logprobs."""
 
     top_logprobs: int | None = None
-    """Number of most likely tokens to return at each token position, each with an
-    associated log probability. `logprobs` must be set to true if this parameter is
-    used."""
+    """Number of most likely tokens to return at each token position, each with
+    an associated log probability.
+
+    `logprobs` must be set to true if this parameter is used.
+    """
 
     stop: list[str] | None = None
     """Sets the stop tokens to use."""
