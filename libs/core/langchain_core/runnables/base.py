c94673f8cb9c5647dd4adb2cc7102541efb749aa
Sateesh <sateeshkumarb@yahoo.com>
2026-06-10 15:24:15 +0530

    Fix typo in documentation for RunnableLambda
    Fixes the issue https://github.com/langchain-ai/docs/issues/4387

--- libs/core/langchain_core/runnables/base.py
+++ libs/core/langchain_core/runnables/base.py
@@ -4610,13 +4610,11 @@ class RunnableLambda(Runnable[Input, Output]):
         await runnable.ainvoke(1)  # returns 2
         await runnable.abatch([1, 2, 3])  # returns [2, 3, 4]
 
-
-        # Alternatively, can provide both synd and sync implementations
         async def add_one_async(x: int) -> int:
             return x + 1
 
-
-        runnable = RunnableLambda(add_one, afunc=add_one_async)
+        # Alternatively, can provide both sync and async implementations
+        runnable = RunnableLambda(func=add_one, afunc=add_one_async)
         runnable.invoke(1)  # Uses add_one
         await runnable.ainvoke(1)  # Uses add_one_async
         ```
