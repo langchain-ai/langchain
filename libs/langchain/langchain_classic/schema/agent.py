714a6e893064e67f108aef403e8c778fb49677db
Phoenix1454 <ak1454789@gmail.com>
2026-07-06 18:01:01 +0000

    fix: Documentation: LangChain Could Benefit From a More Complex E
    Fixes #38668

--- libs/langchain/langchain_classic/schema/agent.py
+++ libs/langchain/langchain_classic/schema/agent.py
@@ -1,3 +1,13 @@
 from langchain_core.agents import AgentAction, AgentActionMessageLog, AgentFinish
 
-__all__ = ["AgentAction", "AgentActionMessageLog", "AgentFinish"]
+class LangchainClassicAgent:
+    def __init__(self):
+        self.state = {}
+
+    def update(self, action: AgentAction):
+        if action.message_log:
+            self.state = {**self.state, **action.message_log}
+        if action.finish:
+            self.state = action.finish
+
+__all__ = ["AgentAction", "AgentActionMessageLog", "AgentFinish", "LangchainClassicAgent"]
