1619f3d3d07365d2cb646a7629b18aaa5b06361f
Christophe Bornet <cbornet@hotmail.com>
2026-07-08 21:32:47 +0200

    chore(standard-tests): fix some typings detected by ty (#38707)

--- libs/standard-tests/tests/unit_tests/test_basic_retriever.py
+++ libs/standard-tests/tests/unit_tests/test_basic_retriever.py
@@ -2,6 +2,7 @@ from typing import Any
 
 from langchain_core.documents import Document
 from langchain_core.retrievers import BaseRetriever
+from typing_extensions import override
 
 from langchain_tests.integration_tests import RetrieversIntegrationTests
 
@@ -10,20 +11,24 @@ class ParrotRetriever(BaseRetriever):
     parrot_name: str
     k: int = 3
 
+    @override
     def _get_relevant_documents(self, query: str, **kwargs: Any) -> list[Document]:
         k = kwargs.get("k", self.k)
         return [Document(page_content=f"{self.parrot_name} says: {query}")] * k
 
 
 class TestParrotRetrieverIntegration(RetrieversIntegrationTests):
+    @override
     @property
     def retriever_constructor(self) -> type[ParrotRetriever]:
         return ParrotRetriever
 
+    @override
     @property
     def retriever_constructor_params(self) -> dict[str, Any]:
         return {"parrot_name": "Polly"}
 
+    @override
     @property
     def retriever_query_example(self) -> str:
         return "parrot"
