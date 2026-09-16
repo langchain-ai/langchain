6758304de097cc68c1c6da5e0dc87b5ba021af5f
Chester Curme <chester.curme@gmail.com>
2026-09-16 12:12:43 -0400

    lint

--- libs/langchain_v1/langchain/agents/factory.py
+++ libs/langchain_v1/langchain/agents/factory.py
@@ -2024,7 +2024,7 @@ def _make_tools_to_model_edge(
         executed_names = [
             name
             for c in last_ai_message.tool_calls
-            if (name := executed_by_id.get(c["id"], c["name"])) in tool_node.tools_by_name
+            if (name := executed_by_id.get(c["id"] or "", c["name"])) in tool_node.tools_by_name
         ]
         if executed_names and all(
             tool_node.tools_by_name[name].return_direct for name in executed_names
